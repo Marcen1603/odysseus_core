8e10bfe3345350ebc92275775c6ac606d2d5f370
Jonas Jacobi <jacobi@uni-oldenburg.de>
2009-08-27 11:00:47 +0000

    ASSIGNED - # 70: Zu benutzende Regeln beim Rewriting konfigurierbar machen http://isdb1.offis.uni-oldenburg.de:8000/odysseus/ticket/70
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3364 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 19b9f2716ea118fb44746a726b2fb10c7faefdf8

--- base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/base/IRewrite.java
+++ base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/base/IRewrite.java
@@ -1,6 +1,10 @@
 package de.uniol.inf.is.odysseus.base;
 
+import java.util.Set;
+
 
 public interface IRewrite {
 	public ILogicalOperator rewritePlan(ILogicalOperator plan);
+
+	public ILogicalOperator rewritePlan(ILogicalOperator plan, Set<String> rulesToApply);
 }
--- base/de.uniol.inf.is.odysseus.rewrite.drools/src/de/uniol/inf/is/odysseus/rewrite/drools/DroolsRewrite.java
+++ base/de.uniol.inf.is.odysseus.rewrite.drools/src/de/uniol/inf/is/odysseus/rewrite/drools/DroolsRewrite.java
@@ -1,8 +1,17 @@
 package de.uniol.inf.is.odysseus.rewrite.drools;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import org.drools.RuleBase;
 import org.drools.StatefulSession;
 import org.drools.agent.RuleAgent;
+import org.drools.rule.Package;
+import org.drools.rule.Rule;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
@@ -22,23 +31,24 @@ import de.uniol.inf.is.odysseus.util.AbstractTreeWalker;
  * '/resources/rewrite/rules/ruleflow.rf'. Failing to load rule files (e.g.
  * because of erroneous rules) will trigger a warning in the log.
  * 
- * @todo TODO konfigurierbar machen, welche regeln ausgefuehrt werden sollen,
- *       evtl auch ueber ruleflows ..
  * @author Jonas Jacobi
  */
 public class DroolsRewrite implements IRewrite {
 
 	private static final String RULE_PATH = "/resources/rewrite/rules";
 	private static final String LOGGER_NAME = "rewrite";
-	private RuleBase rulebase;
+	public static final String PACKAGE_NAME = "de.uniol.inf.is.odysseus.transformation.drools";
+	public RuleBase rulebase;
+	public ReadWriteLock ruleBaseLock = new ReentrantReadWriteLock();
 	private static Logger logger = LoggerFactory.getLogger(LOGGER_NAME);
 	private BundleContext bundleContext;
+	public List<Rule> removedRules = new LinkedList<Rule>();
+	private Set<String> ruleNames;
 
 	public DroolsRewrite() {
 	}
 
-	@Override
-	public ILogicalOperator rewritePlan(ILogicalOperator plan) {
+	public ILogicalOperator rewritePlanInternal(ILogicalOperator plan) {
 		StatefulSession session = rulebase.newStatefulSession();
 		TopAO top = new TopAO();
 		top.setInputAO(0, plan);
@@ -62,6 +72,36 @@ public class DroolsRewrite implements IRewrite {
 		return top.getInputAO();
 	}
 
+	@Override
+	public ILogicalOperator rewritePlan(ILogicalOperator plan,
+			Set<String> rulesToApply) {
+		ruleBaseLock.writeLock().lock();
+		try {
+			Package pkg = this.rulebase.getPackage(PACKAGE_NAME);
+			resetRules(pkg);
+			for (Rule rule : pkg.getRules()) {
+				if (!rulesToApply.contains(rule.getName())) {
+					pkg.removeRule(rule);
+					this.removedRules.add(rule);
+				}
+			}
+			return rewritePlanInternal(plan);
+		} finally {
+			ruleBaseLock.writeLock().unlock();
+		}
+	}
+
+	public void resetRules(Package pkg) {
+		for (Rule rule : removedRules) {
+			pkg.addRule(rule);
+		}
+		removedRules.clear();
+	}
+
+	public Set<String> getRules() {
+		return this.ruleNames;
+	}
+
 	private static void addLogicalOperatorToSession(StatefulSession session,
 			ILogicalOperator op) {
 		if (op == null) {
@@ -78,6 +118,41 @@ public class DroolsRewrite implements IRewrite {
 		this.bundleContext = context.getBundleContext();
 		RuleAgent ra = RuleAgentFactory.createRuleAgent(this.bundleContext,
 				RULE_PATH, LOGGER_NAME);
-		this.rulebase = ra.getRuleBase();
+		this.ruleBaseLock.readLock().lock();
+		try {
+			this.rulebase = ra.getRuleBase();
+			this.ruleNames = new HashSet<String>();
+			Package pkg = this.rulebase.getPackage(PACKAGE_NAME);
+			for (Rule rule : pkg.getRules()) {
+				this.ruleNames.add(rule.getName());
+			}
+		} finally {
+			this.ruleBaseLock.readLock().unlock();
+		}
+	}
+
+	/*
+	 * The locking mechanism allows multiple parallel calls to this method,
+	 * but only one call at a time to rewritePlan(ILogicalOperator, Set<String>)
+	 * as those use different rules and modify the singular rulebase.
+	 */
+	@Override
+	public ILogicalOperator rewritePlan(ILogicalOperator plan) {
+		try {
+			this.ruleBaseLock.writeLock().lock();
+			try {
+				Package pkg = this.rulebase.getPackage(PACKAGE_NAME);
+				for (Rule rule : this.removedRules) {
+					pkg.addRule(rule);
+				}
+				this.removedRules.clear();
+			} finally {
+				this.ruleBaseLock.readLock().lock();
+				this.ruleBaseLock.writeLock().unlock();
+			}
+			return rewritePlanInternal(plan);
+		} finally {
+			this.ruleBaseLock.readLock().unlock();
+		}
 	}
 }
