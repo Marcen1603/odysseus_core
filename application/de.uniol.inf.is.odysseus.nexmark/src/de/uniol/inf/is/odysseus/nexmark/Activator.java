5339ba6ab0957e88e60d1c91843d3107b8fa33c1
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2009-09-03 13:41:38 +0000

    Nexmark-Server als OSGi-Anwendung (Umgebungsvariable pr wird ausgelesen)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3405 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0ba03e3a83fa51572853b0e7772be12d577d49b7

--- application/de.uniol.inf.is.odysseus.nexmark/src/de/uniol/inf/is/odysseus/nexmark/Activator.java
+++ application/de.uniol.inf.is.odysseus.nexmark/src/de/uniol/inf/is/odysseus/nexmark/Activator.java
@@ -2,14 +2,30 @@ package de.uniol.inf.is.odysseus.nexmark;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.nexmark.simulation.NexmarkServer;
 
 public class Activator implements BundleActivator {
-
+	private static final Logger logger = LoggerFactory.getLogger( BundleActivator.class );
+	
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
+		logger.debug("NexMark started ");
+		// Anm. im Moment nur ein einfacher Parametersatz
+		
+		String[] args = new String[3];
+		args[0] = "-pr"; 
+		args[1] = System.getenv("pr");
+		args[2] = "-useNIO";
+		if (args[1] != null){
+			NexmarkServer.main(args);
+		}
+		
 	}
 
 	/*
