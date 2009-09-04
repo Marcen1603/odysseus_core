bb2660092874c37943e1b224f235c113eb56d1c0
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2009-09-04 07:19:44 +0000

    Default Functions load
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3412 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 99d35906738bf4f0dd0560283e7c73379b7a3331

--- base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/base/Activator.java
+++ base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/base/Activator.java
@@ -3,6 +3,12 @@ package de.uniol.inf.is.odysseus.base;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.function.DolToEur;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.function.Now;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.function.ToNumber;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.function.ToString;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFExpression;
+
 public class Activator implements BundleActivator {
 
 	/*
@@ -10,6 +16,11 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
+		// Add default Functions
+		SDFExpression.addFunction(new DolToEur());
+		SDFExpression.addFunction(new Now());
+		SDFExpression.addFunction(new ToNumber());
+		SDFExpression.addFunction(new ToString());
 	}
 
 	/*
