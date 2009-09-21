278c32a51107706e488c73affcf3b2f45962f856
Jonas Jacobi <jacobi@uni-oldenburg.de>
2009-09-21 12:17:15 +0000

    compilefix -> ableiten von IMetaAttribute statt iclone
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3463 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 221d0500a03a529b3119925fc7f53bcdf13a7db0

--- metadata/de.uniol.inf.is.odysseus.latency/src/de/uniol/inf/is/odysseus/latency/ILatency.java
+++ metadata/de.uniol.inf.is.odysseus.latency/src/de/uniol/inf/is/odysseus/latency/ILatency.java
@@ -1,8 +1,8 @@
 package de.uniol.inf.is.odysseus.latency;
 
-import de.uniol.inf.is.odysseus.base.IClone;
+import de.uniol.inf.is.odysseus.base.IMetaAttribute;
 
-public interface ILatency extends IClone{
+public interface ILatency extends IMetaAttribute{
 	public void setLatencyStart(long timestamp);
 	public void setLatencyEnd(long timestamp);
 	public long getLatencyStart();
