f1c2cc15c071b66f75eae26dc6000216aafab74c
Jan Steinke <jsteinke@uni-oldenburg.de>
2009-09-22 12:56:35 +0000

    Bufferplacement-Strategie für Zwischenpuffer, die Punctuations beachten sollen
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3476 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 013dc4b397049b8a8d7a8df722bc3dcbae12961c

---
+++ metadata/priority/de.uniol.inf.is.odysseus.priority/src/de/uniol/inf/is/odysseus/priority/punctuation/PunctuationBufferPlacementStrategy.java
@@ -0,0 +1,41 @@
+package de.uniol.inf.is.odysseus.priority.punctuation;
+
+import java.util.List;
+
+import org.osgi.service.component.ComponentContext;
+
+import de.uniol.inf.is.odysseus.physicaloperator.base.IBuffer;
+import de.uniol.inf.is.odysseus.physicaloperator.base.ISink;
+import de.uniol.inf.is.odysseus.physicaloperator.base.ISource;
+import de.uniol.inf.is.odysseus.physicaloperator.base.Subscription;
+import de.uniol.inf.is.odysseus.planmanagement.optimization.bufferplacement.AbstractBufferPlacementStrategy;
+import de.uniol.inf.is.odysseus.priority.buffer.DirectInterlinkBufferedPipe;
+
+/**
+ * This strategy makes usage of direct-link buffers with punctuation extension instead of
+ * normal BufferedPipes.
+ * @author jan
+ *
+ */
+public class PunctuationBufferPlacementStrategy  extends
+		AbstractBufferPlacementStrategy {
+
+	protected void activate(ComponentContext context){
+		super.activate(context);
+	}
+	
+	// add buffer, if we are a binary operator or if the bottom
+	// operator is a binary one
+	protected boolean bufferNeeded(
+			List<? extends Subscription<? extends ISource<?>>> subscriptions,
+			ISink<?> childSink) {
+		return subscriptions.size() > 1
+				|| childSink.getSubscribedTo().size() > 1;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected IBuffer<?> createNewBuffer() {
+		return  new DirectInterlinkBufferedPipe();
+	}
+}
