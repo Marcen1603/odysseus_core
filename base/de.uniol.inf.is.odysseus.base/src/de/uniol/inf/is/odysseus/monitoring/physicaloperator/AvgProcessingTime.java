5723f81f58e11d864093094dcba270d4c8bedb1c
Jonas Jacobi <jacobi@uni-oldenburg.de>
2009-09-24 11:27:28 +0000

    da physische operatoren jetzt in odysseus.base sind, kann auch ihr monitoring hier hin
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3494 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 852b31afc9eb7be3fb1d683f95f34f9b405c727a

---
+++ base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/monitoring/physicaloperator/AvgProcessingTime.java
@@ -0,0 +1,69 @@
+package de.uniol.inf.is.odysseus.monitoring.physicaloperator;
+
+import de.uniol.inf.is.odysseus.monitoring.AbstractMonitoringData;
+import de.uniol.inf.is.odysseus.physicaloperator.base.IObservablePhysicalOperator;
+import de.uniol.inf.is.odysseus.physicaloperator.base.ISource;
+import de.uniol.inf.is.odysseus.physicaloperator.base.event.POEvent;
+import de.uniol.inf.is.odysseus.physicaloperator.base.event.POEventListener;
+import de.uniol.inf.is.odysseus.physicaloperator.base.event.POEventType;
+
+public class AvgProcessingTime extends AbstractMonitoringData<Double> implements
+		POEventListener {
+
+	private long start = 0;
+	private long lastRun = -1;
+	private double runSum = 0;
+	private long runCount = 0;
+
+	public AvgProcessingTime(IObservablePhysicalOperator target) {
+		super(target);
+		target.subscribe(this, POEventType.ProcessInit);
+		target.subscribe(this, POEventType.ProcessDone);
+	}
+
+	public AvgProcessingTime(AvgProcessingTime avgProcessingTime) {
+		super(avgProcessingTime.getTarget());
+		this.start = avgProcessingTime.start;
+		this.lastRun = avgProcessingTime.lastRun;
+		this.runSum = avgProcessingTime.runSum;
+		this.runCount = avgProcessingTime.runCount;
+	}
+
+	@Override
+	public void poEventOccured(POEvent poEvent) {
+		if (poEvent.getPOEventType().equals(POEventType.ProcessInit)) {
+			start = System.nanoTime();
+		} else {
+			lastRun = System.nanoTime() - start;
+			runCount++;
+			runSum += lastRun;
+		}
+	}
+
+	@Override
+	public String getType() {
+		return MonitoringDataTypes.PROCESSING_COST.name;
+	}
+
+	@Override
+	public Double getValue() {
+		if (runCount > 0) {
+			return runSum / runCount;
+		}
+		return null;
+	}
+
+	@Override
+	public void reset() {
+		start = 0;
+		lastRun = -1;
+		runSum = 0;
+		runCount = 0;
+	}
+
+	@Override
+	public AbstractMonitoringData<Double> clone() {
+		return new AvgProcessingTime(this);
+	}
+
+}
