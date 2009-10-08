3a40330b5b932d1838ca17b2b4ccaf06746e9578
Andre Bolles <andre.bolles@offis.de>
2009-10-08 11:47:14 +0000

    LabdataServer Plug-In added
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3569 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: bd0bf2d502fc9fa2206085df442f569d6505d6af

---
+++ application/de.uniol.inf.is.odysseus.labdataserver/src/de/uniol/inf/is/odysseus/labdataserver/Activator.java
@@ -0,0 +1,30 @@
+package de.uniol.inf.is.odysseus.labdataserver;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	static BundleContext context;
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext c) throws Exception {
+		context = c;
+		LabdataServer.main(null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+	}
+	
+	static BundleContext getContext(){
+		return context;
+	}
+
+}
