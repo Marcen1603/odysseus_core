7a8fb68ec8b7bffee48da6491d21250a39b398f0
Jonas Jacobi <jacobi@uni-oldenburg.de>
2009-10-16 14:39:44 +0000

    moeglichkeit hinzugefuegt attribute auch nur uebeer ihren namen zu identifizieren
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3599 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 202f1996e5e8b85b58ec8ef223ca130c072fbcda

--- relational/de.uniol.inf.is.odysseus.parser.cql/src/de/uniol/inf/is/odysseus/parser/cql/parser/transformation/DirectAttributeResolver.java
+++ relational/de.uniol.inf.is.odysseus.parser.cql/src/de/uniol/inf/is/odysseus/parser/cql/parser/transformation/DirectAttributeResolver.java
@@ -1,5 +1,6 @@
 package de.uniol.inf.is.odysseus.parser.cql.parser.transformation;
 
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.CQLAttribute;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.IAttributeResolver;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
@@ -17,12 +18,23 @@ public class DirectAttributeResolver implements IAttributeResolver {
 	}
 
 	public SDFAttribute getAttribute(String name) {
-		for(SDFAttribute attr : schema){
-			if (attr.getURI(false).equals(name)){
-				return attr;
+		String[] parts = name.split("\\.", 2);
+		SDFAttribute found = null;
+		for (SDFAttribute attr : schema) {
+			if (parts.length == 1) {
+				if(((CQLAttribute)attr).getAttributeName().equals(name)){
+					if (found != null) {
+						throw new IllegalArgumentException("amgigious attribute: " + name);
+					}
+					found = attr;
+				}
+			} else {
+				if (attr.getURI(false).equals(name)) {
+					return attr;
+				}
 			}
 		}
-		return null;
+		return found;
 	}
 
 }
