c5ac681120439de48f5225c2d3df9d75e3e13bfb
Jan Steinke <jsteinke@uni-oldenburg.de>
2009-10-19 17:17:59 +0000

    Bevorzugung von Teilplaenen, die Potenzielle Joinpartner enthalten "die zweite". Funktioniert jetzt.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3602 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 13c1205efce9dca22dfa1df5af34d115b8970040

--- scheduler/de.uniol.inf.is.odysseus.scheduler.strategy.factory.aurorafactory/src/de/uniol/inf/is/odysseus/scheduler/strategy/factory/aurorafactory/impl/priority/AbstractPriorityMinLatency.java
+++ scheduler/de.uniol.inf.is.odysseus.scheduler.strategy.factory.aurorafactory/src/de/uniol/inf/is/odysseus/scheduler/strategy/factory/aurorafactory/impl/priority/AbstractPriorityMinLatency.java
@@ -6,20 +6,35 @@ import java.util.LinkedList;
 import java.util.List;
 
 import de.uniol.inf.is.odysseus.base.FESortedPair;
+import de.uniol.inf.is.odysseus.intervalapproach.JoinTIPO;
+import de.uniol.inf.is.odysseus.monitoring.StaticValueMonitoringData;
 import de.uniol.inf.is.odysseus.physicaloperator.base.IIterableSource;
 import de.uniol.inf.is.odysseus.physicaloperator.base.ISource;
 import de.uniol.inf.is.odysseus.physicaloperator.base.plan.IPartialPlan;
-import de.uniol.inf.is.odysseus.scheduler.strategy.AbstractExecListSchedulingStrategy;
-import de.uniol.inf.is.odysseus.intervalapproach.JoinTIPO;
 import de.uniol.inf.is.odysseus.priority.PriorityPO;
+import de.uniol.inf.is.odysseus.scheduler.strategy.AbstractExecListSchedulingStrategy;
 
 public abstract class AbstractPriorityMinLatency extends
 		AbstractExecListSchedulingStrategy {
 
-	// protected List<Integer> joins = new ArrayList<Integer>();
-
 	public AbstractPriorityMinLatency(IPartialPlan plan, boolean useIter) {
 		super(plan, true);
+	}
+
+	private void initMetadata(List<ISource<?>> opPath) {
+		for (ISource<?> s : opPath) {
+			if (!s.providesMonitoringData("estimated productivity")) {
+				s.addMonitoringData("estimated productivity",
+						new StaticValueMonitoringData<Double>(s,
+								"estimated productivity", 1d));
+			}
+
+			if (!s.providesMonitoringData("estimated processing cost")) {
+				s.addMonitoringData("estimated processing cost",
+						new StaticValueMonitoringData<Double>(s,
+								"estimated processing cost", 1d));
+			}
+		}
 	}
 
 	@Override
@@ -50,22 +65,20 @@ public abstract class AbstractPriorityMinLatency extends
 			List<ISource<?>> opPath = new LinkedList<ISource<?>>();
 
 			getPathToRoot(s, schPath, opPath, null);
-
+			initMetadata(opPath);
 			FESortedPair<Double, List<IIterableSource<?>>> pWithCost = new FESortedPair<Double, List<IIterableSource<?>>>(
-					calcPathCost(opPath)	, schPath);
+					calcPathCost(opPath), schPath);
 
 			if (hasToBePrefered(s, opPath, joins)) {
-				pWithCost.setE1(1.0);
+				pWithCost.setE1(-1.0);
 				executePriorisationActivation(s);
 			}
-			
+
 			pathes.add(pWithCost);
 		}
 		// Sort pathes respecting cost
 		Collections.sort(pathes);
 
-
-		
 		// Add Pathes to execList
 		for (FESortedPair<Double, List<IIterableSource<?>>> p : pathes) {
 			execList.addAll(p.getE2());
@@ -74,8 +87,8 @@ public abstract class AbstractPriorityMinLatency extends
 		return execList;
 	}
 
-	private boolean hasToBePrefered(ISource<?> source,
-			List<ISource<?>> opPath, List<Integer> joins) {
+	private boolean hasToBePrefered(ISource<?> source, List<ISource<?>> opPath,
+			List<Integer> joins) {
 
 		boolean priority = false;
 		boolean join = false;
