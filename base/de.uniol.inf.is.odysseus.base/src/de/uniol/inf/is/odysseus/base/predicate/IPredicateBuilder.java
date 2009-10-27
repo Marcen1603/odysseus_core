2e6099d3b0210154a2e144e35a6e270abd4f5edf
Andre Bolles <andre.bolles@offis.de>
2009-10-27 08:53:02 +0000

    removed IPredicateBuilder
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3608 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: bfa086c10447bd895f90bf72cd40ebcc2b789a8c

--- base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/base/predicate/IPredicateBuilder.java
+++
@@ -1,8 +0,0 @@
-package de.uniol.inf.is.odysseus.base.predicate;
-
-import de.uniol.inf.is.odysseus.metadata.base.MetaAttributeContainer;
-import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.IAttributeResolver;
-
-public interface IPredicateBuilder {
-	IPredicate<MetaAttributeContainer<?>> createPredicate(AbstractPredicate predicate, IAttributeResolver resolver);
-}
