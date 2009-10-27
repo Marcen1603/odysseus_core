fae0d96244b7bed88921a88a80c1a05702244061
Andre Bolles <andre.bolles@offis.de>
2009-10-27 13:50:01 +0000

    more exports in the cql parser are necessary
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3617 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: acce3928b76d29b409409989ef300d70d2475a84

--- relational/de.uniol.inf.is.odysseus.parser.cql/src/de/uniol/inf/is/odysseus/parser/cql/parser/transformation/CreatePredicateVisitor.java
+++ relational/de.uniol.inf.is.odysseus.parser.cql/src/de/uniol/inf/is/odysseus/parser/cql/parser/transformation/CreatePredicateVisitor.java
@@ -1,13 +1,18 @@
 package de.uniol.inf.is.odysseus.parser.cql.parser.transformation;
 
+import java.util.ArrayList;
+
 import org.nfunk.jep.ParseException;
 
 import de.uniol.inf.is.odysseus.base.predicate.AndPredicate;
 import de.uniol.inf.is.odysseus.base.predicate.IPredicate;
 import de.uniol.inf.is.odysseus.base.predicate.NotPredicate;
 import de.uniol.inf.is.odysseus.base.predicate.OrPredicate;
+import de.uniol.inf.is.odysseus.objecttracking.predicate.ProbabilityPredicate;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTAndPredicate;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTBasicPredicate;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTIdentifier;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTMatrixExpression;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTNotPredicate;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTOrPredicate;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTPredicate;
@@ -82,12 +87,124 @@ public class CreatePredicateVisitor extends AbstractDefaultVisitor {
 	@Override
 	public Object visit(ASTProbabilityPredicate probPred, Object data) {
 		try {
-//			IPredicateBuilder builder = (IPredicateBuilder) Class
-//					.forName(
-//							"de.uniol.inf.is.querytranslation.parser.transformation.ProbabilityPredicateBuilder")
-//					.newInstance();
-			throw new RuntimeException("ohne predicate builder machen, wenn das paket da ist das nehmen und als optionalen import markieren");
-//			return builder.createPredicate(probPred, (IAttributeResolver) data);
+			if (probPred.isJoinPredicate()) {
+				// the first child will be an matrix expression
+				ASTMatrixExpression leftMatrix = (ASTMatrixExpression) probPred
+						.jjtGetChild(0);
+				ArrayList leftRows = leftMatrix.getMatrix();
+
+				String leftSource = ((ASTIdentifier) probPred.jjtGetChild(1))
+						.getName();
+
+				ASTMatrixExpression leftVector = (ASTMatrixExpression) probPred
+						.jjtGetChild(2);
+				// it is a column vector n x 1
+				ArrayList leftVRows = leftVector.getMatrix();
+
+				double[][] leftM = new double[leftRows.size()][((ArrayList) leftRows
+						.get(0)).size()];
+				for (int i = 0; i < leftRows.size(); i++) {
+					ArrayList row = (ArrayList) leftRows.get(i);
+					for (int u = 0; u < row.size(); u++) {
+						leftM[i][u] = (Double) row.get(u);
+					}
+				}
+
+				double[] leftV = new double[leftVRows.size()];
+				// i should only be 0 and then the loop should be
+				// finished
+				for (int i = 0; i < leftVRows.size(); i++) {
+					leftV[i] = (Double) ((ArrayList) leftVRows.get(i)).get(0);
+				}
+
+				ASTMatrixExpression rightMatrix = (ASTMatrixExpression) probPred
+						.jjtGetChild(3);
+				ArrayList rightRows = rightMatrix.getMatrix();
+
+				String rightSource = ((ASTIdentifier) probPred.jjtGetChild(4))
+						.getName();
+
+				ASTMatrixExpression rightVector = (ASTMatrixExpression) probPred
+						.jjtGetChild(5);
+				ArrayList rightVRows = rightVector.getMatrix();
+
+				double[][] rightM = new double[rightRows.size()][((ArrayList) rightRows
+						.get(0)).size()];
+				for (int i = 0; i < rightRows.size(); i++) {
+					ArrayList row = (ArrayList) rightRows.get(i);
+					for (int u = 0; u < row.size(); u++) {
+						leftM[i][u] = (Double) row.get(u);
+					}
+				}
+
+				double[] rightV = new double[rightRows.size()];
+				for (int i = 0; i < rightRows.size(); i++) {
+					rightV[i] = (Double) ((ArrayList) rightRows.get(i)).get(0);
+				}
+
+				ArrayList xLow = probPred.getXLow();
+				double[] xL = new double[xLow.size()];
+				for (int i = 0; i < xLow.size(); i++) {
+					xL[i] = (Double) xLow.get(i);
+				}
+
+				ArrayList xUp = probPred.getXUp();
+				double[] xU = new double[xUp.size()];
+				for (int i = 0; i < xUp.size(); i++) {
+					xU[i] = (Double) xUp.get(i);
+				}
+
+				double prob = probPred.getProb();
+
+				return new ProbabilityPredicate(leftSource, leftM, leftV,
+						rightSource, rightM, rightV, xL, xU, prob, probPred
+								.getCovOperandType(), probPred.getCompareOperator());
+			} else {
+				// the first child will be an matrix expression
+				ASTMatrixExpression leftMatrix = (ASTMatrixExpression) probPred
+						.jjtGetChild(0);
+				ArrayList leftRows = leftMatrix.getMatrix();
+
+				String leftSource = ((ASTIdentifier) probPred.jjtGetChild(1))
+						.getName();
+
+				ASTMatrixExpression leftVector = (ASTMatrixExpression) probPred
+						.jjtGetChild(2);
+				ArrayList leftVRows = leftVector.getMatrix();
+
+				double[][] leftM = new double[leftRows.size()][((ArrayList) leftRows
+						.get(0)).size()];
+				for (int i = 0; i < leftRows.size(); i++) {
+					ArrayList row = (ArrayList) leftRows.get(i);
+					for (int u = 0; u < row.size(); u++) {
+						leftM[i][u] = (Double) row.get(u);
+					}
+				}
+
+				double[] leftV = new double[leftVRows.size()];
+				// i should only be 0 and then the loop should be
+				// finished
+				for (int i = 0; i < leftVRows.size(); i++) {
+					leftV[i] = (Double) ((ArrayList) leftVRows.get(i)).get(0);
+				}
+
+				ArrayList xLow = probPred.getXLow();
+				double[] xL = new double[xLow.size()];
+				for (int i = 0; i < xLow.size(); i++) {
+					xL[i] = (Double) xLow.get(i);
+				}
+
+				ArrayList xUp = probPred.getXUp();
+				double[] xU = new double[xUp.size()];
+				for (int i = 0; i < xUp.size(); i++) {
+					xU[i] = (Double) xUp.get(i);
+				}
+
+				double prob = probPred.getProb();
+
+				return new ProbabilityPredicate(leftSource, leftM, leftV, xL, xU,
+						prob, probPred.getCompareOperator());
+			}
 		} catch (Exception e) {
 			throw new RuntimeException(
 					"invalid use of probability predicates - missing plugin");
