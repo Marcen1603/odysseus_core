d7fb0bb687fc3d2123c047127c168488c0f4f94a
Andre Bolles <andre.bolles@offis.de>
2009-11-02 13:40:09 +0000

    added new method for inserting a new father into a physical query plan
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3703 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 6858f7e3548c8f171f288168df0825173a953011

--- base/de.uniol.inf.is.odysseus.transformation.drools/src/de/uniol/inf/is/odysseus/transformation/drools/TransformationHelper.java
+++ base/de.uniol.inf.is.odysseus.transformation.drools/src/de/uniol/inf/is/odysseus/transformation/drools/TransformationHelper.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import de.uniol.inf.is.odysseus.base.ILogicalOperator;
+import de.uniol.inf.is.odysseus.base.ISubscription;
 import de.uniol.inf.is.odysseus.base.LogicalSubscription;
 import de.uniol.inf.is.odysseus.base.Subscription;
 import de.uniol.inf.is.odysseus.logicaloperator.base.TopAO;
@@ -48,6 +49,36 @@ public class TransformationHelper {
 			ret.add(l.getTarget());
 		}
 		return ret;
+	}
+	
+	/**
+	 * Inserts a new operator into a completely transformed physical query plan.
+	 * 
+	 * @param oldFather The old lower operator (from the dataflow point of view)
+	 * @param children The following operators of oldFather (from the dataflow point of view)
+	 * @param newFather The new lower operator. oldFather becomes the father of newFather
+	 * @return the modified children must be returned to update the drools working memory
+	 */
+	public static Collection<ISink> insertNewFather(ISource oldFather, Collection<ISubscription<ISink>> children, IPipe newFather){
+		Collection<ISink> modifiedChildren = new ArrayList<ISink>();
+		
+		// for every child, remove the connection between
+		// its old father and add it to its new father.
+		for(ISubscription<ISink> subscription: children){
+			ISink child = subscription.getTarget();
+			int sinkPort = subscription.getSinkPort();
+			int sourcePort = subscription.getSourcePort();
+			
+			oldFather.unsubscribe(subscription);
+			newFather.subscribe(child, sinkPort, sourcePort);
+			modifiedChildren.add(child);
+		}
+		
+		// then add the new father as child to the old father
+		oldFather.subscribe(newFather, 0, 0);
+		
+		// return the modified children to update the drools working memory
+		return modifiedChildren;
 	}
 
 }
