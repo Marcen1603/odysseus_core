2172dd511161d741dc0fb1f13b670b645e2b5559
Andre Bolles <andre.bolles@offis.de>
2009-11-04 10:28:55 +0000

    SILABParser-Fragment added
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3762 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: faedb4a7c69ec35b23856f8a7f2d3877a063f09a

---
+++ sensor/de.uniol.inf.is.odysseus.objecttracking.parser/src/de/uniol/inf/is/odysseus/objecttracking/parser/SILABVisitor.java
@@ -0,0 +1,47 @@
+package de.uniol.inf.is.odysseus.objecttracking.parser;
+
+import de.uniol.inf.is.odysseus.base.DataDictionary;
+import de.uniol.inf.is.odysseus.logicaloperator.base.AccessAO;
+import de.uniol.inf.is.odysseus.parser.cql.IVisitor;
+import de.uniol.inf.is.odysseus.parser.cql.VisitorFactory;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTHost;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTInteger;
+import de.uniol.inf.is.odysseus.parser.cql.parser.SimpleNode;
+import de.uniol.inf.is.odysseus.parser.cql.parser.transformation.CreateStreamVisitor;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.description.SDFSource;
+
+public class SILABVisitor extends CreateStreamVisitor implements IVisitor{
+
+	static boolean registerd = VisitorFactory.getInstance().setVisitor(new SILABVisitor(), "Silab");
+	@Override
+	public Object visit(SimpleNode node, Object data) {
+			
+		String host = ((ASTHost) node.jjtGetChild(0)).getValue();
+		int port = ((ASTInteger) node.jjtGetChild(1)).getValue().intValue();
+		AccessAO source = null;
+		
+		// TODO Tuple mode and MV mode are not supported with SILAB yet.
+//		if (node.useTupleMode()) {
+//			source = new AccessAO(new SDFSource(name,
+//					"RelationalInputStreamAccessPO"));
+//			source.setPort(port);
+//			source.setHost(host);
+//		} else if (node.useMVMode()) {
+//			source = new AccessAO(new SDFSource(name,
+//					"RelationalAtomicDataInputStreamAccessMVPO"));
+//			source.setPort(port);
+//			source.setHost(host);
+//			source.setOutputSchema(this.attributes);
+//		} else {
+			source = new AccessAO(new SDFSource(this.getName(),
+					"RelationalSILABInputStreamAccessPO"));
+			source.setPort(port);
+			source.setHost(host);
+			source.setOutputSchema(this.getAttributes());
+//		}
+		DataDictionary.getInstance().setView(this.getName(), source);
+		return data;
+		
+	}
+
+}
