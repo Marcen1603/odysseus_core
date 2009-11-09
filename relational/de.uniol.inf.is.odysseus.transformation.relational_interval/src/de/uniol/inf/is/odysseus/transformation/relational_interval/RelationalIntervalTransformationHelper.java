e104ef73679bbadd2fce9a10664ad773e47b9466
Andre Bolles <andre.bolles@offis.de>
2009-11-09 15:14:22 +0000

    Ausgabe entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3883 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 808991a7358ec9f2fc5583ffd98e03f8b1b30f2d

--- relational/de.uniol.inf.is.odysseus.transformation.relational_interval/src/de/uniol/inf/is/odysseus/transformation/relational_interval/RelationalIntervalTransformationHelper.java
+++ relational/de.uniol.inf.is.odysseus.transformation.relational_interval/src/de/uniol/inf/is/odysseus/transformation/relational_interval/RelationalIntervalTransformationHelper.java
@@ -20,17 +20,11 @@ public class RelationalIntervalTransformationHelper {
 		IMetadataUpdater mFac = new RelationalTimestampAttributeTimeIntervalMFactory(
 				attrPos);
 		
-		System.out.println("WinOn 1");
 		MetadataUpdatePO mPO = new MetadataUpdatePO( mFac );
-		System.out.println("WinOn 2");
 		ISource bottom = (ISource)windowPO.getSubscribedTo(0).getTarget();
-		System.out.println("WinOn 3");
 		bottom.unsubscribe(windowPO, 0, 0);
-		System.out.println("WinOn 4");
 		bottom.subscribe(mPO, 0, 0);
-		System.out.println("WinOn 5");
 		mPO.subscribe(windowPO, 0, 0);
-		System.out.println("WinOn 6");
 		
 		return mPO;
 	}
