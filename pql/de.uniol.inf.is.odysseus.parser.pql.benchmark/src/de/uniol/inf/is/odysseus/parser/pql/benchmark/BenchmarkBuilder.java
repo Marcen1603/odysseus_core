1eebeb6b136ecc2a29329e5a8a96b259a6578400
Jonas Jacobi <jacobi@uni-oldenburg.de>
2009-11-09 10:14:22 +0000

    compilefix
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3850 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 6e72485606c86f19713bbf0ee4211d21fcb41bc1

--- pql/de.uniol.inf.is.odysseus.parser.pql.benchmark/src/de/uniol/inf/is/odysseus/parser/pql/benchmark/BenchmarkBuilder.java
+++ pql/de.uniol.inf.is.odysseus.parser.pql.benchmark/src/de/uniol/inf/is/odysseus/parser/pql/benchmark/BenchmarkBuilder.java
@@ -1,26 +1,37 @@
 package de.uniol.inf.is.odysseus.parser.pql.benchmark;
 
 import java.util.List;
-import java.util.Map;
 
 import de.uniol.inf.is.odysseus.base.ILogicalOperator;
+import de.uniol.inf.is.odysseus.benchmarker.impl.BenchmarkAO;
 import de.uniol.inf.is.odysseus.parser.pql.AbstractOperatorBuilder;
+import de.uniol.inf.is.odysseus.parser.pql.DirectParameter;
+import de.uniol.inf.is.odysseus.parser.pql.Parameter.REQUIREMENT;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
-import de.uniol.inf.is.odysseus.benchmarker.impl.BenchmarkAO;
+/**
+ * @author Jonas Jacobi
+ */
 
 public class BenchmarkBuilder extends AbstractOperatorBuilder {
 
 	private static final String PROCESSING_TIME = "TIME";
 	private static final String SELECTIVITY = "SELECTIVITY";
 
-	@Override
-	public ILogicalOperator createOperator(Map<String, Object> parameters,
-			List<ILogicalOperator> inputOps) {
+	private final DirectParameter<Double> selectivity = new DirectParameter<Double>(
+			SELECTIVITY, Double.class, REQUIREMENT.MANDATORY);
 
-		Integer processingTimeInns = getParameter(parameters, PROCESSING_TIME,
-				Integer.class);
-		Double selectivity = getParameter(parameters, SELECTIVITY, Double.class);
+	private final DirectParameter<Integer> processingTimeInns = new DirectParameter<Integer>(
+			PROCESSING_TIME, Integer.class, REQUIREMENT.MANDATORY);
+
+	public BenchmarkBuilder() {
+		setParameters(selectivity, processingTimeInns);
+	}
+
+	@Override
+	protected ILogicalOperator createOperator(List<ILogicalOperator> inputOps) {
+		Integer processingTimeInns = this.processingTimeInns.getValue();
+		Double selectivity = this.selectivity.getValue();
 
 		SDFAttributeList schema = inputOps.get(0).getOutputSchema();	
 		BenchmarkAO benchmarkAO = new BenchmarkAO(processingTimeInns,
