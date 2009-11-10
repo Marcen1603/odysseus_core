24a8cee59dfb493dfdbc20cb1446b50c82e6df55
Andre Bolles <andre.bolles@offis.de>
2009-11-10 08:31:17 +0000

    LoggerHelper vergessen
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3900 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f61845c423a37c910a4024f988cb30befde144df

---
+++ base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/util/LoggerHelper.java
@@ -0,0 +1,55 @@
+package de.uniol.inf.is.odysseus.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**Dieses hier ist ein Hack damit das Drools-Plugin keine Fehler liefert!*/
+public class LoggerHelper {
+
+	private static Map<String, LoggerHelper> instances = new HashMap<String, LoggerHelper>();
+	private Logger _logger = null;
+	
+	private LoggerHelper(String name){
+		_logger = LoggerFactory.getLogger(name);
+	}
+	
+	public static synchronized LoggerHelper getInstance(String loggerName){
+		if(instances.get(loggerName) == null){
+			try {
+				instances.put(loggerName, new LoggerHelper(loggerName));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return instances.get(loggerName);
+	}
+	
+	public void error(String message) {
+		if (_logger != null) {
+			_logger.error(message);
+		}
+	}
+
+	public void info(String info) {
+		if (_logger != null) {
+			_logger.info(info);
+		}
+	}
+
+	public boolean isInfoEnabled() {
+		if (_logger != null) {
+			return _logger.isInfoEnabled();
+		} else {
+			return false;
+		}
+	}
+	
+	public void warn(String message){
+		if (_logger != null) {
+			_logger.warn(message);
+		}
+	}
+
+}
