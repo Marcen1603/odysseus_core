23e2c4787b61f910024f07f9c8037c2ca6c9cd2f
Jonas Jacobi <jacobi@uni-oldenburg.de>
2009-11-12 16:12:20 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3965 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: e7155214795dace4bfce7454fbfc99a53b65e6c5

--- application/de.uniol.inf.is.odysseus.benchmarker.impl/src/de/uniol/inf/is/odysseus/benchmarker/impl/PriorityBenchmarkResult.java
+++ application/de.uniol.inf.is.odysseus.benchmarker.impl/src/de/uniol/inf/is/odysseus/benchmarker/impl/PriorityBenchmarkResult.java
@@ -1,15 +1,20 @@
 package de.uniol.inf.is.odysseus.benchmarker.impl;
 
+import java.io.StringWriter;
+
 import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+import org.simpleframework.xml.core.Persister;
 
 import de.uniol.inf.is.odysseus.benchmarker.AbstractBenchmarkResult;
 import de.uniol.inf.is.odysseus.benchmarker.DescriptiveStatistics;
 import de.uniol.inf.is.odysseus.latency.ILatency;
 import de.uniol.inf.is.odysseus.priority.IPriority;
 
+@Root(name = "priorityresult")
 public class PriorityBenchmarkResult<T extends ILatency & IPriority> extends
 		AbstractBenchmarkResult<T> {
-	@Element
+	@Element(name="prioritizedStatistics")
 	private DescriptiveStatistics prioritizedStats = new DescriptiveStatistics();
 
 	public void add(T object) {
@@ -22,4 +27,17 @@ public class PriorityBenchmarkResult<T extends ILatency & IPriority> extends
 		}
 	};
 
+	@Override
+	public String toString() {
+		Persister p = new Persister();
+		StringWriter writer = new StringWriter();
+		try {
+			p.write(this, writer);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return writer.toString();
+	}
+	
 }
