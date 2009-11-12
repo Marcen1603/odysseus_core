c41d7d97da4920c825955c51160ead1bee7a5ea9
Jan Steinke <jsteinke@uni-oldenburg.de>
2009-11-12 14:30:46 +0000

    bugfix (war dann doch zu simple ;-) )
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@3956 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 86e0c37ea0d370864d717e6f084b0059397a221b

--- scheduler/de.uniol.inf.is.odysseus.scheduler.strategy.factory.aurora_priority/src/de/uniol/inf/is/odysseus/scheduler/strategy/factory/aurora_priority/impl/SimplePriorityMinLatency.java
+++ scheduler/de.uniol.inf.is.odysseus.scheduler.strategy.factory.aurora_priority/src/de/uniol/inf/is/odysseus/scheduler/strategy/factory/aurora_priority/impl/SimplePriorityMinLatency.java
@@ -1,9 +1,12 @@
 package de.uniol.inf.is.odysseus.scheduler.strategy.factory.aurora_priority.impl;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import de.uniol.inf.is.odysseus.physicaloperator.base.ISource;
 import de.uniol.inf.is.odysseus.physicaloperator.base.plan.IPartialPlan;
+import de.uniol.inf.is.odysseus.priority.PostPriorisationPO;
+import de.uniol.inf.is.odysseus.priority.PriorityPO;
 
 public class SimplePriorityMinLatency extends AbstractPriorityMinLatency{
 
@@ -13,8 +16,18 @@ public class SimplePriorityMinLatency extends AbstractPriorityMinLatency{
 
 	@Override
 	protected void executePriorisationActivation(ISource<?> source, List<ISource<?>> opPath) {
-		// Muss nichts weiter machen, da die Strategie nur Teilplaene bevorzugt,
+		// Muss nichts weiter machen, als copartners zu entfernen, da die Strategie nur Teilplaene bevorzugt,
 		// die zu einem Join fuehren, dass priorisierte Datenstromelemente enthalten koennte.
+		
+		for(ISource<?> op : opPath) {		
+			if(op instanceof PriorityPO<?>) {
+				PriorityPO<?> prio = (PriorityPO<?>) op;
+				if(prio.getCopartners() != null) {
+					prio.getCopartners().clear();
+				}
+			}
+		}		
+		
 	}
 
 }
