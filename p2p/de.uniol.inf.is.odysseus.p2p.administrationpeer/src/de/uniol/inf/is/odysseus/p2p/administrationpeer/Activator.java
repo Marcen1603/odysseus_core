415865c552aaef7e8810ffbd305dff6adb2abb3f
Mart Koehler <mkoehler@uni-oldenburg.de>
2009-11-16 01:52:58 +0000

    - Jxta Implementierung aus dem P2P Framework extrahiert
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4008 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a9fda828fccdebb52fc4f25439616f5bd4ec84b4

--- p2p/de.uniol.inf.is.odysseus.p2p.administrationpeer/src/de/uniol/inf/is/odysseus/p2p/administrationpeer/Activator.java
+++ p2p/de.uniol.inf.is.odysseus.p2p.administrationpeer/src/de/uniol/inf/is/odysseus/p2p/administrationpeer/Activator.java
@@ -3,7 +3,7 @@ package de.uniol.inf.is.odysseus.p2p.administrationpeer;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import de.uniol.inf.is.odysseus.p2p.administrationpeer.peerImpl.jxta.AdministrationPeerJxtaImpl;
+//import de.uniol.inf.is.odysseus.p2p.administrationpeer.peerImpl.jxta.AdministrationPeerJxtaImpl;
 
 public class Activator implements BundleActivator {
 	private AbstractAdministrationPeer administrationPeer;
@@ -12,9 +12,9 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		System.out.println("Starte Verwaltungs-Peer");
-		administrationPeer = new AdministrationPeerJxtaImpl();
-		System.out.println("Starte Verwaltungs-Peer2");
+//		System.out.println("Starte Verwaltungs-Peer");
+//		administrationPeer = new AdministrationPeerJxtaImpl();
+//		System.out.println("Starte Verwaltungs-Peer2");
 	}
 
 	/*
