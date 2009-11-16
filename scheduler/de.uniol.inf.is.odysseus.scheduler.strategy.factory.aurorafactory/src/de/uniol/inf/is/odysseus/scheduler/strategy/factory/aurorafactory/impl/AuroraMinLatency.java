66f857c6099004f4ffa72ed70e8d879c1d4a6f70
Jan Steinke <jsteinke@uni-oldenburg.de>
2009-11-16 17:56:29 +0000

    Bisheriges MinLatency hatte gar keine Metadaten zum berechnen der MinLatency ;-)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4011 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: bfcef78054d1c81fd61f6fd38c426f72571168ae

--- scheduler/de.uniol.inf.is.odysseus.scheduler.strategy.factory.aurorafactory/src/de/uniol/inf/is/odysseus/scheduler/strategy/factory/aurorafactory/impl/AuroraMinLatency.java
+++ scheduler/de.uniol.inf.is.odysseus.scheduler.strategy.factory.aurorafactory/src/de/uniol/inf/is/odysseus/scheduler/strategy/factory/aurorafactory/impl/AuroraMinLatency.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 import de.uniol.inf.is.odysseus.base.FESortedPair;
+import de.uniol.inf.is.odysseus.monitoring.StaticValueMonitoringData;
+import de.uniol.inf.is.odysseus.monitoring.physicaloperator.MonitoringDataTypes;
 import de.uniol.inf.is.odysseus.physicaloperator.base.IIterableSource;
 import de.uniol.inf.is.odysseus.physicaloperator.base.ISource;
 import de.uniol.inf.is.odysseus.scheduler.strategy.AbstractExecListSchedulingStrategy;
@@ -33,6 +35,22 @@ public class AuroraMinLatency extends AbstractExecListSchedulingStrategy {
 		this.iterator = this.executionList.iterator();
 	}
 	
+	protected void initMetadata(List<ISource<?>> opPath) {
+		for (ISource<?> s : opPath) {
+			if (!s.providesMonitoringData(MonitoringDataTypes.ESTIMATED_PRODUCTIVITY.name)) {
+				s.addMonitoringData(MonitoringDataTypes.ESTIMATED_PRODUCTIVITY.name,
+						new StaticValueMonitoringData<Double>(s,
+								MonitoringDataTypes.ESTIMATED_PRODUCTIVITY.name, 1d));
+			}
+
+			if (!s.providesMonitoringData(MonitoringDataTypes.ESTIMATED_PROCESSING_COST.name)) {
+				s.addMonitoringData(MonitoringDataTypes.ESTIMATED_PROCESSING_COST.name,
+						new StaticValueMonitoringData<Double>(s,
+								MonitoringDataTypes.ESTIMATED_PROCESSING_COST.name, 1d));
+			}
+		}
+	}	
+	
 	private List<IIterableSource<?>> init(List<IIterableSource<?>> toSchedule) {
 		List<FESortedPair<Double,List<IIterableSource<?>>>> pathes = new LinkedList<FESortedPair<Double,List<IIterableSource<?>>>>();
 		List<IIterableSource<?>> execList = new LinkedList<IIterableSource<?>>();
@@ -41,6 +59,7 @@ public class AuroraMinLatency extends AbstractExecListSchedulingStrategy {
 			List<IIterableSource<?>> schPath = new LinkedList<IIterableSource<?>>();
 			List<ISource<?>> opPath = new LinkedList<ISource<?>>();
 			getPathToRoot(s, schPath, opPath, null);
+			initMetadata(opPath);
 			FESortedPair<Double, List<IIterableSource<?>>> pWithCost = new FESortedPair<Double, List<IIterableSource<?>>>(calcPathCost(opPath), schPath);
 			//System.out.println("Path Costs :"+opPath+" --> "+pWithCost);
 			pathes.add(pWithCost);
