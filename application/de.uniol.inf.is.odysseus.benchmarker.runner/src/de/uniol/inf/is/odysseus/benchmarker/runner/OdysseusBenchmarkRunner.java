f9f048f5884b4b9cc2c35efaacecb046bb52ca7a
Jan Steinke <jsteinke@uni-oldenburg.de>
2009-11-23 17:34:07 +0000

    mini-bugfix
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4055 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e94a488d3a27de64bcfaaaa4cde34b2b8b57e5b4

--- application/de.uniol.inf.is.odysseus.benchmarker.runner/src/de/uniol/inf/is/odysseus/benchmarker/runner/OdysseusBenchmarkRunner.java
+++ application/de.uniol.inf.is.odysseus.benchmarker.runner/src/de/uniol/inf/is/odysseus/benchmarker/runner/OdysseusBenchmarkRunner.java
@@ -58,7 +58,7 @@ public class OdysseusBenchmarkRunner implements IApplication {
 			
 			if(arguments.get(MEMORY_USAGE)) {
 				for(int i=0; i < benchmark.getMemUsageJoin().size();i++) {
-					serializer.write(benchmark.getMemUsageJoin().get(i), new File(i + "JOIN" + filename));
+					serializer.write(benchmark.getMemUsageJoin().get(i), new File(filename + "JOIN" + i));
 				}
 			/*	for(int i=0; i < benchmark.getMemUsageJoinPunctuations().size();i++) {
 					serializer.write(benchmark.getMemUsageJoinPunctuations().get(i), new File(i + "JOINPUNC" + filename));
