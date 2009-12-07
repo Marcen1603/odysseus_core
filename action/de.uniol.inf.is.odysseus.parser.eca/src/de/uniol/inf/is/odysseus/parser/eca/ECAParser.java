7c41006ac5723bf8d357c29764cffe73d20e5fb7
Simon Flandergan <flandergan@uni-oldenburg.de>
2009-12-07 16:00:44 +0000

    eca parser anfaenge
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4207 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3b65346616f11793ecda1cbaac049208190213e5

--- action/de.uniol.inf.is.odysseus.parser.eca/src/de/uniol/inf/is/odysseus/parser/eca/ECAParser.java
+++ action/de.uniol.inf.is.odysseus.parser.eca/src/de/uniol/inf/is/odysseus/parser/eca/ECAParser.java
@@ -1,24 +1,58 @@
 package de.uniol.inf.is.odysseus.parser.eca;
 
 import java.io.Reader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import de.uniol.inf.is.odysseus.base.ILogicalOperator;
 import de.uniol.inf.is.odysseus.base.IQueryParser;
 import de.uniol.inf.is.odysseus.base.QueryParseException;
+import de.uniol.inf.is.odysseus.base.planmanagement.ICompiler;
 
 public class ECAParser implements IQueryParser{
+	private ICompiler compiler;
+	private HashMap<String, String> languagePatterns = new HashMap<String, String>();
+	private Pattern ecaPatternStart;
+	private Pattern ecaPatternEnd;
 
+	public ECAParser () {
+		this.ecaPatternStart = Pattern.compile("WHEN[\\s]+EVENT[\\s]*\\(");
+		
+		String param = "[\\w]+[\\s]*";
+		String method = "[\\w]+[\\s]*\\(["+param+"[,"+param+"]*]?\\)";
+		String actuator = "[\\w]+\\."+method;	
+		this.ecaPatternEnd = Pattern.compile("\\)[\\s]*THEN[\\s]+EXECUTE[\\s]+"+actuator+"[[\\s]+"+actuator+"]*");
+		
+	}
+	
 	@Override
 	public String getLanguage() {
 		return "ECA";
+	}
+	
+	public void bindCompiler (ICompiler compiler){
+		this.compiler = compiler;
 	}
 
 	@Override
 	public List<ILogicalOperator> parse(String query)
 			throws QueryParseException {
-		// TODO Auto-generated method stub
+		//extract internal query
+		
+		String language = this.determineQueryLanguage(query);
 		return null;
+	}
+
+	private String determineQueryLanguage(String query) throws QueryParseException {
+		for (Entry<String, String> entry : this.languagePatterns.entrySet()){
+			if (query.matches(entry.getKey())){
+				return entry.getValue();
+			}
+		}
+		throw new QueryParseException("Unknown Eventlanguage");
 	}
 
 	@Override
@@ -26,6 +60,30 @@ public class ECAParser implements IQueryParser{
 			throws QueryParseException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public boolean match (String string){
+		string = string.toUpperCase();
+		Pattern p = Pattern.compile(this.ecaPatternStart.pattern()+".*"+this.ecaPatternEnd);
+		return p.matcher(string).find();
+	}
+	
+	public static void main(String[] args) {
+		ECAParser parser = new ECAParser();
+		String[] statements = {
+				//correct
+				"WHEN EVENT (CQL) THEN EXECUTE a.a()",
+				"WHEN EVENT  (Select * From a where a.a=5;)THEN EXECUTE a.a(5) ",
+				"WHEN EVENT(Select * From (Select a from a)) THEN  execute a.b()",
+				"WHEN EVENT () then execute a.b(), b.a(5, \"2\")",
+				//incorrect
+				"WHEN EVENT (Select * From THEN execute a.b()",
+				"WHEN EVENT Select * From) THEN execute a.b()",
+				"WHEN event () then a.b()",
+				"WHen event (blub) then execute a.b, b.a()"};		
+		for (String statement : statements){
+			System.out.println(parser.match(statement));
+		}
 	}
 
 }
