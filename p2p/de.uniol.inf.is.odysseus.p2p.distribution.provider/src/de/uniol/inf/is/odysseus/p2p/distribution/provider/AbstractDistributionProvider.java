fadc0af965711c00c86594209fd1d7c6d225313b
Mart Koehler <mkoehler@uni-oldenburg.de>
2009-12-08 16:57:38 +0000

    o Anbindung an die Ausführungsumgebung durch die Nutzung des Callback Objektes direkt in der Verteilung
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4222 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1e537f73012a2a137ba8d642019af5b7cfb1d6a2

--- p2p/de.uniol.inf.is.odysseus.p2p.distribution.provider/src/de/uniol/inf/is/odysseus/p2p/distribution/provider/AbstractDistributionProvider.java
+++ p2p/de.uniol.inf.is.odysseus.p2p.distribution.provider/src/de/uniol/inf/is/odysseus/p2p/distribution/provider/AbstractDistributionProvider.java
@@ -1,21 +1,32 @@
 package de.uniol.inf.is.odysseus.p2p.distribution.provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.p2p.distribution.provider.selector.IClientSelectorFactory;
 import de.uniol.inf.is.odysseus.p2p.peer.AbstractPeer;
 import de.uniol.inf.is.odysseus.p2p.peer.execution.handler.IExecutionHandlerFactory;
-import de.uniol.inf.is.odysseus.p2p.queryhandling.Query;
+import de.uniol.inf.is.odysseus.p2p.peer.execution.listener.IExecutionListenerCallback;
 
-public abstract class AbstractDistributionProvider<T extends AbstractPeer> implements IDistributionProvider<T> {
+public abstract class AbstractDistributionProvider<P extends AbstractPeer> implements IDistributionProvider<P> {
 
-	private T peer;
+	private Logger logger;
+	private P peer;
 	private IExecutionHandlerFactory executionHandlerFactory;
+	private IClientSelectorFactory clientSelectorFactory;
+	
+	public AbstractDistributionProvider () {
+		this.logger = LoggerFactory.getLogger(AbstractDistributionProvider.class);
+	}
 	
 	@Override
-	public void setPeer(T peer) {
+	public void setPeer(P peer) {
 		this.peer = peer;
 	}
 	
 	@Override
-	public abstract void distributePlan(Query query, Object serverResponse);
+	public abstract void distributePlan(IExecutionListenerCallback callback,
+			Object serverResponse);
 
 	@Override
 	public abstract String getDistributionStrategy();
@@ -29,12 +40,19 @@ public abstract class AbstractDistributionProvider<T extends AbstractPeer> imple
 	@Override
 	public abstract void startService();
 	
-	protected T getPeer() {
+	protected P getPeer() {
 		return this.peer;
 	}
 	
 	public void bindExecutionHandlerFactory(IExecutionHandlerFactory factory) {
-		this.executionHandlerFactory = factory;
+		
+		try {
+			getLogger().info(
+					"Binding ExecutionHandlerFactory: " + factory.getName());
+			this.executionHandlerFactory = factory;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void unbindExecutionHandlerFactory(IExecutionHandlerFactory factory) {
@@ -43,8 +61,29 @@ public abstract class AbstractDistributionProvider<T extends AbstractPeer> imple
 		}
 	}
 
+	@Override
 	public IExecutionHandlerFactory getExecutionHandlerFactory() {
 		return this.executionHandlerFactory;
+	}
+
+	@Override
+	public IClientSelectorFactory getClientSelectorFactory() {
+		return this.clientSelectorFactory;
+	}
+	
+	public void bindClientSelectorFactory(IClientSelectorFactory selector) {
+		getLogger().info("Binding Client Selector Factory: "+selector.getName());
+		this.clientSelectorFactory = selector;
+	}
+	
+	public void unbindClientSelectorFactory(IClientSelectorFactory selector) {
+		if(this.clientSelectorFactory == selector) {
+			this.clientSelectorFactory = null;
+		}
+	}
+	
+	public Logger getLogger() {
+		return logger;
 	}
 	
 }
--- p2p/de.uniol.inf.is.odysseus.p2p.distribution.provider/src/de/uniol/inf/is/odysseus/p2p/distribution/provider/IDistributionProvider.java
+++ p2p/de.uniol.inf.is.odysseus.p2p.distribution.provider/src/de/uniol/inf/is/odysseus/p2p/distribution/provider/IDistributionProvider.java
@@ -1,13 +1,16 @@
 package de.uniol.inf.is.odysseus.p2p.distribution.provider;
 
-import de.uniol.inf.is.odysseus.p2p.peer.IPeer;
-import de.uniol.inf.is.odysseus.p2p.queryhandling.Query;
+import de.uniol.inf.is.odysseus.p2p.peer.execution.handler.IExecutionHandlerFactory;
+import de.uniol.inf.is.odysseus.p2p.peer.execution.listener.IExecutionListenerCallback;
+import de.uniol.inf.is.odysseus.p2p.distribution.provider.selector.IClientSelectorFactory;
 
-public interface IDistributionProvider<T extends IPeer> {
+public interface IDistributionProvider<P> {
 	public void initializeService();
 	public void finalizeService();
 	public void startService();
 	public String getDistributionStrategy();
-	public void setPeer(T peer);
-	void distributePlan(Query query, Object serverResponse);
+	public void setPeer(P peer);
+	public void distributePlan(IExecutionListenerCallback callback, Object serverResponse);
+	public IExecutionHandlerFactory getExecutionHandlerFactory();
+	public IClientSelectorFactory getClientSelectorFactory();
 }
