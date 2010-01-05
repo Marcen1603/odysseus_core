faa681253487f39f51e67f9698f437b8ef45b823
Simon Flandergan <flandergan@uni-oldenburg.de>
2010-01-05 10:05:17 +0000

    parser test bundle
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4276 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b95d305ed886f89172fe9938a3a634dcb37363c3

---
+++ action/de.uniol.inf.is.odysseus.parser.eca.test/src/de/uniol/inf/is/odysseus/parser/eca/test/ECAParserTest.java
@@ -0,0 +1,27 @@
+package de.uniol.inf.is.odysseus.parser.eca.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uniol.inf.is.odysseus.planmanagement.executor.IAdvancedExecutor;
+
+public class ECAParserTest {
+
+	private IAdvancedExecutor executor;
+
+	public void bindExecutor(IAdvancedExecutor executor){
+		this.executor = executor;
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testParseString() {
+		
+		
+	}
+
+}
