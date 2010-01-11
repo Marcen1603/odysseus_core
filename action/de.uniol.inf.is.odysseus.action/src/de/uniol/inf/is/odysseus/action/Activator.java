482993acf72068a1a852580fc8ed1d99da366b04
Simon Flandergan <flandergan@uni-oldenburg.de>
2010-01-11 11:20:01 +0000

    mainfeste aufgeräumt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4297 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1b0f44f1f0e9c9709153905d194352c7897b7b07

--- action/de.uniol.inf.is.odysseus.action/src/de/uniol/inf/is/odysseus/action/Activator.java
+++ action/de.uniol.inf.is.odysseus.action/src/de/uniol/inf/is/odysseus/action/Activator.java
@@ -7,18 +7,18 @@ import org.osgi.framework.ServiceReference;
 import de.uniol.inf.is.odysseus.action.actuatorManagement.ActuatorFactory;
 import de.uniol.inf.is.odysseus.action.services.actuator.IActuatorManager;
 
-public class Actuator implements BundleActivator {
+public class Activator implements BundleActivator {
 
 	private IActuatorManager service;
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		ActuatorFactory factory = ActuatorFactory.getInstance();
+		/*ActuatorFactory factory = ActuatorFactory.getInstance();
 		ServiceReference reference = context.getServiceReference(IActuatorManager.class.getName());
 		this.service = (IActuatorManager) context.getService(reference);
 		if (this.service != null){
 			factory.bindActuatorManager(service);
-		}
+		}*/
 
 	}
 
