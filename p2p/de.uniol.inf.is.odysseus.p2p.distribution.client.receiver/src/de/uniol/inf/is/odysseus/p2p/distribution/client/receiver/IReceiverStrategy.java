652c0eff6a8066606b05e10c5e09d2f62d25100f
Mart Koehler <mkoehler@uni-oldenburg.de>
2010-01-13 07:37:36 +0000

    Folgende Plugins wurden entfernt: de.uniol.inf.is.odysseus.p2p.distribution.bidding.client.receiver de.uniol.inf.is.odysseus.p2p.distribution.bidding.provider.selector de.uniol.inf.is.odysseus.p2p.distribution.distribution.client.receiver de.uniol.inf.is.odysseus.p2p.distribution.distribution.provider.selector de.uniol.inf.is.odysseus.p2p.execution.standardhandler
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4347 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9204c7f032abce4d5b0fa6a0af764d633a65793e

--- p2p/de.uniol.inf.is.odysseus.p2p.distribution.client.receiver/src/de/uniol/inf/is/odysseus/p2p/distribution/client/receiver/IReceiverStrategy.java
+++
@@ -1,8 +0,0 @@
-package de.uniol.inf.is.odysseus.p2p.distribution.client.receiver;
-
-import de.uniol.inf.is.odysseus.p2p.queryhandling.Query;
-import de.uniol.inf.is.odysseus.p2p.peer.AbstractPeer;
-
-public interface IReceiverStrategy {
-	public boolean handleQuery(Query query, AbstractPeer peer);
-}
--- p2p/de.uniol.inf.is.odysseus.p2p.distribution.provider.selector/src/de/uniol/inf/is/odysseus/p2p/distribution/provider/selector/IClientSelectorFactory.java
+++
@@ -1,9 +0,0 @@
-package de.uniol.inf.is.odysseus.p2p.distribution.provider.selector;
-
-import de.uniol.inf.is.odysseus.p2p.peer.execution.listener.IExecutionListenerCallback;
-
-
-public interface IClientSelectorFactory {
-	public IClientSelector getNewInstance(int time, IExecutionListenerCallback callback);
-	public String getName();
-}
