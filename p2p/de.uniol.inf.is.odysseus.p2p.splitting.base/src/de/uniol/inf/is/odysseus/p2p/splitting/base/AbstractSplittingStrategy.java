6aaa38747226a880f08af01d75bfd4704795d259
Mart Koehler <mkoehler@uni-oldenburg.de>
2010-01-13 06:52:53 +0000

    o Verwendung des Callbacks für die Ausführungsumgebung
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4329 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d09cb510081ba723422a76f8548ca9a67088a420

--- p2p/de.uniol.inf.is.odysseus.p2p.splitting.base/src/de/uniol/inf/is/odysseus/p2p/splitting/base/AbstractSplittingStrategy.java
+++ p2p/de.uniol.inf.is.odysseus.p2p.splitting.base/src/de/uniol/inf/is/odysseus/p2p/splitting/base/AbstractSplittingStrategy.java
@@ -3,9 +3,23 @@ package de.uniol.inf.is.odysseus.p2p.splitting.base;
 import java.util.ArrayList;
 
 import de.uniol.inf.is.odysseus.base.ILogicalOperator;
+import de.uniol.inf.is.odysseus.p2p.peer.AbstractPeer;
+import de.uniol.inf.is.odysseus.p2p.peer.IPeer;
+import de.uniol.inf.is.odysseus.p2p.peer.execution.listener.IExecutionListenerCallback;
 import de.uniol.inf.is.odysseus.p2p.splitting.refinement.base.IRefinementStrategy;
 
 public abstract class AbstractSplittingStrategy implements ISplittingStrategy {
+
+	private IExecutionListenerCallback callback;
+	private AbstractPeer peer;
+
+	public IExecutionListenerCallback getCallback() {
+		return this.callback;
+	}
+
+	public void setCallback(IExecutionListenerCallback callback) {
+		this.callback = callback;
+	}
 
 	private IRefinementStrategy refinement = null;
 	
@@ -29,5 +43,12 @@ public abstract class AbstractSplittingStrategy implements ISplittingStrategy {
 	public IRefinementStrategy getRefinement() {
 		return refinement;
 	}
-
+	
+	@Override
+	public void setPeer(IPeer peer) {
+		this.peer = (AbstractPeer) peer;
+	}
+	protected AbstractPeer getPeer() {
+		return this.peer;
+	}
 }
