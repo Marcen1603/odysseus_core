5a020e0745ffa28be5390340aafbcd2a92b79e3f
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-01-14 13:32:20 +0000

    StreamTupleConverter ein bischen vereinfacht... (dennoch Diskussionbedarf vorhanden)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4379 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 2fe013b541b23d9b76b61a4cc08833c37b8f5401

--- application/de.uniol.inf.is.odysseus.viewer/src/de/uniol/inf/is/odysseus/viewer/view/stream/StreamTupleConverter.java
+++ application/de.uniol.inf.is.odysseus.viewer/src/de/uniol/inf/is/odysseus/viewer/view/stream/StreamTupleConverter.java
@@ -4,34 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 import de.uniol.inf.is.odysseus.relational.base.RelationalTuple;
-import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttribute;
-import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
 @SuppressWarnings("unchecked")
 public class StreamTupleConverter extends AbstractStreamConverter<RelationalTuple, Map<String, String>> {
 	
+	private Map<String, String> valueMap = new HashMap<String, String>();
+		
 	public StreamTupleConverter( Parameters params ) {
 		super( params );
 	}
 
 	@Override
 	public Map< String, String > convertStreamElement( RelationalTuple element ) {
-		Map<String, String> tempMap = new HashMap<String, String>();
-		
 		for( int i = 0; i < element.getAttributeCount(); i++ ){
-			String name = null;
 //			if( element.getSchema() != null ){
 //				SDFAttributeList schema = element.getSchema();
 //				SDFAttribute attr = schema.getAttribute( i ); 
 //				name = attr.getQualName();
 //			}else{ 
-				name = String.valueOf( i );
+//				name = String.valueOf( i );
 //			}
-			String value = element.getAttribute( i ).toString();
-			tempMap.put( name, value );
+			valueMap.put( String.valueOf( i ), element.getAttribute( i ).toString() );
 		}
 		
-		return tempMap;
+		return valueMap;
 	}
 
 }
