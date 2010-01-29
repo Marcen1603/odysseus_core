055db7eea5ca1eeb24866e91cc3416cd127c3a76
Andre Bolles <andre.bolles@offis.de>
2010-01-29 10:10:15 +0000

    neue ObjectTracking Metadaten Fabrik
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4475 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d682d7323debafb41ee0de97aa363868d70fe4d1

---
+++ sensor/de.uniol.inf.is.odysseus.objecttracking/src/de/uniol/inf/is/odysseus/objecttracking/metadata/factory/ObjectTrackingMetadataFactory.java
@@ -0,0 +1,52 @@
+package de.uniol.inf.is.odysseus.objecttracking.metadata.factory;
+
+import java.util.ArrayList;
+
+import de.uniol.inf.is.odysseus.metadata.base.IMetadataUpdater;
+import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.ObjectTrackingMetadata;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.vocabulary.SDFDatatypes;
+@SuppressWarnings("unchecked")
+/**
+ * This class generates a new IntervalProbabilityLatencyPrediction object.
+ * It fills latencyStart and the covariance. The start timestamp must be
+ * set by another MFactory object. The prediction function will
+ * be set by the SetPredictionOperator.
+ * 
+ * @author Andre Bolles
+ * 
+ */
+public class ObjectTrackingMetadataFactory implements IMetadataUpdater<ObjectTrackingMetadata, MVRelationalTuple<ObjectTrackingMetadata>>{
+
+	SDFAttributeList schema;
+	
+	public ObjectTrackingMetadataFactory(SDFAttributeList schema){
+		this.schema = schema;
+	}
+	
+	@Override
+	public void updateMetadata(MVRelationalTuple<ObjectTrackingMetadata> inElem) {
+		inElem.getMetadata().setLatencyStart(System.nanoTime());
+		
+		double[][] cov = null;
+		int counter = 0;
+		for(SDFAttribute attr: this.schema){
+			if(SDFDatatypes.isMeasurementValue(attr.getDatatype())){
+				ArrayList covariance = ((SDFAttribute)attr).getCovariance();
+				if(cov == null){
+					cov = new double[covariance.size()][covariance.size()];
+				}
+				
+				for(int i = 0; i<covariance.size(); i++){
+					cov[counter][i] = (Double)covariance.get(i);
+				}
+				counter++;
+			}
+		}
+		
+		inElem.getMetadata().setCovariance(cov);
+	}
+
+}
