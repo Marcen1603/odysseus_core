c9e53a467589590b4b7e922147424166d73a87aa
Tobias Witt <twitt@uni-oldenburg.de>
2010-02-20 15:50:07 +0000

    altes paket loeschen
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4651 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: db764c13ba8f59447387f77234ae23d7ade66f26

--- planmanagement/de.uniol.inf.is.odysseus.planmanagement.optimization.migration.costmodel/src/de/uniol/inf/is/odysseus/planmanagement/optimization/migration/costmodel/base/ICost.java
+++
@@ -1,7 +0,0 @@
-package de.uniol.inf.is.odysseus.planmanagement.optimization.migration.costmodel.base;
-
-
-public interface ICost<T> extends Comparable<ICost<T>> {
-	public int getScore();
-	public void setScore(int score);
-}
