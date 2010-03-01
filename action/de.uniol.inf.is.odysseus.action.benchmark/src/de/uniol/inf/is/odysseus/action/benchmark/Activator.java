63e1da0ea9eeae905f6673fdf0d2af256330418d
Simon Flandergan <flandergan@uni-oldenburg.de>
2010-03-01 17:36:11 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4713 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: 6791fce63d6b78dae6b23def56a55893c3c05bd6

--- action/de.uniol.inf.is.odysseus.action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/Activator.java
+++ action/de.uniol.inf.is.odysseus.action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/Activator.java
@@ -10,7 +10,6 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		ActuatorBenchmark.setFileLoc(context.getBundle().getLocation());
 	}
 
 	/*
--- action/de.uniol.inf.is.odysseus.action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/ActuatorBenchmark.java
+++ action/de.uniol.inf.is.odysseus.action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/ActuatorBenchmark.java
@@ -1,13 +1,18 @@
 package de.uniol.inf.is.odysseus.action.benchmark;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ActuatorBenchmark implements IActuatorBenchmark{
 	private Map<String, Integer> tupleNo;
 	private Map<String, Long> timestamps;
+	private FileWriter file;
 	
-	private static String fileLoc;
+	private static String fileLoc = System.getProperty("user.home");
 	
 	public ActuatorBenchmark(){
 		this.tupleNo = new HashMap<String, Integer>();
@@ -17,8 +22,28 @@ public class ActuatorBenchmark implements IActuatorBenchmark{
 	@Override
 	public long notifyEnd(String identifier) {
 		long end = System.currentTimeMillis();
-		long start = this.timestamps.get(identifier);
+		long start = this.timestamps.remove(identifier);
 		long timeInterval = end-start;
+		
+		//write to file
+		if (this.file == null){
+			try {
+				this.file = new FileWriter(
+						fileLoc+
+						File.separator+
+						"odyLog"+System.currentTimeMillis()%100+
+						".log");
+				this.file.append("identifier; time\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			this.file.append(identifier+";"+timeInterval);
+			this.file.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return timeInterval;
 	}
 
@@ -35,9 +60,4 @@ public class ActuatorBenchmark implements IActuatorBenchmark{
 		this.tupleNo.put(identifier, tupleNo);
 		return newIdentifier;
 	}
-	
-	public static void setFileLoc(String fileLoc) {
-		ActuatorBenchmark.fileLoc = fileLoc;
-	}
-
 }
