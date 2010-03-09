979c1ab412c0c50aec5b1396dde984a75d87a888
Simon Flandergan <flandergan@uni-oldenburg.de>
2010-03-09 12:49:20 +0000

    benchmarking ....
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4776 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a9f385f1a6cd95063ac0fc0438cf22715fa4492f

--- action/de.uniol.inf.is.odysseus.action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/BenchmarkData.java
+++ action/de.uniol.inf.is.odysseus.action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/BenchmarkData.java
@@ -3,41 +3,25 @@ package de.uniol.inf.is.odysseus.action.benchmark;
 import java.util.HashMap;
 import java.util.Map;
 
-import de.uniol.inf.is.odysseus.base.IClone;
-import de.uniol.inf.is.odysseus.base.IMetaAttribute;
+import de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval;
 
-public class BenchmarkData implements IMetaAttribute {
-	private String identifier;
+public class BenchmarkData {
 	private long creationTime;
 	private Map<String, Long> outputTimes;
 	
 	public BenchmarkData(String identifier){
-		this.identifier = identifier;
 		this.creationTime = System.nanoTime();
 		this.outputTimes = new HashMap<String, Long>();
 	}
 	
-	/**
-	 * Copy constructor
-	 * @param benchmarkData
-	 */
-	public BenchmarkData(BenchmarkData benchmarkData) {
-		this.identifier = benchmarkData.identifier;
-		this.creationTime = benchmarkData.creationTime;
-		this.outputTimes = new HashMap<String, Long>(benchmarkData.outputTimes);
-	}
 	
-	public void addOutputTime(String id){
-		this.outputTimes.put(id, System.nanoTime());
+	public BenchmarkData(ITimeInterval object) {
+		this.creationTime = object.getStart().getMainPoint();
+		this.outputTimes = new HashMap<String, Long>();
 	}
 
-	@Override
-	public IClone clone() {
-		return new BenchmarkData(this);
-	}
-	
-	public String getIdentifier() {
-		return identifier;
+	public void addOutputTime(String id){
+		this.outputTimes.put(id, System.currentTimeMillis());
 	}
 	
 	public long getCreationTime() {
--- action/de.uniol.inf.is.odysseus.action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/IActuatorBenchmark.java
+++ action/de.uniol.inf.is.odysseus.action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/IActuatorBenchmark.java
@@ -1,10 +1,18 @@
 package de.uniol.inf.is.odysseus.action.benchmark;
 
 
+
+
 public interface IActuatorBenchmark {
 	
-	public enum Operation{DATAEXTRACTION, QUERYPROCESSING, ACTIONEXECTION};
+	public enum Operation{QUERYPROCESSING, ACTIONEXECTION};
 	
 	public void addBenchmarkData(BenchmarkData data);
+	
+	public void addQuery(String query, String lang);
+	
+	public void run();
+	
+	public void stop();
 		
 }
