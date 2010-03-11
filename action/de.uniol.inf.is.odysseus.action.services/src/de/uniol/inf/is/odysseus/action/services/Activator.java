4b0f2f9b15e4176b70552fc8c2482e2e20b5971d
Simon Flandergan <flandergan@uni-oldenburg.de>
2010-03-11 12:56:05 +0000

    -abhaengigkeiten zu knx bundle implementiert -bundles sind nicht im svn -> bei bedard an birger martens wenden
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4783 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 703edb0c75e4efbca3b74dbc6e3467f1d713f0e0

--- action/de.uniol.inf.is.odysseus.action.services/src/de/uniol/inf/is/odysseus/action/services/Activator.java
+++ action/de.uniol.inf.is.odysseus.action.services/src/de/uniol/inf/is/odysseus/action/services/Activator.java
@@ -4,7 +4,7 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
-
+	private static BundleContext context;
 	private ClassLoader contextClassLoader;
 
 	/*
@@ -14,6 +14,8 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext context) throws Exception {
 		//save classloader because dynamic client factory overrides it!
 		this.contextClassLoader = Thread.currentThread().getContextClassLoader();
+		
+		Activator.context = context;
 	}
 
 	/*
@@ -24,6 +26,13 @@ public class Activator implements BundleActivator {
 		if (this.contextClassLoader != null){
 			Thread.currentThread().setContextClassLoader(this.contextClassLoader);
 		}
+		
+		context = null;
+	}
+	
+	
+	public static BundleContext getContext() {
+		return context;
 	}
 
 }
