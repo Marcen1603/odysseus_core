83e4671c37e7a191854d0166cb6c8ecf672532d4
Dennis Geesen <geesen@indivon.de>
2010-03-24 14:43:00 +0000

    siehe vorherige
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4831 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3a5bb5f655475b17c32e586cd49d612ca3c644fb

--- broker/de.uniol.inf.is.odysseus.broker.parser.cql/src/de/uniol/inf/is/odysseus/broker/parser/cql/BrokerVisitor.java
+++ broker/de.uniol.inf.is.odysseus.broker.parser.cql/src/de/uniol/inf/is/odysseus/broker/parser/cql/BrokerVisitor.java
@@ -15,13 +15,17 @@ import de.uniol.inf.is.odysseus.parser.cql.CQLParser;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTAttributeDefinition;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTAttributeDefinitions;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTAttributeType;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTBrokerAsSource;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTBrokerQueue;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTBrokerSelectInto;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTBrokerSimpleSource;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTBrokerSource;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTComplexSelectStatement;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTCreateBroker;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTIdentifier;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTSelectStatement;
 import de.uniol.inf.is.odysseus.parser.cql.parser.ASTSimpleSource;
+import de.uniol.inf.is.odysseus.parser.cql.parser.Node;
 import de.uniol.inf.is.odysseus.parser.cql.parser.transformation.AbstractDefaultVisitor;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
@@ -32,38 +36,61 @@ public class BrokerVisitor extends AbstractDefaultVisitor {
 
 	@Override
 	public Object visit(ASTBrokerSource node, Object data) {
-		// ASTIdentifier ident = (ASTIdentifier) node.jjtGetChild(0);
-		// String name = ident.getName();
-		//
-		// // parse the nested substatement
-		// ASTComplexSelectStatement childNode = (ASTComplexSelectStatement)
-		// node.jjtGetChild(1);
-		// CQLParser v = new CQLParser();
-		// AbstractLogicalOperator result = (AbstractLogicalOperator)
-		// v.visit(childNode, null);
-		//
-		// BrokerAO broker = BrokerAOFactory.getFactory().createBrokerAO(name);
-		// broker.setOutputSchema(result.getOutputSchema());
-		// if (!BrokerDictionary.getInstance().brokerExists(name)) {
-		// BrokerDictionary.getInstance().addBroker(name,
-		// broker.getOutputSchema(), broker.getMetaInputSchema());
-		// }
-		//
-		// // connect the source to broker
-		// int inPort = BrokerDictionary.getInstance().getNextWritePort(name);
-		// broker.subscribeToSource(result, inPort, 0,
-		// result.getOutputSchema());
-		// // make it accessible like a normal source
-		// DataDictionary.getInstance().sourceTypeMap.put(name,
-		// "brokerStreaming");
-		// SDFEntity entity = new SDFEntity(name);
-		// entity.setAttributes(broker.getOutputSchema());
-		// DataDictionary.getInstance().entityMap.put(name, entity);
-		// return broker;
-		return null;
+		BrokerAO readFromBroker = null;
+		if (node.jjtGetChild(0) instanceof ASTBrokerSimpleSource) {
+			readFromBroker = (BrokerAO) visit((ASTBrokerSimpleSource) node.jjtGetChild(0), data);
+		} else if (node.jjtGetChild(0) instanceof ASTBrokerAsSource) {
+			readFromBroker = (BrokerAO) visit((ASTBrokerAsSource) node.jjtGetChild(0), data);
+		}
+		if (node.jjtGetChild(1) != null) {
+			if (node.jjtGetChild(1) instanceof ASTBrokerQueue) {
+				// TODO add queue
+			}
+		}
+
+		return readFromBroker;
+
+	}
+
+	@Override
+	public Object visit(ASTBrokerSimpleSource node, Object data) {
+		return getSimpleSource(node, data);
+	}
+
+	@Override
+	public Object visit(ASTBrokerAsSource node, Object data) {
+		ASTIdentifier ident = (ASTIdentifier) node.jjtGetChild(1);
+		String name = ident.getName();
+
+		// parse the nested substatement
+		ASTComplexSelectStatement childNode = (ASTComplexSelectStatement) node.jjtGetChild(0);
+		CQLParser v = new CQLParser();
+		AbstractLogicalOperator result = (AbstractLogicalOperator) v.visit(childNode, null);
+
+		BrokerAO broker = BrokerAOFactory.getFactory().createBrokerAO(name);
+		broker.setSchema(result.getOutputSchema());
+		if (!BrokerDictionary.getInstance().brokerExists(name)) {
+			BrokerDictionary.getInstance().addBroker(name, broker.getOutputSchema(), broker.getQueueSchema());
+		}
+
+		// connect the source to broker		
+		int dataPort = BrokerDictionary.getInstance().addNewTransaction(name, WriteTransaction.Continuous);
+		broker.subscribeToSource(result, dataPort, 0, result.getOutputSchema());
+		// make it accessible like a normal source
+		DataDictionary.getInstance().sourceTypeMap.put(name, "brokerStreaming");
+		SDFEntity entity = new SDFEntity(name);
+		entity.setAttributes(broker.getOutputSchema());
+		DataDictionary.getInstance().entityMap.put(name, entity);
+		return broker;
+
 	}
 
 	public Object visit(ASTSimpleSource node, Object data) {
+		return getSimpleSource(node, data);
+
+	}
+
+	private Object getSimpleSource(Node node, Object data) {
 		String brokerName = ((ASTIdentifier) node.jjtGetChild(0)).getName();
 		if (BrokerDictionary.getInstance().brokerExists(brokerName)) {
 			BrokerAO broker = BrokerAOFactory.getFactory().createBrokerAO(brokerName);
@@ -71,7 +98,6 @@ public class BrokerVisitor extends AbstractDefaultVisitor {
 		} else {
 			throw new RuntimeException("Broker " + brokerName + " not exists");
 		}
-
 	}
 
 	public Object visit(ASTBrokerSelectInto node, Object data) {
@@ -86,10 +112,7 @@ public class BrokerVisitor extends AbstractDefaultVisitor {
 
 		// add rest from select except of the last one if it's a complex
 		// statement
-		int numChilds = node.jjtGetNumChildren();
-		if (node.jjtGetChild(node.jjtGetNumChildren() - 1) instanceof ASTComplexSelectStatement) {
-			numChilds--;
-		}
+		int numChilds = node.jjtGetNumChildren();	
 		for (int i = 2; i < numChilds; i++) {
 			statement.jjtAddChild(node.jjtGetChild(i), number);
 			number++;
@@ -97,69 +120,48 @@ public class BrokerVisitor extends AbstractDefaultVisitor {
 		// parse first nested statement
 		CQLParser v = new CQLParser();
 		AbstractLogicalOperator topOfSelectStatementOperator = (AbstractLogicalOperator) v.visit(statement, null);
-		AbstractLogicalOperator topOfQueueSelectOperator = null;
-		if (node.jjtGetChild(node.jjtGetNumChildren() - 1) instanceof ASTComplexSelectStatement) {
-			// parse second nested statement
-
-			ASTComplexSelectStatement metaStatement = (ASTComplexSelectStatement) node.jjtGetChild(node.jjtGetNumChildren() - 1);
-			v = new CQLParser();
-			topOfQueueSelectOperator = (AbstractLogicalOperator) v.visit(metaStatement, null);
-		}
 
 		BrokerAO broker = BrokerAOFactory.getFactory().createBrokerAO(brokerName);
 		// add the schemas from existing one
 		if (!BrokerDictionary.getInstance().brokerExists(brokerName)) {
 			throw new RuntimeException("Broker with name \"" + brokerName + "\" not found. You have to create one first.");
-		}		
+		}
 		broker.setSchema(BrokerDictionary.getInstance().getSchema(brokerName));
 		broker.setQueueSchema(BrokerDictionary.getInstance().getQueueSchema(brokerName));
 
-		// check incoming and outgoing subs
-		//boolean readingFromAnyBroker = false;
 		boolean readingFromOwnBroker = false;
-		boolean isQueueWriting = false;
 
-		List<BrokerAO> readingFromBrokers = GraphUtils.getReadingFromBrokers(topOfSelectStatementOperator);		
-//		if (!readingFromBrokers.isEmpty()) {
-//			readingFromAnyBroker = true;
-//		}
+		List<BrokerAO> readingFromBrokers = GraphUtils.getReadingFromBrokers(topOfSelectStatementOperator);
+
 		for (BrokerAO b : readingFromBrokers) {
 			if (b.getIdentifier().equals(broker.getIdentifier())) {
 				readingFromOwnBroker = true;
 			}
 		}
 
-		if (topOfQueueSelectOperator != null) {
-			isQueueWriting = true;
-		}
-
-		// the writing type for the queue is always Timestamp
-		WriteTransaction queueWritingType = WriteTransaction.Timestamp;
 		// a normal writing transaction is continuous
 		WriteTransaction dataWritingType = WriteTransaction.Continuous;
 		// if i read from my own operator, it is a cyclic transaction
-		if (readingFromOwnBroker && isQueueWriting) {
+		if (readingFromOwnBroker) {
 			dataWritingType = WriteTransaction.Cyclic;
 		}
-		int mappedReadingPort = 0;
-		//remap all reading ports so that everyone has its own one 
-		for(BrokerAO readFromBroker : readingFromBrokers){					
+		// remap all reading ports so that everyone has its own one
+		for (BrokerAO readFromBroker : readingFromBrokers) {
 			ReadTransaction dataReadTransaction = ReadTransaction.Continuous;
-			//if i read from my own one -> it is cyclic
-			if(readFromBroker.getIdentifier().equals(brokerName)){
-				dataReadTransaction = ReadTransaction.Cyclic;				
+			// if i read from my own one -> it is cyclic
+			if (readFromBroker.getIdentifier().equals(brokerName)) {
+				dataReadTransaction = ReadTransaction.Cyclic;
 			}
-			// this is a SELECT INTO statement, so that there is automatically a writing transaction
-			// if there would not be a writing transaction, then it is necessary to distinguish between
-			// reading without and with queue (=> OneTime Read instead of cyclic read)
+			// this is a SELECT INTO statement, so that there is automatically a
+			// writing transaction
+			// if there would not be a writing transaction, then it is necessary
+			// to distinguish between
+			// reading without and with queue (=> OneTime Read instead of cyclic
+			// read)
 			int nextPort = BrokerDictionary.getInstance().addNewTransaction(readFromBroker.getIdentifier(), dataReadTransaction);
-			for(LogicalSubscription sub : readFromBroker.getSubscriptions()){				
-				broker.unsubscribeSink(sub.getTarget(), sub.getSinkInPort(), sub.getSourceOutPort(), sub.getSchema());			
-				broker.subscribeSink(sub.getTarget(), sub.getSinkInPort(), nextPort, sub.getSchema());			
-			}	
-			// if it is cyclic reading, the reading port belongs to the writing port
-			if(dataReadTransaction == ReadTransaction.Cyclic){
-				mappedReadingPort = nextPort;
+			for (LogicalSubscription sub : readFromBroker.getSubscriptions()) {
+				broker.unsubscribeSink(sub.getTarget(), sub.getSinkInPort(), sub.getSourceOutPort(), sub.getSchema());
+				broker.subscribeSink(sub.getTarget(), sub.getSinkInPort(), nextPort, sub.getSchema());
 			}
 		}
 
@@ -169,26 +171,9 @@ public class BrokerVisitor extends AbstractDefaultVisitor {
 			message = message + "Schema of Broker: " + broker.getOutputSchema().toString();
 			throw new RuntimeException("Statement and broker do not have the same schema.\n" + message);
 		}
-		// check, if it is an meta-channel nested select
-		if (topOfQueueSelectOperator != null) {
-			if (!schemaEquals(topOfQueueSelectOperator.getOutputSchema(), broker.getQueueSchema())) {
-				String message = "Schema to insert: " + topOfSelectStatementOperator.getOutputSchema().toString() + "\n";
-				message = message + "Queueschema of Broker: " + broker.getQueueSchema().toString();
-				throw new RuntimeException("Queue statement and broker do not have the same schema.\n" + message);
-			}
-		}
 		// connect the nested statement into the broker
 		int dataPort = BrokerDictionary.getInstance().addNewTransaction(brokerName, dataWritingType);
 		broker.subscribeToSource(topOfSelectStatementOperator, dataPort, 0, topOfSelectStatementOperator.getOutputSchema());
-
-		// if there is a queue statement, connect it
-		if (topOfQueueSelectOperator != null) {
-			int queuePort = BrokerDictionary.getInstance().addNewTransaction(brokerName, queueWritingType);
-			broker.subscribeToSource(topOfQueueSelectOperator, queuePort, 0, topOfQueueSelectOperator.getOutputSchema());
-			// remember the connection between reading port and queuePort
-			BrokerDictionary.getInstance().addNewTransactionMapping(brokerName, mappedReadingPort, queuePort);
-		}
-
 		return broker;
 
 	}
@@ -264,5 +249,5 @@ public class BrokerVisitor extends AbstractDefaultVisitor {
 			}
 			return true;
 		}
-	}
+	}	
 }
