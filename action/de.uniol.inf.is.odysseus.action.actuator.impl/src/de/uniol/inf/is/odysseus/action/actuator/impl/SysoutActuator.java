508d3efab4bd57fdab0922d5c9085cd8bd6fc34a
Simon Flandergan <flandergan@uni-oldenburg.de>
2010-04-13 09:56:09 +0000

    kleine aenderungen
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4872 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 556dd0f8919503c285204ab264a5d6167846fad5

---
+++ action/de.uniol.inf.is.odysseus.action.actuator.impl/src/de/uniol/inf/is/odysseus/action/actuator/impl/SysoutActuator.java
@@ -0,0 +1,17 @@
+package de.uniol.inf.is.odysseus.action.actuator.impl;
+
+public class SysoutActuator {
+	
+	public void out(String in){
+		System.out.println(in);
+	}
+	
+	public void out(int in){
+		System.out.println(in);
+	}
+	
+	public void out(double in){
+		System.out.println(in);
+	}
+
+}
