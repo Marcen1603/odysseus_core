9c75cd8a8f3e7e350573cf29cab4d9e8f9ff88c2
Christian Kuka <christian@kuka.cc>
2010-05-21 07:17:01 +0000

    Migration: final migration script
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@4976 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: faea8aa51200474d7acb7828c026745046d7cb51

--- 3rdParty/maven/migrate.sh
+++ 3rdParty/maven/migrate.sh
@@ -1,20 +1,62 @@
 #!/bin/sh
 
+
+echo "Are we in odysseus root directory? (y/n)"
+read yesno
+if [ yesno != "y" ]; then
+    exit 1
+fi
+
 git checkout master
 git branch next
 git checkout next
 git branch migration
 git checkout migration
 
-tryRun="-n"
-
 partyDir="3rdParty/maven"
-echo "cp -uR $partyDir/odysseus-parent ."
-folders="action application base broker cep metadata metadata/pn metadata/interval metadata/priority new_transformation p2p p2p/adminpeer p2p/base p2p/distribution p2p/execution p2p/jxta p2p/operatorpeer p2p/splitting p2p/superpeer p2p/thinpeer planmanagement pql rcp relational scheduler sensor viewer webservice"
+cp -uR $partyDir/odysseus-parent .
+git add odysseus-parent/pom.xml
+
+folders=". action application base broker cep metadata metadata/pn metadata/interval metadata/priority new_transformation p2p p2p/adminpeer p2p/base p2p/distribution p2p/execution p2p/jxta p2p/operatorpeer p2p/splitting p2p/superpeer p2p/thinpeer planmanagement pql rcp relational scheduler sensor viewer webservice"
+
+cat > pom.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<project
+   xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"
+   xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <modelVersion>4.0.0</modelVersion>
+  <parent>
+    <groupId>de.uniol.inf.is.odysseus</groupId>
+    <artifactId>odysseus-pom</artifactId>
+    <version>1.0-SNAPSHOT</version>
+  </parent>
+  <artifactId>odysseus-parent</artifactId>
+  <packaging>pom</packaging>
+  <name>Odysseus</name>
+
+  <modules>
+EOF
 
 for f in $folders; do
     mkdir -p $f
     module=$(echo $f | tr "/" "-")
+    cat > $f/pom.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<project
+   xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"
+   xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <modelVersion>4.0.0</modelVersion>
+  <parent>
+    <groupId>de.uniol.inf.is.odysseus</groupId>
+    <artifactId>odysseus-pom</artifactId>
+    <version>1.0-SNAPSHOT</version>
+  </parent>
+  <artifactId>odysseus-$module-parent</artifactId>
+  <packaging>pom</packaging>
+  <name>Odysseus :: $module</name>
+
+  <modules>
+EOF
     for s in $(find $f -mindepth 1 -maxdepth 1 -type d -name 'de.uniol*'); do
 	basename=${s##*/}
 	isin=0
@@ -29,12 +71,13 @@ for f in $folders; do
 	    artifactId=$(echo $artifactId | tr "." "-")
 	    artifactId=$(echo $artifactId | tr "/" "-")
 	    if [ -d $s/src ]; then
+		echo "<module>odysseus-$artifactId</module>" >> $f/pom.xml
 		mkdir -p $f/odysseus-$artifactId/src/main/java
 		mkdir -p $f/odysseus-$artifactId/src/main/resources
 		mkdir -p $f/odysseus-$artifactId/src/test/java
 		mkdir -p $f/odysseus-$artifactId/src/test/resources
 		if [ -d $partyDir/$f/odysseus-$artifactId ]; then
-		    echo cp -u $partyDir/$f/odysseus-$artifactId/pom.xml $f/odysseus-$artifactId/
+		    cp -u $partyDir/$f/odysseus-$artifactId/pom.xml $f/odysseus-$artifactId/
 		else
 		    echo "Generate pom"
 		    cat > $f/odysseus-$artifactId/pom.xml <<EOF
@@ -102,8 +145,12 @@ EOF
 	    fi
 	fi
     done
+    cat >> $f/pom.xml <<EOF
+  </modules>
+</project>
+EOF
+    git add $f/pom.xml
 done
-
 mkdir -p resources/drools-pathscanner/src/main/java
 mkdir -p resources/drools-pathscanner/src/main/resources
 mkdir -p resources/drools-pathscanner/src/test/java
@@ -128,19 +175,55 @@ git mv -kf resources/de.uniol.org.drools/src/* resources/drools/src/main/java/
 cp -u $partyDir/resources/drools/pom.xml resources/drools/
 git add resources/drools/pom.xml
 
-rm -rf scheduler/odysseus-scheduler
-rm -rf scheduler/odysseus-scheduler-singlethreadscheduler
-
 git mv -kf scheduler/odysseus-scheduler2-0 scheduler/odysseus-scheduler
 git mv -kf scheduler/odysseus-scheduler-singlethreadscheduler2-0 scheduler/odysseus-scheduler-singlethreadscheduler
-cp -u $partyDir/scheduler/odysseus-scheduler/pom.xml scheduler/odysseus-scheduler/
-cp -u $partyDir/scheduler/odysseus-scheduler-singlethreadscheduler/pom.xml scheduler/odysseus-scheduler-singlethreadscheduler
+cp $partyDir/scheduler/odysseus-scheduler/pom.xml scheduler/odysseus-scheduler/
+cp $partyDir/scheduler/odysseus-scheduler-singlethreadscheduler/pom.xml scheduler/odysseus-scheduler-singlethreadscheduler
 git add scheduler/odysseus-scheduler/pom.xml
 git add scheduler/odysseus-scheduler-singlethreadscheduler/pom.xml
+
+    cat > resources/pom.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<project
+   xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"
+   xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <modelVersion>4.0.0</modelVersion>
+  <parent>
+    <groupId>de.uniol.inf.is.odysseus</groupId>
+    <artifactId>odysseus-pom</artifactId>
+    <version>1.0-SNAPSHOT</version>
+  </parent>
+  <artifactId>odysseus-$module-parent</artifactId>
+  <packaging>pom</packaging>
+  <name>Odysseus :: $module</name>
+
+  <modules>
+   <module>drools</module>
+   <module>drools-ruleagent</module>
+   <module>drools-pathscanner</module>
+  </modules>
+</project>
+EOF
+git add resources/pom.xml
+
+for s in $(find $f -mindepth 1 -maxdepth 1 -type f -name 'pom.xml'); do
+    basename=${s%/*}
+    echo "<module>$basename</module>" >> pom.xml
+done
+
+cat >> pom.xml <<EOF
+  </modules>
+</project>
+EOF
+git add pom.xml
 
 echo "Migration processed. Ready to merge? (y/n)"
 read yesno
 if [ $yesno = "y" ]; then
+    git commit -m
     git checkout next
     git merge migration
+else
+    git checkout master
+    git branch -d migration
 fi
