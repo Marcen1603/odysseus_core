4b0c12522e445ae24bf5a16d819d781ec8120228
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-05-28 13:21:18 +0000

    RCP: Unnütze imports entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5048 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 11ac8b7f7c789b63a5169eff44ee83e42e62af3c

--- rcp/de.uniol.inf.is.odysseus.rcp.starter/src/de/uniol/inf/is/odysseus/rcp/application/ViewerPerspective.java
+++ rcp/de.uniol.inf.is.odysseus.rcp.starter/src/de/uniol/inf/is/odysseus/rcp/application/ViewerPerspective.java
@@ -1,15 +1,8 @@
 package de.uniol.inf.is.odysseus.rcp.application;
 
-import java.io.PrintStream;
-
-import org.apache.log4j.Logger;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-import org.eclipse.ui.console.ConsolePlugin;
-import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.IConsoleConstants;
-import org.eclipse.ui.console.MessageConsole;
-import org.eclipse.ui.console.MessageConsoleStream;
 
 public class ViewerPerspective implements IPerspectiveFactory {
 
