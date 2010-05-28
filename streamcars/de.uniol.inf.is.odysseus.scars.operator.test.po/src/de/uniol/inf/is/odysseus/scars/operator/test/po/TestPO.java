c33e8a4768d2c5e9c666243f0403b463519414e2
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-05-28 07:16:01 +0000

    Vergessen, die Testoperatoren einzuchecken
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5032 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 300f75cf871d942991098325a590a085a627b69f

---
+++ streamcars/de.uniol.inf.is.odysseus.scars.operator.test.po/src/de/uniol/inf/is/odysseus/scars/operator/test/po/TestPO.java
@@ -0,0 +1,30 @@
+package de.uniol.inf.is.odysseus.scars.operator.test.po;
+
+import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
+
+public class TestPO<T> extends AbstractPipe<T, T>{
+
+	public TestPO() {
+		
+	}
+	
+	public TestPO(TestPO<T> copy ) {
+		
+	}
+	
+	@Override
+	public OutputMode getOutputMode() {
+		return OutputMode.INPUT;
+	}
+
+	@Override
+	protected void process_next(T object, int port) {
+		System.out.println("TESTPO: Dataelement processed : " + object);
+		transfer(object);
+	}
+
+	@Override
+	public AbstractPipe<T, T> clone() throws CloneNotSupportedException {
+		return new TestPO<T>(this);
+	}
+}
