7e712eb0969f188c60b0ea4d9bde0b74cf95436b
Niko Klein <nklein@uni-oldenburg.de>
2010-05-31 13:14:47 +0000

    Abhängikeiten geändert. Prediction-Algorithmus-Test angepasst.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5061 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 73f155ce94e4be8f458dba60ae601767b83ba24a

--- streamcars/de.uniol.inf.is.odysseus.scars.testcases/src/de/uniol/inf/is/odysseus/testcases/paf_tests/PredictionTest.java
+++ streamcars/de.uniol.inf.is.odysseus.scars.testcases/src/de/uniol/inf/is/odysseus/testcases/paf_tests/PredictionTest.java
@@ -1,6 +1,6 @@
 package de.uniol.inf.is.odysseus.testcases.paf_tests;
 
-import javax.vecmath.Vector3d;
+import org.apache.commons.math.geometry.Vector3D;
 
 import junit.framework.TestCase;
 import de.uniol.inf.is.odysseus.prediction.DefaultPredictionAlgorithmus;
@@ -9,7 +9,7 @@ import de.uniol.inf.is.odysseus.prediction.TestObject;
 public class PredictionTest extends TestCase {
 	
 	public void testPredAlgo() {
-		TestObject obj = new TestObject(5, new Vector3d(10, 20, 0), new Vector3d(1, 0, 0), 5);
+		TestObject obj = new TestObject(5, new Vector3D(10, 20, 0), new Vector3D(1, 0, 0), 5);
 		DefaultPredictionAlgorithmus prdAlgo = new DefaultPredictionAlgorithmus(obj, 10);
 		TestObject newObj = prdAlgo.executePredAlgorithmus();
 		
@@ -17,9 +17,9 @@ public class PredictionTest extends TestCase {
 		assertEquals(newObj.getDirection(), obj.getDirection());
 		assertEquals(newObj.getTimeStamp(), obj.getTimeStamp());
 		
-		assertEquals(35.0d, newObj.getPosition().x);
-		assertEquals(20.0d, newObj.getPosition().y);
-		assertEquals(0.0d, newObj.getPosition().z);
+		assertEquals(35.0d, newObj.getPosition().getX());
+		assertEquals(20.0d, newObj.getPosition().getY());
+		assertEquals(0.0d, newObj.getPosition().getZ());
 		
 	}
 	
