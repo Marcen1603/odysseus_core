24d5ec7a39ce7de2e4ac7518c68f3e2b0bc3e75c
Benjamin Gruenebast <bgruenebast@uni-oldenburg.de>
2010-06-07 12:31:36 +0000

    clone methode fehler behoben
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5126 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1cda92ebd59440a3d4eb5a1e00d47a159486bced

--- streamcars/de.uniol.inf.is.odysseus.scars.base/src/de/uniol/inf/is/odysseus/scars/base/physicaloperator/JDVEDataInputStreamAccessMVPO.java
+++ streamcars/de.uniol.inf.is.odysseus.scars.base/src/de/uniol/inf/is/odysseus/scars/base/physicaloperator/JDVEDataInputStreamAccessMVPO.java
@@ -4,6 +4,7 @@ import de.uniol.inf.is.odysseus.base.OpenFailedException;
 import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
 import de.uniol.inf.is.odysseus.objecttracking.physicaloperator.access.AbstractSensorAccessPO;
+import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractSource;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
 public class JDVEDataInputStreamAccessMVPO <M extends IProbability> extends AbstractSensorAccessPO<MVRelationalTuple<M>, M> {
@@ -29,5 +30,11 @@ public class JDVEDataInputStreamAccessMVPO <M extends IProbability> extends Abst
 
 	@Override
 	public void transferNext() {
+	}
+
+	@Override
+	public AbstractSource<MVRelationalTuple<M>> clone() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
