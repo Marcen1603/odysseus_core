ab5150e984ab70635806b14344f3b2a47eedf355
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-06-09 08:08:32 +0000

    Removed unused package
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5164 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1036f8b50a46371b64009d4ba26570412ba0b843

--- planmanagement/de.uniol.inf.is.odysseus.planmanagement.optimization/src/de/uniol/inf/is/odysseus/planmanagement/optimization/AbstractOptimizer.java
+++ planmanagement/de.uniol.inf.is.odysseus.planmanagement.optimization/src/de/uniol/inf/is/odysseus/planmanagement/optimization/AbstractOptimizer.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
