8adea5215445721d1ade977326c1d2336e875965
Jan Poloczek <jpoloczek@uni-oldenburg.de>
2010-06-09 09:22:31 +0000

    added all test suite for unnest po; and updated test hierachy.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5175 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 49f601622b5b039d84591ce5e3d2f3d4cebd538d

---
+++ or/de.uniol.inf.is.odysseus.physicaloperator.objectrelational/src/de/uniol/inf/is/odysseus/physicaloperator/objectrelational/test/TestAllTests.java
@@ -0,0 +1,14 @@
+package de.uniol.inf.is.odysseus.physicaloperator.objectrelational.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    NestPOTestAllTests.class,
+    UnnestPOTestAllTests.class
+})
+
+public class TestAllTests {
+}
+
