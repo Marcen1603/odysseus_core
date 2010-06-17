dd91f98e254752f8dd7bf3f25171a7c3b41fb3de
Christian Kuka <christian@kuka.cc>
2010-06-17 11:03:55 +0000

    Migration: fix duplicate dependencies
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5247 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b02101cb0605fa330e21fb2f064bb0dc2f290130

--- 3rdParty/maven/createpoms.rb
+++ 3rdParty/maven/createpoms.rb
@@ -1,14 +1,108 @@
 #!/usr/bin/env ruby
 
+class Ext
+  attr_reader :artifactId, :groupId, :version
+  def initialize artifactId, groupId, version
+    @artifactId = artifactId
+    @groupId = groupId
+    @version = version
+  end
+
+  def  to_s
+    "<dependency><groupId>#{@groupId}</groupId><artifactId>#{@artifactId}</artifactId><version>#{@version}</verion></dependency>"
+  end
+
+end
 class ProjectDependencyGenerator
 public
-	attr_reader :projectDependencies, :unresolvedDependencies
-	
+	attr_reader :projectDependencies, :unresolvedDependencies, :unknownDepAr, :externalDependencies
+
 	def parse(dir)
 		@packages = Hash.new
-		@manifestImports =  Hash.new
-		@unresolvedDependencies = Hash.new
-		findSrc(dir, "")
+      @manifestImports =  Hash.new
+      @unresolvedDependencies = Hash.new
+      @unknownDepAr = Array.new
+      @externalDependencies = {
+        "org.osgi.framework" => "<dependency><groupId>org.osgi</groupId><artifactId>org.osgi.core</artifactId><version>4.2.0</version></dependency>",
+		"org.osgi.service.component" => "<dependency><groupId>org.osgi</groupId><artifactId>org.osgi.core</artifactId><version>4.2.0</version></dependency>",
+	"org.osgi.util.tracker" => "<dependency><groupId>org.osgi</groupId><artifactId>org.osgi.core</artifactId><version>4.2.0</version></dependency>",
+"org.osgi.service.framework" => "<dependency><groupId>org.osgi</groupId><artifactId>org.osgi.core</artifactId><version>4.2.0</version></dependency>",
+"org.simpleframework.xml" => "<dependency><groupId>org.simpleframework</groupId><artifactId>simple-xml</artifactId><version>2.3.5</version></dependency>",
+"org.simpleframework.xml.core" => "<dependency><groupId>org.simpleframework</groupId><artifactId>simple-xml</artifactId><version>2.3.5</version></dependency>",
+"org.apache.commons.math" => "<dependency><groupId>org.apache.commons</groupId><artifactId>commons-math</artifactId><version>2.1</version></dependency>",
+"org.apache.commons.math.linear" => "<dependency><groupId>org.apache.commons</groupId><artifactId>commons-math</artifactId><version>2.1</version></dependency>",
+"org.slf4j" => "<dependency><groupId>org.slf4j</groupId><artifactId>com.springsource.slf4j.api</artifactId></dependency>",
+"net.jxta" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.discovery" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.endpoint" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.document" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.exception" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.id" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.impl.peer" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.peer" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.peergroup" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.pipe" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.platform" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.protocol" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.socket" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.util" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+
+"net.jxta.access" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.codat" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.content" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.credential" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.id.jxta" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.logging" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.membership" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.meta" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.rendezvous" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.resolver" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.service" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+"net.jxta.util.documentSerializable" => "<dependency><groupId>jxta</groupId><artifactId>jxta</artifactId><version>2.0</version></dependency>",
+
+
+
+
+
+
+"com.Ostermiller.util" => "<dependency><groupId>org.ostermiller</groupId><artifactId>utils</artifactId><version>1.07.00</version></dependency>",
+
+"org.eclipse.swt" => "<dependency><groupId>org.eclipse</groupId><artifactId>swt</artifactId><version>3.3.0-v3346</version></dependency>",
+"org.eclipse.swt.custom" => "<dependency><groupId>org.eclipse</groupId><artifactId>swt</artifactId><version>3.3.0-v3346</version></dependency>",
+"org.eclipse.swt.events" => "<dependency><groupId>org.eclipse</groupId><artifactId>swt</artifactId><version>3.3.0-v3346</version></dependency>",
+"org.eclipse.swt.graphics" => "<dependency><groupId>org.eclipse</groupId><artifactId>swt</artifactId><version>3.3.0-v3346</version></dependency>",
+"org.eclipse.swt.layout" => "<dependency><groupId>org.eclipse</groupId><artifactId>swt</artifactId><version>3.3.0-v3346</version></dependency>",
+"org.eclipse.swt.widgets" => "<dependency><groupId>org.eclipse</groupId><artifactId>swt</artifactId><version>3.3.0-v3346</version></dependency>",
+
+"org.eclipse.equinox.app" => "<dependency><groupId>org.eclipse.equinox</groupId><artifactId>app</artifactId></dependency>",
+"org.eclipse.osgi.framework.console" => "<dependency><groupId>org.eclipse.osgi</groupId><artifactId>osgi</artifactId></dependency>",
+"org.osgi.service.prefs" => "<dependency><groupId>org.osgi</groupId><artifactId>org.osgi.core</artifactId></dependency>",
+
+"org.eclipse.ui.plugin" => "<dependency><groupId>org.eclipse</groupId><artifactId>eclipse-platform</artifactId><version>3.5.1</version></dependency>",
+"org.eclipse.core.runtime" => "<dependency><groupId>org.eclipse</groupId><artifactId>eclipse-platform</artifactId><version>3.5.1</version></dependency>",
+"org.eclipse.core.commands" => "<dependency><groupId>org.eclipse</groupId><artifactId>eclipse-platform</artifactId><version>3.5.1</version></dependency>",
+"com.hp.hpl.jena.ontology" => "<dependency><groupId>com.hp.hpl.jena</groupId><artifactId>jena</artifactId><version>2.6.3</version></dependency>",
+"com.hp.hpl.jena.rdf.model" => "<dependency><groupId>com.hp.hpl.jena</groupId><artifactId>jena</artifactId><version>2.6.3</version></dependency>",
+"com.hp.hpl.jena.util" => "<dependency><groupId>com.hp.hpl.jena</groupId><artifactId>jena</artifactId><version>2.6.3</version></dependency>",
+"com.hp.hpl.jena.util.iterator" => "<dependency><groupId>com.hp.hpl.jena</groupId><artifactId>jena</artifactId><version>2.6.3</version></dependency>",
+
+"org.openrdf" => "<dependency>    <groupId>org.openrdf</groupId>    <artifactId>sesame</artifactId>    <version>1.2.7</version></dependency>",
+
+
+"org.slf4j.impl" => "<dependency><groupId>org.slf4j</groupId><artifactId>com.springsource.slf4j.log4j</artifactId></dependency>",
+        "org.apache.log4j" => "<dependency>        <groupId>org.apache.log4j</groupId>        <artifactId>com.springsource.org.apache.log4j</artifactId></dependency>",
+        "org.apache.log4j.spi" => "<dependency>        <groupId>org.apache.log4j</groupId>        <artifactId>com.springsource.org.apache.log4j</artifactId></dependency>",
+
+
+"org.apache.cxf.binding" => "<dependency>    <groupId>org.apache.cxf</groupId>    <artifactId>apache-cxf</artifactId>    <version>2.2.9</version></dependency>",
+"org.apache.cxf.endpoint" => "<dependency>    <groupId>org.apache.cxf</groupId>    <artifactId>apache-cxf</artifactId>    <version>2.2.9</version></dependency>",
+"org.apache.cxf.endpoint.dynamic" => "<dependency>    <groupId>org.apache.cxf</groupId>    <artifactId>apache-cxf</artifactId>    <version>2.2.9</version></dependency>",
+"org.apache.cxf.jaxws.endpoint.dynamic" => "<dependency>    <groupId>org.apache.cxf</groupId>    <artifactId>apache-cxf</artifactId>    <version>2.2.9</version></dependency>",
+"org.apache.cxf.service" => "<dependency>    <groupId>org.apache.cxf</groupId>    <artifactId>apache-cxf</artifactId>    <version>2.2.9</version></dependency>",
+"org.apache.cxf.service.model" => "<dependency>    <groupId>org.apache.cxf</groupId>    <artifactId>apache-cxf</artifactId>    <version>2.2.9</version></dependency>",
+        "org.junit" => "      <dependency>            <groupId>junit</groupId>            <artifactId>junit</artifactId></dependency>"}
+
+        findSrc(dir, "")
 		buildDependencies
 		return @projectDependencies
 	end
@@ -19,24 +113,26 @@ private
 		@manifestImports.each do |project, packages|
 			bundles = Array.new
 			unknownDependencies = Array.new
-			packages.each do |package| 
+			packages.each do |package|
 				bundle = @packages[package]
 				if bundle.nil?
 					unknownDependencies << package
+					unknownDepAr << package
 					next
 				end
 				next if bundle == project
 				bundles << bundle
-				
+
 			end
 			#puts "unknown import for project #{project}: #{unknownDependencies.join(', ')}"
 			@projectDependencies[project] = bundles.uniq
+			@unknownDepAr.uniq!
 			@unresolvedDependencies[project] = unknownDependencies
 		end
 	end
-	
+
 	def getPackages(project, prefix, dir)
-		Dir.entries(prefix + "/" + dir).each do 
+		Dir.entries(prefix + "/" + dir).each do
 			|curDirName|
 			next if (curDirName[0] ==  46) #46 = '.'
 			curPackage = dir.empty? ? curDirName : dir + "/" + curDirName
@@ -48,9 +144,9 @@ private
 			end
 		end
 	end
-	
+
 	def findSrc(path, lastdir)
-		Dir.entries(path).each do 
+		Dir.entries(path).each do
 			|curDir|
 			next if (curDir[0] == 46) #46 = '.'
 			curPath = path + "/" + curDir
@@ -67,7 +163,7 @@ private
 			end
 		end
 	end
-	
+
 	def importManifest(project, path)
 		file = File.new(path, 'r')
 		while not file.eof?
@@ -83,7 +179,7 @@ private
 			end
 		end
 	end
-	
+
 	def addImport(project, line)
 		match = (/\w+(\.(\w|_)+)*/.match(line))
 		unless match.nil?
@@ -109,6 +205,7 @@ end
 pdg = ProjectDependencyGenerator.new
 pdg.parse path
 
+#pdg.unknownDepAr.each{|pkg| puts pkg}
 #hier kommst du ...
 if pdg.projectDependencies[bundle]
 	puts "<dependencies>"
@@ -121,6 +218,15 @@ if pdg.projectDependencies[bundle]
 		puts "<version>${project.version}</version>"
        		 puts "</dependency>"
 	}
+  tmp = Array.new
+  pdg.unresolvedDependencies[bundle].each{|v|
+dep = pdg.externalDependencies[v]
+
+    tmp << dep unless dep.nil?
+}
+  tmp.uniq.each{|v|
+    puts v
+  }
 	puts "</dependencies>"
 end
 #pdg.projectDependencies = hashmap von bundle => <dependencies des bundles>
