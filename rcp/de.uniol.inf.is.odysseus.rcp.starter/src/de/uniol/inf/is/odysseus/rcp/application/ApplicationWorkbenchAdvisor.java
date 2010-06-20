9e11b2808ca7eaffbb9dadf9522ffa09535b4069
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-06-20 17:50:36 +0000

    RCP: Anpassungen  - Odysseus RCP launch-cfg angepasst (für neue Perspektive)  - MessageConsole wieder in StarterPlugin verschoben
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5284 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 971f3ca3565981a3787ffcd7972087ca65b625bc

--- rcp/de.uniol.inf.is.odysseus.rcp.starter/src/de/uniol/inf/is/odysseus/rcp/application/ApplicationWorkbenchAdvisor.java
+++ rcp/de.uniol.inf.is.odysseus.rcp.starter/src/de/uniol/inf/is/odysseus/rcp/application/ApplicationWorkbenchAdvisor.java
@@ -1,9 +1,16 @@
 package de.uniol.inf.is.odysseus.rcp.application;
 
+import java.io.PrintStream;
+
+import org.apache.log4j.Logger;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 import de.uniol.inf.is.odysseus.rcp.viewer.perspective.observer.IObserverPerspectiveConstants;
 
@@ -27,5 +34,23 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 	@Override
 	public void postStartup() {
 		super.postStartup();
+		
+		// Log4j-Ausgaben in Console umleiten
+		// Init der Message Console
+		MessageConsole log4jConsole = new MessageConsole("Log4J", null);
+		MessageConsoleStream log4jStream = log4jConsole.newMessageStream();
+		ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[] { log4jConsole });
+		
+		Logger logger = Logger.getRootLogger();
+		logger.addAppender(new MessageConsoleAppender(log4jStream));
+		
+		// Quelle: http://jprog.blogspot.com/2005/09/using-eclipse-console-view-in-rcp.html
+		//
+		MessageConsole myConsole = new MessageConsole("System.out", null); // declare console
+		ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[] { myConsole });
+		MessageConsoleStream stream = myConsole.newMessageStream();
+		PrintStream myS = new PrintStream(stream);
+		System.setOut(myS); // link standard output stream to the console
+		System.setErr(myS); // link error output stream to the console	
 	}
 }
