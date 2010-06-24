42bc3b10db3f1e4605063a1b252af0fcf3c0bbc2
Volker Janz <vjanz@uni-oldenburg.de>
2010-06-24 15:20:47 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5393 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: 62ef598162fc6d7c65a7284d57b89c7b6d78bfc7

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/CorrelationMatrixUtils.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/CorrelationMatrixUtils.java
@@ -1,35 +1,47 @@
 package de.uniol.inf.is.odysseus.assoziation;
 
+import java.util.List;
+
 import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
+import de.uniol.inf.is.odysseus.relational.base.RelationalTuple;
 import de.uniol.inf.is.odysseus.scars.objecttracking.OrAttributeResolver;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
 public class CorrelationMatrixUtils {
 	
-	/***
+	/*
 	 * This function generates a correlation matrix of a given scanlist. It is required that a HypothesisGeneration Operator added the connectionlist to the schema before.
-	 * @param scan Scan with added connection list
-	 * @param pathToConnectionList Path to connection list
-	 * @param pathToOldList Path to old list
-	 * @param pathToNewList Path to new list
-	 * @return
 	 */
-	public static int[][] encodeMatrix(MVRelationalTuple scan, int[] pathToConnectionList, int[] pathToOldList, int[] pathToNewList) {
-		MVRelationalTuple[] objConList = (MVRelationalTuple[]) ((MVRelationalTuple)OrAttributeResolver.resolveTuple(scan, pathToConnectionList)).getAttributes();
+	public static double[][] encodeMatrix(MVRelationalTuple scan, int[] pathToConList, int[] pathToOldList, int[] pathToNewList, int[] pathToConAttrOld, int[] pathToConAttrNew, int[] pathToConAttrRating, SDFAttributeList schema) {
+		MVRelationalTuple[] objConList = (MVRelationalTuple[]) ((MVRelationalTuple)OrAttributeResolver.resolveTuple(scan, pathToConList)).getAttributes();
 		MVRelationalTuple[] newList = (MVRelationalTuple[]) ((MVRelationalTuple)OrAttributeResolver.resolveTuple(scan, pathToNewList)).getAttributes();
 		MVRelationalTuple[] oldList = (MVRelationalTuple[]) ((MVRelationalTuple)OrAttributeResolver.resolveTuple(scan, pathToOldList)).getAttributes();
-		int[][] correlationMatrix = new int[oldList.length][newList.length];
-		/*
-		RatedPair[] ratedPairs = new RatedPair[objConList.length];
-		for(MVRelationalTuple tuple : objConList) {
-			ratedPairs[] = (RatedPair) OrAttributeResolver.resolveTuple(scan, pathToConnectionList);
+		
+		double[][] correlationMatrix = new double[newList.length][oldList.length];
+		
+		for(MVRelationalTuple connectionEntry : objConList) {
+			Object newRef = OrAttributeResolver.resolveTuple(connectionEntry, pathToConAttrNew);
+			Object oldRef = OrAttributeResolver.resolveTuple(connectionEntry, pathToConAttrOld);
+			Object conRating = OrAttributeResolver.resolveTuple(connectionEntry, pathToConAttrRating);
+			
+			int indexOfNewRef = OrAttributeResolver.indexOfAttribute((RelationalTuple<?>) OrAttributeResolver.resolveTuple(scan, pathToNewList), newRef);
+			int indexOfOldRef = OrAttributeResolver.indexOfAttribute((RelationalTuple<?>) OrAttributeResolver.resolveTuple(scan, pathToOldList), oldRef);
+			
+			correlationMatrix[indexOfNewRef][indexOfOldRef] = Double.parseDouble(conRating.toString());
 		}
-		for(RatedPair p : OrAttributeResolver.resolveTuple(scan, pathToConnectionList))
-		RatedPair pair = (RatedPair) OrAttributeResolver.resolveTuple(scan, pathToConnectionList);
-		*/
-		return null;
+		
+		return correlationMatrix;
 	}
 	
-	public static MVRelationalTuple decodeMatrix(int[][] correlationMatrix) {
+	public static MVRelationalTuple[] decodeMatrix(MVRelationalTuple[] newList, MVRelationalTuple[] oldList, int[][] correlationMatrix) {
+		List<MVRelationalTuple> updatedConnections;
+		for(int i = 0; i < newList.length; i++) {
+			for(int j = 0; j < oldList.length; j++) {
+				if(correlationMatrix[i][j] != 0) {
+					// TODO verbindung zur liste hinzufügen am ende in array umwandeln und zurückgeben
+				}
+			}
+		}
 		return null;
 	}
 
