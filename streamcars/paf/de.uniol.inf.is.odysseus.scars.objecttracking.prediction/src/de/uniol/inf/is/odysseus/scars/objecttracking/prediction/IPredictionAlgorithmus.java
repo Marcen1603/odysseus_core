bdf8f6db841573e8cf68393e67f65b35af63172e
Benjamin Gruenebast <bgruenebast@uni-oldenburg.de>
2010-06-24 11:26:03 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5380 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: 54e52d6b2e3b1850c807cf6c8f68c6befca2241c

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.prediction/src/de/uniol/inf/is/odysseus/scars/objecttracking/prediction/IPredictionAlgorithmus.java
+++
@@ -1,7 +0,0 @@
-package de.uniol.inf.is.odysseus.scars.objecttracking.prediction;
-
-public interface IPredictionAlgorithmus {
-	
-	public Object executePredAlgorithmus();
-
-}
--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.prediction/src/de/uniol/inf/is/odysseus/scars/objecttracking/prediction/IPredictionFunction.java
+++
@@ -1,5 +0,0 @@
-package de.uniol.inf.is.odysseus.scars.objecttracking.prediction;
-
-public interface IPredictionFunction {
-
-}
