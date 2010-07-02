24667d56fa945a44f173b3d93aab9f011e3c5e24
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-07-02 08:25:47 +0000

    RCP: Beispielextension für TransformationConfiguration in RCP
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5499 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 6d365de3cf8cc7ab24faf45ff83c0beab99abe0f

---
+++ viewer/de.uniol.inf.is.odysseus.rcp.viewer.objectfusionconfig/src/de/uniol/inf/is/odysseus/rcp/viewer/objectfusionconfig/ObjectFusionParameterTransformationConfiguration.java
@@ -0,0 +1,26 @@
+package de.uniol.inf.is.odysseus.rcp.viewer.objectfusionconfig;
+
+import de.uniol.inf.is.odysseus.base.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.base.planmanagement.query.querybuiltparameter.ParameterTransformationConfiguration;
+import de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval;
+import de.uniol.inf.is.odysseus.rcp.viewer.query.IParameterTransformationConfigurationExtension;
+import de.uniol.inf.is.odysseus.transformation.helper.broker.BrokerTransformationHelper;
+
+
+public class ObjectFusionParameterTransformationConfiguration implements IParameterTransformationConfigurationExtension {
+	
+	private static ParameterTransformationConfiguration trafoConfigParam = new ParameterTransformationConfiguration(
+			new TransformationConfiguration(
+			new BrokerTransformationHelper(),
+			"relational",
+			ITimeInterval.class.getName(),
+			"de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey",
+			"de.uniol.inf.is.odysseus.latency.ILatency",
+			"de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability"));
+
+	@Override
+	public ParameterTransformationConfiguration get() {
+		return trafoConfigParam;
+	}
+
+}
