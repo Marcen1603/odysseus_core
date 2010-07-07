eb43cc1cbdbbc6a2a1ec9b11ffa3c8ac423d4280
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-07-07 16:42:04 +0000

    Plugin nicht mehr notwendig
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5589 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9f112140aa2baffa5f984c253e31117b0c4c7032

--- viewer/de.uniol.inf.is.odysseus.rcp.editor.model/src/de/uniol/inf/is/odysseus/rcp/editor/model/cfg/IOperatorDescriptorProvider.java
+++
@@ -1,9 +0,0 @@
-package de.uniol.inf.is.odysseus.rcp.editor.model.cfg;
-
-import java.util.Collection;
-
-public interface IOperatorDescriptorProvider {
-
-	public Collection<IOperatorDescriptor> getOperatorDescriptors();
-	
-}
