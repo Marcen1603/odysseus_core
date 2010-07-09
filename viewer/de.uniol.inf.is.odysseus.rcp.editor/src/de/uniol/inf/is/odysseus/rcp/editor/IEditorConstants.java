c1e6435cfef73c8856f3967b691077f97507de5c
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-07-09 12:12:34 +0000

    Neues Plugin: "de.uniol.inf.is.odysseus.rcp.editor"  - beinhaltet den Editor, womit man grafisch logische pläne zusammenklicken kann (was noch nicht funktioniert)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5618 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4910467c54f99506811e327d2dc7b7da2ba90e67

---
+++ viewer/de.uniol.inf.is.odysseus.rcp.editor/src/de/uniol/inf/is/odysseus/rcp/editor/IEditorConstants.java
@@ -0,0 +1,8 @@
+package de.uniol.inf.is.odysseus.rcp.editor;
+
+public interface IEditorConstants {
+	public static final String LOGICAL_PLAN_EDITOR_ID = "de.uniol.inf.is.odysseus.rcp.LogicalPlanEditor";
+	public static final String NEW_LOGICAL_PLAN_COMMAND_ID = "de.uniol.inf.is.odysseus.rcp.editor.NewLogicalPlanCommand";
+	
+	public static final String FILENAME_PARAMETER_ID = "de.uniol.inf.is.odysseus.rcp.editor.FilenameParameter";
+}
---
+++ viewer/de.uniol.inf.is.odysseus.rcp.editor/src/de/uniol/inf/is/odysseus/rcp/editor/editorpart/LogicalPlanEditorPart.java
@@ -0,0 +1,103 @@
+package de.uniol.inf.is.odysseus.rcp.editor.editorpart;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.DropTarget;
+import org.eclipse.swt.dnd.DropTargetEvent;
+import org.eclipse.swt.dnd.DropTargetListener;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.EditorPart;
+import org.eclipse.ui.part.FileEditorInput;
+
+public class LogicalPlanEditorPart extends EditorPart implements IEditorPart {
+
+	private FileEditorInput input;
+	
+	public LogicalPlanEditorPart() {
+	}
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		
+	}
+
+	@Override
+	public void doSaveAs() {
+
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+		this.input = (FileEditorInput)input;
+		
+		setSite(site);
+		setInput(input);
+		setPartName(this.input.getName());
+	}
+
+	@Override
+	public boolean isDirty() {
+		return false;
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		return false;
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		
+		Canvas canvas = new Canvas(parent, SWT.NONE);
+		canvas.setBackground(PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_WHITE));
+		
+		
+		int operations = DND.DROP_COPY;
+		Transfer[] transferTypes = new Transfer[]{TextTransfer.getInstance()};
+		DropTarget target = new DropTarget(canvas, operations);
+		target.setTransfer(transferTypes);
+		target.addDropListener(new DropTargetListener() {
+
+			@Override
+			public void dragEnter(DropTargetEvent event) {
+			}
+
+			@Override
+			public void dragLeave(DropTargetEvent event) {
+			}
+
+			@Override
+			public void dragOperationChanged(DropTargetEvent event) {
+			}
+
+			@Override
+			public void dragOver(DropTargetEvent event) {
+				event.detail = DND.DROP_COPY;
+			}
+
+			@Override
+			public void drop(DropTargetEvent event) {
+			}
+
+			@Override
+			public void dropAccept(DropTargetEvent event) {
+			}
+			
+		});	
+	}
+
+	@Override
+	public void setFocus() {
+
+	}
+
+}
