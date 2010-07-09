527ab673d910fc5b6d4d418b90de1ac9d107a666
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-07-09 16:40:44 +0000

    RCP: Unter New... steht ein Wizard für logische Pläne bereit  - macht bisher nix anderes, als nach einem Namen (ohne Endung) zu fragen  - Endung der logischen Pläne ist "*.pln"
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5628 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 2af71df922cd9579f81c6cea298d422b98faee5d

---
+++ viewer/de.uniol.inf.is.odysseus.rcp.editor.navigator/src/de/uniol/inf/is/odysseus/rcp/editor/navigator/wizard/NewLogicalPlanWizard.java
@@ -0,0 +1,72 @@
+package de.uniol.inf.is.odysseus.rcp.editor.navigator.wizard;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewLogicalPlanWizard extends Wizard implements INewWizard {
+
+	private NewLogicalPlanWizardPage page;
+	private IContainer root;
+	ArrayList<String> usedNames = new ArrayList<String>();
+	
+	
+	public NewLogicalPlanWizard() {
+		super();
+		setNeedsProgressMonitor(true);
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		root = null;
+		Object selected = selection.getFirstElement();
+		if( selected instanceof IContainer ) 
+			root = (IContainer)selected;
+		else if( selected instanceof IFile ) 
+			root = ((IFile)selected).getParent();
+		
+		try {
+			for( IResource file : root.members()) {
+				usedNames.add(file.getName());
+			}
+		} catch (CoreException e) {}
+		
+	}
+	
+	@Override
+	public void addPages() {
+		page = new NewLogicalPlanWizardPage(usedNames);
+		
+		addPage(page);
+	}
+
+	@Override
+	public boolean performFinish() {
+		String name = page.getName();
+		
+		String fileName = name + ".pln";
+		
+		Path path = new Path(fileName);
+		IFile newFile = root.getFile(path);
+		try {
+			byte[] bytes = "File contents".getBytes();
+		    InputStream source = new ByteArrayInputStream(bytes);
+			newFile.create(source, IResource.NONE, null);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+
+}
