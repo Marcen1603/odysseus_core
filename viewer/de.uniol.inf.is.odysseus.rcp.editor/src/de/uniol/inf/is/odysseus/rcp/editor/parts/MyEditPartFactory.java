ec6d43b80d665ec8f81a6edd89b84ae491430d32
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-07-13 10:59:18 +0000

    RCP: Refactoring
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5646 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d811191cac8898d453fb0b7ba5e6595ce89e7ba4

---
+++ viewer/de.uniol.inf.is.odysseus.rcp.editor/src/de/uniol/inf/is/odysseus/rcp/editor/parts/MyEditPartFactory.java
@@ -0,0 +1,24 @@
+package de.uniol.inf.is.odysseus.rcp.editor.parts;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.EditPartFactory;
+
+import de.uniol.inf.is.odysseus.rcp.editor.model.Operator;
+import de.uniol.inf.is.odysseus.rcp.editor.model.OperatorPlan;
+
+public class MyEditPartFactory implements EditPartFactory {
+
+	@Override
+	public EditPart createEditPart(EditPart arg0, Object object) {
+		if( object instanceof OperatorPlan ) {
+			return new OperatorPlanEditPart((OperatorPlan)object);
+		}
+		if (object instanceof Operator) {
+			return new OperatorEditPart((Operator) object);
+		} else {
+			assert(false);
+			return null;
+		}
+	}
+
+}
---
+++ viewer/de.uniol.inf.is.odysseus.rcp.editor/src/de/uniol/inf/is/odysseus/rcp/editor/parts/OperatorPlanEditPart.java
@@ -0,0 +1,93 @@
+package de.uniol.inf.is.odysseus.rcp.editor.parts;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.XYLayout;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.EditPolicy;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
+import org.eclipse.gef.editpolicies.RootComponentEditPolicy;
+import org.eclipse.gef.editpolicies.XYLayoutEditPolicy;
+import org.eclipse.gef.requests.ChangeBoundsRequest;
+import org.eclipse.gef.requests.CreateRequest;
+
+import de.uniol.inf.is.odysseus.rcp.editor.model.Operator;
+import de.uniol.inf.is.odysseus.rcp.editor.model.OperatorPlan;
+import de.uniol.inf.is.odysseus.rcp.editor.model.commands.OperatorCreateCommand;
+import de.uniol.inf.is.odysseus.rcp.editor.model.commands.OperatorSetConstraintCommand;
+
+public class OperatorPlanEditPart extends AbstractGraphicalEditPart implements EditPart, PropertyChangeListener {
+
+	public OperatorPlanEditPart(OperatorPlan plan) {
+		setModel(plan);
+	}
+
+	@Override
+	protected IFigure createFigure() {
+		Figure figure = new Figure();
+		figure.setLayoutManager(new XYLayout());
+		figure.setOpaque(true);
+		figure.setBackgroundColor(ColorConstants.white);
+		return figure;
+	}
+
+	@Override
+	public void activate() {
+		((OperatorPlan) getModel()).addPropertyChangeListener(this);
+		super.activate();
+	}
+
+	@Override
+	public void deactivate() {
+		((OperatorPlan) getModel()).removePropertyChangeListener(this);
+		super.deactivate();
+	}
+
+	@Override
+	protected void createEditPolicies() {
+		installEditPolicy(EditPolicy.COMPONENT_ROLE, new RootComponentEditPolicy());
+		installEditPolicy(EditPolicy.LAYOUT_ROLE, new XYLayoutEditPolicy() {
+			@Override
+			protected Command createChangeConstraintCommand(EditPart child, Object constraint) {
+				return null;
+			}
+
+			@Override
+			protected Command createChangeConstraintCommand(ChangeBoundsRequest request, EditPart child, Object constraint) {
+				if (child instanceof OperatorEditPart && constraint instanceof Rectangle) {
+					return new OperatorSetConstraintCommand((Operator) child.getModel(), request, (Rectangle) constraint);
+				}
+				return super.createChangeConstraintCommand(request, child, constraint);
+			}
+
+			@Override
+			protected Command getCreateCommand(CreateRequest request) {
+				Object childClass = request.getNewObjectType();
+				if (childClass == Operator.class) {
+					return new OperatorCreateCommand((Operator) request.getNewObject(), (OperatorPlan) getHost().getModel(), (Rectangle) getConstraintFor(request));
+				}
+				return null;
+			}
+		});
+	}
+
+	@Override
+	protected List<Operator> getModelChildren() {
+		return ((OperatorPlan) getModel()).getOperators();
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		if (evt.getPropertyName().equals(OperatorPlan.PROPERTY_OPERATORS))
+			refreshChildren();
+		else
+			refresh();
+	}
+}
