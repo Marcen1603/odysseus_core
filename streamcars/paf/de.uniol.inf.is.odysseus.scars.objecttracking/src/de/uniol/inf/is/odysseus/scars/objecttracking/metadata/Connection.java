2dfe1539befa9f8a5a9cee1a7c5f6779e89396bd
Volker Janz <vjanz@uni-oldenburg.de>
2010-07-14 15:34:17 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5657 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: 619832cca79be4796f648a5940a1023e92fd1f8b

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking/src/de/uniol/inf/is/odysseus/scars/objecttracking/metadata/Connection.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking/src/de/uniol/inf/is/odysseus/scars/objecttracking/metadata/Connection.java
@@ -1,10 +1,22 @@
 package de.uniol.inf.is.odysseus.scars.objecttracking.metadata;
 
-public abstract class Connection<L, R, W extends java.lang.Number> implements IConnection<L, R, W> {
+public class Connection<L, R, W extends java.lang.Number> implements IConnection<L, R, W> {
 	
 	private L left;
 	private R right;
 	private W rating;
+	
+	public Connection(L left, R right, W rating) {
+		this.left = left;
+		this.right = right;
+		this.rating = rating;
+	}
+	
+	public Connection(Connection<L, R, W> copy) {
+		this.left = copy.getLeft();
+		this.right = copy.getRight();
+		this.rating = copy.getRating();
+	}
 	
 	public L getLeft() {
 		return this.left;
