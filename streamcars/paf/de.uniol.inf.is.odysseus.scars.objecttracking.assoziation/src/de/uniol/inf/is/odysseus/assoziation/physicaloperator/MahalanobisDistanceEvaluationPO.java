0036e7da976b5f1c933223b3ee3bb8bb7a0047c5
Volker Janz <vjanz@uni-oldenburg.de>
2010-07-14 16:26:34 +0000

    Implementation of the AbstractHypothesisEvaluationPO for Mahalanobis Distance
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5659 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 28823c4b123e59ad6a539f23c5a2df35e9039e39

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/physicaloperator/MahalanobisDistanceEvaluationPO.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/physicaloperator/MahalanobisDistanceEvaluationPO.java
@@ -5,25 +5,75 @@ import java.util.ArrayList;
 import org.apache.commons.math.linear.RealMatrix;
 import org.apache.commons.math.linear.RealMatrixImpl;
 
-import de.uniol.inf.is.odysseus.assoziation.logicaloperator.HypothesisEvaluationAO;
 import de.uniol.inf.is.odysseus.base.predicate.IPredicate;
 import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.scars.objecttracking.OrAttributeResolver;
 import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IConnectionContainer;
 
 public class MahalanobisDistanceEvaluationPO<M extends IProbability & IPredictionFunctionKey<IPredicate<MVRelationalTuple<M>>> & IConnectionContainer<MVRelationalTuple<M>, MVRelationalTuple<M>, Double>> extends AbstractHypothesisEvaluationPO<M> {
 	
-	private double treshhold;
+	private double threshold;
 	private String operator;
 	
-	public double evaluate(MVRelationalTuple<M> tupleNew, MVRelationalTuple<M> tupleOld, ArrayList<int[]> mesurementValuePathsNew, ArrayList<int[]> mesurementValuePathsOld) {
-		// TODO: Mahalanobis distance implementieren
-		return 0;
+	public double evaluate(MVRelationalTuple<M> tupleNew, MVRelationalTuple<M> tupleOld, ArrayList<int[]> mesurementValuePathsNew, ArrayList<int[]> mesurementValuePathsOld) {		
+		// new = left; old = right
+		
+		double[] leftMVVector = OrAttributeResolver.getMeasurementValues(mesurementValuePathsNew, tupleNew);
+		RealMatrix leftV = new RealMatrixImpl(leftMVVector); 
+		
+		double[] rightMVVector = OrAttributeResolver.getMeasurementValues(mesurementValuePathsOld, tupleOld);
+		RealMatrix rightV = new RealMatrixImpl(rightMVVector);
+		
+		double[][] rightCov = tupleOld.getMetadata().getCovariance();
+		RealMatrix rightCovMatrix = new RealMatrixImpl(rightCov);
+		RealMatrix rightCovInvMatrix = rightCovMatrix.inverse();
+		
+		RealMatrix distanceMatrix = leftV.subtract(rightV).transpose().multiply(rightCovInvMatrix).multiply(leftV.subtract(rightV));
+		double distance = distanceMatrix.getEntry(0, 0);
+		
+		if(this.operator.equals("<")){
+			if(distance < this.threshold) {
+				return 100;
+			} else {
+				return 0;
+			}
+		}
+		
+		else if(this.operator.equals("<=")){
+			if(distance <= this.threshold) {
+				return 100;
+			} else {
+				return 0;
+			}
+		}
+		
+		else if(this.operator.equals("=")){
+			if(distance == this.threshold) {
+				return 100;
+			} else {
+				return 0;
+			}
+		}
+		
+		else if(this.operator.equals(">=")){
+			if(distance >= this.threshold) {
+				return 100;
+			} else {
+				return 0;
+			}
+		}
+		
+		else if(distance > this.threshold) {
+			return 100;
+		} else {
+			return 0;
+		}
 	}
 	
-	public void setTreshhold(double treshhold) {
-		this.treshhold = treshhold;
+	public void setThreshold(double threshold) {
+		this.threshold = threshold;
 	}
 
 	public void setOperator(String operator) {
