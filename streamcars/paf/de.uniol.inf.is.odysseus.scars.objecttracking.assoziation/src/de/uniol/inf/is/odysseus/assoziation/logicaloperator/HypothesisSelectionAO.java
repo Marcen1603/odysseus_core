d3372571a963c17371aa6d8afaaead35f4621e97
Volker Janz <vjanz@uni-oldenburg.de>
2010-07-15 14:02:12 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5684 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: ef5f6ca65781820839b7a0629da4454fe781f80b

---
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/logicaloperator/HypothesisSelectionAO.java
@@ -0,0 +1,23 @@
+package de.uniol.inf.is.odysseus.assoziation.logicaloperator;
+
+import de.uniol.inf.is.odysseus.logicaloperator.base.AbstractLogicalOperator;
+import de.uniol.inf.is.odysseus.logicaloperator.base.BinaryLogicalOp;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
+
+public class HypothesisSelectionAO<M extends IProbability> extends BinaryLogicalOp {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public SDFAttributeList getOutputSchema() {
+		return null;
+	}
+
+	@Override
+	public AbstractLogicalOperator clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/physicaloperator/HypothesisSelectionPO.java
@@ -0,0 +1,5 @@
+package de.uniol.inf.is.odysseus.assoziation.physicaloperator;
+
+public class HypothesisSelectionPO {
+
+}
