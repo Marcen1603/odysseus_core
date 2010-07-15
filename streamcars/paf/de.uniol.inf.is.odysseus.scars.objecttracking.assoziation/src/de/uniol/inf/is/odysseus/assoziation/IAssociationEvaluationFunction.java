30bb2ab56af21757e0b67e32d5c75f1ca758a94a
Volker Janz <vjanz@uni-oldenburg.de>
2010-07-15 15:09:48 +0000

    Klassen wurden unnötig da die Funktion jetzt im HypothesisSelectionPO untergebracht ist
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5700 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: eaad334d2300fcc5bfb591c2cfe12689e88a9027

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/IAssociationEvaluationFunction.java
+++
@@ -1,7 +0,0 @@
-package de.uniol.inf.is.odysseus.assoziation;
-
-public interface IAssociationEvaluationFunction {
-
-	public double[][] singleMatchingEvaluation(double[][] matchingMatrix);
-	
-}
--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/IAssoziationAlgorithmus.java
+++
@@ -1,7 +0,0 @@
-package de.uniol.inf.is.odysseus.assoziation;
-
-public interface IAssoziationAlgorithmus {
-
-	public Object executeAssoAlgorithmus();
-	
-}
