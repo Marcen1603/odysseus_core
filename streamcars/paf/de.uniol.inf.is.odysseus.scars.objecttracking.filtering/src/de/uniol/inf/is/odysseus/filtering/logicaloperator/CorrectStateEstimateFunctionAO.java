1bc3d27bb205bde3e5912aea96c9b643edd6d98e
Hauke Neemann <hneemann@uni-oldenburg.de>
2010-07-22 13:14:22 +0000

    CorrectStateEstimateFunction (CorrectStateEstimateFunctionAO, CorrectStateEstimateFunctionPO) implementiert.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5749 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 32131944e9dc000692ccb6540f455b66981e7f7e

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.filtering/src/de/uniol/inf/is/odysseus/filtering/logicaloperator/CorrectStateEstimateFunctionAO.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.filtering/src/de/uniol/inf/is/odysseus/filtering/logicaloperator/CorrectStateEstimateFunctionAO.java
@@ -3,19 +3,25 @@ package de.uniol.inf.is.odysseus.filtering.logicaloperator;
 import de.uniol.inf.is.odysseus.logicaloperator.base.AbstractLogicalOperator;
 import de.uniol.inf.is.odysseus.logicaloperator.base.UnaryLogicalOp;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.scars.util.OrAttributeResolver;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
-public class CorrectStateEstimateFunctionAO <M extends IProbability> extends AbstractLogicalOperator {
+public class CorrectStateEstimateFunctionAO <M extends IProbability> extends UnaryLogicalOp {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-	// TODO Weitere benötigte Attribute hier einfügen
 	
-	public CorrectStateEstimateFunctionAO() {
+	private String functionID;
+	
+	private String oldObjListPath;
+	
+	private String newObjListPath;
+
+	public CorrectStateEstimateFunctionAO(String functionId) {
 		super();
+		this.functionID = functionId;
 	}
 	
 	public CorrectStateEstimateFunctionAO(CorrectStateEstimateFunctionAO<M> copy) {
@@ -37,5 +43,28 @@ public class CorrectStateEstimateFunctionAO <M extends IProbability> extends Abs
 		return null;
 	}
 
+	public void initPaths(String oldObjListPath, String newObjListPath) {
+		this.oldObjListPath = oldObjListPath;
+		this.newObjListPath = newObjListPath;
+	}
+
+	public int[] getNewObjListPath() {
+		//this.leftSchema = ((LogicalSubscription[]) this.getSubscriptions().toArray())[0].getSchema();
+		return OrAttributeResolver.getAttributePath(this.getInputSchema(), this.newObjListPath);
+	}
+
+	public int[] getOldObjListPath() {
+		//this.rightSchema = ((LogicalSubscription[]) this.getSubscriptions().toArray())[1].getSchema();
+		return OrAttributeResolver.getAttributePath(this.getInputSchema(), this.oldObjListPath);
+	}
+	
+	public String getFunctionID() {
+		return this.functionID;
+	}
+
+	public void setFunctionID(String functionID) {
+		this.functionID = functionID;
+	}
+	
 	// TODO Weitere benötigte Methoden hier hinzufügen.
 }
--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.filtering/src/de/uniol/inf/is/odysseus/filtering/physicaloperator/CorrectStateEstimateFunctionPO.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.filtering/src/de/uniol/inf/is/odysseus/filtering/physicaloperator/CorrectStateEstimateFunctionPO.java
@@ -1,5 +1,113 @@
 package de.uniol.inf.is.odysseus.filtering.physicaloperator;
 
-public class CorrectStateEstimateFunctionPO {
+import de.uniol.inf.is.odysseus.base.PointInTime;
+import de.uniol.inf.is.odysseus.base.predicate.IPredicate;
+import de.uniol.inf.is.odysseus.filtering.ICorrectStateEstimateFunction;
+import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
+import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.Connection;
+import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IConnectionContainer;
+import de.uniol.inf.is.odysseus.scars.util.OrAttributeResolver;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
+public class CorrectStateEstimateFunctionPO <M extends IProbability & IPredictionFunctionKey<IPredicate<MVRelationalTuple<M>>> & IConnectionContainer<MVRelationalTuple<M>, MVRelationalTuple<M>, Double>> extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> {
+
+	private ICorrectStateEstimateFunction estimateFunction;
+	private int[] oldObjListPath;
+	private int[] newObjListPath;
+	private SDFAttributeList schema;
+
+	public CorrectStateEstimateFunctionPO() {
+
+	}
+	
+	@Override
+	public AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe.OutputMode getOutputMode() {
+		return OutputMode.MODIFIED_INPUT;
+	}
+
+	@Override
+	protected void process_next(MVRelationalTuple<M> object, int port) {
+		// list of new objects
+		MVRelationalTuple<M>[] newList = (MVRelationalTuple<M>[]) ((MVRelationalTuple<M>)OrAttributeResolver.resolveTuple(object, this.newObjListPath)).getAttributes();
+		// list of old objects
+		MVRelationalTuple<M>[] oldList = (MVRelationalTuple<M>[]) ((MVRelationalTuple<M>)OrAttributeResolver.resolveTuple(object, this.oldObjListPath)).getAttributes();
+		// list of connections
+		Connection<MVRelationalTuple<M>, MVRelationalTuple<M>, Double>[] objConList = (Connection<MVRelationalTuple<M>, MVRelationalTuple<M>, Double>[]) object.getMetadata().getConnectionList().toArray();
+
+
+		double[][] gain = null;
+
+		// traverse connection list and filter
+		for(Connection<MVRelationalTuple<M>, MVRelationalTuple<M>, Double> connected : objConList ) {
+
+			MVRelationalTuple<M> oldTuple = connected.getRight();
+			MVRelationalTuple<M> newTuple = connected.getLeft();
+
+			double[] measurementOld = newTuple.getMeasurementValues();
+			double[] measurementNew = oldTuple.getMeasurementValues();
+
+			double[] correctedMeasurement;
+
+
+			// update state estimate
+			estimateFunction.addParameter("measurementOld", measurementOld);
+			estimateFunction.addParameter("measurementNew", measurementNew);
+			estimateFunction.addParameter("gain", gain);
+
+
+			correctedMeasurement = estimateFunction.correctStateEstimate();
+			
+			//set corrected measurement
+			// TODO Hier muss der korrigierte Wert als Metadatum gesetzt werden.
+			// Dazu anschauen: objecttracking.metadata. Ein Interface
+			// anlegen und dieses implementieren. Dann kann man auf 
+			// object.getMetadata().get/setConnectionList() zugreifen.
+		}
+
+		// transfer to broker
+		transfer(object);
+	}
+
+	@Override
+	public void processPunctuation(PointInTime timestamp, int port) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEstimateFunction(ICorrectStateEstimateFunction estimateFunction) {
+		this.estimateFunction = estimateFunction;
+	}
+
+	public int[] getOldObjListPath() {
+		return this.oldObjListPath;
+	}
+	
+	public void setOldObjListPath(int[] oldObjListPath) {
+		this.oldObjListPath = oldObjListPath;
+	}
+
+	public int[] getNewObjListPath() {
+		return this.newObjListPath;
+	}
+	
+	public void setNewObjListPath(int[] newObjListPath) {
+		this.newObjListPath = newObjListPath;
+	}
+
+	public SDFAttributeList getSchema() {
+		return schema;
+	}
+
+	public void setSchema(SDFAttributeList schema) {
+		this.schema = schema;
+	}
 }
