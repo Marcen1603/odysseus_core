795ec62a5c8f578f11aea55a8f98bdb464f67a92
Volker Janz <vjanz@uni-oldenburg.de>
2010-07-22 12:34:22 +0000

    Neues Metadatum für Bewertungen der Objekte im Umweltmodell
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5747 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a28ee6fa6f6cf5846baf06fba2890074f41e9b8e

---
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.metadata/src/de/uniol/inf/is/odysseus/scars/objecttracking/metadata/IRating.java
@@ -0,0 +1,16 @@
+package de.uniol.inf.is.odysseus.scars.objecttracking.metadata;
+
+import de.uniol.inf.is.odysseus.base.IClone;
+import de.uniol.inf.is.odysseus.base.IMetaAttribute;
+
+public interface IRating extends IMetaAttribute, IClone {
+
+	public void setRating(int rating);
+	public int getRating();
+
+	public void incRating();
+	public void decRating();
+
+	public boolean isZero();
+
+}
---
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.metadata/src/de/uniol/inf/is/odysseus/scars/objecttracking/metadata/Rating.java
@@ -0,0 +1,48 @@
+package de.uniol.inf.is.odysseus.scars.objecttracking.metadata;
+
+public class Rating implements IRating {
+
+	private int rating;
+
+	Rating() {
+		this.rating = 0;
+	}
+
+	Rating(int init) {
+		if(init <= 0) {
+			init = 0;
+		}
+		this.rating = init;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getRating() {
+		return this.rating;
+	}
+
+	public void incRating() {
+		this.rating++;
+	}
+
+	public void decRating() {
+		if(this.rating >= 0) {
+			this.rating--;
+		}
+	}
+
+	public boolean isZero() {
+		if(this.rating >= 0) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public IRating clone(){
+		return new Rating(0);
+	}
+
+}
