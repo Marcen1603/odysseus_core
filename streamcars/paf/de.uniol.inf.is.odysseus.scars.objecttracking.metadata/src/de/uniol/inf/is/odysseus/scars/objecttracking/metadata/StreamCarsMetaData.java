3c5db626c0ed48355c8f4593a3cf090b4878d7da
Volker Janz <vjanz@uni-oldenburg.de>
2010-07-22 14:49:35 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5768 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: 8eca049aed83263cef43fd54f6ee9369f1539eba

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.metadata/src/de/uniol/inf/is/odysseus/scars/objecttracking/metadata/StreamCarsMetaData.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.metadata/src/de/uniol/inf/is/odysseus/scars/objecttracking/metadata/StreamCarsMetaData.java
@@ -1,5 +1,7 @@
 package de.uniol.inf.is.odysseus.scars.objecttracking.metadata;
 
+import de.uniol.inf.is.odysseus.objecttracking.metadata.Probability;
+
 /**
  * Combined metadata which implements each metadata that is used in the
  * StreamCars project.
@@ -10,7 +12,7 @@ package de.uniol.inf.is.odysseus.scars.objecttracking.metadata;
  * @param <R> Datatype of the right object (ConnectionContainer)
  * @param <W> Datatype of the rating - has to extend java.lang.Number (Double, Integer, ...) (ConnectionContainer)
  */
-public class StreamCarsMetaData<L, R, W extends java.lang.Number> implements IConnectionContainer<L, R, W>, IRating, IGain  {
+public class StreamCarsMetaData<L, R, W extends java.lang.Number> extends Probability implements IConnectionContainer<L, R, W>, IRating, IGain  {
 
 	/* ############### KONSTRUKTOREN ################ */
 
