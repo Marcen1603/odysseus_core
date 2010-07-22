d23eccdf3b1749452178c5edb4c3fdfcb5680e67
Volker Janz <vjanz@uni-oldenburg.de>
2010-07-22 11:37:57 +0000

    Scars Metadaten in extra Plugin ausgelagert
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5739 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8235bd4b1fd6517f3b2d695d12c98575fdcea807

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.filtering/src/de/uniol/inf/is/odysseus/filtering/FilterPO.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.filtering/src/de/uniol/inf/is/odysseus/filtering/FilterPO.java
@@ -18,26 +18,26 @@ import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 public class FilterPO<M extends IProbability & IPredictionFunctionKey<IPredicate<MVRelationalTuple<M>>> & IConnectionContainer<MVRelationalTuple<M>, MVRelationalTuple<M>, Double>> extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>>  {
 
 	private IGainFunction gainFunction;
-	
+
 	private ICorrectStateEstimateFunction correctStateEstimateFunction;
-	
+
 	private ICorrectStateCovarianceFunction correctStateCovarianceFunction;
-	
+
 	private MVRelationalTuple<M> oldTuple;
-	
+
 	private MVRelationalTuple<M> newTuple;
-	
+
 	private int[] oldObjListPath;
 	private int[] newObjListPath;
-	
+
 	private SDFAttributeList leftSchema;
 	private SDFAttributeList rightSchema;
-	
-		
+
+
 	public FilterPO() {
-	super();	
+	super();
 	}
-	
+
 	public FilterPO(FilterAO<M> filterAO) {
 		this.gainFunction=filterAO.getGainFunction();
 		this.correctStateEstimateFunction=filterAO.getCorrectStateEstimateFunction();
@@ -46,7 +46,7 @@ public class FilterPO<M extends IProbability & IPredictionFunctionKey<IPredicate
 	public FilterPO(FilterPO<M> copy) {
 	super(copy);
 	}
-	
+
 	@Override
 	public AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> clone() {
 		// TODO Auto-generated method stub
@@ -62,66 +62,66 @@ public class FilterPO<M extends IProbability & IPredictionFunctionKey<IPredicate
 	@Override
 	protected void process_next(MVRelationalTuple<M> object, int port) {
 		// TODO Auto-generated method stub
-	
+
 	// list of new objects
-	MVRelationalTuple<M>[] newList = (MVRelationalTuple<M>[]) ((MVRelationalTuple<M>)OrAttributeResolver.resolveTuple(object, this.newObjListPath)).getAttributes();	
+	MVRelationalTuple<M>[] newList = (MVRelationalTuple<M>[]) ((MVRelationalTuple<M>)OrAttributeResolver.resolveTuple(object, this.newObjListPath)).getAttributes();
 	// list of old objects
 	MVRelationalTuple<M>[] oldList = (MVRelationalTuple<M>[]) ((MVRelationalTuple<M>)OrAttributeResolver.resolveTuple(object, this.oldObjListPath)).getAttributes();
 	// list of connections
 	Connection<MVRelationalTuple<M>, MVRelationalTuple<M>, Double>[] objConList = (Connection<MVRelationalTuple<M>, MVRelationalTuple<M>, Double>[]) object.getMetadata().getConnectionList().toArray();
-	
-	
+
+
 	double[][] gain = null;
-	
+
 	// traverse connection list and filter
 	for(Connection<MVRelationalTuple<M>, MVRelationalTuple<M>, Double> connected : objConList ) {
-		
+
 		MVRelationalTuple<M> oldTuple = connected.getRight();
 		MVRelationalTuple<M> newTuple = connected.getLeft();
-		
+
 		double[] measurementOld = newTuple.getMeasurementValues();
 		double[] measurementNew = oldTuple.getMeasurementValues();
-		
+
 		double[][] covarianceNew = newTuple.getMetadata().getCovariance();
 		double[][] covarianceOld = oldTuple.getMetadata().getCovariance();
-		
+
 		double[] correctedMeasurement;
 		double[][] correctedCovariance;
-		
-		
+
+
 		// compute gain
 		gainFunction.addParameter("oldCovariance", covarianceOld);
 		gainFunction.addParameter("newCovariance", covarianceNew);
-		
+
 		gain = gainFunction.computeGain();
-		
+
 		// update state covariance
 		correctStateCovarianceFunction.addParameter("gain",gain);
 		correctStateCovarianceFunction.addParameter("covarianceOld", covarianceOld);
-		
+
 		correctedCovariance = correctStateCovarianceFunction.correctStateCovariance();
-		
+
 		oldTuple.getMetadata().setCovariance(correctedCovariance);
-		
+
 		// update state estimate
 		correctStateEstimateFunction.addParameter("measurementOld", measurementOld);
 		correctStateEstimateFunction.addParameter("measurementNew", measurementNew);
 		correctStateEstimateFunction.addParameter("gain", gain);
-		
-		
-		correctedMeasurement = correctStateEstimateFunction.correctStateEstimate();
-		
 
-	
+
+		correctedMeasurement = correctStateEstimateFunction.correctStateEstimate();
+
+
+
 	}
-		
+
 	// transfer to broker
 	transfer(object);
-	
-	
+
+
 	}
-	
-	
+
+
 
 	public void setOldObjListPath(int[] oldObjListPath) {
 		this.oldObjListPath = oldObjListPath;
@@ -138,11 +138,11 @@ public class FilterPO<M extends IProbability & IPredictionFunctionKey<IPredicate
 	public void setRightSchema(SDFAttributeList rightSchema) {
 		this.rightSchema = rightSchema;
 	}
-	
+
 	@Override
 	public void processPunctuation(PointInTime timestamp, int port) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public int hashcode() {
@@ -151,6 +151,6 @@ public class FilterPO<M extends IProbability & IPredictionFunctionKey<IPredicate
 
 	public boolean equals(Object obj) {
 		return false;
-		
+
 	}
 }
