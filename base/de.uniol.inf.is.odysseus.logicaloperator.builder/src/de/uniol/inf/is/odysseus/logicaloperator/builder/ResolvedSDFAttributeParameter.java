062a58a513a7d7fb67c613f483ab01fe955384cc
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-07-28 13:59:28 +0000

    Bugfix
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5841 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 02d616e89e7527f378dda13e5954c8138b753631

--- base/de.uniol.inf.is.odysseus.logicaloperator.builder/src/de/uniol/inf/is/odysseus/logicaloperator/builder/ResolvedSDFAttributeParameter.java
+++ base/de.uniol.inf.is.odysseus.logicaloperator.builder/src/de/uniol/inf/is/odysseus/logicaloperator/builder/ResolvedSDFAttributeParameter.java
@@ -1,10 +1,15 @@
 package de.uniol.inf.is.odysseus.logicaloperator.builder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttribute;
 
 public class ResolvedSDFAttributeParameter extends
 		AbstractParameter<SDFAttribute> {
 
+	private final Logger logger = LoggerFactory.getLogger(ResolvedSDFAttributeParameter.class);
+	
 	public ResolvedSDFAttributeParameter(String name, REQUIREMENT requirement) {
 		super(name, requirement);
 	}
@@ -14,9 +19,14 @@ public class ResolvedSDFAttributeParameter extends
 		if (getAttributeResolver() == null) {
 			throw new RuntimeException("missing attribute resolver");
 		}
-		SDFAttribute attribute = getAttributeResolver().getAttribute(
-				(String) this.inputValue);
-		setValue(attribute);
+		try {
+			SDFAttribute attribute = getAttributeResolver().getAttribute(
+					(String) this.inputValue);
+			
+			setValue(attribute);
+		} catch( Exception ex ) {
+			logger.error("cannot assign attribute value", ex);
+		}
 	}
 
 }
