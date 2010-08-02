f38fc77d42b504c79fb7143f844ccb36cc5e6a0f
Volker Janz <vjanz@uni-oldenburg.de>
2010-08-02 13:19:03 +0000

    Test für HypothesisSelection (Assoziation letzter Schritt)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5913 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 786b52411ed8401077b13365ad5411b81624a834

---
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/test/TestHypothesisSelectionPO.java
@@ -0,0 +1,50 @@
+package de.uniol.inf.is.odysseus.assoziation.test;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import de.uniol.inf.is.odysseus.assoziation.physicaloperator.HypothesisSelectionPO;
+import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.StreamCarsMetaData;
+
+public class TestHypothesisSelectionPO extends TestCase{
+
+	@Test
+	public void testPO() {
+		HypothesisSelectionPO<StreamCarsMetaData> po = new HypothesisSelectionPO<StreamCarsMetaData>();
+
+		double[][] evalMatrix = new double[3][3];
+		evalMatrix[0][0] = 0;
+		evalMatrix[0][1] = 10;
+		evalMatrix[0][2] = 20;
+
+		evalMatrix[1][0] = 30;
+		evalMatrix[1][1] = 40;
+		evalMatrix[1][2] = 50;
+
+		evalMatrix[2][0] = 60;
+		evalMatrix[2][1] = 70;
+		evalMatrix[2][2] = 70;
+
+		evalMatrix = po.singleMatchingEvaluation(evalMatrix);
+
+		double[][] resMatrix = new double[3][3];
+		resMatrix[0][0] = 0;
+		resMatrix[0][1] = 0;
+		resMatrix[0][2] = 0;
+
+		resMatrix[1][0] = 0;
+		resMatrix[1][1] = 0;
+		resMatrix[1][2] = 50;
+
+		resMatrix[2][0] = 0;
+		resMatrix[2][1] = 70;
+		resMatrix[2][2] = 0;
+
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				assertEquals(evalMatrix[i][j], resMatrix[i][j]);
+			}
+		}
+	}
+}
