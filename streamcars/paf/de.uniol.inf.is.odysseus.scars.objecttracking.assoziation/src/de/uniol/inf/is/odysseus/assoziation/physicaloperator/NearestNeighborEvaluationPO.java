804e57a40c283d2d3f85cc1aef636ca8c6a02ec9
Volker Janz <vjanz@uni-oldenburg.de>
2010-08-02 07:23:59 +0000

    Neue Implementierung des AbstractHypthesisEvaluationPO für die Assoziation
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5899 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 556ebf5de3c1ee3f262812d9a870e024c93701f9

---
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/assoziation/physicaloperator/NearestNeighborEvaluationPO.java
@@ -0,0 +1,33 @@
+package de.uniol.inf.is.odysseus.assoziation.physicaloperator;
+
+import java.util.ArrayList;
+
+import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
+import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IConnectionContainer;
+
+public class NearestNeighborEvaluationPO<M extends IProbability & IConnectionContainer> extends AbstractHypothesisEvaluationPO<M> {
+
+	public double evaluate(MVRelationalTuple<M> tupleNew, MVRelationalTuple<M> tupleOld, ArrayList<int[]> mesurementValuePathsNew, ArrayList<int[]> mesurementValuePathsOld) {
+		return 0;
+	}
+
+	public void initAlgorithmParameter() {
+
+	}
+
+	@Override
+	public AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> clone() {
+		return new NearestNeighborEvaluationPO<M>(this);
+	}
+
+	public NearestNeighborEvaluationPO() {
+
+	}
+
+	public NearestNeighborEvaluationPO(NearestNeighborEvaluationPO<M> clone) {
+		super(clone);
+	}
+
+}
