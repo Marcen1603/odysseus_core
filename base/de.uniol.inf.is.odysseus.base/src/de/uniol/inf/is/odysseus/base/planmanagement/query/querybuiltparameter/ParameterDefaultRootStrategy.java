9bc43a42e9649b3c85e9d44a68250c25244d81c5
Andre Bolles <andre.bolles@offis.de>
2010-08-03 12:12:11 +0000

    ParameterDefaultRootStrategy hatte Compilefehler -> jetzt behoben
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@5938 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0f49061bd5f152e3842c4330bcfd923554012662

--- base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/base/planmanagement/query/querybuiltparameter/ParameterDefaultRootStrategy.java
+++ base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/base/planmanagement/query/querybuiltparameter/ParameterDefaultRootStrategy.java
@@ -1,11 +1,26 @@
 package de.uniol.inf.is.odysseus.base.planmanagement.query.querybuiltparameter;
 
+/**
+ * Since a query can have more than one root,
+ * this class encapsulates strategies for
+ * subscribing the default root to
+ * the multiple roots of a query. For example:
+ * 
+ * A query has 3 roots. The default root of a
+ * query should be an operator that prints
+ * the results of the query to console. However,
+ * should there be 3 different console printing
+ * operators, one for each root of the query, or
+ * should there be a single printing operator,
+ * so that all the roots run into the same output
+ * operator.
+ * 
+ * @author André Bolles
+ *
+ */
+public class ParameterDefaultRootStrategy extends AbstractQueryBuildParameter<IDefaultRootStrategy> {
 
-public class ParameterDefaultRootStrategy extends
-		AbstractQueryBuildParameter<IDefaultRootStrategy> {
-
-	public ParameterDefaultRootStrategy(IDefaultRootStrategy value) {
-		super(value);
+	public ParameterDefaultRootStrategy(IDefaultRootStrategy strategy){
+		super(strategy);
 	}
-
 }
