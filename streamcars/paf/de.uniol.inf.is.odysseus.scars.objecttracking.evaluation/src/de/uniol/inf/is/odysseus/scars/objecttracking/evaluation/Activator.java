e7c951964a741ff3153688289691ffb30a3981ae
Wolf Bauer <wbauer@uni-oldenburg.de>
2010-08-09 14:14:30 +0000

    bugfix
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6027 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: fba0ca271b3e9780760f6a27ccef631cb2929692

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.evaluation/src/de/uniol/inf/is/odysseus/scars/objecttracking/evaluation/Activator.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.evaluation/src/de/uniol/inf/is/odysseus/scars/objecttracking/evaluation/Activator.java
@@ -8,12 +8,6 @@ import org.osgi.framework.BundleContext;
  * The activator class controls the plug-in life cycle
  */
 public class Activator implements BundleActivator{
-
-	// The plug-in ID
-//	public static final String PLUGIN_ID = "de.uniol.inf.is.odysseus.scars.objecttracking.evaluation"; //$NON-NLS-1$
-
-	// The shared instance
-	private static Activator plugin;
 	
 	/**
 	 * The constructor
