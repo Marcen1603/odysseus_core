1395081009bccce0f07bbc8cc83fe35917ab1120
Benjamin Gruenebast <bgruenebast@uni-oldenburg.de>
2010-08-10 14:02:54 +0000

    update der zeitstempel im tuple und in metadaten hinzugefügt...und andere bugfixes
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6105 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c6ece4bfc4f9159d1f49118b5a927a9fca8debe2

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.prediction/src/de/uniol/inf/is/odysseus/scars/objecttracking/prediction/physicaloperator/PredictionPO.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.prediction/src/de/uniol/inf/is/odysseus/scars/objecttracking/prediction/physicaloperator/PredictionPO.java
@@ -3,27 +3,28 @@ package de.uniol.inf.is.odysseus.scars.objecttracking.prediction.physicaloperato
 import de.uniol.inf.is.odysseus.base.OpenFailedException;
 import de.uniol.inf.is.odysseus.base.PointInTime;
 import de.uniol.inf.is.odysseus.base.predicate.IPredicate;
-import de.uniol.inf.is.odysseus.base.predicate.TruePredicate;
-import de.uniol.inf.is.odysseus.intervalapproach.DefaultTISweepArea;
 import de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval;
 import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
 import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
-import de.uniol.inf.is.odysseus.physicaloperator.base.ISweepArea;
 import de.uniol.inf.is.odysseus.scars.objecttracking.prediction.sdf.metadata.IPredictionFunction;
 import de.uniol.inf.is.odysseus.scars.objecttracking.prediction.sdf.metadata.PredictionFunctionContainer;
+import de.uniol.inf.is.odysseus.scars.util.SchemaHelper;
+import de.uniol.inf.is.odysseus.scars.util.SchemaIndexPath;
 import de.uniol.inf.is.odysseus.scars.util.TupleHelper;
+import de.uniol.inf.is.odysseus.scars.util.TupleIndexPath;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
 public class PredictionPO<M extends IProbability & ITimeInterval & IPredictionFunctionKey<IPredicate<MVRelationalTuple<M>>>> extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> {
 
 	
-	private MVRelationalTuple<M> currentTime;
-	private MVRelationalTuple<M> currentScan;
+	private MVRelationalTuple<M> currentTimeTuple;
+	private MVRelationalTuple<M> currentScanTuple;
 	
 	private int[] objListPath;
-	
-	private ISweepArea<MVRelationalTuple<M>> scanBuffer;
+	private SchemaIndexPath currentTimeSchemaPath;
+	private SchemaIndexPath currentScanTimeSchemaPath;
 
 	private PredictionFunctionContainer<M> predictionFunctions;
 	
@@ -35,6 +36,8 @@ public class PredictionPO<M extends IProbability & ITimeInterval & IPredictionFu
 		this.predictionFunctions = new PredictionFunctionContainer<M>(copy.predictionFunctions);
 		objListPath = new int[copy.objListPath.length];
 		System.arraycopy(copy.objListPath, 0, this.objListPath, 0, copy.objListPath.length);
+		this.currentTimeSchemaPath = copy.currentTimeSchemaPath;
+		this.currentScanTimeSchemaPath = copy.currentScanTimeSchemaPath;
 	}
 	
 	public void setPredictionFunctions(PredictionFunctionContainer<M> predictionFunctions) {
@@ -53,30 +56,48 @@ public class PredictionPO<M extends IProbability & ITimeInterval & IPredictionFu
 	@Override
 	protected void process_open() throws OpenFailedException {
 		super.process_open();
-		scanBuffer = new DefaultTISweepArea<MVRelationalTuple<M>>();
-		scanBuffer.setQueryPredicate(new TruePredicate<MVRelationalTuple<M>>());
+		SDFAttributeList sourceTimeSchema = this.getSubscribedToSource(0).getTarget().getOutputSchema();
+		SDFAttributeList scanSchema = this.getSubscribedToSource(1).getTarget().getOutputSchema();
+		SchemaHelper helper1 = new SchemaHelper(sourceTimeSchema);
+		currentTimeSchemaPath = helper1.getSchemaIndexPath(helper1.getStartTimestampAttribute());
+		SchemaHelper helper2 = new SchemaHelper(scanSchema);
+		currentScanTimeSchemaPath = helper2.getSchemaIndexPath(helper2.getStartTimestampAttribute());
 	}
-
+	
 	@Override
 	protected void process_next(MVRelationalTuple<M> object, int port) {
 		if(port == 0) {
-			currentTime = object.clone();
+			currentTimeTuple = object.clone();
 		} else if(port == 1) {
-			scanBuffer.insert(object);
+			currentScanTuple = object.clone();
+		}
+		if(currentTimeTuple != null && currentScanTuple != null) {
+			predictData();
+			MVRelationalTuple<M> tmp = currentScanTuple;
+			currentScanTuple = null;
+			transfer(tmp);
 		}
 	}
 	
+	
+	
 //	@SuppressWarnings("unchecked")
 	private void predictData() {
-		TupleHelper helper = new TupleHelper(currentScan);
+		TupleHelper helper = new TupleHelper(currentScanTuple);
 		MVRelationalTuple<?> list = (MVRelationalTuple<?>)helper.getObject(objListPath, false);
 		for(int index=0; index < list.getAttributeCount(); index++) {
 			MVRelationalTuple<M> obj = list.getAttribute(index);
 			IPredictionFunction<M> pf = predictionFunctions.get(obj.getMetadata().getPredictionFunctionKey());
-			pf.predictData(currentScan, currentTime, index);
+			pf.predictData(currentScanTuple, currentTimeTuple, index);
 			M metadata = obj.getMetadata();
-			pf.predictMetadata(metadata, currentScan, currentTime, index);
+			pf.predictMetadata(metadata, currentScanTuple, currentTimeTuple, index);
 		}
+		TupleIndexPath scanTimeTPath = currentTimeSchemaPath.toTupleIndexPath(currentTimeTuple);
+		Long currentTimeValue = (Long)scanTimeTPath.getTupleObject();
+		
+		currentScanTuple.getMetadata().getStart().setMainPoint(currentTimeValue);
+		TupleIndexPath currentScanTimeTPath = currentScanTimeSchemaPath.toTupleIndexPath(currentScanTuple);
+		currentScanTimeTPath.setTupleObject(currentTimeValue);
 	}
 
 	@Override
