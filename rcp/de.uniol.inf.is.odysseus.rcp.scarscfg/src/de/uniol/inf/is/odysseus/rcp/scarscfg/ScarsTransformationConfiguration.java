b37d5597657d2a87ba5641d42239395106a947fa
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-08-10 10:41:14 +0000

    StreamCars: Neus Plugin de.uniol.inf.is.odysseus.rcp.scarscfg: Neue TransformationConfiguration speziell für StreamCars
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6074 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0d830cd109e95e79fe91b56b12f4d714d3a9cef7

---
+++ rcp/de.uniol.inf.is.odysseus.rcp.scarscfg/src/de/uniol/inf/is/odysseus/rcp/scarscfg/ScarsTransformationConfiguration.java
@@ -0,0 +1,31 @@
+package de.uniol.inf.is.odysseus.rcp.scarscfg;
+
+import de.uniol.inf.is.odysseus.base.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.base.planmanagement.query.querybuiltparameter.ParameterTransformationConfiguration;
+import de.uniol.inf.is.odysseus.rcp.viewer.query.IParameterTransformationConfigurationExtension;
+import de.uniol.inf.is.odysseus.transformation.helper.broker.BrokerTransformationHelper;
+
+public class ScarsTransformationConfiguration implements IParameterTransformationConfigurationExtension {
+
+	private static ParameterTransformationConfiguration trafoConfigParam = new ParameterTransformationConfiguration(
+			new TransformationConfiguration(
+			new BrokerTransformationHelper(),
+			"relational",
+			
+			// ObjectTracking
+			"de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval", // ok
+			"de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey", // ok
+			"de.uniol.inf.is.odysseus.latency.ILatency",  // ok
+			"de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability",  // ok
+			"de.uniol.inf.is.odysseus.objecttracking.metadata.IApplicationTime", // ok
+			
+			// StreamCars
+			"de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IConnectionContainer", // ok
+			"de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IGain")); // ok
+		
+	@Override
+	public ParameterTransformationConfiguration get() {
+		return trafoConfigParam;
+	}
+
+}
