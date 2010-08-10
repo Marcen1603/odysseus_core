686052d63b537cda682a8e49df3de550a6a17b98
Volker Janz <vjanz@uni-oldenburg.de>
2010-08-10 13:53:42 +0000

    Vorher ertellte SweepArea eingesetzt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6094 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e01b2638d080b6ec0ef4f5a48d985a3d5b7c9791

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/scars/objecttracking/association/physicaloperator/HypothesisGenerationPO.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/scars/objecttracking/association/physicaloperator/HypothesisGenerationPO.java
@@ -1,10 +1,15 @@
 package de.uniol.inf.is.odysseus.scars.objecttracking.association.physicaloperator;
 
+import java.util.Iterator;
+
 import de.uniol.inf.is.odysseus.base.PointInTime;
+import de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval;
 import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
 import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
+import de.uniol.inf.is.odysseus.physicaloperator.base.ISweepArea.Order;
 import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IConnectionContainer;
+import de.uniol.inf.is.odysseus.scars.util.PointInTimeSweepArea;
 import de.uniol.inf.is.odysseus.scars.util.SchemaHelper;
 import de.uniol.inf.is.odysseus.scars.util.SchemaIndexPath;
 
@@ -18,23 +23,26 @@ import de.uniol.inf.is.odysseus.scars.util.SchemaIndexPath;
  * @author Volker Janz
  *
  */
-public class HypothesisGenerationPO<M extends IProbability & IConnectionContainer> extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> {
-
-	private MVRelationalTuple<M> predictedObject;
-	private MVRelationalTuple<M> scannedObject;
+public class HypothesisGenerationPO<M extends IProbability & IConnectionContainer & ITimeInterval> extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> {
 
 	private String oldObjListPath;
 	private String newObjListPath;
 
+	private PointInTimeSweepArea<M> sweepPrediction;
+	private PointInTimeSweepArea<M> sweepScanned;
+
 	public HypothesisGenerationPO() {
+		super();
+		sweepPrediction = new PointInTimeSweepArea<M>();
+		sweepScanned = new PointInTimeSweepArea<M>();
 	}
 
 	public HypothesisGenerationPO(HypothesisGenerationPO<M> copy) {
 		super(copy);
-		this.predictedObject = copy.predictedObject;
-		this.scannedObject = copy.scannedObject;
 		this.oldObjListPath = copy.getOldObjListPath();
 		this.newObjListPath = copy.getNewObjListPath();
+		this.sweepPrediction = (PointInTimeSweepArea<M>) copy.sweepPrediction.clone();
+		this.sweepScanned =  (PointInTimeSweepArea<M>) copy.sweepScanned.clone();
 	}
 
 	@Override
@@ -47,42 +55,38 @@ public class HypothesisGenerationPO<M extends IProbability & IConnectionContaine
 	 */
 	@Override
 	protected void process_next(MVRelationalTuple<M> object, int port) {
-		if(this.predictedObject == null || this.scannedObject == null) {
-			switch(port) {
-				/*
-				 *  WICHTIG: Im sekund�ren Zyklus muss das, was aus dem prim�ren Zyklus
-				 *  aus der HypothesisSelection in diesen Operator kommt auf Port 1
-				 *  ankommen!!!!!!!!!
-				 *
-				 */
-				case 0: this.predictedObject = object;
-				case 1: this.scannedObject = object;
-			}
-		} else {
-			MVRelationalTuple<M> output = createOutputTuple(this.scannedObject, this.predictedObject);
-			this.predictedObject = null;
-			this.scannedObject = null;
+		if(port == 0) {
+			this.sweepPrediction.insert(object);
+		} else if(port == 1) {
+			this.sweepScanned.insert(object);
+		}
+
+		Iterator<MVRelationalTuple<M>> itPred = sweepPrediction.query(object, Order.LeftRight);
+		Iterator<MVRelationalTuple<M>> itScan = sweepScanned.query(object, Order.LeftRight);
+
+		if(itPred.hasNext() && itScan.hasNext()) {
+			MVRelationalTuple<M> output = createOutputTuple(itScan.next(), itPred.next());
 			transfer(output);
 		}
 	}
-	
+
 	private MVRelationalTuple<M> createOutputTuple(MVRelationalTuple<M> scannedObject, MVRelationalTuple<M> predictedObject) {
 		SchemaHelper helper = new SchemaHelper(getSubscribedToSource(0).getSchema());
-		
+
 		Object[] association = new Object[3];
-		
+
 		// get timestamp path from scanned data
 		SchemaIndexPath path = helper.getSchemaIndexPath(helper.getStartTimestampAttribute());
 		association[0] = path.toTupleIndexPath(scannedObject).getTupleObject();
-		
+
 		// get scanned objects
 		path = helper.getSchemaIndexPath(this.newObjListPath);
 		association[1] = path.toTupleIndexPath(scannedObject).getTupleObject();
-		
+
 		// get predicted objects
 		path = helper.getSchemaIndexPath(this.oldObjListPath);
 		association[2] =  path.toTupleIndexPath(predictedObject).getTupleObject();
-		
+
 		return new MVRelationalTuple<M>(association);
 	}
 
