0fedd305776c469fc000941c1c6b8ddc05c59cb9
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-08-11 11:39:50 +0000

    Nicht mehr benötigtes Plugin entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6121 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9a09dd246aa34b301921168e700dd70222298c2f

--- rcp/de.uniol.inf.is.odysseus.rcp.editor.navigator/src/de/uniol/inf/is/odysseus/rcp/editor/navigator/INavigatorConstants.java
+++
@@ -1,9 +0,0 @@
-package de.uniol.inf.is.odysseus.rcp.editor.navigator;
-
-public interface INavigatorConstants {
-	public static final String PLUGIN_ID = "de.uniol.inf.is.odysseus.rcp.editor.navigator";
-	
-	public static final String NAVIGATOR_VIEW_ID = "de.uniol.inf.is.odysseus.rcp.editor.navigator";
-	public static final String REFRESH_COMMAND_ID = "de.uniol.inf.is.odysseus.rcp.editor.navigator.RefreshCommand";
-	public static final String COLLAPSE_ALL_COMMAND_ID = "de.uniol.inf.is.odysseus.rcp.editor.navigator.CollapseAllCommand";
-}
