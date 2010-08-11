d9e6650eda9b1d3b844fde43b1fe56520df17885
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-08-11 20:38:17 +0000

    RCP: Neues Plugin: "de.uniol.inf.is.odysseus.rcp.stdcfg"  - Ergebnis von Refactoring  - beinhaltet die Standardtransformationskonfiguration
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6142 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c71ffdce877438f5fc6f1c0fd4a3d6bcebfde463

---
+++ rcp/de.uniol.inf.is.odysseus.rcp.stdcfg/src/de/uniol/inf/is/odysseus/rcp/stdcfg/StandardTransformationConfiguration.java
@@ -0,0 +1,25 @@
+package de.uniol.inf.is.odysseus.rcp.stdcfg;
+
+import de.uniol.inf.is.odysseus.base.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.base.planmanagement.query.querybuiltparameter.ParameterTransformationConfiguration;
+import de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval;
+import de.uniol.inf.is.odysseus.rcp.viewer.query.IParameterTransformationConfigurationExtension;
+import de.uniol.inf.is.odysseus.transformation.helper.relational.RelationalTransformationHelper;
+
+public class StandardTransformationConfiguration implements
+		IParameterTransformationConfigurationExtension {
+
+	@SuppressWarnings("unchecked")
+	private static ParameterTransformationConfiguration trafoConfigParam = 
+			new ParameterTransformationConfiguration(
+					new TransformationConfiguration(
+							new RelationalTransformationHelper(), 
+							"relational", 
+							ITimeInterval.class));
+
+	@Override
+	public ParameterTransformationConfiguration get() {
+		return trafoConfigParam;
+	}
+
+}
