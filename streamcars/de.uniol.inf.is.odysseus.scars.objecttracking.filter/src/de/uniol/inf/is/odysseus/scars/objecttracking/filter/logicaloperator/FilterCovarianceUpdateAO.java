61edc63ce510c1a96544af967ef9f7782bc6393c
Daniel Twumasi <dtwumasi@uni-oldenburg.de>
2010-08-17 11:04:36 +0000

    Aufteilung in 3 AO's.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6243 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 55e1723a2d49e20028af4b7696176f8e578632e7

---
+++ streamcars/de.uniol.inf.is.odysseus.scars.objecttracking.filter/src/de/uniol/inf/is/odysseus/scars/objecttracking/filter/logicaloperator/FilterCovarianceUpdateAO.java
@@ -0,0 +1,95 @@
+package de.uniol.inf.is.odysseus.scars.objecttracking.filter.logicaloperator;
+
+import java.util.HashMap;
+
+import de.uniol.inf.is.odysseus.base.LogicalSubscription;
+import de.uniol.inf.is.odysseus.logicaloperator.base.AbstractLogicalOperator;
+import de.uniol.inf.is.odysseus.logicaloperator.base.UnaryLogicalOp;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.scars.util.OrAttributeResolver;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
+
+public class FilterCovarianceUpdateAO <M extends IProbability> extends UnaryLogicalOp {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	private String functionID;
+	
+	// Optional parameters for the Filter function. Not used right now.
+	private HashMap<Integer, Object> parameters;
+	
+	// schemas
+	private SDFAttributeList leftSchema;
+	private SDFAttributeList rightSchema;
+	
+	public FilterCovarianceUpdateAO()
+	{
+    super();
+  }
+	
+	public FilterCovarianceUpdateAO(FilterCovarianceUpdateAO<M> copy) {
+		super(copy);
+		this.leftSchema = copy.getLeftSchema().clone();
+		this.rightSchema = copy.getRightSchema().clone();
+	}
+
+	@Override
+	public AbstractLogicalOperator clone() {
+		return new FilterCovarianceUpdateAO<M>(this);
+	}
+	
+	public SDFAttributeList getLeftSchema() {
+		return leftSchema;
+	}
+
+	public void setLeftSchema(SDFAttributeList leftSchema) {
+		this.leftSchema = leftSchema;
+	}
+
+	public SDFAttributeList getRightSchema() {
+		return rightSchema;
+	}
+
+	public void setRightSchema(SDFAttributeList rightSchema) {
+		this.rightSchema = rightSchema;
+	}
+	
+	@Override
+	public SDFAttributeList getOutputSchema() {
+		return this.getInputSchema();
+	}
+
+	/**
+	 * @param functionID the functionID to set
+	 */
+	public void setFunctionID(String functionID) {
+		this.functionID = functionID;
+	}
+
+	/**
+	 * @return the functionID
+	 */
+	public String getFunctionID() {
+		return functionID;
+	}
+
+	/**
+	 * @param parameters the parameters to set
+	 */
+	public void setParameters(HashMap<Integer, Object> parameters) {
+		this.parameters = parameters;
+	}
+
+	/**
+	 * @return the parameters
+	 */
+	public HashMap<Integer, Object> getParameters() {
+		return parameters;
+	}
+}
+
---
+++ streamcars/de.uniol.inf.is.odysseus.scars.objecttracking.filter/src/de/uniol/inf/is/odysseus/scars/objecttracking/filter/logicaloperator/FilterEstimateUpdateAO.java
@@ -0,0 +1,129 @@
+package de.uniol.inf.is.odysseus.scars.objecttracking.filter.logicaloperator;
+
+import java.util.HashMap;
+
+import de.uniol.inf.is.odysseus.base.LogicalSubscription;
+import de.uniol.inf.is.odysseus.logicaloperator.base.AbstractLogicalOperator;
+import de.uniol.inf.is.odysseus.logicaloperator.base.UnaryLogicalOp;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.scars.util.OrAttributeResolver;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
+
+public class FilterEstimateUpdateAO <M extends IProbability> extends UnaryLogicalOp {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	private String functionID;
+	
+	// path to new and old objects
+	private String oldObjListPath;
+
+	private String newObjListPath;
+	
+	// Optional parameters for the Filter function. Not used right now.
+	private HashMap<Integer, Object> parameters;
+	
+	// schemas
+	private SDFAttributeList leftSchema;
+	private SDFAttributeList rightSchema;
+	
+	public FilterEstimateUpdateAO()
+	{
+    super();
+  }
+	
+	public FilterEstimateUpdateAO(FilterEstimateUpdateAO<M> copy) {
+		super(copy);
+		this.oldObjListPath = copy.getOldObjListPath();
+		this.newObjListPath = copy.getNewObjListPath();
+		this.leftSchema = copy.getLeftSchema().clone();
+		this.rightSchema = copy.getRightSchema().clone();
+	}
+
+	
+	public String getOldObjListPath() {
+		return oldObjListPath;
+	}
+
+	public void setOldObjListPath(String oldObjListPath) {
+		this.oldObjListPath = oldObjListPath;
+	}
+	
+	public String getNewObjListPath() {
+		return newObjListPath;
+	}
+
+	public void setNewObjListPath(String newObjListPath) {
+		this.newObjListPath = newObjListPath;
+	}
+	
+	public int[] getNewObjListPathInt() {
+		this.leftSchema = ((LogicalSubscription[]) this.getSubscriptions().toArray())[0].getSchema();
+		return OrAttributeResolver.getAttributePath(leftSchema, this.newObjListPath);
+	}
+
+	public int[] getOldObjListPathInt() {
+		this.rightSchema = ((LogicalSubscription[]) this.getSubscriptions().toArray())[1].getSchema();
+		return OrAttributeResolver.getAttributePath(rightSchema, this.oldObjListPath);
+	}
+	
+	@Override
+	public AbstractLogicalOperator clone() {
+		return new FilterEstimateUpdateAO<M>(this);
+	}
+	
+	public SDFAttributeList getLeftSchema() {
+		return leftSchema;
+	}
+
+	public void setLeftSchema(SDFAttributeList leftSchema) {
+		this.leftSchema = leftSchema;
+	}
+
+	public SDFAttributeList getRightSchema() {
+		return rightSchema;
+	}
+
+	public void setRightSchema(SDFAttributeList rightSchema) {
+		this.rightSchema = rightSchema;
+	}
+	
+	@Override
+	public SDFAttributeList getOutputSchema() {
+		return this.getInputSchema();
+	}
+
+	/**
+	 * @param functionID the functionID to set
+	 */
+	public void setFunctionID(String functionID) {
+		this.functionID = functionID;
+	}
+
+	/**
+	 * @return the functionID
+	 */
+	public String getFunctionID() {
+		return functionID;
+	}
+
+	/**
+	 * @param parameters the parameters to set
+	 */
+	public void setParameters(HashMap<Integer, Object> parameters) {
+		this.parameters = parameters;
+	}
+
+	/**
+	 * @return the parameters
+	 */
+	public HashMap<Integer, Object> getParameters() {
+		return parameters;
+	}
+}
+
