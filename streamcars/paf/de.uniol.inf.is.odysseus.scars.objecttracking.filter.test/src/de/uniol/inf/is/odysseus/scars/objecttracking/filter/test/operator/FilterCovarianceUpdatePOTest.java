eb6e7d40f4eb65bc7eac46fafcc3bb9bdfbd09f8
Daniel Twumasi <dtwumasi@uni-oldenburg.de>
2010-08-23 18:25:44 +0000

    Filter Test recovered.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6661 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f82a83b507ee4d07ab44955e877776a903ff17d1

---
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.filter.test/src/de/uniol/inf/is/odysseus/scars/objecttracking/filter/test/operator/FilterCovarianceUpdatePOTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package de.uniol.inf.is.odysseus.scars.objecttracking.filter.test.operator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author dtwumasi
+ *
+ */
+public class FilterCovarianceUpdatePOTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link de.uniol.inf.is.odysseus.scars.objecttracking.filter.physicaloperator.FilterCovarianceUpdatePO#computeAll(de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple)}.
+	 */
+	@Test
+	public void testComputeAll() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.filter.test/src/de/uniol/inf/is/odysseus/scars/objecttracking/filter/test/operator/KalmanCorrectStateEstimatePOTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package de.uniol.inf.is.odysseus.scars.objecttracking.filter.test.operator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author dtwumasi
+ *
+ */
+public class KalmanCorrectStateEstimatePOTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link de.uniol.inf.is.odysseus.scars.objecttracking.filter.physicaloperator.FilterEstimateUpdatePO#computeAll(de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple)}.
+	 */
+	@Test
+	public void testComputeAll() {
+		fail("Not yet implemented");
+	}
+
+}
