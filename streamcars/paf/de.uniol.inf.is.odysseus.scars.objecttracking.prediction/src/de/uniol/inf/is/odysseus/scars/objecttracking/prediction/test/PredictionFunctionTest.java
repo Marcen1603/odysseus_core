0a973fa6c9c8182c1fed7e51eb1f237befc37d9e
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-08-24 04:10:35 +0000

    StreamCars: Warnungen in Prediction entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6673 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7710b6e1c6398f754f5d4b3b85bcbf80c85f5bf5

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.prediction/src/de/uniol/inf/is/odysseus/scars/objecttracking/prediction/test/PredictionFunctionTest.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.prediction/src/de/uniol/inf/is/odysseus/scars/objecttracking/prediction/test/PredictionFunctionTest.java
@@ -40,8 +40,6 @@ public class PredictionFunctionTest {
 		System.out.println();
 		
 		MVRelationalTuple<IProbability> list = scanTuple.getAttribute(0);
-		double[] measurements = list.getMeasurementValues();
-		double one = measurements[0];
 		//String firstAttribute = list.getAttribute(0).toString();
 		//assertEquals(firstAttribute, "9.0|9.0|0.0 | sz=(-) | META | null|405.0 | sz=(-) | META | [[4.5 4.5 4.5 4.5 ];[4.5 4.5 4.5 4.5 ];[4.5 4.5 4.5 4.5 ];[4.5 4.5 4.5 4.5 ];");
 		//String secondAttribute = list.getAttribute(1).toString();
@@ -147,6 +145,7 @@ public class PredictionFunctionTest {
 		return scan;
 	}
 	
+	@SuppressWarnings("unchecked")
 	private static<M extends IProbability> MVRelationalTuple<M> createObjectTuple() {
 		MVRelationalTuple<M> obj = new MVRelationalTuple<M>(2);
 		
@@ -229,6 +228,7 @@ public class PredictionFunctionTest {
 	}
 	
 	private static<M extends IProbability> MVRelationalTuple<M>[] createObjectList() {
+		@SuppressWarnings("unchecked")
 		MVRelationalTuple<M>[] list = new MVRelationalTuple[4];
 		list[0] = createObjectTuple();
 		list[1] = createObjectTuple();
