75a7acb30cc255b9061a8f391d843861428324c2
Niko Klein <nklein@uni-oldenburg.de>
2010-08-24 10:00:55 +0000

    SweepArea angepasst
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6682 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5449ab1095b108c8aacd0da5f5377de7c35df695

--- streamcars/de.uniol.inf.is.odysseus.scars.util/src/de/uniol/inf/is/odysseus/scars/util/PointInTimeSweepArea.java
+++ streamcars/de.uniol.inf.is.odysseus.scars.util/src/de/uniol/inf/is/odysseus/scars/util/PointInTimeSweepArea.java
@@ -30,9 +30,11 @@ public class PointInTimeSweepArea<M extends ITimeInterval & IProbability> extend
 	@Override
 	public void purgeElements(MVRelationalTuple<M> element, Order order) {
 		PointInTime time = element.getMetadata().getStart();
-		for(int i = 0; i < this.elements.size(); i++) {
-			if(this.elements.get(i).getMetadata().getStart().before(time)) {
-				this.elements.remove(i);
+		synchronized (elements) {
+			for(int i = 0; i < this.elements.size(); i++) {
+				if(this.elements.get(i).getMetadata().getStart().before(time)) {
+					this.elements.remove(i);
+				}
 			}
 		}
 	}
@@ -40,12 +42,14 @@ public class PointInTimeSweepArea<M extends ITimeInterval & IProbability> extend
 	@Override
 	public Iterator<MVRelationalTuple<M>> query(MVRelationalTuple<M> element, Order order) {
 		ArrayList<MVRelationalTuple<M>> retval = new ArrayList<MVRelationalTuple<M>>();
-		Iterator<MVRelationalTuple<M>> it = elements.iterator();
-		PointInTime pt = element.getMetadata().getStart();
-		while(it.hasNext()) {
-			MVRelationalTuple<M> tuple = it.next();
-			if(pt.equals(tuple.getMetadata().getStart())) {
-				retval.add(tuple);
+		synchronized (elements) {
+			Iterator<MVRelationalTuple<M>> it = elements.iterator();
+			PointInTime pt = element.getMetadata().getStart();
+			while(it.hasNext()) {
+				MVRelationalTuple<M> tuple = it.next();
+				if(pt.equals(tuple.getMetadata().getStart())) {
+					retval.add(tuple);
+				}
 			}
 		}
 		return retval.iterator();
