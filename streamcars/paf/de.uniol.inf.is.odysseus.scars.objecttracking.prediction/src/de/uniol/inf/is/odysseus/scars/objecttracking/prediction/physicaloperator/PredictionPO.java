9a0f6d19930440a7bb7b5548e7cb5531229661d7
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-08-24 04:07:24 +0000

    StreamCars: Bugfix in PredictionOperator
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6671 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 482db5501bc792a718c038469373527a19a23e33

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.prediction/src/de/uniol/inf/is/odysseus/scars/objecttracking/prediction/physicaloperator/PredictionPO.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.prediction/src/de/uniol/inf/is/odysseus/scars/objecttracking/prediction/physicaloperator/PredictionPO.java
@@ -18,19 +18,18 @@ import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
 public class PredictionPO<M extends IProbability & ITimeInterval & IPredictionFunctionKey<IPredicate<MVRelationalTuple<M>>>> extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> {
 
-	
 	private MVRelationalTuple<M> currentTimeTuple;
 	private MVRelationalTuple<M> currentScanTuple;
-	
+
 	private int[] objListPath;
 	private SchemaIndexPath currentTimeSchemaPath;
 	private SchemaIndexPath currentScanTimeSchemaPath;
 
 	private PredictionFunctionContainer<M> predictionFunctions;
-	
+
 	public PredictionPO() {
 	}
-	
+
 	public PredictionPO(PredictionPO<M> copy) {
 		super(copy);
 		this.predictionFunctions = new PredictionFunctionContainer<M>(copy.predictionFunctions);
@@ -39,11 +38,11 @@ public class PredictionPO<M extends IProbability & ITimeInterval & IPredictionFu
 		this.currentTimeSchemaPath = copy.currentTimeSchemaPath;
 		this.currentScanTimeSchemaPath = copy.currentScanTimeSchemaPath;
 	}
-	
+
 	public void setPredictionFunctions(PredictionFunctionContainer<M> predictionFunctions) {
 		this.predictionFunctions = predictionFunctions;
 	}
-	
+
 	public void setObjectListPath(int[] objListPath) {
 		this.objListPath = objListPath;
 	}
@@ -52,7 +51,7 @@ public class PredictionPO<M extends IProbability & ITimeInterval & IPredictionFu
 	public OutputMode getOutputMode() {
 		return OutputMode.NEW_ELEMENT;
 	}
-	
+
 	@Override
 	protected void process_open() throws OpenFailedException {
 		super.process_open();
@@ -63,53 +62,55 @@ public class PredictionPO<M extends IProbability & ITimeInterval & IPredictionFu
 		SchemaHelper helper2 = new SchemaHelper(scanSchema);
 		currentScanTimeSchemaPath = helper2.getSchemaIndexPath(helper2.getStartTimestampFullAttributeName());
 	}
-	
+
 	@Override
 	protected void process_next(MVRelationalTuple<M> object, int port) {
-		if(port == 0) {
+		if (port == 0) {
 			currentTimeTuple = object.clone();
-		} else if(port == 1) {
+		} else if (port == 1) {
 			currentScanTuple = object.clone();
 		}
-		if(currentTimeTuple != null && currentScanTuple != null) {
+		if (currentTimeTuple != null && currentScanTuple != null) {
 			predictData();
 			MVRelationalTuple<M> tmp = currentScanTuple;
 			currentScanTuple = null;
 			transfer(tmp);
 		}
 	}
-	
-	
-	
-//	@SuppressWarnings("unchecked")
+
+	// @SuppressWarnings("unchecked")
 	private void predictData() {
-		TupleHelper helper = new TupleHelper(currentScanTuple);
-		MVRelationalTuple<?> list = (MVRelationalTuple<?>)helper.getObject(objListPath);
-		for(int index=0; index < list.getAttributeCount(); index++) {
-			MVRelationalTuple<M> obj = list.getAttribute(index);
-			IPredictionFunction<M> pf = predictionFunctions.get(obj.getMetadata().getPredictionFunctionKey());
-			if(pf != null) {
-				pf.predictData(currentScanTuple, currentTimeTuple, index);
-				M metadata = obj.getMetadata();
-				pf.predictMetadata(metadata, currentScanTuple, currentTimeTuple, index);
-			} else {
-				System.err.println("No PredictionFunction assigned (NO DEFAULT PREDICTION_FUNCTION)");
+		if (currentScanTuple != null) {
+			TupleHelper helper = new TupleHelper(currentScanTuple);
+			MVRelationalTuple<?> list = (MVRelationalTuple<?>) helper.getObject(objListPath);
+			for (int index = 0; index < list.getAttributeCount(); index++) {
+				MVRelationalTuple<M> obj = list.getAttribute(index);
+				IPredictionFunction<M> pf = predictionFunctions.get(obj.getMetadata().getPredictionFunctionKey());
+				if (pf != null) {
+					pf.predictData(currentScanTuple, currentTimeTuple, index);
+					M metadata = obj.getMetadata();
+					pf.predictMetadata(metadata, currentScanTuple, currentTimeTuple, index);
+				} else {
+					System.err.println("No PredictionFunction assigned (NO DEFAULT PREDICTION_FUNCTION)");
+				}
+
 			}
-			
+			TupleIndexPath scanTimeTPath = currentTimeSchemaPath.toTupleIndexPath(currentTimeTuple);
+			Long currentTimeValue = (Long) scanTimeTPath.getTupleObject();
+
+			currentScanTuple.getMetadata().getStart().setMainPoint(currentTimeValue);
+			TupleIndexPath currentScanTimeTPath = currentScanTimeSchemaPath.toTupleIndexPath(currentScanTuple);
+			currentScanTimeTPath.setTupleObject(currentTimeValue);
+		} else {
+			System.err.println("Prediction: currentScanTuple was null!!");
 		}
-		TupleIndexPath scanTimeTPath = currentTimeSchemaPath.toTupleIndexPath(currentTimeTuple);
-		Long currentTimeValue = (Long)scanTimeTPath.getTupleObject();
-		
-		currentScanTuple.getMetadata().getStart().setMainPoint(currentTimeValue);
-		TupleIndexPath currentScanTimeTPath = currentScanTimeSchemaPath.toTupleIndexPath(currentScanTuple);
-		currentScanTimeTPath.setTupleObject(currentTimeValue);
 	}
 
 	@Override
 	public void processPunctuation(PointInTime timestamp, int port) {
 		this.sendPunctuation(timestamp);
 	}
-	
+
 	@Override
 	public PredictionPO<M> clone() {
 		return new PredictionPO<M>(this);
