ee31c2bcfb0200bf847d8ccd9791676784ac9c72
Andre Bolles <andre.bolles@offis.de>
2010-08-25 09:18:21 +0000

    TestPO kann jetzt Speicherverbrauch messen
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6701 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 6ebd9f217f301679837c01eb6acd189f3ebbc85c

---
+++ streamcars/de.uniol.inf.is.odysseus.scars.operator.test.po/src/de/uniol/inf/is/odysseus/scars/operator/test/po/Activator.java
@@ -0,0 +1,29 @@
+package de.uniol.inf.is.odysseus.scars.operator.test.po;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	static BundleContext bc;
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		bc = context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		bc = null;
+	}
+	
+	public static BundleContext getBundleContext(){
+		return bc;
+	}
+
+}
--- streamcars/de.uniol.inf.is.odysseus.scars.operator.test.po/src/de/uniol/inf/is/odysseus/scars/operator/test/po/TestPO.java
+++ streamcars/de.uniol.inf.is.odysseus.scars.operator.test.po/src/de/uniol/inf/is/odysseus/scars/operator/test/po/TestPO.java
@@ -1,12 +1,18 @@
 package de.uniol.inf.is.odysseus.scars.operator.test.po;
 
+import org.osgi.util.tracker.ServiceTracker;
+
 import de.uniol.inf.is.odysseus.base.PointInTime;
+import de.uniol.inf.is.odysseus.monitoring.ISystemMonitor;
+import de.uniol.inf.is.odysseus.monitoring.ISystemMonitorFactory;
 import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
 import de.uniol.inf.is.odysseus.util.LoggerHelper;
 
 public class TestPO<T> extends AbstractPipe<T, T>{
 
 	private String name;
+	private ISystemMonitor monitor;
+	private ISystemMonitorFactory factory;
 	
 	public TestPO(String debugName) {
 		this.name = debugName;
@@ -20,13 +26,24 @@ public class TestPO<T> extends AbstractPipe<T, T>{
 	public OutputMode getOutputMode() {
 		return OutputMode.INPUT;
 	}
+	
+	public void process_open(){
+		ServiceTracker tracker = new ServiceTracker(Activator.bc, ISystemMonitorFactory.class.getName(), null);
+		tracker.open();
+		this.monitor = ((ISystemMonitorFactory)tracker.getService()).newSystemMonitor(); 
+		this.monitor.initialize();
+	}
 
 	@Override
 	protected void process_next(T object, int port) {
-		//BENUTZT GEFAELLIGST DEN LOGGER FUER SOWAS! (ja auch nur um kurz was zu testen)!
-		//Ich will keine Sysouts irgendwo im code lesen! :D
-		LoggerHelper.getInstance(this.name).debug("TESTPO: Dataelement processed : " + object);
+		//LoggerHelper.getInstance(this.name).debug("TESTPO: Dataelement processed : " + object);
 		transfer(object);
+	}
+	
+	public void process_done(){
+		LoggerHelper.getInstance(this.name).info("AvgMem: " + this.monitor.getAverageMemoryUsage());
+		LoggerHelper.getInstance(this.name).info("MaxMem: " + this.monitor.getMaxMemoryUsage());
+		this.monitor.stop();
 	}
 
 	@Override
