20a4fc7028334ac41861da6da9aa3e2948e69eb8
Volker Janz <vjanz@uni-oldenburg.de>
2010-08-30 10:46:32 +0000

    Fehler bei MultiDistanceEvaluationPO behoben
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6741 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f00cb3fe5dee8198f6720f668748cfdc60fc78ce

--- streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/scars/objecttracking/association/physicaloperator/MultiDistanceEvaluationPO.java
+++ streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.assoziation/src/de/uniol/inf/is/odysseus/scars/objecttracking/association/physicaloperator/MultiDistanceEvaluationPO.java
@@ -1,10 +1,13 @@
 package de.uniol.inf.is.odysseus.scars.objecttracking.association.physicaloperator;
 
+import weka.core.Attribute;
 import weka.core.ChebyshevDistance;
 import weka.core.DenseInstance;
 import weka.core.DistanceFunction;
 import weka.core.EuclideanDistance;
+import weka.core.FastVector;
 import weka.core.Instance;
+import weka.core.Instances;
 import weka.core.ManhattanDistance;
 import weka.core.MinkowskiDistance;
 import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
@@ -12,6 +15,7 @@ import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
 import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
 import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IConnectionContainer;
 
+@SuppressWarnings("deprecation")
 public class MultiDistanceEvaluationPO<M extends IProbability & IConnectionContainer> extends
     AbstractHypothesisEvaluationPO<M> {
 
@@ -61,21 +65,56 @@ public class MultiDistanceEvaluationPO<M extends IProbability & IConnectionConta
 	}
   }
 
+  @SuppressWarnings({ "unchecked", "deprecation", "rawtypes" })
   @Override
   public double evaluate(double[][] scannedObjCovariance, double[] scannedObjMesurementValues,
       double[][] predictedObjCovariance, double[] predictedObjMesurementValues, double currentRating) {
 
+	FastVector atts = new FastVector();
+
     // Create instances
-    Instance scannedObjInstance = new DenseInstance(scannedObjMesurementValues.length);
-    Instance predictedObjInstance = new DenseInstance(predictedObjMesurementValues.length);
+//    Instance scannedObjInstance = new DenseInstance(scannedObjMesurementValues.length);
+//    Instance predictedObjInstance = new DenseInstance(predictedObjMesurementValues.length);
 
     // Set values
+//    for (int i = 0; i < scannedObjMesurementValues.length; i++) {
+//      scannedObjInstance.setValue(i, scannedObjMesurementValues[i]);
+//    }
+//    for (int i = 0; i < predictedObjMesurementValues.length; i++) {
+//      predictedObjInstance.setValue(i, predictedObjMesurementValues[i]);
+//    }
+
     for (int i = 0; i < scannedObjMesurementValues.length; i++) {
-      scannedObjInstance.setValue(i, scannedObjMesurementValues[i]);
+    	atts.addElement(new Attribute(String.valueOf(i)));
     }
+
+    Instances data = new Instances("Instances", atts, 0);
+
+    // 1st instance
+
+    double[] vals = new double[data.numAttributes()];
+
+    for (int i = 0; i < scannedObjMesurementValues.length; i++) {
+    	vals[i] = scannedObjMesurementValues[i];
+    }
+
+    Instance scannedObjInstance = new DenseInstance(scannedObjMesurementValues.length, vals);
+
+    data.add(scannedObjInstance);
+
+    // 2nd instance
+
+    vals = new double[data.numAttributes()];
+
     for (int i = 0; i < predictedObjMesurementValues.length; i++) {
-      predictedObjInstance.setValue(i, predictedObjMesurementValues[i]);
+    	vals[i] = predictedObjMesurementValues[i];
     }
+
+    Instance predictedObjInstance = new DenseInstance(predictedObjMesurementValues.length, vals);
+
+    data.add(predictedObjInstance);
+
+    df.setInstances(data);
 
     // VJ:
     // What about the covariance matrix? How to combine the weka distance function with the cov.?
