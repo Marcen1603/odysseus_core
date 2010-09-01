180865dcd2f3728004c5e302cc675ac8380dbfcf
Dennis Geesen <geesen@indivon.de>
2010-09-01 11:38:32 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6811 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: f4cb24980cf710282d5c2df7486fc58d11aab54d

--- base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/planmanagement/executor/standardexecutor/StandardExecutor.java
+++ base/de.uniol.inf.is.odysseus.base/src/de/uniol/inf/is/odysseus/planmanagement/executor/standardexecutor/StandardExecutor.java
@@ -180,7 +180,7 @@ public class StandardExecutor extends AbstractExecutor implements IAdvancedExecu
 		getLogger().debug("Translate Queries.");
 		// translate query and build logical plans
 		List<IQuery> queries = compiler().translateQuery(queryStr, parserID);
-		getLogger().debug("#Queryies in queryStr: " + queries.size());
+		getLogger().trace("Number of queries: " + queries.size());
 		// create for each logical plan an intern query
 		for (IQuery query : queries) {
 			query.setBuildParameter(parameters);
