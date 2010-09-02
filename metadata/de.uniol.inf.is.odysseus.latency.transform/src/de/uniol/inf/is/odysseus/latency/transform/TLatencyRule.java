c9f1ca9c67012975b554342d405fd1e0547b0d0c
Dennis Geesen <geesen@indivon.de>
2010-09-02 09:24:02 +0000

    transform für latency
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6822 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b48d71d0c165707beeca99c941a378d56c0212c0

---
+++ metadata/de.uniol.inf.is.odysseus.latency.transform/src/de/uniol/inf/is/odysseus/latency/transform/TLatencyRule.java
@@ -0,0 +1,37 @@
+package de.uniol.inf.is.odysseus.latency.transform;
+
+import de.uniol.inf.is.odysseus.base.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.intervalapproach.JoinTIPO;
+import de.uniol.inf.is.odysseus.latency.LatencyMergeFunction;
+import de.uniol.inf.is.odysseus.metadata.base.CombinedMergeFunction;
+import de.uniol.inf.is.odysseus.transform.rule.AbstractTransformationRule;
+
+public class TLatencyRule extends AbstractTransformationRule<JoinTIPO<?,?>> {
+
+	@Override
+	public int getPriority() {
+		return 0;
+	}
+	
+	@Override
+	public void execute(JoinTIPO<?, ?> joinPO, TransformationConfiguration config) {
+		((CombinedMergeFunction)joinPO.getMetadataMerge()).add(new LatencyMergeFunction());		
+	}
+
+	@Override
+	public boolean isExecutable(JoinTIPO<?, ?> joinPO, TransformationConfiguration config) {
+		if(joinPO.getMetadataMerge() instanceof CombinedMergeFunction){
+			if(config.getMetaTypes().contains("de.uniol.inf.is.odysseus.latency.ILatency")){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String getName() {
+		return  "JoinTIPO add MetadataMerge (ILatency)";
+	}
+
+	
+}
