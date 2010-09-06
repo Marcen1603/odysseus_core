6487f0c444be350d4d9f7dcdc37aaecfd9332b21
Volker Janz <vjanz@uni-oldenburg.de>
2010-09-06 12:34:30 +0000

    New operator for the temporary part of the scars project.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6858 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0e41a7c97ceda736f08f33702c1422df4109adbb

---
+++ streamcars/src/de/uniol/inf/is/odysseus/scars/objecttracking/temporarydatabouncer/logicaloperator/TemporaryDataBouncerAO.java
@@ -0,0 +1,50 @@
+package de.uniol.inf.is.odysseus.scars.objecttracking.temporarydatabouncer.logicaloperator;
+
+import de.uniol.inf.is.odysseus.logicaloperator.base.AbstractLogicalOperator;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
+
+public class TemporaryDataBouncerAO<M extends IProbability> extends AbstractLogicalOperator {
+
+	private static final long serialVersionUID = 1L;
+
+	private String objListPath;
+	private double threshold;
+
+	public TemporaryDataBouncerAO() {
+		super();
+	}
+
+	public TemporaryDataBouncerAO(TemporaryDataBouncerAO<M> copy) {
+		super(copy);
+		this.objListPath = copy.objListPath;
+		this.threshold = copy.threshold;
+	}
+
+	@Override
+	public SDFAttributeList getOutputSchema() {
+		return this.getInputSchema(0);
+	}
+
+	@Override
+	public AbstractLogicalOperator clone() {
+		return new TemporaryDataBouncerAO<M>(this);
+	}
+
+	public String getObjListPath() {
+		return objListPath;
+	}
+
+	public void setObjListPath(String objListPath) {
+		this.objListPath = objListPath;
+	}
+
+	public double getThreshold() {
+		return threshold;
+	}
+
+	public void setThreshold(double threshold) {
+		this.threshold = threshold;
+	}
+
+}
---
+++ streamcars/src/de/uniol/inf/is/odysseus/scars/objecttracking/temporarydatabouncer/physicaloperator/TemporaryDataBouncerPO.java
@@ -0,0 +1,92 @@
+package de.uniol.inf.is.odysseus.scars.objecttracking.temporarydatabouncer.physicaloperator;
+
+
+import java.util.ArrayList;
+
+import de.uniol.inf.is.odysseus.base.PointInTime;
+import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
+import de.uniol.inf.is.odysseus.scars.util.SchemaHelper;
+import de.uniol.inf.is.odysseus.scars.util.SchemaIndexPath;
+import de.uniol.inf.is.odysseus.scars.util.TupleIndexPath;
+
+public class TemporaryDataBouncerPO<M extends IProbability> extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> {
+
+	private String objListPath;
+	private double threshold;
+
+	public TemporaryDataBouncerPO() {
+		super();
+	}
+
+	public TemporaryDataBouncerPO(TemporaryDataBouncerPO<M> clone) {
+		super(clone);
+		this.objListPath = clone.objListPath;
+		this.threshold = clone.threshold;
+	}
+
+	@Override
+	public void processPunctuation(PointInTime timestamp, int port) {
+		this.sendPunctuation(timestamp);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void process_next(MVRelationalTuple<M> object, int port) {
+		SchemaHelper sh = new SchemaHelper(getSubscribedToSource(0).getSchema());
+		// Get the list of cars
+		MVRelationalTuple<M> carListTuple = (MVRelationalTuple<M>) sh.getSchemaIndexPath(this.objListPath).toTupleIndexPath(object).getTupleObject();
+		// Init an arraylist for the elements that should be transfered
+		ArrayList<MVRelationalTuple<M>> transferCarListArrayList = new ArrayList<MVRelationalTuple<M>>();
+
+		double val = 0;
+		for(Object carObject : carListTuple.getAttributes()) {
+			MVRelationalTuple<M> car = (MVRelationalTuple<M>) carObject;
+			val = 0;
+			double[][] cov = car.getMetadata().getCovariance();
+			for(int i = 0; i < cov.length; i++) {
+				val += cov[i][i];
+			}
+			if(val < this.threshold) {
+				transferCarListArrayList.add(car);
+			}
+		}
+
+		MVRelationalTuple<M> tuples = new MVRelationalTuple<M>(transferCarListArrayList.size());
+		int counter = 0;
+		for (MVRelationalTuple<M> mvRelationalTuple : transferCarListArrayList) {
+			tuples.addAttributeValue(counter++, mvRelationalTuple);
+		}
+
+		SchemaIndexPath schemaPath = sh.getSchemaIndexPath(this.objListPath);
+		TupleIndexPath tuplePath = schemaPath.toTupleIndexPath(object);
+		tuplePath.setTupleObject(tuples);
+	}
+
+	@Override
+	public OutputMode getOutputMode() {
+		return OutputMode.MODIFIED_INPUT;
+	}
+
+	@Override
+	public AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> clone() {
+		return new TemporaryDataBouncerPO<M>(this);
+	}
+
+	public String getObjListPath() {
+		return objListPath;
+	}
+
+	public void setObjListPath(String objListPath) {
+		this.objListPath = objListPath;
+	}
+
+	public double getThreshold() {
+		return threshold;
+	}
+
+	public void setThreshold(double threshold) {
+		this.threshold = threshold;
+	}
+}
