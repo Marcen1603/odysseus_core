d73ef066ab07d07a33508321943cd0ab52766d97
Thomas Vogelgesang <Thomas.Vogelgesang@uni-oldenburg.de>
2010-09-06 13:55:34 +0000

    implemented physical operator jdvesink
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6879 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: bfbbfed487f51ab97d4447cddb4613898b291f68

--- streamcars/de.uniol.inf.is.odysseus.scars.operator.jdvesink/src/de/uniol/inf/is/odysseus/scars/operator/jdvesink/physicaloperator/JDVESink.java
+++ streamcars/de.uniol.inf.is.odysseus.scars.operator.jdvesink/src/de/uniol/inf/is/odysseus/scars/operator/jdvesink/physicaloperator/JDVESink.java
@@ -1,5 +1,7 @@
 package de.uniol.inf.is.odysseus.scars.operator.jdvesink.physicaloperator;
 
+import java.nio.ByteBuffer;
+
 import de.uniol.inf.is.odysseus.base.OpenFailedException;
 import de.uniol.inf.is.odysseus.base.PointInTime;
 import de.uniol.inf.is.odysseus.base.predicate.IPredicate;
@@ -10,6 +12,9 @@ import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
 import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
 import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IConnectionContainer;
 import de.uniol.inf.is.odysseus.scars.operator.jdvesink.server.NIOServer;
+import de.uniol.inf.is.odysseus.scars.util.TupleInfo;
+import de.uniol.inf.is.odysseus.scars.util.TupleIterator;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
 public class JDVESink<M extends IProbability & IPredictionFunctionKey<IPredicate<MVRelationalTuple<M>>> & IConnectionContainer & ITimeInterval>
 extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> {
@@ -27,13 +32,6 @@ extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> {
 
 	@Override
 	public void processPunctuation(PointInTime timestamp, int port) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void process_next(MVRelationalTuple<M> object, int port) {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -41,6 +39,67 @@ extends AbstractPipe<MVRelationalTuple<M>, MVRelationalTuple<M>> {
 	protected void process_open() throws OpenFailedException {
 		super.process_open();
 		this.server = new NIOServer(this.port);
+		this.server.start();
+	}
+
+	@Override
+	protected void process_next(MVRelationalTuple<M> object, int port) {
+		// iterate over schema and calculate size of byte buffer
+		int bufferSize = 0;
+		SDFAttributeList schema = this.getSubscribedToSource(0).getSchema();
+		TupleIterator iterator = new TupleIterator(object, schema);
+		for (TupleInfo info : iterator) {
+			//atomare typen beachten, komplexe Ÿbergehen
+			if (!info.isTuple) {
+				if (info.tupleObject instanceof Integer) {
+					bufferSize += 4;
+				} else if (info.tupleObject instanceof Long) {
+					bufferSize += 8;
+				} else if (info.tupleObject instanceof Float) {
+					bufferSize += 4;
+				} else if (info.tupleObject instanceof Double) {
+					bufferSize += 8;
+				} else if (info.tupleObject instanceof String) {
+					bufferSize += ((String)info.tupleObject).length();
+				} else if (info.tupleObject instanceof Byte) {
+					bufferSize++;
+				} else if (info.tupleObject instanceof Character) {
+					bufferSize++;
+				}
+			}
+		}
+		
+		//Allocate byte buffer
+		ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+		
+		// iterate over tuple and write elementary data to byte buffer
+		iterator = new TupleIterator(object, schema);
+		for (TupleInfo info : iterator) {
+			if (!info.isTuple) {
+				if (info.tupleObject instanceof Integer) {
+					buffer.putInt((Integer) info.tupleObject);
+				} else if (info.tupleObject instanceof Long) {
+					buffer.putLong((Long) info.tupleObject);
+				} else if (info.tupleObject instanceof Float) {
+					buffer.putFloat((Float) info.tupleObject);
+				} else if (info.tupleObject instanceof Double) {
+					buffer.putDouble((Double) info.tupleObject);
+				} else if (info.tupleObject instanceof String) {
+					String str = (String)info.tupleObject;
+					for (int i = 0; i < str.length(); i++) {
+						buffer.putChar(str.charAt(i));
+					}
+				} else if (info.tupleObject instanceof Byte) {
+					buffer.put((Byte) info.tupleObject);
+				} else if (info.tupleObject instanceof Character) {
+					buffer.putChar((Character)info.tupleObject);
+				}
+			}
+		}
+		
+		// push byte buffer to server
+		buffer.flip();
+		this.server.addData(buffer);
 	}
 
 	@Override
