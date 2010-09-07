aadf194d0337876f5c8a84a65b8d3206f309e5fc
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-09-07 13:21:41 +0000

    Rahmen fuer SLA basiertes Scheduling, bisher noch keinerlei Funktionalitaet
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6940 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0ef4e16223f84b4e4109bcb274e0a91f6fbf6718

---
+++ scheduler/de.uniol.inf.is.odysseus.scheduler.slascheduler/src/de/uniol/inf/is/odysseus/scheduler/slascheduler/strategy/SimpleSLAScheduler.java
@@ -0,0 +1,60 @@
+package de.uniol.inf.is.odysseus.scheduler.slascheduler.strategy;
+
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.base.IOperatorOwner;
+import de.uniol.inf.is.odysseus.base.IPhysicalOperator;
+import de.uniol.inf.is.odysseus.base.planmanagement.query.IQuery;
+import de.uniol.inf.is.odysseus.base.usermanagement.IServiceLevelAgreement;
+import de.uniol.inf.is.odysseus.base.usermanagement.NotInitializedException;
+import de.uniol.inf.is.odysseus.base.usermanagement.Tenant;
+import de.uniol.inf.is.odysseus.base.usermanagement.TenantManagement;
+import de.uniol.inf.is.odysseus.base.usermanagement.User;
+import de.uniol.inf.is.odysseus.physicaloperator.base.plan.IPartialPlan;
+import de.uniol.inf.is.odysseus.scheduler.priorityscheduler.AbstractDynamicPriorityPlanScheduling;
+import de.uniol.inf.is.odysseus.scheduler.strategy.IScheduling;
+
+public class SimpleSLAScheduler extends AbstractDynamicPriorityPlanScheduling {
+
+	public SimpleSLAScheduler(SimpleSLAScheduler simpleSLAScheduler) {
+		super(simpleSLAScheduler);
+	}
+
+	@Override
+	protected void updatePriorities(IScheduling current) {
+		// Der aktuelle interessiert gar nicht
+		// Jetzt für alle zu schedulden Plaene die entsprechenden SLAs
+		// identifizieren
+		// TODO: STATISCH BERECHNEN
+		synchronized (queue) {
+			for (IScheduling scheduling : queue) {
+				IPartialPlan plan = scheduling.getPlan();
+				List<IPhysicalOperator> roots = plan.getRoots();
+				for (IPhysicalOperator r:roots){
+					// TODO: Ermitteln wie  die currentSLAConformance ist
+					double currentSLAConformance = 0.5; 
+					List<IOperatorOwner> owners = r.getOwner();
+					for (IOperatorOwner owner: owners){
+						if (owner instanceof IQuery){
+							User user = ((IQuery)owner).getUser();
+ 							Tenant t = TenantManagement.getInstance().getTenant(user);
+							IServiceLevelAgreement sla = t.getServiceLevelAgreement();
+							try {
+								double urge = sla.getMaxOcMg(currentSLAConformance);
+							} catch (NotInitializedException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public SimpleSLAScheduler clone() {
+		return new SimpleSLAScheduler(this);
+	}
+
+}
