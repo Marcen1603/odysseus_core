4f35e27e2a7c53fddb66d34a1d149b3d2f3c8b74
Benjamin Gruenebast <bgruenebast@uni-oldenburg.de>
2010-09-07 10:03:27 +0000

    XML Profiler für objecttracking operatoren (test/probe)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@6909 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: fc30179986e29025b70bcd6b8543e9fc96c55f0c

---
+++ streamcars/de.uniol.inf.is.odysseus.scars.profil/src/de/uniol/inf/is/odysseus/scars/profil/ScarsXMLProfiler.java
@@ -0,0 +1,18 @@
+package de.uniol.inf.is.odysseus.scars.profil;
+
+import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.objecttracking.sdf.SDFAttributeListExtended;
+
+public class ScarsXMLProfiler {
+	
+	private static ScarsXMLProfiler instance;
+	
+	private ScarsXMLProfiler() {
+		
+	}
+	public void profile(String operator, int port, SDFAttributeListExtended schema, MVRelationalTuple<IProbability> scan) {
+		
+	}
+
+}
