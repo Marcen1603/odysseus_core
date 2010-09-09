757e601d2e918e4d89572c1433f0f75908b648eb
Jonas Jacobi <jacobi@uni-oldenburg.de>
2010-09-09 11:18:07 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7011 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: 20013cd4f770c4a1149bd5297420141bad4c0802

---
+++ benchmarker.runner/src/de/uniol/inf/is/odysseus/benchmarker/runner/Activator.java
@@ -0,0 +1,75 @@
+package de.uniol.inf.is.odysseus.benchmarker.runner;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "de.uniol.inf.is.odysseus.benchmarker.runner";
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				for (Bundle bundle : context.getBundles()) {
+					boolean isFragment = bundle.getHeaders().get(
+							Constants.FRAGMENT_HOST) != null;
+					if (bundle != context.getBundle() && !isFragment
+							&& bundle.getState() == Bundle.RESOLVED) {
+						try {
+							bundle.start();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		});
+		t.start();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
