73e4e2a1ce7d686f49303c3518ea1fd96d5874e3
Dennis Geesen <geesen@indivon.de>
2010-09-13 07:16:30 +0000

    Bugifixes im broker eingebaut
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7076 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e3eba7de5b52d6cb6cbe1d79909f64009bfbdc53

--- sensor/broker/broker/src/de/uniol/inf/is/odysseus/broker/physicaloperator/BrokerPO.java
+++ sensor/broker/broker/src/de/uniol/inf/is/odysseus/broker/physicaloperator/BrokerPO.java
@@ -21,7 +21,6 @@ import de.uniol.inf.is.odysseus.intervalapproach.TimeIntervalComparator;
 import de.uniol.inf.is.odysseus.metadata.base.IMetaAttributeContainer;
 import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractPipe;
 import de.uniol.inf.is.odysseus.physicaloperator.base.ISink;
-import de.uniol.inf.is.odysseus.physicaloperator.base.ISource;
 import de.uniol.inf.is.odysseus.physicaloperator.base.ISweepArea.Order;
 import de.uniol.inf.is.odysseus.physicaloperator.base.PhysicalSubscription;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
@@ -29,8 +28,7 @@ import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 /**
  * BrokerPO represents the physical implementation of the broker operator.
  * 
- * @param <T>
- *            the reading and writing type of a tuple
+ * @param <T> the reading and writing type of a tuple
  * @author Dennis Geesen
  */
 public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends AbstractPipe<T, T> {
@@ -48,7 +46,7 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	private PriorityQueue<TransactionTS> timestampList = new PriorityQueue<TransactionTS>();
 
 	/** The queue schema. */
-	private SDFAttributeList queueSchema;	
+	private SDFAttributeList queueSchema;
 
 	/**
 	 * The waiting buffer caches all elements before it is their chronological
@@ -63,7 +61,19 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	 * The min is the minimum time of all writing streams except of cyclic
 	 * streams.
 	 */
-	private PointInTime min = null;
+	private PointInTime min = PointInTime.getZeroTime();
+
+	/**
+	 * The minTSList is the minimum time of all timestamp streams except of
+	 * cyclic streams.
+	 */
+	private PointInTime minTSList = PointInTime.getZeroTime();
+
+	/**
+	 * The waitingFor is the minimum time for which the broker waits until
+	 * processing a new request
+	 */
+	private PointInTime waitingFor = PointInTime.getZeroTime();
 
 	/** Sets debug outputs on or off. */
 	private boolean printDebug = true;
@@ -74,8 +84,7 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	/**
 	 * Instantiates a new BrokerPO.
 	 * 
-	 * @param identifier
-	 *            the name of the broker
+	 * @param identifier the name of the broker
 	 */
 	public BrokerPO(String identifier) {
 		this.identifier = identifier;
@@ -85,8 +94,7 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	/**
 	 * Instantiates a new BrokerPO from an old one.
 	 * 
-	 * @param po
-	 *            the old one
+	 * @param po the old one
 	 */
 	public BrokerPO(BrokerPO<T> po) {
 		this.identifier = po.getIdentifier();
@@ -105,8 +113,7 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	/**
 	 * Sets the queue schema.
 	 * 
-	 * @param queueSchema
-	 *            the new queue schema
+	 * @param queueSchema the new queue schema
 	 */
 	public void setQueueSchema(SDFAttributeList queueSchema) {
 		this.queueSchema = queueSchema;
@@ -143,33 +150,43 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 		WriteTransaction type = BrokerDictionary.getInstance().getWriteTypeForPort(this.identifier, port);
 		// setting the minimum time for the current port
 		this.setMinTS(port, object.getMetadata().getStart());
-		
+
 		// if this is a timestamp tuple (from a queue stream)...
 		if (type == WriteTransaction.Timestamp) {
 			// ... save the request as an TransactionTS in the timestamp list
 			PointInTime time = object.getMetadata().getStart();
 			TransactionTS trans = new TransactionTS(getOutgoingPortForIncoming(port), time);
 			timestampList.offer(trans);
-			printDebug("Added to list: "+getNextCyclicTransactionList());
-		} else {			
+			printDebug("Added to list: " + getNextCyclicTransactionList());
+		} else {
+			printDebug("Waiting for data before or equal " + this.waitingFor + "...");
 			// each incoming object will be put into the waiting buffer
 			waitingBuffer.add(object);
+			if (waitingFor != null) {
+				if (object.getMetadata().getStart().equals(waitingFor)) {
+					waitingFor = null;
+					printDebug("... received");
+				}
+			}
 		}
-		
+
 		generateOutput();
 
 	}
-	
-	private synchronized void generateOutput(){
-//		System.out.println("Thread start: " + Thread.currentThread().getId());
+
+	private synchronized void generateOutput() {
+		// System.out.println("Thread start: " +
+		// Thread.currentThread().getId());
 		// determin the minimum of all writing streams
 		this.min = getMinimum();
+		this.minTSList = getMinimumForTSList();
 		// printDebug("Minimun time is " + this.min);
-//		printDebug("Process from " + port + " " + type + ": " + object.toString() + "  (" + this + ")");
+		// printDebug("Process from " + port + " " + type + ": " +
+		// object.toString() + "  (" + this + ")");
 		printDebug("Minimum is: " + this.min);
-		
+
 		contentChanged = false;
-		// if the broker is not in waiting mode...		
+		// if the broker is not in waiting mode...
 		// ... and there is a valid minimum (each writing transaction has at
 		// least one valid tuple or punctuation)
 		if (min != null) {
@@ -189,73 +206,83 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 					break;
 				}
 			}
-		}
-		List<PhysicalSubscription<ISink<? super T>>> destinations = new ArrayList<PhysicalSubscription<ISink<? super T>>>();
-		if (contentChanged) {
-			// get all continuously reading subscriptions for output
-			destinations = getWritingToSinks();
-		}
-		if (!timestampList.isEmpty()) {
-			TransactionTS nextTs = timestampList.peek();
-			// if there is a minimum (at least one valid timestamp from each
-			// writing stream)
-			// and if the next timestamp from a queue stream is before or
-			// equal this minimum
-			if (min != null && nextTs.getPointInTime().beforeOrEquals(min)) {
-				// add the next timestamp transaction to the destinations
-				timestampList.poll();
-				int nextPort = nextTs.getOutgoingPort();
-				PhysicalSubscription<ISink<? super T>> nextSub = getSinkSubscriptionForPort(nextPort);
-				if (nextSub != null) {
-					destinations.add(nextSub);						
-				}
-			}
-		}
-		//printDebug("Next cyclic output: " + getNextCyclicTransactionList());
-		// transfer the content of the the SweepArea to the destinations
-		// (first ones are continuous)
-		if (!this.sweepArea.isEmpty()) {
-			for (PhysicalSubscription<ISink<? super T>> toSub : destinations) {
-				int toPort = toSub.getSourceOutPort();
-				// transfer the whole content to the subscription
-				for (T element : this.sweepArea) {
-					transfer(element, toPort);
-				}
-				// if (last) one is cycle then stop and wait
-				if (BrokerDictionary.getInstance().getReadTypeForPort(getIdentifier(), toPort) == ReadTransaction.Cyclic) {
-					// determine the port the broker has to wait for					
-					return;
-				} else if (BrokerDictionary.getInstance().getReadTypeForPort(getIdentifier(), toPort) == ReadTransaction.OneTime) {
-					// broker don't have to wait, because it is one time
-					// reading.
-					// therefore the timestamp can directly removed
-					this.timestampList.poll();
-				}
-			}
-		}
-		// If the sweep area is empty, then a punctuation must be send.
-		// I think, this.min should be the punctuation of choice.
-		// The punctuation must be send to all following operators, I think.
 
-		else{
-			if(this.min != null){
-				this.sendPunctuation(new PointInTime(this.min));
+			List<PhysicalSubscription<ISink<? super T>>> destinations = new ArrayList<PhysicalSubscription<ISink<? super T>>>();
+			if (contentChanged) {
+				// get all continuously reading subscriptions for output
+				destinations = getWritingToSinks();
 			}
-			
-			// if min is null, you cannot send a punctuation, because
-			// you don't know anything about the time progress
-			// To ensure, that you can send a punctuation, all preceeding
-			// operators have to send a punctuation.
-		}
+			//if we received the content for which we were waiting...
+			if (waitingFor == null) {
+				//... and there is a request, then
+				if (!timestampList.isEmpty()) {
+					TransactionTS nextTs = timestampList.peek();
+					// if there is a minimum (at least one valid timestamp from
+					// each writing timestamp stream)
+					// and if the next timestamp from a queue stream is before
+					// or equal to this minimum
+					if (minTSList != null && nextTs.getPointInTime().beforeOrEquals(minTSList)) {
+						// add the next timestamp transaction to the
+						// destinations
+						timestampList.poll();
+						int nextPort = nextTs.getOutgoingPort();
+						// and we are now wating for this content, so remind the timestamp
+						this.waitingFor = nextTs.getPointInTime();
+						PhysicalSubscription<ISink<? super T>> nextSub = getSinkSubscriptionForPort(nextPort);
+						if (nextSub != null) {
+							destinations.add(nextSub);
+						}
+					}
+				}
+			}
+			// printDebug("Next cyclic output: " +
+			// getNextCyclicTransactionList());
+			// transfer the content of the the SweepArea to the destinations
+			// (first ones are continuous)
+			if (!this.sweepArea.isEmpty()) {
+				for (PhysicalSubscription<ISink<? super T>> toSub : destinations) {
+					int toPort = toSub.getSourceOutPort();
+					// transfer the whole content to the subscription
+					// synchronized -> avoid concurrentModificationExecptions
+					synchronized (this.sweepArea) {
+						for (T element : this.sweepArea) {
+							transfer(element, toPort);
+						}
+					}
+					// if (last) one is cycle then stop and wait
+					if (BrokerDictionary.getInstance().getReadTypeForPort(getIdentifier(), toPort) == ReadTransaction.Cyclic) {
+						// determine the port the broker has to wait for
+						return;
+					} else if (BrokerDictionary.getInstance().getReadTypeForPort(getIdentifier(), toPort) == ReadTransaction.OneTime) {
+						// broker don't have to wait, because it is one time
+						// reading.
+						// therefore the timestamp can directly removed
+						this.timestampList.poll();
+					}
+				}
+			}
+			// If the sweep area is empty, then a punctuation must be send.
+			// I think, this.min should be the punctuation of choice.
+			// The punctuation must be send to all following operators, I think.
 
-//		System.out.println("Thread end: " + Thread.currentThread().getId());
+			else {
+				if (this.min != null) {
+					this.sendPunctuation(new PointInTime(this.min));
+				}
+
+				// if min is null, you cannot send a punctuation, because
+				// you don't know anything about the time progress
+				// To ensure, that you can send a punctuation, all preceeding
+				// operators have to send a punctuation.
+			}
+		}
+		// System.out.println("Thread end: " + Thread.currentThread().getId());
 	}
 
 	/**
 	 * Gets the subscription for a reading port.
 	 * 
-	 * @param port
-	 *            the port
+	 * @param port the port
 	 * @return the sink subscription for port
 	 */
 	private PhysicalSubscription<ISink<? super T>> getSinkSubscriptionForPort(int port) {
@@ -285,19 +312,17 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	/**
 	 * Transfers the given object to the subscription on the given port.
 	 * 
-	 * @param object
-	 *            the object to transfer
-	 * @param sourceOutPort
-	 *            the port of the destination
+	 * @param object the object to transfer
+	 * @param sourceOutPort the port of the destination
 	 */
 	protected void process_transfer(T object, int sourceOutPort) {
-//		synchronized (this.getSubscriptions()) {
-			for (PhysicalSubscription<ISink<? super T>> sink : this.getSubscriptions()) {
-				if (sink.getSourceOutPort() == sourceOutPort) {
-					sink.getTarget().process(object, sink.getSinkInPort(), !this.hasSingleConsumer());
-				}
+		// synchronized (this.getSubscriptions()) {
+		for (PhysicalSubscription<ISink<? super T>> sink : this.getSubscriptions()) {
+			if (sink.getSourceOutPort() == sourceOutPort) {
+				sink.getTarget().process(object, sink.getSinkInPort(), !this.hasSingleConsumer());
 			}
-//		}
+		}
+		// }
 	}
 
 	/*
@@ -340,8 +365,7 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	/**
 	 * Reorganize transactions and sets new cycles up.
 	 * 
-	 * @param cycles
-	 *            the new cycles
+	 * @param cycles the new cycles
 	 */
 	public void reorganizeTransactions(List<CycleSubscription> cycles) {
 		LoggerFactory.getLogger("BrokerPO - reorganize").debug("Setting transaction types");
@@ -349,15 +373,14 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 			LoggerFactory.getLogger("BrokerPO - reorganize").debug(cycle.toString() + " - change transaction type to cyclic");
 			BrokerDictionary.getInstance().setReadTypeForPort(this.identifier, cycle.getOutgoingPort(), ReadTransaction.Cyclic);
 			BrokerDictionary.getInstance().setWriteTypeForPort(this.identifier, cycle.getIncomingPort(), WriteTransaction.Cyclic);
-		}		
+		}
 	}
 
 	/**
 	 * Gets the reading port for a given writing port. The writing port is the
 	 * queue port and the reading port is the assigned reading sink.
 	 * 
-	 * @param income
-	 *            the writing port
+	 * @param income the writing port
 	 * @return the the reading port
 	 */
 	private int getOutgoingPortForIncoming(int income) {
@@ -381,8 +404,7 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	/**
 	 * Prints a debug message.
 	 * 
-	 * @param message
-	 *            the message
+	 * @param message the message
 	 */
 	private void printDebug(String message) {
 		if (this.printDebug) {
@@ -398,69 +420,58 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	 * @return the minimum or null
 	 */
 	private PointInTime getMinimum() {
-		if (ensureTimes()) {
-			PointInTime min = null;
-			for (int i = 0; i < tsmin.length; i++) {
-				if (tsmin[i] == null) {
-					continue;
-				}
+		PointInTime min = null;
+		for (int i = 0; i < tsmin.length; i++) {
+			// if
+			// (BrokerDictionary.getInstance().getWriteTypeForPort(this.getIdentifier(),
+			// i) != WriteTransaction.Timestamp) {
+			if (tsmin[i] != null) {
 				if (min == null || tsmin[i].before(min)) {
 					min = tsmin[i];
 				}
-			}
-			return min;
-		} else {
-			return null;
-		}
-	}
-
-	/**
-	 * Ensures that each writing stream has sent at least one timestamp (element
-	 * or punctuation) except for cyclic transactions.
-	 * 
-	 * @return true, if successful
-	 */
-	public boolean ensureTimes() {
-		for (PhysicalSubscription<ISource<? extends T>> sub : super.getSubscribedToSource()) {
-			int port = sub.getSinkInPort();
-			if (getMinTS(port) == null && (BrokerDictionary.getInstance().getWriteTypeForPort(this.getIdentifier(), port) != WriteTransaction.Cyclic)) {
-				return false;
+			} else {
+				return null;
 			}
 		}
-		return true;
+		return min;
 	}
 
+	
 	/**
-	 * Gets the minimum timestamp for a given port.
+	 * Calculates the current minimum of all timestamp streams. Previously it
+	 * checks whether all timestamp streams have at least one timestamp. if not
+	 * this method returns null.
 	 * 
-	 * @param port
-	 *            the port
-	 * @return the minimum timestamp
+	 * @return the minimum or null
 	 */
-	private PointInTime getMinTS(int port) {
-		if (port < this.tsmin.length) {
-			return this.tsmin[port];
+	private PointInTime getMinimumForTSList() {
+		PointInTime min = null;
+		for (int i = 0; i < tsmin.length; i++) {
+			if (BrokerDictionary.getInstance().getWriteTypeForPort(this.getIdentifier(), i) == WriteTransaction.Timestamp) {
+				if (tsmin[i] != null) {
+					if (min == null || tsmin[i].before(min)) {
+						min = tsmin[i];
+					}
+				} else {
+					return null;
+				}
+			}
 		}
-		return null;
+		return min;
 	}
+			
 
 	/**
 	 * Sets the minimum timestamp for a port.
 	 * 
-	 * @param port
-	 *            the port
-	 * @param time
-	 *            the new timestamp
+	 * @param port the port
+	 * @param time the new timestamp
 	 */
 	private void setMinTS(int port, PointInTime time) {
 		if (this.tsmin.length <= port) {
 			this.tsmin = Arrays.copyOf(this.tsmin, port + 1);
 		}
-		if (BrokerDictionary.getInstance().getWriteTypeForPort(this.getIdentifier(), port) != WriteTransaction.Cyclic) {
-			this.tsmin[port] = time;
-		} else {
-			this.tsmin[port] = null;
-		}
+		this.tsmin[port] = time;
 	}
 
 	/*
@@ -471,7 +482,7 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	 */
 	@Override
 	public void processPunctuation(PointInTime timestamp, int port) {
-		printDebug("Process punctuation: "+timestamp+" on port "+port);
+		printDebug("Process punctuation: " + timestamp + " on port " + port);
 		setMinTS(port, timestamp);
 		generateOutput();
 	}
@@ -505,16 +516,15 @@ public class BrokerPO<T extends IMetaAttributeContainer<ITimeInterval>> extends 
 	public void setPrintDebug(boolean printDebug) {
 		this.printDebug = printDebug;
 	}
-	
+
 	@Override
 	public BrokerPO<T> clone() {
 		return new BrokerPO<T>(this);
 	}
-	
+
 	@Override
 	public String getName() {
 		return super.getName() + " [" + getIdentifier() + "]";
 	}
-	
 
 }
