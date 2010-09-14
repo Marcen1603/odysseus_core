35ea4778b86730c9d688ff86a8133c08a363ff7d
Daniel Twumasi <dtwumasi@uni-oldenburg.de>
2010-09-14 13:54:05 +0000

    Initialierungs Test..
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7136 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 510695fc81862a8ec1284227225893cf479bcd4f

---
+++ sensor/streamcars/paf/de.uniol.inf.is.odysseus.scars.objecttracking.initialization.test/src/de/uniol/inf/is/odysseus/scars/objecttracking/initialization/test/InitializationTest.java
@@ -0,0 +1,122 @@
+package de.uniol.inf.is.odysseus.scars.objecttracking.initialization.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
+import de.uniol.inf.is.odysseus.scars.objecttracking.filter.Parameters;
+import de.uniol.inf.is.odysseus.scars.objecttracking.initialization.AbstractInitializationFunction;
+import de.uniol.inf.is.odysseus.scars.objecttracking.initialization.InitializationFunction;
+import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.StreamCarsMetaData;
+import de.uniol.inf.is.odysseus.scars.testdata.provider.FilterFunctionTestData;
+import de.uniol.inf.is.odysseus.scars.util.SchemaHelper;
+import de.uniol.inf.is.odysseus.scars.util.SchemaIndexPath;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
+
+
+
+
+/**
+ * @author dtwumasi
+ *
+ */
+public class InitializationTest<K> {
+
+	private AbstractInitializationFunction initializationFunction;
+	
+	private MVRelationalTuple<StreamCarsMetaData<K>> measurementTuple;
+
+	private MVRelationalTuple<StreamCarsMetaData<K>> resultTuple;
+	
+	private MVRelationalTuple<StreamCarsMetaData<K>> expectedTuple;
+	
+	private MVRelationalTuple<StreamCarsMetaData<K>> initializationTuple;
+	
+	private SchemaHelper schemaHelper;
+	private SchemaIndexPath newObjectListPath;
+	private SchemaIndexPath oldObjectListPath;
+	
+	private String newObjListPath ="scan.newlist";
+	
+	private String oldObjListPath = "scan.oldlist";
+	
+	SDFAttributeList inputSchema;
+	
+	private HashMap<Enum , Object> parameters;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	
+	FilterFunctionTestData testData = new FilterFunctionTestData();
+	
+	// Measurement Data
+
+	double speedOld = 0.9;
+
+	double posOld = 1.7;
+
+	double[][] covarianceOld = { {5.0,50.0}, {50.0,10.0} };
+
+	double speedNew = 1.0;
+
+	double posNew = 2.0;
+
+	double[][] covarianceNew = { {3.0,21.0}, {21.0,7.0} };
+	
+	double speedIni = 1;
+
+	double posIni = 0.5;
+
+	double[][] covarianceIni = { {1,0}, {0,1} };
+	
+	measurementTuple = testData.generateTestTuple(speedOld, posOld, covarianceOld, speedNew, posNew, covarianceNew, null,5);
+
+	// the expected tuple
+
+	expectedTuple = testData.generateTestTuple(speedIni, posIni, covarianceIni, speedNew, posNew, covarianceNew, null,5);
+	
+	initializationFunction = new InitializationFunction();
+	
+	// Measurement Data
+
+	initializationTuple = testData.generateDataTuple(speedIni, posIni, covarianceIni, null);
+	
+	inputSchema = testData.getSchema();
+	
+	this.schemaHelper = new SchemaHelper(inputSchema);
+
+	this.newObjectListPath = this.schemaHelper.getSchemaIndexPath(newObjListPath);
+	
+
+	this.oldObjectListPath = this.schemaHelper.getSchemaIndexPath(oldObjListPath);
+	
+	parameters = new HashMap();
+	
+	parameters.put(Parameters.InitializationTuple, initializationTuple);
+	
+	initializationFunction.setParameters(parameters);
+	
+	
+	}
+
+	/**
+	 * Test method for {@link de.uniol.inf.is.odysseus.scars.objecttracking.initialization.InitializationFunction#compute(de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple, de.uniol.inf.is.odysseus.scars.util.SchemaIndexPath, de.uniol.inf.is.odysseus.scars.util.SchemaIndexPath)}.
+	 */
+	@Test
+	public void testCompute() {
+		
+		resultTuple = initializationFunction.compute(measurementTuple, newObjectListPath, oldObjectListPath);
+		
+		assertEquals(expectedTuple,resultTuple);
+	
+	}
+
+}
