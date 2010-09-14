c7d594f6b93104c80f3c5b30b57feb3d208f5bd3
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-09-14 13:05:33 +0000

    StreamCars: testdataprovider sendet nun daten MIT metadaten
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7131 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 2417100279e87ddc597bf966748efa1d79c870de

--- sensor/streamcars/scars.operator.testdata/src/de/uniol/inf/is/odysseus/scars/operator/testdata/TestdataProviderPO.java
+++ sensor/streamcars/scars.operator.testdata/src/de/uniol/inf/is/odysseus/scars/operator/testdata/TestdataProviderPO.java
@@ -1,11 +1,24 @@
 package de.uniol.inf.is.odysseus.scars.operator.testdata;
 
+import java.util.HashSet;
+
+import de.uniol.inf.is.odysseus.base.IMetaAttribute;
 import de.uniol.inf.is.odysseus.base.OpenFailedException;
 import de.uniol.inf.is.odysseus.base.PointInTime;
+import de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval;
+import de.uniol.inf.is.odysseus.latency.ILatency;
+import de.uniol.inf.is.odysseus.metadata.base.IMetaAttributeContainer;
+import de.uniol.inf.is.odysseus.metadata.base.MetadataRegistry;
 import de.uniol.inf.is.odysseus.objecttracking.MVRelationalTuple;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IApplicationTime;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
 import de.uniol.inf.is.odysseus.objecttracking.physicaloperator.access.AbstractSensorAccessPO;
 import de.uniol.inf.is.odysseus.physicaloperator.base.AbstractSource;
+import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IConnectionContainer;
+import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IGain;
+import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.StreamCarsMetaData;
+import de.uniol.inf.is.odysseus.scars.objecttracking.metadata.StreamCarsMetaDataInitializer;
 import de.uniol.inf.is.odysseus.scars.testdata.provider.Provider;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
@@ -16,6 +29,7 @@ public class TestdataProviderPO<M extends IProbability> extends AbstractSensorAc
 	private MVRelationalTuple<M> buffer = null;
 	private int counter = 0;
 	private String sourceName;
+	private StreamCarsMetaDataInitializer<StreamCarsMetaData<Object>> metadataCreator;
 	
 	public TestdataProviderPO() {
 		this.provider = new Provider();
@@ -29,9 +43,10 @@ public class TestdataProviderPO<M extends IProbability> extends AbstractSensorAc
 
 	@Override
 	protected void process_open() throws OpenFailedException {
-		provider.setDelay(50);
+		provider.setDelay(1000);
 		provider.setNumOfCars(5);
 		provider.init();
+		metadataCreator = new StreamCarsMetaDataInitializer<StreamCarsMetaData<Object>>(getOutputSchema());
 	}
 
 	@Override
@@ -62,7 +77,7 @@ public class TestdataProviderPO<M extends IProbability> extends AbstractSensorAc
 		 * wird (siehe auch Ticket 225).
 		 */
 		if(!this.isDone()){
-			if (System.currentTimeMillis() - lastTime > 20) {
+			if (System.currentTimeMillis() - lastTime > 1000) {
 				this.buffer = (MVRelationalTuple<M>) provider.nextTuple();
 				return true;
 			}
@@ -84,16 +99,52 @@ public class TestdataProviderPO<M extends IProbability> extends AbstractSensorAc
 
 	@Override
 	public void transferNext() {
-		System.out.println(this.buffer);
-		SDFAttributeList schema = this.provider.getSchema(this.sourceName); 
-		System.out.println(schema);
+		System.out.println("Send Data: " + this.buffer);
+		Class<M> clazz = (Class<M>) MetadataRegistry.getMetadataType(toStringSet(ITimeInterval.class, 
+				IPredictionFunctionKey.class, 
+				ILatency.class, 
+				IProbability.class, 
+				IApplicationTime.class, 
+				IConnectionContainer.class,
+				IGain.class));
+		
+		assignMetadata(clazz, this.buffer);
+		metadataCreator.updateMetadata((MVRelationalTuple<StreamCarsMetaData<Object>>)this.buffer);
+
 		transfer(this.buffer);
 		this.buffer = null;
-		if(this.counter % 5 == 0){
-			sendPunctuation(new PointInTime(this.provider.getLastTimestamp()+1));
-		}
+		System.out.println("Send Punctuation: " + (this.provider.getLastTimestamp()+1));
+		sendPunctuation(new PointInTime(this.provider.getLastTimestamp()+1));
 		lastTime = System.currentTimeMillis();
 
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void assignMetadata(Class<M> clazz, Object tuple ){
+		try {
+			if( tuple instanceof IMetaAttributeContainer ) {
+				((IMetaAttributeContainer<M>)tuple).setMetadata(clazz.newInstance());
+			}
+			if( tuple instanceof MVRelationalTuple<?>) {
+				MVRelationalTuple<M> t = (MVRelationalTuple<M>) tuple;
+				for( int i = 0; i < t.getAttributeCount(); i++ ) {
+					assignMetadata(clazz, t.getAttribute(i));
+				}
+			}
+		} catch( IllegalAccessException ex ) {
+			ex.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static HashSet<String> toStringSet(
+			Class<? extends IMetaAttribute>... combinationOf) {
+		HashSet<String> typeSet = new HashSet<String>();
+		for (Class<?> c : combinationOf) {
+			typeSet.add(c.getName());
+		}
+		return typeSet;
 	}
 
 	@Override
