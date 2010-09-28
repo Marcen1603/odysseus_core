794195b988c9c1edd74ba1237a0ea699bd066abf
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-09-28 07:09:35 +0000

    Session muss Serializable implementieren
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7383 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3e6ce21f321698a63c6d982c3942631421c29c6e

--- core/base/src/de/uniol/inf/is/odysseus/base/usermanagement/Session.java
+++ core/base/src/de/uniol/inf/is/odysseus/base/usermanagement/Session.java
@@ -1,7 +1,10 @@
 package de.uniol.inf.is.odysseus.base.usermanagement;
 
-public class Session {
+import java.io.Serializable;
 
+public class Session implements Serializable{
+
+	private static final long serialVersionUID = 6601485682039247781L;
 	final int key;
 	
 	Session(int key){
