713c46fc62db663ea7cf1089c6f719f8e886eb73
Dennis Geesen <geesen@indivon.de>
2010-10-12 14:19:54 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7488 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: d58f92f1b0f8d827d25ddd03226421906e269865

---
+++ storing/storing.cql/src/de/uniol/inf/is/odysseus/storing/cql/FromTableVisitor.java
@@ -0,0 +1,75 @@
+package de.uniol.inf.is.odysseus.storing.cql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import de.uniol.inf.is.odysseus.datadictionary.DataDictionary;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTCreateFromDatabase;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTIdentifier;
+import de.uniol.inf.is.odysseus.parser.cql.parser.ASTJdbcIdentifier;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.description.SDFSource;
+import de.uniol.inf.is.odysseus.storing.DatabaseServiceLoader;
+import de.uniol.inf.is.odysseus.storing.logicaloperator.DatabaseAccessAO;
+import de.uniol.inf.is.odysseus.usermanagement.User;
+
+public class FromTableVisitor {
+
+	private User user;
+	private String name;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setUser(User user){
+		this.user = user;
+	}
+	
+	public Object visit(ASTCreateFromDatabase node, Object data){
+		DatabaseAccessAO access = null;
+		if(node.jjtGetNumChildren()>1){
+			if(node.jjtGetChild(0) instanceof ASTJdbcIdentifier){
+				String jdbc = ((ASTJdbcIdentifier)node.jjtGetChild(0)).getConnection();
+				String name = ((ASTIdentifier)node.jjtGetChild(1)).getName();
+				boolean sensitiv = false;
+				if(node.jjtGetNumChildren()==3){
+					sensitiv = true;
+				}
+				access = getAccessAOForJDBC(jdbc, name, sensitiv);		
+			}else{
+				String name = ((ASTIdentifier)node.jjtGetChild(0)).getName();			
+				access = getAccessAOForDefault(name, true);
+			}
+		}else{
+			String name = ((ASTIdentifier)node.jjtGetChild(0)).getName();			
+			access = getAccessAOForDefault(name, false);
+		}
+		
+		//***************				
+		DataDictionary.getInstance().setView(name, access, user);		
+		return access;
+	}
+	
+	
+	private DatabaseAccessAO getAccessAOForDefault(String tableName, boolean isTimeSensitiv){
+		DatabaseAccessAO dba = new DatabaseAccessAO(getSource(name), DatabaseServiceLoader.getConnection(), tableName, isTimeSensitiv);
+		return dba;
+	}
+	
+	private DatabaseAccessAO getAccessAOForJDBC(String jdbcString, String tableName, boolean isTimeSensitiv){
+		try {
+			System.err.println("Currently only Derby DB is allowed, because there are no other drivers...");
+			Connection con = DriverManager.getConnection(jdbcString);
+			DatabaseAccessAO dba = new DatabaseAccessAO(getSource(name), con, tableName, isTimeSensitiv);
+			return dba;
+		} catch (SQLException e) {		
+			e.printStackTrace();
+		}
+		return null;		
+	}
+	
+	private SDFSource getSource(String sourceName){
+		return new SDFSource(sourceName, "databaseReading");
+	}
+}
