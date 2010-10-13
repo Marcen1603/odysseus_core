34dd5443984f9d3cfeaf2b3fdb1b2fd99b7c4620
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-10-13 12:28:06 +0000

    RCP: ObjectFusion QueryBuildConfiguration erstellt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7519 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0599ea21cb1c2733e3d293d75dcbfaeeb3b86361

---
+++ sensor/rcp.viewer.objectfusionconfig/src/de/uniol/inf/is/odysseus/rcp/viewer/objectfusionconfig/ObjectFusionQueryBuildConfiguration.java
@@ -0,0 +1,35 @@
+package de.uniol.inf.is.odysseus.rcp.viewer.objectfusionconfig;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval;
+import de.uniol.inf.is.odysseus.planmanagement.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.planmanagement.query.querybuiltparameter.AbstractQueryBuildSetting;
+import de.uniol.inf.is.odysseus.planmanagement.query.querybuiltparameter.ParameterTransformationConfiguration;
+import de.uniol.inf.is.odysseus.rcp.viewer.query.IQueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.transformation.helper.broker.BrokerTransformationHelper;
+
+
+public class ObjectFusionQueryBuildConfiguration implements IQueryBuildConfiguration {
+	
+	private List<AbstractQueryBuildSetting<?>> settings = new ArrayList<AbstractQueryBuildSetting<?>>();
+	
+	public ObjectFusionQueryBuildConfiguration() {
+		settings.add(new ParameterTransformationConfiguration(
+				new TransformationConfiguration(
+						new BrokerTransformationHelper(),
+						"relational",
+						ITimeInterval.class.getName(),
+						"de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey",
+						"de.uniol.inf.is.odysseus.latency.ILatency",
+						"de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability", 
+						"de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval",
+						"de.uniol.inf.is.odysseus.objecttracking.metadata.IApplicationTime")));
+	}
+
+	@Override
+	public List<AbstractQueryBuildSetting<?>> get() {
+		return settings;
+	}
+}
