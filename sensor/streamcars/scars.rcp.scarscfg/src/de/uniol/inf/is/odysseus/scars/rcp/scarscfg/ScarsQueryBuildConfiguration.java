c22dd54ccd479d1f4723619bc2d5b50f0868777b
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-10-13 11:53:48 +0000

    StreamCars: QueryBuildConfiguratiion
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7507 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e626e10b50681ff2be50ff2924d03031768a83f1

---
+++ sensor/streamcars/scars.rcp.scarscfg/src/de/uniol/inf/is/odysseus/scars/rcp/scarscfg/ScarsQueryBuildConfiguration.java
@@ -0,0 +1,49 @@
+package de.uniol.inf.is.odysseus.scars.rcp.scarscfg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.planmanagement.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.planmanagement.query.querybuiltparameter.AbstractQueryBuildSetting;
+import de.uniol.inf.is.odysseus.planmanagement.query.querybuiltparameter.ParameterTransformationConfiguration;
+import de.uniol.inf.is.odysseus.rcp.viewer.query.IQueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.transformation.helper.broker.BrokerTransformationHelper;
+
+public class ScarsQueryBuildConfiguration implements IQueryBuildConfiguration {
+
+	private List<AbstractQueryBuildSetting<?>> settings = new ArrayList<AbstractQueryBuildSetting<?>>();
+	
+	public ScarsQueryBuildConfiguration() {
+		TransformationConfiguration cfg = new TransformationConfiguration(
+				new BrokerTransformationHelper(),
+				"relational",
+				
+				// ObjectTracking
+				"de.uniol.inf.is.odysseus.intervalapproach.ITimeInterval", // ok
+				"de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey", // ok
+				"de.uniol.inf.is.odysseus.latency.ILatency",  // ok
+				"de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability",  // ok
+				"de.uniol.inf.is.odysseus.objecttracking.metadata.IApplicationTime", // ok
+				
+				// StreamCars
+				"de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IConnectionContainer", // ok
+				"de.uniol.inf.is.odysseus.scars.objecttracking.metadata.IGain"); // ok
+				
+		
+		cfg.setOption("IBrokerInterval", true);
+		settings.add(new ParameterTransformationConfiguration(cfg));
+				
+	}
+
+	@Override
+	public List<AbstractQueryBuildSetting<?>> get() {
+		return settings;
+	}
+		
+//	@Override
+//	public ParameterTransformationConfiguration get() {
+//		trafoConfigParam.getValue().setOption("IBrokerInterval", true);
+//		return trafoConfigParam;
+//	}
+
+}
