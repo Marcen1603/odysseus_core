c6b84de0e47348225111e53a5bc6ed899461a65e
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-10-14 11:28:37 +0000

    SchedulingConfig removed from ExecConfig
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7529 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5a6a84e60f71d88154516130c074a8a8bee92714

--- core/base/src/de/uniol/inf/is/odysseus/planmanagement/executor/configuration/ExecutionConfiguration.java
+++ core/base/src/de/uniol/inf/is/odysseus/planmanagement/executor/configuration/ExecutionConfiguration.java
@@ -3,7 +3,6 @@ package de.uniol.inf.is.odysseus.planmanagement.executor.configuration;
 import de.uniol.inf.is.odysseus.planmanagement.configuration.Configuration;
 import de.uniol.inf.is.odysseus.planmanagement.executor.IExecutor;
 import de.uniol.inf.is.odysseus.planmanagement.executor.configuration.setting.SettingBufferPlacementStrategy;
-import de.uniol.inf.is.odysseus.planmanagement.executor.configuration.setting.SettingSchedulingStrategy;
 
 /**
  * ExecutionConfiguration stores the configuration of an {@link IExecutor}.
@@ -32,13 +31,6 @@ public class ExecutionConfiguration extends Configuration<AbstractExecutionSetti
 		return null;
 	}
 	
-	public String getSchedulingStrategy(){
-		SettingSchedulingStrategy s = get(SettingSchedulingStrategy.class);
-		if (s != null){
-			return s.getValue();			
-		}
-		return null;
-	}
 	
 	
 	
