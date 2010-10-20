2f758557dc4dfaf249f10c357fe0aae677821147
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-10-20 07:09:24 +0000

    Refactoring des Namens
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7570 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7da2f57dbcf6adf169016910bb3dfd654dc5462c

--- core/base/src/de/uniol/inf/is/odysseus/planmanagement/query/querybuiltparameter/ParameterRewriteRulesToUse.java
+++ core/base/src/de/uniol/inf/is/odysseus/planmanagement/query/querybuiltparameter/ParameterRewriteRulesToUse.java
@@ -2,10 +2,10 @@ package de.uniol.inf.is.odysseus.planmanagement.query.querybuiltparameter;
 
 import java.util.List;
 
-public class ParameterRulesToUse extends
+public class ParameterRewriteRulesToUse extends
 		AbstractQueryBuildSetting<List<String>> {
 
-	protected ParameterRulesToUse(List<String> value) {
+	protected ParameterRewriteRulesToUse(List<String> value) {
 		super(value);
 	}
 
