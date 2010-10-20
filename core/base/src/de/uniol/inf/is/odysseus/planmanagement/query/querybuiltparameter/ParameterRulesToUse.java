a742fec0855ebb269983f9d8c05f0cfcf79c3e92
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-10-20 07:07:41 +0000

    Parameter: Welche Regeln sollen bei der Transformation verwendet werden
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7569 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d97eda1d6e05af8c58da74864fe3def144366f80

---
+++ core/base/src/de/uniol/inf/is/odysseus/planmanagement/query/querybuiltparameter/ParameterRulesToUse.java
@@ -0,0 +1,12 @@
+package de.uniol.inf.is.odysseus.planmanagement.query.querybuiltparameter;
+
+import java.util.List;
+
+public class ParameterRulesToUse extends
+		AbstractQueryBuildSetting<List<String>> {
+
+	protected ParameterRulesToUse(List<String> value) {
+		super(value);
+	}
+
+}
