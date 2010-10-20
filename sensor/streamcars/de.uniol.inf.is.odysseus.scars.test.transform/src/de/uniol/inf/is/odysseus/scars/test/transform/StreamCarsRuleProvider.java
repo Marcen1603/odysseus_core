56dd6ba1e2b353864491cc16beea02708ad3a502
Andre Bolles <andre.bolles@offis.de>
2010-10-20 09:52:02 +0000

    missing rules added
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7574 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 24e1f2a53ef5bfb1ac1efb5186aab077a498684c

--- sensor/streamcars/de.uniol.inf.is.odysseus.scars.test.transform/src/de/uniol/inf/is/odysseus/scars/test/transform/StreamCarsRuleProvider.java
+++ sensor/streamcars/de.uniol.inf.is.odysseus.scars.test.transform/src/de/uniol/inf/is/odysseus/scars/test/transform/StreamCarsRuleProvider.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.uniol.inf.is.odysseus.ruleengine.rule.IRule;
+import de.uniol.inf.is.odysseus.scars.test.transform.rules.TTestAORule;
 import de.uniol.inf.is.odysseus.transform.flow.ITransformRuleProvider;
 
 
@@ -13,7 +14,7 @@ public class StreamCarsRuleProvider implements ITransformRuleProvider{
 	public List<IRule<?, ?>> getRules() {
 		ArrayList<IRule<?, ?>> rules = new ArrayList<IRule<?, ?>>();
 
-		
+		rules.add(new TTestAORule());
 		return rules;
 	}
 
