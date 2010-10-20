892822a069a7778bd7e15392f19399aab3d4dbe5
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-10-20 10:21:35 +0000

    StreamCars: Korrekturen an einer Transformationsregel
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7576 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 17b5ad943a4a19984219e7ef535e4b6b348d2dc8

--- sensor/streamcars/de.uniol.inf.is.odysseus.scars.transform/src/de/uniol/inf/is/odysseus/scars/transform/rules/THypothesisGenarationAORule.java
+++ sensor/streamcars/de.uniol.inf.is.odysseus.scars.transform/src/de/uniol/inf/is/odysseus/scars/transform/rules/THypothesisGenarationAORule.java
@@ -2,18 +2,18 @@ package de.uniol.inf.is.odysseus.scars.transform.rules;
 
 import java.util.Collection;
 
-import de.uniol.inf.is.odysseus.base.ILogicalOperator;
-import de.uniol.inf.is.odysseus.base.TransformationConfiguration;
 import de.uniol.inf.is.odysseus.latency.ILatency;
+import de.uniol.inf.is.odysseus.logicaloperator.ILogicalOperator;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey;
 import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
+import de.uniol.inf.is.odysseus.planmanagement.TransformationConfiguration;
 import de.uniol.inf.is.odysseus.ruleengine.ruleflow.IRuleFlowGroup;
 import de.uniol.inf.is.odysseus.scars.objecttracking.association.logicaloperator.HypothesisGenerationAO;
 import de.uniol.inf.is.odysseus.scars.objecttracking.association.physicaloperator.HypothesisGenerationPO;
 import de.uniol.inf.is.odysseus.transform.flow.TransformRuleFlowGroup;
 import de.uniol.inf.is.odysseus.transform.rule.AbstractTransformationRule;
 
-public class THypothesisGenarationAORule extends AbstractTransformationRule<HypothesisGenerationAO>{
+public class THypothesisGenarationAORule extends AbstractTransformationRule<HypothesisGenerationAO> {
 
 	@Override
 	public int getPriority() {
@@ -22,8 +22,7 @@ public class THypothesisGenarationAORule extends AbstractTransformationRule<Hypo
 	}
 
 	@Override
-	public void execute(HypothesisGenerationAO operator,
-			TransformationConfiguration config) {
+	public void execute(HypothesisGenerationAO operator, TransformationConfiguration config) {
 		System.out.println("DROOLS: THypothesisGeneration.drl");
 		System.out.println("CREATE HypothesisGenerationPO.");
 		HypothesisGenerationPO genPO = new HypothesisGenerationPO();
@@ -32,33 +31,33 @@ public class THypothesisGenarationAORule extends AbstractTransformationRule<Hypo
 		genPO.setOutputSchema(operator.getOutputSchema());
 
 		Collection<ILogicalOperator> toUpdate = config.getTransformationHelper().replace(operator, genPO);
-		for (ILogicalOperator o:toUpdate){
+		for (ILogicalOperator o : toUpdate) {
 			update(o);
 		}
-		
+
 		insert(genPO);
 		retract(operator);
 		System.out.println("CREATE HypothesisGenerationPO finished.");
-		
+
 	}
 
 	@Override
-	public boolean isExecutable(HypothesisGenerationAO operator,
-			TransformationConfiguration config) {
-		if(config.getMetaTypes().contains(ILatency.class.getCanonicalName()) &&
-				config.getMetaTypes().contains(IProbability.class.getCanonicalName()) &&
-				config.getMetaTypes().contains(IPredictionFunctionKey.class.getCanonicalName()) &&
-				operator.isAllPhysicalInputSet()){
+	public boolean isExecutable(HypothesisGenerationAO operator, TransformationConfiguration config) {
+		if (config.getMetaTypes().contains(ILatency.class.getCanonicalName()) && config.getMetaTypes().contains(IProbability.class.getCanonicalName())
+				&& config.getMetaTypes().contains(IPredictionFunctionKey.class.getCanonicalName()) && operator.isAllPhysicalInputSet()) {
 			return true;
 		}
-		
+
 		return false;
-		
+
 		// DRL-Code
-//		trafo : TransformationConfiguration( metaTypes contains "de.uniol.inf.is.odysseus.latency.ILatency" &&
-//				metaTypes contains "de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability" && 
-//				metaTypes contains "de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey")
-//		$genAO : HypothesisGenerationAO( allPhysicalInputSet == true )
+		// trafo : TransformationConfiguration( metaTypes contains
+		// "de.uniol.inf.is.odysseus.latency.ILatency" &&
+		// metaTypes contains
+		// "de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability" &&
+		// metaTypes contains
+		// "de.uniol.inf.is.odysseus.objecttracking.metadata.IPredictionFunctionKey")
+		// $genAO : HypothesisGenerationAO( allPhysicalInputSet == true )
 
 	}
 
