670c176463e0969326275feac611a8a2eb87fddf
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-10-21 12:15:39 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7601 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: b9dffb3e37de5d75c73a1eb2f374114f71fcd329

--- 00_attic/new_transformation/costmodel.base/src/de/uniol/inf/is/odysseus/new_transformation/costmodel/base/ICost.java
+++
@@ -1,5 +0,0 @@
-package de.uniol.inf.is.odysseus.new_transformation.costmodel.base;
-
-public interface ICost {
-	public boolean isBetterThan(ICost other);
-}
--- 00_attic/new_transformation/new_transformation.stream_characteristics/src/de/uniol/inf/is/odysseus/new_transformation/stream_characteristics/IStreamCharacteristic.java
+++
@@ -1,5 +0,0 @@
-package de.uniol.inf.is.odysseus.new_transformation.stream_characteristics;
-
-public interface IStreamCharacteristic {
-
-}
