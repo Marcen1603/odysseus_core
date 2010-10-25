703d2543998e2bbb16b84e54b0663f502ec4c89d
Jonas Jacobi <jacobi@uni-oldenburg.de>
2010-10-25 15:28:58 +0000

    staerkere optimierung mathematischer ausdruecke
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7658 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 139495b8d547b09769107f111b6620efb4667bca

--- core/mep/src/de/uniol/inf/is/odysseus/mep/ExpressionOptimizer.java
+++ core/mep/src/de/uniol/inf/is/odysseus/mep/ExpressionOptimizer.java
@@ -1,58 +1,146 @@
 package de.uniol.inf.is.odysseus.mep;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 import de.uniol.inf.is.odysseus.mep.functions.AndOperator;
 import de.uniol.inf.is.odysseus.mep.functions.OrOperator;
 
+/**
+ * @author Jonas Jacobi
+ */
 public class ExpressionOptimizer {
 
 	public static IExpression<?> simplifyExpression(IExpression<?> expression) {
-		PushDownConstants pushDown = new PushDownConstants();
 		PreCalculateConstants simplificator = new PreCalculateConstants();
-		expression = (IExpression<?>) expression.acceptVisitor(pushDown, null);
-		return (IExpression<?>) expression.acceptVisitor(simplificator, null);
+		expression = (IExpression<?>) expression.acceptVisitor(simplificator,
+				null);
+		CombineConstants constants = new CombineConstants();
+		expression.acceptVisitor(constants, null);
+
+		return expression;
 	}
-	
-	private static class PushDownConstants implements IExpressionVisitor {
+
+	/**
+	 * combines all constants in a nested call of the same (commutative and associative) operator to a single constant.
+	 */
+	private static class CombineConstants implements IExpressionVisitor {
 
 		@Override
 		public Object visit(Variable variable, Object data) {
-			return variable;
+			return null;
 		}
 
 		@Override
 		public Object visit(Constant<?> constant, Object data) {
-			return constant;
+			return null;
 		}
 
 		@Override
 		public Object visit(IFunction<?> function, Object data) {
-			for(IExpression<?> expr : function.getArguments()){
-				expr.acceptVisitor(this, data);
-			}
-			if (function instanceof IBinaryOperator){
-				IBinaryOperator<?> operator = (IBinaryOperator<?>)function;
-				if (!operator.isAssociative() || !operator.isCommutative()){
-					return function;
-				}
-				IExpression<?> arg0 = operator.getArgument(0);
-				IExpression<?> arg1 = operator.getArgument(1);
-				if (arg0.getClass() == function.getClass() && arg1.isConstant()){
-					IBinaryOperator<?> inputOperator = (IBinaryOperator<?>)arg0;
-					if(!inputOperator.getArgument(0).isConstant()) {
-						IExpression<?> var = inputOperator.getArgument(0);
-						inputOperator.setArgument(0, function.getArgument(1));
-						function.setArgument(1, var);
-					} else {
-						IExpression<?> var = inputOperator.getArgument(1);
-						inputOperator.setArgument(1, function.getArgument(1));
-						function.setArgument(1, var);
+			if (function instanceof IBinaryOperator) {
+				IBinaryOperator<?> op = (IBinaryOperator<?>) function;
+				if (op.isAssociative() && op.isCommutative()) {
+					List<IExpression<?>> expressions = collectExpressions((IBinaryOperator<?>) function);
+					Iterator<IExpression<?>> it = expressions.iterator();
+					List<IExpression<?>> constants = new LinkedList<IExpression<?>>();
+					while (it.hasNext()) {
+						IExpression<?> curExpression = it.next();
+						if (curExpression.isConstant()) {
+							constants.add(curExpression);
+							it.remove();
+						}
 					}
-					inputOperator.acceptVisitor(this, null);
+					if (!constants.isEmpty()) {
+						Constant<?> c = calculateConstant(function.getClass(),
+								constants);
+						function.setArgument(0, c);
+
+						Iterator<IExpression<?>> exprIt = expressions
+								.iterator();
+						IFunction<?> curFunction = function;
+						for (int i = 0; i < expressions.size() - 1; ++i) {
+							try {
+								IFunction<?> newFunc = createFunction(function
+										.getClass());
+								curFunction.setArgument(1, newFunc);
+								newFunc.setArgument(0, exprIt.next());
+								curFunction = newFunc;
+							} catch (Exception e) {
+								throw new RuntimeException(e);
+							}
+						}
+						curFunction.setArgument(1, exprIt.next());
+
+						for (IExpression<?> curExpression : expressions) {
+							curExpression.acceptVisitor(this, null);
+						}
+					}
+				}
+			} else {
+				for (IExpression<?> curArgument : function.getArguments()) {
+					curArgument.acceptVisitor(this, null);
 				}
 			}
-			return function;
+			return null;
 		}
-		
+
+		private Constant<?> calculateConstant(
+				@SuppressWarnings("rawtypes") Class<? extends IFunction> class1,
+				List<IExpression<?>> constants) {
+			if (constants.size() == 1) {
+				return constants.get(0).toConstant();
+			}
+			try {
+				final IFunction<?> function = createFunction(class1);
+				Iterator<IExpression<?>> i = constants.iterator();
+				Constant<?> c = i.next().toConstant();
+				function.setArgument(0, c);
+				while (i.hasNext()) {
+					function.setArgument(1, i.next());
+					c = new Constant<Object>(function.getValue());
+					function.setArgument(0, c);
+				}
+				return c;
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+
+		private IFunction<?> createFunction(
+				@SuppressWarnings("rawtypes") Class<? extends IFunction> class1) {
+			try {
+				IFunction<?> function = class1.newInstance();
+				function.setArguments(new IExpression[function.getArity()]);
+				return function;
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		private List<IExpression<?>> collectExpressions(
+				IBinaryOperator<?> function) {
+			List<IExpression<?>> list = new LinkedList<IExpression<?>>();
+			Stack<IExpression<?>> expressions = new Stack<IExpression<?>>();
+			expressions.push(function);
+			do {
+				IExpression<?> curExpression = expressions.pop();
+				if (curExpression.getClass() == function.getClass()) {
+					for (IExpression<?> expr : curExpression.toFunction()
+							.getArguments()) {
+						expressions.push(expr);
+					}
+				} else {
+					list.add(curExpression);
+				}
+			} while (!expressions.isEmpty());
+
+			return list;
+		}
+
 	}
 
 	/**
