60d8e7d134d66126897027a9e4863c2210d80bc9
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-10-26 13:05:16 +0000

    RCP: ACCESS-Operator verwendet beim Auswahl der Quelle nun ein DropDown-Menü. Dadurch muss der Quellenname nicht manuell eingegeben werden, sondern kann in einer Liste ausgewählt werden.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7677 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 643859e559e0259de80bd941854f5dd656e14905

---
+++ core/rcp/rcp.editor.parameters/src/de/uniol/inf/is/odysseus/rcp/editor/parameters/SourceParameterEditor.java
@@ -0,0 +1,67 @@
+package de.uniol.inf.is.odysseus.rcp.editor.parameters;
+
+import java.util.Map.Entry;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+import de.uniol.inf.is.odysseus.datadictionary.DataDictionary;
+import de.uniol.inf.is.odysseus.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.rcp.editor.parameter.IParameterEditor;
+import de.uniol.inf.is.odysseus.rcp.editor.parameter.SimpleParameterEditor;
+
+public class SourceParameterEditor extends SimpleParameterEditor<String> implements IParameterEditor {
+
+	@Override
+	protected Control createInput(Composite parent) {
+		// Combobox anstatt Texteingabe
+		final Combo combo = new Combo(parent, SWT.BORDER);
+
+		// Liste der Quellen
+		for( Entry<String, ILogicalOperator> e : DataDictionary.getInstance().getViews()) {
+			combo.add(e.getKey());
+		}
+		
+		combo.addModifyListener(new ModifyListener() {
+
+			@Override
+			public void modifyText(ModifyEvent e) {
+				try {
+					if( combo.getText().length() == 0 )
+						setValue(null);
+					else 
+						setValue(convertFromString(combo.getText()));
+										
+				} catch( Exception ex ) {
+					ex.printStackTrace();
+				}
+			}
+			
+		});
+		
+		// Anfangstext setzen
+		try {
+			String txt = convertToString((String)getValue());
+			combo.setText(txt == null ? "" : txt );
+		} catch( Exception ex ) {
+			combo.setText("");
+		}
+
+		return combo;
+	}
+	
+	@Override
+	public String convertFromString(String txt) {
+		return txt;
+	}
+
+	@Override
+	public String convertToString(String object) {
+		return object;
+	}
+
+}
--- core/rcp/rcp.editor/src/de/uniol/inf/is/odysseus/rcp/editor/parameter/SimpleParameterEditor.java
+++ core/rcp/rcp.editor/src/de/uniol/inf/is/odysseus/rcp/editor/parameter/SimpleParameterEditor.java
@@ -1,19 +1,18 @@
 package de.uniol.inf.is.odysseus.rcp.editor.parameter;
 
-import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 public abstract class SimpleParameterEditor<T> extends AbstractParameterEditor {
 	
-	private Text text;
-	private Label label;
+	private Control text;
 	
 	@Override
 	public void createControl(Composite parent) {
@@ -21,11 +20,29 @@ public abstract class SimpleParameterEditor<T> extends AbstractParameterEditor {
 		gl.numColumns = 2;
 		parent.setLayout(gl);
 		
-		label = new Label( parent, SWT.CENTER );
-		label.setText(getAttributeTitle());
-				
-		text = new Text(parent, SWT.SINGLE | SWT.BORDER);
+		// Label
+		createLabel(parent);
+		
+		// Eingabe
+		text = createInput(parent);
 		text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+	}
+	
+	public abstract T convertFromString( String txt );
+	public abstract String convertToString( T object );
+	
+	protected String getAttributeTitle() {
+		return getParameter().getName();
+	}
+
+	protected Label createLabel(Composite parent) {
+		Label label = new Label( parent, SWT.CENTER );
+		label.setText(getAttributeTitle());
+		return label;
+	}
+	
+	protected Control createInput(Composite parent) {
+		final Text text = new Text(parent, SWT.SINGLE | SWT.BORDER);
 		text.addModifyListener(new ModifyListener() {
 
 			@Override
@@ -35,19 +52,15 @@ public abstract class SimpleParameterEditor<T> extends AbstractParameterEditor {
 						setValue(null);
 					else 
 						setValue(convertFromString(text.getText()));
-					
-					if( !validate() ) 
-						markErrors(getErrorText());
-					else
-						unmarkErrors();
-					
+										
 				} catch( Exception ex ) {
-					markErrors( ex.getMessage() );
+					ex.printStackTrace();
 				}
 			}
 			
 		});
-
+		
+		// Anfangstext setzen
 		try {
 			@SuppressWarnings("unchecked")
 			String txt = convertToString((T)getValue());
@@ -55,29 +68,10 @@ public abstract class SimpleParameterEditor<T> extends AbstractParameterEditor {
 		} catch( Exception ex ) {
 			text.setText("");
 		}
-	}
-	
-	public abstract T convertFromString( String txt );
-	public abstract String convertToString( T object );
-	
-	protected void markErrors( String txt ) {
-		text.setForeground(ColorConstants.red);
-		label.setForeground(ColorConstants.red);
-		text.setToolTipText(txt);
-		label.setToolTipText(txt);
-	}
-	
-	protected void unmarkErrors() {
-		text.setForeground(ColorConstants.black);
-		label.setForeground(ColorConstants.black);
-		text.setToolTipText("");
-		label.setToolTipText("");
-	}
-	
-	protected String getAttributeTitle() {
-		return getParameter().getName();
-	}
 
+		return text;
+	}
+	
 	@Override
 	public void close() {
 
