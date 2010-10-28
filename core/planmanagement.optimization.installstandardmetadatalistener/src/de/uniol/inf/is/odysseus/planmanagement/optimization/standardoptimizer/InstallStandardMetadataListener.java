0b0665ee926579d65867849b6bbf915110d433d0
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-10-28 07:46:32 +0000

    PostOptimizationAction to install metadata listener
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7695 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ac0ac79e62bf8d6fbd723fc58befdccd97634873

---
+++ core/planmanagement.optimization.installstandardmetadatalistener/src/de/uniol/inf/is/odysseus/planmanagement/optimization/standardoptimizer/InstallStandardMetadataListener.java
@@ -0,0 +1,25 @@
+package de.uniol.inf.is.odysseus.planmanagement.optimization.standardoptimizer;
+
+import de.uniol.inf.is.odysseus.monitoring.physicaloperator.MonitoringDataTypes;
+import de.uniol.inf.is.odysseus.monitoring.physicalplan.PlanMonitor;
+import de.uniol.inf.is.odysseus.planmanagement.optimization.IPostOptimitzationAction;
+import de.uniol.inf.is.odysseus.planmanagement.optimization.configuration.OptimizationConfiguration;
+import de.uniol.inf.is.odysseus.planmanagement.query.IQuery;
+
+public class InstallStandardMetadataListener implements IPostOptimitzationAction {
+
+	// TODO: Konfigurierbar machen
+	private static long MONITORING_PERIOD = 30000;
+
+	@Override
+	public void run(IQuery query, OptimizationConfiguration parameter) {
+		query.addPlanMonitor(MonitoringDataTypes.DATARATE.name,
+				new PlanMonitor(query, false,
+						MonitoringDataTypes.DATARATE.name,
+						MONITORING_PERIOD));
+		query.addPlanMonitor(MonitoringDataTypes.SELECTIVITY.name,
+				new PlanMonitor(query, false,
+						MonitoringDataTypes.SELECTIVITY.name, -1));
+	}
+
+}
