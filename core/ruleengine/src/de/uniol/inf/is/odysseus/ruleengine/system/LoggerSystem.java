52e8390fcb5ee4ce5a2028a686e165ee5114d51e
Dennis Geesen <geesen@indivon.de>
2010-10-28 09:13:34 +0000

    ein debug-all-flag, wenn man sich alles als debug ausgegeben lassen möchte
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7700 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: afb2725c8950755db6dbb4c0dac5e39ab97dbc6e

--- core/ruleengine/src/de/uniol/inf/is/odysseus/ruleengine/system/LoggerSystem.java
+++ core/ruleengine/src/de/uniol/inf/is/odysseus/ruleengine/system/LoggerSystem.java
@@ -4,64 +4,64 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LoggerSystem {
-	
+
 	private static Logger defaultlogger = LoggerFactory.getLogger("ruleengine");
 	private static Accuracy defaultAccuracy = Accuracy.DEBUG;
-	
-	public enum Accuracy{
-		ERROR,
-		WARN,
-		INFO,
-		DEBUG,		
-		TRACE
+	private static boolean showAllAsDebug = false;
+
+	public enum Accuracy {
+		ERROR, WARN, INFO, DEBUG, TRACE
 	}
-		
-	
-	public static void printlog(String name, String message){
-		printlog(message, LoggerFactory.getLogger(name));		
+
+	public static void printlog(String name, String message) {
+		printlog(message, LoggerFactory.getLogger(name));
 	}
-	
-	public static void printlog(String name, Accuracy accuracy, String message){
+
+	public static void printlog(String name, Accuracy accuracy, String message) {
 		printlog(accuracy, message, LoggerFactory.getLogger(name));
-		
+
 	}
-	
-	//defaults
-	
-	public static void printlog(Accuracy accuracy, String message){
+
+	// defaults
+
+	public static void printlog(Accuracy accuracy, String message) {
 		printlog(accuracy, message, defaultlogger);
 	}
-	
-	public static void printlog(String message){
+
+	public static void printlog(String message) {
 		printlog(message, defaultlogger);
 	}
-	
-	//mappers
-	
-	private static void printlog(String message, Logger thelogger) {		
+
+	// mappers
+
+	private static void printlog(String message, Logger thelogger) {
 		printlog(defaultAccuracy, message, thelogger);
 	}
 
-	private static void printlog(Accuracy accuracy, String message, Logger currentlogger){
-		switch (accuracy) {
-		case TRACE:
-			currentlogger.trace(message);
-			break;
-		case DEBUG:
-			currentlogger.debug(message);
-			break;
-		case INFO:
-			currentlogger.info(message);
-			break;
-		case WARN:
-			currentlogger.warn(message);
-			break;
-		case ERROR:
-			currentlogger.error(message);
-			break;
+	private static void printlog(Accuracy accuracy, String message, Logger currentlogger) {
+		if (showAllAsDebug) {
+			currentlogger.debug("["+accuracy + "] " + message);
+		} else {
+			switch (accuracy) {
+			case TRACE:
+				currentlogger.trace(message);
+				break;
+			case DEBUG:
+				currentlogger.debug(message);
+				break;
+			case INFO:
+				currentlogger.info(message);
+				break;
+			case WARN:
+				currentlogger.warn(message);
+				break;
+			case ERROR:
+				currentlogger.error(message);
+				break;
 
-		default:
-			break;
-		}				
+			default:
+				break;
+			}
+		}
 	}
 }
