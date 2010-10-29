8faa5a28028f752736748a6969468a4bc0b87208
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-10-29 07:39:06 +0000

    Unsinnige Implementierung korrigiert, Abhaengigkeit zum Prioritaetsscheduler entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7711 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 87b7cd82613261694180e02ae8cdd775929c5166

--- scheduling/scheduler.slascheduler/src/de/uniol/inf/is/odysseus/scheduler/slascheduler/strategy/SimpleSLAScheduler.java
+++ scheduling/scheduler.slascheduler/src/de/uniol/inf/is/odysseus/scheduler/slascheduler/strategy/SimpleSLAScheduler.java
@@ -1,58 +1,70 @@
 package de.uniol.inf.is.odysseus.scheduler.slascheduler.strategy;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.uniol.inf.is.odysseus.monitoring.physicalplan.IPlanMonitor;
 import de.uniol.inf.is.odysseus.planmanagement.query.IQuery;
-import de.uniol.inf.is.odysseus.scheduler.priorityscheduler.AbstractDynamicPriorityPlanScheduling;
+import de.uniol.inf.is.odysseus.scheduler.ISchedulingEventListener;
+import de.uniol.inf.is.odysseus.scheduler.singlethreadscheduler.IPartialPlanScheduling;
+import de.uniol.inf.is.odysseus.scheduler.strategy.CurrentPlanPriorityComperator;
 import de.uniol.inf.is.odysseus.scheduler.strategy.IScheduling;
 import de.uniol.inf.is.odysseus.usermanagement.IServiceLevelAgreement;
 import de.uniol.inf.is.odysseus.usermanagement.NotInitializedException;
 import de.uniol.inf.is.odysseus.usermanagement.TenantManagement;
 
-public class SimpleSLAScheduler extends AbstractDynamicPriorityPlanScheduling {
+public class SimpleSLAScheduler implements IPartialPlanScheduling, ISchedulingEventListener{
 
-	public enum PrioCalcMethod {MAX, SUM, AVG}
+	protected final List<IScheduling> queue;
+	
+	public enum PrioCalcMethod {
+		MAX, SUM, AVG
+	}
+
 	private PrioCalcMethod method;
 
 	public SimpleSLAScheduler(PrioCalcMethod method) {
-		super();
+		queue = new ArrayList<IScheduling>();
 		this.method = method;
 	}
 
-	public SimpleSLAScheduler(SimpleSLAScheduler simpleSLAScheduler ) {
-		super(simpleSLAScheduler);
+	public SimpleSLAScheduler(SimpleSLAScheduler simpleSLAScheduler) {
+		queue = new ArrayList<IScheduling>(simpleSLAScheduler.queue);
 		this.method = simpleSLAScheduler.method;
 	}
-	
+
 	@Override
-	protected void updatePriorities(IScheduling current) {
-		// Current Plan is irrelevant
-		synchronized(queue){
+	public IScheduling nextPlan() {
+		synchronized (queue) {
 			Map<IQuery, Double> calcedUrg = new HashMap<IQuery, Double>();
-			for (IScheduling is:queue){
+			for (IScheduling is : queue) {
 				// Calc prio for each query
-				for (IQuery q:is.getPlan().getQueries()){
+				for (IQuery q : is.getPlan().getQueries()) {
 					// A query can be part of more than one scheduling
 					// and only needs to be calced ones
-					if (calcedUrg.containsKey(q)) continue;
-					
-					IPlanMonitor<Double> monitor = q.getPlanMonitor("SLAMonitor");
-					IServiceLevelAgreement sla = TenantManagement.getInstance().getSLAForUser(q.getUser()); 
+					if (calcedUrg.containsKey(q))
+						continue;
+
+					IPlanMonitor<Double> monitor = q
+							.getPlanMonitor("SLAMonitor");
+					IServiceLevelAgreement sla = TenantManagement.getInstance()
+							.getSLAForUser(q.getUser());
 					double urge = 0.0;
 					try {
 						urge = sla.getMaxOcMg(monitor.getValue());
 					} catch (NotInitializedException e) {
 						throw new RuntimeException(e);
 					}
-					calcedUrg.put(q,urge);
+					calcedUrg.put(q, urge);
 				}
 				// Calc Prio for current Scheduling, based
 				// on all queries
 				// e.g. with Max
 				double prio = 0;
-				switch(method){
+				switch (method) {
 				case MAX:
 					prio = calcPrioMax(calcedUrg, is);
 					break;
@@ -63,17 +75,18 @@ public class SimpleSLAScheduler extends AbstractDynamicPriorityPlanScheduling {
 					prio = calcPrioAvg(calcedUrg, is);
 					break;
 				}
-				
 				// Set max as new prio
-				is.getPlan().setCurrentPriority((long)Math.round(prio*100));
+				is.getPlan().setCurrentPriority((long) Math.round(prio * 100));
 			}
+			Collections.sort(queue, new CurrentPlanPriorityComperator());
+			return queue.get(0);
 		}
 	}
 
 	// Calc Prio with maximum
 	private double calcPrioMax(Map<IQuery, Double> calcedUrg, IScheduling is) {
 		double prio = 0;
-		for (IQuery q:is.getPlan().getQueries()){
+		for (IQuery q : is.getPlan().getQueries()) {
 			prio = Math.max(prio, calcedUrg.get(q));
 		}
 		return prio;
@@ -82,24 +95,70 @@ public class SimpleSLAScheduler extends AbstractDynamicPriorityPlanScheduling {
 	// Calc Prio as sum
 	private double calcPrioSum(Map<IQuery, Double> calcedUrg, IScheduling is) {
 		double prio = 0;
-		for (IQuery q:is.getPlan().getQueries()){
-			prio = + calcedUrg.get(q);
+		for (IQuery q : is.getPlan().getQueries()) {
+			prio = +calcedUrg.get(q);
 		}
 		return prio;
 	}
-	
+
 	// Calc Prio as avg
 	private double calcPrioAvg(Map<IQuery, Double> calcedUrg, IScheduling is) {
 		double prio = 0;
-		for (IQuery q:is.getPlan().getQueries()){
-			prio = + calcedUrg.get(q);
+		for (IQuery q : is.getPlan().getQueries()) {
+			prio = +calcedUrg.get(q);
 		}
-		return prio/calcedUrg.size();
+		return prio / calcedUrg.size();
 	}
 
 	@Override
 	public SimpleSLAScheduler clone() {
 		return new SimpleSLAScheduler(this);
+	}
+
+	@Override
+	public void clear() {
+		queue.clear();
+		
+	}
+
+	@Override
+	public void addPlan(IScheduling scheduling) {
+		synchronized (queue) {
+			queue.add(scheduling);
+			Collections.sort(queue, new CurrentPlanPriorityComperator());
+			scheduling.addSchedulingEventListener(this);
+		}
+	}
+
+	@Override
+	public int planCount() {
+		synchronized (queue) {
+			return queue.size();
+		}
+	}
+
+	@Override
+	public void removePlan(IScheduling plan) {
+		synchronized (queue) {
+			queue.remove(plan);
+			plan.removeSchedulingEventListener(this);
+		}
+	}
+
+	@Override
+	public void nothingToSchedule(IScheduling sched) {
+		synchronized (queue) {
+			// keine gute Idee -->
+			// queue.remove(sched);
+		}
+	}
+
+	@Override
+	public void scheddulingPossible(IScheduling sched) {
+		synchronized (queue) {
+			queue.add(sched);
+			Collections.sort(queue, new CurrentPlanPriorityComperator());
+		}
 	}
 
 }
