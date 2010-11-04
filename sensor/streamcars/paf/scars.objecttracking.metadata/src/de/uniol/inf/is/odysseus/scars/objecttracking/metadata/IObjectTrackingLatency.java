dfd62f92b75900f43239784e2c10023a63079121
Volker Janz <vjanz@uni-oldenburg.de>
2010-11-04 10:50:24 +0000

    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7763 aa0bda67-78ed-0310-a382-c5338b0f89ba
    Former-commit-id: 2a179e42def145f0908f6bf5cd26fd52d046548b

--- sensor/streamcars/paf/scars.objecttracking.metadata/src/de/uniol/inf/is/odysseus/scars/objecttracking/metadata/IObjectTrackingLatency.java
+++ sensor/streamcars/paf/scars.objecttracking.metadata/src/de/uniol/inf/is/odysseus/scars/objecttracking/metadata/IObjectTrackingLatency.java
@@ -1,7 +1,6 @@
 package de.uniol.inf.is.odysseus.scars.objecttracking.metadata;
 
 import de.uniol.inf.is.odysseus.IClone;
-import de.uniol.inf.is.odysseus.latency.ILatency;
 import de.uniol.inf.is.odysseus.metadata.IMetaAttribute;
 
 public interface IObjectTrackingLatency extends IMetaAttribute, IClone {
@@ -9,7 +8,5 @@ public interface IObjectTrackingLatency extends IMetaAttribute, IClone {
 	public void setObjectTrackingLatencyStart();
 	public void setObjectTrackingLatencyEnd();
 	public long getObjectTrackingLatency();
-	@Override
-	public ILatency clone();
 
 }
