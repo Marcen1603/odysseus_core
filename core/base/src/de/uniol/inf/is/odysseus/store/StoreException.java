571c6b1baebfa575042a1e5c839348204c3c8f10
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-11-10 12:05:42 +0000

    StoreException is now a RuntimeException
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7862 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5906a658437572a8ecf7fce2211f4f95eeaaeaed

--- core/base/src/de/uniol/inf/is/odysseus/store/StoreException.java
+++ core/base/src/de/uniol/inf/is/odysseus/store/StoreException.java
@@ -2,7 +2,7 @@ package de.uniol.inf.is.odysseus.store;
 
 import java.io.IOException;
 
-public class StoreException extends Exception {
+public class StoreException extends RuntimeException {
 
 	private static final long serialVersionUID = 1244925426430692075L;
 	
