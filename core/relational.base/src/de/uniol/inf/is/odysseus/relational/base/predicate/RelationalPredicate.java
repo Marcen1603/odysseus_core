b980bb401717614fdecf9731be00a188e5c41610
Matthias Zimmermann <mzimmermann@uni-oldenburg.de>
2010-11-14 16:05:05 +0000

    u.a. Anpassung von RelationalPredicate fürs Query Sharing
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7909 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: aa2b87979be2bcb52de6422a9d1b4569dc1b8d63

--- core/relational.base/src/de/uniol/inf/is/odysseus/relational/base/predicate/RelationalPredicate.java
+++ core/relational.base/src/de/uniol/inf/is/odysseus/relational/base/predicate/RelationalPredicate.java
@@ -7,6 +7,11 @@ import java.util.List;
 import java.util.Map;
 
 import de.uniol.inf.is.odysseus.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.mep.AbstractFunction;
+import de.uniol.inf.is.odysseus.mep.IExpression;
+import de.uniol.inf.is.odysseus.mep.IFunction;
+import de.uniol.inf.is.odysseus.mep.MEP;
+import de.uniol.inf.is.odysseus.mep.Variable;
 import de.uniol.inf.is.odysseus.predicate.AbstractPredicate;
 import de.uniol.inf.is.odysseus.predicate.IPredicate;
 import de.uniol.inf.is.odysseus.relational.base.RelationalTuple;
@@ -163,7 +168,13 @@ public class RelationalPredicate extends AbstractPredicate<RelationalTuple<?>> i
 	
 	@Override
 	public boolean equals(IPredicate pred) {
-		return this.equals((Object)pred);
+		if(!this.equals((Object)pred)) {
+			boolean isContainedIn1 = this.isContainedIn(pred);
+			boolean isContainedIn2 = pred.isContainedIn(this);
+			System.out.println(isContainedIn1 + "  +  " + isContainedIn2);
+			return this.isContainedIn(pred) && pred.isContainedIn(this);
+		}
+		return false;
 	}
 	
 	@Override
@@ -171,14 +182,195 @@ public class RelationalPredicate extends AbstractPredicate<RelationalTuple<?>> i
 		if(!(o instanceof RelationalPredicate)) {
 			return false;
 		}
-//		RelationalPredicate rp = (RelationalPredicate)o;
-//		SDFExpression e1 = this.expression;
-//		SDFExpression e2 = rp.expression;
-//		System.out.println("EXPRESSION E1: " + e1.getExpression());
-//		System.out.println("EXPRESSION E2: " + e2.getExpression());
-//		for(SDFAttribute a : e1.getAllAttributes()) {
-//			System.out.println(a.toPointString());
-//		}
+		RelationalPredicate rp2 = (RelationalPredicate)o;
+		
+		// Unterschiedliche Anzahl Attribute
+		if(this.getAttributes().size() != rp2.getAttributes().size()) {
+			return false;
+		}
+		try {
+			IExpression<?> ex1 = MEP.parse(this.expression.getExpression());
+			IExpression<?> ex2 = MEP.parse(rp2.getExpression().getExpression());
+			if(ex1.getType().equals(ex2.getType()) && ex1.isFunction()) {
+				IFunction<?> if1 = (IFunction<?>) ex1;
+				IFunction<?> if2 = (IFunction<?>) ex2;
+				if(if1.getArity() !=2){
+					return false;
+				}
+				
+				IExpression<?> firstArgument1 = if1.getArgument(0);
+				IExpression<?> secondArgument1 = if1.getArgument(1);
+				IExpression<?> firstArgument2 = if2.getArgument(0);
+				IExpression<?> secondArgument2 = if2.getArgument(1);
+				
+				String symbol1 = if1.getSymbol();
+				String symbol2 = if2.getSymbol();
+				// Funktionen enthalten nur ein Attribut und ansonsten Konstanten
+				if(this.getAttributes().size() == 1) {
+					// gleiches Attribut auf der linken Seite, Konstante auf der rechten
+					if(firstArgument1.isVariable() &&  secondArgument1.isConstant() 
+							&& firstArgument2.isVariable() && secondArgument2.isConstant()
+							&& firstArgument1.toVariable().equals(firstArgument2.toVariable())) {
+						Double c1 = Double.parseDouble(secondArgument1.toString());
+						Double c2 = Double.parseDouble(secondArgument2.toString());
+
+						// Funktion kleiner-als
+						if(symbol1.equals("<") && (symbol2.equals("<") || symbol2.equals("<="))
+								&& c1 <= c2) {
+							return true;
+						}
+						// Funktion kleiner-gleich-als
+						if(symbol1.equals("<=") && ((symbol2.equals("<=") && c1 <= c2)
+								|| (symbol2.equals("<") && c1 < c2))) {
+							return true;
+						}
+						// Funktion ist-gleich
+						if(symbol1.equals("==") && symbol2.equals("==") && c1.compareTo(c2) == 0) {
+							return true;
+						}
+						// Funktion größer-als
+						if(symbol1.equals(">") && (symbol2.equals(">") || symbol2.equals(">="))
+								&& c1 >= c2) {
+							return true;
+						}
+						// Funktion größer-gleich-als
+						if(symbol1.equals(">=") && ((symbol2.equals(">=") && c1 >= c2)
+								|| (symbol2.equals(">") && c1 > c2))) {
+							return true;
+						}
+						// gleiches Attribut auf der rechten Seite, Konstante auf der linken Seite
+					} else if(secondArgument1.isVariable() &&  firstArgument1.isConstant() 
+							&& secondArgument2.isVariable() && firstArgument2.isConstant()
+							&& secondArgument1.toVariable().equals(secondArgument2.toVariable())) {
+
+						Double c1 = Double.parseDouble(firstArgument1.toString());
+						Double c2 = Double.parseDouble(firstArgument2.toString());
+
+						// Funktion kleiner-als
+						if(symbol1.equals("<") && (symbol2.equals("<") || symbol2.equals("<="))
+								&& c1 >= c2) {
+							return true;
+						}
+						// Funktion kleiner-gleich-als
+						if(symbol1.equals("<=") && ((symbol2.equals("<=") && c1 >= c2)
+								|| (symbol2.equals("<") && c1 > c2))) {
+							return true;
+						}
+						// Funktion ist-gleich
+						if(symbol1.equals("==") && symbol2.equals("==") && c1.compareTo(c2) == 0) {
+							return true;
+						}
+						// Funktion größer-als
+						if(symbol1.equals(">") && (symbol2.equals(">") || symbol2.equals(">="))
+								&& c1 <= c2) {
+							return true;
+						}
+						// Funktion größer-gleich-als
+						if(symbol1.equals(">=") && ((symbol2.equals(">=") && c1 <= c2)
+								|| (symbol2.equals(">") && c1 < c2))) {
+							return true;
+						}
+						// Attribut bei F1 links, bei F2 rechts
+					} else if(firstArgument1.isVariable() &&  secondArgument1.isConstant() 
+							&& secondArgument2.isVariable() && firstArgument2.isConstant()
+							&& firstArgument1.toVariable().equals(secondArgument2.toVariable())) {
+						Double c1 = Double.parseDouble(secondArgument1.toString());
+						Double c2 = Double.parseDouble(firstArgument2.toString());
+
+						// F1 kleiner-als, F2 größer-als oder größer-gleich-als
+						if(symbol1.equals("<") && (symbol2.equals(">") || symbol2.equals(">=")) && c1 <= c2) {
+							return true;
+						}
+						// F1 kleiner-gleich-als, F2 größer-als oder größer-gleich-als
+						if(symbol1.equals("<=") && (symbol2.equals(">") && c1 < c2)
+								|| (symbol2.equals(">=")) && c1 <= c2) {
+							return true;
+						}
+						// Funktion ist-gleich
+						if(symbol1.equals("==") && symbol2.equals("==") && c1.compareTo(c2) == 0) {
+							return true;
+						}
+						// F1 größer-als, F2 kleiner-als oder kleiner-gleich-als
+						if(symbol1.equals(">") && (symbol2.equals("<") || symbol2.equals("<=")) && c1 >= c2) {
+							return true;
+						}
+						// F1 größer-gleich-als, F2 kleiner-als oder kleiner-gleich-als
+						if(symbol1.equals(">=") && (symbol2.equals("<") && c1 > c2)
+								|| (symbol2.equals("<=")) && c1 >= c2) {
+							return true;
+						}
+
+						// Attribut bei F1 rechts, bei F2 links
+					} else if(secondArgument1.isVariable() && firstArgument1.isConstant() 
+							&& firstArgument2.isVariable() && secondArgument2.isConstant()
+							&& secondArgument1.toVariable().equals(firstArgument2.toVariable())) {
+
+						Double c1 = Double.parseDouble(firstArgument1.toString());
+						Double c2 = Double.parseDouble(secondArgument2.toString());
+						
+						// F1 kleiner-als, F2 größer-als oder größer-gleich-als
+						if(symbol1.equals("<") && (symbol2.equals(">") || symbol2.equals(">=")) && c1 >= c2) {
+							return true;
+						}
+						
+						// F1 kleiner-gleich-als, F2 größer-als oder größer-gleich-als
+						if(symbol1.equals("<=") && (symbol2.equals(">") && c1 > c2)
+								|| (symbol2.equals(">=")) && c1 >= c2) {
+							return true;
+						}
+						
+						// Funktion ist-gleich
+						if(symbol1.equals("==") && symbol2.equals("==") && c1.compareTo(c2) == 0) {
+							return true;
+						}
+
+						// F1 größer-als, F2 kleiner-als oder kleiner-gleich-als
+						if(symbol1.equals(">") && (symbol2.equals("<") || symbol2.equals("<=")) && c1 <= c2) {
+							return true;
+						}
+						
+						// F1 größer-gleich-als, F2 kleiner-als oder kleiner-gleich-als
+						if(symbol1.equals(">=") && (symbol2.equals("<") && c1 < c2)
+								|| (symbol2.equals("<=")) && c1 <= c2) {
+							return true;
+						}
+					}
+				// Funktionen sind Vergleiche zwischen zwei Attributen
+				} else if(this.getAttributes().size()==2) {
+					Variable v11 = firstArgument1.toVariable();
+					Variable v12 = secondArgument1.toVariable();
+					Variable v21 = firstArgument2.toVariable();
+					Variable v22 = secondArgument2.toVariable();
+					
+					// Attribute sind links und rechts gleich
+					if(v11.equals(v21) && v12.equals(v22)) {
+						if((symbol1.equals("<") && symbol2.equals("<"))
+								|| (symbol1.equals("<=") && symbol2.equals("<="))
+								|| (symbol1.equals("==") && symbol2.equals("=="))
+								|| (symbol1.equals(">=") && symbol2.equals(">="))
+								|| (symbol1.equals(">") && symbol2.equals(">"))){
+							return true;
+						}
+					}
+					// linkes Attribut von F1 ist gleich rechtem Attribut von F2 und umgekehrt
+					if(v11.equals(v22) && v12.equals(v21)) {
+						if((symbol1.equals("<") && symbol2.equals(">"))
+								|| (symbol1.equals("<=") && symbol2.equals(">="))
+								|| (symbol1.equals("==") && symbol2.equals("=="))
+								|| (symbol1.equals(">=") && symbol2.equals("<="))
+								|| (symbol1.equals(">") && symbol2.equals("<"))) {
+							return true;
+						}
+					}
+				}
+				
+
+
+			}
+
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 		return false;
 	}
 }
