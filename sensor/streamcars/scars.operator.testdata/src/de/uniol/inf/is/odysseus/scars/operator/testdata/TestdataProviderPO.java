af19a14a3513c950a00b5cd321f823766fe8238e
Timo Michelsen <Timo.Michelsen@gmail.com>
2010-11-15 16:33:24 +0000

    StreamCars: verhalten des Testdataprovider angepasst (der Scheduler war zu langsam... ;-) )
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7929 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d390bde0457f7d96cd2ff26a130c6ea76e864f4a

--- sensor/streamcars/scars.operator.testdata/src/de/uniol/inf/is/odysseus/scars/operator/testdata/TestdataProviderPO.java
+++ sensor/streamcars/scars.operator.testdata/src/de/uniol/inf/is/odysseus/scars/operator/testdata/TestdataProviderPO.java
@@ -13,29 +13,47 @@ import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
 public class TestdataProviderPO<M extends IProbability> extends AbstractSensorAccessPO<MVRelationalTuple<M>, M> {
 
-	private static final int DELAY = 100;
-	private static final int SEND_DELAY = 300;
+	private static final int DELAY = 100; // ms
+	private static final int SEND_DELAY = 15; //ms
+	private static final long SEND_DELAY_NS = SEND_DELAY * 1000000; // ns
 
 	private Provider provider;
 	private long lastTime;
-	private MVRelationalTuple<M> buffer = null;
 	private String sourceName;
+	
+	private int batchSize = 2000;
+	
+	@SuppressWarnings("unchecked")
+	private MVRelationalTuple<M>[] nextElems = new MVRelationalTuple[batchSize];
+	private boolean done = false;
+	
 	public TestdataProviderPO() {
 		this.provider = new Provider();
 	}
 
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public TestdataProviderPO(TestdataProviderPO operator) {
 		this.provider = operator.provider;
 		this.lastTime = operator.lastTime;
+		this.batchSize = operator.batchSize;
+		this.nextElems = new MVRelationalTuple[batchSize];
+		for(int i = 0; i<this.nextElems.length; i++){
+			this.nextElems[i] = operator.nextElems[i].clone();
+		}
+		this.done = operator.done;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void process_open() throws OpenFailedException {
 		provider.setDelay(DELAY);
 		provider.setNumOfCars(5);
 		provider.init();
 		new StreamCarsMetaDataInitializer<StreamCarsMetaData<Object>>(getOutputSchema());
+		
+		for(int i = 0; i<this.batchSize; i++){
+			this.nextElems[i] = (MVRelationalTuple<M>)provider.nextTuple();
+		}
 	}
 
 	@Override
@@ -56,47 +74,87 @@ public class TestdataProviderPO<M extends IProbability> extends AbstractSensorAc
 	protected void process_done() {
 	}
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public boolean hasNext() {
-
-		/*
-		 * Hier wird gewartet, damit die Verarbeitung der Daten besser
-		 * nachvollzogen werden kann und Odysseus / Eclipse nicht �berlastet
-		 * wird (siehe auch Ticket 225).
-		 */
+	public boolean hasNext(){
 		if(!this.isDone()){
-			if (System.currentTimeMillis() - lastTime > SEND_DELAY) {
-				this.buffer = (MVRelationalTuple<M>) provider.nextTuple();
-				return true;
-			}
-			else{
-				return false;
-			}
+			return true;
 		}
 		else{
 			this.propagateDone();
 			return false;
 		}
 	}
+	
+
+//	int counter = 0;
+//	@SuppressWarnings("unchecked")
+//	@Override
+//	public boolean hasNext() {
+//
+//		if(counter % 100 ==0){
+//			for(String curMsg : msg){
+//				System.out.println(curMsg);
+//			}
+//			this.counter = 0;
+//		}
+//		
+//		/*
+//		 * Hier wird gewartet, damit die Verarbeitung der Daten besser
+//		 * nachvollzogen werden kann und Odysseus / Eclipse nicht �berlastet
+//		 * wird (siehe auch Ticket 225).
+//		 */
+//		if(!this.isDone()){
+//			if (System.currentTimeMillis() - lastTime > SEND_DELAY) {
+//				msg[counter] = "Send after : " + (System.currentTimeMillis() - lastTime);
+//				counter++;
+//				this.buffer = (MVRelationalTuple<M>) provider.nextTuple();
+//				return true;
+//			}
+//			else{
+//				return false;
+//			}
+//		}
+//		else{
+//			this.propagateDone();
+//			return false;
+//		}
+//	}
 
 	@Override
 	public boolean isDone() {
-		return false;
+		return this.done;
 	}
 
 
-	@Override
-	public void transferNext() {
-		System.out.println("Send Data: " + this.buffer);
-
-		transfer(this.buffer);
-		this.buffer = null;
-//		System.out.println("Send Punctuation: " + (this.provider.getLastTimestamp()+1));
-//		sendPunctuation(new PointInTime(this.provider.getLastTimestamp()+1));
-		lastTime = System.currentTimeMillis();
-
+	public void transferNext(){
+		long lastTime = System.nanoTime();
+		for(int i = 0; i<this.batchSize; i++){
+			sleep(lastTime + SEND_DELAY_NS);
+			transfer(this.nextElems[i]);
+			lastTime = System.nanoTime();
+		}
+		this.done = true;
 	}
+	
+	private void sleep(long until){
+		while(System.nanoTime() < until){
+			;
+		}
+	}
+	
+//	int counter = 0;
+//	@Override
+//	public void transferNext() {
+//		
+////		counter++;
+////		System.out.println("Send Data #" + counter);
+//		
+//		transfer(this.buffer);
+//		this.buffer = null;
+////		System.out.println("Send Punctuation: " + (this.provider.getLastTimestamp()+1));
+////		sendPunctuation(new PointInTime(this.provider.getLastTimestamp()+1));
+//		lastTime = System.currentTimeMillis();
+//
+//	}
 
 	@SuppressWarnings("unchecked")
 	private void assignMetadata(Class<M> clazz, Object tuple ){
