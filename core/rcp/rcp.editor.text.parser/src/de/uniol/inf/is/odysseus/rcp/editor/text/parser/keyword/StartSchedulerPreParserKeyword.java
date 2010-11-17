e2d253d579f831a3abf1e31f8ac368c66513917a
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-11-17 12:47:50 +0000

    New command to start scheduler
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7965 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 49eed2310b19b6d2c751c4ee7c2ed1e9481456a1

---
+++ core/rcp/rcp.editor.text.parser/src/de/uniol/inf/is/odysseus/rcp/editor/text/parser/keyword/StartSchedulerPreParserKeyword.java
@@ -0,0 +1,35 @@
+package de.uniol.inf.is.odysseus.rcp.editor.text.parser.keyword;
+
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.planmanagement.executor.IExecutor;
+import de.uniol.inf.is.odysseus.planmanagement.executor.exception.PlanManagementException;
+import de.uniol.inf.is.odysseus.rcp.editor.text.parser.IPreParserKeyword;
+import de.uniol.inf.is.odysseus.rcp.editor.text.parser.QueryTextParseException;
+import de.uniol.inf.is.odysseus.rcp.editor.text.parser.activator.ExecutorHandler;
+
+public class StartSchedulerPreParserKeyword implements IPreParserKeyword {
+
+	@Override
+	public void validate(Map<String, String> variables, String parameter)
+			throws QueryTextParseException {
+		IExecutor executor = ExecutorHandler.getExecutor();
+		if (executor == null)
+			throw new QueryTextParseException("No executor found");
+	}
+
+	@Override
+	public void execute(Map<String, String> variables, String parameter)
+			throws QueryTextParseException {
+		IExecutor executor = ExecutorHandler.getExecutor();
+		if (executor == null)
+			throw new QueryTextParseException("No executor found");
+		try {
+			executor.startExecution();
+		} catch (PlanManagementException e) {
+			throw new QueryTextParseException(e);
+		}
+
+	}
+
+}
