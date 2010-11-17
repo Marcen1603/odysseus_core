94db135a21d2f086278e51c12833f07b21c3beca
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-11-17 13:30:01 +0000

    changed to runtime exceptions
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@7967 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7612b83fd7220573aaf8ab7ebd913470aaccde67

--- core/base/src/de/uniol/inf/is/odysseus/usermanagement/UserStoreException.java
+++ core/base/src/de/uniol/inf/is/odysseus/usermanagement/UserStoreException.java
@@ -1,7 +1,7 @@
 package de.uniol.inf.is.odysseus.usermanagement;
 
 
-public class UserStoreException extends Exception {
+public class UserStoreException extends RuntimeException {
 
 	private static final long serialVersionUID = -7816011558858972865L;
 
--- core/base/src/de/uniol/inf/is/odysseus/usermanagement/UsernameAlreadyUsedException.java
+++ core/base/src/de/uniol/inf/is/odysseus/usermanagement/UsernameAlreadyUsedException.java
@@ -1,6 +1,6 @@
 package de.uniol.inf.is.odysseus.usermanagement;
 
-public class UsernameAlreadyUsedException extends Exception {
+public class UsernameAlreadyUsedException extends RuntimeException {
 
 	private static final long serialVersionUID = 3361641628617871479L;
 
