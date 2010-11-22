db499a70f74328ad36d9a64824674cad9b21bbc8
Thomas Vogelgesang <Thomas.Vogelgesang@uni-oldenburg.de>
2010-11-22 16:21:44 +0000

    integrated new testdataprovider into transformation rule
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8037 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 6e5f080ca8fb1106f71511547b9b033396b8ea93

--- sensor/streamcars/de.uniol.inf.is.odysseus.scars.transform/src/de/uniol/inf/is/odysseus/scars/transform/rules/TJDVEAccessMVPOAsListRule.java
+++ sensor/streamcars/de.uniol.inf.is.odysseus.scars.transform/src/de/uniol/inf/is/odysseus/scars/transform/rules/TJDVEAccessMVPOAsListRule.java
@@ -1,10 +1,13 @@
 package de.uniol.inf.is.odysseus.scars.transform.rules;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.uniol.inf.is.odysseus.datadictionary.WrapperPlanFactory;
 import de.uniol.inf.is.odysseus.logicaloperator.AccessAO;
 import de.uniol.inf.is.odysseus.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.objecttracking.metadata.IProbability;
 import de.uniol.inf.is.odysseus.objecttracking.physicaloperator.access.AbstractSensorAccessPO;
 import de.uniol.inf.is.odysseus.planmanagement.TransformationConfiguration;
 import de.uniol.inf.is.odysseus.ruleengine.ruleflow.IRuleFlowGroup;
@@ -12,6 +15,8 @@ import de.uniol.inf.is.odysseus.scars.base.DummyAccessMVPO;
 import de.uniol.inf.is.odysseus.scars.base.JDVEAccessMVPO;
 import de.uniol.inf.is.odysseus.scars.base.SensorAccessAO;
 import de.uniol.inf.is.odysseus.scars.operator.testdata.TestdataProviderPO;
+import de.uniol.inf.is.odysseus.scars.testdata.provider.ExtendedProvider;
+import de.uniol.inf.is.odysseus.scars.testdata.provider.extended.calcmodel.DefaultOvertakeCalcModel;
 import de.uniol.inf.is.odysseus.transform.flow.TransformRuleFlowGroup;
 import de.uniol.inf.is.odysseus.transform.rule.AbstractTransformationRule;
 
@@ -34,6 +39,24 @@ public class TJDVEAccessMVPOAsListRule extends AbstractTransformationRule<Access
 				System.out.println("DummyAccessMVPO created");
 			} else if (operator.getPort() == 5002) {
 				accessPO = new TestdataProviderPO();
+				((TestdataProviderPO)accessPO).setSourceName(accessPOName);
+				System.out.println("TestdataProviderPO created");
+			} else if (operator.getPort() == 5010) {
+				Map<String, String> options = new HashMap<String, String>();
+				options.put(ExtendedProvider.SCHEMA, ExtendedProvider.SCHEMA_SCARS_DEFAULT);
+				options.put(ExtendedProvider.CALCMODEL, ExtendedProvider.CALCMODEL_SCARS_OVERTAKE);
+				Map<String, Object> calcModelParams = new HashMap<String, Object>();
+				calcModelParams.put(DefaultOvertakeCalcModel.LANE_SHIFT_FACTOR, new Float(1.5));
+				accessPO = new TestdataProviderPO<IProbability>(TestdataProviderPO.EXTENDED_PROVIDER, options, calcModelParams);
+				((TestdataProviderPO)accessPO).setSourceName(accessPOName);
+				System.out.println("TestdataProviderPO created");
+			} else if (operator.getPort() == 5011) {
+				Map<String, String> options = new HashMap<String, String>();
+				options.put(ExtendedProvider.SCHEMA, ExtendedProvider.SCHEMA_SCARS_ALTERNATIVE);
+				options.put(ExtendedProvider.CALCMODEL, ExtendedProvider.CALCMODEL_SCARS_OVERTAKE);
+				Map<String, Object> calcModelParams = new HashMap<String, Object>();
+				calcModelParams.put(DefaultOvertakeCalcModel.LANE_SHIFT_FACTOR, new Float(1.5));
+				accessPO = new TestdataProviderPO<IProbability>(TestdataProviderPO.EXTENDED_PROVIDER, options, calcModelParams);
 				((TestdataProviderPO)accessPO).setSourceName(accessPOName);
 				System.out.println("TestdataProviderPO created");
 			}
