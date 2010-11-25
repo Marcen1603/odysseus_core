8638236c93de9ab5696d0bf0c51912188e12af98
Dennis Geesen <geesen@indivon.de>
2010-11-25 13:19:39 +0000

    Datengenerator, der synthetisch anhand von einer normalverteilung und einer gegebenen standardabweichung daten für cluster generiert
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8078 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 75cbc34cd4c8d21a976f34fb53b1f68c94d06f82

---
+++ mining/mining.generator.syntheticclusters/src/mining/generator/syntheticclusters/SyntheticClusterProvider.java
@@ -0,0 +1,84 @@
+package mining.generator.syntheticclusters;
+
+import java.util.Random;
+
+import mining.generator.base.socket.StreamClientHandler;
+import mining.generator.base.tuple.DataTuple;
+
+public class SyntheticClusterProvider extends StreamClientHandler {
+
+	private long time = 0;
+	private int numberOfPotentialClusters = 4; // gerade anzahl
+	private double distanceBetweenPeeks = 70.0d;
+
+	private double standardabweichung = 10.0;
+
+	private Random rand = new Random();
+	private double[] peekX;
+	private double[] peekY;
+
+	@Override
+	public void init() {
+		System.out.println("Creating clusters: " + numberOfPotentialClusters);
+		System.out.println("... with distance between means: " + distanceBetweenPeeks);
+		System.out.println("... with standard derivation: " + standardabweichung);
+		int perLine = (int) Math.ceil(Math.sqrt(numberOfPotentialClusters));
+
+		peekX = new double[numberOfPotentialClusters];
+		peekY = new double[numberOfPotentialClusters];
+
+		int row = 1;
+		int col = 1;
+		for (int i = 0; i < numberOfPotentialClusters; i++) {
+			peekX[i] = (row) * distanceBetweenPeeks;
+			peekY[i] = (col) * distanceBetweenPeeks;
+			System.out.println(" "+(i+1)+". Mean: " + peekX[i] + ":" + peekY[i]);
+			
+			if ((col % perLine) == 0) {
+				row++;
+				col = 1;
+			}else{
+				col++;
+			}
+		}
+
+		// int line = 1;
+		// for(int i=0;i<numberOfPotentialClusters;i++){
+		// peekX[i] = (i+1)*distanceBetweenPeeks;
+		// peekY[i] = line*distanceBetweenPeeks;
+		//
+		// if(((i+1)%perLine)==0){
+		// line++;
+		// }
+		// }
+	}
+
+	@Override
+	public void close() {
+
+	}
+
+	@Override
+	public DataTuple next() {
+		DataTuple tuple = new DataTuple();
+		int nextCluster = rand.nextInt(numberOfPotentialClusters);
+
+		double valX = standardabweichung * rand.nextGaussian() + peekX[nextCluster];
+		double valY = standardabweichung * rand.nextGaussian() + peekY[nextCluster];
+
+		tuple.addAttribute(new Long(time));
+		tuple.addAttribute(new Double(valX));
+		tuple.addAttribute(new Double(valY));
+
+		time++;
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return tuple;
+	}
+
+}
