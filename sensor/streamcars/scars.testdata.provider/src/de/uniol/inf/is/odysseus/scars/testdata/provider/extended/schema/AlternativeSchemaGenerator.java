8357ffec09f27c9e22ff40a7f6ff73aa5726ba8f
Thomas Vogelgesang <Thomas.Vogelgesang@uni-oldenburg.de>
2010-11-29 15:30:59 +0000

    Bugfix: Wrong covarianceMatrix in alternative schema
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8128 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9831aa343784d74bedb3f77c0667d5d03808f267

--- sensor/streamcars/scars.testdata.provider/src/de/uniol/inf/is/odysseus/scars/testdata/provider/extended/schema/AlternativeSchemaGenerator.java
+++ sensor/streamcars/scars.testdata.provider/src/de/uniol/inf/is/odysseus/scars/testdata/provider/extended/schema/AlternativeSchemaGenerator.java
@@ -25,23 +25,22 @@ public class AlternativeSchemaGenerator implements ISchemaGenerator {
 		SDFAttribute posx = new SDFAttribute(sourceName, "posx");
 		posx.setDatatype(SDFDatatypeFactory.getDatatype("MV Float"));
 		posx.setCovariance(Arrays.asList(new Double[] { mvValue,
-				mvCorrelationValue, mvCorrelationValue, mvCorrelationValue,
 				mvCorrelationValue, mvCorrelationValue, mvCorrelationValue }));
 
 		SDFAttribute posy = new SDFAttribute(sourceName, "posy");
 		posy.setDatatype(SDFDatatypeFactory.getDatatype("MV Float"));
 		posy.setCovariance(Arrays.asList(new Double[] { mvCorrelationValue,
-				mvValue, mvCorrelationValue, mvCorrelationValue,
-				mvCorrelationValue, mvCorrelationValue, mvCorrelationValue }));
+				mvValue, mvCorrelationValue, mvCorrelationValue}));
 
 		SDFAttribute velocity = new SDFAttribute(sourceName, "velocity");
 		velocity.setDatatype(SDFDatatypeFactory.getDatatype("MV Float"));
 		velocity.setCovariance(Arrays.asList(new Double[] { mvCorrelationValue,
-				mvCorrelationValue, mvCorrelationValue, mvCorrelationValue,
-				mvCorrelationValue, mvCorrelationValue, mvValue }));
+				mvCorrelationValue, mvValue, mvCorrelationValue}));
 
 		SDFAttribute acceleration = new SDFAttribute(sourceName, "acceleration");
-		acceleration.setDatatype(SDFDatatypeFactory.getDatatype("Float"));
+		acceleration.setDatatype(SDFDatatypeFactory.getDatatype("MV Float"));
+		acceleration.setCovariance(Arrays.asList(new Double[] { mvCorrelationValue,
+				mvCorrelationValue, mvCorrelationValue, mvValue }));
 
 		SDFAttribute car = new SDFAttribute(sourceName, "car");
 		car.setDatatype(SDFDatatypeFactory.getDatatype("Record"));
