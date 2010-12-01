d199b7ffa046b2e11d060a314116e8c92b08b066
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-12-01 08:53:04 +0000

    new ParserKeyword to set or update Odysseus properties
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8166 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b297ff6f72440edbd1ad9744ed4f073f340d1404

---
+++ core/rcp/rcp.editor.text.parser/src/de/uniol/inf/is/odysseus/rcp/editor/text/parser/keyword/OdysseusDefaultsPreParserKeyword.java
@@ -0,0 +1,33 @@
+package de.uniol.inf.is.odysseus.rcp.editor.text.parser.keyword;
+
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.rcp.editor.text.parser.AbstractPreParserKeyword;
+import de.uniol.inf.is.odysseus.rcp.editor.text.parser.QueryTextParseException;
+import de.uniol.inf.is.odysseus.OdysseusDefaults;
+
+public class OdysseusDefaultsPreParserKeyword extends AbstractPreParserKeyword {
+
+	@Override
+	public void validate(Map<String, Object> variables, String parameter)
+			throws QueryTextParseException {
+		// Ignore
+	}
+
+	@Override
+	public Object execute(Map<String, Object> variables, String parameter)
+			throws QueryTextParseException {
+		String[] params = getSimpleParameters(parameter);
+		if (params.length >= 2) {
+			boolean permanently = false;
+			if (params.length == 3) {
+				if ("TRUE".equalsIgnoreCase(params[2])) {
+					permanently = true;
+				}
+			}
+			OdysseusDefaults.set(params[0],params[1],permanently);
+		}
+
+		return null;
+	}
+}
