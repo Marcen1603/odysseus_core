eb44b72807768e70a76a3c72c83a152643dd4b99
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-12-02 15:14:13 +0000

    Dummy SLA Generation
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8203 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 094a5df58a4dda24812c72118fc6bdcb6d8f25c2

--- core/base/src/de/uniol/inf/is/odysseus/usermanagement/test/SLATest.java
+++ core/base/src/de/uniol/inf/is/odysseus/usermanagement/test/SLATest.java
@@ -13,55 +13,18 @@ public class SLATest {
 			throws PercentileConstraintOverlapException,
 			NotInitializedException, IllegalServiceLevelDefinition {
 		IServiceLevelAgreement sla = new ServiceLevelAgreement("Test");
-		PercentileContraint pc = new PercentileContraint(1, 0.9, 0, true);
+		PercentileContraint pc = new PercentileContraint(0.01, 1, 0, true);
 		sla.addPercentilConstraint(pc);
-		pc = new PercentileContraint(0.8, 0.7, 200);
-		sla.addPercentilConstraint(pc);
-
-		pc = new PercentileContraint(0.7, 0.6, 600);
-		sla.addPercentilConstraint(pc);
-
-		pc = new PercentileContraint(0.6, 0.5, 800);
-		sla.addPercentilConstraint(pc);
-
-		pc = new PercentileContraint(0.5, 0.3, 1300);
-		sla.addPercentilConstraint(pc);
-
-		pc = new PercentileContraint(0.3, 0.0, 1400);
-		sla.addPercentilConstraint(pc);
-
-		// Execption provozieren
-		try {
-			pc = new PercentileContraint(0.6, 0.5, 2000000);
-			sla.addPercentilConstraint(pc);
-		} catch (PercentileConstraintOverlapException p) {
-			System.out.println("Richtig erkannt :-) " + p);
-		}
-
-		try {
-			System.out.println("1 --> " + sla.getPercentilConstraint(1));
-		} catch (NotInitializedException e) {
-			System.out.println("Richtig erkannt :-) " + e);
-		}
-
-		try {
-			sla.init();
-		} catch (IllegalServiceLevelDefinition e) {
-			System.out.println("Richtig erkannt :-) " + e);
-
-		}
-		pc = new PercentileContraint(0.9, 0.8, 100);
-		sla.addPercentilConstraint(pc);
-
-		System.out.println(sla);
+		pc = new PercentileContraint(0, 0.01, 1000, true);
 		
+		sla.addPercentilConstraint(pc);
 		sla.init();
 		
 		System.out.println(sla);
 
 		System.out.println("MaxPenalty " + sla.getMaxPenalty());
 
-		System.out.println("i;oc;mg;penalty;slapenalty");
+		System.out.println("i;levelUp;levelDown;urge;slapenalty");
 		for (double i=1;i>=0;i=Math.round((i-0.001)*1000.0)/1000.0){
 			double oc = sla.oc(i);
 			double mg = sla.mg(i);
