90a1957d802288b3640d58179f75afe98e387ec3
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-12-02 15:13:15 +0000

    Implements ICLone
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8201 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f0e3667bea4729c552e5cbef6bc92f7eee0f0fa9

--- core/scheduler.singlethreadscheduler2.0/src/de/uniol/inf/is/odysseus/scheduler/singlethreadscheduler/IPartialPlanScheduling.java
+++ core/scheduler.singlethreadscheduler2.0/src/de/uniol/inf/is/odysseus/scheduler/singlethreadscheduler/IPartialPlanScheduling.java
@@ -1,8 +1,9 @@
 package de.uniol.inf.is.odysseus.scheduler.singlethreadscheduler;
 
+import de.uniol.inf.is.odysseus.IClone;
 import de.uniol.inf.is.odysseus.scheduler.strategy.IScheduling;
 
-public interface IPartialPlanScheduling {
+public interface IPartialPlanScheduling extends IClone{
 
 	void clear();
 
