1edde9a432b3e417083c70df4be50163e60852c8
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-12-07 12:42:48 +0000

    New QueryEditorParameter Parameter DOREWRITE to enable/disable query rewrite
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8246 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 752253194f6e5d2b84eacb50220d1089e9033fba

---
+++ core/rcp/rcp.editor.text.parser/src/de/uniol/inf/is/odysseus/rcp/editor/text/parser/keyword/UseRewritePreParserKeyword.java
@@ -0,0 +1,51 @@
+package de.uniol.inf.is.odysseus.rcp.editor.text.parser.keyword;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.planmanagement.executor.IExecutor;
+import de.uniol.inf.is.odysseus.planmanagement.optimization.configuration.ParameterDoRewrite;
+import de.uniol.inf.is.odysseus.planmanagement.optimization.configuration.ParameterPerformQuerySharing;
+import de.uniol.inf.is.odysseus.planmanagement.query.querybuiltparameter.IQueryBuildSetting;
+import de.uniol.inf.is.odysseus.rcp.editor.text.parser.AbstractPreParserKeyword;
+import de.uniol.inf.is.odysseus.rcp.editor.text.parser.QueryTextParseException;
+import de.uniol.inf.is.odysseus.rcp.editor.text.parser.activator.ExecutorHandler;
+
+public class UseRewritePreParserKeyword extends AbstractPreParserKeyword {
+
+	@Override
+	public void validate(Map<String, Object> variables, String parameter)
+			throws QueryTextParseException {
+		IExecutor executor = ExecutorHandler.getExecutor();
+		if (executor == null)
+			throw new QueryTextParseException("No executor found");
+	}
+
+	@Override
+	public Object execute(Map<String, Object> variables, String parameter)
+			throws QueryTextParseException {
+		IExecutor executor = ExecutorHandler.getExecutor();
+		if (executor == null)
+			throw new QueryTextParseException("No executor found");
+		List<IQueryBuildSetting<?>> config = executor.getQueryBuildConfiguration((String)
+						variables.get("TRANSCFG"));
+		Iterator<IQueryBuildSetting<?>> iter = config.iterator();
+		if (iter != null){
+			while (iter.hasNext()) {
+				IQueryBuildSetting<?> sett = iter.next();
+				if (sett instanceof ParameterDoRewrite) {
+					iter.remove();
+					break;
+				}
+			}
+			if ("TRUE".equals(parameter.toUpperCase())){
+				config.add(ParameterDoRewrite.TRUE);
+			}else{
+				config.add(ParameterDoRewrite.FALSE);
+			}
+		}
+		return null;
+	}
+
+}
