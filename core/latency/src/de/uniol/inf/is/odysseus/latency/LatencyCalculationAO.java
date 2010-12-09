bb524bef5214609e0f873a74e50c735031562220
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-12-09 13:17:45 +0000

    Missing Copy Constructor
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8272 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f78765e9ab2f36b719dc3551e6c6c7c819aaeecf

--- core/latency/src/de/uniol/inf/is/odysseus/latency/LatencyCalculationAO.java
+++ core/latency/src/de/uniol/inf/is/odysseus/latency/LatencyCalculationAO.java
@@ -1,11 +1,20 @@
 package de.uniol.inf.is.odysseus.latency;
 
 import de.uniol.inf.is.odysseus.logicaloperator.AbstractLogicalOperator;
+import de.uniol.inf.is.odysseus.logicaloperator.UnaryLogicalOp;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
 
-public class LatencyCalculationAO extends AbstractLogicalOperator {
+public class LatencyCalculationAO extends UnaryLogicalOp {
 
 	private static final long serialVersionUID = -9078812899082643674L;
+	
+	public LatencyCalculationAO() {
+		super();
+	}
+	
+	public LatencyCalculationAO(LatencyCalculationAO ao){
+		super(ao);
+	}
 	
 	@Override
 	public SDFAttributeList getOutputSchema() {
@@ -14,7 +23,7 @@ public class LatencyCalculationAO extends AbstractLogicalOperator {
 
 	@Override
 	public AbstractLogicalOperator clone() {
-		return new LatencyCalculationAO();
+		return new LatencyCalculationAO(this);
 	}
 
 }
