e7e5021c5179863b97d017aaf5ac91dcf881ca61
Dennis Geesen <geesen@indivon.de>
2010-12-10 15:25:32 +0000

    bugfix im metadataschema
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8292 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d954ab29d18fa9532e758569a4956c3e1f864a1b

--- core/base/src/de/uniol/inf/is/odysseus/sourcedescription/sdf/schema/SDFMetaAttributeList.java
+++ core/base/src/de/uniol/inf/is/odysseus/sourcedescription/sdf/schema/SDFMetaAttributeList.java
@@ -1,17 +1,38 @@
 package de.uniol.inf.is.odysseus.sourcedescription.sdf.schema;
 
+import java.lang.reflect.Method;
+
+import de.uniol.inf.is.odysseus.metadata.PointInTime;
+
 public class SDFMetaAttributeList extends SDFSchemaElementSet<SDFMetaAttribute> {
 
-	private static final long serialVersionUID = -1194514879108835243L;	
+	private static final long serialVersionUID = -1194514879108835243L;
 
-	public SDFMetaAttributeList(){
+	public SDFMetaAttributeList() {
 	}
-	
+
 	public SDFMetaAttributeList(SDFMetaAttributeList attributes1) {
 		super(attributes1);
 	}
 
-	public static SDFMetaAttributeList union(SDFMetaAttributeList attributes1, SDFMetaAttributeList attributes2) {		
+	public SDFAttributeList convertToSDFAttributeList(){
+		SDFAttributeList list = new SDFAttributeList();
+		for(SDFMetaAttribute metaattribute : this){
+			Class<?> metaClass = metaattribute.getMetaAttributeClass();
+			for(Method m : metaClass.getMethods()){
+				SDFDatatype returnSDFType = getAccordingSDFDataType(m.getReturnType());
+				if((m.getParameterTypes().length==0) && (returnSDFType!=null)){
+					SDFAttribute a = new SDFAttribute(metaClass.getName(), m.getName());
+					a.setDatatype(returnSDFType);
+					list.add(a);
+				}
+			}
+		}
+		
+		return list;		
+	}
+
+	public static SDFMetaAttributeList union(SDFMetaAttributeList attributes1, SDFMetaAttributeList attributes2) {
 		if (attributes1 == null || attributes1.size() == 0) {
 			return attributes2;
 		}
@@ -25,5 +46,21 @@ public class SDFMetaAttributeList extends SDFSchemaElementSet<SDFMetaAttribute> 
 			}
 		}
 		return newSet;
+	}
+
+	private SDFDatatype getAccordingSDFDataType(Class<?> returnType) {
+		if (returnType.equals(Integer.class) || returnType.equals(int.class)) {
+			return SDFDatatypeFactory.getDatatype("Integer");
+		}
+		if (returnType.equals(Double.class) || returnType.equals(double.class)) {
+			return SDFDatatypeFactory.getDatatype("Double");
+		}
+		if (returnType.equals(Long.class) || returnType.equals(long.class)) {
+			return SDFDatatypeFactory.getDatatype("Long");
+		}
+		if (returnType.equals(PointInTime.class)) {
+			return SDFDatatypeFactory.getDatatype("PointInTime");
+		}
+		return null;
 	}
 }
