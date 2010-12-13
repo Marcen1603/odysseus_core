cfb6bfea01a3aadfc98ad375f48aeab53c3529d0
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-12-13 08:15:44 +0000

    MEP kommt nicht mit einem Sonderzeichen als erstes Zeichen einer Variablen klar
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8300 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 97455af3f557c9cf9d2190ffeb6ddbd5ad37bef0

--- cep/cep.metamodel/src/de/uniol/inf/is/odysseus/cep/metamodel/MepHelper.java
+++ cep/cep.metamodel/src/de/uniol/inf/is/odysseus/cep/metamodel/MepHelper.java
@@ -12,7 +12,8 @@ public class MepHelper {
 
 	@SuppressWarnings("unchecked")
 	public static IExpression initMEPExpressionFromLabel(String label, Map<CepVariable, Variable> symbolTable /*INOUT*/) throws ParseException {
-		IExpression expression = MEP.parse(label);
+		@SuppressWarnings("rawtypes")
+		IExpression expression = MEP.parse(transformToMepVar(label));
 		Set<Variable> v = expression.getVariables();
 		for (Variable s : v) {
 			symbolTable.put(transformToOutVar(s.getIdentifier()), s);
@@ -21,17 +22,17 @@ public class MepHelper {
 	}
 
 	static public String transformToMepVar(String in) {
-		String str = in.replace(CepVariable.getSeperator()+"-1"+CepVariable.getSeperator(), "$0");
-		str = str.replace(CepVariable.getSeperator(), "$1");
-		str = str.replace("[", "$2");
-		return str.replace("]", "$3");
+		String str = in.replace(CepVariable.getSeperator()+"-1"+CepVariable.getSeperator(), "a$0");
+		str = str.replace(CepVariable.getSeperator(), "a$1");
+		str = str.replace("[", "a$2");
+		return str.replace("]", "a$3");
 	}
 
 	static public CepVariable transformToOutVar(String out) {
-		String str = out.replace("$0",CepVariable.getSeperator()+"-1"+CepVariable.getSeperator());
-		str = str.replace("$1", CepVariable.getSeperator());
-		str = str.replace("$2", "[");
-		str = str.replace("$3", "]");
+		String str = out.replace("a$0",CepVariable.getSeperator()+"-1"+CepVariable.getSeperator());
+		str = str.replace("a$1", CepVariable.getSeperator());
+		str = str.replace("a$2", "[");
+		str = str.replace("a$3", "]");
 		return new CepVariable(str);
 	}
 	
