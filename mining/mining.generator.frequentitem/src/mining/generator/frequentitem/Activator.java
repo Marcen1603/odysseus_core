3d9fa5bc25bb974a35087c8a332a4844f4803e9d
Dennis Geesen <geesen@indivon.de>
2010-12-20 13:14:46 +0000

    initial import of frquent itemset generator
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8362 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d2007914fe66c516c2d20bf0e0b7bae11641c3b7

---
+++ mining/mining.generator.frequentitem/src/mining/generator/frequentitem/Activator.java
@@ -0,0 +1,34 @@
+package mining.generator.frequentitem;
+
+import mining.generator.base.socket.StreamServer;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		StreamServer server = new StreamServer(54321, FrequentItemProvider.class);
+		server.start();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ mining/mining.generator.frequentitem/src/mining/generator/frequentitem/FrequentItemProvider.java
@@ -0,0 +1,84 @@
+package mining.generator.frequentitem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import mining.generator.base.socket.StreamClientHandler;
+import mining.generator.base.tuple.DataTuple;
+
+public class FrequentItemProvider extends StreamClientHandler {
+
+	// CREATE STREAM frequent (timestamp STARTTIMESTAMP, transaction INTEGER, item INTEGER) CHANNEL localhost : 54321;
+
+	private final static String DATA_FILE = "T10I4D100K.dat";
+	// T10I4D100K => 1000 items, average transaction length of 10 and 100k
+	// (=100.000) transactions
+
+	private long time = 0;
+	private int transId = 1;
+	private BufferedReader in;
+
+	@Override
+	public List<DataTuple> next() {
+		List<DataTuple> tuples = new ArrayList<DataTuple>();
+
+		// each line = same transaction
+		String line;
+		try {
+			line = in.readLine();
+
+			if (line == null) {
+				return null;
+			}
+
+			line = line.trim();
+			String parts[] = line.split(" ");
+			for (String part : parts) {
+				int itemId = Integer.parseInt(part.trim());
+				DataTuple tuple = new DataTuple();
+				tuple.addAttribute(new Long(time));
+				tuple.addAttribute(new Integer(transId));
+				tuple.addAttribute(new Integer(itemId));
+				tuples.add(tuple);
+			}
+
+			time = time + 10;
+			transId++;
+
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return null;
+		}
+
+		return tuples;
+	}
+
+	@Override
+	public void init() {
+		URL fileURL = Activator.getContext().getBundle().getEntry("/data/" + DATA_FILE);
+		try {
+			InputStream inputStream = fileURL.openConnection().getInputStream();
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void close() {
+		try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
