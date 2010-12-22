09bae419644a405a6a07e2cef26db7243d2f3b76
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2010-12-22 09:59:00 +0000

    No More Dependencies to prio scheduler
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8404 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 133b3d50459bf12fc75e3ca640f66be5b835ce6b

--- scheduling/scheduler.slascheduler/src/de/uniol/inf/is/odysseus/scheduler/slascheduler/strategy/PrioSLAScheduler.java
+++ scheduling/scheduler.slascheduler/src/de/uniol/inf/is/odysseus/scheduler/slascheduler/strategy/PrioSLAScheduler.java
@@ -1,19 +1,36 @@
 package de.uniol.inf.is.odysseus.scheduler.slascheduler.strategy;
 
-import de.uniol.inf.is.odysseus.scheduler.priorityscheduler.prioritystrategy.SimpleDynamicPriorityPlanScheduling;
+import de.uniol.inf.is.odysseus.scheduler.strategy.IScheduling;
 
 // This class is only for testing purposes to compare
 // sla based scheduling with priority based scheduling
 // do not use this class in real szenarios ;-)
 
-public class PrioSLAScheduler extends SimpleDynamicPriorityPlanScheduling{
+public class PrioSLAScheduler extends AbstractSLAScheduler{
+
+	
+	private int minPrio;
 
 	public PrioSLAScheduler(int minPrio) {
-		super(minPrio);
+		super(PrioCalcMethod.MAX);
+		this.minPrio = minPrio;
 	}
 	
 	public PrioSLAScheduler(PrioSLAScheduler other){
 		super(other);
+		this.minPrio = other.minPrio;
+	}
+
+	@Override
+	protected IScheduling updateMetaAndReturnPlan(IScheduling plan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AbstractSLAScheduler clone() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
