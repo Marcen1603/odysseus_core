99e3895747b59b6d2f6d89592c5ef1307f3b3b6a
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-01-18 13:51:39 +0000

    RCP: Unnötiges PlugIn "rcp.navigator" entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8507 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1c8c40db5a0d3fc6f84a865f0b4aa32c38b20257

--- core/rcp/rcp.navigator/src/de/uniol/inf/is/odysseus/rcp/navigator/INavigatorConstants.java
+++
@@ -1,8 +0,0 @@
-package de.uniol.inf.is.odysseus.rcp.navigator;
-
-public interface INavigatorConstants {
-
-	public static final String PLUGIN_ID = "de.uniol.inf.is.odysseus.rcp.navigator";
-	public static final String NAVIGATOR_VIEW_ID = "de.uniol.inf.is.odysseus.rcp.NavigatorView";
-	
-}
