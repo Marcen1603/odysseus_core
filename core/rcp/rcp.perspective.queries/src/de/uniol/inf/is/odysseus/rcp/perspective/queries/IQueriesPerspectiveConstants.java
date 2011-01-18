992e302d5a2f38dd12664e8bd0b68f9b5580a41a
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-01-18 13:39:40 +0000

    RCP: Unnötiges PlugIn "rcp.perspective.queries" entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8505 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9f828baffe1e9dc46d6e98dacb116a7c38f3d63f

--- core/rcp/rcp.perspective.queries/src/de/uniol/inf/is/odysseus/rcp/perspective/queries/IQueriesPerspectiveConstants.java
+++
@@ -1,5 +0,0 @@
-package de.uniol.inf.is.odysseus.rcp.perspective.queries;
-
-public interface IQueriesPerspectiveConstants {
-	public static final String PERSPECTIVE_ID = "de.uniol.inf.is.odysseus.rcp.perspective.queries";
-}
