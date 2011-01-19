17bde84cc4d7e7dabe62c495a3ac10e24aea4e59
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2011-01-19 09:09:15 +0000

    Renamed
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8534 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 15b9afd491fdba07d52eb19c7571cc6e29835c6f

--- p2p/distribution/de.uniol.inf.is.odysseus.p2p.distribution.client/src/de/uniol/inf/is/odysseus/p2p/distribution/client/IQuerySpecificationHandler.java
+++
@@ -1,8 +0,0 @@
-package de.uniol.inf.is.odysseus.p2p.distribution.client;
-
-import de.uniol.inf.is.odysseus.p2p.distribution.client.queryselection.IQuerySelectionStrategy;
-
-public interface IQuerySpecificationHandler<S> {
-	public void handleQuerySpezification(S adv);
-	public IQuerySelectionStrategy getQuerySelectionStrategy();
-}
--- p2p/distribution/de.uniol.inf.is.odysseus.p2p.distribution.client/src/de/uniol/inf/is/odysseus/p2p/distribution/client/IQuerySpecificationListener.java
+++
@@ -1,9 +0,0 @@
-package de.uniol.inf.is.odysseus.p2p.distribution.client;
-
-import de.uniol.inf.is.odysseus.p2p.distribution.client.queryselection.IQuerySelectionStrategy;
-
-public interface IQuerySpecificationListener<S> extends Runnable {
-	public void startListener();
-	public IQuerySpecificationHandler<S> getQuerySpecificationHandler(S spec);
-	public IQuerySelectionStrategy getQuerySelectionStrategy();
-}
