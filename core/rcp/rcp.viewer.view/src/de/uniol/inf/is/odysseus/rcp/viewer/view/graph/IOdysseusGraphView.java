2f72ccd3d838e970dc3795803017f6f74be8bf66
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-01-24 15:02:00 +0000

    RCP: Unnötiges PlugIn rcp.viewer.view entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8617 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ddd85aa2c8307a2984f8eda9e2a5a65ddd1e60cb

--- core/rcp/rcp.viewer.view/src/de/uniol/inf/is/odysseus/rcp/viewer/view/graph/IOdysseusGraphView.java
+++
@@ -1,9 +0,0 @@
-package de.uniol.inf.is.odysseus.rcp.viewer.view.graph;
-
-import de.uniol.inf.is.odysseus.physicaloperator.IPhysicalOperator;
-import de.uniol.inf.is.odysseus.rcp.viewer.model.graph.IOdysseusGraphModel;
-
-public interface IOdysseusGraphView extends IGraphView<IPhysicalOperator>{
-	@Override
-	public IOdysseusGraphModel getModelGraph();
-}
