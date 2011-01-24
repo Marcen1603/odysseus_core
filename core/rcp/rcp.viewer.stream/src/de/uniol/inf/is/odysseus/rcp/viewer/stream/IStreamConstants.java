5cd5c88c60a9a1e09e9b9caf032d834560be10d6
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-01-24 15:01:41 +0000

    RCP: Unnötiges Plugin rcp.viewer.stream entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8616 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c53ff87e2c1800014971f9070dccc0be8ac90756

--- core/rcp/rcp.viewer.stream/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/IStreamConstants.java
+++
@@ -1,8 +0,0 @@
-package de.uniol.inf.is.odysseus.rcp.viewer.stream;
-
-public interface IStreamConstants {
-	public static final String SHOW_STREAM_COMMAND = "de.uniol.inf.is.odysseus.rcp.viewer.stream.ShowStream";
-	public static final String STREAM_EDITOR_ID = "de.uniol.inf.is.odysseus.rcp.viewer.stream.StreamEditor";
-	
-	public static final String STREAM_EDITOR_TYPE_PARAMETER_ID = "de.uniol.inf.is.odysseus.rcp.viewer.stream.StreamEditorType";
-}
--- core/rcp/rcp.viewer.stream/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/extension/IStreamEditorInput.java
+++
@@ -1,8 +0,0 @@
-package de.uniol.inf.is.odysseus.rcp.viewer.stream.extension;
-
-import org.eclipse.ui.IEditorInput;
-
-public interface IStreamEditorInput extends IEditorInput{
-
-	public IStreamConnection<Object> getStreamConnection();
-}
