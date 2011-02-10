b265c4a51de74e3d9590191680ba6d039381aa28
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-02-10 09:11:15 +0000

    RCP: Unnötiges Plugin entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8670 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7b91acfa600ab776bac680e2e874929dd41aab10

--- core/rcp/rcp.viewer.model/src/de/uniol/inf/is/odysseus/rcp/viewer/model/meta/IMetadataChangeListener.java
+++
@@ -1,8 +0,0 @@
-package de.uniol.inf.is.odysseus.rcp.viewer.model.meta;
-
-
-public interface IMetadataChangeListener<M> {
-
-	public void metadataChanged( IMetadataModel<M> sender, String changedMetadataItemType );
-	
-}
