f5b69acde29343f480d6894e7c68e822df96914b
Stefanie Witzke <switzke@uni-oldenburg.de>
2011-02-11 22:34:37 +0000

    initial commit of benchmark_startBundles needed by benchmarker_gui
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8695 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 6656cbfa1431378270fdffb7c9ff32867d9bbeef

---
+++ benchmark/benchmarker.startBundles/src/de/uniol/inf/is/odysseus/startbundles/Activator.java
@@ -0,0 +1,78 @@
+package de.uniol.inf.is.odysseus.startbundles;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "de.uniol.inf.is.odysseus.startBundels"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		startBundles(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+	
+	private void startBundles(final BundleContext context) {
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				for (Bundle bundle : context.getBundles()) {
+					boolean isFragment = bundle.getHeaders().get(
+							Constants.FRAGMENT_HOST) != null;
+					if (bundle != context.getBundle() && !isFragment
+							&& bundle.getState() == Bundle.RESOLVED) {
+						try {
+							bundle.start();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		});
+		t.start();
+	}
+}
