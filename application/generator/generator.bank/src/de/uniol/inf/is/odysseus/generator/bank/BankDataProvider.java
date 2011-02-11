0d4bb8096a224124e7f41b8053616026408c7197
Dennis Geesen <geesen@indivon.de>
2011-02-11 12:38:29 +0000

    Initial import of bank-data for train and test decision-trees (moved from data-mining-bundle)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8687 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b24e20e0284d261e6614d9fda4dbb80a15968357

---
+++ application/generator/generator.bank/src/de/uniol/inf/is/odysseus/generator/bank/BankDataProvider.java
@@ -0,0 +1,93 @@
+package de.uniol.inf.is.odysseus.generator.bank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.generator.DataTuple;
+import de.uniol.inf.is.odysseus.generator.StreamClientHandler;
+import de.uniol.inf.is.odysseus.generator.StreamServer;
+
+public class BankDataProvider extends StreamClientHandler {
+
+	public static void main(String[] args) throws Exception {
+		StreamServer server = new StreamServer(54321, BankDataProvider.class);
+		server.start();
+	}
+ 
+	private BufferedReader in;
+
+	@Override
+	public void init() {
+		System.out.println("startng stream...");
+		initFileStream();
+	}
+
+	@Override
+	public void close() {
+		try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void initFileStream(){
+		URL fileURL = Activator.getContext().getBundle().getEntry("/data/bank-data.csv");
+		try {
+			InputStream inputStream = fileURL.openConnection().getInputStream();
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			in.readLine();  //skip the header
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+
+	@Override
+	public List<DataTuple> next() {
+		DataTuple tuple = new DataTuple();
+		String line;
+		try {
+			line = in.readLine();
+			if (line == null) {
+
+				System.out.println("restarting stream...");
+				//restart data
+				in.close();
+				initFileStream();
+				line = in.readLine();
+			}
+			String[] rawTuple = line.split(",");
+
+			tuple.addAttribute(rawTuple[0]); // id
+			tuple.addAttribute(new Integer(rawTuple[1])); // age
+			tuple.addAttribute(rawTuple[2]); // sex
+			tuple.addAttribute(rawTuple[3]); // region
+			tuple.addAttribute(new Double(rawTuple[4])); // income
+			tuple.addAttribute(rawTuple[5]); // married
+			tuple.addAttribute(new Integer(rawTuple[6])); // children
+			tuple.addAttribute(rawTuple[7]); // car
+			tuple.addAttribute(rawTuple[8]); // save_act
+			tuple.addAttribute(rawTuple[9]); // current_act
+			tuple.addAttribute(rawTuple[10]); // mortgage
+			tuple.addAttribute(rawTuple[11]); // pep
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(line);
+			List<DataTuple> list = new ArrayList<DataTuple>();
+			list.add(tuple);
+			return list;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
