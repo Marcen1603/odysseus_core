8e94e9d53786224943c51f7437cd884193f5c67f
Dennis Geesen <geesen@indivon.de>
2011-02-11 12:26:31 +0000

    inital imports: - dodgers: erzeugt einen die daten, die aus einer schleife aus der straße vor dem dodgers stadion aufgenommen wurde - frequentitem: erzeugt daten aus einer speziellen datei für frequent-items - synthetic-clusters: erzeugt 2-dimensionale (x- und y-wert) Tupel, die sich entsprechend clustern lassen
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8683 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 91a59a727fa1bbef49b296d7ce9ed2026e30442d

---
+++ application/generator/generator.dodgers/src/de/uniol/inf/is/odysseus/generator/dodgers/Activator.java
@@ -0,0 +1,37 @@
+package de.uniol.inf.is.odysseus.generator.dodgers;
+
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import de.uniol.inf.is.odysseus.generator.StreamServer;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		StreamServer server = new StreamServer(54321, DodgersDataProvider.class);
+		server.start();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
