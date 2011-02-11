cc9c1310a86713fe2794db4400813af7b6c2a4ac
Dennis Geesen <geesen@indivon.de>
2011-02-11 12:11:29 +0000

    Initial import of generator for income-data
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8677 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3d1a9bec5208409d3d48f078d4ffca9531fdd538

---
+++ application/generator/generator.income/src/de/uniol/inf/is/odysseus/generator/income/Activator.java
@@ -0,0 +1,38 @@
+package de.uniol.inf.is.odysseus.generator.income;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import de.uniol.inf.is.odysseus.generator.StreamServer;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		System.out.println("Starting test server...");
+		StreamServer testServer = new StreamServer(54321, AdultTestDataProvider.class);
+		testServer.start();
+		System.out.println("Starting train server...");
+		StreamServer trainServer = new StreamServer(54322, AdultTrainDataProvider.class);
+		trainServer.start();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ application/generator/generator.income/src/de/uniol/inf/is/odysseus/generator/income/AdultTestDataProvider.java
@@ -0,0 +1,103 @@
+package de.uniol.inf.is.odysseus.generator.income;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.generator.DataTuple;
+import de.uniol.inf.is.odysseus.generator.StreamClientHandler;
+
+public class AdultTestDataProvider extends StreamClientHandler {
+
+	
+	private BufferedReader in;	
+	private long number  = 0;
+	
+	
+	
+	@Override
+	public List<DataTuple> next() {
+		DataTuple tuple = new DataTuple();
+		String line;
+		try {
+			line = in.readLine();	
+			if(line==null){
+				return null;
+			}
+			String[] rawTuple = line.split(",");			
+			
+			//number
+			tuple.addAttribute(new Long(number));
+			//age
+			tuple.addAttribute(new Integer(rawTuple[0]));
+			//working class
+			tuple.addAttribute(rawTuple[1].trim());
+			//education
+			tuple.addAttribute(rawTuple[3].trim());
+			//education-num
+			tuple.addAttribute(new Integer(rawTuple[4].trim()));
+			//marriage status
+			tuple.addAttribute(rawTuple[5].trim());
+			//occupation
+			tuple.addAttribute(rawTuple[6].trim());
+			//relationship
+			tuple.addAttribute(rawTuple[7].trim());
+			//race
+			tuple.addAttribute(rawTuple[8].trim());
+			//sex
+			tuple.addAttribute(rawTuple[9].trim());
+			//capital-gain
+			tuple.addAttribute(new Integer(rawTuple[10].trim()));
+			//capital-loss
+			tuple.addAttribute(new Integer(rawTuple[11].trim()));
+			//hours-per-week
+			tuple.addAttribute(new Integer(rawTuple[12].trim()));
+			//country
+			tuple.addAttribute(rawTuple[13].trim());
+			//income
+			tuple.addAttribute(rawTuple[14].trim());
+			
+			
+			
+			number++;
+			try {
+				Thread.sleep(0);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			List<DataTuple> list = new ArrayList<DataTuple>();
+			list.add(tuple);
+			return list;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@Override
+	public void init() {
+		URL fileURL = Activator.getContext().getBundle().getEntry("/data/adult.test");
+		try {
+			InputStream inputStream = fileURL.openConnection().getInputStream();
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+
+	@Override
+	public void close() {
+		try {
+			in.close();
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}	
+		
+	}
+
+
+}
