0021ee985d95930b99725ee74cb3874d2321c9af
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-03-07 20:14:37 +0000

    StreamCars: Test-Operator verschoben
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8795 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 763e838a5cc778e31f4b9fcb09cdc0a0bcf2e8f1

---
+++ sensor/streamcars/de.uniol.inf.is.odysseus.scars/src/de/uniol/inf/is/odysseus/scars/operator/test/po/TestPO.java
@@ -0,0 +1,56 @@
+/** Copyright [2011] [The Odysseus Team]
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package de.uniol.inf.is.odysseus.scars.operator.test.po;
+
+import de.uniol.inf.is.odysseus.metadata.PointInTime;
+import de.uniol.inf.is.odysseus.physicaloperator.AbstractPipe;
+
+public class TestPO<T> extends AbstractPipe<T, T>{
+
+	public TestPO() {
+	}
+	
+	public TestPO(TestPO<T> copy ) {
+		
+	}
+	
+	@Override
+	public OutputMode getOutputMode() {
+		return OutputMode.INPUT;
+	}
+	
+	@Override
+	public void process_open(){
+	}
+
+	@Override
+	protected void process_next(T object, int port) {
+		transfer(object);
+	}
+	
+	@Override
+	public void process_done(){
+	}
+
+	@Override
+	public AbstractPipe<T, T> clone() {
+		return new TestPO<T>(this);
+	}
+
+	@Override
+	public void processPunctuation(PointInTime timestamp, int port) {
+		sendPunctuation(timestamp);
+	}
+}
