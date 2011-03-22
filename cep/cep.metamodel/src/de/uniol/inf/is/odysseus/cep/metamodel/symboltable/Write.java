7b0d744ea416e227e19ffbb767c94ae5f10cfa84
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2011-03-22 14:19:02 +0000

    Aggregationen können nun Objekte von unterschiedlichen Typen lesen und schreiben.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8904 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: acfcf5631438a21333509c65d9da0ceb6c8439bc

--- cep/cep.metamodel/src/de/uniol/inf/is/odysseus/cep/metamodel/symboltable/Write.java
+++ cep/cep.metamodel/src/de/uniol/inf/is/odysseus/cep/metamodel/symboltable/Write.java
@@ -18,7 +18,7 @@ import de.uniol.inf.is.odysseus.physicaloperator.aggregate.basefunctions.Abstrac
 import de.uniol.inf.is.odysseus.physicaloperator.aggregate.basefunctions.IPartialAggregate;
 import de.uniol.inf.is.odysseus.physicaloperator.aggregate.functions.ElementPartialAggregate;
 
-public class Write<R> extends AbstractAggregateFunction<R> {
+public class Write<R> extends AbstractAggregateFunction<R, R> {
 	
 	private static Write instance;
 
