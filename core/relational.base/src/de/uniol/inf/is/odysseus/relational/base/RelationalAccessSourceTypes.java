4e07c168d18b25b0d8bf9d5e5d254ec8b573d763
Andre Bolles <andre.bolles@offis.de>
2011-04-04 10:40:36 +0000

    added source type class to avoid typos
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@8921 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a81e62552057543e4bc99f9c950374b2c1334097

---
+++ core/relational.base/src/de/uniol/inf/is/odysseus/relational/base/RelationalAccessSourceTypes.java
@@ -0,0 +1,7 @@
+package de.uniol.inf.is.odysseus.relational.base;
+
+public class RelationalAccessSourceTypes {
+
+	public final static String RELATIONAL_ATOMIC_DATA_INPUT_STREAM_ACCESS = "RelationalAtomicDataInputStreamAccessPO";
+	public final static String RELATONAL_STREAMING = "RelationalStreaming";
+}
