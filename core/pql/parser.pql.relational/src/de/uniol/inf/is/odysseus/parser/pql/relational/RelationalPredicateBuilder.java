bff286c63359534754c92e222d44aeb34bb0de20
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2011-07-01 07:33:47 +0000

    Umstellung: Verwendung von MEP
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9183 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 29b9e682de4a2a44dcef0eb615850fdfa6de862f

--- core/pql/parser.pql.relational/src/de/uniol/inf/is/odysseus/parser/pql/relational/RelationalPredicateBuilder.java
+++ core/pql/parser.pql.relational/src/de/uniol/inf/is/odysseus/parser/pql/relational/RelationalPredicateBuilder.java
@@ -1,41 +1,33 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.parser.pql.relational;
 
-import java.io.StringReader;
-
 import de.uniol.inf.is.odysseus.logicaloperator.builder.IPredicateBuilder;
-import de.uniol.inf.is.odysseus.parser.pql.relational.predicateParser.ParseException;
-import de.uniol.inf.is.odysseus.parser.pql.relational.predicateParser.RelationalPredicateParser;
 import de.uniol.inf.is.odysseus.predicate.IPredicate;
+import de.uniol.inf.is.odysseus.relational.base.predicate.RelationalPredicate;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.IAttributeResolver;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFExpression;
 
 public class RelationalPredicateBuilder implements IPredicateBuilder {
 
 	@Override
 	public IPredicate<?> createPredicate(IAttributeResolver resolver,
 			String predicate) {
-		RelationalPredicateParser parser = new RelationalPredicateParser(
-				new StringReader(predicate));
-
-		parser.setAttributeResolver(resolver);
-		try {
-			return parser.Predicate();
-		} catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
+		SDFExpression expression = new SDFExpression("", predicate, resolver);
+		RelationalPredicate pred = new RelationalPredicate(expression);
+		return pred;
 	}
 
 }
