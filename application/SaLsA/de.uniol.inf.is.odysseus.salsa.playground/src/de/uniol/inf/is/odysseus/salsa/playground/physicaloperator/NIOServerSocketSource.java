b2b6a6a783effafbd9b6fcb05c8b076266f89515
Kai Papancratz <kapancratz@uni-oldenburg.de>
2011-07-04 12:13:43 +0000

    Add a Odysseus workspace in my repository folder and a folder for queries with a test query from 2011/06/29.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9199 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4b4e78fdc4f9c3f6bf95efdac7dbf678c6ef9cdd

--- application/SaLsA/de.uniol.inf.is.odysseus.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/playground/physicaloperator/NIOServerSocketSource.java
+++ application/SaLsA/de.uniol.inf.is.odysseus.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/playground/physicaloperator/NIOServerSocketSource.java
@@ -11,6 +11,10 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
 import de.uniol.inf.is.odysseus.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.metadata.ITimeInterval;
 import de.uniol.inf.is.odysseus.physicaloperator.AbstractSource;
@@ -19,8 +23,11 @@ import de.uniol.inf.is.odysseus.physicaloperator.OpenFailedException;
 import de.uniol.inf.is.odysseus.physicaloperator.PhysicalSubscription;
 import de.uniol.inf.is.odysseus.relational.base.RelationalTuple;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttributeList;
+import de.uniol.inf.is.odysseus.intervalapproach.TimeInterval;
+import de.uniol.inf.is.odysseus.metadata.PointInTime;
 
-public class NIOServerSocketSource<T> extends AbstractSource<T>{
+
+public class NIOServerSocketSource<T extends IMetaAttribute> extends AbstractSource<RelationalTuple<ITimeInterval>>{
 
 	private InetSocketAddress socketAddress;
 	private ServerSocketChannel server;
@@ -79,11 +86,19 @@ public class NIOServerSocketSource<T> extends AbstractSource<T>{
 	@Override
 	protected void process_close() {
 		worker.interrupt();
+		try {
+			this.key.channel().close();
+			this.key.cancel();
+			this.server.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@Override
-	public AbstractSource<T> clone() {
-		return new NIOServerSocketSource<T>(this);
+	public AbstractSource<RelationalTuple<ITimeInterval>> clone() {
+		return new NIOServerSocketSource(this);
 	}
 
 	public Selector getSelector() {
@@ -99,36 +114,78 @@ public class NIOServerSocketSource<T> extends AbstractSource<T>{
 	}
 	
 	public void request(String request){
-		getLogger().debug("Request: " + request);
-		getLogger().debug("Attribute: " + this.getOutputSchema());
-		
-		if(this.getOutputSchema() != null){
+		if (this.isOpen()) {
+			RelationalTuple tuple = new RelationalTuple<ITimeInterval>(this.getOutputSchema().getAttributeCount());
+			ITimeInterval metadata = null;
 			
-		}
-		
-		RelationalTuple tuple = new RelationalTuple<ITimeInterval>(this.getOutputSchema().getAttributeCount());
-		
-		//tuple.setMetadata()
-		
-		String[] values = request.split(";");
-		for(int i = 0; i < values.length; i++){
-			if(this.getOutputSchema().get(i).getDatatype().getQualName().equals("String")){
-				tuple.addAttributeValue(i, values[i]);
-			}
-			if(this.getOutputSchema().get(i).getDatatype().getQualName().equals("Double")){
-				tuple.addAttributeValue(i, Double.parseDouble(values[i]));
-			}
-			if(this.getOutputSchema().get(i).getDatatype().getQualName().equals("Integer")){
-				//tuple.addAttributeValue(i, Integer.parseInt(values[i]));
-				tuple.setAttribute(i, Integer.parseInt(values[i]));
-			}
-			if(this.getOutputSchema().get(i).getDatatype().getQualName().equals("Geometry")){
-				
-			}	
+			String[] values = request.split(";");
+			boolean transferTuple = true;
 			
-		}			
-		transfer((T)tuple);
-	}
+			if(values.length == this.getOutputSchema().getAttributeCount()){
+				for(int i = 0; i < this.getOutputSchema().getAttributeCount(); i++){
+						//getLogger().debug(i + ":" + values[i] + " " + this.getOutputSchema().get(i).getDatatype().getQualName());
+						if(this.getOutputSchema().get(i).getDatatype().getQualName().equalsIgnoreCase("String")){
+							tuple.addAttributeValue(i, values[i].toString());
+						}
+						if(this.getOutputSchema().get(i).getDatatype().getQualName().equalsIgnoreCase("Double")){
+							tuple.addAttributeValue(i, Double.parseDouble(values[i]));
+						}
+						if(this.getOutputSchema().get(i).getDatatype().getQualName().equalsIgnoreCase("Integer")){
+							tuple.setAttribute(i, Integer.parseInt(values[i]));
+						}
+						if(this.getOutputSchema().get(i).getDatatype().getQualName().equalsIgnoreCase("Long")){
+							tuple.setAttribute(i, Long.parseLong(values[i]));
+							//Simple Hack, only for SaLsA Sensor Cluster!(Like the whole class)
+							if(this.getOutputSchema().get(i).getAttributeName().equalsIgnoreCase("timestamp")){
+								metadata = new TimeInterval(new PointInTime(Long.parseLong(values[i])));	
+							}
+						}
+						if(this.getOutputSchema().get(i).getDatatype().getQualName().equalsIgnoreCase("Object")){
+							
+							
+							String[] coordinateValues = ((String) values[i].subSequence(1, values[i].length()-1)).split(",");
+							Coordinate coordinates[] = new Coordinate[coordinateValues.length];
+							String[] coordinateValue;
+							
+							for(int c = 0; c < coordinateValues.length; c++){
+								coordinateValue = ((String)coordinateValues[c].subSequence(1, coordinateValues[c].length()-1)).split(":");
+								coordinates[c] = new Coordinate(Double.parseDouble(coordinateValue[0]),Double.parseDouble(coordinateValue[1]));
+							}
+							
+							
+							GeometryFactory geoFactory = new GeometryFactory();
+							Geometry geometry = null;
 
+//							if (coordinates.length == 1) {
+//								geometry = geoFactory.createPoint(coordinates[0]);
+//							}
+							
+							if (coordinates.length > 1) {
+								geometry = geoFactory.createLineString(coordinates);
+								tuple.addAttributeValue(i, geometry);
+							}
+							else{
+								transferTuple = false;
+							}
+							
+							
+						}
+				}	
+				if(metadata == null){
+					getLogger().error("no timestamp in Stream");
+					metadata = new TimeInterval(new PointInTime(System.currentTimeMillis()));	
+				}
+				
+				if(transferTuple){
+					tuple.setMetadata(metadata);
+					transfer((RelationalTuple<ITimeInterval>)tuple);
+				}
+
+			}
+			else{
+				getLogger().debug(values.length + " != " + this.getOutputSchema().getAttributeCount());
+			}
+		}
+	}
 	
 }
--- application/SaLsA/de.uniol.inf.is.odysseus.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/playground/physicaloperator/NIOServerSocketWorker.java
+++ application/SaLsA/de.uniol.inf.is.odysseus.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/playground/physicaloperator/NIOServerSocketWorker.java
@@ -2,24 +2,31 @@ package de.uniol.inf.is.odysseus.salsa.playground.physicaloperator;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CoderResult;
 import java.util.Iterator;
 import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NIOServerSocketWorker<T> extends Thread {
+import de.uniol.inf.is.odysseus.metadata.ITimeInterval;
+import de.uniol.inf.is.odysseus.relational.base.RelationalTuple;
 
-	private NIOServerSocketSource<T> nioServer;
-	private ByteBuffer buffer = ByteBuffer.allocate(8192);
+public class NIOServerSocketWorker<RelationalTuple> extends Thread {
+
+	private NIOServerSocketSource nioServer;
+	private ByteBuffer buffer = ByteBuffer.allocate(1024);
 
 	private Charset charset = Charset.defaultCharset();
 	private CharsetDecoder decoder = charset.newDecoder();
+	private CharsetEncoder encoder = charset.newEncoder();
 
 	volatile protected static Logger _logger = null;
 
@@ -30,63 +37,139 @@ public class NIOServerSocketWorker<T> extends Thread {
 		return _logger;
 	}
 
-	public NIOServerSocketWorker(NIOServerSocketSource<T> server) {
+	public NIOServerSocketWorker(NIOServerSocketSource server) {
 		this.nioServer = server;
 	}
 
 	@Override
 	public void run() {
 		getLogger().debug("Start NIOServer SocketWorker");
-		
-		StringBuilder message = new StringBuilder();
-		
+		boolean firstIt = true;
+
 		try {
 			while (!isInterrupted()) {
 				Selector selector = nioServer.getSelector();
 				selector.select();
 				Set keys = selector.selectedKeys();
+				String request = "";
+				
+				for (Iterator i = keys.iterator(); i.hasNext();) {
+					SelectionKey key = (SelectionKey) i.next();
+					i.remove();
 
-				  for (Iterator i = keys.iterator(); i.hasNext();) {
-				        SelectionKey key = (SelectionKey) i.next();
-				        i.remove();
-
-				        if (key == this.nioServer.getKey()) {
-				          if (key.isAcceptable()) {
-				            SocketChannel client = this.nioServer.getServerSocketChannel().accept();
-				            client.configureBlocking(false);
-				            SelectionKey clientkey = client.register(selector, SelectionKey.OP_READ);
-				            clientkey.attach(new Integer(0));
-				          }
-				        } else {
-				          SocketChannel client = (SocketChannel) key.channel();
-				          if (!key.isReadable())
-				            continue;
-				          int bytesread = client.read(buffer);
-				          if (bytesread == -1) {
-				            key.cancel();
-				            client.close();
-				            continue;
-				          }
-				          buffer.flip();
-				          String request = decoder.decode(buffer).toString();
-				          buffer.clear();
-				    
-				          if(request.endsWith("\r\n")){
-				        	this.nioServer.request(message.toString());
-				        	message.delete(0, message.length()-1);
-				          }
-				          else{
-				        	  message.append(request);
-				          }
-				            
-				        }
+					if (key == this.nioServer.getKey()) {
+						if (key.isAcceptable()) {
+							SocketChannel client = this.nioServer.getServerSocketChannel().accept();
+							client.configureBlocking(false);
+							SelectionKey clientkey = client.register(selector, SelectionKey.OP_READ);
+							clientkey.attach(new Integer(0));
+						}
+					} else {
+						SocketChannel client = (SocketChannel) key.channel();
+						if (!key.isReadable())
+							continue;
+						
+						int bytesread = client.read(buffer);
+						if (bytesread == -1) {
+							key.cancel();
+							client.close();
+							continue;
+						}
+					
+						
+						buffer.flip();
+						request += decoder.decode(buffer).toString();	
+						
+						if (request.contains("\n")) {
+							String[] splited = request.split("\n");
+							String newRequest = "";
+							for(String split: splited){
+								if(split.startsWith("S") && split.endsWith("\r")){
+									this.nioServer.request(split.trim());
+								}
+								else{
+									if(split.endsWith("\r")){
+										newRequest += split + '\n';
+									}
+									else{
+										newRequest += split;	
+									}
+								}
+							}
+							request = "";
+							request = newRequest;	
+						}
+						buffer.clear();
+						
 					}
 				}
+			}
 
-			this.nioServer.getServerSocketChannel().close();			
+			this.nioServer.getServerSocketChannel().close();
 			getLogger().debug("Stopped NIOServer SocketWorker");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 }
+
+
+//boolean send = false;
+//
+//if (request.contains("\n")) {
+//	String[] splited = request.split("\n");
+//	String newRequest = "";
+//	for(String split: splited){
+//		if(split.startsWith("S") && split.endsWith(" ")){
+//			getLogger().debug("Send: " + split);
+//		}
+//		else{
+//			if(split.endsWith("\r")){
+//				newRequest += split + '\n';
+//			}
+//			else{
+//				newRequest += split;	
+//			}
+//		}
+//	}
+//	request = "";
+//	request = newRequest;	
+//}
+
+// 
+//String[] splited = request.split("\r\n");
+//if (splited.length > 0) {
+//	if (splited[0].startsWith("S")) {
+//		getLogger().debug("S");
+//		getLogger().debug("Send: " + splited[0].toString());
+//
+//		if (splited[1].startsWith("S")) {
+//			request = request.substring(splited[0]
+//					.length());
+//
+//			getLogger().debug("Rest: " + request);
+//			if (splited.length > 0)
+//				getLogger().debug(
+//						"Rest_vergleich: "
+//								+ splited[1]);
+//		}
+//
+//	} else {
+//		getLogger().debug("NO_S");
+//		if (splited.length > 1) {
+//			if (splited[1].startsWith("S")) {
+//				request = request
+//						.substring(splited[0]
+//								.length() + 2);
+//				getLogger().debug(
+//						"Rest: " + request);
+//				getLogger().debug(
+//						"Rest_vergleich: "
+//								+ splited[1]);
+//			} else {
+//				request = "";
+//			}
+//
+//		}
+//	}
+//}
