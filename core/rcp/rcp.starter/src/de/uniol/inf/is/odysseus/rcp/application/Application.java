fc8263c35ed12335815107272e4268fe94c92363
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-07-05 09:22:22 +0000

    RCP: Warning entfernt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9213 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9b1c5e0a2b45d09f8a7cf36920939f002ab6ef11

--- core/rcp/rcp.starter/src/de/uniol/inf/is/odysseus/rcp/application/Application.java
+++ core/rcp/rcp.starter/src/de/uniol/inf/is/odysseus/rcp/application/Application.java
@@ -43,7 +43,6 @@ public class Application implements IApplication {
 			return IApplication.EXIT_OK;
 		} catch(Throwable t){
 			t.printStackTrace();
-			int i = 0;
 			return null;
 		} finally {
 			display.dispose();
