3e0f93280f7b6b2b639a0969bd077f54d36113a5
Dennis Geesen <geesen@indivon.de>
2011-07-07 15:15:26 +0000

    eine methode hinzugefügt, die auf "startingEquals" in der sweepArea alle element herausholt
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9277 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b759d731f560e297548a672830d05ceb5eb237a0

--- core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/DefaultTISweepArea.java
+++ core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/DefaultTISweepArea.java
@@ -157,6 +157,25 @@ public class DefaultTISweepArea<T extends IMetaAttributeContainer<? extends ITim
 		}
 		return retval.iterator();
 	}
+	
+	public Iterator<T> extractElementsStartingEquals(PointInTime validity) {
+		ArrayList<T> retval = new ArrayList<T>();
+		synchronized (elements) {
+			Iterator<T> li = elements.iterator();
+			while (li.hasNext()) {
+				T s_hat = li.next();
+				// Alle Elemente entfernen, die nicht mehr verschnitten werden
+				// k�nnen (also davor liegen)
+				if (s_hat.getMetadata().getStart().equals(validity)) {
+					retval.add(s_hat);
+					li.remove();
+				} else {
+					break;
+				}
+			}
+		}
+		return retval.iterator();
+	}
 
 	/**
 	 * @return the min start timestamp of all elements currently in the sweep
