680e8a067fab326f9ffc3952ae5ca7b2210447cf
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2011-07-11 11:51:21 +0000

    Debug-Output Removed
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9328 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e55b0254b4928a2d46eb663a7755648022526ed6

--- core/base/src/de/uniol/inf/is/odysseus/physicaloperator/aggregate/AggregateFunctionBuilderRegistry.java
+++ core/base/src/de/uniol/inf/is/odysseus/physicaloperator/aggregate/AggregateFunctionBuilderRegistry.java
@@ -20,7 +20,7 @@ public class AggregateFunctionBuilderRegistry implements
 			IAggregateFunctionBuilder builder) {
 		String datamodel = builder.getDatamodel();
 		Collection<String> functionNames = builder.getFunctionNames();
-		System.out.println("Found new AggregateBuilder " + builder);
+		//System.out.println("Found new AggregateBuilder " + builder);
 		for (String functionName : functionNames) {
 			Pair<String, String> key = new Pair<String, String>(datamodel,
 					functionName);
@@ -28,7 +28,7 @@ public class AggregateFunctionBuilderRegistry implements
 				builders.put(key, builder);
 				aggregateFunctionNames.add(functionName);
 				buildAggregatePattern();
-				System.out.println("Binding " + key);
+				//System.out.println("Binding " + key);
 			} else {
 				throw new RuntimeException(datamodel + " and " + functionName
 						+ " already registered!");
