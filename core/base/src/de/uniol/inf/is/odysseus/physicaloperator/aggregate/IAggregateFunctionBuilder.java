7d7d81c0e3b01f315704a580f9142b403462f7cd
Dennis Geesen <geesen@indivon.de>
2011-07-12 13:48:02 +0000

    removed warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9348 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 75330c341c2d3151f19353dd16b5ab6881c5856f

--- core/base/src/de/uniol/inf/is/odysseus/physicaloperator/aggregate/IAggregateFunctionBuilder.java
+++ core/base/src/de/uniol/inf/is/odysseus/physicaloperator/aggregate/IAggregateFunctionBuilder.java
@@ -5,11 +5,11 @@ import java.util.Collection;
 
 import de.uniol.inf.is.odysseus.physicaloperator.AggregateFunction;
 import de.uniol.inf.is.odysseus.physicaloperator.aggregate.basefunctions.IAggregateFunction;
-@SuppressWarnings({"rawtypes","unchecked"})
+
 public interface IAggregateFunctionBuilder {
 
 	String getDatamodel();
 	Collection<String> getFunctionNames();
-	IAggregateFunction createAggFunction(AggregateFunction e2, int[] posArray);
+	IAggregateFunction<?,?> createAggFunction(AggregateFunction e2, int[] posArray);
 
 }
