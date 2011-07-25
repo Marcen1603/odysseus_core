e972f47e99039d21bef7f8501f12dd34ea034a12
Kai Papancratz <kapancratz@uni-oldenburg.de>
2011-07-25 12:39:23 +0000

    Fixed arrayList bug in extractSegments function and draw enverlopes around polygons.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9473 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 33928b56cb05f2cc972f32fc55fd759bb8780baf

--- application/SaLsA/de.offis.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/function/ExtractSegments.java
+++ application/SaLsA/de.offis.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/function/ExtractSegments.java
@@ -2,7 +2,11 @@ package de.uniol.inf.is.odysseus.salsa.function;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
@@ -10,15 +14,26 @@ import com.vividsolutions.jts.geom.GeometryFactory;
 
 import de.uniol.inf.is.odysseus.intervalapproach.TimeInterval;
 import de.uniol.inf.is.odysseus.mep.AbstractFunction;
+import de.uniol.inf.is.odysseus.mep.IExpression;
+import de.uniol.inf.is.odysseus.metadata.IMetaAttribute;
+import de.uniol.inf.is.odysseus.metadata.ITimeInterval;
+import de.uniol.inf.is.odysseus.metadata.MetaAttributeContainer;
+import de.uniol.inf.is.odysseus.metadata.MetadataRegistry;
+import de.uniol.inf.is.odysseus.metadata.PointInTime;
+import de.uniol.inf.is.odysseus.physicaloperator.MetadataUpdatePO;
 import de.uniol.inf.is.odysseus.relational.base.RelationalTuple;
 import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFExpression;
 
 /**
  * @author Christian Kuka <christian.kuka@offis.de>
+ * @param <T>
+ * @param <IMetadata>
  */
-public class ExtractSegments extends AbstractFunction<List<RelationalTuple<?>>> {
+public class ExtractSegments extends AbstractFunction<List<RelationalTuple<TimeInterval>>> {
     private final GeometryFactory geometryFactory = new GeometryFactory();
-
+    private static Logger LOG = LoggerFactory.getLogger(ExtractSegments.class);
+    
     public static final SDFDatatype[] accTypes0 = new SDFDatatype[] {
         SDFDatatype.SPATIAL_MULTI_POINT
     };
@@ -63,14 +78,32 @@ public class ExtractSegments extends AbstractFunction<List<RelationalTuple<?>>> 
 
     //FIXME need metadata to set timestamp
     @Override
-    public List<RelationalTuple<?>> getValue() {
+    public List<RelationalTuple<TimeInterval>> getValue() {
+
+    	PointInTime startTimestamp = new PointInTime();
+    	
+    	for (IExpression<?> expr : getArguments()) {
+    		
+    		if(expr.getReturnType().isStartTimestamp()){
+    			LOG.debug("Foundtime");
+    			startTimestamp = (PointInTime)expr.getValue();
+    		}
+    		else{
+    			//LOG.debug("Found no time");
+    			startTimestamp = new PointInTime(System.currentTimeMillis());
+    		}
+    	}
+    	
         final Geometry geometry = (Geometry) this.getInputValue(0);
         final Double threshold = (Double) this.getInputValue(1);
         final Coordinate[] coordinates = geometry.getCoordinates();
-        final List<RelationalTuple<?>> segments = new ArrayList<RelationalTuple<?>>();
+        final List<RelationalTuple<TimeInterval>> segments = new LinkedList<RelationalTuple<TimeInterval>>();
         Coordinate tmp = new Coordinate(Double.MAX_VALUE, Double.MAX_VALUE);
         boolean isSegment = false;
         int start = 0;
+        
+        TimeInterval time = new TimeInterval(startTimestamp);
+        
         for (int i = 0; i < coordinates.length; i++) {
             final Coordinate coordinate = coordinates[i];
 
@@ -78,22 +111,22 @@ public class ExtractSegments extends AbstractFunction<List<RelationalTuple<?>>> 
             tmp = coordinate;
             if (distance >= threshold) {
                 if (isSegment) {
-                    final List<Coordinate> segment = new ArrayList<Coordinate>();
+                	
+                	final List<Coordinate> segment = new ArrayList<Coordinate>();
                     for (int j = start; j < i; j++) {
                         if ((coordinates[j].x < Float.MAX_VALUE)
                                 && (coordinates[j].y < Float.MAX_VALUE)) {
                             segment.add(coordinates[j]);
                         }
                     }
+                    RelationalTuple<TimeInterval> tuple = new RelationalTuple<TimeInterval>(1);
+                    tuple.setMetadata(time);
                     if (segment.size() > 1) {
-                        RelationalTuple<?> tuple = new RelationalTuple<TimeInterval>(1);
-                        tuple.addAttributeValue(0, this.geometryFactory.createLineString(segment
-                                .toArray(new Coordinate[] {})));
+                        tuple.setAttribute(0, this.geometryFactory.createLineString(segment.toArray(new Coordinate[] {})));
                         segments.add(tuple);
                     }
                     else if (segment.size() == 1) {
-                        RelationalTuple<?> tuple = new RelationalTuple<TimeInterval>(1);
-                        tuple.addAttributeValue(0, this.geometryFactory.createPoint(segment.get(0)));
+                        tuple.setAttribute(0, this.geometryFactory.createPoint(segment.get(0)));
                         segments.add(tuple);
 
                     }
@@ -120,15 +153,16 @@ public class ExtractSegments extends AbstractFunction<List<RelationalTuple<?>>> 
                     segment.add(coordinates[j]);
                 }
             }
+            
+            RelationalTuple<TimeInterval> tuple = new RelationalTuple<TimeInterval>(1);
             if (segment.size() > 1) {
-                RelationalTuple<?> tuple = new RelationalTuple<TimeInterval>(1);
-                tuple.addAttributeValue(0,
-                        this.geometryFactory.createLineString(segment.toArray(new Coordinate[] {})));
+                tuple.setMetadata(time);
+                tuple.setAttribute(0, this.geometryFactory.createLineString(segment.toArray(new Coordinate[] {})));
                 segments.add(tuple);
             }
             else if (segment.size() == 1) {
-                RelationalTuple<?> tuple = new RelationalTuple<TimeInterval>(1);
-                tuple.addAttributeValue(0, this.geometryFactory.createPoint(segment.get(0)));
+                tuple.setMetadata(time);
+                tuple.setAttribute(0, this.geometryFactory.createPoint(segment.get(0)));
                 segments.add(tuple);
 
             }
