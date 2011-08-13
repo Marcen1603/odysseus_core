fff7b7eac34b8bba099a5eff030d84174e291d0a
Thomas Vogelgesang <Thomas.Vogelgesang@uni-oldenburg.de>
2011-08-13 12:23:07 +0000

    added handling of slas to sla priority scheduler
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9584 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: bdddd93ec73a33bb29e65ec3da3652783c383d39

--- scheduling/de.uniol.inf.is.odysseus.scheduler.slapriorityscheduler/src/de/uniol/inf/is/odysseus/scheduler/slapriorityscheduler/AbstractDynamicPriorityPlanScheduling.java
+++ scheduling/de.uniol.inf.is.odysseus.scheduler.slapriorityscheduler/src/de/uniol/inf/is/odysseus/scheduler/slapriorityscheduler/AbstractDynamicPriorityPlanScheduling.java
@@ -1,43 +1,64 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.scheduler.slapriorityscheduler;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import de.uniol.inf.is.odysseus.planmanagement.query.IQuery;
 import de.uniol.inf.is.odysseus.scheduler.singlethreadscheduler.IPartialPlanScheduling;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.ISLAConformance;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.ISLAConformancePlacement;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.ISLAViolationEventDistributor;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.ISLAViolationEventListener;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.SLAConformanceFactory;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.SLAConformancePlacementFactory;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.SLAViolationEvent;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.SLAViolationLogger;
 import de.uniol.inf.is.odysseus.scheduler.strategy.CurrentPlanPriorityComperator;
 import de.uniol.inf.is.odysseus.scheduler.strategy.IScheduling;
+import de.uniol.inf.is.odysseus.sla.SLA;
 
 abstract public class AbstractDynamicPriorityPlanScheduling implements
-		IPartialPlanScheduling {
+		IPartialPlanScheduling, ISLAViolationEventDistributor {
 
 	final CurrentPlanPriorityComperator comperator = new CurrentPlanPriorityComperator();
 
 	private final List<IScheduling> queue;
 	final private List<IScheduling> lastRun = new LinkedList<IScheduling>();
 
+	private LinkedList<SLAViolationEvent> eventQueue = new LinkedList<SLAViolationEvent>();
+	private List<ISLAViolationEventListener> listeners;
+
 	public AbstractDynamicPriorityPlanScheduling() {
 		queue = new LinkedList<IScheduling>();
+		this.listeners = new ArrayList<ISLAViolationEventListener>();
+		this.addSLAViolationEventListener(new SLAViolationLogger());
 	}
 
+	@SuppressWarnings("unchecked")
 	public AbstractDynamicPriorityPlanScheduling(
 			AbstractDynamicPriorityPlanScheduling dynamicPriorityPlanScheduling) {
 		queue = new LinkedList<IScheduling>(dynamicPriorityPlanScheduling.queue);
+		this.eventQueue = (LinkedList<SLAViolationEvent>) dynamicPriorityPlanScheduling.eventQueue
+				.clone();
+		this.listeners = new ArrayList<ISLAViolationEventListener>(
+				dynamicPriorityPlanScheduling.listeners);
 	}
 
 	abstract protected void updatePriority(IScheduling current);
@@ -47,7 +68,17 @@ abstract public class AbstractDynamicPriorityPlanScheduling implements
 		synchronized (queue) {
 			queue.add(scheduling);
 			// Init with Base Priority
-			scheduling.getPlan().setCurrentPriority(scheduling.getPlan().getBasePriority());
+			scheduling.getPlan().setCurrentPriority(
+					scheduling.getPlan().getBasePriority());
+			// add SLA conformance operator to plan for monitoring
+			IQuery query = scheduling.getPlan().getQueries().get(0);
+			SLA sla = query.getSLA();
+			ISLAConformance conformance = new SLAConformanceFactory()
+					.createSLAConformance(sla, this, query);
+
+			ISLAConformancePlacement placement = new SLAConformancePlacementFactory()
+					.buildSLAConformancePlacement(sla);
+			placement.placeSLAConformance(query, conformance);
 		}
 	}
 
@@ -60,6 +91,12 @@ abstract public class AbstractDynamicPriorityPlanScheduling implements
 
 	@Override
 	public IScheduling nextPlan() {
+		synchronized (eventQueue) {
+			while (!this.eventQueue.isEmpty()) {
+				this.fireSLAViolationEvent(this.eventQueue.pop());
+			}
+		}
+
 		synchronized (lastRun) {
 			if (lastRun.size() > 0) {
 				return updateMetaAndReturnPlan(lastRun.remove(0));
@@ -83,8 +120,12 @@ abstract public class AbstractDynamicPriorityPlanScheduling implements
 			}
 		}
 	}
-	
-	
+
+	private void fireSLAViolationEvent(SLAViolationEvent event) {
+		for (ISLAViolationEventListener listener : this.listeners) {
+			listener.slaViolated(event);
+		}
+	}
 
 	protected IScheduling updateMetaAndReturnPlan(IScheduling toSchedule) {
 		updatePriority(toSchedule);
@@ -107,5 +148,28 @@ abstract public class AbstractDynamicPriorityPlanScheduling implements
 
 	@Override
 	abstract public IPartialPlanScheduling clone();
+
+	@Override
+	public void queueSLAViolationEvent(SLAViolationEvent event) {
+		synchronized (eventQueue) {
+			this.eventQueue.add(event);
+		}
+	}
+
+	public void addSLAViolationEventListener(ISLAViolationEventListener listener) {
+		this.listeners.add(listener);
+	}
+
+	/**
+	 * removes an event listener for {@link SLAViolationEvent}
+	 * 
+	 * @param listener
+	 *            the event listener to remove
+	 * @return
+	 */
+	public boolean removeSLAViolationEventListener(
+			ISLAViolationEventListener listener) {
+		return this.listeners.remove(listener);
+	}
 
 }
