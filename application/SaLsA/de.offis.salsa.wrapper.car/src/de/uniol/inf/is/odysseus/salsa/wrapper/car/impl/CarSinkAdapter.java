80fda9bc68a67bcf17f84cadc5047b74455d20e2
Christian Kuka <christian@kuka.cc>
2011-08-26 12:38:08 +0000

    SALSA: Add salsa demo 2 communication protocol
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9726 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8f8fc0a2dffbd5e608fed59ba263ad2064a0aef3

---
+++ application/SaLsA/de.offis.salsa.wrapper.car/src/de/uniol/inf/is/odysseus/salsa/wrapper/car/impl/CarSinkAdapter.java
@@ -0,0 +1,128 @@
+package de.uniol.inf.is.odysseus.salsa.wrapper.car.impl;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.wrapper.base.AbstractSinkAdapter;
+import de.uniol.inf.is.odysseus.wrapper.base.SinkAdapter;
+import de.uniol.inf.is.odysseus.wrapper.base.model.SinkSpec;
+
+public class CarSinkAdapter extends AbstractSinkAdapter implements SinkAdapter {
+	private static Logger LOG = LoggerFactory.getLogger(CarSinkAdapter.class);
+	private final BlockingQueue<Object[]> messageQueue = new LinkedBlockingQueue<Object[]>();
+	private final Map<SinkSpec, Thread> connectionThreads = new HashMap<SinkSpec, Thread>();
+
+	class CarConnectionHandler extends Thread {
+
+		private final String host;
+		private final int port;
+		private SocketChannel channel;
+		private final ByteBuffer buffer;
+
+		public CarConnectionHandler(final String host, final int port) {
+			this.host = host;
+			this.port = port;
+			buffer = ByteBuffer.allocateDirect(1024);
+		}
+
+		public void run() {
+			try {
+				this.channel = SocketChannel.open();
+				final InetSocketAddress address = new InetSocketAddress(
+						this.host, this.port);
+				this.channel.connect(address);
+				this.channel.configureBlocking(false);
+				while (!Thread.currentThread().isInterrupted()) {
+					Object[] event = messageQueue.take();
+					long timestamp = (Long) event[0];
+					Object[] data = (Object[]) event[1];
+					buffer.putLong(timestamp);
+					// ID
+					buffer.putShort((Short) data[0]);
+					// X Position
+					buffer.putShort((Short) data[1]);
+					// Y Position
+					buffer.putShort((Short) data[2]);
+					// Grid Length
+					buffer.putShort((Short) data[3]);
+					// Grid Width
+					buffer.putShort((Short) data[4]);
+					// Grid Height
+					buffer.putShort((Short) data[5]);
+					// Cell Size
+					buffer.putInt((Integer) data[6]);
+					// Grid
+					Float[][] grid = (Float[][]) data[7];
+					for (int i = 0; i < grid.length; i++) {
+						for (int j = 0; j < grid[i].length; j++) {
+							buffer.putFloat(grid[i][j]);
+						}
+					}
+					buffer.flip();
+					this.channel.write(buffer);
+					if (buffer.hasRemaining()) {
+						buffer.compact();
+					} else {
+						buffer.clear();
+					}
+
+				}
+			} catch (Exception e) {
+				LOG.error(e.getMessage(), e);
+			} finally {
+				if (this.channel != null) {
+					try {
+						this.channel.close();
+					} catch (final IOException e) {
+						LOG.error(e.getMessage(), e);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "SALSA-Car";
+	}
+
+	@Override
+	public void transfer(SinkSpec sink, long timestamp, Object[] data) {
+		this.messageQueue.offer(new Object[] { timestamp, data });
+	}
+
+	@Override
+	protected void destroy(SinkSpec sinkSpec) {
+		this.connectionThreads.get(sinkSpec).interrupt();
+		this.connectionThreads.remove(sinkSpec);
+	}
+
+	@Override
+	protected void init(SinkSpec sinkSpec) {
+		if (!connectionThreads.containsKey(sinkSpec)) {
+			int port = 4444;
+			String host = "localhost";
+			if (sinkSpec.getConfiguration().containsKey("port")) {
+				port = Integer.parseInt(sinkSpec.getConfiguration().get("port")
+						.toString());
+			}
+			if (sinkSpec.getConfiguration().containsKey("host")) {
+				host = sinkSpec.getConfiguration().get("host").toString();
+			}
+			CarConnectionHandler connection = new CarConnectionHandler(host,
+					port);
+			this.connectionThreads.put(sinkSpec, connection);
+			connection.start();
+		}
+	}
+
+}
--- application/SaLsA/de.offis.salsa.wrapper.car/src/de/uniol/inf/is/odysseus/salsa/wrapper/car/impl/CarSourceAdapter.java
+++ application/SaLsA/de.offis.salsa.wrapper.car/src/de/uniol/inf/is/odysseus/salsa/wrapper/car/impl/CarSourceAdapter.java
@@ -1,10 +1,14 @@
 package de.uniol.inf.is.odysseus.salsa.wrapper.car.impl;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,102 +21,153 @@ import de.uniol.inf.is.odysseus.wrapper.base.AbstractPushingSourceAdapter;
 import de.uniol.inf.is.odysseus.wrapper.base.SourceAdapter;
 import de.uniol.inf.is.odysseus.wrapper.base.model.SourceSpec;
 
-public class CarSourceAdapter extends AbstractPushingSourceAdapter implements SourceAdapter {
-    private static Logger LOG = LoggerFactory.getLogger(CarSourceAdapter.class);
+public class CarSourceAdapter extends AbstractPushingSourceAdapter implements
+		SourceAdapter {
+	private static Logger LOG = LoggerFactory.getLogger(CarSourceAdapter.class);
+	private static final int LENGTH = 24;
+	private final Map<SourceSpec, Thread> serverThreads = new HashMap<SourceSpec, Thread>();
 
-    private final Map<SourceSpec, Thread> serverThreads = new HashMap<SourceSpec, Thread>();
+	@Override
+	public String getName() {
+		return "SALSA-Car";
+	}
 
-    @Override
-    public String getName() {
-        return "SALSA-Car";
-    }
+	@Override
+	protected void doDestroy(SourceSpec source) {
+		this.serverThreads.get(source).interrupt();
+		this.serverThreads.remove(source);
+	}
 
-    @Override
-    protected void doDestroy(SourceSpec source) {
-        this.serverThreads.get(source).interrupt();
-        this.serverThreads.remove(source);
-    }
+	@Override
+	protected void doInit(SourceSpec source) {
+		int port = 4444;
+		if (source.getConfiguration().containsKey("port")) {
+			port = Integer.parseInt(source.getConfiguration().get("port")
+					.toString());
+		}
+		final CarServer server = new CarServer(source, this, port);
+		final Thread serverThread = new Thread(server);
+		this.serverThreads.put(source, serverThread);
+		serverThread.start();
+	}
 
-    @Override
-    protected void doInit(SourceSpec source) {
-        int port = 4444;
-        if (source.getConfiguration().containsKey("port")) {
-            port = Integer.parseInt(source.getConfiguration().get("port").toString());
-        }
-        final CarServer server = new CarServer(source, this, port);
-        final Thread serverThread = new Thread(server);
-        this.serverThreads.put(source, serverThread);
-        serverThread.start();
-    }
+	class CarServer implements Runnable {
+		private ServerSocket serverSocket = null;
+		private SourceSpec source;
+		private CarSourceAdapter adapter;
 
-    class CarServer implements Runnable {
-        private ServerSocket serverSocket = null;
-        private SourceSpec source;
-        private CarSourceAdapter adapter;
+		public CarServer(final SourceSpec source,
+				final CarSourceAdapter adapter, final int port) {
+			try {
+				this.serverSocket = new ServerSocket(port);
+			} catch (final IOException e) {
+				CarSourceAdapter.LOG.error(e.getMessage(), e);
+			}
+		}
 
-        public CarServer(final SourceSpec source, final CarSourceAdapter adapter, final int port) {
-            try {
-                this.serverSocket = new ServerSocket(port);
-            }
-            catch (final IOException e) {
-                CarSourceAdapter.LOG.error(e.getMessage(), e);
-            }
-        }
+		@Override
+		public void run() {
+			final List<Thread> processingThreads = new ArrayList<Thread>();
+			while (!Thread.currentThread().isInterrupted()) {
+				Socket socket;
+				try {
+					socket = this.serverSocket.accept();
+					final CarProcessor processor = new CarProcessor(
+							this.source, socket, this.adapter);
+					final Thread processingThread = new Thread(processor);
+					processingThreads.add(processingThread);
+					processingThread.start();
+				} catch (final IOException e) {
+					CarSourceAdapter.LOG.error(e.getMessage(), e);
+				}
+			}
+			for (final Thread processingThread : processingThreads) {
+				processingThread.interrupt();
+			}
+		}
 
-        @Override
-        public void run() {
-            final List<Thread> processingThreads = new ArrayList<Thread>();
-            while (!Thread.currentThread().isInterrupted()) {
-                Socket socket;
-                try {
-                    socket = this.serverSocket.accept();
-                    final CarProcessor processor = new CarProcessor(this.source, socket,
-                            this.adapter);
-                    final Thread processingThread = new Thread(processor);
-                    processingThreads.add(processingThread);
-                    processingThread.start();
-                }
-                catch (final IOException e) {
-                    CarSourceAdapter.LOG.error(e.getMessage(), e);
-                }
-            }
-            for (final Thread processingThread : processingThreads) {
-                processingThread.interrupt();
-            }
-        }
+		class CarProcessor implements Runnable {
+			private final Socket server;
+			private final CarSourceAdapter adapter;
+			private SourceSpec sourceSpec;
 
-        class CarProcessor implements Runnable {
-            private final Socket server;
-            private final CarSourceAdapter adapter;
-            private SourceSpec source;
+			public CarProcessor(final SourceSpec source, final Socket server,
+					final CarSourceAdapter adapter) {
+				this.server = server;
+				this.adapter = adapter;
+			}
 
-            public CarProcessor(final SourceSpec source, final Socket server,
-                    final CarSourceAdapter adapter) {
-                this.server = server;
-                this.adapter = adapter;
-            }
+			@Override
+			public void run() {
+				SocketChannel channel = this.server.getChannel();
+				try {
 
-            @Override
-            public void run() {
-                LineNumberReader reader;
-                try {
-                    reader = new LineNumberReader(new InputStreamReader(
-                            this.server.getInputStream()));
+					final ByteBuffer buffer = ByteBuffer
+							.allocateDirect(64 * 1024);
+					int nbytes = 0;
+					int pos = 0;
+					int size = 0;
 
-                    String line = reader.readLine();
-                    if (line != null) {
-                        while (((line = reader.readLine()) != null)
-                                && (!Thread.currentThread().isInterrupted())) {
-                            final String[] values = line.split(",");
-                            this.adapter.transfer(this.source, System.currentTimeMillis(), values);
-                        }
-                    }
-                }
-                catch (final IOException e) {
-                    CarSourceAdapter.LOG.error(e.getMessage(), e);
-                }
-            }
-        }
+					while (!Thread.currentThread().isInterrupted()) {
+						while ((nbytes = channel.read(buffer)) > 0) {
+							size += nbytes;
+							for (int i = pos; i < size; i++) {
+								if (i == LENGTH) {
+									buffer.position(i + 1);
+									buffer.flip();
 
-    }
+									try {
+										long timestamp = buffer.getLong();
+										int id = buffer.getShort();
+										int x = buffer.getShort();
+										int y = buffer.getShort();
+										float speed = buffer.getFloat();
+										float angle = buffer.getFloat();
+										this.adapter.transfer(sourceSpec,
+												timestamp, new Object[] { id,
+														x, y, speed, angle });
+
+									} catch (final Exception e) {
+										if (CarSourceAdapter.LOG
+												.isDebugEnabled()) {
+											CarSourceAdapter.LOG.debug(
+													e.getMessage(), e);
+											this.dumpPackage(buffer);
+										}
+									}
+									buffer.limit(size);
+
+									buffer.compact();
+									size -= (i + 1);
+									pos = 0;
+									i = 0;
+								}
+							}
+							pos++;
+						}
+
+					}
+
+				} catch (final IOException e) {
+					CarSourceAdapter.LOG.error(e.getMessage(), e);
+				}
+			}
+
+			private void dumpPackage(final ByteBuffer buffer)
+					throws FileNotFoundException {
+				final File debug = new File(getName() + "-"
+						+ sourceSpec.getName() + "-debug.out");
+				final FileOutputStream out = new FileOutputStream(debug, true);
+				final FileChannel debugChannel = out.getChannel();
+				if ((debugChannel != null) && (debugChannel.isOpen())) {
+					try {
+						buffer.flip();
+						debugChannel.write(buffer);
+					} catch (final IOException e) {
+						CarSourceAdapter.LOG.error(e.getMessage(), e);
+					}
+				}
+			}
+		}
+	}
 }
