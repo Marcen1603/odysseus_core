4072434d56095e7e4768ae79c88795607453707e
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-08-26 09:06:50 +0000

    Anpassungen P2P
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9717 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0b28c94e75a2e729f078e2d2b8a7c11601f97e35

--- p2p/distribution/distribution.bidding.client/src/de/uniol/inf/is/odysseus/p2p/distribution/bidding/client/messagehandler/QueryBidResponseHandlerJxtaImpl.java
+++ p2p/distribution/distribution.bidding.client/src/de/uniol/inf/is/odysseus/p2p/distribution/bidding/client/messagehandler/QueryBidResponseHandlerJxtaImpl.java
@@ -15,6 +15,7 @@
 package de.uniol.inf.is.odysseus.p2p.distribution.bidding.client.messagehandler;
 
 import net.jxta.endpoint.Message;
+import de.uniol.inf.is.odysseus.p2p.distribution.bidding.client.BiddingClient;
 import de.uniol.inf.is.odysseus.p2p.jxta.peer.communication.AbstractJxtaMessageHandler;
 import de.uniol.inf.is.odysseus.p2p.peer.ILogListener;
 import de.uniol.inf.is.odysseus.p2p.peer.IQueryProvider;
@@ -22,6 +23,8 @@ import de.uniol.inf.is.odysseus.p2p.peer.execution.listener.IExecutionListener;
 import de.uniol.inf.is.odysseus.p2p.queryhandling.Lifecycle;
 import de.uniol.inf.is.odysseus.p2p.queryhandling.P2PQuery;
 import de.uniol.inf.is.odysseus.p2p.queryhandling.Subplan;
+import de.uniol.inf.is.odysseus.planmanagement.executor.exception.PlanManagementException;
+import de.uniol.inf.is.odysseus.usermanagement.client.GlobalState;
 
 /**
  * This class reacts on messages that state if a peer should process a
@@ -75,12 +78,20 @@ public class QueryBidResponseHandlerJxtaImpl extends AbstractJxtaMessageHandler 
 		} else {
 			log.logAction(subPlanId, "Denied to process plan. Removing plan from store");
 			log.removeQueryOrSubplan(subPlanId);
+			
+			Subplan plan = queryProvider.getQuery(queryId).getSubPlans().get(subPlanId);
+			try {
+				BiddingClient.getExecutor().removeQuery(plan.getQuery().getID(), GlobalState.getActiveUser(""));
+			} catch (PlanManagementException e) {
+				e.printStackTrace();
+			}			
 
 			P2PQuery q = queryProvider.getQuery(queryId);
 			if (q.getId() == queryId && q.getSubPlans().size() == 1) {
 				queryProvider.removeQuery(q.getId());
 			} else if (q.getId() == queryId) {
 				q.getSubPlans().remove(subPlanId);
+				
 			}
 
 		}
