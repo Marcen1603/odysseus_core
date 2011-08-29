7c4fc598031cd16eb6351ed15c44c63a014ad15f
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-08-29 10:01:04 +0000

    AC-Integration
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9763 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e81a6fd7cafe2fe4540846707ed4af02ffbfad26

---
+++ p2p/ac/de.uniol.inf.is.odysseus.p2p.ac.estimator/src/de/uniol/inf/is/odysseus/p2p/ac/estimator/P2PSinkEstimator.java
@@ -0,0 +1,62 @@
+package de.uniol.inf.is.odysseus.p2p.ac.estimator;
+
+import java.util.List;
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.costmodel.operator.DataStream;
+import de.uniol.inf.is.odysseus.costmodel.operator.IOperatorEstimator;
+import de.uniol.inf.is.odysseus.costmodel.operator.OperatorCostModelCfg;
+import de.uniol.inf.is.odysseus.costmodel.operator.OperatorDetailCost;
+import de.uniol.inf.is.odysseus.costmodel.operator.OperatorEstimation;
+import de.uniol.inf.is.odysseus.costmodel.operator.datasrc.IHistogram;
+import de.uniol.inf.is.odysseus.costmodel.operator.relational.CPURateSaver;
+import de.uniol.inf.is.odysseus.costmodel.operator.relational.EstimatorHelper;
+import de.uniol.inf.is.odysseus.p2p.operatorpeer.physicaloperator.base.P2PSinkPO;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttribute;
+
+@SuppressWarnings("rawtypes")
+public class P2PSinkEstimator implements IOperatorEstimator<P2PSinkPO> {
+
+	@Override
+	public Class<P2PSinkPO> getOperatorClass() {
+		return P2PSinkPO.class;
+	}
+
+	@Override
+	public OperatorEstimation estimateOperator(P2PSinkPO instance,
+			List<OperatorEstimation> prevOperators,
+			Map<SDFAttribute, IHistogram> baseHistograms) {
+		
+		OperatorEstimation estimation = new OperatorEstimation(instance);
+		OperatorEstimation lastOpEstimation = prevOperators.get(0);
+
+		/** 1. Histograms **/
+		// project has only one input
+		Map<SDFAttribute, IHistogram> inputHistograms = lastOpEstimation.getHistograms();
+		estimation.setHistograms(inputHistograms);
+
+		/** 2. Selectivity **/
+		estimation.setSelectivity(1.0);
+
+		/** 3. Datarate **/
+		// pure sink has no datarate
+		double datarate = lastOpEstimation.getDataStream().getDataRate();
+		estimation.setDataStream(new DataStream(instance, datarate, lastOpEstimation.getDataStream().getIntervalLength()));
+
+		/** 4. DetailCost **/
+		// TODO: Memorycost
+		double cpu = EstimatorHelper.getAvgCPUTimeMetadata(instance);
+		double cpuCost = 0.0;
+		if (cpu < 0.0) 
+			cpuCost = CPURateSaver.getInstance().get(instance.getClass().getSimpleName()) * datarate;
+		else {
+			cpuCost = cpu * datarate;
+			CPURateSaver.getInstance().set(instance.getClass().getSimpleName(), cpu);
+		}
+		
+		estimation.setDetailCost(new OperatorDetailCost(instance, OperatorCostModelCfg.getInstance().getStandardMemCost(), cpuCost));
+		
+		return estimation;
+	}
+
+}
