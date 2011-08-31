82cab8eb66977de72a4b0ef766e3b23ca3b4e6e7
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2011-08-31 14:48:44 +0000

    Combined Type
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9836 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: cc0ff983d77d414caa7a03861a3d8d49c90937fe

---
+++ metadata/priority/interval_latency_priority/src/de/uniol/inf/is/odysseus/interval_latency_priority/ILatencyPriority.java
@@ -0,0 +1,8 @@
+package de.uniol.inf.is.odysseus.interval_latency_priority;
+
+import de.uniol.inf.is.odysseus.metadata.ILatency;
+import de.uniol.inf.is.odysseus.priority.IPriority;
+
+public interface ILatencyPriority extends ILatency, IPriority {
+
+}
