9c339d7f8a3fce599046bdc27533adf574681fb9
Timo Michelsen <Timo.Michelsen@gmail.com>
2011-09-07 14:04:28 +0000

    AC: JavaDoc
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9887 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9119c84fbed86920cff34431755a2e7148d4344b

--- core/base/src/de/uniol/inf/is/odysseus/costmodel/ICost.java
+++ core/base/src/de/uniol/inf/is/odysseus/costmodel/ICost.java
@@ -4,11 +4,48 @@ import java.util.Collection;
 
 import de.uniol.inf.is.odysseus.physicaloperator.IPhysicalOperator;
 
+/**
+ * Repräsentiert die generellen Kosten im Admission Control. 
+ * Die Implementierung ist abhängig vom Kostenmodell.
+ * 
+ * @author Timo Michelsen
+ *
+ */
 public interface ICost extends Comparable<ICost>{
 
+	/**
+	 * Liefert alle Operatoren, die durch die Kosten
+	 * berücksichtigt wurden.
+	 * 
+	 * @return Liste der beteiltigen Operatoren
+	 */
 	public Collection<IPhysicalOperator> getOperators();
+	
+	/**
+	 * Liefert die Kosten zu einem spezifischen Operator. 
+	 * Bei einigen Kostenmodellen ist die Trennung auf Operatorebene
+	 * nicht möglich. Darauf sollte geachtet werden.
+	 * 
+	 * @param operator Physischer Operator, dessen Kosten zurückgegeben werden sollen.
+	 * @return Kosten zum gegebenen physischen Operator
+	 */
 	public ICost getCostOfOperator(IPhysicalOperator operator);
 	
+	/**
+	 * Fasst diese Kosten mit den gegebenen Kosten zusammen und liefert
+	 * das Ergebnis als neue Instanz.
+	 * 
+	 * @param otherCost Andere Kosten
+	 * @return Zusammengefasste Kosten
+	 */
 	public ICost merge( ICost otherCost );
+	
+	/**
+	 * Subtrahiert diese Kosten mit den gegebenen Kosten und liefert
+	 * das Ergebnis als neue Instanz.
+	 * 
+	 * @param otherCost Andere Kosten
+	 * @return Subtraierte Kosten
+	 */
 	public ICost substract( ICost otherCost );
 }
--- core/base/src/de/uniol/inf/is/odysseus/costmodel/ICostModel.java
+++ core/base/src/de/uniol/inf/is/odysseus/costmodel/ICostModel.java
@@ -4,11 +4,40 @@ import java.util.List;
 
 import de.uniol.inf.is.odysseus.physicaloperator.IPhysicalOperator;
 
+/**
+ * Repräsentiert ein Kostenmodell. Mit dieser lassen sich zu gegebenen
+ * Operatormengen Kostenschätzungen durchführen.
+ * 
+ * @author Timo Michelsen
+ * 
+ */
 public interface ICostModel {
 
-	public ICost estimateCost(List<IPhysicalOperator> operators, boolean onUpdate );
-	
+	/**
+	 * Führt eine Kostenschätzung mit den gegebenen physischen Operatoren durch
+	 * und liefert das Ergebnis als {@link ICost} zurück.
+	 * 
+	 * @param operators
+	 *            Liste der physischen Operatoren
+	 * @param onUpdate
+	 *            Gibt an, ob eine Aktualisierung der Kostenschätzung
+	 *            durchgeführt wird, oder nicht. 
+	 * @return Kostenschätzung
+	 */
+	public ICost estimateCost(List<IPhysicalOperator> operators, boolean onUpdate);
+
+	/**
+	 * Liefert die Maximalekosten
+	 * 
+	 * @return Maximalkosten
+	 */
 	public ICost getMaximumCost();
+
+	/**
+	 * Liefert eine Instanz der Kosten des Kostenmodells mit 0.
+	 * 
+	 * @return Null-Kosten
+	 */
 	public ICost getZeroCost();
-	
+
 }
