a59e9a2f41d0de553e9455c7b59b6dbb299a57ab
Dennis Geesen <geesen@indivon.de>
2011-10-20 15:52:24 +0000

    RCP: Bug gefixt, der zu einem "Invalid Thread Access" beim Aufrufen des Graphen führte
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@9975 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: eede53c0e8af565ac4d3980d48610a235a49f095

--- core/rcp/rcp.viewer/src/de/uniol/inf/is/odysseus/rcp/viewer/commands/CallGraphEditorCommand.java
+++ core/rcp/rcp.viewer/src/de/uniol/inf/is/odysseus/rcp/viewer/commands/CallGraphEditorCommand.java
@@ -1,17 +1,17 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.rcp.viewer.commands;
 
 import java.util.List;
@@ -22,6 +22,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
@@ -38,36 +39,39 @@ import de.uniol.inf.is.odysseus.rcp.viewer.model.create.OdysseusModelProviderSin
 public class CallGraphEditorCommand extends AbstractHandler implements IHandler {
 
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
-		IWorkbenchPage page = window.getActivePage();
-		
-		ISelection selection = window.getSelectionService().getSelection();
-		if( selection instanceof IStructuredSelection ) {
-			IStructuredSelection treeSelection = (IStructuredSelection)selection;
-			Object obj = treeSelection.getFirstElement();
-						
-			// Auswahl holen
-			List<IPhysicalOperator> graph = null;
-			if( obj instanceof IQuery ) {
-				IQuery query = (IQuery)obj;
-				graph = query.getRoots();
-				
-				ISink<?> sink = (ISink<?>)graph.get(0); 
-				IModelProvider<IPhysicalOperator> provider = new OdysseusModelProviderSinkOneWay(sink, query);
-				PhysicalGraphEditorInput input = new PhysicalGraphEditorInput(provider, "Query " + query.getID());
-				
-				try {
-					page.openEditor(input, OdysseusRCPViewerPlugIn.GRAPH_EDITOR_ID);
-					
-				} catch( PartInitException ex ) {
-					System.out.println(ex.getStackTrace());
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		Display.getDefault().syncExec(new Runnable() {
+			public void run() {
+				IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+				IWorkbenchPage page = window.getActivePage();
+
+				ISelection selection = window.getSelectionService().getSelection();
+				if (selection instanceof IStructuredSelection) {
+					IStructuredSelection treeSelection = (IStructuredSelection) selection;
+					Object obj = treeSelection.getFirstElement();
+
+					// Auswahl holen
+					List<IPhysicalOperator> graph = null;
+					if (obj instanceof IQuery) {
+						IQuery query = (IQuery) obj;
+						graph = query.getRoots();
+
+						ISink<?> sink = (ISink<?>) graph.get(0);
+						IModelProvider<IPhysicalOperator> provider = new OdysseusModelProviderSinkOneWay(sink, query);
+						PhysicalGraphEditorInput input = new PhysicalGraphEditorInput(provider, "Query " + query.getID());
+
+						try {
+							page.openEditor(input, OdysseusRCPViewerPlugIn.GRAPH_EDITOR_ID);
+
+						} catch (PartInitException ex) {
+							System.out.println(ex.getStackTrace());
+						}
+					}
 				}
 			}
-		}
-		
+		});
 		return null;
+
 	}
 
 }
