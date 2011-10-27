a8184bfc79562a70cde6c43461b393dd7ee18ab8
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2011-10-27 11:39:20 +0000

    FIX: Window was not functional, Removal of elements was wrong, Simplified
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10037 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b67da3484cb2dd574c1dbb3a10e965569e6eebff

--- core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/window/SlidingPeriodicWindowTIPO.java
+++ core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/window/SlidingPeriodicWindowTIPO.java
@@ -23,19 +23,13 @@
 
 package de.uniol.inf.is.odysseus.intervalapproach.window;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 
-import de.uniol.inf.is.odysseus.intervalapproach.DefaultTISweepArea;
-import de.uniol.inf.is.odysseus.intervalapproach.TimeInterval;
-import de.uniol.inf.is.odysseus.intervalapproach.predicate.LiesInPredicate;
 import de.uniol.inf.is.odysseus.logicaloperator.WindowAO;
 import de.uniol.inf.is.odysseus.metadata.IMetaAttributeContainer;
 import de.uniol.inf.is.odysseus.metadata.ITimeInterval;
-import de.uniol.inf.is.odysseus.metadata.MetaAttributeContainer;
 import de.uniol.inf.is.odysseus.metadata.PointInTime;
-import de.uniol.inf.is.odysseus.physicaloperator.AbstractSweepArea;
-import de.uniol.inf.is.odysseus.physicaloperator.ISweepArea.Order;
+import de.uniol.inf.is.odysseus.physicaloperator.OpenFailedException;
 
 /**
  * This is the physical sliding delta window po. It returns elements after a
@@ -46,42 +40,39 @@ import de.uniol.inf.is.odysseus.physicaloperator.ISweepArea.Order;
 public class SlidingPeriodicWindowTIPO<R extends IMetaAttributeContainer<? extends ITimeInterval>>
 		extends AbstractWindowTIPO<R> {
 
-	/**
-	 * This operator needs a sweeparea if the pos-neg approach will be used.
-	 */
-	private AbstractSweepArea<IMetaAttributeContainer<? extends ITimeInterval>> sa;
-
+	
 	/**
 	 * This is the number of slides that have been processed.
 	 */
-	private long slideNo;
+	private long slideNo = 1;
+
+	/**
+	 * What are the slides in which elements are processed
+	 */
+	final private long windowSlide;;
+
+	
+	/**
+	 * list with buffered elements 
+	 */
+	private LinkedList<R> inputBuffer = new LinkedList<R>();;
 
 	/**
 	 * list with elements to deliver
 	 */
-	private LinkedList<IMetaAttributeContainer<? extends ITimeInterval>> deliveryList;
+	private LinkedList<R> deliveryList = new LinkedList<R>();
+
 
 	/** Creates a new instance of SlidingDeltaWindowPO */
 	public SlidingPeriodicWindowTIPO(WindowAO logical) {
 		super(logical);
-
-		this.sa = new DefaultTISweepArea<IMetaAttributeContainer<? extends ITimeInterval>>();
-		this.deliveryList = new LinkedList<IMetaAttributeContainer<? extends ITimeInterval>>();
-
-		// the execution mode must be positive negative in every case,
-		// because in this window only the start time stamp of the elements
-		// is important, but in interval based mode, also the end time stamp
-		// would be considered and this would lead to an errornous output.
-		this.sa.setQueryPredicate(LiesInPredicate.getInstance());
+		this.windowSlide = logical.getWindowSlide();
 		this.slideNo = 1;
 	}
 
 	public SlidingPeriodicWindowTIPO(SlidingPeriodicWindowTIPO<R> original) {
 		super(original);
-
-		this.sa = original.sa;
-		this.deliveryList = original.deliveryList;
-		this.slideNo = original.slideNo;
+		this.windowSlide = original.windowSlide;
 	}
 
 	@Override
@@ -93,71 +84,55 @@ public class SlidingPeriodicWindowTIPO<R extends IMetaAttributeContainer<? exten
 	public OutputMode getOutputMode() {
 		return OutputMode.MODIFIED_INPUT;
 	}
-
-	@SuppressWarnings("unchecked")
+	
 	@Override
-	public void process_next(R object, int port) {
-		process(object.getMetadata().getStart());
+	protected void process_open() throws OpenFailedException {
+		slideNo = 1;
+		inputBuffer.clear();
+		deliveryList.clear();
+	}
 
-		this.sa.insert(object);
+	@Override
+	public synchronized void process_next(R object, int port) {
+		this.inputBuffer.add(object);
+		process(object.getMetadata().getStart());
 
 		while (!this.deliveryList.isEmpty()) {
 			this.transfer((R) this.deliveryList.removeFirst());
 		}
 	}
 
-	private void process(PointInTime point) {
-		long delta = this.windowAdvance;
+	private synchronized void process(PointInTime point) {
+		long delta = this.windowSlide;
 		long winSize = this.windowSize;
 		long pointInTime = point.getMainPoint();
 		// first check if elements have to be removed and/or delivered
 		if ((pointInTime / delta) >= this.slideNo) {
 			this.slideNo = pointInTime / delta;
 
-			// return all elements with a start timestamp
-			// (t_s/delta) * delta - winsize <= start timestamp < (t_s/ delta) *
-			// delta
+			// Calc the start and end point of the window for all elements that
+			// will be transfered
 			long comp_t_start = this.slideNo * delta - winSize;
 			PointInTime p_start = new PointInTime(Math.max(comp_t_start, 0));
-			// p_end must be infinity because otherwise, the lies in predicate
-			// will
-			// not work. the elements in the sweep area still have no end
-			// timestamp
-			// and therefore their end timestamp is infinity
-			PointInTime p_end = new PointInTime();
-			TimeInterval compVal = new TimeInterval(p_start, p_end);
-			IMetaAttributeContainer<? extends ITimeInterval> compElem = new MetaAttributeContainer<ITimeInterval>(
-					compVal);
-			Iterator<IMetaAttributeContainer<? extends ITimeInterval>> iter = this.sa
-					.query(compElem, Order.LeftRight);
-			while (iter.hasNext()) {
-				IMetaAttributeContainer<? extends ITimeInterval> retval;
-				retval = (IMetaAttributeContainer<? extends ITimeInterval>) iter
-						.next().clone();
-				// edit the time interval
-				retval.getMetadata().setStart(p_start);
-				// the end time stamp must be ( actual / delta )
-				retval.getMetadata().setEnd(
-						new PointInTime(this.slideNo * delta));
-				this.deliveryList.add(retval);
+			PointInTime p_end = new PointInTime(this.slideNo * delta);
+			
+			// 1. Remove all Elements that are not within the window size!
+			while(inputBuffer.getFirst().getMetadata().getStart().before(p_start)){
+				inputBuffer.removeFirst();
 			}
-
-			// remove all elements that have a start timestamp
-			// (t_s / delta) * delta - winsize <= start timestamp < (t_s /
-			// delta) * delta - winsize + delta
-			long remove_t_start = (this.slideNo * delta) - winSize + delta - 1;
-			PointInTime p_remove = new PointInTime(remove_t_start);
-
-			this.sa.purgeElementsBefore(p_remove);
+			
+			// 2. all Elemente before p_end need to be processed
+			while(inputBuffer.getFirst().getMetadata().getStart().before(p_end)){
+				R elem = (R) inputBuffer.removeFirst();
+				elem.getMetadata().setStart(p_start);
+				elem.getMetadata().setEnd(p_end);
+				this.deliveryList.add(elem);
+			}	
 		}
-	}
-
-	@Override
-	public void process_close() {
 	}
 	
 	@Override
-	public void processPunctuation(PointInTime timestamp, int port) {
+	public synchronized void processPunctuation(PointInTime timestamp, int port) {
 		process(timestamp);
 	}
 }
