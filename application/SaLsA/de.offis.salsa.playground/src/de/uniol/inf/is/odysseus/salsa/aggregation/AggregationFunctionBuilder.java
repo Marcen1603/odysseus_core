2c621d561557549b1cbf7cbb85d4b36482aa1f37
Christian Kuka <christian@kuka.cc>
2011-10-27 18:05:20 +0000

    Add punctuation operator and Last/First/Nth aggregation functions
    Aggregation Functions:
    + Last: Select the attribute value of the last tuple inside a window
    + First: Select the attribute value of first tuple inside a window
    + Nth: Select the attribute value of the n-th tuple inside a window
    
    Punctuation Operator:
    Allows the sending of a punctuation (heartbeat) into a stream S1 depending on the
    frequency ratio between S1 and another stream S2. Older
    tuples in S1 will be dropped.
    
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10045 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 83558976ddba9ec8022f2d66aad1ff707bc9efe2

--- application/SaLsA/de.offis.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/aggregation/AggregationFunctionBuilder.java
+++ application/SaLsA/de.offis.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/aggregation/AggregationFunctionBuilder.java
@@ -16,19 +16,15 @@ public class AggregationFunctionBuilder implements IAggregateFunctionBuilder {
     private final static String MERGEFLOATGRID = "MERGEFLOATGRID";
     private final static String MERGEBYTEGRID = "MERGEBYTEGRID";
     private final static String MERGEBOOLEANGRID = "MERGEBOOLEANGRID";
-    private final static String LAST = "LAST";
-    private final static String FIRST = "FIRST";
-    private final static String NTH = "NTH";
     private final static String PMERGE = "PMERGE";
+    
     private static Collection<String> names = new LinkedList<String>();
     {
         names.add(MERGEDOUBLEGRID);
         names.add(MERGEFLOATGRID);
         names.add(MERGEBYTEGRID);
         names.add(MERGEBOOLEANGRID);
-        names.add(LAST);
-        names.add(FIRST);
-        names.add(NTH);
+
         names.add(PMERGE);
     }
 
@@ -56,15 +52,6 @@ public class AggregationFunctionBuilder implements IAggregateFunctionBuilder {
         }
         else if (key.getName().equalsIgnoreCase(MERGEBOOLEANGRID)) {
             aggFunc = new MergeBooleanGrid(pos);
-        }
-        else if (key.getName().equalsIgnoreCase(LAST)) {
-            aggFunc = RelationalLast.getInstance();
-        }
-        else if (key.getName().equalsIgnoreCase(FIRST)) {
-            aggFunc = RelationalFirst.getInstance();
-        }
-        else if (key.getName().equalsIgnoreCase(NTH)) {
-            aggFunc = RelationalNth.getInstance(Integer.parseInt(key.getProperty("nth")));
         }
         else if (key.getName().equalsIgnoreCase(PMERGE)) {
             aggFunc = new RelationalPolygonAggregation(pos);
