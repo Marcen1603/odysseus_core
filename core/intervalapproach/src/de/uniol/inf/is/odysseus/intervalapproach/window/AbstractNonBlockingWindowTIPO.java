30e44d554ed5889a89bdaac3d9583a7f7f32680a
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2011-11-15 15:10:04 +0000

    Handle Cases where size < advance
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10144 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 69eae4b42c21ad12a75fe7f7bd81e88fba309c46

--- core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/window/AbstractNonBlockingWindowTIPO.java
+++ core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/window/AbstractNonBlockingWindowTIPO.java
@@ -1,17 +1,17 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.intervalapproach.window;
 
 import de.uniol.inf.is.odysseus.logicaloperator.WindowAO;
@@ -21,7 +21,6 @@ import de.uniol.inf.is.odysseus.metadata.PointInTime;
 
 public abstract class AbstractNonBlockingWindowTIPO<T extends IMetaAttributeContainer<? extends ITimeInterval>>
 		extends AbstractWindowTIPO<T> {
-
 
 	public AbstractNonBlockingWindowTIPO(WindowAO algebraOp) {
 		super(algebraOp);
@@ -35,12 +34,15 @@ public abstract class AbstractNonBlockingWindowTIPO<T extends IMetaAttributeCont
 	public OutputMode getOutputMode() {
 		return OutputMode.MODIFIED_INPUT;
 	}
-	
+
 	@Override
 	protected void process_next(T object, int port) {
 		ITimeInterval time = object.getMetadata();
-		time.setEnd(this.calcWindowEnd(time));
-		this.transfer(object);
+		PointInTime end = this.calcWindowEnd(time);
+		if (end.after(time.getStart())) {
+			time.setEnd(end);
+			this.transfer(object);
+		}
 	}
 
 	protected abstract PointInTime calcWindowEnd(ITimeInterval interval);
