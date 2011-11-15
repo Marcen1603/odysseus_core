e8e8d62c95149eb036e19219d4c4216fa50e2950
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2011-11-15 09:18:39 +0000

    avoid possible null pointer exception
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10136 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1f294fcfb20af3ba22f3f16fa4bb3c6896fddf87

--- core/rcp/rcp.editor.text/src/de/uniol/inf/is/odysseus/rcp/editor/text/editors/OdysseusScriptEditor.java
+++ core/rcp/rcp.editor.text/src/de/uniol/inf/is/odysseus/rcp/editor/text/editors/OdysseusScriptEditor.java
@@ -1,17 +1,17 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.rcp.editor.text.editors;
 
 import org.eclipse.jface.preference.IPreferenceStore;
@@ -26,8 +26,8 @@ import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 public class OdysseusScriptEditor extends AbstractDecoratedTextEditor {
 
 	public final static String EDITOR_MATCHING_BRACKETS = "matchingBrackets";
-	public final static String EDITOR_MATCHING_BRACKETS_COLOR= "matchingBracketsColor";
-	
+	public final static String EDITOR_MATCHING_BRACKETS_COLOR = "matchingBracketsColor";
+
 	private ColorManager colorManager = new ColorManager();
 	private OdysseusScriptContentOutlinePage outlinePage;
 	private OdysseusOccurrencesUpdater occurrencesUpdater;
@@ -41,27 +41,31 @@ public class OdysseusScriptEditor extends AbstractDecoratedTextEditor {
 	protected void internal_init() {
 		configureInsertMode(SMART_INSERT, false);
 		setDocumentProvider(new OdysseusScriptDocumentProvider());
-		setSourceViewerConfiguration(new OdysseusScriptViewerConfiguration(colorManager, this));			
+		setSourceViewerConfiguration(new OdysseusScriptViewerConfiguration(
+				colorManager, this));
 	}
-	
-	
-	@Override
-	public void createPartControl(Composite parent) {	
-		super.createPartControl(parent);
-		
-		this.occurrencesUpdater = new OdysseusOccurrencesUpdater(this);		
-		((IPostSelectionProvider)getSelectionProvider()).addPostSelectionChangedListener(this.occurrencesUpdater);
-	}
-	
-	
 
-	public void setModel(){
-		if(this.occurrencesUpdater!=null){
+	@Override
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+
+		this.occurrencesUpdater = new OdysseusOccurrencesUpdater(this);
+		((IPostSelectionProvider) getSelectionProvider())
+				.addPostSelectionChangedListener(this.occurrencesUpdater);
+	}
+
+	public void setModel() {
+		if (this.occurrencesUpdater != null) {
 			this.occurrencesUpdater.update(getSourceViewer());
 		}
-		this.outlinePage.setInput(getDocumentProvider().getDocument(getEditorInput()).get());
+		if (getDocumentProvider() != null && getEditorInput() != null
+				&& getDocumentProvider().getDocument(getEditorInput()) != null 
+				&& this.outlinePage != null) {
+			this.outlinePage.setInput(getDocumentProvider().getDocument(
+					getEditorInput()).get());
+		}
 	}
-	
+
 	@Override
 	public void dispose() {
 		colorManager.dispose();
@@ -72,20 +76,25 @@ public class OdysseusScriptEditor extends AbstractDecoratedTextEditor {
 	public Object getAdapter(@SuppressWarnings("rawtypes") Class adapter) {
 		if (IContentOutlinePage.class.equals(adapter)) {
 			if (outlinePage == null) {
-				outlinePage = new OdysseusScriptContentOutlinePage(getDocumentProvider().getDocument(getEditorInput()).get());
+				outlinePage = new OdysseusScriptContentOutlinePage(
+						getDocumentProvider().getDocument(getEditorInput())
+								.get());
 			}
 			return outlinePage;
 		}
 		return super.getAdapter(adapter);
 	}
-	
+
 	@Override
-	protected void configureSourceViewerDecorationSupport(SourceViewerDecorationSupport support) {	
+	protected void configureSourceViewerDecorationSupport(
+			SourceViewerDecorationSupport support) {
 		super.configureSourceViewerDecorationSupport(support);
-		char[] matchChars = {'(', ')', '[', ']', '{', '}'}; 
-		ICharacterPairMatcher matcher = new DefaultCharacterPairMatcher(matchChars);
+		char[] matchChars = { '(', ')', '[', ']', '{', '}' };
+		ICharacterPairMatcher matcher = new DefaultCharacterPairMatcher(
+				matchChars);
 		support.setCharacterPairMatcher(matcher);
-		support.setMatchingCharacterPainterPreferenceKeys(EDITOR_MATCHING_BRACKETS, EDITOR_MATCHING_BRACKETS_COLOR);
+		support.setMatchingCharacterPainterPreferenceKeys(
+				EDITOR_MATCHING_BRACKETS, EDITOR_MATCHING_BRACKETS_COLOR);
 		IPreferenceStore store = getPreferenceStore();
 		store.setDefault("matchingBrackets", true);
 		store.setDefault("matchingBracketsColor", "128,128,128");
