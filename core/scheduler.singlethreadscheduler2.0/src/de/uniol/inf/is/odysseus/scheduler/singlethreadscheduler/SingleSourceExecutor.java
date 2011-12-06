a0a5060beb5441520ed0f159a9591a55af44d239
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2011-12-06 10:08:52 +0000

    Quellen müssen zunächst einmal geöffnet worden sein, bevor sie entfernt werden dürfen --> Auf open warten
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10214 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ef67a566c593b33812c652d97745f6b3890223e8

--- core/scheduler.singlethreadscheduler2.0/src/de/uniol/inf/is/odysseus/scheduler/singlethreadscheduler/SingleSourceExecutor.java
+++ core/scheduler.singlethreadscheduler2.0/src/de/uniol/inf/is/odysseus/scheduler/singlethreadscheduler/SingleSourceExecutor.java
@@ -3,9 +3,12 @@ package de.uniol.inf.is.odysseus.scheduler.singlethreadscheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.uniol.inf.is.odysseus.event.IEvent;
+import de.uniol.inf.is.odysseus.event.IEventListener;
 import de.uniol.inf.is.odysseus.physicaloperator.IIterableSource;
+import de.uniol.inf.is.odysseus.physicaloperator.event.POEventType;
 
-class SingleSourceExecutor extends Thread {
+class SingleSourceExecutor extends Thread implements IEventListener {
 
 	Logger logger = LoggerFactory.getLogger(SingleSourceExecutor.class);
 
@@ -15,21 +18,38 @@ class SingleSourceExecutor extends Thread {
 
 	public SingleSourceExecutor(IIterableSource<?> s,
 			SingleThreadSchedulerWithStrategy singleThreadScheduler) {
+		this.setName(s.getName());
 		this.s = s;
 		this.caller = singleThreadScheduler;
 	}
 
 	@Override
 	public void run() {
-		logger.debug("Added Source " + s);
-		while (!isInterrupted() && s.isOpen() && !s.isDone()) {
-			while (s.hasNext()) {
-				s.transferNext();
+
+		logger.debug("Adding Source " + s);
+		logger.debug("Waiting for open");
+		s.subscribe(this, POEventType.OpenDone);
+		synchronized (this) {
+			while (!s.isOpen()) {
+				try {
+					this.wait(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
+		}
+		logger.debug("Opened ... Start Processing");
+		while (!isInterrupted() && s.isOpen() && !s.isDone() && s.hasNext()) {
+			s.transferNext();
 			Thread.yield();
 		}
-		logger.debug("Remove Source " + s);
+		logger.debug("Removing Source " + s);
 		caller.removeSourceThread(this);
+	}
+
+	@Override
+	public synchronized void eventOccured(IEvent<?, ?> event, long nanoTimestamp) {
+		notifyAll();
 	}
 
 }
