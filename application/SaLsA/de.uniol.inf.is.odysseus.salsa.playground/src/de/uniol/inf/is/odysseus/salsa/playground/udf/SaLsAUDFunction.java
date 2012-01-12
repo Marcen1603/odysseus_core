d99d3c658b0f09ef9d6f1da8e8b1bdba3c99bff6
Kai Papancratz <kapancratz@uni-oldenburg.de>
2012-01-12 08:24:18 +0000

    Write UDO test function for SaLsADummyDataGenerator transformation.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10317 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a4a688d3d09ce9348407659ead6b055108928597

--- application/SaLsA/de.uniol.inf.is.odysseus.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/playground/udf/SaLsAUDFunction.java
+++ application/SaLsA/de.uniol.inf.is.odysseus.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/playground/udf/SaLsAUDFunction.java
@@ -1,10 +1,20 @@
 package de.uniol.inf.is.odysseus.salsa.playground.udf;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.CoordinateSequence;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.Point;
+
 import de.uniol.inf.is.odysseus.logicaloperator.annotations.UserDefinedFunction;
+import de.uniol.inf.is.odysseus.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.physicaloperator.AbstractPipe.OutputMode;
 import de.uniol.inf.is.odysseus.physicaloperator.IUserDefinedFunction;
+import de.uniol.inf.is.odysseus.relational.base.RelationalTuple;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.SDFElement;
 
-@UserDefinedFunction(name="SALSA")
+@UserDefinedFunction(name="salsatransform")
 public class SaLsAUDFunction<R> implements
 		IUserDefinedFunction<R, R> {
 
@@ -17,8 +27,30 @@ public class SaLsAUDFunction<R> implements
 
 	@Override
 	public R process(R in, int port) {
-		System.out.println(this+"("+init+") PROCESS "+in);
-		return in;
+		//System.out.println(this+"("+init+") PROCESS "+in);
+		
+		RelationalTuple<IMetaAttribute> tuple = (RelationalTuple<IMetaAttribute>)in;	
+		
+		//POINT (25 15749)
+		String[] cpoint = tuple.getAttribute(1).toString().substring(7,tuple.getAttribute(1).toString().length()-1).split(" ");	
+		tuple.setAttribute(1, new GeometryFactory().createPoint(new Coordinate(Double.parseDouble(cpoint[0]) , Double.parseDouble(cpoint[1]))));
+		
+		//POLYGON ((10 15734, 40 15764, 20 15744, 30 15754, 10 15734))
+		Coordinate[] coordinates = new Coordinate[5];
+		int i = 0;
+		for(String polyPoint  : tuple.getAttribute(2).toString().substring(9,tuple.getAttribute(2).toString().length()-2).split(",")){				
+			coordinates[i] = new Coordinate(		
+					Double.parseDouble(polyPoint.substring(1).split(" ")[0])
+					, 
+					Double.parseDouble(polyPoint.substring(1).split(" ")[1])
+					);
+			
+			i++;
+		}
+		tuple.setAttribute(2,  new GeometryFactory().createPolygon(new GeometryFactory().createLinearRing(coordinates) , null));
+		
+		System.out.println(tuple.toString());
+		return (R) tuple;
 	}
 
 	@Override
