c18b2436c715ed4f460b0021a44c10532759a767
Christian Kuka <christian@kuka.cc>
2012-01-23 12:19:44 +0000

    SALSA: Update Grid functions to use OpenCV
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10351 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 501c8d46772ec2f7f5b3e57028b37f3b6b97e75f

--- application/SaLsA/de.offis.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/aggregation/GridPartialAggregate.java
+++ application/SaLsA/de.offis.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/aggregation/GridPartialAggregate.java
@@ -1,64 +1,123 @@
 package de.uniol.inf.is.odysseus.salsa.aggregation;
 
+import com.googlecode.javacv.cpp.opencv_core;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import com.googlecode.javacv.cpp.opencv_imgproc;
+
 import de.uniol.inf.is.odysseus.physicaloperator.aggregate.basefunctions.IPartialAggregate;
-import de.uniol.inf.is.odysseus.salsa.model.Grid2D;
+import de.uniol.inf.is.odysseus.salsa.model.Grid;
 
 /**
  * @author Christian Kuka <christian.kuka@offis.de>
  */
 public class GridPartialAggregate<T> implements IPartialAggregate<T> {
-    private final static double FREE = 0.0;
-    private final static double UNKNOWN = -1.0;
-    private final static double OBSTACLE = 1.0;
 
-    private Grid2D grid;
+    private double count;
+    private final Grid grid;
+    private IplImage image;
+    private IplImage mask;
 
-    public GridPartialAggregate(Grid2D grid) {
-        this.grid = grid;
+    public GridPartialAggregate(final Grid grid) {
+        this.count = 1.0;
+        this.grid = new Grid(grid.origin, grid.width * grid.cellsize, grid.depth * grid.cellsize,
+                grid.cellsize, grid.getBuffer());
+        this.image = opencv_core.cvCreateImage(
+                opencv_core.cvSize(this.grid.width, this.grid.depth), opencv_core.IPL_DEPTH_16U, 1);
+        this.mask = opencv_core.cvCreateImage(opencv_core.cvSize(this.grid.width, this.grid.depth),
+                opencv_core.IPL_DEPTH_8U, 1);
+
+        IplImage tmp = opencv_core.cvCreateImage(
+                opencv_core.cvSize(this.grid.width, this.grid.depth), opencv_core.IPL_DEPTH_8U, 1);
+        tmp.getByteBuffer().put(this.grid.getBuffer().duplicate());
+        opencv_core.cvConvertScale(tmp, this.image, 1, 0);
+        opencv_core.cvReleaseImage(tmp);
+        tmp = null;
+
+        this.mask.getByteBuffer().put(this.grid.getBuffer().duplicate());
+        opencv_imgproc.cvThreshold(this.mask, this.mask, 100, 255, opencv_imgproc.CV_THRESH_BINARY);
     }
 
-    public GridPartialAggregate(GridPartialAggregate<T> gridPartialAggregate) {
-        this.grid = gridPartialAggregate.grid;
-    }
-
-    public Grid2D getGrid() {
-        return grid;
-    }
-
-    public void merge(Grid2D grid) {
-        for (int l = 0; l < this.grid.grid.length; l++) {
-            for (int w = 0; w < this.grid.grid[l].length; w++) {
-                if ((l < grid.grid.length) && (w < grid.grid[l].length)) {
-                    // FIXME Implement DST
-                    if (this.grid.get(l, w) == UNKNOWN) {
-                        this.grid.set(l, w, grid.get(l, w));
-                    }
-                    else if ((this.grid.get(l, w) == OBSTACLE) && (grid.get(l, w) == OBSTACLE)) {
-                        this.grid.set(l, w, OBSTACLE);
-                    }
-                    else if ((this.grid.get(l, w) == FREE) && (grid.get(l, w) == OBSTACLE)) {
-                        this.grid.set(l, w, 0.5);
-                    }
-                    else if ((this.grid.get(l, w) == OBSTACLE) && (grid.get(l, w) == FREE)) {
-                        this.grid.set(l, w, 0.5);
-                    }
-                    else if ((this.grid.get(l, w) == FREE) && (grid.get(l, w) == FREE)) {
-                        this.grid.set(l, w, FREE);
-                    }
-                }
-            }
-        }
-    }
-
-    @Override
-    public String toString() {
-        StringBuffer ret = new StringBuffer("GridPartialAggregate (").append(hashCode())
-                .append(")").append(grid);
-        return ret.toString();
+    public GridPartialAggregate(final GridPartialAggregate<T> gridPartialAggregate) {
+        System.out.println("Other const call");
+        this.grid = new Grid(this.grid.origin, this.grid.width * this.grid.cellsize,
+                this.grid.depth * this.grid.cellsize, this.grid.cellsize,
+                gridPartialAggregate.grid.getBuffer());
+        this.image = opencv_core.cvCreateImage(
+                opencv_core.cvSize(this.grid.width, this.grid.depth), opencv_core.IPL_DEPTH_16U, 1);
+        this.mask = opencv_core.cvCreateImage(opencv_core.cvSize(this.grid.width, this.grid.depth),
+                opencv_core.IPL_DEPTH_8U, 1);
+        opencv_core.cvCopy(gridPartialAggregate.image, this.image);
+        opencv_core.cvCopy(gridPartialAggregate.mask, this.mask);
+        this.count = gridPartialAggregate.count;
     }
 
     @Override
     public GridPartialAggregate<T> clone() {
         return new GridPartialAggregate<T>(this);
     }
+
+    public void evaluate() {
+        IplImage image = opencv_core.cvCreateImage(
+                opencv_core.cvSize(this.grid.width, this.grid.depth), opencv_core.IPL_DEPTH_8U, 1);
+
+        opencv_core.cvConvertScale(this.image, image, 1.0 / this.count, 0);
+
+        opencv_core.cvAdd(image, this.mask, image, null);
+
+        for (int d = 0; d < this.grid.depth; d++) {
+            if ((d * image.widthStep()) > this.grid.size) {
+                image.getByteBuffer(d * image.widthStep()).get(this.grid.get(),
+                        d * this.grid.width, image.width());
+            }
+            else {
+                image.getByteBuffer(d * image.widthStep()).get(this.grid.get(),
+                        d * this.grid.width, image.widthStep());
+            }
+        }
+        opencv_core.cvReleaseImage(image);
+        opencv_core.cvReleaseImage(this.image);
+        opencv_core.cvReleaseImage(this.mask);
+        image = null;
+        this.image = null;
+        this.mask = null;
+    }
+
+    public Grid getGrid() {
+        return this.grid;
+    }
+
+    public void merge(final Grid grid) {
+        this.count++;
+        IplImage mask = opencv_core.cvCreateImage(opencv_core.cvSize(grid.width, grid.depth),
+                opencv_core.IPL_DEPTH_8U, 1);
+        mask.getByteBuffer().put(grid.getBuffer().duplicate());
+        opencv_imgproc.cvThreshold(mask, mask, 100, 255, opencv_imgproc.CV_THRESH_BINARY);
+        opencv_core.cvAnd(this.mask, mask, this.mask, null);
+
+        IplImage merge = opencv_core.cvCreateImage(opencv_core.cvSize(grid.width, grid.depth),
+                opencv_core.IPL_DEPTH_16U, 1);
+
+        IplImage tmp = opencv_core.cvCreateImage(
+                opencv_core.cvSize(this.grid.width, this.grid.depth), opencv_core.IPL_DEPTH_8U, 1);
+        tmp.getByteBuffer().put(grid.getBuffer().duplicate());
+        opencv_imgproc.cvThreshold(tmp, tmp, 100, 100, opencv_imgproc.CV_THRESH_TOZERO_INV);
+        opencv_core.cvConvertScale(tmp, merge, 1, 0);
+        opencv_core.cvReleaseImage(tmp);
+        tmp = null;
+
+        opencv_core.cvAdd(this.image, merge, this.image, null);
+
+        opencv_core.cvReleaseImage(merge);
+        opencv_core.cvReleaseImage(mask);
+        mask = null;
+        merge = null;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer ret = new StringBuffer("GridPartialAggregate (").append(this.hashCode())
+                .append(")").append(this.grid);
+        return ret.toString();
+    }
+
 }
