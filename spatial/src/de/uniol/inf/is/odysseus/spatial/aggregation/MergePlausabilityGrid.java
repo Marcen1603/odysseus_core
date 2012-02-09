856427c535231cae4324fa9c9f814034e5daeef1
Stephan Jansen <sjansen@uni-oldenburg.de>
2012-02-09 17:27:58 +0000

    new bundle with spatial.grid works from ckuka
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10497 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 60257bbc92bc81ab93ae669426245d0634eaabd6

---
+++ spatial/src/de/uniol/inf/is/odysseus/spatial/aggregation/MergePlausabilityGrid.java
@@ -0,0 +1,55 @@
+package de.uniol.inf.is.odysseus.spatial.aggregation;
+
+import de.uniol.inf.is.odysseus.physicaloperator.aggregate.basefunctions.AbstractAggregateFunction;
+import de.uniol.inf.is.odysseus.physicaloperator.aggregate.basefunctions.IPartialAggregate;
+import de.uniol.inf.is.odysseus.relational.base.RelationalTuple;
+import de.uniol.inf.is.odysseus.spatial.model.Grid;
+
+/**
+ * @author Christian Kuka <christian.kuka@offis.de>
+ */
+public class MergePlausabilityGrid extends
+		AbstractAggregateFunction<RelationalTuple<?>, RelationalTuple<?>> {
+
+	private final int attribPos;
+
+	public MergePlausabilityGrid(int[] pos) {
+		super("MergePlausabilityGrid");
+		this.attribPos = pos[0];
+	}
+
+	@Override
+	public IPartialAggregate<RelationalTuple<?>> init(
+			final RelationalTuple<?> in) {
+		final IPartialAggregate<RelationalTuple<?>> grid = new GridPartialPlausabilityAggregate<RelationalTuple<?>>(
+				(Grid) in.getAttribute(attribPos));
+		return grid;
+	}
+
+	@Override
+	public IPartialAggregate<RelationalTuple<?>> merge(
+			final IPartialAggregate<RelationalTuple<?>> p,
+			final RelationalTuple<?> toMerge, final boolean createNew) {
+		GridPartialPlausabilityAggregate<RelationalTuple<?>> grid = null;
+		if (createNew) {
+			grid = new GridPartialPlausabilityAggregate<RelationalTuple<?>>(
+					((GridPartialPlausabilityAggregate<RelationalTuple<?>>) p)
+							.getGrid());
+		} else {
+			grid = (GridPartialPlausabilityAggregate<RelationalTuple<?>>) p;
+		}
+		grid.merge((Grid) toMerge.getAttribute(attribPos));
+		return grid;
+	}
+
+	@Override
+	public RelationalTuple<?> evaluate(
+			final IPartialAggregate<RelationalTuple<?>> p) {
+		final GridPartialPlausabilityAggregate<RelationalTuple<?>> grid = (GridPartialPlausabilityAggregate<RelationalTuple<?>>) p;
+		grid.evaluate();
+		@SuppressWarnings("rawtypes")
+		final RelationalTuple<?> tuple = new RelationalTuple(1);
+		tuple.setAttribute(0, grid.getGrid());
+		return tuple;
+	}
+}
