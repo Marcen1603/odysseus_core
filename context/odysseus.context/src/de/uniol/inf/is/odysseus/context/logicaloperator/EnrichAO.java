808f722c3ead94f71abbaca5016a8c08d7dce172
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-02-13 13:30:06 +0000

    First approaches to enrich elements with context information
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10539 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 43850c757fd1fe3fad586150c79390dad6912b12

---
+++ context/odysseus.context/src/de/uniol/inf/is/odysseus/context/logicaloperator/EnrichAO.java
@@ -0,0 +1,87 @@
+package de.uniol.inf.is.odysseus.context.logicaloperator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.context.ContextManagementException;
+import de.uniol.inf.is.odysseus.context.store.ContextStore;
+import de.uniol.inf.is.odysseus.logicaloperator.AbstractLogicalOperator;
+import de.uniol.inf.is.odysseus.logicaloperator.annotations.LogicalOperator;
+import de.uniol.inf.is.odysseus.logicaloperator.annotations.Parameter;
+import de.uniol.inf.is.odysseus.logicaloperator.builder.StringParameter;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.sourcedescription.sdf.schema.SDFSchema;
+
+@LogicalOperator(name = "ENRICH", minInputPorts = 1, maxInputPorts = 1)
+public class EnrichAO extends AbstractLogicalOperator {
+
+	private static final long serialVersionUID = -6701002329614782111L;
+	List<String> contextStoreNames = new ArrayList<String>();
+	private SDFSchema outputSchema = null;
+
+	public EnrichAO(AbstractLogicalOperator op) {
+		super(op);
+	}
+
+	public EnrichAO() {
+		super();
+	}
+
+	@Parameter(name = "storeNames", type = StringParameter.class, isList = true)
+	public void setContextSources(List<String> sources) {
+		this.contextStoreNames = new ArrayList<String>(sources);
+		calcOutputSchema();
+	}
+
+	private void calcOutputSchema() {
+		if (getInputSchema(0) != null) {
+			// Attributes of the source
+			List<SDFAttribute> outattribs = new ArrayList<SDFAttribute>(
+					getInputSchema(0).getAttributes());
+			// Attributes of the context store
+			for (String store : contextStoreNames) {
+				// TODO: Wie trennt man den Namen des Kontext-Stores von einem
+				// ggf. vorhanden Attributnamen im Store?
+				// erstmal mit "."
+				int pointPos = store.indexOf(".");
+				String storeName = store;
+				String attributeName = null;
+				if (pointPos > 0) {
+					storeName = store.substring(0, pointPos);
+					attributeName = store.substring(pointPos + 1);
+				}
+				try {
+					SDFSchema s = ContextStore.getInstance().getStoreSchema(
+							storeName);
+					if (attributeName != null){
+						SDFAttribute attribute = s.findAttribute(attributeName);
+						if (attribute != null){
+							outattribs.add(attribute);
+						}
+					}
+
+				} catch (ContextManagementException e) {
+					e.printStackTrace();
+					// TODO: Error Handling if store not available
+				}
+			}
+			outputSchema = new SDFSchema(
+					getInputSchema(0).getURI() + "_ENRICH", outattribs);
+		}
+
+	}
+
+	@Override
+	public SDFSchema getOutputSchema() {
+		if (outputSchema == null) {
+			calcOutputSchema();
+		}
+		return outputSchema;
+	}
+
+	@Override
+	public AbstractLogicalOperator clone() {
+		return new EnrichAO(this);
+	}
+
+}
