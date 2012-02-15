f70e8942ec4e2ebe9c202421d099ee2c3dce06ff
Kai Papancratz <kapancratz@uni-oldenburg.de>
2012-02-15 12:38:11 +0000

    Implement new MemSize methodes for geometry types with Stepahn Jansen.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10568 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8c6642324807fe7be69b510c2d8661d658cc7d7e

--- spatial/odysseus.spatial/src/de/uniol/inf/is/odysseus/spatial/access/SpatialByteHandler.java
+++ spatial/odysseus.spatial/src/de/uniol/inf/is/odysseus/spatial/access/SpatialByteHandler.java
@@ -1,17 +1,17 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.spatial.access;
 
 import java.io.IOException;
@@ -19,7 +19,11 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.vividsolutions.jts.geom.CoordinateSequence;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKBReader;
@@ -29,40 +33,40 @@ import de.uniol.inf.is.odysseus.physicaloperator.access.AbstractAtomicDataHandle
 
 /**
  * @author Andr� Bolles
- *
+ * 
  */
-public class SpatialByteHandler extends AbstractAtomicDataHandler{
+public class SpatialByteHandler extends AbstractAtomicDataHandler {
 	static protected List<String> types = new ArrayList<String>();
-	static{
+	static {
 		types.add("SpatialGeometry");
 		types.add("SpatialGeometryCollection");
 
 		types.add("SpatialPoint");
 		types.add("SpatialLineString");
 		types.add("SpatialPolygon");
-		
+
 		types.add("SpatialMultiPoint");
 		types.add("SpatialMultiLineString");
 		types.add("SpatialMutliPolygon");
 	}
-	
+
 	WKBReader reader;
 	WKBWriter writer;
-	
-	public SpatialByteHandler(){
+
+	public SpatialByteHandler() {
 		this.reader = new WKBReader();
 		this.writer = new WKBWriter();
 	}
-	
+
 	@Override
 	public Object readData() throws IOException {
 		// the first four bytes are the length
 		// of the geo data
-		
+
 		int length = this.getStream().readInt();
 		byte[] binData = new byte[length];
 		this.getStream().read(binData);
-		
+
 		try {
 			return this.reader.read(binData);
 		} catch (ParseException e) {
@@ -70,7 +74,6 @@ public class SpatialByteHandler extends AbstractAtomicDataHandler{
 		}
 	}
 
-	
 	@Override
 	public Object readData(ByteBuffer buffer) {
 		// the first for bytes are the length of
@@ -78,7 +81,7 @@ public class SpatialByteHandler extends AbstractAtomicDataHandler{
 		int length = buffer.getInt();
 		byte[] binData = new byte[length];
 		buffer.get(binData);
-		
+
 		try {
 			return this.reader.read(binData);
 		} catch (ParseException e) {
@@ -90,47 +93,48 @@ public class SpatialByteHandler extends AbstractAtomicDataHandler{
 	public Object readData(String string) {
 		throw new RuntimeException("Sorry. Currently not implemented");
 	}
-	
+
 	@Override
 	public void writeData(ByteBuffer buffer, Object data) {
-		byte[] binData = this.writer.write((Geometry)data);
-		
+		byte[] binData = this.writer.write((Geometry) data);
+
 		// split integer into 4 bytes
 		byte[] length = this.intToByteArray(binData.length);
-		
+
 		byte[] dataAndLength = new byte[binData.length + length.length];
-		
+
 		// first put length
 		System.arraycopy(length, 0, dataAndLength, 0, length.length);
-		
+
 		// then put data
-		System.arraycopy(binData, 0, dataAndLength, length.length, binData.length);
-		
+		System.arraycopy(binData, 0, dataAndLength, length.length,
+				binData.length);
+
 		// put the data into the byte buffer
 		buffer.put(dataAndLength);
 	}
-	
-	private byte[] intToByteArray(int number){
+
+	private byte[] intToByteArray(int number) {
 		byte[] data = new byte[4];
-		 
+
 		for (int i = 0; i < 4; ++i) {
-		    int shift = i << 3; // i * 8
-		    data[3-i] = (byte)((number & (0xff << shift)) >>> shift);
+			int shift = i << 3; // i * 8
+			data[3 - i] = (byte) ((number & (0xff << shift)) >>> shift);
 		}
-		
+
 		return data;
-		 
+
 	}
-	
-	private int byteArrayToInt(byte[] data){
-		// byte[] -> int
-		int number = 0;     
-		for (int i = 0; i < 4; ++i) {
-		    number |= (data[3-i] & 0xff) << (i << 3);
-		}
-		
-		return number;
-	}
+
+	// private int byteArrayToInt(byte[] data){
+	// // byte[] -> int
+	// int number = 0;
+	// for (int i = 0; i < 4; ++i) {
+	// number |= (data[3-i] & 0xff) << (i << 3);
+	// }
+	//
+	// return number;
+	// }
 
 	@Override
 	final public List<String> getSupportedDataTypes() {
@@ -139,22 +143,66 @@ public class SpatialByteHandler extends AbstractAtomicDataHandler{
 
 	@Override
 	public int memSize(Object attribute) {
-		if(attribute instanceof Geometry){
-			byte[] binData = this.writer.write((Geometry)attribute);
-			// split integer into 4 bytes
-			byte[] length = this.intToByteArray(binData.length);
-			byte[] dataAndLength = new byte[binData.length + length.length];
-			
-			return dataAndLength.length;
+		int size = 0;
+
+		/*	The GeometryCollection handles the MultiPoint, MultiLine, and MultiPolygon as well.
+		 * 
+		 */
+		if (attribute instanceof GeometryCollection) {
+			size += getGeometryCollectionMemSize((GeometryCollection)attribute);
+		}
+
+		if (attribute instanceof Polygon) {
+			size += getPolygonMemSize((Polygon)attribute);
 		}
 		
-		if(attribute instanceof Polygon){
-			byte[] binData = this.writer.write((Geometry)attribute);
-			return binData.length;
+		if (attribute instanceof LineString) {
+			size += getLineStringMemSize((LineString)attribute);
 		}
-		
-		
-		
-		return 0;
+
+		if (attribute instanceof Point) {
+			size += getPointMemSize();
+		}
+
+		return size;
 	}
+	
+	private int getGeometryCollectionMemSize(GeometryCollection collection) {
+		int size = 5;
+		size += 4;
+		for (int i = 0; i < collection.getNumGeometries(); i++) {
+			Geometry geometry = collection.getGeometryN(i);
+			size += memSize(geometry);
+		}
+		return size;
+	}
+
+	private int getPointMemSize() {
+		return 5 + 2 * 16;
+	}
+
+	private int getLineStringMemSize(LineString lineString) {
+		return 5 + getCoordinateSequenceMemSize(lineString
+				.getCoordinateSequence());
+	}
+
+	private int getPolygonMemSize(Polygon polygon) {
+		int size = 5;
+		size += 4;
+		size += getCoordinateSequenceMemSize(polygon.getExteriorRing()
+				.getCoordinateSequence());
+
+		for (int i = 0; i < polygon.getNumInteriorRing(); i++) {
+			size += getCoordinateSequenceMemSize(polygon.getInteriorRingN(i)
+					.getCoordinateSequence());
+		}
+
+		return size;
+	}
+
+	private int getCoordinateSequenceMemSize(
+			CoordinateSequence coordinateSequence) {
+		return 4 + coordinateSequence.size() * 16;
+	}
+
 }
