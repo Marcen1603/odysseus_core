187551eebdc90dc2ab483ef211970a82083fa252
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-02-20 14:11:42 +0000

    ITimeInterval isValid() removed
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10604 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9057744abc0ea249ef09b2f366b8ad94538d6b48

--- core/base/src/de/uniol/inf/is/odysseus/metadata/ITimeInterval.java
+++ core/base/src/de/uniol/inf/is/odysseus/metadata/ITimeInterval.java
@@ -25,8 +25,5 @@ public interface ITimeInterval extends IMetaAttribute, Comparable<ITimeInterval>
 	public void setStart(PointInTime point);
 	public void setEnd(PointInTime point);
 	public void setStartAndEnd(PointInTime start, PointInTime end);
-	public boolean isValid();
-//	@Override
-//	public ITimeInterval clone();
 	public String toString(PointInTime baseTime);
 }
