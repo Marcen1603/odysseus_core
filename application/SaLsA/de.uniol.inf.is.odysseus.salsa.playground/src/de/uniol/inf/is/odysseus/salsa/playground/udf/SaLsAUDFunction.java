1fb899266f65b129c8ec731501c1972adcb112c2
Kai Papancratz <kapancratz@uni-oldenburg.de>
2012-02-21 09:34:53 +0000

    Merge Conflicts in SaLsA Fusion L1 UDO.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10608 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a52b548628af1904b11b6ff05057a33a2b19f0f0

--- application/SaLsA/de.uniol.inf.is.odysseus.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/playground/udf/SaLsAUDFunction.java
+++ application/SaLsA/de.uniol.inf.is.odysseus.salsa.playground/src/de/uniol/inf/is/odysseus/salsa/playground/udf/SaLsAUDFunction.java
@@ -1,154 +1,66 @@
 package de.uniol.inf.is.odysseus.salsa.playground.udf;
 
-import com.vividsolutions.jts.geom.Geometry;
+
+import java.util.Iterator;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import de.uniol.inf.is.odysseus.logicaloperator.annotations.UserDefinedFunction;
 import de.uniol.inf.is.odysseus.metadata.IMetaAttribute;
+import de.uniol.inf.is.odysseus.metadata.ITimeInterval;
+import de.uniol.inf.is.odysseus.metadata.PointInTime;
 import de.uniol.inf.is.odysseus.physicaloperator.AbstractPipe.OutputMode;
+import de.uniol.inf.is.odysseus.physicaloperator.AbstractSweepArea;
 import de.uniol.inf.is.odysseus.physicaloperator.IUserDefinedFunction;
 import de.uniol.inf.is.odysseus.relational.base.RelationalTuple;
-//@UserDefinedFunction(name="salsatransform")
-//public class SaLsAUDFunction<R> implements
-//		IUserDefinedFunction<R, R> {
-//
-//
-//	@Override
-//	public R process(R in, int port) {
-//		//System.out.println(this+"("+init+") PROCESS "+in);
-//		
-//		RelationalTuple<IMetaAttribute> tuple = (RelationalTuple<IMetaAttribute>)in;	
-//		
-//		//POINT (25 15749)
-//		String[] cpoint = tuple.getAttribute(1).toString().substring(7,tuple.getAttribute(1).toString().length()-1).split(" ");	
-//		tuple.setAttribute(1, new GeometryFactory().createPoint(new Coordinate(Double.parseDouble(cpoint[0]) , Double.parseDouble(cpoint[1]))));
-//		
-//		//POLYGON ((10 15734, 40 15764, 20 15744, 30 15754, 10 15734))
-//		Coordinate[] coordinates = new Coordinate[5];
-//		int i = 0;
-//		for(String polyPoint  : tuple.getAttribute(2).toString().substring(9,tuple.getAttribute(2).toString().length()-2).split(",")){				
-//			coordinates[i] = new Coordinate(		
-//					Double.parseDouble(polyPoint.substring(1).split(" ")[0])
-//					, 
-//					Double.parseDouble(polyPoint.substring(1).split(" ")[1])
-//					);
-//			
-//			i++;
-//		}
-//		tuple.setAttribute(2,  new GeometryFactory().createPolygon(new GeometryFactory().createLinearRing(coordinates) , null));
-//		
-//		System.out.println(tuple.toString());
-//		return (R) tuple;
-//=======
-import java.util.ArrayList;
-import java.util.List;
 
 import com.vividsolutions.jts.geom.Dimension;
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Location;
 
-import de.uniol.inf.is.odysseus.metadata.ITimeInterval;
 
 @UserDefinedFunction(name = "FusionL1")
-public class SaLsAUDFunction<R> implements IUserDefinedFunction<R, R> {
-
-	List<RelationalTuple> tupleList = null;
-	List<RelationalTuple> mergetupleList = null;
-	RelationalTuple last = null;
+public class SaLsAUDFunction extends AbstractSweepArea<RelationalTuple<? extends IMetaAttribute>> implements IUserDefinedFunction<RelationalTuple<? extends IMetaAttribute>,RelationalTuple<? extends IMetaAttribute>> {
 	
 	double mDistance = 0.00;
 	
 	@Override
 	public void init(String initString) {
 		mDistance = Double.parseDouble(initString);
-		
-		
-		
-		tupleList = new ArrayList<RelationalTuple>();
-		mergetupleList = new ArrayList<RelationalTuple>();
 	}
 
 	@Override
-	public R process(R in, int port) {
-		RelationalTuple<IMetaAttribute> intuple = (RelationalTuple<IMetaAttribute>) in;
-		mergetupleList = new ArrayList<RelationalTuple>();
+	public RelationalTuple<? extends IMetaAttribute> process(RelationalTuple<? extends IMetaAttribute> in, int port) {
+		RelationalTuple<? extends IMetaAttribute> out = null;
+		this.insert(in);
 		
-		//System.out.println((String)intuple.getAttribute(1));
-		if(last != null){
-			//System.out.println(intuple.getAttribute(1).equals((String)last.getAttribute(1)));
-		}
-		last = intuple;
-		
-		if (((ITimeInterval) intuple.getMetadata()).isValid()) {
-			tupleList.add(intuple);
-
-		} else {
-			tupleList.add(intuple);
-			/*
-			System.out.println("------------------------------");
-			System.out.println("Current: " + tupleList.size());
-			
-			for(RelationalTuple tuple : tupleList){
-				System.out.println((String)tuple.getAttribute(1));
-			}
-			System.out.println("------------------------------");
-			*/
-			if(tupleList.size() > 4){
-				tupleList.clear();
-			}
-			if (tupleList.size() > 1) {
-				//System.out.println("Merge Tuple in List: " + tupleList.size());
-				for (RelationalTuple<IMetaAttribute> wtuple : tupleList) {
-
-					// System.out.println(((List<RelationalTuple<IMetaAttribute>>)intuple.getAttribute(0)).addAll(((List<RelationalTuple<IMetaAttribute>>)wtuple.getAttribute(0))));
-
-					for (int i = 0; i < ((List<RelationalTuple<IMetaAttribute>>) intuple.getAttribute(0)).size(); i++) {
-						Geometry geometry = (Geometry) ((RelationalTuple) ((List<RelationalTuple<IMetaAttribute>>) intuple.getAttribute(0)).get(i)).getAttribute(0);
-						//Enverlope Runs
-						//geometry = geometry.getEnvelope();
-						//geometry = geometry.convexHull();
+		if(!this.elements.isEmpty()){
+			if(((ITimeInterval)in.getMetadata()).getStart().after(((ITimeInterval)this.elements.getFirst().getMetadata()).getEnd())){		
+				//System.out.println("Last Element Received.");
+				
+				for(RelationalTuple<? extends IMetaAttribute> element1: this.elements){
+					Geometry geometry1 = ((Geometry)element1.getAttribute(0));
+					for(RelationalTuple<? extends IMetaAttribute> element2: this.elements){
+						Geometry geometry2 = ((Geometry)element2.getAttribute(0));
 						
-						for (int ii = 0; ii < ((List<RelationalTuple<IMetaAttribute>>) wtuple.getAttribute(0)).size(); ii++) {
-	
-							Geometry geometry_element = (Geometry) ((RelationalTuple) ((List<RelationalTuple<IMetaAttribute>>) wtuple.getAttribute(0)).get(ii)).getAttribute(0);
-							//Enverlope Runs
-							//geometry_element = geometry_element.getEnvelope();
-							
-							//geometry_element = geometry_element.convexHull();
-							
-							//if (geometry.crosses(geometry_element)) {
-								//System.out.println("Merge Candidate");
-							
-							//System.out.println(geometry.distance(geometry_element));
-							if(geometry.distance(geometry_element) < mDistance){
-								if (isCrosses(geometry.getEnvelope().getDimension(), geometry_element.getEnvelope().getDimension())) {
-								//if(geometry.getEnvelope().crosses(geometry_element.getEnvelope())){
-									//System.out.println("Merged");
-									((List<RelationalTuple<IMetaAttribute>>) intuple.getAttribute(0)).get(i).setAttribute(0, (geometry_element.union(geometry)).convexHull());
-									//System.out.println("Add:" + ((List<RelationalTuple<IMetaAttribute>>) intuple.getAttribute(0)).get(i));
-									mergetupleList.add(((List<RelationalTuple<IMetaAttribute>>) intuple.getAttribute(0)).get(i));
-								}
-							//}
-							}
-							else{
-								mergetupleList.add(intuple);
-							}
-							
-							
+						double distance = geometry1.distance(geometry2);
+						if(distance < mDistance){
+							//System.out.println("distance < " + mDistance);
+							if(isCrosses(geometry1.getBoundaryDimension(), geometry2.getBoundaryDimension())){
+								out = in.clone();
+								out.setAttribute(0, geometry1.union(geometry2));
+							}	
 						}
+						
 					}
 				}
 				
-				if(mergetupleList.size() > 0){
-					//System.out.println("Send: " + mergetupleList.size());
-					intuple.setAttribute(0, mergetupleList);
-					tupleList.clear();
-					return (R) intuple;	
-				}
+				//System.out.println("Cleared Sweep Area: " + this.elements.size());
+				this.elements.clear();
 			}
-			
-			
-			tupleList.clear();
 		}
 		
-		return null;
+		return out;
 	}
 
 	public boolean isCrosses(int dimensionOfGeometryA, int dimensionOfGeometryB) {
@@ -204,7 +116,18 @@ public class SaLsAUDFunction<R> implements IUserDefinedFunction<R, R> {
 
 	@Override
 	public OutputMode getOutputMode() {
-		return OutputMode.INPUT;
+		return OutputMode.NEW_ELEMENT;
+	}
+
+	@Override
+	public void purgeElementsBefore(PointInTime time) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public Iterator<RelationalTuple<? extends IMetaAttribute>> extractElementsBefore(
+			PointInTime time) {
+		throw new NotImplementedException();
 	}
 
 
