04a46f74eb63ac540ec421acf6c449134f7181e4
Kai Papancratz <kapancratz@uni-oldenburg.de>
2012-02-29 18:16:43 +0000

    Fixed Fusion product again and clean up the UnNestAO.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10703 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: daa10f10eafcd4c69b0ae31a9e79332e456cd056

--- core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/UnNestAO.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/UnNestAO.java
@@ -20,7 +20,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.uniol.inf.is.odysseus.core.sdf.SDFElement;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.LogicalOperator;
