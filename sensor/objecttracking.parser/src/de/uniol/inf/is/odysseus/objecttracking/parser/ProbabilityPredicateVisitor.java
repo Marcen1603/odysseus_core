6b22a6e4fbc6c1afeebef10b021bfd5516d01edc
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-13 14:22:16 +0000

    Removed many warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11120 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0d0ae701d5782b89059a69e1d9cc44a22e30e136

--- sensor/objecttracking.parser/src/de/uniol/inf/is/odysseus/objecttracking/parser/ProbabilityPredicateVisitor.java
+++ sensor/objecttracking.parser/src/de/uniol/inf/is/odysseus/objecttracking/parser/ProbabilityPredicateVisitor.java
@@ -118,53 +118,52 @@ public class ProbabilityPredicateVisitor implements IVisitor{
 			return new ProbabilityPredicate(leftSource, leftM, leftV,
 					rightSource, rightM, rightV, xL, xU, prob, probPred
 							.getCovOperandType().toString(), probPred.getCompareOperator());
-		} else {
-			// the first child will be an matrix expression
-			ASTMatrixExpression leftMatrix = (ASTMatrixExpression) probPred
-					.jjtGetChild(0);
-			ArrayList leftRows = leftMatrix.getMatrix();
-
-			String leftSource = ((ASTIdentifier) probPred.jjtGetChild(1))
-					.getName();
-
-			ASTMatrixExpression leftVector = (ASTMatrixExpression) probPred
-					.jjtGetChild(2);
-			ArrayList leftVRows = leftVector.getMatrix();
-			
-
-			double[][] leftM = new double[leftRows.size()][((ArrayList) leftRows
-					.get(0)).size()];
-			for (int i = 0; i < leftRows.size(); i++) {
-				ArrayList row = (ArrayList) leftRows.get(i);
-				for (int u = 0; u < row.size(); u++) {
-					leftM[i][u] = (Double) row.get(u);
-				}
-			}
-
-			double[] leftV = new double[leftVRows.size()];
-			// i should only be 0 and then the loop should be
-			// finished
-			for (int i = 0; i < leftVRows.size(); i++) {
-				leftV[i] = (Double) ((ArrayList) leftVRows.get(i)).get(0);
-			}
-
-			ArrayList xLow = probPred.getXLow();
-			double[] xL = new double[xLow.size()];
-			for (int i = 0; i < xLow.size(); i++) {
-				xL[i] = (Double) xLow.get(i);
-			}
-
-			ArrayList xUp = probPred.getXUp();
-			double[] xU = new double[xUp.size()];
-			for (int i = 0; i < xUp.size(); i++) {
-				xU[i] = (Double) xUp.get(i);
-			}
-
-			double prob = probPred.getProb();
-
-			return new ProbabilityPredicate(leftSource, leftM, leftV, xL, xU,
-					prob, probPred.getCompareOperator());
 		}
+        // the first child will be an matrix expression
+        ASTMatrixExpression leftMatrix = (ASTMatrixExpression) probPred
+        		.jjtGetChild(0);
+        ArrayList leftRows = leftMatrix.getMatrix();
+
+        String leftSource = ((ASTIdentifier) probPred.jjtGetChild(1))
+        		.getName();
+
+        ASTMatrixExpression leftVector = (ASTMatrixExpression) probPred
+        		.jjtGetChild(2);
+        ArrayList leftVRows = leftVector.getMatrix();
+        
+
+        double[][] leftM = new double[leftRows.size()][((ArrayList) leftRows
+        		.get(0)).size()];
+        for (int i = 0; i < leftRows.size(); i++) {
+        	ArrayList row = (ArrayList) leftRows.get(i);
+        	for (int u = 0; u < row.size(); u++) {
+        		leftM[i][u] = (Double) row.get(u);
+        	}
+        }
+
+        double[] leftV = new double[leftVRows.size()];
+        // i should only be 0 and then the loop should be
+        // finished
+        for (int i = 0; i < leftVRows.size(); i++) {
+        	leftV[i] = (Double) ((ArrayList) leftVRows.get(i)).get(0);
+        }
+
+        ArrayList xLow = probPred.getXLow();
+        double[] xL = new double[xLow.size()];
+        for (int i = 0; i < xLow.size(); i++) {
+        	xL[i] = (Double) xLow.get(i);
+        }
+
+        ArrayList xUp = probPred.getXUp();
+        double[] xU = new double[xUp.size()];
+        for (int i = 0; i < xUp.size(); i++) {
+        	xU[i] = (Double) xUp.get(i);
+        }
+
+        double prob = probPred.getProb();
+
+        return new ProbabilityPredicate(leftSource, leftM, leftV, xL, xU,
+        		prob, probPred.getCompareOperator());
 	}
 
 }
