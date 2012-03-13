ced16d7bfa1c9e98aacd3e1cd128c52e365cc6ef
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-13 13:38:13 +0000

    Removed many warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10943 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: fac6bce100172b48be994ead629081bae6801ac4

--- service/test/src/de/uniol/inf/is/odysseus/service/test/Activator.java
+++ service/test/src/de/uniol/inf/is/odysseus/service/test/Activator.java
@@ -29,7 +29,8 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext bundleContext) throws Exception {
+	@Override
+    public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		System.out.println("Starting simulated sensor...");
 		
@@ -39,7 +40,8 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext bundleContext) throws Exception {
+	@Override
+    public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 	}
 
