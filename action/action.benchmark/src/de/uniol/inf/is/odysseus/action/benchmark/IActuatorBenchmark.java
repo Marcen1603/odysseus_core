0a90dddd39d3aa381d0a27ce779316947a2cb127
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-13 10:15:56 +0000

    Removed warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10856 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4b59f3c781d596fe2c7083c2d1fb15c8f114ea50

--- action/action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/IActuatorBenchmark.java
+++ action/action.benchmark/src/de/uniol/inf/is/odysseus/action/benchmark/IActuatorBenchmark.java
@@ -19,7 +19,7 @@ package de.uniol.inf.is.odysseus.action.benchmark;
 
 public interface IActuatorBenchmark {
 	
-	public enum Operation{DATAEXTRACTION,QUERYPROCESSING, ACTIONEXECTION};
+	public enum Operation{DATAEXTRACTION,QUERYPROCESSING, ACTIONEXECTION}
 	
 	public void addBenchmarkData(BenchmarkData data);
 		
