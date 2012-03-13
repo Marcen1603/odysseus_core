7913b30707857a1de507411383f2e795d091902a
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-13 14:22:17 +0000

    Removed many warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11122 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9c28ce6249a1b03b233d7b2ca3632b163922fddb

--- mining/cleaning/cleaning.base/src/de/uniol/inf/is/odysseus/mining/cleaning/detection/stateful/OutOfRangeDetection.java
+++ mining/cleaning/cleaning.base/src/de/uniol/inf/is/odysseus/mining/cleaning/detection/stateful/OutOfRangeDetection.java
@@ -42,11 +42,10 @@ public class OutOfRangeDetection extends AbstractSimpleAggregateDetection{
 			String p = getAttributeName()+" < ( (1.0-"+percent+") * "+getAggregationAttribute()+")";
 			p = p+" || "+getAttributeName()+" > ( (1.0+"+percent+") * "+getAggregationAttribute()+")";		
 			return p;
-		}else{
-			String p = getAttributeName()+"+"+this.abweichung+"<"+getAggregationAttribute();
-			p = p+" || "+getAttributeName()+"-"+this.abweichung+">"+getAggregationAttribute();		
-			return p;
 		}
+        String p = getAttributeName()+"+"+this.abweichung+"<"+getAggregationAttribute();
+        p = p+" || "+getAttributeName()+"-"+this.abweichung+">"+getAggregationAttribute();		
+        return p;
 	}	
 
 }
