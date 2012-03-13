66ed3fa0482e2e201842f4352869612b9ee3e892
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-13 13:15:45 +0000

    Removed warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10915 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4325441d9c269c9a5e99d4993077d2c23159bf03

--- mining/clustering/mining.clustering/src/de/uniol/inf/is/odysseus/mining/clustering/logicaloperator/AbstractClusteringAO.java
+++ mining/clustering/mining.clustering/src/de/uniol/inf/is/odysseus/mining/clustering/logicaloperator/AbstractClusteringAO.java
@@ -37,143 +37,143 @@ import de.uniol.inf.is.odysseus.mining.NonNumericAttributeException;
  */
 public abstract class AbstractClusteringAO extends UnaryLogicalOp {
 
-	private static final long serialVersionUID = 5930667720134167936L;
-	protected List<SDFAttribute> attributes;
+    private static final long serialVersionUID = 5930667720134167936L;
+    protected List<SDFAttribute> attributes;
 
-	/**
-	 * Standard constructor.
-	 * 
-	 */
-	protected AbstractClusteringAO() {
+    /**
+     * Standard constructor.
+     * 
+     */
+    protected AbstractClusteringAO() {
 
-	}
+    }
 
-	/**
-	 * Copy constructor.
-	 * 
-	 * @param copy
-	 *            the AbstractClusteringAO to copy.
-	 */
-	public AbstractClusteringAO(AbstractClusteringAO copy) {
-		super(copy);
-		this.attributes = new ArrayList<SDFAttribute>(copy.getAttributes());
-	}
+    /**
+     * Copy constructor.
+     * 
+     * @param copy
+     *            the AbstractClusteringAO to copy.
+     */
+    public AbstractClusteringAO(AbstractClusteringAO copy) {
+        super(copy);
+        this.attributes = new ArrayList<SDFAttribute>(copy.getAttributes());
+    }
 
-	/**
-	 * Returns the list of attributes that should be used for clustering.
-	 * 
-	 * @return the list of attributes.
-	 */
-	public List<SDFAttribute> getAttributes() {
-		return attributes;
-	}
+    /**
+     * Returns the list of attributes that should be used for clustering.
+     * 
+     * @return the list of attributes.
+     */
+    public List<SDFAttribute> getAttributes() {
+        return attributes;
+    }
 
-	/**
-	 * Returns a list of indices identifying the positions of the attributes in
-	 * the input schema that should be used for clustering.
-	 * 
-	 * @return the list of indices identifying the positions of the attributes
-	 *         which should be used for clustering.
-	 * 
-	 */
-	public int[] determineRestrictList() {
-		return calcRestrictList(this.getInputSchema().getAttributes(), attributes);
-	}
+    /**
+     * Returns a list of indices identifying the positions of the attributes in
+     * the input schema that should be used for clustering.
+     * 
+     * @return the list of indices identifying the positions of the attributes
+     *         which should be used for clustering.
+     * 
+     */
+    public int[] determineRestrictList() {
+        return calcRestrictList(this.getInputSchema().getAttributes(), attributes);
+    }
 
-	/**
-	 * Returns a list of indices identifying the positions of the attributes in
-	 * the input schema that should be used for clustering.
-	 * 
-	 * @param inputSchema
-	 *            the input scheme.
-	 * @param attributes
-	 *            the list of attributes that should be used for clustering.
-	 * @return the list of indices identifying the positions of the attributes
-	 *         which should be used for clustering.
-	 */
+    /**
+     * Returns a list of indices identifying the positions of the attributes in
+     * the input schema that should be used for clustering.
+     * 
+     * @param inputSchema
+     *            the input scheme.
+     * @param attributes
+     *            the list of attributes that should be used for clustering.
+     * @return the list of indices identifying the positions of the attributes
+     *         which should be used for clustering.
+     */
 
-	public static int[] calcRestrictList(Collection<SDFAttribute> inputSchema,
-			List<SDFAttribute> attributes) {
-		int[] ret = new int[attributes.size()];
-		int i = 0;
-		for (SDFAttribute a : attributes) {
-			int j = 0;
-			int k = i;
-			for (SDFAttribute b : inputSchema) {
-				if (b.equals(a)) {
-					ret[i++] = j;
-				}
-				++j;
-			}
-			if (k == i) {
-				throw new IllegalArgumentException("no such attribute: " + a);
-			}
-		}
-		return ret;
-	}
+    public static int[] calcRestrictList(Collection<SDFAttribute> inputSchema, List<SDFAttribute> attributes) {
+        int[] ret = new int[attributes.size()];
+        int i = 0;
+        for (SDFAttribute a : attributes) {
+            int j = 0;
+            int k = i;
+            for (SDFAttribute b : inputSchema) {
+                if (b.equals(a)) {
+                    ret[i++] = j;
+                }
+                ++j;
+            }
+            if (k == i) {
+                throw new IllegalArgumentException("no such attribute: " + a);
+            }
+        }
+        return ret;
+    }
 
-	/**
-	 * Sets the list of attributes that should be used for clustering.
-	 * 
-	 * @param attributes
-	 *            the list of attributes.
-	 */
-	@Parameter(type = ResolvedSDFAttributeParameter.class, isList = true)
-	public void setAttributes(List<SDFAttribute> attributes) {
-		this.attributes = attributes;
-	}
+    /**
+     * Sets the list of attributes that should be used for clustering.
+     * 
+     * @param attributes
+     *            the list of attributes.
+     */
+    @Parameter(type = ResolvedSDFAttributeParameter.class, isList = true)
+    public void setAttributes(List<SDFAttribute> attributes) {
+        this.attributes = attributes;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * de.uniol.inf.is.odysseus.core.server.logicaloperator.ILogicalOperator#getOutputSchema
-	 * ()
-	 */
-	@Override
-	public SDFSchema getOutputSchema() {
-		List<SDFAttribute> elems = new ArrayList<SDFAttribute>();
-		SDFAttribute id = new SDFAttribute(null,"cluster_id", SDFDatatype.INTEGER);
-		elems.add(id);
-		elems.addAll(getInputSchema().clone().getAttributes());
-		SDFSchema outputSchema = new SDFSchema("Cluster", elems);
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.uniol.inf.is.odysseus.core.server.logicaloperator.ILogicalOperator
+     * #getOutputSchema ()
+     */
+    @Override
+    public SDFSchema getOutputSchema() {
+        List<SDFAttribute> elems = new ArrayList<SDFAttribute>();
+        SDFAttribute id = new SDFAttribute(null, "cluster_id", SDFDatatype.INTEGER);
+        elems.add(id);
+        elems.addAll(getInputSchema().clone().getAttributes());
+        SDFSchema outputSchema = new SDFSchema("Cluster", elems);
 
-		return outputSchema;
-	}
+        return outputSchema;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.uniol.inf.is.odysseus.core.server.logicaloperator.AbstractLogicalOperator#
-	 * getOutputSchema(int)
-	 */
-	@Override
-	public SDFSchema getOutputSchema(int port) {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.uniol.inf.is.odysseus.core.server.logicaloperator.AbstractLogicalOperator
+     * # getOutputSchema(int)
+     */
+    @Override
+    public SDFSchema getOutputSchema(int port) {
 
-		if (port == 0) {
-			return getOutputSchema();
-		} else {
-			List<SDFAttribute> attribs = new ArrayList<SDFAttribute>();
-			SDFAttribute idA = new SDFAttribute(null,"cluster_id", SDFDatatype.INTEGER);
-			attribs.add(idA);
-			SDFAttribute idCount = new SDFAttribute(null,"cluster_count", SDFDatatype.LONG);
-			attribs.add(idCount);
-			attribs.addAll(attributes);
-			SDFSchema clusterSchema = new SDFSchema("Cluster", attribs);
-			return clusterSchema;
-		}
-	}
+        if (port == 0) {
+            return getOutputSchema();
+        }
+        List<SDFAttribute> attribs = new ArrayList<SDFAttribute>();
+        SDFAttribute idA = new SDFAttribute(null, "cluster_id", SDFDatatype.INTEGER);
+        attribs.add(idA);
+        SDFAttribute idCount = new SDFAttribute(null, "cluster_count", SDFDatatype.LONG);
+        attribs.add(idCount);
+        attribs.addAll(attributes);
+        SDFSchema clusterSchema = new SDFSchema("Cluster", attribs);
+        return clusterSchema;
 
-	@Override
-	public boolean isValid() {
-		Iterator<SDFAttribute> iter = attributes.iterator();
-		while (iter.hasNext()) {
-			SDFAttribute attribute = iter.next();
-			if (!attribute.getDatatype().isNumeric()) {
-				addError(new NonNumericAttributeException(attribute));
-				return false;
-			}
-		}
-		return true;
-	}
+    }
+
+    @Override
+    public boolean isValid() {
+        Iterator<SDFAttribute> iter = attributes.iterator();
+        while (iter.hasNext()) {
+            SDFAttribute attribute = iter.next();
+            if (!attribute.getDatatype().isNumeric()) {
+                addError(new NonNumericAttributeException(attribute));
+                return false;
+            }
+        }
+        return true;
+    }
 }
