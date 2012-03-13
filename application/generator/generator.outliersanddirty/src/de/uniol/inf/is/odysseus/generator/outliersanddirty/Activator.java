da225768d102eb92a94770923a062d1ebedc3b95
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-13 13:38:23 +0000

    Removed many warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@10965 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 76eb84d82a4638adcdf4d36c5516b6dbeec2b6f9

--- application/generator/generator.outliersanddirty/src/de/uniol/inf/is/odysseus/generator/outliersanddirty/Activator.java
+++ application/generator/generator.outliersanddirty/src/de/uniol/inf/is/odysseus/generator/outliersanddirty/Activator.java
@@ -17,7 +17,8 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext bundleContext) throws Exception {
+	@Override
+    public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		
 		System.out.println("Starting Wind Generator server...");
@@ -33,7 +34,8 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext bundleContext) throws Exception {
+	@Override
+    public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 	}
 
