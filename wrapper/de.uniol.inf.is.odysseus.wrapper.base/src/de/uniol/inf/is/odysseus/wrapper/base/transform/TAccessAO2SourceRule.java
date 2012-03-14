7381b22ec58cbcc483b88d296bf8c38ae22e218d
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-03-14 16:08:45 +0000

    Missing Transformation Rule
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11299 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 49e4ea3f2a68883c1fdb92b4eb6ec88050a6284e

---
+++ wrapper/de.uniol.inf.is.odysseus.wrapper.base/src/de/uniol/inf/is/odysseus/wrapper/base/transform/TAccessAO2SourceRule.java
@@ -0,0 +1,65 @@
+package de.uniol.inf.is.odysseus.wrapper.base.transform;
+
+import java.util.Collection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.AccessAO;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.ruleengine.ruleflow.IRuleFlowGroup;
+import de.uniol.inf.is.odysseus.transform.flow.TransformRuleFlowGroup;
+import de.uniol.inf.is.odysseus.transform.rule.AbstractTransformationRule;
+import de.uniol.inf.is.odysseus.wrapper.base.logicaloperator.SourceAO;
+import de.uniol.inf.is.odysseus.wrapper.base.physicaloperator.SourcePO;
+import de.uniol.inf.is.odysseus.wrapper.base.pool.SourcePool;
+
+public class TAccessAO2SourceRule extends AbstractTransformationRule<AccessAO> {
+    private static Logger LOG = LoggerFactory.getLogger(TAccessAO2SourceRule.class);
+
+    @Override
+    public int getPriority() {
+        return 10;
+    }
+
+    @Override
+    public void execute(final AccessAO operator, final TransformationConfiguration config) {
+        try {
+            @SuppressWarnings({ "rawtypes", "unchecked" })
+			SourcePO<?> po = new SourcePO(operator.getOutputSchema(), operator.getAdapter(),
+                    operator.getOptionsMap());
+//            if (SourcePool.hasSemanticallyEqualSource(po)) {
+//                po = SourcePool.getSemanticallyEqualSource(po);
+//            }else {
+                SourcePool.registerSource(operator.getAdapter(), po, operator.getOptionsMap());
+//            }
+            final Collection<ILogicalOperator> toUpdate = config.getTransformationHelper().replace(
+                    operator, po);
+            for (final ILogicalOperator o : toUpdate) {
+                this.update(o);
+            }
+            retract(operator);
+            insert(po);
+        }
+        catch (final Exception e) {
+            TAccessAO2SourceRule.LOG.error(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public boolean isExecutable(final AccessAO operator, final TransformationConfiguration config) {
+        return (operator.getAdapter() != null);
+    }
+
+    @Override
+    public String getName() {
+        return "AccessAO -> SourcePO";
+    }
+
+    @Override
+    public IRuleFlowGroup getRuleFlowGroup() {
+        return TransformRuleFlowGroup.ACCESS;
+    }
+
+}
