70d0463c822f101b94276223df3c707da80f5bb2
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-16 08:11:13 +0000

    Refactored UserManagement
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11324 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f53d4ea97f7db9ed95ff2b4318d05f368a6fecd5

--- core/usermanagement.filestore/src/de/uniol/inf/is/odysseus/usermanagement/filestore/service/impl/UserManagementServiceImpl.java
+++ core/usermanagement.filestore/src/de/uniol/inf/is/odysseus/usermanagement/filestore/service/impl/UserManagementServiceImpl.java
@@ -1,8 +1,11 @@
 package de.uniol.inf.is.odysseus.usermanagement.filestore.service.impl;
 
+import java.io.IOException;
+
 import org.osgi.service.component.ComponentContext;
 
 import de.uniol.inf.is.odysseus.core.server.usermanagement.AbstractUserManagement;
+import de.uniol.inf.is.odysseus.core.server.usermanagement.IGenericDAO;
 import de.uniol.inf.is.odysseus.usermanagement.filestore.service.dao.PrivilegeDAO;
 import de.uniol.inf.is.odysseus.usermanagement.filestore.service.dao.RoleDAO;
 import de.uniol.inf.is.odysseus.usermanagement.filestore.service.dao.UserDAO;
@@ -13,6 +16,10 @@ import de.uniol.inf.is.odysseus.usermanagement.filestore.service.domain.User;
 public class UserManagementServiceImpl extends
 		AbstractUserManagement<User, Role, Privilege> {
 
+    private IGenericDAO<User, String> userDAO;
+    private IGenericDAO<Role, String> roleDAO;
+    private IGenericDAO<Privilege, String> privilegeDAO;
+    
 	@Override
 	protected Role createEmptyRole() {
 		return new Role();
@@ -29,19 +36,48 @@ public class UserManagementServiceImpl extends
 	}
 
 	protected void activate(ComponentContext context) {
-		try {
-			userDAO = UserDAO.getInstance();
-			roleDAO = RoleDAO.getInstance();
-			privilegeDAO = PrivilegeDAO.getInstance();
-
-			initDefaultUsers();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	    initDefaultUsers();
 	}
 
 	protected void deactivate(ComponentContext context) {
 
 	}
+
+    @Override
+    protected IGenericDAO<User, String> getUserDAO() {
+        try {
+            if( userDAO == null ) {
+                userDAO = UserDAO.getInstance();
+                initDefaultUsers();
+            }
+            return userDAO;
+        } catch (IOException e) {
+            throw new RuntimeException("Could not get UserDAO", e);
+        }
+    }
+
+    @Override
+    protected IGenericDAO<Role, String> getRoleDAO() {
+        try {
+            if( roleDAO == null ) {
+                roleDAO = RoleDAO.getInstance();
+            }
+            return roleDAO;
+        } catch (IOException e) {
+            throw new RuntimeException("Could not get RoleDAO", e);
+        }
+    }
+
+    @Override
+    protected IGenericDAO<Privilege, String> getPrivilegeDAO() {
+        try {
+            if( privilegeDAO == null ) {
+                privilegeDAO = PrivilegeDAO.getInstance();
+            }
+            return privilegeDAO;
+        } catch (IOException e) {
+            throw new RuntimeException("Could not get PrivilegeDAO", e);
+        }
+    }
 
 }
