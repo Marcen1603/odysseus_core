fe07f95136c87fae9b846beea39cf8ae3453faa8
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-16 08:11:13 +0000

    Refactored UserManagement
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11323 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 06ded7bf063751fe367a212dbf981d5890fc0470

--- core/usermanagement.mem/src/de/uniol/inf/is/odysseus/usermanagement/mem/service/impl/UserManagementServiceImpl.java
+++ core/usermanagement.mem/src/de/uniol/inf/is/odysseus/usermanagement/mem/service/impl/UserManagementServiceImpl.java
@@ -3,6 +3,7 @@ package de.uniol.inf.is.odysseus.usermanagement.mem.service.impl;
 import org.osgi.service.component.ComponentContext;
 
 import de.uniol.inf.is.odysseus.core.server.usermanagement.AbstractUserManagement;
+import de.uniol.inf.is.odysseus.core.server.usermanagement.IGenericDAO;
 import de.uniol.inf.is.odysseus.usermanagement.mem.service.dao.PrivilegeDAO;
 import de.uniol.inf.is.odysseus.usermanagement.mem.service.dao.RoleDAO;
 import de.uniol.inf.is.odysseus.usermanagement.mem.service.dao.UserDAO;
@@ -10,35 +11,58 @@ import de.uniol.inf.is.odysseus.usermanagement.mem.service.domain.Privilege;
 import de.uniol.inf.is.odysseus.usermanagement.mem.service.domain.Role;
 import de.uniol.inf.is.odysseus.usermanagement.mem.service.domain.User;
 
-public class UserManagementServiceImpl extends AbstractUserManagement<User,Role, Privilege> {
+public class UserManagementServiceImpl extends AbstractUserManagement<User, Role, Privilege> {
 
-	@Override
-	protected Role createEmptyRole() {
-		return new Role();
-	}
+    private IGenericDAO<User, String> userDAO;
+    private IGenericDAO<Role, String> roleDAO;
+    private IGenericDAO<Privilege, String> privilegeDAO;
 
-	@Override
-	protected User createEmptyUser() {
-		return new User();
-	}
+    @Override
+    protected Role createEmptyRole() {
+        return new Role();
+    }
 
-	@Override
-	protected Privilege createEmptyPrivilege() {
-		return new Privilege();
-	}
+    @Override
+    protected User createEmptyUser() {
+        return new User();
+    }
 
-	protected void activate(ComponentContext context) {
-		
-		userDAO = UserDAO.getInstance();
-		roleDAO = RoleDAO.getInstance();
-		privilegeDAO = PrivilegeDAO.getInstance();
-		
-		initDefaultUsers();		
-	}
+    @Override
+    protected Privilege createEmptyPrivilege() {
+        return new Privilege();
+    }
 
-	protected void deactivate(ComponentContext context) {
+    protected void activate(ComponentContext context) {
+        initDefaultUsers();
+    }
 
-	}
+    protected void deactivate(ComponentContext context) {
 
+    }
+
+    @Override
+    protected IGenericDAO<User, String> getUserDAO() {
+        if (userDAO == null) {
+            userDAO = UserDAO.getInstance();
+            initDefaultUsers();
+        }
+        return userDAO;
+    }
+
+    @Override
+    protected IGenericDAO<Role, String> getRoleDAO() {
+        if (roleDAO == null) {
+            roleDAO = RoleDAO.getInstance();
+        }
+        return roleDAO;
+    }
+
+    @Override
+    protected IGenericDAO<Privilege, String> getPrivilegeDAO() {
+        if (privilegeDAO == null) {
+            privilegeDAO = PrivilegeDAO.getInstance();
+        }
+        return privilegeDAO;
+    }
 
 }
