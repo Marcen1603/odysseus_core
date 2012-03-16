c191d539cdb8ed4c4152b86b314d4377c791eab7
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-16 13:37:34 +0000

    Added missing @Override
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11346 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b246081d26e4229f984f9bf77a90c7d6e277110b

--- core/base/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/access/AbstractByteBufferHandler.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/access/AbstractByteBufferHandler.java
@@ -2,6 +2,7 @@ package de.uniol.inf.is.odysseus.core.server.physicaloperator.access;
 
 abstract public class AbstractByteBufferHandler<R,W> implements IInputDataHandler<R,W> {
 
-	public abstract IInputDataHandler<R,W> clone();
+	@Override
+    public abstract IInputDataHandler<R,W> clone();
 	
 }
