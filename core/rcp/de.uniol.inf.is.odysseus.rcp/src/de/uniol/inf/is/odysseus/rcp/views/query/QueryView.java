0d01ed32de176a76dba1e3fbc0d92f9e39fd78e8
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-03-20 13:07:23 +0000

    Moved QueryView in own package (preparations)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11373 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9f56c3f50159c1bf646a32ce861bdec8d5401073

--- core/rcp/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/views/query/QueryView.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/views/query/QueryView.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.uniol.inf.is.odysseus.rcp.views;
+package de.uniol.inf.is.odysseus.rcp.views.query;
 
 import java.util.ArrayList;
 import java.util.Collection;
