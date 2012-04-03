c92353f3a189e577a3428d57e76c263d2a03bb7b
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-04-03 10:45:40 +0000

    FIX: Use number instead of index
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11496 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 826a1e0aeb362fc22debd1b451e416fabcd2f475

--- wrapper/google.protobuf.base/src/de/uniol/inf/is/odysseus/wrapper/google/protobuf/base/GeneratedMessageToTuple.java
+++ wrapper/google.protobuf.base/src/de/uniol/inf/is/odysseus/wrapper/google/protobuf/base/GeneratedMessageToTuple.java
@@ -1,5 +1,6 @@
 package de.uniol.inf.is.odysseus.wrapper.google.protobuf.base;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -7,16 +8,30 @@ import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.GeneratedMessage;
 
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.access.AbstractTransformer;
 import de.uniol.inf.is.odysseus.relational.base.Tuple;
 
 public class GeneratedMessageToTuple extends
 		AbstractTransformer<GeneratedMessage, Tuple<? extends IMetaAttribute>> {
 
-	static GeneratedMessageToTuple instance = new GeneratedMessageToTuple();
+	static Map<SDFSchema,GeneratedMessageToTuple> instanceMap = new HashMap<SDFSchema, GeneratedMessageToTuple>();
+	private SDFSchema schema;
 
-	public static GeneratedMessageToTuple getInstance() {
-		return instance;
+	private GeneratedMessageToTuple(SDFSchema schema) {
+		if (schema == null){
+			throw new IllegalArgumentException("Schema cannot be null!");
+		}
+		this.schema = schema;
+	}
+
+	public static GeneratedMessageToTuple getInstance(SDFSchema schema) {
+		GeneratedMessageToTuple ret = instanceMap.get(schema);
+		if (ret == null){
+			ret = new GeneratedMessageToTuple(schema);
+			instanceMap.put(schema, ret);
+		}
+		return ret;
 	}
 
 	@Override
@@ -24,11 +39,12 @@ public class GeneratedMessageToTuple extends
 
 		Map<FieldDescriptor, Object> test = input.getAllFields();
 		@SuppressWarnings("rawtypes")
-		Tuple<?> ret = new Tuple(test.size());
+		Tuple<?> ret = new Tuple(schema.size());
 
 		for (Entry<FieldDescriptor, Object> ent : test.entrySet()) {
 			if (ent.getValue() != null && ent.getKey() != null) {
-				ret.setAttribute(ent.getKey().getIndex(), ent.getValue());
+				// Numbers starting with 1
+				ret.setAttribute(ent.getKey().getNumber()-1, ent.getValue());
 			}
 		}
 
