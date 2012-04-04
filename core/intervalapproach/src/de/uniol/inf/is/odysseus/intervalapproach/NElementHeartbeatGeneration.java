73147c47bc6353eae8bf1a69b5e6296ac0bf4678
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-04-04 13:40:25 +0000

    HeartbeatGenerationStrategie that send each n call an heart beat
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11528 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 66bd7482dc197bfda28b0891f5734ecbec3aa834

---
+++ core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/NElementHeartbeatGeneration.java
@@ -0,0 +1,42 @@
+package de.uniol.inf.is.odysseus.intervalapproach;
+
+import de.uniol.inf.is.odysseus.core.physicaloperator.ISource;
+import de.uniol.inf.is.odysseus.core.server.metadata.IMetaAttributeContainer;
+import de.uniol.inf.is.odysseus.core.server.metadata.ITimeInterval;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.IHeartbeatGenerationStrategy;
+
+public class NElementHeartbeatGeneration <K extends ITimeInterval, T extends IMetaAttributeContainer<K>> implements IHeartbeatGenerationStrategy<T>{
+
+	private int counter = 0;
+	final private int eachElement;
+	
+	public NElementHeartbeatGeneration() {
+		eachElement = 10;
+	}
+
+	public NElementHeartbeatGeneration(int eachElement) {
+		this.eachElement = eachElement;
+	}
+
+	public NElementHeartbeatGeneration(
+			NElementHeartbeatGeneration<K, T> nElementHeartbeatGeneration) {
+		this.eachElement = nElementHeartbeatGeneration.eachElement;
+		this.counter = nElementHeartbeatGeneration.counter;
+	}
+
+	@Override
+	public void generateHeartbeat(T object, ISource<?> source) {
+		if (counter % eachElement == 0){
+			counter = 0;
+			source.sendPunctuation(object.getMetadata().getStart());
+		}
+		counter++;
+	}
+	
+
+	@Override
+	public NElementHeartbeatGeneration<K, T> clone() {
+		return new NElementHeartbeatGeneration<K, T>(this);
+	}
+
+}
