c083c763aba732a9ef3c6fc6ac48fbd845b07ddb
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-04-13 05:35:12 +0000

    Removed Errors (bundle currently not used)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11565 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 487827f63b2cbb56ee4117052a7f36e0bef6dc7c

--- wrapper/de.uniol.inf.is.odysseus.wrapper.google.protobuf.grandchallenge/src/de/uniol/inf/is/odysseus/wrapper/google/protobuf/grandchallenge/RegisterDatatype.java
+++ wrapper/de.uniol.inf.is.odysseus.wrapper.google.protobuf.grandchallenge/src/de/uniol/inf/is/odysseus/wrapper/google/protobuf/grandchallenge/RegisterDatatype.java
@@ -3,14 +3,12 @@ package de.uniol.inf.is.odysseus.wrapper.google.protobuf.grandchallenge;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import de.uniol.inf.is.odysseus.wrapper.google.protobuf.base.ProtobufTypeRegistry;
-import debs.challenge.msg.CManufacturingMessages.CDataPoint;
 
 public class RegisterDatatype implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		ProtobufTypeRegistry.registerMessageType(CDataPoint.getDescriptor().getFullName(), CDataPoint.getDefaultInstance());
+	//	ProtobufTypeRegistry.registerMessageType(CDataPoint.getDescriptor().getFullName(), CDataPoint.getDefaultInstance());
 	}
 
 	@Override
