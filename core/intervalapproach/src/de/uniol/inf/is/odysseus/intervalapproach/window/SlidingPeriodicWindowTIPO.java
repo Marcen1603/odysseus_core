35c12c3942cf298bd36ac598489083c401504671
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-04-13 12:22:23 +0000

    FIX: Wrong interval calculated
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11576 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 92e75c1f12b76cb974e60ca2a4b23972aefdd059

--- core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/window/SlidingPeriodicWindowTIPO.java
+++ core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/window/SlidingPeriodicWindowTIPO.java
@@ -1,17 +1,17 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /*
  * SlidingDeltaWindowPO.java
  *
@@ -35,12 +35,12 @@ import de.uniol.inf.is.odysseus.core.server.metadata.ITimeInterval;
  * This is the physical sliding delta window po. It returns elements after a
  * period of time (delta) and is blocking all the other time.
  * 
- * @author Andre Bolles <andre.bolles@informatik.uni-oldenburg.de>, Marco Grawunder
+ * @author Andre Bolles <andre.bolles@informatik.uni-oldenburg.de>, Marco
+ *         Grawunder
  */
 public class SlidingPeriodicWindowTIPO<R extends IMetaAttributeContainer<? extends ITimeInterval>>
 		extends AbstractWindowTIPO<R> {
 
-	
 	/**
 	 * This is the number of slides that have been processed.
 	 */
@@ -51,9 +51,8 @@ public class SlidingPeriodicWindowTIPO<R extends IMetaAttributeContainer<? exten
 	 */
 	final private long windowSlide;
 
-	
 	/**
-	 * list with buffered elements 
+	 * list with buffered elements
 	 */
 	private LinkedList<R> inputBuffer = new LinkedList<R>();
 
@@ -61,6 +60,7 @@ public class SlidingPeriodicWindowTIPO<R extends IMetaAttributeContainer<? exten
 	public SlidingPeriodicWindowTIPO(WindowAO logical) {
 		super(logical);
 		this.windowSlide = logical.getWindowSlide();
+		setName(getName() + " size=" + windowSize + " slide=" + windowSlide);
 	}
 
 	public SlidingPeriodicWindowTIPO(SlidingPeriodicWindowTIPO<R> original) {
@@ -77,48 +77,58 @@ public class SlidingPeriodicWindowTIPO<R extends IMetaAttributeContainer<? exten
 	public OutputMode getOutputMode() {
 		return OutputMode.MODIFIED_INPUT;
 	}
-	
+
 	@Override
 	protected void process_open() throws OpenFailedException {
-		slideNo = 1;
+		slideNo = -1;
 		inputBuffer.clear();
 	}
 
 	@Override
-	public synchronized void process_next(R object, int port) {
-		this.inputBuffer.add(object);
-		process(object.getMetadata().getStart());
-	}
-
-	private synchronized void process(PointInTime point) {
-		long delta = this.windowSlide;
-		long winSize = this.windowSize;
-		long pointInTime = point.getMainPoint();
-		// first check if elements have to be removed and/or delivered
-		if ((pointInTime / delta) >= this.slideNo) {
-			this.slideNo = pointInTime / delta;
-
-			// Calc the start and end point of the window for all elements that
-			// will be transfered
-			long comp_t_start = this.slideNo * delta - winSize;
-			PointInTime p_start = new PointInTime(Math.max(comp_t_start, 0));
-			PointInTime p_end = new PointInTime(this.slideNo * delta);
-			
-			// 1. Remove all Elements that are not within the window size!
-			while(!inputBuffer.isEmpty() && inputBuffer.getFirst().getMetadata().getStart().before(p_start)){
-				inputBuffer.removeFirst();
-			}
-			
-			// 2. all elements before p_end need to be processed
-			while(!inputBuffer.isEmpty() && inputBuffer.getFirst().getMetadata().getStart().before(p_end)){
-				R elem = inputBuffer.removeFirst();
-				elem.getMetadata().setStart(p_start);
-				elem.getMetadata().setEnd(p_end);
-				transfer(elem);
-			}	
+	public void process_next(R object, int port) {
+		synchronized (inputBuffer) {
+			this.inputBuffer.add(object);
+			process(object.getMetadata().getStart());
 		}
 	}
-	
+
+	private void process(PointInTime point) {
+		synchronized (inputBuffer) {
+			long delta = this.windowSlide;
+			long winSize = this.windowSize;
+			long pointInTime = point.getMainPoint();
+			// first check if elements have to be removed and/or delivered
+			long slide = pointInTime / delta;
+			if (slide > this.slideNo) {
+				this.slideNo = slide;
+
+				// Calc the start and end point of the window for all elements
+				// that
+				// will be transfered
+				long comp_t_start = (slide+1) * delta - winSize;
+				PointInTime p_start = new PointInTime(Math.max(comp_t_start, 0));
+				PointInTime p_end = new PointInTime((slide + 1) * delta);
+
+				// 1. Remove all Elements that are not within the window size!
+				while (!inputBuffer.isEmpty()
+						&& inputBuffer.getFirst().getMetadata().getStart()
+								.before(p_start)) {
+					inputBuffer.removeFirst();
+				}
+
+				// 2. all elements before p_end need to be processed
+				while (!inputBuffer.isEmpty()
+						&& inputBuffer.getFirst().getMetadata().getStart()
+								.before(p_end)) {
+					R elem = inputBuffer.removeFirst();
+					elem.getMetadata().setStart(p_start);
+					elem.getMetadata().setEnd(p_end);
+					transfer(elem);
+				}
+			}
+		}
+	}
+
 	@Override
 	public synchronized void processPunctuation(PointInTime timestamp, int port) {
 		process(timestamp);
