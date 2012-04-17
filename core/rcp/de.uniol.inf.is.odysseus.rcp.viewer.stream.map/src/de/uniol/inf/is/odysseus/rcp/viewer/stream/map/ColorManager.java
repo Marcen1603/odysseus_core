5b20e2b0bb79bb1827812f6a6dc6fb17dc3989b3
Stephan Jansen <sjansen@uni-oldenburg.de>
2012-04-17 11:33:15 +0000

    StreamMap: Fix: Added missing Class
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11598 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 478de72b24d6e367514aa04de988997c002b939b

---
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.viewer.stream.map/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/map/ColorManager.java
@@ -0,0 +1,67 @@
+package de.uniol.inf.is.odysseus.rcp.viewer.stream.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
+
+public class ColorManager {
+	
+	private static ColorManager instance = null;
+	private Map<int[], Color> colors = new HashMap<int[], Color>();
+	private Map<Object, Image> images = new HashMap<Object, Image>();
+
+	
+	public static ColorManager getInstance() {
+		if( instance == null ) 
+			instance = new ColorManager();
+		return instance;
+	}
+	
+	public Color getColor(RGB rgb) {
+		int[] key = new int[]{rgb.red, rgb.green, rgb.blue};
+		Color c = colors.get(key);
+		if (c == null){
+			colors.put(key, c = new Color(Display.getCurrent(), rgb));
+		}	
+		return c;
+	}
+	
+	public Image getImage(Object o){
+		Image i = images.get(o);
+		return i;
+	}
+	
+	public void addImage(Object o , Image i){
+		Image old = images.put(o, i);
+		if (old != null) old.dispose();
+	}
+	
+	public void dispose(){
+		for (Color c : colors.values()) {
+			c.dispose();
+		}
+		colors.clear();
+		for (Image i : images.values()) {
+			i.dispose();
+		}
+		images.clear();
+	}
+
+	int[] random = {1, 255, 127, 63, 0};
+	public Color randomColor() {
+		int r = (random.length-1)/random[0];
+		int g = (random.length-1)%random[0];
+		int b = g/random[0]++;
+		int[] key = new int[]{random[r], random[g], random[b]};
+		Color c = colors.get(key);
+		if (c == null){
+			colors.put(key, c = new Color(Display.getCurrent(), new RGB(key[0], key[1], key[2])));
+		}	
+		return c;
+	}
+
+}
