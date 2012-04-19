b072504947d3e268d0fe404f8efd33e67375b6c3
Dennis Geesen <geesen@indivon.de>
2012-04-19 08:49:51 +0000

    regression: produce only tuples if there is a valid regression result (!=NaN)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11636 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9743c83ae6bf84cac5e62b15dccd9603cf10cfab

--- mining/statistics/src/de/uniol/inf/is/odysseus/statistics/physicaloperator/RegressionPO.java
+++ mining/statistics/src/de/uniol/inf/is/odysseus/statistics/physicaloperator/RegressionPO.java
@@ -67,32 +67,33 @@ public class RegressionPO<M extends ITimeInterval, R extends Tuple<M>> extends A
 	@Override
 	protected synchronized void process_next(R object, int port) {
 		double x = 0;
-		if(object.getAttribute(xAttribute) instanceof Number){
-			x = ((Number)object.getAttribute(xAttribute)).doubleValue();
+		if (object.getAttribute(xAttribute) instanceof Number) {
+			x = ((Number) object.getAttribute(xAttribute)).doubleValue();
 		}
 		double y = 0;
-		if(object.getAttribute(yAttribute) instanceof Number){
-			y = ((Number)object.getAttribute(yAttribute)).doubleValue();
-		}		 
+		if (object.getAttribute(yAttribute) instanceof Number) {
+			y = ((Number) object.getAttribute(yAttribute)).doubleValue();
+		}
 
 		sweepArea.insert(object);
 
 		Iterator<R> iter = sweepArea.extractElementsBefore(object.getMetadata().getStart());
 		while (iter.hasNext()) {
 			R toRemove = iter.next();
-			double cuX = ((Number)toRemove.getAttribute(xAttribute)).doubleValue();
-			double cuY = ((Number)toRemove.getAttribute(yAttribute)).doubleValue();
+			double cuX = ((Number) toRemove.getAttribute(xAttribute)).doubleValue();
+			double cuY = ((Number) toRemove.getAttribute(yAttribute)).doubleValue();
 			regression.removeData(cuX, cuY);
 		}
 		regression.addData(x, y);
-		 System.err.println("Regression: "+regression.getN());
-		 System.err.println("----------------------");
-		 System.err.println(sweepArea.getSweepAreaAsString(object.getMetadata().getStart()));
-		 System.err.println("----------------------");
+		System.err.println("Regression: " + regression.getN());
+		System.err.println("----------------------");
+		System.err.println(sweepArea.getSweepAreaAsString(object.getMetadata().getStart()));
+		System.err.println("----------------------");
 		double slope = regression.getSlope();
 		double intercept = regression.getIntercept();
-		//only transfer, if one of the parameters change
-		//if (lastSlope != slope || lastIntercept != intercept) {
+		// only transfer, if one of the parameters change
+		// if (lastSlope != slope || lastIntercept != intercept) {
+		if (!Double.isNaN(slope) && !Double.isNaN(intercept)) {
 			lastSlope = slope;
 			lastIntercept = intercept;
 			Tuple<M> ne = new Tuple<M>(2);
@@ -101,10 +102,12 @@ public class RegressionPO<M extends ITimeInterval, R extends Tuple<M>> extends A
 			@SuppressWarnings("unchecked")
 			M meta = (M) object.getMetadata().clone();
 			meta.setStart(sweepArea.getMinTs());
-			meta.setEnd(object.getMetadata().getEnd());			
+			meta.setEnd(object.getMetadata().getEnd());
 			ne.setMetadata(meta);
+
 			transfer(ne);
-		//}
+		}
+		// }
 	}
 
 	@Override
