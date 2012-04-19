7a6858ad98e3c64b260e349420f29599999684a7
Dennis Geesen <geesen@indivon.de>
2012-04-19 08:04:29 +0000

    bessere anpassung der bounds bei linear function charts
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11629 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: cae267e07216b2b1b1be29fa9415f110b4839905

--- core/rcp/rcp.viewer.stream.chart/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/chart/charts/LinearFunctionChart.java
+++ core/rcp/rcp.viewer.stream.chart/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/chart/charts/LinearFunctionChart.java
@@ -1,17 +1,17 @@
 /** Copyright 2012 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.charts;
 
@@ -30,24 +30,32 @@ import de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.schema.IViewableAttribut
 
 /**
  * 
- * @author Dennis Geesen
- * Created at: 18.04.2012
+ * @author Dennis Geesen Created at: 18.04.2012
  */
 public class LinearFunctionChart extends AbstractChart<Double, IMetaAttribute> {
-	
+
 	private XYSeriesCollection dataset = new XYSeriesCollection();
 	private int choosenXValue = 0;
 	private int choosenYValue = 1;
+	private double min = -1;
+	private double max = 3;
 
 	@Override
 	public void chartSettingsChanged() {
-		
-		
+		getChart().getXYPlot().getRangeAxis().setAutoRange(false);
+		resetRanges();
+
+	}
+	
+	
+	private void resetRanges(){
+		getChart().getXYPlot().getRangeAxis().setLowerBound(min);
+		getChart().getXYPlot().getRangeAxis().setUpperBound(max);
 	}
 
 	@Override
 	public String isValidSelection(List<IViewableAttribute> selectAttributes) {
-		if(selectAttributes.size()>1){
+		if (selectAttributes.size() > 1) {
 			return null;
 		}
 		return "The number of choosen attributes should be at least two!";
@@ -58,26 +66,33 @@ public class LinearFunctionChart extends AbstractChart<Double, IMetaAttribute> {
 		getSite().getShell().getDisplay().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-				try {				
-						dataset.removeAllSeries();
-						double m = tuple.get(choosenXValue);
-						double b = tuple.get(choosenYValue);						
-						XYSeries series = new XYSeries("f");
-						series.add(0, b);
-						series.add(1, m+b);
-						dataset.addSeries(series);										
-				} catch (SWTException e) {								
+				try {
+					dataset.removeAllSeries();
+					double m = tuple.get(choosenXValue);
+					double b = tuple.get(choosenYValue);
+					XYSeries series = new XYSeries("f");
+					if(b<min){
+						min = b;
+					}
+					if((m+b)>max){
+						max = m+b;
+					}
+					resetRanges();
+					series.add(0, b);
+					series.add(1, m + b);
+					dataset.addSeries(series);
+				} catch (SWTException e) {
 					dispose();
 					return;
 				}
 			}
 		});
-		
+
 	}
 
 	@Override
-	protected void decorateChart(JFreeChart thechart) {	
-		
+	protected void decorateChart(JFreeChart thechart) {
+
 	}
 
 	@Override
