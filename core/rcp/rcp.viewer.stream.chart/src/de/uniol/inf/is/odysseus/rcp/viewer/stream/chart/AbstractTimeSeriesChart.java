c70c0472d64d91b64b7e9e1253592777016c0525
Dennis Geesen <geesen@indivon.de>
2012-04-23 07:49:56 +0000

    Bei XY-Charts kann die X-Achse nun auch per Hand gewählt werden
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11663 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b47807a5fbd39349f0b0adbd6bed54a61d3b6660

--- core/rcp/rcp.viewer.stream.chart/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/chart/AbstractTimeSeriesChart.java
+++ core/rcp/rcp.viewer.stream.chart/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/chart/AbstractTimeSeriesChart.java
@@ -1,19 +1,21 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.rcp.viewer.stream.chart;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +25,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.data.general.SeriesException;
 import org.jfree.data.time.FixedMillisecond;
+import org.jfree.data.time.RegularTimePeriod;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 
@@ -39,9 +42,13 @@ public abstract class AbstractTimeSeriesChart extends AbstractChart<Double, ITim
 
 	private static final int DEFAULT_MAX_NUMBER_OF_ITEMS = 100;
 
+	private static final String CURRENT_TIME = "Current Time";
+
 	private int maxItems = DEFAULT_MAX_NUMBER_OF_ITEMS;
 
-	private String dateformat = "HH:mm";
+	private String dateformat = "SSS";
+
+	private int choosenXValue = -1;
 
 	@Override
 	public void chartSettingsChanged() {
@@ -72,10 +79,19 @@ public abstract class AbstractTimeSeriesChart extends AbstractChart<Double, ITim
 			@Override
 			public void run() {
 				try {
-					FixedMillisecond ms = new FixedMillisecond(metadata.getStart().getMainPoint());
-					for (int i = 0; i < tuple.size(); i++) {
-						double value = tuple.get(i);
-						series.get(getChoosenAttributes().get(i).getName()).add(ms, value);
+					if (choosenXValue == -1) {
+						FixedMillisecond ms = new FixedMillisecond(metadata.getStart().getMainPoint());
+						for (int i = 0; i < tuple.size(); i++) {
+							double value = tuple.get(i);
+							series.get(getChoosenAttributes().get(i).getName()).add(ms, value);
+						}
+					} else {
+						for (int i = 0; i < tuple.size(); i++) {
+							double value = tuple.get(i);	
+							long x = tuple.get(choosenXValue).longValue();
+							FixedMillisecond ms = new FixedMillisecond(x);
+							series.get(getChoosenAttributes().get(i).getName()).add(ms, value);
+						}
 					}
 
 				} catch (SWTException ex) {
@@ -83,7 +99,7 @@ public abstract class AbstractTimeSeriesChart extends AbstractChart<Double, ITim
 					dispose();
 					return;
 				} catch (SeriesException e) {
-					//System.out.println("Warn: " + e.getLocalizedMessage());
+					// System.out.println("Warn: " + e.getLocalizedMessage());
 				} catch (Exception e) {
 					e.printStackTrace();
 
@@ -117,6 +133,43 @@ public abstract class AbstractTimeSeriesChart extends AbstractChart<Double, ITim
 	@ChartSetting(name = "Date Format", type = Type.SET)
 	public void setDateFormat(String dateFormat) {
 		this.dateformat = dateFormat;
+	}
+
+	@ChartSetting(name = "Value for X-Axis", type = Type.OPTIONS)
+	public List<String> getXValues() {
+		return getValues();
+	}
+
+	private List<String> getValues() {
+		List<String> values = new ArrayList<String>();
+		values.add(CURRENT_TIME);
+		for (IViewableAttribute a : getViewableAttributes()) {
+			values.add(a.getName());
+		}
+
+		return values;
+	}
+
+	@ChartSetting(name = "Value for X-Axis", type = Type.GET)
+	public String getXValue() {
+		if (this.choosenXValue == -1) {
+			return CURRENT_TIME;
+		}
+		return getViewableAttributes().get(this.choosenXValue).getName();
+	}
+
+	@ChartSetting(name = "Value for X-Axis", type = Type.SET)
+	public void setXValue(String value) {
+		if (value.equalsIgnoreCase(CURRENT_TIME)) {
+			this.choosenXValue = -1;
+			return;
+		}
+		for (int i = 0; i < getViewableAttributes().size(); i++) {
+			if (getViewableAttributes().get(i).getName().equals(value)) {
+				this.choosenXValue = i;
+				return;
+			}
+		}
 	}
 
 }
