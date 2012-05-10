3468eace4a1e983c8df584e3163706cae125ec0b
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-05-10 09:44:29 +0000

    Replaced New-Wizard with better version
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11774 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 2c33202fb3f1db5a880caa9ce7241edc473a82ca

--- core/rcp/rcp.editor.text/src/de/uniol/inf/is/odysseus/rcp/editor/text/wizards/OdysseusScriptWizard.java
+++ core/rcp/rcp.editor.text/src/de/uniol/inf/is/odysseus/rcp/editor/text/wizards/OdysseusScriptWizard.java
@@ -1,33 +1,36 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.rcp.editor.text.wizards;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.FileEditorInput;
+
+import com.google.common.base.Optional;
 
 import de.uniol.inf.is.odysseus.rcp.editor.text.OdysseusRCPEditorTextPlugIn;
 import de.uniol.inf.is.odysseus.rcp.exception.ExceptionWindow;
@@ -35,7 +38,7 @@ import de.uniol.inf.is.odysseus.rcp.exception.ExceptionWindow;
 public class OdysseusScriptWizard extends Wizard implements INewWizard {
 
 	private OdysseusScriptWizardPage page;
-	
+
 	public OdysseusScriptWizard() {
 		super();
 		setWindowTitle("New Odysseus Script");
@@ -44,9 +47,9 @@ public class OdysseusScriptWizard extends Wizard implements INewWizard {
 
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		page = new OdysseusScriptWizardPage( getFolder((IResource)selection.getFirstElement()));
+		page = new OdysseusScriptWizardPage("New Odysseus Script", selection);
 	}
-	
+
 	@Override
 	public void addPages() {
 		addPage(page);
@@ -54,38 +57,68 @@ public class OdysseusScriptWizard extends Wizard implements INewWizard {
 
 	@Override
 	public boolean performFinish() {
-		String queryName = page.getFullQueryName();
-		IContainer container = page.getFileContainer();
-		
+
 		try {
-			IPath path = container.getFullPath().append(queryName);
+			String queryFileName = getQueryFileName();
+
+			IPath path = page.getContainerFullPath().append(queryFileName);
 			IFile newFile = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
 			newFile.create(getQueryTemplate(), IResource.NONE, null);
 
 			// open editor
-			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(
-					new FileEditorInput(newFile), 
-					OdysseusRCPEditorTextPlugIn.ODYSSEUS_SCRIPT_EDITOR_ID, 
-					true);
-			
+			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(new FileEditorInput(newFile), OdysseusRCPEditorTextPlugIn.ODYSSEUS_SCRIPT_EDITOR_ID, true);
+
 			return true;
-		} catch (Exception ex ) {
+		} catch (CancelException ex) {
+			return false;
+		} catch (Exception ex) {
 			new ExceptionWindow(ex);
 			return false;
 		}
 	}
-	
+
 	protected InputStream getQueryTemplate() {
 		return new ByteArrayInputStream("///OdysseusScript\n#PARSER CQL\n#TRANSCFG Standard\n#QUERY\n///Your first query here".getBytes());
 	}
 
-	private static IContainer getFolder( IResource resource ) {
-		if( resource instanceof IContainer ) 
-			return (IContainer)resource;
-		if( resource instanceof IFile ) {
-			IFile file = (IFile)resource;
-			return file.getParent();
+	private String getQueryFileName() throws CancelException {
+		String queryFileName = page.getFileName();
+
+		Optional<String> optionalExtension = getFileExtension(queryFileName);
+		if (!optionalExtension.isPresent()) {
+			queryFileName = queryFileName + "." + OdysseusRCPEditorTextPlugIn.QUERY_TEXT_EXTENSION;
+		} else {
+			String extension = optionalExtension.get();
+			if (!extension.equals(OdysseusRCPEditorTextPlugIn.QUERY_TEXT_EXTENSION)) {
+
+				if (!isOtherExtensionOk(extension, OdysseusRCPEditorTextPlugIn.QUERY_TEXT_EXTENSION)) {
+					queryFileName = queryFileName.replace(extension, OdysseusRCPEditorTextPlugIn.QUERY_TEXT_EXTENSION);
+				}
+			}
 		}
-		throw new IllegalArgumentException("unknown resource-type:" + resource.getClass().getName());
+		return queryFileName;
+	}
+
+	private static boolean isOtherExtensionOk(String desiredExtension, String standardExtension) throws CancelException {
+		MessageDialog dlg = new MessageDialog(Display.getCurrent().getActiveShell(), "Custom file extension", null, "Should the file extension '" + desiredExtension + "' be replaced by '" + standardExtension + "'?", MessageDialog.QUESTION, new String[] { "Replace", "Keep", "Cancel" }, 0);
+		int ret = dlg.open();
+		if (ret == 1) {
+			return true;
+		} else if (ret == 0) {
+			return false;
+		}
+		throw new CancelException();
+	}
+
+	private static Optional<String> getFileExtension(String fileName) {
+		int lastPoint = fileName.lastIndexOf(".");
+		if (lastPoint != -1) {
+			return Optional.of(fileName.substring(lastPoint + 1));
+		}
+		return Optional.absent();
 	}
 }
+
+@SuppressWarnings("serial")
+class CancelException extends Exception {
+};
--- core/rcp/rcp.editor.text/src/de/uniol/inf/is/odysseus/rcp/editor/text/wizards/OdysseusScriptWizardPage.java
+++ core/rcp/rcp.editor.text/src/de/uniol/inf/is/odysseus/rcp/editor/text/wizards/OdysseusScriptWizardPage.java
@@ -14,117 +14,18 @@
   */
 package de.uniol.inf.is.odysseus.rcp.editor.text.wizards;
 
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.runtime.Assert;
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 
-import de.uniol.inf.is.odysseus.rcp.editor.text.OdysseusRCPEditorTextPlugIn;
+public class OdysseusScriptWizardPage extends WizardNewFileCreationPage {
 
-public class OdysseusScriptWizardPage extends WizardPage {
-
-	private static final int GRID_LAYOUT_COLUMNS = 2;
-	
-	private static final String INFORMATION_TEXT = "Note: The endling '." + OdysseusRCPEditorTextPlugIn.QUERY_TEXT_EXTENSION +"' will be automatically added";
-	private static final String NO_NAME_ERROR_TEXT = "A name must be specified";
 	private static final String TITLE_TEXT = "New Odysseus Script";
 	private static final String DESCRIPTION_TEXT = "Creates a new file for scripts";
-	private static final String NAME_EXISTS_TEXT = "Name already in use";
 	
-	private String queryName;
-	private IContainer container;
-	
-	public OdysseusScriptWizardPage(IContainer container) {
-		super(TITLE_TEXT);
+	public OdysseusScriptWizardPage(String pageName, IStructuredSelection selection) {
+		super(pageName, selection);
 		
 		setTitle(TITLE_TEXT);
 		setDescription(DESCRIPTION_TEXT);
-		setFileContainer(container);
-	}
-	
-	@Override
-	public void createControl(Composite parent) {
-		Composite composite = new Composite(parent, SWT.NONE);
-		
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = GRID_LAYOUT_COLUMNS;
-		composite.setLayout(gridLayout);
-		
-		createLabel( composite, "Name");
-		final Text nameText = createText(composite, "");
-		createHugeLabel( composite, INFORMATION_TEXT );
-		
-		nameText.addModifyListener(new ModifyListener() {
-			@Override
-			public void modifyText(ModifyEvent e) {
-				if( nameText.getText() != null && nameText.getText().length() > 0 ) {
-					if( !isNameAlreadyUsed(getFileContainer(), nameText.getText())) {
-						setPageComplete(true);
-						setErrorMessage(null);
-						queryName = nameText.getText();
-					} else {
-						setErrorMessage(NAME_EXISTS_TEXT);
-						setPageComplete(false);
-					}
-				} else {
-					setErrorMessage(NO_NAME_ERROR_TEXT);
-					setPageComplete(false);
-				}
-			}
-		});
-		
-		setControl(composite);
-		setPageComplete(false);
-		setErrorMessage(NO_NAME_ERROR_TEXT);
-	}
-	
-	public String getQueryName() {
-		return queryName;
-	}
-	
-	public String getFullQueryName() {
-		return queryName + "." + OdysseusRCPEditorTextPlugIn.QUERY_TEXT_EXTENSION;
-	}
-	
-	public IContainer getFileContainer() {
-		return container;
-	}
-	
-	protected void setFileContainer( IContainer container ) {
-		Assert.isNotNull(container, "container");
-		
-		this.container = container;
-	}
-
-	private static Label createLabel( Composite comp, String text ) {
-		Label label = new Label( comp, SWT.NONE);
-		label.setText(text);
-		return label;
-	}
-	
-	private static Label createHugeLabel( Composite comp, String text ) {
-		Label label = createLabel(comp, text);
-		GridData data = new GridData(GridData.FILL_HORIZONTAL);
-		data.horizontalSpan = GRID_LAYOUT_COLUMNS;
-		label.setLayoutData(data);
-		return label;
-	}
-	
-	private static Text createText( Composite comp, String value ) {
-		Text text = new Text(comp, SWT.SINGLE | SWT.BORDER);
-		text.setText(value);
-		text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		return text;
-	}
-	
-	private static boolean isNameAlreadyUsed( IContainer container, String name ) {
-		return container.findMember(name + "." + OdysseusRCPEditorTextPlugIn.QUERY_TEXT_EXTENSION) != null;
 	}
 }
