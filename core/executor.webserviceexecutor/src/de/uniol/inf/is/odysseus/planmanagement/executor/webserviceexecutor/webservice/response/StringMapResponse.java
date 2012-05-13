ca5db6b9437d7e7ed746f4d85d84b428c7d379e8
Merlin Wasmann <merlin@uni-oldenburg.de>
2012-05-13 21:45:49 +0000

    New responses for the webservice
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11794 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 94335341f28e1c0fac08bc4e86e7ae0ece9cce57

---
+++ core/executor.webserviceexecutor/src/de/uniol/inf/is/odysseus/planmanagement/executor/webserviceexecutor/webservice/response/StringMapResponse.java
@@ -0,0 +1,42 @@
+/** Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.planmanagement.executor.webserviceexecutor.webservice.response;
+
+import java.util.Map;
+
+/**
+ * @author Merlin Wasmann
+ *
+ */
+public class StringMapResponse extends Response {
+	private Map<String, String> responseValue;
+
+	public StringMapResponse() {
+		super();
+	}
+	
+	public StringMapResponse(Map<String, String> responseValue, boolean success) {
+		super(success);
+		this.responseValue = responseValue;
+	}
+	
+	public Map<String, String> getResponseValue() {
+		return this.responseValue;
+	}
+	
+	public void setResponseValue(Map<String, String> value) {
+		this.responseValue = value;
+	}
+}
