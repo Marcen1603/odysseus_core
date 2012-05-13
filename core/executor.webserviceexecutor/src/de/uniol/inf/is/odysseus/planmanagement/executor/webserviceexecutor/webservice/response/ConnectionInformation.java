7b945692c60561d92e751cc964ca984c1d142939
Merlin Wasmann <merlin@uni-oldenburg.de>
2012-05-13 21:52:40 +0000

    ConnectionInformation contains info about port and address of a socketSink
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11803 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 911357feaed53b8a827faa76a9ee29f905978668

---
+++ core/executor.webserviceexecutor/src/de/uniol/inf/is/odysseus/planmanagement/executor/webserviceexecutor/webservice/response/ConnectionInformation.java
@@ -0,0 +1,43 @@
+/** Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.planmanagement.executor.webserviceexecutor.webservice.response;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author Merlin Wasmann
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "connectionInformation", propOrder = {
+    "port",
+    "address"
+})
+public class ConnectionInformation {
+
+	public ConnectionInformation() {
+		
+	}
+	
+	public int port;
+	public String address;
+	
+	public ConnectionInformation(int port, String address) {
+		this.port = port;
+		this.address = address;
+	}
+}
