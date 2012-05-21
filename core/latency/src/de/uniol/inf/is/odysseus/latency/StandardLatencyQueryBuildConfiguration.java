8d1125006f8551d3ab90275971e90cc7036410fa
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-05-21 07:30:44 +0000

    Simple build config with interval and latency (if benchmarking operators are not needed)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@11870 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: cfb3890fcb53239a983c25dc385d30a04873b524

---
+++ core/latency/src/de/uniol/inf/is/odysseus/latency/StandardLatencyQueryBuildConfiguration.java
@@ -0,0 +1,61 @@
+package de.uniol.inf.is.odysseus.latency;
+/** Copyright [2011] [The Odysseus Team]
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.server.metadata.ILatency;
+import de.uniol.inf.is.odysseus.core.server.metadata.ITimeInterval;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.configuration.AbstractQueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.configuration.IQueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.optimization.configuration.ParameterAllowRestructuringOfCurrentPlan;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.optimization.configuration.ParameterDoRewrite;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.optimization.configuration.ParameterPerformQuerySharing;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.optimization.configuration.ParameterShareSimilarOperators;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.IQueryBuildSetting;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.ParameterTransformationConfiguration;
+
+public class StandardLatencyQueryBuildConfiguration extends
+		AbstractQueryBuildConfiguration {
+
+	@SuppressWarnings("unchecked")
+	public StandardLatencyQueryBuildConfiguration() {
+		settings.add(new ParameterTransformationConfiguration(
+				new TransformationConfiguration(
+						"relational", 
+						ITimeInterval.class, ILatency.class)));
+		settings.add(ParameterDoRewrite.TRUE);
+		settings.add(ParameterPerformQuerySharing.TRUE);
+		settings.add(ParameterAllowRestructuringOfCurrentPlan.TRUE);
+		settings.add(ParameterShareSimilarOperators.FALSE);
+	}
+	
+	public StandardLatencyQueryBuildConfiguration(List<IQueryBuildSetting<?>> settings) {
+		settings.addAll(settings);
+	}
+
+	@Override
+	public String getName() {
+		return "StandardLatency";
+	}
+	
+	@Override
+	public IQueryBuildConfiguration clone() {
+		return new StandardLatencyQueryBuildConfiguration(settings);
+	}
+
+}
