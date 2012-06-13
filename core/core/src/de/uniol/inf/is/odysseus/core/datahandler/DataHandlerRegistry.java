a48f8700a36092816882c5057f7cdf61a166c55a
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-06-13 07:45:00 +0000

    Removed null pointer exception if type is not registered
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12073 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: aeb0ae049de8e4327b4bda9164cf4e32f0218846

--- core/core/src/de/uniol/inf/is/odysseus/core/datahandler/DataHandlerRegistry.java
+++ core/core/src/de/uniol/inf/is/odysseus/core/datahandler/DataHandlerRegistry.java
@@ -1,17 +1,17 @@
 /** Copyright [2011] [The Odysseus Team]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.core.datahandler;
 
 import java.util.HashMap;
@@ -22,51 +22,54 @@ import org.slf4j.LoggerFactory;
 
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 
-
 /**
  * @author André Bolles, Marco Grawunder
- *
+ * 
  */
 public class DataHandlerRegistry {
 
 	static Logger logger = LoggerFactory.getLogger(DataHandlerRegistry.class);
-	
-	public DataHandlerRegistry(){
+
+	public DataHandlerRegistry() {
 		logger.debug("Created new DataHandler registry");
 	}
-	
+
 	/**
 	 * HashMap from datatype to data handler
 	 */
 	private static HashMap<String, IDataHandler<?>> dataHandlers = new HashMap<String, IDataHandler<?>>();
-	
-	public static void registerDataHandler(IDataHandler<?> handler){
+
+	public static void registerDataHandler(IDataHandler<?> handler) {
 		String errMsg = "";
-		logger.debug("Register DataHandler "+handler+" for Datatypes "+handler.getSupportedDataTypes());
-		for(String type: handler.getSupportedDataTypes()){
-			if(dataHandlers.containsKey(type.toLowerCase())){
+		logger.debug("Register DataHandler " + handler + " for Datatypes "
+				+ handler.getSupportedDataTypes());
+		for (String type : handler.getSupportedDataTypes()) {
+			if (dataHandlers.containsKey(type.toLowerCase())) {
 				errMsg += "Data handler for " + type + " already registered.\n";
-			}
-			else{
+			} else {
 				dataHandlers.put(type.toLowerCase(), handler);
 			}
 		}
-		
-		if(errMsg != ""){
+
+		if (errMsg != "") {
 			throw new IllegalArgumentException(errMsg);
 		}
 	}
-		
-	public static void removeDataHandler(IDataHandler<?> handler){
-		for(String type: handler.getSupportedDataTypes()){
-			if(dataHandlers.containsKey(type.toLowerCase())){
+
+	public static void removeDataHandler(IDataHandler<?> handler) {
+		for (String type : handler.getSupportedDataTypes()) {
+			if (dataHandlers.containsKey(type.toLowerCase())) {
 				dataHandlers.remove(type.toLowerCase());
 			}
 		}
 	}
-	
-	public static IDataHandler<?> getDataHandler(String dataType, SDFSchema schema){
-		IDataHandler<?> ret = dataHandlers.get(dataType.toLowerCase()).createInstance(schema);
+
+	public static IDataHandler<?> getDataHandler(String dataType,
+			SDFSchema schema) {
+		IDataHandler<?> ret = dataHandlers.get(dataType.toLowerCase());
+		if (ret != null) {
+			ret = ret.createInstance(schema);
+		}
 		return ret;
 	}
 
@@ -76,8 +79,10 @@ public class DataHandlerRegistry {
 
 	public static IDataHandler<?> getDataHandler(String dataType,
 			List<String> schema) {
-		IDataHandler<?> ret = dataHandlers.get(dataType.toLowerCase()).createInstance(schema);
+		IDataHandler<?> ret = dataHandlers.get(dataType.toLowerCase());
+		if (ret != null) {
+			ret = ret.createInstance(schema);
+		}
 		return ret;
 	}
-	
 }
