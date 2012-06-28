ae3ad9cc4f5c4324180181f0922bea4cce9040c7
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-06-28 13:04:45 +0000

    Dashboard: New TestUtil to test private (static) methods easily
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12188 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 40cb5aa986a7636a4525aed8846f5589342c2942

---
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard.test/src/de/uniol/inf/is/odysseus/rcp/dashboard/test/TestUtil.java
@@ -0,0 +1,76 @@
+package de.uniol.inf.is.odysseus.rcp.dashboard.test;
+
+import java.lang.reflect.Method;
+
+public class TestUtil {
+
+	public static Object invoke( String methodName, Class<?> clazz, Object... parameters ) throws Throwable {
+		try {
+			Class<?>[] parameterClasses = getClasses(parameters);
+			Method m = getMethod(methodName, clazz, parameterClasses);
+			
+			m.setAccessible(true);
+			
+			return m.invoke(m, parameters);
+		} catch( Exception ex ) {
+			if( ex.getCause() != null ) {
+				throw ex.getCause();
+			} else {
+				throw ex;
+			}
+		}		
+	}
+	
+	public static Object invoke( String methodName, Object object, Object... parameters ) throws Throwable {
+		try {
+			Class<?>[] parameterClasses = getClasses(parameters);
+			Method m = getMethod(methodName, object.getClass(), parameterClasses);
+			m.setAccessible(true);
+			
+			return m.invoke(object, parameters);
+		} catch( Exception ex ) {
+			if( ex.getCause() != null ) {
+				throw ex.getCause();
+			} else {
+				throw ex;
+			}
+		}
+	}
+
+	private static Class<?>[] getClasses(Object[] parameters) {
+		Class<?>[] classes = new Class<?>[parameters.length];
+		for( int i = 0; i < classes.length; i++ ) {
+			if( parameters[i] != null ) {
+				classes[i] = parameters[i].getClass();
+			} else {
+				classes[i] = Object.class;
+			}
+		}
+		return classes;
+	}
+	
+	private static Method getMethod( String name, Class<?> clazz, Class<?>[] parameterTypes ) throws Exception {
+		try {
+			return clazz.getDeclaredMethod(name, parameterTypes);
+		} catch( NoSuchMethodException ignored ) {
+			
+			// Happens with null-parameters
+			Method[] methods = clazz.getDeclaredMethods();
+			
+			Method foundMethod = null;
+			for( int i = 0; i < methods.length; i++ ) {
+				if( methods[i].getName().equals(name)) {
+					if( foundMethod != null ) {
+						throw new Exception("Method " + name + " of class " + clazz + " is ambiguous to invoke it easily.");
+					}
+					foundMethod = methods[i];
+				}
+			}
+			if( foundMethod == null ) {
+				throw new NoSuchMethodException(name + " of class " + clazz + " not found!");
+			}
+			
+			return foundMethod;
+		}
+	}
+}
