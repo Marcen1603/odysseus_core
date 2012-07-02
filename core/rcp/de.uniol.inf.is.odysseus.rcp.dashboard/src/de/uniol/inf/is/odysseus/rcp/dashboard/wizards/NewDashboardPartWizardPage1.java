23e72d53307cab9d860db4c670ee33df6e453452
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-07-02 09:46:01 +0000

    Dashboard: Added new widgets
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12199 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 49bb187b622854c81625df423dcb996db9a33d4b

--- core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/wizards/NewDashboardPartWizardPage1.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/wizards/NewDashboardPartWizardPage1.java
@@ -4,19 +4,21 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 
+import de.uniol.inf.is.odysseus.rcp.dashboard.DashboardPlugIn;
+
 public class NewDashboardPartWizardPage1 extends WizardNewFileCreationPage {
 
 	public NewDashboardPartWizardPage1(String pageName, IStructuredSelection selection ) {
 		super(pageName, selection);
 		
-		setTitle("Create a new DashboardPart instance.");
-		setDescription("Create a new DashboardPart instance.");
+		setTitle("Choose project and folder");
+		setDescription("Choose the project (and folder) in which the new dashboard part should be placed.");
 	}
 	
 	@Override
 	public void createControl(Composite parent) {
 		super.createControl(parent);
-		setFileName("DashboardPart.prt");
+		setFileName("DashboardPart." + DashboardPlugIn.DASHBOARD_PART_EXTENSION);
 		setPageComplete(validatePage());
 	}
 	
--- core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/wizards/NewDashboardPartWizardPage2.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/wizards/NewDashboardPartWizardPage2.java
@@ -2,21 +2,43 @@ package de.uniol.inf.is.odysseus.rcp.dashboard.wizards;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class NewDashboardPartWizardPage2 extends WizardPage {
 
 	public NewDashboardPartWizardPage2(String pageName) {
 		super(pageName);
+
+		setTitle("Choose type of DashboardPart");
+		setDescription("Choose one type of DashboardPart and configure it.");
 	}
 
 	@Override
 	public void createControl(Composite parent) {
-		
+		initializeDialogUnits(parent);
+
 		Composite rootComposite = new Composite(parent, SWT.NONE);
-		
+		rootComposite.setLayoutData(new GridData((GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL)));
+		rootComposite.setLayout(new GridLayout(1, true));
+
+		Composite choosePartNameComposite = new Composite(rootComposite, SWT.NONE);
+		choosePartNameComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		choosePartNameComposite.setLayout(new GridLayout(2, false));
+
+		Label choosePartNameLabel = new Label(choosePartNameComposite, SWT.NONE);
+		choosePartNameLabel.setText("Type");
+		Text choosePartNameText = new Text(choosePartNameComposite, SWT.NONE);
+		choosePartNameText.setText("DashboardPart Type");
+		choosePartNameText.setLayoutData(new GridData(GridData.BEGINNING));
+
+		setErrorMessage(null);
+		setMessage(null);
 		setControl(rootComposite);
 		setPageComplete(true);
 	}
-	
+
 }
