9f2ee4b88a2a59a5903c08b9d761ac249c3c7389
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-07-03 14:56:36 +0000

    Dashboard: Bugfix in class-hierarchy
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12231 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d57f057b0543a737ae128ced8f021919d4ddcef0

--- core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard.part/src/de/uniol/inf/is/odysseus/rcp/dashboard/part/SampleDashboardPart.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard.part/src/de/uniol/inf/is/odysseus/rcp/dashboard/part/SampleDashboardPart.java
@@ -2,30 +2,16 @@ package de.uniol.inf.is.odysseus.rcp.dashboard.part;
 
 import java.nio.ByteBuffer;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.swt.widgets.Composite;
 
 import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
-import de.uniol.inf.is.odysseus.rcp.dashboard.Configuration;
-import de.uniol.inf.is.odysseus.rcp.dashboard.IDashboardPart;
+import de.uniol.inf.is.odysseus.rcp.dashboard.AbstractDashboardPart;
 
-public class SampleDashboardPart implements IDashboardPart {
-
-	public SampleDashboardPart() {
-	}
-
-	@Override
-	public boolean init(Configuration configuration) {
-		return true;
-	}
-
-	@Override
-	public Configuration getConfiguration() {
-		return null;
-	}
+public class SampleDashboardPart extends AbstractDashboardPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
+		
 	}
 
 	@Override
@@ -42,15 +28,6 @@ public class SampleDashboardPart implements IDashboardPart {
 
 	@Override
 	public void punctuationElementRecieved(PointInTime point, int port) {
-	}
-
-	@Override
-	public void setQueryFile(IFile file) {
-	}
-
-	@Override
-	public IFile getQueryFile() {
-		return null;
 	}
 
 }
