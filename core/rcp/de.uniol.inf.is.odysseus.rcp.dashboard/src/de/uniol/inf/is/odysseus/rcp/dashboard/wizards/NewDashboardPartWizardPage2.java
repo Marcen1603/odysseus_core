367c43d7236deb368662cd846bd63e89a0f6cea6
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-07-03 07:58:59 +0000

    Dashboard: Improvements to new-wizard
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12208 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8ce961d19a2a5919320a6054a83ab61757a6e3a5

--- core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/wizards/NewDashboardPartWizardPage2.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/wizards/NewDashboardPartWizardPage2.java
@@ -1,20 +1,37 @@
 package de.uniol.inf.is.odysseus.rcp.dashboard.wizards;
 
+import java.util.List;
+
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
+
+import com.google.common.base.Optional;
+
+import de.uniol.inf.is.odysseus.rcp.dashboard.DashboardPartRegistry;
+import de.uniol.inf.is.odysseus.rcp.dashboard.desc.DashboardPartDescriptor;
 
 public class NewDashboardPartWizardPage2 extends WizardPage {
+
+	private final List<String> dashboardPartNames;
+
+	private Label descriptionLabel;
+	private Combo choosePartNameCombo;
 
 	public NewDashboardPartWizardPage2(String pageName) {
 		super(pageName);
 
 		setTitle("Choose type of DashboardPart");
 		setDescription("Choose one type of DashboardPart and configure it.");
+
+		dashboardPartNames = determineDashboardPartNames();
 	}
 
 	@Override
@@ -31,14 +48,61 @@ public class NewDashboardPartWizardPage2 extends WizardPage {
 
 		Label choosePartNameLabel = new Label(choosePartNameComposite, SWT.NONE);
 		choosePartNameLabel.setText("Type");
-		Text choosePartNameText = new Text(choosePartNameComposite, SWT.NONE);
-		choosePartNameText.setText("DashboardPart Type");
-		choosePartNameText.setLayoutData(new GridData(GridData.BEGINNING));
 
+		choosePartNameCombo = new Combo(choosePartNameComposite, SWT.BORDER | SWT.READ_ONLY);
+		choosePartNameCombo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		choosePartNameCombo.setItems(dashboardPartNames.toArray(new String[0]));
+
+		descriptionLabel = new Label(rootComposite, SWT.BORDER);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.heightHint = 100;
+		descriptionLabel.setLayoutData(gd);
+		descriptionLabel.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+		descriptionLabel.setText("");
+		choosePartNameCombo.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				selectDashboardPart(choosePartNameCombo.getSelectionIndex());
+			}
+
+		});
+
+		selectDashboardPart(0);
+		finishCreation(rootComposite);
+	}
+
+	private void finishCreation(Composite rootComposite) {
 		setErrorMessage(null);
 		setMessage(null);
 		setControl(rootComposite);
 		setPageComplete(true);
 	}
 
+	private void selectDashboardPart(int index) {
+		choosePartNameCombo.select(index);
+		refreshDescriptionLabel(getDashboardPartName(index));
+	}
+
+	private void refreshDescriptionLabel(String dashboardPartName) {
+		descriptionLabel.setText(getDescription(dashboardPartName));
+	}
+
+	private String getDashboardPartName(int index) {
+		return dashboardPartNames.get(index);
+	}
+
+	private String getDescription(String dashboardPartName) {
+		Optional<DashboardPartDescriptor> optDescriptor = DashboardPartRegistry.getDashboardPartDescriptor(dashboardPartName);
+		if (optDescriptor.isPresent()) {
+			DashboardPartDescriptor descriptor = optDescriptor.get();
+			return descriptor.getDescription();
+		} else {
+			return "";
+		}
+	}
+
+	private static List<String> determineDashboardPartNames() {
+		return DashboardPartRegistry.getDashboardPartNames();
+	}
 }
