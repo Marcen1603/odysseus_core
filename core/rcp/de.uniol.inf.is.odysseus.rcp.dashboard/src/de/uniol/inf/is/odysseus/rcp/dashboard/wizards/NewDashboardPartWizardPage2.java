8bdc950f255a34307b66c79213d9983d9c41b06e
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-07-03 08:54:48 +0000

    Dashboard: Show settings in wizard
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12216 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9b6a4f64804346f1847586263ee81a5cfba2d9c7

--- core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/wizards/NewDashboardPartWizardPage2.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/wizards/NewDashboardPartWizardPage2.java
@@ -2,6 +2,17 @@ package de.uniol.inf.is.odysseus.rcp.dashboard.wizards;
 
 import java.util.List;
 
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -12,18 +23,42 @@ import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 
 import de.uniol.inf.is.odysseus.rcp.dashboard.DashboardPartRegistry;
 import de.uniol.inf.is.odysseus.rcp.dashboard.desc.DashboardPartDescriptor;
+import de.uniol.inf.is.odysseus.rcp.dashboard.desc.SettingDescriptor;
 
 public class NewDashboardPartWizardPage2 extends WizardPage {
 
+	private static class SettingValuePair {
+		public String setting;
+		public String value;
+		public String description;
+
+		public SettingValuePair(String setting, String value, String description) {
+			this.setting = setting;
+			this.value = value;
+			this.description = description;
+		}
+	}
+
+	private static final Logger LOG = LoggerFactory.getLogger(NewDashboardPartWizardPage2.class);
+	private static final int DESCRIPTION_LABEL_HEIGHT = 60;
+
 	private final List<String> dashboardPartNames;
 
-	private Label descriptionLabel;
+	private Label partDescriptionLabel;
 	private Combo choosePartNameCombo;
+	private Label settingDescriptionLabel;
+
+	private TableViewer settingsTable;
+	private List<SettingValuePair> settings;
 
 	public NewDashboardPartWizardPage2(String pageName) {
 		super(pageName);
@@ -53,12 +88,12 @@ public class NewDashboardPartWizardPage2 extends WizardPage {
 		choosePartNameCombo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		choosePartNameCombo.setItems(dashboardPartNames.toArray(new String[0]));
 
-		descriptionLabel = new Label(rootComposite, SWT.BORDER);
+		partDescriptionLabel = new Label(rootComposite, SWT.BORDER);
 		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-		gd.heightHint = 100;
-		descriptionLabel.setLayoutData(gd);
-		descriptionLabel.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
-		descriptionLabel.setText("");
+		gd.heightHint = DESCRIPTION_LABEL_HEIGHT;
+		partDescriptionLabel.setLayoutData(gd);
+		partDescriptionLabel.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+		partDescriptionLabel.setText("");
 		choosePartNameCombo.addSelectionListener(new SelectionAdapter() {
 
 			@Override
@@ -67,6 +102,23 @@ public class NewDashboardPartWizardPage2 extends WizardPage {
 			}
 
 		});
+
+		settingsTable = createSettingsTableViewer(rootComposite);
+		settingsTable.addSelectionChangedListener(new ISelectionChangedListener() {
+
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				selectSetting(settingsTable.getSelection());
+			}
+			
+		});
+
+		settingDescriptionLabel = new Label(rootComposite, SWT.BORDER);
+		GridData gd2 = new GridData(GridData.FILL_HORIZONTAL);
+		gd2.heightHint = DESCRIPTION_LABEL_HEIGHT;
+		settingDescriptionLabel.setLayoutData(gd2);
+		settingDescriptionLabel.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+		settingDescriptionLabel.setText("");
 
 		selectDashboardPart(0);
 		finishCreation(rootComposite);
@@ -81,13 +133,28 @@ public class NewDashboardPartWizardPage2 extends WizardPage {
 
 	private void selectDashboardPart(int index) {
 		choosePartNameCombo.select(index);
-		refreshDescriptionLabel(getDashboardPartName(index));
+		String dashboardName = getDashboardPartName(index);
+		refreshPartDescriptionLabel(dashboardName);
+		refreshSettingsTable(dashboardName);
+	}
+	
+	private void selectSetting(ISelection selection) {
+		IStructuredSelection structuredSelection = (IStructuredSelection)selection;
+		SettingValuePair pair = (SettingValuePair)structuredSelection.getFirstElement();
+		settingDescriptionLabel.setText(pair != null ? pair.description : "");
 	}
 
-	private void refreshDescriptionLabel(String dashboardPartName) {
-		descriptionLabel.setText(getDescription(dashboardPartName));
+	private void refreshSettingsTable(String dashboardName) {
+		settings = getSettings(dashboardName);
+		
+		settingsTable.setInput(settings);
+		settingsTable.refresh();
 	}
 
+	private void refreshPartDescriptionLabel(String dashboardPartName) {
+		partDescriptionLabel.setText(getDescription(dashboardPartName));
+	}
+	
 	private String getDashboardPartName(int index) {
 		return dashboardPartNames.get(index);
 	}
@@ -98,11 +165,81 @@ public class NewDashboardPartWizardPage2 extends WizardPage {
 			DashboardPartDescriptor descriptor = optDescriptor.get();
 			return descriptor.getDescription();
 		} else {
+			LOG.error("Could not find DashboardPartDescriptor for {}.", dashboardPartName);
 			return "";
 		}
 	}
 
+	private static List<SettingValuePair> getSettings(String dashboardPartName) {
+		Optional<DashboardPartDescriptor> optDescriptor = DashboardPartRegistry.getDashboardPartDescriptor(dashboardPartName);
+		List<SettingValuePair> result = Lists.newArrayList();
+		if( optDescriptor.isPresent() ) {
+			DashboardPartDescriptor descriptor = optDescriptor.get();
+			for( String settingDescriptorName : descriptor.getSettingDescriptorNames() ) {
+				Optional<SettingDescriptor<?>> optSettingDescriptor = descriptor.getSettingDescriptor(settingDescriptorName);
+				if( optSettingDescriptor.isPresent() ) {
+					SettingDescriptor<?> settingDescriptor = optSettingDescriptor.get();
+					result.add(new SettingValuePair(settingDescriptorName, toString(settingDescriptor.getDefaultValue()), settingDescriptor.getDescription()));
+				} else {
+					LOG.error("Could not find SettingDescriptor {} in DashboardPart {}", settingDescriptorName, dashboardPartName);
+				}
+			}
+		} else {
+			LOG.error("Could not find DashboardPartDescriptor for {}.", dashboardPartName);
+		}
+		
+		return result;
+	}
+
+	private static TableViewer createSettingsTableViewer(Composite parent) {
+		Composite tableComposite = new Composite(parent, SWT.NONE);
+		tableComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+		TableColumnLayout tableColumnLayout = new TableColumnLayout();
+		tableComposite.setLayout(tableColumnLayout);
+
+		TableViewer tableViewer = new TableViewer(tableComposite, SWT.FULL_SELECTION);
+		Table table = tableViewer.getTable();
+
+		table.setLayoutData(new GridData(GridData.FILL_BOTH));
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+
+		TableViewerColumn settingNameColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+		settingNameColumn.getColumn().setText("Setting");
+		tableColumnLayout.setColumnData(settingNameColumn.getColumn(), new ColumnWeightData(5, 25, true));
+		settingNameColumn.setLabelProvider(new CellLabelProvider() {
+			@Override
+			public void update(ViewerCell cell) {
+				SettingValuePair pair = (SettingValuePair)cell.getElement();
+				cell.setText(pair.setting);
+			}
+		});
+		
+		TableViewerColumn settingValueColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+		settingValueColumn.getColumn().setText("Value");
+		tableColumnLayout.setColumnData(settingValueColumn.getColumn(), new ColumnWeightData(5, 25, true));
+		settingValueColumn.setLabelProvider(new CellLabelProvider() {
+			@Override
+			public void update(ViewerCell cell) {
+				SettingValuePair pair = (SettingValuePair)cell.getElement();
+				cell.setText(pair.value);
+			}
+		});
+
+		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+
+		return tableViewer;
+	}
+
 	private static List<String> determineDashboardPartNames() {
 		return DashboardPartRegistry.getDashboardPartNames();
+	}
+	
+	private static String toString(Object obj) {
+		if( obj == null ) {
+			return "";
+		} else {
+			return obj.toString();
+		}
 	}
 }
