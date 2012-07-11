2e25428eebb935d1b405cc58795746f9f50cf09e
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-07-11 11:05:44 +0000

    Dashboard: Refactoring
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12399 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a50c03712a5b514a246413b44b19e16cab1373ae

---
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard.part/src/de/uniol/inf/is/odysseus/rcp/dashboard/part/AbstractChartDashboardPart.java
@@ -0,0 +1,92 @@
+package de.uniol.inf.is.odysseus.rcp.dashboard.part;
+
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.ui.PlatformUI;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.Dataset;
+import org.jfree.experimental.chart.swt.ChartComposite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
+import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
+import de.uniol.inf.is.odysseus.rcp.dashboard.AbstractDashboardPart;
+
+public abstract class AbstractChartDashboardPart extends AbstractDashboardPart {
+
+	private static final Logger LOG = LoggerFactory.getLogger(AbstractChartDashboardPart.class);
+
+	private JFreeChart chart;
+	private Dataset dataset;
+	private ChartComposite chartComposite;
+
+	@Override
+	public void createPartControl(Composite parent, ToolBar toolbar) {
+		dataset = Preconditions.checkNotNull(createDataset(), "Created data set must not be null!");
+		chart = Preconditions.checkNotNull(createChart(), "Created chart must not be null!");
+
+		decorateChart(chart);
+
+		chartComposite = new ChartComposite(parent, SWT.NONE, this.chart, true);
+		chartComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+	}
+
+	public final Dataset getDataset() {
+		return dataset;
+	}
+
+	public final JFreeChart getChart() {
+		return chart;
+	}
+
+	@Override
+	public void onStart(List<IPhysicalOperator> physicalRoots) throws Exception {
+		super.onStart(physicalRoots);
+		startChart(physicalRoots);
+	}
+
+	@Override
+	public void streamElementRecieved(final Object element, final int port) {
+		if (!(element instanceof Tuple)) {
+			LOG.error("Lines DashboardPart only applyable for Tuples!");
+			return;
+		}
+
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				if (!chartComposite.isDisposed()) {
+					addStreamElementToChart((Tuple<?>) element, port);
+				}
+			}
+		});
+	}
+
+	@Override
+	public void punctuationElementRecieved(final PointInTime point, final int port) {
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				if (!chartComposite.isDisposed()) {
+					addPunctuationToChart(point, port);
+				}
+			}
+		});
+	}
+
+	protected abstract Dataset createDataset();
+	protected abstract JFreeChart createChart();
+	protected abstract void decorateChart(JFreeChart chart);
+	protected abstract void startChart(List<IPhysicalOperator> physicalRoots) throws Exception;
+	protected abstract void addStreamElementToChart(Tuple<?> element, int port);
+	protected abstract void addPunctuationToChart(PointInTime punctuation, int port);
+
+}
--- core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard.part/src/de/uniol/inf/is/odysseus/rcp/dashboard/part/LineChartDashboardPart.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard.part/src/de/uniol/inf/is/odysseus/rcp/dashboard/part/LineChartDashboardPart.java
@@ -1,22 +1,31 @@
+/*******************************************************************************
+ * Copyright 2012 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package de.uniol.inf.is.odysseus.rcp.dashboard.part;
 
 import java.awt.Color;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.ToolBar;
-import org.eclipse.ui.PlatformUI;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.experimental.chart.swt.ChartComposite;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.jfree.data.general.Dataset;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
@@ -27,12 +36,9 @@ import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
-import de.uniol.inf.is.odysseus.rcp.dashboard.AbstractDashboardPart;
 import de.uniol.inf.is.odysseus.rcp.dashboard.Configuration;
 
-public class LinesDashboardPart extends AbstractDashboardPart {
-
-	private static final Logger LOG = LoggerFactory.getLogger(LinesDashboardPart.class);
+public class LineChartDashboardPart extends AbstractChartDashboardPart {
 
 	private static final String TITLE_SETTING = "Title";
 	private static final String X_AXIS_LABEL_SETTING = "X-Axis label";
@@ -42,32 +48,35 @@ public class LinesDashboardPart extends AbstractDashboardPart {
 	private static final String ATTRIBUTE_SEPARATOR = ",";
 	private static final String MAX_DATA_COUNT = "Value count";
 
-	private JFreeChart chart;
-	private DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-	private ChartComposite chartComposite;
-
 	private String[] xAttributes;
 	private String[] yAttributes;
 	private final Map<String, Integer> accessDescriptors = Maps.newHashMap();
 
 	@Override
-	public void createPartControl(Composite parent, ToolBar toolbar) {
-		chart = ChartFactory.createLineChart(getSettingValue(TITLE_SETTING, "Line Chart"), getSettingValue(X_AXIS_LABEL_SETTING, "X-Axis"), getSettingValue(Y_AXIS_LABEL_SETTING, "Y-Axis"), dataset,
-				PlotOrientation.VERTICAL, true, true, false);
+	public void settingChanged(String settingName, Object oldValue, Object newValue) {
+	}
 
+	@Override
+	protected Dataset createDataset() {
+		return new DefaultCategoryDataset();
+	}
+
+	@Override
+	protected JFreeChart createChart() {
+		return ChartFactory.createLineChart(getSettingValue(TITLE_SETTING, "Line Chart"), getSettingValue(X_AXIS_LABEL_SETTING, "X-Axis"), getSettingValue(Y_AXIS_LABEL_SETTING, "Y-Axis"),
+				(DefaultCategoryDataset) getDataset(), PlotOrientation.VERTICAL, true, true, false);
+	}
+
+	@Override
+	protected void decorateChart(JFreeChart chart) {
 		chart.setBackgroundPaint(Color.white);
 
 		CategoryPlot plot = (CategoryPlot) chart.getPlot();
 		plot.setBackgroundPaint(Color.WHITE);
-
-		chartComposite = new ChartComposite(parent, SWT.NONE, this.chart, true);
-		chartComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
 	}
 
 	@Override
-	public void onStart(List<IPhysicalOperator> physicalRoots) throws Exception {
-		super.onStart(physicalRoots);
-
+	protected void startChart(List<IPhysicalOperator> physicalRoots) throws Exception {
 		xAttributes = checkAndSplit((String) getSettingValue(X_AXIS_SETTING, null));
 		yAttributes = checkAndSplit((String) getSettingValue(Y_AXIS_SETTING, null));
 
@@ -75,17 +84,12 @@ public class LinesDashboardPart extends AbstractDashboardPart {
 
 		accessDescriptors.putAll(determineAttributeAccess(xAttributes, physicalRoots));
 		accessDescriptors.putAll(determineAttributeAccess(yAttributes, physicalRoots));
-		
-		dataset.clear();
+
+		((DefaultCategoryDataset) getDataset()).clear();
 	}
 
 	@Override
-	public void streamElementRecieved(Object element, int port) {
-		if (!(element instanceof Tuple)) {
-			LOG.error("Lines DashboardPart only applyable for Tuples!");
-			return;
-		}
-
+	protected void addStreamElementToChart(Tuple<?> element, int port) {
 		final Object[] xAxisValues = determineValues((Tuple<?>) element, xAttributes);
 		final Object[] yAxisValues = determineValues((Tuple<?>) element, yAttributes);
 
@@ -97,26 +101,16 @@ public class LinesDashboardPart extends AbstractDashboardPart {
 	}
 
 	@Override
-	public void punctuationElementRecieved(PointInTime point, int port) {
-	}
-
-	@Override
-	public void settingChanged(String settingName, Object oldValue, Object newValue) {
+	protected void addPunctuationToChart(PointInTime punctuation, int port) {
 
 	}
 
 	private void addDataValue(final Number value, final Comparable<?> xAxis, final Comparable<?> yAxis) {
-		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				if( !chartComposite.isDisposed() ) {
-					dataset.addValue(value, xAxis, yAxis);
-					if( dataset.getColumnCount() > getSettingValue(MAX_DATA_COUNT, 50)) {
-						dataset.removeColumn(0);
-					}
-				}
-			}
-		});
+		DefaultCategoryDataset dataset = (DefaultCategoryDataset) getDataset();
+		dataset.addValue(value, xAxis, yAxis);
+		if (dataset.getColumnCount() > getSettingValue(MAX_DATA_COUNT, 50)) {
+			dataset.removeColumn(0);
+		}
 	}
 
 	private <T> T getSettingValue(String settingName, T defValue) {
