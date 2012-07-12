dbd886bba12764b519ab6896bd1825925df8ddbf
Dennis Geesen <geesen@indivon.de>
2012-07-12 12:21:26 +0000

    set uniform meta data for all features
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12431 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: dc478e6aaec43f2d73230bd3971078feaf1768c1

--- core/rcp/rcp.viewer.stream.chart/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/chart/charts/LinearFunctionChart.java
+++ core/rcp/rcp.viewer.stream.chart/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/chart/charts/LinearFunctionChart.java
@@ -30,6 +30,7 @@
 
 package de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.charts;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -44,6 +45,8 @@ import org.jfree.data.xy.XYSeriesCollection;
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.AbstractJFreeChart;
 import de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.schema.IViewableAttribute;
+import de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.settings.ChartSetting;
+import de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.settings.ChartSetting.Type;
 
 /**
  * 
@@ -52,22 +55,43 @@ import de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.schema.IViewableAttribut
 public class LinearFunctionChart extends AbstractJFreeChart<Double, IMetaAttribute> {
 
 	private XYSeriesCollection dataset = new XYSeriesCollection();
-	private int choosenXValue = 0;
-	private int choosenYValue = 1;
-	private double min = -1;
-	private double max = 3;
+	private int slope = 0;
+	private int intercept = 1;
+	private Integer choosenSlopePort = 0;
+	private Integer choosenInterceptPort = 0;
+	double lowerY = -1;
+	double upperY = 3;
+	double lowerX = -1;
+	double upperX = 3;
 
 	@Override
 	public void chartSettingsChanged() {
 		getChart().getXYPlot().getRangeAxis().setAutoRange(false);
+		getChart().getXYPlot().getDomainAxis().setAutoRange(false);
 		resetRanges();
-
 	}
 	
-	
 	private void resetRanges(){
-		getChart().getXYPlot().getRangeAxis().setLowerBound(min);
-		getChart().getXYPlot().getRangeAxis().setUpperBound(max);
+		getChart().getXYPlot().getRangeAxis().setLowerBound(lowerY);
+		getChart().getXYPlot().getRangeAxis().setUpperBound(upperY);
+		getChart().getXYPlot().getDomainAxis().setLowerBound(lowerX);
+		getChart().getXYPlot().getDomainAxis().setUpperBound(upperX);
+		
+	}
+
+	private void calcRanges(double m, double b){		
+		double mb = m+b;
+		// positive slope		
+		if(mb>b){
+			lowerY = b*0.95;
+			upperY = mb*1.05;
+		}else{
+			lowerY = mb*0.95;
+			upperY = b*1.05;
+		}
+		lowerX = -0.5;
+		upperX = (lowerX+m)*1.05;
+		
 	}
 	
 	@Override
@@ -83,18 +107,13 @@ public class LinearFunctionChart extends AbstractJFreeChart<Double, IMetaAttribu
 			public void run() {
 				try {
 					dataset.removeAllSeries();
-					double m = tuple.get(choosenXValue);
-					double b = tuple.get(choosenYValue);
-					XYSeries series = new XYSeries("f");
-					if(b<min){
-						min = b;
-					}
-					if((m+b)>max){
-						max = m+b;
-					}
+					double m = tuple.get(slope);
+					double b = tuple.get(intercept);
+					XYSeries series = new XYSeries("f");					
+					calcRanges(m, b);
 					resetRanges();
-					series.add(0, b);
-					series.add(1, m + b);
+					series.add(0, b);					
+					series.add(1, m+b);
 					dataset.addSeries(series);
 				} catch (SWTException e) {
 					dispose();
@@ -121,5 +140,63 @@ public class LinearFunctionChart extends AbstractJFreeChart<Double, IMetaAttribu
 	public String getViewID() {
 		return VIEW_ID_PREFIX + ".linearFunctionChart";
 	}
+	
+	
+	@ChartSetting(name = "Value for slope", type = Type.OPTIONS)
+	public List<String> getSlopeValues() {
+		return getValues();
+	}
+
+	@ChartSetting(name = "Value for slope", type = Type.GET)
+	public String getSlopeValue() {
+		return getViewableAttributes(this.choosenSlopePort).get(this.slope).getName();
+	}
+
+	@ChartSetting(name = "Value for slope", type = Type.SET)
+	public void setSlopeValue(String value) {
+		for (Integer port : getPorts()) {
+			for (int i = 0; i < getViewableAttributes(port).size(); i++) {
+				if (getViewableAttributes(port).get(i).getName().equals(value)) {
+					this.slope = i;
+					this.choosenSlopePort = port;
+					return;
+				}
+			}
+		}
+	}
+
+	@ChartSetting(name = "Value for intercept", type = Type.OPTIONS)
+	public List<String> getInterceptValues() {
+		return getValues();
+	}
+
+	@ChartSetting(name = "Value for intercept", type = Type.GET)
+	public String getInterceptValue() {
+		return getViewableAttributes(choosenInterceptPort).get(this.intercept).getName();
+	}
+
+	@ChartSetting(name = "Value for intercept", type = Type.SET)
+	public void setInterceptValue(String value) {
+		for (Integer port : getPorts()) {
+			for (int i = 0; i < getViewableAttributes(port).size(); i++) {
+				if (getViewableAttributes(port).get(i).getName().equals(value)) {
+					this.intercept = i;
+					this.choosenInterceptPort = port;
+					return;
+				}
+			}
+		}
+	}
+	
+	private List<String> getValues() {
+		List<String> values = new ArrayList<String>();
+		for (Integer port : getPorts()) {
+			for (IViewableAttribute a : getViewableAttributes(port)) {
+				values.add(a.getName());
+			}
+		}
+		return values;
+	}
+
 
 }
