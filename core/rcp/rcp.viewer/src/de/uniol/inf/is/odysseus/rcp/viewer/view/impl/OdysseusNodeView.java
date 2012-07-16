4bfe6517278852a60a3e4b2cae4c8f61b12bacda
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-07-16 14:27:57 +0000

    FIX: Possible Null pointer Exception
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12469 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0055a663512ace4027159efe0c7c1740c28c8e21

--- core/rcp/rcp.viewer/src/de/uniol/inf/is/odysseus/rcp/viewer/view/impl/OdysseusNodeView.java
+++ core/rcp/rcp.viewer/src/de/uniol/inf/is/odysseus/rcp/viewer/view/impl/OdysseusNodeView.java
@@ -53,7 +53,7 @@ public class OdysseusNodeView extends DefaultNodeView<IPhysicalOperator> impleme
 		if(vis == false ) {
 			return false;
 		} else {
-			return getModelNode().getContent().hasOwner();
+			return (getModelNode().getContent() != null) &&  getModelNode().getContent().hasOwner();
 		}
 	}
 }
