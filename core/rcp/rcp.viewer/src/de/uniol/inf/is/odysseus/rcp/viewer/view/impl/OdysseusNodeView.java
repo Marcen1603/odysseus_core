3d13357461078eef91d875e3dc3b5156afdcaf4c
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-07-18 07:00:51 +0000

    Bugfix: Arrows with invisible nodes aren't drawn
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12486 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d7fa4c0ac2910658a1b224714917fb31d6d43431

--- core/rcp/rcp.viewer/src/de/uniol/inf/is/odysseus/rcp/viewer/view/impl/OdysseusNodeView.java
+++ core/rcp/rcp.viewer/src/de/uniol/inf/is/odysseus/rcp/viewer/view/impl/OdysseusNodeView.java
@@ -1,18 +1,18 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.rcp.viewer.view.impl;
 
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
@@ -21,39 +21,46 @@ import de.uniol.inf.is.odysseus.rcp.viewer.model.graph.INodeModelChangeListener;
 import de.uniol.inf.is.odysseus.rcp.viewer.model.graph.IOdysseusNodeModel;
 import de.uniol.inf.is.odysseus.rcp.viewer.view.IOdysseusNodeView;
 
-public class OdysseusNodeView extends DefaultNodeView<IPhysicalOperator> implements IOdysseusNodeView, INodeModelChangeListener< IPhysicalOperator > {
-	
-	public OdysseusNodeView( IOdysseusNodeModel data ) {
-		super( data );
+public class OdysseusNodeView extends DefaultNodeView<IPhysicalOperator> implements IOdysseusNodeView, INodeModelChangeListener<IPhysicalOperator> {
+
+	public OdysseusNodeView(IOdysseusNodeModel data) {
+		super(data);
 	}
 
 	@Override
-	public void nodeModelChanged( INodeModel< IPhysicalOperator > sender ) {}
+	public void nodeModelChanged(INodeModel<IPhysicalOperator> sender) {
+	}
 
 	@Override
 	public IOdysseusNodeModel getModelNode() {
-		return (IOdysseusNodeModel)super.getModelNode();
+		return (IOdysseusNodeModel) super.getModelNode();
 	}
 
 	@Override
 	public void connect() {
-		if( getModelNode() != null ) 
-			getModelNode().addNodeModelChangeListener( this );
+		if (getModelNode() != null)
+			getModelNode().addNodeModelChangeListener(this);
 	}
-	
+
 	@Override
 	public void disconnect() {
-		if( getModelNode() != null ) 
-			getModelNode().removeNodeModelChangeListener( this );
+		if (getModelNode() != null)
+			getModelNode().removeNodeModelChangeListener(this);
 	}
 
 	@Override
 	public boolean isVisible() {
 		boolean vis = super.isVisible();
-		if(vis == false ) {
+		if (vis == false) {
 			return false;
-		} else {
-			return getModelNode() != null && getModelNode().getContent() != null &&  getModelNode().getContent().hasOwner();
 		}
+
+		if (getModelNode() != null) {
+			if (getModelNode() != null) {
+				return getModelNode().getContent().hasOwner();
+			}
+		}
+
+		return vis;
 	}
 }
