79c41598f620e5c8e56b78769a95391f8b5397ce
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-08-22 13:14:27 +0000

    Added icon and empty command for adding pql-access-statements
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12693 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 2dfca6f157e110f58628e4c7cfc72b0e05af3883

---
+++ core/rcp/rcp.editor.text.pql/src/de/uniol/inf/is/odysseus/rcp/editor/text/pql/commands/ShowPQLAccessStatementWindowHandler.java
@@ -0,0 +1,15 @@
+package de.uniol.inf.is.odysseus.rcp.editor.text.pql.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ShowPQLAccessStatementWindowHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		return null;
+	}
+
+}
