b3e8faaa14610ff112310ac7237767c1f0b703e8
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-08-27 07:16:04 +0000

    Move SASelect rule to security punctuation, created feature for sp
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12738 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4f34ef15d4021c9840040d6e5c423077c9209a39

---
+++ core/securitypunctuation/src/de/uniol/inf/is/odysseus/securitypunctuation/transform/TSASelectAORule.java
@@ -0,0 +1,65 @@
+/********************************************************************************** 
+  * Copyright 2011 The Odysseus Team
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package de.uniol.inf.is.odysseus.securitypunctuation.transform;
+
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.SelectAO;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.SelectPO;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.intervalapproach.NElementHeartbeatGeneration;
+import de.uniol.inf.is.odysseus.ruleengine.ruleflow.IRuleFlowGroup;
+import de.uniol.inf.is.odysseus.securitypunctuation.physicaloperator.SASelectPO;
+import de.uniol.inf.is.odysseus.transform.flow.TransformRuleFlowGroup;
+import de.uniol.inf.is.odysseus.transform.rule.AbstractTransformationRule;
+
+public class TSASelectAORule extends AbstractTransformationRule<SelectAO> {
+
+	@Override
+	public int getPriority() {		
+		return 100;
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void execute(SelectAO selectAO, TransformationConfiguration transformConfig) {	
+		SelectPO<?> selectPO = new SASelectPO(selectAO.getPredicate());
+		if (selectAO.getHeartbeatRate() > 0){
+			selectPO.setHeartbeatGenerationStrategy(new NElementHeartbeatGeneration(
+					selectAO.getHeartbeatRate()));
+		}
+		defaultExecute(selectAO, selectPO, transformConfig, true, true);
+	}
+
+	@Override
+	public boolean isExecutable(SelectAO operator, TransformationConfiguration transformConfig) {
+		return operator.isAllPhysicalInputSet() && (transformConfig.getOption("isSecurityAware") != null) && ((Boolean)transformConfig.getOption("isSecurityAware")) ;
+	}
+
+	@Override
+	public String getName() {
+		return "SelectAO -> SASelectPO";
+	}
+	
+	@Override
+	public IRuleFlowGroup getRuleFlowGroup() {
+		return TransformRuleFlowGroup.TRANSFORMATION;
+	}
+	
+	@Override
+	public Class<? super SelectAO> getConditionClass() {	
+		return SelectAO.class;
+	}
+
+}
