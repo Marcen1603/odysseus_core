f9623be7947cd6b7de486c6530b061bd64422b15
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-08-27 11:33:32 +0000

    Reactivated odysseus console, initial import. Not functional yet
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12749 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e9694671b431027eb3a2dff4012da4023c68aaec

---
+++ core/server.console/src/de/uniol/inf/is/odysseus/core/server/console/ConsoleSink.java
@@ -0,0 +1,54 @@
+package de.uniol.inf.is.odysseus.core.server.console;
+ 
+import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
+import de.uniol.inf.is.odysseus.core.physicaloperator.OpenFailedException;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractSink;
+
+/** Copyright [2011] [The Odysseus Team]
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+
+
+public class ConsoleSink extends AbstractSink<Object> {
+
+	public ConsoleSink(){
+	}
+	
+	public ConsoleSink(ConsoleSink original){
+		super();
+	}
+	
+	@Override
+	protected void process_open() throws OpenFailedException {	
+	}
+	
+	@Override
+	protected void process_next(Object object, int port) {
+		System.out.println("Port:" + port + ", Object:" + object.toString());
+	}
+
+	@Override
+	public ConsoleSink clone()  {
+		return new ConsoleSink(this);
+	}
+
+	@Override
+	public void processPunctuation(PointInTime timestamp, int port) {
+		System.out.println("Port:" + port + ", PUNCTUATION: " + timestamp);
+	}
+
+	
+}
+
