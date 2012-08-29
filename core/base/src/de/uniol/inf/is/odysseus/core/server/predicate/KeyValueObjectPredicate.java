999247ae4dea0dae16eecb6be4ab9ce9e152d7ee
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-08-29 08:54:46 +0000

    New Predicate that can evaluated KeyValueObjects
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12785 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 2604799e5f85cc151a70d3d938a35c901b00c7ed

---
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/predicate/KeyValueObjectPredicate.java
@@ -0,0 +1,62 @@
+package de.uniol.inf.is.odysseus.core.server.predicate;
+
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.collection.KeyValueObject;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.server.sourcedescription.sdf.schema.SDFExpression;
+
+/**
+ * This predicate can be evaluated with a KeyValueObject. The attribute names in the expression
+ * must correspond to the keys in the KeyValueObject
+ * 
+ * @author Marco Grawunder
+ *
+ */
+
+public class KeyValueObjectPredicate extends AbstractPredicate<KeyValueObject<?>>{
+
+	private static final long serialVersionUID = 2982916759647665469L;
+
+	final SDFExpression expression;
+	final List<SDFAttribute> neededAttributes;
+	
+	public KeyValueObjectPredicate(SDFExpression expression){
+		this.expression = expression;
+		neededAttributes = expression.getAllAttributes();
+	}
+	
+	public KeyValueObjectPredicate(KeyValueObjectPredicate other){
+		this.expression = other.expression == null ? null : other.expression.clone();
+		neededAttributes = expression.getAllAttributes();
+	}
+	
+	@Override
+	public boolean evaluate(KeyValueObject<?> input) {
+		Object[] values = new Object[neededAttributes.size()];
+		for (int i = 0; i < values.length; ++i) {
+			values[i] = input.getAttribute(neededAttributes.get(i).getURI());
+		}
+		this.expression.bindVariables(values);
+		return (Boolean) this.expression.getValue();
+	}
+
+	@Override
+	public boolean evaluate(KeyValueObject<?> left, KeyValueObject<?> right) {
+		Object[] values = new Object[neededAttributes.size()];
+		for (int i = 0; i < values.length; ++i) {
+			values[i] = left.getAttribute(neededAttributes.get(i).getURI());
+			if (values[i]==null){
+				values[i] = right.getAttribute(neededAttributes.get(i).getURI());
+			}
+		}
+		this.expression.bindVariables(values);
+		return (Boolean) this.expression.getValue();
+	}
+
+	@Override
+	public AbstractPredicate<KeyValueObject<?>> clone() {
+		return new KeyValueObjectPredicate(this);
+	}
+
+}
