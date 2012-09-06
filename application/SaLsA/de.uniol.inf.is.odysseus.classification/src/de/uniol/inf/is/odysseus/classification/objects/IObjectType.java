f1934ea8d25e1c497b7e36d340025c0154a126f0
Christian Kuka <christian@kuka.cc>
2012-09-06 09:21:35 +0000

    SALSA: Remove classification bundle
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12873 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e96e1950063ef05bf5d4f72c5c8358b52f89e0fc

--- application/SaLsA/de.uniol.inf.is.odysseus.classification/src/de/uniol/inf/is/odysseus/classification/objects/IObjectType.java
+++
@@ -1,9 +0,0 @@
-package de.uniol.inf.is.odysseus.classification.objects;
-
-/**
- * @author Alexander Funk <alexander.funk@uni-oldenburg.de>
- * @author Christian Kuka <christian.kuka@offis.de>
- */
-public enum IObjectType {
-    GERADE, RUND, KONKAV, V_FORM, ECKIG;
-}
