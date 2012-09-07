1b206c4e15983db1357d5759be0773e56314e903
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-09-07 10:39:53 +0000

    New plugins: rcp.editor.text.pql.test and odysseus.testng
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12893 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a9ceebb4c72652a559e461b4c82be8ffb983a257

---
+++ core/rcp/de.uniol.inf.is.odysseus.testng/src/de/uniol/inf/is/odysseus/testng/TestUtil.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright 2012 The Odysseus Team
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.uniol.inf.is.odysseus.testng;
+
+import java.lang.reflect.Method;
+
+public class TestUtil {
+
+	@SuppressWarnings("unchecked")
+	public static <T> T invoke( String methodName, Class<?> clazz, Object... parameters ) throws Throwable {
+		try {
+			Class<?>[] parameterClasses = getClasses(parameters);
+			Method m = getMethod(methodName, clazz, parameterClasses);
+			
+			m.setAccessible(true);
+			
+			return (T)m.invoke(m, parameters);
+		} catch( Exception ex ) {
+			if( ex.getCause() != null ) {
+				throw ex.getCause();
+			} else {
+				throw ex;
+			}
+		}		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T invoke( String methodName, Object object, Object... parameters ) throws Throwable {
+		try {
+			Class<?>[] parameterClasses = getClasses(parameters);
+			Method m = getMethod(methodName, object.getClass(), parameterClasses);
+			m.setAccessible(true);
+			
+			return (T)m.invoke(object, parameters);
+		} catch( Exception ex ) {
+			if( ex.getCause() != null ) {
+				throw ex.getCause();
+			} else {
+				throw ex;
+			}
+		}
+	}
+
+	private static Class<?>[] getClasses(Object[] parameters) {
+		Class<?>[] classes = new Class<?>[parameters.length];
+		for( int i = 0; i < classes.length; i++ ) {
+			if( parameters[i] != null ) {
+				classes[i] = parameters[i].getClass();
+			} else {
+				classes[i] = Object.class;
+			}
+		}
+		return classes;
+	}
+	
+	private static Method getMethod( String name, Class<?> clazz, Class<?>[] parameterTypes ) throws Exception {
+		try {
+			return clazz.getDeclaredMethod(name, parameterTypes);
+		} catch( NoSuchMethodException ignored ) {
+			
+			// Happens with null-parameters
+			Method[] methods = clazz.getDeclaredMethods();
+			
+			Method foundMethod = null;
+			for( int i = 0; i < methods.length; i++ ) {
+				if( methods[i].getName().equals(name)) {
+					if( foundMethod != null ) {
+						throw new Exception("Method " + name + " of class " + clazz + " is ambiguous to invoke it easily.");
+					}
+					foundMethod = methods[i];
+				}
+			}
+			if( foundMethod == null ) {
+				throw new NoSuchMethodException(name + " of class " + clazz + " not found!");
+			}
+			
+			return foundMethod;
+		}
+	}
+}
