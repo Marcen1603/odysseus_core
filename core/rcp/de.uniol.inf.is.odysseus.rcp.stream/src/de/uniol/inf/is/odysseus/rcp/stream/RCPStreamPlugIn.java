dc22b678de4b1d6ef015518c8b3f674d386f93f2
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-09-14 08:23:47 +0000

    Refactoring
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@12968 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d6cc5fd2b05fe4ebd48bc1e00a4915e57b9bb9be

--- core/rcp/de.uniol.inf.is.odysseus.rcp.stream/src/de/uniol/inf/is/odysseus/rcp/stream/RCPStreamPlugIn.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.stream/src/de/uniol/inf/is/odysseus/rcp/stream/RCPStreamPlugIn.java
@@ -11,18 +11,10 @@ public class RCPStreamPlugIn implements BundleActivator {
 		return context;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		RCPStreamPlugIn.context = bundleContext;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		RCPStreamPlugIn.context = null;
 	}
