eca41a3ae46611ee868072de93b4bfc3deaadad8
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-09-27 13:32:19 +0000

    Added new PlugIn: de.uni.inf.is.odysseus.costmodel.operator.benchmark: Implements a cost-estimator for benchmarkAO
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13139 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ba358322a90e6202a44f656eceb43a15dc8c57a7

---
+++ costmodel/de.uniol.inf.is.odysseus.costmodel.operator.benchmark/src/de/uniol/inf/is/odysseus/costmodel/operator/benchmark/BenchmarkEstimator.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright 2012 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.uniol.inf.is.odysseus.costmodel.operator.benchmark;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.benchmarker.impl.BenchmarkPO;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.costmodel.operator.DataStream;
+import de.uniol.inf.is.odysseus.costmodel.operator.IDataStream;
+import de.uniol.inf.is.odysseus.costmodel.operator.IOperatorEstimator;
+import de.uniol.inf.is.odysseus.costmodel.operator.OperatorCostModelCfg;
+import de.uniol.inf.is.odysseus.costmodel.operator.OperatorDetailCost;
+import de.uniol.inf.is.odysseus.costmodel.operator.OperatorEstimation;
+import de.uniol.inf.is.odysseus.costmodel.operator.datasrc.IHistogram;
+import de.uniol.inf.is.odysseus.costmodel.operator.util.EstimatorHelper;
+
+@SuppressWarnings("rawtypes")
+public class BenchmarkEstimator implements IOperatorEstimator<BenchmarkPO> {
+
+	@Override
+	public Class<BenchmarkPO> getOperatorClass() {
+		return BenchmarkPO.class;
+	}
+
+	@Override
+	public OperatorEstimation estimateOperator(BenchmarkPO instance, List<OperatorEstimation> prevOperators, Map<SDFAttribute, IHistogram> baseHistograms) {
+		OperatorEstimation estimation = new OperatorEstimation(instance);
+		
+		Map<SDFAttribute, IHistogram> histograms = new HashMap<SDFAttribute, IHistogram>();
+		for( OperatorEstimation prevOp : prevOperators) {
+			histograms.putAll(prevOp.getHistograms());
+		}
+		estimation.setHistograms(histograms);
+		
+		estimation.setSelectivity(instance.getSelectivity());
+
+		IDataStream prevStream = prevOperators.get(0).getDataStream();
+		double datarate = EstimatorHelper.getDatarateMetadata(instance);
+		if( datarate < 0.0 ) {
+			datarate = prevStream.getDataRate();
+		}
+		estimation.setDataStream(new DataStream(instance, datarate, prevStream.getIntervalLength()));	
+		
+		double cpu = instance.getProcessingTime() / 1000.0f;
+		double cpuCost = 0.0;
+		if( cpu < 0.0 )
+			cpuCost = OperatorCostModelCfg.getInstance().getStandardCpuCost() * datarate;
+		else
+			cpuCost = cpu * datarate;
+		
+		estimation.setDetailCost(new OperatorDetailCost(instance, OperatorCostModelCfg.getInstance().getStandardMemCost(), cpuCost));
+
+		return estimation;
+	}
+
+}
