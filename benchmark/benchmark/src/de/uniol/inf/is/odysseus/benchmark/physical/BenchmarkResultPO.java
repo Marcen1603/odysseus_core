c374532b3c836818fb914afd898d79266a1decb7
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-10-01 13:15:41 +0000

    OPEN - issue ODY-425: Clean up Benchmarker http://odysseus.offis.uni-oldenburg.de:8081/browse/ODY-425
    Transfer before close!
    
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13179 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 01dc4d3091334c82dec96cd30d75df0c4cb66237

--- benchmark/benchmark/src/de/uniol/inf/is/odysseus/benchmark/physical/BenchmarkResultPO.java
+++ benchmark/benchmark/src/de/uniol/inf/is/odysseus/benchmark/physical/BenchmarkResultPO.java
@@ -1,18 +1,18 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * 
  */
@@ -40,22 +40,21 @@ public class BenchmarkResultPO<M extends ILatency> extends
 
 	private Lock lock = new ReentrantLock();
 	private IBenchmarkResultFactory<M> resultFactory;
-	private Map<Integer,IBenchmarkResult<M>> result = new HashMap<>();
+	private Map<Integer, IBenchmarkResult<M>> result = new HashMap<>();
 	private long resultsToRead = -1;
-	public int counter = 0;
 
-	public BenchmarkResultPO(IBenchmarkResultFactory<M> resultFactory, long resultsToRead) {
+	public BenchmarkResultPO(IBenchmarkResultFactory<M> resultFactory,
+			long resultsToRead) {
 		this.resultFactory = resultFactory;
 		this.resultsToRead = resultsToRead;
 	}
-	
-	public BenchmarkResultPO(BenchmarkResultPO<M> old){
+
+	public BenchmarkResultPO(BenchmarkResultPO<M> old) {
 		this.lock = old.lock;
 		this.resultFactory = old.resultFactory;
 		this.resultsToRead = old.resultsToRead;
-		this.counter = old.counter;
 	}
-	
+
 	@Override
 	public de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe.OutputMode getOutputMode() {
 		return OutputMode.NEW_ELEMENT;
@@ -69,32 +68,31 @@ public class BenchmarkResultPO<M extends ILatency> extends
 	}
 
 	int i = 0;
+
 	@Override
-	protected synchronized void process_next(Tuple<M> object,
-			int port) {
-		if (resultsToRead == -1 || result.get(port).size() < resultsToRead) {
-			result.get(port).add(object.getMetadata());
-		} else {
-			lock.lock();
-			inputDone(port);
-			done(port);
-			lock.unlock();
+	protected synchronized void process_next(Tuple<M> object, int port) {
+		if (isOpen()) {
+			if (resultsToRead == -1 || result.get(port).size() < resultsToRead) {
+				result.get(port).add(object.getMetadata());
+			} else {
+				lock.lock();
+				inputDone(port);
+				done(port);
+				lock.unlock();
+			}
 		}
 	}
 
 	private void inputDone(int port) {
-		if (--counter == 0) {
-			close();
-			result.get(port).setEndTime(System.nanoTime());
-			transfer(result.get(port),port);
-		}
+		result.get(port).setEndTime(System.nanoTime());
+		transfer(result.get(port), port);
+		close();
 	}
-
 
 	@Override
 	protected void process_open() throws OpenFailedException {
 		result.clear();
-		for (PhysicalSubscription<?> s: getSubscribedToSource()){
+		for (PhysicalSubscription<?> s : getSubscribedToSource()) {
 			result.put(s.getSinkInPort(), resultFactory.createBenchmarkResult());
 		}
 	}
@@ -103,12 +101,9 @@ public class BenchmarkResultPO<M extends ILatency> extends
 	public void processPunctuation(PointInTime timestamp, int port) {
 	}
 
-	
 	@Override
-	public BenchmarkResultPO<M> clone()  {
+	public BenchmarkResultPO<M> clone() {
 		return new BenchmarkResultPO<M>(this);
 	}
-
-
 
 }
