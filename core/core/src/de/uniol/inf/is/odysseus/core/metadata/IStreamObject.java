306e4231247ffe1a7f31f5a22fbd46ce72cae396
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-10-19 09:42:20 +0000

    added comments
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13370 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8012d006cd5c05324b03b633d95798f33385a6ae

--- core/core/src/de/uniol/inf/is/odysseus/core/metadata/IStreamObject.java
+++ core/core/src/de/uniol/inf/is/odysseus/core/metadata/IStreamObject.java
@@ -25,8 +25,18 @@ import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
  */
 public interface IStreamObject<M extends IMetaAttribute> extends
 		IClone, Serializable {
+	/**
+	 * Gets the default meta data used processing purposes (e.g. timestamps, priority or latency)
+	 * This meta data is assigned to each object in the meta data creation po 
+	 * 
+	 * @return
+	 */
 	public M getMetadata();
 
+	/**
+	 * Set the metadata object of this tuple. Typically done from the meta data creation po
+	 * @param metadata
+	 */
 	public void setMetadata(M metadata);
 
 	/**
