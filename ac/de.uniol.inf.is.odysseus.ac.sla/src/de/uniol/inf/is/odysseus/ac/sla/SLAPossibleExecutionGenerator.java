490c4efeecea50fd18a523343803a0811d3bcd85
Thomas Vogelgesang <Thomas.Vogelgesang@uni-oldenburg.de>
2012-10-24 14:12:22 +0000

    implemented SLAPossibleExecutionGenerator
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13405 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9f25e86abab7bfa8e3f105c83a13412a65bff71a

--- ac/de.uniol.inf.is.odysseus.ac.sla/src/de/uniol/inf/is/odysseus/ac/sla/SLAPossibleExecutionGenerator.java
+++ ac/de.uniol.inf.is.odysseus.ac.sla/src/de/uniol/inf/is/odysseus/ac/sla/SLAPossibleExecutionGenerator.java
@@ -1,13 +1,16 @@
 package de.uniol.inf.is.odysseus.ac.sla;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import de.uniol.inf.is.odysseus.core.server.ac.IAdmissionControl;
 import de.uniol.inf.is.odysseus.core.server.ac.IPossibleExecution;
 import de.uniol.inf.is.odysseus.core.server.ac.IPossibleExecutionGenerator;
+import de.uniol.inf.is.odysseus.core.server.ac.StandardPossibleExecution;
 import de.uniol.inf.is.odysseus.core.server.costmodel.ICost;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.IPhysicalQuery;
+import de.uniol.inf.is.odysseus.core.server.sla.SLA;
 
 public class SLAPossibleExecutionGenerator implements
 		IPossibleExecutionGenerator {
@@ -16,8 +19,37 @@ public class SLAPossibleExecutionGenerator implements
 	public List<IPossibleExecution> getPossibleExecutions(
 			IAdmissionControl sender, Map<IPhysicalQuery, ICost> queryCosts,
 			ICost maxCost) {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<IPossibleExecution> result = new ArrayList<>();
+		IPhysicalQuery mostExpensiveQuery = null;
+		double maxPenalty = -1.0;
+		ICost maxQueryCost = null;
+
+		for (IPhysicalQuery q : queryCosts.keySet()) {
+			SLA sla = (SLA) q.getParameter(SLA.class.getName());
+
+			if (sla != null) {
+				double killPenalty = sla.getQueryKillPenalty().getCost();
+				
+				if (killPenalty == maxPenalty) {
+					ICost queryCost = queryCosts.get(q);
+					int compare = queryCost.compareTo(maxQueryCost);
+					if (compare > 0) {
+						maxQueryCost = queryCost;
+						mostExpensiveQuery = q;
+					}
+				} else if (killPenalty > maxPenalty) {
+					maxPenalty = killPenalty;
+					mostExpensiveQuery = q;
+					maxQueryCost = queryCosts.get(q);
+				}
+			}
+		}
+		
+		IPossibleExecution possibleExec = StandardPossibleExecution.stopOneQuery(queryCosts, mostExpensiveQuery);
+		result.add(possibleExec);
+		
+		return result;
 	}
 
 }
