1d7ca4d025529a13c072bd119d2fb1b329196a94
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-11-01 15:25:04 +0000

    Refactoring
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13473 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 95e21db5ca768996e1eb6d2204ac09f61120b2f1

--- ac/de.uniol.inf.is.odysseus.ac.standard/src/de/uniol/inf/is/odysseus/ac/standard/StandardPossibleExecutionGenerator.java
+++ ac/de.uniol.inf.is.odysseus.ac.standard/src/de/uniol/inf/is/odysseus/ac/standard/StandardPossibleExecutionGenerator.java
@@ -22,6 +22,8 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
+
 import de.uniol.inf.is.odysseus.core.server.ac.IAdmissionControl;
 import de.uniol.inf.is.odysseus.core.server.ac.IPossibleExecution;
 import de.uniol.inf.is.odysseus.core.server.ac.IPossibleExecutionGenerator;
@@ -31,12 +33,12 @@ import de.uniol.inf.is.odysseus.core.server.costmodel.ICostModel;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.IPhysicalQuery;
 
 /**
- * Standardimplementierung von {@link IPossibleExecutionGenerator}.
- * Erstellt aus der Menge aller Anfragen eine Liste möglicher Anfragekombinationen
- * nach Brute-Force. 
+ * Standardimplementierung von {@link IPossibleExecutionGenerator}. Erstellt aus
+ * der Menge aller Anfragen eine Liste möglicher Anfragekombinationen nach
+ * Brute-Force.
  * 
  * @author Timo Michelsen
- *
+ * 
  */
 public class StandardPossibleExecutionGenerator implements IPossibleExecutionGenerator {
 
@@ -45,136 +47,51 @@ public class StandardPossibleExecutionGenerator implements IPossibleExecutionGen
 	@Override
 	public List<IPossibleExecution> getPossibleExecutions(IAdmissionControl ac, Map<IPhysicalQuery, ICost> queryCosts, ICost maxCost) {
 
-		List<IPossibleExecution> poss = new ArrayList<IPossibleExecution>();
+		List<IPossibleExecution> poss = Lists.newArrayList();
 
 		// first solution: all queries running!
-		ICost costSum = null;
-		for (ICost cost : queryCosts.values()) {
-			if (costSum == null) {
-				costSum = cost;
-			} else {
-				costSum = costSum.merge(cost);
-			}
-		}
+		ICost costSum = sum(queryCosts.values());
 
 		int cmp = costSum.compareTo(maxCost);
 		if (cmp == -1 || cmp == 0) {
 			poss.add(new StandardPossibleExecution(queryCosts.keySet(), new ArrayList<IPhysicalQuery>(), costSum));
 			LOGGER.debug("Possible Execution: execute all queries");
 		}
-		
-		// only one query?
-		StandardAC stdAC = (StandardAC)ac;
-		ICostModel cm = stdAC.getSelectedCostModelInstance();
-		if( queryCosts.size() == 1 ) {
-			// nur eine Anfrage, die gestoppt werden sollte
-			poss.add(new StandardPossibleExecution(new ArrayList<IPhysicalQuery>(), queryCosts.keySet(), cm.getZeroCost()));
+
+		if (poss.isEmpty()) {
+			StandardAC stdAC = (StandardAC) ac;
+			ICostModel cm = stdAC.getSelectedCostModelInstance();
+			
+			List<IPhysicalQuery> runningQueries = Lists.newArrayList();
+			List<IPhysicalQuery> stoppingQueries = Lists.newArrayList();
+
+			ICost actSum = cm.getZeroCost();
+			for (IPhysicalQuery query : queryCosts.keySet()) {
+				ICost cost = queryCosts.get(query);
+				ICost newSum = actSum.merge(cost);
+				if (newSum.compareTo(maxCost) < 0) {
+					runningQueries.add(query);
+					actSum = newSum;
+				} else {
+					stoppingQueries.add(query);
+				}
+			}
+
+			poss.add(new StandardPossibleExecution(runningQueries, stoppingQueries, actSum));
 		}
 
-		if( poss.isEmpty() ) {
-    		// generate exactly one possible execution
-    		// brute-force too load-heavy
-    		List<IPhysicalQuery> runningQueries = new ArrayList<IPhysicalQuery>();
-    		List<IPhysicalQuery> stoppingQueries = new ArrayList<IPhysicalQuery>();
-    		ICost actSum = cm.getZeroCost();
-    		for( IPhysicalQuery query : queryCosts.keySet() ) {
-    			ICost cost = queryCosts.get(query);
-    			ICost newSum = actSum.merge(cost);
-    			if( newSum.compareTo(maxCost) < 0 ) {
-    				runningQueries.add(query);
-    				actSum = newSum;
-    			} else {
-    				stoppingQueries.add(query);
-    			}
-    		}
-    		
-    		poss.add(new StandardPossibleExecution(runningQueries, stoppingQueries, actSum));
-		}
-		
-        return poss;
-				
-		// TOOO HEAVY:
-//		// generate all combinations
-//		List<IQuery> queries = new ArrayList<IQuery>(queryCosts.keySet());
-//		List<List<IQuery>> solutions = powerset(queries);
-//
-//		// test combinations
-//		// one combination contains
-//		// only the quries which must be started
-//		for (List<IQuery> solution : solutions) {
-//
-//			// this solution was calculated above and failed..
-//			// do not try it again
-//			if (solution.size() == 0 || solution.size() == queryCosts.size())
-//				continue;
-//
-//			// get summed cost of solution
-//			ICost solutionCost = null;
-//			for (IQuery solutionQuery : solution) {
-//				if (solutionCost == null)
-//					solutionCost = queryCosts.get(solutionQuery);
-//				else
-//					solutionCost = solutionCost.merge(queryCosts.get(solutionQuery));
-//			}
-//
-//			cmp = solutionCost.compareTo(maxCost);
-//			if (cmp == -1 || cmp == 0) {
-//
-//				// solution is ok...
-//				// get queries who must be stopped
-//				List<IQuery> stoppedQueries = new ArrayList<IQuery>();
-//				for (IQuery query : queryCosts.keySet())
-//					if (!solution.contains(query))
-//						stoppedQueries.add(query);
-//
-//				// generate solution
-//				poss.add(new StandardPossibleExecution(solution, stoppedQueries, solutionCost));
-//				getLogger().debug("Possible Execution: " + solution);
-//				return poss;
-//			}
-//
-//		}
+		return poss;
 	}
 
-//	private static List<List<IQuery>> powerset(List<IQuery> queries) {
-//
-//		List<List<IQuery>> power = new ArrayList<List<IQuery>>();
-//
-//		int elements = queries.size();
-//		int powerElements = (int) Math.pow(2, elements);
-//
-//		for (int i = 0; i < powerElements; i++) {
-//			String binary = intToBinary(i, elements);
-//
-//			// create a new set
-//			List<IQuery> queryList = new ArrayList<IQuery>();
-//
-//			// convert each digit in the current binary number to the
-//			// corresponding element
-//			// in the given set
-//			for (int j = 0; j < binary.length(); j++) {
-//				if (binary.charAt(j) == '1')
-//					queryList.add(queries.get(j));
-//			}
-//
-//			// add the new set to the power set
-//			power.add(queryList);
-//
-//		}
-//
-//		return power;
-//	}
-
-//	private static String intToBinary(int binary, int digits) {
-//
-//		String temp = Integer.toBinaryString(binary);
-//		int foundDigits = temp.length();
-//		String returner = temp;
-//		for (int i = foundDigits; i < digits; i++) {
-//			returner = "0" + returner;
-//		}
-//
-//		return returner;
-//	}
-
+	private static ICost sum(Iterable<ICost> queryCosts) {
+		ICost costSum = null;
+		for (ICost cost : queryCosts) {
+			if (costSum == null) {
+				costSum = cost;
+			} else {
+				costSum = costSum.merge(cost);
+			}
+		}
+		return costSum;
+	}
 }
