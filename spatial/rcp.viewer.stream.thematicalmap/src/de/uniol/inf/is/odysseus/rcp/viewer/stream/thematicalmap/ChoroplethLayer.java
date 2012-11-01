1cc586b68b84efeead6ab3625913e6217a9a5acf
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-11-01 13:38:20 +0000

    Changed so that no compile errors are in the project. Needs FIX!
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13469 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d858ead6adf89b470178aff339b61c2be87849cc

--- spatial/rcp.viewer.stream.thematicalmap/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/thematicalmap/ChoroplethLayer.java
+++ spatial/rcp.viewer.stream.thematicalmap/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/thematicalmap/ChoroplethLayer.java
@@ -10,27 +10,37 @@ import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Polygon;
 
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
+import de.uniol.inf.is.odysseus.rcp.viewer.stream.map.ScreenManager;
 import de.uniol.inf.is.odysseus.rcp.viewer.stream.map.ScreenTransformation;
 import de.uniol.inf.is.odysseus.rcp.viewer.stream.map.layer.AbstractLayer;
 
-public class ChoroplethLayer extends AbstractLayer{
-//	private static final Logger LOG = LoggerFactory.getLogger(ChoroplethLayer.class);
+public class ChoroplethLayer extends AbstractLayer {
+	// private static final Logger LOG =
+	// LoggerFactory.getLogger(ChoroplethLayer.class);
 	private LinkedList<GeometryAndValue> geometryAndValueList = null;
 	private ScreenTransformation transformation = null;
 	private SDFAttribute sdfAttribute = null;
 	private Legend legend = null;
-	
+
 	private int geoStreamPosition;
 	private int valueStreamPosition;
-	
+
 	public int getGeoStreamPosition() {
 		return geoStreamPosition;
 	}
+
 	public int getValueStreamPosition() {
 		return valueStreamPosition;
 	}
-	public ChoroplethLayer(ScreenTransformation transformation, SDFAttribute sdfAttribute, int geoStreamPosition, int valueStreamPosition) {
+
+	public ChoroplethLayer(ScreenTransformation transformation,
+			SDFAttribute sdfAttribute, int geoStreamPosition,
+			int valueStreamPosition) {
+		// FIXME:
+		super(null);
 		this.geometryAndValueList = new LinkedList<GeometryAndValue>();
 		this.transformation = transformation;
 		this.sdfAttribute = sdfAttribute;
@@ -39,11 +49,15 @@ public class ChoroplethLayer extends AbstractLayer{
 		this.valueStreamPosition = valueStreamPosition;
 		this.legend = new Legend();
 	}
-	public ChoroplethLayer(String name, ScreenTransformation transformation, SDFAttribute sdfAttribute, int geoStreamPosition, int valueStreamPosition) {
-		this(transformation, sdfAttribute, geoStreamPosition, valueStreamPosition);
+
+	public ChoroplethLayer(String name, ScreenTransformation transformation,
+			SDFAttribute sdfAttribute, int geoStreamPosition,
+			int valueStreamPosition) {
+		this(transformation, sdfAttribute, geoStreamPosition,
+				valueStreamPosition);
 		this.name = name;
 	}
-	
+
 	public void draw(GC gc) {
 		synchronized (geometryAndValueList) {
 			for (GeometryAndValue geometryAndValue : geometryAndValueList) {
@@ -51,70 +65,122 @@ public class ChoroplethLayer extends AbstractLayer{
 			}
 		}
 	}
+
 	private void drawGeometryAndValue(GeometryAndValue geometryAndValue, GC gc) {
 		if (geometryAndValue.getGeometry() instanceof Polygon) {
-			drawPolygonAndValue((Polygon)geometryAndValue.getGeometry(), geometryAndValue.getValue(), gc);
-		}else if (geometryAndValue.getGeometry() instanceof GeometryCollection) {
-			drawGeometryCollectionAndValue((GeometryCollection) geometryAndValue.getGeometry(), geometryAndValue.getValue(), gc);
+			drawPolygonAndValue((Polygon) geometryAndValue.getGeometry(),
+					geometryAndValue.getValue(), gc);
+		} else if (geometryAndValue.getGeometry() instanceof GeometryCollection) {
+			drawGeometryCollectionAndValue(
+					(GeometryCollection) geometryAndValue.getGeometry(),
+					geometryAndValue.getValue(), gc);
 		}
 	}
-	private void drawGeometryCollectionAndValue(GeometryCollection geometryCollection, Integer value, GC gc) {
+
+	private void drawGeometryCollectionAndValue(
+			GeometryCollection geometryCollection, Integer value, GC gc) {
 		for (int i = 0; i < geometryCollection.getNumGeometries(); i++) {
-			drawGeometryAndValue( new GeometryAndValue(geometryCollection.getGeometryN(i), value) , gc);
+			drawGeometryAndValue(
+					new GeometryAndValue(geometryCollection.getGeometryN(i),
+							value), gc);
 		}
 
 	}
+
 	private void drawPolygonAndValue(Polygon polygon, Integer value, GC gc) {
 		int[][] list = new int[polygon.getNumInteriorRing() + 1][];
 		list[0] = drawLinearRing(polygon.getExteriorRing(), gc);
 		for (int n = 0; n < polygon.getNumInteriorRing(); n++) {
 			list[n + 1] = drawLinearRing(polygon.getInteriorRingN(n), gc);
 		}
-//		this.style.setActiveStyle(polygon);
-//		this.style.draw(gc, list);	
+		// this.style.setActiveStyle(polygon);
+		// this.style.draw(gc, list);
 		legend.getStyleForValue(value).draw(gc, list);
-		
+
 	}
+
 	private int[] drawLinearRing(LineString lineString, GC gc) {
 		int i = 0;
 		int[] path = new int[lineString.getNumPoints()
 				+ lineString.getNumPoints()];
 		for (Coordinate coord : lineString.getCoordinates()) {
-			int[] uv = transformation.transformCoord(coord,lineString.getSRID());
+			int[] uv = transformation.transformCoord(coord,
+					lineString.getSRID());
 			path[i++] = uv[0];
 			path[i++] = uv[1];
 		}
 		return path;
 	}
+
 	public void addGeometryAndValue(Geometry geometry, Integer value) {
 		synchronized (geometryAndValueList) {
-			this.geometryAndValueList.offer(new GeometryAndValue(geometry, value));
+			this.geometryAndValueList.offer(new GeometryAndValue(geometry,
+					value));
 		}
 	}
+
 	public SDFAttribute getNativeAttribute() {
 		return this.sdfAttribute;
 	}
-	
-	class GeometryAndValue{
+
+	class GeometryAndValue {
 		private Geometry geometry;
 		private Integer value;
-		
+
 		public GeometryAndValue(Geometry geometry, Integer value) {
 			super();
 			this.geometry = geometry;
 			this.value = value;
 		}
+
 		public Geometry getGeometry() {
 			return geometry;
 		}
+
 		public void setGeometry(Geometry geometry) {
 			this.geometry = geometry;
 		}
+
 		public Integer getValue() {
 			return value;
 		}
+
 		public void setValue(Integer value) {
 			this.value = value;
 		}
+
 	}
+
+	@Override
+	public void init(ScreenManager screenManager, SDFSchema schema,
+			SDFAttribute attribute) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String[] getSupprtedDatatypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTuple(Tuple<?> tuple) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeLast() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getTupleCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
 }
