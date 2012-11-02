b431a4ee0729d12054e71b143b73d787b23e3783
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-11-02 15:44:45 +0000

    Bugfix: sometimes concurrentModificationException
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13489 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d7d24ecaa700de7c94a97a0f75ee1cf6eea1b74c

--- ac/de.uniol.inf.is.odysseus.ac.standard/src/de/uniol/inf/is/odysseus/ac/standard/StandardAC.java
+++ ac/de.uniol.inf.is.odysseus.ac.standard/src/de/uniol/inf/is/odysseus/ac/standard/StandardAC.java
@@ -572,7 +572,7 @@ public class StandardAC implements IAdmissionControl, IPlanModificationListener 
 	private static List<IPhysicalOperator> getAllOperators(IServerExecutor executor) {
 		List<IPhysicalOperator> operators = new ArrayList<IPhysicalOperator>();
 
-		for (IPhysicalQuery query : executor.getExecutionPlan().getQueries()) {
+		for (IPhysicalQuery query : executor.getExecutionPlan().getQueries().toArray(new IPhysicalQuery[0])) {
 			for (IPhysicalOperator op : query.getPhysicalChilds()) {
 				if (!operators.contains(op) && !op.getClass().getSimpleName().contains("DataSourceObserverSink") && op.getOwner().contains(query)) {
 					operators.add(op);
