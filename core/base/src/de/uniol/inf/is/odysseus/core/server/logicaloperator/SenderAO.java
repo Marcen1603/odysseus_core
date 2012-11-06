558896f3639ef8ed0f37ad7627bf231c0f9b422b
Christian Kuka <christian@kuka.cc>
2012-11-06 12:11:03 +0000

    Add SenderAO for generic transfer of processing results
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13527 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 55e0a96150dfadaa4112cdc38eb2c677b135327a

--- core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/SenderAO.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/SenderAO.java
@@ -1,43 +1,97 @@
+/********************************************************************************** 
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.core.server.logicaloperator;
 
 import java.util.Map;
 
-import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.LogicalOperator;
-import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.Parameter;
-import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.StringParameter;
-
-@LogicalOperator(maxInputPorts = Integer.MAX_VALUE, minInputPorts = 1, name = "Sender")
+/**
+ * Generic sender operator to transfer processing results to arbitrary targets
+ * using existing transport, protocol, and data handler.
+ * Example: <code>
+ * Sender({wrapper='GenericPush', transport='File',protocol='SimpleCSV',
+ * dataHandler='Tuple',options=[['filename','outfile.txt']]}, result)
+ * </code>
+ * 
+ * @author Christian Kuka <christian.kuka@offis.de>
+ */
+// @LogicalOperator(maxInputPorts = Integer.MAX_VALUE, minInputPorts = 1, name =
+// "Sender")
 public class SenderAO extends AbstractLogicalOperator {
     /**
      * 
      */
     private static final long   serialVersionUID = -6830784739913623456L;
-    private String              dataHandler      = "Tuple";
 
+    private String              dataHandler      = "Tuple";
     private String              protocolHandler;
     private String              transportHandler;
     private Map<String, String> optionsMap;
     private String              wrapper;
 
+    /**
+     * @param po
+     *            The {@link AbstractLogicalOperator} operator
+     */
     public SenderAO(AbstractLogicalOperator po) {
         super(po);
     }
 
+    /**
+     * Default constructor
+     */
     public SenderAO() {
         super();
     }
 
+    /**
+     * Creates a new {@link SenderAO} with the given wrapper and options
+     * 
+     * @param wrapper
+     *            The wrapper name
+     * @param optionsMap
+     *            The options
+     */
     public SenderAO(String wrapper, Map<String, String> optionsMap) {
         this.wrapper = wrapper;
         this.optionsMap = optionsMap;
     }
 
+    /**
+     * Creates a new {@link SenderAO} with the given wrapper, data handler, and
+     * options
+     * 
+     * @param wrapper
+     *            The wrapper name
+     * @param dataHandler
+     *            The name of the data handler
+     * @param optionsMap
+     *            The options
+     */
     public SenderAO(String wrapper, String dataHandler, Map<String, String> optionsMap) {
         this.wrapper = wrapper;
         this.dataHandler = dataHandler;
         this.optionsMap = optionsMap;
     }
 
+    /**
+     * Clone constructor
+     * 
+     * @param senderAO
+     *            The {@link SenderAO} instance
+     */
     public SenderAO(SenderAO senderAO) {
         this.wrapper = senderAO.wrapper;
         this.dataHandler = senderAO.dataHandler;
@@ -46,51 +100,108 @@ public class SenderAO extends AbstractLogicalOperator {
         this.transportHandler = senderAO.transportHandler;
     }
 
-    // @Parameter(name = "options", type = MapParameter.class, optional = true,)
+    /**
+     * Set the options of the sender operator for transport and prrotocol
+     * handler
+     * 
+     * @param value
+     *            A {@link Map} of options
+     */
+    // @Parameter(name = "options", type = StringParameter.class, optional =
+    // true, isMap = true)
     public void setOptions(Map<String, String> value) {
         this.optionsMap = value;
     }
 
+    /**
+     * @return The options
+     */
     public Map<String, String> getOptionsMap() {
         return optionsMap;
     }
 
-    @Parameter(name = "wrapper", type = StringParameter.class, optional = false)
+    /**
+     * Set the wrapper type
+     * 
+     * @param wrapper
+     *            The wrapper type
+     */
+    // @Parameter(name = "wrapper", type = StringParameter.class, optional =
+    // false)
     public void setWrapper(String wrapper) {
         this.wrapper = wrapper;
     }
 
+    /**
+     * @return The wrapper type
+     */
     public String getWrapper() {
         return wrapper;
     }
 
+    /**
+     * @return The name of the data handler
+     */
     public String getDataHandler() {
         return dataHandler;
     }
 
-    @Parameter(name = "dataHandler", type = StringParameter.class, optional = false)
+    /**
+     * Set the name of the data handler ie.: Tuple
+     * 
+     * @param dataHandler
+     *            The name of the data handler
+     */
+    // @Parameter(name = "dataHandler", type = StringParameter.class, optional =
+    // false)
     public void setDataHandler(String dataHandler) {
         this.dataHandler = dataHandler;
     }
 
+    /**
+     * @return The name of the protocol handler
+     */
     public String getProtocolHandler() {
         return protocolHandler;
     }
 
-    @Parameter(name = "protocol", type = StringParameter.class, optional = false)
+    /**
+     * Set the name of the protocol handler ie.: CSV
+     * 
+     * @param protocolHandler
+     *            The name of the protocol handler
+     */
+    // @Parameter(name = "protocol", type = StringParameter.class, optional =
+    // false)
     public void setProtocolHandler(String protocolHandler) {
         this.protocolHandler = protocolHandler;
     }
 
+    /**
+     * @return The name of the transport handler
+     */
     public String getTransportHandler() {
         return transportHandler;
     }
 
-    @Parameter(name = "transport", type = StringParameter.class, optional = false)
+    /**
+     * Set the name of the transport handler ie.: TCPClient
+     * 
+     * @param transportHandler
+     *            The name of the transport handler
+     */
+    // @Parameter(name = "transport", type = StringParameter.class, optional =
+    // false)
     public void setTransportHandler(String transportHandler) {
         this.transportHandler = transportHandler;
     }
 
+    /*
+     * (non-Javadoc)
+     * @see
+     * de.uniol.inf.is.odysseus.core.server.logicaloperator.AbstractLogicalOperator
+     * #clone()
+     */
     @Override
     public AbstractLogicalOperator clone() {
         return new SenderAO(this);
---
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/builder/SenderAOBuilder.java
@@ -0,0 +1,120 @@
+/********************************************************************************** 
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.core.server.logicaloperator.builder;
+
+import java.util.HashMap;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.IParameter.REQUIREMENT;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.SenderAO;
+
+/**
+ * Operator builder for the {@link SenderAO}
+ * 
+ * @author Christian Kuka <christian.kuka@offis.de>
+ */
+public class SenderAOBuilder extends AbstractOperatorBuilder {
+    /**
+     * 
+     */
+    private static final long           serialVersionUID = 7352271768755308949L;
+    /** Name of the wrapper */
+    private final StringParameter       wrapper          = new StringParameter("WRAPPER", REQUIREMENT.OPTIONAL);
+    /** Name of the data handler */
+    private final StringParameter       dataHandler      = new StringParameter("DATAHANDLER", REQUIREMENT.OPTIONAL);
+    /** Name of the transport handler */
+    private final StringParameter       transportHandler = new StringParameter("TRANSPORT", REQUIREMENT.OPTIONAL);
+    /** Name of the protocol handler */
+    private final StringParameter       protocolHandler  = new StringParameter("PROTOCOL", REQUIREMENT.OPTIONAL);
+
+    /** Options as key value map */
+    private final ListParameter<Option> options          = new ListParameter<Option>("OPTIONS", REQUIREMENT.OPTIONAL,
+                                                                 new CreateOptionParameter("OPTION",
+                                                                         REQUIREMENT.MANDATORY));
+
+    /**
+     * Default constructor for the operator builder
+     */
+    public SenderAOBuilder() {
+        super("SENDER", 1, Integer.MAX_VALUE);
+        // Add the supported parameters
+        this.addParameters(this.options, this.dataHandler, this.transportHandler, this.protocolHandler, this.wrapper);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.IOperatorBuilder
+     * #cleanCopy()
+     */
+    @Override
+    public IOperatorBuilder cleanCopy() {
+        return new SenderAOBuilder();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.
+     * AbstractOperatorBuilder#internalValidation()
+     */
+    @Override
+    protected boolean internalValidation() {
+        if (!this.wrapper.hasValue()) {
+            this.addError(new IllegalArgumentException(
+                    "to less information for the creation of sender. expecting wrapper."));
+            return false;
+        }
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.
+     * AbstractOperatorBuilder#createOperatorInternal()
+     */
+    @Override
+    protected ILogicalOperator createOperatorInternal() {
+        final ILogicalOperator ao = this.createNewSenderAO();
+        return ao;
+    }
+
+    /**
+     * @return A new instance of {@link SenderAO} with the defined parameter
+     */
+    private ILogicalOperator createNewSenderAO() {
+        final HashMap<String, String> optionsMap = new HashMap<String, String>();
+        if (this.options.hasValue()) {
+            for (final Option option : this.options.getValue()) {
+                optionsMap.put(option.getName().toLowerCase(), option.getValue());
+            }
+        }
+        final SenderAO ao = new SenderAO(this.wrapper.getValue(), optionsMap);
+        if (this.dataHandler.hasValue()) {
+            ao.setDataHandler(this.dataHandler.getValue());
+        }
+        else {
+            ao.setDataHandler(null);
+        }
+        if (this.transportHandler.hasValue()) {
+            ao.setTransportHandler(this.transportHandler.getValue());
+        }
+        if (this.protocolHandler.hasValue()) {
+            ao.setProtocolHandler(this.protocolHandler.getValue());
+        }
+        return ao;
+    }
+
+}
