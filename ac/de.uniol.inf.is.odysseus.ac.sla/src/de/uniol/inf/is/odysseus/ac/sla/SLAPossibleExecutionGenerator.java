a054172e2982df5d6d1be50366231a247c67c1dd
Thomas Vogelgesang <Thomas.Vogelgesang@uni-oldenburg.de>
2012-11-07 10:27:30 +0000

    fixed sla-based possible execution generator
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13570 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: dec0d5f764c39488f9b11cc46ec2bd937bf107f9

--- ac/de.uniol.inf.is.odysseus.ac.sla/src/de/uniol/inf/is/odysseus/ac/sla/SLAPossibleExecutionGenerator.java
+++ ac/de.uniol.inf.is.odysseus.ac.sla/src/de/uniol/inf/is/odysseus/ac/sla/SLAPossibleExecutionGenerator.java
@@ -4,41 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
 import de.uniol.inf.is.odysseus.core.server.ac.IAdmissionControl;
 import de.uniol.inf.is.odysseus.core.server.ac.IPossibleExecution;
 import de.uniol.inf.is.odysseus.core.server.ac.IPossibleExecutionGenerator;
 import de.uniol.inf.is.odysseus.core.server.ac.StandardPossibleExecution;
 import de.uniol.inf.is.odysseus.core.server.costmodel.ICost;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.IServerExecutor;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.IPhysicalQuery;
+import de.uniol.inf.is.odysseus.core.server.scheduler.IScheduler;
 import de.uniol.inf.is.odysseus.core.server.sla.SLA;
+import de.uniol.inf.is.odysseus.scheduler.singlethreadscheduler.IPhysicalQueryScheduling;
+import de.uniol.inf.is.odysseus.scheduler.singlethreadscheduler.SimpleThreadScheduler;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.SLAPartialPlanScheduling;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.SLARegistry;
+import de.uniol.inf.is.odysseus.scheduler.slascheduler.SLARegistryInfo;
 
 public class SLAPossibleExecutionGenerator implements IPossibleExecutionGenerator {
+	
+	private SLARegistry slaRegistry;
 
 	@Override
-	public List<IPossibleExecution> getPossibleExecutions(IAdmissionControl sender, Map<IPhysicalQuery, ICost> queryCosts, ICost overallCost, ICost maxCost) {
-
+	public List<IPossibleExecution> getPossibleExecutions(
+			IAdmissionControl sender, Map<IPhysicalQuery, ICost> queryCosts,
+			ICost overallCost, ICost maxCost) {
+	
 		List<IPossibleExecution> result = new ArrayList<>();
 		IPhysicalQuery mostExpensiveQuery = null;
 		double maxPenalty = -1.0;
-		ICost maxQueryCost = null;
 
 		for (IPhysicalQuery q : queryCosts.keySet()) {
 			SLA sla = (SLA) q.getParameter(SLA.class.getName());
-
-			if (sla != null) {
+			SLARegistryInfo info = this.slaRegistry.getData(q);
+			
+			if (sla != null && info != null) {
 				double killPenalty = sla.getQueryKillPenalty().getCost();
+				double slViolationPenalty = this.getCostOfServiceLevel(info, sla);
 
-				if (killPenalty == maxPenalty) {
-					ICost queryCost = queryCosts.get(q);
-					int compare = queryCost.compareTo(maxQueryCost);
-					if (compare > 0) {
-						maxQueryCost = queryCost;
-						mostExpensiveQuery = q;
-					}
-				} else if (killPenalty > maxPenalty) {
-					maxPenalty = killPenalty;
+				double penaltyDifference = killPenalty - slViolationPenalty;
+				
+				if (penaltyDifference > maxPenalty) {
+					maxPenalty = penaltyDifference;
 					mostExpensiveQuery = q;
-					maxQueryCost = queryCosts.get(q);
 				}
 			}
 		}
@@ -47,6 +54,37 @@ public class SLAPossibleExecutionGenerator implements IPossibleExecutionGenerato
 		result.add(possibleExec);
 
 		return result;
+	}
+	
+	private double getCostOfServiceLevel(SLARegistryInfo info, SLA sla) {
+		double conformance = info.getConformance().getConformance();
+		int slIndex = info.getCostFunction().getCurrentServiceLevelIndex(conformance, sla);
+		double slViolationPenalty = sla.getServiceLevel().get(slIndex).getPenalty().getCost();
+		
+		return slViolationPenalty;
+	}
+	
+	public void bindExecutor(IExecutor executor) {
+		if (executor instanceof IServerExecutor) {
+			IServerExecutor serverExecutor = (IServerExecutor) executor;
+			IScheduler scheduler = serverExecutor.getSchedulerManager().getActiveScheduler();
+			if (scheduler instanceof SimpleThreadScheduler) {
+				SimpleThreadScheduler stScheduler = (SimpleThreadScheduler) scheduler;
+				IPhysicalQueryScheduling[] planScheduling = stScheduler.getPlanScheduling();
+				for (IPhysicalQueryScheduling pqs: planScheduling) {
+					if (pqs instanceof SLAPartialPlanScheduling) {
+						SLAPartialPlanScheduling slaSched = (SLAPartialPlanScheduling)planScheduling[0];
+						this.slaRegistry = slaSched.getRegistry();
+						break;
+					}
+				}
+				
+			}
+		}
+	}
+	
+	public void unbindExecutor(IExecutor executor) {
+		this.slaRegistry = null;
 	}
 
 }
