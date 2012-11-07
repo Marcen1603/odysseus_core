70bbbf2b719f8d14317d50b4bfaa78abd6e3fe1c
Timo Michelsen <Timo.Michelsen@gmail.com>
2012-11-07 09:37:07 +0000

    Testproducer stops generating data when it is closed
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13568 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 6dec116af836f83843aeb60131b368ccde9f78d0

--- benchmark/benchmark/src/de/uniol/inf/is/odysseus/benchmark/physical/TestproducerPO.java
+++ benchmark/benchmark/src/de/uniol/inf/is/odysseus/benchmark/physical/TestproducerPO.java
@@ -1,18 +1,18 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.benchmark.physical;
 
 import java.util.ArrayList;
@@ -25,13 +25,45 @@ import de.uniol.inf.is.odysseus.core.sdf.schema.SDFMetaAttributeList;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractSource;
 import de.uniol.inf.is.odysseus.interval_latency.IntervalLatency;
 
-public class TestproducerPO extends
-		AbstractSource<Tuple<IntervalLatency>>{
+public class TestproducerPO extends AbstractSource<Tuple<IntervalLatency>> {
 
 	private ArrayList<Integer> elementCounts = new ArrayList<Integer>();
 	private ArrayList<Long> frequencies = new ArrayList<Long>();
 	private long delay;
-	
+	private boolean isRunning;
+
+	private final Thread t = new Thread() {
+		@Override
+		public void run() {
+			delayStart(delay);
+			long lastTime = System.nanoTime();
+			for (int j = 0; j < elementCounts.size() && isRunning ; ++j) {
+				Integer count = elementCounts.get(j);
+				Long frequency = frequencies.get(j);
+				long offset = 1000000000 / frequency;
+				for (int i = 0; i < count && isRunning; ++i) {
+					Tuple<IntervalLatency> r = new Tuple<IntervalLatency>(1, false);
+					r.setAttribute(0, i);
+					IntervalLatency meta = new IntervalLatency();
+					long expectedTime = lastTime + offset;
+					meta.setLatencyStart(expectedTime);
+					r.setMetadata(meta);
+					if (offset < 1000000) {
+						while (expectedTime > System.nanoTime()) {
+						}
+					} else {
+						waitSomeTime(offset);
+					}
+					lastTime = expectedTime;
+					transfer(r);
+				}
+			}
+			if( isRunning ) {
+				propagateDone();
+			}
+		}
+	};
+
 	public TestproducerPO(long delay) {
 		this.delay = delay;
 	}
@@ -40,53 +72,30 @@ public class TestproducerPO extends
 		this.elementCounts.add(elementCount);
 		this.frequencies.add(elementsPerSecond);
 	}
-	
+
+	@Override
+	protected void process_close() {
+
+	}
+
 	@Override
 	protected void process_open() throws OpenFailedException {
-		Thread t = new Thread() {
-			@Override
-			public void run() {
-				delayStart(delay);
-				long lastTime = System.nanoTime();
-				for (int j = 0; j < elementCounts.size(); ++j) {
-					Integer count = elementCounts.get(j);
-					Long frequency = frequencies.get(j);
-					long offset = 1000000000 / frequency;
-					for (int i = 0; i < count; ++i) {
-						Tuple<IntervalLatency> r = new Tuple<IntervalLatency>(
-								1, false);
-						r.setAttribute(0, i);
-						IntervalLatency meta = new IntervalLatency();
-						long expectedTime = lastTime + offset;
-						meta.setLatencyStart(expectedTime);
-						r.setMetadata(meta);
-						if( offset < 1000000) {
-							while (expectedTime > System.nanoTime()) {}
-						} else {
-							waitSomeTime(offset);
-						}
-						lastTime = expectedTime;
-						transfer(r);
-					}
-				}
-				propagateDone();
-			}
-
-
-		};
+		isRunning = true;
+		
 		t.setPriority(7); // 7 is over normal
 		t.setDaemon(true);
 		t.start();
 	}
-	
+
 	private static void waitSomeTime(long offset) {
 		try {
 			Thread.sleep(offset / 1000000);
-		} catch (InterruptedException ex) {}
+		} catch (InterruptedException ex) {
+		}
 	}
 
 	@Override
-	public TestproducerPO clone()  {
+	public TestproducerPO clone() {
 		throw new RuntimeException("Clone Not implemented yet");
 	}
 
@@ -94,17 +103,18 @@ public class TestproducerPO extends
 	public SDFMetaAttributeList getMetaAttributeSchema() {
 		List<SDFMetaAttribute> metalist = new ArrayList<SDFMetaAttribute>(super.getMetaAttributeSchema().getAttributes());
 		SDFMetaAttribute mataAttribute = new SDFMetaAttribute(IntervalLatency.class);
-		if(!metalist.contains(mataAttribute)){
+		if (!metalist.contains(mataAttribute)) {
 			metalist.add(mataAttribute);
 		}
 		return new SDFMetaAttributeList(super.getMetaAttributeSchema().getURI(), metalist);
 	}
 
 	private static void delayStart(long delay) {
-		if( delay > 0 ) {
+		if (delay > 0) {
 			try {
 				Thread.sleep(delay);
-			} catch (InterruptedException ex) {}
+			} catch (InterruptedException ex) {
+			}
 		}
-	}	
+	}
 }
