a94d1b6b2275516411bd3aafe307c41c07511cbd
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2012-12-04 07:13:24 +0000

    Print open error message to console
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13810 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3a0e5195cf2a3e95dd7ef5dc5e96bd7d9cd63c19

--- core/executor.standardexecutor/src/de/uniol/inf/is/odysseus/planmanagement/executor/standardexecutor/StandardExecutor.java
+++ core/executor.standardexecutor/src/de/uniol/inf/is/odysseus/planmanagement/executor/standardexecutor/StandardExecutor.java
@@ -569,7 +569,7 @@ public class StandardExecutor extends AbstractExecutor implements IAdmissionList
 			firePlanModificationEvent(new QueryPlanModificationEvent(this, PlanModificationEventType.QUERY_START, queryToStart));
 		} catch (Exception e) {
 			LOG.warn("Query not started. An Error during optimizing occurd (ID: " + queryID + ").");
-			throw new RuntimeException("Query not started. An Error during optimizing occurd (ID: " + queryID + ").", e);
+			throw new RuntimeException("Query not started. An Error during optimizing occurd (ID: " + queryID + "). "+e.getMessage(), e);
 		} finally {
 			this.executionPlanLock.unlock();
 		}
