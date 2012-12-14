8f223e8ca24ee3b507fdc02d9117fcf692b47852
Christian Kuka <christian@kuka.cc>
2012-12-14 14:19:16 +0000

    Add probabilistic transform rules
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@13950 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e71dfaa494c3383d22e17fd10f19851ef9e0e14c

---
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic.transform/src/de/uniol/inf/is/odysseus/probabilistic/transform/RuleProvider.java
@@ -0,0 +1,45 @@
+/********************************************************************************** 
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.probabilistic.transform;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.ruleengine.rule.IRule;
+import de.uniol.inf.is.odysseus.transform.flow.ITransformRuleProvider;
+
+/**
+ * @author Christian Kuka <christian.kuka@offis.de>
+ */
+public class RuleProvider implements ITransformRuleProvider {
+
+    @Override
+    public List<IRule<?, ?>> getRules() {
+        final List<IRule<?, ?>> rules = new ArrayList<IRule<?, ?>>();
+        rules.add(new TProbabilisticAORule());
+        rules.add(new TAggregateProbabilisticRule());
+        rules.add(new TJoinProbabilisticRule());
+
+        // Select AO -> PO Rule
+        rules.add(new TSelectAORule());
+        // Project AO -> PO Rule
+        rules.add(new TProjectAORule());
+        // Map AO -> PO Rule
+        rules.add(new TMapAORule());
+        return rules;
+    }
+
+}
