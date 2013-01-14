1ada2ca1d956428d4477792d7b5e56bc70dd6a77
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-01-14 15:41:42 +0000

    Allow == also for equals
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14037 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b4ce6153d9f1b20157837c7095ce595f33ff7f04

---
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/mep/functions/EqualsOperator2.java
@@ -0,0 +1,100 @@
+/********************************************************************************** 
+  * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.core.server.mep.functions;
+
+import de.uniol.inf.is.odysseus.core.mep.Constant;
+import de.uniol.inf.is.odysseus.core.mep.IExpression;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.core.server.mep.AbstractBinaryOperator;
+import de.uniol.inf.is.odysseus.core.server.mep.IOperator;
+
+public class EqualsOperator2 extends AbstractBinaryOperator<Boolean> {
+
+	private static final long serialVersionUID = 6011800156304578501L;
+	public static final SDFDatatype[] accTypes = new SDFDatatype[] { SDFDatatype.INTEGER, SDFDatatype.LONG, SDFDatatype.DOUBLE, SDFDatatype.FLOAT, SDFDatatype.STRING
+	    };
+
+	private boolean isNumeric = false;
+
+	@Override
+	public int getPrecedence() {
+		return 9;
+	}
+
+	@Override
+	public String getSymbol() {
+		return "==";
+	}
+
+	@Override
+	public void setArgument(int argumentPosition, IExpression<?> argument) {
+		if (argument instanceof Constant) {
+			Constant<?> constArg = (Constant<?>) argument;
+			if (constArg.getReturnType().isNumeric()) {
+				isNumeric = true;
+			}
+		}
+
+		super.setArgument(argumentPosition, argument);
+	}
+
+	@Override
+	public Boolean getValue() {
+		return isNumeric ? getNumericalInputValue(0).equals(getNumericalInputValue(1)) : getInputValue(0).equals(getInputValue(1));
+	}
+
+	@Override
+	public SDFDatatype getReturnType() {
+		return SDFDatatype.BOOLEAN;
+	}
+
+	@Override
+	public de.uniol.inf.is.odysseus.core.server.mep.IOperator.ASSOCIATIVITY getAssociativity() {
+		return ASSOCIATIVITY.LEFT_TO_RIGHT;
+	}
+
+	@Override
+	public boolean isCommutative() {
+		return true;
+	}
+
+	@Override
+	public boolean isAssociative() {
+		return false;
+	}
+
+	@Override
+	public boolean isLeftDistributiveWith(IOperator<Boolean> operator) {
+		return false;
+	}
+
+	@Override
+	public boolean isRightDistributiveWith(IOperator<Boolean> operator) {
+		return false;
+	}
+
+	@Override
+	public SDFDatatype[] getAcceptedTypes(int argPos) {
+		if (argPos < 0) {
+			throw new IllegalArgumentException("negative argument index not allowed");
+		}
+		if (argPos > this.getArity() - 1) {
+			throw new IllegalArgumentException(this.getSymbol() + " has only " + this.getArity() + " argument(s).");
+		} 
+		
+		return accTypes;
+	}
+}
