7e51844862e02e0c5e9965479d1f946e79c68450
Christian Kuka <christian@kuka.cc>
2013-01-29 14:44:56 +0000

    Fix null pointer in Single Element Store
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14092 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ae2b8381eb62a32438faeedb152ca46679ac1e08

--- context/odysseus.context/src/de/uniol/inf/is/odysseus/context/store/types/SingleElementStore.java
+++ context/odysseus.context/src/de/uniol/inf/is/odysseus/context/store/types/SingleElementStore.java
@@ -12,61 +12,65 @@ import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
  * @author Christian Kuka <christian.kuka@offis.de>
  * @param <T>
  */
-public class SingleElementStore<T extends Tuple<? extends ITimeInterval>> extends AbstractContextStore<T> {
-    private T tuple = null;
+public class SingleElementStore<T extends Tuple<? extends ITimeInterval>>
+		extends AbstractContextStore<T> {
+	private T tuple = null;
 
-    public SingleElementStore(String name, SDFSchema schema) {
-        super(name, schema, 1);
-    }
+	public SingleElementStore(String name, SDFSchema schema) {
+		super(name, schema, 1);
+	}
 
-    @Override
-    public void insertValue(T value) {
-        if (validateSchemaSizeOfValue(value)) {
-            this.tuple = value;
-            notifyListener();
-        }
-        else {
-            logger.warn("Context store failure: size of value and schema do not match");
-        }
-    }
+	@Override
+	public void insertValue(T value) {
+		if (validateSchemaSizeOfValue(value)) {
+			if ((this.tuple == null)
+					|| (value.getMetadata().getStart().after(this.tuple
+							.getMetadata().getStart()))) {
+				this.tuple = value;
+				notifyListener();
+			}
+		} else {
+			logger.warn("Context store failure: size of value and schema do not match");
+		}
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<T> getValues(ITimeInterval timeinterval) {
-        List<T> list = new ArrayList<T>();
-        if (this.tuple != null) {
-            list.add((T)this.tuple.clone());
-        }
-        return list;
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> getValues(ITimeInterval timeinterval) {
+		List<T> list = new ArrayList<T>();
+		if (this.tuple != null) {
+			list.add((T) this.tuple.clone());
+		}
+		return list;
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<T> getLastValues() {
-        List<T> list = new ArrayList<T>();
-        if (this.tuple == null) {
-            list.add((T)this.tuple.clone());
-        }
-        return list;
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> getLastValues() {
+		List<T> list = new ArrayList<T>();
+		if (this.tuple == null) {
+			list.add((T) this.tuple.clone());
+		}
+		return list;
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<T> getAllValues() {
-        List<T> list = new ArrayList<T>();
-        if (this.tuple == null) {
-            list.add((T)this.tuple.clone());
-        }
-        return list;
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> getAllValues() {
+		List<T> list = new ArrayList<T>();
+		if (this.tuple == null) {
+			list.add((T) this.tuple.clone());
+		}
+		return list;
+	}
 
-    @Override
-    public void processTime(PointInTime time) {
-    }
+	@Override
+	public void processTime(PointInTime time) {
+	}
 
-    @Override
-    protected void internalClear() {
-        this.tuple = null;
-    }
+	@Override
+	protected void internalClear() {
+		this.tuple = null;
+	}
 
 }
