6f4e14f13acba1e48621dfec504b754b45073761
Christian Kuka <christian@kuka.cc>
2013-01-29 14:45:00 +0000

    Fix probabilistic data generator
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14095 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 431afd29a650ad337a8226b4ee17e77d7f3dcdce

--- probabilistic/de.uniol.inf.is.odysseus.generator.probabilistic/src/de/uniol/inf/is/odysseus/generator/probabilistic/ProbabilisticDataProvider.java
+++ probabilistic/de.uniol.inf.is.odysseus.generator.probabilistic/src/de/uniol/inf/is/odysseus/generator/probabilistic/ProbabilisticDataProvider.java
@@ -15,143 +15,167 @@ import de.uniol.inf.is.odysseus.generator.StreamClientHandler;
  * @author Christian Kuka <christian.kuka@offis.de>
  */
 public class ProbabilisticDataProvider extends StreamClientHandler {
-    private BufferedReader reader;
+	private BufferedReader reader;
 
-    public ProbabilisticDataProvider() {
+	public ProbabilisticDataProvider() {
 
-    }
+	}
 
-    @Override
-    public synchronized List<DataTuple> next() {
-        final List<DataTuple> tuples = new ArrayList<DataTuple>();
-        DataTuple tuple = null;
-        try {
-            tuple = this.generateDataTuple();
-        }
-        catch (final IOException e) {
-            e.printStackTrace();
-        }
-        if (tuple != null) {
-            tuples.add(tuple);
-        }
-        else {
-            this.close();
-            this.init();
-        }
-        try {
-            Thread.sleep(500);
-        }
-        catch (final InterruptedException e) {
-            e.printStackTrace();
-        }
-        return tuples;
-    }
+	@Override
+	public synchronized List<DataTuple> next() {
+		final List<DataTuple> tuples = new ArrayList<DataTuple>();
+		DataTuple tuple = null;
+		try {
+			tuple = this.generateDataTuple();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+		if (tuple != null) {
+			tuples.add(tuple);
+		} else {
+			this.close();
+			this.init();
+		}
+		try {
+			Thread.sleep(500);
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+		return tuples;
+	}
 
-    @Override
-    public void init() {
-        final URL fileURL = Activator.getContext().getBundle().getEntry("/data/data");
-        try {
-            final InputStream inputStream = fileURL.openConnection().getInputStream();
-            this.reader = new BufferedReader(new InputStreamReader(inputStream));
-        }
-        catch (final IOException e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void init() {
+		final URL fileURL = Activator.getContext().getBundle()
+				.getEntry("/data/data");
+		try {
+			final InputStream inputStream = fileURL.openConnection()
+					.getInputStream();
+			this.reader = new BufferedReader(new InputStreamReader(inputStream));
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public void close() {
-        if (this.reader != null) {
-            try {
-                this.reader.close();
-            }
-            catch (final IOException e) {
-                e.printStackTrace();
-            }
-            finally {
-                this.reader = null;
-            }
-        }
-    }
+	@Override
+	public void close() {
+		if (this.reader != null) {
+			try {
+				this.reader.close();
+			} catch (final IOException e) {
+				e.printStackTrace();
+			} finally {
+				this.reader = null;
+			}
+		}
+	}
 
-    @Override
-    public ProbabilisticDataProvider clone() {
-        return new ProbabilisticDataProvider();
-    }
+	@Override
+	public ProbabilisticDataProvider clone() {
+		return new ProbabilisticDataProvider();
+	}
 
-    private DataTuple generateDataTuple() throws IOException {
-        final DataTuple tuple = new DataTuple();
-        String line = null;
-        while (line == null) {
-            line = this.reader.readLine();
-            if (line == null) {
-                break;
-            }
-            if (line.startsWith("#")) {
-                line = null;
-            }
-        }
-        if (line != null) {
-            tuple.addLong(System.currentTimeMillis());
-            final String[] values = line.split(",");
-            for (final String value : values) {
-                if (value.startsWith("(")) {
-                    final String[] probabilisticValues = value.substring(1, value.length() - 1).split(";");
-                    if (probabilisticValues.length > 0) {
-                        if ((probabilisticValues[0].split(":").length == 2)
-                                && (probabilisticValues[0].split(":")[0].contains("."))) {
-                            // Send discrete probabilistic value
-                            tuple.addInteger(probabilisticValues.length);
-                            for (final String probabilisticValue : probabilisticValues) {
-                                final String[] probabilisticParameter = probabilisticValue.split(":");
-                                // The value
-                                tuple.addDouble(probabilisticParameter[0]);
-                                // The probability
-                                tuple.addDouble(probabilisticParameter[1]);
-                            }
-                        }
-                        else {
-                            // Send continuous probabilistic value or covariance
-                            // matrix
-                            tuple.addInteger(probabilisticValues.length);
+	private DataTuple generateDataTuple() throws IOException {
+		final DataTuple tuple = new DataTuple();
+		String line = null;
+		while (line == null) {
+			line = this.reader.readLine();
+			if (line == null) {
+				break;
+			}
+			if (line.startsWith("#")) {
+				line = null;
+			}
+		}
+		if (line != null) {
+			tuple.addLong(System.currentTimeMillis());
+			final String[] values = line.split(",");
+			for (final String value : values) {
+				if (!value.isEmpty()) {
+					if (value.contains("[")) {
+						// Send continuous distribution
+						final String[] mixtures = value.split("\\|");
 
-                            if (!probabilisticValues[0].split(":")[1].contains(".")) {
-                                // The string is a continuous probabilistic
-                                // value
-                                for (final String probabilisticValue : probabilisticValues) {
-                                    final String[] probabilisticParameter = probabilisticValue.split(":");
-                                    // The Covariance ID
-                                    tuple.addByte(probabilisticParameter[0]);
-                                    // The Covariance Index
-                                    tuple.addInteger(probabilisticParameter[1]);
-                                    // The mean
-                                    tuple.addDouble(probabilisticParameter[2]);
-                                    // The probability
-                                    tuple.addDouble(probabilisticParameter[3]);
-                                }
-                            }
-                            else {
-                                // The string is a covariance matrix
-                                for (final String probabilisticValue : probabilisticValues) {
-                                    final String[] covarianceParameter = probabilisticValue.split(":");
-                                    // The Covariance ID
-                                    tuple.addByte(covarianceParameter[0]);
-                                    tuple.addInteger(covarianceParameter.length - 1);
-                                    for (int i = 0; i < (covarianceParameter.length - 1); i++) {
-                                        // The Covariance Entry i
-                                        tuple.addDouble(covarianceParameter[1 + i]);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                else {
-                    tuple.addDouble(value);
-                }
-            }
-            return tuple;
-        }
-        return null;
-    }
+						tuple.addInteger(mixtures.length);
+						for (final String mixture : mixtures) {
+							final String[] probabilisticValues = mixture
+									.split(";");
+							// The weight
+							String weight = probabilisticValues[0];
+							final String[] meanParameter = probabilisticValues[1]
+									.substring(1,
+											probabilisticValues[1].length() - 1)
+									.split(":");
+
+							int dimension = meanParameter.length;
+							// The dimension
+							tuple.addInteger(dimension);
+							tuple.addDouble(weight);
+							for (final String element : meanParameter) {
+								// The mean in dimension i
+								tuple.addDouble(element);
+							}
+							final String[] covarianceParameter = probabilisticValues[2]
+									.substring(1,
+											probabilisticValues[2].length() - 1)
+									.split(":");
+							for (final String element : covarianceParameter) {
+								// The Covariance Entry i
+								tuple.addDouble(element);
+							}
+							if (probabilisticValues.length >= 3) {
+								// The scale factor
+								tuple.addDouble(probabilisticValues[3]);
+							} else {
+								tuple.addDouble(1.0);
+							}
+							if (probabilisticValues.length >= 4) {
+								final String[] supportParameter = probabilisticValues[4]
+										.substring(
+												1,
+												probabilisticValues[4].length() - 1)
+										.split(":");
+								for (int i = 0; i < dimension * 2; i++) {
+									// The support on each dimension
+									tuple.addDouble(supportParameter[i]);
+								}
+							} else {
+								for (int i = 0; i < dimension * 2; i++) {
+									// The support on each dimension
+									if (i % 2 == 0) {
+										tuple.addDouble(Double.NEGATIVE_INFINITY);
+									} else {
+										tuple.addDouble(Double.POSITIVE_INFINITY);
+									}
+								}
+							}
+						}
+
+					} else {
+						if (value.contains(";")) {
+							// Send discrete probabilistic value
+							final String[] probabilisticValues = value
+									.split(";");
+							tuple.addInteger(probabilisticValues.length);
+							for (final String probabilisticValue : probabilisticValues) {
+								final String[] probabilisticParameter = probabilisticValue
+										.split(":");
+								// The value
+								tuple.addDouble(probabilisticParameter[0]);
+								// The probability
+								tuple.addDouble(probabilisticParameter[1]);
+							}
+						} else {
+							// Send continuous probabilistic value (Index to
+							// distribution)
+							tuple.addInteger(value);
+						}
+					}
+				}
+			}
+			System.out.println(tuple);
+			return tuple;
+		}
+		return null;
+	}
 }
