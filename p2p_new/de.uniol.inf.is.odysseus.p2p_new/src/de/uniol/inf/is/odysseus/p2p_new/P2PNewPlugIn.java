90707da3b7592563a900cab525cd01c3a99905da
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-01-31 10:12:18 +0000

    P2P_New: Added test for starting a p2p-network
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14129 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f2369bfe41df5332d81f187ea8bc141073779d89

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/P2PNewPlugIn.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/P2PNewPlugIn.java
@@ -1,30 +1,29 @@
 package de.uniol.inf.is.odysseus.p2p_new;
 
+import net.jxta.peergroup.PeerGroup;
+import net.jxta.platform.NetworkManager;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class P2PNewPlugIn implements BundleActivator {
 
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
+	private static final Logger LOG = LoggerFactory.getLogger(P2PNewPlugIn.class);
+	
+	private NetworkManager manager;
+	
 	public void start(BundleContext bundleContext) throws Exception {
-		P2PNewPlugIn.context = bundleContext;
+		manager = new NetworkManager(NetworkManager.ConfigMode.ADHOC, "Odysseus JXTA");
+		PeerGroup peerGroup = manager.startNetwork();
+		
+		LOG.debug("JXTA-Network started. Peer is in group '{}'", peerGroup);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext bundleContext) throws Exception {
-		P2PNewPlugIn.context = null;
+		manager.stopNetwork();
+		LOG.debug("JXTA-Network stopped");
 	}
 
 }
