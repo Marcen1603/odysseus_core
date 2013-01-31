97400a64f8994cd4e3dc10a8a384ce6874cea2e7
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-01-31 06:54:24 +0000

    Added new plugin for p2p_new (core)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14124 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: fdaca2698e15e3ef7974ffff54992340d8436674

---
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/P2PNewPlugIn.java
@@ -0,0 +1,30 @@
+package de.uniol.inf.is.odysseus.p2p_new;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class P2PNewPlugIn implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		P2PNewPlugIn.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		P2PNewPlugIn.context = null;
+	}
+
+}
