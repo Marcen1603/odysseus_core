d140d7ff5ba66178adafe2bc3e5ab9b4f6866102
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-02-04 09:23:49 +0000

    P2P: Introduced own log4j.properties-file
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14145 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 492f59128e297d3e429ba903f7a114dc59f524c5

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/P2PNewPlugIn.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/P2PNewPlugIn.java
@@ -3,6 +3,8 @@ package de.uniol.inf.is.odysseus.p2p_new;
 import net.jxta.peergroup.PeerGroup;
 import net.jxta.platform.NetworkManager;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
@@ -10,11 +12,15 @@ import org.slf4j.LoggerFactory;
 
 public class P2PNewPlugIn implements BundleActivator {
 
+	private static final String LOG_PROPERTIES_FILENAME = "log4j.properties";
+	
 	private static final Logger LOG = LoggerFactory.getLogger(P2PNewPlugIn.class);
 	
 	private NetworkManager manager;
 	
 	public void start(BundleContext bundleContext) throws Exception {
+		loadOwnLogProperties(bundleContext.getBundle());
+		
 		manager = new NetworkManager(NetworkManager.ConfigMode.ADHOC, "Odysseus JXTA");
 		PeerGroup peerGroup = manager.startNetwork();
 		
@@ -24,6 +30,10 @@ public class P2PNewPlugIn implements BundleActivator {
 	public void stop(BundleContext bundleContext) throws Exception {
 		manager.stopNetwork();
 		LOG.debug("JXTA-Network stopped");
+	}
+
+	private static void loadOwnLogProperties(Bundle bundle) {
+		PropertyConfigurator.configure(bundle.getResource(LOG_PROPERTIES_FILENAME));
 	}
 
 }
