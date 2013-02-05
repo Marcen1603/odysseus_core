a0697f29950460f4e6bee9781b309a8616fd9f1e
Dennis Geesen <geesen@indivon.de>
2013-02-05 13:35:46 +0000

    added constructor for a dynamic instantiation
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14173 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3fe58d035a0c38b4b9f7559c0b2a995a2003fe0e

--- core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/builder/CreateOptionParameter.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/builder/CreateOptionParameter.java
@@ -1,43 +1,44 @@
 /********************************************************************************** 
-  * Copyright 2012 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2012 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package de.uniol.inf.is.odysseus.core.server.logicaloperator.builder;
 
 import java.util.List;
 
-
 public class CreateOptionParameter extends AbstractParameter<Option> {
 
 	private static final long serialVersionUID = 6059114351837558081L;
 
+	public CreateOptionParameter() {
+	}
+
 	public CreateOptionParameter(String name, REQUIREMENT requirement, USAGE usage) {
-		super(name, requirement,usage);
+		super(name, requirement, usage);
 	}
-	
+
 	public CreateOptionParameter(String name, REQUIREMENT requirement) {
-		super(name, requirement,USAGE.RECENT);	
+		super(name, requirement, USAGE.RECENT);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void internalAssignment() {
 		List<String> list = (List<String>) inputValue;
 		if (list.size() != 2) {
-			throw new IllegalArgumentException(
-					"Wrong number of inputs for Option. Expecting name and value.");
+			throw new IllegalArgumentException("Wrong number of inputs for Option. Expecting name and value.");
 		}
 		Option option = new Option(list.get(0), list.get(1));
 		setValue(option);
