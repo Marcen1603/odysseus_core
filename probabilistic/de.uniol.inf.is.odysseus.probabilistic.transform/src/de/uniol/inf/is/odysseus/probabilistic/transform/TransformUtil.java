046cc22d9add036823166426e10d5e214f9945c7
Christian Kuka <christian@kuka.cc>
2013-02-07 17:39:40 +0000

    Add utility functions to check for discrete and continuous probabilistic attributes
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14220 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ca2ad0ce70ea72b4917c06fa289dff29d19d7526

--- probabilistic/de.uniol.inf.is.odysseus.probabilistic.transform/src/de/uniol/inf/is/odysseus/probabilistic/transform/TransformUtil.java
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic.transform/src/de/uniol/inf/is/odysseus/probabilistic/transform/TransformUtil.java
@@ -1,5 +1,10 @@
 package de.uniol.inf.is.odysseus.probabilistic.transform;
 
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.probabilistic.sdf.schema.SDFProbabilisticDatatype;
+
 /**
  * 
  * @author Christian Kuka <christian.kuka@offis.de>
@@ -7,6 +12,80 @@ package de.uniol.inf.is.odysseus.probabilistic.transform;
  */
 public final class TransformUtil {
 	public static final String DATATYPE = "probabilistic";
+
+	/**
+	 * Returns true if this list contains a probabilistic attribute. More
+	 * formally, returns true if and only if this list contains at least one
+	 * attribute that is an {@link SDFProbabilisticDatatype probabilistic
+	 * attribute}
+	 * 
+	 * @param attributes
+	 *            The list of {@link SDFAttribute attributes}
+	 * @return <code>true</code> if this list contains a
+	 *         {@link SDFProbabilisticDatatype probabilistic attribute}
+	 */
+	public static boolean containsProbabilisticAttributes(
+			final List<SDFAttribute> attributes) {
+		boolean containsProbabilisticAttribute = false;
+		for (final SDFAttribute attribute : attributes) {
+			if (attribute.getDatatype() instanceof SDFProbabilisticDatatype) {
+				containsProbabilisticAttribute = true;
+			}
+		}
+		return containsProbabilisticAttribute;
+	}
+
+	/**
+	 * Returns true if this list contains a continuous probabilistic attribute.
+	 * More formally, returns true if and only if this list contains at least
+	 * one attribute that is an {@link SDFProbabilisticDatatype probabilistic
+	 * attribute} that is continuous
+	 * 
+	 * @param attributes
+	 *            The list of {@link SDFAttribute attributes}
+	 * @return <code>true</code> if this list contains a continuous
+	 *         {@link SDFProbabilisticDatatype probabilistic attribute}
+	 */
+	public static boolean containsContinuousProbabilisticAttributes(
+			final List<SDFAttribute> attributes) {
+		boolean containsContinuousProbabilisticAttribute = false;
+		for (final SDFAttribute attribute : attributes) {
+			if (attribute.getDatatype() instanceof SDFProbabilisticDatatype) {
+				SDFProbabilisticDatatype datatype = (SDFProbabilisticDatatype) attribute
+						.getDatatype();
+				if (datatype.isContinuous()) {
+					containsContinuousProbabilisticAttribute = true;
+				}
+			}
+		}
+		return containsContinuousProbabilisticAttribute;
+	}
+
+	/**
+	 * Returns true if this list contains a discrete probabilistic attribute.
+	 * More formally, returns true if and only if this list contains at least
+	 * one attribute that is an {@link SDFProbabilisticDatatype probabilistic
+	 * attribute} that is discrete
+	 * 
+	 * @param attributes
+	 *            The list of {@link SDFAttribute attributes}
+	 * @return <code>true</code> if this list contains a discrete
+	 *         {@link SDFProbabilisticDatatype probabilistic attribute}
+	 */
+	public static boolean containsDiscreteProbabilisticAttributes(
+			final List<SDFAttribute> attributes) {
+		boolean containsDiscreteProbabilisticAttribute = false;
+		for (final SDFAttribute attribute : attributes) {
+			if (attribute.getDatatype() instanceof SDFProbabilisticDatatype) {
+				SDFProbabilisticDatatype datatype = (SDFProbabilisticDatatype) attribute
+						.getDatatype();
+				if (datatype.isDiscrete()) {
+					containsDiscreteProbabilisticAttribute = true;
+				}
+			}
+		}
+		return containsDiscreteProbabilisticAttribute;
+	}
 
 	private TransformUtil() {
 	}
