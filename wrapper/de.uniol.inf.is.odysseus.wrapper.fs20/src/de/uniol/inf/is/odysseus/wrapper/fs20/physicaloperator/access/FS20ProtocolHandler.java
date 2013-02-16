936864d53e56d8df5bd5308f12dd271469719ff4
Christian Kuka <christian@kuka.cc>
2013-02-16 09:00:00 +0000

    Add stub for FS20 protocol handler
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14278 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4919b53a1b64ee07fbfbd19b2a7100365a748c2e

---
+++ wrapper/de.uniol.inf.is.odysseus.wrapper.fs20/src/de/uniol/inf/is/odysseus/wrapper/fs20/physicaloperator/access/FS20ProtocolHandler.java
@@ -0,0 +1,140 @@
+package de.uniol.inf.is.odysseus.wrapper.fs20.physicaloperator.access;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.core.datahandler.IDataHandler;
+import de.uniol.inf.is.odysseus.core.physicaloperator.ITransferHandler;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.AbstractProtocolHandler;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.IProtocolHandler;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.IAccessPattern;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportDirection;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportExchangePattern;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportHandler;
+
+/**
+ * Protocol handler for the FS20 and FHT comunication protocol
+ * 
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ * @param <T>
+ */
+public class FS20ProtocolHandler<T> extends AbstractProtocolHandler<T> {
+
+	protected BufferedReader reader;
+	protected BufferedWriter writer;
+
+	public FS20ProtocolHandler() {
+		super();
+	}
+
+	public FS20ProtocolHandler(ITransportDirection direction,
+			IAccessPattern access) {
+		super(direction, access);
+	}
+
+	@Override
+	public void open() throws UnknownHostException, IOException {
+		getTransportHandler().open();
+		if (getDirection().equals(ITransportDirection.IN)) {
+			if ((this.getAccess().equals(IAccessPattern.PULL))
+					|| (this.getAccess().equals(IAccessPattern.ROBUST_PULL))) {
+				reader = new BufferedReader(new InputStreamReader(
+						getTransportHandler().getInputStream()));
+			}
+		} else {
+			writer = new BufferedWriter(new OutputStreamWriter(
+					getTransportHandler().getOutputStream()));
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		if (getDirection().equals(ITransportDirection.IN)) {
+			if (reader != null) {
+				reader.close();
+			}
+		} else {
+			writer.close();
+		}
+		getTransportHandler().close();
+	}
+
+	@Override
+	public IProtocolHandler<T> createInstance(ITransportDirection direction,
+			IAccessPattern access, Map<String, String> options,
+			IDataHandler<T> dataHandler, ITransferHandler<T> transfer) {
+		FS20ProtocolHandler<T> instance = new FS20ProtocolHandler<T>(direction,
+				access);
+		instance.setDataHandler(dataHandler);
+		instance.setTransfer(transfer);
+		throw new RuntimeException("Not implemented yet");
+		// return instance;
+	}
+
+	@Override
+	public String getName() {
+		return "FS20";
+	}
+
+	@Override
+	public ITransportExchangePattern getExchangePattern() {
+		if (this.getDirection().equals(ITransportDirection.IN)) {
+			return ITransportExchangePattern.InOnly;
+		} else {
+			return ITransportExchangePattern.OutOnly;
+		}
+	}
+
+	@Override
+	public void onConnect(ITransportHandler caller) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDisonnect(ITransportHandler caller) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean hasNext() throws IOException {
+		return reader.ready();
+	}
+
+	@Override
+	public T getNext() throws IOException {
+		throw new RuntimeException("Not implemented yet");
+		// if (reader.ready()) {
+		// return getDataHandler().readData(reader.readLine());
+		// } else {
+		// return null;
+		// }
+	}
+
+	@Override
+	public void write(T object) throws IOException {
+		writer.write(object.toString());
+	}
+
+	@Override
+	public void process(ByteBuffer message) {
+		// getTransfer().transfer(getDataHandler().readData(message));
+		throw new RuntimeException("Not implemented yet");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
