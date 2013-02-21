c04858954cafbf9f0d8a8b98f942028a11174c6e
Christian Kuka <christian@kuka.cc>
2013-02-21 20:58:43 +0000

    Fix license header in text feature
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14328 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e39a569d0c6db6d8c15db1508dcf14f1558179e0

--- text/de.uniol.inf.is.odysseus.text/src/de/uniol/inf/is/odysseus/text/function/ColognePhoneticFunction.java
+++ text/de.uniol.inf.is.odysseus.text/src/de/uniol/inf/is/odysseus/text/function/ColognePhoneticFunction.java
@@ -1,8 +1,24 @@
+/********************************************************************************** 
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.text.function;
 
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 import de.uniol.inf.is.odysseus.core.server.mep.AbstractFunction;
 import org.apache.commons.codec.language.ColognePhonetic;
+
 /**
  * MEP function to compute the cologne phonetic of a string
  * 
@@ -10,7 +26,6 @@ import org.apache.commons.codec.language.ColognePhonetic;
  * 
  */
 public class ColognePhoneticFunction extends AbstractFunction<String> {
-
 
 	/**
 	 * 
@@ -44,7 +59,8 @@ public class ColognePhoneticFunction extends AbstractFunction<String> {
 	@Override
 	public String getValue() {
 		ColognePhonetic colognePhonetic = new ColognePhonetic();
-		String colognePhoneticValue = colognePhonetic.colognePhonetic(getInputValue(0).toString());
+		String colognePhoneticValue = colognePhonetic
+				.colognePhonetic(getInputValue(0).toString());
 		return colognePhoneticValue;
 	}
 
