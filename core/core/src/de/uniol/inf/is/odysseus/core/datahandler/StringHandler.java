7c7a3e849d231625183323882e212c7af6498b16
Christian Kuka <christian@kuka.cc>
2013-02-23 10:43:01 +0000

    Closes - issue ODY-531: StringHandler http://odysseus.informatik.uni-oldenburg.de:8081/browse/ODY-531
    Add charset encoder and decoder to StringHandler
    
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14348 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f4f72180afaba826b018905c17d6162b2c6175c9

--- core/core/src/de/uniol/inf/is/odysseus/core/datahandler/StringHandler.java
+++ core/core/src/de/uniol/inf/is/odysseus/core/datahandler/StringHandler.java
@@ -18,6 +18,11 @@ package de.uniol.inf.is.odysseus.core.datahandler;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.nio.ByteBuffer; 
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +35,9 @@ public class StringHandler extends AbstractDataHandler<String> {
 		types.add("String");
 		types.add("StartTimestampString");
 	}
+	private static Charset charset = Charset.forName("UTF-8");
+	private static CharsetDecoder decoder = charset.newDecoder();
+	private static CharsetEncoder encoder = charset.newEncoder();
 	
 	@Override
 	public IDataHandler<String> getInstance(SDFSchema schema) {
@@ -43,16 +51,26 @@ public class StringHandler extends AbstractDataHandler<String> {
 
 	@Override
 	public String readData(ByteBuffer b) {
-		StringBuffer buf = new StringBuffer();
+		// StringBuffer buf = new StringBuffer();
 		int size = b.getInt();
-		//System.out.println("size "+size);
-		for (int i=0;i<size;i++){
-			char c = b.getChar();
-			//System.out.print(c);
-			buf.append(c);
+		// System.out.println("size "+size);
+		int limit = b.limit();
+		b.limit(b.position() + size);
+		try {
+			CharBuffer decoded = decoder.decode(b);
+			return decoded.toString();
+		} catch (CharacterCodingException e) {
+			e.printStackTrace();
 		}
-		//System.out.println();
-		return buf.toString();
+		b.limit(limit);
+		// for (int i=0;i<size;i++){
+		// char c = b.getChar();
+		// //System.out.print(c);
+		// buf.append(c);
+		// }
+		// System.out.println();
+		// return buf.toString();
+		return "";
 	}
 	
 	@Override
@@ -64,14 +82,22 @@ public class StringHandler extends AbstractDataHandler<String> {
     public void writeData(List<String> output, Object data) {
         output.add((String) data);
     }
+    
 	@Override
 	public void writeData(ByteBuffer buffer, Object data) {
 		String s = (String) data;
-		//System.out.println("write String Data "+s+" "+s.length());		
-		buffer.putInt(s.length());
-		for (int i=0;i<s.length();i++){
-			buffer.putChar(s.charAt(i));
+		// System.out.println("write String Data "+s+" "+s.length());
+		// buffer.putInt(s.length());
+		try {
+			ByteBuffer charBuffer = encoder.encode(CharBuffer.wrap(s));
+			buffer.putInt(charBuffer.capacity());
+			buffer.put(charBuffer);
+		} catch (CharacterCodingException e) {
+			e.printStackTrace();
 		}
+		// for (int i=0;i<s.length();i++){
+		// buffer.putChar(s.charAt(i));
+		// }
 	}
 
 	@Override
