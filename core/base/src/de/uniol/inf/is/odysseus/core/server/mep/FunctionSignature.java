2f07d988c8eacff8dd805efe196631f92fe3c92e
Christian Kuka <christian@kuka.cc>
2013-02-27 12:16:31 +0000

    Add equals and hashCode methods to FunctionSignature
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14401 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5240d73b49e1a37ab542e457cdb331d181329359

--- core/base/src/de/uniol/inf/is/odysseus/core/server/mep/FunctionSignature.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/mep/FunctionSignature.java
@@ -15,6 +15,7 @@
  */
 package de.uniol.inf.is.odysseus.core.server.mep;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,33 +25,106 @@ import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
  * @author Christian Kuka <christian.kuka@offis.de>
  */
 public class FunctionSignature {
-    private String              symbol;
-    private List<SDFDatatype[]> parameter;
+	private final String symbol;
+	private final List<SDFDatatype[]> parameters;
 
-    public FunctionSignature(String symbol, List<SDFDatatype[]> parameter) {
-        this.symbol = symbol.toUpperCase();
-        this.parameter = parameter;
-    }
+	public FunctionSignature(String symbol, List<SDFDatatype[]> parameters) {
+		this.symbol = symbol.toUpperCase();
+		if (parameters == null) {
+			this.parameters = new ArrayList<SDFDatatype[]>();
+		} else {
+			this.parameters = parameters;
+		}
+	}
 
-    public String getSymbol() {
-        return symbol;
-    }
+	public FunctionSignature(FunctionSignature functionSignature) {
+		this.symbol = functionSignature.symbol;
+		this.parameters = new ArrayList<SDFDatatype[]>(
+				functionSignature.parameters);
+	}
 
-    public boolean contains(List<SDFDatatype> parameter) {
-        boolean contains = true;
-        for (int i = 0; i < parameter.size(); i++) {
-            SDFDatatype[] datatypes = this.parameter.get(i);
-            if (!Arrays.asList(datatypes).contains(parameter.get(i))) {
-                contains = false;
-                break;
-            }
-        }
-        return contains;
-    }
+	public String getSymbol() {
+		return symbol;
+	}
 
-    @Override
-    public String toString() {
-        return "FunctionSignature [symbol=" + symbol + ", parameter=" + parameter + "]";
-    }
+	public boolean contains(List<SDFDatatype> parameters) {
+		boolean contains = true;
+		for (int i = 0; i < parameters.size(); i++) {
+			SDFDatatype[] datatypes = this.parameters.get(i);
+			if (!Arrays.asList(datatypes).contains(parameters.get(i))) {
+				contains = false;
+				break;
+			}
+		}
+		return contains;
+	}
+
+	@Override
+	public FunctionSignature clone() {
+		return new FunctionSignature(this);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((parameters == null) ? 0 : parameters.hashCode());
+		result = prime * result + ((symbol == null) ? 0 : symbol.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		FunctionSignature other = (FunctionSignature) obj;
+		if (parameters == null) {
+			if (other.parameters != null) {
+				return false;
+			}
+		} else if (!parameters.equals(other.parameters)) {
+			return false;
+		}
+		if (symbol == null) {
+			if (other.symbol != null) {
+				return false;
+			}
+		} else if (!symbol.equals(other.symbol)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(symbol).append("(");
+		int i = 0;
+		for (SDFDatatype[] parameter : parameters) {
+			int j = 0;
+			if (i > 0) {
+				sb.append(",");
+			}
+			for (SDFDatatype type : parameter) {
+				if (j > 0) {
+					sb.append("|");
+				}
+				sb.append(type);
+				j++;
+			}
+			i++;
+		}
+		sb.append(")");
+
+		return sb.toString();
+	}
 
 }
