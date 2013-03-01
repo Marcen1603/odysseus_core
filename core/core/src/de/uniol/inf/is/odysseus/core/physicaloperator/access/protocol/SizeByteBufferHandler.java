5b635cbba62cb9bfbb10a8dc3e239ec7f60b3ed9
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-03-01 09:19:20 +0000

    Bugfix: SizeByteBufferHandler do not insert size of bytes into bytearray (for sending)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14467 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 243dee2e2a4a761d913cd8a911606ddfeada000b

--- core/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/protocol/SizeByteBufferHandler.java
+++ core/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/protocol/SizeByteBufferHandler.java
@@ -33,133 +33,139 @@ import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITranspor
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportHandler;
 
 public class SizeByteBufferHandler<T> extends AbstractByteBufferHandler<T> {
-	private static final Logger LOG = LoggerFactory
-			.getLogger(SizeByteBufferHandler.class);
-    private int                  size        = -1;
-    private ByteBuffer           sizeBuffer  = ByteBuffer.allocate(4);
-    private int                  currentSize = 0;
-    private ByteBufferHandler<T> objectHandler;
+	private static final Logger LOG = LoggerFactory.getLogger(SizeByteBufferHandler.class);
+	private int size = -1;
+	private ByteBuffer sizeBuffer = ByteBuffer.allocate(4);
+	private int currentSize = 0;
+	private ByteBufferHandler<T> objectHandler;
 
-    public SizeByteBufferHandler() {
-        super();
-    }
+	public SizeByteBufferHandler() {
+		super();
+	}
 
-    public SizeByteBufferHandler(ITransportDirection direction, IAccessPattern access) {
-        super(direction, access);
-    }
+	public SizeByteBufferHandler(ITransportDirection direction, IAccessPattern access) {
+		super(direction, access);
+	}
 
-    @Override
-    public void open() throws UnknownHostException, IOException {
-        sizeBuffer.clear();
-        size = -1;
-        currentSize = 0;
-        getTransportHandler().open();
-    }
+	@Override
+	public void open() throws UnknownHostException, IOException {
+		sizeBuffer.clear();
+		size = -1;
+		currentSize = 0;
+		getTransportHandler().open();
+	}
 
-    @Override
-    public void onConnect(ITransportHandler caller) {
+	@Override
+	public void onConnect(ITransportHandler caller) {
 
-    }
+	}
 
-    @Override
-    public void close() throws IOException {
-        getTransportHandler().close();
-        sizeBuffer.clear();
-        size = -1;
-        currentSize = 0;
-    }
+	@Override
+	public void close() throws IOException {
+		getTransportHandler().close();
+		sizeBuffer.clear();
+		size = -1;
+		currentSize = 0;
+	}
 
-    @Override
-    public void onDisonnect(ITransportHandler caller) {
+	@Override
+	public void onDisonnect(ITransportHandler caller) {
 
-    }
+	}
 
-    @Override
-    public void write(T object) throws IOException {
-    	ByteBuffer buffer = ByteBuffer.allocate(1024);
-    	getDataHandler().writeData(buffer, object);
-    	buffer.flip();
-    	getTransportHandler().send(buffer.array());
-    	//throw new IllegalArgumentException("Currently not implemented");
-    }
+	@Override
+	public void write(T object) throws IOException {
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		getDataHandler().writeData(buffer, object);
+		buffer.flip();
 
-    @Override
-    public void process(ByteBuffer message) {
-        try {
-            while (message.remaining() > 0) {
+		int messageSizeBytes = buffer.remaining();
+		byte[] rawBytes = new byte[messageSizeBytes + 4];
+		insertInt(rawBytes, 0, messageSizeBytes);
+		// buffer.array() returns the complete array (1024 bytes) and
+		// did not apply the "real" size of the object
+		buffer.get(rawBytes, 4, messageSizeBytes);
+		getTransportHandler().send(rawBytes);
+	}
 
-                // size ist dann ungleich -1 wenn die vollst�ndige
-                // Gr��eninformation �bertragen wird
-                // Ansonsten schon mal soweit einlesen
-                if (size == -1) {
-                    while (sizeBuffer.position() < 4 && message.remaining() > 0) {
-                        sizeBuffer.put(message.get());
-                    }
-                    // Wenn alles �bertragen
-                    if (sizeBuffer.position() == 4) {
-                        sizeBuffer.flip();
-                        size = sizeBuffer.getInt();
-                    }
-                }
-                // Es kann auch direkt nach der size noch was im Puffer
-                // sein!
-                // Und Size kann gesetzt worden sein
-                if (size != -1) {
-                    // Ist das was dazukommt kleiner als die finale Gr��e?
-                    if (currentSize + message.remaining() < size) {
-                        currentSize = currentSize + message.remaining();
-                        objectHandler.put(message);
-                    }
-                    else {
-                        // Splitten (wir sind mitten in einem Objekt
-                        // 1. alles bis zur Grenze dem Handler �bergeben
-                        // logger.debug(" "+(size-currentSize));
-                        objectHandler.put(message, size - currentSize);
-                        // 2. das fertige Objekt weiterleiten
-                        getTransfer().transfer(objectHandler.create());
-                        size = -1;
-                        sizeBuffer.clear();
-                        currentSize = 0;
-                        // Dann in der While-Schleife weiterverarbeiten
-                    }
-                }
-            }
-        }
-        catch (IOException e) {
-            LOG.error(e.getMessage(), e);
-        }
-        catch (BufferUnderflowException e) {
-            LOG.error(e.getMessage(), e);
-        }
-        catch (ClassNotFoundException e) {
-            LOG.error(e.getMessage(), e);
-        }
-    }
+	@Override
+	public void process(ByteBuffer message) {
+		try {
+			while (message.remaining() > 0) {
 
-    @Override
-    public IProtocolHandler<T> createInstance(ITransportDirection direction, IAccessPattern access,
-            Map<String, String> options, IDataHandler<T> dataHandler, ITransferHandler<T> transfer) {
-        SizeByteBufferHandler<T> instance = new SizeByteBufferHandler<T>(direction, access);
-        instance.setDataHandler(dataHandler);
-        instance.setTransfer(transfer);
-        instance.objectHandler = new ByteBufferHandler<T>(dataHandler);
-        instance.setByteOrder(options.get("byteorder"));
-        return instance;
-    }
+				// size ist dann ungleich -1 wenn die vollst�ndige
+				// Gr��eninformation �bertragen wird
+				// Ansonsten schon mal soweit einlesen
+				if (size == -1) {
+					while (sizeBuffer.position() < 4 && message.remaining() > 0) {
+						sizeBuffer.put(message.get());
+					}
+					// Wenn alles �bertragen
+					if (sizeBuffer.position() == 4) {
+						sizeBuffer.flip();
+						size = sizeBuffer.getInt();
+					}
+				}
+				// Es kann auch direkt nach der size noch was im Puffer
+				// sein!
+				// Und Size kann gesetzt worden sein
+				if (size != -1) {
+					// Ist das was dazukommt kleiner als die finale Gr��e?
+					if (currentSize + message.remaining() < size) {
+						currentSize = currentSize + message.remaining();
+						objectHandler.put(message);
+					} else {
+						// Splitten (wir sind mitten in einem Objekt
+						// 1. alles bis zur Grenze dem Handler �bergeben
+						// logger.debug(" "+(size-currentSize));
+						objectHandler.put(message, size - currentSize);
+						// 2. das fertige Objekt weiterleiten
+						getTransfer().transfer(objectHandler.create());
+						size = -1;
+						sizeBuffer.clear();
+						currentSize = 0;
+						// Dann in der While-Schleife weiterverarbeiten
+					}
+				}
+			}
+		} catch (IOException e) {
+			LOG.error(e.getMessage(), e);
+		} catch (BufferUnderflowException e) {
+			LOG.error(e.getMessage(), e);
+		} catch (ClassNotFoundException e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
 
-    @Override
-    public String getName() {
-        return "SizeByteBuffer";
-    }
+	@Override
+	public IProtocolHandler<T> createInstance(ITransportDirection direction, IAccessPattern access, Map<String, String> options, IDataHandler<T> dataHandler, ITransferHandler<T> transfer) {
+		SizeByteBufferHandler<T> instance = new SizeByteBufferHandler<T>(direction, access);
+		instance.setDataHandler(dataHandler);
+		instance.setTransfer(transfer);
+		instance.objectHandler = new ByteBufferHandler<T>(dataHandler);
+		instance.setByteOrder(options.get("byteorder"));
+		return instance;
+	}
 
-    @Override
-    public ITransportExchangePattern getExchangePattern() {
-        if (this.getDirection().equals(ITransportDirection.IN)) {
-            return ITransportExchangePattern.InOnly;
-        }
-        else {
-            return ITransportExchangePattern.OutOnly;
-        }
-    }
+	@Override
+	public String getName() {
+		return "SizeByteBuffer";
+	}
+
+	@Override
+	public ITransportExchangePattern getExchangePattern() {
+		if (this.getDirection().equals(ITransportDirection.IN)) {
+			return ITransportExchangePattern.InOnly;
+		} else {
+			return ITransportExchangePattern.OutOnly;
+		}
+	}
+
+	private static void insertInt(byte[] destArray, int offset, int value) {
+		destArray[offset] = (byte) (value >>> 24);
+		destArray[offset + 1] = (byte) (value >>> 16);
+		destArray[offset + 2] = (byte) (value >>> 18);
+		destArray[offset + 3] = (byte) (value);
+	}
 
 }
