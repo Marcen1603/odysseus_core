96986e3d641f0ff5e88949ccaf69fe25cef8c568
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-03-08 10:49:46 +0000

    P2P: added view to see available (other) peers
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14570 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d6e339ecbc899c93f6dfd16dd3f526d34600f584

--- p2p_new/de.uniol.inf.is.odysseus.rcp.p2p_new/src/de/uniol/inf/is/odysseus/rcp/p2p_new/RCPP2PNewPlugIn.java
+++ p2p_new/de.uniol.inf.is.odysseus.rcp.p2p_new/src/de/uniol/inf/is/odysseus/rcp/p2p_new/RCPP2PNewPlugIn.java
@@ -2,17 +2,38 @@ package de.uniol.inf.is.odysseus.rcp.p2p_new;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.p2p_new.IPeerManager;
 
 public class RCPP2PNewPlugIn extends AbstractUIPlugin {
 
-	public static final String PLUGIN_ID = "de.uniol.inf.is.odysseus.rcp.p2p_new"; 
-	
+	private static final Logger LOG = LoggerFactory.getLogger(RCPP2PNewPlugIn.class);
+
+	private static IPeerManager peerManager;
+
 	public void start(BundleContext context) throws Exception {
-		super.start(context);
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
 	}
 
+	public final void bindPeerManager(IPeerManager pm) {
+		peerManager = pm;
+
+		LOG.debug("Bound PeerManager {}", pm);
+	}
+
+	public final void unbindPeerManager(IPeerManager pm) {
+		if( peerManager == pm ) {
+			peerManager = null;
+			
+			LOG.debug("Unbound PeerManager {}", pm);
+		}
+	}
+	
+	public static IPeerManager getPeerManager() {
+		return peerManager;
+	}
 }
---
+++ p2p_new/de.uniol.inf.is.odysseus.rcp.p2p_new/src/de/uniol/inf/is/odysseus/rcp/p2p_new/views/PeerView.java
@@ -0,0 +1,89 @@
+package de.uniol.inf.is.odysseus.rcp.p2p_new.views;
+
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+
+import com.google.common.collect.Lists;
+
+import de.uniol.inf.is.odysseus.p2p_new.IPeerListener;
+import de.uniol.inf.is.odysseus.p2p_new.IPeerManager;
+import de.uniol.inf.is.odysseus.rcp.p2p_new.RCPP2PNewPlugIn;
+
+public class PeerView extends ViewPart implements IPeerListener {
+
+	private Text text;
+	
+	private final List<String> foundPeerIDs = Lists.newArrayList();
+
+	@Override
+	public void createPartControl(Composite parent) {
+		RCPP2PNewPlugIn.getPeerManager().addListener(this);
+		
+		text = new Text(parent, SWT.BORDER | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL );
+		text.setEditable(false);
+		text.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));		
+		
+	}
+
+	@Override
+	public void dispose() {
+		RCPP2PNewPlugIn.getPeerManager().removeListener(this);
+		text.dispose();
+		
+		super.dispose();
+	}
+
+	@Override
+	public void setFocus() {
+		text.setFocus();
+	}
+
+	@Override
+	public void onPeerFound(IPeerManager sender, String peerID) {
+		synchronized( foundPeerIDs ) {
+			foundPeerIDs.add(peerID);
+		}
+		
+		refreshText();
+	}
+
+	@Override
+	public void onPeerLost(IPeerManager sender, String peerID) {
+		synchronized( foundPeerIDs ) {
+			foundPeerIDs.remove(peerID);
+		}
+		
+		refreshText();
+	}
+
+	protected final void refreshText() {
+		Display disp = PlatformUI.getWorkbench().getDisplay();
+		if( !disp.isDisposed() ) {
+			disp.asyncExec(new Runnable() {
+
+				@Override
+				public void run() {
+					if( !text.isDisposed() ) {
+						text.setRedraw(false);
+						
+						text.setText("");
+						synchronized( foundPeerIDs ) {
+							for( String peerID : foundPeerIDs ) {
+								text.append(peerID + "\n");
+							}
+						}
+						
+						text.setRedraw(true);
+					}
+				}
+			});
+		}
+	}
+
+}
