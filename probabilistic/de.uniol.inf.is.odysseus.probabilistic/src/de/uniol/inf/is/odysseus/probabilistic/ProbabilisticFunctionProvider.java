4d30746a3f4d211fa890be877b8166712be85f97
Christian Kuka <christian@kuka.cc>
2013-03-08 12:37:01 +0000

    Add probabilistic power and sqrt function
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14576 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 89b1b0e472eab7da347311ba49610713cd477c5a

--- probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/ProbabilisticFunctionProvider.java
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/ProbabilisticFunctionProvider.java
@@ -47,6 +47,8 @@ import de.uniol.inf.is.odysseus.probabilistic.function.ProbabilisticOrOperator;
 import de.uniol.inf.is.odysseus.probabilistic.function.ProbabilisticPlusNumberLHSOperator;
 import de.uniol.inf.is.odysseus.probabilistic.function.ProbabilisticPlusNumberRHSOperator;
 import de.uniol.inf.is.odysseus.probabilistic.function.ProbabilisticPlusOperator;
+import de.uniol.inf.is.odysseus.probabilistic.function.ProbabilisticPowerOperator;
+import de.uniol.inf.is.odysseus.probabilistic.function.ProbabilisticSQRTFunction;
 import de.uniol.inf.is.odysseus.probabilistic.function.ProbabilisticSmallerEqualsOperator;
 import de.uniol.inf.is.odysseus.probabilistic.function.ProbabilisticSmallerThanOperator;
 
@@ -90,6 +92,8 @@ public class ProbabilisticFunctionProvider implements IFunctionProvider {
 		functions.add(new ProbabilisticDivisionOperator());
 		functions.add(new ProbabilisticDivisionNumberRHSOperator());
 		functions.add(new ProbabilisticDivisionNumberLHSOperator());
+		functions.add(new ProbabilisticPowerOperator());
+		functions.add(new ProbabilisticSQRTFunction());
 
 		functions.add(new ProbabilisticIntegrateMultivariateFunction());
 		functions.add(new ProbabilisticIntegrateFunction());
