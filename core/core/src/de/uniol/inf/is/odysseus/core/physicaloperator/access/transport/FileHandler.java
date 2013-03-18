61240ae02b51935d2403250dac7188c53ef988e4
Dennis Geesen <geesen@indivon.de>
2013-03-18 10:56:10 +0000

    file handler can pre-load a file directly into the memory
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14629 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: cc5fcffc01be175fa7b8a395d56352d211a4e1fb

--- core/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/transport/FileHandler.java
+++ core/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/transport/FileHandler.java
@@ -19,61 +19,131 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Map;
 
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.IProtocolHandler;
 
 public class FileHandler extends AbstractFileHandler {
 
-    public FileHandler() {
-        super();
-    }
+	private boolean preload = false;
+	private FileInputStream fis;
 
-    public FileHandler(IProtocolHandler<?> protocolHandler) {
-        super(protocolHandler);
-    }
+	public FileHandler() {
+		super();
+	}
 
-    @Override
-    public void processInOpen() throws  IOException {
-        final File file = new File(filename);
-        try {
-            in = new FileInputStream(file);
-            fireOnConnect();
-        }
-        catch (Exception e) {
-            fireOnDisconnect();
-            throw e;
-        }
-    }
+	public FileHandler(IProtocolHandler<?> protocolHandler) {
+		super(protocolHandler);
+	}
 
-    @Override
-    public void processOutOpen() throws  IOException {
-        final File file = new File(filename);
-        try {
-            out = new FileOutputStream(file, append);
-            fireOnConnect();
-        }
-        catch (Exception e) {
-            fireOnDisconnect();
-            throw e;
-        }
-    }
- 
+	@Override
+	public void processInOpen() throws IOException {
+		if (!preload) {
+			final File file = new File(filename);
+			try {
+				in = new FileInputStream(file);
+				fireOnConnect();
+			} catch (Exception e) {
+				fireOnDisconnect();
+				throw e;
+			}
+		} else {
+			fis = new FileInputStream(filename);
+			FileChannel channel = fis.getChannel();
+			long size = channel.size();
+			double x = size / (double)Integer.MAX_VALUE;
+			int n = (int) Math.ceil(x);
+			ByteBuffer buffers[] = new ByteBuffer[n];
+			for (int i = 0; i < n; i++) {			
+				buffers[i] = ByteBuffer.allocateDirect(Integer.MAX_VALUE);
+				channel.read(buffers[i]);
+				buffers[i].rewind();
+			}
+			
+			in = createInputStream(buffers);
+			fireOnConnect();
+		}
+	}
 
-    @Override
-    public ITransportHandler createInstance(IProtocolHandler<?> protocolHandler, Map<String, String> options) {
-        FileHandler fh = new FileHandler(protocolHandler);
-        fh.filename = options.get("filename");
-		fh.append = (options.containsKey("append"))?Boolean.parseBoolean(options.get("append")):false;
+	private InputStream createInputStream(final ByteBuffer buffers[]) {
+		InputStream istream = new InputStream() {
+			private int current = 0;
 
-        return fh;
-    }
+			public int read() throws IOException {
+				if (!buffers[current].hasRemaining()) {
+					current++;
+					if (current == buffers.length) {
+						return -1;
+					}
+				}
+				return buffers[current].get() & 0xFF;
+			}
 
+			@Override
+			public int read(byte[] bytes, int off, int len) throws IOException {
+				if (!buffers[current].hasRemaining()) {
+					current++;
+					if (current == buffers.length) {
+						return -1;
+					}
+				}
 
-    @Override
-    public String getName() {
-        return "File";
-    }
-    
+				len = Math.min(len, buffers[current].remaining());
+				buffers[current].get(bytes, off, len);
+				return len;
+			}
+
+			@Override
+			public int available() throws IOException {
+				return buffers[current].remaining();
+			}
+			@Override
+			public void close() throws IOException {			
+				super.close();
+				for(ByteBuffer buffer : buffers){
+					buffer.clear();
+				}
+			}
+		};
+		return istream;
+	}
+
+	@Override
+	public void processOutOpen() throws IOException {
+		final File file = new File(filename);
+		try {
+			out = new FileOutputStream(file, append);
+			fireOnConnect();
+		} catch (Exception e) {
+			fireOnDisconnect();
+			throw e;
+		}
+	}
+
+	@Override
+	public void processInClose() throws IOException {
+		super.processInClose();
+		fis.close();
+	}
+
+	@Override
+	public ITransportHandler createInstance(
+			IProtocolHandler<?> protocolHandler, Map<String, String> options) {
+		FileHandler fh = new FileHandler(protocolHandler);
+		fh.filename = options.get("filename");
+		fh.append = (options.containsKey("append")) ? Boolean
+				.parseBoolean(options.get("append")) : false;
+		fh.preload = (options.containsKey("preload")) ? Boolean
+				.parseBoolean(options.get("preload")) : false;
+		return fh;
+	}
+
+	@Override
+	public String getName() {
+		return "File";
+	}
 
 }
