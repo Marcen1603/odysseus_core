3efafd86d428c6acdbdb3bce84c9bab2e4f5ea31
Dennis Geesen <geesen@indivon.de>
2013-03-19 17:08:32 +0000

    cluster data generator working again
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14644 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ffd9034be59bfb7158a88dc8e3524a536c356026

--- application/generator/generator.syntheticclusters/src/de/uniol/inf/is/odysseus/generator/syntheticclusters/SyntheticClusterProvider.java
+++ application/generator/generator.syntheticclusters/src/de/uniol/inf/is/odysseus/generator/syntheticclusters/SyntheticClusterProvider.java
@@ -108,7 +108,7 @@ public class SyntheticClusterProvider extends StreamClientHandler {
 		time++;
 
 		try {
-			Thread.sleep(5);
+			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
