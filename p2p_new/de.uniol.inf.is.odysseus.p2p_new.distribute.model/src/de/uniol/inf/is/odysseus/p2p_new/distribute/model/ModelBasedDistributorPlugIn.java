b0d583a125dc48c5ccb3843725a86a69b17bebc9
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-03-19 11:06:03 +0000

    P2P: new plugin for model based query distribution
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14640 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8c72b9f493fd8e3245583683442692230d462eb9

---
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new.distribute.model/src/de/uniol/inf/is/odysseus/p2p_new/distribute/model/ModelBasedDistributorPlugIn.java
@@ -0,0 +1,14 @@
+package de.uniol.inf.is.odysseus.p2p_new.distribute.model;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class ModelBasedDistributorPlugIn implements BundleActivator {
+
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+
+}
