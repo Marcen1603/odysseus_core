6e04a029d0d5c4ad317c9109e130e99b8f3ce53c
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-03-19 11:09:57 +0000

    P2P: Added model based distributor class
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14641 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d1c28a388371ccaec965c50d74b8255067eba847

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new.distribute.model/src/de/uniol/inf/is/odysseus/p2p_new/distribute/model/ModelBasedDistributor.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new.distribute.model/src/de/uniol/inf/is/odysseus/p2p_new/distribute/model/ModelBasedDistributor.java
@@ -1,4 +1,4 @@
-package de.uniol.inf.is.odysseus.p2p_new.distributor.modelbased;
+package de.uniol.inf.is.odysseus.p2p_new.distribute.model;
 
 import java.util.List;
 
@@ -7,6 +7,16 @@ import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
 import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
 
 public class ModelBasedDistributor implements ILogicalQueryDistributor {
+
+	// called by OSGi-DS
+	public final void activate() {
+		
+	}
+
+	// called by OSGi-DS
+	public final void deactivate() {
+		
+	}
 
 	@Override
 	public List<ILogicalQuery> distributeLogicalQueries(IExecutor sender, List<ILogicalQuery> queriesToDistribute) {
