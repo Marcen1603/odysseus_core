85275fefd1824c748cd4e855f3e5b2fd6465ad46
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-03-22 08:08:43 +0000

    P2P: Removed unused import
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14672 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3da6af8c9ec54e39d3d178d578615a50f7b990da

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new.distribute.user/src/de/uniol/inf/is/odysseus/p2p_new/distribute/user/UserDefinedDistributor.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new.distribute.user/src/de/uniol/inf/is/odysseus/p2p_new/distribute/user/UserDefinedDistributor.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
 
 import net.jxta.discovery.DiscoveryService;
