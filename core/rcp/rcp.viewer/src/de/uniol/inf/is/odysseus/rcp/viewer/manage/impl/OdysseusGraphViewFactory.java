518e411d36a6dee3ae00b1643d4d965429881366
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-03-25 10:37:53 +0000

    Cleanup
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14705 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4ab0082e6662f6dcb45c8920d9ebe28c99b66fce

--- core/rcp/rcp.viewer/src/de/uniol/inf/is/odysseus/rcp/viewer/manage/impl/OdysseusGraphViewFactory.java
+++ core/rcp/rcp.viewer/src/de/uniol/inf/is/odysseus/rcp/viewer/manage/impl/OdysseusGraphViewFactory.java
@@ -1,18 +1,18 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.rcp.viewer.manage.impl;
 
 import java.util.ArrayList;
@@ -23,6 +23,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
@@ -47,229 +48,221 @@ import de.uniol.inf.is.odysseus.rcp.viewer.view.impl.OdysseusConnectionView;
 import de.uniol.inf.is.odysseus.rcp.viewer.view.impl.OdysseusGraphView;
 import de.uniol.inf.is.odysseus.rcp.viewer.view.impl.OdysseusNodeView;
 
-public class OdysseusGraphViewFactory implements IGraphViewFactory<IPhysicalOperator>{
+public class OdysseusGraphViewFactory implements IGraphViewFactory<IPhysicalOperator> {
 
-	private static final Logger logger = LoggerFactory.getLogger( OdysseusGraphViewFactory.class );
+	private static final Logger logger = LoggerFactory.getLogger(OdysseusGraphViewFactory.class);
 
-	
-//	public OdysseusGraphViewFactory() {
-//		((Log4JLogger)logger).getLogger().setLevel( Level.DEBUG ); 
-//	}
-	
+	// public OdysseusGraphViewFactory() {
+	// ((Log4JLogger)logger).getLogger().setLevel( Level.DEBUG );
+	// }
+
 	@Override
-	public IGraphView<IPhysicalOperator> createGraphView( IGraphModel<IPhysicalOperator> graph, ISymbolConfiguration symbolConfig, ISymbolElementFactory<IPhysicalOperator> symbolFactory ) {
-		
-		if( graph == null ) 
-			throw new IllegalArgumentException("graph is null!");
-		
-		if( symbolConfig == null ) 
-			throw new IllegalArgumentException("symbolConfig is null!");
-		
-		if( symbolFactory == null ) 
-			throw new IllegalArgumentException("symbolFactory is null!");
-		
+	public IGraphView<IPhysicalOperator> createGraphView(IGraphModel<IPhysicalOperator> graph, ISymbolConfiguration symbolConfig, ISymbolElementFactory<IPhysicalOperator> symbolFactory) {
+		Preconditions.checkNotNull(graph, "Graph must not be null!");
+		Preconditions.checkNotNull(symbolConfig, "SymbolConfig must not be null!");
+		Preconditions.checkNotNull(symbolFactory, "SymbolFactory must not be null!");
+
 		// Graph
-		IOdysseusGraphView graphView = new OdysseusGraphView((IOdysseusGraphModel)graph);
-		
+		final IOdysseusGraphView graphView = new OdysseusGraphView((IOdysseusGraphModel) graph);
+
 		// Knoten
 		final Map<String, Collection<SymbolElementInfo>> map = symbolConfig.getMap();
-		if( map == null )
+		if (map == null) {
 			throw new IllegalArgumentException("symbolConfig provides no map!");
-		
-		Collection<? extends INodeModel<IPhysicalOperator>> nodes = graph.getNodes();
-		for( INodeModel<IPhysicalOperator> node : nodes ) {
-			
+		}
+
+		final Collection<? extends INodeModel<IPhysicalOperator>> nodes = graph.getNodes();
+		for (final INodeModel<IPhysicalOperator> node : nodes) {
+
 			// Knoten zum Graphen packen
-			graphView.insertViewedNode( createNodeDisplay(node, symbolFactory, symbolConfig) );
+			graphView.insertViewedNode(createNodeDisplay(node, symbolFactory, symbolConfig));
 		}
-		
+
 		// Verbindungen
-		for( IConnectionModel<IPhysicalOperator> conn : graph.getConnections()) {
-			
-			DefaultConnectionView<IPhysicalOperator> connView = new OdysseusConnectionView(conn,
-					findFirstNodeDisplay(graphView, conn.getStartNode()), 
-					findFirstNodeDisplay(graphView, conn.getEndNode()));
-			
-			createConnectionSymbol( connView, symbolFactory );
-			graphView.insertViewedConnection( connView );
+		for (final IConnectionModel<IPhysicalOperator> conn : graph.getConnections()) {
+
+			final DefaultConnectionView<IPhysicalOperator> connView = new OdysseusConnectionView(conn, findFirstNodeDisplay(graphView, conn.getStartNode()), findFirstNodeDisplay(graphView,
+					conn.getEndNode()));
+
+			createConnectionSymbol(connView, symbolFactory);
+			graphView.insertViewedConnection(connView);
 		}
-		
+
 		// Filter
-		List<INodeView<IPhysicalOperator>> nodesToFilter = Lists.newArrayList();
-		for( INodeView<IPhysicalOperator> node : graphView.getViewedNodes()) {
-			if( PhysicalOperatorFilter.isFiltered(node.getModelNode().getContent())) {
+		final List<INodeView<IPhysicalOperator>> nodesToFilter = Lists.newArrayList();
+		for (final INodeView<IPhysicalOperator> node : graphView.getViewedNodes()) {
+			if (PhysicalOperatorFilter.isFiltered(node.getModelNode().getContent())) {
 				nodesToFilter.add(node);
 			}
 		}
-		
-		for( INodeView<IPhysicalOperator> nodeToFilter : nodesToFilter ) {
+
+		for (final INodeView<IPhysicalOperator> nodeToFilter : nodesToFilter) {
 			graphView.removeViewedNode(nodeToFilter);
 		}
-		
+
 		return graphView;
-	}
-	
-	@SuppressWarnings("unchecked")
-	private static INodeView<IPhysicalOperator> findFirstNodeDisplay( IGraphView<IPhysicalOperator> graphDisplay, INodeModel<IPhysicalOperator> node ) {
-		Collection<INodeView<IPhysicalOperator>> found = findNodeDisplay( graphDisplay, node );
-		if( found.isEmpty() )
-			return null;
-		return found.toArray(new INodeView[1])[0];
-	}
-	
-	private static Collection<INodeView<IPhysicalOperator>> findNodeDisplay( IGraphView<IPhysicalOperator> graphDisplay, INodeModel<IPhysicalOperator> node ) {
-		Collection<INodeView<IPhysicalOperator>> found = new ArrayList<INodeView<IPhysicalOperator>>();
-		for( INodeView<IPhysicalOperator> nodeDisplay : graphDisplay.getViewedNodes()) {
-			if( nodeDisplay.getModelNode() == node )
-				found.add( nodeDisplay );
-		}
-		return found;
-	}
-	
-	private static Collection<IConnectionView<IPhysicalOperator>> findConnectionDisplay( IGraphView<IPhysicalOperator> graphDisplay, IConnectionModel<IPhysicalOperator> conn ) {
-		Collection<IConnectionView<IPhysicalOperator>> found = new ArrayList<IConnectionView<IPhysicalOperator>>();
-		for( IConnectionView<IPhysicalOperator> connDisplay : graphDisplay.getViewedConnections() ) {
-			if( connDisplay.getModelConnection() == conn ) 
-				found.add( connDisplay );
-		}
-		return found;	
 	}
 
 	@Override
-	public void updateGraphDisplay( IGraphModel<IPhysicalOperator> graph, ISymbolConfiguration symbolConfig, ISymbolElementFactory<IPhysicalOperator> symbolFactory, IGraphView<IPhysicalOperator> graphDisplay ) {
-		logger.info( "Updating GraphDisplay" );
+	public void updateGraphDisplay(IGraphModel<IPhysicalOperator> graph, ISymbolConfiguration symbolConfig, ISymbolElementFactory<IPhysicalOperator> symbolFactory,
+			IGraphView<IPhysicalOperator> graphDisplay) {
+		Preconditions.checkNotNull(graph, "Graph must not be null!");
+		Preconditions.checkNotNull(symbolConfig, "symbolConfig must not be null!");
+		Preconditions.checkNotNull(symbolFactory, "symbolFactory must not be null!");
+		Preconditions.checkNotNull(graphDisplay, "graphDisplay must not be null!");
 		
-		if( graph == null ) 
-			throw new IllegalArgumentException("graph is null!");
-		
-		if( symbolConfig == null ) 
-			throw new IllegalArgumentException("symbolConfig is null!");
-		
-		if( symbolFactory == null ) 
-			throw new IllegalArgumentException("symbolFactory is null!");
-		
-		if( graphDisplay == null )
-			throw new IllegalArgumentException("graphDisplay is null!");
-		
+		logger.info("Updating GraphDisplay");
+
 		// Schauen, ob Knoten weggefallen sind
-		for( IOdysseusNodeView nodeDisplay : graphDisplay.getViewedNodes().toArray( new IOdysseusNodeView[0]) ) {
-			INodeModel<IPhysicalOperator> nodeModel = nodeDisplay.getModelNode();
-			if( !graph.getNodes().contains( nodeModel ) ) {
-				graphDisplay.removeViewedNode( nodeDisplay );
+		for (final IOdysseusNodeView nodeDisplay : graphDisplay.getViewedNodes().toArray(new IOdysseusNodeView[0])) {
+			final INodeModel<IPhysicalOperator> nodeModel = nodeDisplay.getModelNode();
+			if (!graph.getNodes().contains(nodeModel)) {
+				graphDisplay.removeViewedNode(nodeDisplay);
 			}
 		}
-		
+
 		// Schauen, ob neue Knoten hinzugefügt worden sind
 		final Map<String, Collection<SymbolElementInfo>> map = symbolConfig.getMap();
-		for( INodeModel<IPhysicalOperator> nodeModel : graph.getNodes() ) {
-			Collection<INodeView<IPhysicalOperator>> nodeDisplays = findNodeDisplay( graphDisplay, nodeModel );
-			if( nodeDisplays.isEmpty() ) {
-				graphDisplay.insertViewedNode(createNodeDisplay(nodeModel, symbolFactory, symbolConfig ));
+		for (final INodeModel<IPhysicalOperator> nodeModel : graph.getNodes()) {
+			final Collection<INodeView<IPhysicalOperator>> nodeDisplays = findNodeDisplay(graphDisplay, nodeModel);
+			if (nodeDisplays.isEmpty()) {
+				graphDisplay.insertViewedNode(createNodeDisplay(nodeModel, symbolFactory, symbolConfig));
 			}
 		}
-		
+
 		// Schauen, ob alte Verbindungen wegfallen
-		for( IConnectionView<IPhysicalOperator> conn : graphDisplay.getViewedConnections() ) {
-			if( !graph.getConnections().contains( conn.getModelConnection() )) {
-				graphDisplay.removeViewedConnection( conn );
+		for (final IConnectionView<IPhysicalOperator> conn : graphDisplay.getViewedConnections()) {
+			if (!graph.getConnections().contains(conn.getModelConnection())) {
+				graphDisplay.removeViewedConnection(conn);
 			}
 		}
-		
+
 		// Schauen, ob neue Verbindungen hinzugekommen sind
-		for( IConnectionModel<IPhysicalOperator> connModel : graph.getConnections() ) {
-			if( findConnectionDisplay( graphDisplay, connModel).isEmpty() ) {
-				
-				INodeView<IPhysicalOperator> startNode = findFirstNodeDisplay(graphDisplay, connModel.getStartNode());
-				INodeView<IPhysicalOperator> endNode = findFirstNodeDisplay(graphDisplay, connModel.getEndNode());
-				
-				if( startNode == null ) {
+		for (final IConnectionModel<IPhysicalOperator> connModel : graph.getConnections()) {
+			if (findConnectionDisplay(graphDisplay, connModel).isEmpty()) {
+
+				final INodeView<IPhysicalOperator> startNode = findFirstNodeDisplay(graphDisplay, connModel.getStartNode());
+				final INodeView<IPhysicalOperator> endNode = findFirstNodeDisplay(graphDisplay, connModel.getEndNode());
+
+				if (startNode == null) {
 					logger.error("StartNode f�r die Verbindung " + connModel.toString() + " nicht gefunden");
 					continue;
 				}
-				if( endNode == null ) {
+				if (endNode == null) {
 					logger.error("EndNode f�r die Verbindung " + connModel.toString() + " nicht gefunden");
 					continue;
 				}
-				
-				DefaultConnectionView<IPhysicalOperator> connView = new OdysseusConnectionView(connModel,
-						startNode, endNode);
-				
-				createConnectionSymbol( connView, symbolFactory );
-				graphDisplay.insertViewedConnection( connView );
+
+				final DefaultConnectionView<IPhysicalOperator> connView = new OdysseusConnectionView(connModel, startNode, endNode);
+
+				createConnectionSymbol(connView, symbolFactory);
+				graphDisplay.insertViewedConnection(connView);
 			}
 		}
-		
+
 		// Die Konfiguration kann sich geändert haben...
 		// daher alle Symbole neu erstellen
 		// sicherheitshalber
-		for( INodeView<IPhysicalOperator> nodeView : graphDisplay.getViewedNodes() ) {
-			if( nodeView.getModelNode() != null )  {
-				Collection<SymbolElementInfo> symbols = map.get( nodeView.getModelNode().getName() );
-				if( symbols != null ) 
-					createSymbols( nodeView, symbols, symbolFactory );
-				else {
-					Collection<SymbolElementInfo> defaultSymbolInfos = symbolConfig.getDefaultSymbolInfos();
-					if( defaultSymbolInfos == null || defaultSymbolInfos.size() == 0) 
-						logger.warn( "No default node for '" + nodeView.getModelNode().getName() + "' specified. Use internal standard.");
-					
-					createSymbols( nodeView, defaultSymbolInfos, symbolFactory );
+		for (final INodeView<IPhysicalOperator> nodeView : graphDisplay.getViewedNodes()) {
+			if (nodeView.getModelNode() != null) {
+				final Collection<SymbolElementInfo> symbols = map.get(nodeView.getModelNode().getName());
+				if (symbols != null) {
+					createSymbols(nodeView, symbols, symbolFactory);
+				} else {
+					final Collection<SymbolElementInfo> defaultSymbolInfos = symbolConfig.getDefaultSymbolInfos();
+					if (defaultSymbolInfos == null || defaultSymbolInfos.size() == 0) {
+						logger.warn("No default node for '" + nodeView.getModelNode().getName() + "' specified. Use internal standard.");
+					}
+
+					createSymbols(nodeView, defaultSymbolInfos, symbolFactory);
 				}
 			}
 		}
-		
-		
-		logger.info( "GraphDisplay updated" );
+
+		logger.info("GraphDisplay updated");
 	}
 
-	private static INodeView<IPhysicalOperator> createNodeDisplay( INodeModel<IPhysicalOperator> node, ISymbolElementFactory<IPhysicalOperator> symbolFactory, ISymbolConfiguration symbolConfig ) {
-		OdysseusNodeView nodeDisplay = new OdysseusNodeView((IOdysseusNodeModel)node);
-		
+	private static void createConnectionSymbol(IConnectionView<IPhysicalOperator> connView, ISymbolElementFactory<IPhysicalOperator> symbolFactory) {
+		final IConnectionSymbolElement<IPhysicalOperator> ele = symbolFactory.createForConnection("Arrow");
+		ele.setConnectionView(connView);
+		connView.getSymbolContainer().add(ele);
+	}
+
+	private static INodeView<IPhysicalOperator> createNodeDisplay(INodeModel<IPhysicalOperator> node, ISymbolElementFactory<IPhysicalOperator> symbolFactory, ISymbolConfiguration symbolConfig) {
+		final OdysseusNodeView nodeDisplay = new OdysseusNodeView((IOdysseusNodeModel) node);
+
 		// Typ ermitteln
 		final String nodeType = node.getName();
 		// Symbole erstellen
-		Map<String, Collection<SymbolElementInfo>> map = symbolConfig.getMap();
-		Collection<SymbolElementInfo> symbols = map.get( nodeType );
-		if( symbols != null ) 
-			createSymbols( nodeDisplay, symbols, symbolFactory );
-		else {
-			Collection<SymbolElementInfo> defaultSymbolInfos = symbolConfig.getDefaultSymbolInfos();
-			if( defaultSymbolInfos == null || defaultSymbolInfos.size() == 0) 
-				logger.warn( "No default node for '" + nodeType + "' specified. Use internal standard.");
-			
-			createSymbols( nodeDisplay, defaultSymbolInfos, symbolFactory );
-			
-		} 
+		final Map<String, Collection<SymbolElementInfo>> map = symbolConfig.getMap();
+		final Collection<SymbolElementInfo> symbols = map.get(nodeType);
+		if (symbols != null) {
+			createSymbols(nodeDisplay, symbols, symbolFactory);
+		} else {
+			final Collection<SymbolElementInfo> defaultSymbolInfos = symbolConfig.getDefaultSymbolInfos();
+			if (defaultSymbolInfos == null || defaultSymbolInfos.size() == 0) {
+				logger.warn("No default node for '" + nodeType + "' specified. Use internal standard.");
+			}
+
+			createSymbols(nodeDisplay, defaultSymbolInfos, symbolFactory);
+
+		}
 		return nodeDisplay;
 	}
-	
-	private static void createConnectionSymbol(IConnectionView<IPhysicalOperator> connView, ISymbolElementFactory<IPhysicalOperator> symbolFactory ) {
-		IConnectionSymbolElement<IPhysicalOperator> ele = symbolFactory.createForConnection("Arrow");
-		ele.setConnectionView( connView );
-		connView.getSymbolContainer().add( ele );
-	}
-	
-	private static void createSymbols(INodeView<IPhysicalOperator> nodeView, Collection<SymbolElementInfo> symbolInfos, ISymbolElementFactory<IPhysicalOperator> symbolFactory ) {
-		if( nodeView == null )
+
+	private static void createSymbols(INodeView<IPhysicalOperator> nodeView, Collection<SymbolElementInfo> symbolInfos, ISymbolElementFactory<IPhysicalOperator> symbolFactory) {
+		if (nodeView == null) {
 			return;
-		if( symbolFactory == null )
+		}
+		if (symbolFactory == null) {
 			return;
-		
+		}
+
 		nodeView.getSymbolContainer().clear();
-			
-		if( symbolInfos != null && symbolInfos.size() > 0) {
-			for( SymbolElementInfo info : symbolInfos) {
-				
-				ISymbolElement<IPhysicalOperator> ele = symbolFactory.createForNode( info );
-				ele.setNodeView( nodeView );
-				
-				nodeView.getSymbolContainer().add( ele );
-				nodeView.setWidth( info.getWidth() );
-				nodeView.setHeight( info.getHeight() );
+
+		if (symbolInfos != null && symbolInfos.size() > 0) {
+			for (final SymbolElementInfo info : symbolInfos) {
+
+				final ISymbolElement<IPhysicalOperator> ele = symbolFactory.createForNode(info);
+				ele.setNodeView(nodeView);
+
+				nodeView.getSymbolContainer().add(ele);
+				nodeView.setWidth(info.getWidth());
+				nodeView.setHeight(info.getHeight());
 			}
 		} else {
-			ISymbolElement<IPhysicalOperator> ele = symbolFactory.createDefaultSymbolElement();
-			ele.setNodeView( nodeView );
-			nodeView.getSymbolContainer().add( ele );
+			final ISymbolElement<IPhysicalOperator> ele = symbolFactory.createDefaultSymbolElement();
+			ele.setNodeView(nodeView);
+			nodeView.getSymbolContainer().add(ele);
 		}
+	}
+
+	private static Collection<IConnectionView<IPhysicalOperator>> findConnectionDisplay(IGraphView<IPhysicalOperator> graphDisplay, IConnectionModel<IPhysicalOperator> conn) {
+		final Collection<IConnectionView<IPhysicalOperator>> found = new ArrayList<IConnectionView<IPhysicalOperator>>();
+		for (final IConnectionView<IPhysicalOperator> connDisplay : graphDisplay.getViewedConnections()) {
+			if (connDisplay.getModelConnection() == conn) {
+				found.add(connDisplay);
+			}
+		}
+		return found;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static INodeView<IPhysicalOperator> findFirstNodeDisplay(IGraphView<IPhysicalOperator> graphDisplay, INodeModel<IPhysicalOperator> node) {
+		final Collection<INodeView<IPhysicalOperator>> found = findNodeDisplay(graphDisplay, node);
+		if (found.isEmpty()) {
+			return null;
+		}
+		return found.toArray(new INodeView[1])[0];
+	}
+
+	private static Collection<INodeView<IPhysicalOperator>> findNodeDisplay(IGraphView<IPhysicalOperator> graphDisplay, INodeModel<IPhysicalOperator> node) {
+		final Collection<INodeView<IPhysicalOperator>> found = new ArrayList<INodeView<IPhysicalOperator>>();
+		for (final INodeView<IPhysicalOperator> nodeDisplay : graphDisplay.getViewedNodes()) {
+			if (nodeDisplay.getModelNode() == node) {
+				found.add(nodeDisplay);
+			}
+		}
+		return found;
 	}
 }
