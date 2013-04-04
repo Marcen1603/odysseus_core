d943effcf3d762e66d9616c7b21072831bdf1f5b
Merlin Wasmann <merlin@uni-oldenburg.de>
2013-04-04 15:38:13 +0000

    Inserted flag so it is possible to add owners only to operators without owner.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14801 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ae7c979b14f688886912d4023a8ae6a5c09c67a9

--- core/core/src/de/uniol/inf/is/odysseus/core/util/SetOwnerGraphVisitor.java
+++ core/core/src/de/uniol/inf/is/odysseus/core/util/SetOwnerGraphVisitor.java
@@ -1,18 +1,18 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.core.util;
 
 import java.util.List;
@@ -37,10 +37,26 @@ public class SetOwnerGraphVisitor<T extends IOwnedOperator> implements
 	T root;
 
 	private IOperatorOwner owner;
+	private boolean onlyOpsWithoutOwner = false;
 
 	public SetOwnerGraphVisitor(IOperatorOwner owner) {
 		this.visited = new IdentityArrayList<T>();
 		this.owner = owner;
+	}
+
+	/**
+	 * Adds the owner to every operator in the plan.
+	 * 
+	 * @param owner
+	 *            added owner
+	 * @param onlyOpsWithoutOwner
+	 *            if true only operators without owner get a new owner.
+	 */
+	public SetOwnerGraphVisitor(IOperatorOwner owner,
+			boolean onlyOpsWithoutOwner) {
+		this.visited = new IdentityArrayList<T>();
+		this.owner = owner;
+		this.onlyOpsWithoutOwner = onlyOpsWithoutOwner;
 	}
 
 	@Override
@@ -67,9 +83,12 @@ public class SetOwnerGraphVisitor<T extends IOwnedOperator> implements
 	@Override
 	public void nodeAction(T op) {
 		if (!this.visited.contains(op)) {
-			if (owner != null){
-				op.addOwner(owner);
-			}else{
+			if (owner != null) {
+				if ((this.onlyOpsWithoutOwner && !op.hasOwner())
+						|| !this.onlyOpsWithoutOwner) {
+					op.addOwner(this.owner);
+				}
+			} else {
 				op.removeAllOwners();
 			}
 			this.visited.add(op);
