9b43dee78246072dda92fd8d1169f17f88d354db
Jan Sören Schwarz <jan.soeren.schwarz@offis.de>
2013-04-05 15:26:56 +0000

    ShotOnGoalDetection updated
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14815 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7a9e949bdc0a0da42e9171fae6bd4a44615c1318

--- application/DebsGrandChallenge2013/src/de/uniol/inf/is/odysseus/debs2013/spatial/ShotOnGoalDetection.java
+++ application/DebsGrandChallenge2013/src/de/uniol/inf/is/odysseus/debs2013/spatial/ShotOnGoalDetection.java
@@ -17,11 +17,12 @@ public abstract class ShotOnGoalDetection extends AbstractFunction<Boolean>{
 		
 	@Override
 	public int getArity() {
-		return 2;
+		return 4;
 	}
 	
 	public static final SDFDatatype[] accTypes = new SDFDatatype[] {
-    	SDFSpatialDatatype.SPATIAL_POINT
+    	SDFSpatialDatatype.SPATIAL_POINT,
+    	SDFDatatype.LONG
 	};
 
 	@Override
@@ -33,12 +34,20 @@ public abstract class ShotOnGoalDetection extends AbstractFunction<Boolean>{
 	public Boolean getValue() {
 		GeometryFactory gf = new GeometryFactory();
 		Coordinate startCoordinate = ((Point) getInputValue(0)).getCoordinate();
-		Coordinate endCoordinate = ((Point) getInputValue(1)).getCoordinate();		
-		Coordinate newEndCoordinate = new Coordinate( endCoordinate.x + (endCoordinate.x - startCoordinate.x) * 10000, 
-														endCoordinate.y + (endCoordinate.y - startCoordinate.y) * 10000);
-		LineString shot = gf.createLineString(new Coordinate[] { startCoordinate, newEndCoordinate });
-		return shot.intersects(goal);
-		
+		Coordinate endCoordinate = ((Point) getInputValue(1)).getCoordinate();	
+		Long tsShot = (Long) getInputValue(2);
+		Long ts = (Long) getInputValue(3);
+		if((ts - tsShot) != 0) {
+			Double speedX = (endCoordinate.x - startCoordinate.x) / (ts - tsShot);
+			Double speedY = (endCoordinate.y - startCoordinate.y) / (ts - tsShot);
+			// Progrnose der Flubbahn in 1,5 Sekunden = 1500000000000 Picosekunden
+			Coordinate newEndCoordinate = new Coordinate( endCoordinate.x + (speedX * 1500000000000.0 * 2), 
+															endCoordinate.y + (speedY * 1500000000000.0 * 2));
+			LineString shot = gf.createLineString(new Coordinate[] { startCoordinate, newEndCoordinate });
+			return shot.intersects(goal);
+		} else {
+			return false;
+		}
 	 }
 
 	@Override
