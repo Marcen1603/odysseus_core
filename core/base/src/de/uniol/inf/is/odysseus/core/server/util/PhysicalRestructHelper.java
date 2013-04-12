89f6f24ad309a0f24ff07773bef014693a29ff7f
Merlin Wasmann <merlin@uni-oldenburg.de>
2013-04-12 08:31:59 +0000

    deleted unused and faulty methods.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@14909 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 68bcebf908c8f29107dad4b85bafb12976d38a13

--- core/base/src/de/uniol/inf/is/odysseus/core/server/util/PhysicalRestructHelper.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/util/PhysicalRestructHelper.java
@@ -37,21 +37,36 @@ import de.uniol.inf.is.odysseus.core.server.physicaloperator.IPipe;
 @SuppressWarnings({"rawtypes","unchecked"})
 public class PhysicalRestructHelper {
 	
+	/**
+	 * Appends the binaryOp to both childs with input ports 0 -> child1, 1 -> child2.
+	 * Happens via subscribeToSource.
+	 * 
+	 * @param binaryOp
+	 * @param child1
+	 * @param child2
+	 */
 	public static void appendBinaryOperator(IPhysicalOperator binaryOp, ISource child1, ISource child2) {
 		((ISink<?>)binaryOp).subscribeToSource((ISource)child1, 0, 0, child1.getOutputSchema());
 		((ISink<?>)binaryOp).subscribeToSource((ISource)child2, 1, 0, child2.getOutputSchema());
-		
-//		child1.connectSink(binaryOp, 0, 0, child1.getOutputSchema());
-//		child2.connectSink(binaryOp, 1, 0, child2.getOutputSchema());
 	}
 	
+	/**
+	 * Appends the parent to the child via subscribeToSource.
+	 * 
+	 * @param parent
+	 * @param child
+	 */
 	public static void appendOperator(IPhysicalOperator parent, ISource child) {
-		// first unsubscribe from original source and then subscribe to new source. and new source must subscribe to original source.
-		((ISink<?>)parent).subscribeToSource((ISource)child, 0, 0, child.getOutputSchema());
-		
-//		child.connectSink(parent, 0, 0, child.getOutputSchema());
+		((ISink<?>)parent).subscribeToSource((ISource)child, 0, 0, child.getOutputSchema());		
 	}
 	
+	/**
+	 * Unsubscribes the the parent from the child and returns the subscription.
+	 * 
+	 * @param parent
+	 * @param child
+	 * @return Subscription parent -> child
+	 */
 	public static PhysicalSubscription<?> removeSubscription(IPhysicalOperator parent, IPhysicalOperator child) {
 		for (PhysicalSubscription<?> sub : ((ISink<?>)parent).getSubscribedToSource()) {
 			if (sub.getTarget().equals(child)) {
@@ -60,48 +75,6 @@ public class PhysicalRestructHelper {
 			}
 		}
 		return null;
-	}
-	
-	/**
-	 * Insert the Operator buffer between source and sinks.
-	 * @param source
-	 * @param sourceOutPort
-	 * @param sinks list of appended sinks to the source.
-	 * @param sinkInPorts list of sinkinports according to each sink.
-	 * @param buffer
-	 */
-	@Deprecated
-	public static <T extends IPipe> void insertOperator(ISource source, int sourceOutPort, List<ISink> sinks, List<Integer> sinkInPorts, T buffer) {
-		if(sinks.size() != sinkInPorts.size()) {
-			throw new IllegalArgumentException("Amount of sinks and sinkinports must be equal");
-		}
-		for(int i = 0; i < sinks.size(); i++) {
-			// remove old connections
-			source.disconnectSink(sinks.get(i), sinkInPorts.get(i), sourceOutPort, source.getOutputSchema());
-		}
-		// create new connections
-		source.connectSink(buffer, 0, sourceOutPort, source.getOutputSchema());
-		for(int i = 0; i < sinks.size(); i++) {
-			buffer.connectSink(sinks.get(i), sinkInPorts.get(i), 0, buffer.getOutputSchema());
-		}
-	}
-	
-	/**
-	 * Insert the operator buffer between source and sink.
-	 * @param source
-	 * @param sourceOutPort
-	 * @param sink
-	 * @param sinkInPort
-	 * @param buffer
-	 */
-	@Deprecated
-	public static <T extends IPipe> void insertOperator(ISource source, int sourceOutPort, ISink sink, int sinkInPort, T buffer) {
-		// Alte Verbindung entfernen
-		source.disconnectSink(sink, sinkInPort, sourceOutPort, source.getOutputSchema());
-
-		// Neue Verbindung erstellen
-		source.connectSink(buffer, 0, sourceOutPort, source.getOutputSchema());
-		buffer.connectSink(sink, sinkInPort, 0, buffer.getOutputSchema());
 	}
 
 	/**
@@ -144,23 +117,6 @@ public class PhysicalRestructHelper {
 		((AbstractSource) source).replaceActiveSubscriptions(toBuffer, toSinks);
 		
 		source.unsubscribeSink(buffer, 0, sourceOutPort, source.getOutputSchema());
-	}
-	
-	/**
-	 * Remove the operator which is between source and sink.
-	 * @param source
-	 * @param sourceOutPort
-	 * @param sink
-	 * @param sinkInPort
-	 * @param buffer
-	 */
-	public static <T extends IPipe> void removeOperator(ISource source, int sourceOutPort, ISink sink, int sinkInPort, T buffer) {
-		// Alte Verbindungen trennen
-		buffer.disconnectSink(sink, sinkInPort, 0, buffer.getOutputSchema());
-		source.disconnectSink(buffer, 0, sourceOutPort, source.getOutputSchema());
-
-		// Neue (alte) Verbindung herstellen
-		source.connectSink(sink, sinkInPort, sourceOutPort, source.getOutputSchema());
 	}
 	
 	public static void replaceChild(IPhysicalOperator parent, IPhysicalOperator child, IPhysicalOperator newChild) {
