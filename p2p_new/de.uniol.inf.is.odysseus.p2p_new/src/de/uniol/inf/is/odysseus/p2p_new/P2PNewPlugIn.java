b95ab1f23771cf9fa0edf5ce75fa4380f1d85ec3
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-05-21 17:40:06 +0000

    P2P: Changed p2p_new to Jxta 2.7
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15281 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: fc238bfb39ebfca72c6de57bcfde45e94f6b8d03

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/P2PNewPlugIn.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/P2PNewPlugIn.java
@@ -59,17 +59,14 @@ public class P2PNewPlugIn implements BundleActivator {
 
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
-//		System.setProperty("net.jxta.impl.cm.cache.impl", "net.jxta.impl.cm.sql.H2AdvertisementCache");
-//		System.setProperty("net.jxta.impl.cm.SrdiIndex.backend.impl", "net.jxta.impl.cm.InMemorySrdi");
-		
 		configureLogging(bundleContext.getBundle());
 
 		ownPeerName = determinePeerName();
 		ownPeerID = IDFactory.newPeerID(PeerGroupID.defaultNetPeerGroupID);
 		
 		final File conf = new File("." + System.getProperty("file.separator") + ownPeerName);
+		NetworkManager.RecursiveDelete(conf);
 		manager = new NetworkManager(NetworkManager.ConfigMode.ADHOC, ownPeerName, conf.toURI());
-		manager.setConfigPersistent(false);
 		
 		configureNetwork(manager.getConfigurator(), ownPeerID, ownPeerName);
 
@@ -147,6 +144,7 @@ public class P2PNewPlugIn implements BundleActivator {
 		configurator.setName(peerName);
 	}
 
+	@SuppressWarnings("deprecation")
 	private static PeerGroup createSubGroup(PeerGroup parentPeerGroup, PeerGroupID subGroupID, String subGroupName) throws PeerGroupException, IOException, Exception {
 		return parentPeerGroup.newGroup(subGroupID, parentPeerGroup.getAllPurposePeerGroupImplAdvertisement(), subGroupName, "");
 	}
