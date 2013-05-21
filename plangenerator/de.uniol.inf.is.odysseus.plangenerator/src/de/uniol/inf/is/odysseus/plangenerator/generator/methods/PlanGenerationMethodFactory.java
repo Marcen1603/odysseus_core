27814703c2ad4dfdd11d2cfc5e4f22cb0e42e615
Merlin Wasmann <merlin@uni-oldenburg.de>
2013-05-21 16:52:30 +0000

    Changed factory according to the new configuration
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15278 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 14cd73a57a704c94293a7a4a2475c2ee62a8c596

--- plangenerator/de.uniol.inf.is.odysseus.plangenerator/src/de/uniol/inf/is/odysseus/plangenerator/generator/methods/PlanGenerationMethodFactory.java
+++ plangenerator/de.uniol.inf.is.odysseus.plangenerator/src/de/uniol/inf/is/odysseus/plangenerator/generator/methods/PlanGenerationMethodFactory.java
@@ -18,6 +18,9 @@ package de.uniol.inf.is.odysseus.plangenerator.generator.methods;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
 import de.uniol.inf.is.odysseus.core.server.costmodel.ICostModel;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.optimization.configuration.PlanGenerationConfiguration;
@@ -27,17 +30,20 @@ import de.uniol.inf.is.odysseus.core.server.planmanagement.optimization.configur
  * 
  */
 public class PlanGenerationMethodFactory {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(PlanGenerationMethodFactory.class);
 
 	public static IPlanGenerationMethod createPlanGenerationMethod(
 			PlanGenerationConfiguration config, ICostModel<ILogicalOperator> costModel) {
 		Map<String, String> configs = config.getValue();
-		if (configs.get("method") == null) {
+		if (configs.get("PLANGENERATIONMETHOD") == null) {
 			// TODO: error handling
-//			configs.put("method", "copy");
-			configs.put("method", "exhaustiveSearch");
+			configs.put("PLANGENERATIONMETHOD", "copy");
+//			configs.put("method", "exhaustiveSearch");
 //			configs.put("method", "dynamicProgramming");
+			LOG.debug("Method not found or empty. Default is copy");
 		}
-		switch (configs.get("method")) {
+		switch (configs.get("PLANGENERATIONMETHOD")) {
 		case "dynamicProgramming":
 			return new DynamicProgramming(costModel);
 
