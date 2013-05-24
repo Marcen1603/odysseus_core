834d7f3639dae61f5d24d01564cd960c791f387a
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-05-24 13:50:17 +0000

    P2P: Added import/export buttons to conventional sources view
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15308 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5dd7b8ebb3d7928259fde1722ee752ec18cc8a78

---
+++ p2p_new/de.uniol.inf.is.odysseus.rcp.p2p_new/src/de/uniol/inf/is/odysseus/rcp/p2p_new/commands/ExportAllCommand.java
@@ -0,0 +1,48 @@
+package de.uniol.inf.is.odysseus.rcp.p2p_new.commands;
+
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.p2p_new.PeerException;
+import de.uniol.inf.is.odysseus.rcp.StatusBarManager;
+import de.uniol.inf.is.odysseus.rcp.p2p_new.service.DataDictionaryService;
+import de.uniol.inf.is.odysseus.rcp.p2p_new.service.P2PDictionaryService;
+import de.uniol.inf.is.odysseus.rcp.p2p_new.service.SessionManagementService;
+
+public class ExportAllCommand extends AbstractHandler implements IHandler {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ExportAllCommand.class);
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		Set<Entry<String, ILogicalOperator>> streamsAndViews = DataDictionaryService.get().getStreamsAndViews(SessionManagementService.getActiveSession());
+		int okCount = 0;
+		for( Entry<String, ILogicalOperator> streamOrView : streamsAndViews ) {
+			String sourceName = streamOrView.getKey();
+			
+			// TODO: Transcfg wählen lassen
+			try {
+				if( !P2PDictionaryService.get().isExported(sourceName) && !P2PDictionaryService.get().isImported(sourceName)) {
+					P2PDictionaryService.get().exportSource(sourceName, "Standard");
+					okCount++;
+				}
+			} catch (PeerException e) {
+				LOG.error("Could not export source {}", sourceName, e);
+			}
+		}
+		
+		if( okCount > 0 ) {
+			StatusBarManager.getInstance().setMessage("Exported " + okCount + " sources");
+		}
+		return null;
+	}
+
+}
