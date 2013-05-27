02ca51c19bd7dacf2ddaa00b3ef852e9b49c267e
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-05-27 13:05:24 +0000

    P2P: Bugfix in connecting between jxtaReceiver und jxtaSender
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15330 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 53a67940215360a8cae59f4c563968534832a070

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/util/JxtaBiDiClientConnection.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/util/JxtaBiDiClientConnection.java
@@ -18,7 +18,7 @@ public class JxtaBiDiClientConnection extends JxtaBiDiConnection {
 
 	@Override
 	public void connect() throws IOException {
-		getPipe().connect(P2PNewPlugIn.getOwnPeerGroup(), null, pipeAdvertisement, 30000, this);
+		getPipe().connect(P2PNewPlugIn.getOwnPeerGroup(), null, pipeAdvertisement, Integer.MAX_VALUE, this);
 		super.connect();
 	}
 }
