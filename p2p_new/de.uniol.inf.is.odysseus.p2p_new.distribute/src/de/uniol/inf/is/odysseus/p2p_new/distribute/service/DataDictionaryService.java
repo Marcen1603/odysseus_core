2c8f446cd0bc1584fcec47b3fbb355c9b9aab7b6
Michael Brand <michael.brand@uni-oldenburg.de>
2013-05-30 14:19:57 +0000

    Created a new class for subcluster in a network of peers. Not finished yet.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15452 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1ea5a6090d13d54de51d679b7d0718040649a54a

---
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new.distribute/src/de/uniol/inf/is/odysseus/p2p_new/distribute/service/DataDictionaryService.java
@@ -0,0 +1,77 @@
+package de.uniol.inf.is.odysseus.p2p_new.distribute.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.core.server.datadictionary.IDataDictionary;
+
+/**
+ * Singleton class for the single, static referenced {@link IDataDictionary}. <br />
+ * The referenced {@link IDataDictionary} can be accessed by calling {@link #get()}.
+ * @author Michael Brand
+ */
+public class DataDictionaryService {
+
+	/**
+	 * The {@link Logger} instance for this class.
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(DataDictionaryService.class);
+	
+	/**
+	 * The referenced {@link IDataDictionary}.
+	 * @see #bindDataDictionary(IDataDictionary)
+	 * @see #unbindDataDictionary(IDataDictionary)
+	 */
+	private static IDataDictionary dictionary;
+	
+	/**
+	 * Binds the referenced {@link IDataDictionary}. <br />
+	 * Called by OSGI-DS.
+	 * @see #unbindDataDictionary(IDataDictionary)
+	 * @param dic An instance of an {@link IDataDictionary} implementation.
+	 */
+	public void bindDataDictionary(IDataDictionary dic) {
+		
+		dictionary = dic;		
+		LOG.debug("DataDictionary bound {}", dic);
+		
+	}
+	
+	/**
+	 * Unbinds an referenced {@link IDataDictionary}, if <code>dic</code> is the binded one. <br />
+	 * Called by OSGI-DS.
+	 * @see #bindDataDictionary(IDataDictionary)
+	 * @param dic An instance of an {@link IDataDictionary} implementation.
+	 */
+	public void unbindDataDictionary(IDataDictionary dic) {
+		
+		if(dictionary == dic) {
+			
+			dictionary = null;			
+			LOG.debug("DataDictionary unbound {}", dic);
+			
+		}
+		
+	}
+	
+	/**
+	 * Returns the referenced {@link IDataDictionary}.
+	 */
+	public static IDataDictionary get() {
+		
+		return dictionary;
+		
+	}
+	
+	/**
+	 * Determines, if a referenced {@link IDataDictionary} is bound.
+	 * @see #bindDataDictionary(IDataDictionary)
+	 * @return <code>{@link #get()} != null</code>
+	 */
+	public static boolean isBound() {
+		
+		return get() != null;
+		
+	}
+	
+}
