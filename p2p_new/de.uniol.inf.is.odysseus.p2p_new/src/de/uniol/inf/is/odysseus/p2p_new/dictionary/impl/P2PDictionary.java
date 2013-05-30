8f98ee144dbd6a522459d79f9cfd91f4ec75986b
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-05-30 07:49:49 +0000

    P2P: Added autoimport-flag: set -Dpeer.autoimport=true to use it. All received sources will be imported automatically
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15429 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 721795eb95ab95adf26b4844e3ff83f490fbc107

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/dictionary/impl/P2PDictionary.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/dictionary/impl/P2PDictionary.java
@@ -50,7 +50,7 @@ import de.uniol.inf.is.odysseus.p2p_new.service.SessionManagementService;
 public class P2PDictionary implements IP2PDictionary, IDataDictionaryListener, IPlanModificationListener {
 
 	private static final Logger LOG = LoggerFactory.getLogger(P2PDictionary.class);
-	private static final String AUTO_IMPORT_SYS_PROPERTY = "peer.autoimport";
+	private static final String AUTOIMPORT_SYS_PROPERTY = "peer.autoimport";
 	private static final int EXPORT_INTERVAL_MILLIS = 15000;
 	private static final int EXPORT_LIFETIME_MILLIS = 35000;
 	
@@ -789,7 +789,7 @@ public class P2PDictionary implements IP2PDictionary, IDataDictionaryListener, I
 	}
 	
 	private static boolean isAutoImport() {
-		String property = System.getProperty(AUTO_IMPORT_SYS_PROPERTY);
+		String property = System.getProperty(AUTOIMPORT_SYS_PROPERTY);
 		if( !Strings.isNullOrEmpty(property)) {
 			return property.equalsIgnoreCase("true");
 		}
