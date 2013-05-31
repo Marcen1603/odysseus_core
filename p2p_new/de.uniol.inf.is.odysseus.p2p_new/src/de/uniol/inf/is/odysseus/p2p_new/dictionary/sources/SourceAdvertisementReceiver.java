9b6b0f741dbfbe6f4b0c5e45c11d73a978528ea9
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-05-31 10:04:37 +0000

    P2P: Reachability-test for published streams
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15472 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 01d0c60bba55a283da55e0e8241a44b74065d7d6

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/dictionary/sources/SourceAdvertisementReceiver.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/dictionary/sources/SourceAdvertisementReceiver.java
@@ -1,13 +1,28 @@
 package de.uniol.inf.is.odysseus.p2p_new.dictionary.sources;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
+
 import net.jxta.document.Advertisement;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.AccessAO;
 import de.uniol.inf.is.odysseus.p2p_new.IAdvertisementListener;
 import de.uniol.inf.is.odysseus.p2p_new.IAdvertisementManager;
 import de.uniol.inf.is.odysseus.p2p_new.dictionary.SourceAdvertisement;
 import de.uniol.inf.is.odysseus.p2p_new.dictionary.impl.P2PDictionary;
+import de.uniol.inf.is.odysseus.p2p_new.provider.JxtaServicesProvider;
 
 public class SourceAdvertisementReceiver implements IAdvertisementListener {
 
+	private static final Logger LOG = LoggerFactory.getLogger(SourceAdvertisementReceiver.class);
+	private static final int REACHABLE_TIMEOUT_MILLIS = 5000;
+	
 	@Override
 	public void advertisementAdded(IAdvertisementManager sender, Advertisement adv) {
 		if (adv instanceof SourceAdvertisement ) {
@@ -18,6 +33,16 @@ public class SourceAdvertisementReceiver implements IAdvertisementListener {
 					// überbleibsel aus alter veröffentlichung (Advertisement-Echo)
 					// --> ignorieren
 					return;
+				}
+				
+				if( srcAdvertisement.isStream() ) {
+					if( !checkStreamAdvertisement(srcAdvertisement) ) {
+						// stream ist ungültig...
+						// wird nicht eingetragen und gleich wieder aus dem
+						// cache entfernt
+						tryFlushAdvertisement(srcAdvertisement);
+						return;
+					}
 				}
 				
 				P2PDictionary.getInstance().addSource(srcAdvertisement);
@@ -31,5 +56,47 @@ public class SourceAdvertisementReceiver implements IAdvertisementListener {
 			final SourceAdvertisement srcAdvertisement = (SourceAdvertisement) adv;
 			P2PDictionary.getInstance().removeSource(srcAdvertisement);
 		}		
+	}
+
+	private static boolean checkStreamAdvertisement(SourceAdvertisement srcAdvertisement) {
+		final AccessAO accessAO = srcAdvertisement.getAccessAO();
+		final Map<String, String> optionsMap = accessAO.getOptionsMap();
+		
+		String host = optionsMap.get("host");
+		if( Strings.isNullOrEmpty(host)) {
+			LOG.error("Host not specified. Invalid sourceAdvertisement in p2p-context: {}", srcAdvertisement.getName());
+			return false;
+		}
+		
+		if( host.equalsIgnoreCase("localhost") && srcAdvertisement.getPeerID().equals(P2PDictionary.getInstance().getLocalPeerID())) {
+			LOG.error("SourceAdvertisement has localhost as host (with non local origin peer): {}", srcAdvertisement.getName());
+			return false;
+		}
+		
+		try {
+			// Nicht zuverlässig. Kann true zurückgeben, obwohl der host
+			// nicht erreichbar ist
+			InetAddress hostAddress = InetAddress.getByName(host);
+			if( !hostAddress.isReachable(REACHABLE_TIMEOUT_MILLIS) ) {
+				LOG.error("Host {} is not reachable");
+				return false;
+			}
+		} catch (UnknownHostException e) {
+			LOG.error("Unknown host {}", host, e );
+			return false;
+		} catch (IOException e) {
+			LOG.error("Could not test for reachability host {}", host, e);
+			return false;
+		} 
+		
+		return true;
+	}
+	
+	private static void tryFlushAdvertisement(final SourceAdvertisement srcAdvertisement) {
+		try {
+			JxtaServicesProvider.getInstance().getDiscoveryService().flushAdvertisement(srcAdvertisement);
+		} catch (IOException e) {
+			LOG.error("Could not flush unneeded source advertisement", e);
+		}
 	}
 }
