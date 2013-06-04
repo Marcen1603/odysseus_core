40523db83121b28d1da538bbb2892e542dc46f35
Michael Brand <michael.brand@uni-oldenburg.de>
2013-06-04 08:46:48 +0000

    IntraqueryLoadBalancer implemented
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15513 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0f3929c3c7b21d2ede0bda09717b9c41ceb29913

---
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new.loadbalancer/src/de/uniol/inf/is/odysseus/p2p_new/lb/IntraqueryLoadBalancer.java
@@ -0,0 +1,57 @@
+package de.uniol.inf.is.odysseus.p2p_new.lb;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.UnionAO;
+import de.uniol.inf.is.odysseus.p2p_new.distribute.QueryPart;
+
+/**
+ * The <code>IntraqueryLoadBalancer</code> parallelizes each {@link ILogicalQuery} on different peers. <br />
+ * The copies of an {@link ILogicalQuery} are assigned to a peer via round robin without the local peer. 
+ * But the local peer will get all results and returns them to the {@link IExecutor}. <br />
+ * To use the <code>IntraqueryLoadBalancer</code> use the following pre-parser-keywords: <br />
+ * #DODISTRIBUTE true <br />
+ * #DISTRIBUTIONTYPE intraquery
+ * @author Michael Brand
+ */
+public class IntraqueryLoadBalancer extends AbstractLoadBalancer {
+
+	@Override
+	public String getName() {
+		
+		return "intraquery";
+		
+	}
+
+	/**
+	 * Creates one {@link Querypart} for the whole {@link ILogicalQuery}.
+	 */
+	@Override
+	protected List<QueryPart> determineQueryParts(List<ILogicalOperator> operators) {
+		
+		return Lists.newArrayList(new QueryPart(operators));
+		
+	}
+
+	@Override
+	protected QueryPart createLocalPart() {
+	
+		// XXX
+		final UnionAO unionAO = new UnionAO();
+		return new QueryPart(Lists.newArrayList((ILogicalOperator) unionAO), AbstractLoadBalancer.getLocalDestinationName());
+	
+	}
+
+	@Override
+	protected int getDegreeOfParallelismn(int wantedDegree, int maxDegree) {
+		
+		// XXX
+		return 2;
+		
+	}
+
+}
