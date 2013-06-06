d44b5bd8b75e69af06c39de28f2228f38d25c738
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-06-06 07:58:19 +0000

    New data handler for AvgSumPartialAggregates
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15569 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 453eaf1969ba2477d9c20db3734c85d13c86d9d3

---
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/datahandler/AvgSumPartialAggregateDataHandler.java
@@ -0,0 +1,81 @@
+package de.uniol.inf.is.odysseus.core.server.datahandler;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.core.datahandler.AbstractDataHandler;
+import de.uniol.inf.is.odysseus.core.datahandler.IDataHandler;
+import de.uniol.inf.is.odysseus.core.datahandler.StringHandler;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.functions.AvgSumPartialAggregate;
+
+public class AvgSumPartialAggregateDataHandler extends
+		AbstractDataHandler<AvgSumPartialAggregate<?>> {
+	
+	@SuppressWarnings("unused")
+	private static final Logger LOG = LoggerFactory.getLogger(StringHandler.class);
+
+	static protected List<String> types = new ArrayList<String>();
+	static {
+		types.add(SDFDatatype.AVG_SUM_PARTIAL_AGGREGATE.getURI());
+	}
+	
+	@Override
+	public IDataHandler<AvgSumPartialAggregate<?>> getInstance(
+			List<String> schema) {
+		return new AvgSumPartialAggregateDataHandler();
+	}
+	
+	@Override
+	protected IDataHandler<AvgSumPartialAggregate<?>> getInstance(
+			SDFSchema schema) {
+		return new AvgSumPartialAggregateDataHandler();
+	}
+	
+	@Override
+	public AvgSumPartialAggregate<?> readData(ByteBuffer buffer) {
+		int count = buffer.getInt();
+		double sum = buffer.getDouble();
+		return new AvgSumPartialAggregate<>(sum, count);
+	}
+
+	@Override
+	public AvgSumPartialAggregate<?> readData(ObjectInputStream inputStream)
+			throws IOException {
+		int count = inputStream.readInt();
+		double sum = inputStream.readDouble();
+		return new AvgSumPartialAggregate<>(sum, count);
+	}
+
+	@Override
+	public AvgSumPartialAggregate<?> readData(String string) {
+		throw new IllegalArgumentException("Sorry, currently not implemented");
+	}
+
+	@Override
+	public void writeData(ByteBuffer buffer, Object data) {
+		AvgSumPartialAggregate<?> agg = (AvgSumPartialAggregate<?>) data;
+		buffer.putInt(agg.getCount());
+		buffer.putDouble(agg.getAggValue());
+	}
+
+	@Override
+	public int memSize(Object attribute) {
+		return Integer.SIZE/8+Double.SIZE/8;
+	}
+
+	@Override
+	public List<String> getSupportedDataTypes() {
+		return types;
+	}
+
+
+
+}
