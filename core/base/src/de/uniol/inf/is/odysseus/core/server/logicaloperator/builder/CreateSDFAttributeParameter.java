2283d1b86f952ad45de7cca9d79070ddaba92d57
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-06-10 12:07:31 +0000

    DataDictionary is already in AbstractParameter
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15605 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 51744acebf79704e68603ebf9e1693e799f58710

--- core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/builder/CreateSDFAttributeParameter.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/builder/CreateSDFAttributeParameter.java
@@ -24,17 +24,15 @@ import com.google.common.base.Strings;
 
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.core.server.datadictionary.DataDictionaryException;
-import de.uniol.inf.is.odysseus.core.server.datadictionary.IDataDictionary;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.QueryParseException;
 
 public class CreateSDFAttributeParameter extends AbstractParameter<SDFAttribute> {
 	
+	@SuppressWarnings("unused")
 	private static final Logger LOG = LoggerFactory.getLogger(CreateSDFAttributeParameter.class);
 	
 	private static final long serialVersionUID = -544787040358885000L;
 	
-	private static IDataDictionary dataDictionary;
-
 	public CreateSDFAttributeParameter() {
 	}
 
@@ -46,20 +44,6 @@ public class CreateSDFAttributeParameter extends AbstractParameter<SDFAttribute>
 		this(name, requirement, USAGE.RECENT);
 	}
 	
-	// called by OSGi-DS
-	public void bindDataDictionary( IDataDictionary dd ) {
-		dataDictionary = dd;
-		
-		LOG.debug("Data Dictionary bound");
-	}
-	
-	// called by OSGi-DS
-	public void unbindDataDictionary( IDataDictionary dd ) {
-		if( dataDictionary == dd ) {
-			dataDictionary = null;
-			LOG.debug("Data Dictionary bound");
-		}
-	}	
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -89,7 +73,7 @@ public class CreateSDFAttributeParameter extends AbstractParameter<SDFAttribute>
 
 	private SDFAttribute determineAttribute(String sourcename, String attributeName, String dataTypeName) {
 		try {
-			return new SDFAttribute(sourcename, attributeName, dataDictionary.getDatatype(dataTypeName));
+			return new SDFAttribute(sourcename, attributeName, getDataDictionary().getDatatype(dataTypeName));
 		} catch (DataDictionaryException e) {
 			throw new QueryParseException(e);
 		}
