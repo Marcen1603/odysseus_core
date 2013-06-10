3d74fe508aab6d4472b138ddd8bebb1b31ca5c2d
Michael Brand <michael.brand@uni-oldenburg.de>
2013-06-10 10:22:42 +0000

    The DistributionMergePO uses a PriorityQueue instead of several SweepAreas. That makes it possible to handle punctuations. javaDoc.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15599 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ae177a9f6c7871b7c8e007173cef37921744b908

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new.distribute/src/de/uniol/inf/is/odysseus/p2p_new/distribute/physicaloperator/DistributionMergePO.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new.distribute/src/de/uniol/inf/is/odysseus/p2p_new/distribute/physicaloperator/DistributionMergePO.java
@@ -1,46 +1,92 @@
 package de.uniol.inf.is.odysseus.p2p_new.distribute.physicaloperator;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 import com.google.common.collect.Maps;
 
-import de.uniol.inf.is.odysseus.core.Order;
+import de.uniol.inf.is.odysseus.core.collection.IPair;
+import de.uniol.inf.is.odysseus.core.collection.Pair;
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
+import de.uniol.inf.is.odysseus.core.metadata.IStreamable;
 import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
+import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPunctuation;
 import de.uniol.inf.is.odysseus.core.physicaloperator.OpenFailedException;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe;
-import de.uniol.inf.is.odysseus.core.server.physicaloperator.sa.ITimeIntervalSweepArea;
-import de.uniol.inf.is.odysseus.core.server.predicate.EqualsPredicate;
-import de.uniol.inf.is.odysseus.intervalapproach.DefaultTISweepArea;
 
-// TODO Check usage of PriorityQueues instead of SweepAreas with IStreamable as elements (see InputAreas) M.B.
-// TODO javaDoc M.B.
+/**
+ * A {@link DistributionMergePO} can be used to realize a {@link DistributionMergeAO}. <br />
+ * The {@link DistributionMergePO} uses a {@link PriorityQueue} and can handle {@link IPunctuations}.
+ * @author Michael Brand
+ */
 public class DistributionMergePO<T extends IStreamObject<? extends ITimeInterval>> 
 		extends AbstractPipe<T, T> {
 	
-	private Map<Integer, DefaultTISweepArea<T>> areas;
+	/**
+	 * The {@link PriorityQueue} to keep in mind which elements have been seen by this {@link DistributionMergePO}.
+	 */
+	private PriorityQueue<IPair<IStreamable, Integer>> inputQueue;
 	
-	public ITimeIntervalSweepArea<T> getSweepArea(int port) throws IndexOutOfBoundsException {
+	/**
+	 * The comparator for pairs of {@link IStreamable} objects and ports. <br />
+	 * The start timestamps of the {@link IStreamable} objects will be compaired.
+	 */
+	Comparator<IPair<IStreamable, Integer>> comp = new Comparator<IPair<IStreamable, Integer>>() {
+
+		@Override
+		public int compare(IPair<IStreamable, Integer> left,
+				IPair<IStreamable, Integer> right) {
+			
+			PointInTime ts_l = getTS(left.getE1(), true);
+			PointInTime ts_r = getTS(right.getE1(), true);
+			
+			return ts_l.compareTo(ts_r);
+			
+		}		
 		
-		return this.areas.get(port);
+	};
+	
+	/**
+	 * Delivers a timestamp of an {@link IStreamable} object.
+	 * @param object The object.
+	 * @param searchForStartTS true, if it is the start timestamp, which should be delivered; false, if it is the end timestamp.
+	 * @return {@link IPunctuation#getTime()} - if <code>object</code> is a punctuation <br />
+	 * {@link ITimeInterval#getStart()} - if <code>object</code> is no punctuation and <code>searchForStartTS == true</code> <br />
+	 * {@link ITimeInterval#getEnd()} - if <code>object</code> is no punctuation and <code>searchForStartTS == false</code> <br />
+	 */
+	@SuppressWarnings("unchecked")
+	private PointInTime getTS(IStreamable object, boolean searchForStartTS) {
 		
+		if(object.isPunctuation())
+			return ((IPunctuation) object).getTime();
+		else if(searchForStartTS)
+			return ((T) object).getMetadata().getStart();
+		else return ((T) object).getMetadata().getEnd();
+				
 	}
 
+	/**
+	 * Constructs a new {@link DistributionMergePO}.
+	 */
 	public DistributionMergePO() {
 		
 		super();
-		this.areas = Maps.newHashMap();
+		this.inputQueue = new PriorityQueue<IPair<IStreamable, Integer>>(10, comp);
 		
 	}
 
+	/**
+	 * Constructs a new {@link DistributionMergePO} as a copy of an existing one.
+	 * @param mergePO The {@link DistributionMergePO} to be copied.
+	 */
 	public DistributionMergePO(DistributionMergePO<T> mergePO) {
 		
 		super(mergePO);
-		this.areas = Maps.newHashMap();
-		for(int port : mergePO.areas.keySet())
-			this.areas.put(port, mergePO.areas.get(port).clone());
+		this.inputQueue = new PriorityQueue<IPair<IStreamable, Integer>>(10, comp);
+		this.inputQueue.addAll(mergePO.inputQueue);
 		
 	}
 	
@@ -58,51 +104,37 @@ public class DistributionMergePO<T extends IStreamObject<? extends ITimeInterval
 		
 	}
 	
-	private void initSweepAreas() {
+	@Override
+	protected synchronized void process_open() throws OpenFailedException {
 		
-		for(int port = 0; port < this.getInputPortCount(); port++) {
-			
-			DefaultTISweepArea<T> area  = new DefaultTISweepArea<T>();
-			area.setQueryPredicate(EqualsPredicate.getInstance());
-			this.areas.put(port, area);
-			
-		}
+		this.inputQueue.clear();
 		
 	}
 	
 	@Override
-	protected void process_open() throws OpenFailedException {
+	protected synchronized void process_close() {
 		
-		for(int port : this.areas.keySet())
-			this.areas.get(port).clear();
-		this.areas.clear();
-		
-		this.initSweepAreas();
+		this.inputQueue.clear();
 		
 	}
 	
 	@Override
-	protected void process_close() {
-		
-		for(int port : this.areas.keySet())
-			this.areas.get(port).clear();
-		
-	}
-	
-	@Override
-	protected void process_done() {
+	protected synchronized void process_done() {
 		
 		if(this.isOpen())
-			this.process_close();
+			this.inputQueue.clear();
 		
 	}
 	
 	@Override
-	protected boolean isDone() {
+	protected synchronized boolean isDone() {
+		
+		if(!this.inputQueue.isEmpty())
+			return false;
 		
 		for(int port = 0; port < this.getInputPortCount(); port++) {
 			
-			if(!this.getSubscribedToSource(port).isDone() || !this.areas.get(port).isEmpty())
+			if(!this.getSubscribedToSource(port).isDone())
 				return false;
 			
 		}
@@ -111,35 +143,89 @@ public class DistributionMergePO<T extends IStreamObject<? extends ITimeInterval
 		
 	}
 	
-	private void mergeElement(T object, int port, Order order) {
+	@Override
+	protected synchronized void process_next(T object, int port) {
+		
+		this.purgeElements(this.getTS(object, true));
+		this.mergeElement(object, port);
+		this.inputQueue.add(new Pair<IStreamable, Integer>(object, port));
+
+	}
+	
+	/**
+	 * Removes all elements from the {@link inputQueue}, which have an end timestamp before or equal to <code>deadline</code>.
+	 */
+	private synchronized void purgeElements(PointInTime deadline) {
+		
+		boolean continuePeeking = true;
+		
+		while(!this.inputQueue.isEmpty() && continuePeeking) {
+			
+			IStreamable elem = this.inputQueue.peek().getE1();
+			PointInTime endTS_elem = this.getTS(elem, false);
+			if(endTS_elem.beforeOrEquals(deadline))
+				this.inputQueue.poll();
+			else continuePeeking = false;
+			
+		}
+		
+	}
+	
+	/**
+	 * Transfers an object if and only if it was not transfered before (which input port doesn't matter). <br />
+	 * As objects can be equal with equal meta data this method determines, if the number of objects equal to <code>object</code> transfered 
+	 * from port <code>port</code> is the maximum count over all ports. Otherwise the object will not transfered.
+	 * @param object The object to be merged
+	 * @param port The port on which the object was arriving.
+	 */
+	@SuppressWarnings("unchecked")
+	private synchronized void mergeElement(T object, int port) {
 		
 		// The maximal count of that object over all ports and on the arriving port
 		int foundMax = 0;
 		int foundOwnPort = 0;
 		
-		for(int p : this.areas.keySet()) {
+		Map<Integer, Integer> countToPortMap = Maps.newHashMap();	// key = port, value = count
+		Iterator<IPair<IStreamable, Integer>> queueIter = this.inputQueue.iterator();
+		while(queueIter.hasNext()) {
 			
-			// Get all objects of this sweeparea which are equals to the given object
-			Iterator<T> areaIter = this.areas.get(p).query(object, order);
+			IPair<IStreamable, Integer> pair = queueIter.next();
+			IStreamable elem = pair.getE1();
+			Integer elemPort = pair.getE2();
 			
-			// Counting the elements
-			int foundCounter = 0;
-			while(areaIter.hasNext()) {
+			if((elem.isPunctuation() && !object.isPunctuation()) || (!elem.isPunctuation() && object.isPunctuation())) {
 				
-				T obj = areaIter.next();
-				if(obj.getMetadata().equals(object.getMetadata())) {
-					
-					// The same object with the same TS
-					foundCounter++;
-					
-				}
+				// only one element is a punctuation
+				continue;
+				
+			} else if(elem.isPunctuation() && object.isPunctuation() && !((IPunctuation) elem).getTime().equals(((IPunctuation) object).getTime())) {
+				
+				// not the same timestamps of the punctuations
+				continue;
+				
+			} else if(!elem.isPunctuation() && !object.isPunctuation() && 
+					!(((T) elem).equals(object) || !((T) elem).getMetadata().equals(object.getMetadata()))) {
+				
+				// either the elements or the timestamps are not equal
+				continue;
+				
+			} else if(countToPortMap.containsKey(elemPort)) {
+				
+				// port is already in the map
+				countToPortMap.put(elemPort, countToPortMap.get(elemPort) + 1);
+				
+			} else {
+				
+				// port isn't in the map yet
+				countToPortMap.put(elemPort, 1);
 				
 			}
 			
-			if(foundCounter > foundMax)
-				foundMax = foundCounter;
-			if(port == p)
-				foundOwnPort = foundCounter;
+			// Check for maximum
+			if(countToPortMap.get(elemPort) > foundMax)
+				foundMax = countToPortMap.get(elemPort);
+			if(port == elemPort)
+				foundOwnPort = countToPortMap.get(elemPort);
 			
 		}
 		
@@ -153,28 +239,6 @@ public class DistributionMergePO<T extends IStreamObject<? extends ITimeInterval
 			
 		}
 		
-	}
-
-	@Override
-	protected void process_next(T object, int port) {
-		
-		DefaultTISweepArea<T> currentArea = this.areas.get(port);
-		
-		// The order doesn't matter for equals predicate.
-		Order order = Order.LeftRight;
-		
-		this.mergeElement(object, port, order);
-		
-		currentArea.insert(object);
-		currentArea.purgeElements(object, order);
-
-	}
-	
-	@Override
-	public void processPunctuation(IPunctuation punctuation, int port) {
-		
-		// TODO Can't put an IPunctuation object into a DefaultTISweepArea<T>. M.B.
-	    
 	}
 
 }
