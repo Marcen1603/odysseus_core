63ba07f767ce18f2e30863f090a072e37f39b409
Mark Preuschaft <mpreuschaft@uni-oldenburg.de>
2013-06-17 18:08:04 +0000

    Changed OutputMode to NEW_ELEMENT
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15682 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8dd01904298899d41a9642ab80f443a0a789c410

--- sentimentdetection/sentimentdetection/src/de/uniol/inf/is/odysseus/sentimentdetection/logicaloperator/SentimentDetectionAO.java
+++ sentimentdetection/sentimentdetection/src/de/uniol/inf/is/odysseus/sentimentdetection/logicaloperator/SentimentDetectionAO.java
@@ -1,6 +1,12 @@
 package de.uniol.inf.is.odysseus.sentimentdetection.logicaloperator;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.AbstractLogicalOperator;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.UnaryLogicalOp;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.LogicalOperator;
@@ -23,6 +29,23 @@ public class SentimentDetectionAO extends UnaryLogicalOp{
 	@Override
 	public AbstractLogicalOperator clone() {
 	     return new SentimentDetectionAO(this);
+	}
+	
+	@Override 
+	public SDFSchema getOutputSchemaIntern(int pos){
+		
+		SDFAttribute sentDetection = new SDFAttribute(null,"decision",SDFDatatype.STRING);
+		
+		List<SDFAttribute> outputAttributes = new ArrayList<SDFAttribute>();
+		
+		outputAttributes.addAll(getInputSchema(0).getAttributes());
+		String name = getInputSchema(0).getURI();
+		
+		outputAttributes.add(sentDetection);
+		
+		setOutputSchema(new SDFSchema(name, outputAttributes));
+		
+		return getOutputSchema();
 	}
 
 }
--- sentimentdetection/sentimentdetection/src/de/uniol/inf/is/odysseus/sentimentdetection/physicaloperator/SentimentDetectionPO.java
+++ sentimentdetection/sentimentdetection/src/de/uniol/inf/is/odysseus/sentimentdetection/physicaloperator/SentimentDetectionPO.java
@@ -1,14 +1,14 @@
 package de.uniol.inf.is.odysseus.sentimentdetection.physicaloperator;
 
 
-import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe;
 
 @SuppressWarnings({ "rawtypes" })
-public class SentimentDetectionPO<T extends IStreamObject<?>> extends
-		AbstractPipe<T, T> {
+public class SentimentDetectionPO<T extends IMetaAttribute> extends AbstractPipe<Tuple <T>,Tuple <T>> {
 	
-	int current = 0 ; 
+	private static int ctr = 0;
 
 	public SentimentDetectionPO() {
 	}
@@ -19,28 +19,62 @@ public class SentimentDetectionPO<T extends IStreamObject<?>> extends
 
 	@Override
 	public OutputMode getOutputMode() {
-		return OutputMode.INPUT;
+		return OutputMode.NEW_ELEMENT;
 	}
 
 	@Override
-	protected void process_next(T object, int port) {
-			
-		if(current %2 == 0 ){
-			System.out.println("Ausgabe an Port 0:");
-			transfer(object,0);
-		}else{
-			System.out.println("Ausgabe an Port 1:");
-			transfer(object,1);
-		}
-		current++;
+	@SuppressWarnings("unchecked")
+	protected void process_next(Tuple object, int port) {
+		//get inputSize of the object	
+		int inputSize = object.size();
 		
+		//create new output Tuple with size + 1 
+		Tuple outputTuple = new Tuple( object.size()+1, false);
+		
+		//Copy object Attributes to the new outputTuple 
+		System.arraycopy(object.getAttributes(), 0, outputTuple.getAttributes(), 0, inputSize);
 	
+		//text positive or negative
+		String erg = detect(object.getAttribute(0).toString());
+		
+		//set the decision Attribute 
+		outputTuple.setAttribute(object.size(), erg);
+			
+		outputTuple.setMetadata(object.getMetadata());
+		outputTuple.setRequiresDeepClone(object.requiresDeepClone());
 	
+		//Test Start
+		System.out.println("Alte Objekt ist: "+ object.toString());
+		System.out.println("Neues Objekt ist: "+ outputTuple.toString());
+		ctr++;
+		//Test End
+		
+		//Transfer to the port 0
+		transfer(outputTuple,0);
 	}
 
 	@Override
 	public SentimentDetectionPO<T> clone() {
 		return new SentimentDetectionPO<T>(this);
+	}
+	
+	/*
+	 * SentimentDetection
+	 * 
+	 */
+	private String detect(String inputText){
+		
+		System.out.println("Der Satz wird analysiert: " + inputText);
+		
+		String erg = "";
+		
+		if(ctr % 2 == 0 ){
+			 erg = "positive";
+		}else{
+			 erg = "negative";
+		}
+	
+		return erg;
 	}
 
 }
