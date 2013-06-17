38dfd26978807836d285d3a86a4568f70cc4dc8b
Dennis Geesen <geesen@indivon.de>
2013-06-17 07:56:25 +0000

    Stream version for http sources
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15670 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0084b4284c6b096bfb0f65b91af279f81abde609

---
+++ wrapper/de.uniol.inf.is.odysseus.wrapper.web/src/de/uniol/inf/is/odysseus/wrapper/web/physicaloperator/access/HTTPStreamTransportHandler.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright 2012 The Odysseus Team
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.uniol.inf.is.odysseus.wrapper.web.physicaloperator.access;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.IProtocolHandler;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.AbstractPullTransportHandler;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportHandler;
+
+/**
+ * @author DGe
+ */
+public class HTTPStreamTransportHandler extends AbstractPullTransportHandler {
+    
+    private InputStream input;
+    private OutputStream output;
+    private String uri;        
+    /**
+ * 
+ */
+    public HTTPStreamTransportHandler() {
+        super();
+    }
+
+    /**
+     * @param protocolHandler
+     */
+    public HTTPStreamTransportHandler(final IProtocolHandler<?> protocolHandler) {
+        super(protocolHandler);
+    }
+
+    @Override
+    public void send(final byte[] message) throws IOException {
+       
+    }
+
+    @Override
+    public ITransportHandler createInstance(final IProtocolHandler<?> protocolHandler, final Map<String, String> options) {
+        final HTTPStreamTransportHandler handler = new HTTPStreamTransportHandler(protocolHandler);
+        handler.init(options);
+        return handler;
+    }
+
+    protected void init(Map<String, String> options) {
+        if (options.get("uri") != null) {
+            setURI(options.get("uri"));
+        }       
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return this.input;
+    }
+
+    @Override
+    public String getName() {
+        return "HTTPStream";
+    }
+
+    @Override
+    public OutputStream getOutputStream() {
+        return this.output;
+    }
+
+    @Override
+    public void processInOpen() throws UnknownHostException, IOException {        
+        URL u = new URL(this.uri);
+        URLConnection uc = u.openConnection();
+        uc.connect();
+        this.input = uc.getInputStream();
+    }
+
+    @Override
+    public void processOutOpen() throws UnknownHostException, IOException {
+    	 URL u = new URL(this.uri);
+         URLConnection uc = u.openConnection();
+         uc.setDoOutput(true);
+         uc.connect();
+         this.output = uc.getOutputStream();
+    }
+
+    @Override
+    public void processInClose() throws IOException {
+        this.input = null;
+        this.fireOnDisconnect();
+    }
+
+    @Override
+    public void processOutClose() throws IOException {
+        this.output = null;
+        this.fireOnDisconnect();
+    }
+    
+    public void setURI(String uri) {
+        this.uri = uri;
+    }
+
+    public String getURI() {
+        return this.uri;
+    }
+  
+}
