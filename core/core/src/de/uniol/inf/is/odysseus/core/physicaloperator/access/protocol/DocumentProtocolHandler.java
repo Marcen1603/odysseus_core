083d32a5dbffb4be38d81e62cc5e7090647587dd
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-06-18 07:58:40 +0000

    Allow multiple calls of document handlers (default is true)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15690 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: bf9914f89c0da8598d01e2c097c103ed432a7058

--- core/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/protocol/DocumentProtocolHandler.java
+++ core/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/protocol/DocumentProtocolHandler.java
@@ -27,252 +27,290 @@ import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITranspor
  * @param <T>
  */
 public class DocumentProtocolHandler<T> extends AbstractProtocolHandler<T> {
-    Logger LOG = LoggerFactory.getLogger(DocumentProtocolHandler.class);
-    protected BufferedReader reader;
-    protected BufferedWriter writer;
-    private long delay;
-    private int nanodelay;
-    private int delayeach = 0;
-    private long delayCounter = 0L;
+	Logger LOG = LoggerFactory.getLogger(DocumentProtocolHandler.class);
+	protected BufferedReader reader;
+	protected BufferedWriter writer;
+	private long delay;
+	private int nanodelay;
+	private int delayeach = 0;
+	private long delayCounter = 0L;
 
-    private long dumpEachDocument = -1;
-    private long lastDocument = -1;
-    protected long documentCounter = 0L;
-    private boolean debug = false;
-    private boolean isDone = false;
-    private StringBuffer measurements = new StringBuffer("");
-    private long measureEachDocument = -1;
-    private long lastDumpTime = 0;
-    private long basetime;
+	private long dumpEachDocument = -1;
+	private long lastDocument = -1;
+	protected long documentCounter = 0L;
+	private boolean debug = false;
+	private boolean isDone = false;
+	private StringBuffer measurements = new StringBuffer("");
+	private long measureEachDocument = -1;
+	private long lastDumpTime = 0;
+	private long basetime;
 
-    public DocumentProtocolHandler() {
-        super();
-    }
+	private boolean oneDocPerCall = true;
 
-    public DocumentProtocolHandler(ITransportDirection direction, IAccessPattern access) {
-        super(direction, access);
-    }
+	public DocumentProtocolHandler() {
+		super();
+	}
 
-    @Override
-    public void open() throws UnknownHostException, IOException {
-        getTransportHandler().open();
-        if (getDirection().equals(ITransportDirection.IN)) {
-            if ((this.getAccess().equals(IAccessPattern.PULL)) || (this.getAccess().equals(IAccessPattern.ROBUST_PULL))) {
-                reader = new BufferedReader(new InputStreamReader(getTransportHandler().getInputStream()));
-            }
-        } else {
-            writer = new BufferedWriter(new OutputStreamWriter(getTransportHandler().getOutputStream()));
-        }
-        delayCounter = 0;
-        documentCounter = 0;
-        isDone = false;
-    }
+	public DocumentProtocolHandler(ITransportDirection direction,
+			IAccessPattern access) {
+		super(direction, access);
+	}
 
-    @Override
-    public void close() throws IOException {
-        try {
-            if (getDirection().equals(ITransportDirection.IN)) {
-                if (reader != null) {
-                    reader.close();
-                }
-            } else {
-                if (writer != null) {
-                    writer.close();
-                }
-            }
-        } catch (Exception e) {
-            LOG.error(e.getMessage(), e);
-            throw new IOException(e);
-        } finally {
-            getTransportHandler().close();
-        }
-    }
+	@Override
+	public void open() throws UnknownHostException, IOException {
+		if (!oneDocPerCall) {
+			intern_open();
+		}
+	}
 
-    @Override
-    public boolean hasNext() throws IOException {
-//        if (reader.ready() == false) {
-//            isDone = true;
-//            return false;
-//        }
-        return true;
-    }
+	private void intern_open() throws UnknownHostException, IOException {
+		getTransportHandler().open();
+		if (getDirection().equals(ITransportDirection.IN)) {
+			if ((this.getAccess().equals(IAccessPattern.PULL))
+					|| (this.getAccess().equals(IAccessPattern.ROBUST_PULL))) {
+				reader = new BufferedReader(new InputStreamReader(
+						getTransportHandler().getInputStream()));
+			}
+		} else {
+			writer = new BufferedWriter(new OutputStreamWriter(
+					getTransportHandler().getOutputStream()));
+		}
+		delayCounter = 0;
+		documentCounter = 0;
+		isDone = false;
+	}
 
-    @Override
-    public T getNext() throws IOException {
-        delay();
-        if (reader.ready()) {
-            StringBuilder builder = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                builder.append(line).append("\n");
-            }
-            // remove last "\n"
-            String result = builder.subSequence(0, builder.length()-1).toString();
+	@Override
+	public void close() throws IOException {
+		if (!oneDocPerCall) {
+			intern_close();
+		}
+	}
 
-            if (debug) {
-                if (dumpEachDocument > 0) {
-                    if (documentCounter % dumpEachDocument == 0) {
-                        long time = System.currentTimeMillis();
-                        LOG.debug(documentCounter + " " + time + " " + (time - lastDumpTime) + " " + line);
-                        lastDumpTime = time;
-                    }
-                }
-                if (measureEachDocument > 0) {
-                    if (documentCounter % measureEachDocument == 0) {
-                        long time = System.currentTimeMillis();
-                        measurements.append(documentCounter).append(";").append(time - basetime).append("\n");
-                    }
-                }
+	private void intern_close() throws IOException {
+		try {
+			if (getDirection().equals(ITransportDirection.IN)) {
+				if (reader != null) {
+					reader.close();
+				}
+			} else {
+				if (writer != null) {
+					writer.close();
+				}
+			}
+		} catch (Exception e) {
+			LOG.error(e.getMessage(), e);
+			throw new IOException(e);
+		} finally {
+			getTransportHandler().close();
+		}
+	}
 
-                if (lastDocument == documentCounter || documentCounter == 0) {
-                    long time = System.currentTimeMillis();
-                    if (documentCounter == 0) {
-                        basetime = time;
-                    }
-                    LOG.debug(documentCounter + " " + time);
-                    measurements.append(documentCounter).append(";").append(time - basetime).append("\n");
-                    if (lastDocument == documentCounter) {
-                        System.out.println(measurements);
-                        isDone = true;
-                    }
-                }
-                documentCounter++;
-            }
-            return getDataHandler().readData(result);
-        } else {
-            return null;
-        }
-    }
+	@Override
+	public boolean hasNext() throws IOException {
+		// if (reader.ready() == false) {
+		// isDone = true;
+		// return false;
+		// }
+		return true;
+	}
 
-    @Override
-    public void write(T object) throws IOException {
-        writer.write(object.toString());
-    }
+	@Override
+	public T getNext() throws IOException {
+		String result = null;
+		delay();
+		if (oneDocPerCall) {
+			intern_open();
+		}
+		if (reader.ready()) {
+			StringBuilder builder = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				builder.append(line).append("\n");
+			}
+			// remove last "\n"
+			result = builder.subSequence(0, builder.length() - 1).toString();
 
-    @Override
-    public boolean isDone() {
-        return isDone;
-    }
+			if (debug) {
+				if (dumpEachDocument > 0) {
+					if (documentCounter % dumpEachDocument == 0) {
+						long time = System.currentTimeMillis();
+						LOG.debug(documentCounter + " " + time + " "
+								+ (time - lastDumpTime) + " " + line);
+						lastDumpTime = time;
+					}
+				}
+				if (measureEachDocument > 0) {
+					if (documentCounter % measureEachDocument == 0) {
+						long time = System.currentTimeMillis();
+						measurements.append(documentCounter).append(";")
+								.append(time - basetime).append("\n");
+					}
+				}
 
-    @Override
-    public IProtocolHandler<T> createInstance(ITransportDirection direction, IAccessPattern access, Map<String, String> options, IDataHandler<T> dataHandler, ITransferHandler<T> transfer) {
-        DocumentProtocolHandler<T> instance = new DocumentProtocolHandler<T>(direction, access);
-        instance.setDataHandler(dataHandler);
-        instance.setTransfer(transfer);
-        instance.init(options);
+				if (lastDocument == documentCounter || documentCounter == 0) {
+					long time = System.currentTimeMillis();
+					if (documentCounter == 0) {
+						basetime = time;
+					}
+					LOG.debug(documentCounter + " " + time);
+					measurements.append(documentCounter).append(";")
+							.append(time - basetime).append("\n");
+					if (lastDocument == documentCounter) {
+						System.out.println(measurements);
+						isDone = true;
+					}
+				}
+				documentCounter++;
+			}
+		}
+		if (oneDocPerCall) {
+			intern_close();
+		}
+		if (result != null) {
+			return getDataHandler().readData(result);
+		} else {
+			return null;
+		}
+	}
 
-        return instance;
-    }
+	@Override
+	public void write(T object) throws IOException {
+		writer.write(object.toString());
+	}
 
-    @Override
-    public String getName() {
-        return "Document";
-    }
+	@Override
+	public boolean isDone() {
+		return isDone;
+	}
 
-    public long getDelay() {
-        return delay;
-    }
+	@Override
+	public IProtocolHandler<T> createInstance(ITransportDirection direction,
+			IAccessPattern access, Map<String, String> options,
+			IDataHandler<T> dataHandler, ITransferHandler<T> transfer) {
+		DocumentProtocolHandler<T> instance = new DocumentProtocolHandler<T>(
+				direction, access);
+		instance.setDataHandler(dataHandler);
+		instance.setTransfer(transfer);
+		instance.init(options);
 
-    public void setDelay(long delay) {
-        this.delay = delay;
-    }
+		return instance;
+	}
 
-    public void setNanodelay(int nanodelay) {
-        this.nanodelay = nanodelay;
-    }
+	@Override
+	public String getName() {
+		return "Document";
+	}
 
-    public int getNanodelay() {
-        return nanodelay;
-    }
+	public long getDelay() {
+		return delay;
+	}
 
-    public int getDelayeach() {
-        return delayeach;
-    }
+	public void setDelay(long delay) {
+		this.delay = delay;
+	}
 
-    public void setDelayeach(int delayeach) {
-        this.delayeach = delayeach;
-    }
+	public void setNanodelay(int nanodelay) {
+		this.nanodelay = nanodelay;
+	}
 
-    @Override
-    public ITransportExchangePattern getExchangePattern() {
-        if (this.getDirection().equals(ITransportDirection.IN)) {
-            return ITransportExchangePattern.InOnly;
-        } else {
-            return ITransportExchangePattern.OutOnly;
-        }
-    }
+	public int getNanodelay() {
+		return nanodelay;
+	}
 
-    @Override
-    public void onConnect(ITransportHandler caller) {
-        // TODO Auto-generated method stub
+	public int getDelayeach() {
+		return delayeach;
+	}
 
-    }
+	public void setDelayeach(int delayeach) {
+		this.delayeach = delayeach;
+	}
 
-    @Override
-    public void onDisonnect(ITransportHandler caller) {
-        // TODO Auto-generated method stub
+	@Override
+	public ITransportExchangePattern getExchangePattern() {
+		if (this.getDirection().equals(ITransportDirection.IN)) {
+			return ITransportExchangePattern.InOnly;
+		} else {
+			return ITransportExchangePattern.OutOnly;
+		}
+	}
 
-    }
+	@Override
+	public void onConnect(ITransportHandler caller) {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void process(ByteBuffer message) {
-        getTransfer().transfer(getDataHandler().readData(message));
-    }
+	}
 
-    protected void init(Map<String, String> options) {
-        if (options.get("delay") != null) {
-            setDelay(Long.parseLong(options.get("delay")));
-        }
-        if (options.get("nanodelay") != null) {
-            setNanodelay(Integer.parseInt(options.get("nanodelay")));
-        }
-        if (options.get("delayeach") != null) {
-            setDelayeach(Integer.parseInt(options.get("delayeach")));
-        }
+	@Override
+	public void onDisonnect(ITransportHandler caller) {
+		// TODO Auto-generated method stub
 
-        if (options.get("dumpeachdocument") != null) {
-            dumpEachDocument = Integer.parseInt(options.get("dumpeachdocument"));
-        }
+	}
 
-        if (options.get("measureeachdocument") != null) {
-            measureEachDocument = Integer.parseInt(options.get("measureeachdocument"));
-            measurements.setLength(0);
-        }
+	@Override
+	public void process(ByteBuffer message) {
+		getTransfer().transfer(getDataHandler().readData(message));
+	}
 
-        if (options.get("lastdocument") != null) {
-            lastDocument = Integer.parseInt(options.get("lastdocument"));
-        }
-        if (options.get("maxdocuments") != null) {
-            lastDocument = Integer.parseInt(options.get("maxdocuments"));
-        }
-        if (options.get("debug") != null) {
-            debug = Boolean.parseBoolean(options.get("debug"));
-        }
-        lastDumpTime = System.currentTimeMillis();
-    }
+	protected void init(Map<String, String> options) {
+		if (options.containsKey("delay")) {
+			setDelay(Long.parseLong(options.get("delay")));
+		}
+		if (options.containsKey("nanodelay")) {
+			setNanodelay(Integer.parseInt(options.get("nanodelay")));
+		}
+		if (options.containsKey("delayeach")) {
+			setDelayeach(Integer.parseInt(options.get("delayeach")));
+		}
 
-    protected void delay() {
-        if (delayeach > 0) {
-            delayCounter++;
-            if (delayCounter < delayeach) {
-                return;
-            }
-            delayCounter = 0;
-        }
-        if (delay > 0) {
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                LOG.debug(e.getMessage(), e);
-            }
-        } else {
-            if (nanodelay > 0) {
-                try {
-                    Thread.sleep(0L, nanodelay);
-                } catch (InterruptedException e) {
-                    LOG.debug(e.getMessage(), e);
-                }
-            }
-        }
-    }
+		if (options.containsKey("dumpeachdocument")) {
+			dumpEachDocument = Integer
+					.parseInt(options.get("dumpeachdocument"));
+		}
+
+		if (options.containsKey("measureeachdocument")) {
+			measureEachDocument = Integer.parseInt(options
+					.get("measureeachdocument"));
+			measurements.setLength(0);
+		}
+
+		if (options.containsKey("lastdocument")) {
+			lastDocument = Integer.parseInt(options.get("lastdocument"));
+		}
+		if (options.containsKey("maxdocuments")) {
+			lastDocument = Integer.parseInt(options.get("maxdocuments"));
+		}
+		if (options.containsKey("debug")) {
+			debug = Boolean.parseBoolean(options.get("debug"));
+		}
+		if (options.containsKey("onedocpercall")){
+			oneDocPerCall = Boolean.parseBoolean(options.get("onedocpercall"));
+		}
+		lastDumpTime = System.currentTimeMillis();
+	}
+
+	protected void delay() {
+		if (delayeach > 0) {
+			delayCounter++;
+			if (delayCounter < delayeach) {
+				return;
+			}
+			delayCounter = 0;
+		}
+		if (delay > 0) {
+			try {
+				Thread.sleep(delay);
+			} catch (InterruptedException e) {
+				LOG.debug(e.getMessage(), e);
+			}
+		} else {
+			if (nanodelay > 0) {
+				try {
+					Thread.sleep(0L, nanodelay);
+				} catch (InterruptedException e) {
+					LOG.debug(e.getMessage(), e);
+				}
+			}
+		}
+	}
 }
