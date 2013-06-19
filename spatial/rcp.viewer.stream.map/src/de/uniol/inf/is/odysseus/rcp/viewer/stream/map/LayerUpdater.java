4d3d616367167f8a07234581743deb2e240baecf
Tobias Leo Brandt <tobias.leo.brandt@uni-oldenburg.de>
2013-06-19 16:12:47 +0000

    LayerUpdater: Overflow-protection
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15726 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f3c417c3e62fbb04a8f8997bb4309fa0d99d1283

--- spatial/rcp.viewer.stream.map/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/map/LayerUpdater.java
+++ spatial/rcp.viewer.stream.map/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/map/LayerUpdater.java
@@ -5,6 +5,7 @@ import java.beans.PropertyChangeListener;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
@@ -26,127 +27,169 @@ import de.uniol.inf.is.odysseus.intervalapproach.DefaultTISweepArea;
 import de.uniol.inf.is.odysseus.rcp.viewer.stream.map.layer.DataSet;
 import de.uniol.inf.is.odysseus.rcp.viewer.stream.map.layer.ILayer;
 
-public class LayerUpdater extends ArrayList<ILayer> implements IStreamElementListener<Object>, Serializable, PropertyChangeListener {
+public class LayerUpdater extends ArrayList<ILayer> implements
+		IStreamElementListener<Object>, Serializable, PropertyChangeListener {
 
-    private static final long serialVersionUID = 1092858542289960843L;
-    
-    private static final Logger LOG = LoggerFactory.getLogger(LayerUpdater.class);
-    
+	private static final long serialVersionUID = 1092858542289960843L;
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(LayerUpdater.class);
+
 	private final IStreamMapEditor streamMapEditor;
 	private final IStreamConnection<Object> connection;
 	private final IPhysicalQuery query;
-	
+	private int maxNumerOfElements;
+
 	private DefaultTISweepArea<Tuple<? extends ITimeInterval>> puffer;
 	private HashMap<Integer, Quadtree> index = new HashMap<Integer, Quadtree>();
 	private HashMap<Integer, Envelope> env = new HashMap<Integer, Envelope>();
 	private int srid;
 
 	private List<Tuple<? extends ITimeInterval>> elementList;
-	
-	public LayerUpdater(IStreamMapEditor streamMapEditor, IPhysicalQuery query, IStreamConnection<Object> connection) {
+
+	public LayerUpdater(IStreamMapEditor streamMapEditor, IPhysicalQuery query,
+			IStreamConnection<Object> connection) {
 		super();
 		this.streamMapEditor = streamMapEditor;
 		this.connection = connection;
 		this.query = query;
 		this.puffer = new DefaultTISweepArea<Tuple<? extends ITimeInterval>>();
 		connection.addStreamElementListener(this);
-		if(!connection.isConnected()){
+		if (!connection.isConnected()) {
 			connection.connect();
 		}
-		
+		maxNumerOfElements = 100000;
+
 	}
 
-	public IStreamConnection<Object> getConnection(){
+	public IStreamConnection<Object> getConnection() {
 		return connection;
 	}
 
 	public IPhysicalQuery getQuery() {
-	    return query;
-    }
-	
+		return query;
+	}
+
 	@Override
 	public void streamElementRecieved(Object element, int port) {
 		if (!(element instanceof Tuple<?>)) {
 			LOG.error("Warning: StreamMap is only for spatial relational tuple!");
 			return;
 		}
-		
-		// SweepArea definiert Element Fenster bzw. die zu visualisierenden Elemente. 
+
+		// SweepArea definiert Element Fenster bzw. die zu visualisierenden
+		// Elemente.
 		@SuppressWarnings("unchecked")
 		Tuple<? extends ITimeInterval> tuple = (Tuple<? extends ITimeInterval>) element;
 		PointInTime timestamp = tuple.getMetadata().getStart().clone();
 		this.streamMapEditor.getScreenManager().setMaxIntervalEnd(timestamp);
 		puffer.insert(tuple);
-    	if (this.streamMapEditor.getScreenManager().getInterval().getEnd().isInfinite()){
-    		addTuple(tuple);
-    	}
-		
+		if (this.streamMapEditor.getScreenManager().getInterval().getEnd()
+				.isInfinite()) {
+			// Add tuple to current list
+			addTuple(tuple);
+		}
+
+		// Prevent an overflow in the puffer
+		if (puffer.size() > maxNumerOfElements) {
+			// TODO: Which size is ok? Config?
+
+			// Remove old element(s)
+			Iterator<Tuple<? extends ITimeInterval>> oldestElements = puffer
+					.peekElementsContaing(puffer.getMinTs(), false);
+			
+			PointInTime deleteTime = null;
+			if(oldestElements.hasNext()) {
+				// We need just one
+				// This should be one of the last elements
+				// of more than one start with the same 
+				// oldest timestamp
+				Tuple<? extends ITimeInterval> elem = oldestElements.next();
+				deleteTime = elem.getMetadata().getEnd();
+			}
+			puffer.purgeElementsBefore(deleteTime.plus(1));
+
+			// Update "current-list", timeSlider and all the other things which
+			// rely on the startTimeStamp
+			streamMapEditor.getScreenManager().setIntervalStart(
+					puffer.getMinTs());
+		}
+
+		// Should we redraw here or just if we added the tupel to the current
+		// list?
 		streamMapEditor.getScreenManager().redraw();
-		
+
 	}
 
 	@Override
 	public void punctuationElementRecieved(IPunctuation point, int port) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void securityPunctuationElementRecieved(ISecurityPunctuation sp, int port) {
+	public void securityPunctuationElementRecieved(ISecurityPunctuation sp,
+			int port) {
 		// TODO Auto-generated method stub
 	}
 
-    /**
-     * @param tuple
-     */
-    public void addTuple(Tuple<? extends ITimeInterval> tuple) {
-    	this.getElementList();
-    	synchronized(elementList){
-    		this.elementList.add(tuple);
-    	}
-		ScreenTransformation transformation = this.streamMapEditor.getScreenManager().getTransformation();
+	/**
+	 * @param tuple
+	 */
+	public void addTuple(Tuple<? extends ITimeInterval> tuple) {
+		this.getElementList();
+		synchronized (elementList) {
+			this.elementList.add(tuple);
+		}
+		ScreenTransformation transformation = this.streamMapEditor
+				.getScreenManager().getTransformation();
 		int destSrid = this.streamMapEditor.getScreenManager().getSRID();
-			for (Entry<Integer, Quadtree> tree : this.index.entrySet()) {
-				synchronized (tree) {
-					DataSet newDataSet = new DataSet(tuple, tree.getKey(), destSrid, transformation);
-					tree.getValue().insert(newDataSet.getEnvelope(), newDataSet);
-					this.env.get(tree.getKey()).expandToInclude(newDataSet.getEnvelope());
+		for (Entry<Integer, Quadtree> tree : this.index.entrySet()) {
+			synchronized (tree) {
+				DataSet newDataSet = new DataSet(tuple, tree.getKey(),
+						destSrid, transformation);
+				tree.getValue().insert(newDataSet.getEnvelope(), newDataSet);
+				this.env.get(tree.getKey()).expandToInclude(
+						newDataSet.getEnvelope());
 
 			}
 		}
-//		if (this.configuration.getMaxTupleCount() < this.dataSets.size()){
-//				LOG.debug("(REMOVE)Current Geometries:" + dataSets.size());
-//			synchronized (dataSets) {
-//				DataSet toRemove = this.dataSets.poll();
-//				this.tree.remove(toRemove.getEnvelope(), toRemove);
-//			}
-//		}
-    }
+		// if (this.configuration.getMaxTupleCount() < this.dataSets.size()){
+		// LOG.debug("(REMOVE)Current Geometries:" + dataSets.size());
+		// synchronized (dataSets) {
+		// DataSet toRemove = this.dataSets.poll();
+		// this.tree.remove(toRemove.getEnvelope(), toRemove);
+		// }
+		// }
+	}
 
-
-	public List<?> query(Envelope searchEnv, int idx){
+	public List<?> query(Envelope searchEnv, int idx) {
 		return getTree(idx).query(searchEnv);
 	}
-	
-	private Quadtree getTree(int idx){
+
+	private Quadtree getTree(int idx) {
 		Quadtree tree = null;
-		synchronized (this.index){
+		synchronized (this.index) {
 			tree = this.index.get(idx);
-			ScreenTransformation transformation = this.streamMapEditor.getScreenManager().getTransformation();
+			ScreenTransformation transformation = this.streamMapEditor
+					.getScreenManager().getTransformation();
 			int destSrid = this.streamMapEditor.getScreenManager().getSRID();
-			if (this.srid != destSrid){
-					tree = null;
-					this.srid = destSrid;
+			if (this.srid != destSrid) {
+				tree = null;
+				this.srid = destSrid;
 			}
-			if (tree == null) { 
+			if (tree == null) {
 				tree = new Quadtree();
 				this.env.put(idx, new Envelope());
 				getElementList();
-				synchronized(elementList){
-					for (Tuple<? extends ITimeInterval> tuple : this.getElementList()) {
-						DataSet newDataSet = new DataSet(tuple, idx, destSrid, transformation);
+				synchronized (elementList) {
+					for (Tuple<? extends ITimeInterval> tuple : this
+							.getElementList()) {
+						DataSet newDataSet = new DataSet(tuple, idx, destSrid,
+								transformation);
 						tree.insert(newDataSet.getEnvelope(), newDataSet);
-						this.env.get(idx).expandToInclude(newDataSet.getEnvelope());
+						this.env.get(idx).expandToInclude(
+								newDataSet.getEnvelope());
 					}
 				}
 				this.index.put(idx, tree);
@@ -154,17 +197,19 @@ public class LayerUpdater extends ArrayList<ILayer> implements IStreamElementLis
 		}
 		return tree;
 	}
-	
+
 	@Override
 	public boolean add(ILayer e) {
 		e.setLayerUpdater(this);
 		return super.add(e);
 	}
-	
-	public List<Tuple<? extends ITimeInterval>> getElementList(){
 
-		if (this.elementList == null){
-			this.elementList = this.puffer.queryOverlapsAsList(this.streamMapEditor.getScreenManager().getInterval());
+	public List<Tuple<? extends ITimeInterval>> getElementList() {
+
+		if (this.elementList == null) {
+			this.elementList = this.puffer
+					.queryOverlapsAsList(this.streamMapEditor
+							.getScreenManager().getInterval());
 		}
 		return this.elementList;
 	}
@@ -182,13 +227,18 @@ public class LayerUpdater extends ArrayList<ILayer> implements IStreamElementLis
 
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
-		if ("intervalStart".equals(evt.getPropertyName())){
-			this.elementList = this.puffer.queryOverlapsAsList(this.streamMapEditor.getScreenManager().getInterval());
-			//System.out.println(this.puffer.getMinTs() + " " + this.puffer.getMaxTs() + " " + this.elementList.size());
+		if ("intervalStart".equals(evt.getPropertyName())) {
+			this.elementList = this.puffer
+					.queryOverlapsAsList(this.streamMapEditor
+							.getScreenManager().getInterval());
+			// System.out.println(this.puffer.getMinTs() + " " +
+			// this.puffer.getMaxTs() + " " + this.elementList.size());
 			this.index = new HashMap<Integer, Quadtree>(this.index.size());
 		}
-		if ("intervalEnd".equals(evt.getPropertyName())){
-			this.elementList = this.puffer.queryOverlapsAsList(this.streamMapEditor.getScreenManager().getInterval());
+		if ("intervalEnd".equals(evt.getPropertyName())) {
+			this.elementList = this.puffer
+					.queryOverlapsAsList(this.streamMapEditor
+							.getScreenManager().getInterval());
 			this.index = new HashMap<Integer, Quadtree>(this.index.size());
 		}
 	}
