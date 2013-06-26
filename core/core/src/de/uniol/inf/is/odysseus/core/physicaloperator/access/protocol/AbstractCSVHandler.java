d7ecc29d526c128f1aa0fb07101a2c1a9816126f
Christian Kuka <christian@kuka.cc>
2013-06-26 08:49:24 +0000

    CSV Handler: Use transport send method instead of output stream write method
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15819 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c4d2e0e455d562c44c1cc7b074789612ecb8798c

--- core/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/protocol/AbstractCSVHandler.java
+++ core/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/protocol/AbstractCSVHandler.java
@@ -5,6 +5,8 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.text.DecimalFormat;
 import java.util.Map;
 
@@ -13,124 +15,115 @@ import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITranspor
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportHandler;
 
 abstract public class AbstractCSVHandler<T> extends LineProtocolHandler<T> {
+    private static final Charset charset = Charset.forName("UTF-8");
+    protected char delimiter;
+    protected char textDelimiter;
+    protected DecimalFormat floatingFormatter;
+    protected DecimalFormat numberFormatter;
+    protected boolean writeMetadata;
+    protected boolean trim = false;
+    protected boolean addLineNumber = false;
+    protected String delimiterString;
 
-	protected char delimiter;
-	protected char textDelimiter;
-	protected DecimalFormat floatingFormatter;
-	protected DecimalFormat numberFormatter;
-	protected boolean writeMetadata;
-	protected boolean trim = false;
-	protected boolean addLineNumber = false;
-	protected String delimiterString;
+    public AbstractCSVHandler(ITransportDirection direction, IAccessPattern access) {
+        super(direction, access);
+    }
 
-	public AbstractCSVHandler(ITransportDirection direction,
-			IAccessPattern access) {
-		super(direction, access);
-	}
+    public AbstractCSVHandler() {
+        super();
+    }
 
-	public AbstractCSVHandler() {
-		super();
-	}
+    @Override
+    protected void init(Map<String, String> options) {
+        super.init(options);
+        if (options.containsKey("delimiter")) {
+            delimiter = options.get("delimiter").toCharArray()[0];
+        } else {
+            delimiter = options.containsKey("csv.delimiter") ? options.get("csv.delimiter").toCharArray()[0] : ",".toCharArray()[0];
+        }
+        if (options.containsKey("textdelimiter")) {
+            textDelimiter = options.get("textdelimiter").toCharArray()[0];
+        } else {
+            textDelimiter = options.containsKey("csv.textdelimiter") ? options.get("csv.textdelimiter").toCharArray()[0] : "'".toCharArray()[0];
+        }
+        // only calc once
+        delimiterString = Character.toString(delimiter);
+        if (options.containsKey("csv.floatingformatter")) {
+            floatingFormatter = new DecimalFormat(options.get("csv.floatingformatter"));
+        }
+        if (options.containsKey("csv.numberformatter")) {
+            numberFormatter = new DecimalFormat(options.get("csv.numberformatter"));
+        }
+        if (options.containsKey("csv.writemetadata")) {
+            writeMetadata = Boolean.parseBoolean(options.get("csv.writemetadata"));
+        }
+        if (options.containsKey("csv.trim")) {
+            trim = Boolean.parseBoolean(options.get("csv.trim"));
+        }
+        if (options.get("addlinenumber") != null) {
+            addLineNumber = Boolean.parseBoolean(options.get("addlinenumber"));
+        }
 
-	@Override
-	protected void init(Map<String, String> options) {
-		super.init(options);
-		if (options.containsKey("delimiter")) {
-			delimiter = options.get("delimiter").toCharArray()[0];
-		} else {
-			delimiter = options.containsKey("csv.delimiter") ? options.get(
-					"csv.delimiter").toCharArray()[0] : ",".toCharArray()[0];
-		}
-		if (options.containsKey("textdelimiter")) {
-			textDelimiter = options.get("textdelimiter").toCharArray()[0];
-		} else {
-			textDelimiter = options.containsKey("csv.textdelimiter") ? options
-					.get("csv.textdelimiter").toCharArray()[0] : "'"
-					.toCharArray()[0];
-		}
-		// only calc once
-		delimiterString = Character.toString(delimiter);
-		if (options.containsKey("csv.floatingformatter")) {
-			floatingFormatter = new DecimalFormat(
-					options.get("csv.floatingformatter"));
-		}
-		if (options.containsKey("csv.numberformatter")) {
-			numberFormatter = new DecimalFormat(
-					options.get("csv.numberformatter"));
-		}
-		if (options.containsKey("csv.writemetadata")) {
-			writeMetadata = Boolean.parseBoolean(options
-					.get("csv.writemetadata"));
-		}
-		if (options.containsKey("csv.trim")){
-			trim = Boolean.parseBoolean(options
-					.get("csv.trim"));
-		}
-		if (options.get("addlinenumber") != null) {
-			addLineNumber = Boolean.parseBoolean(options.get("addlinenumber"));
-		}
+    }
 
-	}
+    @Override
+    public void onConnect(ITransportHandler caller) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void onConnect(ITransportHandler caller) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void onDisonnect(ITransportHandler caller) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void onDisonnect(ITransportHandler caller) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public T getNext() throws IOException {
+        String line = super.getNextLine();
+        if (line != null) {
+            if (addLineNumber) {
+                line = super.lineCounter + delimiterString + line;
+            }
+            return readLine(line);
+        }
+        return null;
+    }
 
-	@Override
-	public T getNext() throws IOException {
-		String line = super.getNextLine();
-		if (line != null) {
-			if (addLineNumber) {
-				line = super.lineCounter + delimiterString + line;
-			}
-			return readLine(line);
-		}
-		return null;
-	}
+    @Override
+    public void process(ByteBuffer message) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(message.array())));
+        if (!firstLineSkipped && !readFirstLine) {
+            try {
+                reader.readLine();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            firstLineSkipped = true;
+        }
+        String line;
+        try {
+            line = reader.readLine();
+            if (line != null) {
+                T retValue = readLine(line);
+                // System.out.println(retValue);
+                getTransfer().transfer(retValue);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void process(ByteBuffer message) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				new ByteArrayInputStream(message.array())));
-		if (!firstLineSkipped && !readFirstLine) {
-			try {
-				reader.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			firstLineSkipped = true;
-		}
-		String line;
-		try {
-			line = reader.readLine();
-			if (line != null) {
-				T retValue = readLine(line);
-				//System.out.println(retValue);
-				getTransfer().transfer(retValue);
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    protected abstract T readLine(String line);
 
-	protected abstract T readLine(String line);
-
-	@Override
-	public void write(T object) throws IOException {
-		StringBuilder out = new StringBuilder();
-		getDataHandler().writeCSVData(out, object, delimiter, textDelimiter,
-				floatingFormatter, numberFormatter, writeMetadata);
-		writer.write(out.toString() + System.lineSeparator());
-	}
+    @Override
+    public void write(T object) throws IOException {
+        StringBuilder out = new StringBuilder();
+        getDataHandler().writeCSVData(out, object, delimiter, textDelimiter, floatingFormatter, numberFormatter, writeMetadata);
+        out.append(System.lineSeparator());
+        getTransportHandler().send(charset.encode(CharBuffer.wrap(out)).array());
+    }
 
 }
