55c71d8a984bbd813205734d2bb174b3da17382e
Christian Kuka <christian@kuka.cc>
2013-07-09 14:56:17 +0000

    Probabilistic: Add additional datatypes in data handler
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15916 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: bf77eae3acffef70311e7e0b0ded8ce65ee7e9dd

--- probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/continuous/datahandler/ProbabilisticContinuousDoubleHandler.java
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/continuous/datahandler/ProbabilisticContinuousDoubleHandler.java
@@ -26,12 +26,17 @@ import de.uniol.inf.is.odysseus.core.datahandler.AbstractDataHandler;
 import de.uniol.inf.is.odysseus.core.datahandler.IDataHandler;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.probabilistic.continuous.datatype.ProbabilisticContinuousDouble;
+import de.uniol.inf.is.odysseus.probabilistic.sdf.schema.SDFProbabilisticDatatype;
 
 public class ProbabilisticContinuousDoubleHandler extends AbstractDataHandler<ProbabilisticContinuousDouble> {
 	static protected List<String> types = new ArrayList<String>();
 	static {
-		ProbabilisticContinuousDoubleHandler.types.add("ProbabilisticContinuousDouble");
-		ProbabilisticContinuousDoubleHandler.types.add("ProbabilisticContinuousFloat");
+		ProbabilisticContinuousDoubleHandler.types.add(SDFProbabilisticDatatype.PROBABILISTIC_CONTINUOUS_BYTE.getURI());
+		ProbabilisticContinuousDoubleHandler.types.add(SDFProbabilisticDatatype.PROBABILISTIC_CONTINUOUS_SHORT.getURI());
+		ProbabilisticContinuousDoubleHandler.types.add(SDFProbabilisticDatatype.PROBABILISTIC_CONTINUOUS_INTEGER.getURI());
+		ProbabilisticContinuousDoubleHandler.types.add(SDFProbabilisticDatatype.PROBABILISTIC_CONTINUOUS_LONG.getURI());
+		ProbabilisticContinuousDoubleHandler.types.add(SDFProbabilisticDatatype.PROBABILISTIC_CONTINUOUS_FLOAT.getURI());
+		ProbabilisticContinuousDoubleHandler.types.add(SDFProbabilisticDatatype.PROBABILISTIC_CONTINUOUS_DOUBLE.getURI());
 	}
 
 	@Override
