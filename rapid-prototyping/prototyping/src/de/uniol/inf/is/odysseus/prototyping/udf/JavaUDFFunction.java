a66c445f2cb356c680ec69010be52377f720eb26
Christian Kuka <christian@kuka.cc>
2013-07-11 22:13:49 +0000

    Prototyping: Fix class loading and compiling
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@15948 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a2b6bac5d66eec14f053bc90a5c2cdb7194e0632

--- rapid-prototyping/prototyping/src/de/uniol/inf/is/odysseus/prototyping/udf/JavaUDFFunction.java
+++ rapid-prototyping/prototyping/src/de/uniol/inf/is/odysseus/prototyping/udf/JavaUDFFunction.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,55 +26,71 @@ import de.uniol.inf.is.odysseus.core.server.physicaloperator.IUserDefinedFunctio
  * 
  */
 @UserDefinedFunction(name = "Java")
-public class JavaUDFFunction implements IUserDefinedFunction<Tuple<? extends IMetaAttribute>, Tuple<? extends IMetaAttribute>> {
+public class JavaUDFFunction
+		implements
+		IUserDefinedFunction<Tuple<? extends IMetaAttribute>, Tuple<? extends IMetaAttribute>> {
 
-    private final Logger LOG = LoggerFactory.getLogger(JavaUDFFunction.class);
+	private final Logger LOG = LoggerFactory.getLogger(JavaUDFFunction.class);
 
-    /** The process method. */
-    private Method udfMethod;
+	/** The process method. */
+	private Method udfMethod;
 
-    @Override
-    public void init(String initString) {
-        String path = initString;
-        File javaFile = new File(path);
-        String fileName = javaFile.getName();
-        String className = fileName.substring(0, fileName.indexOf("."));
+	private Class<?> udfClass;
 
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(javaFile)));
-            StringBuilder content = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-            reader.close();
-            if (LOG.isDebugEnabled()) {
-                LOG.debug(content.toString());
-            }
-            UDFClassLoader udfClassLoader = new UDFClassLoader(className, content.toString());
-            this.udfMethod = udfClassLoader.loadClass(className).getMethod("process");
-        } catch (IOException | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
-            LOG.error(e.getMessage(), e);
-        }
-    }
+	private Object udfInstance;
 
-    @SuppressWarnings("rawtypes")
-    @Override
-    public Tuple<? extends IMetaAttribute> process(Tuple<? extends IMetaAttribute> in, int port) {
-        Tuple<?> ret = null;
-        Object[] retObj = new Object[] {};
-        try {
-            retObj = (Object[]) this.udfMethod.invoke(in.getAttributes(), in.getMetadataMap());
-        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-            LOG.error(e.getMessage(), e);
-        }
-        ret = new Tuple(retObj, false);
-        return ret;
-    }
+	@Override
+	public void init(String initString) {
+		String path = initString;
+		File javaFile = new File(path);
+		String fileName = javaFile.getName();
+		String className = fileName.substring(0, fileName.indexOf("."));
 
-    @Override
-    public OutputMode getOutputMode() {
-        return OutputMode.MODIFIED_INPUT;
-    }
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					new FileInputStream(javaFile)));
+			StringBuilder content = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				content.append(line).append("\n");
+			}
+			reader.close();
+			if (LOG.isDebugEnabled()) {
+				LOG.debug(content.toString());
+			}
+			UDFClassLoader udfClassLoader = new UDFClassLoader(className,
+					content.toString());
+			this.udfClass = udfClassLoader.loadClass(className);
+			this.udfMethod = this.udfClass.getMethod("process", new Class[] {
+					Object[].class, Map.class });
+			this.udfInstance = this.udfClass.newInstance();
+		} catch (IOException | NoSuchMethodException | SecurityException
+				| ClassNotFoundException | InstantiationException
+				| IllegalAccessException e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Tuple<? extends IMetaAttribute> process(
+			Tuple<? extends IMetaAttribute> in, int port) {
+		Tuple<?> ret = null;
+		Object[] retObj = new Object[] {};
+		try {
+			retObj = (Object[]) this.udfMethod.invoke(this.udfInstance,
+					in.getAttributes(), in.getMetadataMap());
+		} catch (IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			LOG.error(e.getMessage(), e);
+		}
+		ret = new Tuple(retObj, false);
+		return ret;
+	}
+
+	@Override
+	public OutputMode getOutputMode() {
+		return OutputMode.MODIFIED_INPUT;
+	}
 
 }
