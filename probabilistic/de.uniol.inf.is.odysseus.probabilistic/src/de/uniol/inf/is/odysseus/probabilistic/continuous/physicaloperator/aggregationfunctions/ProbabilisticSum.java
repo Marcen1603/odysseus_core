71ea1cf41e915ec96239abd48b12a567a07acca3
Christian Kuka <christian@kuka.cc>
2013-07-22 15:43:13 +0000

    Probabilistic: Fix math operators for continuous prob. values
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16011 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d469dba1d21527ce26647a8432dcfd70ccde1dc2

---
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/continuous/physicaloperator/aggregationfunctions/ProbabilisticSum.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2013 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.probabilistic.continuous.physicaloperator.aggregationfunctions;
+
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.AbstractAggregateFunction;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IPartialAggregate;
+import de.uniol.inf.is.odysseus.probabilistic.base.ProbabilisticTuple;
+import de.uniol.inf.is.odysseus.probabilistic.continuous.datatype.NormalDistributionMixture;
+import de.uniol.inf.is.odysseus.probabilistic.continuous.datatype.ProbabilisticContinuousDouble;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class ProbabilisticSum extends AbstractAggregateFunction<ProbabilisticTuple<?>, ProbabilisticTuple<?>> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7877321730422944130L;
+	/** The attribute position. */
+	private final int pos;
+	/** The result data type. */
+	private final String datatype;
+
+	/**
+	 * Gets an instance of {@link ProbabilisticSum}.
+	 * 
+	 * @param pos
+	 *            The attribute position
+	 * @param partialAggregateInput
+	 *            The partial aggregate input
+	 * @param datatype
+	 *            The result datatype
+	 * @return An instance of {@link ProbabilisticSum}
+	 */
+	public static ProbabilisticSum getInstance(final int pos, final boolean partialAggregateInput, final String datatype) {
+		return new ProbabilisticSum(pos, partialAggregateInput, datatype);
+	}
+
+	/**
+	 * Creates a new instance of {@link ProbabilisticSum}.
+	 * 
+	 * @param pos
+	 *            The attribute position
+	 * @param partialAggregateInput
+	 *            The partial aggregate input
+	 * @param datatype
+	 *            The result datatype
+	 */
+	protected ProbabilisticSum(final int pos, final boolean partialAggregateInput, final String datatype) {
+		super("SUM", partialAggregateInput);
+		this.pos = pos;
+		this.datatype = datatype;
+	}
+
+	/*
+	 * 
+	 * @see de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IInitializer#init(java.lang.Object)
+	 */
+	@Override
+	public final IPartialAggregate<ProbabilisticTuple<?>> init(final ProbabilisticTuple<?> in) {
+		final NormalDistributionMixture distribution = in.getDistribution(((ProbabilisticContinuousDouble) in.getAttribute(this.pos)).getDistribution());
+		final SumPartialAggregate<ProbabilisticTuple<?>> pa = new SumPartialAggregate<ProbabilisticTuple<?>>(distribution, datatype);
+		return pa;
+	}
+
+	/*
+	 * 
+	 * @see de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IMerger#merge(de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IPartialAggregate, java.lang.Object, boolean)
+	 */
+	@Override
+	public final IPartialAggregate<ProbabilisticTuple<?>> merge(final IPartialAggregate<ProbabilisticTuple<?>> p, final ProbabilisticTuple<?> toMerge, final boolean createNew) {
+		SumPartialAggregate<ProbabilisticTuple<?>> pa = null;
+		if (createNew) {
+			pa = new SumPartialAggregate<ProbabilisticTuple<?>>(((SumPartialAggregate<ProbabilisticTuple<?>>) p).getSum(), datatype);
+		} else {
+			pa = (SumPartialAggregate<ProbabilisticTuple<?>>) p;
+		}
+		final NormalDistributionMixture distribution = toMerge.getDistribution(((ProbabilisticContinuousDouble) toMerge.getAttribute(this.pos)).getDistribution());
+
+		pa.add(distribution);
+		return pa;
+	}
+
+	/*
+	 * 
+	 * @see de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IEvaluator#evaluate(de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IPartialAggregate)
+	 */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public final ProbabilisticTuple<?> evaluate(final IPartialAggregate<ProbabilisticTuple<?>> p) {
+		final SumPartialAggregate<ProbabilisticTuple<?>> pa = (SumPartialAggregate<ProbabilisticTuple<?>>) p;
+		final ProbabilisticTuple<?> r = new ProbabilisticTuple(1, 1, true);
+		r.setDistribution(0, pa.getSum());
+		r.setAttribute(0, new ProbabilisticContinuousDouble(0));
+		return r;
+	}
+}
