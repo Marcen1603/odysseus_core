ee9612cbe20b9f307d1c95baceb34dbda7d87393
Christian Kuka <christian@kuka.cc>
2013-07-22 16:40:35 +0000

    Probabilistic: Add Avg aggregation for continuous prob. values in short streams
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16015 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3d8804f2e4106477627c255f8a8f1da62c733717

---
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/continuous/physicaloperator/aggregationfunctions/AvgPartialAggregate.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright 2013 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.probabilistic.continuous.physicaloperator.aggregationfunctions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.math3.distribution.MixtureMultivariateNormalDistribution;
+import org.apache.commons.math3.distribution.MultivariateNormalDistribution;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.util.Pair;
+
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IPartialAggregate;
+import de.uniol.inf.is.odysseus.probabilistic.continuous.datatype.NormalDistributionMixture;
+import de.uniol.inf.is.odysseus.probabilistic.math.Interval;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class AvgPartialAggregate<T> implements IPartialAggregate<T> {
+	/** The sum value of the aggregate. */
+	private NormalDistributionMixture sum;
+	/** The count value of the aggregate. */
+	private int count = 0;
+	/** The result data type. */
+	private final String datatype;
+
+	/**
+	 * Creates a new partial aggregate with the given value.
+	 * 
+	 * @param distribution
+	 *            The distribution
+	 * @param datatype
+	 *            The result datatype
+	 */
+	public AvgPartialAggregate(final NormalDistributionMixture distribution, final String datatype) {
+		this.sum = distribution;
+		this.count = 1;
+		this.datatype = datatype;
+	}
+
+	/**
+	 * Copy constructor.
+	 * 
+	 * @param avgPartialAggregate
+	 *            The object to copy from
+	 */
+	public AvgPartialAggregate(final AvgPartialAggregate<T> avgPartialAggregate) {
+		this.sum = avgPartialAggregate.sum;
+		this.count = avgPartialAggregate.count;
+		this.datatype = avgPartialAggregate.datatype;
+	}
+
+	/**
+	 * Gets the value of the sum property.
+	 * 
+	 * @return the sum
+	 */
+	public final NormalDistributionMixture getSum() {
+		return this.sum;
+	}
+
+	/**
+	 * Gets the value of the count property.
+	 * 
+	 * @return the count
+	 */
+	public final int getCount() {
+		return this.count;
+	}
+
+	/**
+	 * Gets the current average.
+	 * 
+	 * @return The average.
+	 */
+	public final NormalDistributionMixture getAvg() {
+		final NormalDistributionMixture result = sum.clone();
+		final List<Pair<Double, MultivariateNormalDistribution>> mvns = new ArrayList<Pair<Double, MultivariateNormalDistribution>>();
+		for (final Pair<Double, MultivariateNormalDistribution> entry : sum.getMixtures().getComponents()) {
+			final MultivariateNormalDistribution normalDistribution = entry.getValue();
+			final Double weight = entry.getKey();
+			final double[] means = normalDistribution.getMeans();
+			for (int i = 0; i < means.length; i++) {
+				means[i] /= count;
+			}
+			final RealMatrix covariances = normalDistribution.getCovariances().scalarMultiply(1.0 / (count * count));
+			MultivariateNormalDistribution component = new MultivariateNormalDistribution(means, covariances.getData());
+			mvns.add(new Pair<Double, MultivariateNormalDistribution>(weight, component));
+		}
+		result.setMixtures(new MixtureMultivariateNormalDistribution(mvns));
+		final Interval[] support = new Interval[result.getSupport().length];
+		for (int i = 0; i < result.getSupport().length; i++) {
+			support[i] = result.getSupport(i).div(count);
+		}
+		result.setSupport(support);
+
+		return result;
+	}
+
+	/**
+	 * Add the given distribution to the aggregate.
+	 * 
+	 * @param value
+	 *            The value to add
+	 */
+	public final void add(final NormalDistributionMixture value) {
+		final List<Pair<Double, MultivariateNormalDistribution>> mixtures = new ArrayList<Pair<Double, MultivariateNormalDistribution>>();
+		for (final Pair<Double, MultivariateNormalDistribution> sumEntry : sum.getMixtures().getComponents()) {
+			final RealMatrix sumMean = MatrixUtils.createColumnRealMatrix(sumEntry.getValue().getMeans());
+			final RealMatrix sumCovarianceMatrix = sumEntry.getValue().getCovariances();
+
+			for (final Pair<Double, MultivariateNormalDistribution> entry : value.getMixtures().getComponents()) {
+				final RealMatrix mean = MatrixUtils.createColumnRealMatrix(entry.getValue().getMeans());
+				final RealMatrix covarianceMatrix = entry.getValue().getCovariances();
+
+				final MultivariateNormalDistribution distribution = new MultivariateNormalDistribution(sumMean.add(mean).getColumn(0), sumCovarianceMatrix.add(covarianceMatrix).getData());
+				mixtures.add(new Pair<Double, MultivariateNormalDistribution>(sumEntry.getKey() * entry.getKey(), distribution));
+			}
+		}
+
+		final NormalDistributionMixture result = new NormalDistributionMixture(mixtures);
+		final Interval[] support = new Interval[sum.getSupport().length];
+		for (int i = 0; i < sum.getSupport().length; i++) {
+			support[i] = sum.getSupport(i).add(value.getSupport(i));
+		}
+		result.setSupport(support);
+		result.setScale(sum.getScale() * value.getScale());
+		this.count++;
+		this.sum = result;
+	}
+
+	/*
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public final String toString() {
+		final StringBuffer ret = new StringBuffer("AvgPartialAggregate (").append(this.hashCode()).append(")").append(this.sum).append(this.count);
+		return ret.toString();
+	}
+
+	/*
+	 * 
+	 * @see java.lang.Object#clone()
+	 */
+	@Override
+	public final AvgPartialAggregate<T> clone() {
+		return new AvgPartialAggregate<T>(this);
+	}
+}
---
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/continuous/physicaloperator/aggregationfunctions/ProbabilisticContinuousAvg.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2013 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.probabilistic.continuous.physicaloperator.aggregationfunctions;
+
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.AbstractAggregateFunction;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IPartialAggregate;
+import de.uniol.inf.is.odysseus.probabilistic.base.ProbabilisticTuple;
+import de.uniol.inf.is.odysseus.probabilistic.continuous.datatype.NormalDistributionMixture;
+import de.uniol.inf.is.odysseus.probabilistic.continuous.datatype.ProbabilisticContinuousDouble;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class ProbabilisticContinuousAvg extends AbstractAggregateFunction<ProbabilisticTuple<?>, ProbabilisticTuple<?>> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6016082419401250119L;
+	/** The attribute position. */
+	private final int pos;
+	/** The result data type. */
+	private final String datatype;
+
+	/**
+	 * Gets an instance of {@link ProbabilisticContinuousAvg}.
+	 * 
+	 * @param pos
+	 *            The attribute position
+	 * @param partialAggregateInput
+	 *            The partial aggregate input
+	 * @param datatype
+	 *            The result datatype
+	 * @return An instance of {@link ProbabilisticContinuousAvg}
+	 */
+	public static ProbabilisticContinuousAvg getInstance(final int pos, final boolean partialAggregateInput, final String datatype) {
+		return new ProbabilisticContinuousAvg(pos, partialAggregateInput, datatype);
+	}
+
+	/**
+	 * Creates a new instance of {@link ProbabilisticContinuousAvg}.
+	 * 
+	 * @param pos
+	 *            The attribute position
+	 * @param partialAggregateInput
+	 *            The partial aggregate input
+	 * @param datatype
+	 *            The result datatype
+	 */
+	protected ProbabilisticContinuousAvg(final int pos, final boolean partialAggregateInput, final String datatype) {
+		super("AVG", partialAggregateInput);
+		this.pos = pos;
+		this.datatype = datatype;
+	}
+
+	/*
+	 * 
+	 * @see de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IInitializer#init(java.lang.Object)
+	 */
+	@Override
+	public final IPartialAggregate<ProbabilisticTuple<?>> init(final ProbabilisticTuple<?> in) {
+		final NormalDistributionMixture distribution = in.getDistribution(((ProbabilisticContinuousDouble) in.getAttribute(this.pos)).getDistribution());
+		final AvgPartialAggregate<ProbabilisticTuple<?>> pa = new AvgPartialAggregate<ProbabilisticTuple<?>>(distribution, datatype);
+		return pa;
+	}
+
+	/*
+	 * 
+	 * @see de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IMerger#merge(de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IPartialAggregate, java.lang.Object, boolean)
+	 */
+	@Override
+	public final IPartialAggregate<ProbabilisticTuple<?>> merge(final IPartialAggregate<ProbabilisticTuple<?>> p, final ProbabilisticTuple<?> toMerge, final boolean createNew) {
+		AvgPartialAggregate<ProbabilisticTuple<?>> pa = null;
+		if (createNew) {
+			pa = new AvgPartialAggregate<ProbabilisticTuple<?>>(((AvgPartialAggregate<ProbabilisticTuple<?>>) p).getSum(), datatype);
+		} else {
+			pa = (AvgPartialAggregate<ProbabilisticTuple<?>>) p;
+		}
+		final NormalDistributionMixture distribution = toMerge.getDistribution(((ProbabilisticContinuousDouble) toMerge.getAttribute(this.pos)).getDistribution());
+
+		pa.add(distribution);
+		return pa;
+	}
+
+	/*
+	 * 
+	 * @see de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IEvaluator#evaluate(de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IPartialAggregate)
+	 */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public final ProbabilisticTuple<?> evaluate(final IPartialAggregate<ProbabilisticTuple<?>> p) {
+		final AvgPartialAggregate<ProbabilisticTuple<?>> pa = (AvgPartialAggregate<ProbabilisticTuple<?>>) p;
+		final ProbabilisticTuple<?> r = new ProbabilisticTuple(1, 1, true);
+		r.setDistribution(0, pa.getAvg());
+		r.setAttribute(0, new ProbabilisticContinuousDouble(0));
+		return r;
+	}
+}
