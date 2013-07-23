f1f119b9c215405ac3d1a167af6c219c6ae44da2
Michael Brand <michael.brand@uni-oldenburg.de>
2013-07-23 09:17:56 +0000

    Created an exception of data fragmentation: the replication, which can be handled as any fragmentation strategies but it will not insert any operators for data fragmentation
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16031 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d7da5111c17ef54a27f612db0ad28d0c021a6b26

---
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new.loadbalancer/src/de/uniol/inf/is/odysseus/p2p_new/lb/fragmentation/Replication.java
@@ -0,0 +1,59 @@
+package de.uniol.inf.is.odysseus.p2p_new.lb.fragmentation;
+
+import java.util.Collection;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.distribution.IDataFragmentation;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.QueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.p2p_new.distribute.logicaloperator.ReplicationMergeAO;
+
+/**
+ * The class for data replication which is therefore an exception of data fragmentation strategies. <br />
+ * {@link #insertOperatorForDistribution(Collection, int, QueryBuildConfiguration)} will not insert any operators.
+ * @author Michael Brand
+ */
+public class Replication implements IDataFragmentation {
+
+	/**
+	 * @see #getName()
+	 */
+	public static final String NAME = "Replication";
+	
+	@Override
+	public String getName() {
+		
+		return Replication.NAME;
+		
+	}
+	
+	@Override
+	public Collection<ILogicalOperator> insertOperatorForDistribution(
+			Collection<ILogicalOperator> operators, int degreeOfParallelism,
+			QueryBuildConfiguration parameters) {
+		
+		return operators;
+		
+	}
+
+	@Override
+	public Class<? extends ILogicalOperator> getOperatorForDistributionClass() {
+		
+		return null;
+		
+	}
+
+	@Override
+	public ILogicalOperator createOperatorForJunction() {
+		
+		return new ReplicationMergeAO();
+		
+	}
+
+	@Override
+	public Class<? extends ILogicalOperator> getOperatorForJunctionClass() {
+		
+		return ReplicationMergeAO.class;
+		
+	}
+
+}
