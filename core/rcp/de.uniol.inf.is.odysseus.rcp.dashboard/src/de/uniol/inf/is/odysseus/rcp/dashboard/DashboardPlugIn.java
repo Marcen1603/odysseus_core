e9bf6e56033c0891ee154b00603a9934838b72da
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-07-24 09:53:31 +0000

    Dashboard: Showing single dashboard parts as views possible
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16052 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 450060192bde895a16442e358852f00fd5606a13

--- core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/DashboardPlugIn.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/DashboardPlugIn.java
@@ -34,6 +34,8 @@ public class DashboardPlugIn extends AbstractUIPlugin {
 	public static final String EXTENSION_POINT_ID = "de.uniol.inf.is.odysseus.rcp.DashboardPart";
 	public static final String DASHBOARD_PART_EXTENSION = "prt";
 	public static final String DASHBOARD_EXTENSION = "dsh";
+	
+	public static final String DASHBOARD_PART_VIEW_ID = "de.uniol.inf.is.odysseus.rcp.views.DashboardPartView";
 
 	public static final String ADD_DASHBOARD_PART_COMMAND_ID = "de.uniol.inf.is.odysseus.rcp.commands.AddDashboardPart";
 
---
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/views/DashboardPartView.java
@@ -0,0 +1,84 @@
+package de.uniol.inf.is.odysseus.rcp.dashboard.views;
+
+import java.io.FileNotFoundException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.ui.part.ViewPart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.rcp.dashboard.DashboardHandlerException;
+import de.uniol.inf.is.odysseus.rcp.dashboard.IDashboardPart;
+import de.uniol.inf.is.odysseus.rcp.dashboard.IDashboardPartHandler;
+import de.uniol.inf.is.odysseus.rcp.dashboard.controller.ControllerException;
+import de.uniol.inf.is.odysseus.rcp.dashboard.controller.DashboardPartController;
+import de.uniol.inf.is.odysseus.rcp.dashboard.handler.XMLDashboardPartHandler;
+import de.uniol.inf.is.odysseus.rcp.dashboard.util.FileUtil;
+
+public class DashboardPartView extends ViewPart {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DashboardPartView.class);
+	private static final IDashboardPartHandler DASHBOARD_PART_HANDLER = new XMLDashboardPartHandler();
+	
+	private Composite parent;
+
+	private IDashboardPart dashboardPart;
+	private DashboardPartController dashboardPartController;
+	
+	private boolean isShowing;
+	
+	@Override
+	public void createPartControl(Composite parent) {
+		parent.setLayout(new GridLayout());
+
+		this.parent = parent;
+	}
+
+	@Override
+	public void setFocus() {
+		
+	}
+
+	public void showDashboardPart(IFile dashboardPartFile) {
+		if( isShowing ) {
+			return;
+		}
+		
+		final Composite comp = new Composite(parent, SWT.NONE);
+		comp.setLayoutData(new GridData(GridData.FILL_BOTH));
+		comp.setLayout(new GridLayout());
+		
+		final ToolBar toolBar = new ToolBar(parent, SWT.WRAP | SWT.RIGHT);
+		
+		try {
+			dashboardPart = DASHBOARD_PART_HANDLER.load(FileUtil.read(dashboardPartFile));
+			dashboardPartController = new DashboardPartController(dashboardPart);
+
+			dashboardPart.createPartControl(comp, toolBar);
+			dashboardPartController.start();
+			
+			parent.layout();
+			
+			isShowing = true;
+			setPartName(dashboardPartFile.getName());
+			
+		} catch (FileNotFoundException | DashboardHandlerException | CoreException | ControllerException e) {
+			LOG.error("Could not load and start dashboard part", e);
+		}
+	}
+	
+	@Override
+	public void dispose() {
+		if( dashboardPartController.isStarted() ) {
+			dashboardPartController.stop();
+		}
+		
+		super.dispose();
+	}
+}
