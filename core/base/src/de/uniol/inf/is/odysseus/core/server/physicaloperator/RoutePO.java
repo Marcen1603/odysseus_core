1d2a6b164398d72bfe2ed94c163a965983e501a7
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-07-26 09:18:54 +0000

    Clone objects in case of overlappingPredicates
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16088 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e74ab825aa4a2272a6eed00696d3c56ed05b3f30

--- core/base/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/RoutePO.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/RoutePO.java
@@ -1,23 +1,25 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.core.server.physicaloperator;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import de.uniol.inf.is.odysseus.core.IClone;
+import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
 import de.uniol.inf.is.odysseus.core.physicaloperator.OpenFailedException;
@@ -26,13 +28,14 @@ import de.uniol.inf.is.odysseus.core.predicate.IPredicate;
 /**
  * @author Marco Grawunder
  */
-@SuppressWarnings({"rawtypes"})
-public class RoutePO<T extends IStreamObject<?>> extends AbstractPipe<T, T> {
+@SuppressWarnings({ "rawtypes" })
+public class RoutePO<T extends IStreamObject<IMetaAttribute>> extends AbstractPipe<T, T> {
 
 	private List<IPredicate<? super T>> predicates;
 	final boolean overlappingPredicates;
 
-	public RoutePO(List<IPredicate<? super T>> predicates, boolean overlappingPredicates)  {
+	public RoutePO(List<IPredicate<? super T>> predicates,
+			boolean overlappingPredicates) {
 		super();
 		this.overlappingPredicates = overlappingPredicates;
 		initPredicates(predicates);
@@ -43,15 +46,14 @@ public class RoutePO<T extends IStreamObject<?>> extends AbstractPipe<T, T> {
 		this.overlappingPredicates = splitPO.overlappingPredicates;
 		initPredicates(splitPO.predicates);
 	}
-	
+
 	private void initPredicates(List<IPredicate<? super T>> predicates) {
-		this.predicates = new ArrayList<IPredicate<? super T>>(predicates.size());
-		for (IPredicate<? super T> p: predicates){
+		this.predicates = new ArrayList<IPredicate<? super T>>(
+				predicates.size());
+		for (IPredicate<? super T> p : predicates) {
 			this.predicates.add(p.clone());
 		}
 	}
-	
-
 
 	@Override
 	public OutputMode getOutputMode() {
@@ -59,45 +61,59 @@ public class RoutePO<T extends IStreamObject<?>> extends AbstractPipe<T, T> {
 	}
 
 	@Override
-	public void process_open() throws OpenFailedException{
+	public void process_open() throws OpenFailedException {
 		super.process_open();
-		for (IPredicate<? super T> p: predicates){
+		for (IPredicate<? super T> p : predicates) {
 			p.init();
-		}		
+		}
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void process_next(T object, int port) {
-		for (int i=0;i<predicates.size();i++){
+		boolean found = false;
+		for (int i = 0; i < predicates.size(); i++) {
 			if (predicates.get(i).evaluate(object)) {
-				transfer(object,i);
-				return;
+				T out = object;
+				// If object is send to multiple output ports
+				// it MUST be cloned!
+				if (overlappingPredicates) {
+					out = (T)object.clone();
+					out.setMetadata((IMetaAttribute)object.getMetadata().clone());
+				}
+				transfer(object, i);
+				found = true;
+				if (!overlappingPredicates) {
+					return;
+				}
 			}
 		}
-		transfer(object,predicates.size());
+		if (!found) {
+			transfer(object, predicates.size());
+		}
 	}
-	
+
 	@Override
 	public RoutePO<T> clone() {
 		return new RoutePO<T>(this);
 	}
-	
+
 	@Override
 	public boolean process_isSemanticallyEqual(IPhysicalOperator ipo) {
-		if(!(ipo instanceof RoutePO)) {
+		if (!(ipo instanceof RoutePO)) {
 			return false;
 		}
 		RoutePO spo = (RoutePO) ipo;
-		if(this.hasSameSources(spo) &&
-				this.predicates.size() == spo.predicates.size()) {
-			for(int i = 0; i<this.predicates.size(); i++) {
-				if(!this.predicates.get(i).equals(spo.predicates.get(i))) {
+		if (this.hasSameSources(spo)
+				&& this.predicates.size() == spo.predicates.size()) {
+			for (int i = 0; i < this.predicates.size(); i++) {
+				if (!this.predicates.get(i).equals(spo.predicates.get(i))) {
 					return false;
 				}
 			}
 			return true;
 		}
-        return false;
+		return false;
 	}
 
 }
