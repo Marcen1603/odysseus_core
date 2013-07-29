ed42562b7c2f797e7ba6b5d82ddb8b7b2a5f1968
Michael Brand <michael.brand@uni-oldenburg.de>
2013-07-29 08:34:31 +0000

    Changed validation of the FragmentationTypePreParserKeyword
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16111 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9145e4a92e2e9fcacb6559e808768dafe8188d76

--- core/script.exector/src/de/uniol/inf/is/odysseus/script/keyword/FragmentationTypePreParserKeyword.java
+++ core/script.exector/src/de/uniol/inf/is/odysseus/script/keyword/FragmentationTypePreParserKeyword.java
@@ -1,6 +1,5 @@
 package de.uniol.inf.is.odysseus.script.keyword;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +15,7 @@ import de.uniol.inf.is.odysseus.script.parser.OdysseusScriptException;
 /**
  * This class represents the keyword for the data fragmentation strategies. <br />
  * In Odysseus Script it can be used as follows: <br />
- * #DATAFRAGMENTATIONTYPE <name>
+ * #DATAFRAGMENTATIONTYPE name source additionalParameters; name source additionalParameters; ...
  * @author Michael Brand
  */
 public class FragmentationTypePreParserKeyword extends AbstractPreParserKeyword {
@@ -25,17 +24,37 @@ public class FragmentationTypePreParserKeyword extends AbstractPreParserKeyword 
 	 * The string representation of the keyword.
 	 */
 	public static final String KEYWORD = "DATAFRAGMENTATIONTYPE";
+	
+	/**
+	 * The separator between different fragmentation strategies for different sources
+	 */
+	public static final String OUTER_SEP = ";";
+	
+	/**
+	 * The separator between fragmentation strategy, source name and other parameters for the fragmentation strategy
+	 */
+	public static final String INNER_SEP = " ";
 
 	@Override
 	public void validate(Map<String, Object> variables, String parameter,
 			ISession caller) throws OdysseusScriptException {
 		
-		String[] parameters = parameter.split(" ");
-		String strategyName = parameters[0];
+		String[] strategies = parameter.split(FragmentationTypePreParserKeyword.OUTER_SEP);
+		if(strategies == null || strategies.length == 0)
+			throw new OdysseusScriptException("No data fragmentation strategy defined.");
 		
-		if(!ExecutorHandler.getServerExecutor().getDataFragmentationNames().contains(strategyName))
-			throw new OdysseusScriptException("Specified data fragmentation strategy '" + strategyName + 
-					"' not found.");
+		for(String strategy : strategies) {
+			
+			String[] parameters = strategy.split(INNER_SEP);
+			if(parameters == null || parameters.length == 0)
+				throw new OdysseusScriptException("At least one data fragmentation strategy is not defined.");
+			
+			String strategyName = parameters[0];
+			if(strategyName == null || !ExecutorHandler.getServerExecutor().getDataFragmentationNames().contains(strategyName))
+				throw new OdysseusScriptException("Specified data fragmentation strategy '" + strategyName + 
+						"' not found.");
+			
+		}
 		
 	}
 
@@ -49,13 +68,6 @@ public class FragmentationTypePreParserKeyword extends AbstractPreParserKeyword 
 			addSettings.add(new ParameterFragmentationType(parameter));
 		
 		return null;
-	}
-	
-	@Override
-	public Collection<String> getAllowedParameters(ISession caller) {
-		
-		return ExecutorHandler.getServerExecutor().getDataFragmentationNames();		
-		
 	}
 
 }
