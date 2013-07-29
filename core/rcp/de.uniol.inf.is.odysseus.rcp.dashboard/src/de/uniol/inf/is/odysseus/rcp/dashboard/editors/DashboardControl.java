8138c77a40798ae507560ae1d83e2085c5c0b916
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-07-29 09:25:14 +0000

    Dashboard: CleanUp; Forgot drawing borders
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16115 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b6fc6efd1181e454bc816b5e55283ba61b6c9259

--- core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/editors/DashboardControl.java
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/editors/DashboardControl.java
@@ -28,41 +28,55 @@ public class DashboardControl {
 	public DashboardControl( Composite parent ) {
 		Preconditions.checkNotNull(parent, "Parent for dashboard control must not be null!");
 		
-		composite = new Composite(parent, SWT.NONE);
+		composite = new Composite(parent, SWT.BORDER);
 		composite.setLayout(new FormLayout());
 		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
 		composite.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
 	}
 	
 	public void setBackgroundImageFile( IFile bgFile, boolean stretched ) {
-		try {
-			if( loadedBackgroundImageFile != bgFile || stretched != isLoadedBackgroundImageStretched ) {
-				
-				disposeBackgroundImage();
-				
-				if( bgFile != null ) {
-					Image image = new Image(Display.getCurrent(), bgFile.getContents());
-		
-					if (stretched) {
-						Image stretchedImage = ImageUtil.resizeImage(image, composite.getSize().x, composite.getSize().x);
-						image.dispose();
-						image = stretchedImage;
-					}
-					composite.setBackgroundImage(image);
-					
-					isLoadedBackgroundImageStretched = stretched;
-					loadedBackgroundImageFile = bgFile;
-					loadedBackgroundImage = image;
-				} else {
-					composite.setBackgroundImage(null);
-					
-					loadedBackgroundImageFile = null;
-					loadedBackgroundImage = null;
-					isLoadedBackgroundImageStretched = false;
-				}
+		if (areImageSettingsChanged(bgFile, stretched)) {
+
+			disposeBackgroundImage();
+
+			if (bgFile != null) {
+				setNewBackgroundImage(bgFile, stretched);
+			} else {
+				clearBackgroundImage();
 			}
+		}
+	}
+
+	private boolean areImageSettingsChanged(IFile bgFile, boolean stretched) {
+		return loadedBackgroundImageFile != bgFile || stretched != isLoadedBackgroundImageStretched;
+	}
+
+	private void clearBackgroundImage() {
+		composite.setBackgroundImage(null);
+		
+		loadedBackgroundImageFile = null;
+		loadedBackgroundImage = null;
+		isLoadedBackgroundImageStretched = false;
+	}
+
+	private void setNewBackgroundImage(IFile bgFile, boolean stretched) {
+		try {
+			Image image = new Image(Display.getCurrent(), bgFile.getContents());
+	
+			if (stretched) {
+				Image stretchedImage = ImageUtil.resizeImage(image, composite.getSize().x, composite.getSize().x);
+				image.dispose();
+				image = stretchedImage;
+			}
+			composite.setBackgroundImage(image);
+			
+			isLoadedBackgroundImageStretched = stretched;
+			loadedBackgroundImageFile = bgFile;
+			loadedBackgroundImage = image;
 		} catch( CoreException ex ) {
 			LOG.error("Could not set background image", ex);
+			
+			clearBackgroundImage();
 		}
 	}
 	
