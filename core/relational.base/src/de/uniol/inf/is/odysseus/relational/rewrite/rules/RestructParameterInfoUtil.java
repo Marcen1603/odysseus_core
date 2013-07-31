33bb2e80b4a3d4b44b105b7e89b728507ad656ef
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-07-31 13:33:56 +0000

    Bugfix in setting predicate parameter info
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16173 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e08374de9189eb90e6bf371f4d95714313895f32

--- core/relational.base/src/de/uniol/inf/is/odysseus/relational/rewrite/rules/RestructParameterInfoUtil.java
+++ core/relational.base/src/de/uniol/inf/is/odysseus/relational/rewrite/rules/RestructParameterInfoUtil.java
@@ -10,9 +10,13 @@ public final class RestructParameterInfoUtil {
 	// TODO: Make indipendent from ILogicalOperator (use ParameterInfo-Map directly)
 	public static void updatePredicateParameterInfo(ILogicalOperator sel) {
 		String predicateString = sel.getParameterInfos().get("PREDICATE");
-		int pos = predicateString.indexOf("(");
-		String newPredicateString = predicateString.substring(0, pos + 1) + "'" + sel.getPredicate().toString() + "')";
-		sel.addParameterInfo("PREDICATE", newPredicateString);
+		if( predicateString != null && predicateString.length() > 0 ) {
+			int pos = predicateString.indexOf("(");
+			String newPredicateString = predicateString.substring(0, pos + 1) + "'" + sel.getPredicate().toString() + "')";
+			sel.addParameterInfo("PREDICATE", newPredicateString);
+		} else {
+			sel.addParameterInfo("PREDICATE", "RelationalPredicate('" + sel.getPredicate().toString() + "')");
+		}
 	}
 
 }
