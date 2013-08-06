0e5d033aaa8dea8b2184fc9870dbc3c113f34373
Dennis Geesen <geesen@indivon.de>
2013-08-06 13:26:40 +0000

    removed blocking from server product
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16275 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f8141aaeb46908d822458b6862f0ea1dc3bef1fe

--- product/de.uniol.inf.is.odysseus.product.server.starter/src/de/uniol/inf/is/odysseus/product/server/starter/OdysseusServerApplication.java
+++ product/de.uniol.inf.is.odysseus.product.server.starter/src/de/uniol/inf/is/odysseus/product/server/starter/OdysseusServerApplication.java
@@ -2,53 +2,20 @@ package de.uniol.inf.is.odysseus.product.server.starter;
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.osgi.framework.BundleContext;
 
 public class OdysseusServerApplication implements IApplication {
 
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
-		System.out.println("Starting Odysseus application");
-		startBundles(context.getBrandingBundle().getBundleContext());		
-		synchronized (this) {
-			this.wait();
-		}
-		System.out.println("Odysseus terminated");
+		// application always terminates successfully
+		// the products needs a "-noExit" in the launch configuration to keep alive after this application shuts down
 		return IApplication.EXIT_OK;
 	}
 
 	@Override
 	public void stop() {
-		synchronized (this) {
-			notifyAll();
-		}
+		// nothing to do
 	}
 	
-	
-	private static void startBundles(final BundleContext context) {
-//		Thread t = new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				try {
-//					Thread.sleep(100);
-//				} catch (InterruptedException e1) {
-//					e1.printStackTrace();
-//				}
-//				for (Bundle bundle : context.getBundles()) {
-//					boolean isFragment = bundle.getHeaders().get(
-//							Constants.FRAGMENT_HOST) != null;
-//					if (bundle != context.getBundle() && !isFragment
-//							&& bundle.getState() == Bundle.RESOLVED) {
-//						try {
-//							bundle.start();
-//						} catch (Exception e) {
-//							e.printStackTrace();
-//						}
-//					}
-//				}
-//			}
-//		});
-//		t.start();
-	}
 
 }
