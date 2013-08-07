cb6fd1531a1d4f82fb1ae0e8df1a217e8688e06f
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-08-07 09:16:43 +0000

    P2P: Changed access-behaviour for getting ISessionManagement-Instance
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16290 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7a745363aeb0743b80886c3823eaece581059556

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/service/SessionManagementService.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/service/SessionManagementService.java
@@ -1,38 +1,15 @@
 package de.uniol.inf.is.odysseus.p2p_new.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import de.uniol.inf.is.odysseus.core.server.usermanagement.ISessionManagement;
+import de.uniol.inf.is.odysseus.core.server.usermanagement.UserManagement;
 import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
 
 public class SessionManagementService {
-
-	private static final Logger LOG = LoggerFactory.getLogger(SessionManagementService.class);
-	private static ISessionManagement sessionManagement;
-	private static ISession activeSession;
-
-	public final void bindSessionManagement(ISessionManagement sm) {
-		sessionManagement = sm;
-		activeSession = sessionManagement.loginSuperUser(null, "");
-
-		LOG.debug("Bound Session Management {}", sm);
-	}
-
-	public final void unbindSessionManagement(ISessionManagement sm) {
-		if (sm == sessionManagement) {
-			sessionManagement = null;
-			activeSession = null;
-
-			LOG.debug("Unbound Session Management");
-		}
-	}
-
 	public static ISession getActiveSession() {
-		return activeSession;
+		return getSessionManagement().loginSuperUser(null, "");
 	}
 
 	public static ISessionManagement getSessionManagement() {
-		return sessionManagement;
+		return UserManagement.getSessionmanagement();
 	}
 }
