ad9c9ed4d3e0d7f299612295b0dacf70fd3d9ece
Dennis Geesen <geesen@indivon.de>
2013-08-07 09:08:52 +0000

    new studio-starter project
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16285 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4a3a781c675e1b5921b695efa8fe9c27f256dae5

---
+++ product/de.uniol.inf.is.odysseus.product.studio.starter/src/de/uniol/inf/is/odysseus/product/studio/starter/OdysseusStudioApplication.java
@@ -0,0 +1,134 @@
+/********************************************************************************** 
+  * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.product.studio.starter;
+
+import java.io.File;
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.jface.window.Window;
+import org.eclipse.osgi.service.datalocation.Location;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.ide.ChooseWorkspaceData;
+import org.eclipse.ui.internal.misc.Policy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class controls all aspects of the application's execution
+ */
+@SuppressWarnings("restriction")
+public class OdysseusStudioApplication implements IApplication {
+
+	private static Logger LOG = LoggerFactory.getLogger(OdysseusStudioApplication.class);	
+	private static final String DEBUG_SWT_SYS_PROPERTY = "debug.swt";
+	
+	@Override
+	public synchronized Object start(IApplicationContext context) {
+
+		Display display;
+		String doDebug = System.getProperty(DEBUG_SWT_SYS_PROPERTY);
+		if( doDebug != null && doDebug.equalsIgnoreCase("true")) {
+			Policy.DEBUG_SWT_GRAPHICS = true;
+			display = PlatformUI.createDisplay();
+			Sleak sleak = new Sleak();
+		    sleak.open();
+		} else {
+			display = PlatformUI.createDisplay();
+		}
+	    
+		try {
+			
+			if( !chooseWorkspace(display) ) {
+				return IApplication.EXIT_OK;
+			}
+
+			
+
+			return PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor()) 
+					== PlatformUI.RETURN_RESTART ? IApplication.EXIT_RESTART : IApplication.EXIT_OK; 
+		} catch (Throwable t) {
+			LOG.error("Exception during running application", t);
+			return null;
+		} finally {
+			display.dispose();
+		}
+	}
+
+	@Override
+	public void stop() {
+		final IWorkbench workbench = PlatformUI.getWorkbench();
+		if (workbench == null)
+			return;
+		final Display display = workbench.getDisplay();
+		display.syncExec(new Runnable() {
+			@Override
+			public void run() {
+				if (!display.isDisposed())
+					workbench.close();
+			}
+		});
+	}	
+
+	private static boolean chooseWorkspace(Display display) {
+		try {
+			File file = new File(System.getProperty("user.home"), "workspace");
+			String path = file.getAbsolutePath().replace(File.separatorChar, '/');
+			
+			URL url = new URL("file", null, path); 
+			ChooseWorkspaceData data = new ChooseWorkspaceData(url);
+
+			ChooseWorkspaceDialogExtended dialog = new ChooseWorkspaceDialogExtended(display.getActiveShell(), data, false, true);
+			dialog.prompt(false);
+			
+			
+			// in case that the workspace was automatically selected
+			if( data.getSelection() != null && !Platform.getInstanceLocation().isSet() ) {
+				if( !releaseAndSetLocation(data.getSelection()) ) {
+					
+					// force showing dialog
+					dialog = new ChooseWorkspaceDialogExtended(display.getActiveShell(), data, false, true);
+//					dialog.setErrorMessage("Could not set workspace.\nPlease choose a different one.");
+					dialog.prompt(true);
+				}
+			}
+			
+			return dialog.getReturnCode() == Window.OK;
+			
+		} catch (Exception e) {
+			LOG.error("Exception during choosing workspace", e);
+			return false;
+		}
+	}
+	
+	private static boolean releaseAndSetLocation(String selection) {
+		try {
+			Location instanceLoc = Platform.getInstanceLocation();
+			if (instanceLoc.isSet()) {
+				instanceLoc.release();
+			}
+	
+			return instanceLoc.set(new URL("file", null, selection), true);
+		} catch( Exception ex ) {
+			LOG.error("Could not release and set location", ex);
+			return false;
+		}
+	}	
+}
