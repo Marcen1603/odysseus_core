e7fed80d6fb0c05228fc6d4478c09efe8215811a
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-08-19 18:41:17 +0000

    Dashboard: Added keyed tables (used for nrel, soccer)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16411 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: da4cf98c51118c7c5444cb97049eab3198468ed2

---
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.dashboard.part/src/de/uniol/inf/is/odysseus/rcp/dashboard/part/KeyedTableConfigurer.java
@@ -0,0 +1,104 @@
+package de.uniol.inf.is.odysseus.rcp.dashboard.part;
+
+import java.util.Collection;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
+import de.uniol.inf.is.odysseus.rcp.dashboard.AbstractDashboardPartConfigurer;
+
+public class KeyedTableConfigurer extends AbstractDashboardPartConfigurer<KeyedTableDashboardPart> {
+
+	private KeyedTableDashboardPart dashboardPart;
+
+	@Override
+	public void init(KeyedTableDashboardPart dashboardPartToConfigure, Collection<IPhysicalOperator> roots) {
+		this.dashboardPart = dashboardPartToConfigure;
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		Composite topComposite = new Composite(parent, SWT.NONE);
+		topComposite.setLayout(new GridLayout(2, false));
+		topComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		createAttributesControls(topComposite);
+		createKeyAttribbutesControls(topComposite);
+		createMaxDataControls(topComposite);
+		createTitleControls(topComposite);
+
+	}
+
+	private void createKeyAttribbutesControls(Composite topComposite) {
+		createLabel(topComposite, "Key attribute");
+		final Text attributesInput = createText(topComposite, dashboardPart.getKeyAttribute());
+		attributesInput.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setKeyAttribute(attributesInput.getText());
+				fireListener();
+			}
+		});
+	}
+
+	private void createTitleControls(Composite topComposite) {
+		createLabel(topComposite, "Title");
+		final Text titleText = createText(topComposite, dashboardPart.getTitle());
+		titleText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setTitle(titleText.getText());
+				fireListener();
+			}
+		});
+	}
+
+	private void createMaxDataControls(Composite topComposite) {
+		createLabel(topComposite, "Max Data Count");
+		final Text maxDataText = createText(topComposite, String.valueOf(dashboardPart.getMaxData()));
+		maxDataText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setMaxData(Integer.valueOf(maxDataText.getText()));
+				fireListener();
+			}
+		});
+	}
+
+	private void createAttributesControls(Composite topComposite) {
+		createLabel(topComposite, "Attributes");
+		final Text attributesInput = createText(topComposite, dashboardPart.getAttributeList());
+		attributesInput.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setAttributeList(attributesInput.getText());
+				fireListener();
+			}
+		});
+	}
+
+	private static Text createText(Composite topComposite, String txt) {
+		Text text = new Text(topComposite, SWT.BORDER);
+		text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		text.setText(txt != null ? txt : "");
+		return text;
+	}
+
+	private static void createLabel(Composite parent, String text) {
+		Label label = new Label(parent, SWT.NONE);
+		label.setText(text);
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+}
