02d90225367d77e5a09ff9780db5e070621c38bc
Dennis Geesen <geesen@indivon.de>
2013-08-19 12:23:59 +0000

    ODY-398 fixed, if a resource is renamed, the editor is closed to avoid phantom-editors
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16407 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d2eb41d04813df698a90d3ea9fb4a97b6d43ed60

--- core/rcp/rcp.editor.text/src/de/uniol/inf/is/odysseus/rcp/editor/text/editors/OdysseusScriptEditor.java
+++ core/rcp/rcp.editor.text/src/de/uniol/inf/is/odysseus/rcp/editor/text/editors/OdysseusScriptEditor.java
@@ -15,6 +15,14 @@
  */
 package de.uniol.inf.is.odysseus.rcp.editor.text.editors;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.IPreferenceStore;
@@ -23,13 +31,18 @@ import org.eclipse.jface.text.source.ICharacterPairMatcher;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.viewers.IPostSelectionProvider;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.ISaveablePart2;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
 import org.eclipse.ui.texteditor.SourceViewerDecorationSupport;
 
 import de.uniol.inf.is.odysseus.rcp.editor.text.editors.coloring.OdysseusOccurrencesUpdater;
 
-public class OdysseusScriptEditor extends AbstractDecoratedTextEditor implements ISaveablePart2 {
+public class OdysseusScriptEditor extends AbstractDecoratedTextEditor implements ISaveablePart2, IResourceChangeListener, IResourceDeltaVisitor {
 
 	public final static String EDITOR_MATCHING_BRACKETS = "matchingBrackets";
 	public final static String EDITOR_MATCHING_BRACKETS_COLOR = "matchingBracketsColor";
@@ -44,8 +57,9 @@ public class OdysseusScriptEditor extends AbstractDecoratedTextEditor implements
 
 	protected void internal_init() {
 		configureInsertMode(SMART_INSERT, false);
-		setDocumentProvider(new OdysseusScriptDocumentProvider());
+		setDocumentProvider(new OdysseusScriptDocumentProvider());		
 		setSourceViewerConfiguration(new OdysseusScriptViewerConfiguration(this));
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
 	}
 
 	@Override
@@ -65,30 +79,82 @@ public class OdysseusScriptEditor extends AbstractDecoratedTextEditor implements
 	}
 
 	@Override
-	protected void configureSourceViewerDecorationSupport(
-			SourceViewerDecorationSupport support) {
+	protected void configureSourceViewerDecorationSupport(SourceViewerDecorationSupport support) {
 		super.configureSourceViewerDecorationSupport(support);
 		char[] matchChars = { '(', ')', '[', ']', '{', '}' };
-		ICharacterPairMatcher matcher = new DefaultCharacterPairMatcher(
-				matchChars);
+		ICharacterPairMatcher matcher = new DefaultCharacterPairMatcher(matchChars);
 		support.setCharacterPairMatcher(matcher);
-		support.setMatchingCharacterPainterPreferenceKeys(
-				EDITOR_MATCHING_BRACKETS, EDITOR_MATCHING_BRACKETS_COLOR);
+		support.setMatchingCharacterPainterPreferenceKeys(EDITOR_MATCHING_BRACKETS, EDITOR_MATCHING_BRACKETS_COLOR);
 		IPreferenceStore store = getPreferenceStore();
 		store.setDefault("matchingBrackets", true);
 		store.setDefault("matchingBracketsColor", "128,128,128");
 	}
 
 	@Override
+	public void dispose() {
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);		
+		super.dispose();
+	}
+
+	@Override
 	public int promptToSaveOnClose() {
-		String[] buttons = new String[]{IDialogConstants.YES_LABEL, IDialogConstants.NO_LABEL, IDialogConstants.CANCEL_LABEL};
-		MessageDialog dialog = new MessageDialog(getEditorSite().getShell(), "Save changes", null,  "Save the changes?", MessageDialog.QUESTION,buttons,0);
+		String[] buttons = new String[] { IDialogConstants.YES_LABEL, IDialogConstants.NO_LABEL, IDialogConstants.CANCEL_LABEL };
+		MessageDialog dialog = new MessageDialog(getEditorSite().getShell(), "Save changes", null, "Save the changes?", MessageDialog.QUESTION, buttons, 0);
 		final int dialogResult = dialog.open();
-		if(dialogResult == 0){
+		if (dialogResult == 0) {
 			return ISaveablePart2.YES;
-		}else if(dialogResult == 1){
+		} else if (dialogResult == 1) {
 			return ISaveablePart2.NO;
 		}
 		return ISaveablePart2.CANCEL;
 	}
+
+	public void resourceChanged(final IResourceChangeEvent event) {
+		if (event.getType() == IResourceChangeEvent.POST_CHANGE) {
+			IResourceDelta delta = event.getDelta();
+			try {
+				delta.accept(this);
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		if (event.getType() == IResourceChangeEvent.PRE_CLOSE) {
+			closeEditor(event.getResource());
+		}
+	}
+
+	@Override
+	public boolean visit(IResourceDelta delta) throws CoreException {
+		IResource resource = delta.getResource();
+		if (resource instanceof IFile) {
+			IFile file = (IFile) resource;
+			if (file.equals(((IFileEditorInput) getEditorInput()).getFile())) {
+				switch (delta.getKind()) {
+				case IResourceDelta.REMOVED:
+				case IResourceDelta.REPLACED:
+					closeEditor(delta.getResource());
+					break;
+				default:
+					break;
+				}
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void closeEditor(final IResource resource) {
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchPage[] pages = getSite().getWorkbenchWindow().getPages();
+				for (int i = 0; i < pages.length; i++) {
+					if (((FileEditorInput) getEditorInput()).getFile().equals(resource)) {
+						IEditorPart editorPart = pages[i].findEditor(getEditorInput());
+						pages[i].closeEditor(editorPart, true);
+					}
+				}
+			}
+		});
+	}
+	
 }
