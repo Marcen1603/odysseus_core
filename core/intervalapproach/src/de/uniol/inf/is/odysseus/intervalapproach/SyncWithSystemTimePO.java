e3de7e582492bd933b918501e89a1a85dab49b3b
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-08-20 09:01:58 +0000

    Missing synchronized
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16419 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8d0da3c4bde11f2dfc8a26ed3a844745a82d9ba6

--- core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/SyncWithSystemTimePO.java
+++ core/intervalapproach/src/de/uniol/inf/is/odysseus/intervalapproach/SyncWithSystemTimePO.java
@@ -35,7 +35,7 @@ public class SyncWithSystemTimePO<R extends IStreamObject<? extends ITimeInterva
 	}
 
 	@Override
-	protected void process_close() {
+	protected synchronized void process_close() {
 		this.terminate = true;
 		this.notifyAll();
 	}
