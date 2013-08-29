85c7a862ca14cf74b22d74ee328aa26336d3f0b4
Christian Kuka <christian@kuka.cc>
2013-08-29 18:08:57 +0000

    Probabilistic: Add Bhattacharyya distance function
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16490 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 465f059e8d5d4b4f6dccbd14303a418ee6ef9997

---
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/continuous/functions/math/AbstractBhattacharyyaDistanceFunction.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright 2013 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.probabilistic.continuous.functions.math;
+
+import org.apache.commons.math3.distribution.MultivariateNormalDistribution;
+import org.apache.commons.math3.linear.CholeskyDecomposition;
+import org.apache.commons.math3.linear.DecompositionSolver;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.NonPositiveDefiniteMatrixException;
+import org.apache.commons.math3.linear.NonSymmetricMatrixException;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.util.FastMath;
+import org.apache.commons.math3.util.Pair;
+
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.probabilistic.continuous.datatype.NormalDistributionMixture;
+import de.uniol.inf.is.odysseus.probabilistic.functions.AbstractProbabilisticFunction;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public abstract class AbstractBhattacharyyaDistanceFunction extends AbstractProbabilisticFunction<Double> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4978963210815853709L;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final int getArity() {
+		return 2;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final String getSymbol() {
+		return "similarity";
+	}
+
+	/**
+	 * Integrates the given distribution from the lower to the upper bound.
+	 * 
+	 * @param a
+	 *            The distribution
+	 * @param b
+	 *            The distribution
+	 * @return The cumulative probability in the given bound
+	 */
+	protected final double getValueInternal(final NormalDistributionMixture a, final NormalDistributionMixture b) {
+		double weightedBhattacharyyaDistance = 0.0;
+		for (final Pair<Double, MultivariateNormalDistribution> aEntry : a.getMixtures().getComponents()) {
+			final RealMatrix aMean = MatrixUtils.createColumnRealMatrix(aEntry.getValue().getMeans());
+			final RealMatrix aCovariance = aEntry.getValue().getCovariances();
+			final double aDeterminant = getDeterminant(aCovariance);
+			for (final Pair<Double, MultivariateNormalDistribution> bEntry : b.getMixtures().getComponents()) {
+				final RealMatrix bMean = MatrixUtils.createColumnRealMatrix(bEntry.getValue().getMeans());
+				final RealMatrix bCovariance = bEntry.getValue().getCovariances();
+				final double bDeterminant = getDeterminant(aCovariance);
+
+				RealMatrix avgCovariance = aCovariance.add(bCovariance).scalarMultiply(0.5);
+				final double avgDeterminant = getDeterminant(avgCovariance);
+				DecompositionSolver solver;
+				try {
+					solver = new CholeskyDecomposition(avgCovariance).getSolver();
+				} catch (NonSymmetricMatrixException | NonPositiveDefiniteMatrixException e) {
+					solver = new LUDecomposition(avgCovariance).getSolver();
+				}
+				RealMatrix avgCovarianceInverse = solver.getInverse();
+
+				RealMatrix bhattacharyyaDistanceTerm1 = aMean.subtract(bMean).transpose().multiply(avgCovarianceInverse).multiply(aMean.subtract(bMean)).scalarMultiply(1.0 / 8.0);
+				double bhattacharyyaDistanceTerm2 = 0.5 * FastMath.log(avgDeterminant / FastMath.sqrt(aDeterminant * bDeterminant));
+				RealMatrix bhattacharyyaDistance = bhattacharyyaDistanceTerm1.scalarAdd(bhattacharyyaDistanceTerm2);
+				// FIXME IS this correct?
+				double weight = aEntry.getKey() * bEntry.getKey();
+				weightedBhattacharyyaDistance += bhattacharyyaDistance.getEntry(0, 0) * weight;
+
+			}
+		}
+		return weightedBhattacharyyaDistance;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final SDFDatatype getReturnType() {
+		return SDFDatatype.DOUBLE;
+	}
+
+	/**
+	 * Calculates the determinat of the given matrix using Cholesky decomposition first and fall back on LU decomposition on error.
+	 * 
+	 * @param matrix
+	 *            The matrix
+	 * @return The determinant
+	 */
+	private double getDeterminant(final RealMatrix matrix) {
+		double determinant;
+		try {
+			determinant = new CholeskyDecomposition(matrix).getDeterminant();
+		} catch (NonSymmetricMatrixException | NonPositiveDefiniteMatrixException e) {
+			determinant = new LUDecomposition(matrix).getDeterminant();
+		}
+		return determinant;
+	}
+}
---
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/continuous/functions/math/BhattacharyyaDistanceFunctionVector.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2013 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.probabilistic.continuous.functions.math;
+
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.probabilistic.continuous.datatype.NormalDistributionMixture;
+import de.uniol.inf.is.odysseus.probabilistic.sdf.schema.SDFProbabilisticDatatype;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class BhattacharyyaDistanceFunctionVector extends AbstractBhattacharyyaDistanceFunction {
+	/**
+ * 
+ */
+	private static final long serialVersionUID = -3080769906129245226L;
+	/**
+	 * Accepted data types.
+	 */
+	public static final SDFDatatype[][] ACC_TYPES = new SDFDatatype[][] { { SDFProbabilisticDatatype.VECTOR_DOUBLE }, { SDFDatatype.VECTOR_DOUBLE } };
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final SDFDatatype[] getAcceptedTypes(final int argPos) {
+		if (argPos < 0) {
+			throw new IllegalArgumentException("negative argument index not allowed");
+		}
+		if (argPos > this.getArity()) {
+			throw new IllegalArgumentException(this.getSymbol() + " has only " + this.getArity() + " argument: Two distribution.");
+		}
+		return BhattacharyyaDistanceFunctionVector.ACC_TYPES[argPos];
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final Double getValue() {
+		final Object[] aVector = this.getInputValue(0);
+		final Object[] bVector = this.getInputValue(1);
+		final NormalDistributionMixture a = (NormalDistributionMixture) aVector[0];
+		final NormalDistributionMixture b = (NormalDistributionMixture) bVector[0];
+		return this.getValueInternal(a, b);
+	}
+
+}
