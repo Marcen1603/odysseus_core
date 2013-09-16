31e5984b70a080f8ac8769d74b6a8139f4e0b67e
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-09-16 10:12:21 +0000

    New Project for odysseus rcp config
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16596 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c53c2e25358c307e28eaa4979dc462e45f27fdf0

---
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.config/src/de/uniol/inf/is/odysseus/rcp/config/OdysseusRCPConfiguartionException.java
@@ -0,0 +1,26 @@
+package de.uniol.inf.is.odysseus.rcp.config;
+
+public class OdysseusRCPConfiguartionException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public OdysseusRCPConfiguartionException() {
+		super();
+	}
+
+	public OdysseusRCPConfiguartionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public OdysseusRCPConfiguartionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public OdysseusRCPConfiguartionException(String message) {
+		super(message);
+	}
+
+	public OdysseusRCPConfiguartionException(Throwable cause) {
+		super(cause);
+	}
+}
