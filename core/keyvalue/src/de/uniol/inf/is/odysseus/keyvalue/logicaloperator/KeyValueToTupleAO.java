8961278b65c084c36f191767bd65553a185df740
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-09-19 07:47:37 +0000

    Removed builder
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16631 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4a1bd5bc015e616c01b08d3baa4a38432954d36e

--- core/keyvalue/src/de/uniol/inf/is/odysseus/keyvalue/logicaloperator/KeyValueToTupleAO.java
+++ core/keyvalue/src/de/uniol/inf/is/odysseus/keyvalue/logicaloperator/KeyValueToTupleAO.java
@@ -1,8 +1,17 @@
 package de.uniol.inf.is.odysseus.keyvalue.logicaloperator;
 
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.AbstractLogicalOperator;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.UnaryLogicalOp;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.LogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.Parameter;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.BooleanParameter;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.CreateSDFAttributeParameter;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.StringParameter;
 
 @LogicalOperator(maxInputPorts=1, minInputPorts=1, name="KeyValueToTuple")
 public class KeyValueToTupleAO extends UnaryLogicalOp{
@@ -10,6 +19,8 @@ public class KeyValueToTupleAO extends UnaryLogicalOp{
 	private static final long serialVersionUID = 4804826171047928513L;
 	
 	private boolean keepInputObject = false;
+	private String type = "";
+	private List<SDFAttribute> attributes;
 
 	public KeyValueToTupleAO() {
 	}
@@ -19,6 +30,17 @@ public class KeyValueToTupleAO extends UnaryLogicalOp{
 		this.keepInputObject = keyValueToTuple.keepInputObject;
 	}
 
+	@Parameter(name="Schema", type=CreateSDFAttributeParameter.class, optional=false, isList=true)
+	public void setAttributes(List<SDFAttribute> attributes){
+		this.attributes = attributes;
+	}
+	
+	@Parameter(name = "type", type=StringParameter.class, optional= false )
+	public void setType(String type){
+		this.type = type;
+	}
+	
+	@Parameter(name = "keepInput", type=BooleanParameter.class )
 	public void setKeepInputObject(boolean keepInputObject) {
 		this.keepInputObject = keepInputObject;
 	}
@@ -27,6 +49,12 @@ public class KeyValueToTupleAO extends UnaryLogicalOp{
 		return keepInputObject;
 	}
 
+	@Override
+	protected SDFSchema getOutputSchemaIntern(int pos) {
+		SDFSchema schema = new SDFSchema(type, Tuple.class, attributes);
+		return schema;
+	}
+	
 	@Override
 	public AbstractLogicalOperator clone() {
 		return new KeyValueToTupleAO(this);
