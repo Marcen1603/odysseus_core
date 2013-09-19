6383a7d6b1da32f22bfa86f811801d7474daf9f4
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-09-19 12:44:44 +0000

    Simplified replaceWithSubplan
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16636 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5f43fc3dfdef44dd6c496e9ed8231dfcdb72a372

--- core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/RestructHelper.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/RestructHelper.java
@@ -29,7 +29,8 @@ import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 
 public class RestructHelper {
-	public static Collection<ILogicalOperator> removeOperator(UnaryLogicalOp remove, boolean reserveOutputSchema) {
+	public static Collection<ILogicalOperator> removeOperator(
+			UnaryLogicalOp remove, boolean reserveOutputSchema) {
 		List<ILogicalOperator> ret = new ArrayList<ILogicalOperator>();
 		Collection<LogicalSubscription> fathers = remove.getSubscriptions();
 		LogicalSubscription child = remove.getSubscribedToSource(0);
@@ -38,7 +39,12 @@ public class RestructHelper {
 		// Subscribe Child to every father of op
 		for (LogicalSubscription father : fathers) {
 			remove.unsubscribeSink(father);
-			child.getTarget().subscribeSink(father.getTarget(), father.getSinkInPort(), child.getSourceOutPort(), reserveOutputSchema ? remove.getOutputSchema() : child.getTarget().getOutputSchema());
+			child.getTarget().subscribeSink(
+					father.getTarget(),
+					father.getSinkInPort(),
+					child.getSourceOutPort(),
+					reserveOutputSchema ? remove.getOutputSchema() : child
+							.getTarget().getOutputSchema());
 			ret.add(father.getTarget());
 		}
 		// prevents duplicate entry if child.getTarget=father.getTarget
@@ -53,56 +59,71 @@ public class RestructHelper {
 	}
 
 	/**
-	 * Insert an operator in the tree at some special point and update all subscriptions i.e. the new Operator gets all subscriptions currently bound to the after operator (looking from root!) and create a new subscription from toInsert to after
+	 * Insert an operator in the tree at some special point and update all
+	 * subscriptions i.e. the new Operator gets all subscriptions currently
+	 * bound to the after operator (looking from root!) and create a new
+	 * subscription from toInsert to after
 	 * 
 	 * @param toInsert
-	 *            Operator that should be inserted as child of the after operator
+	 *            Operator that should be inserted as child of the after
+	 *            operator
 	 * @param after
 	 * @return
 	 */
-	public static Collection<ILogicalOperator> insertOperator(ILogicalOperator toInsert, ILogicalOperator after, int sinkInPort, int toInsertsinkInPort, int toInsertsourceOutPort) {
+	public static Collection<ILogicalOperator> insertOperator(
+			ILogicalOperator toInsert, ILogicalOperator after, int sinkInPort,
+			int toInsertsinkInPort, int toInsertsourceOutPort) {
 		List<ILogicalOperator> ret = new ArrayList<ILogicalOperator>();
 		LogicalSubscription source = after.getSubscribedToSource(sinkInPort);
 		ret.add(source.getTarget());
 		after.unsubscribeFromSource(source);
-		source.getTarget().subscribeSink(toInsert, toInsertsinkInPort, source.getSourceOutPort(), source.getTarget().getOutputSchema());
-		toInsert.subscribeSink(after, sinkInPort, toInsertsourceOutPort, toInsert.getOutputSchema());
+		source.getTarget()
+				.subscribeSink(toInsert, toInsertsinkInPort,
+						source.getSourceOutPort(),
+						source.getTarget().getOutputSchema());
+		toInsert.subscribeSink(after, sinkInPort, toInsertsourceOutPort,
+				toInsert.getOutputSchema());
 		ret.add(after);
 		return ret;
 	}
-	
+
 	/**
-	 * Inserts a new logical operator (toInsert) before the operator before (e.g. closer to the root!)
+	 * Inserts a new logical operator (toInsert) before the operator before
+	 * (e.g. closer to the root!)
+	 * 
 	 * @param toInsert
 	 * @param before
 	 * @return
 	 */
-	public static Collection<ILogicalOperator> insertOperatorBefore(ILogicalOperator toInsert, ILogicalOperator before){
+	public static Collection<ILogicalOperator> insertOperatorBefore(
+			ILogicalOperator toInsert, ILogicalOperator before) {
 		List<ILogicalOperator> ret = new ArrayList<>();
 		Collection<LogicalSubscription> subs = before.getSubscriptions();
-		for (LogicalSubscription sub: subs){
+		for (LogicalSubscription sub : subs) {
 			before.unsubscribeSink(sub);
 			// What about the source out port
-			toInsert.subscribeSink(sub.getTarget(), sub.getSinkInPort(), sub.getSourceOutPort(), sub.getSchema());
+			toInsert.subscribeSink(sub.getTarget(), sub.getSinkInPort(),
+					sub.getSourceOutPort(), sub.getSchema());
 			ret.add(sub.getTarget());
 		}
 		toInsert.subscribeToSource(before, 0, 0, before.getOutputSchema());
 		ret.add(before);
 		ret.add(toInsert);
 		return ret;
-		
+
 	}
 
-	public static Collection<ILogicalOperator> simpleOperatorSwitch(UnaryLogicalOp father, UnaryLogicalOp son) {
+	public static Collection<ILogicalOperator> simpleOperatorSwitch(
+			UnaryLogicalOp father, UnaryLogicalOp son) {
 		// TODO: Can there be more than one father??
-		if (son.getSubscriptions().size() != 1) {			
-			System.out.println("MAY NOT HAPPEN IN SIMPLE SWITCH!!!");								
-			System.out.println("FATHER: "+father);
+		if (son.getSubscriptions().size() != 1) {
+			System.out.println("MAY NOT HAPPEN IN SIMPLE SWITCH!!!");
+			System.out.println("FATHER: " + father);
 			System.out.println("SON HAS MORE THAN ONE FATHER!!!!");
-			System.out.println("SON: "+son);
-			for(LogicalSubscription sub : son.getSubscriptions()){
+			System.out.println("SON: " + son);
+			for (LogicalSubscription sub : son.getSubscriptions()) {
 				System.out.println(sub);
-			}			
+			}
 		}
 		son.unsubscribeSink(son.getSubscriptions().iterator().next());
 
@@ -112,181 +133,216 @@ public class RestructHelper {
 		// TODO: Can there be more than one father??
 		if (father.getSubscriptions().size() != 1) {
 			System.out.println("THIS MAY NOT HAPPEN");
-			for(LogicalSubscription sub : son.getSubscriptions()){
+			for (LogicalSubscription sub : son.getSubscriptions()) {
 				System.out.println(sub);
-			}	
+			}
 		}
 		LogicalSubscription toUp = father.getSubscriptions().iterator().next();
 		father.unsubscribeSink(toUp);
 
-		father.subscribeToSource(toDown.getTarget(), 0, toDown.getSourceOutPort(), toDown.getSchema());
+		father.subscribeToSource(toDown.getTarget(), 0,
+				toDown.getSourceOutPort(), toDown.getSchema());
 		father.subscribeSink(son, 0, 0, father.getOutputSchema());
 
-		son.subscribeSink(toUp.getTarget(), toUp.getSinkInPort(), 0, son.getOutputSchema());
+		son.subscribeSink(toUp.getTarget(), toUp.getSinkInPort(), 0,
+				son.getOutputSchema());
 
-		Collection<ILogicalOperator> toUpdate = new ArrayList<ILogicalOperator>(2);
+		Collection<ILogicalOperator> toUpdate = new ArrayList<ILogicalOperator>(
+				2);
 		toUpdate.add(toDown.getTarget());
 		toUpdate.add(toUp.getTarget());
 		return toUpdate;
 	}
-	
-	public static Collection<ILogicalOperator> replace(ILogicalOperator oldOP, ILogicalOperator newOp){
+
+	public static Collection<ILogicalOperator> replace(ILogicalOperator oldOP,
+			ILogicalOperator newOp) {
 		Collection<ILogicalOperator> touched = new LinkedList<>();
-		
-		Collection<LogicalSubscription> subscriptions = oldOP.getSubscriptions();
-		
-		if (newOp.getSubscriptions().size() > 0 || newOp.getSubscribedToSource().size() > 0){
-			throw new IllegalArgumentException("Replacement operator is not allowed to be connected!");
+
+		Collection<LogicalSubscription> subscriptions = oldOP
+				.getSubscriptions();
+
+		if (newOp.getSubscriptions().size() > 0
+				|| newOp.getSubscribedToSource().size() > 0) {
+			throw new IllegalArgumentException(
+					"Replacement operator is not allowed to be connected!");
 		}
-		
+
 		// Replace subscriptions to sinks
-		for (LogicalSubscription s: subscriptions){
+		for (LogicalSubscription s : subscriptions) {
 			oldOP.unsubscribeSink(s);
-			newOp.subscribeSink(s.getTarget(), s.getSinkInPort(), s.getSourceOutPort(), s.getSchema());
+			newOp.subscribeSink(s.getTarget(), s.getSinkInPort(),
+					s.getSourceOutPort(), s.getSchema());
 			touched.add(s.getTarget());
 		}
-		
+
 		// Replace subscriptions to source
 		subscriptions = oldOP.getSubscribedToSource();
-		for (LogicalSubscription s:subscriptions){
+		for (LogicalSubscription s : subscriptions) {
 			oldOP.unsubscribeFromSource(s);
-			newOp.subscribeToSource(s.getTarget(), s.getSinkInPort(), s.getSourceOutPort(), s.getSchema());
+			newOp.subscribeToSource(s.getTarget(), s.getSinkInPort(),
+					s.getSourceOutPort(), s.getSchema());
 			touched.add(s.getTarget());
 		}
-		
+
 		return touched;
 	}
-	
-	/**
-	 * Replaces a logical operator by a subplan.
-	 * @param oldOp The logical operator to be replaced.
-	 * @param newOp The subplan to be inserted instead.
-	 */
-	public static void replaceWithSubplan(ILogicalOperator oldOp, ILogicalOperator newOp) {
 
-		Collection<ILogicalOperator> operatorsOfSubplan = Lists.newArrayList();
-		RestructHelper.collectOperators(newOp, operatorsOfSubplan);
-		
-		for(ILogicalOperator operator : operatorsOfSubplan) {
-			
-			if(!operator.getSubscriptions().isEmpty()) {
-				
-				// no sink of subplan
-				continue;
-				
-			}
-			
-			// change all subscriptions, which were from oldOp to its targets
-			for(LogicalSubscription subToSink : oldOp.getSubscriptions()) {
-				
-				ILogicalOperator target = subToSink.getTarget();
-				target.unsubscribeFromSource(oldOp, subToSink.getSinkInPort(), subToSink.getSourceOutPort(), 
-						subToSink.getSchema());
-				target.subscribeToSource(operator, subToSink.getSinkInPort(), subToSink.getSourceOutPort(), 
-						subToSink.getSchema());
-				
-			}
-			
+	/**
+	 * Replaces a logical leaf operator by a subplan.
+	 * 
+	 * @param leafOp
+	 *            The logical operator to be replaced.
+	 * @param newOp
+	 *            The subplan to be inserted instead.
+	 */
+	public static void replaceWithSubplan(ILogicalOperator leafOp,
+			ILogicalOperator newOp) {
+
+		if (leafOp.getSubscribedToSource().size() > 0) {
+			throw new IllegalArgumentException(
+					"Method can only be called for a leaf");
 		}
-		
+
+		// change all subscriptions, which were from oldOp to its targets
+		for (LogicalSubscription subToSink : leafOp.getSubscriptions()) {
+
+			ILogicalOperator target = subToSink.getTarget();
+
+			target.unsubscribeFromSource(leafOp, subToSink.getSinkInPort(),
+					subToSink.getSourceOutPort(), subToSink.getSchema());
+			target.subscribeToSource(newOp, subToSink.getSinkInPort(),
+					subToSink.getSourceOutPort(), subToSink.getSchema());
+
+		}
+
 	}
-	
+
 	/**
 	 * Creates a new {@link TopAO} on top of the sinks.
-	 * @param sinks The {@link ILogicalOperator}s which shall be subscribed to new {@link TopAO}.
+	 * 
+	 * @param sinks
+	 *            The {@link ILogicalOperator}s which shall be subscribed to new
+	 *            {@link TopAO}.
 	 * @return The new {@link TopAO}.
 	 */
 	public static TopAO generateTopAO(final Collection<ILogicalOperator> sinks) {
-		
+
 		final TopAO topAO = new TopAO();
 		int inputPort = 0;
-		for(ILogicalOperator sink : sinks)
-			topAO.subscribeToSource(sink, inputPort++, 0, sink.getOutputSchema());
+		for (ILogicalOperator sink : sinks)
+			topAO.subscribeToSource(sink, inputPort++, 0,
+					sink.getOutputSchema());
 
 		return topAO;
-		
+
 	}
-	
+
 	/**
-	 * Removes all {@link TopAO} logical operators from a list of {@link ILogicalOperator}s representing an {@link ILogicalQuery}.
-	 * @param operators The list of {@link ILogicalOperator}s representing an {@link ILogicalQuery}.
+	 * Removes all {@link TopAO} logical operators from a list of
+	 * {@link ILogicalOperator}s representing an {@link ILogicalQuery}.
+	 * 
+	 * @param operators
+	 *            The list of {@link ILogicalOperator}s representing an
+	 *            {@link ILogicalQuery}.
 	 */
 	public static void removeTopAOs(List<ILogicalOperator> operators) {
-		
+
 		final List<ILogicalOperator> operatorsToRemove = Lists.newArrayList();
-		
-		for(final ILogicalOperator operator : operators) {
-			
-			if(operator instanceof TopAO) {
-				
+
+		for (final ILogicalOperator operator : operators) {
+
+			if (operator instanceof TopAO) {
+
 				operator.unsubscribeFromAllSources();
 				operatorsToRemove.add(operator);
-				
+
 			}
-			
+
 		}
 
-		for(final ILogicalOperator operatorToRemove : operatorsToRemove)
+		for (final ILogicalOperator operatorToRemove : operatorsToRemove)
 			operators.remove(operatorToRemove);
-		
+
 	}
-	
+
 	/**
-	 * Searches for a {@link LogicalSubscription} between two {@link ILogicalOperator}s.
-	 * @param source The relative source for the {@link LogicalSubscription}.
-	 * @param sink The relative sink for the {@link LogicalSubscription}.
-	 * @return The {@link LogicalSubscription} between <code>source</code> and <code>sink</code> or null, if there is no such subscription.
+	 * Searches for a {@link LogicalSubscription} between two
+	 * {@link ILogicalOperator}s.
+	 * 
+	 * @param source
+	 *            The relative source for the {@link LogicalSubscription}.
+	 * @param sink
+	 *            The relative sink for the {@link LogicalSubscription}.
+	 * @return The {@link LogicalSubscription} between <code>source</code> and
+	 *         <code>sink</code> or null, if there is no such subscription.
 	 */
-	public static LogicalSubscription determineSubscription(ILogicalOperator source, ILogicalOperator sink) {
-		
-		for(final LogicalSubscription subscription : source.getSubscriptions()) {
-			
-			if(subscription.getTarget().equals(sink))
+	public static LogicalSubscription determineSubscription(
+			ILogicalOperator source, ILogicalOperator sink) {
+
+		for (final LogicalSubscription subscription : source.getSubscriptions()) {
+
+			if (subscription.getTarget().equals(sink))
 				return subscription;
 
 		}
-		
+
 		return null;
-		
+
 	}
-	
+
 	/**
-	 * Generates a new {@link SDFSchema} with a given base name and adopting {@link SDFAttribute}s of an existing {@link SDFSchema}.  
-	 * @param basename The base name for the new {@link SDFSchema}.
-	 * @param outputSchema The {@link SDFSchema} whose {@link SDFAttribute}s shall be adopted.
+	 * Generates a new {@link SDFSchema} with a given base name and adopting
+	 * {@link SDFAttribute}s of an existing {@link SDFSchema}.
+	 * 
+	 * @param basename
+	 *            The base name for the new {@link SDFSchema}.
+	 * @param outputSchema
+	 *            The {@link SDFSchema} whose {@link SDFAttribute}s shall be
+	 *            adopted.
 	 * @return The new {@link SDFSchema}.
 	 */
-	public static SDFSchema generateOutputSchema(String basename, SDFSchema outputSchema) {
-		
+	public static SDFSchema generateOutputSchema(String basename,
+			SDFSchema outputSchema) {
+
 		List<SDFAttribute> attributes = Lists.newArrayList();
-		
-		for(SDFAttribute attribute : outputSchema)
-			attributes.add(new SDFAttribute(basename, attribute.getAttributeName(), attribute));
-		
+
+		for (SDFAttribute attribute : outputSchema)
+			attributes.add(new SDFAttribute(basename, attribute
+					.getAttributeName(), attribute));
+
 		return new SDFSchema(basename, outputSchema.getType(), attributes);
-		
+
 	}
-	
+
 	/**
-	 * Collects recursive all {@link ILogicalOperator}s representing an <code>ILogicalQuery</code>. <br />
-	 * This method should be called with {@link ILogicalQuery#getLogicalPlan()} as <code>currentOperator</code>.
-	 * @param currentOperator The <code>IlogicalOperator</code> to collect next.
-	 * @param list The list of all <code>ILogicalOperators</code> collected so far.
+	 * Collects recursive all {@link ILogicalOperator}s representing an
+	 * <code>ILogicalQuery</code>. <br />
+	 * This method should be called with {@link ILogicalQuery#getLogicalPlan()}
+	 * as <code>currentOperator</code>.
+	 * 
+	 * @param currentOperator
+	 *            The <code>IlogicalOperator</code> to collect next.
+	 * @param list
+	 *            The list of all <code>ILogicalOperators</code> collected so
+	 *            far.
 	 */
-	public static void collectOperators(ILogicalOperator currentOperator, Collection<ILogicalOperator> list) {
-		
-		if(!list.contains(currentOperator)) {
+	public static void collectOperators(ILogicalOperator currentOperator,
+			Collection<ILogicalOperator> list) {
+
+		if (!list.contains(currentOperator)) {
 
 			list.add(currentOperator);
 
-			for(final LogicalSubscription subscription : currentOperator.getSubscriptions())
+			for (final LogicalSubscription subscription : currentOperator
+					.getSubscriptions())
 				RestructHelper.collectOperators(subscription.getTarget(), list);
 
-			for(final LogicalSubscription subscription : currentOperator.getSubscribedToSource())
+			for (final LogicalSubscription subscription : currentOperator
+					.getSubscribedToSource())
 				RestructHelper.collectOperators(subscription.getTarget(), list);
-			
+
 		}
-		
+
 	}
 
 }
