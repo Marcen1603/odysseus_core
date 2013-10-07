1a8b3cfe47455b7f230f1def676fd4fe921f35cc
Christian Kuka <christian@kuka.cc>
2013-10-07 13:26:21 +0000

    Core: Add logger to callback error handler
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16865 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: af646512ffb2a7ae7bd0a596619b894fa442766d

--- core/core/src/de/uniol/inf/is/odysseus/core/connection/CallbackErrorHandler.java
+++ core/core/src/de/uniol/inf/is/odysseus/core/connection/CallbackErrorHandler.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+/**
+ * 
+ * @author Christian Kuka <christian@kuka.cc>
+ *
+ */
 public class CallbackErrorHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(CallbackErrorHandler.class);
 
-	static Logger LOG = LoggerFactory.getLogger(CallbackErrorHandler.class);
-	
     public void handleError(IOException e) {
-    	LOG.error(e.getMessage(),e);
+        LOG.error(e.getMessage(), e);
     }
 
 }
