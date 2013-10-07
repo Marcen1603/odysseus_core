403fa0e5fa4e97ef4ac11cbc33b35c245a3a114c
Christian Kuka <christian@kuka.cc>
2013-10-07 13:24:06 +0000

    Probabilistic: Add euclidean distance function
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16858 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d07602631e0485b84b0a7043698598e97938c098

---
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/continuous/functions/math/EuclideanDistanceFunctionVector.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2013 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.uniol.inf.is.odysseus.probabilistic.continuous.functions.math;
+
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class EuclideanDistanceFunctionVector extends AbstractEuclideanDistanceFunction {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 109458049612648398L;
+	/**
+	 * Accepted data types.
+	 */
+	public static final SDFDatatype[][] ACC_TYPES = new SDFDatatype[][] { { SDFDatatype.MATRIX_DOUBLE }, { SDFDatatype.MATRIX_DOUBLE } };
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final SDFDatatype[] getAcceptedTypes(final int argPos) {
+		if (argPos < 0) {
+			throw new IllegalArgumentException("negative argument index not allowed");
+		}
+		if (argPos > this.getArity()) {
+			throw new IllegalArgumentException(this.getSymbol() + " has only " + this.getArity() + " argument: Two distribution.");
+		}
+		return EuclideanDistanceFunctionVector.ACC_TYPES[argPos];
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final Double getValue() {
+		final RealMatrix a = MatrixUtils.createColumnRealMatrix(((double[][]) this.getInputValue(0))[0]);
+		final RealMatrix b = MatrixUtils.createColumnRealMatrix(((double[][]) this.getInputValue(1))[0]);
+		return this.getValueInternal(a, b);
+	}
+}
