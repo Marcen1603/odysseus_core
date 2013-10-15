418b5fad0a4e3aad0c40fd3ca7f080e7264cf991
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-10-15 11:32:47 +0000

    Removed Copy on Write Array List again
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@16938 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 565e24045265f3293c83d70ea0b6d66aa7154f71

--- core/scheduler.singlethreadscheduler2.0/src/de/uniol/inf/is/odysseus/scheduler/singlethreadscheduler/MultipleSourceExecutor.java
+++ core/scheduler.singlethreadscheduler2.0/src/de/uniol/inf/is/odysseus/scheduler/singlethreadscheduler/MultipleSourceExecutor.java
@@ -1,12 +1,12 @@
 package de.uniol.inf.is.odysseus.scheduler.singlethreadscheduler;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +22,7 @@ public class MultipleSourceExecutor extends Thread implements IEventListener,
 	Logger logger = LoggerFactory.getLogger(MultipleSourceExecutor.class);
 	boolean alldone = false;
 	private boolean interrupt = false;
-	final private List<IIterableSource<?>> sources = new CopyOnWriteArrayList<>();
+	final private List<IIterableSource<?>> sources = new ArrayList<>();
 	final private Comparator<IIterableSource<?>> sourcesComparator = new Comparator<IIterableSource<?>>() {
 		@Override
 		public int compare(IIterableSource<?> left, IIterableSource<?> right) {
