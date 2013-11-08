6a2db68a1660283d9785fa66a537464108aaa2ec
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-11-08 08:36:10 +0000

    New (empty) Plugin for dynamic login functionality
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17149 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4aa5c4c192261343831cb0fd77d7a131940e86e9

---
+++ core/rcp/de.uniol.inf.is.odysseus.rcp.login/src/de/uniol/inf/is/odysseus/rcp/login/RCPLoginPlugIn.java
@@ -0,0 +1,19 @@
+package de.uniol.inf.is.odysseus.rcp.login;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class RCPLoginPlugIn extends AbstractUIPlugin {
+
+	public static final String PLUGIN_ID = "de.uniol.inf.is.odysseus.rcp.login"; 
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+}
