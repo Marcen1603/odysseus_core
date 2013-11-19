ee11773a8ff1cf6c03f11a67fcc1e2bab706b6b9
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-11-19 09:55:54 +0000

    Removed deprecated method
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17256 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5993ecbadce99e338fea6e069724cf4585184d26

--- probabilistic/de.uniol.inf.is.odysseus.probabilistic.transform/src/de/uniol/inf/is/odysseus/probabilistic/transform/continuous/TSampleAORule.java
+++ probabilistic/de.uniol.inf.is.odysseus.probabilistic.transform/src/de/uniol/inf/is/odysseus/probabilistic/transform/continuous/TSampleAORule.java
@@ -18,7 +18,7 @@ package de.uniol.inf.is.odysseus.probabilistic.transform.continuous;
 import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.TransformationConfiguration;
-import de.uniol.inf.is.odysseus.probabilistic.common.SchemaUtils;
+import de.uniol.inf.is.odysseus.probabilistic.base.ProbabilisticTuple;
 import de.uniol.inf.is.odysseus.probabilistic.continuous.logicaloperator.SampleAO;
 import de.uniol.inf.is.odysseus.probabilistic.continuous.physicaloperator.SamplePO;
 import de.uniol.inf.is.odysseus.ruleengine.ruleflow.IRuleFlowGroup;
@@ -30,36 +30,42 @@ import de.uniol.inf.is.odysseus.transform.rule.AbstractTransformationRule;
  * @author Christian Kuka <christian@kuka.cc>
  * 
  */
-@SuppressWarnings({ "all" })
 public class TSampleAORule extends AbstractTransformationRule<SampleAO> {
-    /*
-     * 
-     * @see de.uniol.inf.is.odysseus.ruleengine.rule.IRule#getPriority()
-     */
-    @Override
-    public final int getPriority() {
-        return 0;
-    }
+	/*
+	 * 
+	 * @see de.uniol.inf.is.odysseus.ruleengine.rule.IRule#getPriority()
+	 */
+	@Override
+	public final int getPriority() {
+		return 0;
+	}
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see de.uniol.inf.is.odysseus.ruleengine.rule.IRule#execute(java.lang.Object, java.lang.Object)
+	 * @see
+	 * de.uniol.inf.is.odysseus.ruleengine.rule.IRule#execute(java.lang.Object,
+	 * java.lang.Object)
 	 */
 	@Override
-	public final void execute(final SampleAO operator, final TransformationConfiguration config) {
-		final IPhysicalOperator samplePO = new SamplePO<ITimeInterval>(operator.determineAttributesList(), operator.getSamples());
+	public final void execute(final SampleAO operator,
+			final TransformationConfiguration config) {
+		final IPhysicalOperator samplePO = new SamplePO<ITimeInterval>(
+				operator.determineAttributesList(), operator.getSamples());
 		this.defaultExecute(operator, samplePO, config, true, true);
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see de.uniol.inf.is.odysseus.ruleengine.rule.IRule#isExecutable(java.lang.Object, java.lang.Object)
+	 * @see
+	 * de.uniol.inf.is.odysseus.ruleengine.rule.IRule#isExecutable(java.lang
+	 * .Object, java.lang.Object)
 	 */
 	@Override
-	public final boolean isExecutable(final SampleAO operator, final TransformationConfiguration config) {
-		if ((config.getDataTypes().contains(SchemaUtils.DATATYPE))) {
+	public final boolean isExecutable(final SampleAO operator,
+			final TransformationConfiguration config) {
+		if (operator.getInputSchema().getType() == ProbabilisticTuple.class) {
 			if (operator.isAllPhysicalInputSet()) {
 				return true;
 			}
@@ -90,7 +96,8 @@ public class TSampleAORule extends AbstractTransformationRule<SampleAO> {
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see de.uniol.inf.is.odysseus.ruleengine.rule.AbstractRule#getConditionClass()
+	 * @see
+	 * de.uniol.inf.is.odysseus.ruleengine.rule.AbstractRule#getConditionClass()
 	 */
 	@Override
 	public final Class<? super SampleAO> getConditionClass() {
