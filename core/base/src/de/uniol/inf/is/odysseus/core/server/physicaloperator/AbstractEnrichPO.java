472a753d4afc66aae5a7ad1a20eef071d802eded
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2013-11-26 10:31:28 +0000

    Refactorings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17324 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c76eee322caa10a077fbb4562566892e018d4924

--- core/base/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/AbstractEnrichPO.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/AbstractEnrichPO.java
@@ -3,6 +3,9 @@ package de.uniol.inf.is.odysseus.core.server.physicaloperator;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.uniol.inf.is.odysseus.core.Order;
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.metadata.IMetadataMergeFunction;
@@ -11,27 +14,30 @@ import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
 import de.uniol.inf.is.odysseus.core.physicaloperator.OpenFailedException;
 import de.uniol.inf.is.odysseus.core.server.cache.ICache;
 
-abstract public class AbstractEnrichPO<T extends IStreamObject<M>, M extends IMetaAttribute> extends
-		AbstractPipe<T, T> {
+abstract public class AbstractEnrichPO<T extends IStreamObject<M>, M extends IMetaAttribute>
+		extends AbstractPipe<T, T> {
 
-	private final ICache cacheManager;
+	static final Logger logger = LoggerFactory
+			.getLogger(AbstractEnrichPO.class);
+
+	private final ICache cache;
 	private final IDataMergeFunction<T, M> dataMergeFunction;
 	private final IMetadataMergeFunction<M> metaMergeFunction;
 	private final int[] uniqueKeys;
 
-	public AbstractEnrichPO(ICache cacheManager,
+	public AbstractEnrichPO(ICache cache,
 			IDataMergeFunction<T, M> dataMergeFunction,
 			IMetadataMergeFunction<M> metaMergeFunction, int[] uniqueKeys) {
 		super();
-		this.cacheManager = cacheManager;
+		this.cache = cache;
 		this.dataMergeFunction = dataMergeFunction;
 		this.metaMergeFunction = metaMergeFunction;
 		this.uniqueKeys = uniqueKeys;
 	}
 
-	public AbstractEnrichPO(AbstractEnrichPO<T,M> abstractEnrichPO) {
+	public AbstractEnrichPO(AbstractEnrichPO<T, M> abstractEnrichPO) {
 		super(abstractEnrichPO);
-		this.cacheManager = abstractEnrichPO.cacheManager;
+		this.cache = abstractEnrichPO.cache;
 		this.dataMergeFunction = abstractEnrichPO.dataMergeFunction;
 		this.metaMergeFunction = abstractEnrichPO.metaMergeFunction;
 		this.uniqueKeys = abstractEnrichPO.uniqueKeys;
@@ -41,7 +47,7 @@ abstract public class AbstractEnrichPO<T extends IStreamObject<M>, M extends IMe
 	public de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe.OutputMode getOutputMode() {
 		return OutputMode.NEW_ELEMENT;
 	}
-	
+
 	@Override
 	final protected void process_open() throws OpenFailedException {
 		dataMergeFunction.init();
@@ -52,52 +58,47 @@ abstract public class AbstractEnrichPO<T extends IStreamObject<M>, M extends IMe
 	@Override
 	protected void process_next(T object, int port) {
 		List<IStreamObject<?>> result = null;
-		
-		if (cacheManager == null) {
-			result = internal_process(object);
-		} else if (uniqueKeys == null) {
-			result = process_with_cache(object, object.hashCode());
-		} else {
-			result = process_with_cache(object,
-					object.restrictedHashCode(uniqueKeys));
+		Object key = null;
+		if (cache != null) {
+			if (uniqueKeys != null) {
+				key = object.restrictedHashCode(uniqueKeys);
+			} else {
+				key = object.hashCode();
+			}
+			result = cache.get(key);
 		}
-		// enrichAndTransfer
+
+		// No cache or nothing found in cache
+		if (result == null) {
+			result = internal_process(object);
+		}
+
+		// Store into cache
+		if (cache != null) {
+			cache.put(key, result);
+		}
+
+		// Enrich and transfer
 		for (int i = 0; i < result.size(); i++) {
-			T output = dataMergeFunction.merge(object,
-					(T) result.get(i), metaMergeFunction,
-					Order.LeftRight);
+			T output = dataMergeFunction.merge(object, (T) result.get(i),
+					metaMergeFunction, Order.LeftRight);
 			output.setMetadata((M) object.getMetadata().clone());
 			transfer(output);
-		}	}
-
-	private List<IStreamObject<?>> process_with_cache(T input,
-			Object key) {
-		// Test if query can be answered with elements from cache
-		List<IStreamObject<?>> cached = cacheManager.get(key);
-		if (cached != null) {
-			return cached;
 		}
-
-		// Else retrieve new elements
-		List<IStreamObject<?>> queryResult = internal_process(input);
-
-		cacheManager.put(key, queryResult);
-		return queryResult;
 	}
-
 
 	@Override
 	final protected void process_close() {
 		// Print the Caching Statistics
-		if (this.cacheManager != null) {
-			System.out.println("Caching Statistics: ");
-			System.out.println("Cache hits: " + cacheManager.getCacheHits());
-			System.out.println("Cache miss: " + cacheManager.getCacheMiss());
-			System.out.println("Cache inserts: "
-					+ cacheManager.getCacheInsert());
-			System.out.println("Cache removes: "
-					+ cacheManager.getCacheRemoves());
-			cacheManager.close();
+		if (this.cache != null) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Caching Statistics: ");
+				logger.debug("Cache hits: " + cache.getCacheHits());
+				logger.debug("Cache miss: " + cache.getCacheMiss());
+				logger.debug("Cache inserts: " + cache.getCacheInsert());
+				logger.debug("Cache removes: " + cache.getCacheRemoves());
+			}
+			cache.close();
 		}
 		internal_process_close();
 
@@ -111,17 +112,18 @@ abstract public class AbstractEnrichPO<T extends IStreamObject<M>, M extends IMe
 		}
 
 		@SuppressWarnings("unchecked")
-		AbstractEnrichPO<T,M> other = (AbstractEnrichPO<T,M>) ipo;
+		AbstractEnrichPO<T, M> other = (AbstractEnrichPO<T, M>) ipo;
 		if (!Arrays.equals(uniqueKeys, other.uniqueKeys)) {
 			return false;
 		}
-		;
+		
 		return true;
 	}
 
-
 	abstract protected void internal_process_open();
+
 	abstract protected List<IStreamObject<?>> internal_process(T input);
+
 	abstract protected void internal_process_close();
 
 }
