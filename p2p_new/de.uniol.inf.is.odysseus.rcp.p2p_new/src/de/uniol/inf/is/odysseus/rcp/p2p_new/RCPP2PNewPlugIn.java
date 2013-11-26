aa07935daf28a5b43f149e45b039f0e6d7cf26cf
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-11-26 10:22:27 +0000

    P2P-RCP:Refactoring
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17323 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 89e25086825a60554e411d70ff3656ce864ab133

--- p2p_new/de.uniol.inf.is.odysseus.rcp.p2p_new/src/de/uniol/inf/is/odysseus/rcp/p2p_new/RCPP2PNewPlugIn.java
+++ p2p_new/de.uniol.inf.is.odysseus.rcp.p2p_new/src/de/uniol/inf/is/odysseus/rcp/p2p_new/RCPP2PNewPlugIn.java
@@ -1,9 +1,16 @@
 package de.uniol.inf.is.odysseus.rcp.p2p_new;
 
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
+import de.uniol.inf.is.odysseus.p2p_new.IP2PNetworkManager;
+
 public class RCPP2PNewPlugIn extends AbstractUIPlugin {
+
+	public static final String NO_P2P_CONNECTION_TEXT = "<no p2p connection>";
 
 	@Override
 	public void start(BundleContext context) throws Exception {
@@ -11,5 +18,22 @@ public class RCPP2PNewPlugIn extends AbstractUIPlugin {
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
+	}
+	
+	public static String determineP2PTitleSuffix(IP2PNetworkManager manager) {
+		return manager.getLocalPeerGroupName() + ":" + manager.getLocalPeerName();
+	}
+	
+	public static void setWindowTitleSuffix(final String titleSuffix) {
+		final IWorkbench workbench = PlatformUI.getWorkbench();
+		workbench.getDisplay().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+				if (window != null) {
+					window.getShell().setText( window.getShell().getText() + " - " + titleSuffix);
+				}
+			}
+		});
 	}
 }
--- p2p_new/de.uniol.inf.is.odysseus.rcp.p2p_new/src/de/uniol/inf/is/odysseus/rcp/p2p_new/StartUp.java
+++ p2p_new/de.uniol.inf.is.odysseus.rcp.p2p_new/src/de/uniol/inf/is/odysseus/rcp/p2p_new/StartUp.java
@@ -1,9 +1,6 @@
 package de.uniol.inf.is.odysseus.rcp.p2p_new;
 
 import org.eclipse.ui.IStartup;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
 
 import de.uniol.inf.is.odysseus.rcp.p2p_new.service.P2PNetworkManagerService;
 
@@ -11,20 +8,6 @@ public class StartUp implements IStartup {
 
 	@Override
 	public void earlyStartup() {
-		final IWorkbench workbench = PlatformUI.getWorkbench();
-		workbench.getDisplay().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
-				if (window != null) {
-					window.getShell().setText( window.getShell().getText() + " - " + determineP2PTitleSuffix());
-				}
-			}
-
-		});
-	}
-
-	private static String determineP2PTitleSuffix() {
-		return P2PNetworkManagerService.get().getLocalPeerGroupName() + ":" + P2PNetworkManagerService.get().getLocalPeerName();
+		RCPP2PNewPlugIn.setWindowTitleSuffix(RCPP2PNewPlugIn.determineP2PTitleSuffix(P2PNetworkManagerService.get()));
 	}
 }
