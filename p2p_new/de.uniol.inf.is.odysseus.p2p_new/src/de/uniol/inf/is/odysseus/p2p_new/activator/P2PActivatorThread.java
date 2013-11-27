27fefd07eb9fef11b1f52a967718159ec24d4906
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-11-27 11:41:12 +0000

    P2P: Bugfix, own p2p-log4j.properties was not used
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17371 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e24621950164c0ebef380debd6360173dc394266

--- p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/activator/P2PActivatorThread.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/activator/P2PActivatorThread.java
@@ -2,13 +2,10 @@ package de.uniol.inf.is.odysseus.p2p_new.activator;
 
 import net.jxta.platform.NetworkManager;
 
-import org.apache.log4j.PropertyConfigurator;
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 
 import de.uniol.inf.is.odysseus.core.server.OdysseusConfiguration;
@@ -18,7 +15,6 @@ import de.uniol.inf.is.odysseus.p2p_new.network.P2PNetworkManager;
 public class P2PActivatorThread extends Thread {
 
 	private static final Logger LOG = LoggerFactory.getLogger(P2PActivatorThread.class);
-	private static final java.util.logging.Level JXTA_LOG_LEVEL = java.util.logging.Level.SEVERE;
 
 	private static final String PEER_NAME_SYS_PROPERTY = "peer.name";
 	private static final String PEER_GROUP_NAME_SYS_PROPERTY = "peer.group.name";
@@ -26,26 +22,15 @@ public class P2PActivatorThread extends Thread {
 	private static final String DEFAULT_PEER_NAME = "OdysseusPeer";
 	private static final String DEFAULT_PEER_GROUP_NAME = "OdysseusPeerGroup";
 	
-	private static final String LOG_PROPERTIES_FILENAME = "log4j.properties";
-	private static final String JXTA_LOGGER_NAME = "net.jxta";
-
-	private final BundleContext bundleContext;
-	
 	private NetworkManager manager;
 	
 	public P2PActivatorThread( BundleContext bundleContext ) {
-		Preconditions.checkNotNull(bundleContext, "Bundle context must not be null!");
-		
-		this.bundleContext = bundleContext;
-		
 		setName("P2P Activator Thread");
 		setDaemon(true);
 	}
 	
 	@Override
 	public void run() {
-		configureLogging(bundleContext.getBundle());
-
 		waitForP2PNetworkManager();
 		
 		String ownPeerName = determinePeerName();
@@ -100,18 +85,5 @@ public class P2PActivatorThread extends Thread {
 	
 	public NetworkManager getNetworkManager() {
 		return manager;
-	}
-	
-	private static void configureLogging(Bundle bundle) {
-		java.util.logging.Logger jxtaLogger = java.util.logging.Logger.getLogger(JXTA_LOGGER_NAME);
-		if (jxtaLogger != null) {
-			jxtaLogger.setLevel(JXTA_LOG_LEVEL);
-		} else {
-			LOG.warn("Could not get JXTA-Logger for setting level to {}", JXTA_LOG_LEVEL);
-		}
-		jxtaLogger = java.util.logging.Logger.getLogger("net.jxta.impl.endpoint.router.EndpointRouter");
-		jxtaLogger.setLevel(java.util.logging.Level.OFF);
-		
-		PropertyConfigurator.configure(bundle.getResource(LOG_PROPERTIES_FILENAME));
 	}
 }
--- p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/activator/P2PNewPlugIn.java
+++ p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/activator/P2PNewPlugIn.java
@@ -2,18 +2,29 @@ package de.uniol.inf.is.odysseus.p2p_new.activator;
 
 import net.jxta.document.AdvertisementFactory;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.uniol.inf.is.odysseus.p2p_new.dictionary.SourceAdvertisement;
 import de.uniol.inf.is.odysseus.p2p_new.dictionary.sources.SourceAdvertisementInstantiator;
 
 public class P2PNewPlugIn implements BundleActivator {
 
+	private static final Logger LOG = LoggerFactory.getLogger(P2PNewPlugIn.class);
+	private static final String LOG_PROPERTIES_FILENAME = "log4j.properties";
+	private static final String JXTA_LOGGER_NAME = "net.jxta";
+	private static final java.util.logging.Level JXTA_LOG_LEVEL = java.util.logging.Level.SEVERE;
+	
 	public static final int TRANSPORT_BUFFER_SIZE = 640*480*3+(1+4);
 
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
+		configureLogging(bundleContext.getBundle());
+		
 		registerAdvertisementTypes();
 	}
 
@@ -23,6 +34,19 @@ public class P2PNewPlugIn implements BundleActivator {
 
 	private static void registerAdvertisementTypes() {
 		AdvertisementFactory.registerAdvertisementInstance(SourceAdvertisement.getAdvertisementType(), new SourceAdvertisementInstantiator());
+	}
+	
+	private static void configureLogging(Bundle bundle) {
+		java.util.logging.Logger jxtaLogger = java.util.logging.Logger.getLogger(JXTA_LOGGER_NAME);
+		if (jxtaLogger != null) {
+			jxtaLogger.setLevel(JXTA_LOG_LEVEL);
+		} else {
+			LOG.warn("Could not get JXTA-Logger for setting level to {}", JXTA_LOG_LEVEL);
+		}
+		jxtaLogger = java.util.logging.Logger.getLogger("net.jxta.impl.endpoint.router.EndpointRouter");
+		jxtaLogger.setLevel(java.util.logging.Level.OFF);
+		
+		PropertyConfigurator.configure(bundle.getResource(LOG_PROPERTIES_FILENAME));
 	}
 
 }
