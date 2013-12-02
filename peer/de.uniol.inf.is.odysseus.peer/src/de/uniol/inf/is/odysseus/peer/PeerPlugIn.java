2e79e3130baf31a698f19636dd226faa355ccc09
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-12-02 15:05:59 +0000

    New Peer-Projects
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17407 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9c366aa521d859c16381846d64b180355983b2bc

---
+++ peer/de.uniol.inf.is.odysseus.peer/src/de/uniol/inf/is/odysseus/peer/PeerPlugIn.java
@@ -0,0 +1,15 @@
+package de.uniol.inf.is.odysseus.peer;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class PeerPlugIn implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+}
