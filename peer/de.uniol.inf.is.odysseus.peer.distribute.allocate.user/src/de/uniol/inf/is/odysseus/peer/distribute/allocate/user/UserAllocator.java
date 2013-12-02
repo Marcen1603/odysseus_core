a330b6392b82751e7e4072541c3ea506210f6e74
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-12-02 15:07:08 +0000

    New Peer-Projects
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17411 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3d41a317a7e0106efaceec5cdca39c0f0fa4ba8f

---
+++ peer/de.uniol.inf.is.odysseus.peer.distribute.allocate.user/src/de/uniol/inf/is/odysseus/peer/distribute/allocate/user/UserAllocator.java
@@ -0,0 +1,120 @@
+package de.uniol.inf.is.odysseus.peer.distribute.allocate.user;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+
+import net.jxta.peer.PeerID;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.QueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.peer.distribute.ILogicalQueryPart;
+import de.uniol.inf.is.odysseus.peer.distribute.IQueryPartAllocator;
+import de.uniol.inf.is.odysseus.peer.distribute.QueryPartAllocationException;
+import de.uniol.inf.is.odysseus.peer.distribute.allocate.user.service.P2PDictionaryService;
+import de.uniol.inf.is.odysseus.peer.distribute.allocate.user.service.P2PNetworkManagerService;
+
+public class UserAllocator implements IQueryPartAllocator {
+
+	private static final Random random = new Random(0);
+	
+	@Override
+	public String getName() {
+		return "user";
+	}
+
+	@Override
+	public Map<ILogicalQueryPart, PeerID> allocate(Collection<ILogicalQueryPart> queryParts, Collection<PeerID> knownRemotePeers, PeerID localPeerID, QueryBuildConfiguration config) throws QueryPartAllocationException {
+		
+		Map<ILogicalQueryPart, PeerID> allocationMap = Maps.newHashMap();
+		
+		Map<String, PeerID> peerNameMap = determinePeerNames();
+		peerNameMap.put(P2PNetworkManagerService.get().getLocalPeerName(), localPeerID);
+		peerNameMap.put("local", localPeerID);
+		
+		for( ILogicalQueryPart part : queryParts ) {
+			String destinationName = determineMostMentionedDestinationName(part);
+			
+			if( peerNameMap.containsKey(destinationName)) {
+				allocationMap.put(part, peerNameMap.get(destinationName));
+			} else {
+				PeerID peerID = chooseRandomPeerID( peerNameMap.values(), localPeerID);
+				peerNameMap.put(destinationName, peerID);
+				
+				allocationMap.put(part, peerID);
+			}
+		}
+		
+		return allocationMap;
+	}
+
+	private static Map<String, PeerID> determinePeerNames() {
+		ImmutableList<PeerID> remotePeerIDs = P2PDictionaryService.get().getRemotePeerIDs();
+
+		Map<String, PeerID> peerNameMap = Maps.newHashMap();
+		for( PeerID remotePeerID : remotePeerIDs ) {
+			Optional<String> optPeerName = P2PDictionaryService.get().getRemotePeerName(remotePeerID);
+			if( optPeerName.isPresent() ) {
+				peerNameMap.put(optPeerName.get(), remotePeerID);
+			}
+		}
+		
+		return peerNameMap;
+	}
+
+	private static String determineMostMentionedDestinationName(ILogicalQueryPart part) {
+		ImmutableCollection<ILogicalOperator> operators = part.getOperators();
+		
+		Map<String, Integer> countMap = Maps.newHashMap();
+		for( ILogicalOperator operator : operators ) {
+			String destinationOfOperator = operator.getDestinationName();
+			
+			if( !Strings.isNullOrEmpty(destinationOfOperator)) {
+				if(countMap.containsKey(destinationOfOperator)) {
+					countMap.put( destinationOfOperator, countMap.get(destinationOfOperator) + 1);
+				} else {
+					countMap.put(destinationOfOperator, 1);
+				}
+			}
+		}
+		
+		if( countMap.isEmpty() ) {
+			return "local";
+		}
+		
+		String mostName = "local";
+		int mostCount = Integer.MIN_VALUE;
+		
+		for( String destinationName : countMap.keySet() ) {
+			int count = countMap.get(destinationName);
+			if(count > mostCount ) {
+				mostName = destinationName;
+				mostCount = count;
+			}
+		}
+		
+		return mostName;
+	}
+
+	private static PeerID chooseRandomPeerID(Collection<PeerID> values, PeerID localPeerID) {
+		PeerID current = null;
+		
+		do {
+			int choice = random.nextInt(values.size());
+			Iterator<PeerID> peerIDIterator = values.iterator();
+			for( int i = 0; i < choice + 1; i++ ) {
+				current = peerIDIterator.next();
+			}
+		} while(current == null || current.equals(localPeerID));
+		
+		return current;
+	}
+
+}
---
+++ peer/de.uniol.inf.is.odysseus.peer.distribute.allocate.user/src/de/uniol/inf/is/odysseus/peer/distribute/allocate/user/service/P2PNetworkManagerService.java
@@ -0,0 +1,33 @@
+package de.uniol.inf.is.odysseus.peer.distribute.allocate.user.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.p2p_new.IP2PNetworkManager;
+
+public class P2PNetworkManagerService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(P2PNetworkManagerService.class);
+
+	private static IP2PNetworkManager networkManager;
+
+	public final void bindP2PNetworkManager(IP2PNetworkManager manager) {
+		networkManager = manager;
+		LOG.debug("P2P network manager bound {}", manager);
+	}
+
+	public final void unbindP2PNetworkManager(IP2PNetworkManager manager) {
+		if (networkManager == manager) {
+			networkManager = null;
+			LOG.debug("P2P network manager unbound {}", manager);
+		}
+	}
+
+	public static IP2PNetworkManager get() {
+		return networkManager;
+	}
+
+	public static boolean isBound() {
+		return get() != null;
+	}
+}
