f9680e91aa7c2ac3f69bae413afd8c4b12db0c60
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-12-03 06:16:46 +0000

    Peer/Core: Forgot to commit peer-interface changes
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17419 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4d1301a8dcda970f659c84dc9a18d9f3bcd16b33

--- core/base/src/de/uniol/inf/is/odysseus/core/server/distribution/IQueryDistributor.java
+++ core/base/src/de/uniol/inf/is/odysseus/core/server/distribution/IQueryDistributor.java
@@ -5,8 +5,9 @@ import java.util.Collection;
 import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
 import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.QueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
 
 public interface IQueryDistributor {
 
-	public Collection<ILogicalQuery> distribute( IExecutor executor, Collection<ILogicalQuery> queries, QueryBuildConfiguration config) throws QueryDistributionException; 
+	public Collection<ILogicalQuery> distribute( IExecutor executor, ISession caller, Collection<ILogicalQuery> queries, QueryBuildConfiguration config) throws QueryDistributionException; 
 }
