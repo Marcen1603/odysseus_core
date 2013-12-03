0f212978df1c3c432429d6bb49f3f620df054d71
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-12-03 09:49:47 +0000

    Peer: Added querycloud partitioning
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17436 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 50b781bb8b5e3081ff1400aff23d5ff71c037667

---
+++ peer/de.uniol.inf.is.odysseus.peer.distribute.partition.querycloud/src/de/uniol/inf/is/odysseus/peer/distribute/partition/querycloud/QueryCloudPartitioner.java
@@ -0,0 +1,27 @@
+package de.uniol.inf.is.odysseus.peer.distribute.partition.querycloud;
+
+import java.util.Collection;
+
+import com.google.common.collect.Lists;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.QueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.peer.distribute.ILogicalQueryPart;
+import de.uniol.inf.is.odysseus.peer.distribute.IQueryPartitioner;
+import de.uniol.inf.is.odysseus.peer.distribute.LogicalQueryPart;
+import de.uniol.inf.is.odysseus.peer.distribute.QueryPartitionException;
+
+public class QueryCloudPartitioner implements IQueryPartitioner {
+
+	@Override
+	public String getName() {
+		return "querycloud";
+	}
+
+	@Override
+	public Collection<ILogicalQueryPart> partition(Collection<ILogicalOperator> operators, QueryBuildConfiguration config) throws QueryPartitionException {
+		ILogicalQueryPart part = new LogicalQueryPart(operators);
+		return Lists.newArrayList(part);
+	}
+
+}
