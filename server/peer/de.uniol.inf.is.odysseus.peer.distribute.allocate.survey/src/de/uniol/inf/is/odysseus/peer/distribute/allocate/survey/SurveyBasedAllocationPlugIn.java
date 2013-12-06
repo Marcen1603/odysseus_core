470391ed7aa0d7bf8933cbc7d2bef11dc3e48e4f
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-12-06 10:31:24 +0000

    Peer: Misspelled plugin-name
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17818 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d10553b75085f760feaa824b8f119917e997848e

---
+++ server/peer/de.uniol.inf.is.odysseus.peer.distribute.allocate.survey/src/de/uniol/inf/is/odysseus/peer/distribute/allocate/survey/SurveyBasedAllocationPlugIn.java
@@ -0,0 +1,15 @@
+package de.uniol.inf.is.odysseus.peer.distribute.allocate.survey;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class SurveyBasedAllocationPlugIn implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+}
