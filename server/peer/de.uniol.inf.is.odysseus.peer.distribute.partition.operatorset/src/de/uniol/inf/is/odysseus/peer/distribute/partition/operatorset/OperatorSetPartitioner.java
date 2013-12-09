0e929475363ff57d0323a36e181b13eeb5671f8e
Timo Michelsen <Timo.Michelsen@gmail.com>
2013-12-09 10:33:53 +0000

    Peer: New plugin for operatorSet-partitioning
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17850 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ccfe24d9c40885b59c2a957a9cb79e81b2c202d5

---
+++ server/peer/de.uniol.inf.is.odysseus.peer.distribute.partition.operatorset/src/de/uniol/inf/is/odysseus/peer/distribute/partition/operatorset/OperatorSetPartitioner.java
@@ -0,0 +1,70 @@
+package de.uniol.inf.is.odysseus.peer.distribute.partition.operatorset;
+
+import java.util.Collection;
+
+import com.google.common.collect.Lists;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalSubscription;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.QueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.peer.distribute.ILogicalQueryPart;
+import de.uniol.inf.is.odysseus.peer.distribute.IQueryPartitioner;
+import de.uniol.inf.is.odysseus.peer.distribute.LogicalQueryPart;
+import de.uniol.inf.is.odysseus.peer.distribute.QueryPartitionException;
+
+public class OperatorSetPartitioner implements IQueryPartitioner {
+
+	private static final StatefulOperatorsCollection STATEFUL_OPERATORS = new StatefulOperatorsCollection();
+	
+	@Override
+	public String getName() {
+		return "operatorset";
+	}
+
+	@Override
+	public Collection<ILogicalQueryPart> partition(Collection<ILogicalOperator> ops, QueryBuildConfiguration config) throws QueryPartitionException {
+
+		Collection<ILogicalOperator> operatorsToVisit = Lists.newArrayList(ops);
+		Collection<ILogicalQueryPart> queryParts = Lists.newArrayList();
+		
+		while (!operatorsToVisit.isEmpty()) {
+
+			ILogicalOperator operatorToStart = operatorsToVisit.iterator().next();
+
+			Collection<ILogicalOperator> collectedOperators = Lists.newArrayList();
+			collectUntilStatefulOperators(operatorToStart, collectedOperators);
+			
+			operatorsToVisit.removeAll(collectedOperators);
+			
+			queryParts.add( new LogicalQueryPart(collectedOperators));
+		}
+
+		return queryParts;
+	}
+
+	private static void collectUntilStatefulOperators(ILogicalOperator operator, Collection<ILogicalOperator> visitedOperators) {
+		if( visitedOperators.contains(operator)) {
+			return;
+		}
+		
+		visitedOperators.add(operator);
+
+		if (!isStateful(operator)) {
+			for (LogicalSubscription sub : operator.getSubscriptions()) {
+				collectUntilStatefulOperators(sub.getTarget(), visitedOperators);
+			}
+		}
+
+		for (LogicalSubscription sub : operator.getSubscribedToSource()) {
+			ILogicalOperator targetOperator = sub.getTarget();
+			if (!isStateful(targetOperator)) {
+				collectUntilStatefulOperators(targetOperator, visitedOperators);
+			}
+		}
+	}
+
+	private static boolean isStateful(ILogicalOperator operator) {
+		return STATEFUL_OPERATORS.isStateful(operator);
+	}
+
+}
---
+++ server/peer/de.uniol.inf.is.odysseus.peer.distribute.partition.operatorset/src/de/uniol/inf/is/odysseus/peer/distribute/partition/operatorset/StatefulOperatorsCollection.java
@@ -0,0 +1,30 @@
+package de.uniol.inf.is.odysseus.peer.distribute.partition.operatorset;
+
+import java.util.Collection;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.AggregateAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.JoinAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.UnionAO;
+
+
+public final class StatefulOperatorsCollection {
+
+	private final Collection<Class<? extends ILogicalOperator>> statefulOperators = Lists.newArrayList();
+	
+	public StatefulOperatorsCollection() {
+		statefulOperators.add(UnionAO.class);
+		statefulOperators.add(JoinAO.class);
+		statefulOperators.add(AggregateAO.class);
+		statefulOperators.add(JoinAO.class);
+	}
+
+	public boolean isStateful( ILogicalOperator operator ) {
+		Preconditions.checkNotNull(operator, "Operator to check for statfulness must not be null!");
+		
+		return statefulOperators.contains(operator.getClass());
+	}
+}
