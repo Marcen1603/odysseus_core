43d3be99843974e9ae1c3e3214a944da1161cd4f
Dennis Geesen <geesen@indivon.de>
2013-12-11 09:25:03 +0000

    rcp text coloring for sase
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17914 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d968a40e0eedb36e2e29d9c4f1a6ede14a7a4e62

---
+++ client/studio/rcp.editor.text.sase/src/de/uniol/inf/is/odysseus/rcp/editor/text/sase/SaseFormattingStrategy.java
@@ -0,0 +1,13 @@
+package de.uniol.inf.is.odysseus.rcp.editor.text.sase;
+
+import de.uniol.inf.is.odysseus.rcp.editor.text.editors.formatting.DefaultFormattingStrategy;
+
+public class SaseFormattingStrategy extends DefaultFormattingStrategy {	
+	
+	@Override
+	public String format(String content, boolean isLineStart, String indentation, int[] positions) {		
+		return null;		
+	}
+
+
+}
