3f482e25194c25b56df886aa9f548d0eb638a71b
Christian Kuka <christian@kuka.cc>
2013-12-12 06:18:23 +0000

    Probabilistic: Add completeness aggregation function for discrete and continuous distributions
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@17944 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 20934d8f113844f5559d2f4ff432b24124a7a176

---
+++ server/probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/continuous/physicaloperator/aggregationfunctions/ProbabilisticContinuousCompleteness.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package de.uniol.inf.is.odysseus.probabilistic.continuous.physicaloperator.aggregationfunctions;
+
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IPartialAggregate;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.functions.Completeness;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.functions.CompletenessPartialAggregate;
+import de.uniol.inf.is.odysseus.probabilistic.common.base.ProbabilisticTuple;
+import de.uniol.inf.is.odysseus.probabilistic.common.continuous.datatype.NormalDistributionMixture;
+import de.uniol.inf.is.odysseus.probabilistic.common.continuous.datatype.ProbabilisticContinuousDouble;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class ProbabilisticContinuousCompleteness extends Completeness<ProbabilisticTuple<?>, ProbabilisticTuple<?>> {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -5184003084875276744L;
+    private int pos;
+
+    private ProbabilisticContinuousCompleteness(int pos, boolean partialAggregateInput, String datatype) {
+        super(partialAggregateInput, datatype);
+        this.pos = pos;
+    }
+
+    public static ProbabilisticContinuousCompleteness getInstance(int pos, boolean partialAggregateInput, String datatype) {
+        return new ProbabilisticContinuousCompleteness(pos, partialAggregateInput, datatype);
+    }
+
+    @Override
+    public IPartialAggregate<ProbabilisticTuple<?>> init(ProbabilisticTuple<?> in) {
+        ProbabilisticContinuousDouble distribution = (ProbabilisticContinuousDouble) in.getAttribute(getPos());
+
+        IPartialAggregate<ProbabilisticTuple<?>> pa = new CompletenessPartialAggregate<ProbabilisticTuple<?>>(getCount(in.getDistribution(distribution.getDistribution())));
+        return pa;
+    }
+
+    @Override
+    public IPartialAggregate<ProbabilisticTuple<?>> merge(IPartialAggregate<ProbabilisticTuple<?>> p, ProbabilisticTuple<?> toMerge, boolean createNew) {
+        CompletenessPartialAggregate<ProbabilisticTuple<?>> pa = null;
+        if (createNew) {
+            pa = new CompletenessPartialAggregate<ProbabilisticTuple<?>>(((CompletenessPartialAggregate<ProbabilisticTuple<?>>) p));
+        }
+        else {
+            pa = (CompletenessPartialAggregate<ProbabilisticTuple<?>>) p;
+        }
+        ProbabilisticContinuousDouble distribution = (ProbabilisticContinuousDouble) toMerge.getAttribute(getPos());
+
+        pa.add(getCount(toMerge.getDistribution(distribution.getDistribution())));
+        return pa;
+    }
+
+    @Override
+    public ProbabilisticTuple<?> evaluate(IPartialAggregate<ProbabilisticTuple<?>> p) {
+        CompletenessPartialAggregate<ProbabilisticTuple<?>> pa = (CompletenessPartialAggregate<ProbabilisticTuple<?>>) p;
+
+        @SuppressWarnings("rawtypes")
+        ProbabilisticTuple r = new ProbabilisticTuple(1, false);
+        r.setAttribute(0, pa.getCompleteness());
+        return r;
+    }
+
+    /**
+     * @return the pos
+     */
+    private int getPos() {
+        return this.pos;
+    }
+
+    /**
+     * Gets the count value for a specific attribute value.
+     * 
+     * @param value
+     *            The atribute value
+     */
+    private double getCount(NormalDistributionMixture value) {
+        return 1.0 / value.getScale();
+    }
+}
---
+++ server/probabilistic/de.uniol.inf.is.odysseus.probabilistic/src/de/uniol/inf/is/odysseus/probabilistic/physicaloperator/aggregationfunctions/ProbabilisticTupleCompleteness.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package de.uniol.inf.is.odysseus.probabilistic.physicaloperator.aggregationfunctions;
+
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IPartialAggregate;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.functions.Completeness;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.functions.CompletenessPartialAggregate;
+import de.uniol.inf.is.odysseus.probabilistic.common.base.ProbabilisticTuple;
+import de.uniol.inf.is.odysseus.probabilistic.metadata.IProbabilistic;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class ProbabilisticTupleCompleteness extends Completeness<ProbabilisticTuple<?>, ProbabilisticTuple<?>> {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -1459891904889134576L;
+
+    private ProbabilisticTupleCompleteness(int pos, boolean partialAggregateInput, String datatype) {
+        super(partialAggregateInput, datatype);
+    }
+
+    public static ProbabilisticTupleCompleteness getInstance(int pos, boolean partialAggregateInput, String datatype) {
+        return new ProbabilisticTupleCompleteness(pos, partialAggregateInput, datatype);
+    }
+
+    @Override
+    public IPartialAggregate<ProbabilisticTuple<?>> init(ProbabilisticTuple<?> in) {
+        IPartialAggregate<ProbabilisticTuple<?>> pa = new CompletenessPartialAggregate<ProbabilisticTuple<?>>(getCount((IProbabilistic) in.getMetadata()));
+        return pa;
+    }
+
+    @Override
+    public IPartialAggregate<ProbabilisticTuple<?>> merge(IPartialAggregate<ProbabilisticTuple<?>> p, ProbabilisticTuple<?> toMerge, boolean createNew) {
+        CompletenessPartialAggregate<ProbabilisticTuple<?>> pa = null;
+        if (createNew) {
+            pa = new CompletenessPartialAggregate<ProbabilisticTuple<?>>(((CompletenessPartialAggregate<ProbabilisticTuple<?>>) p));
+        }
+        else {
+            pa = (CompletenessPartialAggregate<ProbabilisticTuple<?>>) p;
+        }
+        pa.add(getCount((IProbabilistic) toMerge.getMetadata()));
+        return pa;
+    }
+
+    @Override
+    public ProbabilisticTuple<?> evaluate(IPartialAggregate<ProbabilisticTuple<?>> p) {
+        CompletenessPartialAggregate<ProbabilisticTuple<?>> pa = (CompletenessPartialAggregate<ProbabilisticTuple<?>>) p;
+
+        @SuppressWarnings("rawtypes")
+        ProbabilisticTuple r = new ProbabilisticTuple(1, false);
+        r.setAttribute(0, pa.getCompleteness());
+        return r;
+    }
+
+    /**
+     * Gets the count value for a specific tuple value.
+     * 
+     * @param value
+     *            The tuple value
+     */
+    private double getCount(IProbabilistic value) {
+         return value.getExistence();
+    }
+}
