d46d988fc38d660cde21e047c8d531b2d6331d22
Michael Brand <michael.brand@uni-oldenburg.de>
2014-01-03 15:14:03 +0000

    The AbstractHorizontalFragmentationQueryPartModificator. Still to fix: Handling of aggregations
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18097 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 74cba6def0d738444f4da4cc3f531370604ba6f4

---
+++ server/peer/de.uniol.inf.is.odysseus.peer.distribute.modify.fragmentation.horizontal/src/de/uniol/inf/is/odysseus/peer/distribute/modify/fragmentation/horizontal/logicaloperator/AbstractFragmentAO.java
@@ -0,0 +1,79 @@
+package de.uniol.inf.is.odysseus.peer.distribute.modify.fragmentation.horizontal.logicaloperator;
+
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.AbstractLogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.UnaryLogicalOp;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.GetParameter;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.Parameter;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.LongParameter;
+
+/**
+ * A {@link AbstractFragmentAO} can be used to fragment incoming streams. <br />
+ * The {@link AbstractFragmentAO} has only one parameter for the number of fragments and it must have exact one input.
+ * @author Michael Brand
+ */
+public abstract class AbstractFragmentAO extends UnaryLogicalOp {
+	
+	private static final long serialVersionUID = -6789007084291408905L;
+
+	/**
+	 * The number of fragments.
+	 */
+	private long numFragments;
+	
+	/**
+	 * Constructs a new {@link AbstractFragmentAO}.
+	 * @see UnaryLogicalOp#UnaryLogicalOp()
+	 */
+	public AbstractFragmentAO() {
+		
+		super();
+		// no fragmentation per default
+		this.setNumberOfFragments(1);
+		
+	}
+
+	/**
+	 * Constructs a new {@link AbstractFragmentAO} as a copy of an existing one.
+	 * @param fragmentAO The {@link AbstractFragmentAO} to be copied.
+	 * @see UnaryLogicalOp#UnaryLogicalOp(AbstractLogicalOperator)
+	 */
+	public AbstractFragmentAO(AbstractFragmentAO fragmentAO) {
+		
+		super(fragmentAO);
+		this.numFragments = fragmentAO.numFragments;
+		
+	}
+
+	@Override
+	public abstract AbstractLogicalOperator clone();
+	
+	/**
+	 * Returns the number of fragments.
+	 */
+	@GetParameter(name = "FRAGMENTS")
+	public long getNumberOfFragments() {
+
+		return this.numFragments;
+		
+	}
+
+	/**
+	 * Sets the number of fragments.
+	 */
+	@Parameter(type = LongParameter.class, name = "FRAGMENTS", optional = false)
+	public void setNumberOfFragments(long numFragments) {
+		
+		this.numFragments = numFragments;
+		this.addParameterInfo("FRAGMENTS", numFragments);
+		
+	}
+	
+	@Override
+	protected SDFSchema getOutputSchemaIntern(int pos) {
+		
+		return super.getOutputSchemaIntern(0);
+		
+	}
+
+}
