20a53b59f7975abf34c37715180a10cd2f3aae38
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-01-07 11:56:47 +0000

    Peer: Removed old rcp-project
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18149 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7fe62fe704eb2f96ad797c9b4cf870cdbcda9b65

--- server/p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/util/IJxtaServerConnectionListener.java
+++
@@ -1,9 +0,0 @@
-package de.uniol.inf.is.odysseus.p2p_new.util;
-
-
-public interface IJxtaServerConnectionListener {
-
-	void connectionAdded( IJxtaServerConnection sender, IJxtaConnection addedConnection );
-	void connectionRemoved( IJxtaServerConnection sender, IJxtaConnection removedConnection );
-	
-}
