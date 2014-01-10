b6abe973654b2f979a92e797de2390a24ece314f
Christian Kuka <christian@kuka.cc>
2014-01-10 21:26:29 +0000

    MEP: Extend deprecated function from existing functions
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18224 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 24730ccb66dc2f1699baa85ab0b2340ed9e3c3b2

--- common/mep/src/de/uniol/inf/is/odysseus/mep/functions/math/GetAbsoluteValue.java
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/functions/math/GetAbsoluteValue.java
@@ -1,82 +1,86 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.mep.functions.math;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
-import de.uniol.inf.is.odysseus.mep.AbstractFunction;
 
 /*
  * Diese Klasse dient dazu, den Betrag eines Wertes zu erhalten.
  * @Deprecated Use AbsoluteFunction
  */
-@Deprecated
-public class GetAbsoluteValue extends AbstractFunction<Object>{
+public class GetAbsoluteValue extends AbsoluteFunction {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 5389590263951333045L;
+    private static final long serialVersionUID = 5389590263951333045L;
 
-	/*
-	 * Liefert die Anzahl der m�glichen �bergabeparameter zur�ck.
-	 */
-	@Override
-	public int getArity() {
-		return 1;
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(GetAbsoluteValue.class);
 
-	/*
-	 * Liefert den Namen zur�ck mit welchem die Funktion aufgerufen wird.
-	 */
-	@Override
-	public String getSymbol() {
-		return "AbsValue";
-	}
+    // /*
+    // * Liefert die Anzahl der m�glichen �bergabeparameter zur�ck.
+    // */
+    // @Override
+    // public int getArity() {
+    // return 1;
+    // }
 
-	/*
-	 * Liefert den Betrag eines �bergebenen Wertes zur�ck.
-	 */
-	@Override
-	public Object getValue() {
-		double d = (Double)getInputValue(0);
-		if(d < 0) {
-			return (d*(-1));
-		}
-        return d;
-	}
+    /*
+     * Liefert den Namen zur�ck mit welchem die Funktion aufgerufen wird.
+     */
+    @Override
+    public String getSymbol() {
+        return "AbsValue";
+    }
 
-	/*
-	 * Liefert den Klassentyp des Wertes der durch getValue() berechnet wird.
-	 */
-	@Override
-	public SDFDatatype getReturnType() {
-		return SDFDatatype.DOUBLE;
-	}
-	
-	public static final SDFDatatype[] accTypes = new SDFDatatype[] { SDFDatatype.INTEGER, SDFDatatype.LONG, SDFDatatype.DOUBLE, SDFDatatype.FLOAT};
-	
-	@Override
-	public SDFDatatype[] getAcceptedTypes(int argPos){
-		if(argPos < 0){
-			throw new IllegalArgumentException("negative argument index not allowed");
-		}
-		if(argPos > 0){
-			throw new IllegalArgumentException("AbsValue has only 1 argument.");
-		}
+    // /*
+    // * Liefert den Betrag eines �bergebenen Wertes zur�ck.
+    // */
+    // @Override
+    // public Object getValue() {
+    // double d = (Double)getInputValue(0);
+    // if(d < 0) {
+    // return (d*(-1));
+    // }
+    // return d;
+    // }
+
+    // /*
+    // * Liefert den Klassentyp des Wertes der durch getValue() berechnet wird.
+    // */
+    // @Override
+    // public SDFDatatype getReturnType() {
+    // return SDFDatatype.DOUBLE;
+    // }
+
+    public static final SDFDatatype[] accTypes = new SDFDatatype[] { SDFDatatype.INTEGER, SDFDatatype.LONG, SDFDatatype.DOUBLE, SDFDatatype.FLOAT };
+
+    @Override
+    public SDFDatatype[] getAcceptedTypes(int argPos) {
+        if (argPos < 0) {
+            throw new IllegalArgumentException("negative argument index not allowed");
+        }
+        if (argPos > 0) {
+            throw new IllegalArgumentException("AbsValue has only 1 argument.");
+        }
+        LOG.debug(getSymbol() + " is deprecated and will be removed in future releases. Please use " + super.getSymbol());
         return accTypes;
-	}
+    }
 
 }
--- common/mep/src/de/uniol/inf/is/odysseus/mep/functions/math/SqrtValue.java
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/functions/math/SqrtValue.java
@@ -1,80 +1,83 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.mep.functions.math;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
-import de.uniol.inf.is.odysseus.mep.AbstractFunction;
 
 /*
  * Diese Klasse dient dazu, aus einem �bergebenen Double-Wert die Wurzel zu ziehen.
  * @Deprecated Use SqrtFunction
  */
-@Deprecated
-public class SqrtValue extends AbstractFunction<Object>{
+public class SqrtValue extends SqrtFunction {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = -7488942114484992572L;
+    private static final long serialVersionUID = -7488942114484992572L;
+    private static final Logger LOG = LoggerFactory.getLogger(SqrtValue.class);
 
-	/*
-	 * Liefert die Anzahl der m�glichen �bergabeparameter zur�ck.
-	 */
-	@Override
-	public int getArity() {
-		return 1;
-	}
+    // /*
+    // * Liefert die Anzahl der m�glichen �bergabeparameter zur�ck.
+    // */
+    // @Override
+    // public int getArity() {
+    // return 1;
+    // }
 
-	/*
-	 * Liefert den Namen zur�ck mit welchem die Funktion aufgerufen wird.
-	 */
-	@Override
-	public String getSymbol() {
-		return "SqrtValue";
-	}
+    /*
+     * Liefert den Namen zur�ck mit welchem die Funktion aufgerufen wird.
+     */
+    @Override
+    public String getSymbol() {
+        return "SqrtValue";
+    }
 
-	/*
-	 * Liefert die Wurzel des �bergebenen Wertes zur�ck.
-	 */
-	@Override
-	public Object getValue() {
-		double d = Math.sqrt((Double)getInputValue(0));
-		//System.out.println("Gating: " + d);
-		return d;
-	}
+    // /*
+    // * Liefert die Wurzel des �bergebenen Wertes zur�ck.
+    // */
+    // @Override
+    // public Object getValue() {
+    // double d = Math.sqrt((Double)getInputValue(0));
+    // //System.out.println("Gating: " + d);
+    // return d;
+    // }
 
-	/*
-	 * Liefert den Klassentyp des Wertes der durch getValue() berechnet wird.
-	 */
-	@Override
-	public SDFDatatype getReturnType() {
-		return SDFDatatype.DOUBLE;
-	}
-	
-	public static final SDFDatatype[] accTypes = new SDFDatatype[]{SDFDatatype.INTEGER, SDFDatatype.LONG, SDFDatatype.DOUBLE, SDFDatatype.FLOAT};
-	
-	@Override
-	public SDFDatatype[] getAcceptedTypes(int argPos){
-		if(argPos < 0){
-			throw new IllegalArgumentException("negative argument index not allowed");
-		}
-		if(argPos > 0){
-			throw new IllegalArgumentException(this.getSymbol() + " has only " +this.getArity() + " argument(s).");
-		}
+    // /*
+    // * Liefert den Klassentyp des Wertes der durch getValue() berechnet wird.
+    // */
+    // @Override
+    // public SDFDatatype getReturnType() {
+    // return SDFDatatype.DOUBLE;
+    // }
+
+    public static final SDFDatatype[] accTypes = new SDFDatatype[] { SDFDatatype.INTEGER, SDFDatatype.LONG, SDFDatatype.DOUBLE, SDFDatatype.FLOAT };
+
+    @Override
+    public SDFDatatype[] getAcceptedTypes(int argPos) {
+        if (argPos < 0) {
+            throw new IllegalArgumentException("negative argument index not allowed");
+        }
+        if (argPos > 0) {
+            throw new IllegalArgumentException(this.getSymbol() + " has only " + this.getArity() + " argument(s).");
+        }
+        LOG.debug(getSymbol() + " is deprecated and will be removed in future releases. Please use " + super.getSymbol());
         return accTypes;
-	}
+    }
 
 }
