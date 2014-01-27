0f67a12c80d8e22cb3a1adecbcd123b139a988e9
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-01-27 13:40:17 +0000

    CostModel: Removed costmodel
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18431 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d86c79c625bbd57986ea60ea8e4090cdbd926609

--- server/costmodel/de.uniol.inf.is.odysseus.costmodel.operator.relational/src/de/uniol/inf/is/odysseus/costmodel/operator/relational/SelectEstimator.java
+++ server/costmodel/de.uniol.inf.is.odysseus.costmodel.operator.relational/src/de/uniol/inf/is/odysseus/costmodel/operator/relational/SelectEstimator.java
@@ -54,26 +54,9 @@ public class SelectEstimator implements IOperatorEstimator<SelectPO> {
 		/** 1. Histograms **/
 		IPredicate predicate = instance.getPredicate();
 
-//		System.out.println("SelectPO - INPUT");
-//		for( Map<SDFAttribute, IHistogram> map : histograms ) {
-//			for( SDFAttribute attribute : map.keySet() ) {
-//				System.out.println(attribute);
-//				System.out.println(map.get(attribute).toString());
-//				System.out.println();
-//			}
-//		}
-		
 		PredicateHistogramHelper helper = new PredicateHistogramHelper(predicate, histograms);
 		estimation.setHistograms(helper.getHistograms());
 		
-//		System.out.println();
-//		System.out.println("SelectPO - OUTPUT");
-//		for( SDFAttribute attribute : estimation.getHistograms().keySet() ) {
-//			System.out.println(attribute);
-//			System.out.println(estimation.getHistograms().get(attribute).toString());
-//			System.out.println();
-//		}
-
 		/** 2. Selectivity **/
 		double selectivity = EstimatorHelper.getSelectivityMetadata(instance);
 		if( selectivity < 0.0 ) {
@@ -82,14 +65,6 @@ public class SelectEstimator implements IOperatorEstimator<SelectPO> {
 		}
 		estimation.setSelectivity(selectivity);
 		
-		// DEBUG
-//		double selectivity = EstimatorHelper.getSelectivityMetadata(instance);
-//		System.out.print(selectivity + ", ");
-//		PredicateSelectivityHelper helper2 = new PredicateSelectivityHelper(predicate, histograms);
-//		double selectivity2 = helper2.getSelectivity();
-//		System.out.println(selectivity2);
-//		
-//		estimation.setSelectivity(selectivity < 0.0 ? selectivity2 : selectivity);
 		
 		/** 3. Datarate **/
 		IDataStream c = prevOperators.get(0).getDataStream();
@@ -98,14 +73,6 @@ public class SelectEstimator implements IOperatorEstimator<SelectPO> {
 			datarate = c.getDataRate() * selectivity;
 		}
 		estimation.setDataStream(new DataStream<SelectPO>(instance, datarate, c.getIntervalLength()));
-		
-//		IDataStream c = prevOperators.get(0).getDataStream();
-//		System.out.print(c.getDataRate() + ",");
-//		double datarate = EstimatorHelper.getDatarateMetadata(instance);
-//		System.out.print(datarate + ",");
-//		double datarate2 = c.getDataRate() * selectivity;
-//		System.out.println(datarate2);
-//		estimation.setDataStream(new DataStream(instance, datarate < 0.0 ? datarate2 : datarate, c.getIntervalLength()));
 
 		/** 4. DetailCost **/
 		double cpu = EstimatorHelper.getMedianCPUTimeMetadata(instance);
@@ -117,13 +84,6 @@ public class SelectEstimator implements IOperatorEstimator<SelectPO> {
 			CPURateSaver.getInstance().set(instance.getClass().getSimpleName(), cpu);
 		}
 		estimation.setDetailCost(new OperatorDetailCost<SelectPO>(instance, MemoryUsageSaver.get(instance), cpuCost));
-		
-//		double cpu = EstimatorHelper.getAvgCPUTimeMetadata(instance);
-//		double cpuCost = ACConstants.SELECT_CPU_COST_PER_TUPLE * c.getDataRate();
-//		double cpuCost2 = cpu * c.getDataRate();
-//		System.out.format("%-8.6f,", cpuCost2);
-//		System.out.format("%-8.6f%n", cpuCost);
-//		estimation.setDetailCost(new OperatorDetailCost(instance, ACConstants.STANDARD_MEM_COST, cpuCost2 >= 0.0 ? cpuCost2 : cpuCost));
 		
 		return estimation;
 	}
