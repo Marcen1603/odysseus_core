90c28709002b9bae45b0ed39e49d9d072c503122
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-02-04 09:46:58 +0000

    Refactoring; Cleanup/ Formatting
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18562 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: fcfc22bdf02074120f71e4b62bc8cbc6ffa7a8ab

--- server/script/script/src/de/uniol/inf/is/odysseus/script/parser/InputStatementParser.java
+++ server/script/script/src/de/uniol/inf/is/odysseus/script/parser/InputStatementParser.java
@@ -16,84 +16,84 @@ public class InputStatementParser {
 	private final String[] textToParse;
 	private final ReplacementContainer replacements;
 	private final List<String> unwrappingFiles = Lists.newArrayList();
-	
-	public InputStatementParser( String[] textToParse, ReplacementContainer replacements ) {
+
+	public InputStatementParser(String[] textToParse, ReplacementContainer replacements) {
 		Preconditions.checkNotNull(textToParse, "Text to check for %s must not be null!", INPUT_KEY);
 		Preconditions.checkNotNull(replacements, "Replacement container must not be null!");
-		
+
 		this.textToParse = textToParse;
 		this.replacements = replacements;
 	}
-	
+
 	public String[] unwrap() throws OdysseusScriptException, ReplacementException {
-		return unwrapImpl( textToParse );
+		return unwrapImpl(textToParse);
 	}
 
-	private String[] unwrapImpl( String[] text ) throws OdysseusScriptException, ReplacementException {
+	private String[] unwrapImpl(String[] text) throws OdysseusScriptException, ReplacementException {
 		List<String> resultText = Lists.newLinkedList();
-		
-		for( String textLine : text ) {
-			
+
+		for (String textLine : text) {
+
 			String line = textLine.trim();
-			if( line.startsWith(INPUT_KEY)) {
+			if (line.startsWith(INPUT_KEY)) {
 				line = replacements.use(line);
-				
+
 				String[] lineParts = line.split("\\ ", 2);
-				
-				if( lineParts.length != 2 ) {
+
+				if (lineParts.length != 2) {
 					throw new OdysseusScriptException("Misused " + INPUT_KEY + ": " + line);
 				}
-				
+
 				String fileName = lineParts[1].trim();
-				
+
 				File includingFile = new File(fileName);
-				if( !includingFile.exists() ) {
+				if (!includingFile.exists()) {
 					throw new OdysseusScriptException("File for " + INPUT_KEY + " '" + fileName + "' does not exist!");
 				}
-				
-				if( unwrappingFiles.contains(fileName)) {
+
+				if (unwrappingFiles.contains(fileName)) {
 					throw new OdysseusScriptException("Cyclic " + INPUT_KEY + " with file '" + fileName + "'");
 				}
-				
+
 				try {
 					unwrappingFiles.add(fileName);
 					String[] fileLines = readTextLinesFromFile(includingFile);
-					
+
 					String[] unwrappedFileLines = unwrapImpl(fileLines);
-					for( String unwrappedFileLine : unwrappedFileLines ) {
+					for (String unwrappedFileLine : unwrappedFileLines) {
 						resultText.add(unwrappedFileLine);
 					}
-					
+
 				} catch (IOException e) {
 					throw new OdysseusScriptException("Could not " + INPUT_KEY + " file '" + fileName + "'", e);
-					
+
 				} finally {
 					unwrappingFiles.remove(fileName);
 				}
-				
+
 			} else {
 				resultText.add(textLine);
 			}
 		}
-		
+
 		return resultText.toArray(new String[resultText.size()]);
 	}
-	
+
 	private static String[] readTextLinesFromFile(File includingFile) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(includingFile));
 		List<String> lines = Lists.newArrayList();
-		
-	    try {
-	        String line = br.readLine();
 
-	        while (line != null) {
-	        	lines.add(line);
-	            line = br.readLine();
-	        }
-	        
-	        return lines.toArray(new String[lines.size()]);
-	    } finally {
-	        br.close();
-	    }
+		try {
+			String line = br.readLine();
+
+			while (line != null) {
+				lines.add(line);
+				line = br.readLine();
+			}
+
+			return lines.toArray(new String[lines.size()]);
+		} finally {
+			br.close();
+		}
 	}
 }
