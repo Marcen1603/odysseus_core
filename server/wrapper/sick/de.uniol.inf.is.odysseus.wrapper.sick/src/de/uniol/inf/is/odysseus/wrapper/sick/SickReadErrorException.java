de2942d587465b22fd8817039cefd2030ff3a832
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-02-05 13:21:03 +0000

    Added Sick Wrapper
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18607 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 95b9326f47fd653e2031e0e7fba5a7c06aa124f1

---
+++ server/wrapper/sick/de.uniol.inf.is.odysseus.wrapper.sick/src/de/uniol/inf/is/odysseus/wrapper/sick/SickReadErrorException.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package de.uniol.inf.is.odysseus.wrapper.sick;
+
+/**
+ * @author ckuka
+ */
+public class SickReadErrorException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -6422352647398741043L;
+
+    /**
+     * 
+     */
+    public SickReadErrorException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public SickReadErrorException(final String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public SickReadErrorException(final Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public SickReadErrorException(final String message, final Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
