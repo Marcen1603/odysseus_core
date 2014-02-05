ce6a5bd2564b4fa984026ab101a83e1435347534
Juergen Boger <jboger@uni-oldenburg.de>
2014-02-05 09:00:52 +0000

    - NMEA Parser library working on Jython - Changed nmea wrapper (now working with NMEA Parser lib)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18593 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 34974b4b2e6078f8243e4f270f1790ad2499a4bb

--- server/wrapper/nmea/de.uniol.inf.is.odysseus.wrapper.nmea/src/de/uniol/inf/is/odysseus/wrapper/nmea/NMEAProtocolHandler.java
+++ server/wrapper/nmea/de.uniol.inf.is.odysseus.wrapper.nmea/src/de/uniol/inf/is/odysseus/wrapper/nmea/NMEAProtocolHandler.java
@@ -18,19 +18,17 @@ package de.uniol.inf.is.odysseus.wrapper.nmea;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.net.UnknownHostException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.impl.BasicEObjectImpl;
 
+import de.offis.nmea.python.PythonScript;
+import de.offis.nmea.sentence.Sentence;
 import de.uniol.inf.is.odysseus.core.collection.KeyValueObject;
 import de.uniol.inf.is.odysseus.core.datahandler.IDataHandler;
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
@@ -39,9 +37,6 @@ import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.IProtocolH
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.IAccessPattern;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportDirection;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportHandler;
-import de.uniol.inf.is.odysseus.wrapper.nmea.message.GGAMessage;
-import de.uniol.inf.is.odysseus.wrapper.nmea.message.GSVMessage;
-import de.uniol.inf.is.odysseus.wrapper.nmea.message.RMCMessage;
 
 /**
  * 
@@ -49,14 +44,8 @@ import de.uniol.inf.is.odysseus.wrapper.nmea.message.RMCMessage;
  * 
  */
 public class NMEAProtocolHandler extends AbstractProtocolHandler<KeyValueObject<? extends IMetaAttribute>> {
-    private static final Logger LOG = LoggerFactory.getLogger(NMEAProtocolHandler.class);
-    public static final Pattern GGA = Pattern.compile("^\\$GPGGA,([0-9.]+),([0-9.]+),(N|S),([0-9.]+),(E|W),([0-8]?),([0-9]+),([0-9.]*),([0-9.]+),M,([0-9.]+),M,,\\*([0-9A-Za-z]+)$");
-    public static final Pattern RMC = Pattern.compile("^\\$GPRMC,([0-9.]+),(A|V),([0-9.]+),(N|S),([0-9.]+),(E|W),([0-9.]+),([0-9.]+),([0-9]+),([0-9.]*),(E|W)?,(N|A|D|E)\\*([0-9A-Za-z]+)$");
-    public static final Pattern GSV = Pattern.compile("^\\$GPGSV,([1-3]+),([1-3]+),([0-9]+),([0-9,]*)\\*([0-9A-Za-z]+)$");
-    public static final Pattern GSVSatellite = Pattern.compile("([0-9]{1,2}),([0-9]+),([0-9]+),([0-9]*)");
-    private Date time;
-    private Date date;
     protected BufferedReader reader;
+    private KeyValueObject<? extends IMetaAttribute> next = null;
 
     public NMEAProtocolHandler() {
     }
@@ -79,85 +68,24 @@ public class NMEAProtocolHandler extends AbstractProtocolHandler<KeyValueObject<
 
     @Override
     public boolean hasNext() throws IOException {
+        if (!reader.ready()) {
+        	return false;
+        }
+        String nmea = reader.readLine();
+        Sentence sentence = (Sentence) PythonScript.getInstance().parseNmea(nmea);
+        if (sentence == null) {
+        	return false;
+        }
+        Map<String, Object> event = new HashMap<String, Object>();
+        toMap(event, "", (BasicEObjectImpl) sentence);
+        next = new KeyValueObject<>(event);
+        next.setMetadata("object", sentence);
         return true;
     }
 
     @Override
     public KeyValueObject<? extends IMetaAttribute> getNext() throws IOException {
-        String data = reader.readLine();
-        Map<String, Object> event = new HashMap<String, Object>();
-        Matcher ggaMatcher = GGA.matcher(data);
-        if (ggaMatcher.find()) {
-            GGAMessage message = new GGAMessage();
-            SimpleDateFormat sdf = new SimpleDateFormat("HHmmss.SSS");
-            try {
-                message.setTime(sdf.parse((!"".equals(ggaMatcher.group(1)) ? ggaMatcher.group(1) : "0")));
-                time = message.getTime();
-            }
-            catch (ParseException e) {
-                LOG.error(e.getMessage(), e);
-            }
-            message.setLatitude(new Double((!"".equals(ggaMatcher.group(2)) ? ggaMatcher.group(2) : "0.0")));
-            message.setLongitude(new Double((!"".equals(ggaMatcher.group(4)) ? ggaMatcher.group(4) : "0.0")));
-            message.setQuality(new BigDecimal((!"".equals(ggaMatcher.group(6)) ? ggaMatcher.group(6) : "0")));
-            message.setSatellites(new Integer((!"".equals(ggaMatcher.group(7)) ? ggaMatcher.group(7) : "0")));
-            message.setDilution(new Double((!"".equals(ggaMatcher.group(8)) ? ggaMatcher.group(8) : "0.0")));
-            message.setAltitude(new Double((!"".equals(ggaMatcher.group(9)) ? ggaMatcher.group(9) : "0.0")));
-            message.setHeight(new Double((!"".equals(ggaMatcher.group(10)) ? ggaMatcher.group(10) : "0.0")));
-            message.setChecksum(ggaMatcher.group(11));
-            event = message.toMap();
-        }
-        Matcher rmcMatcher = RMC.matcher(data);
-        if (rmcMatcher.find()) {
-            RMCMessage message = new RMCMessage();
-            SimpleDateFormat sdfTime = new SimpleDateFormat("HHmmss.SSS");
-            SimpleDateFormat sdfDate = new SimpleDateFormat("ddMMyy");
-            try {
-                message.setTime(sdfTime.parse((!"".equals(rmcMatcher.group(1)) ? rmcMatcher.group(1) : "0.0")));
-                time = message.getTime();
-            }
-            catch (ParseException e) {
-                LOG.error(e.getMessage(), e);
-            }
-            message.setStatus(rmcMatcher.group(2));
-            message.setLatitude(new Double((!"".equals(rmcMatcher.group(3)) ? rmcMatcher.group(3) : "0.0")));
-            message.setLongitude(new Double((!"".equals(rmcMatcher.group(5)) ? rmcMatcher.group(5) : "0.0")));
-            message.setSpeed(new Double((!"".equals(rmcMatcher.group(7)) ? rmcMatcher.group(7) : "0.0")));
-            message.setCourse(new Double((!"".equals(rmcMatcher.group(8)) ? rmcMatcher.group(8) : "0.0")));
-            try {
-                message.setDate(sdfDate.parse((!"".equals(rmcMatcher.group(9)) ? rmcMatcher.group(9) : "0")));
-                date = message.getDate();
-            }
-            catch (ParseException e) {
-                LOG.error(e.getMessage(), e);
-            }
-            message.setVariation(new Double((!"".equals(rmcMatcher.group(10)) ? rmcMatcher.group(10) : "0.0")));
-            message.setMode(rmcMatcher.group(12));
-            message.setChecksum(rmcMatcher.group(13));
-            event = message.toMap();
-        }
-        Matcher gsvMatcher = GSV.matcher(data);
-        if (gsvMatcher.find()) {
-            GSVMessage message = new GSVMessage();
-            message.setTotal(new Integer((!"".equals(gsvMatcher.group(1)) ? gsvMatcher.group(1) : "0")));
-            message.setCount(new Integer((!"".equals(gsvMatcher.group(2)) ? gsvMatcher.group(2) : "0")));
-            Integer satellites = new Integer((!"".equals(gsvMatcher.group(3)) ? gsvMatcher.group(3) : "0"));
-
-            String satelliteList = gsvMatcher.group(4);
-            Matcher gsvSatelliteMatcher = GSVSatellite.matcher(satelliteList);
-            if (gsvSatelliteMatcher.find()) {
-                Integer satelliteId = new Integer((!"".equals(gsvSatelliteMatcher.group(1)) ? gsvSatelliteMatcher.group(1) : "0"));
-                Integer elevation = new Integer((!"".equals(gsvSatelliteMatcher.group(2)) ? gsvSatelliteMatcher.group(2) : "0"));
-                Integer azimuth = new Integer((!"".equals(gsvSatelliteMatcher.group(3)) ? gsvSatelliteMatcher.group(3) : "0"));
-                Integer snr = new Integer((!"".equals(gsvSatelliteMatcher.group(4)) ? gsvSatelliteMatcher.group(4) : "0"));
-                message.addSatellite(satelliteId, elevation, azimuth, snr);
-            }
-            message.setSatellitesNum(satellites);
-            message.setChecksum(gsvMatcher.group(5));
-            event = message.toMap();
-        }
-        event.put("timestamp", date.getTime() + time.getTime());
-        return new KeyValueObject<>(event);
+        return next;
     }
 
     @Override
@@ -201,5 +129,29 @@ public class NMEAProtocolHandler extends AbstractProtocolHandler<KeyValueObject<
             return true;
         }
     }
-
+    
+    public static Map<String, Object> toMap(Map<String, Object> res, String baseName, BasicEObjectImpl eObj) {
+		for (EAttribute attr : eObj.eClass().getEAllAttributes()) {
+			if (attr.getName().equals("nmea")) {
+				continue;
+			}
+			int id = attr.getFeatureID();
+			Object o = eObj.eGet(id, true, true);
+			if (attr.getEType() instanceof EEnum) {
+				res.put(baseName + attr.getName(), o.toString());
+			} else {
+				res.put(baseName + attr.getName(), o);
+			}
+		}
+		
+		for (EReference ref : eObj.eClass().getEAllReferences()) {
+			int id = ref.getFeatureID();
+			Object o = eObj.eGet(id, true, true);
+			if (o == null || !(o instanceof BasicEObjectImpl)) {
+				continue;
+			}
+			toMap(res, ref.getName() + ".", (BasicEObjectImpl) o);
+		}
+		return res;
+	}
 }
