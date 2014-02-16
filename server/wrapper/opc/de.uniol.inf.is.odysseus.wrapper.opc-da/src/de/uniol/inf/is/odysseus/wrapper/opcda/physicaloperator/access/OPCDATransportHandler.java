10587bc44e0156f4bdb944e7ca6b889bce80711d
Christian Kuka <christian@kuka.cc>
2014-02-16 14:29:04 +0000

    Wrapper: Add clsId to OPC wrapper
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18740 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 93105e739d30bd69f5f97ce76d655ab4f06ffde1

--- server/wrapper/opc/de.uniol.inf.is.odysseus.wrapper.opc-da/src/de/uniol/inf/is/odysseus/wrapper/opcda/physicaloperator/access/OPCDATransportHandler.java
+++ server/wrapper/opc/de.uniol.inf.is.odysseus.wrapper.opc-da/src/de/uniol/inf/is/odysseus/wrapper/opcda/physicaloperator/access/OPCDATransportHandler.java
@@ -21,7 +21,6 @@ import java.io.OutputStream;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.Executors;
 
 import org.jinterop.dcom.common.JIException;
@@ -53,218 +52,229 @@ import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
  * 
  * @author Christian Kuka <christian@kuka.cc>
  */
-public class OPCDATransportHandler<T> extends AbstractTransportHandler implements
-		IIteratable<Tuple<IMetaAttribute>> {
-	/** Logger */
-	private final Logger LOG = LoggerFactory
-			.getLogger(OPCDATransportHandler.class);
+public class OPCDATransportHandler<T> extends AbstractTransportHandler implements IIteratable<Tuple<IMetaAttribute>> {
+    /** Logger */
+    private final Logger LOG = LoggerFactory.getLogger(OPCDATransportHandler.class);
 
-	private String host;
+    private String host;
 
-	private String domain;
+    private String domain;
 
-	private String username;
+    private String username;
 
-	private String password;
+    private String password;
 
-	private String id;
+    private String progId;
+    private String clsId;
 
-	private Server server;
-	private AccessBase access;
+    private Server server;
+    private AccessBase access;
 
-	private List<Tuple<IMetaAttribute>> read = new LinkedList<>();
+    private List<Tuple<IMetaAttribute>> read = new LinkedList<>();
 
-	public OPCDATransportHandler() {
-	}
+    public OPCDATransportHandler() {
+    }
 
-	public OPCDATransportHandler(IProtocolHandler<?> protocolHandler,
-			Map<String, String> options) {
-		super(protocolHandler);
-		setOptionsMap(options);
-	}
+    public OPCDATransportHandler(IProtocolHandler<?> protocolHandler, Map<String, String> options) {
+        super(protocolHandler);
+        setOptionsMap(options);
+    }
 
-	@Override
-	public String getName() {
-		return "OPC";
-	}
+    @Override
+    public String getName() {
+        return "OPC";
+    }
 
-	@Override
-	public void setOptionsMap(Map<String, String> options) {
-		super.setOptionsMap(options);
-		this.host = options.containsKey("host") ? options.get("host")
-				: "localhost";
-		this.domain = options.containsKey("domain") ? options.get("domain")
-				: "";
-		this.username = options.containsKey("username") ? options
-				.get("username") : "";
-		this.password = options.containsKey("password") ? options
-				.get("password") : "";
-		this.id = options.containsKey("id") ? options.get("id") : UUID
-				.randomUUID().toString();
-	}
+    @Override
+    public void setOptionsMap(Map<String, String> options) {
+        super.setOptionsMap(options);
+        this.host = options.containsKey("host") ? options.get("host") : "localhost";
+        this.domain = options.containsKey("domain") ? options.get("domain") : "";
+        this.username = options.containsKey("username") ? options.get("username") : null;
+        this.password = options.containsKey("password") ? options.get("password") : null;
+        this.progId = options.containsKey("progId") ? options.get("progId") : null;
+        this.clsId = options.containsKey("clsId") ? options.get("clsId") : null;
+    }
 
-	@Override
-	public void send(byte[] message) throws IOException {
-		throw new IllegalArgumentException("Not implemented");
-	}
+    @Override
+    public void send(byte[] message) throws IOException {
+        throw new IllegalArgumentException("Not implemented");
+    }
 
-	@Override
-	public ITransportHandler createInstance(
-			IProtocolHandler<?> protocolHandler, Map<String, String> options) {
-		return new OPCDATransportHandler<T>(protocolHandler, options);
-	}
+    @Override
+    public ITransportHandler createInstance(IProtocolHandler<?> protocolHandler, Map<String, String> options) {
+        return new OPCDATransportHandler<T>(protocolHandler, options);
+    }
 
-	@Override
-	public OutputStream getOutputStream() {
-		throw new IllegalArgumentException("Not implemented");
-	}
+    @Override
+    public OutputStream getOutputStream() {
+        throw new IllegalArgumentException("Not implemented");
+    }
 
-	@Override
-	public InputStream getInputStream() {
-		throw new IllegalArgumentException("Not implemented");
-	}
+    @Override
+    public InputStream getInputStream() {
+        throw new IllegalArgumentException("Not implemented");
+    }
 
-	@Override
-	public void processInOpen() throws IOException {
-		final ConnectionInformation ci = new ConnectionInformation();
-		ci.setHost(getHost());
-		ci.setDomain(getDomain());
-		ci.setUser(getUsername());
-		ci.setPassword(getPassword());
-		ci.setProgId(getId());
-		server = new Server(ci, Executors.newSingleThreadScheduledExecutor());
-		try {
-			server.connect();
-			access = new SyncAccess(server, 500);
-			for (SDFAttribute attribute : this.getSchema()) {
-				final int position = getSchema().indexOf(attribute);
-				access.addItem(attribute.getAttributeName(),
-						new DataCallback() {
+    @Override
+    public void processInOpen() throws IOException {
+        final ConnectionInformation ci = new ConnectionInformation();
+        ci.setHost(getHost());
+        if (getDomain() != null) {
+            ci.setDomain(getDomain());
+        }
+        if (getUsername() != null) {
+            ci.setUser(getUsername());
+        }
+        if (getPassword() != null) {
+            ci.setPassword(getPassword());
+        }
+        if (getProgId() != null) {
+            ci.setProgId(getProgId());
+        }
+        if (getClsId() != null) {
+            ci.setClsid(getClsId());
+        }
+        server = new Server(ci, Executors.newSingleThreadScheduledExecutor());
+        try {
+            server.connect();
+            access = new SyncAccess(server, 500);
+            for (SDFAttribute attribute : this.getSchema()) {
+                final int position = getSchema().indexOf(attribute);
+                access.addItem(attribute.getAttributeName(), new DataCallback() {
 
-							@Override
-							public void changed(Item item, ItemState state) {
+                    @Override
+                    public void changed(Item item, ItemState state) {
 
-								try {
-									if ((state.getValue().getType() == JIVariant.VT_UI1)
-											|| (state.getValue().getType() == JIVariant.VT_UI2)
-											|| (state.getValue().getType() == JIVariant.VT_UI4)
-											|| (state.getValue().getType() == JIVariant.VT_UINT)) {
-										process(position, state.getValue()
-												.getObjectAsUnsigned()
-												.getValue().intValue());
+                        try {
+                            if ((state.getValue().getType() == JIVariant.VT_UI1) || (state.getValue().getType() == JIVariant.VT_UI2) || (state.getValue().getType() == JIVariant.VT_UI4)
+                                    || (state.getValue().getType() == JIVariant.VT_UINT)) {
+                                process(position, state.getValue().getObjectAsUnsigned().getValue().intValue());
 
-									} else if ((state.getValue().getType() == JIVariant.VT_I1)
-											|| (state.getValue().getType() == JIVariant.VT_I2)
-											|| (state.getValue().getType() == JIVariant.VT_I4)
-											|| (state.getValue().getType() == JIVariant.VT_I8)
-											|| (state.getValue().getType() == JIVariant.VT_INT)) {
+                            }
+                            else if ((state.getValue().getType() == JIVariant.VT_I1) || (state.getValue().getType() == JIVariant.VT_I2) || (state.getValue().getType() == JIVariant.VT_I4)
+                                    || (state.getValue().getType() == JIVariant.VT_I8) || (state.getValue().getType() == JIVariant.VT_INT)) {
 
-										process(position, state.getValue()
-												.getObjectAsUnsigned()
-												.getValue().intValue());
-									} else {
-										process(position, state.getValue()
-												.getObject());
-									}
-								} catch (JIException e) {
-									LOG.error(e.getMessage(), e);
-								}
-							}
-						});
-			}
-			access.bind();
-		} catch (final JIException e) {
-			this.server.dispose();
-			LOG.error(String.format("%08X: %s", e.getErrorCode(),
-					server.getErrorMessage(e.getErrorCode())));
-			throw new IOException(server.getErrorMessage(e.getErrorCode()));
-		} catch (IllegalArgumentException | NotConnectedException
-				| DuplicateGroupException | AddFailedException e) {
-			this.server.dispose();
-			LOG.error(e.getMessage(), e);
-			throw new IOException(e);
-		} catch (AlreadyConnectedException e) {
-			LOG.warn(e.getMessage(), e);
-		}
-	}
+                                process(position, state.getValue().getObjectAsUnsigned().getValue().intValue());
+                            }
+                            else {
+                                process(position, state.getValue().getObject());
+                            }
+                        }
+                        catch (JIException e) {
+                            LOG.error(e.getMessage(), e);
+                        }
+                    }
+                });
+            }
+            access.bind();
+        }
+        catch (final JIException e) {
+            this.server.dispose();
+            LOG.error(String.format("%08X: %s", e.getErrorCode(), server.getErrorMessage(e.getErrorCode())));
+            throw new IOException(server.getErrorMessage(e.getErrorCode()));
+        }
+        catch (IllegalArgumentException | NotConnectedException | DuplicateGroupException | AddFailedException e) {
+            this.server.dispose();
+            LOG.error(e.getMessage(), e);
+            throw new IOException(e);
+        }
+        catch (AlreadyConnectedException e) {
+            LOG.warn(e.getMessage(), e);
+        }
+    }
 
-	private void process(int pos, Object value) {
-		LOG.debug(String.format("%d: %s", pos, value));
-		Tuple<IMetaAttribute> curTuple = null;
-		if (read.size() > 0) {
-			curTuple = read.get(read.size() - 1);
-			if (curTuple.getAttribute(pos) != null) {
-				curTuple = null;
-			}
-		}
-		if (curTuple == null) {
-			curTuple = new Tuple<>(this.getSchema().size(), false);
-			read.add(curTuple);
-		}
-		curTuple.setAttribute(pos, value);
+    private void process(int pos, Object value) {
+        LOG.debug(String.format("%d: %s", pos, value));
+        Tuple<IMetaAttribute> curTuple = null;
+        if (read.size() > 0) {
+            curTuple = read.get(read.size() - 1);
+            if (curTuple.getAttribute(pos) != null) {
+                curTuple = null;
+            }
+        }
+        if (curTuple == null) {
+            curTuple = new Tuple<>(this.getSchema().size(), false);
+            read.add(curTuple);
+        }
+        curTuple.setAttribute(pos, value);
 
-	}
+    }
 
-	@Override
-	public void processInClose() throws IOException {
-		try {
-			if (access != null) {
-				access.unbind();
-			}
-		} catch (JIException e) {
-			LOG.error(e.getMessage(), e);
-			throw new IOException(e);
-		} finally {
-			if (this.server != null) {
-				this.server.disconnect();
-				this.server.dispose();
-			}
-		}
-	}
+    @Override
+    public void processInClose() throws IOException {
+        try {
+            if (access != null) {
+                access.unbind();
+            }
+        }
+        catch (JIException e) {
+            LOG.error(e.getMessage(), e);
+            throw new IOException(e);
+        }
+        finally {
+            if (this.server != null) {
+                this.server.disconnect();
+                this.server.dispose();
+            }
+        }
+    }
 
-	@Override
-	public void processOutOpen() throws IOException {
-		throw new IllegalArgumentException("Not implemented");
-	}
+    @Override
+    public void processOutOpen() throws IOException {
+        throw new IllegalArgumentException("Not implemented");
+    }
 
-	@Override
-	public void processOutClose() throws IOException {
-		throw new IllegalArgumentException("Not implemented");
-	}
+    @Override
+    public void processOutClose() throws IOException {
+        throw new IllegalArgumentException("Not implemented");
+    }
 
-	@Override
-	public boolean hasNext() {
-		return read.size() > 0;
-	}
+    @Override
+    public boolean hasNext() {
+        return read.size() > 0;
+    }
 
-	@Override
-	public Tuple<IMetaAttribute> getNext() {
-		return read.remove(0);
-	}
+    @Override
+    public Tuple<IMetaAttribute> getNext() {
+        return read.remove(0);
+    }
 
-	public String getHost() {
-		return this.host;
-	}
+    public String getHost() {
+        return this.host;
+    }
 
-	public String getDomain() {
-		return this.domain;
-	}
+    public String getDomain() {
+        return this.domain;
+    }
 
-	public String getUsername() {
-		return this.username;
-	}
+    public String getUsername() {
+        return this.username;
+    }
 
-	public String getPassword() {
-		return this.password;
-	}
+    public String getPassword() {
+        return this.password;
+    }
 
-	public String getId() {
-		return this.id;
-	}
+    /**
+     * 
+     * @return the progId
+     */
+    public String getProgId() {
+        return this.progId;
+    }
 
-	@Override
-	public boolean isSemanticallyEqualImpl(ITransportHandler other) {
-		return false;
-	}
+    /**
+     * @return the clsId
+     */
+    public String getClsId() {
+        return this.clsId;
+    }
 
+    @Override
+    public boolean isSemanticallyEqualImpl(ITransportHandler other) {
+        return false;
+    }
+
+   
 }
