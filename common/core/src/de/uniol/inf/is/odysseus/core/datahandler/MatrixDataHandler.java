1f96f2355c21fc1b0e96e3176a263cf3d9efcc8c
Christian Kuka <christian@kuka.cc>
2014-02-24 01:46:59 +0000

    Core: Add matrix and vector data handler
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18822 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: d98130d38e1d4926f3232c5ebe9de434ac44b32d

---
+++ common/core/src/de/uniol/inf/is/odysseus/core/datahandler/MatrixDataHandler.java
@@ -0,0 +1,115 @@
+/**
+ * 
+ */
+package de.uniol.inf.is.odysseus.core.datahandler;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class MatrixDataHandler extends AbstractDataHandler<double[][]> {
+    static protected List<String> types = new ArrayList<String>();
+    static {
+        types.add(SDFDatatype.MATRIX_DOUBLE.getURI());
+    }
+
+    private IDataHandler<double[]> handler = new VectorDataHandler();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double[][] readData(ByteBuffer buffer) {
+        int size = buffer.getInt();
+        double[][] value = new double[size][];
+        for (int i = 0; i < size; i++) {
+            value[i] = this.handler.readData(buffer);
+        }
+        return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double[][] readData(ObjectInputStream inputStream) throws IOException {
+        int size = inputStream.readInt();
+        double[][] value = new double[size][];
+        for (int i = 0; i < size; i++) {
+            value[i] = this.handler.readData(inputStream);
+        }
+        return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double[][] readData(String string) {
+        String[] lines = string.split("\n");
+        int size = lines.length;
+        double[][] value = new double[size][];
+        for (int i = 0; i < size; i++) {
+            value[i] = this.handler.readData(lines[i]);
+        }
+        return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeData(ByteBuffer buffer, Object data) {
+        double[][] value = (double[][]) data;
+        buffer.putInt(value.length);
+        for (double[] v : value) {
+            this.handler.writeData(buffer, v);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int memSize(Object data) {
+        int size = 0;
+        double[][] value = (double[][]) data;
+        for (double[] v : value) {
+            size += this.handler.memSize(v);
+        }
+        return size;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Class<?> createsType() {
+        return double[][].class;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected IDataHandler<double[][]> getInstance(SDFSchema schema) {
+        return new MatrixDataHandler();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> getSupportedDataTypes() {
+        return types;
+    }
+}
