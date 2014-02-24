53db55977d58282091bb58c288e78bc3264e7e1d
Christian Kuka <christian@kuka.cc>
2014-02-24 13:17:22 +0000

    Core: Fix read function for strings in vector and matrix handler
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18844 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 001a45a80cf0d288124608ca2ade6a8e2dc583f3

--- common/core/src/de/uniol/inf/is/odysseus/core/datahandler/FloatDataHandler.java
+++ common/core/src/de/uniol/inf/is/odysseus/core/datahandler/FloatDataHandler.java
@@ -1,5 +1,17 @@
-/**
- * 
+/********************************************************************************** 
+ * Copyright 2014 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package de.uniol.inf.is.odysseus.core.datahandler;
 
@@ -58,7 +70,6 @@ public class FloatDataHandler extends AbstractDataHandler<Float> {
 
     @Override
     public void writeData(ByteBuffer buffer, Object data) {
-        // System.out.println("write Double Data "+(Double)data);
         buffer.putFloat(((Number) data).floatValue());
     }
 
--- common/core/src/de/uniol/inf/is/odysseus/core/datahandler/MatrixDataHandler.java
+++ common/core/src/de/uniol/inf/is/odysseus/core/datahandler/MatrixDataHandler.java
@@ -1,5 +1,17 @@
-/**
- * 
+/********************************************************************************** 
+ * Copyright 2014 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package de.uniol.inf.is.odysseus.core.datahandler;
 
@@ -55,13 +67,18 @@ public class MatrixDataHandler extends AbstractDataHandler<double[][]> {
      */
     @Override
     public double[][] readData(String string) {
-        String[] lines = string.split("\n");
-        int size = lines.length;
-        double[][] value = new double[size][];
-        for (int i = 0; i < size; i++) {
-            value[i] = this.handler.readData(lines[i]);
+        if (string != null && string.length() > 0) {
+            String[] lines = string.split(";");
+            int size = lines.length;
+            double[][] value = new double[size][];
+            for (int i = 0; i < size; i++) {
+                value[i] = this.handler.readData(lines[i]);
+            }
+            return value;
         }
-        return value;
+        else {
+            return null;
+        }
     }
 
     /**
