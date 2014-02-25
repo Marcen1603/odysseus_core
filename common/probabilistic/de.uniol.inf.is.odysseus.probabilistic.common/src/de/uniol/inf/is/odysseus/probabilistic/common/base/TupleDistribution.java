72f9095399191bf3498585de407c928b542f47cb
Christian Kuka <christian@kuka.cc>
2014-02-25 14:18:55 +0000

    Probabilistic: Start work on generic distribution layer
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18878 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5751b51610cb37dce48cfb60ed125204fbea1ec6

---
+++ common/probabilistic/de.uniol.inf.is.odysseus.probabilistic.common/src/de/uniol/inf/is/odysseus/probabilistic/common/base/TupleDistribution.java
@@ -0,0 +1,193 @@
+/**
+ * 
+ */
+package de.uniol.inf.is.odysseus.probabilistic.common.base;
+
+import java.util.Arrays;
+
+import org.apache.commons.math3.distribution.MixtureMultivariateRealDistribution;
+import org.apache.commons.math3.distribution.MultivariateRealDistribution;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import com.google.common.base.Preconditions;
+
+import de.uniol.inf.is.odysseus.probabilistic.common.Interval;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class TupleDistribution {
+    /** The attribute positions. */
+    private int[] attributes;
+    /** The scale. */
+    private double scale;
+    /** The support for each dimension. */
+    private Interval[] support;
+    /** The distribution. */
+    private ExtendedMixtureMultivariateRealDistribution<? extends MultivariateRealDistribution> distribution;
+
+    /**
+     * Gets the dimension of this distribution mixture.
+     * 
+     * @return The dimension
+     */
+    public final int getDimension() {
+        return this.attributes.length;
+    }
+
+    /**
+     * Gets the value of the scale property.
+     * 
+     * @return the scale
+     */
+    public final double getScale() {
+        return this.scale;
+    }
+
+    /**
+     * Gets the value of the support property in the given dimension.
+     * 
+     * @param dimension
+     *            the dimension
+     * @return the support
+     */
+    public final Interval getSupport(final int dimension) {
+        Preconditions.checkPositionIndex(dimension, this.support.length);
+        return this.support[dimension];
+    }
+
+    /**
+     * Gets the value of the support property.
+     * 
+     * @return the support
+     */
+    public final Interval[] getSupport() {
+        return this.support;
+    }
+
+    /**
+     * Gets the value of the attributes property in the given dimension.
+     * 
+     * @param dimension
+     *            the dimension
+     * @return the attributes
+     */
+    public final int getAttribute(final int dimension) {
+        Preconditions.checkPositionIndex(dimension, this.attributes.length);
+        return this.attributes[dimension];
+    }
+
+    /**
+     * Gets the value of the attributes property.
+     * 
+     * @return the attributes
+     */
+    public final int[] getAttributes() {
+        return this.attributes;
+    }
+
+    public TupleDistribution restrict(RealMatrix restrictMatrix) {
+        return null;
+    }
+
+    /**
+     * For a random variable {@code X} whose values are distributed according
+     * to this distribution, this method returns {@code P(X = x)}. In other
+     * words, this method represents the probability mass function (PMF)
+     * for the distribution.
+     * 
+     * @param x
+     *            the point at which the PMF is evaluated
+     * @return the value of the probability mass function at point {@code x}
+     */
+    public  double probability(double[] x){
+    //   return distribution.probability(x);
+        return 0d;
+    }
+
+    /**
+     * Returns the probability density function (PDF) of this distribution
+     * evaluated at the specified point {@code x}. In general, the PDF is
+     * the derivative of the {@link #cumulativeProbability(double) CDF}.
+     * If the derivative does not exist at {@code x}, then an appropriate
+     * replacement should be returned, e.g. {@code Double.POSITIVE_INFINITY},
+     * {@code Double.NaN}, or the limit inferior or limit superior of the
+     * difference quotient.
+     * 
+     * @param x
+     *            the point at which the PDF is evaluated
+     * @return the value of the probability density function at point {@code x}
+     */
+    public double density(double[] x) {
+        return distribution.density(x);
+    }
+
+    /**
+     * For a random variable {@code X} whose values are distributed according
+     * to this distribution, this method returns {@code P(X <= x)}. In other
+     * words, this method represents the (cumulative) distribution function
+     * (CDF) for this distribution.
+     * 
+     * @param x
+     *            the point at which the CDF is evaluated
+     * @return the probability that a random variable with this
+     *         distribution takes a value less than or equal to {@code x}
+     */
+    public double cumulativeProbability(double[] x) {
+//        return distribution.cumulativeProbability(x);
+        return 0d;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(this.attributes);
+        result = prime * result + ((this.distribution == null) ? 0 : this.distribution.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(this.scale);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + Arrays.hashCode(this.support);
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        TupleDistribution other = (TupleDistribution) obj;
+        if (!Arrays.equals(this.attributes, other.attributes)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.scale) != Double.doubleToLongBits(other.scale)) {
+            return false;
+        }
+        if (!Arrays.equals(this.support, other.support)) {
+            return false;
+        }
+        if (this.distribution == null) {
+            if (other.distribution != null) {
+                return false;
+            }
+        }
+        else if (!this.distribution.equals(other.distribution)) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ common/probabilistic/de.uniol.inf.is.odysseus.probabilistic.common/src/de/uniol/inf/is/odysseus/probabilistic/common/discrete/datatype/MultivariateEnumeratedRealDistribution.java
@@ -0,0 +1,233 @@
+/**
+ * 
+ */
+package de.uniol.inf.is.odysseus.probabilistic.common.discrete.datatype;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.math3.distribution.AbstractMultivariateRealDistribution;
+import org.apache.commons.math3.distribution.EnumeratedDistribution;
+import org.apache.commons.math3.random.RandomGenerator;
+import org.apache.commons.math3.random.Well19937c;
+import org.apache.commons.math3.util.Pair;
+
+import de.uniol.inf.is.odysseus.probabilistic.common.base.IMultivariateRealDistribution;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class MultivariateEnumeratedRealDistribution extends AbstractMultivariateRealDistribution implements IMultivariateRealDistribution {
+    protected final EnumeratedDistribution<double[]> innerDistribution;
+
+    /**
+     * 
+     * @param singletons
+     * @param probabilities
+     */
+    public MultivariateEnumeratedRealDistribution(final double[][] singletons, final double[] probabilities) {
+        this(new Well19937c(), singletons, probabilities);
+    }
+
+    /**
+     * 
+     * @param rng
+     * @param singletons
+     * @param probabilities
+     */
+    public MultivariateEnumeratedRealDistribution(RandomGenerator rng, final double[][] singletons, final double[] probabilities) {
+        super(rng, singletons.length);
+        if (singletons[0].length != probabilities.length) {
+            // throw new DimensionMismatchException(probabilities.length,
+            // singletons.length);
+        }
+
+        List<Pair<double[], Double>> samples = new ArrayList<Pair<double[], Double>>(singletons.length);
+
+        for (int i = 0; i < singletons.length; i++) {
+            samples.add(new Pair<double[], Double>(singletons[i], probabilities[i]));
+        }
+
+        innerDistribution = new EnumeratedDistribution<double[]>(rng, samples);
+    }
+
+    public double probability(final double[] a) {
+        double probability = 0;
+
+        for (final Pair<double[], Double> sample : innerDistribution.getPmf()) {
+            boolean inBound = true;
+            for (int d = 0; d < a.length; d++) {
+                if (sample.getKey()[d] > a[d]) {
+                    inBound = false;
+                    break;
+                }
+            }
+            if (inBound) {
+                probability += sample.getValue();
+            }
+        }
+
+        return probability;
+    }
+
+    public double probability(final double[] a, final double[] b) {
+        double probability = 0;
+        if (Arrays.equals(a, b)) {
+            // This is the PMF
+            for (final Pair<double[], Double> sample : innerDistribution.getPmf()) {
+                if ((a == null && sample.getKey() == null) || (a != null && a.equals(sample.getKey()))) {
+                    probability += sample.getValue();
+                }
+            }
+        }
+        else {
+            for (final Pair<double[], Double> sample : innerDistribution.getPmf()) {
+                boolean inBound = true;
+                for (int d = 0; d < a.length; d++) {
+                    if ((sample.getKey()[d] <= a[d]) && (sample.getKey()[d] > b[d])) {
+                        inBound = false;
+                        break;
+                    }
+                }
+                if (inBound) {
+                    probability += sample.getValue();
+                }
+            }
+        }
+
+        return probability;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double[] sample() {
+        return innerDistribution.sample();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double density(double[] x) {
+        // Call PMF of the discrete distribution
+        return probability(x, x);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return {@code sum(singletons[i] * probabilities[i])}
+     */
+    public double[] getNumericalMean() {
+        double[] mean = new double[getDimension()];
+
+        for (final Pair<double[], Double> sample : innerDistribution.getPmf()) {
+            for (int i = 0; i < sample.getKey().length; i++) {
+                mean[i] += sample.getValue() * sample.getKey()[i];
+            }
+        }
+
+        return mean;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return {@code sum((singletons[i] - mean) ^ 2 * probabilities[i])}
+     */
+    public double[][] getNumericalVariance() {
+        double[] mean = new double[getDimension()];
+        double[] meanOfSquares = new double[getDimension()];
+        double[][] result = new double[getDimension()][getDimension()];
+        for (final Pair<double[], Double> sample : innerDistribution.getPmf()) {
+            for (int i = 0; i < sample.getKey().length; i++) {
+                mean[i] += sample.getValue() * sample.getKey()[i];
+                meanOfSquares[i] += sample.getValue() * sample.getKey()[i] * sample.getKey()[i];
+            }
+        }
+        for (int i = 0; i < getDimension(); i++) {
+            result[i][i] = meanOfSquares[i] - mean[i] * mean[i];
+        }
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * Returns the lowest value with non-zero probability.
+     * 
+     * @return the lowest value with non-zero probability.
+     */
+    public double[] getSupportLowerBound() {
+        double[] min = new double[getDimension()];
+        Arrays.fill(min, Double.POSITIVE_INFINITY);
+        for (final Pair<double[], Double> sample : innerDistribution.getPmf()) {
+            for (int i = 0; i < sample.getKey().length; i++) {
+                if (sample.getKey()[i] < min[i] && sample.getValue() > 0) {
+                    min[i] = sample.getKey()[i];
+                }
+            }
+        }
+
+        return min;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * Returns the highest value with non-zero probability.
+     * 
+     * @return the highest value with non-zero probability.
+     */
+    public double[] getSupportUpperBound() {
+        double[] max = new double[getDimension()];
+        Arrays.fill(max, Double.NEGATIVE_INFINITY);
+        for (final Pair<double[], Double> sample : innerDistribution.getPmf()) {
+            for (int i = 0; i < sample.getKey().length; i++) {
+                if (sample.getKey()[i] > max[i] && sample.getValue() > 0) {
+                    max[i] = sample.getKey()[i];
+                }
+            }
+        }
+
+        return max;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * The support of this distribution includes the lower bound.
+     * 
+     * @return {@code true}
+     */
+    public boolean isSupportLowerBoundInclusive() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * The support of this distribution includes the upper bound.
+     * 
+     * @return {@code true}
+     */
+    public boolean isSupportUpperBoundInclusive() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * The support of this distribution is connected.
+     * 
+     * @return {@code true}
+     */
+    public boolean isSupportConnected() {
+        return true;
+    }
+
+}
