8908d3755adf1efcc82ca68b1abdbd1ba7e5a221
Christian Kuka <christian@kuka.cc>
2014-02-27 07:35:42 +0000

    RCP: Add histogram chart
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18908 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 74a23ee4984569f4bcd23d8bbd4faec2c04eedde

---
+++ client/studio/rcp.viewer.stream.chart/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/chart/charts/HistogramChart.java
@@ -0,0 +1,48 @@
+/********************************************************************************** 
+  * Copyright 2014 The Odysseus Team
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.charts;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+
+import de.uniol.inf.is.odysseus.rcp.viewer.stream.chart.AbstractHistogramSeriesChart;
+
+/**
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
+ */
+public class HistogramChart extends AbstractHistogramSeriesChart {
+    /**
+     * 
+     * {@inheritDoc}
+     */
+    @Override
+    protected JFreeChart createChart() {
+        JFreeChart chart = ChartFactory.createHistogram(getTitle(), "", "", this.dataset, PlotOrientation.VERTICAL, true, true, false);
+        return chart;
+    }
+
+    /**
+     * 
+     * {@inheritDoc}
+     */
+    @Override
+    public String getViewID() {
+        return VIEW_ID_PREFIX + ".histogramchart";
+    }
+
+}
