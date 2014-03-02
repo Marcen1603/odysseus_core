b12e72e662640df2dfa6bba772c72bf0579eb538
Christian Kuka <christian@kuka.cc>
2014-03-02 05:04:29 +0000

    Tooling: Add SASE syntax
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18956 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b3439bf5a7a19b49213f64ec9538dcdb37c5acd3

--- tooling/wiki/generate_sase_syntax.sh
+++ tooling/wiki/generate_sase_syntax.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Find all keywords
-parser='../../server/cql/parser.cql/src/de/uniol/inf/is/odysseus/parser/cql/parser/NewSQLParser.jjt'
-keywords=$(cat $parser | tr '\n\r' '\n' | grep -E '^\| < K_[A-Z_]+ : \"' | sed -rn 's/.*\| < K_[A-Z_]+ : \"([a-zA-Z_]*)\" >.*/\U\1/p' | sort | sed -r ':a ;$! N; s/\n(.*)/ \1/; ta ; P ; D')
+# Sase keywords
+keywords=$(echo "CREATE STREAM VIEW PATTERN WHERE WITHIN ENDS AT RETURN SEQ ALT AND AS LEN WEEK WEEKS DAY DAYS HOUR HOURS MINUTE MINUTES SECOND SECONDS MILLISECOND MILLISECONDS SKIP_TILL_NEXT_MATCH SKIP_TILL_ANY_MATCH STRICT_CONTIGUITY AVG MIN MAX SUM COUNT TRUE FALS" | tr ' ' '\n' | sort | sed -r ':a ;$! N; s/\n(.*)/ \1/; ta ; P ; D')
+
 # Find all datatypes
 datatypes=$(find ../../ -name \*.java -type f |xargs grep -E "public .*static .*SDF[a-zA-Z_]*Datatype" | sed -rn 's/.*new SDF[a-zA-Z_]*Datatype\(\"([a-zA-Z_]*)\"\);.*/\U\1/p' | sort | sed -r ':a ;$! N; s/\n(.*)/ \1/; ta ; P ; D')
 
-cat > shBrushCql.js <<- EOF
-SyntaxHighlighter.brushes.Cql = function()
+cat > shBrushSase.js <<- EOF
+SyntaxHighlighter.brushes.Sase = function()
 {
 // Copyright 2014 Christian Kuka christian@kuka.cc
 	
-// CQL keywords
+// SASE keywords
 var keywords =	'$keywords';
 
-// CQL datatypes
+// SASE datatypes
 var datatypes = '$datatypes';
 
 // Odysseus Script
@@ -25,13 +25,13 @@ this.regexList = [
 { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: 'string' },// double quoted strings
 { regex: SyntaxHighlighter.regexLib.multiLineSingleQuotedString, css: 'string' },// single quoted strings
 { regex: new RegExp(this.getKeywords(preprocessors), 'gm'), css: 'preprocessor' },// Odysseus Script
-{ regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'color5' },// CQL keywords
-{ regex: new RegExp(this.getKeywords(datatypes), 'gm'), css: 'color2' }// CQL datatypes
+{ regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'color5' },// SASE keywords
+{ regex: new RegExp(this.getKeywords(datatyps), 'gm'), css: 'color2' }// SASE datatypes
 ];
 };
 
 SyntaxHighlighter.brushes.Processing.prototype = new SyntaxHighlighter.Highlighter();
-SyntaxHighlighter.brushes.Processing.aliases = ['CQL', 'cql'];
+SyntaxHighlighter.brushes.Processing.aliases = ['Sase', 'sase'];
 EOF
 
 
