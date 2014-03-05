1420dd72c0b96850c74c3de30d5b0d964f419312
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-03-05 07:22:49 +0000

    RCP: Removed warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@18999 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7bc3454b63b84889db471c61a3a6d9cd944ffcfa

--- monolithic/studio/de.uniol.inf.is.odysseus.rcp.evaluation/src/de/uniol/inf/is/odysseus/rcp/evaluation/Activator.java
+++ monolithic/studio/de.uniol.inf.is.odysseus.rcp.evaluation/src/de/uniol/inf/is/odysseus/rcp/evaluation/Activator.java
@@ -10,43 +10,23 @@ import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
 	public static final String PLUGIN_ID = "de.uniol.inf.is.odysseus.rcp.evaluation"; //$NON-NLS-1$
 
-	// The shared instance
 	private static Activator plugin;
-
 	private static IExecutor executor;
 	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
 	public static Activator getDefault() {
 		return plugin;
 	}
