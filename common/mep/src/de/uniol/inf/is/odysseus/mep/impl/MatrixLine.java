628dfb278632eb9a0bbe6eaf5fe38ee6777ec1d1
Christian Kuka <christian@kuka.cc>
2014-03-19 07:57:13 +0000

    Matrix: Estimate accepted input types matrix
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@19186 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 836011646efe3137bf17295a31280ed365397c6b

--- common/mep/src/de/uniol/inf/is/odysseus/mep/impl/MatrixLine.java
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/impl/MatrixLine.java
@@ -1,19 +1,21 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.mep.impl;
+
+import java.util.Arrays;
 
 import de.uniol.inf.is.odysseus.core.mep.IExpression;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
@@ -21,24 +23,27 @@ import de.uniol.inf.is.odysseus.mep.AbstractFunction;
 
 public class MatrixLine extends AbstractFunction<double[]> {
 
-	private static final long serialVersionUID = -2878724836669428853L;
-	public static final SDFDatatype[] accTypes = new SDFDatatype[] { SDFDatatype.DOUBLE};
-		
-	public MatrixLine(IExpression<?>[] values) {
-		super("__matrixline",values.length,accTypes,SDFDatatype.VECTOR_DOUBLE);
-		setArguments(values);
-	}
+    private static final long serialVersionUID = -2878724836669428853L;
 
-	@Override
-	public double[] getValue() {
-		int arity = getArity();
-		double[] value = new double[arity];
-		for (int i = 0; i < arity; ++i) {
-			value[i] = (Double) getArgument(i).getValue();
-		}
-		return value;
-	}
+    public MatrixLine(IExpression<?>[] values) {
+        super("__matrixline", values.length, getAccTypes(values.length), SDFDatatype.DOUBLE);
+        setArguments(values);
+    }
 
+    @Override
+    public double[] getValue() {
+        int arity = getArity();
+        double[] value = new double[arity];
+        for (int i = 0; i < arity; ++i) {
+            value[i] = (Double) getArgument(i).getValue();
+        }
+        return value;
+    }
 
-	
+    private static SDFDatatype[][] getAccTypes(int length) {
+        SDFDatatype[][] accTypes = new SDFDatatype[length][];
+        Arrays.fill(accTypes, new SDFDatatype[] { SDFDatatype.DOUBLE });
+        return accTypes;
+    }
+
 }
