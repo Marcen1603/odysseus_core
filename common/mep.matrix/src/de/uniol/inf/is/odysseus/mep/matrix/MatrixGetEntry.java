2400cb0db238898f4c4a0bc0f64d3aed0ae0060f
Christian Kuka <christian@kuka.cc>
2014-03-19 09:09:12 +0000

    Matrix: Fix get entry method
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@19196 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 39e2b64dc3a5731cb7fe3a912afb3d3bf2dccc1d

--- common/mep.matrix/src/de/uniol/inf/is/odysseus/mep/matrix/MatrixGetEntry.java
+++ common/mep.matrix/src/de/uniol/inf/is/odysseus/mep/matrix/MatrixGetEntry.java
@@ -1,46 +1,54 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.mep.matrix;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
 
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 import de.uniol.inf.is.odysseus.mep.AbstractFunction;
 
-
-/*
- * Diese Klasse dient dazu, einen bestimmten Wert aus einer Matrix auszulesen.
+/**
+ * Gets a single entry from a matrix
+ * 
+ * @author Christian Kuka <christian@kuka.cc>
+ * 
  */
-public class MatrixGetEntry extends AbstractFunction<Double>{
+public class MatrixGetEntry extends AbstractFunction<Double> {
 
-	private static final long serialVersionUID = 8960765237278191962L;
-	public static final SDFDatatype[] accTypes = new SDFDatatype[] { SDFDatatype.INTEGER};
+    private static final long serialVersionUID = 8960765237278191962L;
+    public static final SDFDatatype[][] accTypes = new SDFDatatype[][] { SDFDatatype.MATRIXS, SDFDatatype.NUMBERS, SDFDatatype.NUMBERS };
 
-	public MatrixGetEntry() {
-		super("MatrixEntry",3,accTypes,SDFDatatype.DOUBLE);
-	}
-		
-	/*
-	 * Liefert einen bestimmten Wert aus einer �bergebenen Matrix zur�ck.
-	 */
-	@Override
-	public Double getValue() {
-		double d = ((double[][]) getInputValue(0))[((Double)getInputValue(1)).intValue()][((Double)getInputValue(2)).intValue()];
-		return d;
-	}
+    /**
+ * 
+ */
+    public MatrixGetEntry() {
+        super("get", 3, accTypes, SDFDatatype.DOUBLE);
+    }
 
+    /**
+     * 
+     * {@inheritDoc}
+     */
+    @Override
+    public Double getValue() {
+        RealMatrix a = new Array2DRowRealMatrix((double[][]) this.getInputValue(0), false);
+        int row = this.getNumericalInputValue(1).intValue();
+        int column = this.getNumericalInputValue(2).intValue();
+        return a.getEntry(row, column);
+    }
 
-
-	
 }
