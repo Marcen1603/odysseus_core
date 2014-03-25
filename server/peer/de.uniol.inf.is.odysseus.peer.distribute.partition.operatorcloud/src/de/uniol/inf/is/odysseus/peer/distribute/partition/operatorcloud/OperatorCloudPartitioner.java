35b0731b926280b31dded0abb818d56c4aefdb88
Michael Brand <michael.brand@uni-oldenburg.de>
2014-03-25 09:44:30 +0000

    Handling of RenameAOs
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@19296 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8e903f88ded9479b6363504349341af6efc2fc2c

--- server/peer/de.uniol.inf.is.odysseus.peer.distribute.partition.operatorcloud/src/de/uniol/inf/is/odysseus/peer/distribute/partition/operatorcloud/OperatorCloudPartitioner.java
+++ server/peer/de.uniol.inf.is.odysseus.peer.distribute.partition.operatorcloud/src/de/uniol/inf/is/odysseus/peer/distribute/partition/operatorcloud/OperatorCloudPartitioner.java
@@ -6,7 +6,9 @@ import java.util.List;
 import com.google.common.collect.Lists;
 
 import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalSubscription;
 import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.RenameAO;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.QueryBuildConfiguration;
 import de.uniol.inf.is.odysseus.peer.distribute.ILogicalQueryPart;
 import de.uniol.inf.is.odysseus.peer.distribute.IQueryPartitioner;
@@ -24,8 +26,25 @@ public class OperatorCloudPartitioner implements IQueryPartitioner {
 	public Collection<ILogicalQueryPart> partition(Collection<ILogicalOperator> operators, ILogicalQuery query, QueryBuildConfiguration config, List<String> partitionParameters) throws QueryPartitionException {
 		Collection<ILogicalQueryPart> parts = Lists.newArrayList();
 		
-		for( ILogicalOperator operator : operators ) {
-			parts.add( new LogicalQueryPart(operator));
+		// Handling of RenameAOs: Every RenameAO will be in the query part of the operator subscribed by the RenameAO.
+		
+		List<ILogicalOperator> processedOperators = Lists.newArrayList();
+		
+		for(ILogicalOperator operator : operators) {
+			
+			if(processedOperators.contains(operator))
+				continue;
+			
+			List<ILogicalOperator> operatorsForPart = Lists.newArrayList(operator);
+			
+			for(LogicalSubscription subToSink : operator.getSubscriptions()) {
+				
+				if(subToSink.getTarget() instanceof RenameAO)
+					operatorsForPart.add(subToSink.getTarget());
+				
+			}
+			
+			parts.add(new LogicalQueryPart(operatorsForPart));
 		}
 		
 		return parts;
