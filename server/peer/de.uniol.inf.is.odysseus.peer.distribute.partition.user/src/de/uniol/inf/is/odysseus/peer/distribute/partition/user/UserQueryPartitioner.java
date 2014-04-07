2fdb8fafe7129aa7b9fc647595049bd48b4ecfc0
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-04-07 08:45:29 +0000

    Peer: Userdefined partitioner redone --> more stable
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@19564 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ccd2fd4a80daffe52d08819125c8ca3c4001c260

--- server/peer/de.uniol.inf.is.odysseus.peer.distribute.partition.user/src/de/uniol/inf/is/odysseus/peer/distribute/partition/user/UserQueryPartitioner.java
+++ server/peer/de.uniol.inf.is.odysseus.peer.distribute.partition.user/src/de/uniol/inf/is/odysseus/peer/distribute/partition/user/UserQueryPartitioner.java
@@ -7,11 +7,13 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalSubscription;
 import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.StreamAO;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.QueryBuildConfiguration;
@@ -24,7 +26,6 @@ public class UserQueryPartitioner implements IQueryPartitioner {
 
 	private static final Logger LOG = LoggerFactory.getLogger(UserQueryPartitioner.class);
 	private static final String LOCAL_DESTINATION_NAME = "local";
-	private static final String UNKNOWN_DESTINATION_NAME = "____unknown_____";
 
 	@Override
 	public String getName() {
@@ -36,21 +37,21 @@ public class UserQueryPartitioner implements IQueryPartitioner {
 		return determineQueryParts(operators);
 	}
 
-	private static Collection<ILogicalQueryPart> determineQueryParts(Collection<ILogicalOperator> operators) {
+	private static Collection<ILogicalQueryPart> determineQueryParts(Collection<ILogicalOperator> operators) throws QueryPartitionException {
 		Map<ILogicalOperator, String> destinations = determineDestinationNames(operators);
 		Map<String, Collection<ILogicalOperator>> revertedDestinationMap = revertMap(destinations);
-		
+
 		return generateQueryParts(revertedDestinationMap);
 	}
 
 	private static <K, V> Map<K, Collection<V>> revertMap(Map<V, K> map) {
 		Map<K, Collection<V>> revertedMap = Maps.newHashMap();
-		for( V values : map.keySet() ) {
+		for (V values : map.keySet()) {
 			K key = map.get(values);
-			
+
 			Collection<V> coll = revertedMap.get(key);
-			if( coll == null ) {
-				coll = Lists.<V>newArrayList();
+			if (coll == null) {
+				coll = Lists.<V> newArrayList();
 				revertedMap.put(key, coll);
 			}
 			coll.add(values);
@@ -60,55 +61,71 @@ public class UserQueryPartitioner implements IQueryPartitioner {
 
 	private static Collection<ILogicalQueryPart> generateQueryParts(Map<String, Collection<ILogicalOperator>> revertedDestinationMap) {
 		Collection<ILogicalQueryPart> parts = Lists.newArrayList();
-		for( String destinationName : revertedDestinationMap.keySet() ) {
+		for (String destinationName : revertedDestinationMap.keySet()) {
 			LogicalQueryPart queryPart = new LogicalQueryPart(revertedDestinationMap.get(destinationName));
-			parts.add( queryPart);
+			parts.add(queryPart);
 			LOG.debug("Created destination name {} to query part {}", destinationName, queryPart);
 		}
 		return parts;
 	}
 
-	private static Map<ILogicalOperator, String> determineDestinationNames(Collection<ILogicalOperator> operators) {
+	private static Map<ILogicalOperator, String> determineDestinationNames(Collection<ILogicalOperator> operators) throws QueryPartitionException {
 		final Map<ILogicalOperator, String> destinationNames = Maps.newHashMap();
 
-		for (ILogicalOperator operator : operators) {
-			String destinationName = getDestinationName(operator);
-			if( Strings.isNullOrEmpty(destinationName)) {
-				throw new RuntimeException("Could not determine destination name of operator " + operator);
+		for (ILogicalOperator unsetOperator : operators) {
+			Optional<String> optDestinationName = getDestinationNameDown(unsetOperator);
+			if (!optDestinationName.isPresent()) {
+				optDestinationName = getDestinationNameUp(unsetOperator);
 			}
-			destinationNames.put(operator, destinationName);
-		}
-		
-		for (ILogicalOperator operator : operators) {
-			String dst = destinationNames.get(operator);
-			if( dst.equals(UNKNOWN_DESTINATION_NAME) ) {
-				if( operator.getSubscriptions().isEmpty() ) {
-					destinationNames.put(operator, LOCAL_DESTINATION_NAME); 
-				} else {
-					destinationNames.put(operator, destinationNames.get(operator.getSubscriptions().iterator().next().getTarget()));
-				}
+
+			if (optDestinationName.isPresent()) {
+				destinationNames.put(unsetOperator, optDestinationName.get());
+			} else {
+				throw new QueryPartitionException("Could not determine destionation name of operator " + unsetOperator + "!");
 			}
 		}
 
 		return destinationNames;
 	}
 
-	private static String getDestinationName(ILogicalOperator operator) {
+	private static Optional<String> getDestinationNameDown(ILogicalOperator operator) {
 		if (operator instanceof StreamAO) {
-			return LOCAL_DESTINATION_NAME;
+			return Optional.of(LOCAL_DESTINATION_NAME);
 		}
 
 		if (!Strings.isNullOrEmpty(operator.getDestinationName())) {
-			return operator.getDestinationName();
+			return Optional.of(operator.getDestinationName());
 		}
 		if (operator.getSubscribedToSource().size() > 0) {
-			return getDestinationName(getOnePreviousOperator(operator));
+			for(LogicalSubscription sub :  operator.getSubscribedToSource() ) {
+				Optional<String> optName = getDestinationNameDown(sub.getTarget());
+				if( optName.isPresent() ) {
+					return optName;
+				}
+			}
 		}
-		
-		return UNKNOWN_DESTINATION_NAME;
+
+		return Optional.absent();
 	}
 
-	private static ILogicalOperator getOnePreviousOperator(ILogicalOperator operator) {
-		return operator.getSubscribedToSource().iterator().next().getTarget();
+	private static Optional<String> getDestinationNameUp(ILogicalOperator operator) {
+		if (operator instanceof StreamAO) {
+			return Optional.of(LOCAL_DESTINATION_NAME);
+		}
+
+		if (!Strings.isNullOrEmpty(operator.getDestinationName())) {
+			return Optional.of(operator.getDestinationName());
+		}
+		
+		if (operator.getSubscriptions().size() > 0) {
+			for(LogicalSubscription sub :  operator.getSubscriptions() ) {
+				Optional<String> optName = getDestinationNameUp(sub.getTarget());
+				if( optName.isPresent() ) {
+					return optName;
+				}
+			}
+		}
+
+		return Optional.absent();
 	}
 }
