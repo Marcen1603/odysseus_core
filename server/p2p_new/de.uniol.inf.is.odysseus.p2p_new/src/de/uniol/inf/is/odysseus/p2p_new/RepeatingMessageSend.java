c43f38488480119ddd3527ceca82507a824d4b33
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-04-09 06:41:33 +0000

    Peer: Repeating message sends aborts after 60s, warns after 30s (to find programming errors)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@19606 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 950d91ff43b1904d4f1a690029045e94aedafe69

--- server/p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/RepeatingMessageSend.java
+++ server/p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/RepeatingMessageSend.java
@@ -9,13 +9,19 @@ import de.uniol.inf.is.odysseus.p2p_new.util.RepeatingJobThread;
 public class RepeatingMessageSend extends RepeatingJobThread {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RepeatingMessageSend.class);
+	private static final int WARNING_TIME_INTERVAL_MILLIS = 30 * 1000;
+	private static final int ABORT_MILLIS = 60 * 1000;
+	private static final int REPEAT_SEND_INTERVAL_MILLIS = 500;
 	
 	private final IPeerCommunicator communicator;
 	private final IMessage message;
 	private final PeerID peerID;
 	
+	private int sendingTime;
+	private boolean warned;
+	
 	public RepeatingMessageSend( IPeerCommunicator communicator, IMessage message, PeerID destination ) {
-		super(500, "Repeating message thread: " + message.getClass().getName());
+		super(REPEAT_SEND_INTERVAL_MILLIS, "Repeating message thread: " + message.getClass().getName());
 		
 		this.communicator = communicator;
 		this.message = message;
@@ -28,6 +34,16 @@ public class RepeatingMessageSend extends RepeatingJobThread {
 			LOG.debug("Retrying sending message of type '{}'", message.getClass().getName());
 			
 			communicator.send(peerID, message);
+			
+			sendingTime += getIntervalMillis();
+			if( !warned && sendingTime >= WARNING_TIME_INTERVAL_MILLIS ) {
+				LOG.error("RepeatingMessageThread runs more than " + WARNING_TIME_INTERVAL_MILLIS + " ms now! Message: {}", message.getClass().getName());
+				warned = true;
+			} else if( sendingTime > ABORT_MILLIS ) {
+				LOG.error("RepeatingMessageThread runs now more than " + ABORT_MILLIS + " ms now. Abort sending message {}", message.getClass().getName());
+				stopRunning();
+			}
+			
 		} catch (PeerCommunicationException e) {
 			LOG.error("Could not repeatedly send message");
 			stopRunning();
