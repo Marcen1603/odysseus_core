ca9f333a0369459013afe950dd51e97136b33611
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-04-09 06:42:01 +0000

    Peer: ACK'ed PeerClose message
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@19607 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 30b951a7d862ed660224838c6cec78a82a351d1a

--- server/p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/communication/PeerCloseAckMessage.java
+++ server/p2p_new/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/communication/PeerCloseAckMessage.java
@@ -2,6 +2,6 @@ package de.uniol.inf.is.odysseus.p2p_new.communication;
 
 import de.uniol.inf.is.odysseus.p2p_new.EmptyMessage;
 
-public class PeerCloseMessage extends EmptyMessage {
+public class PeerCloseAckMessage extends EmptyMessage {
 
 }
