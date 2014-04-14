7e9ed2a7984b4dc222d3a0811337de411d3742c8
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-04-14 07:45:35 +0000

    Format OdysseusScriptGenerator-class
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@19664 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ace162b1fa18cd62eaa41d637e9cf346fa08a24a

--- server/script/generator/src/de/uniol/inf/is/odysseus/script/generator/impl/OdysseusScriptGenerator.java
+++ server/script/generator/src/de/uniol/inf/is/odysseus/script/generator/impl/OdysseusScriptGenerator.java
@@ -31,48 +31,48 @@ public class OdysseusScriptGenerator implements IOdysseusScriptGenerator {
 
 	// called by OSGi
 	public static void unbindPQLGenerator(IPQLGenerator serv) {
-		if( pqlGenerator == serv ) {
+		if (pqlGenerator == serv) {
 			pqlGenerator = serv;
 		}
 	}
-	
+
 	// called by OSGi
-	public static void bindOdysseusScriptGeneratorHeader( IOdysseusScriptGeneratorHeader serv ) {
+	public static void bindOdysseusScriptGeneratorHeader(IOdysseusScriptGeneratorHeader serv) {
 		headers.add(serv);
 	}
-	
+
 	// called by OSGi
-	public static void unbindOdysseusScriptGeneratorHeader( IOdysseusScriptGeneratorHeader serv ) {
+	public static void unbindOdysseusScriptGeneratorHeader(IOdysseusScriptGeneratorHeader serv) {
 		headers.remove(serv);
 	}
-	
+
 	// called by OSGi
-	public static void bindOdysseusScriptGeneratorFooter( IOdysseusScriptGeneratorFooter serv ) {
+	public static void bindOdysseusScriptGeneratorFooter(IOdysseusScriptGeneratorFooter serv) {
 		footers.add(serv);
 	}
-	
+
 	// called by OSGi
-	public static void unbindOdysseusScriptGeneratorFooter( IOdysseusScriptGeneratorFooter serv ) {
+	public static void unbindOdysseusScriptGeneratorFooter(IOdysseusScriptGeneratorFooter serv) {
 		footers.remove(serv);
 	}
 
 	// called by OSGi
-	public static void bindOdysseusScriptGeneratorQueryHeader( IOdysseusScriptGeneratorQueryHeader serv ) {
+	public static void bindOdysseusScriptGeneratorQueryHeader(IOdysseusScriptGeneratorQueryHeader serv) {
 		queryHeaders.add(serv);
 	}
-	
+
 	// called by OSGi
-	public static void unbindOdysseusScriptGeneratorQueryHeader( IOdysseusScriptGeneratorQueryHeader serv ) {
+	public static void unbindOdysseusScriptGeneratorQueryHeader(IOdysseusScriptGeneratorQueryHeader serv) {
 		queryHeaders.remove(serv);
 	}
-	
+
 	// called by OSGi
-	public static void bindOdysseusScriptGeneratorQueryFooter( IOdysseusScriptGeneratorQueryFooter serv ) {
+	public static void bindOdysseusScriptGeneratorQueryFooter(IOdysseusScriptGeneratorQueryFooter serv) {
 		queryFooters.add(serv);
 	}
-	
+
 	// called by OSGi
-	public static void unbindOdysseusScriptGeneratorQueryFooter( IOdysseusScriptGeneratorQueryFooter serv ) {
+	public static void unbindOdysseusScriptGeneratorQueryFooter(IOdysseusScriptGeneratorQueryFooter serv) {
 		queryFooters.remove(serv);
 	}
 
@@ -81,81 +81,81 @@ public class OdysseusScriptGenerator implements IOdysseusScriptGenerator {
 		Preconditions.checkNotNull(queries, "List of queries to generate odysseus script must not be null!");
 		Preconditions.checkArgument(!queries.isEmpty(), "List of queries to generate odysseus script must not be empty!");
 		Preconditions.checkNotNull(config, "QueryBuildConfiguration must not be null!");
-		
+
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append("#PARSER PQL\n");
-		
+
 		sb.append(generateHeader(queries, config));
-		for( ILogicalQuery query : queries ) {
-			
+		for (ILogicalQuery query : queries) {
+
 			sb.append(generateQueryHeader(query, config));
-			
+
 			sb.append("#ADDQUERY\n");
 			sb.append(pqlGenerator.generatePQLStatement(query.getLogicalPlan()));
-			
+
 			sb.append(generateQueryFooter(query, config));
 			sb.append("\n\n");
 		}
-		
-		sb.append( generateFooter(queries, config ));
-		
+
+		sb.append(generateFooter(queries, config));
+
 		return sb.toString();
 	}
 
 	private static String generateQueryHeader(ILogicalQuery query, QueryBuildConfiguration config) {
 		StringBuilder sb = new StringBuilder();
-		
-		for( IOdysseusScriptGeneratorQueryHeader queryHeader : queryHeaders ) {
+
+		for (IOdysseusScriptGeneratorQueryHeader queryHeader : queryHeaders) {
 			String text = queryHeader.getQueryHeader(query, config);
-			if( !Strings.isNullOrEmpty(text)) {
+			if (!Strings.isNullOrEmpty(text)) {
 				sb.append(text);
 				sb.append("\n");
 			}
 		}
-		
+
 		return sb.toString();
 	}
-	
+
 	private static String generateQueryFooter(ILogicalQuery query, QueryBuildConfiguration config) {
 		StringBuilder sb = new StringBuilder();
-		
-		for( IOdysseusScriptGeneratorQueryFooter queryFooter : queryFooters ) {
+
+		for (IOdysseusScriptGeneratorQueryFooter queryFooter : queryFooters) {
 			String text = queryFooter.getQueryFooter(query, config);
-			if( !Strings.isNullOrEmpty(text)) {
+			if (!Strings.isNullOrEmpty(text)) {
 				sb.append(text);
 				sb.append("\n");
 			}
 		}
-		
+
 		return sb.toString();
 	}
 
 	private static String generateFooter(List<ILogicalQuery> queries, QueryBuildConfiguration config) {
 		StringBuilder sb = new StringBuilder();
-		
-		for( IOdysseusScriptGeneratorFooter footer : footers ) {
+
+		for (IOdysseusScriptGeneratorFooter footer : footers) {
 			String text = footer.getFooter(queries, config);
-			if( !Strings.isNullOrEmpty(text)) {
+			if (!Strings.isNullOrEmpty(text)) {
 				sb.append(text);
 				sb.append("\n");
 			}
 		}
-		
+
 		return sb.toString();
 	}
 
 	private static String generateHeader(List<ILogicalQuery> queries, QueryBuildConfiguration config) {
 		StringBuilder sb = new StringBuilder();
-		
-		for( IOdysseusScriptGeneratorHeader header : headers ) {
+
+		for (IOdysseusScriptGeneratorHeader header : headers) {
 			String text = header.getHeader(queries, config);
-			if( !Strings.isNullOrEmpty(text)) {
+			if (!Strings.isNullOrEmpty(text)) {
 				sb.append(text);
 				sb.append("\n");
 			}
 		}
-		
+
 		return sb.toString();
 	}
 
