972bd80cf2955d7a407d485797c7f6a9a64d38c1
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-04-17 06:36:30 +0000

    New plugin for a new better costmodel
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@19718 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: de0c252b91ee8247f2d188b13cbbfc3f8d761f33

---
+++ server/costmodel/de.uniol.inf.is.odysseus.costmodel/src/de/uniol/inf/is/odysseus/costmodel/CostModelActivator.java
@@ -0,0 +1,16 @@
+package de.uniol.inf.is.odysseus.costmodel;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class CostModelActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+
+}
