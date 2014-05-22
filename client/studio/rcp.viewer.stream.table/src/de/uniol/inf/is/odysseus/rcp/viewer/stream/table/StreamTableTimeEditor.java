dddaa2357d552b56a1487727b9dd12cc205b17be
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-05-22 16:52:19 +0000

    New viewer that show only elements that are valid to the same time as the last read element (something like a sweeparea): Missing class
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20072 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1d7fb1346c5b50e866afd61803634d223e60789c

---
+++ client/studio/rcp.viewer.stream.table/src/de/uniol/inf/is/odysseus/rcp/viewer/stream/table/StreamTableTimeEditor.java
@@ -0,0 +1,45 @@
+/********************************************************************************** 
+  * Copyright 2011 The Odysseus Team
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package de.uniol.inf.is.odysseus.rcp.viewer.stream.table;
+
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
+import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
+
+public class StreamTableTimeEditor extends StreamTableEditor {
+
+	public StreamTableTimeEditor() {
+		super(0);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void updateTuples(Tuple<?> element) {
+		// element must provide time interval, cast and risk cast error
+		// instead of testing each time;
+		Tuple<? extends ITimeInterval> e = (Tuple<? extends ITimeInterval>) element;
+			
+		PointInTime start = e.getMetadata().getStart();
+		for (Tuple<?> t:tuples){
+			if (((Tuple<? extends ITimeInterval>)t).getMetadata().getEnd().beforeOrEquals(start)){
+				tuples.remove(t);
+			}
+			
+		}
+		super.updateTuples(element);
+	}
+
+}
