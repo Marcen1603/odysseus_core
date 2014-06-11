240ab5e82e07d967ee54dd37284b7fe397a01f2a
Carsten Cordes <ccordes@uni-oldenburg.de>
2014-06-11 16:07:22 +0000

    LSOP-64 Bugfix.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20208 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 06fa4df196b6e335a61e9522ae539bd768a89fe7

--- server/peer/de.uniol.inf.is.odysseus.peer.loadbalancing.active/src/de/uniol/inf/is/odysseus/peer/loadbalancing/active/wrapper/JxtaSenderWrapper.java
+++ server/peer/de.uniol.inf.is.odysseus.peer.loadbalancing.active/src/de/uniol/inf/is/odysseus/peer/loadbalancing/active/wrapper/JxtaSenderWrapper.java
@@ -1,17 +1,9 @@
 package de.uniol.inf.is.odysseus.peer.loadbalancing.active.wrapper;
 
-import java.util.List;
-
-import com.google.common.base.Optional;
-
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
-import de.uniol.inf.is.odysseus.core.planmanagement.IOperatorOwner;
 import de.uniol.inf.is.odysseus.p2p_new.data.DataTransmissionException;
-import de.uniol.inf.is.odysseus.p2p_new.data.ITransmissionSender;
 import de.uniol.inf.is.odysseus.p2p_new.logicaloperator.JxtaSenderAO;
 import de.uniol.inf.is.odysseus.p2p_new.physicaloperator.JxtaSenderPO;
-import de.uniol.inf.is.odysseus.p2p_new.service.ServerExecutorService;
-import de.uniol.inf.is.odysseus.p2p_new.service.SessionManagementService;
 import de.uniol.inf.is.odysseus.peer.loadbalancing.active.physicaloperator.JxtaBundleSenderPO;
 
 /***
@@ -62,51 +54,6 @@ public class JxtaSenderWrapper<T extends IStreamObject<?>>  extends JxtaSenderPO
 	@Override
 	public void process_done(int port) {
 		super.process_done(port);
-	}
-
-
-	/**
-	 * Wrapper Function for onReceiveOpen to get right Query ID.
-	 * @param sender Transmission.
-	 */
-	@Override
-	public void onReceiveOpen(ITransmissionSender sender) {
-		
-		super.onReceiveOpen(sender);
-		
-		Optional<Integer> optQueryID = determineQueryID(parent.getOwner());
-		if (optQueryID.isPresent()) {
-			int queryID = optQueryID.get();
-			ServerExecutorService.getServerExecutor().startQuery(queryID, SessionManagementService.getActiveSession());
-		}
-	}
-
-	/**
-	 * Wrapper Function for onReceiveClose to get right sender
-	 * @param sender Transmission
-	 */
-	@Override
-	public void onReceiveClose(ITransmissionSender sender) {
-		
-		super.onReceiveClose(sender);
-		
-		Optional<Integer> optQueryID = determineQueryID(parent.getOwner());
-		if (optQueryID.isPresent()) {
-			int queryID = optQueryID.get();
-			ServerExecutorService.getServerExecutor().stopQuery(queryID, SessionManagementService.getActiveSession());
-		}
-	}
-	
-	/**
-	 * Get Query ID for owner
-	 * @param owner Query owner
-	 * @return Query ID.
-	 */
-	private static Optional<Integer> determineQueryID(List<IOperatorOwner> owner) {
-		if (owner.isEmpty()) {
-			return Optional.absent();
-		}
-		return Optional.of(owner.get(0).getID());
 	}
 	
 }
