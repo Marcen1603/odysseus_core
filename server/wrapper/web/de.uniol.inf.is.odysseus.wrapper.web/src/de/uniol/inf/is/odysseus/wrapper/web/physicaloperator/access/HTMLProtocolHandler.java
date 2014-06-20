63d8441e6d765bb3505ed8a6896030a98d97bd5b
Christian Kuka <christian@kuka.cc>
2014-06-20 16:03:29 +0000

    HTML Wrapper: Fix input stream reader
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20331 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: fe2f47ff7f703136e8891836643af597c05d6b5b

--- server/wrapper/web/de.uniol.inf.is.odysseus.wrapper.web/src/de/uniol/inf/is/odysseus/wrapper/web/physicaloperator/access/HTMLProtocolHandler.java
+++ server/wrapper/web/de.uniol.inf.is.odysseus.wrapper.web/src/de/uniol/inf/is/odysseus/wrapper/web/physicaloperator/access/HTMLProtocolHandler.java
@@ -15,11 +15,11 @@
  ******************************************************************************/
 package de.uniol.inf.is.odysseus.wrapper.web.physicaloperator.access;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -40,6 +40,7 @@ import org.w3c.dom.DocumentFragment;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.html.HTMLDocument;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.SAXNotSupportedException;
@@ -60,258 +61,283 @@ import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
  * 
  * @author Christian Kuka <christian@kuka.cc>
  */
-public class HTMLProtocolHandler<T extends Tuple<?>> extends
-		AbstractProtocolHandler<T> {
+public class HTMLProtocolHandler<T extends Tuple<?>> extends AbstractProtocolHandler<T> {
 
-	Logger LOG = LoggerFactory.getLogger(HTMLProtocolHandler.class);
-	private DOMFragmentParser parser;
-	private InputStream input;
-	private OutputStream output;
-	private long delay;
-	private int nanodelay;
-	private final List<String> xpaths = new ArrayList<String>();
+    Logger LOG = LoggerFactory.getLogger(HTMLProtocolHandler.class);
+    private DOMFragmentParser parser;
+    private InputStream input;
+    private OutputStream output;
+    private long delay;
+    private int nanodelay;
+    private final List<String> xpaths = new ArrayList<String>();
 
-	/**
+    /**
  * 
  */
-	public HTMLProtocolHandler() {
-		super();
-	}
+    public HTMLProtocolHandler() {
+        super();
+    }
 
-	/**
-	 * Create a new HTML Data Handler
-	 * @param transfer 
-	 * @param dataHandler 
-	 * 
-	 * @param schema
-	 */
-	private HTMLProtocolHandler(final ITransportDirection direction,
-			final IAccessPattern access, IDataHandler<T> dataHandler) {
-		super(direction, access, dataHandler);
-		this.parser = new DOMFragmentParser();
+    /**
+     * Create a new HTML Data Handler
+     * 
+     * @param transfer
+     * @param dataHandler
+     * 
+     * @param schema
+     */
+    private HTMLProtocolHandler(final ITransportDirection direction, final IAccessPattern access, IDataHandler<T> dataHandler) {
+        super(direction, access, dataHandler);
+        this.parser = new DOMFragmentParser();
 
-		try {
-			this.parser.setProperty(
-					"http://cyberneko.org/html/properties/default-encoding",
-					"UTF-8");
-			this.parser
-					.setFeature(
-							"http://cyberneko.org/html/features/balance-tags/document-fragment",
-							true);
-		} catch (final SAXNotRecognizedException e) {
-			this.LOG.error(e.getMessage(), e);
-		} catch (final SAXNotSupportedException e) {
-			this.LOG.error(e.getMessage(), e);
-		}
+        try {
+            this.parser.setProperty("http://cyberneko.org/html/properties/default-encoding", "UTF-8");
+            this.parser.setFeature("http://cyberneko.org/html/features/balance-tags/document-fragment", true);
+        }
+        catch (final SAXNotRecognizedException e) {
+            this.LOG.error(e.getMessage(), e);
+        }
+        catch (final SAXNotSupportedException e) {
+            this.LOG.error(e.getMessage(), e);
+        }
 
-	}
+    }
 
-	@Override
-	public void open() throws UnknownHostException, IOException {
-		this.getTransportHandler().open();
-		if (this.getDirection().equals(ITransportDirection.IN)) {
-			if ((this.getAccess().equals(IAccessPattern.PULL))
-					|| (this.getAccess().equals(IAccessPattern.ROBUST_PULL))) {
-				this.input = this.getTransportHandler().getInputStream();
-			}
-		} else {
-			this.output = this.getTransportHandler().getOutputStream();
-		}
-	}
+    @Override
+    public void open() throws UnknownHostException, IOException {
+        this.getTransportHandler().open();
+        if (this.getDirection().equals(ITransportDirection.IN)) {
+            if ((this.getAccess().equals(IAccessPattern.PULL)) || (this.getAccess().equals(IAccessPattern.ROBUST_PULL))) {
+                this.input = this.getTransportHandler().getInputStream();
+            }
+        }
+        else {
+            this.output = this.getTransportHandler().getOutputStream();
+        }
+    }
 
-	@Override
-	public void close() throws IOException {
-		if (this.getDirection().equals(ITransportDirection.IN)) {
-			if (this.input != null) {
-				this.input.close();
-			}
-		} else {
-			this.output.close();
-		}
-		this.getTransportHandler().close();
-	}
+    @Override
+    public void close() throws IOException {
+        if (this.getDirection().equals(ITransportDirection.IN)) {
+            if (this.input != null) {
+                this.input.close();
+            }
+        }
+        else {
+            this.output.close();
+        }
+        this.getTransportHandler().close();
+    }
 
-	@Override
-	public boolean hasNext() throws IOException {
-		return this.input.available() > 0;
-	}
+    @Override
+    public boolean hasNext() throws IOException {
+        return this.input.available() > 0;
+    }
 
-	@Override
-	public T getNext() throws IOException {
-		this.delay();
-		if (this.input.available() > 0) {
-			try {
-				final StringBuilder inputBuffer = new StringBuilder();
-				final BufferedReader in = new BufferedReader(
-						new InputStreamReader(this.input));
-				String buffer = "";
-				while ((buffer = in.readLine()) != null) {
-					inputBuffer.append(buffer);
+    @Override
+    public T getNext() throws IOException {
+        this.delay();
+        if (this.input.available() > 0) {
+            try {
+                final Reader in = new InputStreamReader(this.input);
+                final HTMLDocument document = new HTMLDocumentImpl();
+                final DocumentFragment fragment = document.createDocumentFragment();
+                this.parser.parse(new InputSource(in), fragment);
+                final XPathFactory factory = XPathFactory.newInstance();
+                final XPath xpath = factory.newXPath();
+                final SDFSchema schema = this.getDataHandler().getSchema();
+                final String[] tuple = new String[schema.size()];
+                for (int i = 0; i < this.getXPaths().size(); i++) {
+                    final String path = this.getXPaths().get(i);
+                    try {
+                        final XPathExpression expr = xpath.compile(path);
+                        final NodeList nodes = (NodeList) expr.evaluate(fragment, XPathConstants.NODESET);
+                        if (nodes.getLength() > 0) {
+                            final Node node = nodes.item(0);
+                            final String content = node.getTextContent();
+                            tuple[i] = content;
+                        }
+                    }
+                    catch (final XPathExpressionException e) {
+                        print(fragment);
+                        this.LOG.error(e.getMessage(), e);
+                    }
+                }
+                return this.getDataHandler().readData(tuple);
+            }
+            catch (final SAXException e) {
+                this.LOG.error(e.getMessage(), e);
+            }
+        }
+        return null;
+    }
 
-				}
-				final HTMLDocument document = new HTMLDocumentImpl();
-				final DocumentFragment fragment = document
-						.createDocumentFragment();
-				this.parser.parse(inputBuffer.toString(), fragment);
-				final XPathFactory factory = XPathFactory.newInstance();
-				final XPath xpath = factory.newXPath();
-				final SDFSchema schema = this.getDataHandler().getSchema();
-				final String[] tuple = new String[schema.size()];
-				for (int i = 0; i < this.getXPaths().size(); i++) {
-					final String path = this.getXPaths().get(i);
-					try {
-						final XPathExpression expr = xpath.compile(path);
-						final NodeList nodes = (NodeList) expr.evaluate(
-								fragment, XPathConstants.NODESET);
-						if (nodes.getLength() > 0) {
-							final Node node = nodes.item(0);
-							final String content = node.getTextContent();
-							tuple[i] = content;
-						}
-					} catch (final XPathExpressionException e) {
-						this.LOG.error(e.getMessage(), e);
-					}
-				}
-				return this.getDataHandler().readData(tuple);
-			} catch (final SAXException e) {
-				this.LOG.error(e.getMessage(), e);
-			}
-		}
-		return null;
-	}
+    @Override
+    public void write(final T object) throws IOException {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<table>");
+        final SDFSchema schema = this.getDataHandler().getSchema();
+        for (int i = 0; i < schema.size(); i++) {
+            final String attr = schema.get(i).getAttributeName();
+            sb.append("<tr><td>").append(attr).append("</td>");
+            sb.append("<td>").append(object.getAttribute(i).toString());
+            sb.append("</td></tr>");
+        }
+        sb.append("</table>");
+        sb.append("</body>");
+        sb.append("</html>");
+        this.output.write(object.toString().getBytes());
+    }
 
-	@Override
-	public void write(final T object) throws IOException {
-		final StringBuilder sb = new StringBuilder();
-		sb.append("<html>");
-		sb.append("<head>");
-		sb.append("</head>");
-		sb.append("<body>");
-		sb.append("<table>");
-		final SDFSchema schema = this.getDataHandler().getSchema();
-		for (int i = 0; i < schema.size(); i++) {
-			final String attr = schema.get(i).getAttributeName();
-			sb.append("<tr><td>").append(attr).append("</td>");
-			sb.append("<td>").append(object.getAttribute(i).toString());
-			sb.append("</td></tr>");
-		}
-		sb.append("</table>");
-		sb.append("</body>");
-		sb.append("</html>");
-		System.out.println(sb.toString());
-		this.output.write(object.toString().getBytes());
-	}
+    protected void delay() {
+        if (this.delay > 0) {
+            try {
+                Thread.sleep(this.delay);
+            }
+            catch (final InterruptedException e) {
+                this.LOG.error(e.getMessage(), e);
+            }
+        }
+    }
 
-	protected void delay() {
-		if (this.delay > 0) {
-			try {
-				Thread.sleep(this.delay);
-			} catch (final InterruptedException e) {
-				// interrupting the delay might be correct
-				// e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public IProtocolHandler<T> createInstance(final ITransportDirection direction, final IAccessPattern access, final Map<String, String> options, final IDataHandler<T> dataHandler) {
+        final HTMLProtocolHandler<T> instance = new HTMLProtocolHandler<T>(direction, access, dataHandler);
+        instance.init(options);
+        instance.setOptionsMap(options);
 
-	@Override
-	public IProtocolHandler<T> createInstance(
-			final ITransportDirection direction, final IAccessPattern access,
-			final Map<String, String> options,
-			final IDataHandler<T> dataHandler) {
-		final HTMLProtocolHandler<T> instance = new HTMLProtocolHandler<T>(
-				direction, access, dataHandler);
-		instance.init(options);
-		instance.setOptionsMap(options);
+        final SDFSchema schema = dataHandler.getSchema();
+        final List<String> xpaths = new ArrayList<String>();
+        for (int i = 0; i < schema.size(); i++) {
+            final String attr = schema.get(i).getAttributeName();
+            if (options.containsKey(attr)) {
+                xpaths.add(options.get(attr));
+            }
+        }
+        instance.setXPaths(xpaths);
+        return instance;
+    }
 
-		final SDFSchema schema = dataHandler.getSchema();
-		final List<String> xpaths = new ArrayList<String>();
-		for (int i = 0; i < schema.size(); i++) {
-			final String attr = schema.get(i).getAttributeName();
-			if (options.containsKey(attr)) {
-				xpaths.add(options.get(attr));
-			}
-		}
-		instance.setXPaths(xpaths);
-		return instance;
-	}
+    protected void init(Map<String, String> options) {
+        if (options.get("delay") != null) {
+            setDelay(Long.parseLong(options.get("delay")));
+        }
+        if (options.get("nanodelay") != null) {
+            setNanodelay(Integer.parseInt(options.get("nanodelay")));
+        }
+    }
 
-	protected void init(Map<String, String> options) {
-		if (options.get("delay") != null) {
-			setDelay(Long.parseLong(options.get("delay")));
-		}
-		if (options.get("nanodelay") != null) {
-			setNanodelay(Integer.parseInt(options.get("nanodelay")));
-		}
-	}
+    @Override
+    public String getName() {
+        return "HTML";
+    }
 
-	@Override
-	public String getName() {
-		return "HTML";
-	}
+    public long getDelay() {
+        return delay;
+    }
 
-	public long getDelay() {
-		return delay;
-	}
+    public void setDelay(long delay) {
+        this.delay = delay;
+    }
 
-	public void setDelay(long delay) {
-		this.delay = delay;
-	}
+    public void setNanodelay(int nanodelay) {
+        this.nanodelay = nanodelay;
+    }
 
-	public void setNanodelay(int nanodelay) {
-		this.nanodelay = nanodelay;
-	}
+    public int getNanodelay() {
+        return nanodelay;
+    }
 
-	public int getNanodelay() {
-		return nanodelay;
-	}
+    private List<String> getXPaths() {
+        return Collections.unmodifiableList(this.xpaths);
+    }
 
-	private List<String> getXPaths() {
-		return Collections.unmodifiableList(this.xpaths);
-	}
+    private void setXPaths(final List<String> xpaths) {
+        this.xpaths.clear();
+        this.xpaths.addAll(xpaths);
+    }
 
-	private void setXPaths(final List<String> xpaths) {
-		this.xpaths.clear();
-		this.xpaths.addAll(xpaths);
-	}
+    @Override
+    public ITransportExchangePattern getExchangePattern() {
+        if (this.getDirection().equals(ITransportDirection.IN)) {
+            return ITransportExchangePattern.InOnly;
+        }
+        else {
+            return ITransportExchangePattern.OutOnly;
+        }
+    }
 
-	@Override
-	public ITransportExchangePattern getExchangePattern() {
-		if (this.getDirection().equals(ITransportDirection.IN)) {
-			return ITransportExchangePattern.InOnly;
-		} else {
-			return ITransportExchangePattern.OutOnly;
-		}
-	}
+    @Override
+    public void onConnect(final ITransportHandler caller) {
 
-	@Override
-	public void onConnect(final ITransportHandler caller) {
+    }
 
-	}
+    @Override
+    public void onDisonnect(final ITransportHandler caller) {
+    }
 
-	@Override
-	public void onDisonnect(final ITransportHandler caller) {
-	}
+    @Override
+    public boolean isSemanticallyEqualImpl(IProtocolHandler<?> o) {
+        if (!(o instanceof HTMLProtocolHandler)) {
+            return false;
+        }
+        HTMLProtocolHandler<?> other = (HTMLProtocolHandler<?>) o;
+        if (this.nanodelay != other.getNanodelay() || this.delay != other.getDelay()) {
+            return false;
+        }
+        List<String> otherXPaths = other.getXPaths();
+        if (otherXPaths.size() != this.getXPaths().size()) {
+            return false;
+        }
+        for (String s : this.getXPaths()) {
+            if (!otherXPaths.contains(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-
-	@Override
-	public boolean isSemanticallyEqualImpl(IProtocolHandler<?> o) {
-		if(!(o instanceof HTMLProtocolHandler)) {
-			return false;
-		}
-		HTMLProtocolHandler<?> other = (HTMLProtocolHandler<?>)o;
-		if(this.nanodelay != other.getNanodelay() ||
-				this.delay != other.getDelay()) {
-			return false;
-		}
-		List<String> otherXPaths = other.getXPaths();
-		if(otherXPaths.size() != this.getXPaths().size()) {
-			return false;
-		}
-		for(String s : this.getXPaths()) {
-			if(!otherXPaths.contains(s)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static void print(Node node) {
+        short type = node.getNodeType();
+        switch (type) {
+            case Node.ELEMENT_NODE: {
+                System.out.print('<');
+                System.out.print(node.getNodeName());
+                org.w3c.dom.NamedNodeMap attrs = node.getAttributes();
+                int attrCount = attrs != null ? attrs.getLength() : 0;
+                for (int i = 0; i < attrCount; i++) {
+                    Node attr = attrs.item(i);
+                    System.out.print(' ');
+                    System.out.print(attr.getNodeName());
+                    System.out.print("='");
+                    System.out.print(attr.getNodeValue());
+                    System.out.print('\'');
+                }
+                System.out.print('>');
+                break;
+            }
+            case Node.TEXT_NODE: {
+                System.out.print(node.getNodeValue());
+                break;
+            }
+        }
+        Node child = node.getFirstChild();
+        while (child != null) {
+            print(child);
+            child = child.getNextSibling();
+        }
+        if (type == Node.ELEMENT_NODE) {
+            System.out.print("</");
+            System.out.print(node.getNodeName());
+            System.out.print('>');
+        }
+        else if (type == Node.DOCUMENT_NODE || type == Node.DOCUMENT_FRAGMENT_NODE) {
+            System.out.println();
+        }
+        System.out.flush();
+    }
 }
