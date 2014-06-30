a3104810bcd039e988ed3de9c171b2731136ad14
Michael Brand <michael.brand@uni-oldenburg.de>
2014-06-30 05:06:22 +0000

    removed warning
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20413 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3b98b453a3af68823a3c76017463bd886761a593

--- lsop/de.uniol.inf.is.odysseus.sports.rest/src/serverresources/PostExampleResource.java
+++ lsop/de.uniol.inf.is.odysseus.sports.rest/src/serverresources/PostExampleResource.java
@@ -1,7 +1,6 @@
 package serverresources;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
