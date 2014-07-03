42cefcbdd39984c5df713a12032924be536b25f5
Mark Preuschaft <mpreuschaft@uni-oldenburg.de>
2014-07-03 22:59:13 +0000

    LSOP-119 change DTO object
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20466 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e4f376387c28533ceadb5eec04c649d9e385b7a4

--- lsop/de.uniol.inf.is.odysseus.sports.rest/src/dao/DataTransferObject.java
+++ lsop/de.uniol.inf.is.odysseus.sports.rest/src/dao/DataTransferObject.java
@@ -1,16 +1,19 @@
 package dao;
 
+import java.io.IOException;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DataTransferObject implements Serializable{
 	
 	private static final long serialVersionUID = -3042714386806400699L;
 	private String datatype;
-	private Object payload;
+	private String payload;
 	
 	public DataTransferObject(String datatype, Object payload) {
 		this.datatype = datatype;
-		this.payload = payload;
+		this.payload = getJSONString(payload);
 	}
 
 	public String getDatatype() {
@@ -26,8 +29,20 @@ public class DataTransferObject implements Serializable{
 	}
 
 	public void setPayload(Object payload) {
-		this.payload = payload;
+		this.payload = getJSONString(payload);
 	}
 	
+	public static String getJSONString(Object object) {
+		ObjectMapper mapper = new ObjectMapper();
+		String json = "";
+		try {
+			json = mapper.writeValueAsString(object);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return json;
+
+	}
 	
 }
