b710fbd38c8c760d89f6e85f3fa1202ae0de3796
Christian Kuka <christian@kuka.cc>
2014-07-13 07:49:29 +0000

    Context: Check overlapping timestamp in single element store
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20543 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5d0eaa46c60a930d698754ff124d55efd237ce8d

--- server/contextstore/odysseus.context/src/de/uniol/inf/is/odysseus/context/store/types/SingleElementStore.java
+++ server/contextstore/odysseus.context/src/de/uniol/inf/is/odysseus/context/store/types/SingleElementStore.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
+import de.uniol.inf.is.odysseus.core.metadata.TimeInterval;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
 
@@ -12,65 +13,63 @@ import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
  * @author Christian Kuka <christian.kuka@offis.de>
  * @param <T>
  */
-public class SingleElementStore<T extends Tuple<? extends ITimeInterval>>
-		extends AbstractContextStore<T> {
-	private T tuple = null;
+public class SingleElementStore<T extends Tuple<? extends ITimeInterval>> extends AbstractContextStore<T> {
+    private T tuple = null;
 
-	public SingleElementStore(String name, SDFSchema schema) {
-		super(name, schema, 1);
-	}
+    public SingleElementStore(String name, SDFSchema schema) {
+        super(name, schema, 1);
+    }
 
-	@Override
-	public void insertValue(T value) {
-		if (validateSchemaSizeOfValue(value)) {
-			if ((this.tuple == null)
-					|| (value.getMetadata().getStart().after(this.tuple
-							.getMetadata().getStart()))) {
-				this.tuple = value;
-				notifyListener();
-			}
-		} else {
-			logger.warn("Context store failure: size of value and schema do not match");
-		}
-	}
+    @Override
+    public void insertValue(T value) {
+        if (validateSchemaSizeOfValue(value)) {
+            if ((this.tuple == null) || (value.getMetadata().getStart().after(this.tuple.getMetadata().getStart()))) {
+                this.tuple = value;
+                notifyListener();
+            }
+        }
+        else {
+            logger.warn("Context store failure: size of value and schema do not match");
+        }
+    }
 
-	@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
 	@Override
 	public List<T> getValues(ITimeInterval timeinterval) {
 		List<T> list = new ArrayList<T>();
-		if (this.tuple != null) {
-			list.add((T) this.tuple.clone());
-		}
+        if ((this.tuple != null) && (TimeInterval.overlaps(this.tuple.getMetadata(), timeinterval))) {
+            list.add((T) this.tuple.clone());
+        }
 		return list;
 	}
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<T> getLastValues() {
-		List<T> list = new ArrayList<T>();
-		if (this.tuple == null) {
-			list.add((T) this.tuple.clone());
-		}
-		return list;
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<T> getLastValues() {
+        List<T> list = new ArrayList<T>();
+        if (this.tuple == null) {
+            list.add((T) this.tuple.clone());
+        }
+        return list;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<T> getAllValues() {
-		List<T> list = new ArrayList<T>();
-		if (this.tuple == null) {
-			list.add((T) this.tuple.clone());
-		}
-		return list;
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<T> getAllValues() {
+        List<T> list = new ArrayList<T>();
+        if (this.tuple == null) {
+            list.add((T) this.tuple.clone());
+        }
+        return list;
+    }
 
-	@Override
-	public void processTime(PointInTime time) {
-	}
+    @Override
+    public void processTime(PointInTime time) {
+    }
 
-	@Override
-	protected void internalClear() {
-		this.tuple = null;
-	}
+    @Override
+    protected void internalClear() {
+        this.tuple = null;
+    }
 
 }
