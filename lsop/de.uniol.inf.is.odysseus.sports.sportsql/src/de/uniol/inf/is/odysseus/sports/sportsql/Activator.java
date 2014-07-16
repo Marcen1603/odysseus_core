813e53433750e5dab6568c823947ecb1f2418a9f
Tobias Leo Brandt <tobias.leo.brandt@uni-oldenburg.de>
2014-07-16 13:18:10 +0000

    LSOP-129 We don't need the DataDictionary on this way
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20569 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 869270d4659dc364b8db47062d3fad0da0a3c046

--- lsop/de.uniol.inf.is.odysseus.sports.sportsql/src/de/uniol/inf/is/odysseus/sports/sportsql/Activator.java
+++ lsop/de.uniol.inf.is.odysseus.sports.sportsql/src/de/uniol/inf/is/odysseus/sports/sportsql/Activator.java
@@ -3,18 +3,9 @@ package de.uniol.inf.is.odysseus.sports.sportsql;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import com.google.common.base.Optional;
-
-import de.uniol.inf.is.odysseus.core.server.datadictionary.IDataDictionary;
-
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
-
-	/**
-	 * The bound {@link IDataDictionary}
-	 */
-	public static IDataDictionary dataDict;
 
 	static BundleContext getContext() {
 		return context;
@@ -41,34 +32,5 @@ public class Activator implements BundleActivator {
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
-	}
-
-	/**
-	 * Binds an {@link IDataDictionary}.
-	 * 
-	 * @param dict
-	 *            The {@link IDataDictionary} to bind.
-	 */
-	public static void bindDataDictionary(IDataDictionary dict) {
-		Activator.dataDict = dict;
-	}
-
-	/**
-	 * Unbinds an {@link IDataDictionary}, if <code>dict</code> is the bound
-	 * one.
-	 * 
-	 * @param dict
-	 *            The {@link IDataDictionary} to unbind.
-	 */
-	public static void unbindDataDictionary(IDataDictionary dict) {
-		if (Activator.dataDict != null || Activator.dataDict.equals(dict))
-			Activator.dataDict = null;
-	}
-
-	/**
-	 * Returns the bound {@link IDataDictionary}, if there is one bound.
-	 */
-	public static Optional<IDataDictionary> getDataDictionary() {
-		return Optional.fromNullable(Activator.dataDict);
 	}
 }
