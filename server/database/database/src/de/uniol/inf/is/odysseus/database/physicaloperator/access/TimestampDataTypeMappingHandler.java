9d403a0a19626b487becfc479c4da3d9d01acd7a
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-07-17 15:15:52 +0000

    TimestampDataTypeMapping (with long, untested!)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20590 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 562bea674fec1e52d0b0a534dbcad3eaf382d281

---
+++ server/database/database/src/de/uniol/inf/is/odysseus/database/physicaloperator/access/TimestampDataTypeMappingHandler.java
@@ -0,0 +1,31 @@
+package de.uniol.inf.is.odysseus.database.physicaloperator.access;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.Date;
+
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+
+public class TimestampDataTypeMappingHandler extends
+		AbstractDatatypeMappingHandler<Long> {
+
+	public TimestampDataTypeMappingHandler() {
+		super(SDFDatatype.TIMESTAMP, Types.TIMESTAMP);	
+	}
+
+	@Override
+	public void setValue(PreparedStatement preparedStatement, int position,
+			Object value) throws SQLException {
+		preparedStatement.setTimestamp(position, (Timestamp) new Date((long) value));		
+	}
+
+	@Override
+	public Long getValue(ResultSet result, int position)
+			throws SQLException {
+		return result.getTimestamp(position).getTime();
+	}
+
+}
