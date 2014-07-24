5d373329da13351eb87c07845e4e78731bdf1dfe
Carsten Cordes <ccordes@uni-oldenburg.de>
2014-07-24 20:53:34 +0000

    LSOP-165 Created cornerkicks Parser
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20674 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b4a98e99d2decf4c1aef978c1edb42068e244ece

---
+++ lsop/de.uniol.inf.is.odysseus.sports.sportsql/src/de/uniol/inf/is/odysseus/sports/sportsql/parser/impl/CornerKickGlobalSportsQLParser.java
@@ -0,0 +1,204 @@
+package de.uniol.inf.is.odysseus.sports.sportsql.parser.impl;
+
+import java.util.ArrayList;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.SDFExpressionParameter;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.ISportsQLParser;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.SportsQLParseException;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.SportsQLQuery;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.annotations.SportsQL;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.annotations.SportsQLParameter;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.buildhelper.OperatorBuildHelper;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.buildhelper.SportsQLParameterHelper;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.enums.GameType;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.enums.StatisticType;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.parameter.SportsQLDoubleParameter;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.parameter.SportsQLIntegerParameter;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.parameter.SportsQLSpaceParameter;
+import de.uniol.inf.is.odysseus.sports.sportsql.parser.parameter.SportsQLTimeParameter;
+
+@SportsQL(gameTypes = { GameType.SOCCER }, statisticTypes = { StatisticType.GLOBAL }, name = "cornerkicks", parameters = {
+		@SportsQLParameter(name = "datarate", parameterClass = SportsQLIntegerParameter.class, mandatory = false),
+		@SportsQLParameter(name = "accuracy", parameterClass = SportsQLDoubleParameter.class, mandatory = false),
+		@SportsQLParameter(name = "proximity", parameterClass = SportsQLDoubleParameter.class, mandatory = false),
+		@SportsQLParameter(name = "time", parameterClass = SportsQLTimeParameter.class, mandatory = true),
+		@SportsQLParameter(name = "space", parameterClass = SportsQLSpaceParameter.class, mandatory = true) })
+public class CornerKickGlobalSportsQLParser implements ISportsQLParser {
+
+	@Override
+	public ILogicalQuery parse(SportsQLQuery sportsQL)
+			throws SportsQLParseException {
+
+		//Fetch time and Space parameters from Query.
+		SportsQLTimeParameter timeParameter = SportsQLParameterHelper.getTimeParameter(sportsQL);;
+		SportsQLSpaceParameter spaceParameter = SportsQLParameterHelper.getSpaceParameter(sportsQL);;
+		
+		int xMin = spaceParameter.getStartx();
+		int xMax = spaceParameter.getEndx();
+		int yMin = spaceParameter.getStarty();
+		int yMax = spaceParameter.getEndy();
+
+		//TODO Remove hardcoded information (Data Dictionary?)
+		int goalLineLeft = -33968;
+		int goalLineRight = 33941;
+
+		String sourcename = "soccergame";
+		long startTS = timeParameter.getStart();
+
+		ArrayList<ILogicalOperator> operatorList = new ArrayList<ILogicalOperator>();
+		
+		ILogicalOperator source = OperatorBuildHelper
+				.createAccessAO(sourcename);
+
+		operatorList.add(source);
+		
+		ArrayList<String> projectionAttributes = new ArrayList<String>();
+		projectionAttributes.add("sid");
+		projectionAttributes.add("ts");
+		projectionAttributes.add("x");
+		projectionAttributes.add("y");
+
+		// projected = PROJECT({attributes = ['sid', 'ts', 'x',
+		// 'y']},soccergame)
+		ILogicalOperator projected = OperatorBuildHelper.createProjectAO(
+				projectionAttributes, source);
+		operatorList.add(projected);
+		
+		//Deleted RENAME Operator, because it's not needed.
+		
+		// activeBall = SELECT({
+		// predicate = '(sid = 4 OR sid = 8 OR sid = 10 OR sid = 12) AND x >=
+		// ${x_min}-500 AND x <= ${x_max}+500 AND y >= ${y_min}-500 AND y <=
+		// ${y_max}+500'
+		// },
+		// renamed
+		// )
+		//
+		ArrayList<String> predicates = new ArrayList<String>();
+		predicates.add("sid = 4 OR sid = 8 OR sid = 10 OR sid = 12"); 
+		predicates.add("x >= " + (xMin-500)); 
+		predicates.add("x <= " + (xMax+500));
+		predicates.add("y >= " + (yMin-500));
+		predicates.add("y <= " + (yMax+500));
+		ILogicalOperator activeBall = OperatorBuildHelper.createSelectAO(predicates, projected);
+		operatorList.add(activeBall);
+		
+		//
+		// activeBallBehindGoalline = MAP({
+		// expressions = [
+		// 'sid',
+		// 'ts',
+		// 'x',
+		// 'y',
+		// ['eif(y < ${goalline1}, 1, 0)', 'BallBehindGoalline1_left'],
+		// ['eif(y > ${goalline2}, 1, 0)', 'BallBehindGoalline2_right']
+		// ]}, activeBall)
+		ArrayList<SDFExpressionParameter> expressions = new ArrayList<SDFExpressionParameter>();
+		expressions.add(OperatorBuildHelper.createExpressionParameter("sid", activeBall));
+		expressions.add(OperatorBuildHelper.createExpressionParameter("ts", activeBall));
+		expressions.add(OperatorBuildHelper.createExpressionParameter("x", activeBall));
+		expressions.add(OperatorBuildHelper.createExpressionParameter("y", activeBall));
+		expressions.add(OperatorBuildHelper.createExpressionParameter("eif(y < " + goalLineLeft + ", 1, 0)", "BallBehindGoalline1_left", activeBall));
+		expressions.add(OperatorBuildHelper.createExpressionParameter("eif(y > " + goalLineRight + ", 1, 0)", "BallBehindGoalline2_right", activeBall));
+		
+		ILogicalOperator activeBallBehindGoalline = OperatorBuildHelper.createMapAO(expressions, activeBall, 0, 0);
+		operatorList.add(activeBallBehindGoalline);
+		
+		//
+		// activeBallBehindGoalline_change = CHANGEDETECT({
+		// attr = ['BallBehindGoalline1_left', 'BallBehindGoalline2_right']
+		// }, activeBallBehindGoalline)
+		
+		ArrayList<String> changeDetectBallAttributes = new ArrayList<String>();
+		changeDetectBallAttributes.add("BallBehindGoalline1_left");
+		changeDetectBallAttributes.add("BallBehindGoalline2_right");
+		
+		ILogicalOperator activeBallBehindGoallineChange  = OperatorBuildHelper.createChangeDetectAO(OperatorBuildHelper.createAttributeList(changeDetectBallAttributes, activeBallBehindGoalline), 0, activeBallBehindGoalline);
+		operatorList.add(activeBallBehindGoallineChange);
+		
+		// BallOnCornerSpot = MAP({
+				// expressions = [
+				// ['ts', 'spot_ball_ts'],
+				// ['eif((x <= (${x_min} + 2000) OR x >= (${x_max} - 2000)) AND y >=
+				// (${y_max} - 2000) , 1, 0)', 'BallOnCornerSpot1_bottom'],
+				// ['eif((x <= (${x_min} + 2000) OR x >= (${x_max} - 2000)) AND y <=
+				// (${y_min} + 2000) , 1, 0)', 'BallOnCornerSpot2_top']
+				// ]}, activeBall)
+		ArrayList<SDFExpressionParameter> ballExpressions = new ArrayList<SDFExpressionParameter>();
+		ballExpressions.add(OperatorBuildHelper.createExpressionParameter("ts", "spot_ball_ts",activeBall));
+		ballExpressions.add(OperatorBuildHelper.createExpressionParameter("eif((x <= ("+ xMin + " + 2000) OR x >= ("+ xMax + " - 2000)) AND y >= ("+ yMax +" - 2000) , 1, 0)", "BallOnCornerSpot1_bottom",activeBall));
+		ballExpressions.add(OperatorBuildHelper.createExpressionParameter("eif((x <= ("+ xMin + " + 2000) OR x >= ("+ xMax + " - 2000)) AND y <= ("+ yMin +" + 2000) , 1, 0)", "BallOnCornerSpot2_top",activeBall));
+		ILogicalOperator ballOnCornerSpot = OperatorBuildHelper.createMapAO(ballExpressions, activeBall, 0, 0);
+		operatorList.add(ballOnCornerSpot);
+		
+		// BallOnCornerSpot_change = CHANGEDETECT({
+		// attr = ['BallOnCornerSpot1_bottom', 'BallOnCornerSpot2_top']
+		// }, BallOnCornerSpot)
+		ArrayList<String> changeDetectCornerAttributes = new ArrayList<String>();
+		changeDetectCornerAttributes.add("BallOnCornerSpot1_bottom");
+		changeDetectCornerAttributes.add("BallOnCornerSpot2_top");
+		
+		ILogicalOperator ballOnCornerSpotChange  = OperatorBuildHelper.createChangeDetectAO(OperatorBuildHelper.createAttributeList(changeDetectCornerAttributes, ballOnCornerSpot), 0, ballOnCornerSpot);
+		operatorList.add(ballOnCornerSpotChange);
+		
+		// activeBallBehindGoalline_wnd = ELEMENTWINDOW({SIZE = 1, ADVANCE=1},
+		// activeBallBehindGoalline_change)
+		ILogicalOperator activeBallBehindGoallineWindow = OperatorBuildHelper.createTupleWindowAO(1, 1, activeBallBehindGoallineChange);
+		operatorList.add(activeBallBehindGoallineWindow);
+		
+		// BallOnCornerSpot_wnd = ELEMENTWINDOW({SIZE = 1, ADVANCE=1},
+		// BallOnCornerSpot_change)
+		ILogicalOperator ballOnCornerSpotWindow = OperatorBuildHelper.createTupleWindowAO(1, 1, ballOnCornerSpotChange);
+		operatorList.add(ballOnCornerSpotWindow);
+		
+		//
+		// corner_join = JOIN({predicate='(BallBehindGoalline1_left = 1 OR
+		// BallBehindGoalline2_right = 1) AND ts > (spot_ball_ts -
+		// 30000000000000) AND ts < spot_ball_ts AND
+		// (BallOnCornerSpot1_bottom = 1 OR BallOnCornerSpot2_top = 1) '},
+		// activeBallBehindGoalline_wnd, BallOnCornerSpot_wnd)
+		//
+		
+		ArrayList<String> joinPredicates = new ArrayList<String>();
+		joinPredicates.add("BallBehindGoalline1_left = 1 OR BallBehindGoalline2_right = 1");
+		joinPredicates.add("ts > (spot_ball_ts - 30000000000000)"); 
+		joinPredicates.add("ts < spot_ball_ts");
+		joinPredicates.add("BallOnCornerSpot1_bottom = 1 OR BallOnCornerSpot2_top = 1");
+		
+		ILogicalOperator cornerJoin = OperatorBuildHelper.createJoinAO(joinPredicates, activeBallBehindGoallineWindow, ballOnCornerSpotWindow);
+		operatorList.add(cornerJoin);
+		
+		// corners = CHANGEDETECT({
+		// attr = ['ts'],
+		// }, corner_join)
+		ArrayList<String> changeDetectCorners = new ArrayList<String>();
+		changeDetectCorners.add("ts");
+		ILogicalOperator corners = OperatorBuildHelper.createChangeDetectAO(OperatorBuildHelper.createAttributeList(changeDetectCorners,cornerJoin), 0, cornerJoin);
+		operatorList.add(corners);
+		
+		// corners_time = MAP({
+		// expressions = [
+		// 'BallBehindGoalline1_left',
+		// 'BallBehindGoalline2_right',
+		// ['((spot_ball_ts - ${STARTTS}) / 60000000000000.0)',
+		// 'timeInMinutes'],
+		// 'BallOnCornerSpot1_bottom',
+		// 'BallOnCornerSpot2_top'
+		// ]}, corners)
+
+		ArrayList<SDFExpressionParameter> cornersTimeExpressions = new ArrayList<SDFExpressionParameter>();
+		cornersTimeExpressions.add(OperatorBuildHelper.createExpressionParameter("BallBehindGoalline1_left", corners));
+		cornersTimeExpressions.add(OperatorBuildHelper.createExpressionParameter("BallBehindGoalline2_right", corners));
+		cornersTimeExpressions.add(OperatorBuildHelper.createExpressionParameter("((spot_ball_ts - "+ startTS + ") / 60000000000000.0)","timeInMinutes", corners));
+		cornersTimeExpressions.add(OperatorBuildHelper.createExpressionParameter("BallOnCornerSpot1_bottom", corners));
+		cornersTimeExpressions.add(OperatorBuildHelper.createExpressionParameter("BallOnCornerSpot2_top", corners));
+		
+		ILogicalOperator cornersTime = OperatorBuildHelper.createMapAO(cornersTimeExpressions, corners, 0, 0);
+		operatorList.add(cornersTime);
+		
+		return OperatorBuildHelper.finishQuery(cornersTime, operatorList, sportsQL.getName());
+	}
+
+}
