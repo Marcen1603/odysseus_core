dc3b5322c21a015b077bf30bfd631cb5378596a0
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-07-29 09:04:56 +0000

    Added optional NODE-Parameter to StreamAO (currently used only in P2P context)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20712 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 78401d68e287e482a800f95a2bf1ddda2f2548fb

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/StreamAO.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/StreamAO.java
@@ -1,5 +1,7 @@
 package de.uniol.inf.is.odysseus.core.server.logicaloperator;
 
+import com.google.common.base.Strings;
+
 import de.uniol.inf.is.odysseus.core.collection.Resource;
 import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalOperatorCategory;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
@@ -14,6 +16,7 @@ public class StreamAO extends AbstractLogicalOperator {
 	private static final long serialVersionUID = 1L;
 	private Resource streamname;
 	private SDFSchema schema;
+	private String nodeName;
 
 	public StreamAO() {
 		// we need this
@@ -23,20 +26,28 @@ public class StreamAO extends AbstractLogicalOperator {
 		super();
 		this.streamname = name;
 		setName(this.streamname.getResourceName());
+		addParameterInfo("SOURCE", "'" + streamname.getResourceName() + "'");
 	}
 
 	public StreamAO(StreamAO streamAO) {
 		super(streamAO);
+		
 		this.streamname = streamAO.streamname;
 		this.schema = streamAO.schema;
+		this.nodeName = streamAO.nodeName;
+		
 		setName(streamname.getResourceName());
+		addParameterInfo("SOURCE", "'" + streamname.getResourceName() + "'");
+		
+		if( !Strings.isNullOrEmpty(this.nodeName)) {
+			addParameterInfo("NODE", "'" + this.nodeName + "'");
+		}
 	}
-	
+
 	@Override
 	@Parameter(name = "Name", type = StringParameter.class, optional = true, doc = "Name of the operator (e.g. for visulization).")
 	public void setName(String name) {
 		super.setName(name);
-		addParameterInfo("SOURCE", "'"+name+"'");
 	}
 
 	@Override
@@ -47,22 +58,41 @@ public class StreamAO extends AbstractLogicalOperator {
 	public Resource getStreamname() {
 		return streamname;
 	}
-	
+
 	@Override
 	protected SDFSchema getOutputSchemaIntern(int pos) {
 		return schema;
 	}
 
-	@Parameter(name = "Source", type = SourceParameter.class, optional = false, possibleValues = "__DD_SOURCES", possibleValuesAreDynamic=true)
+	@Parameter(name = "Source", type = SourceParameter.class, optional = false, possibleValues = "__DD_SOURCES", possibleValuesAreDynamic = true)
 	public void setSource(AccessAO inputStream) {
 		this.schema = inputStream.getOutputSchema();
-		this.streamname = inputStream.getAccessAOName();
+		setSourceName(inputStream.getAccessAOName());
+	}
+	
+	public void setSourceName( Resource resource ) {
+		this.streamname = resource;
 		setName(streamname.getResourceName());
+		addParameterInfo("SOURCE", "'" + streamname.getResourceName() + "'");
 	}
 
 	@Override
 	public String toString() {
 		return "StreamAO@" + hashCode() + " " + streamname;
 	}
-
+	
+	@Parameter(name = "Node", type = StringParameter.class, optional = true)	
+	public void setNode( String nodeName ) {
+		this.nodeName = nodeName;
+		
+		if( !Strings.isNullOrEmpty(this.nodeName)) {
+			addParameterInfo("NODE", "'" + nodeName + "'");
+		} else {
+			removeParameterInfo("NODE");
+		}
+	}
+	
+	public String getNode() {
+		return nodeName;
+	}
 }
