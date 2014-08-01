1b4c6d1ae0e9a760669ddc051953632b6e661854
Michael Brand <michael.brand@uni-oldenburg.de>
2014-08-01 14:49:01 +0000

    LSOP-100 Implemented a simple load balancing strategy
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20780 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7952b4c036903e41affada4d1ec4898ddd3fa511

---
+++ server/peer/de.uniol.inf.is.odysseus.peer.loadbalancing.active.simple/src/de/uniol/inf/is/odysseus/peer/loadbalancing/active/simple/Activator.java
@@ -0,0 +1,285 @@
+package de.uniol.inf.is.odysseus.peer.loadbalancing.active.simple;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+
+import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
+import de.uniol.inf.is.odysseus.core.server.usermanagement.ISessionManagement;
+import de.uniol.inf.is.odysseus.core.server.usermanagement.UserManagementProvider;
+import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
+import de.uniol.inf.is.odysseus.p2p_new.IP2PNetworkManager;
+import de.uniol.inf.is.odysseus.p2p_new.dictionary.IP2PDictionary;
+import de.uniol.inf.is.odysseus.peer.resource.IPeerResourceUsageManager;
+
+/**
+ * The {@link BundleActivator} for the bundle <code>de.uniol.inf.is.odysseus.peer.loadbalancing.active.simple</code>.
+ * @author Michael Brand
+ */
+public class Activator implements BundleActivator {
+	
+	/**
+	 * The logger for this class.
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+	
+	/**
+	 * The {@link IExecutor}.
+	 */
+	private static IExecutor cExecutor;
+	
+	/**
+	 * The {@link ISessionManagement}.
+	 */
+	private static ISessionManagement cSessionManagement;
+	
+	/**
+	 * The {@link ISession}.
+	 */
+	private static ISession cSession;
+	
+	/**
+	 * The {@link IPeerResourceUsageManager}.
+	 */
+	private static IPeerResourceUsageManager cResourceManager;
+	
+	/**
+	 * The {@link IP2PDictionary}.
+	 */
+	private static IP2PDictionary cPeerDictionary;
+	
+	/**
+	 * The {@link IP2PNetworkManager}.
+	 */
+	private static IP2PNetworkManager cNetworkmanager;
+	
+	/**
+	 * Gets the executor.
+	 * @return The {@link IExecutor}, if one is bound.
+	 */
+	public static Optional<IExecutor> getExecutor() {
+		
+		return Optional.fromNullable(Activator.cExecutor);
+		
+	}
+	
+	/**
+	 * Binds an executor. <br />
+	 * Called by OSGI-DS.
+	 * @param executor The implementation of {@link IExecutor} to be bound.
+	 */
+	public static void bindExecutor(IExecutor executor) {
+		
+		Preconditions.checkNotNull("The executor to be bound must be not null!");
+		
+		Activator.cExecutor = executor;
+		LOG.debug("Bound {} as an implementation of {}", executor.getClass().getSimpleName(), IExecutor.class.getSimpleName());
+		
+	}
+	
+	/**
+	 * Removes the binding for an executor. <br />
+	 * Called by OSGI-DS.
+	 * @param executor The implementation of {@link IExecutor} to be unbound.
+	 */
+	public static void unbindExecutor(IExecutor executor) {
+		
+		if(executor != null && executor.equals(Activator.cExecutor)) {
+			
+			Activator.cExecutor = null;
+			LOG.debug("Unbound {} as an implementation of {}", executor.getClass().getSimpleName(), IExecutor.class.getSimpleName());
+			
+		}
+		
+	}
+	
+	/**
+	 * Gets the session.
+	 * @return The {@link ISession}, if an {@link ISessionManagement} is bound.
+	 */
+	public static Optional<ISession> getSession() {
+		
+		if(Activator.cSessionManagement == null) {
+			
+			return Optional.absent();
+			
+		} else if(Activator.cSession == null || !Activator.cSession.isValid()) {
+			
+			Activator.cSession = Activator.cSessionManagement.loginSuperUser(null, UserManagementProvider.getDefaultTenant().getName());
+			return Optional.of(Activator.cSession);
+			
+		}
+		
+		return Optional.of(Activator.cSession);
+		
+	}
+	
+	/**
+	 * Binds a session management. <br />
+	 * Called by OSGI-DS.
+	 * @param management The implementation of {@link ISessionManagement} to be bound.
+	 */
+	public static void bindSessionManagement(ISessionManagement management) {
+		
+		Preconditions.checkNotNull("The session management to be bound must be not null!");
+		
+		Activator.cSessionManagement = management;
+		LOG.debug("Bound {} as an implementation of {}", management.getClass().getSimpleName(), ISessionManagement.class.getSimpleName());
+		
+	}
+	
+	/**
+	 * Removes the binding for a session management. <br />
+	 * Called by OSGI-DS.
+	 * @param management The implementation of {@link ISessionManagement} to be unbound.
+	 */
+	public static void unbindSessionManagement(ISessionManagement management) {
+		
+		if(management != null && management.equals(Activator.cSessionManagement)) {
+			
+			Activator.cSessionManagement = null;
+			LOG.debug("Unbound {} as an implementation of {}", management.getClass().getSimpleName(), ISessionManagement.class.getSimpleName());
+			
+		}
+		
+	}
+	
+	/**
+	 * Gets the resource manager.
+	 * @return The {@link IPeerResourceUsageManager}, if one is bound.
+	 */
+	public static Optional<IPeerResourceUsageManager> getResourceManager() {
+		
+		return Optional.fromNullable(Activator.cResourceManager);
+		
+	}
+	
+	/**
+	 * Binds a resource manager. <br />
+	 * Called by OSGI-DS.
+	 * @param manager The implementation of {@link IPeerResourceUsageManager} to be bound.
+	 */
+	public static void bindResourceManager(IPeerResourceUsageManager manager) {
+		
+		Preconditions.checkNotNull("The resource manager to be bound must be not null!");
+		
+		Activator.cResourceManager = manager;
+		LOG.debug("Bound {} as an implementation of {}", manager.getClass().getSimpleName(), IPeerResourceUsageManager.class.getSimpleName());
+		
+	}
+	
+	/**
+	 * Removes the binding for a resource manager. <br />
+	 * Called by OSGI-DS.
+	 * @param manager The implementation of {@link IPeerResourceUsageManager} to be unbound.
+	 */
+	public static void unbindResourceManager(IPeerResourceUsageManager manager) {
+		
+		if(manager != null && manager.equals(Activator.cResourceManager)) {
+			
+			Activator.cResourceManager = null;
+			LOG.debug("Unbound {} as an implementation of {}", manager.getClass().getSimpleName(), IPeerResourceUsageManager.class.getSimpleName());
+			
+		}
+		
+	}
+	
+	/**
+	 * Gets the peer dictionary.
+	 * @return The {@link IP2PDictionary}, if one is bound.
+	 */
+	public static Optional<IP2PDictionary> getPeerDictionary() {
+		
+		return Optional.fromNullable(Activator.cPeerDictionary);
+		
+	}
+	
+	/**
+	 * Binds a peer dictionary. <br />
+	 * Called by OSGI-DS.
+	 * @param dictionary The implementation of {@link IP2PDictionary} to be bound.
+	 */
+	public static void bindPeerDictionary(IP2PDictionary dictionary) {
+		
+		Preconditions.checkNotNull("The peer dictionary to be bound must be not null!");
+		
+		Activator.cPeerDictionary = dictionary;
+		LOG.debug("Bound {} as an implementation of {}", dictionary.getClass().getSimpleName(), IP2PDictionary.class.getSimpleName());
+		
+	}
+	
+	/**
+	 * Removes the binding for a peer dictionary. <br />
+	 * Called by OSGI-DS.
+	 * @param dictionary The implementation of {@link IP2PDictionary} to be unbound.
+	 */
+	public static void unbindPeerDictionary(IP2PDictionary dictionary) {
+		
+		if(dictionary != null && dictionary.equals(Activator.cPeerDictionary)) {
+			
+			Activator.cPeerDictionary = null;
+			LOG.debug("Unbound {} as an implementation of {}", dictionary.getClass().getSimpleName(), IP2PDictionary.class.getSimpleName());
+			
+		}
+		
+	}
+	
+	/**
+	 * Gets the network manager.
+	 * @return The {@link IP2PNetworkManager}, if one is bound.
+	 */
+	public static Optional<IP2PNetworkManager> getNetworkManager() {
+		
+		return Optional.fromNullable(Activator.cNetworkmanager);
+		
+	}
+	
+	/**
+	 * Binds a network manager. <br />
+	 * Called by OSGI-DS.
+	 * @param manager The implementation of {@link IP2PNetworkManager} to be bound.
+	 */
+	public static void bindNetworkManager(IP2PNetworkManager manager) {
+		
+		Preconditions.checkNotNull("The network manager to be bound must be not null!");
+		
+		Activator.cNetworkmanager = manager;
+		LOG.debug("Bound {} as an implementation of {}", manager.getClass().getSimpleName(), IP2PNetworkManager.class.getSimpleName());
+		
+	}
+	
+	/**
+	 * Removes the binding for a network manager. <br />
+	 * Called by OSGI-DS.
+	 * @param manager The implementation of {@link IP2PNetworkManager} to be unbound.
+	 */
+	public static void unbindNetworkManager(IP2PNetworkManager manager) {
+		
+		if(manager != null && manager.equals(Activator.cNetworkmanager)) {
+			
+			Activator.cNetworkmanager = null;
+			LOG.debug("Unbound {} as an implementation of {}", manager.getClass().getSimpleName(), IP2PNetworkManager.class.getSimpleName());
+			
+		}
+		
+	}
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		
+		// Nothing to do.
+		
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		
+		// Nothing to do.
+		
+	}
+
+}
---
+++ server/peer/de.uniol.inf.is.odysseus.peer.loadbalancing.active.simple/src/de/uniol/inf/is/odysseus/peer/loadbalancing/active/simple/SimpleLoadBalancingStrategy.java
@@ -0,0 +1,429 @@
+package de.uniol.inf.is.odysseus.peer.loadbalancing.active.simple;
+
+import net.jxta.peer.PeerID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+
+import de.uniol.inf.is.odysseus.core.collection.IPair;
+import de.uniol.inf.is.odysseus.core.collection.Pair;
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
+import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
+import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
+import de.uniol.inf.is.odysseus.p2p_new.IP2PNetworkManager;
+import de.uniol.inf.is.odysseus.p2p_new.dictionary.IP2PDictionary;
+import de.uniol.inf.is.odysseus.peer.distribute.ILogicalQueryPart;
+import de.uniol.inf.is.odysseus.peer.distribute.LogicalQueryPart;
+import de.uniol.inf.is.odysseus.peer.distribute.QueryPartAllocationException;
+import de.uniol.inf.is.odysseus.peer.distribute.util.LogicalQueryHelper;
+import de.uniol.inf.is.odysseus.peer.loadbalancing.active.ILoadBalancingAllocator;
+import de.uniol.inf.is.odysseus.peer.loadbalancing.active.ILoadBalancingCommunicator;
+import de.uniol.inf.is.odysseus.peer.loadbalancing.active.ILoadBalancingStrategy;
+import de.uniol.inf.is.odysseus.peer.resource.IPeerResourceUsageManager;
+import de.uniol.inf.is.odysseus.peer.resource.IResourceUsage;
+
+/**
+ * The {@link SimpleLoadBalancingStrategy} defines a threshold for the load (CPU and storage). 
+ * If the current load is above this threshold, it takes the first query it gets from {@link IExecutor} and 
+ * allocates it using the allocator set by {@link #setAllocator(ILoadBalancingAllocator)}.
+ * @author Michael Brand
+ */
+public class SimpleLoadBalancingStrategy implements ILoadBalancingStrategy {
+	
+	/**
+	 * The logger for this class.
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(SimpleLoadBalancingStrategy.class);
+	
+	/**
+	 * The name of this strategy.
+	 */
+	public static final String NAME = "Simple";
+	
+	/**
+	 * The default threshold for the CPU load as percentage.
+	 */
+	public static final double DEFAULT_CPU_THRESHOLD = 0.8;
+	
+	/**
+	 * The default threshold for the memory load as percentage.
+	 */
+	public static final double DEFAULT_MEM_THRESHOLD = 0.8;
+	
+	/**
+	 * The default time between look ups of the local resources [ms].
+	 */
+	public static final long DEFAULT_RESOURCE_LOOKUP_TIME = 60000;
+	
+	/**
+	 * The {@link Thread} to look up the current resource usage. <br />
+	 * It will call {@link ILoadBalancingCommunicator#initiateLoadBalancing(PeerID, int)} if load balancing has to be done.
+	 */
+	private class SimpleLBThread extends Thread {
+		
+		/**
+		 * The allocator to use.
+		 */
+		protected ILoadBalancingAllocator mAllocator;
+		
+		/**
+		 * The communicator to use.
+		 */
+		protected ILoadBalancingCommunicator mCommunicator;
+		
+		/**
+		 * The threshold for the CPU load as percentage.
+		 */
+		protected volatile double mCPUThreshold = DEFAULT_CPU_THRESHOLD;
+		
+		/**
+		 * The threshold for the memory load as percentage.
+		 */
+		protected volatile double mMemThreshold = DEFAULT_MEM_THRESHOLD;
+		
+		/**
+		 * The time between look ups of the local resources [ms].
+		 */
+		protected volatile long mResourceLookupTime = DEFAULT_RESOURCE_LOOKUP_TIME;
+		
+		/**
+		 * The current resource usage.
+		 */
+		protected IResourceUsage mUsage;
+		
+		/**
+		 * Compares the current with the last usage.
+		 * @param currentUsage The current {@link IResourceUsage}.
+		 * @param lastUsage The last known {@link IResourceUsage}.
+		 * @return True, if CPU or storage usage have changed.
+		 */
+		private boolean hasUsageChanged(IResourceUsage currentUsage, IResourceUsage lastUsage) {
+			
+			return 	currentUsage.getCpuMax() != lastUsage.getCpuMax() ||
+					currentUsage.getCpuFree() != lastUsage.getCpuFree() ||
+					currentUsage.getMemMaxBytes() != lastUsage.getMemMaxBytes() ||
+					currentUsage.getMemFreeBytes() != lastUsage.getMemFreeBytes();
+			
+		}
+		
+		/**
+		 * Checks, if load balancing should be initiated.
+		 * @param usage The current {@link IResourceUsage}.
+		 * @return True, if there is more CPU in use as determined by {@link #mCPUThreshold} or if there is more memory in use as determined by {@link #mMemThreshold}.
+		 * @throws InterruptedException if any error occurs.
+		 */
+		private boolean loadBalancingNeeded(IResourceUsage usage) throws InterruptedException {
+		
+			final double cpuMax = usage.getCpuMax();
+			final double cpuFree = usage.getCpuFree();
+			final long memMax = usage.getMemMaxBytes();
+			final long memFree = usage.getMemFreeBytes();
+			
+			// Preconditions for upcoming fractions
+			if(cpuMax == 0) {
+				
+				throw new InterruptedException("CPU max has the value 0");
+				
+			} else if(memMax == 0) {
+				
+				throw new InterruptedException("Mem max has the value 0");
+				
+			}
+			
+			final double cpuUsedPercentage = (cpuMax - cpuFree) / cpuMax;
+			final double memUsedPercentage = (memMax - memFree) / cpuMax;
+			return cpuUsedPercentage > this.mCPUThreshold || memUsedPercentage > this.mMemThreshold;
+			
+		}
+		
+		/**
+		 * Gets the query to remove.
+		 * @return A pair of {@link ILogicalQuery} and the corresponding query ID.
+		 * @throws InterruptedException if any error occurs.
+		 */
+		private IPair<ILogicalQuery, Integer> getQueryToRemove() throws InterruptedException {
+			
+			final IExecutor executor = Activator.getExecutor().get();
+			final ISession session = Activator.getSession().get();
+			final ImmutableCollection<Integer> queryIDs = ImmutableList.copyOf(executor.getLogicalQueryIds(session));
+			if(queryIDs.isEmpty()) {
+				
+				throw new InterruptedException("No query found to be handled over!");
+				
+			}
+			
+			final int queryID = queryIDs.iterator().next();
+			return new Pair<ILogicalQuery, Integer>(executor.getLogicalQueryById(queryID, session), queryID);
+			
+		}
+		
+		/**
+		 * Gets the ID of the peer to allocate.
+		 * @param query The {@link ILogicalQuery} to be allocated.
+		 * @return The ID of the peer, to which <code>query</code> has been allocated to.
+		 * @throws InterruptedException if any error occurs.
+		 */
+		private PeerID getPeerIDToAllocate(ILogicalQuery query) throws InterruptedException {
+			
+			final IP2PDictionary peerDictionary = Activator.getPeerDictionary().get();
+			final IP2PNetworkManager networkManager = Activator.getNetworkManager().get();
+			final ImmutableCollection<ILogicalOperator> partOperators = ImmutableList.copyOf(LogicalQueryHelper.getAllOperators(query.getLogicalPlan()));
+			final ILogicalQueryPart queryPart = new LogicalQueryPart(partOperators);
+			final ImmutableCollection<ILogicalQueryPart> queryParts = ImmutableList.copyOf(Lists.newArrayList(queryPart));			
+			final ImmutableCollection<PeerID> remotePeers = ImmutableList.copyOf(peerDictionary.getRemotePeerIDs());
+			final PeerID localPeerID = networkManager.getLocalPeerID();
+			ImmutableMap<ILogicalQueryPart, PeerID> allocationMap;
+			
+			try {
+				
+				allocationMap= ImmutableMap.copyOf(this.mAllocator.allocate(queryParts, query, remotePeers, localPeerID));
+				
+			} catch(QueryPartAllocationException e) {
+				
+				throw new InterruptedException("Error while allocation: " + e.getMessage());
+				
+			}
+			
+			if(allocationMap.isEmpty()) {
+				
+				throw new InterruptedException("No peers found for load balancing!");
+				
+			}
+			
+			return allocationMap.get(queryPart);
+			
+		}
+		
+		/**
+		 * Updates the resource usage and checks, if the threshold has been reached. <br />
+		 * Initializes load balancing, if that happens.
+		 * @param usage The new {@link IResourceUsage}.
+		 * @throws InterruptedException if any error occurs
+		 */
+		private void updateUsage(IResourceUsage usage) throws InterruptedException {
+			
+			// Preconditions
+			if(this.mAllocator == null) {
+				
+				throw new InterruptedException("No allocator set");
+				
+			} else if(this.mCommunicator == null) {
+				
+				throw new InterruptedException("No communicator set");
+				
+			} else if(!Activator.getExecutor().isPresent()) {
+				
+				throw new InterruptedException("No executor bound");
+				
+			} else if(!Activator.getSession().isPresent()) {
+				
+				throw new InterruptedException("No session management bound");
+				
+			} else if(!Activator.getPeerDictionary().isPresent()) {
+				
+				throw new InterruptedException("No peer dictionary set");
+				
+			} else if(!Activator.getNetworkManager().isPresent()) {
+				
+				throw new InterruptedException("No network manager set");
+				
+			}
+					
+			this.mUsage = usage;
+			
+			if(this.loadBalancingNeeded(usage)) {
+				
+				// Get the query
+				IPair<ILogicalQuery, Integer> queryAndID = this.getQueryToRemove();
+				
+				// Get the peer
+				PeerID peerID = this.getPeerIDToAllocate(queryAndID.getE1());
+				
+				// Initialize the load balancing
+				this.mCommunicator.initiateLoadBalancing(peerID, queryAndID.getE2());
+				
+			}
+			
+		}
+		
+		/**
+		 * Creates a new {@link SimpleLBThread}.
+		 */
+		public SimpleLBThread() {
+			
+			super("Simple Load Balancing Thread");
+			
+		}
+		
+		@Override
+		public void run() {
+			
+			Preconditions.checkArgument(Activator.getResourceManager().isPresent(), "No resource manager bound");
+			
+			final IPeerResourceUsageManager resourceManager = Activator.getResourceManager().get();
+			
+			while(this.isAlive() && !this.isInterrupted()) {
+			
+				try {
+				
+					final Optional<IResourceUsage> lastUsage = Optional.fromNullable(this.mUsage);
+					final IResourceUsage usage = resourceManager.getLocalResourceUsage();
+					
+					if(!lastUsage.isPresent() || this.hasUsageChanged(usage, lastUsage.get())) {
+						
+						this.updateUsage(usage);
+						
+					}
+					
+					Thread.sleep(this.mResourceLookupTime);
+					
+				} catch(InterruptedException e) {
+					
+					LOG.error(e.getMessage());
+					break;
+					
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	/**
+	 * The {@link SimpleLBThread} to look up the current resource usage. <br />
+	 * It will call {@link ILoadBalancingCommunicator#initiateLoadBalancing(PeerID, int)} if load balancing has to be done.
+	 */
+	private SimpleLBThread mLookupThread = new SimpleLBThread();
+
+	@Override
+	public String getName() {
+		
+		return NAME;
+		
+	}
+
+	@Override
+	public void setAllocator(ILoadBalancingAllocator allocator) {
+		
+		Preconditions.checkNotNull(allocator, "The load balancing allocator to be used must be not null!");
+		
+		synchronized (this.mLookupThread.mAllocator) {
+			
+			this.mLookupThread.mAllocator = allocator;
+			
+		}
+		
+		LOG.debug("Set {} as implementation of {}", allocator.getClass().getSimpleName(), ILoadBalancingAllocator.class.getSimpleName());
+		
+	}
+
+	@Override
+	public void setCommunicator(ILoadBalancingCommunicator communicator) {
+		
+		Preconditions.checkNotNull(communicator, "The load balancing communicator to be used must be not null!");
+		
+		synchronized (this.mLookupThread.mCommunicator) {
+
+			this.mLookupThread.mCommunicator = communicator;
+			
+		}
+		
+		LOG.debug("Set {} as implementation of {}", communicator.getClass().getSimpleName(), ILoadBalancingCommunicator.class.getSimpleName());
+		
+	}
+	
+	/**
+	 * Gets the CPU threshold.
+	 * @return The threshold for the CPU load as percentage.
+	 */
+	public double getCPUThreshold() {
+		
+		return this.mLookupThread.mCPUThreshold;
+		
+	}
+	
+	/**
+	 * Sets the CPU threshold.
+	 * @param threshold The threshold for the CPU load as percentage.
+	 */
+	public void setCPUThreshold(double threshold) {
+		
+		Preconditions.checkArgument(threshold >= 0 && threshold <= 100, threshold + " is not a valid percentage for the CPU threshold!");
+		
+		this.mLookupThread.mCPUThreshold = threshold;
+		LOG.debug("Set the CPU threshold to {} percent", threshold);
+		
+	}
+	
+	/**
+	 * Gets the memory threshold.
+	 * @return The threshold for the memory load as percentage.
+	 */
+	public double getMemoryThreshold() {
+		
+		return this.mLookupThread.mMemThreshold;
+		
+	}
+	
+	/**
+	 * Sets the memory threshold.
+	 * @param threshold The threshold for the memory load as percentage.
+	 */
+	public void setMemoryThreshold(double threshold) {
+		
+		Preconditions.checkArgument(threshold >= 0 && threshold <= 100, threshold + " is not a valid percentage for the memory threshold!");
+		
+		this.mLookupThread.mMemThreshold = threshold;
+		LOG.debug("Set the memory threshold to {} percent", threshold);
+		
+	}
+	
+	/**
+	 * Gets the loop up time.
+	 * @return The time between look ups of the local resources [ms].
+	 */
+	public long getLookupTime() {
+		
+		return this.mLookupThread.mResourceLookupTime;
+		
+	}
+	
+	/**
+	 * Sets the loop up time.
+	 * @param time The time between look ups of the local resources [ms].
+	 */
+	public void setLookupTime(long time) {
+		
+		Preconditions.checkArgument(time > 0, time + " is not a valid time [ms] for resource look ups!");
+		
+		this.mLookupThread.mResourceLookupTime = time;
+		LOG.debug("Set the resource look up time to {} ms", time);
+		
+	}
+
+	@Override
+	public void startMonitoring() throws LoadBalancingException {
+		
+		this.mLookupThread.start();
+		LOG.debug("Started monitoring local resources");
+		
+	}
+
+	@Override
+	public void stopMonitoring() {
+		
+		this.mLookupThread.interrupt();
+		LOG.debug("Stopped monitoring local resources");
+		
+	}
+
+}
