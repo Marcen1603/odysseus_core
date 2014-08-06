7b59d8c698b192894dee914d0a57ef00577952f6
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-08-06 12:40:36 +0000

    New plugin: Wrapper for temperature sensor
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20836 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 75f7c7b82cac884b6e8daccdbb4c1f46d15da7ed

---
+++ server/wrapper/temper1/src/de/uniol/inf/is/odysseus/wrapper/temper1/logicaloperator/Temper1AccessAO.java
@@ -0,0 +1,69 @@
+package de.uniol.inf.is.odysseus.wrapper.temper1.logicaloperator;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalOperatorCategory;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.ProtocolHandlerRegistry;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.AbstractAccessAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.AbstractLogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.LogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.Parameter;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.StringParameter;
+import de.uniol.inf.is.odysseus.core.server.util.Constants;
+
+
+@LogicalOperator(maxInputPorts = 0, minInputPorts = 0, name = "Temper1ACCESS", doc="Returns the value of a temperature sensor of the type TEMPer1.", category={LogicalOperatorCategory.SOURCE})
+public class Temper1AccessAO extends AbstractAccessAO {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public Temper1AccessAO() {
+		super();
+		//setTransportHandler(HTTPStreamTransportHandler.NAME);
+		setWrapper(Constants.GENERIC_PULL);
+		
+		List<SDFAttribute> schema = new LinkedList<>();
+		schema.add(new SDFAttribute(null, "value", SDFDatatype.STRING, null));
+		schema.add(new SDFAttribute(null, "temperature", SDFDatatype.DOUBLE, null));
+		schema.add(new SDFAttribute(null, "symbol", SDFDatatype.STRING, null));
+		schema.add(new SDFAttribute(null, "timestamp", SDFDatatype.TIMESTAMP, null));
+		setAttributes(schema);
+	}
+	
+	public Temper1AccessAO(Temper1AccessAO temper1AccessAO) {
+		super(temper1AccessAO);
+	}
+	
+	@Override
+	public AbstractLogicalOperator clone() {
+		return new Temper1AccessAO(this);
+	}
+	
+	
+	@Override
+	@Parameter(type = StringParameter.class, name = "protocol", optional = false, possibleValues="getProtocolValues", doc = "The name of the protocol handler to use, e.g. Csv or SizeByteBuffer.")
+	public void setProtocolHandler(String protocolHandler) {
+		super.setProtocolHandler(protocolHandler);
+	}
+	
+	public List<String> getProtocolValues(){
+		return ProtocolHandlerRegistry.getHandlerNames();
+	}
+	
+	@Override
+	@Parameter(type = StringParameter.class, name = "DataHandler", optional = false, possibleValues="getDataHandlerValues", doc = "The name of the datahandler to use, e.g. Tuple or Document.")
+	public void setDataHandler(String dataHandler) {
+		super.setDataHandler(dataHandler);
+	}
+	
+	//@Parameter(type = StringParameter.class, name = HTTPStreamTransportHandler.URI, optional = false, doc = "URI")
+	//public void setURI(String uri){
+	//	addOption(HTTPStreamTransportHandler.URI, uri);
+	//}
+	
+}
