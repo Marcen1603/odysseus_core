5b47e14955b70a5dcb2d6d895c9998ffd96f26cd
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-08-07 11:17:52 +0000

    SystemLoad update interval is configurable now... setting name is "systemLoadInterval", default value is 3000 ms
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20852 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 2de89804052e9b55ff76f3293ec4fd41b7bc1e2d

--- server/systemload/de.uniol.inf.is.odysseus.systemload/src/de/uniol/inf/is/odysseus/systemload/impl/BufferedSigarWrapper.java
+++ server/systemload/de.uniol.inf.is.odysseus.systemload/src/de/uniol/inf/is/odysseus/systemload/impl/BufferedSigarWrapper.java
@@ -6,13 +6,18 @@ import org.hyperic.sigar.Sigar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.uniol.inf.is.odysseus.core.server.OdysseusConfiguration;
+
 public class BufferedSigarWrapper {
 
 	private static final Logger LOG = LoggerFactory.getLogger(BufferedSigarWrapper.class);
 
+	private static final String BUFFER_TIME_SETTING_NAME = "systemLoadInterval";
 	private static final long DEFAULT_NET_BANDWIDTH_KB = 1024 * 10;
-	private static final long BUFFER_TIME_MILLIS = 3 * 1000;
-	
+	private static final long BUFFER_TIME_MILLIS_DEFAULT = 3 * 1000;
+
+	private static long BUFFER_TIME_MILLIS = -1;
+
 	private final Sigar sigar;
 
 	private long previousInputTotal = 0;
@@ -20,11 +25,11 @@ public class BufferedSigarWrapper {
 
 	private double cpuMax;
 	private long netMax;
-	
+
 	private double cpuFreeBuffer;
 	private double netInBuffer;
 	private double netOutBuffer;
-	
+
 	private long cpuFreeTimestamp;
 	private long netInTimestamp;
 	private long netOutTimestamp;
@@ -34,6 +39,17 @@ public class BufferedSigarWrapper {
 
 		cpuMax = getCpuMaxImpl();
 		netMax = getNetMaxImpl();
+
+		if (BUFFER_TIME_MILLIS == -1) {
+			checkBufferTimeMillis();
+		}
+	}
+
+	private static void checkBufferTimeMillis() {
+		BUFFER_TIME_MILLIS = OdysseusConfiguration.getLong(BUFFER_TIME_SETTING_NAME, BUFFER_TIME_MILLIS_DEFAULT);
+		if (BUFFER_TIME_MILLIS <= 0) {
+			BUFFER_TIME_MILLIS = BUFFER_TIME_MILLIS_DEFAULT;
+		}
 	}
 
 	private double getCpuMaxImpl() {
@@ -67,18 +83,19 @@ public class BufferedSigarWrapper {
 
 	public double getCpuFree() {
 		try {
-			if( System.currentTimeMillis() - cpuFreeTimestamp > BUFFER_TIME_MILLIS ) {
+			if (System.currentTimeMillis() - cpuFreeTimestamp > BUFFER_TIME_MILLIS) {
 				CpuPerc perc = sigar.getCpuPerc();
 				double cpuFree = cpuMax - (perc != null ? perc.getUser() : 0.0) * cpuMax;
 				double result = Math.min(cpuMax, Math.max(0, cpuFree));
 				if (Double.isNaN(result)) {
 					result = 0.0;
 				}
-				
+
 				cpuFreeBuffer = result;
 				cpuFreeTimestamp = System.currentTimeMillis();
+				checkBufferTimeMillis();
 			}
-			
+
 			return cpuFreeBuffer;
 
 		} catch (Throwable t) {
@@ -89,16 +106,17 @@ public class BufferedSigarWrapper {
 
 	public double getNetInputRate() {
 		try {
-			if( System.currentTimeMillis() - netInTimestamp > BUFFER_TIME_MILLIS ) {
+			if (System.currentTimeMillis() - netInTimestamp > BUFFER_TIME_MILLIS) {
 				String interfaceName = sigar.getNetInterfaceConfig(null).getName();
 				NetInterfaceStat net = sigar.getNetInterfaceStat(interfaceName);
 				long inputTotal = net != null ? net.getRxBytes() : previousInputTotal;
 				previousInputTotal = inputTotal;
-				
+
 				netInBuffer = Math.max(0, (inputTotal - previousInputTotal) / 1024);
 				netInTimestamp = System.currentTimeMillis();
+				checkBufferTimeMillis();
 			}
-			
+
 			return netInBuffer;
 
 		} catch (Throwable t) {
@@ -109,18 +127,19 @@ public class BufferedSigarWrapper {
 
 	public double getNetOutputRate() {
 		try {
-			if( System.currentTimeMillis() - netOutTimestamp > BUFFER_TIME_MILLIS ) {
+			if (System.currentTimeMillis() - netOutTimestamp > BUFFER_TIME_MILLIS) {
 				String interfaceName = sigar.getNetInterfaceConfig(null).getName();
 				NetInterfaceStat net = sigar.getNetInterfaceStat(interfaceName);
 				long outputTotal = net != null ? net.getTxBytes() : previousOutputTotal;
 				previousOutputTotal = outputTotal;
-	
+
 				netOutBuffer = Math.max(0, (outputTotal - previousOutputTotal) / 1024);
 				netOutTimestamp = System.currentTimeMillis();
+				checkBufferTimeMillis();
 			}
-			
+
 			return netOutBuffer;
-			
+
 		} catch (Throwable t) {
 			LOG.warn("Could not get net output rate from sigar", t);
 			return 0.0;
