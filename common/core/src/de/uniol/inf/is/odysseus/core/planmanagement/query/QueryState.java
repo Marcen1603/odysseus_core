6fc012b9d1c3d919e10aadc8b9e1197456d4198b
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-08-12 14:32:02 +0000

    StateMachine for QueryStates
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20904 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 2df262ae4b6aad2aa303c352beb7c416f77884b0

---
+++ common/core/src/de/uniol/inf/is/odysseus/core/planmanagement/query/QueryState.java
@@ -0,0 +1,106 @@
+package de.uniol.inf.is.odysseus.core.planmanagement.query;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum QueryState {
+	INACTIVE, PARTIAL, ACTIVE, PARTIAL_SUSPENDED, SUSPENDED;
+
+	static Map<QueryState, QueryState[]> reachable = new HashMap<>();
+	static {
+		reachable.put(INACTIVE, new QueryState[] { PARTIAL, ACTIVE });
+		reachable
+				.put(ACTIVE, new QueryState[] { PARTIAL, INACTIVE, SUSPENDED });
+		reachable.put(PARTIAL, new QueryState[] { INACTIVE, ACTIVE,
+				PARTIAL_SUSPENDED });
+		reachable.put(PARTIAL_SUSPENDED,
+				new QueryState[] { SUSPENDED, PARTIAL });
+		reachable
+				.put(SUSPENDED, new QueryState[] { ACTIVE, PARTIAL_SUSPENDED });
+	}
+
+	static public QueryState[] reachable(QueryState current) {
+		return reachable.get(current);
+	}
+
+	static public QueryState next(QueryState current, QueryFunction function) {
+		switch (current) {
+		case INACTIVE:
+			if (function == QueryFunction.START) {
+				return ACTIVE;
+			}
+			if (function == QueryFunction.PARTIAL) {
+				return PARTIAL;
+			}
+			break;
+		case ACTIVE:
+			if (function == QueryFunction.STOP) {
+				return INACTIVE;
+			}
+			if (function == QueryFunction.PARTIAL) {
+				return PARTIAL;
+			}
+			if (function == QueryFunction.SUSPEND) {
+				return SUSPENDED;
+			}
+			break;
+		case PARTIAL:
+			if (function == QueryFunction.STOP) {
+				return INACTIVE;
+			}
+			if (function == QueryFunction.FULL) {
+				return PARTIAL;
+			}
+			if (function == QueryFunction.SUSPEND) {
+				return PARTIAL_SUSPENDED;
+			}
+			break;
+		case PARTIAL_SUSPENDED:
+			if (function == QueryFunction.RESTART) {
+				return PARTIAL;
+			}
+			if (function == QueryFunction.FULL) {
+				return SUSPENDED;
+			}
+			break;
+		case SUSPENDED:
+			if (function == QueryFunction.RESTART) {
+				return ACTIVE;
+			}
+			if (function == QueryFunction.PARTIAL) {
+				return PARTIAL_SUSPENDED;
+			}
+			break;
+		}
+		throw new IllegalStateException("Cannot call " + function.name()
+				+ " in " + current.name());
+	}
+
+	public static void main(String[] args) {
+		for (QueryState s : QueryState.values()) {
+			System.out.print("Reachable from " + s.name() + ": ");
+			print(reachable(s));
+		}
+
+		for (QueryState s : QueryState.values()) {
+			for (QueryFunction f : QueryFunction.values()) {
+				try {
+					System.out.print("Move from " + s.name() + " with " + f.name()
+							+ " to " + next(s, f));
+				} catch (IllegalStateException e) {
+					System.out.print("INVALID! " + e.getMessage());
+				}
+				System.out.println();
+			}
+		}
+
+	}
+
+	private static void print(QueryState[] reachable) {
+		for (QueryState s : reachable) {
+			System.out.print(s.name() + " ");
+		}
+		System.out.println();
+	}
+
+}
