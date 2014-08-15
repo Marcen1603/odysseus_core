2c118d64471334139c59da83e204712f41163e40
Christian Kuka <christian@kuka.cc>
2014-08-15 13:36:10 +0000

    Wrapper: Fix header
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20948 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 77b261269971755a1ea207fbbe6de22dcc8ef302

--- server/wrapper/tika/de.uniol.inf.is.odysseus.wrapper.tika/src/de/uniol/inf/is/odysseus/wrapper/tika/physicaloperator/access/TikaProtocolHandler.java
+++ server/wrapper/tika/de.uniol.inf.is.odysseus.wrapper.tika/src/de/uniol/inf/is/odysseus/wrapper/tika/physicaloperator/access/TikaProtocolHandler.java
@@ -1,19 +1,18 @@
 /*******************************************************************************
- * Copyright (C) 2014  Christian Kuka <christian@kuka.cc>
+ * Copyright 2014 The Odysseus Team
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.uniol.inf.is.odysseus.wrapper.tika.physicaloperator.access;
 
 import java.io.ByteArrayInputStream;
@@ -27,6 +26,7 @@ import java.util.Map.Entry;
 
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.language.LanguageIdentifier;
+import org.apache.tika.metadata.HttpHeaders;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.AutoDetectParser;
@@ -128,10 +128,10 @@ public class TikaProtocolHandler extends LineProtocolHandler<KeyValueObject<? ex
         try {
             this.parser.parse(stream, new BodyContentHandler(stringWriter), metadata, context);
             String content = stringWriter.toString();
-            if (metadata.get(Metadata.CONTENT_LANGUAGE) == null) {
+            if (metadata.get(HttpHeaders.CONTENT_LANGUAGE) == null) {
                 LanguageIdentifier identifier = new LanguageIdentifier(content);
                 String language = identifier.getLanguage();
-                metadata.add(Metadata.CONTENT_LANGUAGE, language);
+                metadata.add(HttpHeaders.CONTENT_LANGUAGE, language);
             }
             for (final String key : metadata.names()) {
                 event.put(key.toLowerCase(Locale.US), metadata.get(key));
@@ -163,10 +163,10 @@ public class TikaProtocolHandler extends LineProtocolHandler<KeyValueObject<? ex
         try {
             this.parser.parse(stream, new BodyContentHandler(stringWriter), metadata, context);
             String content = stringWriter.toString();
-            if (metadata.get(Metadata.CONTENT_LANGUAGE) == null) {
+            if (metadata.get(HttpHeaders.CONTENT_LANGUAGE) == null) {
                 LanguageIdentifier identifier = new LanguageIdentifier(content);
                 String language = identifier.getLanguage();
-                metadata.add(Metadata.CONTENT_LANGUAGE, language);
+                metadata.add(HttpHeaders.CONTENT_LANGUAGE, language);
             }
             for (final String key : metadata.names()) {
                 event.put(key.toLowerCase(Locale.US), metadata.get(key));
