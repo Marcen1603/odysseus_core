6bba5f7046aaa3b89ab977ca064316012cc54894
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-08-18 08:49:30 +0000

    Avoid potential null pointer for "empty" version
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@20967 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 52d59c86171143f98de07b27bab95d93f8507590

--- common/mep/src/de/uniol/inf/is/odysseus/mep/functions/IfFunction.java
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/functions/IfFunction.java
@@ -1,18 +1,18 @@
 /********************************************************************************** 
-  * Copyright 2011 The Odysseus Team
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2011 The Odysseus Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.uniol.inf.is.odysseus.mep.functions;
 
 import org.slf4j.Logger;
@@ -26,42 +26,42 @@ public class IfFunction extends AbstractFunction<Object> {
 	private static final Logger LOG = LoggerFactory.getLogger(IfFunction.class);
 	private static final long serialVersionUID = 7330905069703118113L;
 
-	public static SDFDatatype[][] accTypes = new SDFDatatype[][]{
-		{SDFDatatype.BOOLEAN},
-		{SDFDatatype.OBJECT},
-		{SDFDatatype.OBJECT}};
+	public static SDFDatatype[][] accTypes = new SDFDatatype[][] {
+			{ SDFDatatype.BOOLEAN }, { SDFDatatype.OBJECT },
+			{ SDFDatatype.OBJECT } };
 
-	
 	public IfFunction() {
-		super("eif",3,accTypes,null);
+		super("eif", 3, accTypes, null);
 	}
-	
 
 	@Override
 	public Object getValue() {
 		return getInputValue(0) ? getInputValue(1) : getInputValue(2);
 	}
 
-	
+
 	@Override
 	protected SDFDatatype determineReturnType() {
 		// if then and else arguments have the same type, we are sure to return
 		// a value of that type
-		SDFDatatype type1 = getArguments()[1].getReturnType();
-		SDFDatatype type2 = getArguments()[2].getReturnType();
-		if (type1 == type2) {
-			return getArguments()[1].getReturnType();
-		}
-		
-		if( type1.compatibleTo(type2)) {
-			return type2;
-		} else if( type2.compatibleTo(type1)) {
-			return type1;
-		} else {
-			LOG.warn("Incompatible return types in if-Function");
+		if (getArguments() != null && getArguments().length == 2) {
+			SDFDatatype type1 = getArguments()[1].getReturnType();
+			SDFDatatype type2 = getArguments()[2].getReturnType();
+			if (type1 == type2) {
+				return getArguments()[1].getReturnType();
+			}
+
+			if (type1.compatibleTo(type2)) {
+				return type2;
+			} else if (type2.compatibleTo(type1)) {
+				return type1;
+			} else {
+				LOG.warn("Incompatible return types in if-Function");
+				return SDFDatatype.OBJECT;
+			}
+		}else{
 			return SDFDatatype.OBJECT;
 		}
 	}
-	
 
 }
