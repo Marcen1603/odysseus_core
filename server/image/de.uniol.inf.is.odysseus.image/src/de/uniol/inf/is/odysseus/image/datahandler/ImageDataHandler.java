af03d62b24f44ac23063fba89e66cfb527b47755
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-08-19 12:36:14 +0000

    read image from input stream
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21014 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f2de600e4b96c5cb1630468fad7453e5b8848efc

--- server/image/de.uniol.inf.is.odysseus.image/src/de/uniol/inf/is/odysseus/image/datahandler/ImageDataHandler.java
+++ server/image/de.uniol.inf.is.odysseus.image/src/de/uniol/inf/is/odysseus/image/datahandler/ImageDataHandler.java
@@ -15,11 +15,16 @@
  */
 package de.uniol.inf.is.odysseus.image.datahandler;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import de.uniol.inf.is.odysseus.core.datahandler.AbstractDataHandler;
 import de.uniol.inf.is.odysseus.core.datahandler.IDataHandler;
@@ -31,77 +36,94 @@ import de.uniol.inf.is.odysseus.image.common.datatype.Image;
  * 
  */
 public class ImageDataHandler extends AbstractDataHandler<Image> {
-    static protected List<String> types = new ArrayList<String>();
-    static {
-        ImageDataHandler.types.add("Image");
-    }
+	static protected List<String> types = new ArrayList<String>();
+	static {
+		ImageDataHandler.types.add("Image");
+	}
 
-    @Override
-    public IDataHandler<Image> getInstance(final SDFSchema schema) {
-        return new ImageDataHandler();
-    }
+	@Override
+	public IDataHandler<Image> getInstance(final SDFSchema schema) {
+		return new ImageDataHandler();
+	}
 
-    public ImageDataHandler() {
-        super();
-    }
+	public ImageDataHandler() {
+		super();
+	}
 
-    @Override
-    public Image readData(final ObjectInputStream inputStream) throws IOException {
-        int width = inputStream.readInt();
-        int height = inputStream.readInt();
-        Image image = new Image(width, height);
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                image.set(x, y, inputStream.readDouble());
-            }
-        }
-        return image;
-    }
+	@Override
+	public Image readData(final ObjectInputStream inputStream)
+			throws IOException {
+		int width = inputStream.readInt();
+		int height = inputStream.readInt();
+		Image image = new Image(width, height);
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				image.set(x, y, inputStream.readDouble());
+			}
+		}
+		return image;
+	}
 
-    @Override
-    public Image readData(final String string) {
-        return this.readData(ByteBuffer.wrap(string.getBytes()));
-    }
+	@Override
+	public Image readData(final String string) {
+		return this.readData(ByteBuffer.wrap(string.getBytes()));
+	}
 
-    @Override
-    public Image readData(final ByteBuffer buffer) {    	 	
-    	int width = buffer.getInt();
-        int height = buffer.getInt();
-        Image image = new Image(width, height);
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                image.set(x, y, buffer.getDouble());
-            }
-        }
-        return image;
-    }
+	@Override
+	public Image readData(InputStream inputStream) throws IOException {
+		BufferedImage bImage = ImageIO.read(inputStream);
+		int width = bImage.getWidth();
+		int height = bImage.getHeight();
+		Image image = new Image(width, height);
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				image.set(x, y, bImage.getRGB(x, y));
+			}
+		}
+		return image;
 
-    @Override
-    public void writeData(final ByteBuffer buffer, final Object data) {
-        final Image value = (Image) data;
-        buffer.putInt(value.getWidth());
-        buffer.putInt(value.getHeight());
-        for (int x = 0; x < value.getWidth(); x++) {
-            for (int y = 0; y < value.getHeight(); y++) {
-                buffer.putDouble(value.get(x, y));
-            }
-        }
-    }
+	}
 
-    @Override
-    final public List<String> getSupportedDataTypes() {
-        return ImageDataHandler.types;
-    }
+	@Override
+	public Image readData(final ByteBuffer buffer) {
+		int width = buffer.getInt();
+		int height = buffer.getInt();
+		Image image = new Image(width, height);
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				image.set(x, y, buffer.getDouble());
+			}
+		}
+		return image;
+	}
 
-    @Override
-    public int memSize(final Object attribute) {
-        final Image image = (Image) attribute;
-        return (2 * Integer.SIZE + image.getWidth() * image.getHeight() * Double.SIZE) / 8;
-    }
+	@Override
+	public void writeData(final ByteBuffer buffer, final Object data) {
+		final Image value = (Image) data;
+		buffer.putInt(value.getWidth());
+		buffer.putInt(value.getHeight());
+		for (int x = 0; x < value.getWidth(); x++) {
+			for (int y = 0; y < value.getHeight(); y++) {
+				buffer.putDouble(value.get(x, y));
+			}
+		}
+	}
 
-    @Override
-    public Class<?> createsType() {
-        return Image.class;
-    }
+	@Override
+	final public List<String> getSupportedDataTypes() {
+		return ImageDataHandler.types;
+	}
+
+	@Override
+	public int memSize(final Object attribute) {
+		final Image image = (Image) attribute;
+		return (2 * Integer.SIZE + image.getWidth() * image.getHeight()
+				* Double.SIZE) / 8;
+	}
+
+	@Override
+	public Class<?> createsType() {
+		return Image.class;
+	}
 
 }
