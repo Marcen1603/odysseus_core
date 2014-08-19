15c9841aada554d74bdd8cd81d932c8baaebef13
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-08-19 10:49:26 +0000

    AC: New plugin for admission rules
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21002 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0a58ca297be770f6deb9c87ff02a2fd9aa1770ea

---
+++ server/admission/de.uniol.inf.is.odysseus.admission.rules/src/de/uniol/inf/is/odysseus/admission/rules/DefaultAdmissionRuleProvider.java
@@ -0,0 +1,21 @@
+package de.uniol.inf.is.odysseus.admission.rules;
+
+import java.util.Collection;
+
+import com.google.common.collect.Lists;
+
+import de.uniol.inf.is.odysseus.admission.IAdmissionRule;
+import de.uniol.inf.is.odysseus.admission.IAdmissionRuleProvider;
+
+public class DefaultAdmissionRuleProvider implements IAdmissionRuleProvider {
+
+	@Override
+	public Collection<IAdmissionRule<?>> getRules() {
+		Collection<IAdmissionRule<?>> rules = Lists.newArrayList();
+		
+//		rules.add(new AlwaysStopStartedQueriesAdmissionRule());
+		
+		return rules;
+	}
+
+}
