76cd1f8b3cdad3211736152d17e68f829722ed88
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-08-19 13:45:22 +0000

    AC: Added action to add new admission events
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21023 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5399e4476b1af27d44971a00efb7fc05575a2a29

--- server/admission/de.uniol.inf.is.odysseus.admission/src/de/uniol/inf/is/odysseus/admission/IAdmissionActions.java
+++ server/admission/de.uniol.inf.is.odysseus.admission/src/de/uniol/inf/is/odysseus/admission/IAdmissionActions.java
@@ -3,4 +3,6 @@ package de.uniol.inf.is.odysseus.admission;
 public interface IAdmissionActions {
 
 	public <T extends IAdmissionActionComponent> T getAdmissionActionComponent( Class<T> componentClass );
+	
+	public void addEvent( IAdmissionEvent event );
 }
