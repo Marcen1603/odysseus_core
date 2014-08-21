64e6659490a8c08102c3310698ffb75d98d42e1c
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-08-21 08:49:56 +0000

    RCP: Popup-Menu in SourcesView only is only shown when a source is directly selected
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21043 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: ec373695eac05238e4a4dd4ee114b3c04e8e368f

--- client/studio/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/commands/AbstractStartQueriesCommand.java
+++ client/studio/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/commands/AbstractStartQueriesCommand.java
@@ -3,8 +3,6 @@ package de.uniol.inf.is.odysseus.rcp.commands;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.commands.ExecutionException;
-
 import de.uniol.inf.is.odysseus.core.planmanagement.query.QueryState;
 
 public abstract class AbstractStartQueriesCommand extends AbstractQueryCommand {
@@ -14,7 +12,7 @@ public abstract class AbstractStartQueriesCommand extends AbstractQueryCommand {
 	static final IQueryCommandAction resume = new ResumeQueryCommandAction();
 
 
-	public Object execute(List<Integer> selectedObj) throws ExecutionException {
+	public Object execute(List<Integer> selectedObj) {
 		// Split to queries that should be started and that should be resumed
 		List<QueryState> states = getQueryStates(selectedObj);
 		List<Integer> toStart = new ArrayList<>();
