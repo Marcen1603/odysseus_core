4d4450909d5cec96481fc69060e5e81c90ee8910
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-08-22 12:38:47 +0000

    AC: Added system load information for admission control; status component instances are cached now
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21088 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 96728630c9a83641599219a610760530ae251927

--- server/admission/de.uniol.inf.is.odysseus.admission/src/de/uniol/inf/is/odysseus/admission/status/AdmissionStatus.java
+++ server/admission/de.uniol.inf.is.odysseus.admission/src/de/uniol/inf/is/odysseus/admission/status/AdmissionStatus.java
@@ -1,14 +1,27 @@
 package de.uniol.inf.is.odysseus.admission.status;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
 import de.uniol.inf.is.odysseus.admission.IAdmissionStatus;
 import de.uniol.inf.is.odysseus.admission.IAdmissionStatusComponent;
 import de.uniol.inf.is.odysseus.admission.activator.AdmissionPlugIn;
 
 public class AdmissionStatus implements IAdmissionStatus {
 
+	private final Map<Class<? extends IAdmissionStatusComponent>, IAdmissionStatusComponent> cachedComponents = Maps.newHashMap();
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public <T extends IAdmissionStatusComponent> T getStatusComponent(Class<T> statusType) {
-		return AdmissionPlugIn.getAdmissionStatusComponentRegistry().getAdmissionStatusComponent(statusType);
+		if( cachedComponents.containsKey(statusType)) {
+			return (T) cachedComponents.get(statusType);
+		}
+		T newComponent = AdmissionPlugIn.getAdmissionStatusComponentRegistry().getAdmissionStatusComponent(statusType);
+		cachedComponents.put(statusType, newComponent);
+		
+		return newComponent;
 	}
 
 	@Override
