c3d7ffec9bf0cea98371c1662b214ffccfb78017
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-08-27 11:07:38 +0000

    Webservice: changed access rules
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21116 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: debb6b190d166370b2032edb0bc0ea4f90339396

--- server/core/webservice.executor.server/src/de/uniol/inf/is/odysseus/planmanagement/executor/webservice/server/webservice/WebserviceStarter.java
+++ server/core/webservice.executor.server/src/de/uniol/inf/is/odysseus/planmanagement/executor/webservice/server/webservice/WebserviceStarter.java
@@ -1,12 +1,22 @@
 package de.uniol.inf.is.odysseus.planmanagement.executor.webservice.server.webservice;
 
 
+import java.net.InetSocketAddress;
+import java.security.SecureRandom;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.TrustManager;
 import javax.xml.ws.Endpoint;
+
+import com.sun.net.httpserver.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.uniol.inf.is.odysseus.core.server.OdysseusConfiguration;
+import de.uniol.inf.is.odysseus.planmanagement.executor.webservice.server.SecurityProviderServiceBinding;
 
 public class WebserviceStarter {
 	
@@ -41,56 +51,57 @@ public class WebserviceStarter {
 	}
 	
 	
+	@SuppressWarnings("restriction")
 	public static void start(boolean sslClientAuthentification, boolean sslServerAuthentification) {
-//		try {
-//			Endpoint endpoint = Endpoint.create(new WebserviceServer());
-//			SSLContext ssl = SSLContext.getInstance("TLS");
-//
-//			KeyManager[] keyManagers = null;
-//			TrustManager[] trustManagers =null;
-//			HttpsConfigurator configurator = null;
-//
-//			if (sslServerAuthentification) {
-//				keyManagers = SecurityProviderServiceBinding.getSecurityProvider().getKeyManagers();
-//				configurator = new HttpsConfigurator(ssl);
-//			}
-//			
-//			if (sslClientAuthentification) {
-//				trustManagers = SecurityProviderServiceBinding.getSecurityProvider().getTrustManagers();
-//				configurator = new HttpsConfigurator(ssl) {
-//				     @Override
-//				     public void configure(HttpsParameters params) {
-//				         SSLParameters sslParams = getSSLContext().getDefaultSSLParameters();
-//				         sslParams.setNeedClientAuth(true);
-//				         params.setSSLParameters(sslParams);
-//				      }
-//				 };	
-//			}
-//			
-//			ssl.init(keyManagers, trustManagers, new SecureRandom());			
-//	
-//			
-//			
-//			int port = Integer.parseInt(OdysseusConfiguration.get("WebService.Port"));
-//			int maxPort = Integer.parseInt(OdysseusConfiguration.get("WebService.MaxPort"));
-//			
-//			while (port <= maxPort ){
-//				try {
-//					HttpsServer httpsServer = HttpsServer.create(new InetSocketAddress("localhost", port), port);
-//					httpsServer.setHttpsConfigurator(configurator);
-//					HttpContext context = httpsServer.createContext(OdysseusConfiguration.get("WebService.Endpoint2"));
-//					httpsServer.start();
-//					endpoint.publish(context);
-//					LOGGER.info("Webservice published at https://localhost:" + port+OdysseusConfiguration.get("WebService.Endpoint2"));
-//					break;
-//				} catch (Exception e) {
-//
-//				}
-//				port++;
-//			}			
-//		} catch (Exception e) {
-//			LOGGER.error("Webservice could not be published", e);
-//		}
+		try {
+			Endpoint endpoint = Endpoint.create(new WebserviceServer());
+			SSLContext ssl = SSLContext.getInstance("TLS");
+
+			KeyManager[] keyManagers = null;
+			TrustManager[] trustManagers =null;
+			HttpsConfigurator configurator = null;
+
+			if (sslServerAuthentification) {
+				keyManagers = SecurityProviderServiceBinding.getSecurityProvider().getKeyManagers();
+				configurator = new HttpsConfigurator(ssl);
+			}
+			
+			if (sslClientAuthentification) {
+				trustManagers = SecurityProviderServiceBinding.getSecurityProvider().getTrustManagers();
+				configurator = new HttpsConfigurator(ssl) {
+				     @Override
+				     public void configure(HttpsParameters params) {
+				         SSLParameters sslParams = getSSLContext().getDefaultSSLParameters();
+				         sslParams.setNeedClientAuth(true);
+				         params.setSSLParameters(sslParams);
+				      }
+				 };	
+			}
+			
+			ssl.init(keyManagers, trustManagers, new SecureRandom());			
+	
+			
+			
+			int port = Integer.parseInt(OdysseusConfiguration.get("WebService.Port"));
+			int maxPort = Integer.parseInt(OdysseusConfiguration.get("WebService.MaxPort"));
+			
+			while (port <= maxPort ){
+				try {
+					HttpsServer httpsServer = HttpsServer.create(new InetSocketAddress("localhost", port), port);
+					httpsServer.setHttpsConfigurator(configurator);
+					HttpContext context = httpsServer.createContext(OdysseusConfiguration.get("WebService.Endpoint2"));
+					httpsServer.start();
+					endpoint.publish(context);
+					LOGGER.info("Webservice published at https://localhost:" + port+OdysseusConfiguration.get("WebService.Endpoint2"));
+					break;
+				} catch (Exception e) {
+
+				}
+				port++;
+			}			
+		} catch (Exception e) {
+			LOGGER.error("Webservice could not be published", e);
+		}
 	}
 
 }
