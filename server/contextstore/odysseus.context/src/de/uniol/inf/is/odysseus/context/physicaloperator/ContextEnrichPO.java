104ab1a3c7f54b80de65d78061f55444e0284d35
Christian Kuka <christian@kuka.cc>
2014-09-07 02:17:59 +0000

    Context: Create new object using appendList method instead of new tuple object when using outer enrich
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21222 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c26150b3faf41c42aadc5e0fdf1fc5a9dfae8ae0

--- server/contextstore/odysseus.context/src/de/uniol/inf/is/odysseus/context/physicaloperator/ContextEnrichPO.java
+++ server/contextstore/odysseus.context/src/de/uniol/inf/is/odysseus/context/physicaloperator/ContextEnrichPO.java
@@ -31,6 +31,7 @@
 package de.uniol.inf.is.odysseus.context.physicaloperator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import de.uniol.inf.is.odysseus.context.store.IContextStore;
@@ -41,13 +42,13 @@ import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
 import de.uniol.inf.is.odysseus.core.metadata.TimeInterval;
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPunctuation;
 import de.uniol.inf.is.odysseus.core.physicaloperator.OpenFailedException;
+import de.uniol.inf.is.odysseus.core.sdf.schema.DirectAttributeResolver;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.core.server.metadata.CombinedMergeFunction;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.IDataMergeFunction;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.IHasMetadataMergeFunction;
-import de.uniol.inf.is.odysseus.core.sdf.schema.DirectAttributeResolver;
 
 /**
  * 
@@ -55,102 +56,109 @@ import de.uniol.inf.is.odysseus.core.sdf.schema.DirectAttributeResolver;
  */
 public class ContextEnrichPO<M extends ITimeInterval> extends AbstractPipe<Tuple<M>, Tuple<M>> implements IHasMetadataMergeFunction<M> {
 
-	private IContextStore<Tuple<M>> store;
-	protected IDataMergeFunction<Tuple<M>, M> dataMerge;
-	protected IMetadataMergeFunction<M> metadataMerge;
-	private List<String> attribtues;
-	private int[] restrictList;
-	private boolean outer;
+    private IContextStore<Tuple<M>> store;
+    protected IDataMergeFunction<Tuple<M>, M> dataMerge;
+    protected IMetadataMergeFunction<M> metadataMerge;
+    private List<String> attribtues;
+    private int[] restrictList;
+    private boolean outer;
 
-	public ContextEnrichPO(IContextStore<Tuple<M>> store, boolean outer, IDataMergeFunction<Tuple<M>, M> dataMerge, CombinedMergeFunction<M> metadataMerge, List<String> attributes) {
-		super();
-		this.store = store;
-		this.metadataMerge = metadataMerge;
-		this.dataMerge = dataMerge;
-		this.attribtues = attributes;
-		this.outer = outer;
-	}
+    public ContextEnrichPO(IContextStore<Tuple<M>> store, boolean outer, IDataMergeFunction<Tuple<M>, M> dataMerge, CombinedMergeFunction<M> metadataMerge, List<String> attributes) {
+        super();
+        this.store = store;
+        this.metadataMerge = metadataMerge;
+        this.dataMerge = dataMerge;
+        this.attribtues = attributes;
+        this.outer = outer;
+    }
 
-	public ContextEnrichPO(ContextEnrichPO<M> enrichPO) {
-		super();
-		this.store = enrichPO.store;
-		this.metadataMerge = enrichPO.metadataMerge.clone();
-		this.dataMerge = enrichPO.dataMerge.clone();
-		this.attribtues = new ArrayList<String>(enrichPO.attribtues);
-		this.outer = enrichPO.outer;
-	}
+    public ContextEnrichPO(ContextEnrichPO<M> enrichPO) {
+        super();
+        this.store = enrichPO.store;
+        this.metadataMerge = enrichPO.metadataMerge.clone();
+        this.dataMerge = enrichPO.dataMerge.clone();
+        this.attribtues = new ArrayList<String>(enrichPO.attribtues);
+        this.outer = enrichPO.outer;
+    }
 
-	@Override
-	protected void process_next(Tuple<M> object, int port) {
-		ITimeInterval ti = object.getMetadata();
-		List<Tuple<M>> values = this.store.getValues(ti);
-		for (Tuple<M> value : values) {			
-			Tuple<M> res = value.restrict(this.restrictList, true);
-			Tuple<M> newElement = dataMerge.merge(object, res, metadataMerge, Order.LeftRight);		
-			transfer(newElement);
-		}
-		if(values.size()==0 && outer){
-			Tuple<M> nullTuple = new Tuple<M>(this.restrictList.length, object.requiresDeepClone());
-			nullTuple.setMetadata(object.getMetadata());
-			Tuple<M> newElement = dataMerge.merge(object, nullTuple, metadataMerge, Order.LeftRight);		
-			transfer(newElement);
-		}
-	}
+    @Override
+    protected void process_next(Tuple<M> object, int port) {
+        ITimeInterval ti = object.getMetadata();
+        List<Tuple<M>> values = this.store.getValues(ti);
+        for (Tuple<M> value : values) {
+            Tuple<M> res = value.restrict(this.restrictList, true);
+            Tuple<M> newElement = dataMerge.merge(object, res, metadataMerge, Order.LeftRight);
+            transfer(newElement);
+        }
+        if (values.size() == 0 && outer) {
+            Object[] nullArray = new Object[this.restrictList.length];
+            Arrays.fill(nullArray, null);
+            Tuple<M> newElement = object.appendList(Arrays.asList(nullArray), true);
+            transfer(newElement);
+        }
+    }
 
-	private void calcRestrictList() {		
-		SDFSchema storeSchema = this.store.getSchema();
-		int[] ret = new int[this.attribtues.size()];
-		DirectAttributeResolver dar = new DirectAttributeResolver(storeSchema);
-		int i=0;
-		for(String attName : this.attribtues){
-			SDFAttribute a = dar.getAttribute(attName);
-			int j=0;
-			for(SDFAttribute b : storeSchema){
-				if(b.equals(a)){
-					ret[i] = j;
-				}
-				j++;
-			}
-			i++;
-		}			
-		this.restrictList= ret;
-	}
+    private void calcRestrictList() {
+        SDFSchema storeSchema = this.store.getSchema();
+        int[] ret = new int[this.attribtues.size()];
+        DirectAttributeResolver dar = new DirectAttributeResolver(storeSchema);
+        int i = 0;
+        for (String attName : this.attribtues) {
+            SDFAttribute a = dar.getAttribute(attName);
+            int j = 0;
+            for (SDFAttribute b : storeSchema) {
+                if (b.equals(a)) {
+                    ret[i] = j;
+                }
+                j++;
+            }
+            i++;
+        }
+        this.restrictList = ret;
+    }
 
-	@Override
-	public void processPunctuation(IPunctuation punctuation, int port) {
-		// if getValues is invoked, the start timestamp is used for purging of
-		// the store
-		// so, we "simulate" this for a punctuation
-		TimeInterval ti = new TimeInterval(punctuation.getTime());
-		this.store.getValues(ti);
-	}
+    @Override
+    public void processPunctuation(IPunctuation punctuation, int port) {
+        // if getValues is invoked, the start timestamp is used for purging of
+        // the store
+        // so, we "simulate" this for a punctuation
+        TimeInterval ti = new TimeInterval(punctuation.getTime());
+        this.store.getValues(ti);
+    }
 
-	@Override
-	protected void process_open() throws OpenFailedException {
-		super.process_open();
-		this.calcRestrictList();
-		this.metadataMerge.init();
-		this.dataMerge.init();
-	}
+    @Override
+    protected void process_open() throws OpenFailedException {
+        super.process_open();
+        this.calcRestrictList();
+        this.metadataMerge.init();
+        this.dataMerge.init();
+    }
 
-	@Override
-	protected void process_done() {
-		super.process_done();
-	}
+    @Override
+    protected void process_done() {
+        super.process_done();
+    }
 
-	@Override
-	public ContextEnrichPO<M> clone() {
-		return new ContextEnrichPO<M>(this);
-	}
+    @Override
+    public ContextEnrichPO<M> clone() {
+        return new ContextEnrichPO<M>(this);
+    }
 
-	@Override
-	public OutputMode getOutputMode() {
-		return OutputMode.MODIFIED_INPUT;
-	}
-	
-	@Override
-	public IMetadataMergeFunction<M> getMetadataMerge() {
-		return metadataMerge;
-	}
+    @Override
+    public OutputMode getOutputMode() {
+        return OutputMode.MODIFIED_INPUT;
+    }
+
+    @Override
+    public IMetadataMergeFunction<M> getMetadataMerge() {
+        return metadataMerge;
+    }
+
+    /**
+     * @param metadataMerge
+     */
+    public void setMetadataMerge(IMetadataMergeFunction<M> metadataMerge) {
+        this.metadataMerge = metadataMerge;
+    }
 
 }
