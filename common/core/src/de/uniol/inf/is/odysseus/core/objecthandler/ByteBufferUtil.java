4f8724290124cae0c71e803f32dc92c6aeb7067d
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-09-08 15:14:04 +0000

    Added param to allow to create objects with and without metadata
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21248 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 7c6da3d9565a2ab9caf395657cd31de587c7a05f

--- common/core/src/de/uniol/inf/is/odysseus/core/objecthandler/ByteBufferUtil.java
+++ common/core/src/de/uniol/inf/is/odysseus/core/objecthandler/ByteBufferUtil.java
@@ -14,7 +14,6 @@ public class ByteBufferUtil {
 	private ByteBufferUtil() {
 	}
 
-	
 	public static IPunctuation createPunctuation(ByteBuffer messageBuffer) {
 		final byte[] rawData = new byte[messageBuffer.remaining()];
 		messageBuffer.get(rawData, 0, rawData.length);
@@ -23,28 +22,32 @@ public class ByteBufferUtil {
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static <T extends IStreamObject> T createStreamObject(ByteBuffer byteBuffer, IDataHandler<T> dataHandler) throws BufferUnderflowException {
+	public static <T extends IStreamObject> T createStreamObject(
+			ByteBuffer byteBuffer, IDataHandler<T> dataHandler)
+			throws BufferUnderflowException {
 		T retval = null;
 		try {
 			retval = dataHandler.readData(byteBuffer);
 
 			if (byteBuffer.remaining() > 0) {
-				
+
 				int metadataBytesLength = byteBuffer.getInt();
 				byte[] metadataBytes = new byte[metadataBytesLength];
 				byteBuffer.get(metadataBytes);
 
-				IMetaAttribute metadata = (IMetaAttribute) ObjectByteConverter.bytesToObject(metadataBytes);
+				IMetaAttribute metadata = (IMetaAttribute) ObjectByteConverter
+						.bytesToObject(metadataBytes);
 				retval.setMetadata(metadata);
-				
-				if( byteBuffer.remaining() > 0 ) {
+
+				if (byteBuffer.remaining() > 0) {
 					int metadataMapBytesLength = byteBuffer.getInt();
 					byte[] metadataMapBytes = new byte[metadataMapBytesLength];
 					byteBuffer.get(metadataMapBytes);
-					
-					Map<String, Object> metadataMap = (Map<String, Object>) ObjectByteConverter.bytesToObject(metadataMapBytes);
+
+					Map<String, Object> metadataMap = (Map<String, Object>) ObjectByteConverter
+							.bytesToObject(metadataMapBytes);
 					System.err.println(metadataMap);
-					
+
 					retval.setMetadataMap(metadataMap);
 				}
 			}
@@ -54,23 +57,28 @@ public class ByteBufferUtil {
 
 		return retval;
 	}
-	
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static <T extends IStreamObject> void toBuffer(ByteBuffer buffer, T object, IDataHandler<T> dataHandler){
+	public static <T extends IStreamObject> void toBuffer(ByteBuffer buffer,
+			T object, IDataHandler<T> dataHandler, boolean withMetadata) {
 		dataHandler.writeData(buffer, object);
-		
-		Object metadata = object.getMetadata();
-		if (metadata != null) {
-			byte[] metadataBytes = ObjectByteConverter.objectToBytes(object.getMetadata());
-			buffer.putInt(metadataBytes.length);
-			buffer.put(metadataBytes);
-		}
-		
-		Map<String, Object> metadataMap = object.getMetadataMap();
-		if( !metadataMap.isEmpty() ) {
-			byte[] metadataMapBytes = ObjectByteConverter.objectToBytes(metadataMap);
-			buffer.putInt(metadataMapBytes.length);
-			buffer.put(metadataMapBytes);
+
+		if (withMetadata) {
+			Object metadata = object.getMetadata();
+			if (metadata != null) {
+				byte[] metadataBytes = ObjectByteConverter.objectToBytes(object
+						.getMetadata());
+				buffer.putInt(metadataBytes.length);
+				buffer.put(metadataBytes);
+			}
+
+			Map<String, Object> metadataMap = object.getMetadataMap();
+			if (!metadataMap.isEmpty()) {
+				byte[] metadataMapBytes = ObjectByteConverter
+						.objectToBytes(metadataMap);
+				buffer.putInt(metadataMapBytes.length);
+				buffer.put(metadataMapBytes);
+			}
 		}
 	}
 }
