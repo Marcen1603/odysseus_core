0f6640fd50fdb38029f2208e31f4ae7aadae0df9
Mark Milster <Mark Milster@uni-oldenburg.de>
2014-09-15 09:48:10 +0000

    New Dashboardpart to show a Stream of Colors
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21333 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f11419e2f936c99155837adf7abad42ea6cada72

---
+++ client/dashboard/de.uniol.inf.is.odysseus.rcp.dashboard.colors/src/de/uniol/inf/is/odysseus/rcp/dashboard/colors/config/ColorListConfigurer.java
@@ -0,0 +1,203 @@
+package de.uniol.inf.is.odysseus.rcp.dashboard.colors.config;
+
+import java.util.Collection;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+
+import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
+import de.uniol.inf.is.odysseus.rcp.dashboard.AbstractDashboardPartConfigurer;
+import de.uniol.inf.is.odysseus.rcp.dashboard.DashboardPartUtil;
+import de.uniol.inf.is.odysseus.rcp.dashboard.colors.parts.ColorListDashboardPart;
+
+/**
+ * 
+ * @author MarkMilster
+ *
+ */
+public class ColorListConfigurer extends AbstractDashboardPartConfigurer<ColorListDashboardPart> {
+
+	private ColorListDashboardPart dashboardPart;
+
+	@Override
+	public void init(ColorListDashboardPart dashboardPartToConfigure, Collection<IPhysicalOperator> roots ) {
+		dashboardPart = dashboardPartToConfigure;
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		Composite topComposite = new Composite(parent, SWT.NONE);
+		topComposite.setLayout(new GridLayout(2, false));
+		topComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		createMaxDataControls(topComposite);
+		createUpdateIntervalControls(topComposite);
+		createTimestampControls(topComposite);
+		createFixedTimestampControls(topComposite);
+		createMovingTimestampControls(topComposite);
+		createTimestampTextControls(topComposite);
+		createColorSizeControls(topComposite);
+		createLineSpaceControls(topComposite);
+		createShowHeartbeatsControls(topComposite);
+	}
+
+	private void createLineSpaceControls(Composite topComposite) {
+		DashboardPartUtil.createLabel(topComposite, "Line Space");
+		final Text lineSpaceText = DashboardPartUtil.createText(topComposite, String.valueOf(dashboardPart.getLineSpace()));
+		lineSpaceText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		lineSpaceText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setLineSpace(Integer.valueOf(lineSpaceText.getText()));
+				fireListener();
+			}
+		});
+	}
+
+	private void createTimestampTextControls(Composite topComposite) {
+		DashboardPartUtil.createLabel(topComposite, "Timestamp Textsize");
+		final Text TimestampTextsizeText = DashboardPartUtil.createText(topComposite, String.valueOf(dashboardPart.getTimestampTextSize()));
+		TimestampTextsizeText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		TimestampTextsizeText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setTimestampTextSize(Integer.valueOf(TimestampTextsizeText.getText()));
+				fireListener();
+			}
+		});
+	}
+
+	private void createColorSizeControls(Composite topComposite) {
+		DashboardPartUtil.createLabel(topComposite, "Color width");
+		final Text colorWidthText = DashboardPartUtil.createText(topComposite, String.valueOf(dashboardPart.getColorWidth()));
+		colorWidthText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		colorWidthText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setColorWidth(Integer.valueOf(colorWidthText.getText()));
+				fireListener();
+			}
+		});
+		
+		DashboardPartUtil.createLabel(topComposite, "Color height");
+		final Text colorHeightText = DashboardPartUtil.createText(topComposite, String.valueOf(dashboardPart.getColorHeight()));
+		colorHeightText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		colorHeightText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setColorWidth(Integer.valueOf(colorHeightText.getText()));
+				fireListener();
+			}
+		});
+	}
+
+	private void createMovingTimestampControls(Composite topComposite) {
+		DashboardPartUtil.createLabel(topComposite, "Distance Moving Timestamps");
+		final Text distanceMovingTimestampsText = DashboardPartUtil.createText(topComposite, String.valueOf(dashboardPart.getMovingTimestampDiffElements()));
+		distanceMovingTimestampsText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		distanceMovingTimestampsText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setMovingTimestampDiffElements(Long.valueOf(distanceMovingTimestampsText.getText()));
+				fireListener();
+			}
+		});
+	}
+
+	private void createFixedTimestampControls(Composite topComposite) {
+		DashboardPartUtil.createLabel(topComposite, "Difference Fixed Timestamps");
+		final Text diffFixedTimestampsText = DashboardPartUtil.createText(topComposite, String.valueOf(dashboardPart.getFixedTimestampDiffMilliseconds()));
+		diffFixedTimestampsText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		diffFixedTimestampsText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setFixedTimestampDiffMilliseconds(Long.valueOf(diffFixedTimestampsText.getText()));
+				fireListener();
+			}
+		});
+	}
+
+	private void createTimestampControls(Composite topComposite) {
+		DashboardPartUtil.createLabel(topComposite, "Fixed Timestamps");
+		final Combo comboLocked = createBooleanComboDropDown(topComposite, dashboardPart.isFixedTimestamps());
+		comboLocked.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				dashboardPart.setFixedTimestamps(comboLocked.getSelectionIndex() == 0);
+				fireListener();
+			}
+		});
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	private void createMaxDataControls(Composite topComposite) {
+		DashboardPartUtil.createLabel(topComposite, "Max Elements");
+		final Text maxElementsText = DashboardPartUtil.createText(topComposite, String.valueOf(dashboardPart.getMaxElements()));
+		maxElementsText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		maxElementsText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setMaxElements(Integer.valueOf(maxElementsText.getText()));
+				fireListener();
+			}
+		});
+		
+		DashboardPartUtil.createLabel(topComposite, "Max Elements per Line");
+		final Text maxElementsLineText = DashboardPartUtil.createText(topComposite, String.valueOf(dashboardPart.getMaxElementsLine()));
+		maxElementsLineText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		maxElementsLineText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setMaxElementsLine(Integer.valueOf(maxElementsLineText.getText()));
+				fireListener();
+			}
+		});
+	}
+
+	private void createUpdateIntervalControls(Composite topComposite) {
+		DashboardPartUtil.createLabel(topComposite, "Update interval (ms)");
+		final Text attributesInput = DashboardPartUtil.createText(topComposite, String.valueOf(dashboardPart.getUpdateInterval()));
+		attributesInput.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		attributesInput.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dashboardPart.setUpdateInterval(Long.valueOf(attributesInput.getText()));
+				fireListener();
+			}
+		});
+	}
+
+	private void createShowHeartbeatsControls(Composite topComposite) {
+		DashboardPartUtil.createLabel(topComposite, "Show Heartbeats");
+		final Combo comboLocked = createBooleanComboDropDown(topComposite, dashboardPart.isShowHeartbeats());
+		comboLocked.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				dashboardPart.setShowHeartbeats(comboLocked.getSelectionIndex() == 0);
+				fireListener();
+			}
+		});
+	}
+
+	private static Combo createBooleanComboDropDown(Composite tableComposite, boolean isSetToTrue) {
+		Combo comboDropDown = new Combo(tableComposite, SWT.DROP_DOWN | SWT.BORDER | SWT.READ_ONLY);
+		comboDropDown.add("true");
+		comboDropDown.add("false");
+		comboDropDown.select(isSetToTrue ? 0 : 1);
+		comboDropDown.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		return comboDropDown;
+	}
+
+}
---
+++ client/dashboard/de.uniol.inf.is.odysseus.rcp.dashboard.colors/src/de/uniol/inf/is/odysseus/rcp/dashboard/colors/parts/ColorListDashboardPart.java
@@ -0,0 +1,340 @@
+package de.uniol.inf.is.odysseus.rcp.dashboard.colors.parts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.ui.PlatformUI;
+
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
+import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
+import de.uniol.inf.is.odysseus.core.physicaloperator.IPunctuation;
+import de.uniol.inf.is.odysseus.core.securitypunctuation.ISecurityPunctuation;
+import de.uniol.inf.is.odysseus.rcp.dashboard.AbstractDashboardPart;
+
+/**
+ * 
+ * @author MarkMilster
+ *
+ */
+public class ColorListDashboardPart extends AbstractDashboardPart {
+
+	private Canvas canvas;
+	private List<Color> colorList = new ArrayList<Color>();
+	private Thread updateThread;
+	private long updateInterval = 100;
+	private boolean showHeartbeats = false;
+	/**
+	 * @return the updateInterval
+	 */
+	public long getUpdateInterval() {
+		return updateInterval;
+	}
+
+	/**
+	 * @param updateInterval the updateInterval to set
+	 */
+	public void setUpdateInterval(long updateInterval) {
+		this.updateInterval = updateInterval;
+	}
+
+	private int maxElements = 200;
+	private int maxElementsLine = 100;
+	/**
+	 * @return the maxElementsLine
+	 */
+	public int getMaxElementsLine() {
+		return maxElementsLine;
+	}
+
+	/**
+	 * @param maxElementsLine the maxElementsLine to set
+	 */
+	public void setMaxElementsLine(int maxElementsLine) {
+		if (maxElementsLine < this.maxElements) {
+			this.maxElementsLine = maxElementsLine;
+		}
+	}
+
+	private GC gc;
+	private int colorWidth = 20;
+	/**
+	 * @return the timestampTextSize
+	 */
+	public int getTimestampTextSize() {
+		return timestampTextSize;
+	}
+
+	/**
+	 * @param timestampTextSize the timestampTextSize to set
+	 */
+	public void setTimestampTextSize(int timestampTextSize) {
+		this.timestampTextSize = timestampTextSize;
+	}
+
+	/**
+	 * @return the colorWidth
+	 */
+	public int getColorWidth() {
+		return colorWidth;
+	}
+
+	/**
+	 * @param colorWidth the colorWidth to set
+	 */
+	public void setColorWidth(int colorWidth) {
+		this.colorWidth = colorWidth;
+	}
+
+	/**
+	 * @return the colorHeight
+	 */
+	public int getColorHeight() {
+		return colorHeight;
+	}
+
+	/**
+	 * @param colorHeight the colorHeight to set
+	 */
+	public void setColorHeight(int colorHeight) {
+		this.colorHeight = colorHeight;
+	}
+
+	/**
+	 * @return the lineSpace
+	 */
+	public int getLineSpace() {
+		return lineSpace;
+	}
+
+	/**
+	 * @param lineSpace the lineSpace to set
+	 */
+	public void setLineSpace(int lineSpace) {
+		this.lineSpace = lineSpace;
+	}
+
+	private int colorHeight = 80;
+	private int lineSpace = 20;
+	private List<Double> timestampList = new ArrayList<Double>();
+	private long movingTimestampDiffElements = 10; // in Elements
+	/**
+	 * @return the movingTimestampDiffElements
+	 */
+	public long getMovingTimestampDiffElements() {
+		return movingTimestampDiffElements;
+	}
+
+	/**
+	 * @param movingTimestampDiffElements the movingTimestampDiffElements to set
+	 */
+	public void setMovingTimestampDiffElements(long movingTimestampDiffElements) {
+		this.movingTimestampDiffElements = movingTimestampDiffElements;
+	}
+
+	/**
+	 * @return the fixedTimestampDiffMilliseconds
+	 */
+	public long getFixedTimestampDiffMilliseconds() {
+		return fixedTimestampDiffMilliseconds;
+	}
+
+	/**
+	 * @param fixedTimestampDiffMilliseconds the fixedTimestampDiffMilliseconds to set
+	 */
+	public void setFixedTimestampDiffMilliseconds(
+			long fixedTimestampDiffMilliseconds) {
+		this.fixedTimestampDiffMilliseconds = fixedTimestampDiffMilliseconds;
+	}
+
+	private long fixedTimestampDiffMilliseconds = 100000; // in Milliseconds
+	private long counter;
+	private int timestampTextSize = 12;
+	private boolean fixedTimestamps = true;
+
+	/**
+	 * @return the fixedTimestamps
+	 */
+	public boolean isFixedTimestamps() {
+		return fixedTimestamps;
+	}
+
+	/**
+	 * @param fixedTimestamps the fixedTimestamps to set
+	 */
+	public void setFixedTimestamps(boolean fixedTimestamps) {
+		this.fixedTimestamps = fixedTimestamps;
+	}
+
+	@Override
+	public void createPartControl(final Composite parent, ToolBar toolbar) {
+		// SWT.BORDER | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.WRAP
+		parent.setLayout(new FillLayout());
+		canvas = new Canvas(parent, 0);
+		canvas.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+		gc = new GC(canvas);
+		
+		updateThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (!parent.isDisposed()) {
+					final Display disp = PlatformUI.getWorkbench().getDisplay();
+					if (!disp.isDisposed()) {
+						disp.asyncExec(new Runnable() {
+							@Override
+							public void run() {
+								if (!canvas.isDisposed()) {
+									refreshCanvas();
+								}
+							}
+						});
+					}
+
+					waiting(updateInterval );
+				}
+			}
+
+		});
+
+		updateThread.setName("StreamList Updater");
+		updateThread.start();
+	}
+	
+	private static void waiting(long length) {
+		try {
+			Thread.sleep(length);
+		} catch (final InterruptedException e) {
+		}
+	}
+	
+	private void refreshCanvas() {
+		int lineNumber = 0;
+		int elementsInLine = 0;
+		int xPos = 0;
+		double lastTimestamp = 0-fixedTimestampDiffMilliseconds;	
+		Font font = new Font(Display.getCurrent(), "Tahoma", timestampTextSize , SWT.NORMAL);
+		gc.setFont(font);
+		//to clear everything
+		gc.setBackground(new Color(Display.getCurrent(), 255, 255, 255));
+		gc.fillRectangle(0, 0, canvas.getSize().x, canvas.getSize().y);
+		for (int i=0; i < colorList.size(); i++) {
+			gc.setBackground(colorList.get(i));
+			if (elementsInLine >= maxElementsLine) {
+				lineNumber ++;
+				elementsInLine = 0;
+				xPos = 0;
+			}
+			gc.fillRectangle(xPos, (lineNumber*colorHeight)+(lineNumber*lineSpace), colorWidth, colorHeight );
+			elementsInLine ++;
+			xPos += colorWidth;
+
+			if (fixedTimestamps) {
+				if (timestampList.get(i) >= (lastTimestamp+fixedTimestampDiffMilliseconds)) {
+					System.out.println(String.valueOf(timestampList.get(i)));
+					
+					gc.drawString(String.valueOf(timestampList.get(i)), xPos-(colorWidth),  (lineNumber*colorHeight)+(lineNumber*lineSpace)+colorHeight, true);
+					lastTimestamp = timestampList.get(i);
+				}
+			} else {
+				// moving Timestamps
+				if (timestampList.get(i) != null) {
+					gc.drawString(String.valueOf(timestampList.get(i)), xPos-(colorWidth),  (lineNumber*colorHeight)+(lineNumber*lineSpace)+colorHeight, true);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void streamElementRecieved(IPhysicalOperator senderOperator,
+			IStreamObject<?> element, int port) {
+		synchronized (colorList) {
+			if (fixedTimestamps) {
+				timestampList.add(getTimestamp(element));
+			} else {
+				// moving Timestamps
+				counter ++;
+				if (counter == movingTimestampDiffElements) {
+					counter = 0;
+					timestampList.add(getTimestamp(element));
+				} else {
+					timestampList.add(null);
+				}
+			}
+			
+			colorList.add(elementToColor(element));
+			if (!isInfinite() && colorList.size() > maxElements) {
+				// colorList.size() equals timestampList.size() 
+				colorList.remove(0);
+				timestampList.remove(0);
+			}
+		}
+	}
+	
+	private Double getTimestamp(IStreamObject<?> element) {
+		Tuple<?> tuple = (Tuple<?>) element;
+		//TODO vorher klar machen ob an Position 1 ueberhaupt der Timestamp ist
+		return tuple.getAttribute(1);
+	}
+
+	public Color elementToColor(IStreamObject<?> element) {
+		Tuple<?> tuple = (Tuple<?>) element;
+		//TODO alle drei Werte nehmen -> das hier ist nur für Testzwecke mit einem Wert
+		Double d = tuple.getAttribute(0);
+		Color color = new Color(Display.getCurrent(), 0, 0,  d.intValue());
+		return color;
+	}
+
+	@Override
+	public void punctuationElementRecieved(IPhysicalOperator senderOperator,
+			IPunctuation point, int port) {
+		synchronized (colorList) {
+			if (!point.isHeartbeat() || showHeartbeats) {
+				if (!isInfinite() && colorList.size() > maxElements) {
+					colorList.remove(0);
+				}
+			}
+		}
+	}
+	
+	private boolean isInfinite() {
+		return maxElements < 0;
+	}
+
+	@Override
+	public void securityPunctuationElementRecieved(IPhysicalOperator senderOperator, ISecurityPunctuation sp, int port) {
+		punctuationElementRecieved(senderOperator, sp, port);
+	}
+
+	public void setMaxElements(int maxElements) {
+		this.maxElements = maxElements;
+	}
+
+	public int getMaxElements() {
+		return this.maxElements;
+	}
+
+	/**
+	 * @return the showHeartbeats
+	 */
+	public boolean isShowHeartbeats() {
+		return showHeartbeats;
+	}
+
+	/**
+	 * @param showHeartbeats the showHeartbeats to set
+	 */
+	public void setShowHeartbeats(boolean showHeartbeats) {
+		this.showHeartbeats = showHeartbeats;
+	}
+
+}
