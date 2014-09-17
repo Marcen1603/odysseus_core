6936a61de41f4838616a5f0cae36f57d5d8111cc
Michael Brand <michael.brand@uni-oldenburg.de>
2014-09-17 05:37:42 +0000

    LSOP-214: Empty IRecoveryCommunicator as a service and empty implementation class
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21371 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4c4df4cd37cf3e29a617ec44d78054520ce059a7

---
+++ server/peer/de.uniol.inf.is.odysseus.peer.recovery/src/de/uniol/inf/is/odysseus/peer/recovery/IRecoveryCommunicator.java
@@ -0,0 +1,14 @@
+package de.uniol.inf.is.odysseus.peer.recovery;
+
+/**
+ * A recovery communicator handles the communication between peers for recovery
+ * mechanisms.
+ * 
+ * @author Michael Brand
+ *
+ */
+public interface IRecoveryCommunicator {
+
+	// to be enhanced with methods
+
+}
