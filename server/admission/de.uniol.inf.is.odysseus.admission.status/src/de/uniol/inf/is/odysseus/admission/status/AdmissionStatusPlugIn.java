4a7503ea113be8c93fb3afc7917e2ea654ea524f
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-09-18 07:54:01 +0000

    AC: Added system load printer
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21408 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: fa67c7a5a8b65f42e98fbaaebb0911e57ef89eee

--- server/admission/de.uniol.inf.is.odysseus.admission.status/src/de/uniol/inf/is/odysseus/admission/status/AdmissionStatusPlugIn.java
+++ server/admission/de.uniol.inf.is.odysseus.admission.status/src/de/uniol/inf/is/odysseus/admission/status/AdmissionStatusPlugIn.java
@@ -3,6 +3,7 @@ package de.uniol.inf.is.odysseus.admission.status;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import de.uniol.inf.is.odysseus.admission.status.impl.SystemLoadPrinter;
 import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.IServerExecutor;
 import de.uniol.inf.is.odysseus.costmodel.logical.ILogicalCostModel;
@@ -10,13 +11,16 @@ import de.uniol.inf.is.odysseus.costmodel.physical.IPhysicalCostModel;
 
 public class AdmissionStatusPlugIn implements BundleActivator {
 
+	private static final SystemLoadPrinter SYSTEM_LOAD_PRINTER = new SystemLoadPrinter();
+	private static final boolean USE_SYSTEM_LOAD_PRINTER = true;
+	
 	private static IServerExecutor executor;
 	private static IPhysicalCostModel physicalCostModel;
 	private static ILogicalCostModel logicalCostModel;
 
 	// called by OSGi-DS
 	public static void bindExecutor(IExecutor serv) {
-		executor = (IServerExecutor)serv;
+		executor = (IServerExecutor) serv;
 	}
 
 	// called by OSGi-DS
@@ -49,15 +53,21 @@ public class AdmissionStatusPlugIn implements BundleActivator {
 			logicalCostModel = null;
 		}
 	}
-	
+
 	@Override
 	public void start(BundleContext context) throws Exception {
+		if( USE_SYSTEM_LOAD_PRINTER ) {
+			SYSTEM_LOAD_PRINTER.start();
+		}
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
+		if( USE_SYSTEM_LOAD_PRINTER ) {
+			SYSTEM_LOAD_PRINTER.stopRunning();
+		}
 	}
-	
+
 	public static IServerExecutor getServerExecutor() {
 		return executor;
 	}
@@ -65,7 +75,7 @@ public class AdmissionStatusPlugIn implements BundleActivator {
 	public static ILogicalCostModel getLogicalCostModel() {
 		return logicalCostModel;
 	}
-	
+
 	public static IPhysicalCostModel getPhysicalCostModel() {
 		return physicalCostModel;
 	}
--- server/admission/de.uniol.inf.is.odysseus.admission.status/src/de/uniol/inf/is/odysseus/admission/status/ExecutorAdmissionStatusComponent.java
+++ server/admission/de.uniol.inf.is.odysseus.admission.status/src/de/uniol/inf/is/odysseus/admission/status/ExecutorAdmissionStatusComponent.java
@@ -2,21 +2,37 @@ package de.uniol.inf.is.odysseus.admission.status;
 
 import java.util.Collection;
 
+import com.google.common.collect.Lists;
+
 import de.uniol.inf.is.odysseus.admission.IAdmissionStatusComponent;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.IPhysicalQuery;
 
 public class ExecutorAdmissionStatusComponent implements IAdmissionStatusComponent {
 
 	public int getRunningQueryCount() {
+		return getRunningQueryIDs().size();
+	}
+	
+	public Collection<Integer> getRunningQueryIDs() {
 		Collection<IPhysicalQuery> queries = AdmissionStatusPlugIn.getServerExecutor().getExecutionPlan().getQueries();
-		int runningQueryCount = 0;
+		Collection<Integer> runningQueryIDs = Lists.newArrayList();
 		for( IPhysicalQuery query : queries ) {
 			if( query.isOpened() ) {
-				runningQueryCount++;
+				runningQueryIDs.add(query.getID());
 			}
 		}
 		
-		return runningQueryCount;
+		return runningQueryIDs;
 	}
 	
+	public boolean hasRunningQueries() {
+		Collection<IPhysicalQuery> queries = AdmissionStatusPlugIn.getServerExecutor().getExecutionPlan().getQueries();
+		for( IPhysicalQuery query : queries ) {
+			if( query.isOpened() ) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
