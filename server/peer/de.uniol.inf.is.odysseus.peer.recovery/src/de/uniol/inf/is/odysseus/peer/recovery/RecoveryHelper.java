4b8760b0e04550a594b74fa787628eeb494fd4f0
Tobias Leo Brandt <tobias.leo.brandt@uni-oldenburg.de>
2014-09-18 19:24:20 +0000

    LSOP-215 Recovery - Communicator: Forgot messages, etc.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21447 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c63b36ea887f6c16d624fc63ecd21ee87508bf82

---
+++ server/peer/de.uniol.inf.is.odysseus.peer.recovery/src/de/uniol/inf/is/odysseus/peer/recovery/RecoveryHelper.java
@@ -0,0 +1,32 @@
+package de.uniol.inf.is.odysseus.peer.recovery;
+
+import java.util.Collection;
+
+import de.uniol.inf.is.odysseus.core.collection.Context;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.IServerExecutor;
+import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
+import de.uniol.inf.is.odysseus.peer.recovery.internal.RecoveryCommunicator;
+
+public class RecoveryHelper {
+	
+	/**
+	 * Installs and executes a query from PQL.
+	 * 
+	 * @param pql
+	 *            PQL to execute.
+	 */
+	public static Collection<Integer> installAndRunQueryPartFromPql(String pql) {
+		
+		IServerExecutor executor = RecoveryCommunicator.getExecutor();
+		ISession session = RecoveryCommunicator.getActiveSession();
+		
+		Collection<Integer> installedQueries = executor.addQuery(pql, "PQL",
+				session, "Standard", Context.empty());
+		for (int query : installedQueries) {
+			executor.startQuery(query, session);
+		}
+		return installedQueries;
+
+	}
+
+}
