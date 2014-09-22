d37ca1479524e9d1b2044bfccc6115886ab2621e
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-09-22 15:38:53 +0000

    Changed to new abstract transformation class
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21573 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e45738c10290bb31c2d4ebaa592d76ec8119b478

--- server/probabilistic/de.uniol.inf.is.odysseus.probabilistic.transform/src/de/uniol/inf/is/odysseus/probabilistic/transform/TProbabilisticApplicationTimestampRule.java
+++ server/probabilistic/de.uniol.inf.is.odysseus.probabilistic.transform/src/de/uniol/inf/is/odysseus/probabilistic/transform/TProbabilisticApplicationTimestampRule.java
@@ -1,25 +1,16 @@
 package de.uniol.inf.is.odysseus.probabilistic.transform;
 
-import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
-import de.uniol.inf.is.odysseus.core.server.logicaloperator.TimestampAO;
-import de.uniol.inf.is.odysseus.core.server.planmanagement.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
 import de.uniol.inf.is.odysseus.probabilistic.common.base.ProbabilisticTuple;
 import de.uniol.inf.is.odysseus.relational_interval.transform.TApplicationTimestampRule;
 
 public class TProbabilisticApplicationTimestampRule extends TApplicationTimestampRule{
 
+	@SuppressWarnings("unchecked")
 	@Override
-	public boolean isExecutable(TimestampAO operator,
-			TransformationConfiguration transformConfig) {
-		if (operator.getInputSchema(0).getType() == ProbabilisticTuple.class
-				&& transformConfig.getMetaTypes().contains(
-						ITimeInterval.class.getCanonicalName())) {
-			if (operator.isAllPhysicalInputSet()) {
-				return true;
-			}
-		}
-		return false;
+	protected Class<? extends IStreamObject<?>> getStreamType() {
+		return (Class<? extends IStreamObject<?>>) ProbabilisticTuple.class;
 	}
-	
+		
 }
 
