505326bc2ff38a81268d131bf2aa944417a66717
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-09-23 08:34:51 +0000

    RCP: New Plugin to show messages from infoService in error log view
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21592 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 48988d1809e0cb22bfeadde5b112fede928e3854

---
+++ client/studio/de.uniol.inf.is.odysseus.rcp.info/src/de/uniol/inf/is/odysseus/rcp/info/RCPInfoServiceListener.java
@@ -0,0 +1,40 @@
+package de.uniol.inf.is.odysseus.rcp.info;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+import de.uniol.inf.is.odysseus.core.infoservice.IInfoServiceListener;
+import de.uniol.inf.is.odysseus.core.infoservice.InfoType;
+
+public class RCPInfoServiceListener implements IInfoServiceListener {
+
+	@Override
+	public void newInfo(InfoType infoType, String message, Throwable throwable) {
+		if( Activator.isStarted() ) {
+			IStatus status = createStatus( infoType, message, throwable );
+			
+			Activator.getInstance().getLog().log(status);
+		}
+	}
+
+	private static IStatus createStatus(InfoType infoType, String message, Throwable throwable) {
+		if( throwable == null ) {
+			return new Status(toStatus(infoType), "InfoService", message);
+		} 
+			
+		return new Status(toStatus(infoType), "InfoService", message, throwable);
+	}
+
+	private static int toStatus(InfoType infoType) {
+		switch( infoType ) {
+		case ERROR:
+			return IStatus.ERROR;
+		case INFORMATION:
+			return IStatus.INFO;
+		case WARNING:
+			return IStatus.WARNING;
+		default:
+			return IStatus.INFO;
+		}
+	}
+}
