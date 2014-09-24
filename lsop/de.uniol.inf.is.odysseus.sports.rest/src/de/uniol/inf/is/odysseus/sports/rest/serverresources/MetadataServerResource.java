6863810edea7137e8b474ba8d1adccf0e5db663e
Thore Stratmann <tstratmann@uni-oldenburg.de>
2014-09-24 13:07:37 +0000

    LSOP-278: bugfix
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21648 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a320c1836ace7734561f83363093c3cd7a9742c3

--- lsop/de.uniol.inf.is.odysseus.sports.rest/src/de/uniol/inf/is/odysseus/sports/rest/serverresources/MetadataServerResource.java
+++ lsop/de.uniol.inf.is.odysseus.sports.rest/src/de/uniol/inf/is/odysseus/sports/rest/serverresources/MetadataServerResource.java
@@ -87,10 +87,10 @@ public class MetadataServerResource extends ServerResource implements IMetadataR
 			goals.add(new GoalInfo(position_left, first_post_left, second_post_left, deep_left, height_left));
 			
 			int position_right =1;
-			double first_post_right = SoccerDDCAccess.getGoalareaLeftYMin();
-			double second_post_right = SoccerDDCAccess.getGoalareaLeftYMax();
-			double deep_right = SoccerDDCAccess.getGoalareaLeftX();
-			double height_right = SoccerDDCAccess.getGoalareaLeftZMax();			
+			double first_post_right = SoccerDDCAccess.getGoalareaRightYMin();
+			double second_post_right = SoccerDDCAccess.getGoalareaRightYMax();
+			double deep_right = SoccerDDCAccess.getGoalareaRightX();
+			double height_right = SoccerDDCAccess.getGoalareaRightZMax();			
 			//add the right goal to the list
 			goals.add(new GoalInfo(position_right, first_post_right, second_post_right, deep_right, height_right));
 			
