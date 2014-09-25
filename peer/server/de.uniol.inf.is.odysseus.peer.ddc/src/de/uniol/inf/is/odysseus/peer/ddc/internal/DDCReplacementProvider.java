3b6891f7439848877c062380de69e3b29b76c6a0
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-09-25 10:50:55 +0000

    Peer: DDC provides its keys (and values) to the odysseus script parser
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21670 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 148b660ed5edf6678c4d30a1c049b62b3b36f6a2

---
+++ peer/server/de.uniol.inf.is.odysseus.peer.ddc/src/de/uniol/inf/is/odysseus/peer/ddc/internal/DDCReplacementProvider.java
@@ -0,0 +1,63 @@
+package de.uniol.inf.is.odysseus.peer.ddc.internal;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+
+import de.uniol.inf.is.odysseus.peer.ddc.DDCKey;
+import de.uniol.inf.is.odysseus.peer.ddc.IDistributedDataContainer;
+import de.uniol.inf.is.odysseus.peer.ddc.MissingDDCEntryException;
+import de.uniol.inf.is.odysseus.script.parser.IReplacementProvider;
+
+/**
+ * Connection class between DDC and OdysseusScriptParser
+ * 
+ * @author Timo
+ */
+public class DDCReplacementProvider implements IReplacementProvider {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DDCReplacementProvider.class);
+
+	private static IDistributedDataContainer ddc;
+
+	// called by OSGi-DS
+	public static void bindDDC(IDistributedDataContainer serv) {
+		ddc = serv;
+	}
+
+	// called by OSGi-DS
+	public static void unbindDDC(IDistributedDataContainer serv) {
+		if (ddc == serv) {
+			ddc = null;
+		}
+	}
+
+	@Override
+	public Collection<String> getReplacementKeys() {
+		Set<DDCKey> keys = ddc.getKeys();
+		return toStringKeys(keys);
+	}
+
+	private static Collection<String> toStringKeys(Set<DDCKey> keys) {
+		Collection<String> stringKeys = Lists.newArrayListWithCapacity(keys.size());
+		for (DDCKey key : keys) {
+			stringKeys.add(key.toString());
+		}
+		return stringKeys;
+	}
+
+	@Override
+	public String getReplacementValue(String key) {
+		try {
+			return ddc.get(new DDCKey(key)).toString();
+		} catch (MissingDDCEntryException e) {
+			LOG.error("Could not get key '{}' from DDC", key, e);
+			return "";
+		}
+	}
+
+}
