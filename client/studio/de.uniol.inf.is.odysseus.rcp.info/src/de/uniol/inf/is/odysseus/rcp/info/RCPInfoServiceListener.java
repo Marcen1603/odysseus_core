991c2206ec57167eac11aa3b104cebfb79395e02
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-10-14 14:45:39 +0000

    RCPInfoServiceListener calls slf4j too.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@21996 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 18638817cbaf5406800ae7030701607ee063c2db

--- client/studio/de.uniol.inf.is.odysseus.rcp.info/src/de/uniol/inf/is/odysseus/rcp/info/RCPInfoServiceListener.java
+++ client/studio/de.uniol.inf.is.odysseus.rcp.info/src/de/uniol/inf/is/odysseus/rcp/info/RCPInfoServiceListener.java
@@ -2,6 +2,8 @@ package de.uniol.inf.is.odysseus.rcp.info;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.uniol.inf.is.odysseus.core.infoservice.IInfoServiceListener;
 import de.uniol.inf.is.odysseus.core.infoservice.InfoType;
@@ -9,29 +11,45 @@ import de.uniol.inf.is.odysseus.rcp.exception.ExceptionWindow;
 
 public class RCPInfoServiceListener implements IInfoServiceListener {
 
+	private static final Logger LOG = LoggerFactory
+			.getLogger(RCPInfoServiceListener.class);
+
 	@Override
-	public void newInfo(InfoType infoType, String message, Throwable throwable, String source) {
-		if( Activator.isStarted() ) {
-			IStatus status = createStatus( infoType, message, throwable, source );
-			
-			if (infoType == InfoType.ERROR){
+	public void newInfo(InfoType infoType, String message, Throwable throwable,
+			String source) {
+		if (Activator.isStarted()) {
+			IStatus status = createStatus(infoType, message, throwable, source);
+
+			if (infoType == InfoType.ERROR) {
 				new ExceptionWindow(message, throwable);
 			}
-			
+
 			Activator.getInstance().getLog().log(status);
+		}
+		switch (infoType) {
+		case ERROR:
+			LOG.error(message, throwable);
+			break;
+		case INFORMATION:
+			LOG.info(message);
+			break;
+		case WARNING:
+			LOG.warn(message, throwable);
+			break;
 		}
 	}
 
-	private static IStatus createStatus(InfoType infoType, String message, Throwable throwable, String source) {
-		if( throwable == null ) {
+	private static IStatus createStatus(InfoType infoType, String message,
+			Throwable throwable, String source) {
+		if (throwable == null) {
 			return new Status(toStatus(infoType), source, message);
-		} 
-			
+		}
+
 		return new Status(toStatus(infoType), source, message, throwable);
 	}
 
 	private static int toStatus(InfoType infoType) {
-		switch( infoType ) {
+		switch (infoType) {
 		case ERROR:
 			return IStatus.ERROR;
 		case INFORMATION:
