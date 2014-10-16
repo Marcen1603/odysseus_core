3237229ae7bd9f5987495ac9855129b86c4b1176
Andre Albrecht <aalbrecht@uni-oldenburg.de>
2014-10-16 19:12:44 +0000

    Temper1 raspberry pi bug fix.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22042 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: cef747faa14a651ead8cd4a6495b79ca9716c3cd

--- wrapper/temper1/src/de/uniol/inf/is/odysseus/wrapper/temper1/physicaloperator/access/Temper1TransportHandler.java
+++ wrapper/temper1/src/de/uniol/inf/is/odysseus/wrapper/temper1/physicaloperator/access/Temper1TransportHandler.java
@@ -1,12 +1,13 @@
 package de.uniol.inf.is.odysseus.wrapper.temper1.physicaloperator.access;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,121 +24,175 @@ import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.AbstractS
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportHandler;
 import de.uniol.inf.is.odysseus.core.sdf.unit.SDFTimeUnit;
 
+public class Temper1TransportHandler extends
+		AbstractSimplePullTransportHandler<Tuple<?>> {
+	private static Logger LOG = LoggerFactory
+			.getLogger(Temper1TransportHandler.class);
 
-public class Temper1TransportHandler extends AbstractSimplePullTransportHandler<Tuple<?>>{
-	private static Logger LOG = LoggerFactory.getLogger(Temper1TransportHandler.class);
-	
 	public static final String NAME = "Temper1";
 	private static final int VENDOR_ID = 3141;
-    private static final int PRODUCT_ID = 29697;
-    private static final int BUFSIZE = 2048;
-    private static ArrayList<HIDDeviceInfo> temperaturSensors = new ArrayList<HIDDeviceInfo>();
-    public static final String TEMPNUMBER = "tempnumber";
-    
+	private static final int PRODUCT_ID = 29697;
+	private static final int BUFSIZE = 2048;
+	private static ArrayList<HIDDeviceInfo> temperaturSensors = new ArrayList<HIDDeviceInfo>();
+	public static final String TEMPNUMBER = "tempnumber";
+	public static final String METHOD_HID_MANAGER = "hidmanager";
+	public static final String METHOD_RPI_TEMPER = "rpitemper";
+
 	static {
-    	ClassPathLibraryLoader.loadNativeHIDLibrary();
+		ClassPathLibraryLoader.loadNativeHIDLibrary();
 	}
-	
+
 	private int currentTempNumber;
 	private long lastUpdateConnectedTemperatureSensorsTime;
 	private long DELTATIME_FOR_DEVICE_UPDATE_MS = 10000;
 
+	private HIDManager hidManager;
+
+	private String methodToGetTemperature;
+
 	public Temper1TransportHandler() {
 		// TODO Auto-generated constructor stub
+
+		// UnsatisfiedLinkError
+		try {
+			setHidManager(HIDManager.getInstance());
+
+			methodToGetTemperature = METHOD_HID_MANAGER;
+		} catch (UnsatisfiedLinkError ex) {
+			LOG.error(ex.getMessage(), ex);
+
+			methodToGetTemperature = METHOD_RPI_TEMPER;
+		} catch (IOException ex) {
+			LOG.error(ex.getMessage(), ex);
+		}
 	}
 
 	public Temper1TransportHandler(Temper1TransportHandler other) {
 		// TODO Auto-generated constructor stub
-		
+
 	}
 
 	@Override
-	public ITransportHandler createInstance(IProtocolHandler<?> protocolHandler, OptionMap options) {
+	public ITransportHandler createInstance(
+			IProtocolHandler<?> protocolHandler, OptionMap options) {
 		updateConnectedTemperatureSensors();
-		
+
 		Temper1TransportHandler tHandler = new Temper1TransportHandler();
-		
+
 		if (options.containsKey(TEMPNUMBER)) {
 			tHandler.setTempNumber(options.get(TEMPNUMBER));
-	    }
-		
+		}
+
 		protocolHandler.setTransportHandler(tHandler);
-		
+
 		return tHandler;
 	}
 
 	private void updateConnectedTemperatureSensors() {
-		long delta = System.currentTimeMillis() - lastUpdateConnectedTemperatureSensorsTime;
-		
-		if(delta >= DELTATIME_FOR_DEVICE_UPDATE_MS ){
-			//System.out.println("updateConnectedTemperatureSensors");
-			
+		long delta = System.currentTimeMillis()
+				- lastUpdateConnectedTemperatureSensorsTime;
+
+		if (delta >= DELTATIME_FOR_DEVICE_UPDATE_MS) {
+			// System.out.println("updateConnectedTemperatureSensors");
+
 			temperaturSensors.clear();
 			try {
-	            //com.codeminders.hidapi.ClassPathLibraryLoader.loadNativeHIDLibrary();
-	            HIDDeviceInfo[] infos = HIDManager.getInstance().listDevices();
-	            
-	            for (HIDDeviceInfo info : infos) {
-	                if(info.getVendor_id()==VENDOR_ID && info.getProduct_id()==PRODUCT_ID && info.getUsage()==1){
-	                	if(!temperaturSensors.contains(info)){
-	                		temperaturSensors.add(info);
-	                	}
-	                }
-	            }
-	            
-	            //System.out.println("size:"+temperaturSensors.size());
-			}catch(IOException ex){
-				//ex.printStackTrace();
+				// com.codeminders.hidapi.ClassPathLibraryLoader.loadNativeHIDLibrary();
+				HIDDeviceInfo[] infos = HIDManager.getInstance().listDevices();
+
+				for (HIDDeviceInfo info : infos) {
+					if (info.getVendor_id() == VENDOR_ID
+							&& info.getProduct_id() == PRODUCT_ID
+							&& info.getUsage() == 1) {
+						if (!temperaturSensors.contains(info)) {
+							temperaturSensors.add(info);
+						}
+					}
+				}
+
+				// System.out.println("size:"+temperaturSensors.size());
+			} catch (IOException ex) {
+				// ex.printStackTrace();
 				LOG.error(ex.getMessage(), ex);
-				
+
 				LOG.debug("try to load other lib for rpi...");
-				
-				
-				echoTest();
-				
-				
-	        } catch (Exception ex) {
-	            //ex.printStackTrace();
-	            LOG.error(ex.getMessage(), ex);
-	        }
-			
-			lastUpdateConnectedTemperatureSensorsTime = System.currentTimeMillis();
+
+			} catch (Exception ex) {
+				// ex.printStackTrace();
+				LOG.error(ex.getMessage(), ex);
+			}
+
+			lastUpdateConnectedTemperatureSensorsTime = System
+					.currentTimeMillis();
 		}
 	}
 
-	private static void echoTest() {
+	private static float getTemperatureFromRPiTemperBinary(int deviceNumber) throws Exception {
 		try {
 			final Process p = Runtime.getRuntime().exec("lib/pcsensor");
-			
+
+			/*
 			new Thread(new Runnable() {
-			    public void run() {
-			        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			        String line = null; 
-			        
-			        try {
-			            while ((line = input.readLine()) != null) {
-			                System.out.println(line);
-			            }
-			        } catch (IOException e) {
-			            e.printStackTrace();
-			        }
-			    }
+				public void run() {
+					BufferedReader input = new BufferedReader(
+							new InputStreamReader(p.getInputStream()));
+					String line = null;
+
+					try {
+						while ((line = input.readLine()) != null) {
+							return parseTemperature(line);
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
 			}).start();
+			*/
 			
+			BufferedReader input = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));
+			String line = null;
+
+			try {
+				int lineNumber = 0;
+				while ((line = input.readLine()) != null) {
+					if(lineNumber==deviceNumber){
+						String temperature = parseTemperature(line);
+						Float temperatureFloat = Float.parseFloat(temperature);
+						return temperatureFloat.floatValue();
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 			p.waitFor();
-			
+
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		} catch(Exception e2){
+		} catch (Exception e2) {
 			e2.printStackTrace();
 		}
+		throw new Exception("No temperature value available.");
 	}
 	
+	private static String parseTemperature(String line) {
+		// Example: 2014/10/16 17:23:41 Temperature 71.71F 22.06C
+		Pattern pattern = Pattern.compile(".*([0-9]{2}\\.[0-9]{2})[C]$");
+
+        Matcher matcher = pattern.matcher(line);
+        if(matcher.matches()) {
+        	return matcher.group(1);
+        }else{
+        	throw new IllegalStateException("No match found");
+        }
+	}
+
 	private void setTempNumber(String tempNumber) {
 		Integer tempNumberInteger = Integer.parseInt(tempNumber);
-		
+
 		this.currentTempNumber = tempNumberInteger.intValue();
 	}
 
@@ -145,78 +200,76 @@ public class Temper1TransportHandler extends AbstractSimplePullTransportHandler<
 	public String getName() {
 		return NAME;
 	}
-	
+
 	/**
-	 * Nach
-	 * Quelle:
-	 * http://www.igorkromin.net/index.php/2013/02/16/using-java-hidapi-on-os-x-to-read-temperature-from-the-temper1-sensor/
-	 * @param hidDeviceInfo 
-	 * @throws IOException 
+	 * Nach Quelle:
+	 * http://www.igorkromin.net/index.php/2013/02/16/using-java-hidapi
+	 * -on-os-x-to-read-temperature-from-the-temper1-sensor/
+	 * 
+	 * @param hidDeviceInfo
+	 * @throws IOException
 	 * 
 	 */
-    @SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	private static float readDevice(HIDDeviceInfo device) throws IOException {
-    	HIDDevice dev;
-    	
-        HIDManager hid_mgr = HIDManager.getInstance();
-        //dev = hid_mgr.openById(VENDOR_ID, PRODUCT_ID, null);
-        dev = hid_mgr.openByPath(device.getPath());
-        
-        byte[] temp = new byte[] {
-            (byte)0x01, (byte)0x80, (byte)0x33, (byte)0x01, 
-            (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00
-        };
-        
-        try
-        {
-        	int res = dev.write(temp);
-        	
-            byte[] buf = new byte[BUFSIZE];
-            int n = dev.read(buf);
-                
-            int rawtemp = (buf[3] & (byte)0xFF) + (buf[2] << 8);
-            if ((buf[2] & 0x80) != 0) {
-                /* return the negative of magnitude of the temperature */
-                rawtemp = -((rawtemp ^ 0xffff) + 1);
-            }
-            
-            return c_to_u(raw_to_c(rawtemp), 'C');
-        }
-        finally
-        {
-            dev.close();
-            hid_mgr.release();    
-        }
-    }
-    
-    /**
-	 * Quelle: 
-	 * http://www.igorkromin.net/index.php/2013/02/16/using-java-hidapi-on-os-x-to-read-temperature-from-the-temper1-sensor/
+		HIDDevice dev;
+
+		HIDManager hid_mgr = HIDManager.getInstance();
+		// dev = hid_mgr.openById(VENDOR_ID, PRODUCT_ID, null);
+		dev = hid_mgr.openByPath(device.getPath());
+
+		byte[] temp = new byte[] { (byte) 0x01, (byte) 0x80, (byte) 0x33,
+				(byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00 };
+
+		try {
+			int res = dev.write(temp);
+
+			byte[] buf = new byte[BUFSIZE];
+			int n = dev.read(buf);
+
+			int rawtemp = (buf[3] & (byte) 0xFF) + (buf[2] << 8);
+			if ((buf[2] & 0x80) != 0) {
+				/* return the negative of magnitude of the temperature */
+				rawtemp = -((rawtemp ^ 0xffff) + 1);
+			}
+
+			return c_to_u(raw_to_c(rawtemp), 'C');
+		} finally {
+			dev.close();
+			hid_mgr.release();
+		}
+	}
+
+	/**
+	 * Quelle:
+	 * http://www.igorkromin.net/index.php/2013/02/16/using-java-hidapi-on
+	 * -os-x-to-read-temperature-from-the-temper1-sensor/
+	 * 
 	 * @param rawtemp
 	 * @return
 	 */
-	private static float raw_to_c(int rawtemp)
-    {
-        float temp_c = rawtemp * (125.f / 32000.f);
-        return temp_c;
-    }
-    
+	private static float raw_to_c(int rawtemp) {
+		float temp_c = rawtemp * (125.f / 32000.f);
+		return temp_c;
+	}
+
 	/**
 	 * Quelle:
-	 * http://www.igorkromin.net/index.php/2013/02/16/using-java-hidapi-on-os-x-to-read-temperature-from-the-temper1-sensor/
+	 * http://www.igorkromin.net/index.php/2013/02/16/using-java-hidapi-on
+	 * -os-x-to-read-temperature-from-the-temper1-sensor/
+	 * 
 	 * @param deg_c
 	 * @param unit
 	 * @return
 	 */
-    private static float c_to_u(float deg_c, char unit)
-    {
-        if (unit == 'F')
-            return (deg_c * 1.8f) + 32.f;
-        else if (unit == 'K')
-            return (deg_c + 273.15f);
-        else
-            return deg_c;
-    }
+	private static float c_to_u(float deg_c, char unit) {
+		if (unit == 'F')
+			return (deg_c * 1.8f) + 32.f;
+		else if (unit == 'K')
+			return (deg_c + 273.15f);
+		else
+			return deg_c;
+	}
 
 	@Override
 	public boolean hasNext() {
@@ -227,47 +280,77 @@ public class Temper1TransportHandler extends AbstractSimplePullTransportHandler<
 	@Override
 	public Tuple<?> getNext() {
 		Tuple<?> tuple = new Tuple(2, false);
-		
-		int deviceNumber = this.currentTempNumber < temperaturSensors.size() ? this.currentTempNumber : 0;
-		
+
+		int deviceNumber = this.currentTempNumber < temperaturSensors.size() ? this.currentTempNumber
+				: 0;
+
 		try {
-			tuple.setAttribute(0, readDevice(temperaturSensors.get(deviceNumber)));
+			switch (this.methodToGetTemperature) {
+			case METHOD_HID_MANAGER:
+				tuple.setAttribute(0, getTemperatureFromHIDManager(deviceNumber));
+				break;
+			case METHOD_RPI_TEMPER:
+				tuple.setAttribute(0, getTemperatureFromRPiTemperBinary(deviceNumber));
+				break;
+			default:
+				break;
+			}
 			
+			// readDevice(temperaturSensors.get(deviceNumber)));
+
 			/*
-			tuple.setAttribute(0, readDevice(temperaturSensors.get(--i)));
-			if(i==0){
-				i=temperaturSensors.size();
-			}*/
+			 * tuple.setAttribute(0, readDevice(temperaturSensors.get(--i)));
+			 * if(i==0){ i=temperaturSensors.size(); }
+			 */
 		} catch (IOException e) {
 			tuple.setAttribute(0, getSimulatedTemperature());
 			updateConnectedTemperatureSensors();
-		} catch(Exception e){
+		} catch (Exception e) {
 			tuple.setAttribute(0, getSimulatedTemperature());
 			updateConnectedTemperatureSensors();
 		}
-		
+
 		tuple.setAttribute(1, new SDFTimeUnit("YYYY-MM-dd HH-mm-ss"));
-		
+
 		return tuple;
 	}
-	
-	private static float getSimulatedTemperature(){
-    	Calendar calendar = Calendar.getInstance();
-    	calendar.setTime(new Date(System.currentTimeMillis()));
-    	float seconds = calendar.get(Calendar.SECOND);
-    	
-    	float y=0;
-		if(seconds>=30){
-			y = ((seconds*-1)+60)/60 ; // 30...0
-		}else{
-			y = seconds/60 ;//0...30
+
+	private float getTemperatureFromHIDManager(int deviceNumber) throws IOException {
+		switch (this.methodToGetTemperature) {
+		case METHOD_HID_MANAGER:
+			return readDevice(temperaturSensors.get(deviceNumber));
+		case METHOD_RPI_TEMPER:
+			
+			break;
 		}
-		
-    	return 10 + y*30; //10..15..20..25..20..15..10
-    }
-	
+		throw new IOException("No temperature sensor available.");
+	}
+
+	private static float getSimulatedTemperature() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date(System.currentTimeMillis()));
+		float seconds = calendar.get(Calendar.SECOND);
+
+		float y = 0;
+		if (seconds >= 30) {
+			y = ((seconds * -1) + 60) / 60; // 30...0
+		} else {
+			y = seconds / 60;// 0...30
+		}
+
+		return 10 + y * 30; // 10..15..20..25..20..15..10
+	}
+
 	@Override
 	public boolean isSemanticallyEqualImpl(ITransportHandler other) {
 		return false;
+	}
+
+	public HIDManager getHidManager() {
+		return hidManager;
+	}
+
+	public void setHidManager(HIDManager hidManager) {
+		this.hidManager = hidManager;
 	}
 }
