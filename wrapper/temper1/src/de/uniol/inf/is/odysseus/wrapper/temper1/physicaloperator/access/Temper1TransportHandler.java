5538947f48b81b0ff609a7d85bfb24a5e630f376
Andre Albrecht <aalbrecht@uni-oldenburg.de>
2014-10-16 22:02:02 +0000

    Permission change of the temper1 fallback binary in the tmp folder.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22050 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 87f264883dc9f0dfb9e179de602c3c7158148402

--- wrapper/temper1/src/de/uniol/inf/is/odysseus/wrapper/temper1/physicaloperator/access/Temper1TransportHandler.java
+++ wrapper/temper1/src/de/uniol/inf/is/odysseus/wrapper/temper1/physicaloperator/access/Temper1TransportHandler.java
@@ -52,78 +52,13 @@ public class Temper1TransportHandler extends
 	private int currentTempNumber;
 	private long lastUpdateConnectedTemperatureSensorsTime;
 	private long DELTATIME_FOR_DEVICE_UPDATE_MS = 10000;
-
 	private HIDManager hidManager;
-
 	private static String methodToGetTemperature;
-
 	private static String rpiTemper1TempPath;
-
-	private static boolean initRPiTemperBinRunSeccessfull=false;
-
-	private static boolean exceptionWasThrownInitRPiTemperBin=false;
-
+	private static boolean initRPiTemperBinRunSuccessfull = false;
+	private static boolean exceptionWasThrownInitRPiTemperBin = false;
 	private static long exceptionThrownInitRPiTemperBinLastTime;
-
 	private static long lastExceptionThrown;
-	
-
-	public static String getMethodToGetTemperature() {
-		return methodToGetTemperature;
-	}
-
-	public static void setMethodToGetTemperature(String methodToGetTemperature) {
-		Temper1TransportHandler.methodToGetTemperature = methodToGetTemperature;
-	}
-
-	private static void initRPiTemperBin() {
-		try {
-			String path = RPI_TEMPER_BIN;
-			InputStream in = ClassPathLibraryLoader.class
-					.getResourceAsStream(path);
-			if (in != null) {
-				try {
-					// always write to different location
-					String tempName = path.substring(path.lastIndexOf('/') + 1);
-					File fileOut = File.createTempFile(tempName.substring(0,
-							tempName.lastIndexOf('.')), tempName.substring(
-							tempName.lastIndexOf('.'), tempName.length()));
-					fileOut.deleteOnExit();
-
-					OutputStream out = new FileOutputStream(fileOut);
-					byte[] buf = new byte[1024];
-					int len;
-					while ((len = in.read(buf)) > 0) {
-						out.write(buf, 0, len);
-					}
-
-					out.close();
-					// Runtime.getRuntime().load(fileOut.toString());
-
-					// rpiTemperPath = tempName;
-					setRpiTemperPath(fileOut.getAbsolutePath());
-
-					System.out.println("rpitemper path:" + tempName);
-					System.out.println("rpitemper path2:"
-							+ fileOut.getAbsolutePath());
-
-				} finally {
-					in.close();
-					setInitRPiTemperBinRunSeccessfull(true);
-				}
-			}
-			if(exceptionWasThrownInitRPiTemperBin){
-				exceptionWasThrownInitRPiTemperBin=false;
-			}
-		} catch (IOException ex) {
-			long delta = System.currentTimeMillis() - exceptionThrownInitRPiTemperBinLastTime;
-			if(!exceptionWasThrownInitRPiTemperBin || delta >= DELTA_THROWEXCEPTIONS){
-				ex.printStackTrace();
-				exceptionThrownInitRPiTemperBinLastTime = System.currentTimeMillis();
-				exceptionWasThrownInitRPiTemperBin=true;
-			}
-		}
-	}
 
 	public Temper1TransportHandler() {
 		try {
@@ -132,17 +67,17 @@ public class Temper1TransportHandler extends
 			methodToGetTemperature = METHOD_HID_MANAGER;
 		} catch (UnsatisfiedLinkError ex) {
 			LOG.error(ex.getMessage(), ex);
-			
-			initRPiTemperBin();
-			
+
+			// use the fallback program:
 			methodToGetTemperature = METHOD_RPI_TEMPER;
+			initRPiTemperBin();
 		} catch (IOException ex) {
 			LOG.error(ex.getMessage(), ex);
 		}
 	}
 
 	public Temper1TransportHandler(Temper1TransportHandler other) {
-		
+
 	}
 
 	@Override
@@ -161,104 +96,59 @@ public class Temper1TransportHandler extends
 		return tHandler;
 	}
 
-	private void updateConnectedTemperatureSensors() {
-		if(methodToGetTemperature==null || getHidManager()==null || methodToGetTemperature.isEmpty() || !methodToGetTemperature.equals(METHOD_HID_MANAGER) ){
-			return;
-		}
-		
-		long delta = System.currentTimeMillis()
-				- lastUpdateConnectedTemperatureSensorsTime;
-
-		if (delta >= DELTATIME_FOR_DEVICE_UPDATE_MS) {
-			// System.out.println("updateConnectedTemperatureSensors");
-
-			temperaturSensors.clear();
-			try {
-				// com.codeminders.hidapi.ClassPathLibraryLoader.loadNativeHIDLibrary();
-				HIDDeviceInfo[] infos = HIDManager.getInstance().listDevices();
-
-				for (HIDDeviceInfo info : infos) {
-					if (info.getVendor_id() == VENDOR_ID
-							&& info.getProduct_id() == PRODUCT_ID
-							&& info.getUsage() == 1) {
-						if (!temperaturSensors.contains(info)) {
-							temperaturSensors.add(info);
-						}
-					}
-				}
-
-				// System.out.println("size:"+temperaturSensors.size());
-			} catch (IOException ex) {
-				// ex.printStackTrace();
-				LOG.error(ex.getMessage(), ex);
-
-				LOG.debug("try to load other lib for rpi...");
-
-			} catch (Exception ex) {
-				// ex.printStackTrace();
-				LOG.error(ex.getMessage(), ex);
-			}
-
-			lastUpdateConnectedTemperatureSensorsTime = System
-					.currentTimeMillis();
-		}
+	@Override
+	public String getName() {
+		return NAME;
 	}
 
-	//This method is a fallback for some raspberry pi models
-	private static float getTemperatureFromRPiTemperBinary(int deviceNumber)
-			throws Exception {
+	@Override
+	public boolean hasNext() {
+		return true;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Tuple<?> getNext() {
+		Tuple<?> tuple = new Tuple(2, false);
+
+		int deviceNumber = this.currentTempNumber < temperaturSensors.size() ? this.currentTempNumber
+				: 0;
+
 		try {
-			final Process p = Runtime.getRuntime().exec(getRpiTemper1TempPath());
+			tuple.setAttribute(0, getTemperature(deviceNumber));
+			tuple.setAttribute(1, getTemperature(deviceNumber));
+			// readDevice(temperaturSensors.get(deviceNumber)));
 
 			/*
-			 * new Thread(new Runnable() { public void run() { BufferedReader
-			 * input = new BufferedReader( new
-			 * InputStreamReader(p.getInputStream())); String line = null;
-			 * 
-			 * try { while ((line = input.readLine()) != null) { return
-			 * parseTemperature(line); } } catch (IOException e) {
-			 * e.printStackTrace(); } } }).start();
+			 * tuple.setAttribute(0, readDevice(temperaturSensors.get(--i)));
+			 * if(i==0){ i=temperaturSensors.size(); }
 			 */
-
-			BufferedReader input = new BufferedReader(new InputStreamReader(
-					p.getInputStream()));
-			String line = null;
-
-			try {
-				int lineNumber = 0;
-				while ((line = input.readLine()) != null) {
-					if (lineNumber == deviceNumber) {
-						String temperature = parseTemperature(line);
-						Float temperatureFloat = Float.parseFloat(temperature);
-						return temperatureFloat.floatValue();
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			p.waitFor();
-
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (Exception e2) {
-			e2.printStackTrace();
+		} catch (IOException e) {
+			tuple.setAttribute(0, getSimulatedTemperature());
+			tuple.setAttribute(1, getSimulatedTemperature());
+			updateConnectedTemperatureSensors();
+		} catch (Exception e) {
+			tuple.setAttribute(0, getSimulatedTemperature());
+			tuple.setAttribute(1, getSimulatedTemperature());
+			updateConnectedTemperatureSensors();
 		}
-		throw new Exception("No temperature value available.");
+
+		// tuple.setAttribute(1, new SDFTimeUnit("YYYY-MM-dd HH-mm-ss"));
+
+		return tuple;
 	}
 
-	private static String parseTemperature(String line) {
-		// Example: 2014/10/16 17:23:41 Temperature 71.71F 22.06C
-		Pattern pattern = Pattern.compile(".*([0-9]{2}\\.[0-9]{2})[C]$");
+	@Override
+	public boolean isSemanticallyEqualImpl(ITransportHandler other) {
+		return false;
+	}
 
-		Matcher matcher = pattern.matcher(line);
-		if (matcher.matches()) {
-			return matcher.group(1);
-		} else {
-			throw new IllegalStateException("No match found");
-		}
+	public static String getMethodToGetTemperature() {
+		return Temper1TransportHandler.methodToGetTemperature;
+	}
+
+	public static void setMethodToGetTemperature(String methodToGetTemperature) {
+		Temper1TransportHandler.methodToGetTemperature = methodToGetTemperature;
 	}
 
 	private void setTempNumber(String tempNumber) {
@@ -267,10 +157,20 @@ public class Temper1TransportHandler extends
 		this.currentTempNumber = tempNumberInteger.intValue();
 	}
 
-	@Override
-	public String getName() {
-		return NAME;
+	private float getTemperature(int deviceNumber) throws Exception {
+		switch (methodToGetTemperature) {
+		case METHOD_HID_MANAGER:
+			return getTemperatureFromHIDManager(temperaturSensors
+					.get(deviceNumber));
+		case METHOD_RPI_TEMPER:
+			return getTemperatureFromRPiTemperBinary(deviceNumber);
+		}
+		throw new IOException("No temperature sensor available.");
 	}
+
+	/*****************************************************************/
+	// Temper1 get temperature with the HID-API
+	/*****************************************************************/
 
 	/**
 	 * Nach Quelle:
@@ -282,7 +182,8 @@ public class Temper1TransportHandler extends
 	 * 
 	 */
 	@SuppressWarnings("unused")
-	private static float readDevice(HIDDeviceInfo device) throws IOException {
+	private static float getTemperatureFromHIDManager(HIDDeviceInfo device)
+			throws IOException {
 		HIDDevice dev;
 
 		HIDManager hid_mgr = HIDManager.getInstance();
@@ -342,73 +243,6 @@ public class Temper1TransportHandler extends
 			return deg_c;
 	}
 
-	@Override
-	public boolean hasNext() {
-		return true;
-	}
-
-	@SuppressWarnings("rawtypes")
-	@Override
-	public Tuple<?> getNext() {
-		Tuple<?> tuple = new Tuple(2, false);
-
-		int deviceNumber = this.currentTempNumber < temperaturSensors.size() ? this.currentTempNumber
-				: 0;
-
-		try {
-			tuple.setAttribute(0, getTemperature(deviceNumber));
-			tuple.setAttribute(1, getTemperature(deviceNumber));
-			// readDevice(temperaturSensors.get(deviceNumber)));
-
-			/*
-			 * tuple.setAttribute(0, readDevice(temperaturSensors.get(--i)));
-			 * if(i==0){ i=temperaturSensors.size(); }
-			 */
-		} catch (IOException e) {
-			tuple.setAttribute(0, getSimulatedTemperature());
-			tuple.setAttribute(1, getSimulatedTemperature());
-			updateConnectedTemperatureSensors();
-		} catch (Exception e) {
-			tuple.setAttribute(0, getSimulatedTemperature());
-			tuple.setAttribute(1, getSimulatedTemperature());
-			updateConnectedTemperatureSensors();
-		}
-
-		//tuple.setAttribute(1, new SDFTimeUnit("YYYY-MM-dd HH-mm-ss"));
-
-		return tuple;
-	}
-
-	private float getTemperature(int deviceNumber) throws Exception {
-		switch (methodToGetTemperature) {
-		case METHOD_HID_MANAGER:
-			return readDevice(temperaturSensors.get(deviceNumber));
-		case METHOD_RPI_TEMPER:
-			return getTemperatureFromRPiTemperBinary(deviceNumber);
-		}
-		throw new IOException("No temperature sensor available.");
-	}
-
-	private static float getSimulatedTemperature() {
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(new Date(System.currentTimeMillis()));
-		float seconds = calendar.get(Calendar.SECOND);
-		
-		float y = 0;
-		if (seconds >= 30) {
-			y = ((seconds * -1) + 60) / 60; // 30...0
-		} else {
-			y = seconds / 60;// 0...30
-		}
-
-		return 10 + y * 30; // 10..15..20..25..20..15..10
-	}
-
-	@Override
-	public boolean isSemanticallyEqualImpl(ITransportHandler other) {
-		return false;
-	}
-
 	public HIDManager getHidManager() {
 		return hidManager;
 	}
@@ -417,41 +251,209 @@ public class Temper1TransportHandler extends
 		this.hidManager = hidManager;
 	}
 
+	private void updateConnectedTemperatureSensors() {
+		if (methodToGetTemperature == null || getHidManager() == null
+				|| methodToGetTemperature.isEmpty()
+				|| !methodToGetTemperature.equals(METHOD_HID_MANAGER)) {
+			return;
+		}
+
+		long delta = System.currentTimeMillis()
+				- lastUpdateConnectedTemperatureSensorsTime;
+
+		if (delta >= DELTATIME_FOR_DEVICE_UPDATE_MS) {
+			temperaturSensors.clear();
+			try {
+				// com.codeminders.hidapi.ClassPathLibraryLoader.loadNativeHIDLibrary();
+				HIDDeviceInfo[] infos = HIDManager.getInstance().listDevices();
+
+				for (HIDDeviceInfo info : infos) {
+					if (info.getVendor_id() == VENDOR_ID
+							&& info.getProduct_id() == PRODUCT_ID
+							&& info.getUsage() == 1) {
+						if (!temperaturSensors.contains(info)) {
+							temperaturSensors.add(info);
+						}
+					}
+				}
+			} catch (IOException ex) {
+				LOG.error(ex.getMessage(), ex);
+			} catch (Exception ex) {
+				LOG.error(ex.getMessage(), ex);
+			}
+
+			lastUpdateConnectedTemperatureSensorsTime = System
+					.currentTimeMillis();
+		}
+	}
+
+	/*****************************************************************/
+	// Temper1 get temperature with the fallback program: rpitemper.bin
+	// note: in some cases the temper1 is not working
+	/*****************************************************************/
+	private static void initRPiTemperBin() {
+		try {
+			String path = RPI_TEMPER_BIN;
+			InputStream in = ClassPathLibraryLoader.class
+					.getResourceAsStream(path);
+			if (in != null) {
+				try {
+					// always write to different location
+					String tempName = path.substring(path.lastIndexOf('/') + 1);
+					File fileOut = File.createTempFile(tempName.substring(0,
+							tempName.lastIndexOf('.')), tempName.substring(
+							tempName.lastIndexOf('.'), tempName.length()));
+					fileOut.deleteOnExit();
+
+					OutputStream out = new FileOutputStream(fileOut);
+					byte[] buf = new byte[1024];
+					int len;
+					while ((len = in.read(buf)) > 0) {
+						out.write(buf, 0, len);
+					}
+
+					out.close();
+					// Runtime.getRuntime().load(fileOut.toString());
+
+					// rpiTemperPath = tempName;
+					setRpiTemperPath(fileOut.getAbsolutePath());
+
+				} finally {
+					in.close();
+					setInitRPiTemperBinRunSuccessfull(true);
+				}
+			}
+			if (exceptionWasThrownInitRPiTemperBin) {
+				exceptionWasThrownInitRPiTemperBin = false;
+			}
+		} catch (IOException ex) {
+			long delta = System.currentTimeMillis()
+					- exceptionThrownInitRPiTemperBinLastTime;
+			if (!exceptionWasThrownInitRPiTemperBin
+					|| delta >= DELTA_THROWEXCEPTIONS) {
+				ex.printStackTrace();
+				exceptionThrownInitRPiTemperBinLastTime = System
+						.currentTimeMillis();
+				exceptionWasThrownInitRPiTemperBin = true;
+			}
+		}
+	}
+
+	// This method is a fallback for some raspberry pi models
+	private static synchronized float getTemperatureFromRPiTemperBinary(
+			int deviceNumber) throws Exception {
+		try {
+			final Process p = Runtime.getRuntime()
+					.exec(getRpiTemper1TempPath());
+			/*
+			 * new Thread(new Runnable() { public void run() { BufferedReader
+			 * input = new BufferedReader( new
+			 * InputStreamReader(p.getInputStream())); String line = null;
+			 * 
+			 * try { while ((line = input.readLine()) != null) { return
+			 * parseTemperature(line); } } catch (IOException e) {
+			 * e.printStackTrace(); } } }).start();
+			 */
+
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+			String line = null;
+
+			try {
+				int lineNumber = 0;
+				while ((line = input.readLine()) != null) {
+					if (lineNumber == deviceNumber) {
+						String temperature = parseTemperature(line);
+						Float temperatureFloat = Float.parseFloat(temperature);
+						return temperatureFloat.floatValue();
+					}
+				}
+			} catch (IOException e) {
+				System.out.println("11111");
+				LOG.error(e.getMessage(), e);
+			}
+			p.waitFor();
+		} catch (IOException e) {
+			System.out.println("22222");
+			LOG.error(e.getMessage(), e);
+		} catch (InterruptedException e) {
+			System.out.println("33333");
+			LOG.error(e.getMessage(), e);
+		} catch (Exception e) {
+			System.out.println("44444");
+			LOG.error(e.getMessage(), e);
+		}
+		throw new Exception("No temperature value available.");
+	}
+
+	private static String parseTemperature(String line) {
+		// Example: 2014/10/16 17:23:41 Temperature 71.71F 22.06C
+		Pattern pattern = Pattern.compile(".*([0-9]{2}\\.[0-9]{2})[C]$");
+
+		Matcher matcher = pattern.matcher(line);
+		if (matcher.matches()) {
+			return matcher.group(1);
+		} else {
+			throw new IllegalStateException("No match found");
+		}
+	}
+
 	public static String getRpiTemper1TempPath() {
-		if(!isInitRPiTemperBinRunSeccessfull()){
+		if (!isInitRPiTemperBinRunSuccessfull()) {
 			initRPiTemperBin();
 		}
-		
+
 		return rpiTemper1TempPath;
 	}
 
 	public static void setRpiTemperPath(String rpiTemper1TempPath) {
+		LOG.debug(RPI_TEMPER_BIN + " path:" + rpiTemper1TempPath);
 		Temper1TransportHandler.rpiTemper1TempPath = rpiTemper1TempPath;
-		
+
 		chmod("777", rpiTemper1TempPath, true);
 	}
 
-	private static void chmod(String mod, String rpiTemper1TempPath, boolean wait) {
+	private static void chmod(String mod, String rpiTemper1TempPath,
+			boolean wait) {
 		try {
-			Process p1 = Runtime.getRuntime().exec("chmod "+mod+" "+rpiTemper1TempPath);
-			if(wait){
+			Process p1 = Runtime.getRuntime().exec(
+					"chmod " + mod + " " + rpiTemper1TempPath);
+			if (wait) {
 				p1.waitFor();
 			}
 		} catch (IOException | InterruptedException e) {
 			long delta = System.currentTimeMillis() - lastExceptionThrown;
-			if(delta >= DELTA_THROWEXCEPTIONS){
+			if (delta >= DELTA_THROWEXCEPTIONS) {
 				e.printStackTrace();
 				lastExceptionThrown = System.currentTimeMillis();
 			}
 		}
 	}
-	
-	public static boolean isInitRPiTemperBinRunSeccessfull() {
-		return initRPiTemperBinRunSeccessfull;
+
+	public static boolean isInitRPiTemperBinRunSuccessfull() {
+		return initRPiTemperBinRunSuccessfull;
 	}
 
-	public static void setInitRPiTemperBinRunSeccessfull(
-			boolean initRPiTemperBinRunSeccessfull) {
-		Temper1TransportHandler.initRPiTemperBinRunSeccessfull = initRPiTemperBinRunSeccessfull;
+	public static void setInitRPiTemperBinRunSuccessfull(
+			boolean initRPiTemperBinRunSuccessfull) {
+		Temper1TransportHandler.initRPiTemperBinRunSuccessfull = initRPiTemperBinRunSuccessfull;
+	}
+
+	/*****************************************************************/
+	// get simulated temperature values
+	/*****************************************************************/
+	private static float getSimulatedTemperature() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date(System.currentTimeMillis()));
+		float seconds = calendar.get(Calendar.SECOND);
+
+		float y = 0;
+		if (seconds >= 30) {
+			y = ((seconds * -1) + 60) / 60; // 30...0
+		} else {
+			y = seconds / 60;// 0...30
+		}
+
+		return 10 + y * 30; // 10..15..20..25..20..15..10
 	}
 }
