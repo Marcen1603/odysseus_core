a16aedfb95f1c4765574fea5a5562ea0cdb0445c
Michael Brand <michael.brand@uni-oldenburg.de>
2014-10-16 13:47:39 +0000

    Core: Interface for stateful physical operators
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22035 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 00e62c8ad6b4980845818a697a1e99d1ca083076

---
+++ common/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/IStatefulPO.java
@@ -0,0 +1,12 @@
+package de.uniol.inf.is.odysseus.core.physicaloperator;
+
+import java.io.Serializable;
+
+/**
+ * Interface to specify if an physical operator is stateful.
+ *
+ */
+public interface IStatefulPO {
+	public Serializable getState();
+	public void setState(Serializable state);
+}
