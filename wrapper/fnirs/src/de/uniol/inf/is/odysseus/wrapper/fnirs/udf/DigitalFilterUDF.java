e8d2771459df807bf055bc9f33beb314e97468a3
Henrik Surm <hsurm@uni-oldenburg.de>
2014-10-24 13:47:29 +0000

    Changed filter implementation so it processes each attribute of a tuple
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22168 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 61a04dc3df4c7b9d5cc936c17293c02c7802541f

--- wrapper/fnirs/src/de/uniol/inf/is/odysseus/wrapper/fnirs/udf/DigitalFilterUDF.java
+++ wrapper/fnirs/src/de/uniol/inf/is/odysseus/wrapper/fnirs/udf/DigitalFilterUDF.java
@@ -11,11 +11,36 @@ import de.uniol.inf.is.odysseus.core.server.physicaloperator.IUserDefinedFunctio
  * 
  * @author Henrik Surm
  */
+
+class Filter
+{
+	// Order = 6, Sampling rate = 6.??Hz, Cutoff Freq = 0.08Hz
+	private final int 	 NZEROS = 6;
+	private final int 	 NPOLES = 6;
+	private final double GAIN   = 9.070781465e+06;
+
+	private final double[] xv = new double[NZEROS+1];
+	private final double[] yv = new double[NPOLES+1];
+
+	public double doFilter(double input)
+	{ 
+		xv[0] = xv[1]; xv[1] = xv[2]; xv[2] = xv[3]; xv[3] = xv[4]; xv[4] = xv[5]; xv[5] = xv[6]; 
+	    xv[6] = input / GAIN;
+	    yv[0] = yv[1]; yv[1] = yv[2]; yv[2] = yv[3]; yv[3] = yv[4]; yv[4] = yv[5]; yv[5] = yv[6]; 
+	    yv[6] =   (xv[0] + xv[6]) + 6 * (xv[1] + xv[5]) + 15 * (xv[2] + xv[4])
+	    		+ 20 * xv[3]
+	    		+ ( -0.5710336809 * yv[0]) + (  3.7457469236 * yv[1])
+	    		+ (-10.2538608560 * yv[2]) + ( 14.9952062960 * yv[3])
+	            + (-12.3564755620 * yv[4]) + (  5.4404098241 * yv[5]);
+	    return yv[6];
+	}	
+}
+
 @UserDefinedFunction(name = "DigitalFilter")
 public class DigitalFilterUDF implements IUserDefinedFunction<Tuple<? extends IMetaAttribute>, Tuple<? extends IMetaAttribute>> 
 {
 	private static final long serialVersionUID = 4077769822100028025L;
-	private int pos;
+	private Filter[] filters;
 	
 	public DigitalFilterUDF() 
 	{		 
@@ -27,36 +52,47 @@ public class DigitalFilterUDF implements IUserDefinedFunction<Tuple<? extends IM
 	@Override
 	public void init(String initString) 
 	{
-		if (initString != null)
-			pos = Integer.parseInt(initString);
+		int numFilters = Integer.parseInt(initString);
+		filters = new Filter[numFilters];
+		
+		for (int i=0;i<numFilters;i++)
+			filters[i] = new Filter();
 	}
 	
-	private final int 	 NZEROS = 4;
-	private final int 	 NPOLES = 4;
-	private final double GAIN   = 1.177840024e+00;
+	// Order = 4, Sampling rate = 100Hz, Cutoff Freq = 10Hz
+/*	private final int 	 NZEROS = 8;
+	private final int 	 NPOLES = 8;
+	private final double GAIN   = 4.173684699e+04;
 
 	private final double[] xv = new double[NZEROS+1];
 	private final double[] yv = new double[NPOLES+1];
 
 	private double doFilter(double input)
 	{ 
-		xv[0] = xv[1]; xv[1] = xv[2]; xv[2] = xv[3]; xv[3] = xv[4]; 
-		xv[4] = input / GAIN;
-		yv[0] = yv[1]; yv[1] = yv[2]; yv[2] = yv[3]; yv[3] = yv[4]; 
-		yv[4] =   (xv[0] + xv[4]) + 4 * (xv[1] + xv[3]) + 6 * xv[2]
-	                     + ( -0.7208209234 * yv[0]) + ( -3.1190000182 * yv[1])
-	                     + ( -5.0713777302 * yv[2]) + ( -3.6729890606 * yv[3]);
-		return yv[4];
-	}	
+		xv[0] = xv[1]; xv[1] = xv[2]; xv[2] = xv[3]; xv[3] = xv[4]; xv[4] = xv[5]; xv[5] = xv[6]; xv[6] = xv[7]; xv[7] = xv[8]; 
+        xv[8] = input / GAIN;
+        yv[0] = yv[1]; yv[1] = yv[2]; yv[2] = yv[3]; yv[3] = yv[4]; yv[4] = yv[5]; yv[5] = yv[6]; yv[6] = yv[7]; yv[7] = yv[8]; 
+        yv[8] =   (xv[0] + xv[8]) + 8 * (xv[1] + xv[7]) + 28 * (xv[2] + xv[6])
+                     + 56 * (xv[3] + xv[5]) + 70 * xv[4]
+                     + ( -0.0372001007 * yv[0]) + (  0.4172171570 * yv[1])
+                     + ( -2.0792738030 * yv[2]) + (  6.0252603973 * yv[3])
+                     + (-11.1293310390 * yv[4]) + ( 13.4577198900 * yv[5])
+                     + (-10.4450410660 * yv[6]) + (  4.7845148950 * yv[7]);
+        return yv[8];
+	}*/	
+
 	
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override public Tuple<? extends IMetaAttribute> process(Tuple<? extends IMetaAttribute> in, int port) 
 	{
-		double result = doFilter((Double)in.getAttribute(pos));
-		
-		in.setAttribute(pos, result);
+		int num = in.getAttributes().length;
+		for (int i=0;i<num;i++)
+		{
+			double result = filters[i].doFilter((Double)in.getAttribute(i));
+			in.setAttribute(i, result);
+		}
 		
 		return in;
 	}
