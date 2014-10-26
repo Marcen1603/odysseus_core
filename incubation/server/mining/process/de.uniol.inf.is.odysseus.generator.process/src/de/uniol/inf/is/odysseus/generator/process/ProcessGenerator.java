bd186f8430c7cf36bbee699e971edcc6728b6a61
Phillip Geers <pgeers@uni-oldenburg.de>
2014-10-26 08:27:44 +0000

    changed process generator
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22170 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9c8450c13d420f2b3fd982d84aa096e057905de0

--- incubation/server/mining/process/de.uniol.inf.is.odysseus.generator.process/src/de/uniol/inf/is/odysseus/generator/process/ProcessGenerator.java
+++ incubation/server/mining/process/de.uniol.inf.is.odysseus.generator.process/src/de/uniol/inf/is/odysseus/generator/process/ProcessGenerator.java
@@ -8,47 +8,43 @@ import com.google.common.collect.Lists;
 import de.uniol.inf.is.odysseus.generator.AbstractDataGenerator;
 import de.uniol.inf.is.odysseus.generator.DataTuple;
 import de.uniol.inf.is.odysseus.generator.IDataGenerator;
-import de.uniol.inf.is.odysseus.generator.process.util.CSVLogParser;
+import de.uniol.inf.is.odysseus.generator.process.util.XESLogDataImporter;
 
 public class ProcessGenerator extends AbstractDataGenerator{
 	
-	List<String> csvLines = Lists.newArrayList();
-	int lineCounter = 1;
+	List<DataTuple> eventTuples = Lists.newArrayList();
+	int elementCounter = 0;
 	
 	@Override
 	public List<DataTuple> next() throws InterruptedException {
-		DataTuple tuple = new DataTuple();
-		String[] actualLine = csvLines.get(lineCounter-1).split(",");
-		lineCounter++;
-		// Case id
-		tuple.addInteger(actualLine[0]);
-		// Activity name
-		tuple.addString(actualLine[3]);		
+
 	
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(1);
 		} catch (InterruptedException e) {		
 			e.printStackTrace();
 		}
 				
-		List<DataTuple> list = new ArrayList<DataTuple>();
-		list.add(tuple);		
-		if((csvLines.size()%lineCounter) == 0){
-			lineCounter = 1;
+		if(elementCounter%eventTuples.size() == 0 && elementCounter != 0){
+			elementCounter = 0;
 		}
+		List<DataTuple> list = new ArrayList<DataTuple>();
+		list.add(eventTuples.get(elementCounter));		
+		elementCounter++;
+
 		return list;
 	}
 
 	@Override
 	public void close() {
-		csvLines.clear();
-		lineCounter = 1;
+		eventTuples.clear();
+		elementCounter = 0;
 		System.out.println("Close Process Generator ...");
 	}
 
 	@Override
 	protected void process_init() {
-		csvLines = CSVLogParser.getCSVLines();
+		eventTuples = XESLogDataImporter.getEvents();
 		System.out.println("Init Process Generator ...");
 	}
 
---
+++ incubation/server/mining/process/de.uniol.inf.is.odysseus.generator.process/src/de/uniol/inf/is/odysseus/generator/process/util/XESLogDataImporter.java
@@ -0,0 +1,113 @@
+package de.uniol.inf.is.odysseus.generator.process.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.generator.DataTuple;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+public class XESLogDataImporter {
+	
+	public final static String TRACE = "trace";
+	public final static String STRING = "string";
+	public final static String VALUE = "value";
+	public final static String EVENT = "event";
+	private final static String FILEPATH = "/home/phil/Documents/eventlogs/Chapter_7/Lfull.xes";
+	private static Map<String,Integer> traceCounter = new HashMap();
+	
+	
+	public static List<DataTuple> getEvents(){
+		List<DataTuple> eventTuples = Lists.newArrayList();
+		
+		File document = new File(FILEPATH);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
+
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(document);
+			doc.getDocumentElement().normalize();		 
+
+			NodeList nlist = doc.getDocumentElement().getElementsByTagName(TRACE);
+	        for (int i = 0; i < nlist.getLength(); i++){
+	        	// Trace Elemente
+	        	StringBuilder keyBuilder = new StringBuilder();
+	        	Element element = (Element)nlist.item(i);
+	        	
+	        	Element caseID = (Element)element.getElementsByTagName(STRING).item(0);
+	        	String caze =null;
+	        	if(caseID != null) {
+	        		caze = caseID.getAttribute(VALUE);
+	        	}
+	        	keyBuilder.append(caze);
+	        	
+	        	NodeList eventTags = element.getElementsByTagName(EVENT);
+	        	keyBuilder.append("[");
+	        	for(int j =0; j< eventTags.getLength(); j++){
+	        		
+	        		Element eventTag =((Element)eventTags.item(j));
+	        		
+	        		NodeList eventData = eventTag.getElementsByTagName(STRING);
+	        		for(int k =0; k<eventData.getLength();k++){
+	        			Element dataElement = ((Element)eventData.item(k));
+	        			if(dataElement.getAttribute("key").equals("concept:name")){
+	        				// Aktivitätsname des Events
+	        				keyBuilder.append(dataElement.getAttribute(VALUE));
+	        				eventTuples.add(createTuple(caze, dataElement.getAttribute(VALUE)));
+	        			}
+	        		}   		
+	        	}
+	        	keyBuilder.append("]");
+	        	incMap(keyBuilder.toString());
+
+	        } 
+	        
+	        for(Map.Entry e : traceCounter.entrySet()){
+	        	System.out.println(e.getKey()+ ": "+e.getValue());
+	        }
+			 
+				 
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ImmutableList.copyOf(eventTuples);
+	}
+
+	private static void incMap(String key){
+		int freq=0;
+		if(traceCounter.containsKey(key)){
+			freq = traceCounter.get(key).intValue();
+			traceCounter.remove(key);
+			traceCounter.put(key, freq+1);	
+		} else {
+			traceCounter.put(key, 1);	
+		}		
+	}
+	
+	private static DataTuple createTuple(String caseID,String activityname){
+		DataTuple tuple = new DataTuple();
+		tuple.addString(caseID);
+		tuple.addString(activityname);
+		return tuple;
+	}
+
+}
