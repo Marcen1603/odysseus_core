fca8c0c2e6393e4953dd71249fa4b950940a9a31
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-10-30 09:25:32 +0000

    Reset sysout and syserr when shutting down
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22248 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0b4f7961c4a5028b6ae4ed045e71310f0e44956e

--- client/studio/rcp.console/src/de/uniol/inf/is/odysseus/rcp/console/Activator.java
+++ client/studio/rcp.console/src/de/uniol/inf/is/odysseus/rcp/console/Activator.java
@@ -1,5 +1,7 @@
 package de.uniol.inf.is.odysseus.rcp.console;
 
+import java.io.PrintStream;
+
 import org.apache.log4j.Logger;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -12,6 +14,9 @@ public class Activator implements BundleActivator {
 		return context;
 	}
 
+	private PrintStream err;
+	private PrintStream out;
+
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
@@ -20,11 +25,15 @@ public class Activator implements BundleActivator {
 		oc.createConsole();
 		// add it to log4j
 		Logger.getRootLogger().addAppender(oc);
+		err = System.err;
+		out = System.out;
 	}
 
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+		System.setErr(err);
+		System.setOut(out);
 	}
 
 }
