21a020cc56026a6c8e2431b8dee968188b9a02e7
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-11-03 14:23:30 +0000

    AggregateAO: Removed unused import
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22302 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5c6ddfbf884a6188c7f51e724c4b137c8a409c09

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/AggregateAO.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/AggregateAO.java
@@ -17,7 +17,6 @@ package de.uniol.inf.is.odysseus.core.server.logicaloperator;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
