3eaf05f00459f43eb3216a4597a150ea4944f7f0
Andre Albrecht <aalbrecht@uni-oldenburg.de>
2014-11-12 20:07:35 +0000

    missing file added.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22428 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 34c854d339ae57216eaec84bfc2686f17dde2b2b

---
+++ peer/server/de.uniol.inf.is.odysseus.peer.smarthome.contextawareness.server/src/de/uniol/inf/is/odysseus/peer/smarthome/contextawareness/parser/keyword/SmartDeviceAddSensorNamePreParserKeyword.java
@@ -0,0 +1,100 @@
+package de.uniol.inf.is.odysseus.peer.smarthome.contextawareness.parser.keyword;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.core.collection.Context;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.command.IExecutorCommand;
+import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
+import de.uniol.inf.is.odysseus.peer.smarthome.contextawareness.fielddevice.Sensor;
+import de.uniol.inf.is.odysseus.peer.smarthome.contextawareness.server.SmartDeviceServer;
+import de.uniol.inf.is.odysseus.peer.smarthome.contextawareness.server.SmartHomeServerPlugIn;
+import de.uniol.inf.is.odysseus.script.parser.AbstractPreParserKeyword;
+import de.uniol.inf.is.odysseus.script.parser.OdysseusScriptException;
+
+public class SmartDeviceAddSensorNamePreParserKeyword extends
+		AbstractPreParserKeyword {
+
+	public static final String KEYWORD = "SMARTDEVICE_ADD_SENSOR_NAME";
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(SmartHomeServerPlugIn.class);
+
+	@Override
+	public void validate(Map<String, Object> variables, String parameter,
+			ISession caller, Context context) throws OdysseusScriptException {
+		if (parameter.length() == 0)
+			throw new OdysseusScriptException("Parameter needed for #"
+					+ KEYWORD);
+
+		Object sensorRawSourceName = variables
+				.get(SmartDeviceSensorRawSourceName.KEYWORD);
+		
+		
+		if (sensorRawSourceName == null
+				|| !(sensorRawSourceName instanceof String)
+				|| sensorRawSourceName.equals("")) {
+			throw new OdysseusScriptException("#"+SmartDeviceSensorRawSourceName.KEYWORD+" needed for the sensor.");
+		}
+		
+		Object sensorQueryName = variables
+				.get("QNAME");
+		if (sensorQueryName == null
+				|| !(sensorQueryName instanceof String)
+				|| sensorQueryName.equals("")) {
+			throw new OdysseusScriptException("#QNAME needed for the sensor.");
+		}
+
+		String[] splitted = parameter.trim().split(" ");
+		@SuppressWarnings("unused")
+		List<String> parameters = generateParameterList(splitted);
+
+		variables.put(KEYWORD, parameter);
+	}
+
+	private List<String> generateParameterList(String[] splitted) {
+		ArrayList<String> list = new ArrayList<>();
+
+		for (int i = 0; i < splitted.length; i++) {
+			list.add(splitted[i]);
+		}
+
+		return list;
+	}
+
+	@Override
+	public List<IExecutorCommand> execute(Map<String, Object> variables,
+			String parameter, ISession caller, Context context)
+			throws OdysseusScriptException {
+		variables.put(KEYWORD, parameter);
+
+		String sensorName = parameter;
+		final String sensorRawSourceName = (String) variables.get(SmartDeviceSensorRawSourceName.KEYWORD);
+		String sensorQueryName = (String) variables.get("QNAME");
+		
+		LOG.debug("SensorName:" + sensorName);
+		LOG.debug("SensorRawSourceName:"+sensorRawSourceName);
+		LOG.debug("SensorQueryName:"+sensorQueryName);
+		
+		Sensor sensor = new Sensor(parameter, "", "") {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public Map<String, String> getQueriesForRawValues() {
+				Map<String,String> map = new HashMap<String, String>();
+				
+				map.put(sensorRawSourceName, "");
+				
+				return map;
+			}
+		};
+		SmartDeviceServer.getInstance().getLocalSmartDevice().addConnectedFieldDevice(sensor);
+		
+		return null;
+	}
+
+}
