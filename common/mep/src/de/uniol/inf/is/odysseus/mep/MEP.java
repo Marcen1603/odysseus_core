0a5c8895a20cfc9e83f22e7cf9271f0c92a3bbd6
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-11-14 11:06:10 +0000

    Moved Method to searchBundleForMepFunctions to MEP --> other bundles can use this function
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22451 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 84b043aae5f11d0128de30544165f5e610584ee0

--- common/mep/src/de/uniol/inf/is/odysseus/mep/MEP.java
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/MEP.java
@@ -16,11 +16,15 @@
 package de.uniol.inf.is.odysseus.mep;
 
 import java.io.StringReader;
+import java.lang.reflect.Modifier;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.osgi.framework.Bundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -468,4 +472,53 @@ public class MEP implements IExpressionParser {
 	public static ImmutableSet<FunctionSignature> getFunctions() {
 		return functionStore.getSignatures();
 	}
+	
+	
+	public static void searchBundleForMepFunctions(Bundle bundle) {
+		Enumeration<URL> entries = bundle.findEntries("/bin/", "*.class", true);
+		// collect logical operators and register parameters first
+		// add logical operators afterwards, because they may need the newly
+		// registered parameters
+		if (entries == null) {
+			entries = bundle.findEntries("/", "*.class", true);
+			if (entries == null) {
+				return;
+			}
+		}
+		while (entries.hasMoreElements()) {
+			URL curURL = entries.nextElement();
+			Class<?> classObject = loadClass(bundle, curURL);
+			if (IFunction.class.isAssignableFrom(classObject)
+					&& !Modifier.isAbstract(classObject.getModifiers())) {
+				try {
+					MEP.registerFunction((IFunction<?>) classObject
+							.newInstance());
+				} catch (InstantiationException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	public static Class<?> loadClass(Bundle bundle, URL curURL) {
+		String file = curURL.getFile();
+		int start = 1;
+		String className = "";
+		try {
+			if (file.startsWith("/bin/")) {
+				start = "/bin/".length();
+			}
+			// remove potential '/bin' and 'class' and change path to package
+			// name
+			className = file.substring(start, file.length() - 6).replace('/',
+					'.');
+			Class<?> classObject = bundle.loadClass(className);
+			return classObject;
+		} catch (Exception e) {
+
+		}
+		return null;
+	}
+
 }
