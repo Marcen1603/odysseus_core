b2ebbe9ddb76f9c7f7c5b4cd2e76d5763ce5baa8
Michael Brand <michael.brand@uni-oldenburg.de>
2014-11-18 09:29:16 +0000

    removed warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22498 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f26c97abc679688e2c5e51565886ac0dea5fdb78

--- peer/server/de.uniol.inf.is.odysseus.peer.distribute.modify.fragmentation.horizontal.hash/src/de/uniol/inf/is/odysseus/peer/distribute/modify/fragmentation/horizontal/hash/physicaloperator/HashFragmentPO.java
+++ peer/server/de.uniol.inf.is.odysseus.peer.distribute.modify.fragmentation.horizontal.hash/src/de/uniol/inf/is/odysseus/peer/distribute/modify/fragmentation/horizontal/hash/physicaloperator/HashFragmentPO.java
@@ -1,18 +1,9 @@
 package de.uniol.inf.is.odysseus.peer.distribute.modify.fragmentation.horizontal.hash.physicaloperator;
 
-import java.util.List;
-
-import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
-
-import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
-import de.uniol.inf.is.odysseus.core.metadata.IStreamable;
-import de.uniol.inf.is.odysseus.core.physicaloperator.IPunctuation;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe;
-import de.uniol.inf.is.odysseus.core.server.planmanagement.QueryParseException;
 import de.uniol.inf.is.odysseus.peer.distribute.modify.fragmentation.horizontal.hash.logicaloperator.HashFragmentAO;
 import de.uniol.inf.is.odysseus.peer.distribute.modify.fragmentation.horizontal.physicaloperator.AbstractFragmentPO;
 
@@ -60,10 +51,12 @@ public class HashFragmentPO<T extends IStreamObject<IMetaAttribute>> extends
 					throw new IllegalArgumentException("Attribute "+restrictAttribute+" not found in input schema");
 				}
 			}
-		}else{
-			indices = null;
-		}
 			
+		} else{
+			
+			indices = null;
+			
+		}
 			
 	}
 
@@ -72,6 +65,7 @@ public class HashFragmentPO<T extends IStreamObject<IMetaAttribute>> extends
 	public AbstractPipe<T, T> clone() {
 
 		throw new IllegalArgumentException("Currently not implemented");
+		
 	}
 
 	@Override
@@ -80,9 +74,13 @@ public class HashFragmentPO<T extends IStreamObject<IMetaAttribute>> extends
 		final int hashCode;
 
 		if (this.isPartialKey()) {
+			
 			hashCode = Math.abs(object.restrictedHashCode(indices));
+			
 		} else{
+			
 			hashCode = Math.abs(object.hashCode());
+			
 		}
 		
 		return hashCode % this.numFragments;
