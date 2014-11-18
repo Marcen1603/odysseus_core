72535e80ad5c609734dceec05184b107b4e42a21
Tobias Leo Brandt <tobias.leo.brandt@uni-oldenburg.de>
2014-11-18 19:44:17 +0000

    Forgot new java file ... can anyone remove all these class files?
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22534 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1dc7feb65fc314ba00d6069ac57e0f36826e37ba

---
+++ peer/server/de.uniol.inf.is.odysseus.peer.recovery/src/de/uniol/inf/is/odysseus/peer/recovery/protocol/RecoveryBackupMessageHandler.java
@@ -0,0 +1,66 @@
+package de.uniol.inf.is.odysseus.peer.recovery.protocol;
+
+import net.jxta.peer.PeerID;
+import de.uniol.inf.is.odysseus.peer.recovery.messages.BackupInformationMessage;
+import de.uniol.inf.is.odysseus.peer.recovery.util.LocalBackupInformationAccess;
+
+/**
+ * This class handles incoming BackupInformationMessages. It can save new information or update existing information.
+ * 
+ * @author Tobias Brandt
+ *
+ */
+public class RecoveryBackupMessageHandler {
+
+	/**
+	 * Handles an incoming BackupInformationMessage, e.g. to save new information.
+	 * 
+	 * @param sender
+	 *            Sender of this message
+	 * @param backupInformationMessage
+	 *            The informationMessage to handle
+	 */
+	public static void handleInstruction(PeerID sender, BackupInformationMessage backupInformationMessage) {
+		int messageType = backupInformationMessage.getMessageType();
+
+		switch (messageType) {
+		case BackupInformationMessage.NEW_INFO:
+			handleNewInformationMessage(backupInformationMessage);
+			break;
+		case BackupInformationMessage.UPDATE_INFO:
+			handleUpdateInformationMessage(sender, backupInformationMessage);
+			break;
+		}
+	}
+
+	/**
+	 * Handles new information -> Just saves it
+	 * 
+	 * @param backupInformationMessage
+	 *            The backup-information to handle
+	 */
+	private static void handleNewInformationMessage(BackupInformationMessage backupInformationMessage) {
+		LocalBackupInformationAccess.getStore().add(backupInformationMessage.getInfo());
+	}
+
+	/**
+	 * Handles updated information. Checks, if we already have information about this peer with this shared query id and
+	 * if so, updates this information
+	 * 
+	 * @param sender
+	 *            Sender of this message
+	 * @param backupInformationMessage
+	 *            The informationMessage to handle
+	 */
+	private static void handleUpdateInformationMessage(PeerID sender, BackupInformationMessage backupInformationMessage) {
+		// Just save the info, if we already have information about this
+		if (LocalBackupInformationAccess.getStoredPQLStatements(backupInformationMessage.getInfo().getSharedQuery(),
+				sender) != null) {
+
+			LocalBackupInformationAccess.updateInfo(backupInformationMessage.getInfo().getSharedQuery(),
+					backupInformationMessage.getInfo().getAboutPeer(), backupInformationMessage.getInfo().getPQL(),
+					backupInformationMessage.getInfo().getLocationPeer(), backupInformationMessage.getInfo()
+							.getLocalPQL());
+		}
+	}
+}
