a528f2b8cf338dfc3cb35744b731bb6241d6eaa5
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-12-02 14:23:26 +0000

    Removed Warnings, removed typo
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22766 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1e725967e17f484a7e6db79ee6d8cb5a28ad4951

--- client/dashboard/rcp.dashboard.part.graphics/src/de/uniol/inf/is/odysseus/rcp/dashboard/part/graphics/figure/MultipleImagesPictogramFigure.java
+++ client/dashboard/rcp.dashboard.part.graphics/src/de/uniol/inf/is/odysseus/rcp/dashboard/part/graphics/figure/MultipleImagesPictogramFigure.java
@@ -15,8 +15,6 @@
  ******************************************************************************/
 package de.uniol.inf.is.odysseus.rcp.dashboard.part.graphics.figure;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/PredicateWindowAO.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/PredicateWindowAO.java
@@ -7,7 +7,7 @@ import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.Paramete
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.BooleanParameter;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.PredicateParameter;
 
-@LogicalOperator(minInputPorts = 1, maxInputPorts = 1, name = "PREDICATEWINDOW", category = { LogicalOperatorCategory.BASE }, doc = "This is an predicated based window, set start and end condition with predicates.", url = "http://odysseus.offis.uni-oldenburg.de:8090/display/ODYSSEUS/PredicateWindow", hidden = true)
+@LogicalOperator(minInputPorts = 1, maxInputPorts = 1, name = "PREDICATEWINDOW", category = { LogicalOperatorCategory.BASE }, doc = "This is a predicated based window, set start and end condition with predicates.", url = "http://odysseus.offis.uni-oldenburg.de:8090/display/ODYSSEUS/PredicateWindow", hidden = true)
 public class PredicateWindowAO extends AbstractWindowAO {
 
 	private static final long serialVersionUID = 8834015972527486443L;
