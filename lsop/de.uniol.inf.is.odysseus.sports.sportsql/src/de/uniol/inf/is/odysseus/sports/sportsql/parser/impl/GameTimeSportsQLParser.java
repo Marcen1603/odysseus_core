eb5c3f1d84243e7513502f3726da673e0a276d1c
Tobias Leo Brandt <tobias.leo.brandt@uni-oldenburg.de>
2014-12-04 19:34:38 +0000

    LSOP-304 Moved static values into variables
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22796 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3ae1ad03c66fe5bde0168da6a32fbcb2684d8889

--- lsop/de.uniol.inf.is.odysseus.sports.sportsql/src/de/uniol/inf/is/odysseus/sports/sportsql/parser/impl/GameTimeSportsQLParser.java
+++ lsop/de.uniol.inf.is.odysseus.sports.sportsql/src/de/uniol/inf/is/odysseus/sports/sportsql/parser/impl/GameTimeSportsQLParser.java
@@ -34,11 +34,7 @@ import de.uniol.inf.is.odysseus.sports.sportsql.parser.parameter.SportsQLTimePar
  * 
  * Example Query:
  * 
- * 	{ 	
- * 		"statisticType": "global",
- * 		"gameType": "soccer", 
- * 		"name": "gameTime"
- * 	}
+ * { "statisticType": "global", "gameType": "soccer", "name": "gameTime" }
  * 
  * @author Thore Stratmann
  *
@@ -47,12 +43,15 @@ import de.uniol.inf.is.odysseus.sports.sportsql.parser.parameter.SportsQLTimePar
 		@SportsQLParameter(name = "startts", parameterClass = SportsQLLongParameter.class, mandatory = false),
 		@SportsQLParameter(name = "endts", parameterClass = SportsQLLongParameter.class, mandatory = false),
 		@SportsQLParameter(name = "isSelectDataBetweenTimestamps", parameterClass = SportsQLBooleanParameter.class, mandatory = false),
-		@SportsQLParameter(name = "addMilliseconds", parameterClass = SportsQLBooleanParameter.class, mandatory = false),})
+		@SportsQLParameter(name = "addMilliseconds", parameterClass = SportsQLBooleanParameter.class, mandatory = false), })
 public class GameTimeSportsQLParser implements ISportsQLParser {
 
+	private static String COUNT_SECONDS_UP_TO = "60";
+	private static String COUNT_MILLISECONDS_UP_TO = "1000";
+
 	@Override
-	public ILogicalQuery parse(ISession session,SportsQLQuery sportsQL)
-			throws SportsQLParseException, MissingDDCEntryException {
+	public ILogicalQuery parse(ISession session, SportsQLQuery sportsQL) throws SportsQLParseException,
+			MissingDDCEntryException {
 
 		List<ILogicalOperator> allOperators = new ArrayList<ILogicalOperator>();
 
@@ -60,11 +59,11 @@ public class GameTimeSportsQLParser implements ISportsQLParser {
 
 		// 1. Only data stream elements between starttimestamp and endtimestamp
 		ILogicalOperator gameTimeSelect = null;
-		
+
 		if (isSelectDataBetweenTimestampsEnabled(sportsQL)) {
 			String startts = getStartTimestamp(sportsQL);
 			String endts = getEndTimestamp(sportsQL);
-			
+
 			List<String> gameTimeSelectPredicates = new ArrayList<String>();
 			gameTimeSelectPredicates.add(SoccerGameAttributes.TS + ">= " + startts);
 			gameTimeSelectPredicates.add(SoccerGameAttributes.TS + "<= " + endts);
@@ -77,62 +76,65 @@ public class GameTimeSportsQLParser implements ISportsQLParser {
 
 		// 3. Convert decimal minutes to minute, second and millisecond
 		List<SDFExpressionParameter> expressions = new ArrayList<SDFExpressionParameter>();
-		expressions.add(OperatorBuildHelper
-				.createExpressionParameter("DoubleToInteger(ts/"+TimeUnitHelper.getBTUtoMinutesFactor(TimeUnit.valueOf(AbstractSportsDDCAccess.getBasetimeunit().toLowerCase()))+")", OperatorBuildHelper.ATTRIBUTE_MINUTE, gameTimeSelect));
-		expressions.add(OperatorBuildHelper
-				.createExpressionParameter("DoubleToInteger((ts/"+TimeUnitHelper.getBTUtoSecondsFactor(TimeUnit.valueOf(AbstractSportsDDCAccess.getBasetimeunit().toLowerCase()))+") % 60)", OperatorBuildHelper.ATTRIBUTE_SECOND, gameTimeSelect));
-		if(addMilliseconds(sportsQL)) {
-			expressions.add(OperatorBuildHelper.
-					createExpressionParameter("DoubleToInteger((ts/"+TimeUnitHelper.getBTUtoMillisecondsFactor(TimeUnit.valueOf(AbstractSportsDDCAccess.getBasetimeunit().toLowerCase()))+") % 1000)", OperatorBuildHelper.ATTRIBUTE_MILLISECOND, gameTimeSelect));
+		expressions.add(OperatorBuildHelper.createExpressionParameter(
+				"DoubleToInteger(ts/"
+						+ TimeUnitHelper.getBTUtoMinutesFactor(TimeUnit.valueOf(AbstractSportsDDCAccess
+								.getBasetimeunit().toLowerCase())) + ")", OperatorBuildHelper.ATTRIBUTE_MINUTE,
+				gameTimeSelect));
+		expressions.add(OperatorBuildHelper.createExpressionParameter(
+				"DoubleToInteger((ts/"
+						+ TimeUnitHelper.getBTUtoSecondsFactor(TimeUnit.valueOf(AbstractSportsDDCAccess
+								.getBasetimeunit().toLowerCase())) + ") % " + COUNT_SECONDS_UP_TO + ")",
+				OperatorBuildHelper.ATTRIBUTE_SECOND, gameTimeSelect));
+		if (addMilliseconds(sportsQL)) {
+			expressions.add(OperatorBuildHelper.createExpressionParameter(
+					"DoubleToInteger((ts/"
+							+ TimeUnitHelper.getBTUtoMillisecondsFactor(TimeUnit.valueOf(AbstractSportsDDCAccess
+									.getBasetimeunit().toLowerCase())) + ") % " + COUNT_MILLISECONDS_UP_TO + ")",
+					OperatorBuildHelper.ATTRIBUTE_MILLISECOND, gameTimeSelect));
 		}
-		
-		MapAO timeMap = OperatorBuildHelper.createMapAO(expressions, gameTimeSelect,
-				0, 0, false);
+
+		MapAO timeMap = OperatorBuildHelper.createMapAO(expressions, gameTimeSelect, 0, 0, false);
 		timeMap.initialize();
 		allOperators.add(timeMap);
 
 		// 4. Remove duplicate data stream elements
 		ArrayList<String> gameTimeChangeDetectAttributes = new ArrayList<String>();
-		if(addMilliseconds(sportsQL)) {
+		if (addMilliseconds(sportsQL)) {
 			gameTimeChangeDetectAttributes.add(OperatorBuildHelper.ATTRIBUTE_MILLISECOND);
 		} else {
 			gameTimeChangeDetectAttributes.add(OperatorBuildHelper.ATTRIBUTE_SECOND);
 		}
-		
-		ChangeDetectAO gameTimeChangeDetect = OperatorBuildHelper
-				.createChangeDetectAO(OperatorBuildHelper.createAttributeList(
-						gameTimeChangeDetectAttributes, timeMap), 0,
-						timeMap);
+
+		ChangeDetectAO gameTimeChangeDetect = OperatorBuildHelper.createChangeDetectAO(
+				OperatorBuildHelper.createAttributeList(gameTimeChangeDetectAttributes, timeMap), 0, timeMap);
 		allOperators.add(gameTimeChangeDetect);
 
 		// 5. Finish
-		return OperatorBuildHelper.finishQuery(gameTimeChangeDetect,
-				allOperators, sportsQL.getDisplayName());
+		return OperatorBuildHelper.finishQuery(gameTimeChangeDetect, allOperators, sportsQL.getDisplayName());
 	}
 
 	private String getStartTimestamp(SportsQLQuery sportsQL) {
-		ISportsQLParameter starttsParameter = sportsQL.getParameters().get(
-				"startts");
+		ISportsQLParameter starttsParameter = sportsQL.getParameters().get("startts");
 		return ((SportsQLLongParameter) starttsParameter).getValue() + ".0";
 	}
 
 	private String getEndTimestamp(SportsQLQuery sportsQL) {
-		ISportsQLParameter starttsParameter = sportsQL.getParameters().get(
-				"endts");
+		ISportsQLParameter starttsParameter = sportsQL.getParameters().get("endts");
 		return ((SportsQLLongParameter) starttsParameter).getValue() + ".0";
 	}
-	
+
 	private boolean isSelectDataBetweenTimestampsEnabled(SportsQLQuery sportsQL) {
-		ISportsQLParameter selectDataBetweenTimestamps = sportsQL.getParameters().get("selectDataBetweenTimestamps");	
+		ISportsQLParameter selectDataBetweenTimestamps = sportsQL.getParameters().get("selectDataBetweenTimestamps");
 		if (selectDataBetweenTimestamps != null) {
 			return ((SportsQLBooleanParameter) selectDataBetweenTimestamps).getValue();
 		} else {
 			return false;
 		}
 	}
-	
+
 	private boolean addMilliseconds(SportsQLQuery sportsQL) {
-		ISportsQLParameter addMilliseconds = sportsQL.getParameters().get("addMilliseconds");	
+		ISportsQLParameter addMilliseconds = sportsQL.getParameters().get("addMilliseconds");
 		if (addMilliseconds != null) {
 			return ((SportsQLBooleanParameter) addMilliseconds).getValue();
 		} else {
