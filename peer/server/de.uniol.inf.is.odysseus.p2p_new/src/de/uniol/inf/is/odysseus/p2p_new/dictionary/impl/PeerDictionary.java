6dae50f8cc354f6da5c5ab19140259c3d8a147dc
Timo Michelsen <Timo.Michelsen@gmail.com>
2014-12-08 07:29:14 +0000

    Peer: Refactoring/CleanUp
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22808 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 79e1326be4081895498a2f6a7d336c0700fa695e

--- peer/server/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/dictionary/impl/PeerDictionary.java
+++ peer/server/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/dictionary/impl/PeerDictionary.java
@@ -33,65 +33,23 @@ import de.uniol.inf.is.odysseus.p2p_new.provider.JxtaServicesProvider;
 public class PeerDictionary implements IPeerDictionary, IAdvertisementDiscovererListener {
 
 	private static final Logger LOG = LoggerFactory.getLogger(PeerDictionary.class);
-
 	private static final String UNKNOWN_PEER_NAME = "<unknown>";
-
 	private static final Map<PeerAdvertisement, Long> toFlushMap = Maps.newConcurrentMap();
-
-	private static void tryFlushAdvertisement(Advertisement srcAdvertisement) {
-
-		try {
-
-			if (JxtaServicesProvider.isActivated()) {
-
-				JxtaServicesProvider.getInstance().flushAdvertisement(srcAdvertisement);
-
-			}
-
-		} catch (IOException e) {
-
-			LOG.error("Could not flush advertisement {}", srcAdvertisement, e);
-
-		}
-
-	}
-
-	private final List<IPeerDictionaryListener> listeners = Lists.newArrayList();
 
 	private static PeerDictionary instance;
 
+	private final List<IPeerDictionaryListener> listeners = Lists.newArrayList();
 	private final Map<PeerID, String> remotePeerNameMap = Maps.newHashMap();
-
 	private final Map<PeerID, String> remotePeerAddressMap = Maps.newHashMap();
 
 	private Collection<PeerID> currentPeerIDs = Lists.newArrayList();
 
-	private static PeerID toPeerID(String text) {
-
-		try {
-
-			final URI id = new URI(text);
-			return (PeerID) IDFactory.fromURI(id);
-
-		} catch (URISyntaxException | ClassCastException ex) {
-
-			LOG.error("Could not set id", ex);
-			return null;
-
-		}
-
-	}
-
 	public static PeerDictionary getInstance() {
-
 		return instance;
-
 	}
 
 	public static boolean isActivated() {
-
 		return instance != null;
-
 	}
 
 	private Collection<PeerID> toPeerIDs(Collection<PeerAdvertisement> peerAdvs) {
@@ -101,98 +59,71 @@ public class PeerDictionary implements IPeerDictionary, IAdvertisementDiscoverer
 
 		LOG.debug("Determining peers");
 		for (PeerAdvertisement adv : peerAdvs) {
-
 			if (!localPeerID.equals(adv.getPeerID())) {
-
 				if (JxtaServicesProvider.getInstance().isReachable(adv.getPeerID())) {
-
 					ids.add(adv.getPeerID());
 					LOG.debug("Peer " + adv.getName() + " is reachable");
 					toFlushMap.remove(adv);
-
 				} else {
-
 					LOG.debug("Peer " + adv.getName() + " is NOT reachable anymore");
 					remotePeerNameMap.remove(adv.getPeerID());
 					remotePeerAddressMap.remove(adv.getPeerID());
 					tryFlushAdvertisement(adv);
-
 				}
-
 			}
-
 		}
 
 		return ids;
 
 	}
 
+	private static void tryFlushAdvertisement(Advertisement srcAdvertisement) {
+		try {
+			if (JxtaServicesProvider.isActivated()) {
+				JxtaServicesProvider.getInstance().flushAdvertisement(srcAdvertisement);
+			}
+		} catch (IOException e) {
+			LOG.error("Could not flush advertisement {}", srcAdvertisement, e);
+		}
+	}
+
 	public void bindListener(IPeerDictionaryListener serv) {
-
 		addListener(serv);
-
 	}
 
 	public void unbindListener(IPeerDictionaryListener serv) {
-
 		removeListener(serv);
-
 	}
 
 	private void firePeerAddedEvent(PeerID peer) {
-
-		Preconditions.checkNotNull(peer);
-
 		for (IPeerDictionaryListener listener : this.listeners) {
-
 			try {
-
 				listener.peerAdded(peer);
-
 			} catch (Throwable t) {
-
 				LOG.error("Listener {} got an error", listener, t);
-
 			}
-
 		}
-
 	}
 
 	private void firePeerRemovedEvent(PeerID peer) {
-
-		Preconditions.checkNotNull(peer);
-
 		for (IPeerDictionaryListener listener : this.listeners) {
-
 			try {
-
 				listener.peerRemoved(peer);
-
 			} catch (Throwable t) {
-
 				LOG.error("Listener {} got an error", listener, t);
-
 			}
-
 		}
-
 	}
 
 	public void activate() {
-
-		instance = this;
-
 		Thread waitingThread = new Thread(new Runnable() {
-
 			@Override
 			public void run() {
-
 				P2PNetworkManager.waitFor();
 				P2PNetworkManager.getInstance().addAdvertisementListener(PeerDictionary.this);
-
+				
+				instance = PeerDictionary.this;
 			}
-
 		});
 
 		waitingThread.setDaemon(true);
@@ -202,66 +133,46 @@ public class PeerDictionary implements IPeerDictionary, IAdvertisementDiscoverer
 	}
 
 	public void deactivate() {
-
 		instance = null;
-
 	}
 
 	@Override
 	public void addListener(IPeerDictionaryListener listener) {
-
 		Preconditions.checkNotNull(listener, "Listener to add must not be null!");
 		synchronized (listeners) {
-
 			listeners.add(listener);
-
 		}
-
 	}
 
 	@Override
 	public void removeListener(IPeerDictionaryListener listener) {
-
 		Preconditions.checkNotNull(listener, "Listener to remove must not be null!");
 		synchronized (listeners) {
-
 			listeners.remove(listener);
-
 		}
-
 	}
 
 	@Override
 	public ImmutableCollection<PeerID> getRemotePeerIDs() {
-
 		return ImmutableList.copyOf(currentPeerIDs);
-
 	}
 
 	@Override
 	public String getRemotePeerName(PeerID peerID) {
-
 		Preconditions.checkNotNull(peerID, "PeerID to get the name from must not be null!");
 
 		if (peerID.equals(P2PNetworkManager.getInstance().getLocalPeerID())) {
-
 			return P2PNetworkManager.getInstance().getLocalPeerName();
-
 		}
 		if (remotePeerNameMap.containsKey(peerID)) {
-
 			return remotePeerNameMap.get(peerID);
-
 		}
 
 		Collection<PeerAdvertisement> peerAdvs = JxtaServicesProvider.getInstance().getPeerAdvertisements();
 		for (PeerAdvertisement peerAdv : peerAdvs) {
-
 			if (peerAdv.getPeerID().equals(peerID)) {
-
 				remotePeerNameMap.put(peerID, peerAdv.getName());
 				return peerAdv.getName();
-
 			}
 		}
 
@@ -270,56 +181,51 @@ public class PeerDictionary implements IPeerDictionary, IAdvertisementDiscoverer
 
 	@Override
 	public String getRemotePeerName(String peerIDString) {
-
 		Preconditions.checkArgument(!Strings.isNullOrEmpty(peerIDString), "PeerIDString must not be null or empty!");
 		return getRemotePeerName(toPeerID(peerIDString));
+	}
 
+	private static PeerID toPeerID(String text) {
+		try {
+			final URI id = new URI(text);
+			return (PeerID) IDFactory.fromURI(id);
+		} catch (URISyntaxException | ClassCastException ex) {
+			LOG.error("Could not set id", ex);
+			return null;
+		}
 	}
 
 	@Override
 	public Optional<String> getRemotePeerAddress(PeerID peerID) {
-
 		Preconditions.checkNotNull(peerID, "PeerID to get the address from must not be null!");
-
 		if (peerID.equals(P2PNetworkManager.getInstance().getLocalPeerID())) {
-
 			return Optional.of("127.0.0.1");
-
 		}
 
 		String address = remotePeerAddressMap.get(peerID);
 		if (Strings.isNullOrEmpty(address)) {
-
 			Optional<String> newAddress = JxtaServicesProvider.getInstance().getRemotePeerAddress(peerID);
 			if (newAddress.isPresent() && !newAddress.get().startsWith("0.0.0.0")) {
-
 				remotePeerAddressMap.put(peerID, newAddress.get());
 				return newAddress;
-
 			}
 
 			return Optional.absent();
-
 		}
 
 		return Optional.of(address);
-
 	}
 
 	@Override
 	public void advertisementDiscovered(Advertisement adv) {
-
 		// Nothing to do.
-
 	}
 
 	@Override
 	public void updateAdvertisements() {
-
 		Collection<PeerID> peers = toPeerIDs(JxtaServicesProvider.getInstance().getPeerAdvertisements());
-
 		Collection<PeerID> oldPeers = ImmutableSet.copyOf(currentPeerIDs);
-		
+
 		synchronized (currentPeerIDs) {
 			currentPeerIDs = peers;
 		}
