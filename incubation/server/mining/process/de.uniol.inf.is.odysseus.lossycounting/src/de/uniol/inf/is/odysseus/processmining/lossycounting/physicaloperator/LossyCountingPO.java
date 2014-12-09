18018e2e9773f9f226f9af83cba583e46efee83c
Phillip Geers <pgeers@uni-oldenburg.de>
2014-12-09 21:42:20 +0000

    lossy counter update
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22840 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8be5167ad591ee87d7f6dad0b0a234807bc5033b

--- incubation/server/mining/process/de.uniol.inf.is.odysseus.lossycounting/src/de/uniol/inf/is/odysseus/processmining/lossycounting/physicaloperator/LossyCountingPO.java
+++ incubation/server/mining/process/de.uniol.inf.is.odysseus.lossycounting/src/de/uniol/inf/is/odysseus/processmining/lossycounting/physicaloperator/LossyCountingPO.java
@@ -1,197 +1,251 @@
 package de.uniol.inf.is.odysseus.processmining.lossycounting.physicaloperator;
 
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
 
 import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe;
+import de.uniol.inf.is.odysseus.processmining.common.AbstractLCTuple;
+import de.uniol.inf.is.odysseus.processmining.common.CaseTuple;
+import de.uniol.inf.is.odysseus.processmining.common.DFRTuple;
+import de.uniol.inf.is.odysseus.processmining.common.DirectlyFollowLoopTuple;
+import de.uniol.inf.is.odysseus.processmining.common.InductiveMinerTransferTuple;
+import de.uniol.inf.is.odysseus.processmining.common.LCTupleType;
+import de.uniol.inf.is.odysseus.processmining.common.LCTuplesFactory;
 
-public class LossyCountingPO <T extends IMetaAttribute> extends AbstractPipe<Tuple<T>, Tuple<T>> {
+public class LossyCountingPO<T extends IMetaAttribute> extends
+		AbstractPipe<Tuple<T>, Tuple<T>> {
 
-		private final int TUPLE_FREQ_POS = 0;
-		private final int TUPLE_BUCKET_POS = 1;
-		private final int TUPLE_NAME_POS = 2;
-		private int error = 10; // corresponds to w
-		private HashMap<Object,Tuple<T>> activities = Maps.newHashMap();
-		private HashMap<Object,Tuple<T>> cases = Maps.newHashMap();
-		private HashMap<Object,Tuple<T>> relations = Maps.newHashMap();
-		private int iterations = 0; // corresponds to N
-		private int currentBucket = 1;
-		private String caseId;
-		private String activityName;
-		
-		
-		public LossyCountingPO() {
-			super();
-		}
-		
-		public LossyCountingPO(LossyCountingPO<T> lossyCountinPO) {
-			super(lossyCountinPO);
-			this.error = lossyCountinPO.error;
-			this.activities = lossyCountinPO.activities;
-			this.iterations = lossyCountinPO.iterations;
-			System.out.print("ERROR PO: "+this.error +"\n");
-			System.err
-			.println("The use of a copy constructor is only parially "
-					+ "supported in DBEnrichPO. The cacheManager will not be copied.");
-		}
-		
-		@Override
-		public OutputMode getOutputMode() {
-			return OutputMode.INPUT;
-		}
-	
-		@Override
-		public boolean isSemanticallyEqual(IPhysicalOperator ipo) {
-			if (this == ipo)
-				return true;
-			if (!super.equals(ipo))
-				return false;
-			if (getClass() != ipo.getClass())
-				return false;
-			return true;
-		}
+	private int error = 100; // corresponds to w
+	private HashMap<Object, AbstractLCTuple> activities = Maps.newHashMap();
+	private HashMap<Object, AbstractLCTuple> endActivities = Maps.newHashMap();
+	private HashMap<Object, AbstractLCTuple> cases = Maps.newHashMap();
+	private HashMap<Object, AbstractLCTuple> directlyFollowRelations = Maps
+			.newHashMap();
+	private Multimap<Object, AbstractLCTuple> directlyFollowRelationLoops = ArrayListMultimap
+			.create();
+	private HashMap<Object, AbstractLCTuple> shortLoops = Maps.newHashMap();
+	private HashMap<Object, AbstractLCTuple> starts = Maps.newHashMap();
+	private int iterations = 0; // corresponds to N
+	private int currentBucket = 1;
+	private String caseId;
+	private String activityName;
 
-		@Override
-		protected void process_next(Tuple<T> object, int port) {
-			if(object instanceof Tuple){
-				Object[] attributes = object.getAttributes();
-				caseId = (String) attributes[0];
-				activityName = (String)attributes[1];
-				System.out.println(caseId+";"+activityName);
-				updateData(activityName,activities); // Updates the activity map
-				//printInfo(activityName);
-				updateCases(caseId, activityName);
+	public LossyCountingPO() {
+		super();
+	}
 
+	public LossyCountingPO(LossyCountingPO<T> lossyCountinPO) {
+		super(lossyCountinPO);
+		this.error = lossyCountinPO.error;
+		this.activities = lossyCountinPO.activities;
+		this.iterations = lossyCountinPO.iterations;
+		System.out.print("ERROR PO: " + this.error + "\n");
+	}
+
+	@Override
+	protected void process_next(Tuple<T> object, int port) {
+		if (object instanceof Tuple) {
+			Object[] attributes = object.getAttributes();
+			// Values of the object Tuple
+			caseId = (String) attributes[0];
+			activityName = (String) attributes[1];
+
+			AbstractLCTuple aTuple = LCTuplesFactory.createActivityTuple(
+					activityName, currentBucket - 1);
+			AbstractLCTuple cTuple = LCTuplesFactory.createCaseTuple(caseId,
+					activityName, currentBucket - 1);
+			accumulate(aTuple, activities);
+			accumulate(cTuple, cases);
+
+			iterations++;
+			if (iterations % 1000 == 0) {
 				dataCleansing();
-				iterations++;
-				
-				if(iterations%20 == 0){
-					Tuple<T> mapTransfer = new Tuple<T>(1,true);
-					mapTransfer.setAttribute(0, this.relations);
-					transfer(mapTransfer);
-				}
+				InductiveMinerTransferTuple iMTransferTuple = new InductiveMinerTransferTuple();
+				iMTransferTuple
+						.setDirectlyFollowRelations(directlyFollowRelations);
+				iMTransferTuple.setStartActivites(starts);
+				iMTransferTuple.setShortLoops(shortLoops);
+				System.out.println("Transfering...");
+				calculateEndActivities();
+				iMTransferTuple.setEndActivities(endActivities);
+				transfer(iMTransferTuple);
 			}
 		}
-		
-		
-		private void updateData(Object attribute,HashMap<Object,Tuple<T>> map ){
-			if(map.containsKey(attribute)){
-				incrementFrequenz(attribute,map);
-			} else {
-				addTupleToMap(attribute,map);
-			}
-		}
-		
-		private void updateCases(String caseId, String newActivityName){
-			if(cases.containsKey(caseId)){
-				//Erzeuge Relationsschlüssel
-				String relationKeyName = (String)cases.get(caseId).getAttribute(TUPLE_NAME_POS)+ "" +newActivityName;
+	}
 
-				incrementAndReplaceCaseData(caseId,newActivityName,cases);
-				//updates the frequenz counter of the relations Tuples
-				updateData(relationKeyName, relations);
-				
+	private void calculateEndActivities(){
+		
+		for(Object key : cases.keySet()){
 			
+			if(endActivities.containsKey(cases.get(key).getActivity())){
+				endActivities.get(cases.get(key).getActivity()).incrementFrequency();
 			} else {
-				addCaseToMap(caseId,newActivityName,cases);
+				endActivities.put(cases.get(key).getActivity(), LCTuplesFactory.createActivityTuple(cases.get(key).getActivity(),0));
 			}
-		}
-		
-		/**
-		 * 
-		 * @param key to get the needed Tuple from map
-		 * @param map  which contains the needed Tuple
-		 */
-		private void incrementFrequenz(Object key, HashMap<Object, Tuple<T>> map){
-			int freq = ((Integer)(map.get(key).getAttribute(0))).intValue();
-			Tuple<T> tuple = new Tuple<T>(2,true);
-			map.remove(key);
-			tuple.setAttribute(TUPLE_FREQ_POS,Integer.valueOf(freq+1));
-			tuple.setAttribute(TUPLE_BUCKET_POS,Integer.valueOf(currentBucket-1));
-			map.put(key, tuple);
-		}
-		
-		private void addTupleToMap(Object key,HashMap<Object,Tuple<T>> map){
-			Tuple<T> tuple = new Tuple<T>(2,true);
-			tuple.setAttribute(TUPLE_FREQ_POS,Integer.valueOf(1));
-			tuple.setAttribute(TUPLE_BUCKET_POS,Integer.valueOf(currentBucket-1));
-			map.put(key, tuple);
-		}
-		
-		private void incrementAndReplaceCaseData(Object key, String activityName, HashMap<Object, Tuple<T>> map){
-			int freq = ((Integer)(map.get(key).getAttribute(1))).intValue();
-			map.remove(key);
-			Tuple<T> tuple = new Tuple<T>(3,true);
-			tuple.setAttribute(TUPLE_FREQ_POS,Integer.valueOf(freq+1));
-			tuple.setAttribute(TUPLE_BUCKET_POS,Integer.valueOf(currentBucket-1));
-			tuple.setAttribute(TUPLE_NAME_POS,activityName);
-			map.put(key, tuple);
-		}
-		
-		private void addCaseToMap(Object key, String activityName,HashMap<Object,Tuple<T>> map){
-			Tuple<T> tuple = new Tuple<T>(3,true);
-			
-			tuple.setAttribute(TUPLE_FREQ_POS,Integer.valueOf(1));
-			tuple.setAttribute(TUPLE_BUCKET_POS,Integer.valueOf(currentBucket-1));
-			tuple.setAttribute(TUPLE_NAME_POS, activityName);
-			map.put(key, tuple);
 			
 		}
 		
-		private void dataCleansing(){
-			if((iterations%error) == 0){
-				activities = cleanMap(activities);
-				cases = cleanMap(cases);
-				relations = cleanMap(relations);
-				currentBucket++;
+	}
+	/**
+	 * Updates the tuples frequency or adds the tuple to the given map
+	 * 
+	 * @param key
+	 * @param map
+	 */
+	private void accumulate(AbstractLCTuple tuple,
+			HashMap<Object, AbstractLCTuple> tupleMap) {
+		if (tupleMap.containsKey(tuple.getIdentifier())) {
+			tupleMap.get(tuple.getIdentifier()).incrementFrequency();
+			if (tuple.getType().equals(LCTupleType.Case)) {
+				checkForDFGCreationAndLoopCheck((CaseTuple) tuple);
 			}
-		}
-		
-		private HashMap<Object,Tuple<T>> cleanMap(HashMap<Object,Tuple<T>> map){
-			HashMap<Object,Tuple<T>> newMap = Maps.newHashMap();
-			for(Map.Entry<Object,Tuple<T>> entry : map.entrySet()){
-				int actualFrequenz = ((Integer)entry.getValue().getAttribute(TUPLE_FREQ_POS)).intValue();
-				int actualBucket = ((Integer)entry.getValue().getAttribute(TUPLE_BUCKET_POS)).intValue();
-				if(!(actualFrequenz+actualBucket <= currentBucket)){
-					newMap.put(entry.getKey(), entry.getValue());
+		} else {
+			tupleMap.put(tuple.getIdentifier(), tuple);
+			// the first activity of a case is its start activity
+			if (tuple.getType().equals(LCTupleType.Case)) {
+				if (starts.containsKey(activityName)) {
+					starts.get(activityName).incrementFrequency();
 				} else {
-				//	System.out.println("DELETED: " + entry.getKey() +";Anzahl: "+entry.getValue().getAttribute(TUPLE_FREQ_POS)+"<="+currentBucket );
+					AbstractLCTuple newInstance = LCTuplesFactory.createActivityTuple(tuple.getActivity(), tuple.getMaxError());
+					starts.put(activityName, newInstance);
 				}
 			}
-			return newMap;
 		}
-		
-		@SuppressWarnings("unused")
-		private void printActivities(String name){
-			System.out.println("++++++++++++++ Activities ++++++++++++++");
-			System.out.println("Actual Activity: "+ name);
-			System.out.println("Map after Update: ");
-			for(Map.Entry<Object,Tuple<T>> entry : activities.entrySet()){
-				System.out.println(entry.getKey() + "; Anzahl: " + entry.getValue().getAttribute(0) +" Bucket: "+ entry.getValue().getAttribute(1));
+	}
+
+	/**
+	 * Checks if its needed to create a directly follow Relation and accumulates
+	 * it to the relation map Futhermore it changes the activityname of the
+	 * contained tuple of the case map by the new one
+	 * 
+	 * @param caseTuple
+	 */
+	private void checkForDFGCreationAndLoopCheck(CaseTuple caseTuple) {
+		// Create directly follow Relation and add or increment it
+		// temporary variables
+		AbstractLCTuple tempCase = cases.get(caseTuple.getIdentifier());
+		String activity = tempCase.getActivity();
+		String newerActivity = caseTuple.getActivity();
+
+		// create directly follow Relation and accumulate
+		AbstractLCTuple rTuple = LCTuplesFactory.createDFRTuple(activity,
+				newerActivity, currentBucket - 1);
+		accumulate(rTuple, directlyFollowRelations);
+
+		// check for loops of length 1 and add them to shortLoops if needed
+		if (directlyFollowRelationLoops.containsKey(((DFRTuple) rTuple)
+				.getFollowActivity())) {
+			String loopIdentifier = ((DFRTuple) rTuple).getFollowActivity();
+			Multimap<Object, AbstractLCTuple> tempMultiMap = ArrayListMultimap
+					.create(directlyFollowRelationLoops);
+
+			for (AbstractLCTuple loopTuple : tempMultiMap.get(loopIdentifier)) {
+				String loopStart = loopTuple.getActivity();
+				String loopEnd = ((DFRTuple) rTuple).getFollowActivity();
+				boolean hasSameMiddle = rTuple.getActivity().equals(
+						((DirectlyFollowLoopTuple) loopTuple)
+								.getFollowActivity()) ? true : false;
+				
+				if (loopStart.equals(loopEnd)
+						&& hasSameMiddle
+						&& ((DirectlyFollowLoopTuple) loopTuple).getCaseID()
+								.equals(caseTuple.getCaseID())) {
+
+					String loopBody = ((DFRTuple) rTuple).getFollowActivity()
+							+ rTuple.getActivity();
+					// Add a new DFRTuple representing a Loop
+					shortLoops.put(loopBody, LCTuplesFactory.createDFRTuple(
+							((DFRTuple) rTuple).getFollowActivity(),
+							rTuple.getActivity(), currentBucket - 1));
+					directlyFollowRelationLoops.remove(loopIdentifier,
+							loopTuple);
+				}
 			}
-			System.out.println("++++++++++++++ Activities ++++++++++++++");
+
+		} else {
+			// Add directly follow relation with the associated case id
+			DirectlyFollowLoopTuple loopTuple = (DirectlyFollowLoopTuple) LCTuplesFactory
+					.createDFRLoopTuple(caseId, rTuple.getActivity(),
+							((DFRTuple) rTuple).getFollowActivity(),
+							currentBucket - 1);
+			directlyFollowRelationLoops.put(activity, loopTuple);
 		}
-		
-		public void printRelations(){
-			//Test Output
-			System.out.println("++++++++++++++ Relationen wos++++++++++++++");
-			List<String> sortedList = Lists.newArrayList();
-			for(Map.Entry<Object,Tuple<T>> entry : relations.entrySet()){
-				//System.out.println(entry.getKey() + "; Anzahl: " + entry.getValue().getAttribute(0) +" Bucket: "+ entry.getValue().getAttribute(1));
-				sortedList.add((String) entry.getKey());
+		// Replace Activity
+		tempCase.setActivity(caseTuple.getActivity());
+	}
+
+	// Datacleaning methods
+	private void dataCleansing() {
+			activities = cleanMap(activities);
+			cases = cleanMap(cases);
+			directlyFollowRelations = cleanMap(directlyFollowRelations);
+			directlyFollowRelationLoops = cleanMap(directlyFollowRelationLoops);
+			starts = cleanMap(starts);
+			shortLoops = cleanMap(shortLoops);
+			currentBucket++;
+	}
+
+	
+	/**
+	 * Removes all infrequent tuple of map
+	 * @param map
+	 * @return
+	 */
+	private HashMap<Object, AbstractLCTuple> cleanMap(
+			HashMap<Object, AbstractLCTuple> map) {
+		HashMap<Object, AbstractLCTuple> newMap = Maps.newHashMap();
+		for (Map.Entry<Object, AbstractLCTuple> entry : map.entrySet()) {
+			int actualFrequenz = entry.getValue().getFrequency();
+			int actualBucket = entry.getValue().getMaxError();
+			if (!(actualFrequenz + actualBucket <= currentBucket)) {
+				newMap.put(entry.getKey(), entry.getValue());
 			}
-			Collections.sort(sortedList);
-			for(String str : sortedList){
-				System.out.println(str+ ":" +relations.get(str));	
-			}
-			System.out.println("++++++++++++++ Relationen wos ++++++++++++++");
 		}
+		return newMap;
+	}
+
+	/**
+	 * Removes all infrequent tuple of map
+	 * @param map
+	 * @return
+	 */
+	private Multimap<Object, AbstractLCTuple> cleanMap(
+			Multimap<Object, AbstractLCTuple> map) {
+		Multimap<Object, AbstractLCTuple> newMap = ArrayListMultimap.create();
+
+		for (Object key : map.keySet()) {
+			for (AbstractLCTuple tuple : map.get(key)) {
+				int actualFrequenz = tuple.getFrequency();
+				int actualBucket = tuple.getMaxError();
+				if (!(actualFrequenz + actualBucket <= currentBucket)) {
+					newMap.put(key, tuple);
+				}
+			}
+
+		}
+		return newMap;
+	}
+
+	@Override
+	public OutputMode getOutputMode() {
+		return OutputMode.NEW_ELEMENT;
+	}
+
+	@Override
+	public boolean isSemanticallyEqual(IPhysicalOperator ipo) {
+		if (this == ipo)
+			return true;
+		if (!super.equals(ipo))
+			return false;
+		if (getClass() != ipo.getClass())
+			return false;
+		return true;
+	}
 }
