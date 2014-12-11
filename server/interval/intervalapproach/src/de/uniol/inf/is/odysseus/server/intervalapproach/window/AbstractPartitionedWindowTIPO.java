80238712bf292d0156932a639804e04ae486ffcc
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2014-12-11 13:10:57 +0000

    FIX: ArrayOutOfBounds
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@22877 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e11821bb14361b0b0e013b9e247e98aa4d358265

--- server/interval/intervalapproach/src/de/uniol/inf/is/odysseus/server/intervalapproach/window/AbstractPartitionedWindowTIPO.java
+++ server/interval/intervalapproach/src/de/uniol/inf/is/odysseus/server/intervalapproach/window/AbstractPartitionedWindowTIPO.java
@@ -23,22 +23,24 @@ import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.IGroupPro
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.NoGroupProcessor;
 import de.uniol.inf.is.odysseus.server.intervalapproach.window.state.SlidingElementWindowTIPOState;
 
-abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITimeInterval>> extends AbstractWindowTIPO<T> implements IStatefulPO{
+abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITimeInterval>>
+		extends AbstractWindowTIPO<T> implements IStatefulPO {
 
-	static final Logger LOG = LoggerFactory.getLogger(AbstractPartitionedWindowTIPO.class);
-	
+	static final Logger LOG = LoggerFactory
+			.getLogger(AbstractPartitionedWindowTIPO.class);
+
 	private IGroupProcessor<T, T> groupProcessor = new NoGroupProcessor<T, T>();
 
 	protected ITransferArea<T, T> transferArea = new TITransferArea<>();
 
 	private Map<Long, List<T>> buffers = new HashMap<>();
-	
+
 	private PointInTime lastTs = null;
 
 	public AbstractPartitionedWindowTIPO(AbstractWindowAO ao) {
 		super(ao);
 	}
-	
+
 	@Override
 	public OutputMode getOutputMode() {
 		return OutputMode.MODIFIED_INPUT;
@@ -47,7 +49,7 @@ abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITim
 	public void setGroupProcessor(IGroupProcessor<T, T> groupProcessor) {
 		this.groupProcessor = groupProcessor;
 	}
-	
+
 	@Override
 	public void process_open() {
 		buffers.clear();
@@ -67,7 +69,7 @@ abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITim
 			}
 			buffer.add(object);
 			process(object, buffer, bufferId, lastTs);
-			
+
 			if (buffer.size() == 0) {
 				buffers.remove(bufferId);
 			}
@@ -78,8 +80,9 @@ abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITim
 		// Determine min element in transferBuffer and send hearbeat
 	}
 
-	abstract protected void process(T object, List<T> buffer, Long bufferId, PointInTime ts);
-	
+	abstract protected void process(T object, List<T> buffer, Long bufferId,
+			PointInTime ts);
+
 	protected PointInTime getMinTs() {
 		// MinTs is the oldest element of all buffers
 		// because the buffers are sorted by time, only the first element has to
@@ -89,33 +92,36 @@ abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITim
 		}
 		PointInTime min = null;
 		for (List<T> buffer : buffers.values()) {
-			T e = buffer.get(0);
-			PointInTime test = e.getMetadata().getStart();
-			if (min == null || min.after(test)) {
-				min = test;
+			if (!buffer.isEmpty()) {
+				T e = buffer.get(0);
+				PointInTime test = e.getMetadata().getStart();
+				if (min == null || min.after(test)) {
+					min = test;
+				}
 			}
 		}
 		return min;
 	}
-	
+
 	@Override
 	protected void process_done() {
 		synchronized (buffers) {
 			for (List<T> b : buffers.values()) {
-                if (b.size() > 0) {
-                	// Transfer elements in buffers
-                	// use lastTs+1 as timestamp for the last element (else the last element
-                	// will be removed)
-                    transferBuffer(b, b.size(), lastTs.plus(1));
-                    for (T t : b) {
-                        transferArea.transfer(t);
-                    }
-                }
+				if (b.size() > 0) {
+					// Transfer elements in buffers
+					// use lastTs+1 as timestamp for the last element (else the
+					// last element
+					// will be removed)
+					transferBuffer(b, b.size(), lastTs.plus(1));
+					for (T t : b) {
+						transferArea.transfer(t);
+					}
+				}
 			}
 		}
 		transferArea.newHeartbeat(lastTs, 0);
 	}
-	
+
 	protected void transferBuffer(List<T> buffer, long numberofelements,
 			PointInTime ts) {
 		synchronized (buffer) {
@@ -165,7 +171,7 @@ abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITim
 			transferArea.newHeartbeat(minTs, 0);
 		}
 	}
-	
+
 	@Override
 	public long getElementsStored1() {
 		long size = 0;
@@ -175,7 +181,7 @@ abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITim
 		}
 		return size;
 	}
-	
+
 	@Override
 	public Serializable getState() {
 		SlidingElementWindowTIPOState<T> state = new SlidingElementWindowTIPOState<T>();
@@ -195,7 +201,7 @@ abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITim
 			groupProcessor = state.getGroupProcessor();
 			lastTs = state.getLastTs();
 			transferArea = state.getTransferArea();
-			transferArea.setTransfer(this);			
+			transferArea.setTransfer(this);
 		} catch (Throwable T) {
 			LOG.error("The serializable state to set for the SlidingElementWindowTIPO is not a valid SlidingElementWindowTIPOState!");
 		}
@@ -205,5 +211,4 @@ abstract public class AbstractPartitionedWindowTIPO<T extends IStreamObject<ITim
 		return null;
 	}
 
-	
 }
