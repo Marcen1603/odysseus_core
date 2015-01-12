6ab51cb5bed15b11b1122ceb1269df3450fdac2f
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-01-12 10:43:26 +0000

    Peer: DataSourceAdvertisement finished (still not used, yet). Next step: Replacing old SourceAdvertisement with the new one
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23109 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 94c2b6467f7eb68205ea73c383b2fb5d29a97ce9

--- peer/server/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/dictionary/DataSourceAdvertisement.java
+++ peer/server/de.uniol.inf.is.odysseus.p2p_new/src/de/uniol/inf/is/odysseus/p2p_new/dictionary/DataSourceAdvertisement.java
@@ -3,15 +3,21 @@ package de.uniol.inf.is.odysseus.p2p_new.dictionary;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Enumeration;
+import java.util.Objects;
 
 import net.jxta.document.Advertisement;
+import net.jxta.document.Attributable;
 import net.jxta.document.Document;
 import net.jxta.document.Element;
 import net.jxta.document.MimeMediaType;
+import net.jxta.document.StructuredDocument;
 import net.jxta.document.StructuredDocumentFactory;
 import net.jxta.document.TextElement;
 import net.jxta.id.ID;
+import net.jxta.id.IDFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,7 +55,7 @@ public class DataSourceAdvertisement extends Advertisement implements Serializab
 	}
 
 	public DataSourceAdvertisement(DataSourceAdvertisement adv) {
-		Preconditions.checkNotNull(adv, "Advertisement to copy must not be null!");
+		Preconditions.checkNotNull(adv, "DataSourceAdvertisement to copy must not be null!");
 
 		id = adv.id;
 		name = adv.name;
@@ -67,11 +73,50 @@ public class DataSourceAdvertisement extends Advertisement implements Serializab
 
 	@Override
 	public Document getDocument(MimeMediaType asMimeType) {
-		return null;
+		final StructuredDocument<?> doc = StructuredDocumentFactory.newStructuredDocument(asMimeType, getAdvertisementType());
+		if (doc instanceof Attributable) {
+			((Attributable) doc).addAttribute("xmlns:jxta", "http://jxta.org");
+		}
+
+		appendElement(doc, ID_TAG, id.toString());
+		appendElement(doc, NAME_TAG, name);
+		appendElement(doc, PQL_TAG, pql);
+		
+		return doc;
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static Element appendElement(Element appendTo, String tag, String value) {
+		final Element ele = appendTo.getRoot().createElement(tag, value);
+		appendTo.appendChild(ele);
+		return ele;
 	}
 	
 	private void handleElement(TextElement<?> elem) {
-		
+		if (elem.getName().equals(ID_TAG)) {
+			setID(toID(elem));
+
+		} else if (elem.getName().equals(NAME_TAG)) {
+			setName(elem.getTextValue());
+			
+		} else if (elem.getName().equals(PQL_TAG)) {
+			setPQLText(elem.getTextValue());
+
+		}
+	}
+	
+	private static ID toID(TextElement<?> elem) {
+		return toID(elem.getTextValue());
+	}
+	
+	private static ID toID(String text) {
+		try {
+			final URI id = new URI(text);
+			return IDFactory.fromURI(id);
+		} catch (URISyntaxException | ClassCastException ex) {
+			LOG.error("Could not set id", ex);
+			return null;
+		}
 	}
 
 	@Override
@@ -104,4 +149,27 @@ public class DataSourceAdvertisement extends Advertisement implements Serializab
 		return INDEX_FIELDS;
 	}
 
+	public static String getAdvertisementType() {
+		return ADVERTISEMENT_TYPE;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (!(obj instanceof DataSourceAdvertisement)) {
+			return false;
+		}
+		DataSourceAdvertisement adv = (DataSourceAdvertisement) obj;
+		return
+				Objects.equals(adv.id, id) &&
+				Objects.equals(adv.name, name) && 
+				Objects.equals(adv.pql, pql);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, id, pql);
+	}
 }
