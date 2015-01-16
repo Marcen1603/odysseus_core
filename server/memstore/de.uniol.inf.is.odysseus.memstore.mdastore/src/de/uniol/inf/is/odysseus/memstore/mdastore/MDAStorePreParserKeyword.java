b2393a8169866771831cefb472ceb84e13e193c7
Michael Brand <michael.brand@uni-oldenburg.de>
2015-01-16 13:50:16 +0000

    MDAStore: fixed keyword
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23184 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a0de0c77a668ecafbf52b8b82f6d5c1d69a4a7dc

--- server/memstore/de.uniol.inf.is.odysseus.memstore.mdastore/src/de/uniol/inf/is/odysseus/memstore/mdastore/MDAStorePreParserKeyword.java
+++ server/memstore/de.uniol.inf.is.odysseus.memstore.mdastore/src/de/uniol/inf/is/odysseus/memstore/mdastore/MDAStorePreParserKeyword.java
@@ -15,21 +15,22 @@ import de.uniol.inf.is.odysseus.script.parser.OdysseusScriptException;
 public class MDAStorePreParserKeyword extends AbstractPreParserKeyword {
 
 	public static final String KEYWORD = "MDASTORE_INIT";
-	
+
 	private static final int MIN_ATTRIBUTE_COUNT = 3;
-	
+
 	private static final String PATTERN = "Name ClassName ValuesOfFirstDimension ... ValuesOfNthDimension";
-	
+
 	@Override
 	public void validate(Map<String, Object> variables, String parameter,
 			ISession caller, Context context) throws OdysseusScriptException {
-		if( Strings.isNullOrEmpty(parameter)) {
+		if (Strings.isNullOrEmpty(parameter)) {
 			throw new OdysseusScriptException("MDAStore name is missing!");
 		}
-		
+
 		String[] splitted = parameter.trim().split(" ");
-		if(splitted.length < MIN_ATTRIBUTE_COUNT) {
-			throw new OdysseusScriptException(KEYWORD + " needs at least " + MIN_ATTRIBUTE_COUNT + " attributes: " + PATTERN + "!");
+		if (splitted.length < MIN_ATTRIBUTE_COUNT) {
+			throw new OdysseusScriptException(KEYWORD + " needs at least "
+					+ MIN_ATTRIBUTE_COUNT + " attributes: " + PATTERN + "!");
 		}
 	}
 
@@ -41,94 +42,38 @@ public class MDAStorePreParserKeyword extends AbstractPreParserKeyword {
 		String[] splitted = parameter.trim().split(" ");
 		String name = splitted[0];
 		MDAStore<?> store = MDAStoreManager.create(name);
-		
+
 		String className = splitted[1];
 		Class<?> clz = null;
 		try {
 			clz = Class.forName(className);
-		} catch(Throwable e) {
-			System.err.println(className + " is not full qualified name of a comparable class!");
+		} catch (Throwable e) {
+			throw new OdysseusScriptException(className
+					+ " is not full qualified name of a comparable class!");
 		}
-		
+
 		List values = Lists.newArrayList();
-		for(int i = 2; i < splitted.length; i++) {
+		for (int i = 2; i < splitted.length; i++) {
 			String[] splitted_intern = splitted[i].trim().split(",");
 			List values_intern = Lists.newArrayList();
-			for(int j = 0; j < splitted_intern.length; j++) {
-				if(Number.class.isAssignableFrom(clz)) {
-					values_intern.add(clz.cast(Double.valueOf(splitted_intern[j])));
-				} else {
-					values_intern.add(clz.cast(splitted_intern[j]));
+			for (int j = 0; j < splitted_intern.length; j++) {
+				try {
+					if (Number.class.isAssignableFrom(clz)) {
+						values_intern.add(clz.cast(Double.valueOf(
+								splitted_intern[j]).doubleValue()));
+					} else {
+						values_intern.add(clz.cast(splitted_intern[j]));
+					}
+				} catch (Throwable e) {
+					throw new OdysseusScriptException("Could not cast "
+							+ splitted_intern[j] + " to " + className);
 				}
 			}
 			values.add(values_intern);
 		}
-		
+
 		store.initialize(values);
 		return null;
-	}
-	
-	/*
-	 * Simple Test.
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static void main(String[] args) {
-		MDAStore store = test("TestStore java.lang.Double 10.0,5.0,0.0,20.0,15.0");
-
-		int[] cell = store.getCellIndices(Lists
-				.newArrayList(15.0));
-		List<Double> val = store.getCellValues((List<Double>) Lists
-				.newArrayList(15.0));
-		System.out.println("Cell1 = " + cell[0] + ", Value = " + val.get(0));
-		cell = store.getCellIndices((List<Double>) Lists.newArrayList(12.0));
-		val = store.getCellValues((List<Double>) Lists.newArrayList(12.0));
-		System.out.println("Cell2 = " + cell[0] + ", Value = " + val.get(0));
-		cell = store.getCellIndices((List<Double>) Lists.newArrayList(-5.0));
-		val = store.getCellValues((List<Double>) Lists.newArrayList(-5.0));
-		System.out.println("Cell3 = " + cell[0] + ", Value = " + val.get(0));
-		cell = store.getCellIndices((List<Double>) Lists.newArrayList(100.0));
-		val = store.getCellValues((List<Double>) Lists.newArrayList(100.0));
-		System.out.println("Cell4 = " + cell[0] + ", Value = " + val.get(0));
-	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private static MDAStore<?> test(String parameter) {
-		if( Strings.isNullOrEmpty(parameter)) {
-			System.err.println("MDAStore name is missing!");
-		}
-		
-		String[] splitted = parameter.trim().split(" ");
-		if(splitted.length < MIN_ATTRIBUTE_COUNT) {
-			System.err.println(KEYWORD + " needs at least " + MIN_ATTRIBUTE_COUNT + " attributes: " + PATTERN + "!");
-		}
-		
-		String name = splitted[0];
-		MDAStore<?> store = MDAStoreManager.create(name);
-		
-		String className = splitted[1];
-		Class<?> clz = null;
-		try {
-			clz = Class.forName(className);
-		} catch(Throwable e) {
-			System.err.println(className + " is not full qualified name of a comparable class!");
-		}
-		
-		List values = Lists.newArrayList();
-		for(int i = 2; i < splitted.length; i++) {
-			String[] splitted_intern = splitted[i].trim().split(",");
-			List values_intern = Lists.newArrayList();
-			for(int j = 0; j < splitted_intern.length; j++) {
-				if(Number.class.isAssignableFrom(clz)) {
-					values_intern.add(clz.cast(Double.valueOf(splitted_intern[j])));
-				} else {
-					values_intern.add(clz.cast(splitted_intern[j]));
-				}
-			}
-			values.add(values_intern);
-		}
-		
-		store.initialize(values);
-		return store;
 	}
 
 }
