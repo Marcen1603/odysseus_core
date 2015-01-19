dd27054256c5a6a34fb9e236727e8d03156574c2
Michael Brand <michael.brand@uni-oldenburg.de>
2015-01-19 08:01:49 +0000

    ODY-881 fixed the sync within ReplicationMergePO
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23190 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9ad1e0a34fc66bfd97b088cfac3ddedd99c20de8

--- peer/server/de.uniol.inf.is.odysseus.peer.distribute.modify.replication/src/de/uniol/inf/is/odysseus/peer/distribute/modify/replication/physicaloperator/ReplicationMergePO.java
+++ peer/server/de.uniol.inf.is.odysseus.peer.distribute.modify.replication/src/de/uniol/inf/is/odysseus/peer/distribute/modify/replication/physicaloperator/ReplicationMergePO.java
@@ -22,298 +22,358 @@ import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe;
 import de.uniol.inf.is.odysseus.peer.distribute.modify.replication.logicaloperator.ReplicationMergeAO;
 
 /**
- * A {@link ReplicationMergePO} can be used to realize a {@link ReplicationMergeAO}. <br />
- * The {@link ReplicationMergePO} uses a {@link PriorityQueue} and can handle {@link IPunctuations}.
+ * A {@link ReplicationMergePO} can be used to realize a
+ * {@link ReplicationMergeAO}. <br />
+ * The {@link ReplicationMergePO} uses a {@link PriorityQueue} and can handle
+ * {@link IPunctuations}.
+ * 
  * @author Michael Brand
  */
-public class ReplicationMergePO<T extends IStreamObject<? extends ITimeInterval>> 
+public class ReplicationMergePO<T extends IStreamObject<? extends ITimeInterval>>
 		extends AbstractPipe<T, T> {
-	
+
 	/**
 	 * The logger for this class.
 	 */
-	private static final Logger log = LoggerFactory.getLogger(ReplicationMergePO.class);
-	
+	private static final Logger log = LoggerFactory
+			.getLogger(ReplicationMergePO.class);
+
 	/**
-	 * The {@link PriorityQueue} to keep in mind which elements have been seen by this {@link ReplicationMergePO}.
+	 * The {@link PriorityQueue} to keep in mind which elements have been seen
+	 * by this {@link ReplicationMergePO}.
 	 */
 	protected PriorityQueue<IPair<IStreamable, Integer>> inputQueue;
-	
+
 	/**
 	 * The youngest start timestamp of all seen objects.
 	 */
 	private PointInTime youngestTS;
-	
+
+	// Is needed to sync youngestTS, since youngest TS may be null
+	private Object dummyForSync = new Object();
+
 	/**
 	 * The comparator for pairs of {@link IStreamable} objects and ports. <br />
-	 * The start timestamps of the {@link IStreamable} objects will be compaired.
+	 * The start timestamps of the {@link IStreamable} objects will be
+	 * compaired.
 	 */
-	Comparator<IPair<IStreamable, Integer>> comp = new Comparator<IPair<IStreamable, Integer>>() {
+	transient Comparator<IPair<IStreamable, Integer>> comp = new Comparator<IPair<IStreamable, Integer>>() {
 
 		@Override
 		public int compare(IPair<IStreamable, Integer> left,
 				IPair<IStreamable, Integer> right) {
-			
+
 			PointInTime ts_l = getTS(left.getE1(), true);
 			PointInTime ts_r = getTS(right.getE1(), true);
-			
+
 			return ts_l.compareTo(ts_r);
-			
-		}		
-		
+
+		}
+
 	};
-	
+
 	/**
 	 * Delivers a timestamp of an {@link IStreamable} object.
-	 * @param object The object.
-	 * @param searchForStartTS true, if it is the start timestamp, which should be delivered; false, if it is the end timestamp.
-	 * @return {@link IPunctuation#getTime()} - if <code>object</code> is a punctuation <br />
-	 * {@link ITimeInterval#getStart()} - if <code>object</code> is no punctuation and <code>searchForStartTS == true</code> <br />
-	 * {@link ITimeInterval#getEnd()} - if <code>object</code> is no punctuation and <code>searchForStartTS == false</code> <br />
+	 * 
+	 * @param object
+	 *            The object.
+	 * @param searchForStartTS
+	 *            true, if it is the start timestamp, which should be delivered;
+	 *            false, if it is the end timestamp.
+	 * @return {@link IPunctuation#getTime()} - if <code>object</code> is a
+	 *         punctuation <br />
+	 *         {@link ITimeInterval#getStart()} - if <code>object</code> is no
+	 *         punctuation and <code>searchForStartTS == true</code> <br />
+	 *         {@link ITimeInterval#getEnd()} - if <code>object</code> is no
+	 *         punctuation and <code>searchForStartTS == false</code> <br />
 	 */
 	@SuppressWarnings("unchecked")
 	private PointInTime getTS(IStreamable object, boolean searchForStartTS) {
-		
-		if(object.isPunctuation())
+
+		if (object.isPunctuation())
 			return ((IPunctuation) object).getTime();
-		else if(searchForStartTS)
+		else if (searchForStartTS)
 			return ((T) object).getMetadata().getStart();
-		else return ((T) object).getMetadata().getEnd();
-				
+		else
+			return ((T) object).getMetadata().getEnd();
+
 	}
 
 	/**
 	 * Constructs a new {@link ReplicationMergePO}.
 	 */
 	public ReplicationMergePO() {
-		
+
 		super();
-		this.inputQueue = new PriorityQueue<IPair<IStreamable, Integer>>(10, comp);
+		this.inputQueue = new PriorityQueue<IPair<IStreamable, Integer>>(10,
+				comp);
 		this.youngestTS = null;
-		
+
 	}
 
 	/**
 	 * Constructs a new {@link ReplicationMergePO} as a copy of an existing one.
-	 * @param mergePO The {@link ReplicationMergePO} to be copied.
+	 * 
+	 * @param mergePO
+	 *            The {@link ReplicationMergePO} to be copied.
 	 */
 	public ReplicationMergePO(ReplicationMergePO<T> mergePO) {
-		
+
 		super(mergePO);
-		this.inputQueue = new PriorityQueue<IPair<IStreamable, Integer>>(10, comp);
+		this.inputQueue = new PriorityQueue<IPair<IStreamable, Integer>>(10,
+				comp);
 		this.inputQueue.addAll(mergePO.inputQueue);
 		this.youngestTS = mergePO.youngestTS;
-		
+
 	}
-	
+
 	@Override
 	public AbstractPipe<T, T> clone() {
-		
+
 		return new ReplicationMergePO<T>(this);
-		
+
 	}
 
 	@Override
 	public OutputMode getOutputMode() {
-		
+
 		return OutputMode.INPUT;
-		
-	}
-	
-	@Override
-	protected synchronized void process_open() throws OpenFailedException {
-		
-		this.inputQueue.clear();
-		this.youngestTS = null;
-		
-	}
-	
-	@Override
-	protected synchronized void process_close() {
-		
-		this.inputQueue.clear();
-		this.youngestTS = null;
-		
-	}
-	
-	@Override
-	protected synchronized void process_done() {
-		
-		if(this.isOpen()) {
-			
-			this.inputQueue.clear();
-			this.youngestTS = null;
-			
-		}
-		
-	}
-	
-	@Override
-	protected synchronized boolean isDone() {
-		
-		if(!this.inputQueue.isEmpty())
-			return false;
-		
-		for(int port = 0; port < this.getInputPortCount(); port++) {
-			
-			if(!this.getSubscribedToSource(port).isDone())
-				return false;
-			
-		}
-		
-		return true;
-		
-	}
-	
-	@Override
-	protected void process_next(T object, int port) {
-		
-		if(!this.precheck(object))
-			return;
-		
-		this.purgeElements(this.getTS(object, true));
-		this.mergeElement(object, port);
-		this.inputQueue.add(new Pair<IStreamable, Integer>(object, port));
 
 	}
-	
+
 	@Override
-	public synchronized void processPunctuation(IPunctuation punctuation, int port) {
-		
-		if(!this.precheck(punctuation))
+	protected void process_open() throws OpenFailedException {
+
+		synchronized (this.inputQueue) {
+			this.inputQueue.clear();
+		}
+		synchronized (this.dummyForSync) {
+			this.youngestTS = null;
+		}
+
+	}
+
+	@Override
+	protected void process_close() {
+
+		synchronized (this.inputQueue) {
+			this.inputQueue.clear();
+		}
+		synchronized (this.dummyForSync) {
+			this.youngestTS = null;
+		}
+
+	}
+
+	@Override
+	protected void process_done() {
+
+		if (this.isOpen()) {
+			synchronized (this.inputQueue) {
+				this.inputQueue.clear();
+			}
+			synchronized (this.dummyForSync) {
+				this.youngestTS = null;
+			}
+
+		}
+
+	}
+
+	@Override
+	protected boolean isDone() {
+
+		synchronized (this.inputQueue) {
+			if (!this.inputQueue.isEmpty())
+				return false;
+		}
+
+		for (int port = 0; port < this.getInputPortCount(); port++) {
+
+			if (!this.getSubscribedToSource(port).isDone())
+				return false;
+
+		}
+
+		return true;
+
+	}
+
+	@Override
+	protected void process_next(T object, int port) {
+
+		if (!this.precheck(object))
 			return;
-			
+
+		this.purgeElements(this.getTS(object, true));
+		this.mergeElement(object, port);
+		synchronized (this.inputQueue) {
+			this.inputQueue.add(new Pair<IStreamable, Integer>(object, port));
+		}
+
+	}
+
+	@Override
+	public void processPunctuation(IPunctuation punctuation, int port) {
+
+		if (!this.precheck(punctuation))
+			return;
+
 		this.purgeElements(this.getTS(punctuation, true));
 		this.mergeElement(punctuation, port);
-		this.inputQueue.add(new Pair<IStreamable, Integer>(punctuation, port));
-		
+		synchronized (this.inputQueue) {
+			this.inputQueue.add(new Pair<IStreamable, Integer>(punctuation,
+					port));
+		}
+
 	}
-	
+
 	/**
-	 * Checks, if the start timestamp of the object is younger or equal to the youngest seen and updates the youngest seen if necessary.
-	 * @return True, if the object is is younger or equal. False, means that the object can be rejected.
+	 * Checks, if the start timestamp of the object is younger or equal to the
+	 * youngest seen and updates the youngest seen if necessary.
+	 * 
+	 * @return True, if the object is is younger or equal. False, means that the
+	 *         object can be rejected.
 	 */
-	private synchronized boolean precheck(IStreamable object) {
-		
+	private boolean precheck(IStreamable object) {
+
 		final PointInTime ts = this.getTS(object, true);
-		
-		if(this.youngestTS != null && ts.before(this.youngestTS))
-			return false;		
-		else if(this.youngestTS == null || ts.after(this.youngestTS)) {
-			
-			this.youngestTS = ts;
-			ReplicationMergePO.log.debug("Set youngest timestamp to {}.", this.youngestTS);
-			
-		}
-		
-		return true;
-		
-	}
-	
-	/**
-	 * Removes all elements from the {@link inputQueue}, which have an start timestamp before to <code>deadline</code>.
-	 */
-	private synchronized void purgeElements(PointInTime deadline) {
-		
-		boolean continuePeeking = true;
-		
-		while(!this.inputQueue.isEmpty() && continuePeeking) {
-			
-			IPair<IStreamable, Integer> pair = this.inputQueue.peek();
-			// TODO why can that be null?
-			if(pair == null) {
-				log.error("Element to purge is null!");
-				this.inputQueue.poll();
-				continue;
+
+		synchronized (this.dummyForSync) {
+			if (this.youngestTS != null && ts.before(this.youngestTS))
+				return false;
+			else if (this.youngestTS == null || ts.after(this.youngestTS)) {
+
+				this.youngestTS = ts;
+				ReplicationMergePO.log.debug("Set youngest timestamp to {}.",
+						this.youngestTS);
+
 			}
-			IStreamable elem = pair.getE1();
-			PointInTime endTS_elem = this.getTS(elem, true);
-			if(endTS_elem.before(deadline))
-				this.inputQueue.poll();
-			else continuePeeking = false;
-			
 		}
-		
+		return true;
+
 	}
-	
+
 	/**
-	 * Transfers an object if and only if it was not transfered before (which input port doesn't matter). <br />
-	 * As objects can be equal with equal meta data this method determines, if the number of objects equal to <code>object</code> transfered 
-	 * from port <code>port</code> is the maximum count over all ports. Otherwise the object will not transfered.
-	 * @param object The object to be merged
-	 * @param port The port on which the object was arriving.
+	 * Removes all elements from the {@link inputQueue}, which have an start
+	 * timestamp before to <code>deadline</code>.
+	 */
+	private void purgeElements(PointInTime deadline) {
+
+		boolean continuePeeking = true;
+
+		synchronized (this.inputQueue) {
+			while (!this.inputQueue.isEmpty() && continuePeeking) {
+
+				IPair<IStreamable, Integer> pair = this.inputQueue.peek();
+				IStreamable elem = pair.getE1();
+				PointInTime endTS_elem = this.getTS(elem, true);
+				if (endTS_elem.before(deadline))
+					this.inputQueue.poll();
+				else
+					continuePeeking = false;
+
+			}
+		}
+
+	}
+
+	/**
+	 * Transfers an object if and only if it was not transfered before (which
+	 * input port doesn't matter). <br />
+	 * As objects can be equal with equal meta data this method determines, if
+	 * the number of objects equal to <code>object</code> transfered from port
+	 * <code>port</code> is the maximum count over all ports. Otherwise the
+	 * object will not transfered.
+	 * 
+	 * @param object
+	 *            The object to be merged
+	 * @param port
+	 *            The port on which the object was arriving.
 	 */
 	@SuppressWarnings("unchecked")
-	private synchronized void mergeElement(IStreamable object, int port) {
-		
-		// The maximal count of that object over all ports and on the arriving port
+	private void mergeElement(IStreamable object, int port) {
+
+		// The maximal count of that object over all ports and on the arriving
+		// port
 		int foundMax = 0;
 		int foundOwnPort = 0;
-		
-		Map<Integer, Integer> countToPortMap = Maps.newHashMap();	// key = port, value = count
-		Iterator<IPair<IStreamable, Integer>> queueIter = this.inputQueue.iterator();
-		while(queueIter.hasNext()) {
-			
-			IPair<IStreamable, Integer> pair = queueIter.next();
-			// TODO why can that be null?
-			if(pair == null) {
-				log.error("Element to merge is null!");
-				continue;
+
+		Map<Integer, Integer> countToPortMap = Maps.newHashMap(); // key = port,
+																	// value =
+																	// count
+		synchronized (this.inputQueue) {
+			Iterator<IPair<IStreamable, Integer>> queueIter = this.inputQueue
+					.iterator();
+			while (queueIter.hasNext()) {
+
+				IPair<IStreamable, Integer> pair = queueIter.next();
+				IStreamable elem = pair.getE1();
+				Integer elemPort = pair.getE2();
+
+				if ((elem.isPunctuation() && !object.isPunctuation())
+						|| (!elem.isPunctuation() && object.isPunctuation())) {
+
+					// only one element is a punctuation
+					continue;
+
+				} else if (elem.isPunctuation()
+						&& object.isPunctuation()
+						&& !((IPunctuation) elem).getTime().equals(
+								((IPunctuation) object).getTime())) {
+
+					// not the same timestamps of the punctuations
+					continue;
+
+				} else if (!elem.isPunctuation()
+						&& !object.isPunctuation()
+						&& (!((T) elem).equals(object) || !((T) elem)
+								.getMetadata().equals(
+										((T) object).getMetadata()))) {
+
+					// either the elements or the timestamps are not equal
+					continue;
+
+				} else if (countToPortMap.containsKey(elemPort)) {
+
+					// port is already in the map
+					countToPortMap.put(elemPort,
+							countToPortMap.get(elemPort) + 1);
+
+				} else {
+
+					// port isn't in the map yet
+					countToPortMap.put(elemPort, 1);
+
+				}
+
+				// Check for maximum
+				if (countToPortMap.get(elemPort) > foundMax)
+					foundMax = countToPortMap.get(elemPort);
+				if (port == elemPort)
+					foundOwnPort = countToPortMap.get(elemPort);
+
 			}
-			IStreamable elem = pair.getE1();
-			Integer elemPort = pair.getE2();
-			
-			if((elem.isPunctuation() && !object.isPunctuation()) || (!elem.isPunctuation() && object.isPunctuation())) {
-				
-				// only one element is a punctuation
-				continue;
-				
-			} else if(elem.isPunctuation() && object.isPunctuation() && !((IPunctuation) elem).getTime().equals(((IPunctuation) object).getTime())) {
-				
-				// not the same timestamps of the punctuations
-				continue;
-				
-			} else if(!elem.isPunctuation() && !object.isPunctuation() && 
-					(!((T) elem).equals(object) || !((T) elem).getMetadata().equals(((T) object).getMetadata()))) {
-				
-				// either the elements or the timestamps are not equal
-				continue;
-				
-			} else if(countToPortMap.containsKey(elemPort)) {
-				
-				// port is already in the map
-				countToPortMap.put(elemPort, countToPortMap.get(elemPort) + 1);
-				
-			} else {
-				
-				// port isn't in the map yet
-				countToPortMap.put(elemPort, 1);
-				
-			}
-			
-			// Check for maximum
-			if(countToPortMap.get(elemPort) > foundMax)
-				foundMax = countToPortMap.get(elemPort);
-			if(port == elemPort)
-				foundOwnPort = countToPortMap.get(elemPort);
-			
+
 		}
-		
-		if(foundMax == 0 || foundMax == foundOwnPort) {
-			
+
+		if (foundMax == 0 || foundMax == foundOwnPort) {
+
 			/*
-			 * First appearance of that object -> transfer
-			 * Else would mean, that the object appeared on a different port earlier -> drop
+			 * First appearance of that object -> transfer Else would mean, that
+			 * the object appeared on a different port earlier -> drop
 			 */
-			
-			ReplicationMergePO.log.debug("Transfering object {} from port {}.", object, port);
-			
-			if(object.isPunctuation())
-				this.sendPunctuation((IPunctuation) object); 
-			else this.transfer((T) object);
-			
+
+			ReplicationMergePO.log.debug("Transfering object {} from port {}.",
+					object, port);
+
+			if (object.isPunctuation())
+				this.sendPunctuation((IPunctuation) object);
+			else
+				this.transfer((T) object);
+
 		}
-		
+
 	}
 
 }
