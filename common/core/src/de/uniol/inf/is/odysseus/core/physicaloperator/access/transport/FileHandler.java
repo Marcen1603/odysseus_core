8a76bd07fa5ce3a01650758e29ffdbc4724f4242
Henrik Surm <hsurm@uni-oldenburg.de>
2015-01-22 14:00:15 +0000

    Added delayOpenIn and delayOpenOut options
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23251 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1b500a6561f769c46661c17e3057e460bfe438a6

--- common/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/transport/FileHandler.java
+++ common/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/transport/FileHandler.java
@@ -17,11 +17,14 @@ package de.uniol.inf.is.odysseus.core.physicaloperator.access.transport;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+
 import de.uniol.inf.is.odysseus.core.collection.OptionMap;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.IProtocolHandler;
 
@@ -30,56 +33,115 @@ public class FileHandler extends AbstractFileHandler {
 	public static final String FILENAME = "filename";
 	public static final String PRELOAD = "preload";
 	public static final String CREATEDIR = "createdir";
+	public static final String DELAYOPENIN = "delayopenin";
+	public static final String DELAYOPENOUT = "delayopenout";
 	public static final String NAME = "File";
 
 	
 	private boolean createDir = false;
 	private boolean preload = false;
-	private FileInputStream fis;
+	private boolean delayOpenIn = false;
+	private boolean delayOpenOut = false;
 
 	public FileHandler() {
 		super();
 	}
 
-	public FileHandler(IProtocolHandler<?> protocolHandler, OptionMap options) {
+	public FileHandler(IProtocolHandler<?> protocolHandler, OptionMap options) 
+	{
 		super(protocolHandler, options);
-		preload = (options.containsKey(PRELOAD)) ? Boolean
-				.parseBoolean(options.get(PRELOAD)) : false;
-				
+		preload = options.getBoolean(PRELOAD, false); 
 		createDir = options.getBoolean(CREATEDIR, false);
+		delayOpenIn  = options.getBoolean(DELAYOPENIN, false);
+		delayOpenOut = options.getBoolean(DELAYOPENOUT, false);
+		
+		if (preload && delayOpenIn)
+			throw new IllegalArgumentException("Can't specify preload and delayOpenIn at the same time!");
 	}
 
 	@Override
-	public void processInOpen() throws IOException {
-		if (!preload) {
-			final File file = new File(filename);
-			try {
-				in = new FileInputStream(file);
-				fireOnConnect();
-			} catch (Exception e) {
-				fireOnDisconnect();
-				throw e;
+	public void processInOpen() throws IOException 
+	{
+		try 
+		{
+			if (!preload) 
+			{
+				if (!delayOpenIn)
+					in = new FileInputStream(new File(filename));
+				else
+					in = createDelayInputStream(new File(filename));					
+			} 
+			else 
+			{	
+				in = createPreloadInputStream();
 			}
-		} else {
-			fis = new FileInputStream(filename);
-			FileChannel channel = fis.getChannel();
-			long size = channel.size();
-			double x = size / (double) Integer.MAX_VALUE;
-			int n = (int) Math.ceil(x);
-			ByteBuffer buffers[] = new ByteBuffer[n];
-			for (int i = 0; i < n; i++) {
-				buffers[i] = ByteBuffer.allocateDirect(Integer.MAX_VALUE);
-				channel.read(buffers[i]);
-				buffers[i].rewind();
-			}
-
-			in = createInputStream(buffers);
-			fireOnConnect();
+			
+			fireOnConnect();			
 		}
+		catch (Exception e) 
+		{
+			fireOnDisconnect();
+			throw e;
+		}			
 	}
 
-	private InputStream createInputStream(final ByteBuffer buffers[]) {
-		InputStream istream = new InputStream() {
+	private InputStream createDelayInputStream(File file) 
+	{
+		return new InputStream() 
+		{
+			private InputStream stream = null;			
+			
+			@Override public int read() throws IOException 
+			{
+				return stream.read();
+			}
+
+			@Override public int read(byte[] bytes, int off, int len) throws IOException 
+			{
+				return stream.read(bytes, off, len);			
+			}
+
+			@Override public int available() throws IOException 
+			{
+				try
+				{
+					if (stream == null)
+						stream = new FileInputStream(new File(filename));					
+				}
+				catch (FileNotFoundException e)
+				{
+					return 0;
+				}
+				
+				return stream.available();
+			}
+
+			@Override public void close() throws IOException 
+			{
+				super.close();
+				
+				if (stream != null)
+					stream.close();
+			}
+		};
+	}
+
+	private InputStream createPreloadInputStream() throws IOException 
+	{
+		FileInputStream fis = new FileInputStream(filename);
+		FileChannel channel = fis.getChannel();
+		long size = channel.size();
+		double x = size / (double) Integer.MAX_VALUE;
+		int n = (int) Math.ceil(x);
+		final ByteBuffer buffers[] = new ByteBuffer[n];
+		for (int i = 0; i < n; i++) {
+			buffers[i] = ByteBuffer.allocateDirect(Integer.MAX_VALUE);
+			channel.read(buffers[i]);
+			buffers[i].rewind();
+		}
+		fis.close();
+		
+		return new InputStream() {
 			private int current = 0;
 
 			@Override
@@ -120,13 +182,10 @@ public class FileHandler extends AbstractFileHandler {
 				}
 			}
 		};
-		return istream;
 	}
 
-	@Override
-	public void processOutOpen() throws IOException {		
-		final File file = new File(filename);
-		
+	private OutputStream createOutputStream(File file) throws IOException
+	{
 		if (createDir)
 		{
 			File parentFile = file.getParentFile(); 
@@ -134,21 +193,48 @@ public class FileHandler extends AbstractFileHandler {
 				if (!parentFile.mkdirs())
 					throw new IOException("Could not create directory for " + filename);
 		}
+
+		return new FileOutputStream(file, append);
+	}
+	
+	private OutputStream createDelayOutputStream(final File file) 
+	{
+		return new OutputStream() 
+		{
+			private OutputStream stream = null;
+
+			@Override public void close() throws IOException 
+			{
+				super.close();
+				
+				if (stream != null)
+					stream.close();
+			}
+
+			@Override public void write(int val) throws IOException 
+			{
+				if (stream == null)
+					stream = createOutputStream(file);
+				
+				stream.write(val);
+			}
+		};
+	}	
+	
+	@Override
+	public void processOutOpen() throws IOException 
+	{
+		final File file = new File(filename);
 		
 		try {
-			out = new FileOutputStream(file, append);
+			if (!delayOpenOut)
+				out = createOutputStream(file);
+			else
+				out = createDelayOutputStream(file);
 			fireOnConnect();
 		} catch (Exception e) {
 			fireOnDisconnect();
 			throw e;
-		}
-	}
-
-	@Override
-	public void processInClose() throws IOException {
-		super.processInClose();
-		if (fis != null) {
-			fis.close();
 		}
 	}
 
@@ -174,6 +260,12 @@ public class FileHandler extends AbstractFileHandler {
     	} else if(this.preload != other.preload) {
     		return false;
     	} else if(!this.filename.equals(other.filename)) {
+    		return false;
+    	} else if (createDir != other.createDir) {
+    		return false;
+    	} else if (delayOpenIn != other.delayOpenIn) {
+    		return false;
+    	} else if (delayOpenOut != other.delayOpenOut) {
     		return false;
     	}
     	
