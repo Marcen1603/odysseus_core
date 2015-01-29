5450507ceaeb53a327f09c8a8cf54b2812d90d63
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-01-29 16:08:46 +0000

    SportsQL-Parser: Removed unused imports
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23361 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 67ae8eced1b6963a0585067ec5343f8f356c81ae

--- lsop/de.uniol.inf.is.odysseus.sports.sportsql/src/de/uniol/inf/is/odysseus/sports/sportsql/parser/SportsQLParser.java
+++ lsop/de.uniol.inf.is.odysseus.sports.sportsql/src/de/uniol/inf/is/odysseus/sports/sportsql/parser/SportsQLParser.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.common.collect.Lists;
-
 import de.uniol.inf.is.odysseus.core.collection.Context;
 import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
 import de.uniol.inf.is.odysseus.core.server.datadictionary.IDataDictionary;
