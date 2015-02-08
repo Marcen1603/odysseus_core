cd3d92bf4d03341d8aa9e4e261fc5fdcf797bd82
Marcus Behrendt <mbehrendt@uni-oldenburg.de>
2015-02-08 17:41:49 +0000

    SafeteyCommit
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23468 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: e047f114fd087a09c2d7bad62dea8d9fc9c90626

---
+++ incubation/server/trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/physical/compare/uots/AbstractFactory.java
@@ -0,0 +1,23 @@
+package de.uniol.inf.is.odysseus.trajectory.physical.compare.uots;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractFactory<P, K> {
+	
+	
+	private final Map<K, P> products = new HashMap<>();
+	
+	public void P create(K key) {
+		key = this.convertKey(key);
+		P product = this.products.get(key);
+		if(product == null) {
+			this.products.put(key, product = this.createProduct(key));
+		}
+		return product;
+	}
+	
+	protected abstract K convertKey(K key);
+	
+	protected abstract P createProduct(K convertedKey);
+}
