e1113bdc2e736eef37732772aa25a23f7cd9dc8e
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-02-09 02:40:00 +0000

    Only a warning, if user wants to drop a store that does not exist
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23477 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a480f75b98eee2c6efe602e35acb46128effcae9

--- server/memstore/de.uniol.inf.is.odysseus.memstore.mdastore/src/de/uniol/inf/is/odysseus/memstore/mdastore/commands/DropMDAStoreCommand.java
+++ server/memstore/de.uniol.inf.is.odysseus.memstore.mdastore/src/de/uniol/inf/is/odysseus/memstore/mdastore/commands/DropMDAStoreCommand.java
@@ -3,6 +3,8 @@ package de.uniol.inf.is.odysseus.memstore.mdastore.commands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.uniol.inf.is.odysseus.core.infoservice.InfoService;
+import de.uniol.inf.is.odysseus.core.infoservice.InfoServiceFactory;
 import de.uniol.inf.is.odysseus.core.server.datadictionary.IDataDictionaryWritable;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.IServerExecutor;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.command.AbstractExecutorCommand;
@@ -14,7 +16,9 @@ public class DropMDAStoreCommand extends AbstractExecutorCommand {
 
 	private static final Logger LOG = LoggerFactory
 			.getLogger(DropMDAStoreCommand.class);
-	
+	private static final InfoService INFO = InfoServiceFactory
+			.getInfoService(DropMDAStoreCommand.class);
+
 	private String name;
 
 	public DropMDAStoreCommand(ISession caller, String name) {
@@ -25,11 +29,11 @@ public class DropMDAStoreCommand extends AbstractExecutorCommand {
 	@Override
 	public void execute(IDataDictionaryWritable dd, IUserManagementWritable um,
 			IServerExecutor executor) {
-		if (!MDAStoreManager.exists(name)){
-			throw new IllegalArgumentException("Store with name "+name+" not defined");
+		if (!MDAStoreManager.exists(name)) {
+			INFO.warning("Store with name " + name + " not defined");
 		}
 		MDAStoreManager.remove(name);
-		LOG.info("MDAStore "+name+" removed");
+		LOG.info("MDAStore " + name + " removed");
 	}
 
 }
