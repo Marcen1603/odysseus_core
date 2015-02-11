cffa313338021a338bc567d24c1480965962ef99
Marcus Behrendt <mbehrendt@uni-oldenburg.de>
2015-02-11 06:58:43 +0000

    implement substitution transform rule
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23503 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 11c7cd76ce30db6ffe094d511c78acad4508eab0

--- incubation/server/trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/transform/TTrajectoryContructAORule.java
+++ incubation/server/trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/transform/TTrajectoryContructAORule.java
@@ -7,16 +7,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
 import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
 import de.uniol.inf.is.odysseus.core.sdf.schema.DirectAttributeResolver;
 import de.uniol.inf.is.odysseus.core.sdf.schema.IAttributeResolver;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFExpression;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.AggregateAO;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.PredicateWindowAO;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.RestructHelper;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.SelectAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.UnaryLogicalOp;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.AggregateItem;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.AggregateFunction;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.TransformationConfiguration;
 import de.uniol.inf.is.odysseus.core.server.predicate.TruePredicate;
 import de.uniol.inf.is.odysseus.mep.MEP;
@@ -24,6 +28,7 @@ import de.uniol.inf.is.odysseus.parser.pql.relational.RelationalPredicateBuilder
 import de.uniol.inf.is.odysseus.relational.base.predicate.RelationalPredicate;
 import de.uniol.inf.is.odysseus.ruleengine.ruleflow.IRuleFlowGroup;
 import de.uniol.inf.is.odysseus.trajectory.logicaloperator.TrajectoryConstructAO;
+import de.uniol.inf.is.odysseus.trajectory.logicaloperator.builder.NestAggregateItem;
 import de.uniol.inf.is.odysseus.trajectory.physical.TrajectoryConstructPO;
 import de.uniol.inf.is.odysseus.trajectory.physical.construct.DefaultTrajectoryConstructStrategy;
 import de.uniol.inf.is.odysseus.trajectory.physical.construct.FulltrajectoryConstructStrategy;
@@ -46,52 +51,69 @@ public class TTrajectoryContructAORule extends AbstractTransformationRule<Trajec
 	@Override
 	public void execute(TrajectoryConstructAO operator, TransformationConfiguration config) {
 		
+		UnaryLogicalOp op = operator;
 		final List<SDFAttribute> groupOrPartitionBy = Arrays.asList(operator.getTrajectoryId());
 		
-		final AggregateAO aggregateAO = new AggregateAO();
-		aggregateAO.setGroupingAttributes(groupOrPartitionBy);
-		aggregateAO.setAggregationItems(Arrays.asList((AggregateItem)operator.getPositionMapping()));
-		
-		RestructHelper.insertOperatorBefore(aggregateAO, operator);
-		this.insert(aggregateAO);
-		
 		if(!operator.getSubtrajectories()) {
-			final PredicateWindowAO predicateWindowAO = new PredicateWindowAO();
-			predicateWindowAO.setSameStarttime(true);
-			predicateWindowAO.setPartitionBy(groupOrPartitionBy);
-			
-			final IAttributeResolver attributeResolver = new DirectAttributeResolver(operator.getInputSchema());
-			final RelationalPredicateBuilder builder = new RelationalPredicateBuilder();
-			
-			final RelationalPredicate predStart = (RelationalPredicate)builder.createPredicate(attributeResolver, "State = 0");
-			predStart.init(operator.getInputSchema(), operator.getInputSchema());
-			predicateWindowAO.setStartCondition(predStart);
-			
-			final RelationalPredicate predEnd = (RelationalPredicate)builder.createPredicate(attributeResolver, "State = -1");
-			predEnd.init(operator.getInputSchema(), operator.getInputSchema());
-			predicateWindowAO.setEndCondition(predEnd);
-			
-			RestructHelper.insertOperatorBefore(predicateWindowAO, operator);
-			this.insert(predicateWindowAO);
+			op = this.insertPredicateWindowAO(op, groupOrPartitionBy);
 		}
+		
+		this.insertAggregateAO(op, groupOrPartitionBy, Arrays.asList((AggregateItem)operator.getPositionMapping()));
 	
 		RestructHelper.removeOperator(operator, false);
 		this.retract(operator);
 	}
 
+	private final UnaryLogicalOp insertPredicateWindowAO(final UnaryLogicalOp operatorBefore, List<SDFAttribute> groupOrPartitionBy) {
+		final PredicateWindowAO predicateWindowAO = new PredicateWindowAO();
+		predicateWindowAO.setSameStarttime(true);
+		predicateWindowAO.setPartitionBy(groupOrPartitionBy);
+		
+		final IAttributeResolver attributeResolver = new DirectAttributeResolver(operatorBefore.getInputSchema());
+		final RelationalPredicateBuilder builder = new RelationalPredicateBuilder();
+		
+		final RelationalPredicate predStart = (RelationalPredicate)builder.createPredicate(attributeResolver, "State = 0");
+		predStart.init(operatorBefore.getInputSchema(), operatorBefore.getInputSchema());
+		predicateWindowAO.setStartCondition(predStart);
+		
+		final RelationalPredicate predEnd = (RelationalPredicate)builder.createPredicate(attributeResolver, "State = -1");
+		predEnd.init(operatorBefore.getInputSchema(), operatorBefore.getInputSchema());
+		predicateWindowAO.setEndCondition(predEnd);
+		
+		predicateWindowAO.setOutputSchema(operatorBefore.getInputSchema());
+		
+		RestructHelper.insertOperatorBefore(predicateWindowAO, operatorBefore);
+		this.insert(predicateWindowAO);
+		
+		return predicateWindowAO;
+	}
+	
+	private final void insertAggregateAO(final ILogicalOperator operatorBefore, List<SDFAttribute> groupBy, List<AggregateItem> aggregates) {
+		final AggregateAO aggregateAO = new AggregateAO();
+		
+		RestructHelper.insertOperatorBefore(aggregateAO, operatorBefore);
+		
+		aggregateAO.setGroupingAttributes(groupBy);
+		aggregateAO.setAggregationItems(aggregates);
+		
+		this.insert(aggregateAO);
+	}
+	
+	
 	@Override
-	 public boolean isExecutable(TrajectoryConstructAO operator, TransformationConfiguration transformConfig) {
-		 return true;
-	 }
-	 
-	 @Override
-	 public String getName() {
-		 return "TrajectoryConstructAO -> PO";
-	 }
-	  
-	 @Override
-	 public IRuleFlowGroup getRuleFlowGroup() {
-		 return TransformRuleFlowGroup.SUBSTITUTION;
-	 }
+	public boolean isExecutable(TrajectoryConstructAO operator,
+			TransformationConfiguration transformConfig) {
+		return true;
+	}
+
+	@Override
+	public String getName() {
+		return "TrajectoryConstructAO -> PO";
+	}
+
+	@Override
+	public IRuleFlowGroup getRuleFlowGroup() {
+		return TransformRuleFlowGroup.SUBSTITUTION;
+	}
 
 }
