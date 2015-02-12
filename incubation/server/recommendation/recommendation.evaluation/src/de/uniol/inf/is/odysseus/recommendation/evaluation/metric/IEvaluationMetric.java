4ec3171ecc3170eedd2a3fdff87174a7c343d29e
Cornelius Ludmann <Cornelius.Ludmann@uni-oldenburg.de>
2015-02-12 07:26:51 +0000

    recsys feature revised
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23538 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a5f432402d2a0f8e870afed33e0f4fb19af1b17e

--- incubation/server/recommendation/recommendation.evaluation/src/de/uniol/inf/is/odysseus/recommendation/evaluation/metric/IEvaluationMetric.java
+++
@@ -1,7 +0,0 @@
-package de.uniol.inf.is.odysseus.recommendation.evaluation.metric;
-
-public interface IEvaluationMetric {
-
-	public Object calculateMetric(Object classValue, Double clazzValue, double fadingFactor);
-	
-}
