0346f31eaf78f6cf410cdd34b439b6d6dad10ec3
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-02-24 14:51:42 +0000

    simple version working
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23651 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 60610ea846b846554b0ffd1c086a907ef077b83a

--- client/client.console/src/de/uniol/inf/is/odysseus/client/console/Console.java
+++ client/client.console/src/de/uniol/inf/is/odysseus/client/console/Console.java
@@ -16,15 +16,11 @@ import de.uniol.inf.is.odysseus.core.planmanagement.executor.IClientExecutor;
 import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
 import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
 
-public class Console implements IApplication{
-	
+public class Console implements IApplication {
+
 	static private IExecutor executor;
-	private static String command;
-	private static String filename;
-	private static String username;
-	private static String password;
-		
-	
+	private static ISession session;
+
 	// called by OSGi-DS
 	public void unbindExecutor(IExecutor exec) {
 		if (exec == executor) {
@@ -34,33 +30,29 @@ public class Console implements IApplication{
 
 	// called by OSGi-DS
 	public void bindExecutor(IExecutor exec) {
+		executor = exec;
 		synchronized (Console.class) {
-			executor = exec;		
 			Console.class.notifyAll();
 		}
 	}
 
-	
-	
-	static private void runCommand() throws FileNotFoundException {
-		if (executor != null && command != null){
-			if (command.equals("exec")){
-				ISession session = executor.login(username, password.getBytes());
-				if (session != null){
-					if (filename != null){
-						FileInputStream inputStream = new FileInputStream(filename);
-						String query = readFileLines(inputStream);
-						executor.addQuery(query, "OdysseusScript", session, Context.empty());
-					}
-				}
+	static private void runExecuteFileCommand(String filename)
+			throws FileNotFoundException {
+		if (session != null) {
+			if (filename != null) {
+				FileInputStream inputStream = new FileInputStream(filename);
+				String query = readFileLines(inputStream);
+				executor.addQuery(query, "OdysseusScript", session,
+						Context.empty());
 			}
+		}else{
+			throw new RuntimeException("Not logged in!");
 		}
-		
 	}
-	
-	
+
 	private static String readFileLines(InputStream inputStream) {
-		BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				inputStream));
 		String inputLine = null;
 		StringBuffer query = new StringBuffer();
 
@@ -68,7 +60,7 @@ public class Console implements IApplication{
 			while ((inputLine = in.readLine()) != null) {
 				query.append(inputLine).append("\n");
 			}
-			
+
 			in.close();
 		} catch (IOException ignore) {
 		}
@@ -77,16 +69,16 @@ public class Console implements IApplication{
 
 	public Object start(IApplicationContext context) throws Exception {
 		context.applicationRunning();
-		
+
 		String[] args = (String[]) context.getArguments().get(
 				"application.args");
-		
+
 		String host = "localhost";
 		long port = 9669;
 		String instance = "odysseus";
-		
-		synchronized(Console.class){
-			while (executor == null){
+
+		synchronized (Console.class) {
+			while (executor == null) {
 				try {
 					Console.class.wait(1000);
 				} catch (InterruptedException e) {
@@ -94,44 +86,59 @@ public class Console implements IApplication{
 				}
 			}
 		}
-		
-		String wsdlLocation = "http://"+host+":"+port+"/"+instance+"?wsdl";
-		
+
+		String wsdlLocation = "http://" + host + ":" + port + "/" + instance
+				+ "?wsdl";
+
 		String serviceNamespace = "http://webservice.server.webservice.executor.planmanagement.odysseus.is.inf.uniol.de/";
 		String service = "WebserviceServerService";
-		boolean connected = ((IClientExecutor) executor).connect(wsdlLocation + ";" + serviceNamespace + ";" + service);
-		
-		if (!connected){
-			System.err.println("Could not connect to "+wsdlLocation);
+		boolean connected = false;
+		try {
+			connected = ((IClientExecutor) executor).connect(wsdlLocation + ";"
+					+ serviceNamespace + ";" + service);
+		} catch (Exception e) {
+
 		}
-		
-		if (args.length == 0){
-			System.out.println("Console mode currently not supported. Please use command line mode: ");
-			System.out.println("Usage: <url> <username> <password> <command> <parameter>");
-		}else if (args.length > 3){
-			username = args[0];
-			password = args[1];
-			command = args[2];
-			if (command.toLowerCase().equals("exec")){
-				if (args.length != 2){
-					System.out.println("Usage: exec filename");
-				}else{
-					filename = args[1];
-					runCommand();
-				}
-			}else{
-				System.err.println("Unknow command "+command);
-			}
-		}else{
-			
+		if (!connected) {
+			System.err.println("Could not connect to " + wsdlLocation);
+			return IApplicationContext.EXIT_ASYNC_RESULT;
 		}
-		return IApplicationContext.EXIT_ASYNC_RESULT;	
+
+		String username = "System";
+		String password = "manager";
+
+		session = executor.login(username, password.getBytes());
+
+		String file = "E:/workspaces/mgrawunder/WorkspaceMarco1/Nexmark/StreamSourcesSimple.qry";
+		runExecuteFileCommand(file);
+
+		// if (args.length == 0){
+		// System.out.println("Console mode currently not supported. Please use command line mode: ");
+		// System.out.println("Usage: -url <url> -u <username> -p <password> -c <command> <parameter>");
+		// }else if (args.length > 3){
+		// username = args[0];
+		// password = args[1];
+		// command = args[2];
+		// if (command.toLowerCase().equals("exec")){
+		// if (args.length != 2){
+		// System.out.println("Usage: exec filename");
+		// }else{
+		// filename = args[1];
+		// runCommand();
+		// }
+		// }else{
+		// System.err.println("Unknow command "+command);
+		// }
+		// }else{
+		//
+		// }
+		return IApplicationContext.EXIT_ASYNC_RESULT;
 	}
-	
+
 	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
