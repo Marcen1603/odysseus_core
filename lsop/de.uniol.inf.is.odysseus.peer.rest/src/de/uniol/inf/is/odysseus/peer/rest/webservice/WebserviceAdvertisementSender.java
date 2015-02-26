e9c8e0f83f4052c04a0a9826930764d2ba718397
Thore Stratmann <tstratmann@uni-oldenburg.de>
2015-02-26 15:32:35 +0000

    Removed osgi warnings
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23670 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 357575c5f686a62a0282073a387d073123b7505e

--- lsop/de.uniol.inf.is.odysseus.peer.rest/src/de/uniol/inf/is/odysseus/peer/rest/webservice/WebserviceAdvertisementSender.java
+++ lsop/de.uniol.inf.is.odysseus.peer.rest/src/de/uniol/inf/is/odysseus/peer/rest/webservice/WebserviceAdvertisementSender.java
@@ -7,9 +7,6 @@ import net.jxta.id.IDFactory;
 import net.jxta.peer.PeerID;
 import net.jxta.peergroup.PeerGroupID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import de.uniol.inf.is.odysseus.p2p_new.IJxtaServicesProvider;
 import de.uniol.inf.is.odysseus.p2p_new.util.RepeatingJobThread;
 import de.uniol.inf.is.odysseus.rest.service.RestService;
@@ -22,8 +19,6 @@ import de.uniol.inf.is.odysseus.rest.service.RestService;
  */
 public class WebserviceAdvertisementSender {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(WebserviceAdvertisementSender.class);
 	private static final long ADVLIFETIME = 180000;
 	private static final long ADVEXPIRATIONTIME = 180000;
 //	private static final long WAIT_TIME_MILLIS = 21 * 1000;
