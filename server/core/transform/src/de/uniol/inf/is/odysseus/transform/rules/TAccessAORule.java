3a62460cfe9008a802b50aa6676aeac00dd83ec2
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-03-17 07:15:12 +0000

    Removed Warning (the waring occurs, if the access ao is part of a view)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23773 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 52ce49da4cbdc51f261f3b99a3f1d0b12fc42d87

--- server/core/transform/src/de/uniol/inf/is/odysseus/transform/rules/TAccessAORule.java
+++ server/core/transform/src/de/uniol/inf/is/odysseus/transform/rules/TAccessAORule.java
@@ -155,13 +155,13 @@ public class TAccessAORule extends AbstractTransformationRule<AbstractAccessAO> 
 			}
 
 		} else {
-			if (operator.getWrapper() != null) {
-				// throw new
-				// TransformationException("Multiple definiton of source with name "+operator.getAccessAOName());
-				infoService
-						.warning("Potential problem? Multiple definition of source with name "
-								+ operator.getAccessAOName());
-			}
+//			if (operator.getWrapper() != null) {
+//				// throw new
+//				// TransformationException("Multiple definiton of source with name "+operator.getAccessAOName());
+//				infoService
+//						.warning("Potential problem? Multiple definition of source with name "
+//								+ operator.getAccessAOName());
+//			}
 		}
 		defaultExecute(operator, accessPO, config, true, true);
 	}
