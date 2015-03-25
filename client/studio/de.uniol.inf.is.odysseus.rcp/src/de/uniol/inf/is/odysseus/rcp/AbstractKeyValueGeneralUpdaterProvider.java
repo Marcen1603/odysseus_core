39d9e2f3d4bc568324ee3658bcfb0c6107b8a4c3
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-03-25 07:22:53 +0000

    RCP: Better thread name for updater in operatorDetailView
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23869 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f3c295211d31bf50665219f03337e7d3aafc4870

--- client/studio/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/AbstractKeyValueGeneralUpdaterProvider.java
+++ client/studio/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/AbstractKeyValueGeneralUpdaterProvider.java
@@ -6,52 +6,53 @@ import org.eclipse.ui.PlatformUI;
 
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
 
-
 public abstract class AbstractKeyValueGeneralUpdaterProvider extends AbstractKeyValueGeneralProvider {
 
 	private static final int UPDATE_INTERVAL_MILLIS = 1000;
-	
+
 	private Thread updateThread;
 	private boolean updateRunning = true;
 
 	@Override
 	public void createPartControl(Composite parent, final IPhysicalOperator operator) {
 		super.createPartControl(parent, operator);
-		
-		updateThread = new Thread( new Runnable() {
+
+		updateThread = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				while( updateRunning ) {
+				while (updateRunning) {
 					Display disp = PlatformUI.getWorkbench().getDisplay();
-					if( !disp.isDisposed() ) {
+					if (!disp.isDisposed()) {
 						disp.asyncExec(new Runnable() {
 							@Override
 							public void run() {
-								refresh();
+								if (updateRunning) {
+									refresh();
+								}
 							}
 						});
 					}
-					trySleep();		
+					trySleep();
 				}
 			}
 		});
 		updateThread.setDaemon(true);
-		updateThread.setName("Operator detail info updater");
+		updateThread.setName("Operator detail info updater of operator '" + operator.getClass().getSimpleName() + "'");
 		updateThread.start();
 	}
-	
+
 	@Override
 	public void dispose() {
 		updateRunning = false;
-		
+
 		super.dispose();
 	}
-	
+
 	private static void trySleep() {
 		try {
 			Thread.sleep(UPDATE_INTERVAL_MILLIS);
 		} catch (InterruptedException e) {
 		}
-	}	
-	
+	}
+
 }
