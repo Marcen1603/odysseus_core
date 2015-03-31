f9fd17b087726320c8f524e36af90665dcfebad1
Jan Sören Schwarz <jan.soeren.schwarz@offis.de>
2015-03-31 09:34:42 +0000

    Made MosaikProtocolHandler more robust
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23951 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: de860a1c6e26a83ec275e891e136a22b67446fa9

--- wrapper/mosaik/src/de/uniol/inf/is/odysseus/wrapper/mosaik/MosaikProtocolHandler.java
+++ wrapper/mosaik/src/de/uniol/inf/is/odysseus/wrapper/mosaik/MosaikProtocolHandler.java
@@ -81,110 +81,106 @@ public class MosaikProtocolHandler<T extends KeyValueObject<IMetaAttribute>> ext
 	@SuppressWarnings("unchecked")
 	@Override
 	public void process(long callerId, ByteBuffer message) {
-		// TODO: check if callerId is relevant
-		try {     
-			int size;
-			if(!dividedMsg) {
-				//read in header
-				ByteBuffer header = ByteBuffer.allocate(4);
-				header.order(ByteOrder.BIG_ENDIAN);
-		        message.get(header.array(), 0, 4);
-		        size = header.getInt(0);	
-			} else {
-				size = dividedSize;
-			}
-			
-			//read in message
-	        int read = 0;  
-			int remaining = message.remaining();      
-			byte[] messageByteArray = new byte[remaining];
-			if(size > remaining) {
-				byte[] tmpMessageByteArray = new byte[remaining];
-				message.get(tmpMessageByteArray, 0, remaining);
+		try {
+			while(message.remaining() > 0) {
+				int size;
 				if(!dividedMsg) {
-					dividedMsg = true;
-					alreadyRead = 0;
-					dividedSize = size;
-					dividedMsgByteArray = new ByteArrayOutputStream();
+					//read in header
+					ByteBuffer header = ByteBuffer.allocate(4);
+					header.order(ByteOrder.BIG_ENDIAN);
+			        message.get(header.array(), 0, 4);
+			        size = header.getInt(0);	
+				} else {
+					size = dividedSize;
 				}
-				dividedMsgByteArray.write(tmpMessageByteArray);
-				alreadyRead += remaining;
-				if(alreadyRead == size) {
-					dividedMsg = false;
-					messageByteArray = dividedMsgByteArray.toByteArray();
+				
+				//read in message
+				int remaining = message.remaining();      
+				byte[] messageByteArray = new byte[remaining];
+				if(size > remaining) {
+					byte[] tmpMessageByteArray = new byte[remaining];
+					message.get(tmpMessageByteArray, 0, remaining);
+					if(!dividedMsg) {
+						dividedMsg = true;
+						alreadyRead = 0;
+						dividedSize = size;
+						dividedMsgByteArray = new ByteArrayOutputStream();
+					}
+					dividedMsgByteArray.write(tmpMessageByteArray);
+					alreadyRead += remaining;
+					if(alreadyRead == size) {
+						dividedMsg = false;
+						messageByteArray = dividedMsgByteArray.toByteArray();
+					}
+				} else {
+			        message.get(messageByteArray, 0, size);
 				}
-			} else {
-		        while (read < size) {
-		            message.get(messageByteArray, read, size - read);
-		            if (messageByteArray.length < 0) {
-		                throw new IOException("Unexpected end of stream");
-		            }
-		            read += messageByteArray.length;
-		        }
+				if(messageByteArray.length == size) {
+					//handle complete message
+					final String messageSring = new String(messageByteArray, "UTF-8");
+					final JSONArray payload = (JSONArray) parser.parse(messageSring);
+			        
+					// Expand payload
+					final int msgType = ((Number) payload.get(0)).intValue();
+					if (msgType != REQ) {
+						throw new IOException("Expected message type 0, got " + msgType);
+					}
+					int msgId = ((Number) payload.get(1)).intValue();
+					final JSONArray call = (JSONArray) payload.get(2);
+
+					// Set request data
+					String method = (String) call.get(0);
+					JSONArray args = (JSONArray) call.get(1);
+					JSONObject kwargs = (JSONObject) call.get(2);
+
+					Object result;	  
+					boolean stop = false;
+					switch (method) {
+					case "init":
+						final String sid = (String) args.get(0);
+						result = this.sim.init(sid, kwargs);
+						break;
+					case "create":
+						final int num = ((Number) args.get(0)).intValue();
+						final String model = (String) args.get(1);
+						result = this.sim.create(num, model, kwargs);
+						break;
+					case "step":
+						final long time = ((Number) args.get(0)).longValue();
+						final JSONObject inputs = (JSONObject) args.get(1);
+						result = this.sim.step(time, inputs);
+						break;
+					case "get_data":
+						final JSONObject outputs = (JSONObject) args.get(0);
+						result = this.sim.getData(outputs);
+						break;
+					case "stop":
+						stop = true;
+						result = null;
+						this.close();
+						getTransfer().transfer(null);
+						break;
+					default:
+						throw new RuntimeException("Unkown method: " + method);
+					}
+
+					if(!stop) {
+						//send response
+						final JSONArray reply = new JSONArray();
+						reply.add(SUCCESS);
+						reply.add(msgId);
+						reply.add(result);
+						ByteArrayOutputStream sendData = new ByteArrayOutputStream();
+						ByteBuffer sendHeader = ByteBuffer.allocate(4);
+						sendHeader.putInt(0, reply.toString().length());
+						sendData.write(sendHeader.array());
+						sendData.write(reply.toString().getBytes());
+						getTransportHandler().send(sendData.toByteArray());
+					}
+				} else if(messageByteArray.length > size) {
+		            throw new IOException("Unexpected message");
+				}
 			}
-			if(messageByteArray.length == size) {
-				//handle complete message
-				final String messageSring = new String(messageByteArray, "UTF-8");
-				final JSONArray payload = (JSONArray) parser.parse(messageSring);    
-		        
-				// Expand payload
-				final int msgType = ((Number) payload.get(0)).intValue();
-				if (msgType != REQ) {
-					throw new IOException("Expected message type 0, got " + msgType);
-				}
-				int msgId = ((Number) payload.get(1)).intValue();
-				final JSONArray call = (JSONArray) payload.get(2);
-
-				// Set request data
-				String method = (String) call.get(0);
-				JSONArray args = (JSONArray) call.get(1);
-				JSONObject kwargs = (JSONObject) call.get(2);
-
-				Object result;	  
-				boolean stop = false;
-				switch (method) {
-				case "init":
-					final String sid = (String) args.get(0);
-					result = this.sim.init(sid, kwargs);
-					break;
-				case "create":
-					final int num = ((Number) args.get(0)).intValue();
-					final String model = (String) args.get(1);
-					result = this.sim.create(num, model, kwargs);
-					break;
-				case "step":
-					final long time = ((Number) args.get(0)).longValue();
-					final JSONObject inputs = (JSONObject) args.get(1);
-					result = this.sim.step(time, inputs);
-					break;
-				case "get_data":
-					final JSONObject outputs = (JSONObject) args.get(0);
-					result = this.sim.getData(outputs);
-					break;
-				case "stop":
-					stop = true;
-					result = null;
-					this.close();
-					getTransfer().transfer(null);
-					break;
-				default:
-					throw new RuntimeException("Unkown method: " + method);
-				}
-
-				if(!stop) {
-					//send response
-					final JSONArray reply = new JSONArray();
-					reply.add(SUCCESS);
-					reply.add(msgId);
-					reply.add(result);
-					ByteArrayOutputStream sendData = new ByteArrayOutputStream();
-					ByteBuffer sendHeader = ByteBuffer.allocate(4);
-					sendHeader.putInt(0, reply.toString().length());
-					sendData.write(sendHeader.array());
-					sendData.write(reply.toString().getBytes());
-					getTransportHandler().send(sendData.toByteArray());
-				}
-			} 
 		} catch (Exception e) {
 			e.printStackTrace();
 			LOG.debug(e.getMessage());
