12b489446c851b6fd8324058999561be1f844087
Henrik Surm <hsurm@uni-oldenburg.de>
2015-04-08 08:57:55 +0000

    Added digital filter operator for low-, high- and bandpasses
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@23996 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 286b0d5d48db5898afc1d11ef72cf777ab1107bb

---
+++ wrapper/fnirs/src/de/uniol/inf/is/odysseus/wrapper/fnirs/physicaloperator/RelationalDigitalFilterPO.java
@@ -0,0 +1,179 @@
+package de.uniol.inf.is.odysseus.wrapper.fnirs.physicaloperator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
+import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
+import de.uniol.inf.is.odysseus.core.physicaloperator.IPunctuation;
+import de.uniol.inf.is.odysseus.core.physicaloperator.OpenFailedException;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.sink.SenderPO;
+import de.uniol.inf.is.odysseus.wrapper.fnirs.digitalfilter.DigitalFilter;
+import de.uniol.inf.is.odysseus.wrapper.fnirs.digitalfilter.FilterLoop;
+
+abstract class TupleAttributeFilter
+{
+	public abstract Object filter(Object obj);
+}
+
+class DoubleAttributeFilter extends TupleAttributeFilter
+{
+	FilterLoop loop;
+	
+	public DoubleAttributeFilter(DigitalFilter filter)
+	{
+		loop = new FilterLoop(filter);
+	}
+	
+	public Object filter(Object obj)
+	{
+		return (Double) loop.filterStep((Double) obj);
+	}
+}
+
+class VectorAttributeFilter extends TupleAttributeFilter
+{
+	DigitalFilter filter;
+	FilterLoop[] loops;
+	
+	public VectorAttributeFilter(DigitalFilter filter)
+	{
+		this.filter = filter;
+	}
+	
+	public Object filter(Object obj)
+	{
+		double[] vector = (double[]) obj;
+		
+		if (loops == null)
+		{
+			loops = new FilterLoop[vector.length];
+			for (int i=0;i<vector.length;i++)
+				loops[i] = new FilterLoop(filter);
+		}
+		else
+			if (loops.length != vector.length)
+				throw new IllegalArgumentException("Vector length may not change!");
+			
+		for (int i=0;i<loops.length;i++)
+			vector[i] = loops[i].filterStep(vector[i]);
+		
+		return vector;
+	}
+}
+
+public class RelationalDigitalFilterPO<T extends IMetaAttribute> extends AbstractPipe<Tuple<T>, Tuple<T>> 
+{
+	static Logger LOG = LoggerFactory.getLogger(SenderPO.class);
+
+	private DigitalFilter filter;
+	
+	private Map<Integer, TupleAttributeFilter> attributeFilters = new HashMap<>();
+	private int[] attributes;
+	
+	
+	public RelationalDigitalFilterPO() 
+	{
+	}
+
+	public RelationalDigitalFilterPO(RelationalDigitalFilterPO<T> other) 
+	{
+		super(other);
+		filter = other.filter;
+		attributeFilters = other.attributeFilters;
+		attributes = other.attributes;
+	}
+
+	public void setFilter(DigitalFilter filter, int[] attributes, SDFSchema inputSchema)
+	{
+		this.filter = filter;
+		
+		if (attributes == null)
+		{
+			attributes = new int[inputSchema.size()];
+			for (int i=0;i<attributes.length;i++)
+				attributes[i] = i;
+		}
+		
+		this.attributes = attributes;
+		
+		for (int idx : attributes)
+		{
+			if (inputSchema.get(idx).getDatatype() == SDFDatatype.DOUBLE)
+				attributeFilters.put(idx, new DoubleAttributeFilter(filter));
+			else
+			if (inputSchema.get(idx).getDatatype() == SDFDatatype.VECTOR_DOUBLE)
+				attributeFilters.put(idx, new VectorAttributeFilter(filter));
+			else
+				throw new RuntimeException("Data type " + inputSchema.get(idx).getDatatype().getQualName() + " not supported for digital filter operator");
+		}
+	}
+	
+	public DigitalFilter getFilter()
+	{
+		return filter;
+	}
+	
+	@Override
+	public void processPunctuation(IPunctuation punctuation, int port) 
+	{
+		sendPunctuation(punctuation);
+	}
+
+	@Override
+	protected void process_open() throws OpenFailedException 
+	{
+		if (!isOpen()) 
+		{
+			super.process_open();
+		}
+	}
+
+	@Override
+	protected void process_close() 
+	{
+		if (isOpen()) 
+		{
+			super.process_close();
+		}
+	}
+
+	@Override
+	public OutputMode getOutputMode() 
+	{
+		return OutputMode.MODIFIED_INPUT;
+	}
+
+	@Override
+	public boolean isSemanticallyEqual(IPhysicalOperator operator) {
+		if (!(operator instanceof RelationalDigitalFilterPO)) {
+			return false;
+		}
+		@SuppressWarnings("unchecked")
+		RelationalDigitalFilterPO<T> filterPO = (RelationalDigitalFilterPO<T>) operator;
+
+		if (!filter.equals(filterPO.filter)) return false;
+		if (!attributes.equals(filterPO.attributes)) return false;
+		
+		return true;
+	}
+
+	@Override
+	protected void process_next(Tuple<T> object, int port) 
+	{
+		for (int idx : attributes)
+		{
+			object.setAttribute(idx, attributeFilters.get(idx).filter(object.getAttribute(idx)));
+		}
+		
+		transfer(object);
+	}	
+}
+
