6b62c3d4985c7092a00b9723b5e3e27ac0a1fdff
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-05-04 12:52:36 +0000

    For each function (only for scalar lists)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24225 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 38b2ab4c23e0dfb0a40441a4fa5304efd2d688cc

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/function/RetrieveQueryIDsFunction.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/function/RetrieveQueryIDsFunction.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
-import de.uniol.inf.is.odysseus.core.predicate.TuplePredicate;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFExpression;
