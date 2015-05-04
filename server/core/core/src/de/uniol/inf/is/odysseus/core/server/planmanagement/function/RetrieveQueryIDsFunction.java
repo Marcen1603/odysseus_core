08ca14ec5c18fc818b750c4af19b2a4a17e1d3df
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-05-04 12:36:51 +0000

    Priority and state
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24224 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 02c3b928bbfed767436103a66d9736102eb1f384

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/function/RetrieveQueryIDsFunction.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/function/RetrieveQueryIDsFunction.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.predicate.TuplePredicate;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFExpression;
 import de.uniol.inf.is.odysseus.core.server.ExecutorBinder;
@@ -19,16 +22,19 @@ public class RetrieveQueryIDsFunction extends AbstractFunction<List<Integer>> {
 
 	private static final long serialVersionUID = -7472947357374882538L;
 
-	private static final SDFDatatype[][] ACC_TYPES = new SDFDatatype[][] {
-			{ SDFDatatype.STRING }, { SDFDatatype.STRING },
-			{ SDFDatatype.BOOLEAN } };
+	private static final SDFDatatype[][] ACC_TYPES = new SDFDatatype[][] { { SDFDatatype.STRING } };
 
-	private TuplePredicate predicate;
-	
-	public RetrieveQueryIDsFunction() {
-		super("retrieveQueryIDs", 3, ACC_TYPES, SDFDatatype.LIST_INTEGER, false);
+	private enum QInformation {
+		QUERY_PRIORTIY, QUERY_STATE
 	}
 
+	List<QInformation> toRead;
+
+	private SDFExpression predicate;
+
+	public RetrieveQueryIDsFunction() {
+		super("retrieveQueryIDs", 1, ACC_TYPES, SDFDatatype.LIST_INTEGER, false);
+	}
 
 	@Override
 	public List<Integer> getValue() {
@@ -38,23 +44,59 @@ public class RetrieveQueryIDsFunction extends AbstractFunction<List<Integer>> {
 		if (sessions != null) {
 			if (predicate == null) {
 				String predicateString = (String) getInputValue(0);
-				String getAttribute = (String) getInputValue(1);
-				boolean asc = (Boolean) getInputValue(2);
-				predicate = new TuplePredicate(new SDFExpression(predicateString, MEP.getInstance()));
-				System.out.println(predicate.getAttributes());
-				
+				predicate = new SDFExpression(
+						predicateString, MEP.getInstance());
+				toRead = new ArrayList<>(predicate.getAllAttributes().size());
+				for (SDFAttribute attribute : predicate.getAllAttributes()) {
+					toRead.add(getQInformation(attribute));
+				}
 			}
+
 			IServerExecutor exec = ExecutorBinder.getExecutor();
 			IExecutionPlan plan = exec.getExecutionPlan();
 			Collection<IPhysicalQuery> queries = plan.getQueries();
 			for (IPhysicalQuery q : queries) {
-				if (true) {
+				Tuple<IMetaAttribute> queryValues = getQueryValues(toRead, q);
+				predicate.bindVariables(queryValues.getAttributes());
+				if (predicate.getValue()) {
 					ret.add(q.getID());
 				}
 			}
 
 		}
 		return ret;
+	}
+
+	private Tuple<IMetaAttribute> getQueryValues(List<QInformation> toRead,
+			IPhysicalQuery q) {
+		Tuple<IMetaAttribute> t = new Tuple<IMetaAttribute>(toRead.size(),
+				false);
+		for (int i = 0; i < toRead.size(); i++) {
+			t.setAttribute(i, getQueryValue(toRead.get(i), q));
+		}
+
+		return t;
+	}
+
+	private Object getQueryValue(QInformation qInformation, IPhysicalQuery q) {
+		switch (qInformation) {
+		case QUERY_PRIORTIY:
+			return q.getPriority();
+		case QUERY_STATE:
+			return q.getState();
+		}
+
+		return null;
+	}
+
+	private QInformation getQInformation(SDFAttribute attribute) {
+		if (attribute.getAttributeName().equalsIgnoreCase("priority")) {
+			return QInformation.QUERY_PRIORTIY;
+		}
+		if (attribute.getAttributeName().equalsIgnoreCase("state")) {
+			return QInformation.QUERY_STATE;
+		}
+		throw new IllegalArgumentException("Information "+attribute+" not defined");
 	}
 
 }
