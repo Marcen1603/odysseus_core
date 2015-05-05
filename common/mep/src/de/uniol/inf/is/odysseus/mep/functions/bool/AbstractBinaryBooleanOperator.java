480c4ad76715d1c15144a77adac206a980c64a6f
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-05-05 08:05:17 +0000

    Allow <Object> = <BOOLEAN>
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24233 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: cecc298a262d4ae9797bc82a45ee3c5a3826ad77

--- common/mep/src/de/uniol/inf/is/odysseus/mep/functions/bool/AbstractBinaryBooleanOperator.java
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/functions/bool/AbstractBinaryBooleanOperator.java
@@ -7,7 +7,7 @@ abstract public class AbstractBinaryBooleanOperator extends
 		AbstractBinaryOperator<Boolean> {
 
 	private static final long serialVersionUID = 5174424163393401545L;
-	public static final SDFDatatype[][] accTypes = new SDFDatatype[][]{new SDFDatatype[]{ SDFDatatype.BOOLEAN },new SDFDatatype[]{ SDFDatatype.BOOLEAN }};
+	public static final SDFDatatype[][] accTypes = new SDFDatatype[][]{new SDFDatatype[]{ SDFDatatype.BOOLEAN, SDFDatatype.OBJECT },new SDFDatatype[]{ SDFDatatype.BOOLEAN }};
 	
 	public AbstractBinaryBooleanOperator(String symbol) {
 		super(symbol, accTypes, SDFDatatype.BOOLEAN);
