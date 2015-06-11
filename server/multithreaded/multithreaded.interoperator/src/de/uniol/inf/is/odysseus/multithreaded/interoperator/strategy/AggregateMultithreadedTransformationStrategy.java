51415144b55274d5227836184e20419098703f82
Chris Toenjes-Deye <ctoenjesdeye@uni-oldenburg.de>
2015-06-11 17:20:45 +0000

    Strategy for Multithreaded Aggregations without grouping (not working until now)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24553 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 199b22b79efcac70818fd44cdf5fe4866c2a7c85

--- server/multithreaded/multithreaded.interoperator/src/de/uniol/inf/is/odysseus/multithreaded/interoperator/strategy/AggregateMultithreadedTransformationStrategy.java
+++ server/multithreaded/multithreaded.interoperator/src/de/uniol/inf/is/odysseus/multithreaded/interoperator/strategy/AggregateMultithreadedTransformationStrategy.java
@@ -1,7 +1,13 @@
 package de.uniol.inf.is.odysseus.multithreaded.interoperator.strategy;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalSubscription;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.AggregateAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.BufferAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.UnionAO;
+import de.uniol.inf.is.odysseus.server.fragmentation.horizontal.logicaloperator.RoundRobinFragmentAO;
 
 public class AggregateMultithreadedTransformationStrategy extends
 		AbstractMultithreadedTransformationStrategy<AggregateAO> {
@@ -31,8 +37,89 @@ public class AggregateMultithreadedTransformationStrategy extends
 	@Override
 	public boolean transform(ILogicalOperator operator,
 			int degreeOfParallelization) {
-		// TODO Auto-generated method stub
-		return false;
+		AggregateAO aggregateOperator = (AggregateAO) operator;
+
+		CopyOnWriteArrayList<LogicalSubscription> upstreamOperatorSubscriptions = new CopyOnWriteArrayList<LogicalSubscription>();
+		upstreamOperatorSubscriptions.addAll(aggregateOperator
+				.getSubscribedToSource());
+
+		CopyOnWriteArrayList<LogicalSubscription> downstreamOperatorSubscriptions = new CopyOnWriteArrayList<LogicalSubscription>();
+		downstreamOperatorSubscriptions.addAll(aggregateOperator
+				.getSubscriptions());
+
+		// remove subscriptions
+		for (LogicalSubscription upstreamOperatorSubscription : upstreamOperatorSubscriptions) {
+			ILogicalOperator target = upstreamOperatorSubscription.getTarget();
+			target.unsubscribeSink(upstreamOperatorSubscription);
+		}
+
+		for (LogicalSubscription downstreamOperatorSubscription : downstreamOperatorSubscriptions) {
+			ILogicalOperator target = downstreamOperatorSubscription
+					.getTarget();
+			target.unsubscribeFromSource(downstreamOperatorSubscription);
+		}
+
+		// round robin fragmentation, because there is no aggregate grouping
+		RoundRobinFragmentAO fragment = new RoundRobinFragmentAO();
+		fragment.setNumberOfFragments(degreeOfParallelization);
+		fragment.setName("Round Robin Fragment");
+
+		// subscribe new operator
+		for (LogicalSubscription upstreamOperatorSubscription : upstreamOperatorSubscriptions) {
+			aggregateOperator
+					.unsubscribeFromSource(upstreamOperatorSubscription);
+			fragment.subscribeToSource(
+					upstreamOperatorSubscription.getTarget(),
+					upstreamOperatorSubscription.getSinkInPort(),
+					upstreamOperatorSubscription.getSourceOutPort(),
+					upstreamOperatorSubscription.getTarget().getOutputSchema());
+		}
+
+		UnionAO union = new UnionAO();
+		union.setName("Union");
+
+		for (int i = 0; i < degreeOfParallelization; i++) {
+			BufferAO buffer = new BufferAO();
+			buffer.setName("Buffer_" + i);
+			buffer.setThreaded(true);
+			buffer.setMaxBufferSize(10000000);
+
+			AggregateAO newAggregateOperator = aggregateOperator.clone();
+			newAggregateOperator.setName(aggregateOperator.getName() + "_pa_"
+					+ i);
+			newAggregateOperator.setUniqueIdentifier(aggregateOperator
+					.getUniqueIdentifier() + "_pa_" + i);
+			newAggregateOperator.setOutputPA(true); // use partial aggregates
+
+			buffer.subscribeToSource(fragment, 0, i, fragment.getOutputSchema());
+
+			newAggregateOperator.subscribeToSource(buffer, 0, 0,
+					buffer.getOutputSchema());
+
+			union.subscribeToSource(newAggregateOperator, i, 0,
+					newAggregateOperator.getOutputSchema());
+		}
+
+		// combine partial aggregates
+		AggregateAO combinePAAggregateOperator = aggregateOperator.clone();
+		combinePAAggregateOperator.setName(aggregateOperator.getName()
+				+ "_combinePA");
+		combinePAAggregateOperator.setUniqueIdentifier(aggregateOperator
+				.getUniqueIdentifier() + "_combinePA");
+
+		combinePAAggregateOperator.subscribeToSource(union, 0, 0,
+				union.getOutputSchema());
+
+		for (LogicalSubscription downstreamOperatorSubscription : downstreamOperatorSubscriptions) {
+			aggregateOperator.unsubscribeSink(downstreamOperatorSubscription);
+			downstreamOperatorSubscription.getTarget().subscribeToSource(
+					combinePAAggregateOperator,
+					downstreamOperatorSubscription.getSinkInPort(),
+					downstreamOperatorSubscription.getSourceOutPort(),
+					combinePAAggregateOperator.getOutputSchema());
+		}
+
+		return true;
 	}
 
 }
