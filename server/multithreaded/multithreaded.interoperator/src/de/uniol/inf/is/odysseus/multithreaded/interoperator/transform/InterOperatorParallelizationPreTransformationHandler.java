833e45edb83f4a7d4cf74c2a5f9b502d6993ec89
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-06-19 09:55:18 +0000

    Recover old plan in case of exception
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24646 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 0e58e87c0a436454b1337565208fd0ce2d1e9a16

--- server/multithreaded/multithreaded.interoperator/src/de/uniol/inf/is/odysseus/multithreaded/interoperator/transform/InterOperatorParallelizationPreTransformationHandler.java
+++ server/multithreaded/multithreaded.interoperator/src/de/uniol/inf/is/odysseus/multithreaded/interoperator/transform/InterOperatorParallelizationPreTransformationHandler.java
@@ -15,6 +15,7 @@ import de.uniol.inf.is.odysseus.core.server.logicaloperator.TopAO;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.IServerExecutor;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.QueryBuildConfiguration;
 import de.uniol.inf.is.odysseus.core.server.util.CollectOperatorLogicalGraphVisitor;
+import de.uniol.inf.is.odysseus.core.server.util.CopyLogicalGraphVisitor;
 import de.uniol.inf.is.odysseus.core.server.util.GenericGraphWalker;
 import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
 import de.uniol.inf.is.odysseus.multithreaded.interoperator.parameter.MultithreadedOperatorParameter;
@@ -44,138 +45,158 @@ public class InterOperatorParallelizationPreTransformationHandler extends
 	public void preTransform(IServerExecutor executor, ISession caller,
 			ILogicalQuery query, QueryBuildConfiguration config,
 			List<Pair<String, String>> handlerParameters, Context context) {
-		if (handlerParameters.size() != PARAMETER_COUNT) {
-			throw new IllegalArgumentException();
-		} else {
-			// Determine parameters
-			for (Pair<String, String> pair : handlerParameters) {
-				ParallelizationParameter parameter = ParallelizationParameter
-						.getParameterByName(pair.getE1());
-				switch (parameter) {
-				case GLOBAL_DEGREE_OF_PARALLELIZATION:
-					try {
-						globalDegreeOfParallelization = Integer.parseInt(pair
-								.getE2());
-					} catch (NumberFormatException e) {
-						throw new IllegalArgumentException();
+		ILogicalOperator logicalPlan = query.getLogicalPlan();
+
+		CopyLogicalGraphVisitor<ILogicalOperator> copyVisitor = new CopyLogicalGraphVisitor<ILogicalOperator>(
+				query);
+		GenericGraphWalker walker = new GenericGraphWalker();
+		walker.prefixWalk(logicalPlan, copyVisitor);
+		ILogicalOperator savedPlan = copyVisitor.getResult();
+		
+		try {
+			if (handlerParameters.size() != PARAMETER_COUNT) {
+				throw new IllegalArgumentException();
+			} else {
+				// Determine parameters
+				for (Pair<String, String> pair : handlerParameters) {
+					ParallelizationParameter parameter = ParallelizationParameter
+							.getParameterByName(pair.getE1());
+					switch (parameter) {
+					case GLOBAL_DEGREE_OF_PARALLELIZATION:
+						try {
+							globalDegreeOfParallelization = Integer
+									.parseInt(pair.getE2());
+						} catch (NumberFormatException e) {
+							throw new IllegalArgumentException();
+						}
+						break;
+					case GLOBAL_BUFFERSIZE:
+						try {
+							globalBufferSize = Integer.parseInt(pair.getE2());
+						} catch (NumberFormatException e) {
+							throw new IllegalArgumentException();
+						}
+						break;
+					default:
+						break;
 					}
-					break;
-				case GLOBAL_BUFFERSIZE:
-					try {
-						globalBufferSize = Integer.parseInt(pair.getE2());
-					} catch (NumberFormatException e) {
-						throw new IllegalArgumentException();
-					}
-					break;
-				default:
-					break;
 				}
-			}
 
-			List<String> operatorIds = new ArrayList<String>();
-			MultithreadedOperatorParameter multithreadedOperatorParameter = config
-					.get(MultithreadedOperatorParameter.class);
-			if (multithreadedOperatorParameter != null) {
-				operatorIds.addAll(multithreadedOperatorParameter
-						.getOperatorIds());
-			}
+				List<String> operatorIds = new ArrayList<String>();
+				MultithreadedOperatorParameter multithreadedOperatorParameter = config
+						.get(MultithreadedOperatorParameter.class);
+				if (multithreadedOperatorParameter != null) {
+					operatorIds.addAll(multithreadedOperatorParameter
+							.getOperatorIds());
+				}
 
-			// Transform Plan
-			ILogicalOperator logicalPlan = query.getLogicalPlan();
-			if (logicalPlan instanceof TopAO) {
-				Set<Class<? extends ILogicalOperator>> set = new HashSet<>();
-				List<Class<? extends ILogicalOperator>> validTypes = MultithreadedTransformationStrategyRegistry
-						.getValidTypes();
-				if (validTypes != null) {
-					set.addAll(validTypes);
-					CollectOperatorLogicalGraphVisitor<ILogicalOperator> collVisitor = new CollectOperatorLogicalGraphVisitor<>(
-							set, true);
-					GenericGraphWalker collectWalker = new GenericGraphWalker();
-					collectWalker.prefixWalk(logicalPlan, collVisitor);
+				// Transform Plan
 
-					for (ILogicalOperator operatorForTransformation : collVisitor
-							.getResult()) {
-						// operator has no custom settings or no settings are
-						// available, then choose best strategy and transform
-						if (!operatorIds.isEmpty()
-								&& (operatorForTransformation
-										.getUniqueIdentifier() == null || !operatorIds
-										.contains(operatorForTransformation
-												.getUniqueIdentifier()
-												.toLowerCase()))) {
-							// if we have an set of operatorIds, only this
-							// operators should be transformed. If then the id
-							// of the given operator is null or not in the set
-							// of operatorIds, no transformation is done
-							continue;
-						} else if (operatorIds.isEmpty()) {
-							IMultithreadedTransformationStrategy<? extends ILogicalOperator> bestStrategy = getBestStrategy(operatorForTransformation);
-							MultithreadedOperatorSettings settings = MultithreadedOperatorSettings
-									.createDefaultSettings(bestStrategy,
-											globalDegreeOfParallelization,
-											globalBufferSize);
-							bestStrategy.transform(operatorForTransformation,
-									settings);
-						} else if (operatorIds
-								.contains(operatorForTransformation
-										.getUniqueIdentifier().toLowerCase())) {
-							// if custom settings for operator
-							// are available
-							MultithreadedOperatorSettings settingsForOperator = multithreadedOperatorParameter
-									.getSettingsForOperator(operatorForTransformation
-											.getUniqueIdentifier());
-							if (settingsForOperator.hasMultithreadingStrategy()) {
-								IMultithreadedTransformationStrategy<? extends ILogicalOperator> selectedStrategy = MultithreadedTransformationStrategyRegistry
-										.getStrategiesByName(settingsForOperator
-												.getMultithreadingStrategy());
-								settingsForOperator
-										.doPostCalculationsForSettings(
-												selectedStrategy,
-												globalDegreeOfParallelization,
-												globalBufferSize);
 
-								if (selectedStrategy.getOperatorType() == operatorForTransformation
-										.getClass()) {
-									int compatibility = selectedStrategy
-											.evaluateCompatibility(operatorForTransformation);
-									if (compatibility > 0) {
-										selectedStrategy.transform(
-												operatorForTransformation,
-												settingsForOperator);
-									} else {
-										throw new IllegalArgumentException(
-												"Strategy "
-														+ selectedStrategy
-																.getName()
-														+ " is not compatible with selected operator with id "
-														+ operatorForTransformation
-																.getUniqueIdentifier());
-									}
-								} else {
-									throw new IllegalArgumentException(
-											"Strategy with name "
-													+ settingsForOperator
-															.getMultithreadingStrategy()
-													+ " is not compatible with Operator of type "
-													+ operatorForTransformation
-															.getClass());
-								}
+				if (logicalPlan instanceof TopAO) {
+					Set<Class<? extends ILogicalOperator>> set = new HashSet<>();
+					List<Class<? extends ILogicalOperator>> validTypes = MultithreadedTransformationStrategyRegistry
+							.getValidTypes();
+					if (validTypes != null) {
+						set.addAll(validTypes);
+						CollectOperatorLogicalGraphVisitor<ILogicalOperator> collVisitor = new CollectOperatorLogicalGraphVisitor<>(
+								set, true);
+						GenericGraphWalker collectWalker = new GenericGraphWalker();
+						collectWalker.prefixWalk(logicalPlan, collVisitor);
 
-							} else {
+						for (ILogicalOperator operatorForTransformation : collVisitor
+								.getResult()) {
+							// operator has no custom settings or no settings
+							// are
+							// available, then choose best strategy and
+							// transform
+							if (!operatorIds.isEmpty()
+									&& (operatorForTransformation
+											.getUniqueIdentifier() == null || !operatorIds
+											.contains(operatorForTransformation
+													.getUniqueIdentifier()
+													.toLowerCase()))) {
+								// if we have an set of operatorIds, only this
+								// operators should be transformed. If then the
+								// id
+								// of the given operator is null or not in the
+								// set
+								// of operatorIds, no transformation is done
+								continue;
+							} else if (operatorIds.isEmpty()) {
 								IMultithreadedTransformationStrategy<? extends ILogicalOperator> bestStrategy = getBestStrategy(operatorForTransformation);
-								settingsForOperator
-										.doPostCalculationsForSettings(
-												bestStrategy,
+								MultithreadedOperatorSettings settings = MultithreadedOperatorSettings
+										.createDefaultSettings(bestStrategy,
 												globalDegreeOfParallelization,
 												globalBufferSize);
 								bestStrategy.transform(
-										operatorForTransformation,
-										settingsForOperator);
+										operatorForTransformation, settings);
+							} else if (operatorIds
+									.contains(operatorForTransformation
+											.getUniqueIdentifier()
+											.toLowerCase())) {
+								// if custom settings for operator
+								// are available
+								MultithreadedOperatorSettings settingsForOperator = multithreadedOperatorParameter
+										.getSettingsForOperator(operatorForTransformation
+												.getUniqueIdentifier());
+								if (settingsForOperator
+										.hasMultithreadingStrategy()) {
+									IMultithreadedTransformationStrategy<? extends ILogicalOperator> selectedStrategy = MultithreadedTransformationStrategyRegistry
+											.getStrategiesByName(settingsForOperator
+													.getMultithreadingStrategy());
+									settingsForOperator
+											.doPostCalculationsForSettings(
+													selectedStrategy,
+													globalDegreeOfParallelization,
+													globalBufferSize);
+
+									if (selectedStrategy.getOperatorType() == operatorForTransformation
+											.getClass()) {
+										int compatibility = selectedStrategy
+												.evaluateCompatibility(operatorForTransformation);
+										if (compatibility > 0) {
+											selectedStrategy.transform(
+													operatorForTransformation,
+													settingsForOperator);
+										} else {
+											throw new IllegalArgumentException(
+													"Strategy "
+															+ selectedStrategy
+																	.getName()
+															+ " is not compatible with selected operator with id "
+															+ operatorForTransformation
+																	.getUniqueIdentifier());
+										}
+									} else {
+										throw new IllegalArgumentException(
+												"Strategy with name "
+														+ settingsForOperator
+																.getMultithreadingStrategy()
+														+ " is not compatible with Operator of type "
+														+ operatorForTransformation
+																.getClass());
+									}
+
+								} else {
+									IMultithreadedTransformationStrategy<? extends ILogicalOperator> bestStrategy = getBestStrategy(operatorForTransformation);
+									settingsForOperator
+											.doPostCalculationsForSettings(
+													bestStrategy,
+													globalDegreeOfParallelization,
+													globalBufferSize);
+									bestStrategy.transform(
+											operatorForTransformation,
+											settingsForOperator);
+								}
 							}
 						}
 					}
 				}
 			}
+		} catch (Exception e) {
+			query.setLogicalPlan(savedPlan, true);
+			throw e;
 		}
 	}
 
