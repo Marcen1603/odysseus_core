86006c997a4e73c9473cb6070c7e090e14d96dd2
Christian Kuka <christian@kuka.cc>
2015-06-21 11:10:32 +0000

    ODY-725 Add double click listener to create dashboardparts from dashboardpart view
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24668 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4ec8a39bc6d23b6f8f877163faa867fadb793450

--- client/dashboard/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/views/dashboardpart/DashboardPartView.java
+++ client/dashboard/de.uniol.inf.is.odysseus.rcp.dashboard/src/de/uniol/inf/is/odysseus/rcp/dashboard/views/dashboardpart/DashboardPartView.java
@@ -1,33 +1,52 @@
 /*******************************************************************************
- * Copyright (C) 2015  Christian Kuka <christian@kuka.cc>
+ * Copyright 2015 The Odysseus Team
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.uniol.inf.is.odysseus.rcp.dashboard.views.dashboardpart;
 
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.part.ViewPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.rcp.dashboard.DashboardHandlerException;
+import de.uniol.inf.is.odysseus.rcp.dashboard.DashboardPartRegistry;
+import de.uniol.inf.is.odysseus.rcp.dashboard.DashboardPlugIn;
+import de.uniol.inf.is.odysseus.rcp.dashboard.IDashboardPart;
+import de.uniol.inf.is.odysseus.rcp.dashboard.IDashboardPartHandler;
+import de.uniol.inf.is.odysseus.rcp.dashboard.handler.XMLDashboardPartHandler;
+import de.uniol.inf.is.odysseus.rcp.dashboard.queryprovider.ResourceFileQueryTextProvider;
+import de.uniol.inf.is.odysseus.rcp.editor.text.editors.OdysseusScriptEditor;
 
 /**
  * @author Christian Kuka <christian@kuka.cc>
@@ -56,6 +75,30 @@ public class DashboardPartView extends ViewPart {
         getTreeViewer().getControl().setMenu(contextMenu);
         getSite().registerContextMenu(menuManager, getTreeViewer());
 
+        getTreeViewer().addDoubleClickListener(new IDoubleClickListener() {
+
+            @Override
+            public void doubleClick(DoubleClickEvent event) {
+
+                if (event.getSelection().isEmpty()) {
+                    return;
+                }
+                if (event.getSelection() instanceof IStructuredSelection) {
+                    IStructuredSelection selection = (IStructuredSelection) event.getSelection();
+                    for (Iterator<?> iterator = selection.iterator(); iterator.hasNext();) {
+                        IConfigurationElement dashboardPart = (IConfigurationElement) iterator.next();
+                        try {
+                            createDashboardPart(dashboardPart);
+                        }
+                        catch (InstantiationException | CoreException | DashboardHandlerException e) {
+                            LOG.error(e.getMessage(), e);
+                        }
+
+                    }
+                }
+
+            }
+        });
     }
 
     @Override
@@ -81,6 +124,7 @@ public class DashboardPartView extends ViewPart {
                     try {
                         IConfigurationElement[] extensions = Platform.getExtensionRegistry().getConfigurationElementsFor("de.uniol.inf.is.odysseus.rcp.DashboardPart");
                         getTreeViewer().setInput(extensions);
+
                     }
                     catch (Exception e) {
                         LOG.error("Exception during setting input for treeViewer in dashboard part list", e);
@@ -95,4 +139,23 @@ public class DashboardPartView extends ViewPart {
         this.viewer = viewer;
     }
 
+    public void createDashboardPart(IConfigurationElement element) throws InstantiationException, CoreException, DashboardHandlerException {
+        IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+        if (editor instanceof OdysseusScriptEditor) {
+            ResourceFileQueryTextProvider query = new ResourceFileQueryTextProvider(((OdysseusScriptEditor) editor).getFile());
+            String name = element.getAttribute("name");
+            final IDashboardPart dashboardPart = DashboardPartRegistry.createDashboardPart(name);
+            final IPath path = query.getFile().getParent().getFullPath().append("New DashboardPart" + "." + DashboardPlugIn.DASHBOARD_PART_EXTENSION);
+            final IFile dashboardPartFile = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+
+            dashboardPartFile.create(null, true, null);
+
+            dashboardPart.setQueryTextProvider(query);
+
+            final IDashboardPartHandler handler = new XMLDashboardPartHandler();
+            handler.save(dashboardPart, dashboardPartFile);
+
+            PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(new FileEditorInput(dashboardPartFile), DashboardPlugIn.DASHBOARD_PART_EDITOR_ID, true);
+        }
+    }
 }
