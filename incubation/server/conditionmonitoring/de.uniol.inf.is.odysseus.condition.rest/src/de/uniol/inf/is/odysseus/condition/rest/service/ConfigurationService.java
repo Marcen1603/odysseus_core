35c9201c58fe9822d9117a03c7da57f1cec8d807
Tobias Leo Brandt <tobias.leo.brandt@uni-oldenburg.de>
2015-06-22 13:53:13 +0000

    CM: The client can connect even if the queries are already running
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24679 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 01aa025ed4eaa83004708205ca10570ded6ec773

--- incubation/server/conditionmonitoring/de.uniol.inf.is.odysseus.condition.rest/src/de/uniol/inf/is/odysseus/condition/rest/service/ConfigurationService.java
+++ incubation/server/conditionmonitoring/de.uniol.inf.is.odysseus.condition.rest/src/de/uniol/inf/is/odysseus/condition/rest/service/ConfigurationService.java
@@ -9,6 +9,7 @@ import de.uniol.inf.is.odysseus.condition.rest.datatypes.Configuration;
 import de.uniol.inf.is.odysseus.condition.rest.datatypes.QueryInformation;
 import de.uniol.inf.is.odysseus.condition.rest.datatypes.ServerConfiguration;
 import de.uniol.inf.is.odysseus.core.collection.Context;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.QueryParseException;
 import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
 
 public class ConfigurationService {
@@ -22,29 +23,48 @@ public class ConfigurationService {
 			// First the sources from files
 			for (QueryInformation sourcePath : serverConfig.getSourcePaths()) {
 				String content = new String(Files.readAllBytes(Paths.get(sourcePath.getContent())));
-				ExecutorServiceBinding.getExecutor().addQuery(content, sourcePath.getParser(), session, new Context());
+				try {
+					ExecutorServiceBinding.getExecutor().addQuery(content, sourcePath.getParser(), session,
+							new Context());
+				} catch (QueryParseException e) {
+					e.printStackTrace();
+				}
 			}
 
 			// Then sources from strings
 			for (QueryInformation source : serverConfig.getSources()) {
-				ExecutorServiceBinding.getExecutor().addQuery(source.getContent(), source.getParser(), session,
-						new Context());
+				try {
+					ExecutorServiceBinding.getExecutor().addQuery(source.getContent(), source.getParser(), session,
+							new Context());
+				} catch (QueryParseException e) {
+					e.printStackTrace();
+				}
 			}
 
 			// After the sources we can start the queries
 			for (QueryInformation query : serverConfig.getQueryPaths()) {
 				String content = new String(Files.readAllBytes(Paths.get(query.getContent())));
-				ExecutorServiceBinding.getExecutor().addQuery(content, query.getParser(), session, new Context());
+				try {
+					ExecutorServiceBinding.getExecutor().addQuery(content, query.getParser(), session, new Context());
+				} catch (QueryParseException e) {
+					e.printStackTrace();
+				}
 			}
 
 			// Last but not least the queries from strings
 			for (QueryInformation query : serverConfig.getQueries()) {
-				ExecutorServiceBinding.getExecutor().addQuery(query.getContent(), query.getParser(), session,
-						new Context());
+				try {
+					ExecutorServiceBinding.getExecutor().addQuery(query.getContent(), query.getParser(), session,
+							new Context());
+				} catch (QueryParseException e) {
+					e.printStackTrace();
+				}
 			}
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (QueryParseException e) {
 			e.printStackTrace();
 		}
 
