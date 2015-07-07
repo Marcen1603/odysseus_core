cec5d126ddc2e37d87263c228b22ca3bcd306dbb
Henrik Surm <hsurm@uni-oldenburg.de>
2015-07-07 07:16:44 +0000

    Removed duplicate project
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24840 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b6a7536e011340e9f274b62875b9120f6461b593

--- incubation/common/sensormanagement.common/src/de/uniol/inf/is/odysseus/sensormanagement/common/utilities/SimpleCallbackListener.java
+++
@@ -1,7 +0,0 @@
-package de.uniol.inf.is.odysseus.sensormanagement.common.utilities;
-
-public abstract class SimpleCallbackListener<Base, Type> implements CallbackListener<Base, Type>
-{
-	@Override public void listeningStarted(Base sender) {}
-	@Override public void listeningStopped(Base sender) {}
-}
--- incubation/common/sensormanagement.common/src/de/uniol/inf/is/odysseus/sensormanagement/common/utilities/XmlMarshalHelperHandler.java
+++
@@ -1,9 +0,0 @@
-package de.uniol.inf.is.odysseus.sensormanagement.common.utilities;
-
-import java.io.File;
-
-public interface XmlMarshalHelperHandler {
-
-	void onUnmarshalling(File xmlFile);
-	void onUnmarshalling(String xmlString);
-}
