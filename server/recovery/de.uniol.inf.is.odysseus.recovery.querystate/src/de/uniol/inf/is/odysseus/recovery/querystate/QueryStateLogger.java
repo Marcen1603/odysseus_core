c85b853e603b4a2b1d1a2f57b38580cb47a6f36d
Michael Brand <michael.brand@uni-oldenburg.de>
2015-07-09 12:16:42 +0000

    Recovery: Logging of query states
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24901 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 45e5aac4200542532ee5b6bfd5b436fda3939d99

---
+++ server/recovery/de.uniol.inf.is.odysseus.recovery.querystate/src/de/uniol/inf/is/odysseus/recovery/querystate/QueryStateLogger.java
@@ -0,0 +1,173 @@
+package de.uniol.inf.is.odysseus.recovery.querystate;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+
+import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.IServerExecutor;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.eventhandling.planmodification.IPlanModificationListener;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.eventhandling.planmodification.event.AbstractPlanModificationEvent;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.eventhandling.planmodification.event.PlanModificationEventType;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.eventhandling.queryadded.IQueryAddedListener;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.query.IPhysicalQuery;
+import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
+import de.uniol.inf.is.odysseus.recovery.querystate.queryaddedInfo.AbstractQueryAddedInfo;
+import de.uniol.inf.is.odysseus.recovery.querystate.queryaddedInfo.QueryAddedEntryInfo;
+import de.uniol.inf.is.odysseus.recovery.querystate.queryaddedInfo.SourceAddedEntryInfo;
+import de.uniol.inf.is.odysseus.recovery.systemlog.ISystemLog;
+
+/**
+ * Class to add the following actions to the system log: adding of a query (or
+ * source definition) and changing the state of a query.
+ * 
+ * @author Michael Brand
+ *
+ */
+public class QueryStateLogger implements IQueryAddedListener,
+		IPlanModificationListener {
+
+	/**
+	 * The logger for this class.
+	 */
+	private static final Logger cLog = LoggerFactory
+			.getLogger(QueryStateLogger.class);
+
+	/**
+	 * The system log, if bound.
+	 */
+	private static Optional<ISystemLog> cSystemLog = Optional.absent();
+
+	/**
+	 * Binds an implementation of the system log.
+	 * 
+	 * @param log
+	 *            The implementation to bind.
+	 */
+	public static void bindSystemLog(ISystemLog log) {
+		cSystemLog = Optional.of(log);
+		cLog.debug("Bound '{}' as an implementation of ISystemLog.", log
+				.getClass().getSimpleName());
+	}
+
+	/**
+	 * Unbinds an implementation of the system log.
+	 * 
+	 * @param log
+	 *            The implementation to unbind.
+	 */
+	public static void unbindSystemLog(ISystemLog log) {
+		if (cSystemLog.isPresent() && log == cSystemLog.get()) {
+			cSystemLog = Optional.absent();
+			cLog.debug("Unbound '{}' as an implementation of ISystemLog.", log
+					.getClass().getSimpleName());
+		}
+	}
+
+	/**
+	 * The executor, if bound.
+	 */
+	private static Optional<IServerExecutor> cExecutor;
+
+	/**
+	 * Binds an implementation of the executor.
+	 * 
+	 * @param executor
+	 *            The implementation to bind.
+	 */
+	public void bindExecutor(IExecutor executor) {
+		IServerExecutor serverExecutor = (IServerExecutor) executor;
+		serverExecutor.addQueryAddedListener(this);
+		serverExecutor.addPlanModificationListener(this);
+		cExecutor = Optional.of(serverExecutor);
+	}
+
+	/**
+	 * Unbinds an implementation of the executor.
+	 * 
+	 * @param executor
+	 *            The implementation to unbind.
+	 */
+	public void unbindExecutor(IExecutor executor) {
+		if (cExecutor.isPresent() && cExecutor.get() == executor) {
+			IServerExecutor serverExecutor = (IServerExecutor) executor;
+			serverExecutor.removeQueryAddedListener(this);
+			serverExecutor.removePlanModificationListener(this);
+			cExecutor = Optional.absent();
+		}
+
+	}
+
+	/**
+	 * The tag for the system log entry for an added query.
+	 */
+	public static final String TAG_QUERYADDED = "QUERYADDED";
+
+	/**
+	 * Gets the tag for the system log entry for a changed query state.
+	 * 
+	 * @param The
+	 *            id of the query.
+	 */
+	public static final String getTagQueryRemoved(int id) {
+		return "QUERYSTATECHANGED " + id;
+	}
+
+	/**
+	 * The comment for the system log entry for a removed query.
+	 */
+	public static final String COMMENT_QUERYREMOVED = "REMOVED";
+
+	@Override
+	public void queryAddedEvent(String query, List<Integer> queryIds,
+			String buildConfig, String parserID, ISession user) {
+		AbstractQueryAddedInfo entry;
+		if (queryIds.isEmpty()) {
+			// Source definition
+			entry = new SourceAddedEntryInfo();
+			cLog.debug("Source added: " + query);
+		} else {
+			entry = new QueryAddedEntryInfo();
+			((QueryAddedEntryInfo) entry).ids = Lists.newArrayList(queryIds);
+			cLog.debug("Query added: " + query);
+		}
+		entry.parserId = parserID;
+		entry.queryText = query;
+		entry.buildConfiguration = buildConfig;
+		entry.session = user;
+
+		// Write to log
+		if (cSystemLog.isPresent()) {
+			cSystemLog.get().write(TAG_QUERYADDED, System.nanoTime(),
+					entry.toBase64Binary());
+		}
+	}
+
+	@Override
+	public void planModificationEvent(AbstractPlanModificationEvent<?> eventArgs) {
+		if (!PlanModificationEventType.QUERY_ADDED.equals(eventArgs
+				.getEventType())
+				&& !PlanModificationEventType.QUERY_REOPTIMIZE.equals(eventArgs
+						.getEventType())) {
+			// Query state changed
+			IPhysicalQuery query = (IPhysicalQuery) eventArgs.getValue();
+			int queryId = query.getID();
+			String comment;
+			if (PlanModificationEventType.QUERY_REMOVE.equals(eventArgs
+					.getEventType())) {
+				comment = "REMOVED";
+			} else {
+				comment = query.getState().toString();
+			}
+			if (cSystemLog.isPresent()) {
+				cSystemLog.get().write(getTagQueryRemoved(queryId),
+						System.nanoTime(), comment);
+			}
+		}
+	}
+
+}
---
+++ server/recovery/de.uniol.inf.is.odysseus.recovery.querystate/src/de/uniol/inf/is/odysseus/recovery/querystate/queryaddedInfo/AbstractQueryAddedInfo.java
@@ -0,0 +1,97 @@
+package de.uniol.inf.is.odysseus.recovery.querystate.queryaddedInfo;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
+
+/**
+ * All information needed to recover a query added event (normal queries as well
+ * as source definitions). <br />
+ * To write those information to the system log and to read them,
+ * {@link #toBase64Binary()} and {@link #fromBase64Binary(String)} can be used.
+ * 
+ * @author Michael Brand
+ *
+ */
+public abstract class AbstractQueryAddedInfo implements Serializable {
+
+	/**
+	 * The version of this class for serialization.
+	 */
+	private static final long serialVersionUID = 4007923324539604364L;
+
+	/**
+	 * The logger for this class.
+	 */
+	private static final Logger cLog = LoggerFactory
+			.getLogger(AbstractQueryAddedInfo.class);
+
+	/**
+	 * The parser id.
+	 */
+	public String parserId;
+
+	/**
+	 * The query build configuration.
+	 */
+	public String buildConfiguration;
+
+	/**
+	 * The full query text (e.g., Odysseus Script).
+	 */
+	public String queryText;
+
+	/**
+	 * The current session (and corresponding user).
+	 */
+	public ISession session;
+
+	/**
+	 * Encode to a Base64Binary.
+	 * 
+	 * @return A string representing the binary.
+	 */
+	public String toBase64Binary() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+			oos.writeObject(this);
+			return new String(DatatypeConverter.printBase64Binary(baos
+					.toByteArray()));
+		} catch (IOException e) {
+			cLog.error("Info is not serializable!", e);
+			return null;
+		}
+	}
+
+	/**
+	 * Decode from a Base64Binary.
+	 * 
+	 * @param str
+	 *            A string representing the binary.
+	 * @return The decoded information.
+	 */
+	public static AbstractQueryAddedInfo fromBase64Binary(String str) {
+		byte[] data = DatatypeConverter.parseBase64Binary(str);
+		try (ObjectInputStream ois = new ObjectInputStream(
+				new ByteArrayInputStream(data))) {
+			return (AbstractQueryAddedInfo) ois.readObject();
+		} catch (IOException e) {
+			cLog.error("Info is not serializable!", e);
+			return null;
+		} catch (ClassNotFoundException e) {
+			cLog.error("Unknown class for decoding!", e);
+			return null;
+		}
+	}
+
+}
