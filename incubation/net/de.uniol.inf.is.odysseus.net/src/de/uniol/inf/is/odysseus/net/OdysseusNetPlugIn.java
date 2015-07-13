ebbf1308aeb15911ccf464673d5b4dab56caa16d
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-07-13 15:01:36 +0000

    Start of OdysseusNet: First (empty) base-project
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24953 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 604e621957e2566a325126b3cf913c39bdfcfece

---
+++ incubation/net/de.uniol.inf.is.odysseus.net/src/de/uniol/inf/is/odysseus/net/OdysseusNetPlugIn.java
@@ -0,0 +1,14 @@
+package de.uniol.inf.is.odysseus.net;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class OdysseusNetPlugIn implements BundleActivator {
+
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+	
+}
