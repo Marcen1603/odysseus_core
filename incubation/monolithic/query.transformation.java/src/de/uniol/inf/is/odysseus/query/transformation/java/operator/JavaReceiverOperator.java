5da271e66f5feffc2396bbce77baf4b9f551b538
Mark Preuschaft <mpreuschaft@uni-oldenburg.de>
2015-07-14 10:03:12 +0000

    add stream support (testing)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24975 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 51551ec977c92bb3a5abeae882a671811b9879cc

---
+++ incubation/monolithic/query.transformation.java/src/de/uniol/inf/is/odysseus/query/transformation/java/operator/JavaReceiverOperator.java
@@ -0,0 +1,78 @@
+package de.uniol.inf.is.odysseus.query.transformation.java.operator;
+
+import java.io.IOException;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportDirection;
+import de.uniol.inf.is.odysseus.core.server.datadictionary.DataDictionaryProvider;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.AccessAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.StreamAO;
+import de.uniol.inf.is.odysseus.core.server.physicaloperator.access.push.ReceiverPO;
+import de.uniol.inf.is.odysseus.core.server.usermanagement.UserManagementProvider;
+import de.uniol.inf.is.odysseus.core.usermanagement.ITenant;
+import de.uniol.inf.is.odysseus.query.transformation.java.mapping.TransformationInformation;
+import de.uniol.inf.is.odysseus.query.transformation.java.model.ProtocolHandlerParameter;
+import de.uniol.inf.is.odysseus.query.transformation.java.utils.CreateDefaultCode;
+import de.uniol.inf.is.odysseus.query.transformation.operator.AbstractTransformationOperator;
+import de.uniol.inf.is.odysseus.query.transformation.operator.CodeFragmentInfo;
+
+public class JavaReceiverOperator extends AbstractTransformationOperator{
+	
+	public JavaReceiverOperator(){
+		 this.implClass = ReceiverPO.class;
+		 this.name = "StreamAO";
+		 this.targetPlatform = "Java";
+			defaultImports();
+	}
+
+	@Override
+	public CodeFragmentInfo getCode(ILogicalOperator operator) {
+		CodeFragmentInfo receiverPO = new CodeFragmentInfo();
+		StringBuilder code = new StringBuilder();
+		StreamAO streamAO = (StreamAO)operator;
+		
+		String operatorVariable = TransformationInformation.getInstance().getVariable(operator);
+		
+		ITenant tenant = UserManagementProvider.getDefaultTenant();
+		ILogicalOperator logicalPlan = DataDictionaryProvider.getDataDictionary(tenant).getStreamForTransformation(streamAO.getStreamname(),  null);
+
+		AccessAO accessAO = (AccessAO)logicalPlan;
+		
+	
+		 String transportHandler = accessAO.getTransportHandler();
+		 String dataHandler = accessAO.getDataHandler();
+		 String wrapper = accessAO.getWrapper();
+		 String protocolHandler = accessAO.getProtocolHandler();
+		 ITransportDirection direction = ITransportDirection.IN;
+		 
+		 ProtocolHandlerParameter protocolHandlerParameter = new ProtocolHandlerParameter(null,transportHandler,dataHandler,wrapper,protocolHandler);
+		
+		 CodeFragmentInfo codeAccessFramwork = CreateDefaultCode.codeForAccessFrameworkNeu(protocolHandlerParameter, accessAO.getOptionsMap(),operator, direction);
+		 receiverPO.addCodeFragmentInfo(codeAccessFramwork);
+		 
+		 
+		//now create the AccessPO
+			code.append("ReceiverPO "+operatorVariable+"PO = new ReceiverPO("+operatorVariable+"ProtocolHandler,0);");
+			code.append("\n");
+			code.append(operatorVariable+"PO.setOutputSchema("+operatorVariable+"SDFSchema);");
+			code.append("\n");
+			code.append("\n");
+		 
+			
+			receiverPO.addCode(code.toString());
+	
+			receiverPO.addImport(ReceiverPO.class.getName());
+
+			receiverPO.addImport(IOException.class.getName());
+			
+			
+		return receiverPO;
+	}
+
+	@Override
+	public void defineImports() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
