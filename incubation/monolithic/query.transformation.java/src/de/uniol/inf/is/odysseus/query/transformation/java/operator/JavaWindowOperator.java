1d72645e43de1d95b4773247ed20f3d07435ae6d
Mark Preuschaft <mpreuschaft@uni-oldenburg.de>
2015-07-14 07:50:26 +0000

    change TimeWindow
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24973 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 1f0c22c5320f697e3c37ce10b2334f9ef6c8aac1

--- incubation/monolithic/query.transformation.java/src/de/uniol/inf/is/odysseus/query/transformation/java/operator/JavaWindowOperator.java
+++ incubation/monolithic/query.transformation.java/src/de/uniol/inf/is/odysseus/query/transformation/java/operator/JavaWindowOperator.java
@@ -1,16 +1,13 @@
 package de.uniol.inf.is.odysseus.query.transformation.java.operator;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
-import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.AbstractWindowWithWidthAO;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.WindowType;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.TimeValueItem;
 import de.uniol.inf.is.odysseus.query.transformation.java.mapping.TransformationInformation;
-import de.uniol.inf.is.odysseus.query.transformation.java.utils.TransformSDFAttribute;
 import de.uniol.inf.is.odysseus.query.transformation.java.utils.TransformSDFSchema;
 import de.uniol.inf.is.odysseus.query.transformation.operator.AbstractTransformationOperator;
 import de.uniol.inf.is.odysseus.query.transformation.operator.CodeFragmentInfo;
@@ -29,7 +26,7 @@ public class JavaWindowOperator extends AbstractTransformationOperator {
 	public CodeFragmentInfo getCode(ILogicalOperator operator) {
 		CodeFragmentInfo slidingWindow = new CodeFragmentInfo();
 		
-		
+	
 		StringBuilder code = new StringBuilder();
 
 		String operatorVariable = TransformationInformation.getInstance()
@@ -37,43 +34,18 @@ public class JavaWindowOperator extends AbstractTransformationOperator {
 
 		AbstractWindowWithWidthAO windowAO = (AbstractWindowWithWidthAO) operator;
 		
-		WindowType windowType = windowAO.getWindowType();
 		TimeUnit baseTimeUnit =windowAO.getBaseTimeUnit();
 		TimeValueItem windowSize = windowAO.getWindowSize();
 		TimeValueItem windowAdvance = windowAO.getWindowAdvance();
-		TimeValueItem windowSlide = windowAO.getWindowSlide();
-		boolean partioned = windowAO.isPartitioned();
-		List<SDFAttribute> partitionedBy = windowAO.getPartitionBy();
 		SDFSchema inputSchema = windowAO.getInputSchema();
 		
-		
-		 if(partioned){
-				CodeFragmentInfo sdfAttributeInfo = TransformSDFAttribute.getCodeForSDFAttribute(partitionedBy, operatorVariable+"PartitionBy");
-				code.append(sdfAttributeInfo.getCode());
-		}
-		 
 		 CodeFragmentInfo sdfInputSchema  = TransformSDFSchema.getCodeForSDFSchema(inputSchema, operatorVariable+"InputSchema");
 		 code.append(sdfInputSchema.getCode());
 	
 		 code.append("SlidingAdvanceTimeWindowTIPO "+operatorVariable+"PO = new SlidingAdvanceTimeWindowTIPO(");
-		 code.append("WindowType."+windowType.toString().toUpperCase()+",");
 		 code.append("TimeUnit."+baseTimeUnit.toString().toUpperCase()+",");
 		 code.append("new TimeValueItem("+windowSize.getTime()+",TimeUnit.valueOf(\""+windowSize.getUnit().toString()+"\")),");
 		 code.append("new TimeValueItem("+windowAdvance.getTime()+",TimeUnit.valueOf(\""+windowAdvance.getUnit().toString()+"\")),");
-		 
-		 if(windowSlide!=null){
-			 code.append("new TimeValueItem("+windowSlide.getTime()+",TimeUnit.valueOf(\""+windowSlide.getUnit().toString()+"\",");
-		 }else{
-			 code.append("null,");
-		 }
-		 code.append(partioned+",");
-		 
-		 if(partioned){
-			     code.append(operatorVariable+"PartitionBySDFAttributeList,");
-			}else{
-			     code.append("null,");
-		 }
-		
 	     code.append(operatorVariable+"InputSchemaSDFSchema");
 		 code.append(");");
 		
