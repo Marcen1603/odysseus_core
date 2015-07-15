23f094a333152e7268ea16a8091b843c5d5936a3
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-07-15 12:35:34 +0000

    Net: Added LOG-statements (#2)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@24999 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 050a4f7d5ebea87d11fb33242e1f3b327a1330ef

--- incubation/net/de.uniol.inf.is.odysseus.net.discovery/src/de/uniol/inf/is/odysseus/net/discovery/impl/NodeDiscovererBinder.java
+++ incubation/net/de.uniol.inf.is.odysseus.net.discovery/src/de/uniol/inf/is/odysseus/net/discovery/impl/NodeDiscovererBinder.java
@@ -2,6 +2,9 @@ package de.uniol.inf.is.odysseus.net.discovery.impl;
 
 import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.collect.Lists;
 
 import de.uniol.inf.is.odysseus.net.discovery.INodeDiscoverer;
@@ -9,16 +12,24 @@ import de.uniol.inf.is.odysseus.net.discovery.INodeDiscovererManager;
 
 public class NodeDiscovererBinder {
 
+	private static final Logger LOG = LoggerFactory.getLogger(NodeDiscovererBinder.class);
+	
 	private static final Collection<INodeDiscoverer> CACHE = Lists.newArrayList();
 	private static INodeDiscovererManager discovererManager;
 
 	// called by OSGi-DS
 	public static void bindNodeDiscoverer(INodeDiscoverer serv) {
 		synchronized( CACHE ) {
+			LOG.info("Bound node discoverer {}", serv);
+			
 			if( discovererManager == null ) {
 				CACHE.add(serv);
+				LOG.debug("...into cache");
+				
 			} else {
 				discovererManager.add(serv);
+				LOG.debug("...into discoverer manager");
+				
 			}
 		}
 	}
@@ -27,6 +38,7 @@ public class NodeDiscovererBinder {
 	public static void unbindNodeDiscoverer(INodeDiscoverer serv) {
 		synchronized( CACHE ) {
 			CACHE.remove(serv);
+			LOG.info("Unbound node discoverer {}", serv);
 			
 			if( discovererManager != null ) {
 				discovererManager.remove(serv);
@@ -38,12 +50,17 @@ public class NodeDiscovererBinder {
 	public static void bindNodeDiscovererManager(INodeDiscovererManager serv) {
 		synchronized( CACHE ) {
 			discovererManager = serv;
+			LOG.info("Bound discoverer manager");
 			
-			for( INodeDiscoverer discoverer : CACHE) {
-				discovererManager.add(discoverer);
+			if( !CACHE.isEmpty() ) {
+				LOG.debug("Emptying cache of {} discoverers", CACHE.size());
+				
+				for( INodeDiscoverer discoverer : CACHE) {
+					discovererManager.add(discoverer);
+				}
+				
+				CACHE.clear();
 			}
-			
-			CACHE.clear();
 		}
 	}
 
@@ -52,6 +69,7 @@ public class NodeDiscovererBinder {
 		synchronized( CACHE ) {
 			if (discovererManager == serv) {
 				discovererManager = null;
+				LOG.info("Unbound discoverer manager");
 				
 				CACHE.clear();
 			}			
