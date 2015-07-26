dfa7b6ae2419a485f229da2e644b4c382ee6f3ce
Michael Brand <michael.brand@uni-oldenburg.de>
2015-07-26 13:39:37 +0000

    Kafka: removed source. Seems not to work correctly, if it is in a bundle of it's own. BaDaSt: added Kafka jar
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@25143 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: b51e4710c6627b7cfeb77b13ba5f6f94324c3131

--- application/BaDaSt/de.uniol.inf.is.odysseus.badast/src/de/uniol/inf/is/odysseus/badast/BaDaStApplication.java
+++ application/BaDaSt/de.uniol.inf.is.odysseus.badast/src/de/uniol/inf/is/odysseus/badast/BaDaStApplication.java
@@ -4,17 +4,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import kafka.server.KafkaConfig;
-import kafka.server.KafkaServerStartable;
-
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.zookeeper.server.ServerConfig;
 import org.apache.zookeeper.server.ZooKeeperServerMain;
 import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
-import org.eclipse.equinox.app.IApplication;
-import org.eclipse.equinox.app.IApplicationContext;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServerStartable;
 
 /**
  * BaDaSt - Backup of Data Streams application. <br />
@@ -24,66 +24,58 @@ import org.slf4j.LoggerFactory;
  * @author Michael Brand
  *
  */
-public class BaDaStApplication implements IApplication {
+public class BaDaStApplication implements BundleActivator {
 
 	/**
 	 * The logger for this class.
 	 */
-	private static final Logger cLog = LoggerFactory
-			.getLogger(BaDaStApplication.class);
-	
+	private static final Logger cLog = LoggerFactory.getLogger(BaDaStApplication.class);
+
 	/**
 	 * The only instance.
 	 */
 	private static BaDaStApplication cInstance;
-	
+
 	/**
 	 * Gets the only instance.
+	 * 
 	 * @return The application.
 	 */
 	public static BaDaStApplication getInstance() {
-		if(cInstance == null) {
+		if (cInstance == null) {
 			cInstance = new BaDaStApplication();
 		}
 		return cInstance;
 	}
 
 	/**
-	 * Starts first a ZooKeeper server and afterwards a Kafka server.
-	 * 
-	 * @param args
-	 *            No functionality.
-	 */
-	public static void main(String[] args) {
-		getInstance().run();
-	}
-	
-	/**
 	 * The host name.
 	 */
 	private String mHost = "localhost";
-	
+
 	/**
 	 * Gets the host.
+	 * 
 	 * @return The host name of the Kafka server.
 	 */
 	public String getHost() {
 		return this.mHost;
 	}
-	
+
 	/**
 	 * The port number.
 	 */
 	private int mPort = 9092;
-	
+
 	/**
 	 * Gets the port.
+	 * 
 	 * @return The port number of the Kafka server.
 	 */
 	public int getPort() {
 		return this.mPort;
 	}
-	
+
 	/**
 	 * The Kafka Server.
 	 */
@@ -104,8 +96,7 @@ public class BaDaStApplication implements IApplication {
 	 * /config/log4j.properties.
 	 */
 	private void configureLogging() {
-		PropertyConfigurator.configure(BaDaStApplication.class.getClassLoader()
-				.getResource("log4j.properties"));
+		PropertyConfigurator.configure(BaDaStApplication.class.getClassLoader().getResource("config/log4j.properties"));
 	}
 
 	/**
@@ -115,7 +106,7 @@ public class BaDaStApplication implements IApplication {
 	private void startZooKeeperServer() {
 		final Properties props = new Properties();
 		try (InputStream stream = BaDaStApplication.class.getClassLoader()
-				.getResourceAsStream("zookeeper.properties")) {
+				.getResourceAsStream("config/zookeeper.properties")) {
 			props.load(stream);
 		} catch (IOException e) {
 			cLog.error("ZooKeeper failed!", e);
@@ -143,7 +134,7 @@ public class BaDaStApplication implements IApplication {
 	}
 
 	/**
-	 * Waits one second. 
+	 * Waits one second.
 	 */
 	private void wait_() {
 		try {
@@ -160,18 +151,17 @@ public class BaDaStApplication implements IApplication {
 	 */
 	private void startKafkaServer() {
 		final Properties props = new Properties();
-		try (InputStream stream = BaDaStApplication.class.getClassLoader()
-				.getResourceAsStream("kafka.properties")) {
+		try (InputStream stream = BaDaStApplication.class.getClassLoader().getResourceAsStream("config/kafka.properties")) {
 			props.load(stream);
 		} catch (IOException e) {
 			cLog.error("Kafka failed!", e);
 			return;
 		}
-		
-		if(props.containsKey("host.name")) {
+
+		if (props.containsKey("host.name")) {
 			this.mHost = props.getProperty("host.name");
 		}
-		if(props.containsKey("port")) {
+		if (props.containsKey("port")) {
 			this.mPort = Integer.parseInt(props.getProperty("port"));
 		}
 
@@ -181,8 +171,7 @@ public class BaDaStApplication implements IApplication {
 			public void run() {
 				try {
 					KafkaConfig config = new KafkaConfig(props);
-					BaDaStApplication.this.mKafkaServer = new KafkaServerStartable(
-							config);
+					BaDaStApplication.this.mKafkaServer = new KafkaServerStartable(config);
 					BaDaStApplication.this.mKafkaServer.startup();
 				} catch (Exception e) {
 					cLog.error("Kafka failed!", e);
@@ -194,16 +183,18 @@ public class BaDaStApplication implements IApplication {
 	}
 
 	@Override
-	public Object start(IApplicationContext context) throws Exception {
-		context.applicationRunning();
-		getInstance().run();
-		return IApplicationContext.EXIT_ASYNC_RESULT;
+	public void start(BundleContext context) throws Exception {
+		cInstance = this;
+		run();
 	}
 
 	@Override
-	public void stop() {
-		if(this.mKafkaServer != null) {
+	public void stop(BundleContext context) throws Exception {
+		if (this.mKafkaServer != null) {
 			this.mKafkaServer.shutdown();
+		}
+		if (cInstance != null && cInstance == this) {
+			cInstance = null;
 		}
 	}
 
