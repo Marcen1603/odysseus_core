d8b6f7720dd45c69af65c6b42ff02e5837533d18
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-07-27 10:25:59 +0000

    Bugfix in Spelling ElementWindow
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@25153 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 6861be552628d9b1906963eb2d5cb1f8b26731b3

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/builder/CreateSDFAttributeParameter.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/builder/CreateSDFAttributeParameter.java
@@ -31,20 +31,17 @@ import de.uniol.inf.is.odysseus.core.sdf.unit.SDFUnit;
 import de.uniol.inf.is.odysseus.core.server.datadictionary.DataDictionaryException;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.QueryParseException;
 
-public class CreateSDFAttributeParameter extends
-		AbstractParameter<SDFAttribute> {
+public class CreateSDFAttributeParameter extends AbstractParameter<SDFAttribute> {
 
 	@SuppressWarnings("unused")
-	private static final Logger LOG = LoggerFactory
-			.getLogger(CreateSDFAttributeParameter.class);
+	private static final Logger LOG = LoggerFactory.getLogger(CreateSDFAttributeParameter.class);
 
 	private static final long serialVersionUID = -544787040358885000L;
 
 	public CreateSDFAttributeParameter() {
 	}
 
-	public CreateSDFAttributeParameter(String name, REQUIREMENT requirement,
-			USAGE usage) {
+	public CreateSDFAttributeParameter(String name, REQUIREMENT requirement, USAGE usage) {
 		super(name, requirement, usage);
 	}
 
@@ -70,14 +67,11 @@ public class CreateSDFAttributeParameter extends
 		List<String> list = (List<String>) l1;
 
 		if (list.size() == 3) {
-			setValue(determineAttribute(list.get(0), list.get(1), list.get(2),
-					constraintList));
+			setValue(determineAttribute(list.get(0), list.get(1), list.get(2), constraintList));
 		} else if (list.size() == 2) {
-			setValue(determineAttribute(null, list.get(0), list.get(1),
-					constraintList));
+			setValue(determineAttribute(null, list.get(0), list.get(1), constraintList));
 		} else {
-			throw new IllegalArgumentException(
-					"Wrong number of inputs for SDFAttribute. Expecting [sourcename] attributename and datatype [constraintlist].");
+			throw new IllegalArgumentException("Wrong number of inputs for SDFAttribute. Expecting [sourcename] attributename and datatype [constraintlist].");
 		}
 	}
 
@@ -89,29 +83,23 @@ public class CreateSDFAttributeParameter extends
 		if (!Strings.isNullOrEmpty(getValue().getSourceName())) {
 			sb.append("'").append(getValue().getSourceName()).append("',");
 		}
-		sb.append("'").append(attributeFullName).append("','")
-				.append(getValue().getDatatype().getURI()).append("'");
-		if (getValue().getUnit() != null
-				|| getValue().getDtConstraints().size() > 0) {
+		sb.append("'").append(attributeFullName).append("','").append(getValue().getDatatype().getURI()).append("'");
+		if (getValue().getUnit() != null || getValue().getDtConstraints().size() > 0) {
 			sb.append(",[");
 			if (getValue().getUnit() != null) {
-				sb.append("['Unit','").append(getValue().getUnit())
-						.append("']");
+				sb.append("['Unit','").append(getValue().getUnit()).append("']");
 				if (getValue().getDtConstraints().size() > 0) {
 					sb.append(",");
 				}
 			}
 			if (getValue().getDtConstraints().size() > 0) {
-				Iterator<SDFConstraint> iter = getValue().getDtConstraints()
-						.iterator();
+				Iterator<SDFConstraint> iter = getValue().getDtConstraints().iterator();
 				for (int i = 0; i < getValue().getDtConstraints().size(); i++) {
 					SDFConstraint cs = iter.next();
-					sb.append("['").append(cs.getQualName()).append("','")
-							.append(cs.getValue()).append("']");
+					sb.append("['").append(cs.getURIWithoutQualName()).append("','").append(cs.getValue()).append("']");
 					if (i < getValue().getDtConstraints().size() - 1) {
 						sb.append(",");
 					}
-
 				}
 			}
 			sb.append("]");
@@ -121,25 +109,19 @@ public class CreateSDFAttributeParameter extends
 		return sb.toString();
 	}
 
-	private SDFAttribute determineAttribute(String sourcename,
-			String attributeName, String dataTypeName,
-			List<List<String>> constraintList) {
+	private SDFAttribute determineAttribute(String sourcename, String attributeName, String dataTypeName, List<List<String>> constraintList) {
 		try {
 			SDFUnit unit = null;
 			List<SDFConstraint> dtList = new LinkedList<>();
 			if (constraintList != null && constraintList.size() > 0) {
 				for (List<String> pair : constraintList) {
 					if (pair.size() != 2) {
-						throw new IllegalArgumentException(
-								"Wrong Constraint definition part. Use ['type', 'value']");
+						throw new IllegalArgumentException("Wrong Constraint definition part. Use ['type', 'value']");
 					}
-					if (pair.get(0).equalsIgnoreCase(
-							SDFUnit.class.getSimpleName())
-							|| pair.get(0).equalsIgnoreCase("Unit")) {
+					if (pair.get(0).equalsIgnoreCase(SDFUnit.class.getSimpleName()) || pair.get(0).equalsIgnoreCase("Unit")) {
 						unit = new SDFUnit(pair.get(1));
 					} else {
-						dtList.add(new SDFConstraint(pair.get(0).toLowerCase(),
-								pair.get(1)));
+						dtList.add(new SDFConstraint(pair.get(0).toLowerCase(), pair.get(1)));
 					}
 				}
 			}
@@ -150,29 +132,22 @@ public class CreateSDFAttributeParameter extends
 			}
 			if (pos > 0) {
 				String upperTypeStr = dataTypeName.substring(0, pos);
-				SDFDatatype upperType = getDataDictionary().getDatatype(
-						upperTypeStr);
-				String subTypeListStr = dataTypeName.substring(pos + 1,
-						dataTypeName.length() - 1);
+				SDFDatatype upperType = getDataDictionary().getDatatype(upperTypeStr);
+				String subTypeListStr = dataTypeName.substring(pos + 1, dataTypeName.length() - 1);
 				final SDFDatatype dataType;
 				List<SDFDatatype> subTypeList = new LinkedList<SDFDatatype>();
 				String[] subtypes = subTypeListStr.split(",");
 				if (subtypes.length > 0) {
 					for (String t : subtypes) {
-						subTypeList.add(getDataDictionary().getDatatype(
-								t.trim()));
+						subTypeList.add(getDataDictionary().getDatatype(t.trim()));
 					}
 				} else {
-					subTypeList.add(getDataDictionary().getDatatype(
-							subTypeListStr));
+					subTypeList.add(getDataDictionary().getDatatype(subTypeListStr));
 				}
-				dataType = getDataDictionary().getDatatype(upperType,
-						subTypeList);
-				return new SDFAttribute(sourcename, attributeName, dataType,
-						unit, dtList);
+				dataType = getDataDictionary().getDatatype(upperType, subTypeList);
+				return new SDFAttribute(sourcename, attributeName, dataType, unit, dtList);
 			}
-			return new SDFAttribute(sourcename, attributeName,
-					getDataDictionary().getDatatype(dataTypeName), unit, dtList);
+			return new SDFAttribute(sourcename, attributeName, getDataDictionary().getDatatype(dataTypeName), unit, dtList);
 		} catch (DataDictionaryException e) {
 			throw new QueryParseException(e);
 		}
