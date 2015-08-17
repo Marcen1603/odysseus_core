0d8eeb66c0ae087bb450f9cf83ed3a1e5b86e1e7
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-08-17 09:49:59 +0000

    Potential null pointer
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@25363 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c0c981d89072b30b12be59595391821afe18d780

--- client/studio/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/exception/ExceptionErrorDialog.java
+++ client/studio/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/exception/ExceptionErrorDialog.java
@@ -25,70 +25,70 @@ import org.eclipse.ui.PlatformUI;
 import de.uniol.inf.is.odysseus.rcp.l10n.OdysseusNLS;
 
 public class ExceptionErrorDialog extends ErrorDialog {
-    private static final int BUGREPORT_ID = IDialogConstants.CLIENT_ID + 1;
-    private final Throwable exception;
+	private static final int BUGREPORT_ID = IDialogConstants.CLIENT_ID + 1;
+	private final Throwable exception;
 
-    public static void open(final IStatus status, final Throwable exception) {
-        PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+	public static void open(final IStatus status, final Throwable exception) {
+		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
 
-            @Override
-            public void run() {
-                final Shell shell;
-                if (PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell() != null) {
-                    shell = new Shell(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
-                }
-                else {
-                    shell = new Shell();
-                }
+			@Override
+			public void run() {
+				final Shell shell;
+				if (PlatformUI.getWorkbench() != null && PlatformUI.getWorkbench().getActiveWorkbenchWindow() != null
+						&& PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell() != null) {
+					shell = new Shell(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+				} else {
+					shell = new Shell();
+				}
 
-                final ErrorDialog dialog = new ExceptionErrorDialog(shell, status, exception);
-                dialog.open();
+				final ErrorDialog dialog = new ExceptionErrorDialog(shell, status, exception);
+				dialog.open();
 
-            }
+			}
 
-        });
-    }
+		});
+	}
 
-    /**
-     * Class constructor.
-     *
-     * @param parentShell
-     * @param status
-     */
-    public ExceptionErrorDialog(final Shell parentShell, final IStatus status, final Throwable exception) {
-        this(parentShell, status, 0xFFFF, exception);
-    }
+	/**
+	 * Class constructor.
+	 *
+	 * @param parentShell
+	 * @param status
+	 */
+	public ExceptionErrorDialog(final Shell parentShell, final IStatus status, final Throwable exception) {
+		this(parentShell, status, 0xFFFF, exception);
+	}
 
-    /**
-     * Class constructor.
-     *
-     * @param parentShell
-     * @param status
-     * @param displayMask
-     */
-    public ExceptionErrorDialog(final Shell parentShell, final IStatus status, final int displayMask, final Throwable exception) {
-        super(parentShell, null, null, status, displayMask);
-        this.exception = exception;
-    }
+	/**
+	 * Class constructor.
+	 *
+	 * @param parentShell
+	 * @param status
+	 * @param displayMask
+	 */
+	public ExceptionErrorDialog(final Shell parentShell, final IStatus status, final int displayMask,
+			final Throwable exception) {
+		super(parentShell, null, null, status, displayMask);
+		this.exception = exception;
+	}
 
-    @Override
-    protected void createButtonsForButtonBar(final Composite parent) {
-        super.createButtonsForButtonBar(parent);
-        this.createSendBugReportButton(parent);
-    }
+	@Override
+	protected void createButtonsForButtonBar(final Composite parent) {
+		super.createButtonsForButtonBar(parent);
+		this.createSendBugReportButton(parent);
+	}
 
-    protected void createSendBugReportButton(final Composite parent) {
-        this.createButton(parent, IDialogConstants.CLIENT_ID + 1, OdysseusNLS.SendBugReport, false);
-    }
+	protected void createSendBugReportButton(final Composite parent) {
+		this.createButton(parent, IDialogConstants.CLIENT_ID + 1, OdysseusNLS.SendBugReport, false);
+	}
 
-    @Override
-    protected void buttonPressed(final int id) {
-        if (id == ExceptionErrorDialog.BUGREPORT_ID) {
-            final BugReport report = new BugReport(this.exception);
-            report.openEditor();
-        }
-        else {
-            super.buttonPressed(id);
-        }
-    }
+	@Override
+	protected void buttonPressed(final int id) {
+		if (id == ExceptionErrorDialog.BUGREPORT_ID) {
+			final BugReport report = new BugReport(this.exception);
+			report.openEditor();
+		} else {
+			super.buttonPressed(id);
+		}
+	}
 }
