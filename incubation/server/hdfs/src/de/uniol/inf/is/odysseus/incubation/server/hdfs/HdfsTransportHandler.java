b5e58d6dfca0094bda118c6adf38dacc0eb0f669
Stefan Nica <snica@uni-oldenburg.de>
2015-08-26 08:12:27 +0000

    Wrapper open/close done, not yet able to connect to hdfs, because it needs some credentials.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@25466 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3dcde52cbdb3dca8baf91c6a8fd96610241f932c

---
+++ incubation/server/hdfs/src/de/uniol/inf/is/odysseus/incubation/server/hdfs/HdfsTransportHandler.java
@@ -0,0 +1,123 @@
+package de.uniol.inf.is.odysseus.incubation.server.hdfs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import de.uniol.inf.is.odysseus.core.collection.OptionMap;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.IProtocolHandler;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.AbstractTransportHandler;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportHandler;
+
+public class HdfsTransportHandler extends AbstractTransportHandler {
+
+	public static final String NAME = "HDFSTransportHandler";
+	private String fsDefaultName;
+	private String pathToFile;
+	private boolean append;
+
+	private FSDataOutputStream dfsOutput;
+	private FSDataInputStream dfsInput;
+
+	private Configuration conf;
+	private FileSystem fs;
+
+	public HdfsTransportHandler(IProtocolHandler<?> protocolHandler, OptionMap options) {
+		super(protocolHandler, options);
+		this.fsDefaultName = options.get("fs.default.name");
+		this.pathToFile = options.get("Filename");
+		this.append = Boolean.parseBoolean(options.get("append"));
+
+	}
+
+	public void createConnection() {
+
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", fsDefaultName);
+		try {
+			fs = FileSystem.get(conf);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public ITransportHandler createInstance(IProtocolHandler<?> protocolHandler, OptionMap options) {
+
+		HdfsTransportHandler instance = new HdfsTransportHandler(protocolHandler, options);
+
+		protocolHandler.setTransportHandler(instance);
+
+		return instance;
+
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public void processInOpen() throws IOException {
+		createConnection();
+		if (!fs.exists(new Path(pathToFile)) || !fs.isFile(new Path(pathToFile)))
+			System.out.println("File doesn't exist!");
+		else
+			dfsInput = fs.open(new Path(pathToFile));
+
+	}
+
+	@Override
+	public void processOutOpen() throws IOException {
+		createConnection();
+		if (!fs.exists(new Path(pathToFile)))
+			System.out.println("File already exists!");
+		else
+			dfsOutput = fs.create(new Path(pathToFile));
+
+	}
+
+	@Override
+	public void processInClose() throws IOException {
+
+		dfsInput.close();
+
+	}
+
+	@Override
+	public void processOutClose() throws IOException {
+
+		dfsOutput.close();
+
+	}
+
+	@Override
+	public void send(byte[] message) throws IOException {
+
+	}
+
+	@Override
+	public InputStream getInputStream() {
+		return this.dfsInput;
+	}
+
+	@Override
+	public OutputStream getOutputStream() {
+		return this.dfsOutput;
+	}
+
+	@Override
+	public boolean isSemanticallyEqualImpl(ITransportHandler other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
