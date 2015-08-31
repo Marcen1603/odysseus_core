6bcfab63fa7df2040728b3375d8af172e0bf7124
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-08-31 12:20:03 +0000

    Net: Preparations with netty
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@25517 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 527ef9b972abab6e01f7823a5f909f1c37b08032

--- incubation/net/de.uniol.inf.is.odysseus.net.discovery.broadcast/src/de/uniol/inf/is/odysseus/net/discovery/broadcast/BroadcastDiscoveryPlugIn.java
+++ incubation/net/de.uniol.inf.is.odysseus.net.discovery.broadcast/src/de/uniol/inf/is/odysseus/net/discovery/broadcast/BroadcastDiscoveryPlugIn.java
@@ -5,20 +5,13 @@ import org.osgi.framework.BundleContext;
 
 public class BroadcastDiscoveryPlugIn implements BundleActivator {
 
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
-	}
 
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
-		BroadcastDiscoveryPlugIn.context = bundleContext;
 	}
 
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
-		BroadcastDiscoveryPlugIn.context = null;
 	}
 
 }
