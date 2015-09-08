24fcde568739d4d98cac632a6c5259a276ccc184
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-09-08 11:07:36 +0000

    Init Worker in open
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@25694 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8734cad6a5f6c818e6ece75028cf2c1ba898399d

--- server/interval/intervalapproach/src/de/uniol/inf/is/odysseus/server/intervalapproach/threaded/ThreadedAggregateTIPO.java
+++ server/interval/intervalapproach/src/de/uniol/inf/is/odysseus/server/intervalapproach/threaded/ThreadedAggregateTIPO.java
@@ -44,7 +44,7 @@ import de.uniol.inf.is.odysseus.server.intervalapproach.AggregateTIPO;
  * @author ChrisToenjesDeye
  */
 public class ThreadedAggregateTIPO<Q extends ITimeInterval, R extends IStreamObject<Q>, W extends IStreamObject<Q>>
-		extends AggregateTIPO<Q, R, W> implements IThreadedPO {
+		extends AggregateTIPO<Q, R, W>implements IThreadedPO {
 
 	private int degree;
 	private int maxBufferSize;
@@ -70,27 +70,23 @@ public class ThreadedAggregateTIPO<Q extends ITimeInterval, R extends IStreamObj
 	 * @param buffersize
 	 * @param useRoundRobinAllocation
 	 */
-	public ThreadedAggregateTIPO(SDFSchema inputSchema, SDFSchema outputSchema,
-			List<SDFAttribute> groupingAttributes,
-			Map<SDFSchema, Map<AggregateFunction, SDFAttribute>> aggregations,
-			boolean fastGrouping, IMetadataMergeFunction<Q> metadataMerge,
-			int degree, int buffersize, boolean useRoundRobinAllocation) {
-		super(inputSchema, outputSchema, groupingAttributes, aggregations,
-				fastGrouping, metadataMerge);
+	public ThreadedAggregateTIPO(SDFSchema inputSchema, SDFSchema outputSchema, List<SDFAttribute> groupingAttributes,
+			Map<SDFSchema, Map<AggregateFunction, SDFAttribute>> aggregations, boolean fastGrouping,
+			IMetadataMergeFunction<Q> metadataMerge, int degree, int buffersize, boolean useRoundRobinAllocation) {
+		super(inputSchema, outputSchema, groupingAttributes, aggregations, fastGrouping, metadataMerge);
 		this.maxBufferSize = buffersize;
 		this.degree = degree;
 		this.useRoundRobinAllocation = useRoundRobinAllocation;
 		// create threadgroup for grouping of worker threads
-		workerThreadGroup = new ThreadGroup(
-				"ThreadedAggregateTIPO worker threads "
-						+ UUID.randomUUID().toString());
+		workerThreadGroup = new ThreadGroup("ThreadedAggregateTIPO worker threads " + UUID.randomUUID().toString());
 
-		// for each degree, create blocking queue and worker thread
+	}
+
+	private void initWorker(int degree) {
 		for (int i = 0; i < degree; i++) {
-			ArrayBlockingQueue<Pair<Long, R>> blockingQueue = new ArrayBlockingQueue<Pair<Long, R>>(
-					maxBufferSize);
-			ThreadedAggregateTIPOWorker<Q, R, W> worker = new ThreadedAggregateTIPOWorker<Q, R, W>(
-					workerThreadGroup, this, i, blockingQueue);
+			ArrayBlockingQueue<Pair<Long, R>> blockingQueue = new ArrayBlockingQueue<Pair<Long, R>>(maxBufferSize);
+			ThreadedAggregateTIPOWorker<Q, R, W> worker = new ThreadedAggregateTIPOWorker<Q, R, W>(workerThreadGroup,
+					this, i, blockingQueue);
 			queueMap.put(i, blockingQueue);
 			threadMap.put(i, worker);
 		}
@@ -101,6 +97,8 @@ public class ThreadedAggregateTIPO<Q extends ITimeInterval, R extends IStreamObj
 	 */
 	@Override
 	protected void process_open() throws OpenFailedException {
+		// for each degree, create blocking queue and worker thread
+		initWorker(degree);
 
 		IGroupProcessor<R, W> g = getGroupProcessor();
 		synchronized (g) {
@@ -114,9 +112,7 @@ public class ThreadedAggregateTIPO<Q extends ITimeInterval, R extends IStreamObj
 
 		// start worker threads
 		for (ThreadedAggregateTIPOWorker<Q, R, W> worker : threadMap.values()) {
-			if (worker.getState() == Thread.State.NEW){
-				worker.start();				
-			}
+			worker.start();
 		}
 	}
 
@@ -145,21 +141,22 @@ public class ThreadedAggregateTIPO<Q extends ITimeInterval, R extends IStreamObj
 					groups.put(groupID, sa);
 				}
 			}
-		}
 
-		// we need to use the threads in round robin mode, to do a identically
-		// load on the different cores
-		int threadNumber = getThreadNumber(groupID);
+			// we need to use the threads in round robin mode, to do a
+			// identically
+			// load on the different cores
+			int threadNumber = getThreadNumber(groupID);
 
-		// put element into queue, if queue is full this thread need to wait
-		// until worker thread takes elements out of it
-		ArrayBlockingQueue<Pair<Long, R>> queue = queueMap.get(threadNumber);
-		try {
-			// put object and groupId into queue
-			Pair<Long, R> pair = new Pair<Long, R>(groupID, object);
-			queue.put(pair);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+			// put element into queue, if queue is full this thread need to wait
+			// until worker thread takes elements out of it
+			ArrayBlockingQueue<Pair<Long, R>> queue = queueMap.get(threadNumber);
+			try {
+				// put object and groupId into queue
+				Pair<Long, R> pair = new Pair<Long, R>(groupID, object);
+				queue.put(pair);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -236,16 +233,15 @@ public class ThreadedAggregateTIPO<Q extends ITimeInterval, R extends IStreamObj
 	}
 
 	@Override
-	public void createOutput(
-			List<PairMap<SDFSchema, AggregateFunction, W, Q>> existingResults,
-			Long groupID, PointInTime timestamp, int inPort) {
+	public void createOutput(List<PairMap<SDFSchema, AggregateFunction, W, Q>> existingResults, Long groupID,
+			PointInTime timestamp, int inPort) {
 		super.createOutput(existingResults, groupID, timestamp, inPort);
 	}
 
 	@Override
 	public List<PairMap<SDFSchema, AggregateFunction, W, Q>> updateSA(
-			AggregateTISweepArea<PairMap<SDFSchema, AggregateFunction, IPartialAggregate<R>, Q>> sa,
-			R elemToAdd, boolean outputPA) {
+			AggregateTISweepArea<PairMap<SDFSchema, AggregateFunction, IPartialAggregate<R>, Q>> sa, R elemToAdd,
+			boolean outputPA) {
 		return super.updateSA(sa, elemToAdd, outputPA);
 	}
 
@@ -267,8 +263,7 @@ public class ThreadedAggregateTIPO<Q extends ITimeInterval, R extends IStreamObj
 		map.putAll(super.getKeyValues());
 		map.put("Number of threads", String.valueOf(degree));
 		map.put("Buffersize", String.valueOf(maxBufferSize));
-		map.put("Use RoundRobin allocation",
-				String.valueOf(useRoundRobinAllocation));
+		map.put("Use RoundRobin allocation", String.valueOf(useRoundRobinAllocation));
 		return map;
 
 	}
