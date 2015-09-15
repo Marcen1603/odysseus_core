e5f6d7af8fad17dcd4d2fd907c740e03cf772639
Michael Brand <michael.brand@uni-oldenburg.de>
2015-09-15 07:55:00 +0000

    ParameterQueryName: Serializable
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@25835 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: a6a38bfaf9b5e390512e93a99e320ee7cbe36e4c

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/optimization/configuration/ParameterQueryName.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/optimization/configuration/ParameterQueryName.java
@@ -1,9 +1,13 @@
 package de.uniol.inf.is.odysseus.core.server.planmanagement.optimization.configuration;
 
+import java.io.Serializable;
+
 import de.uniol.inf.is.odysseus.core.server.planmanagement.configuration.Setting;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.IQueryBuildSetting;
 
-public class ParameterQueryName extends Setting<String> implements IQueryBuildSetting<String>{
+public class ParameterQueryName extends Setting<String> implements IQueryBuildSetting<String>, Serializable {
+
+	private static final long serialVersionUID = 6807349033596261427L;
 
 	public ParameterQueryName(String value) {
 		super(value);
