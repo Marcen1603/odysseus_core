5b563645b6d3737a47a80d1a98eebfc92dd75a2f
Carsten Cordes <ccordes@uni-oldenburg.de>
2015-09-19 18:03:33 +0000

    Random Selector now ignores Queries with DataratePOs and CalcLatencyPOs
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@25906 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: f3bf4b231460959f0b3179575440a2becc54b127

--- incubation/server/loadbalancing/de.uniol.inf.is.odysseus.peer.loadbalancing.dynamic.selector.random/src/de/uniol/inf/is/odysseus/peer/loadbalancing/dynamic/selector/random/RandomQuerySelectorImpl.java
+++ incubation/server/loadbalancing/de.uniol.inf.is.odysseus.peer.loadbalancing.dynamic.selector.random/src/de/uniol/inf/is/odysseus/peer/loadbalancing/dynamic/selector/random/RandomQuerySelectorImpl.java
@@ -6,12 +6,39 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.uniol.inf.is.odysseus.core.planmanagement.executor.IExecutor;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.IServerExecutor;
 import de.uniol.inf.is.odysseus.peer.loadbalancing.dynamic.IQuerySelector;
 import de.uniol.inf.is.odysseus.peer.loadbalancing.dynamic.common.QueryCostMap;
+import de.uniol.inf.is.odysseus.peer.loadbalancing.dynamic.common.QueryLoadInformation;
+import de.uniol.inf.is.odysseus.peer.loadbalancing.dynamic.preprocessing.TransformationHelper;
+import de.uniol.inf.is.odysseus.peer.loadbalancing.dynamic.registries.interfaces.IExcludedQueriesRegistry;
 
 public class RandomQuerySelectorImpl implements IQuerySelector {
 
-
+	private IExcludedQueriesRegistry excludedQueriesRegistry;
+	private IServerExecutor executor;
+	
+	public void bindExecutor(IExecutor serv) {
+		this.executor = (IServerExecutor)serv;
+	}
+	
+	public void unbindExecutor(IExecutor serv) {
+		if(this.executor==serv) {
+			this.executor = null;
+		}
+	}
+	
+	public void bindExcludedQueriesRegistry(IExcludedQueriesRegistry serv) {
+		this.excludedQueriesRegistry = serv;
+	}
+	
+	public void unbindExcludedQueriesRegistry(IExcludedQueriesRegistry serv) {
+		if(excludedQueriesRegistry==serv) {
+			excludedQueriesRegistry = null;
+		}
+	}
+	
 	private static final Logger LOG = LoggerFactory.getLogger(RandomQuerySelectorImpl.class);
 	
 	@Override
@@ -22,6 +49,10 @@ public class RandomQuerySelectorImpl implements IQuerySelector {
 	@Override
 	public QueryCostMap selectQueries(QueryCostMap costMap, double minCpuLoad,
 			double minMemLoad, double minNetLoad) {
+		
+		//Do not select Queries with Calc Latency and DataratePOs
+		costMap = removeCalclatencyAndDataratePOQueries(costMap);
+		
 		QueryCostMap result = new QueryCostMap();
 		
 		
@@ -43,6 +74,24 @@ public class RandomQuerySelectorImpl implements IQuerySelector {
 		int indexOfResult = rnd.nextInt(integers.size());
 		return integers.get(indexOfResult);
 		
+	}
+	
+	
+	private QueryCostMap removeCalclatencyAndDataratePOQueries(QueryCostMap costmap) {
+		QueryCostMap result = new QueryCostMap();
+		for(QueryLoadInformation info : costmap.getQueriesSortedByCost()) {
+			if(TransformationHelper.hasCalclatencyPOs(info.getQueryId(), executor)) {
+				excludedQueriesRegistry.excludeQueryIdFromLoadBalancing(info.getQueryId());
+			}
+			
+			if(TransformationHelper.hasDataratePOs(info.getQueryId(), executor)) {
+				excludedQueriesRegistry.excludeQueryIdFromLoadBalancing(info.getQueryId());
+			}
+			result.add(info);
+			
+		}
+		
+		return result;
 	}
 
 }
