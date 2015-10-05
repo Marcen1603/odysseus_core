236c8e908111c6d7fb155d1f8ef77a33dc357c4c
Mark Preuschaft <mpreuschaft@uni-oldenburg.de>
2015-10-05 07:42:57 +0000

    change to java 1.8
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@25981 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 9b510e3f59bbd994290ed10cb22efd64295ad2e3

--- incubation/monolithic/query.codegenerator/src/de/uniol/inf/is/odysseus/query/codegenerator/keywords/QueryCodegenerationConstants.java
+++ incubation/monolithic/query.codegenerator/src/de/uniol/inf/is/odysseus/query/codegenerator/keywords/QueryCodegenerationConstants.java
@@ -2,7 +2,7 @@ package de.uniol.inf.is.odysseus.query.codegenerator.keywords;
 
 public class QueryCodegenerationConstants {
 	
-		// custom pattern for parsing parameters of QUERYCODEGENERATION keyword 
+		// custom pattern for parsing parameters of CODEGENERATION keyword 
 		public static final String PATTERN_CUSTOM_PARAMETER_VALUE =  ".*"+"|"+"(?:[a-zA-Z]\\:)\\\\([\\w-]+\\\\)*\\w([\\w-.])+";
 
 
