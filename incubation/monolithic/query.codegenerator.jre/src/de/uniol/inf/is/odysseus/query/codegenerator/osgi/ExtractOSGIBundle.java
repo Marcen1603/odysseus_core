7dd75a7699252c786d7d9f0c4da45845c0ceeafb
Mark Preuschaft <mpreuschaft@uni-oldenburg.de>
2015-10-13 10:35:52 +0000

    refactoring ExtractOSGIBundle
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26084 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c6c4390d55f887e1824fa74eecd46f4b671b5afd

--- incubation/monolithic/query.codegenerator.jre/src/de/uniol/inf/is/odysseus/query/codegenerator/osgi/ExtractOSGIBundle.java
+++ incubation/monolithic/query.codegenerator.jre/src/de/uniol/inf/is/odysseus/query/codegenerator/osgi/ExtractOSGIBundle.java
@@ -22,17 +22,15 @@ import de.uniol.inf.is.odysseus.core.Activator;
 import de.uniol.inf.is.odysseus.query.codegenerator.utils.ExecuteShellComand;
 import de.uniol.inf.is.odysseus.query.codegenerator.utils.Utils;
 
-
-//TODO mac, unix support?
 public class ExtractOSGIBundle {
 	
 	private static Logger LOG = LoggerFactory.getLogger(ExtractOSGIBundle.class);
 	
 	private boolean EXTRACT_SOURCE = false;
-	static Map<String,String> requiredBundleByBundle = new HashMap<String,String>();
-	static Map<String,String> bundles = new HashMap<String, String>();
-	static Set<String> importedClassList = new HashSet<String>();
-	static List<String> onlyJarBundles = new ArrayList<String>();
+	
+	private Map<String,String> requiredBundleByBundle = new HashMap<String,String>();
+	private Map<String,String> bundlesToCopy = new HashMap<String, String>();
+	private List<String> onlyJarFilesToCopy = new ArrayList<String>();
 	
 	public ExtractOSGIBundle(){
 	
@@ -41,142 +39,20 @@ public class ExtractOSGIBundle {
 	public ExtractOSGIBundle(boolean extractSource){
 		EXTRACT_SOURCE  = extractSource;
 	}
-	
-	
-	public Set<String> extractOSGIBundle(Set<String> importList , String tempPath, String copyToDirectory){
-		Set<String> copyJars = new HashSet<String>();
-		getBundles(importList);
-		
-	
-		for (Map.Entry<String, String> bundleString : requiredBundleByBundle.entrySet())
-		{
-	
 
-			for(Bundle entry : Activator.getBundleContext().getBundles()){
-				//if(bundleString.getKey().equals(entry.getSymbolicName()) && bundleString.getValue().equals(entry.getVersion().toString())){
-				if(bundleString.getKey().equals(entry.getSymbolicName())){
-					String bundleClassPathValue =entry.getHeaders().get("Bundle-ClassPath");
-					
-					if(bundleClassPathValue != null && bundleClassPathValue.contains(".jar")){
-						//only a jar file
 	
-						List<String> jarList = Lists.newArrayList(Splitter.on(",").split(entry.getHeaders().get("Bundle-ClassPath")));
-						
+	public Set<String> extractOSGIBundle(Set<String> importList , String targetDirectory, String copyToDirectory){
+	
+		resolveBundles(importList);
+	
+		resolveRequiredBundlesByBundle();
 		
-						String path = entry.getLocation().replaceFirst("reference:file:/", "");
-						
-						for(String jarFile :jarList){
-							if(jarFile.contains(".jar")){
-								onlyJarBundles.add(path+jarFile);
-							}
-						
-						}
-						
-	
-					}else{
-						//complex bundle
-						String path = entry.getLocation();
-						bundles.put(path.replaceFirst("reference:file:/", ""), entry.getSymbolicName());
-					}
-					
-			
-				}
-				
-			}
-			
-		}
-		
-	
-	
-		for (Map.Entry<String, String> entry : bundles.entrySet())
-		{
-
-			//TODO unix mac support...
-		    String path = entry.getKey().replace("/", File.separator);
-		 
-		    String name = entry.getValue();
-	
-		    String[] commands = new String[3];  
-		    
-		    
-		    File filePath = new File(path+File.separator+"src");
-		    if(filePath.exists() && filePath.isDirectory()) { 
-		    	
-		    
-		    switch (Utils.getOS()) {
-	            case WINDOWS:
-	            	
-	        	    String[] volume = path.split(":"); 
-	    		  
-	      		    commands[0] = "cmd";
-	      		    commands[1] = "/c";
-	                  /* Command to execute */
-	      		    
-	      		    if(EXTRACT_SOURCE){
-	      		    	commands[2] = volume[0]+": && cd "+path+" && xcopy "+path+"src\\* "+path+"bin /s /e /c /y && jar -cvf \""+tempPath+"/"+copyToDirectory +"/"+name+".jar\" *.properties -C bin .";
-	      		    }else{ 
-	      			    commands[2] = volume[0]+": && cd "+path+" && jar -cvf \""+tempPath+"/"+copyToDirectory +"/"+name+".jar\" *.properties -C bin .";
-	      		    }
-	      		    
-	                break;
-	            case LINUX:
-	          	
-	      		    commands[0] = "sh";
-	      		    commands[1] = "-c";
-	                  /* Command to execute */
-	      		    
-	      		    if(EXTRACT_SOURCE){
-	      		    	commands[2] = "cd "+path+" && cp -r "+path+"src/* "+path+"bin && jar cvf "+tempPath+"/"+copyToDirectory +"/"+name+".jar *.properties  -C bin .";
-	      		    }else{ 
-	      			    commands[2] = "cd "+path+" && jar -cvf "+tempPath+"/"+copyToDirectory +"/"+name+".jar *.properties -C bin .";
-	      		    }
-	            	break;
-	            case MAC:
-	         
-	      		    commands[0] = "sh";
-	      		    commands[1] = "-c";
-	                  /* Command to execute */
-	      		    
-	      		    if(EXTRACT_SOURCE){
-	      		    	commands[2] = "cd "+path+" && cp -r "+path+"src/* "+path+"bin && jar cvf "+tempPath+"/"+copyToDirectory +"/"+name+".jar *.properties -C bin .";
-	      		    }else{ 
-	      			    commands[2] = "cd "+path+" && jar cvf "+tempPath+"/"+copyToDirectory +"/"+name+".jar *.properties -C bin .";
-	      		    }
-	            	break;
-	            default:
-	            	break;
-	         	
-		    }
-		    
-		    
-		    ExecuteShellComand.excecuteCommand(commands,false);
-            
-            copyJars.add(name+".jar");
-		    }
-		}
-		
-		
-		for(String filePath : onlyJarBundles){
-			
-			String fileName = new File(filePath).getName();
-			String path = filePath.replace(fileName, "");
-			String[] volume = filePath.split(":"); 
-			
-		    String[] commands = new String[3];
-		    commands[0] = "cmd";
-		    commands[1] = "/c";
-		    
-		    commands[2] = volume[0]+": && cd "+path+" && copy \""+filePath+"\" \""+tempPath+"/"+copyToDirectory+"/"+fileName+"\""; 
-		    ExecuteShellComand.excecuteCommand(commands, false);
-		    
-		    copyJars.add(fileName);
-		}
-	
-		return copyJars;
+		return copyBundles( targetDirectory, copyToDirectory);	
 	}
 	
 	
-	private  List<RequireBundleModel> getRequireBundleList(String requiredBundleString){
+
+	private List<RequireBundleModel> getRequireBundleList(String requiredBundleString){
 		List<RequireBundleModel> requireBundleList = new ArrayList<RequireBundleModel>();
 		
 		if(requiredBundleString != null){
@@ -193,45 +69,35 @@ public class ExtractOSGIBundle {
 					}
 				
 				}else{
-					
-					
 					requireBundleList.add(new RequireBundleModel(bundleString, null));
 				}
 				
 			}
 		}
 		
-	
-		
-		
 		return requireBundleList;
 	}
 	
 
 	
-	private void getBundles(Set<String> importList){
+	private void resolveBundles(Set<String> importList){
 		//get needed bundle
 				for(String neededClass : importList){
 					String path;
 					try {
-					
 						Bundle neededBundel = org.osgi.framework.FrameworkUtil.getBundle(Class.forName(neededClass));
-						
 					
 						if(neededBundel != null){				
 							URL entryss = neededBundel.getEntry(".project");
-							
-						    LOG.debug("Bundle:" +neededBundel.getSymbolicName());
-							
+												
 							List<RequireBundleModel> requiredBundeList = getRequireBundleList(neededBundel.getHeaders().get("Require-Bundle"));
-							
-							//TODO add Bundle with only jar 
+						
 							for(RequireBundleModel  bundle: requiredBundeList){
 								requiredBundleByBundle.put(bundle.getName(), bundle.getVersion());
 							}
 							
 						
-							String bundleClassPathValue =neededBundel.getHeaders().get("Bundle-ClassPath");
+							String bundleClassPathValue = neededBundel.getHeaders().get("Bundle-ClassPath");
 							
 							if(bundleClassPathValue != null && bundleClassPathValue.contains(".jar")){
 								//only a jar file
@@ -241,26 +107,17 @@ public class ExtractOSGIBundle {
 								
 								for(String jarFile :jarList){
 									if(jarFile.contains(".jar")){
-										onlyJarBundles.add(pathNeu+jarFile);
+										onlyJarFilesToCopy.add(pathNeu+jarFile);
 									}
 								
 								}
 							}
 							
-				
-			
-							List<RequireBundleModel> test = getRequireBundleList(neededBundel.getHeaders().get("Import-Package"));
-							for(RequireBundleModel  className: test){
-								importedClassList.add(className.getName());
-							}
-							
-							
-			
+
 							path = FileLocator.toFileURL(entryss).getPath();
-				
-							bundles.put(path.replaceFirst("/", "").replace(".project",""), neededBundel.getSymbolicName());
+							bundlesToCopy.put(path.replaceFirst("/", "").replace(".project",""), neededBundel.getSymbolicName());
 							
-							LOG.info("Bundle:" +neededBundel.getSymbolicName() +" found, in path "+path.replaceFirst("/", "").replace(".project",""));
+							LOG.info("Bundle: " +neededBundel.getSymbolicName() +" found, in path "+path.replaceFirst("/", "").replace(".project",""));
 							
 						}
 					} catch (IOException | ClassNotFoundException e) {
@@ -269,5 +126,155 @@ public class ExtractOSGIBundle {
 				}
 			}
 
+	
+	public void resolveRequiredBundlesByBundle(){
+		for (Map.Entry<String, String> bundleString : requiredBundleByBundle.entrySet())
+		{
 
+			for(Bundle entry : Activator.getBundleContext().getBundles()){
+				if(bundleString.getKey().equals(entry.getSymbolicName())){
+					String bundleClassPathValue =entry.getHeaders().get("Bundle-ClassPath");
+					
+					if(bundleClassPathValue != null && bundleClassPathValue.contains(".jar")){
+						//only a jar file
+	
+						List<String> jarList = Lists.newArrayList(Splitter.on(",").split(entry.getHeaders().get("Bundle-ClassPath")));
+						
+		
+						String path = entry.getLocation().replaceFirst("reference:file:/", "");
+						
+						for(String jarFile :jarList){
+							if(jarFile.contains(".jar")){
+								onlyJarFilesToCopy.add(path+jarFile);
+							}
+						
+						}
+					
+					}else{
+						//complex bundle
+						String path = entry.getLocation();
+						bundlesToCopy.put(path.replaceFirst("reference:file:/", ""), entry.getSymbolicName());
+					}
+					
+			
+				}
+				
+			}
+			
+		}
+	}
+	
+	
+	private  Set<String> copyBundles(String targetDirectory,String copyToDirectory){
+		Set<String> copiedJars = new HashSet<String>();
+		
+		
+		for (Map.Entry<String, String> entry : bundlesToCopy.entrySet())
+		{
+		    String path = entry.getKey().replace("/", File.separator);
+
+		    String name = entry.getValue();
+	
+		    String[] commands = new String[3];  
+		    File filePath = new File(path+File.separator+"src");
+		    if(filePath.exists() && filePath.isDirectory()) { 
+		    	
+		    
+		    switch (Utils.getOS()) {
+	            case WINDOWS:
+	            	
+	        	    String[] volume = path.split(":"); 
+	    		  
+	      		    commands[0] = "cmd";
+	      		    commands[1] = "/c";
+	                  /* Command to execute */
+	      		    
+	      		    if(EXTRACT_SOURCE){
+	      		    	commands[2] = volume[0]+": && cd "+path+" && xcopy "+path+"src\\* "+path+"bin /s /e /c /y && jar -cvf \""+targetDirectory+"/"+copyToDirectory +"/"+name+".jar\" *.properties -C bin .";
+	      		    }else{ 
+	      			    commands[2] = volume[0]+": && cd "+path+" && jar -cvf \""+targetDirectory+"/"+copyToDirectory +"/"+name+".jar\" *.properties -C bin .";
+	      		    }
+	      		    
+	                break;
+	            case LINUX:
+	          	
+	      		    commands[0] = "sh";
+	      		    commands[1] = "-c";
+	                  /* Command to execute */
+	      		    
+	      		    if(EXTRACT_SOURCE){
+	      		    	commands[2] = "cd "+path+" && cp -r "+path+"src/* "+path+"bin && jar cvf "+targetDirectory+"/"+copyToDirectory +"/"+name+".jar *.properties  -C bin .";
+	      		    }else{ 
+	      			    commands[2] = "cd "+path+" && jar -cvf "+targetDirectory+"/"+copyToDirectory +"/"+name+".jar *.properties -C bin .";
+	      		    }
+	            	break;
+	            case MAC:
+	         
+	      		    commands[0] = "sh";
+	      		    commands[1] = "-c";
+	                  /* Command to execute */
+	      		    
+	      		    if(EXTRACT_SOURCE){
+	      		    	commands[2] = "cd "+path+" && cp -r "+path+"src/* "+path+"bin && jar cvf "+targetDirectory+"/"+copyToDirectory +"/"+name+".jar *.properties -C bin .";
+	      		    }else{ 
+	      			    commands[2] = "cd "+path+" && jar cvf "+targetDirectory+"/"+copyToDirectory +"/"+name+".jar *.properties -C bin .";
+	      		    }
+	            	break;
+	            default:
+	            	break;
+	         	
+		    }
+		    
+		    
+		    ExecuteShellComand.excecuteCommand(commands,false);
+            
+            copiedJars.add(name+".jar");
+		    }
+		}
+		
+		
+		for(String filePath : onlyJarFilesToCopy){
+			
+			String fileName = new File(filePath).getName();
+			String path = filePath.replace(fileName, "");
+			String[] volume = filePath.split(":"); 
+			
+		    String[] commands = new String[3];
+		
+		    switch (Utils.getOS()) {
+            case WINDOWS:
+                commands[0] = "cmd";
+    		    commands[1] = "/c";
+    		    
+    		    commands[2] = volume[0]+": && cd "+path+" && copy \""+filePath+"\" \""+targetDirectory+"/"+copyToDirectory+"/"+fileName+"\""; 
+        	
+                break;
+            case LINUX:
+                commands[0] = "sh";
+    		    commands[1] = "-c";
+    		    
+    		    commands[2] = volume[0]+": && cd "+path+" && cp \""+filePath+"\" \""+targetDirectory+"/"+copyToDirectory+"/"+fileName+"\""; 
+      		
+            	
+            	break;
+            case MAC:
+                commands[0] = "sh";
+    		    commands[1] = "-c";
+    		    
+    		    commands[2] = volume[0]+": && cd "+path+" && cp \""+filePath+"\" \""+targetDirectory+"/"+copyToDirectory+"/"+fileName+"\""; 
+      		 
+            	break;
+            default:
+            	break;
+         	
+	    }
+		   
+		    ExecuteShellComand.excecuteCommand(commands, false);
+		    
+		    copiedJars.add(fileName);
+   
+		}
+	
+		return copiedJars;
+	}
 }
