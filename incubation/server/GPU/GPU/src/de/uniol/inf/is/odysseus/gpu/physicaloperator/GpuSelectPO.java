1854023d1cdb88b4e551ce38aff85788b50d14ed
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-10-19 09:21:36 +0000

    GPU: Paar Erläuterungen
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26112 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 27a788fc7ff3c6a443b29d4526cbd3c4135a0738

--- incubation/server/GPU/GPU/src/de/uniol/inf/is/odysseus/gpu/physicaloperator/GpuSelectPO.java
+++ incubation/server/GPU/GPU/src/de/uniol/inf/is/odysseus/gpu/physicaloperator/GpuSelectPO.java
@@ -22,11 +22,16 @@ import java.net.URL;
 import java.nio.file.Paths;
 import java.util.StringTokenizer;
 
+import javax.smartcardio.ATR;
+
 import org.osgi.framework.BundleContext;
 
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
 import de.uniol.inf.is.odysseus.core.physicaloperator.OpenFailedException;
 import de.uniol.inf.is.odysseus.core.predicate.IPredicate;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.SelectAO;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.SelectPO;
 import de.uniol.inf.is.odysseus.gpu.prepare;
@@ -38,179 +43,180 @@ import de.uniol.inf.is.odysseus.gpu.prepare;
 
 public class GpuSelectPO<T extends IStreamObject<?>> extends SelectPO<T> {
 
-	
-	
-	//standard const
+	// standard const
 	private String predicate[] = new String[3];
-	
+
 	private Pointer P_Input, P_Output, P_Predicate = new Pointer();
-	
+
 	private int gridDimX = 1;
-	private int blockDimX= 1;
-	
+	private int blockDimX = 1;
+
 	private static String ptxFileName;
-	
-	private static CUcontext context= new CUcontext();
-    
+
+	private static CUcontext context = new CUcontext();
+
 	private static CUdevice device = new CUdevice();
-			
-    private static CUmodule modul = new CUmodule();
-    
-    private static CUfunction function = new CUfunction();
-		
-	
+
+	private static CUmodule modul = new CUmodule();
+
+	private static CUfunction function = new CUfunction();
+
+	private int attributeIndex;
+
 	@SuppressWarnings("unchecked")
 	public GpuSelectPO(SelectAO ao) {
 		super((IPredicate<? super T>) ao.getPredicate());
 	}
-	
+
 	// copy const
 	public GpuSelectPO(GpuSelectPO<T> po) {
 		super(po);
 	}
-	
+
 	@Override
 	public void process_open() throws OpenFailedException {
-		
+
 		super.process_open();
-		
+
 		setName("GPU Select");
-		
-		System.out.println("Beginn CUDA");   
-		
-		
-		
+
+		System.out.println("Beginn CUDA");
+
 		try {
-			
-			//CUDA .ptx laden
+
+			// CUDA .ptx laden
 			ptxFileName = prepare.CUDA("CUDA/Hello.cu");
-			
+
 			// Predikate vorbereiten
-			StringTokenizer TokenPredicate = new StringTokenizer(super.getPredicate().toString(), " ");	
-			
+			StringTokenizer TokenPredicate = new StringTokenizer(super.getPredicate().toString(), " ");
+
 			predicate[0] = TokenPredicate.nextToken();
 			predicate[1] = TokenPredicate.nextToken();
 			predicate[2] = TokenPredicate.nextToken();
-			
-			predicate[0] = predicate[0].substring(1, predicate[0].length()-1);
-			predicate[2] = predicate[2].substring(1, predicate[2].length()-1);
-			System.out.println(predicate);   
-			
-			//CUDA Try- catch-Block 
-			
-			// CUDA-Vorbereitung		
-					
-			cuInit(0);	       
-	        //device = new CUdevice();
-	        
-	        cuDeviceGet(device, 0);	        
-	     
-			// Initialisieren des Treibers und schaffen eines Kontext für das Gerät 0
-	        
+
+			predicate[0] = predicate[0].substring(1, predicate[0].length() - 1);
+			predicate[2] = predicate[2].substring(1, predicate[2].length() - 1);
+			System.out.println(predicate);
+
+			// CUDA Try- catch-Block
+
+			// CUDA-Vorbereitung
+
+			cuInit(0);
+			// device = new CUdevice();
+
+			cuDeviceGet(device, 0);
+
+			// Initialisieren des Treibers und schaffen eines Kontext für das
+			// Gerät 0
+
+			// Elemente vorbereiten
+			if (getInputSchema(0).findAttributeIndex(predicate[2]) != -1) {
+				attributeIndex = getInputSchema(0).findAttributeIndex(predicate[2]);
+			} else {
+				attributeIndex = getInputSchema(0).findAttributeIndex(predicate[0]);
+			}
+
+			SDFDatatype attributeDatatype = getInputSchema(0).getAttribute(attributeIndex).getDatatype();
+			if (attributeDatatype.equals(SDFDatatype.INTEGER)) {
+				// int auf gpu reservieren
+			} else if (attributeDatatype.equals(SDFDatatype.FLOAT)) {
+				// float auf gpu reservieren
+			} else {
+				throw new OpenFailedException("Nicht unterstützter Datentyp " + attributeDatatype);
+			}
+
+			// usw.
+
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
-		}	
-        		
-        //Finally allekolierten Speicher freigeben
-		
+		}
+
+		// Finally allekolierten Speicher freigeben
+
 	}
-	
+
 	@Override
-    protected void process_next(T element, int port) {
-    	//CUDA Zeitmessen
-		
+	protected void process_next(T element, int port) {
+		if (!(element instanceof Tuple)) {
+			System.err.println("GPU kann nur mit Tupel arbeiten");
+			return;
+		}
+
+		// CUDA Zeitmessen
+
 		long start = System.nanoTime();
-		
-		int Attribute;
-		
-		//Elemente vorbereiten
-		if(getInputSchema(port).findAttributeIndex(predicate[2]) != -1)
-		{
-		
-			Attribute = getInputSchema(port).findAttributeIndex(predicate[2]);
-		
-		}else{
-		
-			Attribute = getInputSchema(port).findAttributeIndex(predicate[0]);
-		
-		}		
-    	
-    	
-		
-		transfer(CUDA_process(element,Attribute));		
-		
-		
-		
-		//Elemente sammeln
-		
-		
+
+		Tuple<?> result = CUDA_process((Tuple<?>) element, attributeIndex);
+		transfer((T) result);
+
+		// Elemente sammeln
+
 		// CUDA Speicher allokeieren
+
+		// Elemente auf die GPU übertragen
+
+		// Elemente von der GPU zurückholen
+
+		// transfer jedes/ reihe von Elements
+
+		// System.out.println(element.toString());
+
+		// System.out.println(Attribute);
+
+		// Finally allekolierten Speicher freigeben
+
+	}
+
+	@Override
+	protected void process_close() {
+		super.process_close();
+
+		// CUDA-Nachbereitung
+		try {
+			cuModuleUnload(modul);
+
+			if (context != null) {
+				cuCtxDestroy(context);
+			}
+
+			// Speicher auf der GPU freigeben
+			// cuMemFree();
+		} finally {
+
+		}
+	}
+
+	private Tuple<?> CUDA_process(Tuple<?> element, int attribute) {
+
+		cuCtxCreate(context, 0, device);
+
+		cuModuleLoad(modul, ptxFileName);
+
+		cuModuleGetFunction(function, modul, "Hello");
+
+		Object attributeValue = element.getAttribute(attribute);
+		// objekt + datentyp (s. processOpen)
 		
-		//Elemente auf die GPU übertragen
 		
-		//Elemente von der GPU zurückholen
-		
-		//transfer jedes/ reihe von Elements 
-		
-		
-		
-		//System.out.println(element.toString());	
-		
-		
-		//System.out.println(Attribute);
-		
-		//Finally allekolierten Speicher freigeben
-    	
-    }
-    
-    @Override
-    protected void process_close() {
-    	super.process_close();
-    	
-    	// CUDA-Nachbereitung
-        try{
-    	 cuModuleUnload(modul);    	 
-         
-         if (context != null)
-         {
-             cuCtxDestroy(context);
-         }
-   	
-         //Speicher auf der GPU freigeben
-        // cuMemFree();
-        }finally{
-        	
-        }
-    }
-    
-    private T CUDA_process(T element, int attribute){
-    	
-    	
-    	cuCtxCreate(context, 0, device);
-        		
-    	cuModuleLoad(modul,ptxFileName);
-	    
-    	cuModuleGetFunction(function , modul, "Hello");
-    	
-    	System.out.println(element.toString());
-    	
-//    	cuLaunchKernel(function, 
-//				gridDimX, 1, 1, 
-//				blockDimX, 5, 5, 
-//				0, null, 
-//				null, null);
-//		
-//		cuCtxSynchronize();
-		
-		cuModuleUnload(modul);    	 
-        
-        if (context != null)
-        {
-            cuCtxDestroy(context);
-        }
-		
+		System.out.println(element.toString());
+
+		// cuLaunchKernel(function,
+		// gridDimX, 1, 1,
+		// blockDimX, 5, 5,
+		// 0, null,
+		// null, null);
+		//
+		// cuCtxSynchronize();
+
+		cuModuleUnload(modul);
+
+		if (context != null) {
+			cuCtxDestroy(context);
+		}
+
 		return element;
-    }
-   
+	}
+
 }
