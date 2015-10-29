f4b92add7e21cb945455254443a4963f707c5405
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-10-29 10:01:54 +0000

    OdysseusNet: Refactoring
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26208 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 925036e51f348090d7cb882d4a0d64688d5b550f

--- incubation/net/de.uniol.inf.is.odysseus.net.discovery.broadcast/src/de/uniol/inf/is/odysseus/net/discovery/broadcast/request/BroadcastRequestHandler.java
+++ incubation/net/de.uniol.inf.is.odysseus.net.discovery.broadcast/src/de/uniol/inf/is/odysseus/net/discovery/broadcast/request/BroadcastRequestHandler.java
@@ -1,7 +1,9 @@
 package de.uniol.inf.is.odysseus.net.discovery.broadcast.request;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,11 +25,11 @@ import io.netty.util.CharsetUtil;
 public class BroadcastRequestHandler extends SimpleChannelInboundHandler<DatagramPacket> {
 
 	private static final Logger LOG = LoggerFactory.getLogger(BroadcastRequestHandler.class);
-	
+
 	private final String ownNodeName;
 	private final OdysseusNodeID ownNodeID;
 	private final IOdysseusNodeManager manager;
-	
+
 	public BroadcastRequestHandler(OdysseusNetStartupData data, IOdysseusNodeManager manager) {
 		Preconditions.checkNotNull(manager, "manager must not be null!");
 		Preconditions.checkNotNull(data, "Data must not be null!");
@@ -36,37 +38,46 @@ public class BroadcastRequestHandler extends SimpleChannelInboundHandler<Datagra
 		this.ownNodeID = data.getNodeID();
 		this.manager = manager;
 	}
-	
+
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
 		String msgText = msg.content().toString(CharsetUtil.UTF_8);
-		if(BroadcastDiscoveryPlugIn.BROADCAST_REQUEST_CONTENT.equals(msgText)) {
-			LOG.debug("Got broadcast request message");
-			
-			JSONObject json = new JSONObject();
-			json.put("nodeName", ownNodeName);
-			json.put("nodeID", ownNodeID);
-			
-			String jsonString = json.toString();
-			InetSocketAddress address = new InetSocketAddress(msg.sender().getAddress(), msg.sender().getPort());
-			DatagramPacket packet = new DatagramPacket(Unpooled.copiedBuffer(jsonString, CharsetUtil.UTF_8), address);
-			
-			LOG.debug("Sending request answer to {}", packet.recipient());
-			ctx.write(packet);
+		if (BroadcastDiscoveryPlugIn.BROADCAST_REQUEST_CONTENT.equals(msgText)) {
+			sendAnswerMessage(ctx, msg.sender());
 		} else {
-			JSONObject jsonObject = new JSONObject(msgText);
-			if( jsonObject.has("nodeName") && jsonObject.has("nodeID")) {
-				String nodeName = jsonObject.getString("nodeName");
-				String nodeIDStr = jsonObject.getString("nodeID");
-				
-				IOdysseusNode newNode = new BroadcastOdysseusNode(OdysseusNodeID.fromString(nodeIDStr), nodeName, msg.sender().getAddress());
-				if( !manager.existsNode(newNode)) {
-					manager.addNode(newNode);
-				}
+			processAnswerMessage(msg.sender().getAddress(), msgText);
+		}
+	}
+
+	private void sendAnswerMessage(ChannelHandlerContext ctx, InetSocketAddress destination) throws JSONException {
+		LOG.debug("Got broadcast request message");
+
+		JSONObject json = new JSONObject();
+		json.put("nodeName", ownNodeName);
+		json.put("nodeID", ownNodeID);
+
+		String jsonString = json.toString();
+		DatagramPacket packet = new DatagramPacket(Unpooled.copiedBuffer(jsonString, CharsetUtil.UTF_8), destination);
+
+		LOG.debug("Sending request answer to {}", packet.recipient());
+		ctx.write(packet);
+	}
+
+	private void processAnswerMessage(InetAddress sender, String msgText) throws JSONException {
+		LOG.debug("Process answer message from {}", sender);
+		JSONObject jsonObject = new JSONObject(msgText);
+		if (jsonObject.has("nodeName") && jsonObject.has("nodeID")) {
+			String nodeName = jsonObject.getString("nodeName");
+			String nodeIDStr = jsonObject.getString("nodeID");
+
+			IOdysseusNode newNode = new BroadcastOdysseusNode(OdysseusNodeID.fromString(nodeIDStr), nodeName, sender);
+			if (!manager.existsNode(newNode)) {
+				manager.addNode(newNode);
+				LOG.debug("New node added: {}", newNode);
 			}
 		}
 	}
-	
+
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
 		super.channelReadComplete(ctx);
