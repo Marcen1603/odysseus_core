4d6f4118615ad2a5493bf2514f984ddb64af59a3
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-10-29 13:16:58 +0000

    OdysseusNet: New plugin for rcp (#2)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26218 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 6fc5d53dddbc93f14ad37878ebd5811dd1341883

---
+++ incubation/net/de.uniol.inf.is.odysseus.net.rcp/src/de/uniol/inf/is/odysseus/net/rcp/OdysseusNetRCPPlugIn.java
@@ -0,0 +1,15 @@
+package de.uniol.inf.is.odysseus.net.rcp;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class OdysseusNetRCPPlugIn extends AbstractUIPlugin {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
+}
---
+++ incubation/net/de.uniol.inf.is.odysseus.net.rcp/src/de/uniol/inf/is/odysseus/net/rcp/views/NodeViewPart.java
@@ -0,0 +1,20 @@
+package de.uniol.inf.is.odysseus.net.rcp.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class NodeViewPart extends ViewPart {
+
+	public static final String VIEW_ID = "de.uniol.inf.is.odysseus.net.rcp.NodeView";
+	
+	@Override
+	public void createPartControl(Composite parent) {
+
+	}
+
+	@Override
+	public void setFocus() {
+
+	}
+
+}
