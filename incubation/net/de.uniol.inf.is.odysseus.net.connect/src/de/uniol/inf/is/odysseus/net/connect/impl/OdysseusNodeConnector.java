14b4281b39686de96563b802ccbe6c62399ed4cd
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-10-30 16:12:59 +0000

    OdysseusNet: Some steps for selecting connections
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26234 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c15e38ffa52e3ba1d41e9a98f2fb8d984b5634cd

--- incubation/net/de.uniol.inf.is.odysseus.net.connect/src/de/uniol/inf/is/odysseus/net/connect/impl/OdysseusNodeConnector.java
+++ incubation/net/de.uniol.inf.is.odysseus.net.connect/src/de/uniol/inf/is/odysseus/net/connect/impl/OdysseusNodeConnector.java
@@ -1,36 +1,57 @@
 package de.uniol.inf.is.odysseus.net.connect.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Optional;
+
 import de.uniol.inf.is.odysseus.net.IOdysseusNode;
 import de.uniol.inf.is.odysseus.net.IOdysseusNodeManager;
 import de.uniol.inf.is.odysseus.net.IOdysseusNodeManagerListener;
+import de.uniol.inf.is.odysseus.net.config.OdysseusNetConfiguration;
+import de.uniol.inf.is.odysseus.net.connect.IOdysseusNodeConnectionSelector;
 import de.uniol.inf.is.odysseus.net.connect.IOdysseusNodeConnectionSelectorManager;
 
 public class OdysseusNodeConnector implements IOdysseusNodeManagerListener {
 
+	private static final Logger LOG = LoggerFactory.getLogger(OdysseusNodeConnector.class);
+	private static final String SELECTOR_CONFIG_KEY = "net.connect.selector.name";
+	private static final String DEFAULT_SELECTOR_NAME = "ConnectAllSelector";
+	
 	private static IOdysseusNodeManager nodeManager;
 	private static IOdysseusNodeConnectionSelectorManager connectionSelectorManager;
+	
+	private IOdysseusNodeConnectionSelector connectionSelector;
 
 	// called by OSGi-DS
 	public void bindOdysseusNodeManager(IOdysseusNodeManager serv) {
 		nodeManager = serv;
+		
+		LOG.info("Bound odysseus node manager {}", serv);
 	}
 
 	// called by OSGi-DS
 	public void unbindOdysseusNodeManager(IOdysseusNodeManager serv) {
 		if (nodeManager == serv) {
 			nodeManager = null;
+			
+			LOG.info("Unbound odysseus node manager {}", serv);
 		}
 	}
 
 	// called by OSGi-DS
 	public void bindOdysseusNodeConnectionSelectorManager(IOdysseusNodeConnectionSelectorManager serv) {
 		connectionSelectorManager = serv;
+		
+		LOG.debug("Bound odysseus node connection selector manager {}", serv);
 	}
 
 	// called by OSGi-DS
 	public void unbindOdysseusNodeConnectionSelectorManager(IOdysseusNodeConnectionSelectorManager serv) {
 		if (connectionSelectorManager == serv) {
 			connectionSelectorManager = null;
+			
+			LOG.info("Unbound odysseus node connection selector manager {}", serv);
 		}
 	}
 	
@@ -38,7 +59,13 @@ public class OdysseusNodeConnector implements IOdysseusNodeManagerListener {
 	public void activate() {
 		nodeManager.addListener(this);
 		
-		// TODO: get selector
+		String selectorName = OdysseusNetConfiguration.get(SELECTOR_CONFIG_KEY, DEFAULT_SELECTOR_NAME);
+		Optional<IOdysseusNodeConnectionSelector> optSelector = connectionSelectorManager.getSelector(selectorName);
+		if( optSelector.isPresent() ) {
+			connectionSelector = optSelector.get();
+		} else {
+			LOG.error("Could not find connection selector called '{}'", selectorName);
+		}
 	}
 	
 	// called by OSGi-DS
@@ -48,7 +75,11 @@ public class OdysseusNodeConnector implements IOdysseusNodeManagerListener {
 
 	@Override
 	public void nodeAdded(IOdysseusNodeManager sender, IOdysseusNode node) {
-		// TODO: use selector to determine if a communication connection to this node is needed
+		if( connectionSelector != null ) {
+			if( connectionSelector.doConnect(node) ) {
+				
+			}
+		}
 	}
 
 	@Override
