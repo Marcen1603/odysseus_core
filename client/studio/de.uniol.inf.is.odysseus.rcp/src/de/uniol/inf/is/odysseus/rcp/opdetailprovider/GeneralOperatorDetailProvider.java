02f149ac36b26e68b1efcc1344d9608bda768905
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2015-10-30 13:57:27 +0000

    Avoid Nullpointer
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26232 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 2d7436e7d1e54d73a6c6e017b19b351fba963366

--- client/studio/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/opdetailprovider/GeneralOperatorDetailProvider.java
+++ client/studio/de.uniol.inf.is.odysseus.rcp/src/de/uniol/inf/is/odysseus/rcp/opdetailprovider/GeneralOperatorDetailProvider.java
@@ -13,24 +13,26 @@ import de.uniol.inf.is.odysseus.rcp.AbstractKeyValueGeneralProvider;
 public class GeneralOperatorDetailProvider extends AbstractKeyValueGeneralProvider {
 
 	private static final String TAB_TITLE = "General";
-	
+
 	@Override
 	protected Map<String, String> getKeyValuePairs(IPhysicalOperator operator) {
 		Map<String, String> map = Maps.newHashMap();
-		
+
 		map.put("Name", operator.getName());
 		map.put("Class", operator.getClass().getSimpleName());
 		map.put("Hash", String.valueOf(operator.hashCode()));
 		map.put("Owners", determineOwners(operator));
 		map.put("Open", operator.isOpen() ? "true" : "false");
-		map.put("Done", operator.isDone()+"");
+		map.put("Done", operator.isDone() + "");
 		map.put("Type", determineType(operator));
-		
-		for( String dataName : operator.getProvidedMonitoringData() ) {
-			IMonitoringData<?> data = operator.getMonitoringData(dataName);
-			map.put(dataName, data.getValue().toString());
+
+		if (operator.getProvidedMonitoringData() != null) {
+			for (String dataName : operator.getProvidedMonitoringData()) {
+				IMonitoringData<?> data = operator.getMonitoringData(dataName);
+				map.put(dataName, data.getValue().toString());
+			}
 		}
-		
+
 		return map;
 	}
 
@@ -40,9 +42,9 @@ public class GeneralOperatorDetailProvider extends AbstractKeyValueGeneralProvid
 	}
 
 	private static String determineType(IPhysicalOperator operator) {
-		if( operator.isPipe() ) {
+		if (operator.isPipe()) {
 			return "Pipe";
-		} else if( operator.isSink() ) {
+		} else if (operator.isSink()) {
 			return "Sink";
 		}
 		return "Source";
@@ -51,9 +53,9 @@ public class GeneralOperatorDetailProvider extends AbstractKeyValueGeneralProvid
 	private static String determineOwners(IPhysicalOperator operator) {
 		StringBuilder sb = new StringBuilder();
 		List<IOperatorOwner> owners = operator.getOwner();
-		for( int i = 0; i < owners.size(); i++ ) {
+		for (int i = 0; i < owners.size(); i++) {
 			sb.append(owners.get(i).getID());
-			if( i < owners.size() - 1 ) {
+			if (i < owners.size() - 1) {
 				sb.append(", ");
 			}
 		}
