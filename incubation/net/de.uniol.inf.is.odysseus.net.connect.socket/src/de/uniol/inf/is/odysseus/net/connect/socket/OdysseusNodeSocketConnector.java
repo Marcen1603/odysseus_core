3f68e1043a9708da14a67185c9600d180312fe6b
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-11-02 10:52:40 +0000

    OdysseusNet: added new plugin for socket-based connections (#3)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26245 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 4ee8fdc3d0209a525ae43e7b30ec57086bd0f3a0

---
+++ incubation/net/de.uniol.inf.is.odysseus.net.connect.socket/src/de/uniol/inf/is/odysseus/net/connect/socket/OdysseusNodeSocketConnector.java
@@ -0,0 +1,116 @@
+package de.uniol.inf.is.odysseus.net.connect.socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Optional;
+
+import de.uniol.inf.is.odysseus.net.IOdysseusNetComponent;
+import de.uniol.inf.is.odysseus.net.IOdysseusNode;
+import de.uniol.inf.is.odysseus.net.OdysseusNetException;
+import de.uniol.inf.is.odysseus.net.connect.IOdysseusNodeConnection;
+import de.uniol.inf.is.odysseus.net.connect.IOdysseusNodeConnector;
+import de.uniol.inf.is.odysseus.net.connect.OdysseusNetConnectionException;
+
+public class OdysseusNodeSocketConnector implements IOdysseusNodeConnector, IOdysseusNetComponent {
+
+	private static final String SERVER_PORT_PROPERTY_KEY = "serverPort";
+
+	private static final Logger LOG = LoggerFactory.getLogger(OdysseusNodeSocketConnector.class);
+
+	private int serverPort;
+
+	@Override
+	public void init(IOdysseusNode localNode) throws OdysseusNetException {
+		serverPort = findFreePort();
+		LOG.info("Choosing port {} as server port", serverPort);
+
+		// port will be shared with other nodes
+		localNode.addProperty(SERVER_PORT_PROPERTY_KEY, "" + serverPort);
+	}
+
+	@Override
+	public void start() throws OdysseusNetException {
+//		startServer(serverPort);
+	}
+
+	@Override
+	public IOdysseusNodeConnection connect(IOdysseusNode node) throws OdysseusNetConnectionException {
+		int nodeServerPort = determineServerPort(node);
+		LOG.info("Connection to node {} at its server port {}", node.getID(), nodeServerPort);
+
+		return null;
+	}
+
+	private static int determineServerPort(IOdysseusNode node) throws OdysseusNetConnectionException {
+		Optional<String> optNodeServerPortStr = node.getProperty(SERVER_PORT_PROPERTY_KEY);
+		if (optNodeServerPortStr.isPresent()) {
+			Optional<Integer> optNodeServerPort = tryConvertToInt(optNodeServerPortStr.get());
+			if (optNodeServerPort.isPresent()) {
+				return optNodeServerPort.get();
+			} 
+			throw new OdysseusNetConnectionException("Could not determine server port of node " + node);
+		} 
+		throw new OdysseusNetConnectionException("Property 'serverPort' is not available for node " + node);
+	}
+
+	@Override
+	public void disconnect(IOdysseusNode node) {
+		// TODO: remove connection
+	}
+
+	@Override
+	public void stop() {
+		// TODO: Stop server
+
+	}
+
+	@Override
+	public void terminate(IOdysseusNode localNode) {
+		// do nothing
+	}
+
+	@Override
+	public boolean isConnected(IOdysseusNode node) {
+		// TODO
+		return false;
+	}
+
+	private static Optional<Integer> tryConvertToInt(String text) {
+		try {
+			return Optional.of(Integer.parseInt(text));
+		} catch (Throwable t) {
+			return Optional.absent();
+		}
+	}
+
+//	private void startServer(int port) throws OdysseusNetConnectionException {
+//		 try {
+//		LOG.info("Starting server at port {}", port);
+//		 ServerSocket s = new ServerSocket(port);
+//		
+//		 } catch (IOException e) {
+//		 throw new OdysseusNetConnectionException("Could not start server for
+//		 connections", e);
+//		 }
+//	}
+
+	private static int findFreePort() throws OdysseusNetException {
+		try {
+			ServerSocket s = new ServerSocket(0);
+			s.setReuseAddress(true);
+			int port = s.getLocalPort();
+			try {
+				s.close();
+			} catch (IOException ignore) {
+			}
+
+			return port;
+		} catch (IOException e) {
+			throw new OdysseusNetException("Could not find a free port", e);
+		}
+	}
+}
