a87758fbc2bfdd6ea72a990b41976200351c860b
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-11-03 09:44:41 +0000

    OdysseusNet: Added adapter for OydsseusNet components
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26261 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 06a9542bbeb8ef32f75729ab41835c0c41e7984b

---
+++ incubation/net/de.uniol.inf.is.odysseus.net/src/de/uniol/inf/is/odysseus/net/OdysseusNetComponentAdapter.java
@@ -0,0 +1,25 @@
+package de.uniol.inf.is.odysseus.net;
+
+public class OdysseusNetComponentAdapter implements IOdysseusNetComponent {
+
+	@Override
+	public void init(IOdysseusNode localNode) throws OdysseusNetException {
+		// do nothing
+	}
+
+	@Override
+	public void start() throws OdysseusNetException {
+		// do nothing
+	}
+
+	@Override
+	public void stop() {
+		// do nothing
+	}
+
+	@Override
+	public void terminate(IOdysseusNode localNode) {
+		// do nothing
+	}
+
+}
