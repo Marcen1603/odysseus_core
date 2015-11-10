966cef861a692a2e9c5b4755816f055c890b16fc
Cornelius Ludmann <Cornelius.Ludmann@uni-oldenburg.de>
2015-11-10 10:58:23 +0000

    TupleToKeyValuePO: Converts now LIST_TUPLE as Datatype in Tuple to KeyValue.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26386 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 10868f1f5a2a524dd32eae14c1e8bb49e788a8d4

--- server/keyvalue/keyvalue/src/de/uniol/inf/is/odysseus/keyvalue/physicaloperator/TupleToKeyValuePO.java
+++ server/keyvalue/keyvalue/src/de/uniol/inf/is/odysseus/keyvalue/physicaloperator/TupleToKeyValuePO.java
@@ -15,12 +15,15 @@
  */
 package de.uniol.inf.is.odysseus.keyvalue.physicaloperator;
 
+import java.util.List;
+
 import de.uniol.inf.is.odysseus.core.collection.KeyValueObject;
 import de.uniol.inf.is.odysseus.core.collection.NestedKeyValueObject;
 import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPunctuation;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe;
 
 /**
@@ -32,44 +35,51 @@ import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractPipe;
  */
 
 public class TupleToKeyValuePO<M extends IMetaAttribute> extends AbstractPipe<Tuple<M>, KeyValueObject<M>> {
-	
+
 	public TupleToKeyValuePO() {
 		super();
 	}
-	
+
 	public TupleToKeyValuePO(TupleToKeyValuePO<M> tupleToKeyValuePO) {
 		super(tupleToKeyValuePO);
 	}
-	
+
 	@Override
 	public OutputMode getOutputMode() {
 		return OutputMode.NEW_ELEMENT;
 	}
 
 	@SuppressWarnings("unchecked")
-    @Override
+	@Override
 	protected void process_next(Tuple<M> input, int port) {
 		KeyValueObject<M> output = null;
 		if (getOutputSchema().getType() == KeyValueObject.class)
 			output = new KeyValueObject<M>();
-		else
-		if (getOutputSchema().getType() == NestedKeyValueObject.class)
+		else if (getOutputSchema().getType() == NestedKeyValueObject.class)
 			output = new NestedKeyValueObject<M>();
-		
+
 		if (output != null) {
 			int pos = 0;
 			for (SDFAttribute attr : getOutputSchema()) {
-				output.setAttribute(attr.getQualName(), input.getAttribute(pos++));
+				if (attr.getDatatype() == SDFDatatype.LIST_TUPLE) {
+					List<?> o = input.getAttribute(pos++);
+					for (Object oo : o) {
+						Tuple<?> t = (Tuple<?>) oo;
+						for (int i = 0; i < t.size(); ++i) {
+							output.addAttributeValue(attr.getQualName() + "_" + i, t.getAttribute(i));
+						}
+					}
+				} else
+					output.setAttribute(attr.getQualName(), input.getAttribute(pos++));
 			}
 			output.setMetadata((M) input.getMetadata().clone());
 			transfer(output);
 		}
 	}
-	
+
 	@Override
 	public void processPunctuation(IPunctuation punctuation, int port) {
 		sendPunctuation(punctuation);
 	}
-
 
 }
