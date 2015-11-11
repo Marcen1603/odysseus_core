c36b69bf7be3d252fd84944f34935f719487ff57
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-11-11 12:12:13 +0000

    OdysseusNet: Bugfix for linux
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26402 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 8c93b982efe236b0ef416c280fc8c1af0ac03bd8

--- server/net/core/de.uniol.inf.is.odysseus.net.discovery.broadcast/src/de/uniol/inf/is/odysseus/net/discovery/broadcast/request/BroadcastHandler.java
+++ server/net/core/de.uniol.inf.is.odysseus.net.discovery.broadcast/src/de/uniol/inf/is/odysseus/net/discovery/broadcast/request/BroadcastHandler.java
@@ -2,7 +2,6 @@ package de.uniol.inf.is.odysseus.net.discovery.broadcast.request;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
 import java.util.Iterator;
 
 import org.json.JSONException;
@@ -10,12 +9,14 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 
 import de.uniol.inf.is.odysseus.net.IOdysseusNode;
 import de.uniol.inf.is.odysseus.net.OdysseusNode;
 import de.uniol.inf.is.odysseus.net.OdysseusNodeID;
 import de.uniol.inf.is.odysseus.net.discovery.broadcast.BroadcastDiscoveryPlugIn;
+import de.uniol.inf.is.odysseus.net.util.InetAddressUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -28,7 +29,7 @@ public class BroadcastHandler extends SimpleChannelInboundHandler<DatagramPacket
 
 	private final IOdysseusNode localNode;
 	private final int ownBroadcastPort;
-	
+
 	public BroadcastHandler(IOdysseusNode localNode, int broadcastPort) {
 		Preconditions.checkNotNull(localNode, "localNode must not be null!");
 
@@ -38,15 +39,15 @@ public class BroadcastHandler extends SimpleChannelInboundHandler<DatagramPacket
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
-		if( isOwnMessage(msg)) {
+		if (isOwnMessage(msg)) {
 			return;
 		}
-		
+
 		String msgText = msg.content().toString(CharsetUtil.UTF_8);
 		if (BroadcastDiscoveryPlugIn.BROADCAST_REQUEST_CONTENT.equals(msgText)) {
 			LOG.debug("Got broadcast request message from {}", msg.sender());
 			sendAnswerMessage(ctx, msg.sender());
-			
+
 		} else {
 			LOG.debug("Process answer message from {}", msg.sender());
 			processAnswerMessage(msg.sender().getAddress(), msgText);
@@ -54,26 +55,30 @@ public class BroadcastHandler extends SimpleChannelInboundHandler<DatagramPacket
 	}
 
 	private boolean isOwnMessage(DatagramPacket msg) {
-		try {
-			InetSocketAddress sender = msg.sender();
-			return sender.getAddress().equals(InetAddress.getLocalHost()) && sender.getPort() == ownBroadcastPort;
-		} catch (UnknownHostException e) {
+		InetSocketAddress sender = msg.sender();
+		Optional<String> optAddress = InetAddressUtil.getRealInetAddress();
+		if (!optAddress.isPresent()) {
 			return false;
 		}
+
+		if (sender.getAddress().getHostAddress().equals(optAddress.get())) {
+			return sender.getPort() == ownBroadcastPort;
+		}
+		return false;
 	}
 
 	private void sendAnswerMessage(ChannelHandlerContext ctx, InetSocketAddress destination) throws JSONException {
 		JSONObject json = new JSONObject();
 		json.put("nodeName", localNode.getName());
 		json.put("nodeID", localNode.getID());
-		
+
 		JSONObject propertyJson = new JSONObject();
-		for( String propertyKey : localNode.getProperyKeys() ) {
+		for (String propertyKey : localNode.getProperyKeys()) {
 			String propertyValue = localNode.getProperty(propertyKey).get();
 			propertyJson.put(propertyKey, propertyValue);
 		}
 		json.put("properties", propertyJson);
-		
+
 		String jsonString = json.toString();
 		DatagramPacket packet = new DatagramPacket(Unpooled.copiedBuffer(jsonString, CharsetUtil.UTF_8), destination);
 
@@ -87,11 +92,11 @@ public class BroadcastHandler extends SimpleChannelInboundHandler<DatagramPacket
 			String nodeName = jsonObject.getString("nodeName");
 			String nodeIDStr = jsonObject.getString("nodeID");
 			IOdysseusNode newNode = new OdysseusNode(OdysseusNodeID.fromString(nodeIDStr), nodeName);
-			
+
 			JSONObject propertyJson = jsonObject.getJSONObject("properties");
 			Iterator<?> keyIterator = propertyJson.keys();
-			while( keyIterator.hasNext() ) {
-				String key = (String)keyIterator.next();
+			while (keyIterator.hasNext()) {
+				String key = (String) keyIterator.next();
 				String value = propertyJson.getString(key);
 				newNode.addProperty(key, value);
 			}
