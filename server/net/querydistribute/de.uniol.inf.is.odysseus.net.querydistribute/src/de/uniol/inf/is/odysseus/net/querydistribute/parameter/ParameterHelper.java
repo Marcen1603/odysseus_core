39d7ed6ef96286953d38fd1d08a9767bddc7eda9
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-11-17 10:53:22 +0000

    OdysseusNet: added default distribution strategy which is used when user don't specify them (default-partition: querycloud, default-allocate:querycount); default strategies can be specified in config-file and/or in vm-params
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26478 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 69de3abca57c25305ab50b792d6c751e8b2052ee

--- server/net/querydistribute/de.uniol.inf.is.odysseus.net.querydistribute/src/de/uniol/inf/is/odysseus/net/querydistribute/parameter/ParameterHelper.java
+++ server/net/querydistribute/de.uniol.inf.is.odysseus.net.querydistribute/src/de/uniol/inf/is/odysseus/net/querydistribute/parameter/ParameterHelper.java
@@ -2,10 +2,13 @@ package de.uniol.inf.is.odysseus.net.querydistribute.parameter;
 
 import java.util.List;
 
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 
 import de.uniol.inf.is.odysseus.core.server.distribution.QueryDistributionException;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.querybuiltparameter.QueryBuildConfiguration;
+import de.uniol.inf.is.odysseus.net.config.OdysseusNetConfiguration;
 import de.uniol.inf.is.odysseus.net.querydistribute.IQueryDistributionPostProcessor;
 import de.uniol.inf.is.odysseus.net.querydistribute.IQueryDistributionPreProcessor;
 import de.uniol.inf.is.odysseus.net.querydistribute.IQueryPartAllocator;
@@ -19,8 +22,22 @@ import de.uniol.inf.is.odysseus.net.querydistribute.service.QueryPartitionerRegi
 
 public final class ParameterHelper {
 	
+	private static final String DEFAULT_PREPROCESSORS_CONFIG_KEY = "net.querydistribute.preprocess";
+	private static final String DEFAULT_QUERY_PARTITIONER_CONFIG_KEY = "net.querydistribute.partition";
+	private static final String DEFAULT_QUERY_PART_MODIFICATOR_CONFIG_KEY = "net.querydistribute.modification";
+	private static final String DEFAULT_QUERY_PART_ALLOCATOR_CONFIG_KEY = "net.querydistribute.allocation";
+	private static final String DEFAULT_POSTPROCESSORS_CONFIG_KEY = "net.querydistribute.postprocess";
+	
+	private static final String DEFAULT_QUERY_PARTITIONER_NAME = "querycloud";
+	private static final String DEFAULT_QUERY_PART_ALLOCATOR_NAME = "querycount";
+
+
 	public static List<InterfaceParametersPair<IQueryPartitioner>> determineQueryPartitioners(QueryBuildConfiguration config) throws QueryDistributionException {
-		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryPartitionerParameter.class, "Query partitioner");
+		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryPartitionerParameter.class);
+		
+		if( pairs.isEmpty() ) {
+			pairs.add(determineDefaultQueryPartitioner());
+		}
 		
 		List<InterfaceParametersPair<IQueryPartitioner>> resultList = Lists.newArrayList();
 		for( InterfaceNameParametersPair pair : pairs ) {
@@ -29,8 +46,24 @@ public final class ParameterHelper {
 		return resultList;
 	}
 
+	private static InterfaceNameParametersPair determineDefaultQueryPartitioner() throws QueryDistributionException {
+		String defaultPartitionerFromConfig = OdysseusNetConfiguration.get(DEFAULT_QUERY_PARTITIONER_CONFIG_KEY, DEFAULT_QUERY_PARTITIONER_NAME);
+		if( !QueryPartitionerRegistry.getInstance().contains(defaultPartitionerFromConfig) ) {
+			throw new QueryDistributionException("Could not find default query partitioner " + defaultPartitionerFromConfig );
+		}
+		
+		return new InterfaceNameParametersPair(defaultPartitionerFromConfig, Lists.newArrayList());
+	}
+
 	public static List<InterfaceParametersPair<IQueryPartModificator>> determineQueryPartModificators(QueryBuildConfiguration config) throws QueryDistributionException {
-		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryPartModificatorParameter.class, "Query part modificator", true);
+		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryPartModificatorParameter.class);
+		
+		if( pairs.isEmpty() ) {
+			List<InterfaceNameParametersPair> defaultModificators = determineDefaultQueryPartModificators();
+			if( !defaultModificators.isEmpty() ) {
+				pairs.addAll(defaultModificators);
+			}
+		}
 		
 		List<InterfaceParametersPair<IQueryPartModificator>> resultList = Lists.newArrayList();
 		for( InterfaceNameParametersPair pair : pairs ) {
@@ -39,8 +72,33 @@ public final class ParameterHelper {
 		return resultList;
 	}
 
+	private static List<InterfaceNameParametersPair> determineDefaultQueryPartModificators() throws QueryDistributionException {
+		Optional<String> optDefaultModificators = OdysseusNetConfiguration.get(DEFAULT_QUERY_PART_MODIFICATOR_CONFIG_KEY);
+		List<InterfaceNameParametersPair> result = Lists.newArrayList();
+		if( optDefaultModificators.isPresent() ) {
+			String defaultModificators = optDefaultModificators.get();
+			if( !Strings.isNullOrEmpty(defaultModificators)) {
+				String[] defaultModificatorsArray = defaultModificators.split(",");
+				for( String defaultModificator : defaultModificatorsArray ) {
+					if( !QueryPartModificatorRegistry.getInstance().contains(defaultModificator)) {
+						throw new QueryDistributionException("Could not find query part modificator " + defaultModificator);
+					}
+				
+					result.add(new InterfaceNameParametersPair(defaultModificator, Lists.newArrayList()));
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+
 	public static List<InterfaceParametersPair<IQueryPartAllocator>> determineQueryPartAllocators(QueryBuildConfiguration config) throws QueryDistributionException {
-		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryPartAllocatorParameter.class, "Query part allocator");
+		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryPartAllocatorParameter.class);
+		
+		if( pairs.isEmpty() ) {
+			pairs.add(determineDefaultQueryPartAllocator());
+		}
 		
 		List<InterfaceParametersPair<IQueryPartAllocator>> resultMap = Lists.newArrayList();
 		for( InterfaceNameParametersPair pair : pairs ) {
@@ -49,8 +107,24 @@ public final class ParameterHelper {
 		return resultMap;
 	}
 	
+	private static InterfaceNameParametersPair determineDefaultQueryPartAllocator() throws QueryDistributionException {
+		String defaultAllocatorFromConfig = OdysseusNetConfiguration.get(DEFAULT_QUERY_PART_ALLOCATOR_CONFIG_KEY, DEFAULT_QUERY_PART_ALLOCATOR_NAME);
+		if( !QueryPartAllocatorRegistry.getInstance().contains(defaultAllocatorFromConfig) ) {
+			throw new QueryDistributionException("Could not find default query part allocator " + defaultAllocatorFromConfig );
+		}
+		
+		return new InterfaceNameParametersPair(defaultAllocatorFromConfig, Lists.newArrayList());
+	}
+
 	public static List<InterfaceParametersPair<IQueryDistributionPreProcessor>> determineQueryDistributionPreProcessors(QueryBuildConfiguration config) throws QueryDistributionException {
-		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryDistributionPreProcessorParameter.class, "Query distribution preprocessor", true);
+		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryDistributionPreProcessorParameter.class);
+		
+		if( pairs.isEmpty() ) {
+			List<InterfaceNameParametersPair> defaultPreprocessors = determineDefaultPreProcessors();
+			if( !defaultPreprocessors.isEmpty() ) {
+				pairs.addAll(defaultPreprocessors);
+			}
+		}
 		
 		List<InterfaceParametersPair<IQueryDistributionPreProcessor>> resultMap = Lists.newArrayList();
 		for( InterfaceNameParametersPair pair : pairs ) {
@@ -59,8 +133,35 @@ public final class ParameterHelper {
 		return resultMap;
 	}
 	
+	private static List<InterfaceNameParametersPair> determineDefaultPreProcessors() throws QueryDistributionException {
+		Optional<String> optDefaultPreProcessors = OdysseusNetConfiguration.get(DEFAULT_PREPROCESSORS_CONFIG_KEY);
+		List<InterfaceNameParametersPair> result = Lists.newArrayList();
+		if( optDefaultPreProcessors.isPresent() ) {
+			String defaultPreProcessors = optDefaultPreProcessors.get();
+			if( !Strings.isNullOrEmpty(defaultPreProcessors)) {
+				String[] defaultPreProcessorsArray = defaultPreProcessors.split(",");
+				for( String defaultPreProcessor : defaultPreProcessorsArray ) {
+					if( !QueryDistributionPreProcessorRegistry.getInstance().contains(defaultPreProcessor)) {
+						throw new QueryDistributionException("Could not find query distribution preprocessor " + defaultPreProcessor);
+					}
+					
+					result.add(new InterfaceNameParametersPair(defaultPreProcessor, Lists.newArrayList()));
+				}
+			}
+		}
+		
+		return result;
+	}
+
 	public static List<InterfaceParametersPair<IQueryDistributionPostProcessor>> determineQueryDistributionPostProcessors(QueryBuildConfiguration config) throws QueryDistributionException {
-		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryDistributionPostProcessorParameter.class, "Query distribution postprocessor", true);
+		List<InterfaceNameParametersPair> pairs = getPairsOfParameter(config, QueryDistributionPostProcessorParameter.class);
+		
+		if( pairs.isEmpty() ) {
+			List<InterfaceNameParametersPair> defaultPostProcessors = determineDefaultPostProcessors();
+			if( !defaultPostProcessors.isEmpty() ) {
+				pairs.addAll(defaultPostProcessors);
+			}
+		}
 		
 		List<InterfaceParametersPair<IQueryDistributionPostProcessor>> resultMap = Lists.newArrayList();
 		for( InterfaceNameParametersPair pair : pairs ) {
@@ -69,18 +170,30 @@ public final class ParameterHelper {
 		return resultMap;
 	}
 	
-	private static List<InterfaceNameParametersPair> getPairsOfParameter( QueryBuildConfiguration config, Class<? extends AbstractQueryDistributionParameter> settingType, String settingName ) throws QueryDistributionException {
-		return getPairsOfParameter(config, settingType, settingName, false);
+	private static List<InterfaceNameParametersPair> determineDefaultPostProcessors() throws QueryDistributionException {
+		Optional<String> optDefaultPostProcessors = OdysseusNetConfiguration.get(DEFAULT_POSTPROCESSORS_CONFIG_KEY);
+		List<InterfaceNameParametersPair> result = Lists.newArrayList();
+		if( optDefaultPostProcessors.isPresent() ) {
+			String defaultPostProcessors = optDefaultPostProcessors.get();
+			if( !Strings.isNullOrEmpty(defaultPostProcessors)) {
+				String[] defaultPostProcessorsArray = defaultPostProcessors.split(",");
+				for( String defaultPostProcessor : defaultPostProcessorsArray ) {
+					if( !QueryDistributionPostProcessorRegistry.getInstance().contains(defaultPostProcessor)) {
+						throw new QueryDistributionException("Could not find query distribution postprocessor " + defaultPostProcessor);
+					}
+					
+					result.add(new InterfaceNameParametersPair(defaultPostProcessor, Lists.newArrayList()));
+				}
+			}
+		}
+		
+		return result;
 	}
-	
-	private static List<InterfaceNameParametersPair> getPairsOfParameter( QueryBuildConfiguration config, Class<? extends AbstractQueryDistributionParameter> settingType, String settingName, boolean isOptional ) throws QueryDistributionException {
+
+	private static List<InterfaceNameParametersPair> getPairsOfParameter( QueryBuildConfiguration config, Class<? extends AbstractQueryDistributionParameter> settingType)  {
 		AbstractQueryDistributionParameter setting = config.get(settingType);
 		if( setting == null ) {
-			if( isOptional ) {
-				return Lists.newArrayList();
-			}
-			
-			throw new QueryDistributionException("Setting of " + settingName + " is not set but needed.");
+			return Lists.newArrayList();
 		}
 				
 		return setting.getPairs();
