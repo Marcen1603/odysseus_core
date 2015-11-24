8eb0947386a56cc711cbb18556e6eb6556137996
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-11-24 14:00:02 +0000

    OdysseusNet: added console command to list stored distributed data from container
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26544 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 537f5c0ee23fd9cfba7cee38e15ed00e55628b22

--- server/net/data/de.uniol.inf.is.odysseus.net.data.console/src/de/uniol/inf/is/odysseus/net/data/console/DistributedDataConsole.java
+++ server/net/data/de.uniol.inf.is.odysseus.net.data.console/src/de/uniol/inf/is/odysseus/net/data/console/DistributedDataConsole.java
@@ -1,12 +1,20 @@
 package de.uniol.inf.is.odysseus.net.data.console;
 
+import java.util.Collection;
+
+import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.eclipse.osgi.framework.console.CommandProvider;
 
+import com.google.common.base.Strings;
+
+import de.uniol.inf.is.odysseus.net.data.IDistributedData;
 import de.uniol.inf.is.odysseus.net.data.IDistributedDataManager;
+import de.uniol.inf.is.odysseus.net.data.container.IDistributedDataContainer;
 
 public class DistributedDataConsole implements CommandProvider {
 
 	private static IDistributedDataManager distributedDataManager;
+	private static IDistributedDataContainer ddContainer;
 
 	// called by OSGi-DS
 	public static void bindDistributedDataManager(IDistributedDataManager serv) {
@@ -20,6 +28,18 @@ public class DistributedDataConsole implements CommandProvider {
 		}
 	}
 	
+	// called by OSGi-DS
+	public static void bindDistributedDataContainer(IDistributedDataContainer serv) {
+		ddContainer = serv;
+	}
+
+	// called by OSGi-DS
+	public static void unbindDistributedDataContainer(IDistributedDataContainer serv) {
+		if (ddContainer == serv) {
+			ddContainer = null;
+		}
+	}
+	
 	@Override
 	public String getHelp() {
 		StringBuilder sb = new StringBuilder();
@@ -27,4 +47,34 @@ public class DistributedDataConsole implements CommandProvider {
 		return sb.toString();
 	}
 
+	public void _listDistributedData( CommandInterpreter ci ) {
+		String filter = ci.nextArgument();
+		
+		Collection<String> names = ddContainer.getNames();
+		if( names.isEmpty() ) {
+			ci.println("Distributed DataContainer is empty");
+			return;
+		}
+		
+		for( String name : names ) {
+			
+			if( Strings.isNullOrEmpty(filter) || name.contains(filter)) {
+				Collection<IDistributedData> datas = ddContainer.get(name);
+				if( !datas.isEmpty() ) {
+					ci.println(name);
+					for( IDistributedData data : datas ) {
+						ci.print("\t");
+						ci.print(data.getUUID());
+						ci.print(":");
+						ci.println(data.getData().toString());
+					}
+					ci.println();
+				}
+			}
+		}
+	}
+	
+	public void _lsDistributedData( CommandInterpreter ci ) {
+		_listDistributedData(ci);
+	}
 }
