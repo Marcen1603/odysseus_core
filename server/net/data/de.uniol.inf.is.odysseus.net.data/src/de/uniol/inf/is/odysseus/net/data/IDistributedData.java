ff86d5c8f6af4ca1e78a995032e2b1d5134b20d9
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-11-24 13:48:36 +0000

    OdysseusNet: definded distributed data interface
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26539 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: db94d1db2781b16454f0ef2c0a63afaf5e4027f8

--- server/net/data/de.uniol.inf.is.odysseus.net.data/src/de/uniol/inf/is/odysseus/net/data/IDistributedData.java
+++ server/net/data/de.uniol.inf.is.odysseus.net.data/src/de/uniol/inf/is/odysseus/net/data/IDistributedData.java
@@ -1,5 +1,17 @@
 package de.uniol.inf.is.odysseus.net.data;
 
+import java.util.UUID;
+
+import org.json.JSONObject;
+
 public interface IDistributedData {
 
+	public String getName();
+	public UUID getUUID();
+	
+	public JSONObject getData();
+	public boolean isPersistent();
+	public long getLifetime();
+	
+	public long getTimestamp();
 }
