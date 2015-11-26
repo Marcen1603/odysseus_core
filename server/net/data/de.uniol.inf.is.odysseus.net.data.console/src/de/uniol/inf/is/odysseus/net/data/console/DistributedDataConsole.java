2791e1a7000d27d3b09d5658443645228ae19b8d
Timo Michelsen <Timo.Michelsen@gmail.com>
2015-11-26 11:02:52 +0000

    OdysseusNet: Added console command to get details about distributed data (e.g., for debugging)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26566 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 899495f18eab899b42d528ff18f4af3bfc502cfb

--- server/net/data/de.uniol.inf.is.odysseus.net.data.console/src/de/uniol/inf/is/odysseus/net/data/console/DistributedDataConsole.java
+++ server/net/data/de.uniol.inf.is.odysseus.net.data.console/src/de/uniol/inf/is/odysseus/net/data/console/DistributedDataConsole.java
@@ -1,10 +1,15 @@
 package de.uniol.inf.is.odysseus.net.data.console;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
+import java.util.UUID;
 
 import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.eclipse.osgi.framework.console.CommandProvider;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Strings;
 
 import de.uniol.inf.is.odysseus.net.data.IDistributedData;
@@ -12,6 +17,8 @@ import de.uniol.inf.is.odysseus.net.data.IDistributedDataManager;
 
 public class DistributedDataConsole implements CommandProvider {
 
+	private static final DateFormat DATE_FORMAT = new SimpleDateFormat();
+	
 	private static IDistributedDataManager distributedDataManager;
 
 	// called by OSGi-DS
@@ -31,6 +38,7 @@ public class DistributedDataConsole implements CommandProvider {
 		StringBuilder sb = new StringBuilder();
 		sb.append("--- Distributed Data Commands ---\n");
 		sb.append("  listDistributedData/ls... <filter>                      - Lists stored data in distributed data container\n");
+		sb.append("  describeDistributedData/desc... <ddid | ddname>         - Prints more details about a specific distributed data element\n");
 		return sb.toString();
 	}
 
@@ -68,5 +76,69 @@ public class DistributedDataConsole implements CommandProvider {
 	
 	public void _lsDistributedData( CommandInterpreter ci ) {
 		_listDistributedData(ci);
+	}
+	
+	public void _describeDistributedData( CommandInterpreter ci ) {
+		if( distributedDataManager == null ) {
+			ci.println("Distributed data manager not available");
+			return;
+		}
+
+		String dataText = ci.nextArgument();
+		if( Strings.isNullOrEmpty(dataText)) {
+			ci.println("usage: describeDistributedData <ddname | ddid>");
+			return;
+		}
+		
+		Optional<UUID> optUUID = tryToUUID(dataText);
+		if( optUUID.isPresent() ) {
+			UUID distributedDataUUID = optUUID.get();
+			
+			Optional<IDistributedData> optDistributedData = distributedDataManager.get(distributedDataUUID);
+			if( optDistributedData.isPresent() ) {
+				IDistributedData distributedData = optDistributedData.get();
+				printDistributedDataDetails(ci, distributedData);
+			} else {
+				ci.println("Distributed data with UUID " + distributedDataUUID + " not found");
+			}
+		} else {
+			Collection<IDistributedData> distributedDatas = distributedDataManager.get(dataText);
+			if( !distributedDatas.isEmpty() ) {
+				ci.println("There are " + distributedDatas.size() + " entries with name " + dataText);
+				ci.println();
+				for( IDistributedData distributedData : distributedDatas ) {
+					printDistributedDataDetails(ci, distributedData);
+					ci.println();
+				}
+			} else {
+				ci.println("There is no distributed data with name " + dataText);
+			}
+		}
+	}
+
+	private void printDistributedDataDetails(CommandInterpreter ci, IDistributedData distributedData) {
+		ci.println("UUID          : " + distributedData.getUUID());
+		ci.println("Name          : " + distributedData.getName());
+		ci.println("Creater NodeID: " + distributedData.getCreator());
+		ci.println("Persistent    : " + distributedData.isPersistent());
+		ci.println("Lifetime      : " + distributedData.getLifetime());
+		ci.println("Timestamp     : " + distributedData.getTimestamp() + " (" + convertTimestampToDate(distributedData.getTimestamp()) + ")");
+		ci.println("Data          : " + distributedData.getData());
+	}
+
+	private Optional<UUID> tryToUUID(String text) {
+		try {
+			return Optional.of(UUID.fromString(text));
+		} catch( IllegalArgumentException ex ) {
+			return Optional.absent();
+		}
+	}
+	
+	private static String convertTimestampToDate(long startupTimestamp) {
+		return DATE_FORMAT.format(new Date(startupTimestamp));
+	}
+	
+	public void _descDistributedData( CommandInterpreter ci ) {
+		_describeDistributedData(ci);
 	}
 }
