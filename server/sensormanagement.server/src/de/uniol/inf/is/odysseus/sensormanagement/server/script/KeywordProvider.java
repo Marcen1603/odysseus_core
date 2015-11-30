e574fc675305356e8de2e5a8769c6f730642420d
Henrik Surm <hsurm@uni-oldenburg.de>
2015-11-30 09:39:22 +0000

    Added Odysseus Script keywords, ignored bin folders
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26612 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 205bbd1490ed7d082e467777ba2eb9ab276c6d6c

---
+++ server/sensormanagement.server/src/de/uniol/inf/is/odysseus/sensormanagement/server/script/KeywordProvider.java
@@ -0,0 +1,20 @@
+package de.uniol.inf.is.odysseus.sensormanagement.server.script;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.script.parser.IPreParserKeyword;
+import de.uniol.inf.is.odysseus.script.parser.IPreParserKeywordProvider;
+
+public class KeywordProvider implements IPreParserKeywordProvider {
+
+	@Override
+	public Map<String, Class<? extends IPreParserKeyword>> getKeywords() {
+		Map<String, Class<? extends IPreParserKeyword>> keywords = new HashMap<String, Class<? extends IPreParserKeyword>>();
+		
+		keywords.put(InitSensorManagementKeyword.NAME, InitSensorManagementKeyword.class);
+
+		return keywords;
+	}
+
+}
