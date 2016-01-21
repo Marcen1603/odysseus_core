3058452819bd5aa61e251f38bbc1747d4361c27f
Christian Kuka <christian@kuka.cc>
2016-01-21 16:50:07 +0000

    Add double click listener to PQL operator view to insert operator name and parameter
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@26913 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 58df0c19412d6eec54a9619bba48d9cd6ddf3cb3

--- client/studio/rcp.editor.text.pql/src/de/uniol/inf/is/odysseus/rcp/editor/text/pql/PQLOperatorView.java
+++ client/studio/rcp.editor.text.pql/src/de/uniol/inf/is/odysseus/rcp/editor/text/pql/PQLOperatorView.java
@@ -2,25 +2,40 @@ package de.uniol.inf.is.odysseus.rcp.editor.text.pql;
 
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalOperatorInformation;
+import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalParameterInformation;
 import de.uniol.inf.is.odysseus.rcp.OdysseusRCPPlugIn;
 import de.uniol.inf.is.odysseus.rcp.editor.text.pql.actions.OpenWikiAction;
 
 public class PQLOperatorView extends ViewPart {
+    private static final Logger LOG = LoggerFactory.getLogger(PQLOperatorView.class);
 
 	private TreeViewer treeViewer;
 	private boolean showOptionalParameters = true;
@@ -67,7 +82,36 @@ public class PQLOperatorView extends ViewPart {
 
         // Set the MenuManager
         treeViewer.getControl().setMenu(contextMenu);
+        treeViewer.addDoubleClickListener(new IDoubleClickListener() {
 
+            @Override
+            public void doubleClick(DoubleClickEvent event) {
+
+                if (event.getSelection().isEmpty()) {
+                    return;
+                }
+                if (event.getSelection() instanceof IStructuredSelection) {
+                    IStructuredSelection selection = (IStructuredSelection) event.getSelection();
+                    for (Iterator<?> iterator = selection.iterator(); iterator.hasNext();) {
+                        try {
+                            Object value = iterator.next();
+                            if (value instanceof LogicalOperatorInformation) {
+                                insertSelection((LogicalOperatorInformation) value);
+                            }
+                            else if (value instanceof LogicalParameterInformation) {
+                                insertSelection((LogicalParameterInformation) value);
+                            }
+                        }
+                        catch (BadLocationException e) {
+                            LOG.error(e.getMessage(), e);
+                        }
+
+                    }
+                }
+
+            }
+
+        });
 	}
 
 	@Override
@@ -135,4 +179,34 @@ public class PQLOperatorView extends ViewPart {
 		});
 		return unsortedList;
 	}
+
+    private void insertSelection(LogicalOperatorInformation element) throws BadLocationException {
+        IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+        if (editor instanceof ITextEditor) {
+            ISelectionProvider selectionProvider = ((ITextEditor) editor).getSelectionProvider();
+            ISelection selection = selectionProvider.getSelection();
+            final IDocumentProvider documentProvider = ((ITextEditor) editor).getDocumentProvider();
+            final IDocument document = documentProvider.getDocument(editor.getEditorInput());
+            if (selection instanceof ITextSelection) {
+                ITextSelection textSelection = (ITextSelection) selection;
+                document.replace(textSelection.getOffset(), 0, element.getOperatorName());
+
+            }
+        }
+    }
+
+    private void insertSelection(LogicalParameterInformation element) throws BadLocationException {
+        IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+        if (editor instanceof ITextEditor) {
+            ISelectionProvider selectionProvider = ((ITextEditor) editor).getSelectionProvider();
+            ISelection selection = selectionProvider.getSelection();
+            final IDocumentProvider documentProvider = ((ITextEditor) editor).getDocumentProvider();
+            final IDocument document = documentProvider.getDocument(editor.getEditorInput());
+            if (selection instanceof ITextSelection) {
+                ITextSelection textSelection = (ITextSelection) selection;
+                document.replace(textSelection.getOffset(), 0, element.getName());
+
+            }
+        }
+    }
 }
