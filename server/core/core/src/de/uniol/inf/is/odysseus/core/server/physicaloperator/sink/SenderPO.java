3e0f166552da29829153874319756d04f000e2f2
Michael Brand <michael.brand@uni-oldenburg.de>
2016-02-02 15:30:47 +0000

    Reverted "SenderPO: empty methods for subclasses to post process elements". Not needed.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27000 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 32dd1af35aeb5fa282d67542f0ef91ff4490c30a

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/sink/SenderPO.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/sink/SenderPO.java
@@ -32,45 +32,31 @@ public class SenderPO<T extends IStreamObject<?>> extends AbstractSink<T> {
 		if (action != null) {
 			switch (action) {
 			case REMOVE_QUERY:
-					
+
 				break;
 			case STOP_QUERY:
-				for(IOperatorOwner owner : getOwner()){
+				for (IOperatorOwner owner : getOwner()) {
 					close(owner);
 				}
 				break;
 			}
 		} else {
-			// Pre processing stuff
-			this.preProcessElement(object, port);
-			
 			try {
 				protocolHandler.write(object);
 			} catch (IOException e) {
-				LOG.error("Could not write {} into {}", new Object[] { object,
-						protocolHandler, e });
+				LOG.error("Could not write {} into {}", new Object[] { object, protocolHandler, e });
 			}
-			
-			// Post processing stuff
-			this.postProcessElement(object, port);
 		}
 
 	}
 
 	@Override
 	public void processPunctuation(IPunctuation punctuation, int port) {
-		// Pre processing stuff
-		this.preProcessPunctuation(punctuation, port);
-					
 		try {
 			protocolHandler.writePunctuation(punctuation);
 		} catch (IOException e) {
-			LOG.error("Could not write {} into {}", new Object[] { punctuation,
-					protocolHandler, e });
+			LOG.error("Could not write {} into {}", new Object[] { punctuation, protocolHandler, e });
 		}
-		
-		// Post processing stuff
-		this.postProcessPunctuation(punctuation, port);
 	}
 
 	@Override
@@ -93,8 +79,7 @@ public class SenderPO<T extends IStreamObject<?>> extends AbstractSink<T> {
 				protocolHandler.close();
 				super.process_close();
 			} catch (Exception e) {
-				LOG.error("Could not close protocol handler {}",
-						protocolHandler, e);
+				LOG.error("Could not close protocol handler {}", protocolHandler, e);
 				throw new OpenFailedException(e);
 			}
 		}
@@ -103,52 +88,6 @@ public class SenderPO<T extends IStreamObject<?>> extends AbstractSink<T> {
 	@Override
 	public AbstractSink<T> clone() {
 		return new SenderPO<T>(this);
-	}
-	
-	/**
-	 * Override this method to pre process elements. <br />
-	 * <br />
-	 * This method is called by {@link #process_next(IStreamObject, int)} before writing the element by the protocol handler.
-	 * @param element See {@link #process_next(IStreamObject, int)}.
-	 * @param port See {@link #process_next(IStreamObject, int)}.
-	 */
-	protected void preProcessElement(@SuppressWarnings("unused") T element, @SuppressWarnings("unused") int port) {
-		return;
-	}
-	
-	/**
-	 * Override this method to pre process punctuations. <br />
-	 * <br />
-	 * This method is called by {@link #processPunctuation(IPunctuation, int)} before writing the punctuation by the protocol handler.
-	 * @param punctuation See {@link #processPunctuation(IPunctuation, int)}.
-	 * @param port See {@link #processPunctuation(IPunctuation, int)}.
-	 */
-	@SuppressWarnings("static-method")
-	protected void preProcessPunctuation(@SuppressWarnings("unused") IPunctuation punctuation, @SuppressWarnings("unused") int port) {
-		return;
-	}
-	
-	/**
-	 * Override this method to post process elements. <br />
-	 * <br />
-	 * This method is called by {@link #process_next(IStreamObject, int)} after writing the element by the protocol handler.
-	 * @param element See {@link #process_next(IStreamObject, int)}.
-	 * @param port See {@link #process_next(IStreamObject, int)}.
-	 */
-	protected void postProcessElement(@SuppressWarnings("unused") T element, @SuppressWarnings("unused") int port) {
-		return;
-	}
-	
-	/**
-	 * Override this method to post process punctuations. <br />
-	 * <br />
-	 * This method is called by {@link #processPunctuation(IPunctuation, int)} after writing the punctuation by the protocol handler.
-	 * @param punctuation See {@link #processPunctuation(IPunctuation, int)}.
-	 * @param port See {@link #processPunctuation(IPunctuation, int)}.
-	 */
-	@SuppressWarnings("static-method")
-	protected void postProcessPunctuation(@SuppressWarnings("unused") IPunctuation punctuation, @SuppressWarnings("unused") int port) {
-		return;
 	}
 
 }
