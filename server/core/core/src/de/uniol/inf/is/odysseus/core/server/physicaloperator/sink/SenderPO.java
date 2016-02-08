7b1611bdeb92dcee624a61c215533dd0aa498824
Cornelius Ludmann <Cornelius.Ludmann@uni-oldenburg.de>
2016-02-08 09:46:38 +0000

    SenderPO: Added key value provider.
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27033 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 15868275fd60840b9c57d3d53e5a0b03acd1cbdc

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/sink/SenderPO.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/sink/SenderPO.java
@@ -1,19 +1,25 @@
 package de.uniol.inf.is.odysseus.core.server.physicaloperator.sink;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.uniol.inf.is.odysseus.core.datahandler.IStreamObjectDataHandler;
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
+import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperatorKeyValueProvider;
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPunctuation;
 import de.uniol.inf.is.odysseus.core.physicaloperator.OpenFailedException;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.AbstractProtocolHandler;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.IProtocolHandler;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.ProtocolHandlerAction;
+import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportHandler;
 import de.uniol.inf.is.odysseus.core.planmanagement.IOperatorOwner;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.AbstractSink;
 
-public class SenderPO<T extends IStreamObject<?>> extends AbstractSink<T> {
+public class SenderPO<T extends IStreamObject<?>> extends AbstractSink<T> implements IPhysicalOperatorKeyValueProvider {
 	static Logger LOG = LoggerFactory.getLogger(SenderPO.class);
 	private IProtocolHandler<T> protocolHandler;
 
@@ -88,6 +94,28 @@ public class SenderPO<T extends IStreamObject<?>> extends AbstractSink<T> {
 	@Override
 	public AbstractSink<T> clone() {
 		return new SenderPO<T>(this);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperatorKeyValueProvider#getKeyValues()
+	 */
+	@Override
+	public Map<String, String> getKeyValues() {
+		Map<String, String> m = new HashMap<>();
+		if(protocolHandler instanceof IPhysicalOperatorKeyValueProvider) {
+			m.putAll(((IPhysicalOperatorKeyValueProvider) protocolHandler).getKeyValues());
+		}
+		if(protocolHandler instanceof AbstractProtocolHandler) {
+			ITransportHandler transportHandler = ((AbstractProtocolHandler<?>) protocolHandler).getTransportHandler();
+			if(transportHandler instanceof IPhysicalOperatorKeyValueProvider) {
+				m.putAll(((IPhysicalOperatorKeyValueProvider) transportHandler).getKeyValues());
+			}
+			IStreamObjectDataHandler<?> dataHandler = ((AbstractProtocolHandler<?>) protocolHandler).getDataHandler();
+			if(dataHandler instanceof IPhysicalOperatorKeyValueProvider) {
+				m.putAll(((IPhysicalOperatorKeyValueProvider) dataHandler).getKeyValues());
+			}
+		}
+		return m;
 	}
 
 }
