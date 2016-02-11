60fff015fc521bebf04fb185aedf022b46fba7e1
Michael Brand <michael.brand@uni-oldenburg.de>
2016-02-11 08:26:02 +0000

    Recovery: no need to calc the end of convergence phase for time windows (see comments in source code)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27071 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 37f4b932b918b84b14625f059136a6139ea7d14a

--- server/recovery/de.uniol.inf.is.odysseus.recovery.gaprecovery/src/de/uniol/inf/is/odysseus/recovery/gaprecovery/physicaloperator/TWConvergenceDetectorPO.java
+++ server/recovery/de.uniol.inf.is.odysseus.recovery.gaprecovery/src/de/uniol/inf/is/odysseus/recovery/gaprecovery/physicaloperator/TWConvergenceDetectorPO.java
@@ -2,12 +2,8 @@ package de.uniol.inf.is.odysseus.recovery.gaprecovery.physicaloperator;
 
 import java.util.Arrays;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
-import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
 import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.TimeWindowAO;
 import de.uniol.inf.is.odysseus.recovery.gaprecovery.GapRecoveryExecutor;
@@ -31,11 +27,6 @@ import de.uniol.inf.is.odysseus.trust.Trust;
 @SuppressWarnings("nls")
 public class TWConvergenceDetectorPO<StreamObject extends IStreamObject<IMetaAttribute>>
 		extends AbstractConvergenceDetectorPO<StreamObject> {
-
-	/**
-	 * The logger for all classes related to the Convergence Detector.
-	 */
-	private static final Logger cLog = LoggerFactory.getLogger("ConvergenceDetector");
 
 	/**
 	 * The time stamp, which marks the end of the convergence phase. Inclusive,
@@ -75,65 +66,84 @@ public class TWConvergenceDetectorPO<StreamObject extends IStreamObject<IMetaAtt
 
 	@Override
 	protected void process_next(StreamObject object, int port) {
-		if(this.correctMetaData == null) {
+		if (this.correctMetaData == null) {
 			// Check meta data
-			this.correctMetaData = new Boolean(Arrays.asList(object.getMetadata().getClasses()).containsAll(Arrays.asList(NEEDED_METADATA_CLASSES)));
+			this.correctMetaData = new Boolean(Arrays.asList(object.getMetadata().getClasses())
+					.containsAll(Arrays.asList(NEEDED_METADATA_CLASSES)));
 		}
-		
+
 		if (this.mEndReached || !this.correctMetaData.booleanValue()) {
 			// We're done - shortcut
 			transfer(object);
 			return;
 		}
-		
-		PointInTime ts = ((ITimeInterval) object.getMetadata()).getStart();
-		if (this.mTconv == null) {
-			/*
-			 * First element after gap recovery, so we have to calculate Tconv:
-			 * 
-			 * Tconf = ts + lc
-			 * 
-			 * lc is the length of the convergence phase, so its endpoint is at
-			 * ts + lc.
-			 * 
-			 * lc = omega - pos(e)
-			 * 
-			 * pos(e) is the position of the element within the latest open
-			 * window measured in time instants, so omega - pos(e) is the time
-			 * span left in window and therefore it is the convergence phase
-			 * length.
-			 * 
-			 * pos(e) = (ts-t0) mod b
-			 * 
-			 * t0 is the point in time opening the first window (in Odysseus t0
-			 * is always 0).
-			 */
-			this.mTconv = PointInTime.plus(ts, getWindowWidth() - (ts.getMainPoint() % getWindowAdvance()));
 
-			if (cLog.isDebugEnabled()) {
-				cLog.debug("Start of convergence phase is {}.", ts);
-				cLog.debug("End of convergence phase is calculated as {}.", this.mTconv);
-			}
-		} else if (ts.after(this.mTconv)) {
-			/*
-			 * Were are after the convergence phase
-			 */
-			this.mEndReached = true;
-			if (cLog.isDebugEnabled()) {
-				cLog.debug("First point in time after convergence phase is {}.", ts);
-			}
-		}
-		// Else we are in a convergence phase
-
-		if (!this.mEndReached) {
-			/*
-			 * We can not trust that element, e.g., a wrong aggregation due to the
-			 * gap. At this juncture, there is no possibility in Odysseus to
-			 * determine Repeatable operators.
-			 */
-			((ITrust) object.getMetadata()).setTrust(0);
-		}
+		((ITrust) object.getMetadata()).setTrust(0);
+		this.mEndReached = true;
 		transfer(object);
+
+		/*
+		 * It is not necessary to calculate the end of the convergence phase.
+		 * The trust will be set to 0 for every element aggregated with the
+		 * first after offline phase (meta data merge).
+		 */
+
+		// PointInTime ts = ((ITimeInterval) object.getMetadata()).getStart();
+		// if (this.mTconv == null) {
+		// /*
+		// * First element after gap recovery, so we have to calculate Tconv:
+		// *
+		// * Tconf = ts + lc
+		// *
+		// * lc is the length of the convergence phase, so its endpoint is at
+		// * ts + lc.
+		// *
+		// * lc = omega - pos(e)
+		// *
+		// * pos(e) is the position of the element within the latest open
+		// * window measured in time instants, so omega - pos(e) is the time
+		// * span left in window and therefore it is the convergence phase
+		// * length.
+		// *
+		// * pos(e) = (ts-t0) mod b
+		// *
+		// * t0 is the point in time opening the first window (in Odysseus t0
+		// * is always 0).
+		// */
+		//// this.mTconv = PointInTime.plus(ts, getWindowWidth() -
+		// (ts.getMainPoint() % getWindowAdvance()));
+		// long pos = ts.getMainPoint() % getWindowAdvance();
+		// System.err.println(pos);
+		// long lc = getWindowWidth() - pos;
+		// System.err.println(lc);
+		// this.mTconv = PointInTime.plus(ts, lc);
+		// System.err.println(this.mTconv);
+		//
+		// if (cLog.isDebugEnabled()) {
+		// cLog.debug("Start of convergence phase is {}.", ts);
+		// cLog.debug("End of convergence phase is calculated as {}.",
+		// this.mTconv);
+		// }
+		// } else if (ts.after(this.mTconv)) {
+		// /*
+		// * Were are after the convergence phase
+		// */
+		// this.mEndReached = true;
+		// if (cLog.isDebugEnabled()) {
+		// cLog.debug("First point in time after convergence phase is {}.", ts);
+		// }
+		// }
+		// // Else we are in a convergence phase
+		//
+		// if (!this.mEndReached) {
+		// /*
+		// * We can not trust that element, e.g., a wrong aggregation due to the
+		// * gap. At this juncture, there is no possibility in Odysseus to
+		// * determine Repeatable operators.
+		// */
+		// ((ITrust) object.getMetadata()).setTrust(0);
+		// }
+		// transfer(object);
 
 	}
 
