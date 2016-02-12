faa54f1110b3a4f949845936730b6ae6eebb947e
Timo Michelsen <Timo.Michelsen@gmail.com>
2016-02-12 09:46:09 +0000

    VisualOdysseusScript: DefinesVisualOdysseusScriptBlock "gesprengt"
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27098 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5dfb35ba990c902eed4ce4fd9e138834bd3cd7ef

--- monolithic/studio/de.uniol.inf.is.odysseus.rcp.editor.script/src/de/uniol/inf/is/odysseus/rcp/editor/script/blocks/DefinesVisualOdysseusScriptBlock.java
+++ monolithic/studio/de.uniol.inf.is.odysseus.rcp.editor.script/src/de/uniol/inf/is/odysseus/rcp/editor/script/blocks/DefinesVisualOdysseusScriptBlock.java
@@ -1,58 +1,38 @@
 package de.uniol.inf.is.odysseus.rcp.editor.script.blocks;
 
-import java.util.List;
 import java.util.Map;
 
-import org.eclipse.jface.layout.TableColumnLayout;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.ColumnWeightData;
-import org.eclipse.jface.viewers.ICellModifier;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TableViewerColumn;
-import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.TableItem;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Lists;
 
-import de.uniol.inf.is.odysseus.core.collection.Pair;
 import de.uniol.inf.is.odysseus.rcp.editor.script.IVisualOdysseusScriptBlock;
 import de.uniol.inf.is.odysseus.rcp.editor.script.IVisualOdysseusScriptContainer;
 import de.uniol.inf.is.odysseus.rcp.editor.script.VisualOdysseusScriptException;
 import de.uniol.inf.is.odysseus.rcp.editor.script.impl.VisualOdysseusScriptPlugIn;
-import de.uniol.inf.is.odysseus.rcp.util.CellLabelProviderAndSorter;
+import de.uniol.inf.is.odysseus.rcp.util.ImageButton;
 
-public class DefinesVisualOdysseusScriptBlock implements IVisualOdysseusScriptBlock {
+public class DefinesVisualOdysseusScriptBlock implements IVisualOdysseusScriptBlock, IDefinitionsListListener {
 
-	private static class StringPair extends Pair<String, String> {
-		private static final long serialVersionUID = 1L;
+	private DefinitionsList definitionsList = new DefinitionsList();
 
-		public StringPair(String str1, String str2) {
-			super(str1, str2);
-		}
-	}
-
-	private List<StringPair> pairs = Lists.newArrayList();
+	private IVisualOdysseusScriptContainer container;
+	private DefinitionsTableViewer tableViewer;
 
 	public DefinesVisualOdysseusScriptBlock(Map<String, String> keyValuePairs) {
 		Preconditions.checkNotNull(keyValuePairs, "keyValuePairs must not be null!");
 
 		for (String key : keyValuePairs.keySet()) {
-			pairs.add(new StringPair(key, keyValuePairs.get(key)));
+			definitionsList.add(new Definition(key, keyValuePairs.get(key)));
 		}
 	}
-	
+
 	@Override
 	public String getTitle() {
 		return "Definitions";
@@ -60,15 +40,17 @@ public class DefinesVisualOdysseusScriptBlock implements IVisualOdysseusScriptBl
 
 	@Override
 	public void createPartControl(Composite parent, IVisualOdysseusScriptContainer container) {
+		this.container = container;
+
 		Composite topComposite = new Composite(parent, SWT.NONE);
 		topComposite.setLayout(new GridLayout());
-			
+
 		Composite contentComposite = new Composite(topComposite, SWT.NONE);
 		contentComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		GridLayout contentLayout = new GridLayout(2, false);
 		contentLayout.horizontalSpacing = 0;
 		contentComposite.setLayout(contentLayout);
-		
+
 		Composite buttonComposite = new Composite(contentComposite, SWT.NONE);
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.verticalSpacing = 0;
@@ -79,177 +61,72 @@ public class DefinesVisualOdysseusScriptBlock implements IVisualOdysseusScriptBl
 		buttonCompositeGridData.verticalAlignment = SWT.TOP;
 		buttonComposite.setLayoutData(buttonCompositeGridData);
 
-		/*** Table ***/
-		Composite tableComposite = new Composite(contentComposite, SWT.BORDER);
-		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-		gd.verticalAlignment = SWT.TOP;
-		gd.grabExcessVerticalSpace = true;
-		tableComposite.setLayoutData(gd);
-		TableColumnLayout tableColumnLayout = new TableColumnLayout();
-		tableComposite.setLayout(tableColumnLayout);
+		tableViewer = new DefinitionsTableViewer(contentComposite, definitionsList);
 
-		TableViewer tableViewer = new TableViewer(tableComposite, SWT.SINGLE | SWT.FULL_SELECTION);
-		tableViewer.getTable().setHeaderVisible(true);
-		tableViewer.getTable().setLinesVisible(true);
-		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
-
-		/************* Key ****************/
-		TableViewerColumn keyColumn = new TableViewerColumn(tableViewer, SWT.NONE);
-		keyColumn.getColumn().setText("Key");
-		keyColumn.setLabelProvider(new CellLabelProviderAndSorter<StringPair, String>(tableViewer, keyColumn) {
-			@Override
-			protected String getValue(StringPair pair) {
-				return pair.getE1();
-			}
-		});
-		tableColumnLayout.setColumnData(keyColumn.getColumn(), new ColumnWeightData(10, 15, true));
-
-		/************* Value ****************/
-		TableViewerColumn valueColumn = new TableViewerColumn(tableViewer, SWT.NONE);
-		valueColumn.getColumn().setText("Value");
-		valueColumn.setLabelProvider(new CellLabelProviderAndSorter<StringPair, String>(tableViewer, valueColumn) {
-			@Override
-			protected String getValue(StringPair pair) {
-				return pair.getE2();
-			}
-		});
-		tableColumnLayout.setColumnData(valueColumn.getColumn(), new ColumnWeightData(10, 15, true));
-
-		// cell editing
-		tableViewer.setCellModifier(new ICellModifier() {
-
-			@Override
-			public boolean canModify(Object element, String property) {
-				return true;
-			}
-
-			@Override
-			public Object getValue(Object element, String property) {
-				StringPair pair = (StringPair) element;
-				if ("key".equals(property)) {
-					return pair.getE1();
-				}
-				return pair.getE2();
-			}
-
-			@Override
-			public void modify(Object element, String property, Object value) {
-				TableItem tableItem = (TableItem) element;
-				StringPair row = (StringPair) tableItem.getData();
-				String valueStr = (String) value;
-				if ("key".equals(property)) {
-					if (!row.getE1().equals(valueStr)) {
-						row.setE1((String) value);
-						
-						container.setDirty(true);
-						tableViewer.refresh();
-					}
-				} else {
-					if (!row.getE2().equals(valueStr)) {
-						row.setE2((String) value);
-						
-						container.setDirty(true);
-						tableViewer.refresh();
-					}
-				}
-			}
-		});
-
-		tableViewer.setColumnProperties(new String[] { "key", "value" });
-		tableViewer.setCellEditors(new CellEditor[] { new TextCellEditor(tableViewer.getTable()), new TextCellEditor(tableViewer.getTable()) });
-
-		tableViewer.setInput(pairs);
-		
 		/*** Buttons ***/
-		Button addButton = createImageButton(buttonComposite, "add", "Add new definition");
-		addButton.addSelectionListener(new SelectionAdapter() {
+		ImageButton addButton = createImageButtonWithLayout(buttonComposite, "add", "Add new definition");
+		addButton.getButton().addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				pairs.add(new StringPair("key", "value"));
-				
-				markChanged(container, tableViewer);
+				definitionsList.addNewDefinition();
 			}
 		});
-		
-		Button removeButton = createImageButton(buttonComposite, "remove", "Remove definition");
-		removeButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				Optional<StringPair> optPair = getTableViewerSelection(tableViewer);
-				if( optPair.isPresent() ) {
-					pairs.remove(optPair.get());
-					
-					markChanged(container, tableViewer);
-				}
-			}
-		});
-		
-		Button moveUpButton = createImageButton(buttonComposite, "moveUp", "Move definition up");
-		moveUpButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				Optional<StringPair> optPair = getTableViewerSelection(tableViewer);
-				if( optPair.isPresent() ) {
-					int index = pairs.indexOf(optPair.get());
-					if( index > 0 ) {
-						StringPair temp = pairs.get(index - 1);
-						pairs.set(index - 1, optPair.get());
-						pairs.set(index, temp);
 
-						markChanged(container, tableViewer);
-					}
+		ImageButton removeButton = createImageButtonWithLayout(buttonComposite, "remove", "Remove definition");
+		removeButton.getButton().addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Optional<Definition> optPair = tableViewer.getSelection();
+				if (optPair.isPresent()) {
+					definitionsList.remove(optPair.get());
+				}
+			}
+		});
+
+		ImageButton moveUpButton = createImageButtonWithLayout(buttonComposite, "moveUp", "Move definition up");
+		moveUpButton.getButton().addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Optional<Definition> optPair = tableViewer.getSelection();
+				if (optPair.isPresent()) {
+					definitionsList.moveUp(optPair.get());
+				}
+			}
+		});
+
+		ImageButton moveDownButton = createImageButtonWithLayout(buttonComposite, "moveDown", "Move definition down");
+		moveDownButton.getButton().addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Optional<Definition> optPair = tableViewer.getSelection();
+				if (optPair.isPresent()) {
+					definitionsList.moveDown(optPair.get());
 				}
 			}
 		});
 		
-		Button moveDownButton = createImageButton(buttonComposite, "moveDown", "Move definition down");
-		moveDownButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				Optional<StringPair> optPair = getTableViewerSelection(tableViewer);
-				if( optPair.isPresent() ) {
-					int index = pairs.indexOf(optPair.get());
-					if( index < pairs.size() - 1 ) {
-						StringPair temp = pairs.get(index + 1);
-						pairs.set(index + 1, optPair.get());
-						pairs.set(index, temp);
-						
-						markChanged(container, tableViewer);
-					}
-				}
-			}
-		});
+		definitionsList.addListener(this);
+	}
+
+	@Override
+	public void definitionListChanged(DefinitionsList sender) {
+		markChanged();
 	}
 
 	@Override
 	public void dispose() {
+		tableViewer.dispose();
+		definitionsList.removeListener(this);
+	}
 
+	private static ImageButton createImageButtonWithLayout(Composite parent, String imageKey, String tooltipText) {
+		ImageButton imageButton = new ImageButton(parent, VisualOdysseusScriptPlugIn.getImageManager().get(imageKey), tooltipText);
+		imageButton.getButton().setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		return imageButton;
 	}
-	
-	@SuppressWarnings("unchecked")
-	private static <T> Optional<T> getTableViewerSelection( TableViewer tableViewer ) {
-		ISelection selection = tableViewer.getSelection();
-		if( selection.isEmpty() ) {
-			return Optional.absent();
-		}
-		
-		IStructuredSelection structSelection = (IStructuredSelection) selection;
-		return Optional.of((T)structSelection.getFirstElement());
-	}
-	
-	private static Button createImageButton( Composite parent, String imageKey, String tooltipText) {
-		Button button = new Button(parent, SWT.PUSH);
-		button.setImage(VisualOdysseusScriptPlugIn.getImageManager().get(imageKey));
-		button.setToolTipText(tooltipText);
-		button.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
-		return button;
-	}
-	
-	private static void markChanged(IVisualOdysseusScriptContainer container, TableViewer tableViewer ) {
-		tableViewer.refresh();
-		tableViewer.getTable().getParent().layout();
-		
+
+	private void markChanged() {
 		container.layoutAll();
 		container.setDirty(true);
 	}
@@ -258,17 +135,16 @@ public class DefinesVisualOdysseusScriptBlock implements IVisualOdysseusScriptBl
 	public String generateOdysseusScript() throws VisualOdysseusScriptException {
 		StringBuilder scriptBuilder = new StringBuilder();
 
-		for( int i = 0; i < pairs.size(); i++ ) {
-			StringPair pair = pairs.get(i);
-			
-			if( i != 0 ) {
+		for (int i = 0; i < definitionsList.size(); i++) {
+			Definition pair = definitionsList.get(i);
+
+			if (i != 0) {
 				scriptBuilder.append("\n");
 			}
-			
+
 			scriptBuilder.append("#DEFINE ").append(pair.getE1()).append(" ").append(pair.getE2());
 		}
-		
+
 		return scriptBuilder.toString();
 	}
-
 }
---
+++ monolithic/studio/de.uniol.inf.is.odysseus.rcp.editor.script/src/de/uniol/inf/is/odysseus/rcp/editor/script/blocks/IDefinitionsListListener.java
@@ -0,0 +1,7 @@
+package de.uniol.inf.is.odysseus.rcp.editor.script.blocks;
+
+public interface IDefinitionsListListener {
+
+	public void definitionListChanged( DefinitionsList sender );
+	
+}
