8864f694d6d7584148e365d5adc480a436bdec9b
Timo Michelsen <Timo.Michelsen@gmail.com>
2016-02-25 09:26:33 +0000

    OdysseusNet: Added option to choose ports between query parts randomly (instead of linearly)
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27233 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 524da3babf6520185c6a2c516db8ddd10bd2040f

--- server/net/querydistribute/de.uniol.inf.is.odysseus.net.querydistribute/src/de/uniol/inf/is/odysseus/net/querydistribute/transmit/ServerPortReserver.java
+++ server/net/querydistribute/de.uniol.inf.is.odysseus.net.querydistribute/src/de/uniol/inf/is/odysseus/net/querydistribute/transmit/ServerPortReserver.java
@@ -1,7 +1,7 @@
 package de.uniol.inf.is.odysseus.net.querydistribute.transmit;
 
-import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,37 +18,56 @@ import de.uniol.inf.is.odysseus.net.config.OdysseusNetConfiguration;
 public class ServerPortReserver implements IOdysseusNodeCommunicatorListener {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ServerPortReserver.class);
-	
+
 	private static final String MINPORT_CONFIG_KEY = "net.querydistribute.minport";
 	private static final String MAXPORT_CONFIG_KEY = "net.querydistribute.maxport";
+	private static final String RANDOMPORT_CONFIG_KEY = "net.querydistribute.randomport";
 	private static final int MINPORT_DEFAULT = 10000;
 	private static final int MAXPORT_DEFAULT = 20000;
+	private static final boolean RANDOMPORT_DEFAULT = false;
+	private static final int MAX_RANDOM_TRIES = 10;
+
+	private static final Random RAND = new Random();
 
 	private IOdysseusNodeCommunicator nodeCommunicator;
-	
+
 	private int minPort;
 	private int maxPort;
+	private boolean randomPort;
 	private int nextFreePort;
-	
+
 	public void start(IOdysseusNodeCommunicator communicator) {
 		Preconditions.checkNotNull(communicator, "communicator must not be null!");
 		nodeCommunicator = communicator;
-		
-		
+
 		minPort = determineMinPort();
 		maxPort = determineMaxPort();
+		randomPort = determineRandomPort();
 		nextFreePort = minPort;
 
 		nodeCommunicator.registerMessageType(ReserveServerPortMessage.class);
 		nodeCommunicator.registerMessageType(ServerPortReservedMessage.class);
-		
+
 		nodeCommunicator.addListener(this, ReserveServerPortMessage.class);
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("ServerPortReserver started. MinPort is {}, maxPort is {}", minPort, maxPort);
+			if (randomPort) {
+				LOG.debug("ServerPortReserver chooses randomly");
+			} else {
+				LOG.debug("ServerPortReserver chooses linear from min to max");
+			}
+		}
 	}
-	
+
+	private static boolean determineRandomPort() {
+		return determineBooleanConfigValue(RANDOMPORT_CONFIG_KEY, RANDOMPORT_DEFAULT);
+	}
+
 	private static int determineMinPort() {
 		return determineIntConfigValue(MINPORT_CONFIG_KEY, MINPORT_DEFAULT);
 	}
-	
+
 	private static int determineMaxPort() {
 		return determineIntConfigValue(MAXPORT_CONFIG_KEY, MAXPORT_DEFAULT);
 	}
@@ -60,9 +79,11 @@ public class ServerPortReserver implements IOdysseusNodeCommunicatorListener {
 
 	@Override
 	public void receivedMessage(IOdysseusNodeCommunicator communicator, IOdysseusNode senderNode, IMessage message) {
-		if( message instanceof ReserveServerPortMessage) {
+		if (message instanceof ReserveServerPortMessage) {
+			LOG.debug("Beginning reserving port for node {}", senderNode);
 			int reservedPort = reservePort();
-			
+			LOG.debug("Reserved port is {}", reservedPort);
+
 			ServerPortReservedMessage answer = new ServerPortReservedMessage(reservedPort);
 			try {
 				communicator.send(senderNode, answer);
@@ -73,36 +94,58 @@ public class ServerPortReserver implements IOdysseusNodeCommunicatorListener {
 	}
 
 	private synchronized int reservePort() {
-		for( int port = nextFreePort; port <= maxPort; port++ ) {
-			try {
-				ServerSocket s = new ServerSocket(port);
-				s.close();
-				
-				nextFreePort = port + 1;
-				if( nextFreePort > maxPort ) {
-					nextFreePort = minPort; // loop
+		if (!randomPort) {
+			for (int port = nextFreePort; port <= maxPort; port++) {
+				if (isPortFree(port)) {
+					nextFreePort = port + 1;
+					if (nextFreePort > maxPort) {
+						nextFreePort = minPort; // loop
+					}
+
+					return port;
 				}
-				
-				return port;
-				
-			} catch (IOException e) {
-				// failed to create server socket --> port not free
+			}
+		} else {
+			for( int tryIndex = 0; tryIndex < MAX_RANDOM_TRIES; tryIndex++ ) {
+				int port = RAND.nextInt(maxPort - minPort) + minPort;
+				if(isPortFree(port)) {
+					return port;
+				}
 			}
 		}
-		
+
 		LOG.error("Could not reserve a port between {} and {}", minPort, maxPort);
-		
+
 		return -1; // failed to reserve port
 	}
-	
-	private static int determineIntConfigValue( String key, int defaultValue ) {
-		String portStr = OdysseusNetConfiguration.get(key, defaultValue + "");
+
+	private boolean isPortFree(int port) {
 		try {
-			return Integer.valueOf(portStr);
-		} catch( Throwable t ) {
-			LOG.error("Could not determine minimum port from config", t);
-			return MINPORT_DEFAULT;
-		}		
+			ServerSocket s = new ServerSocket(port);
+			s.close();
+			return true;
+		} catch (Throwable t) {
+			return false;
+		}
 	}
 
+	private static int determineIntConfigValue(String key, int defaultValue) {
+		String cfgValue = OdysseusNetConfiguration.get(key, defaultValue + "");
+		try {
+			return Integer.valueOf(cfgValue);
+		} catch (Throwable t) {
+			LOG.error("Could not determine minimum port from config", t);
+			return defaultValue;
+		}
+	}
+
+	private static boolean determineBooleanConfigValue(String key, boolean defaultValue) {
+		String cfgValue = OdysseusNetConfiguration.get(key, defaultValue + "");
+		try {
+			return Boolean.valueOf(cfgValue);
+		} catch (Throwable t) {
+			LOG.error("Could not determine minimum port from config", t);
+			return defaultValue;
+		}
+	}
 }
