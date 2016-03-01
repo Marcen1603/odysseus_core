39445ec1effbc1f238b665ce21055ac5e97f309f
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2016-03-01 14:46:20 +0000

    Removed Warning
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27305 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: aa649a5155e2418ae7cd618584a45c69c72fd235

--- common/mep/src/de/uniol/inf/is/odysseus/mep/functions/list/ListFirstElementFunction.java
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/functions/list/ListFirstElementFunction.java
@@ -2,8 +2,6 @@ package de.uniol.inf.is.odysseus.mep.functions.list;
 
 import java.util.List;
 
-import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
-
 public class ListFirstElementFunction extends AbstractListPosElementFunction {
 
 	private static final long serialVersionUID = -3282877303737235603L;
