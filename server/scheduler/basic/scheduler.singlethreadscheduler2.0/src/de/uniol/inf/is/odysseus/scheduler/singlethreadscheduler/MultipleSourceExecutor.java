2b05607ae2ec1444d4b38daf2a0cc76e86ed2820
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2016-04-21 12:59:07 +0000

    Removed "Hack" ... Just to see if jenkins errors stay
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27474 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 5304f71ed8dafb317f19525c1b5800539a3edea5

--- server/scheduler/basic/scheduler.singlethreadscheduler2.0/src/de/uniol/inf/is/odysseus/scheduler/singlethreadscheduler/MultipleSourceExecutor.java
+++ server/scheduler/basic/scheduler.singlethreadscheduler2.0/src/de/uniol/inf/is/odysseus/scheduler/singlethreadscheduler/MultipleSourceExecutor.java
@@ -62,7 +62,7 @@ public class MultipleSourceExecutor extends Thread implements ISourceExecutor {
 				while (sources.size() > 0) {
 					updateSources();
 					// Need to delay some time to catch an open
-					delay(10);
+					//delay(10);
 					boolean processableSources = processSources();
 					updateSources();
 					if (!processableSources) {
