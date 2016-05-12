63a41e829ab14ae9da1df88a9f47a01b35582218
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2016-05-12 08:37:00 +0000

    Remove Principal from Interface ...
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27497 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 3fe95169093f31df97eaa4264a6be32a24b3cf33

--- common/core/src/de/uniol/inf/is/odysseus/core/usermanagement/ITenant.java
+++ common/core/src/de/uniol/inf/is/odysseus/core/usermanagement/ITenant.java
@@ -1,11 +1,9 @@
 package de.uniol.inf.is.odysseus.core.usermanagement;
 
 import java.io.Serializable;
-import java.security.Principal;
 
-public interface ITenant extends IAbstractEntity, Principal, Comparable<ITenant>, Serializable{
+public interface ITenant extends IAbstractEntity, Comparable<ITenant>, Serializable{
 	
-	@Override
 	String getName();
 	void setName(String name);
 
