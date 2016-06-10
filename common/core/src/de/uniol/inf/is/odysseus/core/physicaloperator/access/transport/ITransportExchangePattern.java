42ca4952cfb694694ec185cd6afae4d989d5ee53
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2016-06-10 08:50:10 +0000

    Missing commit
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27561 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 62d3d31d6fbab38da806ea81328a314d4616adff

--- common/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/transport/ITransportExchangePattern.java
+++ common/core/src/de/uniol/inf/is/odysseus/core/physicaloperator/access/transport/ITransportExchangePattern.java
@@ -4,5 +4,5 @@ package de.uniol.inf.is.odysseus.core.physicaloperator.access.transport;
  * @author Christian Kuka <christian.kuka@offis.de>
  */
 public enum ITransportExchangePattern {
-    InOnly, RobustInOnly, InOut, InOptionalOut, OutOnly, RobustOutOnly, OutIn, OutOptionalIn
+    InOnly, RobustInOnly, InOut, InOptionalOut, OutOnly, RobustOutOnly, OutIn, OutOptionalIn, Undefined
 }
