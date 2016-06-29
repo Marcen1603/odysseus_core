112faada0678163293687d5542966fe9635b9152
Michael Brand <michael.brand@uni-oldenburg.de>
2016-06-29 04:41:28 +0000

    Enhanced operator collector class to collect stateful operators and controllable subscriptions from physical plan
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27595 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: 83b79204f4acea53b19e03904053032a4e065e69

--- common/core/src/de/uniol/inf/is/odysseus/core/util/OperatorCollector.java
+++ common/core/src/de/uniol/inf/is/odysseus/core/util/OperatorCollector.java
@@ -3,11 +3,16 @@ package de.uniol.inf.is.odysseus.core.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.uniol.inf.is.odysseus.core.ISubscriber;
+import de.uniol.inf.is.odysseus.core.ISubscription;
 import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
 import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalSubscription;
+import de.uniol.inf.is.odysseus.core.physicaloperator.ControllablePhysicalSubscription;
+import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
+import de.uniol.inf.is.odysseus.core.physicaloperator.IStatefulPO;
 
 /**
- * Helper class to collect operators from plan.
+ * Helper class to collect operators and subscriptions from plan.
  * 
  * @author Michael Brand
  *
@@ -36,6 +41,75 @@ public class OperatorCollector {
 		ops.add(op);
 		for (LogicalSubscription sub : op.getSubscribedToSource()) {
 			collectRecursively(sub.getTarget(), ops);
+		}
+	}
+
+	/**
+	 * Collects all stateful operators from a physical plan.
+	 */
+	public static List<IStatefulPO> collect(List<IPhysicalOperator> roots) {
+		List<IStatefulPO> ops = new ArrayList<>();
+		for (IPhysicalOperator root : roots) {
+			collectRecursively(root, ops);
+		}
+		return ops;
+	}
+
+	/**
+	 * Collects all stateful operators from a physical plan recursively from top
+	 * to sources.
+	 * 
+	 * @param op
+	 *            The current operator to add and check.
+	 * @param operators
+	 *            All already collected operators.
+	 */
+	private static void collectRecursively(IPhysicalOperator op, List<IStatefulPO> ops) {
+		if (op instanceof IStatefulPO) {
+			ops.add((IStatefulPO) op);
+		}
+
+		if (op instanceof ISubscriber) {
+			@SuppressWarnings("unchecked")
+			ISubscriber<? extends IPhysicalOperator, ISubscription<? extends IPhysicalOperator>> sink = (ISubscriber<? extends IPhysicalOperator, ISubscription<? extends IPhysicalOperator>>) op;
+			for (ISubscription<? extends IPhysicalOperator> sub : sink.getSubscribedToSource()) {
+				collectRecursively(sub.getTarget(), ops);
+			}
+		}
+	}
+
+	/**
+	 * Collects all controllable physical subscriptions from a physical plan.
+	 */
+	public static List<ControllablePhysicalSubscription<? extends IPhysicalOperator>> collectSubcriptions(
+			List<IPhysicalOperator> roots) {
+		List<ControllablePhysicalSubscription<? extends IPhysicalOperator>> subs = new ArrayList<>();
+		for (IPhysicalOperator root : roots) {
+			collectSubscriptionsRecursively(root, subs);
+		}
+		return subs;
+	}
+
+	/**
+	 * Collects all controllable physical subscriptions from a physical plan
+	 * recursively from top to sources.
+	 * 
+	 * @param op
+	 *            The current operator.
+	 * @param operators
+	 *            All already collected controllable subscriptions.
+	 */
+	private static void collectSubscriptionsRecursively(IPhysicalOperator op,
+			List<ControllablePhysicalSubscription<? extends IPhysicalOperator>> subs) {
+		if (op instanceof ISubscriber) {
+			@SuppressWarnings("unchecked")
+			ISubscriber<? extends IPhysicalOperator, ISubscription<? extends IPhysicalOperator>> sink = (ISubscriber<? extends IPhysicalOperator, ISubscription<? extends IPhysicalOperator>>) op;
+			for (ISubscription<? extends IPhysicalOperator> sub : sink.getSubscribedToSource()) {
+				if (sub instanceof ControllablePhysicalSubscription) {
+					subs.add((ControllablePhysicalSubscription<? extends IPhysicalOperator>) sub);
+				}
+				collectSubscriptionsRecursively(sub.getTarget(), subs);
+			}
 		}
 	}
 
