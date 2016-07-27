eeda42facd5ee337465144b0885eab4b491b209b
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2016-07-27 01:29:16 +0000

    Removed unused code
    git-svn-id: http://isdb1.offis.uni-oldenburg.de/repos/odysseus/trunk@27661 aa0bda67-78ed-0310-a382-c5338b0f89ba
    
    Former-commit-id: c75962d82913f99798b4157e205b195f0dc06fc0

--- server/core/de.uniol.inf.is.odysseus.rest/src/de/uniol/inf/is/odysseus/rest/serverresources/GetQueryIdsServerResource.java
+++ server/core/de.uniol.inf.is.odysseus.rest/src/de/uniol/inf/is/odysseus/rest/serverresources/GetQueryIdsServerResource.java
@@ -11,6 +11,12 @@ import de.uniol.inf.is.odysseus.rest.ExecutorServiceBinding;
 import de.uniol.inf.is.odysseus.rest.dto.request.SessionRequestDTO;
 import de.uniol.inf.is.odysseus.rest.dto.response.GenericResponseDTO;
 
+/**
+ * Retrieve all query ids
+ * @author Marco Grawunder
+ *
+ */
+
 public class GetQueryIdsServerResource extends AbstractSessionServerResource {
 
 	public static final String PATH = "getQueryIds";
@@ -18,17 +24,7 @@ public class GetQueryIdsServerResource extends AbstractSessionServerResource {
 	@Post
 	public GenericResponseDTO<List<Integer>> getQueryIds(SessionRequestDTO sessionRequestDTO){
 		ISession session = this.loginWithToken(sessionRequestDTO.getToken());
-		Collection<Integer> quids = ExecutorServiceBinding.getExecutor().getLogicalQueryIds(session) ;
-//		// First simple test with String
-//		StringBuilder result = new StringBuilder();
-//		Iterator<Integer> iter = quids.iterator();
-//		if (iter.hasNext()){
-//			result.append(iter.next());
-//		}
-//		while(iter.hasNext()){
-//			result.append(",").append(iter.next());
-//		}
-		
+		Collection<Integer> quids = ExecutorServiceBinding.getExecutor().getLogicalQueryIds(session) ;		
 		return new GenericResponseDTO<List<Integer>>(new ArrayList<Integer>(quids));
 	}
 }
