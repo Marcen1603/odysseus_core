9ff86d702498776fbdb36c6640608dbcb734211b
Michael Brand <michael.brand@uni-oldenburg.de>
2016-11-11 17:12:49 +0100

    ODY-1152: Precise Recovery Fix
    Duplication elimination should not compare all
    meta data; only TI if present
    
    Former-commit-id: 380ab5fa8faf97e9db3816eb263a6675ac1583ed

--- server/recovery/de.uniol.inf.is.odysseus.recovery.duplicateelemination/src/de/uniol/inf/is/odysseus/recovery/duplicateelemination/physicaloperator/SenderWithDuplicateEliminationPO.java
+++ server/recovery/de.uniol.inf.is.odysseus.recovery.duplicateelemination/src/de/uniol/inf/is/odysseus/recovery/duplicateelemination/physicaloperator/SenderWithDuplicateEliminationPO.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
+import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
 import de.uniol.inf.is.odysseus.core.physicaloperator.IPunctuation;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.IProtocolHandler;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.sink.SenderPO;
@@ -20,7 +21,7 @@ import de.uniol.inf.is.odysseus.core.util.OsgiObjectInputStream;
  * operator is in recovery mode, the last stored element is read from disk and
  * all elements, which arrive before that element, will be discarded (because
  * they are duplicates).
- * 
+ *
  * @author Michael Brand
  */
 public class SenderWithDuplicateEliminationPO<StreamObject extends IStreamObject<?>>
@@ -44,7 +45,7 @@ public class SenderWithDuplicateEliminationPO<StreamObject extends IStreamObject
 
 	/**
 	 * Creates a new sender.
-	 * 
+	 *
 	 * @param protocolHandler
 	 *            See {@link SenderPO#SenderPO(IProtocolHandler)}.
 	 * @param sinkName
@@ -65,7 +66,7 @@ public class SenderWithDuplicateEliminationPO<StreamObject extends IStreamObject
 
 	/**
 	 * Creates a new sender as a copy of an existing one.
-	 * 
+	 *
 	 * @param other
 	 *            The sender to clone.
 	 */
@@ -80,9 +81,23 @@ public class SenderWithDuplicateEliminationPO<StreamObject extends IStreamObject
 	protected void process_next(StreamObject object, int port) {
 		if (this.watermarkReached) {
 			super.process_next(object, port);
-		} else if (object.equals((IStreamObject<IMetaAttribute>) this.watermark, true)) {
+		} else if (object.equals((IStreamObject<IMetaAttribute>) this.watermark, false)
+				&& equalTimeIntervals(object, this.watermark)) {
 			this.watermarkReached = true;
 		}
+	}
+
+	private boolean equalTimeIntervals(StreamObject object1, StreamObject object2) {
+		if (!(object1.getMetadata() instanceof ITimeInterval) || !(object2.getMetadata() instanceof ITimeInterval)) {
+			// No TI used. Hard to say whether such objects should be marked as
+			// equals. But for me, they are more equals than not equals.
+			return true;
+		}
+		ITimeInterval ti1 = (ITimeInterval) object1.getMetadata();
+		ITimeInterval ti2 = (ITimeInterval) object2.getMetadata();
+		// Calling equals on ti1 and ti2 does not work for combined meta
+		// attributes
+		return ti1.getStart().equals(ti2.getStart()) && ti1.getEnd().equals(ti2.getEnd());
 	}
 
 	@Override
