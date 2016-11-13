f110574901fd82256c8f3ad708ed2a9cef50564b
Dennis Nowak <dennis.nowak@uni-oldenburg.de>
2016-11-13 01:14:00 +0100

    fixed warnings
    Former-commit-id: 9385e81b7272dfe8ad1c4305e150ea53a9c9da27

--- server/parallelization/parallelization.initialization/src/de/uniol/inf/is/odysseus/parallelization/initialization/plananalysis/StandardPlanAnalyser.java
+++ server/parallelization/parallelization.initialization/src/de/uniol/inf/is/odysseus/parallelization/initialization/plananalysis/StandardPlanAnalyser.java
@@ -5,12 +5,8 @@ package de.uniol.inf.is.odysseus.parallelization.initialization.plananalysis;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import de.uniol.inf.is.odysseus.core.logicaloperator.ILogicalOperator;
 import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
-import de.uniol.inf.is.odysseus.core.server.util.FindSourcesLogicalVisitor;
 import de.uniol.inf.is.odysseus.core.server.util.GenericGraphWalker;
 import de.uniol.inf.is.odysseus.parallelization.initialization.strategies.IParallelizationIndividualConfiguration;
 
@@ -19,15 +15,13 @@ import de.uniol.inf.is.odysseus.parallelization.initialization.strategies.IParal
  *
  */
 public class StandardPlanAnalyser extends AbstractPlanAnalyser {
-	
-	private static Logger LOG = LoggerFactory.getLogger(StandardPlanAnalyser.class);
 
 	/* (non-Javadoc)
 	 * @see de.uniol.inf.is.odysseus.parallelization.initialization.plananalysis.IPlanAnalyser#getParalisableOperators(de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery)
 	 */
 	@Override
 	public List<IParallelizationIndividualConfiguration> getParalisableOperators(ILogicalQuery query) {
-		GenericGraphWalker graphWalker = new GenericGraphWalker<>();
+		GenericGraphWalker<ILogicalOperator> graphWalker = new GenericGraphWalker<>();
 		FindParallelizationPossibilitiesVisitor<ILogicalOperator> parallelizeVisitor = new FindParallelizationPossibilitiesVisitor<>(String.valueOf(query.getID()));
 		graphWalker.prefixWalk(query.getLogicalPlan(), parallelizeVisitor);
 		return parallelizeVisitor.getResult();
