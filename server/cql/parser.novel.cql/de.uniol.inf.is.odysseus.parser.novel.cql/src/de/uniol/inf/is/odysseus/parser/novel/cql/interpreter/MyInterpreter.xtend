713a8bfdbf05ab80c83f6543c1a5407385840e07
Jens Plümer <jens.pluemer@uni-oldenburg.de>
2016-11-16 12:11:39 +0100

    Problems with class files.
    Former-commit-id: e89aeb943090552369c82f4c51d77bd04a7c2a37

---
+++ server/cql/parser.novel.cql/de.uniol.inf.is.odysseus.parser.novel.cql/src/de/uniol/inf/is/odysseus/parser/novel/cql/interpreter/MyInterpreter.xtend
@@ -0,0 +1,9 @@
+package de.uniol.inf.is.odysseus.parser.novel.cql.interpreter
+
+import org.eclipse.xtext.xbase.interpreter.impl.XbaseInterpreter
+
+class MyInterpreter extends XbaseInterpreter
+{
+
+	
+}
---
+++ server/cql/parser.novel.cql/de.uniol.inf.is.odysseus.parser.novel.cql/xtend-gen/de/uniol/inf/is/odysseus/parser/novel/cql/interpreter/MyInterpreter.java
@@ -0,0 +1,7 @@
+package de.uniol.inf.is.odysseus.parser.novel.cql.interpreter;
+
+import org.eclipse.xtext.xbase.interpreter.impl.XbaseInterpreter;
+
+@SuppressWarnings("all")
+public class MyInterpreter extends XbaseInterpreter {
+}
