c18de8e2253b48675e6e38b9b5f18ef2d9da7c3f
Marco Grawunder <Marco Grawunder@uni-oldenburg.de>
2016-11-16 13:16:28 +0100

    ODY-1178: Datarate is now multivalue Former-commit-id: 15fa66c791e18086978b2402e028dc049b712e24

--- server/datarate/de.uniol.inf.is.odysseus.datarate/src/de/uniol/inf/is/odysseus/datarate/physicaloperator/DataratePO.java
+++ server/datarate/de.uniol.inf.is.odysseus.datarate/src/de/uniol/inf/is/odysseus/datarate/physicaloperator/DataratePO.java
@@ -11,62 +11,60 @@ import de.uniol.inf.is.odysseus.datarate.logicaloperator.DatarateAO;
 
 public class DataratePO<T extends IStreamObject<?>> extends AbstractPipe<T, T> {
 
-	private int updateRate;
-	
+	final private int updateRate;
+	final private String key;
+
 	private long elementsRead = 0;
 	private long lastTimestamp = -1;
 	private double currentDatarate = 0;
-	
+
 	public DataratePO( DatarateAO ao ) {
 		updateRate = ao.getUpdateRate();
+		key = ao.getKey();
 	}
-	
-	public DataratePO( DataratePO<T> other ) {
-		updateRate = other.updateRate;
-	}
-	
+
 	@Override
 	public OutputMode getOutputMode() {
 		return OutputMode.INPUT;
 	}
-	
+
 	@Override
 	protected void process_open() throws OpenFailedException {
 		elementsRead = 0;
 		lastTimestamp = System.nanoTime();
 	}
-	
+
 	@Override
 	protected void process_next(T object, int port) {
 		IMetaAttribute metadata = object.getMetadata();
 		if( metadata instanceof IDatarate ) {
 			if( updateRate > 0 ) {
-				
-				elementsRead++;		
+
+				elementsRead++;
 				if (elementsRead == updateRate) {
 					long now = System.nanoTime();
 					long lastPeriodNano = now - lastTimestamp;
-					
+
 					double lastDataRateNano = updateRate / (double)lastPeriodNano;
-					
+
 					currentDatarate = lastDataRateNano * 1000000000.0;
 					lastTimestamp = now;
 					elementsRead = 0;
 				}
-				
+
 			}
-			
-			((IDatarate)metadata).setDatarate(currentDatarate);
+
+			((IDatarate)metadata).setDatarate(key, currentDatarate);
 		}
-		
+
 		transfer(object);
 	}
-	
+
 	@Override
 	public void processPunctuation(IPunctuation punctuation, int port) {
 		sendPunctuation(punctuation);
 	}
-		
+
 	@Override
 	public boolean process_isSemanticallyEqual(IPhysicalOperator ipo) {
 		if( !(ipo instanceof DataratePO)) {
@@ -74,9 +72,5 @@ public class DataratePO<T extends IStreamObject<?>> extends AbstractPipe<T, T> {
 		}
 		return ((DataratePO<?>)ipo).updateRate == updateRate;
 	}
-	
-	@Override
-	public String getName() {
-		return "Datarate";
-	}
+
 }
