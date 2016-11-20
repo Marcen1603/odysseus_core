d901f03ec175b2b2fc41584e0e5b63465a89c74b
Christoph Schröer <christoph.schroeer@uni-oldenburg.de>
2016-11-20 15:43:44 +0100

    RestructHelper: InsertBefore with ports
    Former-commit-id: 19ff206eaf950a3e34ce46cc2df818340d1d2b9f

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/RestructHelper.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/RestructHelper.java
@@ -151,6 +151,34 @@ public class RestructHelper {
 		return ret;
 
 	}
+	
+	/**
+	 * Inserts a new logical operator (toInsert) before the operator before
+	 * (e.g. closer to the root!)
+	 * 
+	 * @param toInsert
+	 * @param before
+	 * @return
+	 */
+	public static Collection<ILogicalOperator> insertOperatorBefore(
+			ILogicalOperator toInsert, ILogicalOperator before,  int sinkInPort, int sourceOutPort) {
+		List<ILogicalOperator> ret = new ArrayList<>();
+		Collection<LogicalSubscription> subs = before.getSubscriptions();
+		for (LogicalSubscription sub : subs) {
+			if(sourceOutPort == sub.getSourceOutPort()){
+				before.unsubscribeSink(sub);
+				// What about the source out port
+				toInsert.subscribeSink(sub.getTarget(), sub.getSinkInPort(),
+						sub.getSourceOutPort(), sub.getSchema());
+				ret.add(sub.getTarget());
+			}
+		}
+		toInsert.subscribeToSource(before, sinkInPort, sourceOutPort, before.getOutputSchema());
+		ret.add(before);
+		ret.add(toInsert);
+		return ret;
+
+	}
 
 	public static Collection<ILogicalOperator> simpleOperatorSwitch(
 			UnaryLogicalOp father, UnaryLogicalOp son) {
