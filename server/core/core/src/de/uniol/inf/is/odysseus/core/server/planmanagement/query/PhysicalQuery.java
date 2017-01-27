c88e40fb2c8414a15f2fa27c9a2cf64deab9c743
Marco Grawunder <Marco Grawunder@uni-oldenburg.de>
2017-01-27 07:45:31 +0100

    Separate starting from opening
    Former-commit-id: 2775295827cc786df3157c73abdee506b40217d1

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/query/PhysicalQuery.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/query/PhysicalQuery.java
@@ -544,12 +544,17 @@ public class PhysicalQuery implements IPhysicalQuery {
 			this.isStarting = true;
 			doneRoots.clear();
 			this.queryListener = queryListener;
-			queryStartedTS = System.currentTimeMillis();
 			for (IPhysicalOperator curRoot : getRoots()) {
 				// this also works for cyclic plans,
 				// since if an operator is already open/started, the
 				// following roots will not be called any more.
 				curRoot.open(this);
+			}
+			queryStartedTS = System.currentTimeMillis();
+			for (IPhysicalOperator curRoot : getRoots()) {
+				// this also works for cyclic plans,
+				// since if an operator is already open/started, the
+				// following roots will not be called any more.
 				curRoot.start(this);
 			}
 			setState(nextState);
