bf37a59e72e971ac43a2839243ec9d9a19e18f66
Yannick Habecker <yannick@habecker.net>
2017-02-20 13:29:19 +0100

    moved tests to git
    Former-commit-id: bb53b6531f478ade52652d54467d2382101bc4d3

---
+++ server/nlp/de.uniol.inf.is.odysseus.nlp.test/test/de/uniol/inf/is/odysseus/nlp/test/datastructure/annotations/mock/AnotherTestAnnotation.java
@@ -0,0 +1,10 @@
+package de.uniol.inf.is.odysseus.nlp.test.datastructure.annotations.mock;
+
+import de.uniol.inf.is.odysseus.nlp.datastructure.annotations.Annotation;
+
+public class AnotherTestAnnotation extends Annotation{
+	@Override
+	public Object toObject() {
+		return null;
+	}
+}
---
+++ server/nlp/de.uniol.inf.is.odysseus.nlp.test/test/de/uniol/inf/is/odysseus/nlp/test/datastructure/annotations/mock/TestAnnotation.java
@@ -0,0 +1,10 @@
+package de.uniol.inf.is.odysseus.nlp.test.datastructure.annotations.mock;
+
+import de.uniol.inf.is.odysseus.nlp.datastructure.annotations.Annotation;
+
+public class TestAnnotation extends Annotation{
+	@Override
+	public Object toObject() {
+		return null;
+	}
+}
