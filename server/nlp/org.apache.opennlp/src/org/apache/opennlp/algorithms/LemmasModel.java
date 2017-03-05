ca804cd1fbfe6e69fe04851a9985c7e859db175c
Yannick Habecker <yannick@habecker.net>
2017-03-05 12:51:09 +0100

    Implemented lemmas Training
    Former-commit-id: 390fb086f5ee6c10784541969b9a5366c2c43187

--- server/nlp/org.apache.opennlp/src/org/apache/opennlp/algorithms/LemmasModel.java
+++ server/nlp/org.apache.opennlp/src/org/apache/opennlp/algorithms/LemmasModel.java
@@ -16,10 +16,22 @@ import de.uniol.inf.is.odysseus.nlp.datastructure.annotations.implementations.Pa
 import de.uniol.inf.is.odysseus.nlp.datastructure.annotations.implementations.Tokens;
 import de.uniol.inf.is.odysseus.nlp.datastructure.exception.NLPException;
 import de.uniol.inf.is.odysseus.nlp.datastructure.exception.NLPModelNotFoundException;
+import de.uniol.inf.is.odysseus.nlp.datastructure.exception.NLPTrainingFailedException;
+import opennlp.tools.chunker.ChunkSample;
+import opennlp.tools.chunker.ChunkSampleStream;
+import opennlp.tools.chunker.ChunkerFactory;
+import opennlp.tools.chunker.ChunkerME;
 import opennlp.tools.lemmatizer.DictionaryLemmatizer;
+import opennlp.tools.lemmatizer.LemmaSample;
+import opennlp.tools.lemmatizer.LemmaSampleStream;
 import opennlp.tools.lemmatizer.Lemmatizer;
+import opennlp.tools.lemmatizer.LemmatizerFactory;
 import opennlp.tools.lemmatizer.LemmatizerME;
 import opennlp.tools.lemmatizer.LemmatizerModel;
+import opennlp.tools.util.MarkableFileInputStreamFactory;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+import opennlp.tools.util.TrainingParameters;
 
 public class LemmasModel extends OpenNLPModel<Lemmas> {
 	private LemmatizerModel model;
@@ -43,7 +55,14 @@ public class LemmasModel extends OpenNLPModel<Lemmas> {
 
 	@Override
 	public void train(String languageCode, File file, String charSet, OptionMap options) {
-		// TODO Auto-generated method stub
+		try(ObjectStream<String> lineStream = new PlainTextByLineStream(new MarkableFileInputStreamFactory(file), charSet);
+				ObjectStream<LemmaSample> sampleStream = new LemmaSampleStream(lineStream)) {
+				LemmatizerFactory factory = new LemmatizerFactory();
+				model = LemmatizerME.train(languageCode, sampleStream, TrainingParameters.defaultParams(), factory);
+				this.lemmatizer = new LemmatizerME(model);
+			} catch (IOException e) {
+				throw new NLPTrainingFailedException(e.getMessage());
+			}
 	}
 
 	@Override
