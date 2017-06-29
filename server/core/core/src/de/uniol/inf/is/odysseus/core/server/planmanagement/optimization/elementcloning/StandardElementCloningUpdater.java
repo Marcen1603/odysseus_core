d8d0430807b3e80980f9e7e3b35882793f993b37
Hendrik Müller <hendrik.mueller@uni-oldenburg.de>
2017-06-29 17:20:07 +0200

    removed import warning
    Former-commit-id: 37c8c77ae9ac7da9071f0757f8edd1e096579750

--- server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/optimization/elementcloning/StandardElementCloningUpdater.java
+++ server/core/core/src/de/uniol/inf/is/odysseus/core/server/planmanagement/optimization/elementcloning/StandardElementCloningUpdater.java
@@ -7,7 +7,6 @@ import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.plan.IExecutionPlan;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.IPhysicalQuery;
 import de.uniol.inf.is.odysseus.core.server.usermanagement.SessionManagement;
-import de.uniol.inf.is.odysseus.core.server.usermanagement.UserManagementProvider;
 import de.uniol.inf.is.odysseus.core.server.util.GenericGraphWalker;
 import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
 import de.uniol.inf.is.odysseus.core.util.IGraphNodeVisitor;
