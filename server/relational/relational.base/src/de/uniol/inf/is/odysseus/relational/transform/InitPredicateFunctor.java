418250a6d50e9889df16086f97b01f6e8b7b9b38
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2017-08-16 13:07:58 +0200

    ODY-1325: Suppressed deprecation warning Former-commit-id: 16d59d286ca998b369207d56ab16b6b825b74dcc

--- server/relational/relational.base/src/de/uniol/inf/is/odysseus/relational/transform/InitPredicateFunctor.java
+++ server/relational/relational.base/src/de/uniol/inf/is/odysseus/relational/transform/InitPredicateFunctor.java
@@ -37,6 +37,7 @@ public class InitPredicateFunctor implements IUnaryFunctor<IPredicate<?>> {
 		this.leftSchema = op.getInputSchema(0);
 		this.rightSchema = op.getNumberOfInputs() > 1 ? op.getInputSchema(1) : null;
 	}
+	@SuppressWarnings("deprecation")
 	@Override
 	public void call(IPredicate<?> parameter) {
 		if(parameter instanceof IRelationalPredicate) {
