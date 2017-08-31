a88fd631beb4d4fe7dc86c22c08e8053fe0e48d9
Jens Plümer <jens.pluemer@uni-oldenburg.de>
2017-08-31 15:56:37 +0200

    Fixed eca-project and remove some temporary files from repo.
    Former-commit-id: af86cf37f87381171da24899d55d16d1e9d7b332

--- monolithic/eca/de.uniol.inf.is.odysseus.eca.plugin/src/de/uniol/inf/is/odysseus/eca/plugin/Activator.java
+++ monolithic/eca/de.uniol.inf.is.odysseus.eca.plugin/src/de/uniol/inf/is/odysseus/eca/plugin/Activator.java
@@ -3,6 +3,8 @@ package de.uniol.inf.is.odysseus.eca.plugin;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import de.uniol.inf.is.odysseus.rcp.OdysseusRCPPlugIn;
+
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
@@ -17,6 +19,7 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		OdysseusRCPPlugIn.waitForExecutor();
 	}
 
 	/*
