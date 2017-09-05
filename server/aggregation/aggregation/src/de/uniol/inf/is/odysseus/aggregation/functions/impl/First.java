a365d7ad9b0841e359ab126e09949e07e8cd9323
Tobias Brandt <t.brandt91@gmail.com>
2017-09-05 21:09:23 +0200

    ODY-1362 Added state from previous commit in spatial
    Former-commit-id: dbdb1e9a29364a3b99d87ca5df6ba5021fcff6d7

---
+++ server/aggregation/aggregation/src/de/uniol/inf/is/odysseus/aggregation/functions/impl/First.java
@@ -0,0 +1,84 @@
+package de.uniol.inf.is.odysseus.aggregation.functions.impl;
+
+import java.util.Collection;
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.aggregation.functions.AbstractIncrementalAggregationFunction;
+import de.uniol.inf.is.odysseus.aggregation.functions.IAggregationFunction;
+import de.uniol.inf.is.odysseus.aggregation.functions.factory.IAggregationFunctionFactory;
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
+import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
+import de.uniol.inf.is.odysseus.core.sdf.schema.IAttributeResolver;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
+
+public class First<M extends ITimeInterval, T extends Tuple<M>> extends AbstractIncrementalAggregationFunction<M, T>
+		implements IAggregationFunctionFactory {
+
+	private static final long serialVersionUID = -2033932993111049995L;
+
+	private T firstTuple;
+	private final SDFSchema subSchema;
+	private boolean alreadyReturned = false;;
+	
+	public First() {
+		super();
+		this.subSchema = null;
+	}
+
+	public First(final int[] attributes, final String outputAttributeName, final SDFSchema subSchema) {
+		super(attributes, new String[] { outputAttributeName });
+		this.subSchema = subSchema;
+	}
+
+	public First(final First<M, T> other) {
+		super(other);
+		this.subSchema = null;
+	}
+
+	@Override
+	public void addNew(T newElement) {
+		if (this.firstTuple == null) {
+			this.firstTuple = newElement;
+		}
+	}
+
+	@Override
+	public void removeOutdated(Collection<T> outdatedElements, T trigger, PointInTime pointInTime) {
+		if (outdatedElements.contains(this.firstTuple)) {
+			this.firstTuple = null;
+			this.alreadyReturned = false;
+		}
+	}
+
+	@Override
+	public Object[] evalute(T trigger, PointInTime pointInTime) {
+		if (!this.alreadyReturned && this.firstTuple != null) {
+			this.alreadyReturned = true;
+			return this.firstTuple.getAttributes();
+		}
+		return new Tuple[] { };
+	}
+
+	@Override
+	public Collection<SDFAttribute> getOutputAttributes() {
+		return subSchema.getAttributes();
+	}
+
+	@Override
+	public boolean checkParameters(Map<String, Object> parameters, IAttributeResolver attributeResolver) {
+		return true;
+	}
+
+	@Override
+	public IAggregationFunction createInstance(Map<String, Object> parameters, IAttributeResolver attributeResolver) {
+		return new First<>(null, "", attributeResolver.getSchema().get(0));
+	}
+
+	@Override
+	public AbstractIncrementalAggregationFunction<M, T> clone() {
+		return new First<>(this);
+	}
+
+}
