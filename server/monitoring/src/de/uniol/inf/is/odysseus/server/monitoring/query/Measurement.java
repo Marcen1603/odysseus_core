7e3297cdacd7176ef5c1fe33508086f91cb73e58
Thorben Röhrs <thorben.roehrs@uni-oldenburg.de>
2017-09-13 21:53:16 +0200

    Calculation of Latency changed to ProcessInit and PushInit Event. Renamed Measurements. Methods for average Latency added.
    Former-commit-id: f17e71c2cbc32ff5f0fd9700ac8774ea6b765d0b

--- server/monitoring/src/de/uniol/inf/is/odysseus/server/monitoring/query/Measurement.java
+++ server/monitoring/src/de/uniol/inf/is/odysseus/server/monitoring/query/Measurement.java
@@ -8,14 +8,14 @@ import de.uniol.inf.is.odysseus.core.physicaloperator.IPhysicalOperator;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.IPhysicalQuery;
 import de.uniol.inf.is.odysseus.server.monitoring.physicaloperator.OperatorLatency;
 
-public class Measurements {
+public class Measurement {
 
 	private Map<String, OperatorLatency> latencys;
 	private long currentLatency;
 	private Map<String, OperatorLatency> temporaryLatencys;
 	private List<String> roots;
 
-	public Measurements(IPhysicalQuery query) {
+	public Measurement(IPhysicalQuery query) {
 		this.temporaryLatencys = new HashMap<String, OperatorLatency>();
 		this.latencys = new HashMap<String, OperatorLatency>();
 		this.roots = new ArrayList<String>();
@@ -34,9 +34,9 @@ public class Measurements {
 	 *            Timestamp of the PushInitEvent in ns.
 	 */
 	public synchronized void createNewTempLatency(String operatorName, long nanoTimestamp) {
-		OperatorLatency l = new OperatorLatency();
-		l.startMeasurement(nanoTimestamp);
-		temporaryLatencys.put(operatorName, l);
+			OperatorLatency l = new OperatorLatency();
+			l.startMeasurement(nanoTimestamp);
+			this.temporaryLatencys.put(operatorName, l);
 	}
 
 	/**
@@ -48,66 +48,30 @@ public class Measurements {
 	 *            Timestamp of the PushInitEvent in ns.
 	 */
 	public synchronized void calcTemporaryOperatorLatency(String operatorName, long nanoTimestamp) {
-		for (String tempLatency : temporaryLatencys.keySet()) {
+		for (String tempLatency : this.temporaryLatencys.keySet()) {
 			if (tempLatency.equals(operatorName)) {
-				OperatorLatency l = temporaryLatencys.get(tempLatency);
+				OperatorLatency l = this.temporaryLatencys.get(tempLatency);
 				l.stopMeasurement(nanoTimestamp);
-				temporaryLatencys.put(operatorName, l);
-				// TODO: Remove
+				this.temporaryLatencys.put(operatorName, l);
 
 				if (roots.contains(tempLatency)) {
-					latencys.putAll(temporaryLatencys);
-					temporaryLatencys.clear();
+					this.latencys.putAll(this.temporaryLatencys);
+					this.temporaryLatencys.clear();
 					calcQueryLatency();
-					System.out.println("Total Latency: " + currentLatency);
+					System.out.println("Total Latency: " + this.currentLatency);
 				}
 			}
 		}
-	}
 
-	/**
-	 * Runs through the temporaryLatencys on ProcessDoneEvent to look which
-	 * Latency can be calculated. Copys the temporaryLatency to the
-	 * currentLatencys and calculates it.
-	 * 
-	 * @param operatorName
-	 *            Name of the Operator.
-	 */
-	// public synchronized void commitTemporaryLatency(String operatorName) {
-	// // Wait wenn Start oder Endzeit fehlt
-	// for (int i = 0; i <= temporaryLatencys.size(); i++) {
-	// if (temporaryLatencys.get(i) != null &&
-	// temporaryLatencys.get(i).containsKey(operatorName)) {
-	// if (!temporaryLatencys.get(i).get(operatorName).isCalculated()) {
-	// try {
-	// wait();
-	// } catch (InterruptedException e) {
-	// e.printStackTrace();
-	// }
-	// }
-	// if (roots.contains(operatorName)) {
-	// latencys.putAll(temporaryLatencys.get(i));
-	// temporaryLatencys.remove(i);
-	// calcQueryLatency();
-	// System.out.println("Total Latency: " + currentLatency);
-	//
-	// i = temporaryLatencys.size() + 1;
-	// } else {
-	// temporaryLatencys.remove(i);
-	// i = temporaryLatencys.size() + 1;
-	// System.out.println("Temps deleted");
-	// }
-	// }
-	// }
-	// }
+	}
 
 	/**
 	 * Calculates the Latency in latencys
 	 */
 	private synchronized void calcQueryLatency() {
 		long latency = 0;
-		for (String o : latencys.keySet()) {
-			latency += latencys.get(o).getLatency();
+		for (String o : this.latencys.keySet()) {
+			latency += this.latencys.get(o).getLatency();
 			System.out.println(o + " Latenz: " + latency);
 		}
 		setCurrentLatency(latency);
--- server/monitoring/src/de/uniol/inf/is/odysseus/server/monitoring/query/QueryManager.java
+++ server/monitoring/src/de/uniol/inf/is/odysseus/server/monitoring/query/QueryManager.java
@@ -1,39 +1,97 @@
 package de.uniol.inf.is.odysseus.server.monitoring.query;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalSubscription;
 import de.uniol.inf.is.odysseus.core.planmanagement.executor.IUpdateEventListener;
+import de.uniol.inf.is.odysseus.core.planmanagement.query.ILogicalQuery;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.query.IPhysicalQuery;
-import de.uniol.inf.is.odysseus.server.monitoring.physicaloperator.OperatorLatency;
+import de.uniol.inf.is.odysseus.server.monitoring.system.SystemUsage;
 
-public class QueryManager implements IUpdateEventListener{
-	List<IPhysicalQuery> subquerys = new ArrayList<IPhysicalQuery>();
-	private IPhysicalQuery query; 
+public class QueryManager implements IUpdateEventListener {
+	Map<IPhysicalQuery, Measurement> subquerys = new HashMap<IPhysicalQuery, Measurement>();
+	private IPhysicalQuery query;
+	// private static IExecutor executor;
+	private long totalQueryLatency;
+	private long averageLatency;
+	private long[] lastLatencys = new long[10];
+	private int pointer = 0;
+	private SystemUsage systemUsage;
 
+	// TODO: IUpdateEventListener.QUERY see AbstractExecutor
 	public QueryManager(IPhysicalQuery q) {
-		query=q;
+		query = q;
 		devideQuery();
 		addEventListener();
+//		this.systemUsage=new SystemUsage();
+//		new Thread(this.systemUsage).start();
+		// IExecutor executor = QueryManager.getExecutor();
+		// executor.addUpdateEventListener(this, IUpdateEventListener.QUERY,
+		// null);
 	}
 
-	private void devideQuery(){
-		//TODO: Threaded Buffer splitting the plan
-		subquerys.add(query);
+	private void devideQuery() {
+		// TODO: Threaded Buffer splitting the plan
+		 ILogicalQuery q = query.getLogicalQuery();
+		for (LogicalSubscription o : q.getLogicalPlan().getSubscriptions()){
+			System.out.println("Subscription: "+q.toString());
+		}
+		subquerys.put(query, new Measurement(query));
 	}
 
 	private void addEventListener() {
-		for (IPhysicalQuery q : subquerys) {
-			new QueryEventListener(q);
+		for (IPhysicalQuery q : subquerys.keySet()) {
+			new QueryEventListener(q, subquerys.get(q));
 		}
 	}
-	
+
+	private void calcLatency() {
+		this.totalQueryLatency = 0;
+		for (IPhysicalQuery q : subquerys.keySet()) {
+			this.setTotalQueryLatency(this.getTotalQueryLatency() + subquerys.get(q).getCurrentLatency());
+		}
+		calcAverageLatency();
+	}
+
+	/**
+	 * Calculates latency over the last 20 tupel for the whole Query
+	 */
+	private void calcAverageLatency() {
+		long averageLatency=0;
+		lastLatencys[pointer]=this.getTotalQueryLatency();
+		for (int i = 0; i < lastLatencys.length; i++) {
+			this.setAverageLatency(this.getAverageLatency() + lastLatencys[i]);
+		}
+		this.setAverageLatency(this.getAverageLatency()/lastLatencys.length);
+		pointer=(pointer++)%lastLatencys.length;
+	}
 
 	@Override
 	public void eventOccured(String type) {
+		if (type == IUpdateEventListener.QUERY) {
+			refreshQuerys();
+		}
+	}
+
+	private void refreshQuerys() {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public long getTotalQueryLatency() {
+		return totalQueryLatency;
+	}
+
+	public void setTotalQueryLatency(long totalQueryLatency) {
+		this.totalQueryLatency = totalQueryLatency;
+	}
+
+	public long getAverageLatency() {
+		return averageLatency;
+	}
+
+	private void setAverageLatency(long averageLatency) {
+		this.averageLatency = averageLatency;
 	}
 }
