12132492efb6a494bc5ba143649b6caae9751a43
Jens Plümer <jens.pluemer@uni-oldenburg.de>
2017-09-30 19:36:16 +0200

    Changed server application to server product configuration and changed prefixes of EStream projects
    Former-commit-id: 0807dfc8e7f66931592f48a89fc0f3a5779fa5c2

--- test/de.pgestream.odysseus.test/product/src/de/uniol/inf/is/odysseus/test/context/ITestContext.java
+++
@@ -1,9 +0,0 @@
-package de.uniol.inf.is.odysseus.test.context;
-
-import java.net.URL;
-
-public interface ITestContext {	
-	public String getUsername();
-	public String getPassword();
-	public URL getDataRootPath();
-}
