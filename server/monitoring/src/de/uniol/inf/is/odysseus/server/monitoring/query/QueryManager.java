aedee5d2d6e3347bea2417746f03beb65ac3de14
Thorben Röhrs <thorben.roehrs@uni-oldenburg.de>
2017-10-04 20:15:43 +0200

    Average calculation revised. QueryManager can divide an Query and handle multiple queries.
    Former-commit-id: 65942a863d05e028bd324b0165e12cdaede309fe

--- server/monitoring/src/de/uniol/inf/is/odysseus/server/monitoring/query/QueryManager.java
+++ server/monitoring/src/de/uniol/inf/is/odysseus/server/monitoring/query/QueryManager.java
@@ -19,30 +19,16 @@ import de.uniol.inf.is.odysseus.rcp.OdysseusRCPPlugIn;
 import de.uniol.inf.is.odysseus.server.monitoring.system.SystemUsage;
 
 public class QueryManager implements IUpdateEventListener {
-	Map<IPhysicalQuery, List<SubQuery>> subquerys = new HashMap<IPhysicalQuery, List<SubQuery>>();
-	private List<IPhysicalQuery> queries = new ArrayList<IPhysicalQuery>();
+	private static Map<IPhysicalQuery, List<SubQuery>> subquerys = new HashMap<IPhysicalQuery, List<SubQuery>>();
 	private static final QueryManager manager = new QueryManager();
 
-	private long totalQueryLatency;
-	private long averageLatency;
-	private long[] lastLatencys = new long[20];
-	private int pointer = 0;
+	private static Map<IPhysicalQuery, AverageLatency> latencys = new HashMap<IPhysicalQuery, AverageLatency>();
 	private ThreadCalculateLatency thread = new ThreadCalculateLatency();
 
 	// TODO: IUpdateEventListener.QUERY see AbstractExecutor
 	private QueryManager() {
-
-		// divideQuery();
-		// this.systemUsage = SystemUsage.getInstance();
-		// new Thread(this.systemUsage).start();
-
-		// executor.addUpdateEventListener(this, IUpdateEventListener.QUERY,
-		// null);
-		// query.getRoots().get(0).
 		OdysseusRCPPlugIn.getExecutor().addUpdateEventListener(this, IUpdateEventListener.QUERY, null);
 		thread.start();
-		// StandardExecutor.getInstance().addUpdateEventListener(this,
-		// "IUpdateEventListener.QUERY", null);
 	}
 
 	public static QueryManager getInstance() {
@@ -50,33 +36,13 @@ public class QueryManager implements IUpdateEventListener {
 	}
 
 	/**
-	 * Devides a query in a set of subquerys to handle the calculation for the
+	 * Divides a query in a set of subqueries to handle the calculation of the
 	 * latency
 	 * 
-	 * @param p
+	 * @param q
+	 *            IPhysicalQuery
 	 */
-	@SuppressWarnings("rawtypes")
 	private void divideQuery(IPhysicalQuery q) {
-		// SubQuery subquery = new SubQuery();
-		// subquery.setRoot(q.getRoots().get(0));
-		// for (IPhysicalOperator o : q.getPhysicalChilds()) {
-		// List<IIterableSource<?>> test = q.getIterableSources();
-		// List<IIterableSource<?>> test1 = q.getIteratableLeafSources();
-		// List<IPhysicalOperator> test2 = q.getLeafSources();
-		// if (!q.getLeafSources().contains(o) &&
-		// !q.getIterableSources().contains(o)
-		// && !q.getIteratableLeafSources().contains(o)) {
-		// subquery.addOperator(o);
-		// List<SubQuery> list = new ArrayList<SubQuery>();
-		// list.add(subquery);
-		// subquerys.put(q, list);
-		// } else {
-		// System.out.println(o.toString());
-		// }
-		// }
-		// subquery.addEventListener();
-
-		// countSubscriptions(q);
 		boolean adding = true;
 		while (adding) {
 			adding = false;
@@ -85,8 +51,30 @@ public class QueryManager implements IUpdateEventListener {
 						&& !q.getIteratableLeafSources().contains(operator)) {
 					List<IPhysicalOperator> subscriptions = getSubscriptions(operator);
 					boolean added = false;
-					// TODO: IF Operator is Join or Aggregate or Buffer -> new
-					// SubQuery -> setRoot(o)
+					if (subqueriesContainsOperator(q, operator)) {
+						added = true;
+					}
+					// TODO: Remove identification about their name
+					if (!added && (operator.getName().contains(
+							"Buffer") /*
+										 * || operator.getName().contains(
+										 * "Aggregate")
+										 */)) {
+						for (SubQuery subquery : subquerys.get(q)) {
+							if (!subquery.hasRoot()) {
+								subquery.setRoot(operator);
+								added = true;
+								adding = true;
+							}
+						}
+						if (!added) {
+							SubQuery s = new SubQuery(thread);
+							s.setRoot(operator);
+							subquerys.get(q).add(s);
+							added = true;
+							adding = true;
+						}
+					}
 					if (subscriptions.isEmpty() && !added) {
 						for (SubQuery subquery : subquerys.get(q)) {
 							if (!subquery.hasRoot()) {
@@ -98,7 +86,8 @@ public class QueryManager implements IUpdateEventListener {
 							SubQuery s = new SubQuery(thread);
 							s.setRoot(operator);
 							subquerys.get(q).add(s);
-							added=true;
+							added = true;
+							adding = true;
 						}
 					}
 
@@ -108,6 +97,19 @@ public class QueryManager implements IUpdateEventListener {
 						// setRoot(JOIN/Aggregate);
 						// adding = true;
 						// }
+						for (IPhysicalOperator subscription : subscriptions) {
+							if (subscription.getName().contains("Buffer")
+							/*
+							 * || subscription.getName().contains("Aggregate")
+							 */) {
+								SubQuery s = new SubQuery(thread);
+								s.setRoot(operator);
+								subquerys.get(q).add(s);
+								added = true;
+								adding = true;
+							}
+						}
+
 						for (SubQuery subquery : subquerys.get(q)) {
 							if (!added) {
 								added = addOperatorToSubQuery(operator, subscriptions, subquery);
@@ -121,11 +123,11 @@ public class QueryManager implements IUpdateEventListener {
 						}
 					}
 				}
-				if (adding) {
-					for (SubQuery subquery : subquerys.get(q)) {
-						for (IPhysicalOperator o : subquery.getOperator()) {
-							subquery.removeOperator(o);
-						}
+			}
+			if (adding) {
+				for (SubQuery subquery : subquerys.get(q)) {
+					for (int i = 0; i < subquery.getOperator().size(); i++) {
+						subquery.removeOperator(subquery.getOperator().get(i));
 					}
 				}
 			}
@@ -135,6 +137,36 @@ public class QueryManager implements IUpdateEventListener {
 		}
 	}
 
+	/**
+	 * Checks if the operator is in any SubQuery for the given query.
+	 * 
+	 * @param query
+	 *            IPhysicalQuery
+	 * @param operator
+	 *            IPhysicalOperator
+	 * @return True if operator is in any SubQuery, false if not.
+	 */
+	private boolean subqueriesContainsOperator(IPhysicalQuery query, IPhysicalOperator operator) {
+		for (SubQuery subquery : subquerys.get(query)) {
+			if (subquery.containsOperator(operator)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Adds Operator to a SubQuery
+	 * 
+	 * @param o
+	 *            IPhysicalOperator
+	 * @param subscriptions
+	 *            Subscriptions for o
+	 * @param subquery
+	 *            SubQuery
+	 * @return True if the operator has been added to the subquery, false if
+	 *         not.
+	 */
 	private boolean addOperatorToSubQuery(IPhysicalOperator o, List<IPhysicalOperator> subscriptions,
 			SubQuery subquery) {
 		for (IPhysicalOperator subscription : subscriptions) {
@@ -153,6 +185,7 @@ public class QueryManager implements IUpdateEventListener {
 	 *            IPhysicalOperator to look for subsciptions.
 	 * @return List of operator to which o is subscribed.
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private List<IPhysicalOperator> getSubscriptions(IPhysicalOperator o) {
 		List<IPhysicalOperator> subscriptions = new ArrayList<IPhysicalOperator>();
 
@@ -168,53 +201,31 @@ public class QueryManager implements IUpdateEventListener {
 	}
 
 	/**
-	 * TODO: Evtl. umbenennen zu hasSeveralSubsciptions und Rückgabe
-	 * ergänzen|remove test
+	 * Calculates latency over the last 20 tuple for the whole Query
 	 * 
 	 * @param q
+	 *            Query for which the latency is calculated
 	 */
-	private void countSubscriptions(IPhysicalQuery q) {
-		HashMap<IPhysicalOperator, Integer> subscriptioncount = new HashMap<IPhysicalOperator, Integer>();
-		for (IPhysicalOperator operator : q.getPhysicalChilds()) {
-			ISource o = (ISource) operator;
-			if (o instanceof AbstractSource) {
-				List<?> subscriptions = ((AbstractSource) o).getSubscriptions();
-				for (int i = 0; i < subscriptions.size(); i++) {
-					ControllablePhysicalSubscription subscription = (ControllablePhysicalSubscription) subscriptions
-							.get(i);
-					IPhysicalOperator test = (IPhysicalOperator) subscription.getTarget();
-					if (subscriptioncount.containsKey(test)) {
-						int value = subscriptioncount.get(test) + 1;
-						subscriptioncount.put(test, value);
-					} else {
-						subscriptioncount.put(test, 1);
-					}
-				}
-
+	public static void calcAverageLatency(IPhysicalQuery q) {
+		for (SubQuery subquery : subquerys.get(q)) {
+			AverageLatency l = latencys.get(q);
+			if (l == null) {
+				l = new AverageLatency();
+				l.addLatency(getTotalQueryLatency(q));
+			} else {
+				l.addLatency(getTotalQueryLatency(q));
 			}
+			latencys.put(q, l);
 		}
+		System.out.println(latencys.get(q).getAverageLatency());
 	}
 
-	private void calcLatency() {
-		// this.totalQueryLatency = 0;
-		// for (SubQuery q : subquerys.keySet()) {
-		// this.setTotalQueryLatency(this.getTotalQueryLatency() +
-		// q.getMeasurement().getCurrentLatency());
-		// }
-		// calcAverageLatency();
-	}
-
-	/**
-	 * Calculates latency over the last 20 tupel for the whole Query
-	 */
-	private void calcAverageLatency() {
-		this.averageLatency = 0;
-		lastLatencys[pointer] = this.getTotalQueryLatency();
-		for (int i = 0; i < lastLatencys.length; i++) {
-			this.setAverageLatency(this.getAverageLatency() + lastLatencys[i]);
+	private static long getTotalQueryLatency(IPhysicalQuery q) {
+		long sum = 0;
+		for (SubQuery subquery : subquerys.get(q)) {
+			sum += subquery.getMeasurement().getCurrentLatency();
 		}
-		this.setAverageLatency(this.getAverageLatency() / lastLatencys.length);
-		pointer = (pointer++) % lastLatencys.length;
+		return sum;
 	}
 
 	@Override
@@ -225,29 +236,19 @@ public class QueryManager implements IUpdateEventListener {
 		}
 	}
 
-	// TODO
+	// TODO: Where do i get the queries from
 	private void refreshQuerys() {
 		ISession session = OdysseusRCPPlugIn.getActiveSession();
 		List<StoredProcedure> storedProcedures = OdysseusRCPPlugIn.getExecutor().getStoredProcedures(session);
 		System.out.println(storedProcedures.toString());
 	}
 
-	public long getTotalQueryLatency() {
-		return totalQueryLatency;
-	}
-
-	public void setTotalQueryLatency(long totalQueryLatency) {
-		this.totalQueryLatency = totalQueryLatency;
-	}
-
-	public long getAverageLatency() {
-		return averageLatency;
-	}
-
-	private void setAverageLatency(long averageLatency) {
-		this.averageLatency = averageLatency;
-	}
-
+	/**
+	 * Adds a new query to the querymanager if it not exists
+	 * 
+	 * @param q
+	 *            Query which will be added
+	 */
 	public void addQuery(IPhysicalQuery q) {
 		if (!subquerys.keySet().contains(q)) {
 			List<SubQuery> list = new ArrayList<SubQuery>();
@@ -264,6 +265,7 @@ class SubQuery {
 	private ArrayList<IPhysicalOperator> operator;
 	private IPhysicalOperator root;
 	private QueryEventListener eventListener;
+	private long lastLatency;
 
 	public SubQuery() {
 		if (getThread() == null) {
@@ -296,8 +298,18 @@ class SubQuery {
 		}
 	}
 
+	/**
+	 * Checks if the SubQuery contains an Operator.
+	 * 
+	 * @param o
+	 *            IPhysicalOperator to check.
+	 * @return true if the operator is included, false if not.
+	 */
 	public boolean containsOperator(IPhysicalOperator o) {
-		if (getOperator().contains(o) || getRoot().equals(o)) {
+		if (getRoot() != null && getRoot().equals(o)) {
+			return true;
+		}
+		if (getOperator() != null && getOperator().contains(o)) {
 			return true;
 		}
 		return false;
@@ -321,17 +333,19 @@ class SubQuery {
 		for (IPhysicalOperator o : this.getOperator()) {
 			getEventListener().addOperator(o);
 		}
-		getEventListener().addOperator(getRoot());
+		if (getRoot().getName().contains("Buffer")) {
+			getEventListener().addBuffer(getRoot());
+		} else {
+			getEventListener().addOperator(getRoot());
+		}
 	}
 
 	public void removeOperator(IPhysicalOperator o) {
-		for (IPhysicalOperator op : operator) {
-			if (op.equals(o)) {
-				eventListener.removeOperator(op);
-				operator.remove(op);
-				if (operator.isEmpty()) {
-					stopThread();
-				}
+		if (getOperator() != null && getOperator().contains(o)) {
+			// getEventListener().removeOperator(o);
+			getOperator().remove(o);
+			if (operator.isEmpty()) {
+				// stopThread();
 			}
 		}
 
@@ -383,4 +397,49 @@ class SubQuery {
 	public void setEventListener(QueryEventListener eventListener) {
 		this.eventListener = eventListener;
 	}
+
+	public long getLastLatency() {
+		return lastLatency;
+	}
+
+	public void setLastLatency(long latency) {
+		this.lastLatency = latency;
+	}
+}
+
+class AverageLatency {
+	private long averageLatency;
+	private int pointer = 0;
+	private long[] lastLatencys = new long[20];
+
+	public AverageLatency() {
+
+	}
+
+	public void addLatency(long latency) {
+		averageLatency = 0;
+		lastLatencys[pointer] = latency;
+		for (long l : lastLatencys) {
+			averageLatency += l;
+		}
+		averageLatency = averageLatency / lastLatencys.length;
+		pointer = (pointer++) % lastLatencys.length;
+	}
+
+	public long getAverageLatency() {
+		return averageLatency;
+	}
+
+	public void setAverageLatency(long averageLatency) {
+		this.averageLatency = averageLatency;
+	}
+
+	public long[] getLastLatencys() {
+		return lastLatencys;
+	}
+
+	public void setLastLatencys(long[] lastLatencys) {
+		this.lastLatencys = lastLatencys;
+	}
+
 }
