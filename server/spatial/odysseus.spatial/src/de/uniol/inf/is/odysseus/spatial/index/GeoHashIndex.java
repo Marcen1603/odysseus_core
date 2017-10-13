7c893a7c4b4689aec7e7eb817be4fda2f4c30ddf
tbrandt <tobias.leo.brandt@uni-oldenburg.de>
2017-10-13 15:11:52 +0200

    ODY-1395 Idea: it's not necessary to index the old elements, only
    latest elements are necessary for this use case
    
    Former-commit-id: 8722bb506681afba3e05b17d31f38c15c7bf3c1e

--- server/spatial/odysseus.spatial/src/de/uniol/inf/is/odysseus/spatial/index/GeoHashIndex.java
+++ server/spatial/odysseus.spatial/src/de/uniol/inf/is/odysseus/spatial/index/GeoHashIndex.java
@@ -2,7 +2,9 @@ package de.uniol.inf.is.odysseus.spatial.index;
 
 import java.util.List;
 import java.util.Map;
+import java.util.NavigableMap;
 
+import ch.hsr.geohash.GeoHash;
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
 import de.uniol.inf.is.odysseus.core.metadata.TimeInterval;
@@ -12,27 +14,19 @@ import de.uniol.inf.is.odysseus.spatial.datatype.TrajectoryElement;
 
 public class GeoHashIndex implements SpatialIndex {
 
+	// The precision of the GeoHashes
+	public static final int BIT_PRECISION = 64;
+	
+	// All points
+	protected NavigableMap<GeoHash, List<TrajectoryElement>> pointMap;
+	
 	@Override
 	public void add(LocationMeasurement locationMeasurement, IStreamObject<? extends IMetaAttribute> streamElement) {
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public Map<String, List<ResultElement>> queryCircle(double centerLatitude, double centerLongitude, double radius,
-			TimeInterval interval) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public Map<String, ResultElement> queryCircleOnLatestElements(double centerLatitude, double centerLongitude,
-			double radius, TimeInterval interval) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Map<String, List<TrajectoryElement>> approximateCircle(double centerLatitude, double centerLongitude,
 			double radius, TimeInterval interval) {
 		// TODO Auto-generated method stub
 		return null;
--- server/spatial/odysseus.spatial/src/de/uniol/inf/is/odysseus/spatial/index/SpatialIndex.java
+++ server/spatial/odysseus.spatial/src/de/uniol/inf/is/odysseus/spatial/index/SpatialIndex.java
@@ -1,6 +1,5 @@
 package de.uniol.inf.is.odysseus.spatial.index;
 
-import java.util.List;
 import java.util.Map;
 
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
@@ -29,27 +28,6 @@ public interface SpatialIndex {
 	public void add(LocationMeasurement locationMeasurement, IStreamObject<? extends IMetaAttribute> streamElement);
 
 	/**
-	 * Calculates all neighbors within the given radius around the center. Returns
-	 * all elements within that circle, no matter if they are the latest elements or
-	 * not. Filtering for time can be done.
-	 * 
-	 * @param centerLatitude
-	 *            The latitude of the center of the circle
-	 * @param centerLongitude
-	 *            The longitude of the center of the circle
-	 * @param radius
-	 *            The radius in meters around the center
-	 * @param interval
-	 *            Filters the elements for only the elements within this time
-	 *            interval. Put null or an interval (0,inf] to not filter.
-	 * @return A map with all elements for which their location is in the
-	 *         neighborhood around the center. Map: ID of the object -> results for
-	 *         that object
-	 */
-	public Map<String, List<ResultElement>> queryCircle(double centerLatitude, double centerLongitude, double radius,
-			TimeInterval interval);
-
-	/**
 	 * Calculates all neighbors within the given radius around the center. Only uses
 	 * the latest info from each object. Can filter by time interval.
 	 * 
@@ -68,27 +46,6 @@ public interface SpatialIndex {
 	 *         object -> result for that object
 	 */
 	public Map<String, ResultElement> queryCircleOnLatestElements(double centerLatitude, double centerLongitude,
-			double radius, TimeInterval interval);
-
-	/**
-	 * Approximates a circle query on all data in the index. Results can be
-	 * inaccurate (especially include more data than in an accurate query). Avoids
-	 * distance calculations and only works with the given index structure, hence
-	 * improves performance if accuracy is not the main goal.
-	 * 
-	 * @param centerLatitude
-	 *            The latitude of the center of the circle
-	 * @param centerLongitude
-	 *            The longitude of the center of the circle
-	 * @param radius
-	 *            The radius in meters around the center
-	 * @param interval
-	 *            Filters the elements for only the elements within this time
-	 *            interval. Put null or an interval (0,inf] to not filter.
-	 * @return An approximation of elements within the circle. Map: ID of the object
-	 *         -> results for that object
-	 */
-	public Map<String, List<TrajectoryElement>> approximateCircle(double centerLatitude, double centerLongitude,
 			double radius, TimeInterval interval);
 
 	/**
