9cb889df7776d3f1f7fa47ff0513625093ffcd6f
Michael Brand <michael.brand@uni-oldenburg.de>
2017-10-18 18:40:08 +0200

    ODY-1401: Wrapper-project for NetzDatenStrom specific stuff. First content is a MEP function that takes a list of strings or KVObjects, which represent edges of a graph, and returns a Graph object
    Former-commit-id: d4b72366aff83c33c44a29abfc93be27e9d3a3a4

---
+++ wrapper/de.uniol.inf.is.odysseus.wrapper.netzdatenstrom/src/de/uniol/inf/is/odysseus/wrapper/netzdatenstrom/NetzDatenStromMEPFunctions.java
@@ -0,0 +1,32 @@
+package de.uniol.inf.is.odysseus.wrapper.netzdatenstrom;
+
+import java.util.Arrays;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.mep.IMepFunction;
+import de.uniol.inf.is.odysseus.mep.IFunctionProvider;
+import de.uniol.inf.is.odysseus.wrapper.netzdatenstrom.topology.NDSGraphFromJSONString;
+
+/**
+ * Provides the MEP functions that closely related to the research project
+ * "NetzDatenStrom". Provides MEP functions are: <br />
+ * <ul>
+ * <li>{@link NDSGraphFromJSONString}</li>
+ * </ul>
+ *
+ * @author Michael Brand (michael.brand@uol.de)
+ *
+ */
+public class NetzDatenStromMEPFunctions implements IFunctionProvider {
+
+	/**
+	 * Instances of the provided MEP functions.
+	 */
+	private static final IMepFunction<?>[] functions = new IMepFunction[] { new NDSGraphFromJSONString() };
+
+	@Override
+	public List<IMepFunction<?>> getFunctions() {
+		return Arrays.asList(functions);
+	}
+
+}
