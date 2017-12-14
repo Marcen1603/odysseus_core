94f65bd48c7f4f9aeb14ceae980cc64a12126461
Thomas Vogelgesang <thomas.vogelgesang@uni-oldenburg.de>
2017-12-14 11:37:09 +0100

    Creating Key value objects from mail message
    Former-commit-id: e2c2b0a68448bbe9512a71332caf7c1cffc51425

--- wrapper/mail/src/de/uniol/inf/is/odysseus/wrapper/mail/physicaloperator/access/MailProtocolHandler.java
+++ wrapper/mail/src/de/uniol/inf/is/odysseus/wrapper/mail/physicaloperator/access/MailProtocolHandler.java
@@ -5,7 +5,10 @@ import java.io.InputStream;
 import java.net.UnknownHostException;
 
 import javax.mail.Address;
+import javax.mail.Flags;
+import javax.mail.Flags.Flag;
 import javax.mail.Message;
+import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
 
 import org.slf4j.Logger;
@@ -13,30 +16,42 @@ import org.slf4j.LoggerFactory;
 
 import de.uniol.inf.is.odysseus.core.collection.OptionMap;
 import de.uniol.inf.is.odysseus.core.datahandler.IStreamObjectDataHandler;
+import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.AbstractProtocolHandler;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.protocol.IProtocolHandler;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.IAccessPattern;
 import de.uniol.inf.is.odysseus.core.physicaloperator.access.transport.ITransportDirection;
 import de.uniol.inf.is.odysseus.keyvalue.datatype.KeyValueObject;
 
-public class MailProtocolHandler<T extends KeyValueObject<?>> extends AbstractProtocolHandler<T> {
-	
+public class MailProtocolHandler extends AbstractProtocolHandler<KeyValueObject<IMetaAttribute>> {
+
+	private static final String FLAG_EXTENSION = "_flag";
+	private static final String SENT_DATE = "SentDate";
+	private static final String SUBJECT = "Subject";
+	private static final String REPLY_TO = "ReplyTo";
+	private static final String BCC = "BCC";
+	private static final String CC = "CC";
+	private static final String FROM = "From";
+	private static final String TO = "To";
+
 	/** Logger */
 	private final Logger LOG = LoggerFactory.getLogger(MailProtocolHandler.class);
 
-	private static String NAME= "Mail";
-	
+	private static String NAME = "Mail";
+
 	private MailInputStream inputStream;
-	
+
 	public MailProtocolHandler(ITransportDirection direction, IAccessPattern access, OptionMap options,
-			IStreamObjectDataHandler<T> dataHandler) {
+			IStreamObjectDataHandler<KeyValueObject<IMetaAttribute>> dataHandler) {
 		super(direction, access, dataHandler, options);
 	}
 
 	@Override
-	public IProtocolHandler<T> createInstance(ITransportDirection direction, IAccessPattern access, OptionMap options,
-			IStreamObjectDataHandler<T> dataHandler) {
-		IProtocolHandler<T> instance = new MailProtocolHandler<T>(direction, access, options, dataHandler);
+	public IProtocolHandler<KeyValueObject<IMetaAttribute>> createInstance(ITransportDirection direction,
+			IAccessPattern access, OptionMap options,
+			IStreamObjectDataHandler<KeyValueObject<IMetaAttribute>> dataHandler) {
+		IProtocolHandler<KeyValueObject<IMetaAttribute>> instance = new MailProtocolHandler(direction, access, options,
+				dataHandler);
 		return instance;
 	}
 
@@ -47,7 +62,7 @@ public class MailProtocolHandler<T extends KeyValueObject<?>> extends AbstractPr
 
 	@Override
 	public boolean isSemanticallyEqualImpl(IProtocolHandler<?> other) {
-		if (!(other instanceof MailProtocolHandler<?>)) {
+		if (!(other instanceof MailProtocolHandler)) {
 			return false;
 		}
 		// the datahandler was already checked in the
@@ -59,10 +74,10 @@ public class MailProtocolHandler<T extends KeyValueObject<?>> extends AbstractPr
 	public void open() throws UnknownHostException, IOException {
 		getTransportHandler().open();
 		InputStream in = this.getTransportHandler().getInputStream();
-		if (inputStream == null || !(inputStream instanceof MailInputStream)){
-			throw new IOException("Transport handler does not provide mail input stream"); 
+		if (inputStream == null || !(inputStream instanceof MailInputStream)) {
+			throw new IOException("Transport handler does not provide mail input stream");
 		}
-		this.inputStream = (MailInputStream)in;
+		this.inputStream = (MailInputStream) in;
 	}
 
 	@Override
@@ -71,26 +86,66 @@ public class MailProtocolHandler<T extends KeyValueObject<?>> extends AbstractPr
 	}
 
 	@Override
-	public T getNext() throws IOException {
-		T kvObject = null;//readNextMessage();
-		return kvObject;
-	}
+	public KeyValueObject<IMetaAttribute> getNext() throws IOException {
+		KeyValueObject<IMetaAttribute> kvo = KeyValueObject.createInstance();
 
-	private void readNextMessage() {
-		KeyValueObject<?> kvo = KeyValueObject.createInstance();
-		
 		Message message = inputStream.GetNextMessage();
+		if (message == null) {
+			throw new IOException("Mail was null");
+		}
+
 		try {
-			Address[] allRecepients = message.getAllRecipients();
-			String[] allRecepientsStr = new String[allRecepients.length]; //insert strings instead of Address objects?
-			for (int i =0; i < allRecepients.length; i++) {
-				allRecepientsStr[i] = allRecepients[i].toString();
-			}
-			kvo.setAttribute("RECEPIENTS", allRecepientsStr);
+			addAddresses(kvo, FROM, message.getFrom());
+			addAddresses(kvo, TO, message.getRecipients(RecipientType.TO));
+			addAddresses(kvo, CC, message.getRecipients(RecipientType.CC));
+			addAddresses(kvo, BCC, message.getRecipients(RecipientType.BCC));
+			addAddresses(kvo, REPLY_TO, message.getReplyTo());
+
+			kvo.setAttribute(SUBJECT, message.getSubject());
+			kvo.setAttribute(SENT_DATE, message.getSentDate());
+
+			AddFlags(kvo, message);
+			
+			
+
 		} catch (MessagingException e) {
 			this.LOG.error(e.getMessage(), e);
 		}
+
+		return kvo;
 	}
 
-	
+	private void AddFlags(KeyValueObject<IMetaAttribute> kvo, Message message) throws MessagingException {
+		Flags flags = message.getFlags();
+		if (flags != null) {
+			addFlag(kvo, flags, Flag.ANSWERED);
+			addFlag(kvo, flags, Flag.DELETED);
+			addFlag(kvo, flags, Flag.DRAFT);
+			addFlag(kvo, flags, Flag.FLAGGED);
+			addFlag(kvo, flags, Flag.RECENT);
+			addFlag(kvo, flags, Flag.SEEN);
+			addFlag(kvo, flags, Flag.USER);
+		}
+	}
+
+	private void addFlag(KeyValueObject<IMetaAttribute> kvo, Flags flags, Flag flag) {
+		kvo.setAttribute(flag.toString() + FLAG_EXTENSION, flags.contains(flag));
+	}
+
+	private void addAddresses(KeyValueObject<IMetaAttribute> kvo, String key, Address[] addresses) {
+		if (addresses != null && addresses.length > 0) {
+			kvo.setAttribute(key, addressesToStrings(addresses));
+		}
+	}
+
+	private String[] addressesToStrings(Address[] addresses) {
+		String[] strings = new String[addresses.length];
+
+		for (int i = 0; i < addresses.length; i++) {
+			strings[i] = addresses[i].toString();
+		}
+
+		return strings;
+	}
+
 }
