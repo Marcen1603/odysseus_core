c8e7025ede4e2d879014de40ae31995fe4943500
Thomas Vogelgesang <thomas.vogelgesang@uni-oldenburg.de>
2017-12-21 16:19:06 +0100

    cleaned up imports
    Former-commit-id: 72726bb229efe1607c7bb3374ced3f6bced5d7ed

--- wrapper/mail/src/de/uniol/inf/is/odysseus/wrapper/mail/mimetype/handler/AbstractMimeTypeHandler.java
+++ wrapper/mail/src/de/uniol/inf/is/odysseus/wrapper/mail/mimetype/handler/AbstractMimeTypeHandler.java
@@ -1,11 +1,5 @@
 package de.uniol.inf.is.odysseus.wrapper.mail.mimetype.handler;
 
-import java.io.IOException;
-import java.util.Set;
-
-import javax.mail.MessagingException;
-import javax.mail.Part;
-
 public abstract class AbstractMimeTypeHandler<T> implements IMimeTypeHandler<T> {
 
 	private MimeTypeHandlerRegistry<T> registry;
