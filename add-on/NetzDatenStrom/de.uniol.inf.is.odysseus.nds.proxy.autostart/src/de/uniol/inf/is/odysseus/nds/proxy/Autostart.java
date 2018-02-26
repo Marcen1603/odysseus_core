e7b2252b738ac3869fc9d290c9dd5f772007fce2
Michael Brand <michael.brand@uni-oldenburg.de>
2018-02-26 15:12:35 +0100

    NetzDatenStrom: platform independent path to autostart scripts
    Former-commit-id: 5bcd002fd5e9bb3cb496c98707b44dacc1351d81

--- add-on/NetzDatenStrom/de.uniol.inf.is.odysseus.nds.proxy.autostart/src/de/uniol/inf/is/odysseus/nds/proxy/Autostart.java
+++ add-on/NetzDatenStrom/de.uniol.inf.is.odysseus.nds.proxy.autostart/src/de/uniol/inf/is/odysseus/nds/proxy/Autostart.java
@@ -1,5 +1,7 @@
 package de.uniol.inf.is.odysseus.nds.proxy;
 
+import java.io.File;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -16,7 +18,7 @@ public class Autostart implements BundleActivator {
 	private static IExecutor executor;
 	private static boolean autostartExecuted = false;
 
-	private static String[] PATHES = { "/autostart/Usermanagement.qry" };
+	private static String[] PATHES = { File.separator + "autostart" + File.separator + "Usermanagement.qry" };
 
 	// called by OSGi-DS
 	public void unbindExecutor(IExecutor exec) {
