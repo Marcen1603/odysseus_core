b1968fcd8dd4659f6fa4854fbdf5f9e4c923eff2
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2018-03-01 15:06:30 +0100

    ODY-1502: use DD to retrieve DataHandlerRegistry Former-commit-id: b87db6370629458b184204db6147f1d73034f536

--- server/cql/parser.cql/src/de/uniol/inf/is/odysseus/parser/cql/CQLParser.java
+++ server/cql/parser.cql/src/de/uniol/inf/is/odysseus/parser/cql/CQLParser.java
@@ -1491,7 +1491,7 @@ public class CQLParser implements NewSQLParserVisitor, IQueryParser {
 				.getUnquotedName();
 		List<SDFAttribute> attributes = visit(defs, null);
 		@SuppressWarnings("rawtypes")
-		Class<? extends IStreamObject> type = DataHandlerRegistry.instance
+		Class<? extends IStreamObject> type = getDataDictionary().getDataHandlerRegistry(getCaller())
 				.getCreatedType(datahandler);
 		if (type == null) {
 			type = Tuple.class;
@@ -1576,7 +1576,7 @@ public class CQLParser implements NewSQLParserVisitor, IQueryParser {
 				.getUnquotedName();
 
 		@SuppressWarnings("rawtypes")
-		Class<? extends IStreamObject> type = DataHandlerRegistry.instance
+		Class<? extends IStreamObject> type = getDataDictionary().getDataHandlerRegistry(getCaller())
 				.getCreatedType(datahandler);
 		if (type == null) {
 			type = Tuple.class;
