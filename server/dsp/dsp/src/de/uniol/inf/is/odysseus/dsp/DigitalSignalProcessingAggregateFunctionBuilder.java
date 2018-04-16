6e2fa1f663668a3915597cefe3d73b50686a2364
Philipp Mudra <philipp.mudra@web.de>
2018-04-16 14:53:42 +0200

    Replace dummy aggregation function with FFT
    Former-commit-id: 9f913045f593fe09cab6488f9c3136927b4be7ea

--- server/dsp/dsp/src/de/uniol/inf/is/odysseus/dsp/DigitalSignalProcessingAggregateFunctionBuilder.java
+++ server/dsp/dsp/src/de/uniol/inf/is/odysseus/dsp/DigitalSignalProcessingAggregateFunctionBuilder.java
@@ -2,16 +2,19 @@ package de.uniol.inf.is.odysseus.dsp;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.metadata.IStreamObject;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFSchema;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.AbstractAggregateFunctionBuilder;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.AggregateFunction;
 import de.uniol.inf.is.odysseus.core.server.physicaloperator.aggregate.basefunctions.IAggregateFunction;
 
-public class RelationalConstAggregateFunctionBuilder extends AbstractAggregateFunctionBuilder {
-
+public class DigitalSignalProcessingAggregateFunctionBuilder extends AbstractAggregateFunctionBuilder {
+	
 	@SuppressWarnings("rawtypes")
 	@Override
 	public Class<? extends IStreamObject> getDatamodel() {
@@ -19,15 +22,23 @@ public class RelationalConstAggregateFunctionBuilder extends AbstractAggregateFu
 	}
 
 	@Override
+	public SDFDatatype getDatatype(String functionName, List<SDFAttribute> input) {
+		if (functionName.equalsIgnoreCase("FFT")) {
+			return SDFDatatype.LIST_DOUBLE;
+		}
+		return super.getDatatype(functionName, input);
+	}
+
+	@Override
 	public Collection<String> getFunctionNames() {
-		return Collections.singleton("CONST");
+		return Collections.singleton("FFT");
 	}
 
 	@Override
 	public IAggregateFunction<?, ?> createAggFunction(AggregateFunction e2, SDFSchema e1, int[] posArray,
 			boolean partialAggregateInput, String datatype) {
-		if (e2.getName().equalsIgnoreCase("CONST")) {
-			return new RelationalConst(partialAggregateInput);
+		if (e2.getName().equalsIgnoreCase("FFT")) {
+			return new RelationalFFT(posArray[0], partialAggregateInput);
 		}
 		throw new IllegalArgumentException();
 	}
