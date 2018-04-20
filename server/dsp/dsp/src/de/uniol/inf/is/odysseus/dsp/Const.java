b444a3f9476dab436a228cf1c105864158c536ce
Philipp Mudra <philipp.mudra@web.de>
2018-04-20 21:56:25 +0200

    Remove old style FFT aggregation
    Former-commit-id: 18c2d644a65d33c7d2e5855c7b51632fb83d0cee

--- server/dsp/dsp/src/de/uniol/inf/is/odysseus/dsp/Const.java
+++ server/dsp/dsp/src/de/uniol/inf/is/odysseus/dsp/Const.java
@@ -1,8 +1,7 @@
 package de.uniol.inf.is.odysseus.dsp;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-
 import de.uniol.inf.is.odysseus.aggregation.functions.AbstractNonIncrementalAggregationFunction;
 import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
@@ -10,13 +9,24 @@ import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 
-public class Const<M extends ITimeInterval, T extends Tuple<M>> extends AbstractNonIncrementalAggregationFunction<M, T> {
+public class Const<M extends ITimeInterval, T extends Tuple<M>>
+		extends AbstractNonIncrementalAggregationFunction<M, T> {
 
 	private static final long serialVersionUID = 4701984246348134053L;
 
+	public Const() {
+		super(null, new String[] { "v1", "v3" });
+	}
+
 	@Override
 	public Object[] evaluate(Collection<T> elements, T trigger, PointInTime pointInTime) {
-		return new Integer[] { 42 };
+		
+//		FastFourierTransformer fastFourierTransformer = new FastFourierTransformer(DftNormalization.STANDARD);
+//		Complex[] transform = fastFourierTransformer.transform(values.stream().mapToDouble(Double::doubleValue).toArray(), TransformType.FORWARD);
+//		
+//		return Arrays.asList(transform).stream().map(Complex::abs).collect(Collectors.toList());
+		
+		return new Integer[] { 42, 14 };
 	}
 
 	@Override
@@ -26,7 +36,7 @@ public class Const<M extends ITimeInterval, T extends Tuple<M>> extends Abstract
 
 	@Override
 	public Collection<SDFAttribute> getOutputAttributes() {
-		return Collections.singleton(new SDFAttribute(null, "value", SDFDatatype.INTEGER));
+		return Arrays.asList(new SDFAttribute(null, outputAttributeNames[0], SDFDatatype.INTEGER), new SDFAttribute(null, outputAttributeNames[1], SDFDatatype.INTEGER));
 	}
 
 }
