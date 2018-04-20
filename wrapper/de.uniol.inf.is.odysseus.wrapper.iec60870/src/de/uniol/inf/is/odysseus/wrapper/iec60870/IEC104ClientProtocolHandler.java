c4afb36b48e26a8aed413bd6a7b7440b56a0f943
Michael Brand <michael.brand@uni-oldenburg.de>
2018-04-20 11:04:56 +0200

    ODY-1546: updated OJ104 lib
    Former-commit-id: 1fcdcb849e6778fade73987cf8ef78a1df0dcf3e

--- wrapper/de.uniol.inf.is.odysseus.wrapper.iec60870/src/de/uniol/inf/is/odysseus/wrapper/iec60870/IEC104ClientProtocolHandler.java
+++ wrapper/de.uniol.inf.is.odysseus.wrapper.iec60870/src/de/uniol/inf/is/odysseus/wrapper/iec60870/IEC104ClientProtocolHandler.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
