586ca4d2290cf3d8ace9fb9eb3288e2e93e6e588
Michael Brand <michael.brand@uni-oldenburg.de>
2018-04-22 11:06:09 +0200

    ODY-1546: added MEP functions to get InformationElementSequences and ITimeTags
    Former-commit-id: 5e1f56baef0ea099140211bcb36e562aee7fba39

---
+++ wrapper/de.uniol.inf.is.odysseus.wrapper.iec60870/src/de/uniol/inf/is/odysseus/wrapper/iec60870/mep/IEC104MEPFunctionProvider.java
@@ -0,0 +1,32 @@
+package de.uniol.inf.is.odysseus.wrapper.iec60870.mep;
+
+import java.util.Arrays;
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.mep.IMepFunction;
+import de.uniol.inf.is.odysseus.mep.IFunctionProvider;
+
+/**
+ * Provides the MEP functions that depend on the IEC 60870-5-104 protocol
+ * handlers (and the oj104 library). Provided MEP functions are: <br />
+ * <ul>
+ * <li>{@link Get104ElementSequences}</li>
+ * <li>{@link Get104TimeTag}</li>
+ * </ul>
+ *
+ * @author Michael Brand (michael.brand@uol.de)
+ *
+ */
+public class IEC104MEPFunctionProvider implements IFunctionProvider {
+
+	/**
+	 * Instances of the provided MEP functions.
+	 */
+	private static final IMepFunction<?>[] functions = new IMepFunction[] { new Get104ElementSequences(), new Get104TimeTag() };
+
+	@Override
+	public List<IMepFunction<?>> getFunctions() {
+		return Arrays.asList(functions);
+	}
+
+}
