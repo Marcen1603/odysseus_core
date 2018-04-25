7d8ecbc0937972becc804662d882ac3fec6f90a5
Michael Brand <michael.brand@uni-oldenburg.de>
2018-04-25 11:30:04 +0200

    ODY-1546: MEP function to transform a 104 time tag to a long timestamp
    Former-commit-id: d778b0167263c24b27089752f997a18cd2787749

--- wrapper/de.uniol.inf.is.odysseus.wrapper.iec60870/src/de/uniol/inf/is/odysseus/wrapper/iec60870/mep/IEC104MEPFunctionProvider.java
+++ wrapper/de.uniol.inf.is.odysseus.wrapper.iec60870/src/de/uniol/inf/is/odysseus/wrapper/iec60870/mep/IEC104MEPFunctionProvider.java
@@ -13,6 +13,7 @@ import de.uniol.inf.is.odysseus.mep.IFunctionProvider;
  * <li>{@link Get104ElementSequences}</li>
  * <li>{@link Get104TimeTag}</li>
  * <li>{@link Set104TimeTag}</li>
+ * <li>{@link Transform104TimeTagToLong}</li>
  * </ul>
  *
  * @author Michael Brand (michael.brand@uol.de)
@@ -23,7 +24,7 @@ public class IEC104MEPFunctionProvider implements IFunctionProvider {
 	/**
 	 * Instances of the provided MEP functions.
 	 */
-	private static final IMepFunction<?>[] functions = new IMepFunction[] { new Get104ElementSequences(), new Get104TimeTag(), new Set104TimeTag() };
+	private static final IMepFunction<?>[] functions = new IMepFunction[] { new Get104ElementSequences(), new Get104TimeTag(), new Set104TimeTag(), new Transform104TimeTagToLong() };
 
 	@Override
 	public List<IMepFunction<?>> getFunctions() {
