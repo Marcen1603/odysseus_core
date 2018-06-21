dfdef0ce9ceb9b1f9cc902395ba1b5e3f7624883
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2018-06-21 09:08:53 +0200

    ODY-1600: Odysseus Script cleanup, new commands as cql replacement Former-commit-id: 981478590eedf2df1dff09789555b226474f32f7

---
+++ server/script/script.executor/src/de/uniol/inf/is/odysseus/script/keyword/feature/ResetUpdateSitePreparserKeyword.java
@@ -0,0 +1,32 @@
+package de.uniol.inf.is.odysseus.script.keyword.feature;
+
+import java.util.List;
+import java.util.Map;
+
+import de.uniol.inf.is.odysseus.core.collection.Context;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.IServerExecutor;
+import de.uniol.inf.is.odysseus.core.server.planmanagement.executor.command.IExecutorCommand;
+import de.uniol.inf.is.odysseus.core.usermanagement.ISession;
+import de.uniol.inf.is.odysseus.script.parser.AbstractPreParserKeyword;
+import de.uniol.inf.is.odysseus.script.parser.OdysseusScriptException;
+import de.uniol.inf.is.odysseus.updater.FeatureUpdateUtility;
+
+public class ResetUpdateSitePreparserKeyword extends AbstractPreParserKeyword {
+
+	public static final String NAME = "RESETUPDATESITE";
+
+	@Override
+	public void validate(Map<String, Object> variables, String parameter,
+			ISession caller, Context context, IServerExecutor executor)
+			throws OdysseusScriptException {
+
+	}
+
+	@Override
+	public List<IExecutorCommand> execute(Map<String, Object> variables,
+			String parameter, ISession caller, Context context,
+			IServerExecutor executor) throws OdysseusScriptException {
+		FeatureUpdateUtility.clearRepositoryLocation(caller);
+		return null;	
+	}
+}
