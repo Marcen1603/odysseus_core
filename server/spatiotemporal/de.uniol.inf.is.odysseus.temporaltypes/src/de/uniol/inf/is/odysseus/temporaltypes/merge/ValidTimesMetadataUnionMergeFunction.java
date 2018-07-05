637cd1114de7be77240a2fe68245ae7cc92c1d55
Tobias Brandt <tobias.leo.brandt@uni-oldenburg.de>
2018-07-05 16:11:09 +0200

    ODY-1605 Union merge function can work with different base date times
    Former-commit-id: 8b991adc017883aeb8b74e372673d25e261fff9e

--- server/spatiotemporal/de.uniol.inf.is.odysseus.temporaltypes/src/de/uniol/inf/is/odysseus/temporaltypes/merge/ValidTimesMetadataUnionMergeFunction.java
+++ server/spatiotemporal/de.uniol.inf.is.odysseus.temporaltypes/src/de/uniol/inf/is/odysseus/temporaltypes/merge/ValidTimesMetadataUnionMergeFunction.java
@@ -1,7 +1,10 @@
 package de.uniol.inf.is.odysseus.temporaltypes.merge;
 
+import java.util.concurrent.TimeUnit;
+
 import de.uniol.inf.is.odysseus.core.metadata.IInlineMetadataMergeFunction;
 import de.uniol.inf.is.odysseus.core.metadata.IMetaAttribute;
+import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
 import de.uniol.inf.is.odysseus.core.metadata.TimeInterval;
 import de.uniol.inf.is.odysseus.temporaltypes.metadata.IValidTime;
 import de.uniol.inf.is.odysseus.temporaltypes.metadata.IValidTimes;
@@ -23,24 +26,38 @@ public class ValidTimesMetadataUnionMergeFunction implements IInlineMetadataMerg
 
 	private IValidTimes unionMerge(IValidTimes result, IValidTimes inLeft, IValidTimes inRight) {
 
+		TimeUnit smallerTimeUnit = getSmallerTimeUnit(inLeft.getPredictionTimeUnit(), inRight.getPredictionTimeUnit());
+		result.setTimeUnit(smallerTimeUnit);
+
 		for (IValidTime leftValidTime : inLeft.getValidTimes()) {
-			result = mergeInto(leftValidTime, result);
+			result = mergeInto(leftValidTime, inLeft.getPredictionTimeUnit(), result, smallerTimeUnit);
 		}
 
 		for (IValidTime rightValidTime : inRight.getValidTimes()) {
-			result = mergeInto(rightValidTime, result);
+			result = mergeInto(rightValidTime, inRight.getPredictionTimeUnit(), result, smallerTimeUnit);
 		}
 
 		return result;
 	}
 
-	private IValidTimes mergeInto(IValidTime merge, IValidTimes into) {
+	private TimeUnit getSmallerTimeUnit(TimeUnit left, TimeUnit right) {
+		int timeUnitCompare = left.compareTo(right);
+		if (timeUnitCompare == 0) {
+			return left;
+		} else if (timeUnitCompare < 0) {
+			return left;
+		} else {
+			return right;
+		}
+	}
+
+	private IValidTimes mergeInto(IValidTime merge, TimeUnit mergeTimeUnit, IValidTimes into, TimeUnit targetTimeUnit) {
 
 		IValidTime toRemove = null;
 		IValidTime toAdd = null;
 
 		for (IValidTime time : into.getValidTimes()) {
-			IValidTime union = union(merge, time);
+			IValidTime union = union(merge, mergeTimeUnit, time, into.getPredictionTimeUnit(), targetTimeUnit);
 			if (union != null) {
 				toRemove = time;
 				toAdd = union;
@@ -58,9 +75,23 @@ public class ValidTimesMetadataUnionMergeFunction implements IInlineMetadataMerg
 		return into;
 	}
 
-	private IValidTime union(IValidTime left, IValidTime right) {
-		TimeInterval leftInterval = new TimeInterval(left.getValidStart(), left.getValidEnd());
-		TimeInterval rightInterval = new TimeInterval(right.getValidStart(), right.getValidEnd());
+	private IValidTime union(IValidTime left, TimeUnit leftTimeUnit, IValidTime right, TimeUnit rightTimeUnit,
+			TimeUnit targetTimeUnit) {
+
+		long leftTargetStartTime = targetTimeUnit.convert(left.getValidStart().getMainPoint(), leftTimeUnit);
+		PointInTime leftStart = new PointInTime(leftTargetStartTime);
+		
+		long leftTargetEndTime = targetTimeUnit.convert(left.getValidEnd().getMainPoint(), leftTimeUnit);
+		PointInTime leftEnd = new PointInTime(leftTargetEndTime);
+
+		long rightTargetTime = targetTimeUnit.convert(right.getValidStart().getMainPoint(), rightTimeUnit);
+		PointInTime rightStart = new PointInTime(rightTargetTime);
+		
+		long rightTargetEndTime = targetTimeUnit.convert(right.getValidEnd().getMainPoint(), rightTimeUnit);
+		PointInTime rightEnd = new PointInTime(rightTargetEndTime);
+
+		TimeInterval leftInterval = new TimeInterval(leftStart, leftEnd);
+		TimeInterval rightInterval = new TimeInterval(rightStart, rightEnd);
 		TimeInterval unified = ValidTime.union(leftInterval, rightInterval);
 		if (unified == null) {
 			return null;
