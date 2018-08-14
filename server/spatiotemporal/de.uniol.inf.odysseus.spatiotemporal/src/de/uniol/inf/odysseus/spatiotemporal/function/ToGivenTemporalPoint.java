b8a700cd28c62f919c1e4938e106039c784df717
Tobias Brandt <tobias.leo.brandt@uni-oldenburg.de>
2018-08-14 16:32:11 +0200

    ODY-1618 Read timestamps for LineString
    Former-commit-id: 2638174923cef93c2758e97ab706c88b81f74c12

--- server/spatiotemporal/de.uniol.inf.odysseus.spatiotemporal/src/de/uniol/inf/odysseus/spatiotemporal/function/ToGivenTemporalPoint.java
+++ server/spatiotemporal/de.uniol.inf.odysseus.spatiotemporal/src/de/uniol/inf/odysseus/spatiotemporal/function/ToGivenTemporalPoint.java
@@ -1,13 +1,23 @@
 package de.uniol.inf.odysseus.spatiotemporal.function;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import com.github.filosganga.geogson.gson.GeometryAdapterFactory;
 import com.github.filosganga.geogson.jts.JtsAdapterFactory;
+import com.github.filosganga.geogson.model.Feature;
 import com.github.filosganga.geogson.model.FeatureCollection;
+import com.github.filosganga.geogson.model.Geometry;
+import com.github.filosganga.geogson.model.LineString;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.PrecisionModel;
 
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFConstraint;
@@ -46,10 +56,51 @@ public class ToGivenTemporalPoint extends AbstractFunction<TemporalGeometry> {
 
 		String geoJson = this.getInputValue(0);
 		FeatureCollection features = this.gson.fromJson(geoJson, FeatureCollection.class);
+		Feature feature = features.features().get(0);
 
-		TemporalFunction<GeometryWrapper> temporalFunction = new TrajectoryMovingPointFunction();
+		List<Long> timeList = getTimes(feature);
+		List<Point> points = getPoints(feature);
+
+		TemporalFunction<GeometryWrapper> temporalFunction = new TrajectoryMovingPointFunction(timeList, points);
 		TemporalGeometry temporalPoint = new TemporalGeometry(temporalFunction);
 		return temporalPoint;
+	}
+
+	private List<Point> getPoints(Feature feature) {
+		Geometry<?> trajectory = feature.geometry();
+		List<Point> points = new ArrayList<>();
+		if (trajectory instanceof LineString) {
+			LineString lineString = (LineString) trajectory;
+			List<com.github.filosganga.geogson.model.Point> points2 = lineString.points();
+			for (com.github.filosganga.geogson.model.Point gsonPoint : points2) {
+				Coordinate coordinate = new Coordinate(gsonPoint.lat(), gsonPoint.lon());
+				GeometryFactory factory = new GeometryFactory();
+				Point point = factory.createPoint(coordinate);
+				points.add(point);
+			}
+		}
+		return points;
+	}
+
+	private List<Long> getTimes(Feature feature) {
+		/*
+		 * Use the time element similar to the TimeDimension of Leaflet, as it fits best
+		 * to trajectories (LineString):
+		 * https://github.com/socib/Leaflet.TimeDimension/issues/99
+		 */
+		JsonElement jsonElement = feature.properties().get("times");
+		if (!jsonElement.isJsonArray()) {
+			return null;
+		}
+		JsonArray asJsonArray = jsonElement.getAsJsonArray();
+		List<Long> timeList = new ArrayList<>();
+		Iterator<JsonElement> iterator = asJsonArray.iterator();
+		while (iterator.hasNext()) {
+			JsonElement next = iterator.next();
+			long asLong = next.getAsLong();
+			timeList.add(asLong);
+		}
+		return timeList;
 	}
 
 	@Override
--- server/spatiotemporal/de.uniol.inf.odysseus.spatiotemporal/src/de/uniol/inf/odysseus/spatiotemporal/types/point/TrajectoryMovingPointFunction.java
+++ server/spatiotemporal/de.uniol.inf.odysseus.spatiotemporal/src/de/uniol/inf/odysseus/spatiotemporal/types/point/TrajectoryMovingPointFunction.java
@@ -1,13 +1,29 @@
 package de.uniol.inf.odysseus.spatiotemporal.types.point;
 
+import java.util.List;
+
+import com.vividsolutions.jts.geom.Point;
+
 import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
 import de.uniol.inf.is.odysseus.spatial.geom.GeometryWrapper;
 import de.uniol.inf.is.odysseus.temporaltypes.types.TemporalFunction;
 
 public class TrajectoryMovingPointFunction implements TemporalFunction<GeometryWrapper> {
+	
+	private List<Long> timeList;
+	private List<Point> points;
+	
+	public TrajectoryMovingPointFunction(List<Long> timeList, List<Point> points) {
+		this.timeList = timeList;
+		this.points = points;
+	}
 
 	@Override
 	public GeometryWrapper getValue(PointInTime time) {
+		
+		// Search for the correct part
+		
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
