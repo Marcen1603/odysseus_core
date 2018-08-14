3f37a41e11ac5ce1692b92e708760df28a911dae
Tobias Brandt <tobias.leo.brandt@uni-oldenburg.de>
2018-08-14 11:08:51 +0200

    ODY-1618 Read geoJson
    Former-commit-id: 348e92e87d1f36f1554d897cf7ebb07eec5c6dfe

--- server/spatiotemporal/de.uniol.inf.odysseus.spatiotemporal/src/de/uniol/inf/odysseus/spatiotemporal/function/ToGivenTemporalPoint.java
+++ server/spatiotemporal/de.uniol.inf.odysseus.spatiotemporal/src/de/uniol/inf/odysseus/spatiotemporal/function/ToGivenTemporalPoint.java
@@ -1,19 +1,32 @@
 package de.uniol.inf.odysseus.spatiotemporal.function;
 
+import java.util.Collection;
+
 import com.github.filosganga.geogson.gson.GeometryAdapterFactory;
+import com.github.filosganga.geogson.jts.JtsAdapterFactory;
+import com.github.filosganga.geogson.model.FeatureCollection;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.PrecisionModel;
 
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFConstraint;
 import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
 import de.uniol.inf.is.odysseus.mep.AbstractFunction;
+import de.uniol.inf.is.odysseus.spatial.geom.GeometryWrapper;
 import de.uniol.inf.is.odysseus.spatial.sourcedescription.sdf.schema.SDFSpatialDatatype;
+import de.uniol.inf.is.odysseus.temporaltypes.types.TemporalDatatype;
+import de.uniol.inf.is.odysseus.temporaltypes.types.TemporalFunction;
 import de.uniol.inf.odysseus.spatiotemporal.types.point.TemporalGeometry;
+import de.uniol.inf.odysseus.spatiotemporal.types.point.TrajectoryMovingPointFunction;
 
 public class ToGivenTemporalPoint extends AbstractFunction<TemporalGeometry> {
 
 	private static final long serialVersionUID = 7234937168792519636L;
 
 	public static final SDFDatatype[][] accTypes = new SDFDatatype[][] { { SDFDatatype.STRING } };
+
+	private Gson gson;
 
 	// For OSGi
 	public ToGivenTemporalPoint() {
@@ -22,13 +35,26 @@ public class ToGivenTemporalPoint extends AbstractFunction<TemporalGeometry> {
 
 	@Override
 	public TemporalGeometry getValue() {
-		// TODO Auto-generated method stub
-		
-		Gson gson = new GsonBuilder()
-				   .registerTypeAdapterFactory(new GeometryAdapterFactory())
-				   .create();
-		
-		return null;
+
+		// See https://github.com/filosganga/geogson
+		if (this.gson == null) {
+			this.gson = new GsonBuilder()
+					.registerTypeAdapterFactory(
+							new JtsAdapterFactory(new GeometryFactory(new PrecisionModel(100), 4326)))
+					.registerTypeAdapterFactory(new GeometryAdapterFactory()).create();
+		}
+
+		String geoJson = this.getInputValue(0);
+		FeatureCollection features = this.gson.fromJson(geoJson, FeatureCollection.class);
+
+		TemporalFunction<GeometryWrapper> temporalFunction = new TrajectoryMovingPointFunction();
+		TemporalGeometry temporalPoint = new TemporalGeometry(temporalFunction);
+		return temporalPoint;
+	}
+
+	@Override
+	public Collection<SDFConstraint> getConstraintsToAdd() {
+		return TemporalDatatype.getTemporalConstraint();
 	}
 
 }
---
+++ server/spatiotemporal/de.uniol.inf.odysseus.spatiotemporal/src/de/uniol/inf/odysseus/spatiotemporal/types/point/TrajectoryMovingPointFunction.java
@@ -0,0 +1,15 @@
+package de.uniol.inf.odysseus.spatiotemporal.types.point;
+
+import de.uniol.inf.is.odysseus.core.metadata.PointInTime;
+import de.uniol.inf.is.odysseus.spatial.geom.GeometryWrapper;
+import de.uniol.inf.is.odysseus.temporaltypes.types.TemporalFunction;
+
+public class TrajectoryMovingPointFunction implements TemporalFunction<GeometryWrapper> {
+
+	@Override
+	public GeometryWrapper getValue(PointInTime time) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
