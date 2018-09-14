41b8df3cf88977ee1d09bad9a4a00a35eefd323d
Tobias Brandt <tobias.leo.brandt@uni-oldenburg.de>
2018-09-14 14:47:59 +0200

    ODY-1635 Explanatory comment for marker interface

--- temporaltypes/server/de.uniol.inf.is.odysseus.temporaltypes/src/de/uniol/inf/is/odysseus/temporaltypes/function/RemoveTemporalFunction.java
+++ temporaltypes/server/de.uniol.inf.is.odysseus.temporaltypes/src/de/uniol/inf/is/odysseus/temporaltypes/function/RemoveTemporalFunction.java
@@ -1,5 +1,12 @@
 package de.uniol.inf.is.odysseus.temporaltypes.function;
 
+/**
+ * A marker interface. A class that implements this interface takes a temporal
+ * input and produces a non-temporal output.
+ * 
+ * @author Tobias Brandt
+ *
+ */
 public interface RemoveTemporalFunction {
 
 }
