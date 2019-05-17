720d98dbaee1cee8ae7aded8a8a878600e076698
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2019-05-17 14:01:45 +0200

    ODY-1768: Connectors for plan inputs

---
+++ server/core/src/de/uniol/inf/is/odysseus/core/server/logicaloperator/ConnectorAO.java
@@ -0,0 +1,33 @@
+package de.uniol.inf.is.odysseus.core.server.logicaloperator;
+
+import de.uniol.inf.is.odysseus.core.logicaloperator.LogicalOperatorCategory;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.LogicalOperator;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.Parameter;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.StringParameter;
+
+@LogicalOperator(name = "Connector", minInputPorts = 0, maxInputPorts = Integer.MAX_VALUE, category = {
+		LogicalOperatorCategory.PLAN }, doc = "This operator serves as a representer. Other plans can connect to this Connector")
+public class ConnectorAO extends AbstractAccessAO{
+
+	private static final long serialVersionUID = -2867000369059509011L;
+
+	public ConnectorAO() {
+		setWrapper("GenericPush");
+		setTransportHandler("none");
+	}
+
+	public ConnectorAO(ConnectorAO connectorAO) {
+		super(connectorAO);
+	}
+
+	@Parameter(type = StringParameter.class, name = "Type", isList = false, optional = true, doc = "What kind of object are delivered. Default is 'tuple'")
+	public void setType(String type) {
+		super.setDataHandler(type);
+	}
+
+	@Override
+	public AbstractLogicalOperator clone() {
+		return new ConnectorAO(this);
+	}
+
+}
---
+++ server/core/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/ConnectorPO.java
@@ -0,0 +1,6 @@
+package de.uniol.inf.is.odysseus.core.server.physicaloperator;
+
+// Use MergePO as connector but provide another name
+public class ConnectorPO extends MergePO {
+
+}
