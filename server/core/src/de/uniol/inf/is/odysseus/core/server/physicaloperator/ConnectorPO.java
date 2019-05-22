c528f3765fe63cf89871c5b0624fc25d4ab45dd0
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2019-05-22 09:50:25 +0200

    ODY-1768: JavaDoc

--- server/core/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/ConnectorPO.java
+++ server/core/src/de/uniol/inf/is/odysseus/core/server/physicaloperator/ConnectorPO.java
@@ -1,6 +1,12 @@
 package de.uniol.inf.is.odysseus.core.server.physicaloperator;
 
-// Use MergePO as connector but provide another name
+/**
+ * This physical operator can be used to connect other operators in an already defined query. As an addition to MergePO this operator
+ * has a port number. It is typically used in SubQueryPO to define, which inputs of the SubQueryPO relate to which input in the subplan
+ * 
+ * @author Marco Grawunder
+ *
+ */
 public class ConnectorPO extends MergePO {
 	
 	private final int port;
