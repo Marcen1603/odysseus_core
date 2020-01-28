3fb8c3772b2fa215deb8fb11abc7617d806e48e3
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2020-01-28 10:11:13 +0100

    ODY-1778: Replaced boolean by separator

--- common/mep/src/de/uniol/inf/is/odysseus/mep/functions/list/ListToStringFunction2.java
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/functions/list/ListToStringFunction2.java
@@ -8,25 +8,25 @@ import de.uniol.inf.is.odysseus.mep.AbstractFunction;
 public class ListToStringFunction2 extends AbstractFunction<String> {
 
 	private static final long serialVersionUID = 8703882849717127378L;
-	private static final SDFDatatype[][] ACC_TYPES = new SDFDatatype[][] { SDFDatatype.getLists(), {SDFDatatype.BOOLEAN}};
+	private static final SDFDatatype[][] ACC_TYPES = new SDFDatatype[][] { SDFDatatype.getLists(),
+			{ SDFDatatype.STRING } };
 
-    public ListToStringFunction2() {
-        super("toString", 2, ACC_TYPES, SDFDatatype.STRING, false);
-    }
-	
+	public ListToStringFunction2() {
+		super("toString", 2, ACC_TYPES, SDFDatatype.STRING, false);
+	}
+
 	@Override
 	public String getValue() {
-        List<?> list = (List<?>) getInputValue(0);
-        boolean flatten = getInputValue(1);
-        if (flatten) {
-        	StringBuilder res = new StringBuilder();
-        	for (Object o:list) {
-        		res.append(o);
-        	}
-        	return res.toString();
-        }
-        
-		return list.toString();
+		List<?> list = (List<?>) getInputValue(0);
+		String sep = getInputValue(1);
+		StringBuilder res = new StringBuilder();
+		if (!list.isEmpty()) {
+			res.append(list.get(0));
+			for (int i = 1; i < list.size(); i++) {
+				res.append(sep).append(list.get(i));
+			}
+		}
+		return res.toString();
 	}
 
 }
