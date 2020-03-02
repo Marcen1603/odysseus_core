6d0030e56b53d3704e2d29a770bd5629492abdd3
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2020-03-02 16:25:29 +0100

    Sum function for lists with tuples

---
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/functions/list/ListSumFunction2.java
@@ -0,0 +1,37 @@
+package de.uniol.inf.is.odysseus.mep.functions.list;
+
+import java.util.List;
+
+import de.uniol.inf.is.odysseus.core.collection.Tuple;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFDatatype;
+import de.uniol.inf.is.odysseus.mep.AbstractFunction;
+
+public class ListSumFunction2 extends AbstractFunction<Double> {
+
+	private static final long serialVersionUID = -3282877303737235603L;
+
+	private static final SDFDatatype[][] accTypes = new SDFDatatype[][] { {SDFDatatype.LIST_TUPLE}, SDFDatatype.DISCRETE_NUMBERS};
+
+	public ListSumFunction2() {
+		super("sum", 2, accTypes, SDFDatatype.DOUBLE, false);
+	}
+
+	@Override
+	public Double getValue() { 
+		List<Tuple<?>> l = getInputValue(0);
+		int pos = getNumericalInputValue(1).intValue();
+		
+		if(l == null) {
+			return null;
+		}
+		double result = 0;
+		for (Tuple<?> v:l){
+			if(v != null) {
+				result += ((Number)v.getAttribute(pos)).doubleValue();
+			}
+		}
+		return result;
+	}
+		
+
+}
