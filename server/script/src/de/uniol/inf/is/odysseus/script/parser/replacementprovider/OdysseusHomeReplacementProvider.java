034eee762e8ed36c41104c5a65222a3005f1693b
tbrandt <tobias.brandt@offis.de>
2020-03-05 10:08:57 +0100

    ODY-1920 Get autostart path from autostart bundle

--- server/script/src/de/uniol/inf/is/odysseus/script/parser/replacementprovider/OdysseusHomeReplacementProvider.java
+++ server/script/src/de/uniol/inf/is/odysseus/script/parser/replacementprovider/OdysseusHomeReplacementProvider.java
@@ -1,27 +1,25 @@
 package de.uniol.inf.is.odysseus.script.parser.replacementprovider;
 
-import java.io.File;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import de.uniol.inf.is.odysseus.core.server.OdysseusConfiguration;
 import de.uniol.inf.is.odysseus.script.parser.IReplacementProvider;
+import de.uniol.inf.is.odysseus.server.autostart.Autostart;
 
 public class OdysseusHomeReplacementProvider implements IReplacementProvider {
 
 	private Map<String, String> replacements;
 	private static final String ODYSSEUS_HOME = "odysseusHome";
-	private static final String ODYSSEUS_AUTOSTART_FOLDER = "odysseusAutostartFolder";
+	private static final String ODYSSEUS_AUTOSTART_DIRECTORY = "odysseusAutostartDirectory";
 	private static final String ODYSSEUS_AUTOSTART_FILE = "odysseusAutostartFile";
 
 	public OdysseusHomeReplacementProvider() {
 		replacements = new HashMap<String, String>(2);
 		replacements.put(ODYSSEUS_HOME.toUpperCase(), OdysseusConfiguration.instance.getHomeDir());
-		replacements.put(ODYSSEUS_AUTOSTART_FOLDER.toUpperCase(),
-				OdysseusConfiguration.instance.getHomeDir() + "/autostart" + File.separator);
-		replacements.put(ODYSSEUS_AUTOSTART_FILE.toUpperCase(),
-				OdysseusConfiguration.instance.getHomeDir() + "/autostart" + File.separator + "autostart.qry");
+		replacements.put(ODYSSEUS_AUTOSTART_DIRECTORY.toUpperCase(), Autostart.getAutostartDirectoryPath());
+		replacements.put(ODYSSEUS_AUTOSTART_FILE.toUpperCase(), Autostart.getAutostartQueryFilePath());
 	}
 
 	@Override
