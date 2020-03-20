7628081fae5edeee447d791c69c92f6b7e90039b
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2020-03-20 09:01:09 +0100

    Extended sort for lists

--- common/mep/src/de/uniol/inf/is/odysseus/mep/functions/list/ListSort.java
+++ common/mep/src/de/uniol/inf/is/odysseus/mep/functions/list/ListSort.java
@@ -2,6 +2,7 @@ package de.uniol.inf.is.odysseus.mep.functions.list;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import de.uniol.inf.is.odysseus.core.mep.IMepExpression;
@@ -28,11 +29,34 @@ public class ListSort extends AbstractFunction<List> {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List getValue() {
+		List inputList = (List)getInputValue(0);
+		if (inputList.isEmpty()) {
+			return new ArrayList((List<?>) getInputValue(0));
+		}
 		// Copy list, else input will be sorted!
-		List l = new ArrayList( (List<?>) getInputValue(0));
+		List l = new ArrayList( inputList);
 
-		Collections.sort(l);
-
+		// Default behaviour
+		if (inputList.get(0) instanceof Comparable) {
+			Collections.sort(l);
+		}else if (inputList.get(0) instanceof ArrayList) {
+			// Hopefully, the ArrayList contains comparable objects
+			// else there will be an exception
+			Collections.sort(l, new Comparator<ArrayList<Comparable>>() {
+				public int compare(ArrayList<Comparable> o1, ArrayList<Comparable>  o2) {
+					int min = Math.min(o1.size(), o2.size());
+					int compare = 0;
+					for (int i=0;i<min;i++) {
+						Comparable left = o1.get(i);
+						Comparable right = o2.get(i);
+						compare = left.compareTo(right);
+					}
+					return compare;
+				};
+			});
+		}
+		
+		
 		return l;
 	}
 
